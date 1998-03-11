3ac212961fe11154dc1af8929f98f0b085240097
anton <anton>
1998-03-11 14:01:44 +0000

    added calc of normalized surface area per residue; added -v to reduce output; made 'surface.dat' an explicit library file; made all solv.acc.surf. related files optional, solv.acc.surf. is only calc-ed if necessary

--- src/tools/do_dssp.c
+++ src/tools/do_dssp.c
@@ -46,7 +46,8 @@ static char *SRCID_do_dssp_c = "$Id$";
 
 static void strip_dssp(char *dsspfile,int nres,int r0,
 		       bool bPhobres[],real t,real dt,
-		       FILE *ss,FILE *acc,FILE *acct,t_matrix *mat)
+		       FILE *ss,FILE *acc,FILE *acct,
+		       t_matrix *mat,int average_area[])
 {
   static bool bFirst=TRUE;
   static char *ssbuf;
@@ -75,7 +76,9 @@ static void strip_dssp(char *dsspfile,int nres,int r0,
     
     buf[39]='\0';
     sscanf(&(buf[34]),"%d",&iacc);
+    if (acc)
     fprintf(acc,"%4d ",iacc);
+    average_area[nr]+=iacc;
     if (bPhobres[nr])
       iaccb+=iacc;
     else
@@ -83,15 +86,10 @@ static void strip_dssp(char *dsspfile,int nres,int r0,
   }
   ssbuf[nr]='\0';
   
-  
+  if (ss)
   fprintf(ss,"%10g  & {\\tt %s } \\\\\n",t,ssbuf);
   
   if (bFirst) {
-    /* We can only write the header here, because DSSP may not recognize
-     * all the residues that we have (such as ACE, NAC, etc)
-     * And therefore the number of residues from the topology is not
-     * the same as the one we have here.
-     */
     sprintf(mat->title,"Secondary structure");
     mat->legend[0]=0;
     sprintf(mat->label_x,"Time (ps)");
@@ -122,7 +120,9 @@ static void strip_dssp(char *dsspfile,int nres,int r0,
   frame++;
   mat->nx=frame;
   
+  if (acc)
   fprintf(acc,"\n");
+  if (acct)
   fprintf(acct,"%10g  %10g  %10g\n",t,0.01*iaccb,0.01*iaccf);
   fclose(in);
 }
@@ -156,6 +156,32 @@ static void check_oo(t_atoms *atoms)
   }
 }
 
+static void norm_acc(char *surffn, t_atoms *atoms, 
+		     real av_area[], real norm_av_area[])
+{
+  int i,n,n_surf;
+  
+  char **surf_res, **surf_lines;
+  double *surf;
+  
+  n_surf = get_lines(surffn, &surf_lines);
+  snew(surf, n_surf);
+  snew(surf_res, n_surf);
+  for(i=0; (i<n_surf); i++) {
+    snew(surf_res[i], 5);
+    sscanf(surf_lines[i],"%s %lf",surf_res[i],&surf[i]);
+  }
+  
+  for(i=0; (i<atoms->nres); i++) {
+    n = search_str(n_surf,surf_res,*atoms->resname[i]);
+    if ( n != -1)
+      norm_av_area[i] = av_area[i] / surf[n];
+    else 
+      fprintf(stderr,"Residue %s not found in surface database (%s)\n",
+	      *atoms->resname[i],surffn);
+  }
+}
+
 int main(int argc,char *argv[])
 {
   static char *desc[] = {
@@ -164,16 +190,19 @@ int main(int argc,char *argv[])
     "calling the dssp program. If you do not have the dssp program,",
     "get it. do_dssp assumes that the dssp executable is in",
     "/home/mdgroup/dssp/dssp. If that is not the case, then you should",
-    "set an environment variable [BB]DSSP[bb] as in: [PAR]",
+    "set an environment variable [BB]DSSP[bb] pointing to the dssp",
+    "executable as in: [PAR]",
     "[TT]setenv DSSP /usr/local/bin/dssp[tt][PAR]",
-    "where the right hand side should point to the dssp executable.[PAR]",
     "The output of dssp may be visualized, as a table in [BB]LaTeX[bb],",
     "as an X PixMap or as color postscript using a postprocessing program",
     "called xpm2ps."
   };
   static int  r0=1;
   static real dt=0.0;
+  static bool bVerbose;
   t_pargs pa[] = {
+    { "-v",  FALSE, etBOOL, &bVerbose,
+      "Generate miles of useless information" },
     { "-dt", FALSE, etREAL, &dt,
       "Time interval between frames." },
     { "-r0", FALSE, etINT,  &r0,
@@ -189,7 +218,7 @@ int main(int argc,char *argv[])
   t_atoms    *atoms;
   t_matrix   mat;
   int        nres,nr0;
-  bool       *bPhbres;
+  bool       *bPhbres,bDoAcc;
   real       t,nt;
   int        i,natoms,nframe=0;
   matrix     box;
@@ -197,6 +226,8 @@ int main(int argc,char *argv[])
   char       *grpnm;
   atom_id    *index;
   rvec       *x;
+  int        *average_area;
+  real       *av_area, *norm_av_area;
   char       pdbfile[L_tmpnam],tmpfile[L_tmpnam];
   char       dssp[256],*dptr;
   t_filenm   fnm[] = {
@@ -204,10 +235,12 @@ int main(int argc,char *argv[])
     { efTPX, NULL,   NULL,      ffREAD },
     { efNDX, NULL,   NULL,      ffOPTRD },
     { efMAP, "-map", "ss",      ffLIBRD },
-    { efTEX, "-os",  "ss",      ffWRITE },
-    { efOUT, "-oa",  "area",    ffWRITE },
-    { efXVG, "-a",   "totarea", ffWRITE },
-    { efXPM, "-ss",  "ss",      ffWRITE }
+    { efDAT, "-sf",  "surface", ffLIBRD },
+    { efXPM, "-ss",  "ss",      ffWRITE },
+    { efTEX, "-os",  "ss",      ffOPTWR },
+    { efOUT, "-oa",  "area",    ffOPTWR },
+    { efXVG, "-a",   "totarea", ffOPTWR },
+    { efXVG, "-aa",  "averarea",ffOPTWR }
   };
 #define NFILE asize(fnm)
 
@@ -215,6 +248,9 @@ int main(int argc,char *argv[])
   parse_common_args(&argc,argv,PCA_CAN_TIME | PCA_CAN_VIEW,TRUE,
 	 	    NFILE,fnm,asize(pa),pa,asize(desc),desc,asize(bugs),bugs);
 		    
+  bDoAcc=(ftp2bSet(efOUT,NFILE,fnm) || 
+	  opt2bSet("-a" ,NFILE,fnm) || 
+	  opt2bSet("-aa",NFILE,fnm));
   top=read_top(ftp2fn(efTPX,NFILE,fnm));
   atoms=&(top->atoms);
   check_oo(atoms);
@@ -236,31 +272,46 @@ int main(int argc,char *argv[])
   (void) tmpnam(tmpfile);
   if ((dptr=getenv("DSSP")) == NULL)
     dptr="/home/mdgroup/dssp/dssp";
-  sprintf(dssp,"%s %s %s > /dev/null",dptr,pdbfile,tmpfile);
-  fprintf(stderr,"dssp cmd='%s'\n",dssp);
+  sprintf(dssp,"%s %s %s %s > /dev/null %s",
+	  dptr,bDoAcc?"":"-na",pdbfile,tmpfile,bVerbose?"":"2> /dev/null");
+  if (bVerbose)
+    fprintf(stderr,"dssp cmd='%s'\n",dssp);
   
-  ss=opt2FILE("-os",NFILE,fnm,"w");
-  acc=opt2FILE("-oa",NFILE,fnm,"w");
-  acct=xvgropen(ftp2fn(efXVG,NFILE,fnm),"Solvent Accessible Surface Area",
+  if (ftp2bSet(efOUT,NFILE,fnm))
+    acc=ftp2FILE(efOUT,NFILE,fnm,"w");
+  else
+    acc=NULL;
+    
+  if (opt2bSet("-a",NFILE,fnm)) {
+    acct=xvgropen(opt2fn("-a",NFILE,fnm),"Solvent Accessible Surface Area",
 		"Time (ps)","Area (nm\\S2\\N)");
+    xvgr_legend(acct,2,leg);
+  } else
+    acct=NULL;
+  
+  if (ftp2bSet(efTEX,NFILE,fnm))
+    ss=ffopen(ftp2fn(efTEX,NFILE,fnm),"w");
+  else
+    ss=NULL;
+  
   mat.map=NULL;
   mat.nmap=getcmap(libopen(opt2fn("-map",NFILE,fnm)),
 		   opt2fn("-map",NFILE,fnm),&(mat.map));
-  
-  xvgr_legend(acct,2,leg);
-  
+
   natoms=read_first_x(&status,ftp2fn(efTRX,NFILE,fnm),&t,&x,box);
   if (natoms != atoms->nr) 
     fatal_error(0,"Trajectory does not match topology!");
   
+  snew(average_area,atoms->nres+10);
+  snew(av_area,atoms->nres+10);
+  snew(norm_av_area,atoms->nres+10);
   nt=t;
   do {
     if (t >= nt) {
-      fprintf(stderr,"\rt=%.2f",t);
       rm_pbc(&(top->idef),atoms->nr,box,x,x);
       write_pdb_conf_indexed(pdbfile,atoms,x,box,gnx,index);
       system(dssp);
-      strip_dssp(tmpfile,nres,r0,bPhbres,t,dt,ss,acc,acct,&mat);
+      strip_dssp(tmpfile,nres,r0,bPhbres,t,dt,ss,acc,acct,&mat,average_area);
       remove(tmpfile);
       remove(pdbfile);
       nt+=dt;
@@ -269,17 +320,37 @@ int main(int argc,char *argv[])
   } while(read_next_x(status,&t,natoms,x,box));
   fprintf(stderr,"\n");
   close_trj(status);
-  fclose(ss);
-  fclose(acc);
-  fclose(acct);
-  ss=ffopen(opt2fn("-ss",NFILE,fnm),"w");
+  if (ss)
+    ffclose(ss);
+  if (acc)
+    ffclose(acc);
+  if (acct)
+    ffclose(acct);
+  
+  ss=ffopen(ftp2fn(efXPM,NFILE,fnm),"w");
   write_xpm_m(ss,mat);
   ffclose(ss);
   remove(tmpfile);
   remove(pdbfile);
   
-  xvgr_file(ftp2fn(efXVG,NFILE,fnm),"-nxy");
-  xvgr_file(opt2fn("-ss",NFILE,fnm),NULL);
+  for(i=0; (i<atoms->nres); i++)
+    av_area[i] = average_area[i]/(real) nframe;
+    
+  norm_acc(opt2fn("-sf",NFILE,fnm), atoms, av_area, norm_av_area);
+  
+  if (opt2bSet("-aa",NFILE,fnm)) {
+    acc=xvgropen(opt2fn("-aa",NFILE,fnm),"Average Accessible Area",
+		 "Residue","A\\S2");
+    for(i=0; (i<atoms->nres); i++) {
+      fprintf(acc,"%5d  %10g %10g\n",i+r0,av_area[i], norm_av_area[i]);
+    }
+    ffclose(acc);
+  }
+  
+  if (opt2bSet("-a",NFILE,fnm))
+    xvgr_file(opt2fn("-a",NFILE,fnm),"-nxy");
+  if (opt2bSet("-aa",NFILE,fnm))
+    xvgr_file(opt2fn("-aa",NFILE,fnm),NULL);
   
   thanx(stdout);
   
