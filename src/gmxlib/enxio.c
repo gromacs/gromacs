598371579b260e7b456bace960b2830e1366cbb8
spoel <spoel>
1998-04-29 12:17:09 +0000

    Fixed a bug in edr_nms, which led to a read overflow when writing energy names

--- src/gmxlib/enxio.c
+++ src/gmxlib/enxio.c
@@ -58,8 +58,8 @@ static void edr_nms(XDR *xdr,int *nre,char ***nms)
   if (NM == NULL) {
     snew(NM,*nre);
   }
-  for(i=0; (i<*nre); i++)
-    xdr_string(xdr,&(NM[i]),STRLEN);
+  for(i=0; (i<*nre); i++) 
+    xdr_string(xdr,&(NM[i]),NM[i] ? strlen(NM[i]) : STRLEN);
   *nms=NM;
 }
 
