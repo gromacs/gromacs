9c895ff4b05bbc64bc6d92b3abfbea0015fef0a6
anton <anton>
1998-05-13 07:41:26 +0000

    updated to new io routines (enxio), added some options, MAJOR REWRITE OF CODE

--- src/tools/eneconv.c
+++ src/tools/eneconv.c
@@ -29,28 +29,44 @@
 static char *SRCID_eneconv_c = "$Id$";
 
 #include <string.h>
+#include <math.h>
 #include "string2.h"
 #include "typedefs.h"
 #include "smalloc.h"
-#include "enerio.h"
 #include "statutil.h"
 #include "disre.h"
 #include "names.h"
 #include "copyrite.h"
 #include "macros.h"
+#include "enxio.h"
 
 typedef struct {
-  real     t,tstart;
-  t_energy *e;
+  real      t,tstart;
+  t_energy  *e;
+  t_drblock *dr;
 } t_fixe;
 
-static void set_fixe(t_fixe *fixe,real t,real tstart,
-		     int nre,t_energy ee[])
+bool bRmod(double a,double b)
+{
+  int iq;
+  double tol = 1e-6;
+  
+  iq = ((1.0+tol)*a)/b;
+  
+  if (fabs(a-b*iq) <= tol*a)
+    return TRUE;
+  else
+    return FALSE;
+}
+
+static void set_fixe(t_fixe *fixe, real t, real tstart,
+		     int nre, t_energy ee[], t_drblock *dr)
 {
   fixe->t      = t;
   fixe->tstart = tstart;
   snew(fixe->e,nre);
   memcpy(fixe->e,ee,sizeof(ee[0])*nre);
+  fixe->dr=dr;
 }
 
 int cmpfix(const void *a,const void *b)
@@ -79,6 +95,16 @@ int cmpfix(const void *a,const void *b)
   }
 }
 
+bool bRgt(double a,double b)
+{
+  double tol = 1e-6;
+  
+  if ( a > (b - tol*(a+b)) )
+    return TRUE;
+  else
+    return FALSE;
+}
+
 void analyse_energies(int nre,int nrf,t_fixe f[])
 {
   real     tstart,t;
@@ -99,7 +125,7 @@ void analyse_energies(int nre,int nrf,t_fixe f[])
       if (f[i].tstart != tstart) {
 	tstart = f[i].tstart;
 	/* Walk back until we have the same time */
-	while ((i > 0) && (f[i-1].t > t)) {
+	while ((i > 0) && bRgt(f[i-1].t,t) ) {
 	  /* Set time to -1 to indicate that this should not be stored */
 	  f[i-1].t = -1;
 	  nrkill ++;
@@ -134,204 +160,169 @@ void analyse_energies(int nre,int nrf,t_fixe f[])
 int main(int argc,char *argv[])
 {
   static char *desc[] = {
-    "with only [TT]-o[tt] specified: ",
+    "with only [TT]-o[tt] specified:[BR]",
     "repairs energy files, by checking the contents and",
     "concatenating the files in the right order. In case of double", 
     "time frames one of these is skipped.",
     "The input files are taken from the command line, such that the command",
     "[TT]fix_ene -o fixed.ene *.ene[tt] should do the trick.",
+    "(note that in this case the -o option should come first and ",
+    "should include a filename)",
     "[PAR]",
     "With [TT]-f[tt] and [TT]-od[tt] specified: converts edr to ene.",
     "[PAR]",
     "With [TT]-fd[tt] and [TT]-o[tt] specified: converts ene to edr."
   };
+  
+  static char *bugs[] = {
+    "Will use rediculous amounts of memory for large files, which "
+    "slows things down."
+  };
 
-  FILE     *in,*out;
-  t_energy *ee;
-  int      step,nrt,nre,nresav;
-  real     t,tsav,tstart;
-  bool     bSetTime,bF,bFD,bO,bOD,bFix,bEne2Edr,bEdr2Ene;
-  char     **fnms,fn[STRLEN];
-  int      nfile;
-  t_fixe   *fixe=NULL;
-  int      *set,i,j;
-  char     **enm;
+  int       in,out;
+  t_energy  *ee=NULL;
+  int       step,nrt,nre,nresav;
+  real      t,tsav,tstart;
+  bool      bSetTime;
+  char      **fnms,fn[STRLEN];
+  int       nfile;
+  t_fixe    *fixe=NULL;
+  int       *set,i,j;
+  char      **enm=NULL;
+  t_drblock *dr=NULL;
+
   t_filenm fnm[] = {
-    { efENE, "-f", NULL,    ffOPTRD },
-    { efEDR, "-fd", NULL,   ffOPTRD },
-    { efENE, "-o", "fixed", ffOPTWR },
-    { efEDR, "-od","fixed", ffOPTWR }
+    { efENX, "-f", NULL,    ffREAD  },
+    { efENX, "-o", "fixed", ffOPTWR },
   };
 #define NFILE asize(fnm)
-  static real t0=-1, dt=-1;
+  
+  static real t0=-1, timestep=-1, delta_t=0.0, toffset=0;
   t_pargs pa[] = {
-    { "-t",  FALSE, etREAL, &t0, "starting time" },
-    { "-dt", FALSE, etREAL, &dt, "timestep" }
+    { "-dt",       FALSE,  etREAL, &delta_t,
+      "only write out frame when (t MOD delta_t) == offset" },
+    { "-offset",   FALSE, etREAL, &toffset,
+      "time offset for -dt option" },
+    { "-t0",       FALSE, etREAL, &t0, 
+      "change starting time" },
+    { "-timestep", FALSE, etREAL, &timestep, 
+      "change timestep between frames" }
   };
   
   CopyRight(stderr,argv[0]);
-  parse_common_args(&argc,argv,PCA_NOEXIT_ON_ARGS,
-		    FALSE,NFILE,fnm,asize(pa),pa,asize(desc),desc,0,NULL);
+  parse_common_args(&argc,argv,PCA_NOEXIT_ON_ARGS,FALSE,
+		    NFILE,fnm,asize(pa),pa,asize(desc),desc,asize(bugs),bugs);
   
-  bSetTime=((t0 != -1) || (dt != -1));
-  bF =opt2bSet("-f",NFILE,fnm);
-  bFD=opt2bSet("-fd",NFILE,fnm);
-  bO =opt2bSet("-o",NFILE,fnm);
-  bOD=opt2bSet("-od",NFILE,fnm);
-  if (bF && bFD) fatal_error(0,"conflicting arguments: -f -fd");
-  if (bO && bOD) fatal_error(0,"conflicting arguments: -o -od");
-  bFix=bO && !( bOD || bF || bFD );
-  bEne2Edr=!bFix && bF && bOD;
-  bEdr2Ene=( !bFix && !bEne2Edr && bFD && bO );
-  if (bFix) {
+  snew(fnms,argc-1);
+  nfile=0;
+  bSetTime=FALSE;
+  for(i=1; (i<argc); i++)
+    if (strcmp(fn,argv[i]) != 0)
+      fnms[nfile++]=argv[i];
+  if (nfile) {
+    fprintf(stderr,"Files to fix:");
+    for (i=0; (i<nfile); i++)
+      fprintf(stderr,"%s ",fnms[i]);
+    fprintf(stderr,"\n");
+  }
+  bSetTime=((t0 != -1) || (timestep != -1));
+  
+  if (!opt2bSet("-f",NFILE,fnm)) {
   
     strcpy(fn,opt2fn("-o",NFILE,fnm));
-    out=ffopen(fn,"w");
+    out=open_enx(fn,"w");
     
     nresav=0;
     nrt=0;
     
-    snew(fnms,argc-1);
-    nfile=0;
-    bSetTime=FALSE;
-    for(i=1; (i<argc); i++) {
-      if (strcmp(fn,argv[i]) == 0) {
-	fprintf(stderr,"Skipping %s\n",fn);
-	continue;
-      }
-      else {
-	fnms[nfile++]=argv[i];
-      }
-    }
     if (nfile==0)
       fatal_error(0,"No input files to fix!");
-    if (bSetTime && (nfile != 1))
-      fatal_error(0,"Can only set the time for one file at a time!");
     
     for(i=0; (i<nfile); i++) {
-      in=ffopen(fnms[i],"r");
+      in = open_enx(fnms[i],"r");
       fprintf(stderr,"Opened %s\n",fnms[i]);
-      rd_ener_nms(in,&nre,&enm);
+      do_enxnms(in,&nre,&enm);
       tstart=-1;
-      if (nresav == 0) {
-	wr_ener_nms(out,nre,enm);
+      if (i == 0) {
+	do_enxnms(out,&nre,&enm);
 	nresav=nre;
 	snew(ee,nre);
       }
       else if (nre != nresav)
 	fatal_error(0,"Energy files don't match, different number"
-		    " of energies");
-      while (rd_ener(in,&t,&step,ee,NULL)) {
+		    " of energies (%s)",fnms[i]);
+      step=0;
+      while (do_enx(in,&t,&step,&nre,ee,dr)) {
 	fprintf(stderr,"\rRead frame %d, time %g",nrt,t);
 	if (tstart == -1)
 	  tstart = t;
 	srenew(fixe,++nrt);
-	set_fixe(&fixe[nrt-1],t,tstart,nre,ee);
+	set_fixe(&fixe[nrt-1],t,tstart,nre,ee,dr);
       }
-      fclose(in);
+      close_enx(in);
       fprintf(stderr,"\n");
     }
-    fprintf(stderr,"Analysing energies...\n");
+    fprintf(stderr,"Sorting energies...\n");
     qsort(fixe,nrt,sizeof(fixe[0]),cmpfix);
+    fprintf(stderr,"Analysing energies...\n");
     analyse_energies(nre,nrt,fixe);
     
     tsav=-1;
     j=0;
-    if ((t0==-1) && (dt>0))
+    if ((t0==-1) && (timestep>0))
       t0=tstart;
     fprintf(stderr,"Now writing %s...\n",fn);
     for(i=0; (i<nrt); i++) { 
       if (bSetTime) {
-	if (dt>0)
-	  fixe[i].t=t0+j*dt;
+	if (timestep>0)
+	  fixe[i].t=t0+j*timestep;
 	else if (t0>=0) 
 	  fixe[i].t += (t0-tstart);
       }
-      if ( bSetTime || (fixe[i].t != tsav) ) {
-	wr_ener(out,fixe[i].t,i,nre,fixe[i].e,NULL);
+      if ( ( bRmod(fixe[i].t-toffset,delta_t) || (delta_t==0) ) && 
+	   ( bSetTime || ( fixe[i].t != tsav ) ) ) {
+	do_enx(out,&(fixe[i].t),&i,&nre,fixe[i].e,fixe[i].dr);
 	fprintf(stderr,"\rWrite frame %d, t %f",j,fixe[i].t);
 	j++;
       }
       tsav=fixe[i].t;
     }
-    fclose(out);
+    close_enx(out);
     fprintf(stderr,"\n");
-    
-  } else if (bEne2Edr) {
-    FILE      *in;
-    t_energy  *ee;
-    t_drblock *dr;
-    XDR       xdr;
-    int       nre,j;
-    real      t;
-    char      **enm;
-    
-    in=ftp2FILE(efENE,NFILE,fnm,"r");
+  } else {
+    in = open_enx(opt2fn("-f",NFILE,fnm),"r");
     
     dr=get_drblock();
-    rd_ener_nms(in,&nre,&enm);
+    do_enxnms(in,&nre,&enm);
     snew(ee,nre);
-    xdropen(&xdr,ftp2fn(efEDR,NFILE,fnm),"w");
-    edr_nms(&xdr,&nre,&enm);
+    
+    out = open_enx(opt2fn("-o",NFILE,fnm),"w");
+    do_enxnms(out,&nre,&enm);
     
     tstart=-1;
-    while (rd_ener(in,&t,&j,ee,dr))  {
+    i=0;
+    while (do_enx(in,&t,&j,&nre,ee,dr))  {
       if (tstart==-1) {
 	tstart=t;
-	if ((t0==-1) && (dt>0))
+	if ((t0==-1) && (timestep>0))
 	  t0=tstart;
       }
       if ((j%10) == 0)
 	fprintf(stderr,"\rRead frame %d, time %g",j,t);
-      if (dt>0)
-	t=t0+j*dt;
+      if (timestep>0)
+	t=t0+j*timestep;
       else if (t0>=0) 
 	t += (t0-tstart);
-      edr_io(&xdr,&t,&j,&nre,ee,dr);
-      j++;
-    }
-    fprintf(stderr,"\n");
-    
-  } else if (bEdr2Ene) {
-    FILE      *out;
-    t_energy  *ee;
-    t_drblock *dr;
-    XDR       xdr;
-    int       nre,j;
-    real      t;
-    char      **enm;
-    
-    tstart=-1;
-    xdropen(&xdr,ftp2fn(efEDR,NFILE,fnm),"r");
-    enm = NULL;
-    edr_nms(&xdr,&nre,&enm);
-    
-    dr=get_drblock();
-    snew(ee,nre);
-    
-    out=ftp2FILE(efENE,NFILE,fnm,"w");
-    wr_ener_nms(out,nre,enm);
-    
-    j=0;
-    while ( edr_io(&xdr,&t,&j,&nre,ee,dr)) {
-      if (tstart==-1) {
-	tstart=t;
-	if ((t0==-1) && (dt>0))
-	  t0=tstart;
+      if ( bRmod(t-toffset,delta_t) || (delta_t==0) ) {
+	fprintf(stderr,"  ->  write %d, time %g",i,t);
+	do_enx(out,&t,&j,&nre,ee,dr);
+	i++;
       }
-      if ((j%10) == 0)
-	fprintf(stderr,"\rRead frame %d, time %g",j,t);
-      if (dt>0)
-	t=t0+j*dt;
-      else if (t0>=0) 
-	t += (t0-tstart);
-      wr_ener(out,t,j,nre,ee,dr);
       j++;
     }
     fprintf(stderr,"\n");
     
-  } else 
-    fatal_error(0,"Nothing to be done....");
+  };
   
   thanx(stdout);
   
