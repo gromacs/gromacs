148941a00b392afdd18c11c0bcf7a26e3f0d1034
anton <anton>
1998-05-28 14:21:20 +0000

    removed obsolete stuff, updated pr_header to tpx

--- include/sheader.h
+++ include/sheader.h
@@ -38,44 +38,9 @@ static char *SRCID_sheader_h = "$Id$";
 
 #include <stdio.h>
 #include "typedefs.h"
+#include "tpxio.h"
 
-typedef struct		/* This struct describes the order and the	*/
-  /* sizes of the structs in a statusfile, sizes are given in bytes.	*/
-{
-  int	ir_size;	/* Non zero if input_rec is present		*/
-  int	e_size;		/* Non zero if energies are present		*/
-  int	box_size;	/* Non zero if a box is present			*/
-  int   vir_size;       /* Non zero if a virial is present              */
-  int   pres_size;      /* Non zero if a pressure is present            */
-  int	top_size;	/* Non zero if a topology is present		*/
-  int	sym_size;	/* Non zero if a symbol table is present	*/
-  int	x_size;		/* Non zero if coordinates are present		*/
-  int	v_size;		/* Non zero if velocities are present		*/
-  int	f_size;		/* Non zero if forces are present		*/
-
-  int	natoms;		/* The total number of atoms			*/
-  int	step;		/* Current step number				*/
-  int	nre;		/* Nr of energies 				*/
-  real	t;		/* Current time					*/
-  real	lambda;		/* Current value of lambda			*/
-} t_statheader;
-
-extern bool is_status(FILE *fp);
-/* Return TRUE when the file is a statusfile, FALSE otherwise */
-
-extern void wr_header(FILE *fp,t_statheader *sh);
-     /*
-      * Writes a (status) header to the file, specified by fp.
-      */
-
-extern char *rd_header(FILE *fp,t_statheader *sh);
-     /*
-      * Reads a (status) header from the file, specified by fp. If
-      * available, it returns the version string from the file, else
-      * it returns a version string from the statusio module.
-      */
-
-extern void pr_header(FILE *fp,int indent,char *title,t_statheader *sh);
+extern void pr_header(FILE *fp,int indent,char *title,t_tpxheader *sh);
      /*
       * This routine prints out a (human) readable representation of
       * a header to the file fp. Ident specifies the number of spaces
--- src/gmxlib/sheader.c
+++ src/gmxlib/sheader.c
@@ -29,100 +29,37 @@
 static char *SRCID_sheader_c = "$Id$";
 
 #include <string.h>
-#include "symtab.h"
-#include "magic.h"
-#include "fatal.h"
 #include "sheader.h"
 #include "txtdump.h"
-#include "copyrite.h"
-#include "binio.h"
-#include "recio.h"
-
-#define BUFSIZE		128
-#define	SKIP_SCCS	5
+#include "tpxio.h"
 
 static char *version="@(#) sheader.c 1.5 12/16/92";
 
-void wr_header(FILE *fp,t_statheader *sh)
-{
-  static int magic=GROMACS_MAGIC;
-  int len;
-  
-  len=strlen(version)+1;
-  blockwrite(fp,magic);
-  blockwrite(fp,len);
-  cblockwrite(fp,version,len);
-  header_blockio(fp,write,*sh);
-}
-
-bool is_status(FILE *fp)
-{
-  static int magic=0;
-
-  blockread(fp,magic);
-  rewind(fp);
-  
-  if (magic == GROMACS_MAGIC)
-    return TRUE;
-  else
-    return FALSE;
-}
-
-char *rd_header(FILE *fp,t_statheader *sh)
-{
-  static char header_version[BUFSIZE];
-  static int magic=0;
-  int len;
-
-  blockread(fp,magic);
-  blockread(fp,len);
-  cblockread(fp,header_version,len);
-  header_blockio(fp,read,*sh);
-  if (magic!=GROMACS_MAGIC) {
-    fprintf(stderr,"ERROR: incorrect tpb or trj file version.\n");
-    fprintf(stderr,"       make sure you use GROMACS %s throughout\n",
-	    GromacsVersion());
-    fprintf(stderr,"       for trj files you can use trjconv -patch to update the version number\n");
-    exit(1);
-  }
-  return header_version;
-}
-
-void pr_header(FILE *fp,int indent,char *title,t_statheader *sh)
+void pr_header(FILE *fp,int indent,char *title,t_tpxheader *sh)
 {
   if (available(fp,sh,title))
     {
       indent=pr_title(fp,indent,title);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"ir_size     = %d\n",sh->ir_size);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"e_size      = %d\n",sh->e_size);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"box_size    = %d\n",sh->box_size);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"vir_size    = %d\n",sh->vir_size);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"pres_size   = %d\n",sh->pres_size);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"top_size    = %d\n",sh->top_size);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"sym_size    = %d\n",sh->sym_size);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"x_size      = %d\n",sh->x_size);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"v_size      = %d\n",sh->v_size);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"f_size      = %d\n",sh->f_size);
-
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"natoms      = %d\n",sh->natoms);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"step        = %d\n",sh->step);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"nre         = %d\n",sh->nre);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"t           = %e\n",sh->t);
-      (void) pr_indent(fp,indent);
-      (void) fprintf(fp,"lambda      = %e\n",sh->lambda);
+      pr_indent(fp,indent);
+      fprintf(fp,"bIr    = %spresent\n",sh->bIr?"":"not ");
+      pr_indent(fp,indent);
+      fprintf(fp,"bBox   = %spresent\n",sh->bBox?"":"not ");
+      pr_indent(fp,indent);
+      fprintf(fp,"bTop   = %spresent\n",sh->bTop?"":"not ");
+      pr_indent(fp,indent);
+      fprintf(fp,"bX     = %spresent\n",sh->bX?"":"not ");
+      pr_indent(fp,indent);
+      fprintf(fp,"bV     = %spresent\n",sh->bV?"":"not ");
+      pr_indent(fp,indent);
+      fprintf(fp,"bF     = %spresent\n",sh->bF?"":"not ");
+      
+      pr_indent(fp,indent);
+      fprintf(fp,"natoms = %d\n",sh->natoms);
+      pr_indent(fp,indent);
+      fprintf(fp,"step   = %d\n",sh->step);
+      pr_indent(fp,indent);
+      fprintf(fp,"t      = %e\n",sh->t);
+      pr_indent(fp,indent);
+      fprintf(fp,"lambda = %e\n",sh->lambda);
     }
 }
