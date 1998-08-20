c52a283239162ef23de031095dcfaf6bd33cc858
hess <hess>
1998-08-20 12:07:47 +0000

    Pieter improved the overlap and out-of-the-box check

--- src/tools/addconf.c
+++ src/tools/addconf.c
@@ -34,15 +34,56 @@ static char *SRCID_addconf_c = "$Id$";
 #include "addconf.h"
 #include "gstat.h"
 
+
+real mydist2(rvec x,rvec y,matrix box)
+{
+  rvec dx;
+  int d;
+
+  for(d=0;(d<DIM);d++) {
+    dx[d]=fabs(x[d]-y[d]); 
+    while ( dx[d] >= box[d][d] )
+      dx[d] -= box[d][d];
+
+    if (fabs(dx[d]-box[d][d])<=dx[d])
+      dx[d]-=box[d][d];
+  }  
+
+
+  return iprod(dx,dx);
+}
+
+#define MARGE 0.2
+bool in_the_box(rvec x,matrix box)
+{
+  if (((x[0]>=-1 * MARGE)&&(x[0]<=box[0][0]+MARGE))&&
+      ((x[1]>=-1 * MARGE)&&(x[1]<=box[1][1]+MARGE))&&
+      ((x[2]>=-1 * MARGE)&&(x[2]<=box[2][2]+MARGE)))
+    return TRUE;
+  else
+    return FALSE;
+  
+}
+
+bool out_the_box(rvec x,matrix box)
+{
+  if (((x[0]>=MARGE)&&(x[0]<=box[0][0]-MARGE))&&
+      ((x[1]>=MARGE)&&(x[1]<=box[1][1]-MARGE))&&
+      ((x[2]>=MARGE)&&(x[2]<=box[2][2]-MARGE)))
+    return FALSE;
+  else
+    return TRUE;  
+}
+
 void add_conf(t_atoms *atoms_1,rvec *x_1,rvec *v_1,real *r_1,
 	      int NTB,matrix box_1,
 	      t_atoms *atoms_2,rvec *x_2,rvec *v_2,real *r_2,
 	      bool bVerbose,int maxmol)
 {
-  int  i,j,m,resnr,nresadd;
+  int  i,j,m,resnr,nresadd,d,k;
   int  *add;
   real d2,vdw2;
-
+  
   if (atoms_2->nr <= 0) {
     fatal_error(0,"Nothing to add");
   }
@@ -67,19 +108,20 @@ void add_conf(t_atoms *atoms_1,rvec *x_1,rvec *v_1,real *r_1,
       nresadd ++;
     }
   }
-  fprintf(stderr,"There are %d residues before testing overlap\n",nresadd);
-  if (atoms_1->nr == 0) {
-    /* Fill an empty box with solvent */
-    for (j=0; (j<atoms_2->nr); j++) {
-      while ((add[j]==0) && (j<atoms_2->nr))
-	j++;
-      if (in_box(NTB,box_1,(x_2)[j]) != 0) {
+  
+  
+  /* check solvent with solute */
+  for(i=0;(i<atoms_1->nr);i++) {
+    for(j=0;(j<atoms_2->nr);j++) {
+      d2=dist2((x_1)[i],(x_2)[j],box_1);
+      vdw2=sqr((r_1)[i] + (r_2)[j]);
+      if (d2 < vdw2) {
 	if (add[j] == 2)
 	  nresadd--;
 	add[j] = 0;
       }
       if (add[j]==0) {	
-        resnr=atoms_2->atom[j].resnr;
+	resnr=atoms_2->atom[j].resnr;
 	while((resnr==atoms_2->atom[j-1].resnr) && (j>0))
 	  j--;
 	if (j<0)
@@ -87,46 +129,64 @@ void add_conf(t_atoms *atoms_1,rvec *x_1,rvec *v_1,real *r_1,
 	while((resnr==atoms_2->atom[j].resnr) && (j<atoms_2->nr)) {
 	  if (add[j] == 2)
 	    nresadd--;
-          add[j] = 0;
+	  add[j]=0;
 	  j++;
-        }
+	}
       }
     }
-  } 
-  else {
-    /* if an atom of configuration_2 is too close to an atom of configuration_1
-     * add[j]=0
-     * if an atom of configuration_2 is not inside the box add[j]=0
-     */
-    for (i=0; (i<atoms_1->nr); i++) {
-      if (bVerbose && ((i % 10)==0))
-	fprintf(stderr,"\r%d atoms out of %d",i,atoms_1->nr);
-      for (j=0; (j<atoms_2->nr); j++) {
-	d2=dist2((x_1)[i],(x_2)[j],box_1);
-	vdw2=sqr((r_1)[i] + (r_2)[j]);
-	if ((d2 < vdw2) || (in_box(NTB,box_1,(x_2)[j]) != 0)) {
-	  if (add[j] == 2)
-	    nresadd--;
-	  add[j] = 0;
-	}
-	if (add[j]==0) {	
-	  resnr=atoms_2->atom[j].resnr;
-	  while((resnr==atoms_2->atom[j-1].resnr) && (j>0))
-	    j--;
-	  if (j<0)
-	    warning("j < 0 in addconf");
-	  while((resnr==atoms_2->atom[j].resnr) && (j<atoms_2->nr)) {
-	    if (add[j] == 2)
-	      nresadd--;
-	    add[j]=0;
-	    j++;
+  }
+  
+  /* check solvent with itself */
+  for(i=0;(i<atoms_2->nr);i++) {
+    bool bAdd = TRUE;
+
+    
+    /* remove atoms that are too far away */
+    if ((out_the_box(x_2[i],box_1)==TRUE)&&
+	(in_the_box(x_2[i],box_1)==FALSE))
+      bAdd = FALSE;
+    
+    /* check only the atoms that are in the border */
+    if ( bAdd == TRUE ) {
+      if ((out_the_box(x_2[i],box_1)==TRUE)&&
+	  (in_the_box(x_2[i],box_1)==TRUE)) {
+	
+#define EPS 0.00001
+	/* check with other border atoms */
+	for(j=0;(j<atoms_2->nr);j++) {
+	  if (add[j]!=0) {
+	    if ( atoms_2->atom[i].resnr!=atoms_2->atom[j].resnr) {
+	      if ((in_the_box(x_2[j],box_1)==TRUE)&&
+		  (out_the_box(x_2[j],box_1)==TRUE)) {
+		d2=mydist2((x_2)[i],(x_2)[j],box_1);
+		vdw2=sqr((r_2)[i] + (r_2)[j]);
+		if ((d2 < vdw2)||(d2<EPS)) {
+		  bAdd = FALSE;
+		}
+	      }
+	    }    
 	  }
 	}
       }
     }
-    if (bVerbose)
-      fprintf(stderr,"\n");
+    
+    
+    if ( bAdd == FALSE ) {
+      for(k=i;((k<atoms_2->nr)&&
+	       (atoms_2->atom[k].resnr==atoms_2->atom[i].resnr));k++) {
+	if (add[k] == 2)
+	  nresadd--;
+	add[k] = 0;
+      }
+      for(k=i;((k>=0)&&
+	       (atoms_2->atom[k].resnr==atoms_2->atom[i].resnr));k--) {
+	if (add[k] == 2)
+	  nresadd--;
+	add[k] = 0;
+      }
+    }
   }
+  
   /* add[j]=0 of all the atoms of a molecule containing one or more atoms 
    * add[i]=0
    */
@@ -156,4 +216,6 @@ void add_conf(t_atoms *atoms_1,rvec *x_1,rvec *v_1,real *r_1,
     }
   sfree(add);
 }/*add_conf()*/
+
+
 
