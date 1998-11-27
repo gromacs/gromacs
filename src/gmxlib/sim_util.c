68cc5d78d9399a6c4424da170e5c6296a43951cb
hess <hess>
1998-11-27 16:27:06 +0000

    put sum_forces back

--- src/gmxlib/sim_util.c
+++ src/gmxlib/sim_util.c
@@ -98,6 +98,14 @@ static void pr_commrec(FILE *log,t_commrec *cr)
 	  cr->pid,cr->nprocs,cr->left,cr->right);
 }
 
+static void sum_forces(int start,int end,rvec f[],rvec flr[])
+{
+  int i;
+  
+  for(i=start; (i<end); i++)
+    rvec_inc(f[i],flr[i]);
+}
+
 static void reset_forces(bool bNS,rvec f[],t_forcerec *fr,int natoms)
 {
   int i;
