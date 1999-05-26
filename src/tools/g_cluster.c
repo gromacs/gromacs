a62d15704192d492b3588a8278fa1a332e0ca6f3
spoel <spoel>
1999-05-26 20:46:38 +0000

    Made program such that it can read traj in memory

--- src/tools/g_cluster.c
+++ src/tools/g_cluster.c
@@ -46,8 +46,8 @@ static char *SRCID_g_rmsdist_c = "$Id$";
 #include "matio.h"
 #include "callf77.h"
 
-static void calc_dist(int nind,atom_id index[],
-		      rvec x[],matrix box,real **d)
+static void calc_dist_ind(int nind,atom_id index[],
+			  rvec x[],matrix box,real **d)
 {
   int     i,j;
   real    *xi;
@@ -57,6 +57,21 @@ static void calc_dist(int nind,atom_id index[],
     xi=x[index[i]];
     for(j=i+1; (j<nind); j++) {
       pbc_dx(xi,x[index[j]],dx);
+      d[i][j]=norm(dx);
+    }
+  }
+}
+
+static void calc_dist(int nind,rvec x[],matrix box,real **d)
+{
+  int     i,j;
+  real    *xi;
+  rvec    dx;
+
+  for(i=0; (i<nind-1); i++) {
+    xi=x[i];
+    for(j=i+1; (j<nind); j++) {
+      pbc_dx(xi,x[j],dx);
       d[i][j]=norm(dx);
     }
   }
@@ -135,6 +150,45 @@ void sort_matrix(int n1,real **mat)
   sfree(index);
 }
 
+void gather(int n1,real **mat)
+{
+}
+
+rvec **read_whole_trj(char *fn,int isize,atom_id index[],int skip,int *nframe)
+{
+  rvec   **xx,*x;
+  matrix box;
+  real   t;
+  int    i,i0,j,nf;
+  int    natom,nbytes,status;
+  
+  nf     = *nframe;
+  nbytes = nf*isize*sizeof(x[0]);
+  printf("Total memory required to read the whole trajectory: %d bytes\n",
+	 nbytes);
+  snew(xx,nf);
+  for(j=0; (j<nf); j++)
+    snew(xx[j],isize);
+  natom = read_first_x(&status,fn,&t,&x,box);
+  i  = 0;
+  i0 = 0;
+  do {
+    if ((i % skip) == 0) {
+      /* Store only the interesting atoms */
+      for(j=0; (j<isize); j++) 
+	copy_rvec(x[index[j]],xx[i0][j]);
+      i0 ++;
+    }
+    i++;
+  } while ((read_next_x(status,&t,natom,x,box)) && (i0 < nf));
+  fprintf(stderr,"\n");
+  printf("Read %d frames from trajectory %s\n",i0,fn);
+  *nframe = i0;
+  sfree(x);
+  
+  return xx;
+}
+
 int main(int argc,char *argv[])
 {
   static char *desc[] = {
@@ -151,19 +205,19 @@ int main(int argc,char *argv[])
   };
   
   FILE         *fp;
-  int          natom1,natom2,i1,i2,i1s,i2s,i,teller,nf;
+  int          natom,i1,i2,i1s,i2s,i,teller,nf,nrms;
   real         t,t1,t2;
 
   matrix       box;
-  rvec         *x1,*x2;
-  char         *fn1,*fn2;
+  rvec         *x1,*x2,**xx;
+  char         *fn;
   real         **rms,maxrms,*resnr;
   real         *rms0,*eigval;
   t_topology   top;
   bool         bSameF;
   t_rgb        rlo,rhi;
   
-  int      status1,status2,isize;
+  int      status1,status2,isize,nbytes;
   atom_id  *index;
   char     *grpname;
   real     **d1,**d2;
@@ -171,7 +225,7 @@ int main(int argc,char *argv[])
   
   static int  nlevels=40,nframes=100,skip=1;
   static real scalemax=-1.0,rmscut=0.1;
-  static bool bEigen=FALSE;
+  static bool bEigen=FALSE,bMem=FALSE;
   t_pargs pa[] = {
     { "-nlevels",   FALSE, etINT,  &nlevels,
       "Discretize rms in # levels" },
@@ -184,13 +238,14 @@ int main(int argc,char *argv[])
     { "-skip", FALSE, etINT, &skip,
       "Skip N frames after each one used." },
     { "-eigen", FALSE, etBOOL, &bEigen,
-      "Diagonalize the rms matrix instead of sorting it" }
+      "Diagonalize the rms matrix instead of sorting it" },
+    { "-mem",  FALSE, etBOOL, &bMem,
+      "Read the whole trajectory in memory (can be large...)" }
   };
   t_filenm fnm[] = {
-    { efTRX, "-f1",   NULL,       ffREAD  },
-    { efTRX, "-f2",   NULL,       ffOPTRD },
+    { efTRX, "-f",    NULL,       ffREAD  },
     { efTPS, "-s",    NULL,       ffREAD  },
-    { efNDX, NULL,   NULL,        ffOPTRD },
+    { efNDX, NULL,    NULL,        ffOPTRD },
     { efXPM, "-o",   "rmsd",      ffWRITE },
     { efXPM, "-os",  "rmsd-mat",  ffWRITE },
     { efXVG, "-ev",  "rms-eig",   ffWRITE }
@@ -236,89 +291,83 @@ int main(int argc,char *argv[])
   init_pbc(box,FALSE);
 
   /* Loop over first coordinate file */
-  fn1 = opt2fn("-f1",NFILE,fnm);
-  fn2 = opt2fn("-f2",NFILE,fnm);
-  bSameF = !opt2bSet("-f2",NFILE,fnm) || (strcmp(fn1,fn2) == 0);
-  if (bSameF) 
-    fprintf(stderr,"Reading the same file twice. "
-	    "Will only do half of matrix\n");
-
-  /* Read first frames of both files */  
-  natom1 = read_first_x(&status1,fn1,&t1,&x1,box);
-  natom2 = read_first_x(&status2,fn2,&t2,&x2,box);
+  fn = opt2fn("-f",NFILE,fnm);
   
-  /* Consistency checks */
-  if (natom1 != natom2) 
-    fprintf(stderr,"Warning: natoms in %s is %d, in %s it is %d\n",
-	    fn1,natom1,fn2,natom2);
-  if (isize > natom1) 
-    fatal_error(0,"Index (from index file or topology)"
-		" does not match trajectory %s",fn1);
-  if (isize > natom2) 
-    fatal_error(0,"Index (from index file or topology)"
-		" does not match trajectory %s",fn2);
-		
-  /* Initiate locals */	
-  i1     = 0;
   maxrms = 0;
-  nf     = 0;
-  do { 
-    /* Loop over first file */
-    if (i1 >= nframes)
-      break;
-    else if ((i1 % skip) == 0) {
-      /* Print counter */
-      if ((i1 % 10) == 0)
-	fprintf(stderr,"\ri1 = %d                               \n",i1);
-      
-      /* Compute distance matrix for first structure */
-      calc_dist(isize,index,x1,box,d1);
-      
-      /* Find the starting frame for the second file */
-      if (bSameF)
-	i2 = i1+1;
-      else
-	i2 = 0;
-	
-      /* Rewind second traj, and skip to first interesting frame */
-      rewind_trj(status2);
-      for(i=0; (i<=i2); i++)
-	if (!read_next_x(status2,&t2,natom2,x2,box))
-	  break;
-      do {
-	/* Loop over second file */
-	if (i2 >= nframes)
-	  break;
-	else if ((i2 % skip) == 0) {
-	  /* Indices in RMS matrix */
-	  i1s = i1/skip;
-	  i2s = i2/skip;
-	  
-	  /* Compute distance matrix for second structure */  
-	  calc_dist(isize,index,x2,box,d2);
-	  
-	  /* Compute RMS between two distance matrices */
-	  rms[i1s][i2s] = rms_diff(isize,d1,d2);
-	  
-	  if (bSameF)
-	    rms[i2s][i1s] = rms[i1s][i2s];
-	    
-	  /* Store maximum value for plotting */
-	  maxrms = max(maxrms,rms[i1s][i2s]);
-	}
-	i2++;
-      } while (read_next_x(status2,&t2,natom2,x2,box));
-      nf++;
-    }
-    i1++;
-  } while(read_next_x(status1,&t1,natom1,x1,box));
-  fprintf(stderr,"\n");
-  
-  /* Done with computing, close the files */
-  /*close_trx(status1);
-  close_trx(status2);
-  */
 
+  if (bMem) {
+    xx = read_whole_trj(fn,isize,index,skip,&nf);
+    fprintf(stderr,"Computing %dX%d RMS matrix\n",nf,nf);
+    nrms = (nf*(nf-1))/2;
+    for(i1=0; (i1<nf); i1++) {
+      calc_dist(isize,xx[i1],box,d1);
+      for(i2=i1+1; (i2<nf); i2++) {
+	calc_dist(isize,xx[i2],box,d2);
+	rms[i1][i2] = rms[i2][i1] = rms_diff(isize,d1,d2);
+	
+	/* Store maximum value for plotting */
+	maxrms = max(maxrms,rms[i1][i2]);
+      }
+      nrms -= (nf-i1-1);
+      fprintf(stderr,"\r# RMS calculations left: %d   ",nrms);
+    }
+    fprintf(stderr,"\nDone\n");
+  }
+  else {
+    /* Read first frames of both files */  
+    natom = read_first_x(&status1,fn,&t1,&x1,box);
+    natom = read_first_x(&status2,fn,&t1,&x2,box);
+    
+    /* Consistency checks */
+    if (isize > natom) 
+      fatal_error(0,"Index (from index file or topology)"
+		  " does not match trajectory %s",fn);
+    
+    /* Initiate locals */	
+    i1     = 0;
+    i1s    = 0;
+    i2s    = 0;
+    do { 
+      /* Loop over first file */
+      if ((i1 % skip) == 0) {
+	/* Print counter */
+	fprintf(stderr,"\ri1 = %d                               \n",i1);
+	
+	/* Compute distance matrix for first structure */
+	calc_dist_ind(isize,index,x1,box,d1);
+	
+	/* Find the starting frame for the second file */
+	i2 = i1+1;
+	
+	/* Rewind second traj, and skip to first interesting frame */
+	rewind_trj(status2);
+	for(i=0; (i<=i2); i++)
+	  if (!read_next_x(status2,&t2,natom,x2,box))
+	    break;
+	
+	do {
+	  /* Loop over second file */
+	  if ((i2 % skip) == 0) {
+	    /* Compute distance matrix for second structure */  
+	    calc_dist_ind(isize,index,x2,box,d2);
+	    
+	    /* Compute RMS between two distance matrices */
+	    rms[i1s][i2s] = rms[i2s][i1s] = rms_diff(isize,d1,d2);
+	    
+	    /* Store maximum value for plotting */
+	    maxrms = max(maxrms,rms[i1s][i2s]);
+	    
+	    i2s++;
+	  }
+	  i2++;
+	} while ((read_next_x(status2,&t2,natom,x2,box)) && (i2s < nf));
+	i1s++;
+      }
+      i1++;
+    } while ((read_next_x(status1,&t1,natom,x1,box)) && (i1s < nf));
+    fprintf(stderr,"\n");
+    nf = i1;
+  }
   fprintf(stderr,"The maximum RMS was %g nm, number of frames for matrix %d\n",
 	  maxrms,nf);
   
@@ -337,6 +386,9 @@ int main(int argc,char *argv[])
   else {
     /* Do a diagonalization */
     snew(eigval,nf);
+    /*for(i1=0; (i1<nf); i1++)
+      for(i2=0; (i2<nf); i2++)
+      rms[i1][i2] = maxrms - rms[i1][i2];*/
     ql77(nf,rms0,eigval); 
     fp = xvgropen(opt2fn("-ev",NFILE,fnm),"Eigenvalues","index","value");
     for(i=0; (i<nf); i++)
