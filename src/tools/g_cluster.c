7e88cb9e82d29679cb9cf66e1f6fef61305ac620
spoel <spoel>
1999-05-27 12:09:54 +0000

    Many additions

--- src/tools/g_cluster.c
+++ src/tools/g_cluster.c
@@ -31,6 +31,7 @@ static char *SRCID_g_rmsdist_c = "$Id$";
 #include <math.h>
 #include <string.h>
 #include "macros.h"
+#include "assert.h"
 #include "smalloc.h"
 #include "typedefs.h"
 #include "copyrite.h"
@@ -45,6 +46,34 @@ static char *SRCID_g_rmsdist_c = "$Id$";
 #include "futil.h"
 #include "matio.h"
 #include "callf77.h"
+
+typedef struct {
+  int  i,j;
+  real dist;
+} t_dist;
+
+typedef struct {
+  int  conf,clust;
+} t_clustid;
+
+static real **mk_matrix(int nf,bool b1D)
+{
+  real **rms;
+  real *rms0=NULL;
+  int  i;
+  
+  snew(rms,nf);
+  if (b1D)
+    snew(rms0,nf*nf); 
+  
+  for(i=0; (i<nf); i++) {
+    if (b1D)
+      rms[i] = &(rms0[i*nf]);
+    else
+      snew(rms[i],nf);
+  }
+  return rms;
+}
 
 static void calc_dist_ind(int nind,atom_id index[],
 			  rvec x[],matrix box,real **d)
@@ -118,9 +147,9 @@ static int icomp(const void *a,const void *b)
   ia = *(int *)a;
   ib = *(int *)b;
   
-  if (mcpy[ia][0] < mcpy[ib][0])
+  if (mcpy[ia][nnn] < mcpy[ib][nnn])
     return -1;
-  else if (mcpy[ia][0] > mcpy[ib][0])
+  else if (mcpy[ia][nnn] > mcpy[ib][nnn])
     return 1;
   else
     return 0;  
@@ -150,8 +179,92 @@ void sort_matrix(int n1,real **mat)
   sfree(index);
 }
 
-void gather(int n1,real **mat)
+static int dcomp(const void *a,const void *b)
 {
+  t_dist *da,*db;
+  
+  da = (t_dist *)a;
+  db = (t_dist *)b;
+  
+  if (da->dist < db->dist)
+    return -1;
+  else if (da->dist > db->dist)
+    return 1;
+  return 0;
+}
+
+static int ccomp(const void *a,const void *b)
+{
+  t_clustid *da,*db;
+  
+  da = (t_clustid *)a;
+  db = (t_clustid *)b;
+  
+  return da->clust - db->clust;
+}
+
+void gather(int n1,real **mat,real cutoff)
+{
+  t_clustid *c;
+  t_dist    *d;
+  int    i,j,k,nn,cid;
+  
+  /* First we sort the entries in the RMS matrix */
+  nn = ((n1-1)*n1)/2;
+  snew(d,nn);
+  for(i=k=0; (i<n1); i++)
+    for(j=i+1; (j<n1); j++,k++) {
+      d[k].i    = i;
+      d[k].j    = j;
+      d[k].dist = mat[i][j];
+    }
+  assert(k == nn);
+  qsort(d,nn,sizeof(d[0]),dcomp);
+  
+  /* Now we make a cluster index for all of the conformations */
+  snew(c,n1);
+  for(i=0; (i<n1); i++) {
+    c[i].conf = i;
+    c[i].clust = i;
+  }
+    
+  /* Now we check the closest structures, and equalize their cluster
+   * numbers 
+   */
+  for(k=0; (k<nn) && (d[k].dist < cutoff); k++) {
+    i = d[k].i;
+    j = d[k].j;
+    c[j].clust = c[i].clust;
+  }
+  printf("Sorting and renumbering clusters\n");
+  /* Sort on cluster number */
+  qsort(c,n1,sizeof(c[0]),ccomp);
+
+  /* Renumber clusters */
+  cid = c[0].clust;
+  for(k=1; (k<n1); k++) {
+    if (c[k].clust != cid) {
+      cid ++;
+      c[k].clust = cid;
+    }
+    else
+      c[k].clust = c[k-1].clust;
+  }
+  for(k=0; (k<n1); k++)
+    printf("Cluster index for conformation %d: %d\n",c[k].conf,c[k].clust);
+
+  mcpy = mk_matrix(n1,FALSE);
+  for(i=0; (i<n1); i++) {
+    for(j=0; (j<n1); j++)
+      mcpy[c[i].conf][c[j].conf] = mat[i][j];
+  }
+  for(i=0; (i<n1); i++) {
+    for(j=0; (j<n1); j++)
+      mat[i][j] = mcpy[i][j];
+  }
+    
+  sfree(c);
+  sfree(d);
 }
 
 rvec **read_whole_trj(char *fn,int isize,atom_id index[],int skip,int *nframe)
@@ -189,6 +302,25 @@ rvec **read_whole_trj(char *fn,int isize,atom_id index[],int skip,int *nframe)
   return xx;
 }
 
+void rms_dist(char *fn,int nf,real **mat,real maxrms)
+{
+  FILE   *fp;
+  int    i,j,*histo;
+  real   fac;
+  
+  fac = 100/maxrms;
+  snew(histo,101);
+  for(i=0; (i<nf); i++) 
+    for(j=i+1; (j<nf); j++)
+      histo[(int)(fac*mat[i][j])]++;
+      
+  fp = xvgropen(fn,"RMS Distribution","RMS (nm)","a.u.");
+  for(i=0; (i<101); i++)
+    fprintf(fp,"%10g  %10d\n",i/fac,histo[i]);
+  fclose(fp);
+  sfree(histo);
+}
+
 int main(int argc,char *argv[])
 {
   static char *desc[] = {
@@ -211,6 +343,7 @@ int main(int argc,char *argv[])
   matrix       box;
   rvec         *x1,*x2,**xx;
   char         *fn;
+  double       sum;
   real         **rms,maxrms,*resnr;
   real         *rms0,*eigval;
   t_topology   top;
@@ -223,9 +356,9 @@ int main(int argc,char *argv[])
   real     **d1,**d2;
   char     buf[255];
   
-  static int  nlevels=40,nframes=100,skip=1;
+  static int  nlevels=40,nframes=100,skip=0;
   static real scalemax=-1.0,rmscut=0.1;
-  static bool bEigen=FALSE,bMem=FALSE;
+  static bool bEigen=FALSE,bMem=TRUE,bLink=FALSE;
   t_pargs pa[] = {
     { "-nlevels",   FALSE, etINT,  &nlevels,
       "Discretize rms in # levels" },
@@ -235,20 +368,23 @@ int main(int argc,char *argv[])
       "Maximum level in matrices" },
     { "-nframes", FALSE, etINT, &nframes,
       "Max number of frames" },
-    { "-skip", FALSE, etINT, &skip,
+    { "-skip",  FALSE, etINT, &skip,
       "Skip N frames after each one used." },
     { "-eigen", FALSE, etBOOL, &bEigen,
       "Diagonalize the rms matrix instead of sorting it" },
-    { "-mem",  FALSE, etBOOL, &bMem,
+    { "-link",  FALSE, etBOOL, &bLink,
+      "Use linkage algorithm for clustering" },
+    { "-mem",   FALSE, etBOOL, &bMem,
       "Read the whole trajectory in memory (can be large...)" }
   };
   t_filenm fnm[] = {
     { efTRX, "-f",    NULL,       ffREAD  },
     { efTPS, "-s",    NULL,       ffREAD  },
-    { efNDX, NULL,    NULL,        ffOPTRD },
+    { efNDX, NULL,    NULL,       ffOPTRD },
     { efXPM, "-o",   "rmsd",      ffWRITE },
     { efXPM, "-os",  "rmsd-mat",  ffWRITE },
-    { efXVG, "-ev",  "rms-eig",   ffWRITE }
+    { efXVG, "-ev",  "rms-eig",   ffWRITE },
+    { efXVG, "-dist","rms-dist",  ffWRITE }
   };
 #define NFILE asize(fnm)
 
@@ -274,18 +410,16 @@ int main(int argc,char *argv[])
     snew(d2[i],isize);
   }
   nf = nframes/skip+1;
-  snew(rms,nf);
+  
+  rms = mk_matrix(nf,bEigen);
   if (bEigen)
-    snew(rms0,nf*nf); 
-
+    rms0 = rms[0];
+  else
+    rms0 = NULL;
+    
   snew(resnr,nf);
-  for(i=0; (i<nf); i++) {
-    if (bEigen)
-      rms[i] = &(rms0[i*nf]);
-    else
-      snew(rms[i],nf);
-    resnr[i] = 0;
-  }
+  for(i=0; (i<nf); i++) 
+    resnr[i] = i;
 
   /*set box type*/
   init_pbc(box,FALSE);
@@ -294,7 +428,7 @@ int main(int argc,char *argv[])
   fn = opt2fn("-f",NFILE,fnm);
   
   maxrms = 0;
-
+  sum    = 0;
   if (bMem) {
     xx = read_whole_trj(fn,isize,index,skip,&nf);
     fprintf(stderr,"Computing %dX%d RMS matrix\n",nf,nf);
@@ -307,6 +441,7 @@ int main(int argc,char *argv[])
 	
 	/* Store maximum value for plotting */
 	maxrms = max(maxrms,rms[i1][i2]);
+	sum += rms[i1][i2];
       }
       nrms -= (nf-i1-1);
       fprintf(stderr,"\r# RMS calculations left: %d   ",nrms);
@@ -356,7 +491,7 @@ int main(int argc,char *argv[])
 	    
 	    /* Store maximum value for plotting */
 	    maxrms = max(maxrms,rms[i1s][i2s]);
-	    
+	    sum += rms[i1s][i2s];
 	    i2s++;
 	  }
 	  i2++;
@@ -368,8 +503,13 @@ int main(int argc,char *argv[])
     fprintf(stderr,"\n");
     nf = i1;
   }
-  fprintf(stderr,"The maximum RMS was %g nm, number of frames for matrix %d\n",
-	  maxrms,nf);
+  fprintf(stderr,"The maximum RMS was %g nm\n"
+	  "Average RMS was %g\n"
+	  "number of frames for matrix %d\n",
+	  maxrms,2*sum/(nf*(nf-1)),nf);
+  
+  /* Plot the rms distribution */
+  rms_dist(opt2fn("-dist",NFILE,fnm),nf,rms,maxrms);
   
   /* Set colors for plotting: white = zero RMS, black = maximum */
   rlo.r=1.0, rlo.g=1.0, rlo.b=0.0;
@@ -380,9 +520,9 @@ int main(int argc,char *argv[])
 	    "RMS (n)","Confs 1","Confs 2",
 	    nf,nf,resnr,resnr,rms,0.0,maxrms,rlo,rhi,&nlevels);
 
-  if (!bEigen) 
+  if (bLink) 
     /* Now sort the matrix and write it out again */
-    sort_matrix(nf,rms);
+    gather(nf,rms,rmscut);
   else {
     /* Do a diagonalization */
     snew(eigval,nf);
