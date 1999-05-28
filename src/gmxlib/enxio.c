84254ec87f8a71ff1576999a9f2b561486d20926
anton <anton>
1999-05-28 07:36:50 +0000

    print 'Last frame' on new line

--- src/gmxlib/enxio.c
+++ src/gmxlib/enxio.c
@@ -219,7 +219,7 @@ bool do_enx(int fp,real *t,int *step,int *nre,t_energy ener[],
   }
   if (!bOK) {
     if (bRead) {
-      fprintf(stderr,"\rLast frame read %d                     ",framenr-1);
+      fprintf(stderr,"\nLast frame read %d                     ",framenr-1);
       fprintf(stderr,"\nWARNING: Incomplete frame: nr %6d time %8.3f\n",
 	      framenr,*t);
     } else 
