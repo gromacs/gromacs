c6d5709ab15301eb8438a2d0223c7f2396f9afac
anton <anton>
1999-08-09 13:18:59 +0000

    complete re-write of pdb2gmx datastructure: cleaned up 15+ different structs to 6 or so! The code should now be ready for a more logical division between gmxlib and kernel and also for implemting of multi-chain specbond support (only need some sucker to do it). Also implemented standard directive block structure for termini database and full bonded interactions support for them and fixed some flaws in the GROMOS termini database files (charges in vacuum and bonded interaction types)

---
+++ src/kernel/hizzie.h
@@ -0,0 +1,38 @@
+/*
+ *       $Id$
+ *
+ *       This source code is part of
+ *
+ *        G   R   O   M   A   C   S
+ *
+ * GROningen MAchine for Chemical Simulations
+ *
+ *            VERSION 2.0
+ * 
+ * Copyright (c) 1991-1997
+ * BIOSON Research Institute, Dept. of Biophysical Chemistry
+ * University of Groningen, The Netherlands
+ * 
+ * Please refer to:
+ * GROMACS: A message-passing parallel molecular dynamics implementation
+ * H.J.C. Berendsen, D. van der Spoel and R. van Drunen
+ * Comp. Phys. Comm. 91, 43-56 (1995)
+ *
+ * Also check out our WWW page:
+ * http://rugmd0.chem.rug.nl/~gmx
+ * or e-mail to:
+ * gromacs@chem.rug.nl
+ *
+ * And Hey:
+ * Gyas ROwers Mature At Cryogenic Speed
+ */
+
+#ifndef _hizzie_h
+#define _hizzie_h
+
+static char *SRCID_hizzie_h = "$Id$";
+
+extern void set_histp(t_atoms *pdba,rvec *x,real angle,real distance);
+/* calculate HIStidine protonation state */
+
+#endif	/* _pdb2gmx_h */
