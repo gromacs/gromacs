f5e7736123c77e51eb1477df1d7053cb5e2cdff2
spoel <spoel>
1999-10-21 20:02:05 +0000

    Added a lot

--- src/local/mkice.c
+++ src/local/mkice.c
@@ -40,6 +40,7 @@ static char *SRCID_mkice_c = "$Id$";
 #include "vec.h"
 #include "pbc.h"
 #include "physics.h"
+#include "names.h"
 #include "txtdump.h"
 #include "trnio.h"
 #include "symtab.h"
@@ -48,10 +49,26 @@ static char *SRCID_mkice_c = "$Id$";
 #define ODIST 0.274
 #define HDIST 0.1
 #define TET   109.47
+#define DCONS 0.117265878
 
 static char *watname[] = { "OW1", "HW2", "HW3", "DW", "SW" };
 static real qspc[]     = { -0.82, 0.41, 0.41 };
-static real qyaw[]     = { 1.24588, 0.62134,0.62134, 0.0,  -2.48856 };
+static real qyaw[]     = { 1.24588, 0.62134, 0.62134, 0.0, -2.48856 };
+static real spc_lj[3][6] = {
+  { 2.6171e-3, 2.6331e-6, 0, 0, 0, 0 },
+  { 0,      0,      0, 0, 0, 0 },
+  { 0,      0,      0, 0, 0, 0 }
+};
+#define CHH 2e-9
+#define CHS 2e-9
+#define COS 2e-6
+static real yaw_lj[5][10] = {
+  { 0, 0,   0, 0,   0,   0, 0, 0, 0,      COS },
+  { 0, 0,   0, CHH, 0, CHH, 0, 0, 0,      CHS },
+  { 0, 0,   0, CHH, 0, CHH, 0, 0, 0,      CHS },
+  { 0, 0,   0, 0,   0,   0, 0, 0, 0,      0   },
+  { 0, COS, 0, CHS, 0, CHS, 0, 0, 2.6e-3, 0   }
+};
 
 void unitcell(rvec x[],rvec box,bool bYaw)
 {
@@ -88,7 +105,6 @@ void unitcell(rvec x[],rvec box,bool bYaw)
   };
   int  i,iin,iout,j,m;
   rvec tmp,t2,dip;
-  real a = 0.8;
   
   clear_rvec(dip);
   for(i=0; (i<8); i++) {
@@ -109,15 +125,17 @@ void unitcell(rvec x[],rvec box,bool bYaw)
     if (bYaw)
       for(m=0; (m<DIM); m++) 
 	x[iout+3][m] = x[iout+4][m] = 
-	  a*x[iout][m]+0.5*(1-a)*(x[iout+1][m]+x[iout+2][m]);
+	  (1-2*DCONS)*x[iout][m]+DCONS*(x[iout+1][m]+x[iout+2][m]);
   }
-  printf("Dipole: %10.5f%10.5f%10.5f (e nm)\n",dip[XX],dip[YY],dip[ZZ]);
   
   box[XX] = 2*cx;
   box[YY] = 2*(cy2+cy);
   box[ZZ] = 2*(1+cz);
   for(i=0; (i<DIM); i++)
     box[i] *= ODIST;
+    
+  printf("Unitcell:  %10.5f  %10.5f  %10.5f\n",box[XX],box[YY],box[ZZ]);
+  printf("Dipole:    %10.5f  %10.5f  %10.5f (e nm)\n",dip[XX],dip[YY],dip[ZZ]);
 }
 
 static real calc_ener(real c6,real c12,rvec dx,tensor vir)
@@ -183,45 +201,105 @@ void interactions(FILE *fp,rvec x[],matrix box,real rcut,bool bYaw)
   fprintf(fp,"LJ energy is %g\n",elj);
 }
 
-void virial(FILE *fp,int nmol,rvec x[],matrix box,real rcut,bool bYaw,real q[])
+void virial(FILE *fp,bool bFull,int nmol,rvec x[],matrix box,real rcut,
+	    bool bYaw,real q[],bool bLJ)
 {
-  int  i,j,im,jm,natmol,ik,jk,m;
-  rvec dx,ftot;
-  real dx2,dx1,fscal;
+  int  i,j,im,jm,natmol,ik,jk,m,ninter;
+  rvec dx,f,ftot,dvir,vir,pres,xcmi,xcmj,*force;
+  real dx6,dx2,dx1,fscal,c6,c12,vcoul,v12,v6,vctot,v12tot,v6tot;
   
   init_pbc(box,FALSE);
   /* Initiate the periodic boundary conditions. Set bTruncOct to
    * TRUE when using a truncated octahedron box.
    */
-  fprintf(fp,"%3s - %3s: %6s %6s %6s %6s %8s %8s %8s\n",
-	  "ai","aj","dx","dy","dz","|d|","fx","fy","fz");
+  fprintf(fp,"%3s   -  %3s: %6s %6s %6s  %6s %8s %8s %8s\n",
+	  "ai","aj","dx","dy","dz","|d|","virx","viry","virz");
   clear_rvec(ftot);
+  clear_rvec(vir);
+  ninter = 0;
+  vctot  = 0;
+  v12tot = 0;
+  v6tot  = 0;
   natmol = bYaw ? 5 : 3;
+  snew(force,nmol*natmol);
+  
   for(i=0; (i<nmol); i++) {
     im = natmol*i;
+    /* Center of geometry */
+    clear_rvec(xcmi);
+    for(ik=0; (ik<natmol); ik++)
+      rvec_inc(xcmi,x[im+ik]);
+    for(m=0; (m<DIM); m++)
+      xcmi[m] /= natmol;
+
     for(j=i+1; (j<nmol); j++) {
       jm = natmol*j;
-      /* First check OW-OW distance */
-      pbc_dx(x[im],x[jm],dx);
+      /* Center of geometry */
+      clear_rvec(xcmj);
+      for(jk=0; (jk<natmol); jk++)
+	rvec_inc(xcmj,x[jm+jk]);
+      for(m=0; (m<DIM); m++)
+	xcmj[m] /= natmol;
+
+      /* First check COM-COM distance */
+      pbc_dx(xcmi,xcmj,dx);
       if (norm(dx) < rcut) {
+	ninter++;
 	/* Neirest neighbour molecules! */
 	for(ik=0; (ik<natmol); ik++) 
 	  for(jk=0; (jk<natmol); jk++) {
 	    pbc_dx(x[im+ik],x[jm+jk],dx);
-	    dx2 = iprod(dx,dx);
-	    dx1 = sqrt(dx2);
-	    fscal = q[ik]*q[jk]/(dx2*dx1);
-	    fprintf(fp,"%3s%3d-%3s%3d: %6.3f %6.3f %6.3f %6.3f %8.3f %8.3f %8.3f\n",
-		    watname[ik],im+ik,watname[jk],jm+jk,
-		    dx[XX],dx[YY],dx[ZZ],norm(dx),
-		    dx[XX]*fscal,dx[YY]*fscal,dx[ZZ]*fscal);
-	    for(m=0; (m<DIM); m++)
-	      ftot[m] += dx[m]*fscal;
+	    dx2    = iprod(dx,dx);
+	    dx1    = sqrt(dx2);
+	    vcoul  = q[ik]*q[jk]*ONE_4PI_EPS0/dx1;
+	    vctot += vcoul;
+	    
+	    if (bLJ) {
+	      if (bYaw) {
+		c6  = yaw_lj[ik][2*jk];
+		c12 = yaw_lj[ik][2*jk+1];
+	      }
+	      else {
+		c6  = spc_lj[ik][2*jk];
+		c12 = spc_lj[ik][2*jk+1];
+	      }
+	      dx6    = dx2*dx2*dx2;
+	      v6     = c6/dx6;
+	      v12    = c12/(dx6*dx6);
+	      v6tot -= v6;
+	      v12tot+= v12;
+	      fscal  = (vcoul+12*v12-6*v6)/dx2;
+	    }
+	    else
+	      fscal  = vcoul/dx2;
+	    for(m=0; (m<DIM); m++) {
+	      f[m]     = dx[m]*fscal;
+	      dvir[m]  = -0.5*dx[m]*f[m];
+	      vir[m]  += dvir[m];
+	    }
+	    rvec_inc(force[ik+im],f);
+	    rvec_dec(force[jk+jm],f);
+	    if (bFull)
+	      fprintf(fp,"%3s%4d-%3s%4d: %6.3f %6.3f %6.3f %6.3f"
+		      " %8.3f %8.3f %8.3f\n",
+		      watname[ik],im+ik,watname[jk],jm+jk,
+		      dx[XX],dx[YY],dx[ZZ],norm(dx),
+		      dvir[XX],dvir[YY],dvir[ZZ]);
 	  }
       }
     }
   }
-  pr_rvec(fp,0,"ftot",ftot,DIM);
+  fprintf(fp,"There were %d interactions between the %d molecules\n",
+	  ninter,nmol);
+  fprintf(fp,"Vcoul: %10.4e  V12: %10.4e  V6: %10.4e  Vtot: %10.4e\n",
+	  vctot,v12tot,v6tot,vctot+v12tot+v6tot);
+  pr_rvec(fp,0,"vir ",vir,DIM);
+  
+  for(m=0; (m<DIM); m++) 
+    pres[m] = -2*PRESFAC/(det(box))*vir[m];
+  pr_rvec(fp,0,"pres",pres,DIM);
+  pr_rvecs(fp,0,"force",force,natmol*nmol);
+  sfree(force);
 }
 
 int main(int argc,char *argv[])
@@ -232,16 +310,22 @@ int main(int argc,char *argv[])
     "and all oxygens are tetrahedrally coordinated."
   };
   static int nx=1,ny=1,nz=1;
-  static bool bYaw=FALSE;
+  static bool bYaw=FALSE,bLJ=TRUE,bFull=TRUE,bSeries=FALSE;
+  static real rcut=0.3;
   t_pargs pa[] = {
-    { "-nx", FALSE, etINT, &nx, "nx" },
-    { "-ny", FALSE, etINT, &ny, "ny" },
-    { "-nz", FALSE, etINT, &nz, "nz" },
-    { "-yaw",FALSE, etBOOL,&bYaw,"Generate dummies and shell positions" }
+    { "-nx", FALSE, etINT, {&nx}, "nx" },
+    { "-ny", FALSE, etINT, {&ny}, "ny" },
+    { "-nz", FALSE, etINT, {&nz}, "nz" },
+    { "-yaw",FALSE, etBOOL,{&bYaw},"Generate dummies and shell positions" },
+    { "-lj", FALSE, etBOOL,{&bLJ},"Use LJ as well as coulomb for virial calculation" },
+    { "-rcut",FALSE,etREAL,{&rcut},"Cut-off for virial calculations" },
+    { "-full",FALSE,etBOOL,{&bFull},"Full virial output" },
+    { "-series",FALSE, etBOOL, {&bSeries}, "Do a series of virial calculations with different cut-off (from 0.3 up till the specified one)" }
   };
   t_filenm fnm[] = {
-    { efPDB, "-p", "ice", FALSE },
-    { efTRN, "-o", "ice", FALSE }
+    { efPDB, "-p", "ice", ffWRITE },
+    { efSTO, "-c", NULL,  ffOPTRD },
+    { efTRN, "-o", "ice", ffOPTWR }
   };
 #define NFILE asize(fnm)
 
@@ -256,6 +340,11 @@ int main(int argc,char *argv[])
   CopyRight(stdout,argv[0]);
   parse_common_args(&argc,argv,0,FALSE,NFILE,fnm,asize(pa),pa,asize(desc),
 		    desc,0,NULL);
+  if (debug) {
+    fprintf(debug,"nx  = %3d, ny  = %3d,  nz   = %3d\n",nx,ny,nz);
+    fprintf(debug,"YAW = %3s, LJ  = %3s,  rcut = %g\n",yesno_names[bYaw],
+	    yesno_names[bLJ],rcut);
+  }
 
   natmol = bYaw ? 5 : 3;
   natom = natmol*8;
@@ -283,7 +372,7 @@ int main(int argc,char *argv[])
     boxje[XX][XX] = box[XX];
     boxje[YY][YY] = box[YY];
     boxje[ZZ][ZZ] = box[ZZ];
-    interactions(debug,xx,boxje,0.9,bYaw);
+    /*interactions(debug,xx,boxje,0.9,bYaw);*/
   }
   n=0;
   for(i=0; (i<nx); i++) {
@@ -299,21 +388,30 @@ int main(int argc,char *argv[])
     }
   }
     
-  printf("box = (%10.5f%10.5f%10.5f)\n",box[XX],box[YY],box[ZZ]);
-  /*calc_vecs3(nx,ny,nz,pdba,bReset);*/
-  
   clear_mat(boxje);
   boxje[XX][XX] = box[XX]*nx;
   boxje[YY][YY] = box[YY]*ny;
   boxje[ZZ][ZZ] = box[ZZ]*nz;
   
-  if (debug)
-    virial(debug,nmax/natmol,xx,boxje,0.3,bYaw,bYaw ? qyaw : qspc);
-    
+  printf("Crystal:   %10.5f  %10.5f  %10.5f\n",
+	 nx*box[XX],ny*box[YY],nz*box[ZZ]);
+  
+  if (debug) {
+    if (bSeries)
+      for(i=3; (i<=10*rcut); i++) {
+	fprintf(debug,"This is with rcut = %g\n",i*0.1);
+	virial(debug,bFull,nmax/natmol,xx,boxje,
+	       0.1*i,bYaw,bYaw ? qyaw : qspc,bLJ);
+      }    
+    else
+      virial(debug,bFull,nmax/natmol,xx,boxje,
+	     rcut,bYaw,bYaw ? qyaw : qspc,bLJ);
+  }
   write_sto_conf(ftp2fn(efPDB,NFILE,fnm),
 		 "Ice Ih crystal generated by mkice",pdba,xx,NULL,boxje);
-
-  write_trn(ftp2fn(efTRN,NFILE,fnm),0,0,0,boxje,nmax,xx,NULL,NULL);
+		 
+  if (ftp2bSet(efTRN,NFILE,fnm))
+    write_trn(ftp2fn(efTRN,NFILE,fnm),0,0,0,boxje,nmax,xx,NULL,NULL);
 	       
   return 0;
 }
