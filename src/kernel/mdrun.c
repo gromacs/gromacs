e5c0a43a685918b15de5a008056fb9eaae4f6d9f
hess <hess>
1999-10-25 11:55:37 +0000

    changed dvdl to dgdl

--- src/kernel/mdrun.c
+++ src/kernel/mdrun.c
@@ -66,7 +66,7 @@ int main(int argc,char *argv[])
     "When running in parallel with PVM or an old version of MPI the",
     "[TT]-np[tt] option must be given to indicate the number of",
     "processors.[PAR]",
-    "The option [TT]-dvdl[tt] is only used when free energy perturbation is",
+    "The option [TT]-dgdl[tt] is only used when free energy perturbation is",
     "turned on.[PAR]",
     "With [TT]-rerun[tt] an input trajectory can be given for which ",
     "forces and energies will be (re)calculated.[PAR]",
@@ -95,7 +95,7 @@ int main(int argc,char *argv[])
     { efSTO, "-c", "confout", ffWRITE },
     { efENX, "-e", "ener",    ffWRITE },
     { efLOG, "-g", "md",      ffWRITE },
-    { efXVG, "-dvdl", "dvdl", ffOPTWR },
+    { efXVG, "-dgdl", "dgdl", ffOPTWR },
     { efTRX, "-rerun", "rerun", ffOPTRD },
     /* function "optRerunMDset" (in runner.c) checks if -rerun is specified */
     /* { efHAT, "-hat","ghat",   ffOPTRD }, currently useless */
--- src/mdlib/runner.c
+++ src/mdlib/runner.c
@@ -312,8 +312,8 @@ void init_md(t_commrec *cr,t_inputrec *ir,real *t,real *t0,
       if (ir->bPert)
 	*fp_dvdl =
 	  xvgropen(opt2fn("-dvdl",nfile,fnm),
-		   "dV/d\\8l\\4","Time (ps)",
-		   "dV/d\\8l\\4 (kJ mol\\S-1\\N nm\\S-2\\N \\8l\\4\\S-1\\N)");
+		   "dG/d\\8l\\4","Time (ps)",
+		   "dG/d\\8l\\4 (kJ mol\\S-1\\N nm\\S-2\\N \\8l\\4\\S-1\\N)");
       *mdebin = init_mdebin(*fp_ene,grps,&(top->atoms),&(top->idef),
 			    bLR,bLJLR,bBHAM,b14,ir->bPert,ir->epc,
 			    ir->bDispCorr);
