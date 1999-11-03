ded20ebd07864397cb981a300bbbad0bc416bf59
hess <hess>
1999-11-03 19:17:53 +0000

    fixed damage done by copyrgt

--- src/kernel/calcgrid.h
+++ src/kernel/calcgrid.h
@@ -28,6 +28,7 @@
  */
 static char *SRCID_calcgrid_h = "$Id$";
 
+real calc_grid(matrix box,real gr_sp,int *nx,int *ny,int *nz,int nprocs);
 /* Sets the number of grid points for each zero n* to the first reasonable
  * number which gives a spacing equal to or smaller than gr_sp.
  * nx and ny should be divisible by nprocs, an error is generated when this
