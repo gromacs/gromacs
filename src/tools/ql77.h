8b2f99a7c0f64eaba20cdfd5858ed9c3d50073d7
hess <hess>
1999-11-04 14:45:36 +0000

    fixed damage of copyrgt

--- src/tools/ql77.h
+++ src/tools/ql77.h
@@ -28,6 +28,7 @@
  */
 static char *SRCID_ql77_h = "$Id$";
 
+extern void ql77 (int n,real *x,real *d);
 /* Determine the eigenvalues d[n] and eigenvectors   *
  * of the symmetric n x n matrix x. The eigenvectors *
  * are stored  in x as x[i+n*v], where v is the      *
