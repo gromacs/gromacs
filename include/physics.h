39554e895b27d045d5b553ae6a1c249ba8f4990f
hess <hess>
2000-03-23 17:07:32 +0000

    undid last change and fixed the formatting

--- include/physics.h
+++ include/physics.h
@@ -52,33 +52,34 @@ static char *SRCID_physics_h = "$Id$";
 #endif
 #endif
 
-#define ANGSTROM	 (1e-10)			/* Old...	*/
-#define KILO 		 (1e3)				/* Thousand	*/
-#define NANO		 (1e-9)				/* A Number	*/
-#define PICO		 (1e-12)			/* A Number	*/
-#define A2NM		 (ANGSTROM/NANO)		/* NANO	        */
-#define NM2A		 (NANO/ANGSTROM)		/* 10.0		*/
-#define RAD2DEG		 (180.0/M_PI)			/* Conversion	*/
-#define DEG2RAD		 (M_PI/180.0)			/* id		*/
-#define CAL2JOULE	 (4.184)			/* id		*/
-#define E_CHARGE         (1.60217733e-19)		/* Coulomb	*/
+#define ANGSTROM	 (1e-10)		/* Old...	*/
+#define KILO 		 (1e3)			/* Thousand	*/
+#define NANO		 (1e-9)			/* A Number	*/
+#define PICO		 (1e-12)		/* A Number	*/
+#define A2NM		 (ANGSTROM/NANO)	/* NANO	        */
+#define NM2A		 (NANO/ANGSTROM)	/* 10.0		*/
+#define RAD2DEG		 (180.0/M_PI)		/* Conversion	*/
+#define DEG2RAD		 (M_PI/180.0)		/* id		*/
+#define CAL2JOULE	 (4.184)		/* id		*/
+#define E_CHARGE         (1.60217733e-19)	/* Coulomb	*/
 
-#define AMU              (1.6605402e-27)                /* kg           */
-#define BOLTZMANN	 (1.380658e-23)			/* (J/K)	*/
-#define AVOGADRO	 (6.0221367e23)			/* ()		*/
-#define RGAS             (BOLTZMANN*AVOGADRO/KILO)      /* (kJ/(mol K)) */
-#define BOLTZ            (RGAS)	                        /* (kJ/(mol K))	*/
-#define FARADAY          (E_CHARGE*AVOGADRO)            /* (C/mol)      */
+#define AMU              (1.6605402e-27)        /* kg           */
+#define BOLTZMANN	 (1.380658e-23)		/* (J/K)	*/
+#define AVOGADRO	 (6.0221367e23)		/* ()		*/
+#define RGAS             (BOLTZMANN*AVOGADRO)   /* (J/(mol K))  */
+#define BOLTZ            (RGAS/KILO)            /* (kJ/(mol K)) */
+#define FARADAY          (E_CHARGE*AVOGADRO)    /* (C/mol)      */
 
-#define EPSILON0 	 (5.72765E-4)			/* (e^2 Na/(kJ nm))    */
-#define SPEEDOFLIGHT     (3.0e5)                        /* nm/ps               */
-#define ATOMICMASS_keV   (940000.0)                     /* Atomic mass in keV  */
-#define ELECTRONMASS_keV (512.0)                        /* Electron mas in keV */
+#define EPSILON0 	 (5.72765E-4)		/* (e^2 Na/(kJ nm))     */
+#define SPEEDOFLIGHT     (3.0e5)                /* nm/ps                */
+#define ATOMICMASS_keV   (940000.0)             /* Atomic mass in keV   */
+#define ELECTRONMASS_keV (512.0)                /* Electron mas in keV  */
 
-#define FACEL		  332.0636*CAL2JOULE	        /* (sqrt(ONE_4PI_EPS0)) */
+#define FACEL		  332.0636*CAL2JOULE    /* (sqrt(ONE_4PI_EPS0)) */
 #define ONE_4PI_EPS0	  FACEL*0.1
-#define PRESFAC           (16.6054)                     /* bar / pressure unity */
-#define ENM2DEBYE         48.0321               /* Convert electron nm to debye */
+#define PRESFAC           (16.6054)             /* bar / pressure unity */
+#define ENM2DEBYE         48.0321               /* Convert electron nm  *
+						 * to debye             */
 
 /* to convert from a acceleration in (e V)/(amu nm) */
 /* FIELDFAC is also Faraday's constant and E_CHARGE/(1e6 AMU) */
