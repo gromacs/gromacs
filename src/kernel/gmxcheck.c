d5dc2165608ce39fbda93e29109b2647b883db35
hess <hess>
2000-03-28 11:16:14 +0000

    now default niced

--- src/kernel/gmxcheck.c
+++ src/kernel/gmxcheck.c
@@ -385,7 +385,7 @@ int main(int argc,char *argv[])
   };
 
   CopyRight(stdout,argv[0]);
-  parse_common_args(&argc,argv,0,FALSE,NFILE,fnm,asize(pa),pa,
+  parse_common_args(&argc,argv,0,TRUE,NFILE,fnm,asize(pa),pa,
 		    asize(desc),desc,0,NULL);
   
   if (ftp2bSet(efTRX,NFILE,fnm))
