5b75cf948e51a6be9f7db74f9b9d2717119fd73d
anton <anton>
2000-07-18 07:52:39 +0000

    cleaned up and uses pdb model numbering for output

--- src/tools/g_confrms.c
+++ src/tools/g_confrms.c
@@ -28,11 +28,6 @@
  */
 static char *SRCID_g_confrms_c = "$Id$";
 
-/* 
- * fit coordinates of file 1 to coordinates of file 2
- * fit is done on the atoms as indicated by the NDX- file 
- */
-
 #include "filenm.h"
 #include "smalloc.h"
 #include "macros.h"
@@ -53,25 +48,21 @@ static char *SRCID_g_confrms_c = "$Id$";
 #include "txtdump.h"
 #include "do_fit.h"
 
-#define EPS  1.0e-09
-
 void rm_gropbc(t_atoms *atoms,rvec x[],matrix box)
 {
   real dist;
   int  n,d;
   
   /* check periodic boundary */
-  for(d=0;(d<DIM);d++) {
-    for(n=1;(n<atoms->nr);n++) {
+  for(d=0; d<DIM; d++)
+    for(n=1; n<atoms->nr; n++) {
       dist = x[n][d]-x[n-1][d];
-      if ( fabs(dist) > 0.9 * box[d][d]  ) {
+      if ( fabs(dist) > 0.9 * box[d][d]  )
 	if ( dist >  0 )
 	  x[n][d]-=box[d][d];
 	else
 	  x[n][d]+=box[d][d];
-      } 	
     }
-  }
 }
 
 int main (int argc,char *argv[])
@@ -83,217 +74,175 @@ int main (int argc,char *argv[])
     "only the two index groups used for the fit need to be identical.",
     "[PAR]",
     "The superimposed structures are written to file. In a [TT].pdb[tt] file",
-    "the two structures will have chain identifiers 'A' and 'B' respectively.",
-    "When the option [TT]-one[tt] is set, only the fitted structure is",
-    "written to file and the chain identifiers are not changed."
+    "the two structures will be written as separate models",
+    "(use [TT]rasmol -nmrpdb[tt]).",
   };
-  static bool bSecond=FALSE,bRmpbc=FALSE;
+  static bool bOne=FALSE,bRmpbc=FALSE;
   
   t_pargs pa[] = {
-    { "-one", FALSE, etBOOL, {&bSecond}, "Only write the fitted structure to file" },
-    { "-pbc", FALSE, etBOOL, {&bRmpbc}, "Try to make molecules whole again" }
+    { "-one",FALSE,etBOOL,{&bOne},"Only write the fitted structure to file" },
+    { "-pbc",FALSE,etBOOL,{&bRmpbc},"Try to make molecules whole again" }
   };
   t_filenm fnm[] = {
     { efTPS, "-f1",  "conf1.gro", ffREAD  },
-    { efSTX, "-f2",  "conf2", ffREAD  },
-    { efSTO, "-o",    "fit.pdb",  ffWRITE },
-    { efNDX, "-n1" ,  "fit1.ndx",   ffOPTRD  },
-    { efNDX, "-n2" ,  "fit2.ndx",   ffOPTRD  }
+    { efSTX, "-f2",  "conf2",     ffREAD  },
+    { efSTO, "-o",   "fit.pdb",   ffWRITE },
+    { efNDX, "-n1" , "fit1.ndx",  ffOPTRD },
+    { efNDX, "-n2" , "fit2.ndx",  ffOPTRD }
   };
-
 #define NFILE asize(fnm)
   
-  /* the two gromos files */
+  /* the two structure files */
   FILE    *fp;
-  char    title_1[STRLEN],title_2[STRLEN],*name1,*name2;
+  char    title1[STRLEN],title2[STRLEN],*name1,*name2;
   t_topology top;
-  t_atoms atoms_1,atoms_2;
-  int     natoms_1,natoms_2,warn=0;
+  t_atoms atoms1,atoms2;
+  int     natoms1,natoms2,warn=0;
   matrix  box;
   atom_id at;
   real    *w_rls,mass,totmass;
-  rvec    *x_1,*v_1,*x_2,*v_2,*xf_1,*xf_2,*fit_x;
-  matrix  box_1,box_2;
+  rvec    *x1,*v1,*x2,*v2,*fit_x;
+  matrix  box1,box2;
   
   /* counters */
-  int   i,j,m;
+  int     i,j,m;
   
   /* center of mass calculation */
-  real tmas_1,tmas_2;
-  rvec xcm_1,xcm_2;
-
+  real    tmas1,tmas2;
+  rvec    xcm1,xcm2;
+  
   /* variables for fit */
-  char *groupnames_1,*groupnames_2;
-  int isize_1,isize_2;
-  atom_id *index_1,*index_2;
-  real rms;
-
+  char    *groupnames1,*groupnames2;
+  int     isize1,isize2;
+  atom_id *index1,*index2;
+  real    rms;
+  
   CopyRight(stderr,argv[0]);
   parse_common_args(&argc,argv,0,TRUE,
 		    NFILE,fnm,asize(pa),pa,asize(desc),desc,0,NULL);
   
-  /* reading first structure file, nb this is going 
-   * to be the reference structure*/
+  /* reading reference structure from first structure file */
   fprintf(stderr,"\nReading first structure file\n");
-  read_tps_conf(ftp2fn(efTPS,NFILE,fnm),title_1,&top,&x_1,&v_1,box,TRUE);
-  atoms_1 = top.atoms;
+  read_tps_conf(ftp2fn(efTPS,NFILE,fnm),title1,&top,&x1,&v1,box,TRUE);
+  atoms1 = top.atoms;
   fprintf(stderr,"%s\nContaining %d atoms in %d residues\n",
-	  title_1,atoms_1.nr,atoms_1.nres);
-  srenew(atoms_1.resname,atoms_1.nres);
-    
+	  title1,atoms1.nr,atoms1.nres);
+  srenew(atoms1.resname,atoms1.nres);
+  
   if ( bRmpbc ) 
-    rm_gropbc(&atoms_1,x_1,box_1);
-
-
-  /* check if we have an NDX file */
-  /*
-  if ( opt2bSet("-n1",NFILE,fnm) ) {
-    fprintf(stderr,"\nSelect group for root least square fit\n");
-    rd_index(opt2fn("-n1",NFILE,fnm),1,&isize_1,&index_1,&groupnames_1);
-  } else {
-    isize_1 = atoms_1.nr;
-    snew(index_1,isize_1);
-    for(i=0;(i<isize_1);i++)
-      index_1[i]=i;
-    groupnames_1 = title_1;
-  }
-  */
-  get_index(&atoms_1,opt2fn_null("-n1",NFILE,fnm),
-	    1,&isize_1,&index_1,&groupnames_1);
- 
-  if (isize_1 < 3) 
+    rm_gropbc(&atoms1,x1,box1);
+  
+  get_index(&atoms1,opt2fn_null("-n1",NFILE,fnm),
+	    1,&isize1,&index1,&groupnames1);
+  
+  if (isize1 < 3) 
     fatal_error(0,"Need >= 3 points to fit!\n");
-
-
-  /* reading second gromos file */
-  get_stx_coordnum(opt2fn("-f2",NFILE,fnm),&(atoms_2.nr));
-  snew(x_2,atoms_2.nr);
-  snew(v_2,atoms_2.nr);
-  snew(atoms_2.resname,atoms_2.nr);
-  snew(atoms_2.atom,atoms_2.nr);
-  snew(atoms_2.atomname,atoms_2.nr);
+  
+  /* reading second structure file */
+  get_stx_coordnum(opt2fn("-f2",NFILE,fnm),&(atoms2.nr));
+  snew(x2,atoms2.nr);
+  snew(v2,atoms2.nr);
+  snew(atoms2.resname,atoms2.nr);
+  snew(atoms2.atom,atoms2.nr);
+  snew(atoms2.atomname,atoms2.nr);
   fprintf(stderr,"\nReading second structure file\n");
-  read_stx_conf(opt2fn("-f2",NFILE,fnm),title_2,&atoms_2,x_2,v_2,box_2);
+  read_stx_conf(opt2fn("-f2",NFILE,fnm),title2,&atoms2,x2,v2,box2);
   fprintf(stderr,"%s\nContaining %d atoms in %d residues\n",
-	  title_2,atoms_2.nr,atoms_2.nres);
-  srenew(atoms_2.resname,atoms_2.nres);
-
+	  title2,atoms2.nr,atoms2.nres);
+  srenew(atoms2.resname,atoms2.nres);
+  
   if ( bRmpbc ) 
-    rm_gropbc(&atoms_2,x_2,box_2);
-
-  get_index(&atoms_2,opt2fn_null("-n2",NFILE,fnm),
-	    1,&isize_2,&index_2,&groupnames_2);
-
+    rm_gropbc(&atoms2,x2,box2);
+  
+  get_index(&atoms2,opt2fn_null("-n2",NFILE,fnm),
+	    1,&isize2,&index2,&groupnames2);
+  
   /* check isizes, must be equal */
-  if ( isize_2 != isize_1 )
-    fatal_error(0,"isize_2 != isize_2\n");
-
-  if (isize_2 < 3) 
+  if ( isize2 != isize1 )
+    fatal_error(0,"isize2 != isize2\n");
+  
+  if (isize2 < 3) 
     fatal_error(0,"Need >= 3 points to fit!\n");
-
-  for(i=0; (i<isize_1); i++) {
-    name1=*atoms_1.atomname[index_1[i]];
-    name2=*atoms_2.atomname[index_2[i]];
+  
+  for(i=0; i<isize1; i++) {
+    name1=*atoms1.atomname[index1[i]];
+    name2=*atoms2.atomname[index2[i]];
     if (strcmp(name1,name2)) {
       if (warn<20)
 	fprintf(stderr,
 		"Warning: atomnames at index %d don't match: %u %s, %u %s\n",
-		i+1,index_1[i]+1,name1,index_2[i]+1,name2);
+		i+1,index1[i]+1,name1,index2[i]+1,name2);
       warn++;
     }
   }
   if (warn)
     fprintf(stderr,"%d atomname%s did not match\n",warn,(warn==1) ? "":"s");
-
+  
   /* calculate center of mass of reference structure */
-  for(m=0;(m<3);m++)
-    xcm_1[m]=0;
-  for(i=0;(i<isize_1);i++)
-    for(m=0;(m<DIM);m++) 
-      xcm_1[m]+=x_1[index_1[i]][m];
-  for(m=0;(m<3);m++)
-    xcm_1[m]/=isize_1;
-  for(i=0;(i<atoms_1.nr);i++)
-    for(m=0;(m<DIM);m++)
-      x_1[i][m]-=xcm_1[m];
+  for(m=0; m<DIM; m++) {
+    xcm1[m]=0;
+    for(i=0; i<isize1; i++)
+      xcm1[m]+=x1[index1[i]][m];
+    xcm1[m]/=isize1;
+    for(i=0; i<atoms1.nr; i++)
+      x1[i][m]-=xcm1[m];
+  }
   
   /* calculate center of mass of structure to be fitted */
-  for(m=0;(m<3);m++)
-    xcm_2[m]=0;
-  for(i=0;(i<isize_2);i++)
-    for(m=0;(m<DIM);m++) 
-      xcm_2[m]+=x_2[index_2[i]][m];
-  for(m=0;(m<3);m++)
-    xcm_2[m]/=isize_2;
-  for(i=0;(i<atoms_2.nr);i++)
-    for(m=0;(m<DIM);m++)
-      x_2[i][m]-=xcm_2[m];
-  
-  snew(w_rls,atoms_2.nr);
-  snew(fit_x,atoms_2.nr);
-  for(at=0; at<isize_1; at++) {
-    w_rls[index_2[at]] = 1.0;
-    copy_rvec(x_1[index_1[at]],fit_x[index_2[at]]);
+  for(m=0; m<DIM; m++) {
+    xcm2[m]=0;
+    for(i=0; i<isize2; i++)
+      xcm2[m]+=x2[index2[i]][m];
+    xcm2[m]/=isize2;
+    for(i=0; i<atoms2.nr; i++)
+      x2[i][m]-=xcm2[m];
   }
-
-  /*do the least squares fit to the reference structure*/
-  /*
-  rms = my_fit(isize_1,index_1,index_2,x_1,atoms_2.nr,x_2);
-  */
-  do_fit(atoms_2.nr,w_rls,fit_x,x_2);
- 
+  
+  snew(w_rls,atoms2.nr);
+  snew(fit_x,atoms2.nr);
+  for(at=0; at<isize1; at++) {
+    w_rls[index2[at]] = 1.0;
+    copy_rvec(x1[index1[at]],fit_x[index2[at]]);
+  }
+  
+  /* do the least squares fit to the reference structure */
+  do_fit(atoms2.nr,w_rls,fit_x,x2);
+  
+  /* calculate the rms deviation */
   rms     = 0;
   totmass = 0;
-  for(at=0; at<isize_1; at++) {
-    mass     = w_rls[index_2[at]];
-    for(m=0; m<3; m++)
-      rms     += sqr(x_1[index_1[at]][m] - x_2[index_2[at]][m])*mass;
+  for(at=0; at<isize1; at++) {
+    mass = w_rls[index2[at]];
+    for(m=0; m<DIM; m++)
+      rms += sqr(x1[index1[at]][m] - x2[index2[at]][m])*mass;
     totmass += mass;
   }
   rms = sqrt(rms/totmass);
-
+  
   fprintf(stderr,"Root mean square deviation after lsq fit = %g\n",rms);
-
+  
   /* reset coordinates of reference and fitted structure */
-  for(i=0;(i<atoms_1.nr);i++) {
-    for(m=0;(m<3);m++)
-      x_1[i][m]+=xcm_1[m];
-  }
-  for(i=0;(i<atoms_2.nr);i++) {
-    for(m=0;(m<3);m++)
-      x_2[i][m]+=xcm_1[m];
-  }
-
-  /* calculate the rms deviation */
+  for(i=0; i<atoms1.nr; i++)
+    for(m=0; m<DIM; m++)
+      x1[i][m]+=xcm1[m];
+  for(i=0; i<atoms2.nr; i++)
+    for(m=0; m<DIM; m++)
+      x2[i][m]+=xcm1[m];
   
-  
-
   /* write gromos file of fitted structure(s) */
   fp=ffopen(opt2fn("-o",NFILE,fnm),"w");
   if (fn2ftp(opt2fn("-o",NFILE,fnm))==efGRO) {
-    if (!bSecond)
-      write_hconf(fp,title_1,&atoms_1,x_1,v_1,box_1);
-    write_hconf(fp,title_2,&atoms_2,x_2,v_2,box_2);
+    if (!bOne)
+      write_hconf(fp,title1,&atoms1,x1,v1,box1);
+    write_hconf(fp,title2,&atoms2,x2,v2,box2);
   } else {
-    if (bSecond)
-      write_pdbfile(fp,title_2,&atoms_2,x_2,box_2,0,TRUE);
-    else {
-      write_pdbfile(fp,title_1,&atoms_1,x_1,box_1,'A',FALSE);
-      write_pdbfile(fp,title_2,&atoms_2,x_2,box_2,'B',TRUE);
-    }
+    if (!bOne)
+      write_pdbfile(fp,title1,&atoms1,x1,box1,0,1);
+    write_pdbfile(fp,title2,&atoms2,x2,box2,0,bOne ? -1 : 2);
   }
   fclose(fp);
-
+  
   thanx(stdout);
   
   return 0;
 }
-
-
-
-
-
-
-
-
-
