c5da3905bbb7cfff2ffc513796a1b28c6efbb8ca
lindahl <lindahl>
2000-11-08 18:23:33 +0000

    Added the c_rf reaction-field constant to the inner loops, and implemented coul-only rf SSE loops.

--- include/inner_sse.h
+++ include/inner_sse.h
@@ -31,10 +31,15 @@ void inl1100_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
 		 float shiftvec[],float fshift[],int gid[],float pos[],
 		 float faction[],float charge[],float facel,float Vc[],
 		 int type[],int ntype,float nbfp[],float Vnb[]);
+void inl2000_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
+		 float shiftvec[],float fshift[],int gid[],float pos[],
+		 float faction[],float charge[],float facel,float Vc[],
+		 float krf, float crf);
 void inl2100_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
 		 float shiftvec[],float fshift[],int gid[],float pos[],
 		 float faction[],float charge[],float facel,float Vc[],
-		 float krf, int type[],int ntype,float nbfp[],float Vnb[]);
+		 float krf, float crf, int type[],int ntype,
+		 float nbfp[],float Vnb[]);
 void inl1110_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
 		 float shiftvec[],float fshift[],int gid[],float pos[],
 		 float faction[],float charge[],float facel,float Vc[],
@@ -44,18 +49,28 @@ void inl1120_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
 		 float shiftvec[],float fshift[],int gid[],float pos[],
 		 float faction[],float charge[],float facel,float Vc[],
 		 int type[],int ntype,float nbfp[],float Vnb[]);
+void inl2020_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
+		 float shiftvec[],float fshift[],int gid[],float pos[],
+		 float faction[],float charge[],float facel,float Vc[],
+		 float krf, float crf);
 void inl2120_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
 		 float shiftvec[],float fshift[],int gid[],float pos[],
 		 float faction[],float charge[],float facel,float Vc[],
-		 float krf, int type[],int ntype,float nbfp[],float Vnb[]);
+		 float krf, float crf, int type[],int ntype,
+		 float nbfp[],float Vnb[]);
 void inl1130_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
 		 float shiftvec[],float fshift[],int gid[],float pos[],
 		 float faction[],float charge[],float facel,float Vc[],
 		 int type[],int ntype,float nbfp[],float Vnb[]);
+void inl2030_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
+		 float shiftvec[],float fshift[],int gid[],float pos[],
+		 float faction[],float charge[],float facel,float Vc[],
+		 float krf, float crf);
 void inl2130_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
 		 float shiftvec[],float fshift[],int gid[],float pos[],
 		 float faction[],float charge[],float facel,float Vc[],
-		 float krf, int type[],int ntype,float nbfp[],float Vnb[]);
+		 float krf, float crf, int type[],int ntype,
+		 float nbfp[],float Vnb[]);
 void inl3000_sse(int nri,int iinr[],int jindex[],int jjnr[],int shift[],
 		 float shiftvec[],float fshift[],int gid[],float pos[],
 		 float faction[],float charge[],float facel,float Vc[],
