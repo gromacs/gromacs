8a32349df026423f514a2c5584da2bcf9de27f91
lindahl <lindahl>
2001-03-20 12:05:53 +0000

    Enabled x86 truncation by default for gcc and x86 in pme.c, this is already default everywhere else.

--- src/mdlib/pme.c
+++ src/mdlib/pme.c
@@ -109,7 +109,7 @@ void calc_idx(int natoms,matrix recipbox,
   int  *idxptr,tix,tiy,tiz;
   real *xptr,tx,ty,tz;
   real rxx,ryx,ryy,rzx,rzy,rzz;
-#if (defined __GNUC__ && defined _lnx_ && defined FAST_X86TRUNC)
+#if (defined __GNUC__ && defined _lnx_ && !defined DISABLE_X86TRUNC)
   int x86_cw,x86_cwsave;
 
   asm("fnstcw %0" : "=m" (*&x86_cwsave));
@@ -134,7 +134,7 @@ void calc_idx(int natoms,matrix recipbox,
     ty = ny + ny * (                  xptr[YY] * ryy + xptr[ZZ] * rzy );
     tz = nz + nz * (                                   xptr[ZZ] * rzz );
     
-#if (defined __GNUC__ && defined _lnx_ && defined FAST_X86TRUNC)
+#if (defined __GNUC__ && defined _lnx_ && !defined DISABLE_X86TRUNC)
     x86trunc(tx,tix);
     x86trunc(ty,tiy);
     x86trunc(tz,tiz);
@@ -157,7 +157,7 @@ void calc_idx(int natoms,matrix recipbox,
     range_check(idxptr[ZZ],nz);
 #endif
   }  
-#if (defined __GNUC__ && defined _lnx_ && defined FAST_X86TRUNC)  
+#if (defined __GNUC__ && defined _lnx_ && !defined DISABLE_X86TRUNC)  
   asm("fldcw %0" : : "m" (*&x86_cwsave));
 #endif
 }
