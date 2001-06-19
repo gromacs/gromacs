c93d46e27fe1bacdab4417e3b2dcd1eb7d15c6ca
hess <hess>
2001-06-19 12:01:45 +0000

    added options.c to generate the options html and tex manual pages

---
+++ src/contrib/options.c
@@ -0,0 +1,106 @@
+/*
+ * $Id$
+ * 
+ *                This source code is part of
+ * 
+ *                 G   R   O   M   A   C   S
+ * 
+ *          GROningen MAchine for Chemical Simulations
+ * 
+ *                        VERSION 3.0
+ * 
+ * Copyright (c) 1991-2001
+ * BIOSON Research Institute, Dept. of Biophysical Chemistry
+ * University of Groningen, The Netherlands
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * If you want to redistribute modifications, please consider that
+ * scientific software is very special. Version control is crucial -
+ * bugs must be traceable. We will be happy to consider code for
+ * inclusion in the official distribution, but derived work must not
+ * be called official GROMACS. Details are found in the README & COPYING
+ * files - if they are missing, get the official version at www.gromacs.org.
+ * 
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the papers on the package - you can find them in the top README file.
+ * 
+ * Do check out http://www.gromacs.org , or mail us at gromacs@gromacs.org .
+ * 
+ * And Hey:
+ * GROup of MAchos and Cynical Suckers
+ */
+static char *SRCID_options_c = "$Id$";
+
+#include "typedefs.h"
+#include "macros.h"
+#include "statutil.h"
+#include "copyrite.h"
+
+/* 
+ * This program is needed to create the files:
+ *   options.html
+ *   options.tex
+ * for the html and latex manuals.
+ * It should be ran with the option: -hidden
+ */
+
+int main(int argc,char *argv[])
+{
+  static char *desc[] = {
+    "All GROMACS programs have 6 standard options,",
+    "of which 4 are hidden by default:"
+  };
+
+  static char *bugs[] = {
+    "If the configuration script found Motif (or Lesstif) on your system, "
+    "all GROMACS programs will have an additional option:[BR]"
+    "[TT]-X[tt] bool [TT]no[tt] Use dialog box GUI to edit command line options",
+    
+    "When compiled on an SGI-IRIX system, all GROMACS programs have an "
+    "additional option:[BR]"
+    "[TT]-npri[tt] int [TT]0[tt] Set non blocking priority (try 128)",
+
+    "Optional files are not used unless the option is set, in contrast to "
+    "non optional files, where the default file name is used when the "
+    "option is not set.",
+
+    "All GROMACS programs will accept file options without a file extension "
+    "or filename being specified. In such cases the default filenames will "
+    "be used. With multiple input file types, such as generic structure "
+    "format, the directory will be searched for files of each type with the "
+    "supplied or default name. When no such file is found, or with output "
+    "files the first file type will be used.",
+
+    "All GROMACS programs with the exception of [TT]mdrun[tt], "
+    "[TT]nmrun[tt] and [TT]eneconv[tt] check if the command line options "
+    "are valid.  If this is not the case, the program will be halted.",
+
+    "Enumerated options (enum) should be used with one of the arguments "
+    "listed in the option description, the argument may be abbreviated. "
+    "The first match to the shortest argument in the list will be selected.",
+
+    "Vector options can be used with 1 or 3 parameters. When only one "
+    "parameter is supplied the two others are also set to this value.",
+
+    "All GROMACS programs can read compressed or g-zipped files. There "
+    "might be a problem with reading compressed [TT].xtc[tt], "
+    "[TT].trr[tt] and [TT].trj[tt] files, but these will not compress "
+    "very well anyway.",
+
+    "Most GROMACS programs can process a trajectory with less atoms than "
+    "the run input or structure file, but only if the trajectory consists "
+    "of the first n atoms of the run input or structure file."
+  };
+
+  CopyRight(stderr,argv[0]);
+  parse_common_args(&argc,argv,0,TRUE,
+		    0,NULL,0,NULL,asize(desc),desc,asize(bugs),bugs);
+  
+  thanx(stderr);
+  
+  return 0;
+}
