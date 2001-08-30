ac0958e304aebf94ad3a5aa16adefb8affd6eb5f
spoel <spoel>
2001-08-30 14:48:23 +0000

    Use more correct boundaries to prevent SEGVs

--- src/mdlib/tables.c
+++ src/mdlib/tables.c
@@ -74,67 +74,36 @@ typedef struct {
 #define pow3(x) ((x)*(x)*(x))
 #define pow4(x) ((x)*(x)*(x)*(x))
 
-static void spline(real x[],real y[],int n,real yp1,real ypn,real y2[])
-{
-  int  i,k;
-  real p,qn,sig,un,*u;
-  
-  snew(u,n+1);
-  if (yp1 > 0.99e30)
-    y2[1]=u[1]=0.0;
-  else {
-    y2[1] = -0.5;
-    u[1]=(3.0/(x[2]-x[1]))*((y[2]-y[1])/(x[2]-x[1])-yp1);
-  }
-  for (i=2;i<=n-1;i++) {
-    sig=(x[i]-x[i-1])/(x[i+1]-x[i-1]);
-    p=sig*y2[i-1]+2.0;
-    y2[i]=(sig-1.0)/p;
-    u[i]=(y[i+1]-y[i])/(x[i+1]-x[i]) - (y[i]-y[i-1])/(x[i]-x[i-1]);
-    u[i]=(6.0*u[i]/(x[i+1]-x[i-1])-sig*u[i-1])/p;
-  }
-  if (ypn > 0.99e30)
-    qn=un=0.0;
-  else {
-    qn=0.5;
-    un=(3.0/(x[n]-x[n-1]))*(ypn-(y[n]-y[n-1])/(x[n]-x[n-1]));
-  }
-  y2[n]=(un-qn*u[n-1])/(qn*y2[n-1]+1.0);
-  for (k=n-1;k>=1;k--)
-    y2[k]=y2[k]*y2[k+1]+u[k];
-  sfree(u);
-}
-
 static void splint(real xa[],real ya[],real y2a[],
 		   int n,real x,real *y,real *yp)
 {
   int  klo,khi,k;
   real h,b,a,eps;
+  real F,G,H;
   
   klo=1;
   khi=n;
-  while (khi-klo > 1) {
+  while ((khi-klo) > 1) {
     k=(khi+klo) >> 1;
-    if (xa[k] > x) khi=k;
-    else klo=k;
+    if (xa[k] > x) 
+      khi=k;
+    else 
+      klo=k;
   }
-  h=xa[khi]-xa[klo];
-  if (h == 0.0) fatal_error(0,"Bad XA input to routine SPLINT");
-  a=(xa[khi]-x)/h;
-  b=(x-xa[klo])/h;
-  *y=a*ya[klo]+b*ya[khi]+((a*a*a-a)*y2a[klo]+(b*b*b-b)*y2a[khi])*(h*h)/6.0;
-  *yp=(ya[khi]-ya[klo])/h+((3*a*a-1)*y2a[klo]-(3*b*b-1)*y2a[khi])*h/6.0;
+  h = xa[khi]-xa[klo];
+  if (h == 0.0) 
+    fatal_error(0,"Bad XA input to function splint");
+  a   = (xa[khi]-x)/h;
+  b   = (x-xa[klo])/h;
+  *y  = a*ya[klo]+b*ya[khi]+((a*a*a-a)*y2a[klo]+(b*b*b-b)*y2a[khi])*(h*h)/6.0;
+  *yp = (ya[khi]-ya[klo])/h+((3*a*a-1)*y2a[klo]-(3*b*b-1)*y2a[khi])*h/6.0;
   
-  {
-    real F,G,H;
-    
-    eps = b;
-    F = (ya[khi]-ya[klo]-(h*h/6.0)*(2*y2a[klo]+y2a[khi]));
-    G = (h*h/2.0)*y2a[klo];
-    H   = (h*h/6.0)*(y2a[khi]-y2a[klo]);
-    *y  = ya[klo] + eps*F + eps*eps*G + eps*eps*eps*H;
-    *yp = (F + 2*eps*G + 3*eps*eps*H)/h;
-  }
+  eps = b;
+  F   = (ya[khi]-ya[klo]-(h*h/6.0)*(2*y2a[klo]+y2a[khi]));
+  G   = (h*h/2.0)*y2a[klo];
+  H   = (h*h/6.0)*(y2a[khi]-y2a[klo]);
+  *y  = ya[klo] + eps*F + eps*eps*G + eps*eps*eps*H;
+  *yp = (F + 2*eps*G + 3*eps*eps*H)/h;
 }
 
 static void copy2table(int n,int offset,int stride,
@@ -144,7 +113,7 @@ static void copy2table(int n,int offset,int stride,
   int  i,nn0;
   real F,G,H,h;
     
-  for(i=1; (i<n); i++) {
+  for(i=1; (i<n-1); i++) {
     h   = x[i+1]-x[i];
     F   = (Vtab[i+1]-Vtab[i]-(h*h/6.0)*(2*Vtab2[i]+Vtab2[i+1]));
     G   = (h*h/2.0)*Vtab2[i];
@@ -156,7 +125,7 @@ static void copy2table(int n,int offset,int stride,
     dest[nn0+3] = H;
   }
   if (r_zeros > 0.0) {
-    for(i=1; (i<n); i++) {
+    for(i=1; (i<n-1); i++) {
       if (0.5*(x[i]+x[i+1]) >= r_zeros) {
 	nn0 = offset+i*stride;
 	dest[nn0]   = 0.0;
@@ -177,11 +146,11 @@ static void init_table(FILE *fp,int n,int nx0,int tabsel,
   td->nx0 = nx0;
   td->tabscale = tabscale;
   if (bAlloc) {
-    snew(td->x,td->nx+1);
-    snew(td->v,td->nx+1);
-    snew(td->v2,td->nx+1);
+    snew(td->x,td->nx);
+    snew(td->v,td->nx);
+    snew(td->v2,td->nx);
   }
-  for(i=td->nx0; (i<=td->nx); i++)
+  for(i=td->nx0; (i<td->nx); i++)
     td->x[i] = i/tabscale;
 }
 
@@ -302,7 +271,7 @@ static void fill_table(t_tabledata *td,int tp,t_forcerec *fr)
 #ifdef DEBSW
   fp=xvgropen("switch.xvg","switch","r","s");
 #endif
-  for(i=td->nx0; (i<=td->nx); i++) {
+  for(i=td->nx0; (i<td->nx); i++) {
     r     = td->x[i];
     r2    = r*r;
     r6    = 1.0/(r2*r2*r2);
@@ -569,7 +538,7 @@ void make_tables(FILE *out,t_forcerec *fr,bool bVerbose,char *fn)
   
     if (bDebugMode() && bVerbose) {
       fp=xvgropen(fns[k],fns[k],"r","V"); 
-      for(i=td[k].nx0; (i<td[k].nx); i++) {
+      for(i=td[k].nx0+1; (i<td[k].nx-1); i++) {
 	for(j=0; (j<4); j++) {
 	  x0=td[k].x[i]+0.25*j*(td[k].x[i+1]-td[k].x[i]);
 	  splint(td[k].x,td[k].v,td[k].v2,nx-3,x0,&y0,&yp);
