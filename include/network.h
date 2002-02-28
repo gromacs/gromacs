b5187b06c9f35067fe59e4b375b0d89343f5232f
spoel <spoel>
2002-02-28 21:55:48 +0000

    Updated copyrgt

--- include/network.h
+++ include/network.h
@@ -7,12 +7,8 @@
  * 
  *          GROningen MAchine for Chemical Simulations
  * 
- *                        VERSION 3.0
- * 
- * Copyright (c) 1991-2001
- * BIOSON Research Institute, Dept. of Biophysical Chemistry
- * University of Groningen, The Netherlands
- * 
+ *                        VERSION 3.1
+ * Copyright (c) 1991-2001, University of Groningen, The Netherlands
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -28,16 +24,16 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the papers on the package - you can find them in the top README file.
  * 
- * Do check out http://www.gromacs.org , or mail us at gromacs@gromacs.org .
+ * For more info, check our website at http://www.gromacs.org
  * 
  * And Hey:
- * GRoups of Organic Molecules in ACtion for Science
+ * Getting the Right Output Means no Artefacts in Calculating Stuff
  */
+#ifndef _network_h
+#define _network_h
 
-#ifndef _gmx_comm_h
-#define _gmx_comm_h
+static char *SRCID_network_h = "$Id$";
 
-static char *SRCID_gmx_comm_h = "$Id$";
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -193,4 +189,4 @@ if (bDebugMode()) fprintf(fp,"NODEID=%d, %s  %d\n",gmx_node_id(),__FILE__,__LINE
 #define debug_gmx()
 #endif
 
-#endif	/* _gmx_comm_h */
+#endif	/* _network_h */
--- include/nrama.h
+++ include/nrama.h
@@ -7,12 +7,8 @@
  * 
  *          GROningen MAchine for Chemical Simulations
  * 
- *                        VERSION 3.0
- * 
- * Copyright (c) 1991-2001
- * BIOSON Research Institute, Dept. of Biophysical Chemistry
- * University of Groningen, The Netherlands
- * 
+ *                        VERSION 3.1
+ * Copyright (c) 1991-2001, University of Groningen, The Netherlands
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -28,10 +24,10 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the papers on the package - you can find them in the top README file.
  * 
- * Do check out http://www.gromacs.org , or mail us at gromacs@gromacs.org .
+ * For more info, check our website at http://www.gromacs.org
  * 
  * And Hey:
- * Good ROcking Metal Altar for Chronical Sinners
+ * Getting the Right Output Means no Artefacts in Calculating Stuff
  */
 
 #ifndef _nrama_h
