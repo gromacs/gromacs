72ca70d1bcc0087c83507e05f59d0bc14b4f120a
spoel <spoel>
2002-07-17 20:17:33 +0000

    Updated

--- src/contrib/xplor2gmx.pl
+++ src/contrib/xplor2gmx.pl
@@ -13,48 +13,112 @@
 # David van der Spoel (spoel@gromacs.org), July 2002
 #
 
+# Turn debugging off (0), or on ( > 0).
 $debug = 1;
+# Turn atom name translation on and off
+$trans = 1;
 
 $res0  = shift || die "I need the residue offset\n";
 $pdb   = shift || die "I need the name of the pdb file with correct atom numbers\n";
+$tbl   = "/home/spoel/wd/gmx/share/top/atom_nom.tbl";
 
 printf "[ distance_restraints ]\n";
 printf "; Read an xplor distance restraint file, and output GROMACS distance restraints.\n";
 printf "; This also needs a pdb file with correct GROMACS atom numbering.\n";
 printf "; I used $pdb for the atom numbers\n";
 printf "; This also needs the offset in residues.\n";
-printf "; I used $res0 for the offset\n";
+printf "; I used $res0 for the residue offset\n";
 
+# Read the pdb file
 # If things go wrong, check whether your pdb file is read correctly.
 $natom = 0;
-open(PDB,$pdb) || die "Can not open file $pdb";
+$nres  = 0;
+open(PDB,$pdb) || die "Can not open file $pdb\n";
 while ($line = <PDB>) {
     if (index($line,"ATOM") >= 0) {
 	@tmp = split(' ',$line);
 	$aname[$natom] = $tmp[2];
 	$resnr[$natom] = $tmp[4];
+	if ($resnr[$natom] > $nres) {
+	    $rname[$nres] = $tmp[3];
+	    $nres++;
+	}
 	$natom++;
+	
     }
 }
 close PDB;
 printf "; I found $natom atoms in the pdb file $pdb\n";
+printf "; I found $nres residues in the pdb file $pdb\n";
+if ($debug > 1) {
+    for ($i = 0; ($i < $natom); $i ++) {
+	printf("; %5d  %5s  %5s  %5d\n",$i+1,$aname[$i],
+	       $rname[$resnr[$i]-1],$resnr[$i]);
+    }
+}
+
+#
+# Read the name translation table.
+# Source for this comes from: http://www.bmrb.wisc.edu/ref_info/atom_nom.tbl
+# It was adapted slightly for GROMACS use, but only as regards formatting,
+# not for content
+#
+open(TBL,$tbl) || die "Can not open atom-name translation table $tbl\n";
+$ntbl=0;
+while ($line = <TBL>) {
+    @ttt = split('#',$line);
+    @tmp = split(' ',$ttt[0]);
+    if ($#tmp == 2) {
+	# New table entry
+	$tblres[$ntbl] = $tmp[0];
+	$tblxplor[$ntbl] = $tmp[1];
+	$tblgmx[$ntbl] = $tmp[2];
+	$ntbl++;
+    }
+}
+close TBL;
+printf "; Read $ntbl entries from $tbl\n";
 
 @templates = (
- [ "HH#",	"HH11",	"HH12",	"HH21",	"HH22" ],
- [ "HE#",	"HE1",	"HE2" ],
- [ "HD2#",	"HD21",	"HD22" ],
- [ "HD1#", "HD11",	"HD12" ],
- [ "HD#",  "HD1",  "HD2",	"HD11",	"HD12",	"HD13",	"HD21",	"HD22",	"HD23" ],
- [ "HG2#", "HG21",	"HG22",	"HG23" ],
- [ "HG1#",	"HG11",	"HG12",	"HG13" ],
- [ "HG#",  "HG1",	"HG2" ],
- [ "HB#",	"HB1",	"HB2" ],
- [ "HZ#",  "HZ1", "HZ2", "HZ3" ],
- [ "HN",	"H" ],
+ [ "HH#",  "HH11",	"HH12",	"HH21",	"HH22" ],
+ [ "HE#",  "HE",        "HE1",	"HE2"	],
+ [ "HD2#", "HD21",	"HD22"	],
+ [ "HD1#", "HD11",	"HD12"	],
+ [ "HD#",  "HD1",	"HD2",	"HD11",	"HD12",	"HD13",	"HD21",	"HD22",	"HD23" ],
+ [ "HG2#", "HG21",	"HG22",	"HG23"	],
+ [ "HG1#", "HG11",	"HG12",	"HG13"	],
+ [ "HG#",  "HG",         "HG1",	"HG2",	"HG11",	"HG12",	"HG13",	"HG21",	"HG22",	"HG23"  ],
+ [ "HB#",  "HB",         "HB1",	"HB2"	],
+ [ "HZ#",  "HZ",         "HZ1",	"HZ2",	"HZ3"	],
+ [ "HN",   "H" ],
 );
+
+$ntranslated = 0;
+$nuntransltd = 0;
+sub transl_aname {
+    my $atom   = shift(@_);
+    my $resnr  = shift(@_);
+
+    if ( $trans == 1 ) {    
+	for(my $i = 0; ($i < $ntbl); $i ++) {
+	    if ($tblres[$i] eq $rname[$resnr]) {
+		if ($tblxplor[$i] eq $atom) {
+		    $ntranslated++;
+		    return $tblgmx[$i];
+		}
+	    }
+	}
+    }
+    $nuntransltd++;
+    if ($debug > 1) {
+	printf "; No name change for $rname[$resnr] $atom\n";
+    }
+    return $atom;
+}
 
 sub expand_template {
     my $atom  = shift(@_);
+    my $rnum  = shift(@_)-1;
     my $bdone = 0;
     my @atoms;
     my $jj = 0;
@@ -63,17 +127,18 @@ sub expand_template {
 	$templ = $templates[$tt];
 	if ($atom eq $templ->[0]) {
 	    for ($jj = 1; ($jj <= $#{$templ}); $jj++) {
-		push @atoms, $templ->[$jj];
+		push @atoms, transl_aname($templ->[$jj],$rnum);
 	    }
 	    $bdone  = 1;
 	}
     }
     if ($bdone == 0) {
-	push @atoms, $atom;
+	push @atoms, transl_aname($atom,$rnum);
     }
-    if ($debug == 1) {
+    if ($debug > 0) {
 	my $natom = $#atoms+1;
-	printf "; Found $natom elements for atom $atom:";
+	printf("; Found $natom elements for atom $rname[$rnum] %d $atom:",
+	       $rnum+1);
 	for $aa ( @atoms ) {
 	    printf " $aa";
 	}
@@ -82,7 +147,7 @@ sub expand_template {
     return @atoms;
 }
 
-if ($debug == 1) {
+if ($debug > 1) {
     printf "; There are $#templates template entries\n";
     for ($tt=0; ($tt <= $#templates); $tt++) {
 	$templ  = $templates[$tt];
@@ -91,9 +156,10 @@ if ($debug == 1) {
     }
 }
 
-$myindex = 0;
-$linec   = 0;
-$npair   = 0;
+$myindex     = 0;
+$linec       = 0;
+$npair       = 0;
+$nunresolved = 0;
 while ($line = <STDIN>) {
     @ttt = split('!',$line);
     if ((index($ttt[0],"assign") >= 0) && (index($ttt[0],"!assign") < 0)) {
@@ -114,16 +180,17 @@ while ($line = <STDIN>) {
 
 	@expdata = split('\)',$tmp[2]);
 	@dist    = split(' ',$expdata[1]);
-		
+
+	$bOK = 0;
 	foreach $a1 ( @at1 ) {
 	    @info1  = split(' ',$a1);
 	    $r1     = $info1[1] - $res0;
-	    @atoms1 = expand_template($info1[4]);
+	    @atoms1 = expand_template($info1[4],$r1);
 
 	    foreach $a2 ( @at2 ) {
 		@info2  = split(' ',$a2);
 		$r2     = $info2[1] - $res0;
-		@atoms2 = expand_template($info2[4]);
+		@atoms2 = expand_template($info2[4],$r2);
 
 		for ($i = 0; ($i < $natom) && ($resnr[$i] < $r1); $i++) { ; }
 		for ( ; ($i < $natom) && ($resnr[$i] == $r1); $i++) { 
@@ -139,7 +206,8 @@ while ($line = <STDIN>) {
 					$low    = $dd-$dminus;
 					$up1    = $dd+$dplus;
 					$up2    = $up1+1;
-					printf("%5d %5d %5d %5d %5d %7.3f %7.3f %7.3f 1.0; res $r1 $ii - res $r2 $jj\n",$i,$j,1,$myindex,1,$low,$up1,$up2);
+					printf("%5d %5d %5d %5d %5d %7.3f %7.3f %7.3f 1.0; res $r1 $ii - res $r2 $jj\n",$i+1,$j+1,1,$myindex,1,$low,$up1,$up2);
+					$bOK = 1;
 					$npair++;
 				    }
 				}
@@ -150,9 +218,18 @@ while ($line = <STDIN>) {
 	    }
 	    $myindex++;
 	}
+	if ($bOK == 0) {
+	    printf "; UNRESOLVED: $ttt[0]\n";
+	    $nunresolved++;
+	}
     }
     $linec++;
 }
 
 printf "; A total of $myindex lines with distance restraints were read from $linec input lines\n";
 printf "; From this, $npair actual restraints were generated.\n";
+printf "; A total of $nunresolved lines could not be interpreted\n";
+printf "; In the process $ntranslated atoms had a name change\n";
+printf "; However for $nuntransltd no name change could be found\n";
+printf "; usually these are either HN->H renamings or not-existing atoms\n";
+printf "; generated by the template expansion (e.g. HG#)\n";
