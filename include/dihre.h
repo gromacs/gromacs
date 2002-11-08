ce8e75e957a87fb761b0169ca4a4a48f803acaaf
lindahl <lindahl>
2002-11-08 23:27:30 +0000

    Added headers for dihedral restraint routines.

---
+++ include/dihre.h
@@ -0,0 +1,62 @@
+/*
+ * $Id$
+ * 
+ *                This source code is part of
+ * 
+ *                 G   R   O   M   A   C   S
+ * 
+ *          GROningen MAchine for Chemical Simulations
+ * 
+ *                        VERSION 3.1
+ * Copyright (c) 1991-2001, University of Groningen, The Netherlands
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * If you want to redistribute modifications, please consider that
+ * scientific software is very special. Version control is crucial -
+ * bugs must be traceable. We will be happy to consider code for
+ * inclusion in the official distribution, but derived work must not
+ * be called official GROMACS. Details are found in the README & COPYING
+ * files - if they are missing, get the official version at www.gromacs.org.
+ * 
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the papers on the package - you can find them in the top README file.
+ * 
+ * For more info, check our website at http://www.gromacs.org
+ * 
+ * And Hey:
+ * Grunge ROck MAChoS
+ */
+
+#ifndef _dihre_h
+#define _dihre_h
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef CPLUSPLUS
+extern "C" {
+#endif
+
+#include "sysstuff.h"
+#include "typedefs.h"
+
+void init_dihres(FILE *log,int nbonds,t_iatom forceatoms[],t_iparams ip[],
+		 t_inputrec *ir,t_commrec *mcr,t_fcdata *fcd);
+
+
+extern real ta_dihres(int nbonds,t_iatom fa[],t_iparams *fp,
+		      rvec x[],rvec f[],t_forcerec *fr,t_graph *g,
+		      matrix box,real lambda,real *dvdlambda,
+		      t_mdatoms *md,int ngrp,real egnb[],real egcoul[],
+		      t_fcdata *fcd);
+/* Calculate the distance restraint forces, return the potential */
+
+#ifdef CPLUSPLUS
+}
+#endif
+
+#endif	/* _disre_h */
