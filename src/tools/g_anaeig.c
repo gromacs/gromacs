7143c2e62e88622a61e1f3d50478c7d88627dc96
hess <hess>
2003-07-29 15:15:54 +0000

    removed the unit in the -disp output

--- src/tools/g_anaeig.c
+++ src/tools/g_anaeig.c
@@ -580,7 +580,7 @@ static void components(char *outfile,int natoms,real *sqrtm,
     for(i=0; i<natoms; i++)
       y[g][i] = norm(eigvec[v][i])/sqrtm[i];
   }
-  write_xvgr_graphs(outfile,noutvec,"Atom displacements (nm)","Atom number",
+  write_xvgr_graphs(outfile,noutvec,"Atom displacements","Atom number",
 		    ylabel,natoms,x,y,1,TRUE,FALSE);
   fprintf(stderr,"\n");
 }
