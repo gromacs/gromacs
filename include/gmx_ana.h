1c2df9e823a785698f4fed8f1cd9d40ccea9145b
lindahl <lindahl>
2004-01-26 11:22:05 +0000

    Added analysis header that used to be generated in src/tools

---
+++ include/gmx_ana.h
@@ -0,0 +1,146 @@
+#ifndef _gmx_ana_h
+#define _gmx_ana_h
+
+int 
+gmx_analyze(int argc,char *argv[]);
+
+int 
+gmx_anaeig(int argc,char *argv[]);
+
+int 
+gmx_angle(int argc,char *argv[]);
+
+int 
+gmx_bond(int argc,char *argv[]);
+
+int 
+gmx_bundle(int argc,char *argv[]);
+
+int 
+gmx_chi(int argc,char *argv[]);
+
+int 
+gmx_cluster(int argc,char *argv[]);
+
+int 
+gmx_confrms(int argc,char *argv[]);
+
+int 
+gmx_covar(int argc,char *argv[]);
+
+int 
+gmx_density(int argc,char *argv[]);
+
+int 
+gmx_dielectric(int argc,char *argv[]);
+
+int 
+gmx_dih(int argc,char *argv[]);
+
+int 
+gmx_dipoles(int argc,char *argv[]);
+
+int 
+gmx_disre(int argc,char *argv[]);
+
+int 
+gmx_dist(int argc,char *argv[]);
+
+int 
+gmx_dyndom(int argc,char *argv[]);
+
+int 
+gmx_enemat(int argc,char *argv[]);
+
+int 
+gmx_energy(int argc,char *argv[]);
+
+int 
+gmx_lie(int argc,char *argv[]);
+
+int 
+gmx_filter(int argc,char *argv[]);
+
+int 
+gmx_gyrate(int argc,char *argv[]);
+
+int 
+gmx_h2order(int argc,char *argv[]);
+
+int 
+gmx_hbond(int argc,char *argv[]);
+
+int 
+gmx_helix(int argc,char *argv[]);
+
+int 
+gmx_mindist(int argc,char *argv[]);
+
+int 
+gmx_msd(int argc,char *argv[]);
+
+int 
+gmx_morph(int argc,char *argv[]);
+
+int 
+gmx_nmeig(int argc,char *argv[]);
+
+int 
+gmx_nmens(int argc,char *argv[]);
+
+int 
+gmx_order(int argc,char *argv[]);
+
+int 
+gmx_potential(int argc,char *argv[]);
+
+int 
+gmx_rama(int argc,char *argv[]);
+
+int 
+gmx_rdf(int argc,char *argv[]);
+
+int 
+gmx_rms(int argc,char *argv[]);
+
+int 
+gmx_rmsdist(int argc,char *argv[]);
+
+int 
+gmx_rmsf(int argc,char *argv[]);
+
+int 
+gmx_rotacf(int argc,char *argv[]);
+
+int 
+gmx_saltbr(int argc,char *argv[]);
+
+int 
+gmx_sas(int argc,char *argv[]);
+
+int 
+gmx_sgangle(int argc,char *argv[]);
+
+int 
+gmx_sorient(int argc,char *argv[]);
+
+int 
+gmx_tcaf(int argc,char *argv[]);
+
+int 
+gmx_traj(int argc,char *argv[]);
+
+int 
+gmx_velacc(int argc,char *argv[]);
+
+int 
+gmx_clustsize(int argc,char *argv[]);
+
+int 
+gmx_mdmat(int argc,char *argv[]);
+
+int 
+gmx_wham(int argc,char *argv[]);
+
+#endif
+/* _gmx_ana_h */
