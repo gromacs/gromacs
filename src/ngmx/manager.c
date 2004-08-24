ccf5c5652d149591d042e5226fa61a5e4dda6abc
spoel <spoel>
2004-08-24 07:54:13 +0000

    Fixed compilation issue, due to the fact that pbc routines do not retrun a string anymore.

--- src/ngmx/manager.c
+++ src/ngmx/manager.c
@@ -322,11 +322,7 @@ static bool step_man(t_manager *man,int *nat)
   if (ncount == man->nSkip) {
     switch (man->molw->boxtype) {
     case esbTri:
-      warn = put_atoms_in_triclinic_unitcell(man->box,man->natom,man->x);
-      if (warn && !bWarn) {
-	fprintf(stderr,"\n%s\n",warn);
-	bWarn = TRUE;
-      }
+      put_atoms_in_triclinic_unitcell(man->box,man->natom,man->x);
       break;
     case esbTrunc:
       put_atoms_in_compact_unitcell(man->box,man->natom,man->x);
