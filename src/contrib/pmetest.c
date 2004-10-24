97878df63b6c93c60cf36d68eceef58ffbb0468d
spoel <spoel>
2004-10-24 14:05:49 +0000

    Now works even in parallel

--- src/contrib/pmetest.c
+++ src/contrib/pmetest.c
@@ -52,6 +52,7 @@
 #include "ewald_util.h"
 #include "nsb.h"
 #include "pme.h"
+#include "xvgr.h"
 #include "pbc.h"
 #include "mpi.h"
 #include "block_tx.h"
@@ -76,18 +77,18 @@ static int comp_xptr(const void *a,const void *b)
 
 static void do_my_pme(FILE *fp,real tm,bool bVerbose,t_inputrec *ir,
 		      rvec x[],rvec xbuf[],rvec f[],
-		      real charge[],real qbuf[],matrix box,
+		      real charge[],real qbuf[],matrix box,bool bSort,
 		      t_commrec *cr,t_nsborder *nsb,t_nrnb *nrnb,
 		      real ewaldcoeff,t_block *excl,real qtot,
-		      int index[])
+		      int index[],FILE *fp_xvg)
 {
   real   ener,vcorr,q;
   tensor vir,vir_corr,vir_tot;
   rvec   mu_tot;
   int    i,m,ii;
   t_forcerec *fr;
-  bool   bSort = FALSE;
-  
+
+  /* Initiate local variables */  
   snew(fr,1);
   fr->ewaldcoeff = ewaldcoeff;
   fr->f_el_recip = f;
@@ -96,7 +97,7 @@ static void do_my_pme(FILE *fp,real tm,bool bVerbose,t_inputrec *ir,
   
   /* Assume x is in the box */
   clear_rvec(mu_tot);
-  for(i=START(nsb); (i<HOMENR(nsb)); i++) {
+  for(i=START(nsb); (i<START(nsb)+HOMENR(nsb)); i++) {
     q = charge[i];
     for(m=0; (m<DIM); m++) 
       mu_tot[m] += q*x[i][m];
@@ -111,14 +112,17 @@ static void do_my_pme(FILE *fp,real tm,bool bVerbose,t_inputrec *ir,
     xptr = x;
     qsort(index,sizeof(index[0]),nsb->natoms,comp_xptr);
   }
-  for(i=0; (i<nsb->natoms); i++) {
+  for(i=START(nsb); (i<START(nsb)+HOMENR(nsb)); i++) {
     ii = index[i];
     qbuf[i] = charge[ii];
     copy_rvec(x[ii],xbuf[i]);
   }
-  
-  
-  put_atoms_in_box(box,nsb->natoms,x);      
+  if (debug) {
+    pr_rvec(debug,0,"qbuf",qbuf,nsb->natoms,TRUE);
+    pr_rvecs(debug,0,"xbuf",xbuf,nsb->natoms);
+    pr_rvecs(debug,0,"box",box,DIM);
+  }  
+  put_atoms_in_box(box,HOMENR(nsb),xbuf+START(nsb));      
   ener  = do_pme(fp,bVerbose,ir,xbuf,f,qbuf,box,cr,
 		 nsb,nrnb,vir,ewaldcoeff,FALSE);
   vcorr = ewald_LRcorrection(fp,nsb,cr,fr,qbuf,excl,
@@ -129,6 +133,8 @@ static void do_my_pme(FILE *fp,real tm,bool bVerbose,t_inputrec *ir,
   gmx_sum(1,&vcorr,cr);
   fprintf(fp,"Time: %10.3f Energy: %12.5e  Correction: %12.5e  Total: %12.5e\n",
 	  tm,ener,vcorr,ener+vcorr);
+  if (fp_xvg) 
+    fprintf(fp_xvg,"%10.3f %12.5e\n",tm,ener+vcorr);
   if (bVerbose) {
     m_add(vir,vir_corr,vir_tot);
     gmx_sum(9,vir_tot[0],cr);
@@ -149,14 +155,16 @@ int main(int argc,char *argv[])
   static t_filenm fnm[] = {
     { efTPX, NULL,      NULL,       ffREAD  },
     { efTRN, "-o",      NULL,       ffWRITE },
-    { efLOG, "-g",      "pmetest",  ffWRITE },
-    { efTRX, "-f",      NULL,       ffOPTRD }
+    { efLOG, "-g",      "pme",      ffWRITE },
+    { efTRX, "-f",      NULL,       ffOPTRD },
+    { efXVG, "-x",      "ener-pme", ffWRITE }
   };
 #define NFILE asize(fnm)
 
   /* Command line options ! */
   static bool bVerbose=FALSE;
   static bool bOptFFT=FALSE;
+  static bool bSort=FALSE;
   static int  ewald_geometry=eewg3D;
   static int  nnodes=1;
   static int  nthreads=1;
@@ -171,6 +179,8 @@ int main(int argc,char *argv[])
       "Number of threads to start on each node" },
     { "-v",       FALSE, etBOOL,{&bVerbose},  
       "Be loud and noisy" },
+    { "-sort",    FALSE, etBOOL,{&bSort},  
+      "Sort coordinates. Crucial for domain decomposition." },
     { "-grid",    FALSE, etRVEC,{&grid},
       "Number of grid cells in X, Y, Z dimension (if -1 use from tpr)" },
     { "-order",   FALSE, etINT, {&pme_order},
@@ -180,6 +190,7 @@ int main(int argc,char *argv[])
     { "-tol",     FALSE, etREAL, {&dtol},
       "Tolerance for Ewald summation" }
   };
+  FILE        *fp;
   t_inputrec  *ir;
   t_topology  top;
   t_tpxheader tpx;
@@ -194,17 +205,20 @@ int main(int argc,char *argv[])
   real        *charge,*qbuf;
   matrix      box;
   
+  /* Start the actual parallel code if necessary */
   cr   = init_par(&argc,&argv);
   root = 0;
-
+  
   if (MASTER(cr)) 
     CopyRight(stderr,argv[0]);
-
+  
+  /* Parse command line on all processors, arguments are passed on in 
+   * init_par (see above)
+   */
   parse_common_args(&argc,argv,
 		    PCA_KEEP_ARGS | PCA_NOEXIT_ON_ARGS | PCA_BE_NICE |
 		    PCA_CAN_SET_DEFFNM | (MASTER(cr) ? 0 : PCA_QUIET),
 		    NFILE,fnm,asize(pa),pa,asize(desc),desc,0,NULL);
-  bVerbose = bVerbose && MASTER(cr);
   
 #ifndef USE_MPI
   if (nnodes > 1) 
@@ -215,6 +229,7 @@ int main(int argc,char *argv[])
     gmx_fatal(FARGS,"GROMACS compiled without threads support - can only use one thread");
 #endif
 
+  /* Open log files on all processors */
   open_log(ftp2fn(efLOG,NFILE,fnm),cr);
   snew(ir,1);
   
@@ -238,6 +253,7 @@ int main(int argc,char *argv[])
       ir->nky = grid[YY];
       ir->nkz = grid[ZZ];
     }
+    /* Check command line parameters for consistency */
     if ((ir->nkx <= 0) || (ir->nky <= 0) || (ir->nkz <= 0))
       gmx_fatal(FARGS,"PME grid = %d %d %d",ir->nkx,ir->nky,ir->nkz);
     if (opt2parg_bSet("-rc",asize(pa),pa)) 
@@ -248,34 +264,41 @@ int main(int argc,char *argv[])
       ir->pme_order = pme_order;
     if (ir->pme_order <= 0)
       gmx_fatal(FARGS,"pme_order should be > 0 (not %d)",ir->pme_order);
-    if (opt2parg_bSet("-tol",asize(pa),pa)) {
+    if (opt2parg_bSet("-tol",asize(pa),pa))
       ir->ewald_rtol = dtol;
-    }
     if (ir->ewald_rtol <= 0)
       gmx_fatal(FARGS,"ewald_tol should be > 0 (not %f)",ir->ewald_rtol);
   }
   else {
     init_top(&top);
   }
+
+  /* Add parallellization code here */
+  snew(nsb,1);
+  if (MASTER(cr)) {
+    ncg = top.blocks[ebCGS].multinr[0];
+    for(i=0; (i<cr->nnodes-1); i++)
+      top.blocks[ebCGS].multinr[i] = min(ncg,(ncg*(i+1))/cr->nnodes);
+    for( ; (i<MAXNODES); i++)
+      top.blocks[ebCGS].multinr[i] = ncg;
+  }
   if (PAR(cr)) {
     /* Distribute the data over processors */
     MPI_Bcast(&natoms,1,MPI_INT,root,MPI_COMM_WORLD);
-    MPI_Bcast(&(ir->nkx),1,MPI_INT,root,MPI_COMM_WORLD);
-    MPI_Bcast(&(ir->nky),1,MPI_INT,root,MPI_COMM_WORLD);
-    MPI_Bcast(&(ir->nkz),1,MPI_INT,root,MPI_COMM_WORLD);
-    MPI_Bcast(&(ir->pme_order),1,MPI_INT,root,MPI_COMM_WORLD);
-    MPI_Bcast(&(ir->rcoulomb),1,GMX_MPI_REAL,root,MPI_COMM_WORLD);
-    MPI_Bcast(&(ir->ewald_rtol),1,GMX_MPI_REAL,root,MPI_COMM_WORLD);
+    /* Set some variables to zero to avoid core dumps */
+    ir->opts.ngtc = ir->opts.ngacc = ir->opts.ngfrz = ir->opts.ngener = 0;
+    MPI_Bcast(ir,sizeof(*ir),MPI_BYTE,root,MPI_COMM_WORLD);
     MPI_Bcast(&qtot,1,GMX_MPI_REAL,root,MPI_COMM_WORLD);
-    for(i=1; (i<cr->nnodes); i++) {
-      if (MASTER(cr)) {
+    /* Call some dedicated communication routines, master sends n-1 times */
+    if (MASTER(cr)) {
+      for(i=1; (i<cr->nnodes); i++) {
 	mv_block(i,&(top.blocks[ebCGS]));
 	mv_block(i,&(top.atoms.excl));
       }
-      else {
-	ld_block(0,&(top.blocks[ebCGS]));
-	ld_block(0,&(top.atoms.excl));
-      }
+    }
+    else {
+      ld_block(root,&(top.blocks[ebCGS]));
+      ld_block(root,&(top.atoms.excl));
     }
     if (!MASTER(cr)) {
       snew(charge,natoms);
@@ -284,15 +307,9 @@ int main(int argc,char *argv[])
     MPI_Bcast(charge,natoms,GMX_MPI_REAL,root,MPI_COMM_WORLD);
   }
   ewaldcoeff = calc_ewaldcoeff(ir->rcoulomb,ir->ewald_rtol);
-  if (MASTER(cr))
-    fprintf(stdlog,"Ewald parameters:\n"
-	    "rc         = %12.5e\n"
-	    "tol        = %12.5e\n"
-	    "ewaldcoeff = %12.5e\n"
-	    "grid       = %4d %4d %4d\n",
-	    ir->rcoulomb,ir->ewald_rtol,ewaldcoeff,ir->nkx,ir->nky,ir->nkz);
-  else
-    fprintf(stdlog,"Done communicating data\n");
+  if (bVerbose)
+    pr_inputrec(stdlog,0,"Inputrec",ir);
+
   /* Allocate memory for temp arrays etc. */
   snew(xbuf,natoms);
   snew(f,natoms);
@@ -303,33 +320,31 @@ int main(int argc,char *argv[])
   init_pme(stdlog,cr,ir->nkx,ir->nky,ir->nkz,ir->pme_order,
 	   natoms,bOptFFT,ewald_geometry);
 	   
-  /* MFlops accounting, quite useless here */
+  /* MFlops accounting */
   init_nrnb(&nrnb);
   
-  /* Add parallellization code here */
-  snew(nsb,1);
-  ncg = top.blocks[ebCGS].multinr[0];
-  for(i=0; (i<cr->nnodes-1); i++)
-    top.blocks[ebCGS].multinr[i] = min(ncg,(ncg*(i+1))/cr->nnodes);
-  for( ; (i<MAXNODES); i++)
-    top.blocks[ebCGS].multinr[i] = ncg;
-  
+  /* Initialize the work division */
   calc_nsb(stdlog,&(top.blocks[ebCGS]),cr->nnodes,nsb,0);
+  nsb->nodeid = cr->nodeid;
   print_nsb(stdlog,"pmetest",nsb);  
-  fflush(stdlog);
   
-  /* First do PME based on coordinates in tpr file */
+  /* First do PME based on coordinates in tpr file, send them to
+   * other processors if needed.
+   */
   if (MASTER(cr))
     fprintf(stdlog,"-----\n"
 	    "Results based on tpr file %s\n",ftp2fn(efTPX,NFILE,fnm));
   if (PAR(cr)) {
-    MPI_Bcast(x,natoms*DIM,GMX_MPI_REAL,root,MPI_COMM_WORLD);
-    MPI_Bcast(box,DIM*DIM,GMX_MPI_REAL,root,MPI_COMM_WORLD);
+    MPI_Bcast(x[0],natoms*DIM,GMX_MPI_REAL,root,MPI_COMM_WORLD);
+    MPI_Bcast(box[0],DIM*DIM,GMX_MPI_REAL,root,MPI_COMM_WORLD);
     MPI_Bcast(&t,1,GMX_MPI_REAL,root,MPI_COMM_WORLD);
   }
-  do_my_pme(stdlog,0,bVerbose,ir,x,xbuf,f,charge,qbuf,box,
-	    cr,nsb,&nrnb,ewaldcoeff,&(top.atoms.excl),qtot,index);
-  
+  do_my_pme(stdlog,0,bVerbose,ir,x,xbuf,f,charge,qbuf,box,bSort,
+	    cr,nsb,&nrnb,ewaldcoeff,&(top.atoms.excl),qtot,index,NULL);
+
+  /* If we have a trajectry file, we will read the frames in it and compute
+   * the PME energy.
+   */
   if (ftp2bSet(efTRX,NFILE,fnm)) {
     fprintf(stdlog,"-----\n"
 	    "Results based on trx file %s\n",ftp2fn(efTRX,NFILE,fnm));
@@ -338,30 +353,46 @@ int main(int argc,char *argv[])
       natoms = read_first_x(&status,ftp2fn(efTRX,NFILE,fnm),&t,&x,box); 
       if (natoms != top.atoms.nr)
 	gmx_fatal(FARGS,"natoms in trx = %d, in tpr = %d",natoms,top.atoms.nr);
+      fp = xvgropen(ftp2fn(efXVG,NFILE,fnm),"PME Energy","Time (ps)","E (kJ/mol)");
     }
-    if (PAR(cr)) {
-      MPI_Bcast(x,natoms*DIM,GMX_MPI_REAL,root,MPI_COMM_WORLD);
-      MPI_Bcast(box,DIM*DIM,GMX_MPI_REAL,root,MPI_COMM_WORLD);
-      MPI_Bcast(&t,1,GMX_MPI_REAL,root,MPI_COMM_WORLD);
-    }
+    else
+      fp = NULL;
     do {
-      do_my_pme(stdlog,t,bVerbose,ir,x,xbuf,f,charge,qbuf,box,cr,
-		nsb,&nrnb,ewaldcoeff,&(top.atoms.excl),qtot,index);
-      bCont = read_next_x(status,&t,natoms,x,box);
+      /* Send coordinates, box and time to the other nodes */
+      if (PAR(cr)) {
+	MPI_Bcast(x[0],natoms*DIM,GMX_MPI_REAL,root,MPI_COMM_WORLD);
+	MPI_Bcast(box[0],DIM*DIM,GMX_MPI_REAL,root,MPI_COMM_WORLD);
+	MPI_Bcast(&t,1,GMX_MPI_REAL,root,MPI_COMM_WORLD);
+      }
+      /* Call the PME wrapper function */
+      do_my_pme(stdlog,t,bVerbose,ir,x,xbuf,f,charge,qbuf,box,bSort,cr,
+		nsb,&nrnb,ewaldcoeff,&(top.atoms.excl),qtot,index,fp);
+      /* Only the master processor reads more data */
+      if (MASTER(cr))
+	bCont = read_next_x(status,&t,natoms,x,box);
+      /* Check whether we need to continue */
       if (PAR(cr))
 	MPI_Bcast(&bCont,1,MPI_INT,root,MPI_COMM_WORLD);
     } while (bCont);
-    if (MASTER(cr)) 
+    
+    /* Finish I/O, close files */
+    if (MASTER(cr)) {
       close_trx(status);
+      fclose(fp);
+    }
   }
+  
   if (bVerbose) {
+    /* Do some final I/O about performance, might be useful in debugging */
     fprintf(stdlog,"-----\n");
     print_nrnb(stdlog,&nrnb);
   }
   
+  /* Finish the parallel stuff */  
   if (gmx_parallel_env)
     gmx_finalize(cr);
 
+  /* Thank the audience, as usual */
   if (MASTER(cr)) 
     thanx(stderr);
 
