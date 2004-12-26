a5ba3010fe04b7cf856f510b9a6c1e9e53dbb3b7
lindahl <lindahl>
2004-12-26 18:45:22 +0000

    Updated neighborlist structure for new solvent optimization and nonbonded kernel pointers.

--- include/types/nblist.h
+++ include/types/nblist.h
@@ -37,31 +37,27 @@
 #include <config.h>
 #endif
 
-typedef struct {
-  int     il_code;             /* Code that determines the innerloop    */
-                               /* corresponding to codes in nrnb.h      */
-  int     nri,maxnri;          /* Current/max number of i particles	*/
-  int     nrj,maxnrj;	       /* Current/max number of j particles	*/
-  int     maxlen;              /* maxnr of j atoms for a single i atom 	*/
-  int     solvent;             /* type of solvent optimization          */
-  atom_id *iinr;	       /* The i-elements			*/
-  int     *gid;                /* Index in energy arrays                */
-  int     *shift;              /* Shift vector index                    */
-  int     *jindex;             /* Index in jjnr                         */
-  atom_id *jjnr;	       /* The j-atom list                       */
-  int     *nsatoms;            /* list with number of atoms for general */
-                               /* solvents. There are two entries for 	*/
-                               /* each molecule - first is total natoms */
-                               /* and second how many at the beginning 	*/
-                               /* have LJ interactions.                 */
-                               /* This is NOT used for water!           */
-#ifdef USE_THREADS
-  int      count;              /* counter to multithread the innerloops */
-  pthread_mutex_t *mtx;        /* mutex to lock the counter             */
-#else
-  int      pad1,*pad2;         /* padding to make size constant         */
-#endif
+typedef struct 
+{
+  int             il_code;      /* Innerloop index from nrnb.h, used     */
+                                /* for flop accounting.                  */
+  int             coultype;     /* Coulomb interaction for this list     */
+  int             vdwtype;      /* VdW interaction for this list         */
+  int             free_energy;  /* Free energy setting for this list     */
+  int             solvent_opt;  /* Atom, water, or water-water list      */
+
+  int             nri,maxnri;   /* Current/max number of i particles	 */
+  int             nrj,maxnrj;   /* Current/max number of j particles	 */
+  int             maxlen;       /* maxnr of j atoms for a single i atom  */
+  int *           iinr;	        /* The i-elements	   	         */
+  int *           gid;          /* Index in energy arrays                */
+  int *           shift;        /* Shift vector index                    */
+  int *           jindex;       /* Index in jjnr                         */
+  int *           jjnr;	        /* The j-atom list                       */
+  int             count;        /* counter to multithread the innerloops */
+  void *          mtx;          /* mutex to lock the counter             */
 } t_nblist;
+
 
 /* For atom I =  nblist->iinr[N] (0 <= N < nblist->nri) there can be
  * several neighborlists (N's), for different energy groups (gid) and
