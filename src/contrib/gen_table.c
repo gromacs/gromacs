8c25c3c8ff04baf2021424329b85667e0fec375a
spoel <spoel>
2005-01-16 20:58:19 +0000

    Updated by Paul

--- src/contrib/gen_table.c
+++ src/contrib/gen_table.c
@@ -1,11 +1,13 @@
 #include "math.h"
+#include "string.h"
+#include "copyrite.h"
 #include "stdio.h"
 #include "typedefs.h"
 #include "macros.h"
 #include "vec.h"
 #include "statutil.h"
 
-enum { mGuillot, mN61, mMaaren, mNR };
+enum { mGuillot, mN61, mMaaren, mGuillot_maple, mNR };
 
 void do_n61(FILE *fp,int eel,double resolution,int npow)
 {
@@ -46,7 +48,7 @@ static double erf1(double x)
   return (2/sqrt(M_PI))*exp(-x*x);
 }
 
-static double lo_do_guillot(double r,double xi,
+void lo_do_guillot(double r,double xi,
 			    double *vc,double *vc2,
 			    double *vd,double *vd2,
 			    double *vr,double *vr2)
@@ -72,6 +74,33 @@ static double lo_do_guillot(double r,double xi,
   *vr2  = (sqpi*(*vr)/(2.0*z*z)+(1.0/(z*z)+1)*exp(-z*z))/(sqpi*sqr(xir));
 }
 
+void lo_do_guillot_maple(double r,double xi,
+				  double *vc,double *vc2,
+				  double *vd,double *vd2,
+				  double *vr,double *vr2)
+{
+  double qO     = -0.888;
+  double qOd    = 0.226;
+  double f0     = qOd/qO;
+  double xir    = 0.0615;
+  double sqpi   = sqrt(M_PI);
+  double r1,r2,z;
+  
+  r1    = r/(2*xi);
+  r2    = r/(sqrt(2)*xi);
+  *vc   = 1/r + f0*f0*erf(r/xi/2.0)/r + 2.0*f0*erf(r*sqrt(2.0)/xi/2.0)/r;
+  *vc2  = 2.0/(r*r*r)-f0*f0/sqrt(M_PI)/(xi*xi*xi)*exp(-r*r/(xi
+*xi)/4.0)/2.0-2.0*f0*f0/sqrt(M_PI)*exp(-r*r/(xi*xi)/4.0)/xi/(r*
+r)+2.0*f0*f0*erf(r/xi/2.0)/(r*r*r)-2.0*f0/sqrt(M_PI)/(xi*xi*xi)
+*exp(-r*r/(xi*xi)/2.0)*sqrt(2.0)-4.0*f0/sqrt(M_PI)*exp(-r*r/(xi
+*xi)/2.0)*sqrt(2.0)/xi/(r*r)+4.0*f0*erf(r*sqrt(2.0)/xi/2.0)/(r*r*r);
+  *vd   = -1.0/(r*r*r*r*r*r);
+  *vd2  = -42.0/(r*r*r*r*r*r*r*r);
+  z     = r/(2.0*xir);
+  *vr   = 2.0*erfc(r/xir/2.0)/r*xir;
+  *vr2  = 1.0/sqrt(M_PI)/(xir*xir)*exp(-r*r/(xir*xir)/4.0)+4.0/sqrt(M_PI)*exp(-r*r/(xir*xir)/4.0)/(r*r)+4.0*erfc(r/xir/2.0)/(r*r*r)*xir;
+}
+
 static void do_guillot(FILE *fp,int eel,double resolution)
 {
   int    i,i0,imax;
@@ -91,6 +120,26 @@ static void do_guillot(FILE *fp,int eel,double resolution)
 	    r,vc,vc2,vd,vd2,vr,vr2);
   }
 }
+
+static void do_guillot_maple(FILE *fp,int eel,double resolution)
+{
+  int    i,i0,imax;
+  double xi     = 0.15;
+  double r,vc,vc2,vd,vd2,vr,vr2;
+
+  imax = 3/resolution;
+  for(i=0; (i<=imax); i++) {
+    r     = i*resolution;
+    /* Avoid very large numbers */
+    if (r < 0.04) {
+      vc = vc2 = vd = vd2 = vr = vr2 = 0;
+    }
+    else 
+      lo_do_guillot_maple(r,xi,&vc,&vc2,&vd,&vd2,&vr,&vr2);
+    fprintf(fp,"%12.5e  %12.5e  %12.5e   %12.5e  %12.5e  %12.5e  %12.5e\n",
+	    r,vc,vc2,vd,vd2,vr,vr2);
+  }
+} 
 
 static void do_maaren(FILE *fp,int eel,double resolution,int npow)
 {
@@ -122,11 +171,11 @@ int main(int argc,char *argv[])
     "potentials."
   };
   static char *opt[]     = { NULL, "cut", "rf", "pme", NULL };
-  static char *model[]   = { NULL, "guillot", "n61", "maaren", NULL };
+  static char *model[]   = { NULL, "guillot", "n61", "maaren", "guillot_maple", NULL };
   static real resolution = 0.001;
   static int  npow       = 12;
   t_pargs pa[] = {
-    { "-e",      FALSE, etENUM, {opt},
+    { "-el",      FALSE, etENUM, {opt},
       "Electrostatics type: cut, rf or pme" },
     { "-m",      FALSE, etENUM, {model},
       "Model for the tables" },
@@ -141,7 +190,7 @@ int main(int argc,char *argv[])
   };
 #define NFILE asize(fnm)
   FILE *fp;
-  int  eel,m;
+  int  eel=0,m=0;
   
   CopyRight(stderr,argv[0]);
   parse_common_args(&argc,argv,PCA_CAN_VIEW | PCA_CAN_TIME | PCA_BE_NICE,
@@ -161,6 +210,8 @@ int main(int argc,char *argv[])
     m = mN61;
   else if (strcmp(model[0],"guillot") == 0) 
     m = mGuillot;
+  else if (strcmp(model[0],"guillot_maple") == 0) 
+    m = mGuillot_maple;
   else 
     gmx_fatal(FARGS,"Invalid argument %s for option -m",opt[0]);
     
@@ -168,6 +219,9 @@ int main(int argc,char *argv[])
   switch (m) {
   case mGuillot:
     do_guillot(fp,eel,resolution);
+    break;
+  case mGuillot_maple:
+    do_guillot_maple(fp,eel,resolution);
     break;
   case mMaaren:
     do_maaren(fp,eel,resolution,npow);
