513ea2d9149a31995c85106a764d18687ff59fdc
lindahl <lindahl>
2005-02-02 12:57:52 +0000

    Added BLAS,LAPACK, and ARPACK headers.

---
+++ include/gmx_blas.h
@@ -0,0 +1,183 @@
+
+#ifndef _GMX_BLAS_H_
+#define _GMX_BLAS_H_
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+
+
+/** @file blas.h
+ *
+ *  @brief Header definitions for the standard BLAS library.
+ *
+ * This is the subset of BLAS routines used for the
+ * linear algebra operations in Gromacs. 
+ * Do NOT use this for other purposes - we only provide this as a 
+ * simple fallback/reference implementation when no optimized BLAS 
+ * is present. If you need an implementation for your own code 
+ * there are several much faster versions out there.
+ *
+ * All routines are compatible with the BLAS reference implementation,
+ * meaning they assume fortran-style matrix row/column organization.
+ *
+ * There is plenty of documentation for these routines available
+ * at http://www.netlib.org/blas , so there is no point in repeating
+ * it here.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#if 0
+}
+#endif
+
+#ifndef F77_FUNC
+#define F77_FUNC(name,NAME) name ## _
+#endif
+
+
+/* Double precision versions */
+double
+F77_FUNC(dasum,DASUM)(int *n, double *dx, int *incx);
+
+void
+F77_FUNC(daxpy,DAXPY)(int *n, double *da, double *dx, int *incx, double *dy, int *incy);
+
+void
+F77_FUNC(dcopy,DCOPY)(int *n, double *dx, int *incx, double *dy, int *incy);
+
+double
+F77_FUNC(ddot,DDOT)(int *n, double *dx, int *incx, double *dy, int *incy);
+
+void
+F77_FUNC(dgemm,DGEMM)(char *transa, char *transb, int *m, int *n, int *k, 
+       double *alpha, double *a, int *lda, double *b, int *ldb, 
+       double *beta, double *c, int *ldc);
+
+void
+F77_FUNC(dgemv,DGEMV)(char *trans, int *m, int *n, double *alpha, double *a, int *lda,
+       double *x, int *incx, double *beta, double *y, int *incy);
+
+void
+F77_FUNC(dger,DGER)(int *m, int *n, double *alpha, double *x, int *incx, 
+      double *y, int *incy, double *a, int *lda);
+
+double
+F77_FUNC(dnrm2,DNRM2)(int  *n, double *x, int *incx);
+
+void
+F77_FUNC(drot,DROT)(int *n, double *dx, int *incx, 
+      double *dy, int *incy, double *c, double *s);
+
+void 
+F77_FUNC(dscal,DSCAL)(int *n, double *fact, double *dx, int *incx);
+
+void
+F77_FUNC(dswap,DSWAP)(int *n, double *dx, int *incx, double *dy, int *incy);
+
+void
+F77_FUNC(dsymv,DSYMV)(char *uplo, int *n, double *alpha, double *a, int *lda,
+       double *x, int *incx, double *beta, double *y, int *incy);
+
+void
+F77_FUNC(dsyr2,DSYR2)(char *uplo, int *n, double *alpha, double *x, int *incx,
+       double *y, int *incy, double *a, int *lda);
+
+void
+F77_FUNC(dsyr2k,DSYR2K)(char *uplo, char *trans, int *n, int *k, double *alpha, double *a,
+        int *lda, double *b, int *ldb, double *beta, double *c, int *ldc);
+
+void 
+F77_FUNC(dtrmm,DTRMM)(char *side, char *uplo, char *transa, char *diag, int *m, int *n, 
+       double *alpha, double *a, int *lda, double *b, int *ldb);
+
+void 
+F77_FUNC(dtrmv,DTRMV)(char *uplo, char *trans, char *diag, int *n, 
+       double *a, int *lda, double *x, int *incx);
+
+void
+F77_FUNC(dtrsm,DTRSM)(char *side, char *uplo, char *transa, char *diag, int *m, int *n,
+       double *alpha, double *a,int *lda, double *b, int *ldb);
+
+int
+F77_FUNC(idamax,IDAMAX)(int *n, double *dx, int *incx);
+
+
+
+/* Single precision versions */
+float
+F77_FUNC(sasum,SASUM)(int *n, float *dx, int *incx);
+
+void
+F77_FUNC(saxpy,SAXPY)(int *n, float *da, float *dx, int *incx, float *dy, int *incy);
+
+void
+F77_FUNC(scopy,SCOPY)(int *n, float *dx, int *incx, float *dy, int *incy);
+
+float
+F77_FUNC(sdot,SDOT)(int *n, float *dx, int *incx, float *dy, int *incy);
+
+void
+F77_FUNC(sgemm,SGEMM)(char *transa, char *transb, int *m, int *n, int *k, 
+       float *alpha, float *a, int *lda, float *b, int *ldb, 
+       float *beta, float *c, int *ldc);
+
+void
+F77_FUNC(sgemv,SGEMV)(char *trans, int *m, int *n, float *alpha, float *a, int *lda,
+       float *x, int *incx, float *beta, float *y, int *incy);
+
+void
+F77_FUNC(sger,SGER)(int *m, int *n, float *alpha, float *x, int *incx, 
+      float *y, int *incy, float *a, int *lda);
+
+float
+F77_FUNC(snrm2,SNRM2)(int  *n, float *x, int *incx);
+
+void
+F77_FUNC(srot,SROT)(int *n, float *dx, int *incx, 
+      float *dy, int *incy, float *c, float *s);
+
+void 
+F77_FUNC(sscal,SSCAL)(int *n, float *fact, float *dx, int *incx);
+
+void
+F77_FUNC(sswap,SSWAP)(int *n, float *dx, int *incx, float *dy, int *incy);
+
+void
+F77_FUNC(ssymv,SSYMV)(char *uplo, int *n, float *alpha, float *a, int *lda,
+       float *x, int *incx, float *beta, float *y, int *incy);
+
+void
+F77_FUNC(ssyr2,SSYR2)(char *uplo, int *n, float *alpha, float *x, int *incx,
+       float *y, int *incy, float *a, int *lda);
+
+void
+F77_FUNC(ssyr2k,SSYR2K)(char *uplo, char *trans, int *n, int *k, float *alpha, float *a,
+        int *lda, float *b, int *ldb, float *beta, float *c, int *ldc);
+
+void 
+F77_FUNC(strmm,STRMM)(char *side, char *uplo, char *transa, char *diag, int *m, int *n, 
+       float *alpha, float *a, int *lda, float *b, int *ldb);
+
+void 
+F77_FUNC(strmv,STRMV)(char *uplo, char *trans, char *diag, int *n, 
+       float *a, int *lda, float *x, int *incx);
+
+void
+F77_FUNC(strsm,STRSM)(char *side, char *uplo, char *transa, char *diag, int *m, int *n,
+       float *alpha, float *a,int *lda, float *b, int *ldb);
+
+int
+F77_FUNC(isamax,ISAMAX)(int *n, float *dx, int *incx);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /* _BLAS_H_ */
