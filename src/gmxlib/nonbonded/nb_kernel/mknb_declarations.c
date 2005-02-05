a9a1ded84e0ef1a3ccc224e445a839d4e4d25ac8
spoel <spoel>
2005-02-05 01:40:44 +0000

    Outcommented a line which generated SEGV in every mdrun and is not useful.

--- src/gmxlib/nonbonded/nb_kernel/mknb_declarations.c
+++ src/gmxlib/nonbonded/nb_kernel/mknb_declarations.c
@@ -353,7 +353,7 @@ mknb_initialize_data(void)
 		mknb_assign("krf",        "*p_krf");
 		mknb_assign("crf",        "*p_crf");
 		mknb_assign("tabscale",   "*p_tabscale");
-		mknb_assign("gbtabscale", "*p_gbtabscale");
+		/* mknb_assign("gbtabscale", "*p_gbtabscale");*/
 	}	   
 
 	/* assign the charge combinations for OO,OH and HH, 
