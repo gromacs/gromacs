d7dd7e7034d217416fe1efb286c85a7695d54406
spoel <spoel>
2005-06-07 19:57:32 +0000

    Implemented first version of dihedral restraints

--- src/gmxlib/dihres.c
+++ src/gmxlib/dihres.c
@@ -42,6 +42,7 @@
 #include "sysstuff.h"
 #include "smalloc.h"
 #include "macros.h"
+#include "physics.h"
 #include "vec.h"
 #include "futil.h"
 #include "xvgr.h"
@@ -89,14 +90,18 @@ void init_dihres(FILE *fplog,int nfa,const t_iatom forceatoms[],
 
 real ta_dihres(int nfa,const t_iatom forceatoms[],const t_iparams ip[],
 	       const rvec x[],rvec f[],rvec fshift[],
-	       int ePBC,const t_graph *g,
+	       const t_pbc *pbc,const t_graph *g,
 	       real lambda,real *dvdlambda,
 	       const t_mdatoms *md,t_fcdata *fcd)
 {
   real vtot = 0;
-  int ai,aj,ak,al,i,k,type,typep,label,power;
-  real phi,dphi,kfac;
-  k=0;
+  int  ai,aj,ak,al,i,k,type,typep,label,power,t1,t2,t3;
+  real phi0,phi,ddphi,ddp,dp,dp2,dphi,kfac,cos_phi,sign,d2r,fc;
+  rvec r_ij,r_kj,r_kl,m,n;
+  
+  fc  = fcd->dihres.dihre_fc;
+  d2r = DEG2RAD;
+  k   = 0;
   for(i=0; (i<nfa); ) {
     type = forceatoms[i++];
     ai   = forceatoms[i++];
@@ -104,13 +109,43 @@ real ta_dihres(int nfa,const t_iatom forceatoms[],const t_iparams ip[],
     ak   = forceatoms[i++];
     al   = forceatoms[i++];
     
-    phi = ip[type].dihres.phi;
-    dphi = ip[type].dihres.dphi;
-    kfac = ip[type].dihres.kfac; 
+    phi0  = ip[type].dihres.phi*d2r;
+    dphi  = ip[type].dihres.dphi*d2r;
+    kfac  = ip[type].dihres.kfac*fc; 
     power = ip[type].dihres.power;
     label = ip[type].dihres.label;
-    printf("dihres[%d]: %d %d %d %d : phi=%f, dphi=%f, kfac=%f, power=%d, label=%d\n",
-	   k++,ai,aj,ak,al,phi,dphi,kfac,power,label);
+    if (debug)
+      fprintf(debug,"dihres[%d]: %d %d %d %d : phi=%f, dphi=%f, kfac=%f, power=%d, label=%d\n",
+	      k++,ai,aj,ak,al,phi,dphi,kfac,power,label);
+    
+    phi = dih_angle(x[ai],x[aj],x[ak],x[al],pbc,r_ij,r_kj,r_kl,m,n,
+		    &cos_phi,&sign,&t1,&t2,&t3);	   /* 84 flops */
+    
+    /* phi can jump if phi0 is close to Pi/-Pi, which will cause huge
+     * force changes if we just apply a normal harmonic.
+     * Instead, we first calculate phi-phi0 and take it modulo (-Pi,Pi).
+     * This means we will never have the periodicity problem, unless
+     * the dihedral is Pi away from phiO, which is very unlikely due to
+     * the potential.
+     */
+    dp = phi-phi0;  
+    if (fabs(dp) > dphi) {
+      /* dp cannot be outside (-2*pi,2*pi) */
+      if (dp >= M_PI)
+	dp -= 2*M_PI;
+      else if(dp < -M_PI)
+	dp += 2*M_PI;
+
+      ddp = dp-dphi;      
+      vtot += 0.5*kfac*ddp*ddp;
+      if (phi < phi0)
+	ddphi = kfac*ddp;
+      else
+	ddphi = -kfac*ddp;
+      
+      do_dih_fup(ai,aj,ak,al,(real)(-ddphi),r_ij,r_kj,r_kl,m,n,
+		 f,fshift,pbc,g,x,t1,t2,t3);			/* 112		*/
+    }
   }
   return vtot;
 }
