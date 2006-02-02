d9c136eda5ba4f80adb004460e03e363ecfd60b0
hess <hess>
2006-02-02 10:22:01 +0000

    changed the theta2 distribution plot from 3cos2-1 to cos and added the option -rbin

--- src/tools/gmx_sorient.c
+++ src/tools/gmx_sorient.c
@@ -105,7 +105,7 @@ int gmx_sorient(int argc,char *argv[])
   
   FILE    *fp;
   int     i,j,p,sa0,sa1,sa2,n,ntot,nf,m,*hist1,*hist2,*histn,nbin1,nbin2,nrbin;
-  real    *histi1,*histi2,invbw,rbinw,invrbw;
+  real    *histi1,*histi2,invbw,invrbw;
   int     *isize,nrefgrp,nrefat;
   atom_id **index;
   char    **grpname;
@@ -133,7 +133,7 @@ int gmx_sorient(int argc,char *argv[])
     "Only solvent molecules between [TT]-rmin[tt] and [TT]-rmax[tt] are",
     "considered for [TT]-o[tt] and [TT]-no[tt] each frame.[PAR]",
     "[TT]-o[tt]: distribtion of cos(theta1) for rmin<=r<=rmax.[PAR]",
-    "[TT]-no[tt]: distribution of 3cos^2(theta2)-1 for rmin<=r<=rmax.[PAR]",
+    "[TT]-no[tt]: distribution of cos(theta2) for rmin<=r<=rmax.[PAR]",
     "[TT]-ro[tt]: <cos(theta1)> and <3cos^2(theta2)-1> as a function of the",
     "distance.[PAR]",
     "[TT]-co[tt]: the sum over all solvent molecules within distance r",
@@ -141,13 +141,14 @@ int gmx_sorient(int argc,char *argv[])
   };
   
   static bool bCom = FALSE,bPBC = FALSE;
-  static real rmin=0.0,rmax=0.5,binwidth=0.02;
+  static real rmin=0.0,rmax=0.5,binwidth=0.02,rbinw=0.02;
   t_pargs pa[] = {
     { "-com",  FALSE, etBOOL,  {&bCom},
       "Use the center of mass as the reference postion" },
-    { "-rmin",  FALSE, etREAL, {&rmin}, "Minimum distance" },
-    { "-rmax",  FALSE, etREAL, {&rmax}, "Maximum distance" },
-    { "-bin",   FALSE, etREAL, {&binwidth}, "Binwidth" },
+    { "-rmin",  FALSE, etREAL, {&rmin}, "Minimum distance (nm)" },
+    { "-rmax",  FALSE, etREAL, {&rmax}, "Maximum distance (nm)" },
+    { "-cbin",  FALSE, etREAL, {&binwidth}, "Binwidth for the cosine" },
+    { "-rbin",  FALSE, etREAL, {&rbinw}, "Binwidth for r (nm)" },
     { "-pbc",   FALSE, etBOOL, {&bPBC}, "Check PBC for the center of mass calculation. Only necessary when your reference group consists of several molecules." }
   };
   
@@ -201,9 +202,8 @@ int gmx_sorient(int argc,char *argv[])
 
   invbw = 1/binwidth;
   nbin1 = (int)(2*invbw + 0.5);
-  nbin2 = (int)(3*invbw + 0.5);
+  nbin2 = (int)(invbw + 0.5);
 
-  rbinw = 0.02;
   invrbw = 1/rbinw;
   
   ntot = 0;
@@ -255,8 +255,8 @@ int gmx_sorient(int argc,char *argv[])
 	  (histn[(int)(invrbw*r)])++;
 	  if (r2>=rmin2 && r2<rmax2) {
 	    (hist1[(int)(invbw*(inp + 1))])++;
-	    (hist2[(int)(invbw*(3*sqr(outp)))])++;
-	    n++;
+	    (hist2[(int)(invbw*fabs(outp))])++;
+	     n++;
 	  }
 	}
       }
@@ -291,10 +291,10 @@ int gmx_sorient(int argc,char *argv[])
   
   sprintf(str,"Solvent normal orientation between %g and %g nm",rmin,rmax);
   fp=xvgropen(opt2fn("-no",NFILE,fnm), 
-	      str,"3 cos\\S2\\N(\\8q\\4\\s2\\N) - 1","");
+	      str,"cos(\\8q\\4\\s2\\N)","");
   fprintf(fp,"@ subtitle \"average shell size %.1f molecules\"\n",nav);
   for(i=0; i<nbin2; i++) {
-    fprintf(fp,"%g %g\n",(i+0.5)*binwidth-1,3*normfac*hist2[i]);
+    fprintf(fp,"%g %g\n",(i+0.5)*binwidth,normfac*hist2[i]);
   }
   fclose(fp);
 
