f3206f0902d14bf889d8fcff1d82a3cc1f8b94e9
hess <hess>
2007-07-10 15:52:18 +0000

    added a debug print for signal installation

--- src/kernel/md.c
+++ src/kernel/md.c
@@ -323,10 +323,16 @@ void mdrunner(t_commrec *cr,int nfile,t_filenm fnm[],
      * (A user signal from the PME nodes (if any)
      * is communicated to the PP nodes.
      */
-    if (getenv("GMX_NO_TERM") == NULL)
+    if (getenv("GMX_NO_TERM") == NULL) {
+      if (debug)
+	fprintf(debug,"Installing signal handler for SIGTERM\n");
       signal(SIGTERM,signal_handler);
-    if (getenv("GMX_NO_USR1") == NULL)
+    }
+    if (getenv("GMX_NO_USR1") == NULL) {
+      if (debug)
+	fprintf(debug,"Installing signal handler for SIGUSR1\n");
       signal(SIGUSR1,signal_handler);
+    }
     break;
   default:
     /* No signal handling */
