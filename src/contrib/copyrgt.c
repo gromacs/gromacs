30de274f219230e51dcea7996f0d5bc20fb8eeaa
spoel <spoel>
2007-09-19 10:41:54 +0000

    Updated

--- src/contrib/copyrgt.c
+++ src/contrib/copyrgt.c
@@ -7,10 +7,10 @@
  * 
  *          GROningen MAchine for Chemical Simulations
  * 
- *                        VERSION 3.2.0
+ *                        VERSION 3.3.2
  * Written by David van der Spoel, Erik Lindahl, Berk Hess, and others.
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
- * Copyright (c) 2001-2004, The GROMACS development team,
+ * Copyright (c) 2001-2007, The GROMACS development team,
  * check out http://www.gromacs.org for more information.
 
  * This program is free software; you can redistribute it and/or
@@ -31,7 +31,7 @@
  * For more info, check our website at http://www.gromacs.org
  * 
  * And Hey:
- * Good gRace! Old Maple Actually Chews Slate
+ * Groningen Machine for Chemical Simulation
  */
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -96,8 +96,9 @@ void head(FILE *out, char *fn_, bool bH,
     fprintf(out,"%s %s\n",ccont,CopyrightText[i]);
   for(i=0; (i<NH2); i++)
     fprintf(out,"%s %s\n",ccont,head2[i]);
-
-  fprintf(out,"%s %s\n",ccont,bromacs(buf,STRLEN-1));
+  
+  bromacs(buf,STRLEN-1);
+  fprintf(out,"%s %s\n",ccont,buf);
   fprintf(out,"%s\n",cend);
   if (bH) {
     fprintf(out,"\n");
