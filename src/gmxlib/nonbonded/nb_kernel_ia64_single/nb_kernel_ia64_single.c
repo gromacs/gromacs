d6e388ec4cedc8adc6c46a0e0544699820d154d1
lindahl <lindahl>
2009-01-15 20:52:13 +0000

    Check that log file pointer is non-null before using it.

--- src/gmxlib/nonbonded/nb_kernel_ia64_single/nb_kernel_ia64_single.c
+++ src/gmxlib/nonbonded/nb_kernel_ia64_single/nb_kernel_ia64_single.c
@@ -188,8 +188,11 @@ nb_kernel_setup_ia64_single(FILE *log,nb_kernel_t **list)
             break;
     }
 
-    fprintf(log,"Using single precision ia64 assembly kernels.");
-
+    if(NULL != log)
+	{
+		fprintf(log,"Using single precision ia64 assembly kernels.");
+	}
+	
     for(i=0;i<eNR_NBKERNEL_NR;i++)
     {
         p = kernellist_ia64_single[i];
