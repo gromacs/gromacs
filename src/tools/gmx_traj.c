970699b8c4ed766a7d788d2d6aad78c8fb88bf07
hess <hess>
2009-01-28 10:59:42 +0000

    fixed crash with -mol

--- src/tools/gmx_traj.c
+++ src/tools/gmx_traj.c
@@ -601,7 +601,7 @@ int gmx_traj(int argc,char *argv[])
     snew(isize,ngroups);
     snew(index,ngroups);
     for (i=0; i<ngroups; i++) {
-      if (index0[0][i] < 0 || index[0][i] >= mols->nr)
+      if (index0[0][i] < 0 || index0[0][i] >= mols->nr)
 	gmx_fatal(FARGS,"Molecule index (%d) is out of range (%d-%d)",
 		  index0[0][i]+1,1,mols->nr);
       isize[i] = atndx[index0[0][i]+1] - atndx[index0[0][i]];
