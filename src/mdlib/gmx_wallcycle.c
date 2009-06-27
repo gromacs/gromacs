f73747fa7def0500ac0d3c34ea740b361c354815
Berk <hess@blu.(none)>
2009-06-27 09:29:35 +0200

    removed static variables

--- src/mdlib/gmx_wallcycle.c
+++ src/mdlib/gmx_wallcycle.c
@@ -50,26 +50,31 @@
 #include "thread_mpi.h"
 #endif
 
-typedef struct gmx_wallcycle {
-  int          n;
-  gmx_cycles_t c;
-  gmx_cycles_t start;
-  gmx_cycles_t last;
+typedef struct
+{
+    int          n;
+    gmx_cycles_t c;
+    gmx_cycles_t start;
+    gmx_cycles_t last;
+} wallcc_t;
+
+typedef struct gmx_wallcycle
+{
+    wallcc_t     *wcc;
+    /* variables for testing/debugging */
+    bool         wc_barrier;
+    wallcc_t     *wcc_all;
+    int          wc_depth;
+    int          ewc_prev;
+    gmx_cycles_t cycle_prev;
+#ifdef GMX_MPI
+    MPI_Comm     mpi_comm_mygroup;
+#endif
 } gmx_wallcycle_t_t;
 
 /* Each name should not exceed 19 characters */
 static const char *wcn[ewcNR] =
   { "Run", "Step", "PP during PME", "Domain decomp.", "DD comm. load", "DD comm. bounds", "Vsite constr.", "Send X to PME", "Comm. coord.", "Neighbor search", "Force", "Wait + Comm. F", "PME mesh", "PME mesh", "Wait + Comm. X/F", "Wait + Recv. PME F", "Vsite spread", "Write traj.", "Update", "Constraints", "Comm. energies", "Test", "Born radii" };
-
-/* variables for testing/debugging */
-static bool              wc_barrier=FALSE;
-static gmx_wallcycle_t_t *wc_all=NULL;
-static int               wc_depth=0;
-static int               ewc_prev=-1;
-static gmx_cycles_t      cycle_prev;
-#ifdef GMX_MPI
-static MPI_Comm          wc_mpi_comm_mygroup;
-#endif
 
 bool wallcycle_have_counter(void)
 {
@@ -78,187 +83,239 @@ bool wallcycle_have_counter(void)
 
 gmx_wallcycle_t wallcycle_init(FILE *fplog,t_commrec *cr)
 {
-  gmx_wallcycle_t_t *wc;
+    gmx_wallcycle_t wc;
+    
+    if (!wallcycle_have_counter())
+    {
+        return NULL;
+    }
+
+    snew(wc,1);
+
+    wc->wc_barrier = FALSE;
+    wc->wcc_all    = NULL;
+    wc->wc_depth   = 0;
+    wc->ewc_prev   = -1;
 
 #ifdef GMX_MPI
-    if (PAR(cr) && getenv("GMX_CYCLE_BARRIER") != NULL) {
+    if (PAR(cr) && getenv("GMX_CYCLE_BARRIER") != NULL)
+    {
         if (fplog) 
         {
             fprintf(fplog,"\nWill call MPI_Barrier before each cycle start/stop call\n\n");
         }
-        wc_barrier = TRUE;
-        wc_mpi_comm_mygroup = cr->mpi_comm_mygroup;
+        wc->wc_barrier = TRUE;
+        wc->mpi_comm_mygroup = cr->mpi_comm_mygroup;
     }
 #endif
 
-  if (wallcycle_have_counter()) {
-    snew(wc,ewcNR);
-    if (getenv("GMX_CYCLE_ALL") != NULL) {
+    snew(wc->wcc,ewcNR);
+    if (getenv("GMX_CYCLE_ALL") != NULL)
+    {
         if (fplog) 
         {
             fprintf(fplog,"\nWill time all the code during the run\n\n");
-            snew(wc_all,ewcNR*ewcNR);
         }
+        snew(wc->wcc_all,ewcNR*ewcNR);
     }
-  } else {
-    wc = NULL;
-  }
 
-  return wc;
+    return wc;
 }
 
-static void wallcycle_all_start(int ewc,gmx_cycles_t cycle)
+void wallcycle_destroy(gmx_wallcycle_t wc)
 {
-  ewc_prev = ewc;
-  cycle_prev = cycle;
+    if (wc == NULL)
+    {
+        return;
+    }
+    
+    if (wc->wcc != NULL)
+    {
+        sfree(wc->wcc);
+    }
+    if (wc->wcc_all != NULL)
+    {
+        sfree(wc->wcc_all);
+    }
+    sfree(wc);
 }
 
-static void wallcycle_all_stop(int ewc,gmx_cycles_t cycle)
+static void wallcycle_all_start(gmx_wallcycle_t wc,int ewc,gmx_cycles_t cycle)
 {
-  wc_all[ewc_prev*ewcNR+ewc].n += 1;
-  wc_all[ewc_prev*ewcNR+ewc].c += cycle - cycle_prev;
+    wc->ewc_prev = ewc;
+    wc->cycle_prev = cycle;
+}
+
+static void wallcycle_all_stop(gmx_wallcycle_t wc,int ewc,gmx_cycles_t cycle)
+{
+    wc->wcc_all[wc->ewc_prev*ewcNR+ewc].n += 1;
+    wc->wcc_all[wc->ewc_prev*ewcNR+ewc].c += cycle - wc->cycle_prev;
 }
 
 void wallcycle_start(gmx_wallcycle_t wc, int ewc)
 {
-  gmx_cycles_t cycle;
+    gmx_cycles_t cycle;
+
+    if (wc == NULL)
+    {
+        return;
+    }
 
 #ifdef GMX_MPI
-  if (wc_barrier)
-    MPI_Barrier(wc_mpi_comm_mygroup);
+    if (wc->wc_barrier)
+    {
+        MPI_Barrier(wc->mpi_comm_mygroup);
+    }
 #endif
 
-  if (wc) {
     cycle = gmx_cycles_read();
-    wc[ewc].start = cycle;
-    if (wc_all) {
-      wc_depth++;
-      if (ewc == ewcRUN)
-	wallcycle_all_start(ewc,cycle);
-      else if (wc_depth == 3) {
-	wallcycle_all_stop(ewc,cycle);
-      }
+    wc->wcc[ewc].start = cycle;
+    if (wc->wcc_all != NULL)
+    {
+        wc->wc_depth++;
+        if (ewc == ewcRUN)
+        {
+            wallcycle_all_start(wc,ewc,cycle);
+        }
+        else if (wc->wc_depth == 3)
+        {
+            wallcycle_all_stop(wc,ewc,cycle);
+        }
     }
-  }
 }
 
 double wallcycle_stop(gmx_wallcycle_t wc, int ewc)
 {
-  gmx_cycles_t cycle,last;
-
-#ifdef GMX_MPI
-    if (wc_barrier)
-      MPI_Barrier(wc_mpi_comm_mygroup);
-#endif
-
-  if (wc) {
-    cycle = gmx_cycles_read();
-    last = cycle - wc[ewc].start;
-    wc[ewc].c += last;
-    wc[ewc].n++;
-    if (wc_all) {
-      wc_depth--;
-      if (ewc == ewcRUN)
-	wallcycle_all_stop(ewc,cycle);
-      else if (wc_depth == 2)
-	wallcycle_all_start(ewc,cycle);
+    gmx_cycles_t cycle,last;
+    
+    if (wc == NULL)
+    {
+        return 0;
     }
-  } else {
-    last = 0;
-  }
+    
+#ifdef GMX_MPI
+    if (wc->wc_barrier)
+    {
+        MPI_Barrier(wc->mpi_comm_mygroup);
+    }
+#endif
+    
+    cycle = gmx_cycles_read();
+    last = cycle - wc->wcc[ewc].start;
+    wc->wcc[ewc].c += last;
+    wc->wcc[ewc].n++;
+    if (wc->wcc_all)
+    {
+        wc->wc_depth--;
+        if (ewc == ewcRUN)
+        {
+            wallcycle_all_stop(wc,ewc,cycle);
+        }
+        else if (wc->wc_depth == 2)
+        {
+            wallcycle_all_start(wc,ewc,cycle);
+        }
+    }
 
-  return last;
+    return last;
 }
 
 void wallcycle_reset_all(gmx_wallcycle_t wc)
 {
     int i;
 
-    if (wc)
+    if (wc == NULL)
     {
-        for(i=0; i<ewcNR; i++)
-        {
-            wc[i].n = 0;
-            wc[i].c = 0;
-            wc[i].start = 0;
-            wc[i].last = 0;
-        }
+        return;
+    }
+
+    for(i=0; i<ewcNR; i++)
+    {
+        wc->wcc[i].n = 0;
+        wc->wcc[i].c = 0;
+        wc->wcc[i].start = 0;
+        wc->wcc[i].last = 0;
     }
 }
 
 void wallcycle_sum(t_commrec *cr, gmx_wallcycle_t wc,double cycles[])
 {
+    wallcc_t *wcc;
     double buf[ewcNR],*cyc_all,*buf_all;
     int    i;
 
-    if (wc)
+    if (wc == NULL)
     {
-        if (wc[ewcDDCOMMLOAD].n > 0)
-        {
-            wc[ewcDOMDEC].c -= wc[ewcDDCOMMLOAD].c;
-        }
-        if (wc[ewcDDCOMMBOUND].n > 0)
-        {
-            wc[ewcDOMDEC].c -= wc[ewcDDCOMMBOUND].c;
-        }
-        if (wc[ewcPMEMESH].n > 0)
-        {
-            wc[ewcFORCE].c -= wc[ewcPMEMESH].c;
-        }
+        return;
+    }
 
-        if (wc[ewcPMEMESH_SEP].n > 0)
-        {
-            /* This must be a PME only node, calculate the Wait + Comm. time */
-            wc[ewcPMEWAITCOMM].c = wc[ewcRUN].c - wc[ewcPMEMESH_SEP].c;
-        }
-        else
-        {
-            /* Correct the PME mesh only call count */
-            wc[ewcPMEMESH_SEP].n = wc[ewcFORCE].n;
-            wc[ewcPMEWAITCOMM].n = wc[ewcFORCE].n;
-        }
-        
-        /* Store the cycles in a double buffer for summing */
+    wcc = wc->wcc;
+
+    if (wcc[ewcDDCOMMLOAD].n > 0)
+    {
+        wcc[ewcDOMDEC].c -= wcc[ewcDDCOMMLOAD].c;
+    }
+    if (wcc[ewcDDCOMMBOUND].n > 0)
+    {
+        wcc[ewcDOMDEC].c -= wcc[ewcDDCOMMBOUND].c;
+    }
+    if (wcc[ewcPMEMESH].n > 0)
+    {
+        wcc[ewcFORCE].c -= wcc[ewcPMEMESH].c;
+    }
+    
+    if (wcc[ewcPMEMESH_SEP].n > 0)
+    {
+        /* This must be a PME only node, calculate the Wait + Comm. time */
+        wcc[ewcPMEWAITCOMM].c = wcc[ewcRUN].c - wcc[ewcPMEMESH_SEP].c;
+    }
+    else
+    {
+        /* Correct the PME mesh only call count */
+        wcc[ewcPMEMESH_SEP].n = wcc[ewcFORCE].n;
+        wcc[ewcPMEWAITCOMM].n = wcc[ewcFORCE].n;
+    }
+    
+    /* Store the cycles in a double buffer for summing */
+    for(i=0; i<ewcNR; i++)
+    {
+        cycles[i] = (double)wcc[i].c;
+    }
+    
+    if (wcc[ewcUPDATE].n > 0)
+    {
+        /* Remove the constraint part from the update count */
+        cycles[ewcUPDATE] -= cycles[ewcCONSTR];
+    }
+    
+#ifdef GMX_MPI    
+    if (cr->nnodes > 1)
+    {
+        MPI_Allreduce(cycles,buf,ewcNR,MPI_DOUBLE,MPI_SUM,
+                      cr->mpi_comm_mysim);
         for(i=0; i<ewcNR; i++)
         {
-            cycles[i] = (double)wc[i].c;
+            cycles[i] = buf[i];
         }
-        
-        if (wc[ewcUPDATE].n > 0)
+        if (wc->wcc_all != NULL)
         {
-            /* Remove the constraint part from the update count */
-            cycles[ewcUPDATE] -= cycles[ewcCONSTR];
-        }
-        
-#ifdef GMX_MPI    
-        if (cr->nnodes > 1)
-        {
-            MPI_Allreduce(cycles,buf,ewcNR,MPI_DOUBLE,MPI_SUM,
+            snew(cyc_all,ewcNR*ewcNR);
+            snew(buf_all,ewcNR*ewcNR);
+            for(i=0; i<ewcNR*ewcNR; i++)
+            {
+                cyc_all[i] = wc->wcc_all[i].c;
+            }
+            MPI_Allreduce(cyc_all,buf_all,ewcNR*ewcNR,MPI_DOUBLE,MPI_SUM,
                           cr->mpi_comm_mysim);
-            for(i=0; i<ewcNR; i++)
+            for(i=0; i<ewcNR*ewcNR; i++)
             {
-                cycles[i] = buf[i];
+                wc->wcc_all[i].c = buf_all[i];
             }
-            if (wc_all)
-            {
-                snew(cyc_all,ewcNR*ewcNR);
-                snew(buf_all,ewcNR*ewcNR);
-                for(i=0; i<ewcNR*ewcNR; i++)
-                {
-                    cyc_all[i] = wc_all[i].c;
-                }
-                MPI_Allreduce(cyc_all,buf_all,ewcNR*ewcNR,MPI_DOUBLE,MPI_SUM,
-                              cr->mpi_comm_mysim);
-                for(i=0; i<ewcNR*ewcNR; i++)
-                {
-                    wc_all[i].c = buf_all[i];
-                }
-                sfree(buf_all);
-                sfree(cyc_all);
-            }
+            sfree(buf_all);
+            sfree(cyc_all);
         }
-#endif
     }
+#endif
 }
 
 static void print_cycles(FILE *fplog, double c2t, const char *name, int nnodes,
@@ -279,64 +336,82 @@ static void print_cycles(FILE *fplog, double c2t, const char *name, int nnodes,
 void wallcycle_print(FILE *fplog, int nnodes, int npme, double realtime,
 		     gmx_wallcycle_t wc, double cycles[])
 {
-  double c2t,tot,sum;
-  int    i,j,npp;
-  char   buf[STRLEN];
-  const char *myline = "-----------------------------------------------------------------------";
-  
-  if (wc) {
-    if (npme > 0) {
-      npp = nnodes - npme;
-    } else {
-      npp = nnodes;
+    double c2t,tot,sum;
+    int    i,j,npp;
+    char   buf[STRLEN];
+    const char *myline = "-----------------------------------------------------------------------";
+    
+    if (wc == NULL)
+    {
+        return;
+    }
+
+    if (npme > 0)
+    {
+        npp = nnodes - npme;
+    }
+    else
+    {
+        npp = nnodes;
     }
     tot = cycles[ewcRUN];
     /* Conversion factor from cycles to seconds */
     if (tot > 0)
+    {
       c2t = nnodes*realtime/tot;
+    }
     else
+    {
       c2t = 0;
+    }
 
     fprintf(fplog,"\n     R E A L   C Y C L E   A N D   T I M E   A C C O U N T I N G\n\n");
 
     fprintf(fplog," Computing:         Nodes     Number     G-Cycles    Seconds     %c\n",'%');
     fprintf(fplog,"%s\n",myline);
     sum = 0;
-    for(i=ewcPPDURINGPME+1; i<ewcNR; i++) {
-      print_cycles(fplog,c2t,wcn[i],
-		   (i==ewcPMEMESH_SEP || i==ewcPMEWAITCOMM) ? npme : npp,
-		   wc[i].n,cycles[i],tot);
-      sum += cycles[i];
+    for(i=ewcPPDURINGPME+1; i<ewcNR; i++)
+    {
+        print_cycles(fplog,c2t,wcn[i],
+                     (i==ewcPMEMESH_SEP || i==ewcPMEWAITCOMM) ? npme : npp,
+                     wc->wcc[i].n,cycles[i],tot);
+        sum += cycles[i];
     }
-    if (wc_all) {
-      for(i=0; i<ewcNR; i++) {
-	for(j=0; j<ewcNR; j++) {
-	  sprintf(buf,"%-9s",wcn[i]);
-	  buf[9] = ' ';
-	  sprintf(buf+10,"%-9s",wcn[j]);
-	  buf[19] = '\0';
-	  print_cycles(fplog,c2t,buf,
-		       (i==ewcPMEMESH_SEP || i==ewcPMEWAITCOMM) ? npme : npp,
-		       wc_all[i*ewcNR+j].n,wc_all[i*ewcNR+j].c,tot);
-	  sum += wc_all[i*ewcNR+j].c;
-	}
-      }
+    if (wc->wcc_all != NULL)
+    {
+        for(i=0; i<ewcNR; i++)
+        {
+            for(j=0; j<ewcNR; j++)
+            {
+                sprintf(buf,"%-9s",wcn[i]);
+                buf[9] = ' ';
+                sprintf(buf+10,"%-9s",wcn[j]);
+                buf[19] = '\0';
+                print_cycles(fplog,c2t,buf,
+                             (i==ewcPMEMESH_SEP || i==ewcPMEWAITCOMM) ? npme : npp,
+                             wc->wcc_all[i*ewcNR+j].n,
+                             wc->wcc_all[i*ewcNR+j].c,
+                             tot);
+                sum += wc->wcc_all[i*ewcNR+j].c;
+            }
+        }
     }
     print_cycles(fplog,c2t,"Rest",npp,0,tot-sum,tot);
     fprintf(fplog,"%s\n",myline);
     print_cycles(fplog,c2t,"Total",nnodes,0,tot,tot);
     fprintf(fplog,"%s\n",myline);
-
-    if (cycles[ewcMoveE] > tot*0.05) {
-      sprintf(buf,
-	      "NOTE: %d %% of the run time was spent communicating energies,\n"
-              "      you might want to use the -nosum option of mdrun\n",
-	      (int)(100*cycles[ewcMoveE]/tot+0.5));
-      if (fplog) {
-	fprintf(fplog,"\n%s\n",buf);
-      }
-      /* Only the sim master calls this function, so always print to stderr */
-      fprintf(stderr,"\n%s\n",buf);
+    
+    if (cycles[ewcMoveE] > tot*0.05)
+    {
+        sprintf(buf,
+                "NOTE: %d %% of the run time was spent communicating energies,\n"
+                "      you might want to use the -nosum option of mdrun\n",
+                (int)(100*cycles[ewcMoveE]/tot+0.5));
+        if (fplog)
+        {
+            fprintf(fplog,"\n%s\n",buf);
+        }
+        /* Only the sim master calls this function, so always print to stderr */
+        fprintf(stderr,"\n%s\n",buf);
     }
-  }
 }
