b28de915ea4a0a86ba6597c3de44802ad7966b06
Sander Pronk <sander@csbm09.cbr.su.se>
2009-08-14 20:40:06 +0200

    added cmake/ThreadMPI

---
+++ cmake/ThreadMPI.cmake
@@ -0,0 +1,22 @@
+
+
+include(CheckIncludeFiles)
+
+#option(THREAD_PTHREADS "Use posix threads" ON)
+
+include(FindThreads)
+if (CMAKE_USE_PTHREADS_INIT)
+    check_include_files(pthread.h    HAVE_PTHREAD_H)
+    set(THREAD_PTHREADS 1)
+    add_definitions(-DTHREAD_PTHREADS)
+    set(THREAD_SRC pthreads.c)
+    set(THREAD_LIBRARY ${CMAKE_THREAD_LIBS_INIT})
+else (CMAKE_USE_PTHREADS_INIT)
+    if (CMAKE_USE_WIN32_THREADS_INIT)
+        set(THREAD_WIN32 1)
+        add_definitions(-DTHREAD_WIN32)
+        set(THREAD_SRC winthreads.c)
+        set(THREAD_LIBRARY )
+    endif (CMAKE_USE_WIN32_THREADS_INIT)
+endif (CMAKE_USE_PTHREADS_INIT)
+
