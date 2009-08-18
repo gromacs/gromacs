012345883c86c8e99d38e91c46ada46562ea9547
Sander Pronk <sander@csbm09.cbr.su.se>
2009-08-18 19:32:49 +0200

    gmxlib links thread_mpi functions directly

--- cmake/ThreadMPI.cmake
+++ cmake/ThreadMPI.cmake
@@ -9,13 +9,17 @@ if (CMAKE_USE_PTHREADS_INIT)
     check_include_files(pthread.h    HAVE_PTHREAD_H)
     #set(THREAD_PTHREADS 1)
     add_definitions(-DTHREAD_PTHREADS)
-    set(THREAD_SRC pthreads.c)
+    set(THREAD_MPI_SRC thread_mpi/threads.c thread_mpi/tmpi_init.c 
+                       thread_mpi/comm.c thread_mpi/send_recv.c 
+                       thread_mpi/multicast.c)
     set(THREAD_LIB ${CMAKE_THREAD_LIBS_INIT})
 else (CMAKE_USE_PTHREADS_INIT)
     if (CMAKE_USE_WIN32_THREADS_INIT)
         set(THREAD_WIN32 1)
         add_definitions(-DTHREAD_WIN32)
-        set(THREAD_SRC winthreads.c)
+        set(THREAD_MPI_SRC thread_mpi/threads.c thread_mpi/tmpi_init.c 
+                           thread_mpi/comm.c thread_mpi/send_recv.c 
+                           thread_mpi/multicast.c)
         set(THREAD_LIBRARY )
     endif (CMAKE_USE_WIN32_THREADS_INIT)
 endif (CMAKE_USE_PTHREADS_INIT)
