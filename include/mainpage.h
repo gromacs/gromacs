55b352f9fb2f4ee3e5143e5b7c2840d8ace9d9d5
Sander Pronk <sander@csbm09.cbr.su.se>
2009-09-18 13:21:19 +0200

    Added mainpage.h as the location for the main page of Doxygen documentation.
    include/mainpage.h is now the location of the 'main page' (index.html) of
    Doxygen documentation. When, at some point in the future, there is an
    obvious location for this documentation, we should move it there.

---
+++ include/mainpage.h
@@ -0,0 +1,13 @@
+/* This file just holds the main page for doxygen. When, at some point in the
+    future, there is an obvious location for this documentation, we should
+    move it there. */
+
+/** \mainpage Gromacs Gromacs 
+
+  GROMACS is a versatile package to perform molecular dynamics, i.e.  simulate 
+  the Newtonian equations of motion for systems with hundreds, to millions 
+  of particles.
+
+  */
+
+
