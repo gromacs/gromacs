d9ea73386ef2229c54974909ebafe6555cbeb23e
Erik Lindahl <lindahl@cbr.su.se>
2009-09-27 12:03:06 -0700

    Fixed more headers for MSVC cmake builds

--- src/gmxlib/nonbonded/nb_kernel_sse2_single/nb_kernel_allvsallgb_sse2_single.h
+++ src/gmxlib/nonbonded/nb_kernel_sse2_single/nb_kernel_allvsallgb_sse2_single.h
@@ -1,6 +1,6 @@
 
-#ifndef _NB_KERNEL_ALLVSALL_SSE2_SINGLE_H
-#define _NB_KERNEL_ALLVSALL_SSE2_SINGLE_H
+#ifndef _NB_KERNEL_ALLVSALLGB_SSE2_SINGLE_H
+#define _NB_KERNEL_ALLVSALLGB_SSE2_SINGLE_H
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -10,15 +10,16 @@
 #include "typedefs.h"
 
 void
-nb_kernel_allvsall_sse2_single(t_forcerec *           fr,
-                               t_mdatoms *            mdatoms,
-                               t_blocka *             excl,    
-                               real *                 x,
-                               real *                 f,
-                               real *                 Vc,
-                               real *                 Vvdw,
-                               int *                  outeriter,
-                               int *                  inneriter,
-                               void *                 work);
+nb_kernel_allvsallgb_sse2_single(t_forcerec *           fr,
+                                 t_mdatoms *            mdatoms,
+                                 t_blocka *             excl,    
+                                 real *                 x,
+                                 real *                 f,
+                                 real *                 Vc,
+                                 real *                 Vvdw,
+                                 real *                 Vpol,
+                                 int *                  outeriter,
+                                 int *                  inneriter,
+                                 void *                 work);
 
 #endif
