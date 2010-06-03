27da1a584823382789a2e2708c8f0db871e0d7a4
Berk Hess <hess@csbl10.(none)>
2010-06-03 10:33:35 +0200

    fixed bug in enum parsing error message

--- src/gmxlib/readinp.c
+++ src/gmxlib/readinp.c
@@ -463,12 +463,12 @@ int get_eeenum(int *ninp,t_inpfile **inp,const char *name,const char **defs,
       break;
   
   if (defs[i] == NULL) {
-    n = sprintf(buf,"Invalid enum '%s' for variable %s, using '%s'\n",
+    n += sprintf(buf,"Invalid enum '%s' for variable %s, using '%s'\n",
 		(*inp)[ii].value,name,defs[0]);
-    n = sprintf(buf+n,"Next time use one of:");
+    n += sprintf(buf+n,"Next time use one of:");
     j=0;
     while (defs[j]) {
-      n = sprintf(buf+n," '%s'",defs[j]);
+      n += sprintf(buf+n," '%s'",defs[j]);
       j++;
     }
     if (wi != NULL) {
