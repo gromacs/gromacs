8684c947198a1d0c2ecdf3d97a0ae14a3180a732
David van der Spoel <spoel@xray.bmc.uu.se>
2010-08-16 11:14:38 +0200

    Added a file physics.c with routines that convert values from and to gromacs units. The physics.h header file describes which units are supported (in a list of enums). Example: x = convert2gmx(y,eg2cAngstrom); will convert y in Angstrom to gromacs units (nm). Back transform by y = gmx2convert(x,eg2cAngstrom);

--- include/physics.h
+++ include/physics.h
@@ -140,6 +140,21 @@ extern "C" {
 #define unit_density_SI unit_mass_SI "/" unit_length_SI "^3"
 #define unit_invvisc_SI unit_length_SI " " unit_time_SI "/" unit_mass_SI
 
+  /* The routines below can be used for converting units from or to GROMACS
+     internal units. */
+  enum { eg2cAngstrom, eg2cNm, eg2cBohr, eg2cKcal_Mole, 
+	 eg2cHartree, eg2cHartree_e, eg2cAngstrom3, eg2cCoulomb,
+	 eg2cDebye, eg2cElectron, eg2cBuckingham, eg2cNR };
+  
+  /* Convert value x to GROMACS units. Energy -> Energy, Length -> Length etc. 
+     The type of x is deduced from unit, 
+     which should be taken from the enum above. */
+  extern double convert2gmx(double x,int unit);
+  
+  /* Conert value x from GROMACS units to the desired one. 
+     The type of return value is deduced from unit, see above */
+  double gmx2convert(double x,int unit);
+
 #ifdef __cplusplus
 }
 #endif
