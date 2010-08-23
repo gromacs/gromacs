824d8444ee2d53cafd2ed130f1cc2446a6b5f7b3
Sander Pronk <pronk@cbr.su.se>
2010-08-23 21:45:16 +0200

    Modified memory barrier on ppc/xlc.

--- include/thread_mpi/atomic/xlc_ppc.h
+++ include/thread_mpi/atomic/xlc_ppc.h
@@ -50,7 +50,7 @@ files.
 #endif
 
 
-#define tMPI_Atomic_memory_barrier()  { __asm__ __volatile__("\t eieio\n"\
+#define tMPI_Atomic_memory_barrier()  { __asm__ __volatile__("\t lwsync\n"\
                                                              : : :"memory" ); }
 
 
