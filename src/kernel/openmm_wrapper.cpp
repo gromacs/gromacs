a0489ee1b7cb5e34e66a3b7d8bd46ae55bbbf388
Szilard <pszilard@darklair.(none)>
2010-08-31 16:43:37 +0200

    Removed leftover debug message from openmm_wrapper

--- src/kernel/openmm_wrapper.cpp
+++ src/kernel/openmm_wrapper.cpp
@@ -1284,14 +1284,13 @@ void* openmm_init(FILE *fplog, const char *platformOptStr,
             }
 
             /* For now this is just to double-check if OpenMM selected the GPU we wanted,
-            but when we'll let OpenMM select the GPU automatically, it will query the devideId.
+            but when we'll let OpenMM select the GPU automatically, it will query the deviceId.
             */            
             if (tmp != devId)
             {
                 gmx_fatal(FARGS, "Internal error: OpenMM is using device #%d"
                         "while initialized for device #%d", tmp, devId);
             }        
-            cout << ">>>>> OpenMM devId=" << tmp << endl;
             
             /* check GPU compatibility */
             char gpuname[STRLEN];
