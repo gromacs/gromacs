gromacs/gromacs

commit d750b9ce3339061801319e19a141685664f893bc

Most (if not all) file operations are now thread safe.
The basic data structures and call interfaces in gmxfio and trxio have
been changed to allow for thread-safety (the original ones relied on global
variables to keep part of their state in).
Reading and writing from and to files with gmxfio has changed:
there is a file input/output data structure, and function names
are now more descriptive.
Reading trajectories has changed: there is a status data strucuture to keep
track of frame numbers and the file pointer.

src/gmxlib/checkpoint.c
@@ -1035,7 +1035,7 @@ void write_checkpoint(const char *fn,FILE *fplog,t_commrec *cr,
                       int eIntegrator,int simulation_part,
                       gmx_large_int_t step,double t,t_state *state)
 {
-    int  fp;
+    t_fileio *fp;
     int  file_version;
     char *version;
     char *btime;
@@ -1305,7 +1305,8 @@ static void read_checkpoint(const char *fn,FILE **pfplog,
                             t_state *state,bool *bReadRNG,bool *bReadEkin,
                             int *simulation_part,bool bAppendOutputFiles)
 {
-    int  fp,i,j,rc;
+    t_fileio *fp;
+    int  i,j,rc;
     int  file_version;
     char *version,*btime,*buser,*bmach,*fprog,*ftime;
 	char filename[STRLEN],buf[STEPSTRSIZE];
@@ -1316,11 +1317,11 @@ static void read_checkpoint(const char *fn,FILE **pfplog,
     int  ret;
 	gmx_file_position_t *outputfiles;
 	int  nfiles;
-	int chksum_file;
+	t_fileio *chksum_file;
 	FILE* fplog = *pfplog;
 	unsigned char digest[16];
 #if !((defined WIN32 || defined _WIN32 || defined WIN64 || defined _WIN64) && !defined __CYGWIN__ && !defined __CYGWIN32__)
-	struct flock fl = { F_WRLCK, SEEK_SET, 0,       0,     0 }; 
+	struct flock fl = { 0, SEEK_SET, 0,       F_WRLCK,     0 }; 
 #endif
 	
     const char *int_warn=
@@ -1684,7 +1685,7 @@ void load_checkpoint(const char *fn,FILE **fplog,
 	ir->simulation_part += 1;
 }
 
-static void read_checkpoint_data(int fp,int *simulation_part,
+static void read_checkpoint_data(t_fileio *fp,int *simulation_part,
                                  gmx_large_int_t *step,double *t,t_state *state,
                                  bool bReadRNG,
                                  int *nfiles,gmx_file_position_t **outputfiles)
@@ -1754,7 +1755,7 @@ void
 read_checkpoint_state(const char *fn,int *simulation_part,
                       gmx_large_int_t *step,double *t,t_state *state)
 {
-    int  fp;
+    t_fileio *fp;
     
     fp = gmx_fio_open(fn,"r");
     read_checkpoint_data(fp,simulation_part,step,t,state,TRUE,NULL,NULL);
@@ -1764,7 +1765,7 @@ read_checkpoint_state(const char *fn,int *simulation_part,
 	}
 }
 
-void read_checkpoint_trxframe(int fp,t_trxframe *fr)
+void read_checkpoint_trxframe(t_fileio *fp,t_trxframe *fr)
 {
     t_state state;
     int simulation_part;
@@ -1808,7 +1809,7 @@ void read_checkpoint_trxframe(int fp,t_trxframe *fr)
 
 void list_checkpoint(const char *fn,FILE *out)
 {
-    int  fp;
+    t_fileio *fp;
     int  file_version;
     char *version,*btime,*buser,*bmach,*fprog,*ftime;
     int  eIntegrator,simulation_part,nppnodes,npme;
@@ -1874,7 +1875,7 @@ bool read_checkpoint_simulation_part(const char *filename, int *simulation_part,
                                      bool bAppendReq,
                                      const char *part_suffix,bool *bAddPart)
 {
-    int  fp;
+    t_fileio *fp;
     gmx_large_int_t step=0;
 	double t;
     t_state state;
@@ -1887,7 +1888,7 @@ bool read_checkpoint_simulation_part(const char *filename, int *simulation_part,
     bAppend = FALSE;
 
     if (SIMMASTER(cr)) {
-        if(!gmx_fexist(filename) || ( (fp = gmx_fio_open(filename,"r")) < 0 ))
+        if(!gmx_fexist(filename) || (!(fp = gmx_fio_open(filename,"r")) ))
         {
             *simulation_part = 0;
         }

commit bbbf516aaef9af58ed9734c0cdafc6891fdd93e0

fixed locking of log file - broken by d750b9ce

src/gmxlib/checkpoint.c
@@ -1463,7 +1463,7 @@ static void read_checkpoint(const char *fn,FILE **pfplog,
 	FILE* fplog = *pfplog;
 	unsigned char digest[16];
 #if !((defined WIN32 || defined _WIN32 || defined WIN64 || defined _WIN64) && !defined __CYGWIN__ && !defined __CYGWIN32__)
-	struct flock fl = { 0, SEEK_SET, 0,       F_WRLCK,     0 }; 
+	struct flock fl = { F_WRLCK, SEEK_SET, 0,       0,     0 }; 
 #endif
 	
     const char *int_warn=