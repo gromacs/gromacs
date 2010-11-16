ddf2d29d86328ba580307cf4746d54114798a616
Berk Hess <hess@csbl10.(none)>
2010-11-16 09:07:09 +0100

    cmake fftw threading on by default

--- cmake/FindFFTW3.cmake
+++ cmake/FindFFTW3.cmake
@@ -1,35 +1,49 @@
 # - Find FFTW3
 # Find the native FFTW3 includes and library, double precision
 #
-#  FFTW3_INCLUDE_DIR    - where to find fftw3.h
-#  FFTW3_LIBRARIES   - List of libraries when using FFTW.
-#  FFTW3_FOUND       - True if FFTW found.
-#
-# The FFTW3 root installation directory can be provided in the FFTW3_ROOT_DIR
+#  FFTW3_INCLUDE_DIR    where to find fftw3.h
+#  FFTW3_LIBRARIES      List of libraries when using FFTW.
+#  FFTW3_FOUND          True if FFTW found.
+#  FFTW3_THREADS        True if threaded version was found
 
 if (FFTW3_INCLUDE_DIR AND FFTW3_LIBRARIES)
   # Already in cache, be silent
   set (FFTW3_FIND_QUIETLY TRUE)
 endif (FFTW3_INCLUDE_DIR AND FFTW3_LIBRARIES)
 
-file(TO_CMAKE_PATH "$ENV{FFTW3_ROOT_DIR}" _env_FFTW3_ROOT_DIR)
-
 find_path (FFTW3_INCLUDE_DIR fftw3.h
-                PATHS "${_env_FFTW3_ROOT_DIR}/include"
-		CACHE STRING "Path to double precision FFTW3 headers")
+		CACHE STRING "Path to headers for double precision FFTW3")
 
 find_library (FFTW3_LIBRARIES 
-		NAMES fftw3
-                PATHS "${_env_FFTW3_ROOT_DIR}/lib"
-                      "${FFTW3_INCLUDE_DIR}/../lib" 
-		CACHE STRING "Double precision FFTW3 libraries")
+                NAMES fftw3
+                PATHS "${FFTW3_INCLUDE_DIR}/../lib"
+                CACHE STRING "Double precision FFTW3 libraries")
+
+# if we are using threading try to find threaded fftw3
+if(GMX_OPENMP)
+    find_library (_FFTW3_thread_libs
+                    NAMES fftw3_threads
+                    PATHS "${FFTW3F_INCLUDE_DIR}/../lib")
+     if(_FFTW3_thread_libs)
+        set(_theads True)
+        list(APPEND FFTW3_LIBRARIES ${_FFTW3_thread_libs})
+    else()
+        message(WARNING "Threaded FFTW3F not found, FFT will run in serial (slower)!")
+    endif()
+endif()
+
 
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-set(__MSG "Could not find FFTW3. Provide the fftw3 install directory in the FFTW3_ROOT_DIR environment variable.")
-find_package_handle_standard_args (FFTW3 ${__MSG} FFTW3_LIBRARIES FFTW3_INCLUDE_DIR)
+find_package_handle_standard_args (FFTW3 DEFAULT_MSG FFTW3_LIBRARIES FFTW3_INCLUDE_DIR)
 
-mark_as_advanced (FFTW3_LIBRARIES FFTW3_INCLUDE_DIR)
+if(_threads)
+    set(FFTW3_THREADS CACHE STRING "Are we using threads FFTW3?" "True")
+else()
+    set(FFTW3_THREADS CACHE STRING "Are we using threads FFTW3?" "False")
+endif()
+
+mark_as_advanced (FFTW3_LIBRARIES FFTW3_INCLUDE_DIR FFTW3_THREADS)
 
 
--- cmake/FindFFTW3F.cmake
+++ cmake/FindFFTW3F.cmake
@@ -1,33 +1,47 @@
 # - Find FFTW3F
 # Find the native FFTW3 includes and library, single precision
 #
-#  FFTW3F_INCLUDE_DIR    - where to find fftw3.h
-#  FFTW3F_LIBRARIES   - List of libraries when using FFTW.
-#  FFTW3F_FOUND       - True if FFTW found.
-#
-# The FFTW3F root installation directory can be provided in the FFTW3F_ROOT_DIR
+#  FFTW3F_INCLUDE_DIR   where to find fftw3.h
+#  FFTW3F_LIBRARIES     List of libraries when using FFTW.
+#  FFTW3F_FOUND         True if FFTW found.
+#  FFTW3F_THREADS       True if threaded version was found
 
 if (FFTW3F_INCLUDE_DIR AND FFTW3F_LIBRARIES)
   # Already in cache, be silent
   set (FFTW3F_FIND_QUIETLY TRUE)
 endif (FFTW3F_INCLUDE_DIR AND FFTW3F_LIBRARIES)
 
-file(TO_CMAKE_PATH "$ENV{FFTW3F_ROOT_DIR}" _env_FFTW3F_ROOT_DIR)
-
 find_path (FFTW3F_INCLUDE_DIR fftw3.h 
-            PATHS "${_env_FFTW3F_ROOT_DIR}/include"
-	    CACHE STRING "Path to single precision FFTW3 headers")
+	CACHE STRING "Path to headers for single precision FFTW3")
 
 find_library (FFTW3F_LIBRARIES 
-		NAMES fftw3f
-                PATHS "${_env_FFTW3F_ROOT_DIR}/lib"
-                      "${FFTW3F_INCLUDE_DIR}/../lib" 
-		CACHE STRING "Single precision FFTW3 libraries")
+                NAMES fftw3f
+                PATHS "${FFTW3F_INCLUDE_DIR}/../lib"
+                CACHE STRING "Single precision FFTW3 libraries")
+
+# if we are using threading try to find threaded fftw3
+if(GMX_OPENMP)
+    find_library (_FFTW3F_thread_libs
+                    NAMES fftw3f_threads
+                    PATHS "${FFTW3F_INCLUDE_DIR}/../lib")
+    if(_FFTW3F_thread_libs)
+        set(_threads True)
+        list(APPEND FFTW3F_LIBRARIES ${_FFTW3F_thread_libs})
+    else()
+        message(WARNING "Threaded FFTW3F not found, FFT will run in serial (slower)!")
+    endif()
+endif()
+
 
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-set(__MSG "Could not find FFTW3F. Provide the fftw3 install directory in the FFTW3F_ROOT_DIR environment variable.")
-find_package_handle_standard_args (FFTW3F ${__MSG} FFTW3F_LIBRARIES FFTW3F_INCLUDE_DIR)
+find_package_handle_standard_args (FFTW3F DEFAULT_MSG FFTW3F_LIBRARIES FFTW3F_INCLUDE_DIR)
 
-mark_as_advanced (FFTW3F_LIBRARIES FFTW3F_INCLUDE_DIR)
+if(_threads)
+    set(FFTW3F_THREADS CACHE STRING "Are we using threads FFTW3F?" "True")
+else()
+    set(FFTW3F_THREADS CACHE STRING "Are we using threads FFTW3F?" "False")
+endif()
+
+mark_as_advanced (FFTW3F_LIBRARIES FFTW3F_INCLUDE_DIR FFTW3F_THREADS)
