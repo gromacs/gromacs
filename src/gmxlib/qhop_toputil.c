cd9b41d9c59fc6aae8d05d3fcefc667113ced44e
Erik Marklund <erikm@anfinsen.bmc.uu.se>
2011-02-11 16:30:02 +0100

    Added functions for (de)constraing hydrogens

--- src/gmxlib/qhop_toputil.c
+++ src/gmxlib/qhop_toputil.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "types/mdatom.h"
 #include "types/topology.h"
 #include "types/idef.h"
@@ -69,7 +70,6 @@ extern gmx_bool get_proton_presence(const qhop_H_exist *Hext, const int atomid)
   return Hext->H[Hext->atomid2H[atomid]] == (char)1;
 }
 
-
 extern void unindex_ilists(t_qhop_residue *qres)
 {
   int i, j;
@@ -87,7 +87,7 @@ extern void unindex_ilists(t_qhop_residue *qres)
   qres->nr_indexed = 0;
 }
 
-extern void index_ilists(t_qhop_residue *qres, qhop_db *db,
+extern void index_ilists(t_qhop_residue *qres, const qhop_db *db,
 			 const gmx_localtop_t *top, const t_commrec *cr)
 {
   int b, i, ni, *ra, ia, ft, itype, nia, *l2g, bt, j;
@@ -112,6 +112,12 @@ extern void index_ilists(t_qhop_residue *qres, qhop_db *db,
     {
 
       itype = db->rb.btype[bt];
+      if (itype <= 0)
+	{
+	  /* No bondeds of this type */
+	  continue;
+	}
+
       nia = interaction_function[itype].nratoms;
 
       i = 0;
@@ -149,24 +155,221 @@ extern void index_ilists(t_qhop_residue *qres, qhop_db *db,
 		}
 
 	      if (!bMatch)
+		{
+		  /* Test the other order */
+
+		  bMatch = TRUE;
+
+		  for (j=0; j<nia; j++)
+		    {
+		      /* Translate atoms in ilist to residue local numbers */
+		      ia = bDD ?
+			l2g[ilist[itype].iatoms[i+j]] - qres->atoms[0] : /* local != global*/
+			ilist[itype].iatoms[i+j] - qres->atoms[0];       /* local != global*/
+
+		      if (ia != ra[nia-j-1])
+			{
+			  /* No match. On to the next interaction; */
+			  bMatch = FALSE;
+			  break;
+			}
+		    }
+
+		}
+
+	      if (!bMatch)
 		{ 
 		  i += (nia); /* proceed with next interaction */
 		}
+	      else
+		{
+		  /* Ok, we've found it */
+		  break;
+		}
+
 	    } /* while */
+
+	  if (bMatch)
+	    {
+	      /* interaction found. */
+	      qres->bindex.ilist_pos[itype][b] = (t_iatom)i;
+	      /* Should one store ft somewhere? */
+	      qres->bindex.indexed[itype][b] = TRUE;
+	      qres->nr_indexed++;
+	    }
+	  else
+	    {
+	      gmx_fatal(FARGS, "Interaction not found in ilist");
+	    }
+
+
 	} /* bonded loop*/
 
-      if (bMatch)
+    }
+}
+
+/* returns the index in top->idef.il[?].iatom[] where bond to this proton is found. */
+extern int qhop_get_proton_bond_params(const qhop_db *db, const t_qhoprec *qr, t_qhop_atom *qatom, gmx_localtop_t *top, int proton_id, const t_commrec *cr)
+{
+  int b, bi, i, ai;
+  const int niatoms = 3;
+
+  gmx_bool bMatch;
+
+  t_restp *rtp;
+
+  t_qhop_residue *qres;
+
+  qres = &(qr->qhop_residues[qatom->qres_id]);
+
+  rtp  = &(db->rtp[db->rb.res[qres->rtype][qres->res].rtp]);
+
+  /* find the parameters for this constraint.
+   * We assume that there's only one constraint
+   * involving this hydrogen. This is NOT the case with
+   * angle constraints. */
+  for (b=0; b < rtp->rb[ebtsBONDS].nb; b++)
+    {
+      /* bi will index the list of bonded interactions of qres->rtype*/
+      bi = db->rb.res[qres->rtype][qres->res].biMap[ebtsBONDS][b];
+
+      for (i=0; i < (niatoms-1); i++)
 	{
-	  /* interaction found. */
-	  qres->bindex.ilist_pos[itype][b] = (t_iatom)i;
-	  qres->bindex.indexed[itype][b] = TRUE;
-	  qres->nr_indexed++;
-	}
-      else
-	{
-	  gmx_fatal(FARGS, "Interaction not found in ilist");
+	  /* Assume global atom numbering. CHANGE!*/
+	  ai = proton_id - qres->atoms[0]; /* residue local atom id of the proton. */
+	  if (db->rb.ba[qres->rtype][ebtsBONDS][bi][i] == ai)
+	    {
+	      return db->rb.res[qres->rtype][qres->res].findex[ebtsBONDS][bi];
+	    }
 	}
     }
+  gmx_fatal(FARGS, "Constraint not found!");
+}
+
+extern void qhop_constrain(t_qhop_residue *qres, t_qhoprec *qr, const qhop_db *db, gmx_localtop_t *top, int proton_id, const t_commrec *cr)
+{
+  int nr, nalloc, heavy, h, rt, r, b, bi, i, params, ai, *g2l, *iatoms;
+  const int niatoms = 3;
+
+  gmx_bool bMatch;
+
+  char *heavyname, *hname;
+
+  t_qhop_atom *qatom;
+
+  t_restp *rtp;
+
+  bMatch = FALSE;
+
+  if (qres->nr_indexed == 0)
+    {
+      index_ilists(qres, db, top, cr);
+    }
+
+  for (heavy=0; (heavy < qres->nr_titrating_sites) && (!bMatch); heavy++)
+    {
+      qatom = &(qr->qhop_atoms[qres->titrating_sites[heavy]]);
+      for (h=0; h < qatom->nr_protons; h++)
+	{
+	  if (qatom->protons[h] == proton_id)
+	    {
+	      heavyname = qatom->atomname;
+	      heavy = qatom->atom_id;
+	      bMatch = TRUE;
+	      break;
+	    }
+	}
+    }
+
+  if (!bMatch)
+    {
+      /* doing nothing. The proton wasn't found on this residue. */
+      return;
+    }
+
+  qhop_get_proton_bond_params(db, qr, qatom, top, proton_id, cr);
+  
+  if (DOMAINDECOMP(cr))
+    {
+      g2l = cr->dd->gatindex; /* use this to map global to local atomnumbers below */
+    }
+
+  /* Make a new entry at the end of the constraint ilist. */
+  nr = top->idef.il[F_CONSTR].nr;
+  nalloc = top->idef.il[F_CONSTR].nalloc;
+  
+  if (nr+niatoms > nalloc)
+    {
+      srenew(top->idef.il[F_CONSTR].iatoms, nalloc+niatoms);
+      top->idef.il[F_CONSTR].nalloc += niatoms;
+    }
+
+  top->idef.il[F_CONSTR].iatoms[nr]   = params;
+  top->idef.il[F_CONSTR].iatoms[nr+1] = DOMAINDECOMP(cr) ? g2l[heavy] : heavy;
+  top->idef.il[F_CONSTR].iatoms[nr+2] = DOMAINDECOMP(cr) ? g2l[h]     : h;
+  top->idef.il[F_CONSTR].nr += 3;
+
+  /* Update bonded index */
+
+  h = proton_id - qres->atoms[0]; /* Make residue local index */
+
+  /* Now find the bonded term we're adding */
+  for (b=0; b < qres->bindex.nr[F_CONSTR]; b++)
+    {
+      bi = db->rb.res[qres->rtype][qres->res].biMap[ebtsBONDS][b]; /* Map to restype bondeds */
+
+      rtp = &(db->rtp[db->rb.res[qres->rtype][qres->res].rtp]);
+
+      iatoms = db->rb.ba[qres->rtype][ebtsBONDS][bi];
+
+      if (h == iatoms[0] || h == iatoms[1])
+	{
+	  qres->bindex.ilist_pos[F_CONSTR][b] = nr;
+	  return;
+	}
+    }
+  gmx_fatal(FARGS, "Could not find the constraint in the rtp data.");
+}
+
+extern void qhop_deconstrain(t_qhop_residue *qres, const qhop_db *db, gmx_localtop_t *top, int proton_id, const t_commrec *cr)
+{
+  int b, ip, *iatoms;
+  const int niatoms = 3;
+
+  if (qres->nr_indexed == 0)
+    {
+      index_ilists(qres, db, top, cr);
+    }
+
+  for (b=0; b < qres->bindex.nr[F_CONSTR]; b++)
+    {
+      ip = qres->bindex.ilist_pos[F_CONSTR][b];
+
+      if (ip == -1)
+	{
+	  /* Can't remove this constraint, it's not really indexed
+	   * which probably means it's not there. */
+	  return;
+	}
+
+      iatoms = &(top->idef.il[F_CONSTR].iatoms[ip]);
+
+      if (iatoms[1] == proton_id || iatoms[2] == proton_id)
+	{
+	  /* Here it is. Remove it from the list. */
+
+	  /* Here the ilist is shifted by niatoms to erase the constraint in question. */
+	  memmove((void*)&(iatoms[0]),
+		  (void*)&(iatoms[niatoms]),
+		  top->idef.il[F_CONSTR].nr-ip-niatoms);
+
+	  top->idef.il[F_CONSTR].nr -= niatoms;
+
+	  qres->bindex.ilist_pos[F_CONSTR][b] = -1;
+
+	  break;
+	}
+    }  
 }
 
 
@@ -527,14 +730,6 @@ extern int which_subRes(const gmx_mtop_t *top, const t_qhoprec *qr,
 /* 	} */
 /*     } */
 }
-
-/* Do we need this? */
-static void invert_dihedrals(qhop_db *db, t_qhoprec *qr, t_qhop_residue *qres, t_qhop_atom *qatom, t_ilist *ilist)
-{
-  int i;
-  i=0;
-}
-
 
 extern void qhop_swap_bondeds(t_qhop_residue *swapres, qhop_res *prod)
 {
