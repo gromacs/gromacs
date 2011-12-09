58ba12aa8651d9b8b06c56c52247018349ce9024
Roland Schulz <roland@utk.edu>
2011-12-09 15:22:05 -0500

    Fixes name collision of min/max with C++ std::min/std::max
    Fixes issue #806
    min/max is defined as the macro for C compililation and
    as std::min/std::max for C++ compilation.
    
    Change-Id: I612d00e092ef4979a5a50b4e2d56fb767a04041e

--- src/gromacs/selection/params.cpp
+++ src/gromacs/selection/params.cpp
@@ -55,18 +55,6 @@
 #include "scanner.h"
 #include "selelem.h"
 
-template <typename T>
-static T min(T a, T b)
-{
-    return (a < b) ? a : b;
-}
-
-template <typename T>
-static T max(T a, T b)
-{
-    return (a > b) ? a : b;
-}
-
 /*!
  * \param[in] name   Name of the parameter to search.
  * \param[in] nparam Number of parameters in the \p param array.
