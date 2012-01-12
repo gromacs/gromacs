ded339ed5f7d7af8a8f26c44a47ff716a121e3c1
Roland Schulz <roland@utk.edu>
2012-01-12 11:21:47 -0500

    Merges in threading branch as one squashed commit
    Implements OpenMP threading for PME
    
    All PME (including SSE) code by Berk.
    FFT, timer, and CPU affinity code by Roland.
    Two bugfixes by Szilard.
    
    Conflicts:
    	include/gmx_wallcycle.h
    	src/gmxlib/trxio.c
    	src/mdlib/Makefile.am
    	src/mdlib/fft5d.c
    	src/mdlib/gmx_wallcycle.c
    	src/tools/acf.test.c
    
    Change-Id: I4bdd0e29caf135ba6b4cc7297640f29f0d136656

--- admin/GerritBuild.bat
+++ admin/GerritBuild.bat
@@ -1,6 +1,17 @@
 REM Requires Windows SDK and CMake to be in the path
-REM Run Windows SDK SetEnv script
-SetEnv /Release && ^
-cmake -G "Visual Studio 10 Win64" -D GMX_DOUBLE=%GMX_DOUBLE% -D GMX_MPI=%GMX_MPI% -D GMX_OPENMP=%GMX_OPENMP% -DGMX_DEFAULT_SUFFIX=off . && ^
+REM OpenMP is only available in SDK 6.1. Both SDKs support 32bit and 64bit.
+REM Arbirtary choice to use 32bit with 6.1 (default)
+REM 6.1 requires delayed expansion. If not enabled restarting cmd with /v
+set A=B
+if not !A!==B ( cmd /v /c admin\GerritBuild )
+if "%GMX_OPENMP%"=="on" (
+   set SetEnvCmd="C:\Program Files\Microsoft SDKs\Windows\v6.1\Bin\SetEnv" /Release
+   set Target="NMake Makefiles"
+) else (
+   set SetEnvCmd="C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv" /Release
+   set Target="Visual Studio 10 Win64"
+)
+%SetEnvCmd% && ^
+cmake -G %Target% -D GMX_DOUBLE=%GMX_DOUBLE% -D GMX_MPI=%GMX_MPI% -D GMX_OPENMP=%GMX_OPENMP% -DGMX_DEFAULT_SUFFIX=off . && ^
 msbuild /m:2 /p:Configuration=MinSizeRel All_Build.vcxproj && ^
 ctest -D ExperimentalTest -C MinSizeRel -V
