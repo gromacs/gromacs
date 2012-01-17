bd3c143d73b7bffcfb51ac3ebb2d512e2990f830
Michael Shirts <michael.shirts@virginia.edu>
2012-01-17 09:51:13 -0500

    Merging in free energy, expanded ensemble, and andersen temperature control with 4.6 Conflicts: 	src/gmxlib/mvdata.c 	src/gmxlib/txtdump.c 	src/kernel/readir.c 	src/mdlib/sim_util.c 	src/tools/gmx_membed.c
    Change-Id: Ide69a8dbfb96072d5c55d0a051dc9db788cbe144

--- include/typedefs.h
+++ include/typedefs.h
@@ -137,7 +137,9 @@ void init_inputrec(t_inputrec *ir);
 void init_energyhistory(energyhistory_t * enerhist);
 void done_energyhistory(energyhistory_t * enerhist);
 void init_gtc_state(t_state *state,int ngtc, int nnhpres, int nhchainlength);
-void init_state(t_state *state,int natoms,int ngtc, int nnhpres, int nhchainlength);
+void init_state(t_state *state,int natoms,int ngtc, int nnhpres, int nhchainlength, int nlambda);
+void init_df_history(df_history_t *dfhist, int nlambda, real wl_delta);
+void copy_df_history(df_history_t * df_dest, df_history_t *df_source);
 
 void copy_blocka(const t_blocka *src,t_blocka *dest);
 
