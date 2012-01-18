0ea88f06c6093521e640cbbd31ac36c2d1a8040b
Roland Schulz <roland@utk.edu>
2012-01-18 04:21:18 -0500

    Not ready yet: New approach for GerritBuild script
    - Allows to make changes to build in seperate RelEng repository
    - Replaces Matrix view by a linear list of configurations
    
    I submit this even though it isn't ready yet to be able to test.
    
    Change-Id: I7f5c76805836c4cd06527b949ab7ac68f27a31b5

--- admin/GerritBuild.bat
+++ admin/GerritBuild.bat
@@ -1,7 +1,7 @@
-REM Requires Windows SDK and CMake to be in the path
-REM Run Windows SDK SetEnv script
-SetEnv /Release && ^
-cmake -G "Visual Studio 10 Win64" -D GMX_DOUBLE=%GMX_DOUBLE% -D GMX_MPI=%GMX_MPI% -D GMX_OPENMP=%GMX_OPENMP% -DGMX_DEFAULT_SUFFIX=off . && ^
-msbuild /m:2 /p:Configuration=MinSizeRel All_Build.vcxproj && ^
-ctest -D ExperimentalTest -C MinSizeRel -V
-
+mkdir releng
+cd releng
+git init
+git fetch ssh://jenkins@gerrit.gromacs.org:29418/releng refs/heads/1
+git checkout -q -f FETCH_HEAD
+cd ..
+python -u releng/GerritBuild.py
