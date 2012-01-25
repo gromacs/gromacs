1f32a224f7544db6a361f65f1c0eac6367f40d09
Christoph Junghans <junghans@votca.org>
2012-01-24 17:27:39 -0700

    make gromacs manual build again
    * bug was introduced in 78534fcaabfc8e3990ea50b1a798dc7713995745
    * updated ref to make it consistent with the manual
    
    Change-Id: I9664bfa5e4d6487ed664d2f38bebd1daa92496ba

--- src/gmxlib/copyrite.c
+++ src/gmxlib/copyrite.c
@@ -527,7 +527,7 @@ void please_cite(FILE *fp,const char *key)
       "S. Fritsch, C. Junghans and K. Kremer",
       "Adaptive molecular simulation study on structure formation of toluene around C60 using Gromacs",
       "J. Chem. Theo. Comp.",
-      0, 2012, "???" },
+      0, 2012, "doi:10.1021/ct200706f" },
     { "Junghans10",
       "C. Junghans and S. Poblete",
       "A reference implementation of the adaptive resolution scheme in ESPResSo",
