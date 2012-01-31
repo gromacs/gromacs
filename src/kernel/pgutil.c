b25390095eaf1c757c33d10bb632ae79cf0dd5a4
Mark Abraham <mark.j.abraham@gmail.com>
2012-02-01 00:43:18 +1100

    Improved error message
    Users would be confused by "Atom N3 is used in an interaction of
    type atom in the topology database..." so when "atom" is the
    bondtype we don't mention the fact.
    
    Change-Id: Ie8cb3ba64f39d3746b470fa6ae44f5a6f4b6a16c

--- src/kernel/pgutil.c
+++ src/kernel/pgutil.c
@@ -42,24 +42,38 @@
 #include "string.h"
 #include "gmx_fatal.h"
 
+#define BUFSIZE 1024
 static void atom_not_found(int fatal_errno,const char *file,int line,
 			   const char *atomname,int resind,
 			   const char *bondtype,gmx_bool bDontQuit)
 {
-  if (strcmp(bondtype,"check") != 0) {
-    if (bDontQuit) {
-      gmx_warning("WARNING: Atom %s is used in an interaction of type %s in the\n"
-		  "topology database, but an atom of that name was not found in\n"
-		  "residue number %d.\n",
-		  atomname,bondtype,resind+1);
-    } else {
-      gmx_fatal(fatal_errno,file,line,
-		"Atom %s is used in an interaction of type %s in the topology\n"
-		"database, but an atom of that name was not found in residue\n"
-		"number %d.\n",
-		atomname,bondtype,resind+1);
+    char message_buffer[BUFSIZE];
+    if (strcmp(bondtype,"check") != 0)
+    {
+        if (0 != strcmp(bondtype, "atom"))
+        {
+            snprintf(message_buffer, 1024,
+                     "Atom %s is used in an interaction of type %s in the topology\n"
+                     "database, but an atom of that name was not found in residue\n"
+                     "number %d.\n",
+                     atomname,bondtype,resind+1);
+        }
+        else
+        {
+            snprintf(message_buffer, 1024,
+                     "Atom %s is used in the topology database, but an atom of that\n"
+                     "name was not found in residue number %d.\n",
+                     atomname,resind+1);
+        }
+        if (bDontQuit)
+        {
+            gmx_warning("WARNING: %s", message_buffer);
+        }
+        else
+        {
+            gmx_fatal(fatal_errno,file,line,message_buffer);
+        }
     }
-  }
 }
 	
 atom_id search_atom(const char *type,int start,int natoms,t_atom at[],
