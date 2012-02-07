3d705af4c7672ba3662cc39f38df7ae8df7fbab9
Szilard Pall <pszilard@cbr.su.se>
2012-02-07 19:57:15 +0100

    OpenMP fix in fft5d_plan_3d
    This causes a crash with gcc 4.5/4.6, but only when *not* compiling with
    thread-MPI. This might be caused by a gcc bug.

--- src/mdlib/fft5d.c
+++ src/mdlib/fft5d.c
@@ -529,6 +529,7 @@ fft5d_plan fft5d_plan_3d(int NG, int MG, int KG, MPI_Comm comm[2], int flags, t_
              */
 #pragma omp parallel for num_threads(nthreads) schedule(static) ordered
             for(t=0; t<nthreads; t++)
+#pragma omp ordered
             {
                 int tsize = ((t+1)*pM[s]*pK[s]/nthreads)-(t*pM[s]*pK[s]/nthreads);
 
