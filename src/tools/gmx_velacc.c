68063591c63dd0d1700ef4f692963a287f0e1bb5
David van der Spoel <spoel@xray.bmc.uu.se>
2012-02-28 21:53:28 +0100

    Vibration spectra from velocit ACF or Normal modes.
    The velocity autocorrelation function can be converted into
    a vibrational spectrum by performing a fourier transform
    and plotting the amplitude squared as a function of
    frequency or wavenumber. This has now been automated in
    g_velacc with the -os flag. Likewise, an option -os
    has been added to the g_nmeig program to produce a spectrum
    based on the harmonic approximation.
    
    Change-Id: I7e3d669b9108e3a91856ff20688f6216a035bf4a

--- src/tools/gmx_velacc.c
+++ src/tools/gmx_velacc.c
@@ -57,7 +57,7 @@
 #include "strdb.h"
 #include "xvgr.h"
 #include "gmx_ana.h"
-
+#include "gmx_fft.h"
 
 static void index_atom2mol(int *n,atom_id *index,t_block *mols)
 {
@@ -106,7 +106,55 @@ static void precalc(t_topology top,real normm[]){
 
 }
 
+static void calc_spectrum(int n,real c[],real dt,const char *fn,
+			  output_env_t oenv,gmx_bool bRecip)
+{
+  FILE *fp;
+  gmx_fft_t fft;
+  int  i,status;
+  real *data;
+  real nu,omega,recip_fac;
 
+  snew(data,n*2);
+  for(i=0; (i<n); i++)
+    data[i] = c[i];
+
+  if ((status = gmx_fft_init_1d_real(&fft,n,GMX_FFT_FLAG_NONE)) != 0)
+    gmx_fatal(FARGS,"Invalid fft return status %d",status);
+
+  if ((status = gmx_fft_1d_real(fft, GMX_FFT_REAL_TO_COMPLEX,data,data)) != 0)
+    gmx_fatal(FARGS,"Invalid fft return status %d",status);
+
+  fp = xvgropen(fn,"Vibrational Power Spectrum",
+		bRecip ? "\\f{12}w\\f{4} (cm\\S-1\\N)" :
+		"\\f{12}n\\f{4} (ps\\S-1\\N)",
+		"a.u.",oenv);
+  /* This is difficult.
+   * The length of the ACF is dt (as passed to this routine).
+   * We pass the vacf with N time steps from 0 to dt.
+   * That means that after FFT we have lowest frequency = 1/dt
+   * then 1/(2 dt) etc. (this is the X-axis of the data after FFT).
+   * To convert to 1/cm we need to have to realize that
+   * E = hbar w = h nu = h c/lambda. We want to have reciprokal cm
+   * on the x-axis, that is 1/lambda, so we then have
+   * 1/lambda = nu/c. Since nu has units of 1/ps and c has gromacs units
+   * of nm/ps, we need to multiply by 1e7.
+   * The timestep between saving the trajectory is
+   * 1e7 is to convert nanometer to cm
+   */
+  recip_fac = bRecip ? (1e7/SPEED_OF_LIGHT) : 1.0;
+  for(i=0; (i<n); i+=2) {
+    nu = i/(2*dt);
+    omega = nu*recip_fac;
+    /* Computing the square magnitude of a complex number, since this is a power
+     * spectrum.
+     */
+    fprintf(fp,"%10g  %10g\n",omega,sqr(data[i])+sqr(data[i+1]));
+  }
+  xvgrclose(fp);
+  gmx_fft_destroy(fft);
+  sfree(data);
+}
 
 int gmx_velacc(int argc,char *argv[])
 {
@@ -123,10 +171,12 @@ int gmx_velacc(int argc,char *argv[])
     "much shorter than the time scale of the autocorrelation."
   };
   
-  static gmx_bool bM=FALSE,bMol=FALSE;
+  static gmx_bool bMass=FALSE,bMol=FALSE,bRecip=TRUE;
   t_pargs pa[] = {
-    { "-m", FALSE, etBOOL, {&bM},
+    { "-m", FALSE, etBOOL, {&bMass},
       "Calculate the momentum autocorrelation function" },
+    { "-recip", FALSE, etBOOL, {&bRecip},
+      "Use cm^-1 on X-axis instead of 1/ps for spectra." },
     { "-mol", FALSE, etBOOL, {&bMol},
       "Calculate the velocity acf of molecules" }
   };
@@ -140,10 +190,14 @@ int gmx_velacc(int argc,char *argv[])
   atom_id    *index;
   char       *grpname;
   char       title[256];
-  real       t0,t1,m;
+  /* t0, t1 are the beginning and end time respectively. 
+   * dt is the time step, mass is temp variable for atomic mass.
+   */
+  real       t0,t1,dt,mass;
   t_trxstatus *status;
-  int        teller,n_alloc,i,j,tel3,k,l;
+  int        counter,n_alloc,i,j,counter_dim,k,l;
   rvec       mv_mol;
+  /* Array for the correlation function */
   real       **c1;
   real	     *normm=NULL;
   output_env_t oenv;
@@ -154,7 +208,8 @@ int gmx_velacc(int argc,char *argv[])
     { efTRN, "-f",    NULL,   ffREAD  },
     { efTPS, NULL,    NULL,   ffOPTRD }, 
     { efNDX, NULL,    NULL,   ffOPTRD },
-    { efXVG, "-o",    "vac",  ffWRITE }
+    { efXVG, "-o",    "vac",  ffWRITE },
+    { efXVG, "-os",   "spectrum", ffOPTWR }
   };
 #define NFILE asize(fnm)
   int     npargs;
@@ -166,7 +221,7 @@ int gmx_velacc(int argc,char *argv[])
   parse_common_args(&argc,argv,PCA_CAN_VIEW | PCA_CAN_TIME | PCA_BE_NICE,
 		    NFILE,fnm,npargs,ppa,asize(desc),desc,0,NULL,&oenv);
 
-  if (bMol || bM) {
+  if (bMol || bMass) {
     bTPS = ftp2bSet(efTPS,NFILE,fnm) || !ftp2bSet(efNDX,NFILE,fnm);
   }
 
@@ -194,58 +249,65 @@ int gmx_velacc(int argc,char *argv[])
   t0=fr.time;
       
   n_alloc=0;
-  teller=0;
+  counter=0;
   do {
-    if (teller >= n_alloc) {
+    if (counter >= n_alloc) {
       n_alloc+=100;
       for(i=0; i<gnx; i++)
 	srenew(c1[i],DIM*n_alloc);
     }
-    tel3=3*teller;
+    counter_dim=DIM*counter;
     if (bMol)
       for(i=0; i<gnx; i++) {
 	clear_rvec(mv_mol);
 	k=top.mols.index[index[i]];
 	l=top.mols.index[index[i]+1];
 	for(j=k; j<l; j++) {
-   	  if (bM)
-	    m = top.atoms.atom[j].m;
+   	  if (bMass)
+	    mass = top.atoms.atom[j].m;
 	  else
-	    m = normm[j];
-	  mv_mol[XX] += m*fr.v[j][XX];
-	  mv_mol[YY] += m*fr.v[j][YY];
-	  mv_mol[ZZ] += m*fr.v[j][ZZ];
+	    mass = normm[j];
+	  mv_mol[XX] += mass*fr.v[j][XX];
+	  mv_mol[YY] += mass*fr.v[j][YY];
+	  mv_mol[ZZ] += mass*fr.v[j][ZZ];
 	}
-	c1[i][tel3+XX]=mv_mol[XX];
-	c1[i][tel3+YY]=mv_mol[YY];
-	c1[i][tel3+ZZ]=mv_mol[ZZ];
+	c1[i][counter_dim+XX]=mv_mol[XX];
+	c1[i][counter_dim+YY]=mv_mol[YY];
+	c1[i][counter_dim+ZZ]=mv_mol[ZZ];
       }
      else
       for(i=0; i<gnx; i++) {
-        if (bM)
-	  m = top.atoms.atom[index[i]].m;
+        if (bMass)
+	  mass = top.atoms.atom[index[i]].m;
 	else
-	 m = 1;
-	c1[i][tel3+XX]=m*fr.v[index[i]][XX];
-	c1[i][tel3+YY]=m*fr.v[index[i]][YY];
-	c1[i][tel3+ZZ]=m*fr.v[index[i]][ZZ];
+	 mass = 1;
+	c1[i][counter_dim+XX]=mass*fr.v[index[i]][XX];
+	c1[i][counter_dim+YY]=mass*fr.v[index[i]][YY];
+	c1[i][counter_dim+ZZ]=mass*fr.v[index[i]][ZZ];
       }
 
     t1=fr.time;
 
-    teller ++;
+    counter ++;
   } while (read_next_frame(oenv,status,&fr));
   
-	close_trj(status);
-
-  do_autocorr(ftp2fn(efXVG,NFILE,fnm), oenv,
-	      bM ? 
+  close_trj(status);
+  /* Compute time step between frames */
+  dt = (t1-t0)/(counter-1);
+  do_autocorr(opt2fn("-o",NFILE,fnm), oenv,
+	      bMass ? 
 	      "Momentum Autocorrelation Function" :
 	      "Velocity Autocorrelation Function",
-	      teller,gnx,c1,(t1-t0)/(teller-1),eacVector,TRUE);
-  
-  do_view(oenv,ftp2fn(efXVG,NFILE,fnm),"-nxy");
-  
+	      counter,gnx,c1,dt,eacVector,TRUE);
+
+  do_view(oenv,opt2fn("-o",NFILE,fnm),"-nxy");
+
+  if (opt2bSet("-os",NFILE,fnm)) {
+    calc_spectrum(counter/2,(real *) (c1[0]),(t1-t0)/2,opt2fn("-os",NFILE,fnm),
+		  oenv,bRecip);
+    do_view(oenv,opt2fn("-os",NFILE,fnm),"-nxy");
+  }
+
   thanx(stderr);
   
   return 0;
