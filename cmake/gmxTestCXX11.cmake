ef67afb73fcb1f05805cd23195e2de5569b24cf6
Roland Schulz <roland@utk.edu>
2012-03-02 19:16:07 -0500

    Use smart pointers for internal memory management.
    - Replaced uses of std::auto_ptr with boost::scoped_ptr where memory was
      allocated locally and release() was not used, or refactored the use of
      pointers out completely.
    - Use gmx_unique_ptr in containers that are responsible for freeing
      the contained objects.
    - Removed an unnecessary #include <memory>.
    
    Change-Id: I5f3cfbeb5c274426b0ffe9068a065fd42c2ed64b

---
+++ cmake/gmxTestCXX11.cmake
@@ -0,0 +1,31 @@
+include(CheckCXXSourceCompiles)
+MACRO(GMX_TEST_CXX11 VARIABLE FLAG)
+  MESSAGE(STATUS "Checking for C++11 support")
+  if(NOT WIN32)
+    set(CXX11_FLAG "-std=c++0x")
+  else()
+    set(CXX11_FLAG "/Qstd=c++0x")
+  endif()
+  CHECK_CXX_COMPILER_FLAG("${CXX11_FLAG}" CXXFLAG_STD_CXX0X)
+  if(NOT CXXFLAG_STD_CXX0X)
+    set(CXX11_FLAG "")
+  endif()
+  set(CMAKE_REQUIRED_DEFINITIONS "${CXX11_FLAG}")
+  check_cxx_source_compiles(
+"#include <vector>
+#include <memory>
+#include <algorithm>
+int main() {
+  typedef std::unique_ptr<int> intPointer;
+  intPointer p(new int(10));
+  std::vector<intPointer> v;
+  v.push_back(std::move(p));
+}" CXX11_SUPPORT_OK)
+  if(CXX11_SUPPORT_OK)
+    set(${VARIABLE} ${CXX11_SUPPORT_OK})
+    set(${FLAG} ${CXX11_FLAG})
+    MESSAGE(STATUS "Checking for C++11 support - yes")
+  else()
+    MESSAGE(STATUS "Checking for C++11 support - no")
+  endif()
+ENDMACRO()
