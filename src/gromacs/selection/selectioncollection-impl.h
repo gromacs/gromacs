79fbfb69cf7d3d986620a6c87cc102118a2f98fc
Roland Schulz <roland@utk.edu>
2012-03-02 19:16:07 -0500

    Use smart pointers for internal memory management.
    - Replaced uses of std::auto_ptr with boost::scoped_ptr where memory was
      allocated locally and release() was not used, or refactored the use of
      pointers out completely.
    - Use gmx_unique_ptr in containers that are responsible for freeing
      the contained objects.
    - Removed an unnecessary #include <memory>.
    
    Change-Id: I5f3cfbeb5c274426b0ffe9068a065fd42c2ed64b

--- src/gromacs/selection/selectioncollection-impl.h
+++ src/gromacs/selection/selectioncollection-impl.h
@@ -44,7 +44,9 @@
 #include <string>
 #include <vector>
 
-#include <typedefs.h>
+#include "../legacyheaders/typedefs.h"
+
+#include "gromacs/utility/uniqueptr.h"
 
 #include "../options/options.h"
 #include "../utility/flags.h"
@@ -54,6 +56,12 @@
 namespace gmx
 {
 class Selection;
+
+//! Smart pointer for managing a selection.
+// Could be unique_ptr.
+typedef gmx_unique_ptr<Selection>::type SelectionPointer;
+//! Shorthand for storing a list of selections internally.
+typedef std::vector<SelectionPointer> SelectionList;
 }
 
 /*! \internal \brief
@@ -66,7 +74,7 @@ struct gmx_ana_selcollection_t
     /** Root of the selection element tree. */
     struct t_selelem           *root;
     /** Array of compiled selections. */
-    std::vector<gmx::Selection *>  sel;
+    gmx::SelectionList             sel;
     /** Number of variables defined. */
     int                            nvars;
     /** Selection strings for variables. */
@@ -113,8 +121,6 @@ class SelectionCollection::Impl
             SelectionOptionStorage     *storage;
         };
 
-        //! Shorthand for a list of selections stored internally.
-        typedef std::vector<Selection *> SelectionList;
         //! Shorthand for a list of selection requests.
         typedef std::vector<SelectionRequest> RequestList;
 
