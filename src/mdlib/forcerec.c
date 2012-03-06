2dd32527450eedbf9867053a889f69966f9c5182
Jochen Hub <jhub@gwdg.de>
2012-03-06 21:35:25 +0100

    Added flat-bottom position restraints
    Change-Id: I6b61361c160df35d905b212fa0e7b45a992b8f35

--- src/mdlib/forcerec.c
+++ src/mdlib/forcerec.c
@@ -1551,9 +1551,9 @@ void init_forcerec(FILE *fp,
     
     fr->bF_NoVirSum = (EEL_FULL(fr->eeltype) ||
                        gmx_mtop_ftype_count(mtop,F_POSRES) > 0 ||
+                       gmx_mtop_ftype_count(mtop,F_FBPOSRES) > 0 ||
                        IR_ELEC_FIELD(*ir) ||
-                       (fr->adress_icor != eAdressICOff)
-                      );
+                       (fr->adress_icor != eAdressICOff));
     
     /* Mask that says whether or not this NBF list should be computed */
     /*  if (fr->bMask == NULL) {
