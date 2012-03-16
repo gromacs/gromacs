gromacs/gromacs

commit 553d4b1369ab133bb747354faa23db986d5d299f

Fix most errors found by cppcheck
While some are only style issues (e.g. include "" instead of <>),
it includes a couple of real bugs.
- Not correct handling of '\0' termination of string
- assignment in if statement
- using of variable before checking that it isn't null

Warnings from a couple of non-critical categories aren't fixed.

To run cppcheck (tested with latest 1.53):
cppcheck -j8 `find src -name '*.c' -o -name '*.cpp' -o -name '*.cu'` \
   -DHAVE_CONFIG_H -DSIZEOF_LONG_LONG_INT=8 -DSIZEOF_INT=4 -DLINUX -I include \
  --enable=style \
  --suppress=variableScope --suppress=unusedVariable --suppress=unreadVariable \
  --suppress=invalidscanf --suppress=unusedStructMember --suppress=sizeofCalculation \
  --suppress=unnecessaryForwardDeclaration --suppress=*:src/gmxlib/gmx_lapack/* \
  --suppress=*:src/contrib/*  --suppress=missingInclude \
  --template gcc  --inline-suppr

Change-Id: I1eabfce3e6cf8a6bed0f1282868667e290159279

src/mdlib/forcerec.c
@@ -1022,7 +1022,6 @@ static void make_nbf_tables(FILE *fp,const output_env_t oenv,
 {
   char buf[STRLEN];
   int i,j;
-  void *      p_tmp;
 
   if (tabfn == NULL) {
     if (debug)
@@ -1048,18 +1047,8 @@ static void make_nbf_tables(FILE *fp,const output_env_t oenv,
    * to do this...
    */
   
-  /* 8 fp entries per vdw table point, n+1 points, and 16 bytes extra to align it. */
-  p_tmp = malloc(8*(nbl->tab.n+1)*sizeof(real)+16);
-  
-  /* align it - size_t has the same same as a pointer */
-  nbl->vdwtab = (real *) (((size_t) p_tmp + 16) & (~((size_t) 15)));  
-
-  /* 4 fp entries per coul table point, n+1 points, and 16 bytes extra to align it. */
-  p_tmp = malloc(4*(nbl->tab.n+1)*sizeof(real)+16);
-  
-  /* align it - size_t has the same same as a pointer */
-  nbl->coultab = (real *) (((size_t) p_tmp + 16) & (~((size_t) 15)));  
-
+  snew_aligned(nbl->vdwtab,8*nbl->tab.n,16);
+  snew_aligned(nbl->coultab,4*nbl->tab.n,16);
   
   for(i=0; i<=nbl->tab.n; i++) {
     for(j=0; j<4; j++)

commit f7751e6d6817c2bf5c4c4c11fe03f539df899efb

fixed incorrect table allocation
This caused problems after commit 553d4b13.
The bug was already in older versions, but was hidden due to
the over allocation for pointer alignment.

Change-Id: Ifc50ee66fd4e963aab20ff1231c90a472fd5c443

src/mdlib/forcerec.c
@@ -1047,8 +1047,8 @@ static void make_nbf_tables(FILE *fp,const output_env_t oenv,
    * to do this...
    */
   
-  snew_aligned(nbl->vdwtab,8*nbl->tab.n,16);
-  snew_aligned(nbl->coultab,4*nbl->tab.n,16);
+  snew_aligned(nbl->vdwtab,8*(nbl->tab.n+1),16);
+  snew_aligned(nbl->coultab,4*(nbl->tab.n+1),16);
   
   for(i=0; i<=nbl->tab.n; i++) {
     for(j=0; j<4; j++)