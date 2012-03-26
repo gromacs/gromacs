2eac9a8dfbc10fb1663a660e1e8eee41b3856b1d
Christoph Junghans <junghans@votca.org>
2012-03-26 16:52:22 -0600

    Fixed rest of doxygen warnings
    Change-Id: I85ff8930a788d91b36339dd029c331db19b8e919

--- src/gromacs/trajectoryanalysis/nbsearch.h
+++ src/gromacs/trajectoryanalysis/nbsearch.h
@@ -28,14 +28,6 @@
  *
  * For more info, check our website at http://www.gromacs.org
  */
-/*! \file
- * \brief
- * C++ wrapper for analysis tool neighborhood searching.
- *
- * \author Teemu Murtola <teemu.murtola@cbr.su.se>
- * \inpublicapi
- * \ingroup module_trajectoryanalysis
- */
 #ifndef GMX_TRAJECTORYANALYSIS_NBSEARCH_H
 #define GMX_TRAJECTORYANALYSIS_NBSEARCH_H
 
@@ -44,11 +36,6 @@
 namespace gmx
 {
 
-/*! \brief
- * C++ wrapper for neighborhood searching in selection/nbsearch.h.
- *
- * \ingroup module_trajectoryanalysis
- */
 class NeighborhoodSearch
 {
     public:
