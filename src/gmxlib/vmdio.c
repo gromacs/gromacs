b3283703391bf01f22c87bf2d7a23b334d5e12dc
Mark Abraham <mark.j.abraham@gmail.com>
2012-04-11 12:26:22 +1000

    Added Windows-specific #defines, and use them
    The lack of an elegant way to code "do this on Windows/
    Cygwin/whatever" may have contributed to 4.5.5 not compiling with
    threading under Cygwin (IssueID #833, which was fixed elsewhere).
    
    Change-Id: I07fa69825181e2b04cb89798898898f1d258d371

--- src/gmxlib/vmdio.c
+++ src/gmxlib/vmdio.c
@@ -18,6 +18,7 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+#include "gmxconfig.h"
 
 
 
@@ -78,7 +79,7 @@ OTHER DEALINGS WITH THE SOFTWARE.
  */
 #include "molfile_plugin.h"
 #include "vmddlopen.h"
-#if !((defined WIN32 || defined _WIN32 || defined WIN64 || defined _WIN64) && !defined __CYGWIN__ && !defined __CYGWIN32__)
+#ifndef GMX_NATIVE_WINDOWS
 #include <glob.h>
 #else
 #include <windows.h>
@@ -237,7 +238,7 @@ int load_vmd_library(const char *fn, t_gmxvmdplugin *vmdplugin)
     int i;
     int ret=0;
     char pathenv_buffer[GMX_PATH_MAX];
-#if !((defined WIN32 || defined _WIN32 || defined WIN64 || defined _WIN64) && !defined __CYGWIN__ && !defined __CYGWIN32__)
+#ifndef GMX_NATIVE_WINDOWS
     glob_t globbuf;
     const char *defpath_suffix = "/plugins/*/molfile";
     const char *defpathenv = GMX_VMD_PLUGIN_PATH;
@@ -286,7 +287,7 @@ int load_vmd_library(const char *fn, t_gmxvmdplugin *vmdplugin)
         }
     }
     strncpy(pathname,pathenv,sizeof(pathname));
-#if !((defined WIN32 || defined _WIN32 || defined WIN64 || defined _WIN64) && !defined __CYGWIN__ && !defined __CYGWIN32__)
+#ifndef GMX_NATIVE_WINDOWS
     strcat(pathname,"/*.so");
     glob(pathname, 0, NULL, &globbuf);
     if (globbuf.gl_pathc == 0)
