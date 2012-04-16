e9f63fcbf35ef7c32343d1035b7ac087642d8f0f
Teemu Murtola <teemu.murtola@gmail.com>
2012-04-16 08:27:22 +0300

    Add documentation for template.cpp.
    - Add more description of template.cpp to its Doxygen comments,
      referencing other parts of the Doxygen documentation where
      appropriate.
    - Adjust the naming convention in template.cpp to use a trailing
      underscore instead of a leading one (the convention elsewhere in the
      code is currently a bit mixed, but in recent code I've used the
      trailing underscore).
    
    Resolves #641.
    
    Change-Id: Ie653550f3a3ace13482ba9a08f35344dbe188ada

--- share/template/template.cpp
+++ share/template/template.cpp
@@ -59,13 +59,15 @@ class AnalysisTemplate : public TrajectoryAnalysisModule
     private:
         class ModuleData;
 
-        Options                          _options;
-        std::string                      _fnDist;
-        double                           _cutoff;
-        Selection                        _refsel;
-        SelectionList                    _sel;
-        AnalysisData                     _data;
-        AnalysisDataAverageModulePointer _avem;
+        Options                          options_;
+
+        std::string                      fnDist_;
+        double                           cutoff_;
+        Selection                        refsel_;
+        SelectionList                    sel_;
+
+        AnalysisData                     data_;
+        AnalysisDataAverageModulePointer avem_;
 };
 
 /*! \brief
@@ -89,7 +91,7 @@ class AnalysisTemplate::ModuleData : public TrajectoryAnalysisModuleData
                    const SelectionCollection &selections,
                    double cutoff, int posCount)
             : TrajectoryAnalysisModuleData(module, opt, selections),
-              _nb(cutoff, posCount)
+              nb_(cutoff, posCount)
         {
         }
 
@@ -99,13 +101,12 @@ class AnalysisTemplate::ModuleData : public TrajectoryAnalysisModuleData
         }
 
         //! Neighborhood search data for distance calculation.
-        NeighborhoodSearch      _nb;
+        NeighborhoodSearch      nb_;
 };
 
 
 AnalysisTemplate::AnalysisTemplate()
-    : _options("template", "Template options"), _cutoff(0.0),
-      _avem(new AnalysisDataAverageModule())
+    : options_("template", "Template options"), cutoff_(0.0)
 {
 }
 
@@ -130,26 +131,26 @@ AnalysisTemplate::initOptions(TrajectoryAnalysisSettings *settings)
         NULL
     };
 
-    _options.setDescription(desc);
+    options_.setDescription(desc);
 
-    _options.addOption(FileNameOption("o")
+    options_.addOption(FileNameOption("o")
         .filetype(eftPlot).writeOnly()
-        .store(&_fnDist).defaultValueIfSet("avedist")
+        .store(&fnDist_).defaultValueIfSet("avedist")
         .description("Average distances from reference group"));
 
-    _options.addOption(SelectionOption("reference")
-        .store(&_refsel).required()
+    options_.addOption(SelectionOption("reference")
+        .store(&refsel_).required()
         .description("Reference group to calculate distances from"));
-    _options.addOption(SelectionOption("select")
-        .storeVector(&_sel).required().multiValue()
+    options_.addOption(SelectionOption("select")
+        .storeVector(&sel_).required().multiValue()
         .description("Groups to calculate distances to"));
 
-    _options.addOption(DoubleOption("cutoff").store(&_cutoff)
+    options_.addOption(DoubleOption("cutoff").store(&cutoff_)
         .description("Cutoff for distance calculation (0 = no cutoff)"));
 
     settings->setFlag(TrajectoryAnalysisSettings::efRequireTop);
 
-    return _options;
+    return options_;
 }
 
 
@@ -157,20 +158,21 @@ void
 AnalysisTemplate::initAnalysis(const TrajectoryAnalysisSettings &settings,
                                const TopologyInformation & /*top*/)
 {
-    _data.setColumnCount(_sel.size());
-    registerAnalysisDataset(&_data, "avedist");
+    data_.setColumnCount(sel_.size());
+    registerAnalysisDataset(&data_, "avedist");
 
-    _data.addModule(_avem);
+    avem_.reset(new AnalysisDataAverageModule());
+    data_.addModule(avem_);
 
-    if (!_fnDist.empty())
+    if (!fnDist_.empty())
     {
         AnalysisDataPlotModulePointer plotm(
             new AnalysisDataPlotModule(settings.plotSettings()));
-        plotm->setFileName(_fnDist);
+        plotm->setFileName(fnDist_);
         plotm->setTitle("Average distance");
         plotm->setXAxisIsTime();
         plotm->setYLabel("Distance (nm)");
-        _data.addModule(plotm);
+        data_.addModule(plotm);
     }
 }
 
@@ -180,7 +182,7 @@ AnalysisTemplate::startFrames(const AnalysisDataParallelOptions &opt,
                               const SelectionCollection &selections)
 {
     return TrajectoryAnalysisModuleDataPointer(
-            new ModuleData(this, opt, selections, _cutoff, _refsel.posCount()));
+            new ModuleData(this, opt, selections, cutoff_, refsel_.posCount()));
 }
 
 
@@ -188,15 +190,15 @@ void
 AnalysisTemplate::analyzeFrame(int frnr, const t_trxframe &fr, t_pbc *pbc,
                                TrajectoryAnalysisModuleData *pdata)
 {
-    AnalysisDataHandle  dh = pdata->dataHandle(_data);
-    NeighborhoodSearch &nb = static_cast<ModuleData *>(pdata)->_nb;
-    const Selection    &refsel = pdata->parallelSelection(_refsel);
+    AnalysisDataHandle  dh = pdata->dataHandle(data_);
+    NeighborhoodSearch &nb = static_cast<ModuleData *>(pdata)->nb_;
+    const Selection    &refsel = pdata->parallelSelection(refsel_);
 
     nb.init(pbc, refsel.positions());
     dh.startFrame(frnr, fr.time);
-    for (size_t g = 0; g < _sel.size(); ++g)
+    for (size_t g = 0; g < sel_.size(); ++g)
     {
-        const Selection &sel = pdata->parallelSelection(_sel[g]);
+        const Selection &sel = pdata->parallelSelection(sel_[g]);
         int   nr = sel.posCount();
         real  frave = 0.0;
         for (int i = 0; i < nr; ++i)
@@ -221,10 +223,10 @@ void
 AnalysisTemplate::writeOutput()
 {
     // We print out the average of the mean distances for each group.
-    for (size_t g = 0; g < _sel.size(); ++g)
+    for (size_t g = 0; g < sel_.size(); ++g)
     {
         fprintf(stderr, "Average mean distance for '%s': %.3f nm\n",
-                _sel[g].name(), _avem->average(g));
+                sel_[g].name(), avem_->average(g));
     }
 }
 
