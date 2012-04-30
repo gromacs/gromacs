7b9b714cb60e7ab34983e6244f919832d8c4b981
Roland Schulz <roland@utk.edu>
2012-04-29 22:20:20 -0400

    Fix gmxdump error introduced by 553d4b1369
    Change-Id: Ib3ebe7231e3be0f2bdeda21cf5c589f6d5144c5c

--- src/kernel/gmxdump.c
+++ src/kernel/gmxdump.c
@@ -39,6 +39,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <assert.h>
 #include "main.h"
 #include "macros.h"
 #include "futil.h"
@@ -290,7 +291,7 @@ void list_ene(const char *fn)
     int        ndr;
     ener_file_t in;
     gmx_bool       bCont;
-    gmx_enxnm_t *enm;
+    gmx_enxnm_t *enm=NULL;
     t_enxframe *fr;
     int        i,j,nre,b;
     real       rav,minthird;
@@ -299,6 +300,7 @@ void list_ene(const char *fn)
     printf("gmxdump: %s\n",fn);
     in = open_enx(fn,"r");
     do_enxnms(in,&nre,&enm);
+    assert(enm);
 
     printf("energy components:\n");
     for(i=0; (i<nre); i++) 
