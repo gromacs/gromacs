gromacs/gromacs

commit 553d4b1369ab133bb747354faa23db986d5d299f

Fix most errors found by cppcheck
While some are only style issues (e.g. include "" instead of <>),
it includes a couple of real bugs.
- Not correct handling of '\0' termination of string
- assignment in if statement
- using of variable before checking that it isn't null

Warnings from a couple of non-critical categories aren't fixed.

To run cppcheck (tested with latest 1.53):
cppcheck -j8 `find src -name '*.c' -o -name '*.cpp' -o -name '*.cu'` \
   -DHAVE_CONFIG_H -DSIZEOF_LONG_LONG_INT=8 -DSIZEOF_INT=4 -DLINUX -I include \
  --enable=style \
  --suppress=variableScope --suppress=unusedVariable --suppress=unreadVariable \
  --suppress=invalidscanf --suppress=unusedStructMember --suppress=sizeofCalculation \
  --suppress=unnecessaryForwardDeclaration --suppress=*:src/gmxlib/gmx_lapack/* \
  --suppress=*:src/contrib/*  --suppress=missingInclude \
  --template gcc  --inline-suppr

Change-Id: I1eabfce3e6cf8a6bed0f1282868667e290159279

src/kernel/gmxdump.c
@@ -320,7 +320,7 @@ void list_ene(const char *fn)
     int        ndr;
     ener_file_t in;
     gmx_bool       bCont;
-    gmx_enxnm_t *enm=NULL;
+    gmx_enxnm_t *enm;
     t_enxframe *fr;
     int        i,j,nre,b;
     real       rav,minthird;

commit fa9eacaba38c4d60fc69e8925b74b330140c735d

Fix gmxdump error introduced by 553d4b1369
Change-Id: Ib3ebe7231e3be0f2bdeda21cf5c589f6d5144c5c

src/kernel/gmxdump.c
@@ -290,7 +290,7 @@ void list_ene(const char *fn)
     int        ndr;
     ener_file_t in;
     gmx_bool       bCont;
-    gmx_enxnm_t *enm;
+    gmx_enxnm_t *enm=NULL;
     t_enxframe *fr;
     int        i,j,nre,b;
     real       rav,minthird;