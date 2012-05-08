569003873cf15b892538bcec4802d25a1daf415f
Roland Schulz <roland@utk.edu>
2012-05-07 22:19:12 -0400

    Fixing more Clang warnings
    Change-Id: Ic2189579eaaa262d4907d912d59a3e78274bd4ed

--- src/gmxlib/futil.c
+++ src/gmxlib/futil.c
@@ -436,6 +436,11 @@ gmx_bool make_backup(const char * name)
     int  count_max;
     char * backup;
 
+    if (name == NULL) 
+    {
+        return FALSE;
+    }
+
 #ifdef GMX_FAHCORE
     return FALSE; /* skip making backups */
 #else
