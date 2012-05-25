6afee3a60b6a34d97cc8c9e7f061b2abce4f8bc5
Martin Hoefling <martin.hoefling@gmx.de>
2012-05-25 12:09:07 +0200

    fixed [dd] and [TT] typos in documentation of mdrun and gmx_sans
    Change-Id: Ia51f67b2b056f66072875d2c7a366ae4685bf9d4

--- src/kernel/mdrun.c
+++ src/kernel/mdrun.c
@@ -289,7 +289,7 @@ int main(int argc,char *argv[])
     "appropriate options have been given. Currently under",
     "investigation are: polarizability and X-ray bombardments.",
     "[PAR]",
-    "The option [TT]-membed[dd] does what used to be g_membed, i.e. embed",
+    "The option [TT]-membed[tt] does what used to be g_membed, i.e. embed",
     "a protein into a membrane. The data file should contain the options",
     "that where passed to g_membed before. The [TT]-mn[tt] and [TT]-mp[tt]",
     "both apply to this as well.",
