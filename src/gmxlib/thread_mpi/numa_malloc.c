acf142cca7794ef2ac605a714a719ece41539c51
Mark Abraham <mark.j.abraham@gmail.com>
2012-05-25 19:45:09 +1000

    Continuing to fix threading on Cygwin
    Fixes #833
    
    Change-Id: I5d577a149ad18b295559f6d87c24dc42bffd281e

--- src/gmxlib/thread_mpi/numa_malloc.c
+++ src/gmxlib/thread_mpi/numa_malloc.c
@@ -23,7 +23,7 @@
 #endif
 
 
-#if ! (defined(WIN32) || defined( _WIN32 ) || defined(WIN64) || defined( _WIN64 ) || defined (__CYGWIN__))
+#if !((defined(WIN32) || defined( _WIN32 ) || defined(WIN64) || defined( _WIN64 )) && !defined (__CYGWIN__) && !defined (__CYGWIN32__))
 
 
 /* We don't have specific NUMA aware allocators: */
