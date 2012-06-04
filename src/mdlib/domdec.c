gromacs/gromacs

commit 367b11222b427a3f15f83b175824f39503cf67d1

changed the force output of the gpu kernels to float3
Also cleaned up some of the cluster size related code in nbnxn_search.c

src/mdlib/forcerec.c
@@ -2069,7 +2069,6 @@ static void init_nb_verlet(FILE *fp,
                       DOMAINDECOMP(cr) ? & cr->dd->nc : NULL,
                       DOMAINDECOMP(cr) ? domdec_zones(cr->dd) : NULL,
                       nbv->grp[0].kernel_type,
-                      NBNXN_GPU_CLUSTER_SIZE,
                       gmx_omp_nthreads_get(emntNonbonded));
 
     for(i=0; i<nbv->nloc; i++)

src/mdlib/nbnxn_search.c
@@ -165,7 +165,7 @@
 /* This define is a lazy way to avoid interdependence of the grid
  * and searching data structures.
  */
-#define NBNXN_NA_SC_MAX (NSUBCELL*16)
+#define NBNXN_NA_SC_MAX (NSUBCELL*8)
 
 #ifdef NBNXN_SEARCH_SSE
 #define GMX_SSE_HERE
@@ -400,16 +400,6 @@ static void nbs_cycle_print(FILE *fp,const nbnxn_search_t nbs)
     fprintf(fp,"\n");
 }
 
-static gmx_bool kernel_cj_half_ci(int nb_kernel_type)
-{
-#ifndef GMX_DOUBLE
-    return FALSE;
-#else
-    /* With double precision SSE is only 2-wide and we use 4x2 atom kernels */
-    return (nb_kernel_type == nbk4xN_S128);
-#endif
-}
-
 static void nbnxn_grid_init(nbnxn_grid_t * grid,
                             gmx_bool cj_half_ci)
 {
@@ -437,6 +427,60 @@ static int get_2log(int n)
     }
 
     return log2;
+}
+static int kernel_to_ci_size(nb_kernel_type)
+{
+    switch (nb_kernel_type)
+    {
+    case nbk4x4_PlainC:
+    case nbk4xN_S128:
+    case nbk4xN_S256:
+        return NBNXN_CPU_CLUSTER_I_SIZE;
+    case nbk8x8x8_CUDA:
+    case nbk8x8x8_PlainC:
+        /* The cluster size for super/sub lists is only set here.
+         * Any value should work for the pair-search and atomdata code.
+         * The kernels, of course, might require a particular value.
+         */
+        return NBNXN_GPU_CLUSTER_SIZE;
+    default:
+        gmx_incons("unknown kernel type");
+    }
+
+    return 0;
+}
+
+static int kernel_to_cj_size(nb_kernel_type)
+{
+    switch (nb_kernel_type)
+    {
+    case nbk4x4_PlainC:
+        return 4;
+#ifndef GMX_DOUBLE
+    case nbk4xN_S128:
+        return 4;
+    case nbk4xN_S256:
+        return 8;
+#else
+    case nbk4xN_S128:
+        return 2;
+    case nbk4xN_S256:
+        return 4;
+#endif
+    case nbk8x8x8_CUDA:
+    case nbk8x8x8_PlainC:
+        return kernel_to_ci_size(nb_kernel_type);
+    default:
+        gmx_incons("unknown kernel type");
+    }
+
+    return 0;
+}
+
+static gmx_bool kernel_cj_half_ci(int nb_kernel_type)
+{
+    return (kernel_to_cj_size(nb_kernel_type)*2 ==
+            kernel_to_ci_size(nb_kernel_type));
 }
 
 static int ci_to_cj(int na_cj_2log,int ci)
@@ -479,7 +523,6 @@ void nbnxn_init_search(nbnxn_search_t * nbs_ptr,
                        ivec *n_dd_cells,
                        gmx_domdec_zones_t *zones,
                        int nb_kernel_type_loc,
-                       int natoms_cluster,
                        int nthread_max)
 {
     nbnxn_search_t nbs;
@@ -513,7 +556,7 @@ void nbnxn_init_search(nbnxn_search_t * nbs_ptr,
 
     if (!nbnxn_kernel_pairlist_simple(nb_kernel_type_loc))
     {
-        nbs->na_c = natoms_cluster;
+        nbs->na_c = kernel_to_ci_size(nb_kernel_type_loc);
 
         if (nbs->na_c*NSUBCELL > NBNXN_NA_SC_MAX)
         {
@@ -573,26 +616,6 @@ void nbnxn_init_search(nbnxn_search_t * nbs_ptr,
     }
 }
 
-static int simple_XFormat_cj_size(int XFormat,gmx_bool cj_half_ci)
-{
-    int cj_size;
-
-    if (XFormat != nbatX8)
-    {
-        cj_size = 4;
-    }
-    else
-    {
-        cj_size = 8;
-    }
-    if (cj_half_ci)
-    {
-        cj_size /= 2;
-    }
-
-    return cj_size;
-}
-
 static real grid_atom_density(int n,rvec corner0,rvec corner1)
 {
     rvec size;
@@ -614,9 +637,10 @@ static int set_grid_size_xy(const nbnxn_search_t nbs,
     real adens,tlen,tlen_x,tlen_y,nc_max;
     int  t;
 
-    if (grid->simple)
+    cj_size = grid->na_c;
+    if (grid->cj_half_ci)
     {
-        cj_size = simple_XFormat_cj_size(XFormat,grid->cj_half_ci);
+        cj_size /= 2;
     }
 
     rvec_sub(corner1,corner0,size);
@@ -1183,11 +1207,11 @@ static void copy_int_to_nbat_int(const int *a,int na,int na_round,
     }
 }
 
-static void clear_nbat_real(int na,int nbatXFormat,real *xnb,int a0)
+static void clear_nbat_real(int na,int nbatFormat,real *xnb,int a0)
 {
     int a,d,j,c;
 
-    switch (nbatXFormat)
+    switch (nbatFormat)
     {
     case nbatXYZ:
         for(a=0; a<na; a++)
@@ -1245,7 +1269,7 @@ static void clear_nbat_real(int na,int nbatXFormat,real *xnb,int a0)
 }
 
 static void copy_rvec_to_nbat_real(const int *a,int na,int na_round,
-                                   rvec *x,int nbatXFormat,real *xnb,int a0,
+                                   rvec *x,int nbatFormat,real *xnb,int a0,
                                    int cx,int cy,int cz)
 {
     int i,j,c;
@@ -1258,7 +1282,7 @@ static void copy_rvec_to_nbat_real(const int *a,int na,int na_round,
  */
 #define NBAT_FAR_AWAY 107
 
-    switch (nbatXFormat)
+    switch (nbatFormat)
     {
     case nbatXYZ:
         j = a0*3;
@@ -2056,9 +2080,10 @@ static void nbnxn_atomdata_realloc(nbnxn_atomdata_t *nbat,int n)
                     nbat->alloc,nbat->free);
     for(t=0; t<nbat->nout; t++)
     {
+        /* Allocate one element extra for possible signaling with CUDA */
         nb_realloc_void((void **)&nbat->out[t].f,
-                        nbat->natoms*nbat->xstride*sizeof(*nbat->out[t].f),
-                        n*nbat->xstride*sizeof(*nbat->out[t].f),
+                        nbat->natoms*nbat->fstride*sizeof(*nbat->out[t].f),
+                        (n+1)*nbat->fstride*sizeof(*nbat->out[t].f),
                         nbat->alloc,nbat->free);
     }
     nbat->nalloc = n;
@@ -4499,15 +4524,7 @@ static void nbnxn_make_pairlist_part(const nbnxn_search_t nbs,
 
     nbl->na_sc = gridj->na_sc;
     nbl->na_ci = gridj->na_c;
-    if (nbl->simple)
-    {
-        nbl->na_cj = simple_XFormat_cj_size(nbat->XFormat,gridj->cj_half_ci);
-    }
-    else
-    {
-        /* Not used, only set for completeness */
-        nbl->na_cj = 8;
-    }
+    nbl->na_cj = kernel_to_cj_size(nb_kernel_type);
     na_cj_2log = get_2log(nbl->na_cj);
 
     nbl->rlist  = rlist;
@@ -5231,7 +5248,6 @@ void nbnxn_make_pairlist(const nbnxn_search_t nbs,
 
 /* Initializes an nbnxn_atomdata_output_t data structure */
 static void nbnxn_atomdata_output_init(nbnxn_atomdata_output_t *out,
-                                       int XFormat,
                                        int nb_kernel_type,
                                        int nenergrp,int stride,
                                        gmx_nbat_alloc_t *ma)
@@ -5247,8 +5263,7 @@ static void nbnxn_atomdata_output_init(nbnxn_atomdata_output_t *out,
     if (nb_kernel_type == nbk4xN_S128 ||
         nb_kernel_type == nbk4xN_S256)
     {
-        cj_size = simple_XFormat_cj_size(XFormat,
-                                         kernel_cj_half_ci(nb_kernel_type));
+        cj_size = kernel_to_cj_size(nb_kernel_type);
         out->nVS = nenergrp*nenergrp*stride*(cj_size>>1)*cj_size;
         ma((void **)&out->VSvdw,out->nVS*sizeof(*out->VSvdw));
         ma((void **)&out->VSc  ,out->nVS*sizeof(*out->VSc  ));
@@ -5505,10 +5520,13 @@ void nbnxn_atomdata_init(FILE *fp,
             nbat->XFormat = nbatXYZ;
             break;
         }
+
+        nbat->FFormat = nbat->XFormat;
     }
     else
     {
         nbat->XFormat = nbatXYZQ;
+        nbat->FFormat = nbatXYZ;
     }
     nbat->q       = NULL;
     nbat->nenergrp = n_energygroups;
@@ -5530,6 +5548,7 @@ void nbnxn_atomdata_init(FILE *fp,
     nbat->energrp = NULL;
     nbat->alloc((void **)&nbat->shift_vec,SHIFTS*sizeof(*nbat->shift_vec));
     nbat->xstride = (nbat->XFormat == nbatXYZQ ? 4 : 3);
+    nbat->fstride = (nbat->FFormat == nbatXYZQ ? 4 : 3);
     nbat->x       = NULL;
     nbat->nout    = nout;
     snew(nbat->out,nbat->nout);
@@ -5537,7 +5556,7 @@ void nbnxn_atomdata_init(FILE *fp,
     for(i=0; i<nbat->nout; i++)
     {
         nbnxn_atomdata_output_init(&nbat->out[i],
-                                   nbat->XFormat,nb_kernel_type,
+                                   nb_kernel_type,
                                    nbat->nenergrp,1<<nbat->neg_2log,
                                    nbat->alloc);
     }
@@ -5869,7 +5888,7 @@ nbnxn_atomdata_add_nbat_f_to_f_part(const nbnxn_search_t nbs,
     cell = nbs->cell;
 
     /* Loop over all columns and copy and fill */
-    switch (nbat->XFormat)
+    switch (nbat->FFormat)
     {
     case nbatXYZ:
     case nbatXYZQ:
@@ -5879,7 +5898,7 @@ nbnxn_atomdata_add_nbat_f_to_f_part(const nbnxn_search_t nbs,
 
             for(a=a0; a<a1; a++)
             {
-                i = cell[a]*nbat->xstride;
+                i = cell[a]*nbat->fstride;
                 
                 f[a][XX] += fnb[i];
                 f[a][YY] += fnb[i+1];
@@ -5890,7 +5909,7 @@ nbnxn_atomdata_add_nbat_f_to_f_part(const nbnxn_search_t nbs,
         {
             for(a=a0; a<a1; a++)
             {
-                i = cell[a]*nbat->xstride;
+                i = cell[a]*nbat->fstride;
                 
                 for(fa=0; fa<nfa; fa++)
                 {

src/mdlib/nbnxn_search.h
@@ -52,7 +52,6 @@ void nbnxn_init_search(nbnxn_search_t * nbs_ptr,
                        ivec *n_dd_cells,
                        gmx_domdec_zones_t *zones,
 		       int nb_kernel_type_loc,
-                       int natoms_cluster,
                        int nthread_max);
 
 /* Put the atoms on the pair search grid.

include/types/nbnxn_pairlist.h
@@ -199,6 +199,7 @@ typedef struct {
     int  *type;      /* Atom types                                         */
     real *lj_comb;   /* LJ parameters per atom for combining for pairs     */
     int  XFormat;    /* The format of x (and q), enum                      */
+    int  FFormat;    /* The format of f, enum                              */
     real *q;         /* Charges, can be NULL if incorporated in x          */
     int  na_c;       /* The number of atoms per cluster                    */
     int  nenergrp;   /* The number of energy groups                        */
@@ -207,6 +208,7 @@ typedef struct {
     gmx_bool dynamic_box; /* Do we need to update shift_vec every step?    */
     rvec *shift_vec; /* Shift vectors, copied from t_forcerec              */
     int  xstride;    /* stride for a coordinate in x (usually 3 or 4)      */
+    int  fstride;    /* stride for a coordinate in f (usually 3 or 4)      */
     real *x;         /* x and possibly q, size natoms*xstride              */
     int  nout;       /* The number of force arrays                         */
     nbnxn_atomdata_output_t *out;  /* Output data structures               */

src/gmxlib/cuda_tools/vectype_ops.cuh
@@ -97,6 +97,12 @@ inline __host__ __device__ void operator*=(float3 &a, float3 b)
 {
     a.x *= b.x; a.y *= b.y; a.z *= b.z;
 }
+inline __device__ void atomicAdd(float3 *addr, float3 val)
+{
+    atomicAdd(&addr->x, val.x);
+    atomicAdd(&addr->y, val.y);
+    atomicAdd(&addr->z, val.z);
+}
 /****************************************************************/
 
 /**** float4 ****/
@@ -141,18 +147,6 @@ inline __host__ __device__ float norm(float4 a)
 inline __host__ __device__ float dist3(float4 a, float4 b)
 {
     return norm(b - a);
-}
-inline __device__ void atomicAdd3(float4 *addr, float4 val)
-{
-    atomicAdd(&addr->x, val.x);
-    atomicAdd(&addr->y, val.y);
-    atomicAdd(&addr->z, val.z);
-}
-inline __device__ void atomicAdd(float4 *addr, float3 val)
-{
-    atomicAdd(&addr->x, val.x);
-    atomicAdd(&addr->y, val.y);
-    atomicAdd(&addr->z, val.z);
 }
 
 #endif /* VECTYPE_OPS_CUH */

src/mdlib/nbnxn_cuda/nbnxn_cuda.cu
@@ -340,9 +340,9 @@ void nbnxn_cuda_launch_cpyback(nbnxn_cuda_ptr_t cu_nb,
                                int aloc)
 {
     cudaError_t stat;
-    int adat_begin, adat_len, adat_last;  /* local/nonlocal offset and length used for xq and f */
+    int adat_begin, adat_len, adat_end;  /* local/nonlocal offset and length used for xq and f */
     int iloc = -1;
-    float4 *signal_field;
+    float3 *signal_field;
 
     /* determine interaction locality from atom locality */
     if (LOCAL_A(aloc))
@@ -380,13 +380,13 @@ void nbnxn_cuda_launch_cpyback(nbnxn_cuda_ptr_t cu_nb,
     {
         adat_begin  = 0;
         adat_len    = adat->natoms_local;
-        adat_last   = cu_nb->atdat->natoms_local - 1;
+        adat_end    = cu_nb->atdat->natoms_local;
     }
     else
     {
         adat_begin  = adat->natoms_local;
         adat_len    = adat->natoms - adat->natoms_local;
-        adat_last   = cu_nb->atdat->natoms - 1;
+        adat_end    = cu_nb->atdat->natoms;
     }
 
     /* beginning of timed D2H section */
@@ -398,16 +398,16 @@ void nbnxn_cuda_launch_cpyback(nbnxn_cuda_ptr_t cu_nb,
 
     if (!cu_nb->use_stream_sync)
     {
-        /* Set the 4th unsused component of the last element in the GPU force array
+        /* Set the z component of the extra element in the GPU force array
            (separately for l/nl parts) to a specific bit-pattern that we can check 
            for while waiting for the transfer to be done. */
-        signal_field = adat->f + adat_last;
-        stat = cudaMemsetAsync(&signal_field->w, 0xAA, sizeof(signal_field->z), stream);
+        signal_field = adat->f + adat_end;
+        stat = cudaMemsetAsync(&signal_field->z, 0xAA, sizeof(signal_field->z), stream);
         CU_RET_ERR(stat, "cudaMemsetAsync of the signal_field failed");
 
         /* For safety reasons set a few (5%) forces to NaN. This way even if the
            polling "hack" fails with some future NVIDIA driver we'll get a crash. */
-        for (int i = adat_begin; i < 4*adat_last + 2; i += (adat_last - adat_begin)/20)
+        for (int i = adat_begin; i < 3*adat_end + 2; i += adat_len/20)
         {
 #ifdef NAN
             nbatom->out[0].f[i] = NAN;
@@ -426,7 +426,7 @@ void nbnxn_cuda_launch_cpyback(nbnxn_cuda_ptr_t cu_nb,
         }
 
         /* Clear the bits in the force array (on the CPU) that we are going to poll. */
-        nbatom->out[0].f[adat_last*4 + 3] = 0;
+        nbatom->out[0].f[adat_end*3 + 2] = 0;
     }
 
     /* With DD the local D2H transfer can only start after the non-local 
@@ -438,8 +438,8 @@ void nbnxn_cuda_launch_cpyback(nbnxn_cuda_ptr_t cu_nb,
     }
 
     /* DtoH f */
-    cu_copy_D2H_async(nbatom->out[0].f + adat_begin * 4, adat->f + adat_begin, 
-                      adat_len * sizeof(*adat->f), stream);
+    cu_copy_D2H_async(nbatom->out[0].f + adat_begin * 3, adat->f + adat_begin, 
+                      (adat_len + 1) * sizeof(*adat->f), stream);
 
     /* After the non-local D2H is launched the nonlocal_done event can be
        recorded which signals that the local D2H can proceed. This event is not
@@ -484,7 +484,7 @@ void nbnxn_cuda_wait_gpu(nbnxn_cuda_ptr_t cu_nb,
                          float *e_lj, float *e_el, rvec *fshift)
 {
     cudaError_t stat;
-    int i, adat_last, iloc = -1;
+    int i, adat_end, iloc = -1;
     unsigned int *signal_bytes;
     unsigned int signal_pattern;
 
@@ -532,11 +532,11 @@ void nbnxn_cuda_wait_gpu(nbnxn_cuda_ptr_t cu_nb,
     /* calculate the atom data index range based on locality */
     if (LOCAL_A(aloc))
     {
-        adat_last = cu_nb->atdat->natoms_local - 1;
+        adat_end = cu_nb->atdat->natoms_local;
     }
     else
     {
-        adat_last = cu_nb->atdat->natoms - 1;
+        adat_end = cu_nb->atdat->natoms;
     }
 
     if (cu_nb->use_stream_sync)
@@ -548,7 +548,7 @@ void nbnxn_cuda_wait_gpu(nbnxn_cuda_ptr_t cu_nb,
     {
         /* Busy-wait until we get the signalling pattern set in last 4-bytes 
            of the l/nl float vector. */
-        signal_bytes    = (unsigned int*)&nbatom->out[0].f[adat_last*4 + 3];
+        signal_bytes    = (unsigned int*)&nbatom->out[0].f[adat_end*3 + 2];
         signal_pattern  = 0xAAAAAAAA; /* FIXME move this to a module-global constant */
         while (*signal_bytes != signal_pattern) 
         {

src/mdlib/nbnxn_cuda/nbnxn_cuda_data_mgmt.cu
@@ -532,7 +532,7 @@ static void nbnxn_cuda_clear_e_fshift(nbnxn_cuda_ptr_t cu_nb)
 
 void nbnxn_cuda_clear_outputs(nbnxn_cuda_ptr_t cu_nb, int flags)
 {
-    nbnxn_cuda_clear_f(cu_nb, cu_nb->atdat->natoms);
+    nbnxn_cuda_clear_f(cu_nb, cu_nb->atdat->natoms + 1);
     /* clear shift force array and energies if the outputs were 
        used in the current step */
     if (flags & GMX_FORCE_VIRIAL)
@@ -565,9 +565,10 @@ void nbnxn_cuda_init_atomdata(nbnxn_cuda_ptr_t cu_nb,
 
     /* need to reallocate if we have to copy more atoms than the amount of space
        available and only allocate if we haven't initilzed yet, i.e d_atdat->natoms == -1 */
-    if (natoms > d_atdat->nalloc)
+    /* Add one element to f for possible signalling */
+    if (natoms + 1 > d_atdat->nalloc)
     {
-        nalloc = natoms * 1.2 + 100;
+        nalloc = (natoms + 1) * 1.2 + 100;
     
         /* free up first if the arrays have already been initialized */
         if (d_atdat->nalloc != -1)
@@ -577,6 +578,7 @@ void nbnxn_cuda_init_atomdata(nbnxn_cuda_ptr_t cu_nb,
             cu_free_buffered(d_atdat->atom_types);
         }
         
+        /* Add one element for possible signalling */
         stat = cudaMalloc((void **)&d_atdat->f, nalloc*sizeof(*d_atdat->f));
         CU_RET_ERR(stat, "cudaMalloc failed on d_atdat->f");
         stat = cudaMalloc((void **)&d_atdat->xq, nalloc*sizeof(*d_atdat->xq));

src/mdlib/nbnxn_cuda/nbnxn_cuda_kernel_1.cuh
@@ -69,7 +69,7 @@ __global__ void FUNCTION_NAME(k_nbnxn, 1)
     const int *atom_types       = atdat.atom_types;
     int ntypes                  = atdat.ntypes;
     const float4 *xq            = atdat.xq;
-    float4 *f                   = atdat.f;
+    float3 *f                   = atdat.f;
     const float3 *shift_vec     = atdat.shift_vec;
     float rcoulomb_sq           = nbparam.rcoulomb_sq;
 #ifdef EL_EWALD

commit 6c5ea014d0ddb5d5ce86ad5cdfd10158d8d9be18

fixed nbnxn AVX 4x8 pair-search, broken by 367b1122

src/mdlib/domdec.c
@@ -9239,7 +9239,7 @@ void dd_partition_system(FILE            *fplog,
                               fr->cginfo,
                               state_local->x,
                               ncg_moved,comm->moved,
-                              fr->nbv->grp[eintLocal].nbl_lists.simple,
+                              fr->nbv->grp[eintLocal].kernel_type,
                               fr->nbv->grp[eintLocal].nbat);
 
             nbnxn_get_ncells(fr->nbv->nbs,&ncells_new[XX],&ncells_new[YY]);

src/mdlib/forcerec.c
@@ -2068,7 +2068,6 @@ static void init_nb_verlet(FILE *fp,
     nbnxn_init_search(&nbv->nbs,
                       DOMAINDECOMP(cr) ? & cr->dd->nc : NULL,
                       DOMAINDECOMP(cr) ? domdec_zones(cr->dd) : NULL,
-                      nbv->grp[0].kernel_type,
                       gmx_omp_nthreads_get(emntNonbonded));
 
     for(i=0; i<nbv->nloc; i++)

src/mdlib/nbnxn_search.c
@@ -165,7 +165,7 @@
 /* This define is a lazy way to avoid interdependence of the grid
  * and searching data structures.
  */
-#define NBNXN_NA_SC_MAX (NSUBCELL*8)
+#define NBNXN_NA_SC_MAX (NSUBCELL*NBNXN_GPU_CLUSTER_SIZE)
 
 #ifdef NBNXN_SEARCH_SSE
 #define GMX_MM128_HERE
@@ -259,6 +259,7 @@ typedef struct {
 
     gmx_bool simple;
     int  na_c;       /* Number of atoms per cluster          */
+    int  na_cj;      /* Number of atoms for list j-clusters  */
     int  na_sc;      /* Number of atoms per super-cluster    */
     int  na_c_2log;  /* 2log of na_c                         */
 
@@ -280,7 +281,6 @@ typedef struct {
     int   *nsubc;        /* The number of sub cells for each super cell */
     float *bbcz;         /* Bounding boxes in z for the super cells     */
     float *bb;           /* 3D bounding boxes for the sub cells         */
-    gmx_bool cj_half_ci; /* j-cluster size half of i-cluster size?      */
     float *bbj;          /* 3D j-b.boxes for SSE-double or AVX-single   */
     int   *flags;        /* Flag for the super cells                    */
     int   nc_nalloc;
@@ -316,9 +316,6 @@ typedef struct nbnxn_search {
     gmx_bool DomDec;
     ivec dd_dim;
     gmx_domdec_zones_t *zones;
-
-    int  na_c;      /* Number of atoms per cluster for non-simple */
-    int  na_c_2log; /* 2log of na_c                               */
 
     int  ngrid;
     nbnxn_grid_t *grid;
@@ -400,14 +397,12 @@ static void nbs_cycle_print(FILE *fp,const nbnxn_search_t nbs)
     fprintf(fp,"\n");
 }
 
-static void nbnxn_grid_init(nbnxn_grid_t * grid,
-                            gmx_bool cj_half_ci)
+static void nbnxn_grid_init(nbnxn_grid_t * grid)
 {
     grid->cxy_na      = NULL;
     grid->cxy_ind     = NULL;
     grid->cxy_nalloc  = 0;
     grid->bb          = NULL;
-    grid->cj_half_ci  = cj_half_ci;
     grid->bbj         = NULL;
     grid->nc_nalloc   = 0;
 }
@@ -478,12 +473,6 @@ static int kernel_to_cj_size(int nb_kernel_type)
     return 0;
 }
 
-static gmx_bool kernel_cj_half_ci(int nb_kernel_type)
-{
-    return (kernel_to_cj_size(nb_kernel_type)*2 ==
-            kernel_to_ci_size(nb_kernel_type));
-}
-
 static int ci_to_cj(int na_cj_2log,int ci)
 {
     switch (na_cj_2log)
@@ -523,7 +512,6 @@ gmx_bool nbnxn_kernel_pairlist_simple(int nb_kernel_type)
 void nbnxn_init_search(nbnxn_search_t * nbs_ptr,
                        ivec *n_dd_cells,
                        gmx_domdec_zones_t *zones,
-                       int nb_kernel_type_loc,
                        int nthread_max)
 {
     nbnxn_search_t nbs;
@@ -551,23 +539,10 @@ void nbnxn_init_search(nbnxn_search_t * nbs_ptr,
         }
     }
 
-    if (!nbnxn_kernel_pairlist_simple(nb_kernel_type_loc))
-    {
-        nbs->na_c = kernel_to_ci_size(nb_kernel_type_loc);
-
-        if (nbs->na_c*NSUBCELL > NBNXN_NA_SC_MAX)
-        {
-            gmx_fatal(FARGS,
-                      "na_c (%d) is larger than the maximum allowed value (%d)",
-                      nbs->na_c,NBNXN_NA_SC_MAX/NSUBCELL);
-        }
-        nbs->na_c_2log = get_2log(nbs->na_c);
-    }
-
     snew(nbs->grid,nbs->ngrid);
     for(g=0; g<nbs->ngrid; g++)
     {
-        nbnxn_grid_init(&nbs->grid[g],kernel_cj_half_ci(nb_kernel_type_loc));
+        nbnxn_grid_init(&nbs->grid[g]);
     }
     nbs->cell        = NULL;
     nbs->cell_nalloc = 0;
@@ -628,17 +603,10 @@ static int set_grid_size_xy(const nbnxn_search_t nbs,
                             real atom_density,
                             int XFormat)
 {
-    int  cj_size=-1;
     rvec size;
     int  na_c;
     real adens,tlen,tlen_x,tlen_y,nc_max;
     int  t;
-
-    cj_size = grid->na_c;
-    if (grid->cj_half_ci)
-    {
-        cj_size /= 2;
-    }
 
     rvec_sub(corner1,corner0,size);
 
@@ -647,11 +615,10 @@ static int set_grid_size_xy(const nbnxn_search_t nbs,
         /* target cell length */
         if (grid->simple)
         {
-            na_c = grid->na_c;
             /* To minimize the zero interactions, we should make
              * the largest of the i/j cell cubic.
              */
-            na_c = max(na_c,cj_size);
+            na_c = max(grid->na_c,grid->na_cj);
 
             /* Approximately cubic cells */
             tlen   = pow(na_c/atom_density,1.0/3.0);
@@ -695,13 +662,13 @@ static int set_grid_size_xy(const nbnxn_search_t nbs,
     }
 
     /* Worst case scenario of 1 atom in each last cell */
-    if (!grid->simple || cj_size <= 4)
+    if (grid->na_cj <= grid->na_c)
     {
         nc_max = n/grid->na_sc + grid->ncx*grid->ncy;
     }
     else
     {
-        nc_max = n/grid->na_sc + grid->ncx*grid->ncy*cj_size/4;
+        nc_max = n/grid->na_sc + grid->ncx*grid->ncy*grid->na_cj/grid->na_c;
     }
 
     if (nc_max > grid->nc_nalloc)
@@ -728,14 +695,14 @@ static int set_grid_size_xy(const nbnxn_search_t nbs,
 
         if (grid->simple)
         {
-            if (cj_size == 4)
+            if (grid->na_cj == grid->na_c)
             {
                 grid->bbj = grid->bb;
             }
             else
             {
                 sfree_aligned(grid->bbj);
-                snew_aligned(grid->bbj,bb_nalloc*4/cj_size,16);
+                snew_aligned(grid->bbj,bb_nalloc*4/grid->na_cj,16);
             }
         }
 
@@ -1498,7 +1465,7 @@ void fill_cell(const nbnxn_search_t nbs,
         bb_ptr = grid->bb + offset;
 
 #if defined GMX_DOUBLE && defined NBNXN_SEARCH_SSE
-        if (grid->cj_half_ci)
+        if (2*grid->na_cj == grid->na_c)
         {
             calc_bounding_box_x_x4_halves(na,nbat->x+X4_IND_A(a0),bb_ptr,
                                           grid->bbj+offset*2);
@@ -2099,7 +2066,7 @@ void nbnxn_put_on_grid(nbnxn_search_t nbs,
                        const int *atinfo,
                        rvec *x,
                        int nmoved,int *move,
-                       gmx_bool simple,
+                       int nb_kernel_type,
                        nbnxn_atomdata_t *nbat)
 {
     nbnxn_grid_t *grid;
@@ -2110,20 +2077,12 @@ void nbnxn_put_on_grid(nbnxn_search_t nbs,
 
     nbs_cycle_start(&nbs->cc[enbsCCgrid]);
 
-    grid->simple = simple;
+    grid->simple = nbnxn_kernel_pairlist_simple(nb_kernel_type);
 
-    if (grid->simple)
-    {
-        grid->na_c      = SSE_WIDTH;
-        grid->na_sc     = SSE_WIDTH;
-        grid->na_c_2log = SSE_WIDTH_2LOG;
-    }
-    else
-    {
-        grid->na_c      = nbs->na_c;
-        grid->na_sc     = NSUBCELL*nbs->na_c;
-        grid->na_c_2log = nbs->na_c_2log;
-    }
+    grid->na_c      = kernel_to_ci_size(nb_kernel_type);
+    grid->na_cj     = kernel_to_cj_size(nb_kernel_type);
+    grid->na_sc     = (grid->simple ? 1 : NSUBCELL)*grid->na_c;
+    grid->na_c_2log = get_2log(grid->na_c);
 
     nbat->na_c = grid->na_c;
 
@@ -2208,7 +2167,7 @@ void nbnxn_put_on_grid_nonlocal(nbnxn_search_t nbs,
                                 const gmx_domdec_zones_t *zones,
                                 const int *atinfo,
                                 rvec *x,
-                                gmx_bool simple,
+                                int nb_kernel_type,
                                 nbnxn_atomdata_t *nbat)
 {
     int  zone,d;
@@ -2230,7 +2189,7 @@ void nbnxn_put_on_grid_nonlocal(nbnxn_search_t nbs,
                           atinfo,
                           x,
                           0,NULL,
-                          simple,
+                          nb_kernel_type,
                           nbat);
     }
 }

src/mdlib/nbnxn_search.h
@@ -51,7 +51,6 @@ gmx_bool nbnxn_kernel_pairlist_simple(int nb_kernel_type);
 void nbnxn_init_search(nbnxn_search_t * nbs_ptr,
                        ivec *n_dd_cells,
                        gmx_domdec_zones_t *zones,
-		       int nb_kernel_type_loc,
                        int nthread_max);
 
 /* Put the atoms on the pair search grid.
@@ -72,7 +71,7 @@ void nbnxn_put_on_grid(nbnxn_search_t nbs,
                        const int *atinfo,
                        rvec *x,
                        int nmoved,int *move,
-                       gmx_bool simple,
+                       int nb_kernel_type,
                        nbnxn_atomdata_t *nbat);
 
 /* As nbnxn_put_on_grid, but for the non-local atoms
@@ -83,7 +82,7 @@ void nbnxn_put_on_grid_nonlocal(nbnxn_search_t nbs,
                                 const gmx_domdec_zones_t *zones,
                                 const int *atinfo,
                                 rvec *x,
-                                gmx_bool simple,
+				int nb_kernel_type,
                                 nbnxn_atomdata_t *nbat);
 
 /* Add simple grid type information to the local super/sub grid */

src/mdlib/sim_util.c
@@ -830,7 +830,7 @@ void do_force_cutsVERLET(FILE *fplog,t_commrec *cr,
                               0,vzero,box_diag,
                               0,mdatoms->homenr,-1,fr->cginfo,x,
                               0,NULL,
-                              nbv->grp[eintLocal].nbl_lists.simple,
+                              nbv->grp[eintLocal].kernel_type,
                               nbv->grp[eintLocal].nbat);
             wallcycle_sub_stop(wcycle,ewcsNBS_GRID_LOCAL);
         }
@@ -839,7 +839,7 @@ void do_force_cutsVERLET(FILE *fplog,t_commrec *cr,
             wallcycle_sub_start(wcycle,ewcsNBS_GRID_NONLOCAL);
             nbnxn_put_on_grid_nonlocal(nbv->nbs,domdec_zones(cr->dd),
                                        fr->cginfo,x,
-                                       nbv->grp[eintNonlocal].nbl_lists.simple,
+                                       nbv->grp[eintNonlocal].kernel_type,
                                        nbv->grp[eintNonlocal].nbat);
             wallcycle_sub_stop(wcycle,ewcsNBS_GRID_NONLOCAL);
         }