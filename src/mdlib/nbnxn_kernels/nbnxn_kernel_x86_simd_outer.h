gromacs/gromacs

commit 224ec73c0ca476d93bfd04bcd6174787fef996fd

renamed the nbnxn sse/avx kernels to x86_simd128/256

src/mdlib/nbnxn_kernels/nbnxn_kernel_x86_simd_outer.h
@@ -51,18 +51,20 @@
 #define UNROLLI    NBNXN_CPU_CLUSTER_I_SIZE
 #define UNROLLJ    SSE_OR_AVX_WIDTH
 
+#if defined GMX_MM128_HERE || defined GMX_DOUBLE
+#define STRIDE     4
+#endif
 #if defined GMX_MM256_HERE && !defined GMX_DOUBLE
 #define STRIDE     8
-#else
-#define STRIDE     4
 #endif 
 
-#ifndef GMX_MM256_HERE
+#ifdef GMX_MM128_HERE
 #ifndef GMX_DOUBLE
 /* SSE single precision 4x4 kernel */
 #define SUM_SIMD(x) SUM_SIMD4(x)
 #define TAB_FDV0
-#else
+#endif
+#ifdef GMX_MM256_HERE
 /* SSE double precision 4x2 kernel */
 #define SUM_SIMD(x) (x[0]+x[1])
 #endif
@@ -79,7 +81,7 @@
 
 #define SIMD_MASK_ALL   0xffffffff
 
-#include "nbnxn_kernel_sse_utils.h"
+#include "nbnxn_kernel_x86_simd_utils.h"
 
 /* All functionality defines are set here, except for:
  * CALC_ENERGIES, ENERGY_GROUPS which are defined before.
@@ -116,19 +118,20 @@
 #endif
 
 #ifdef GMX_MM128_HERE
-#define NBK_FUNC_NAME_SSE_OR_AVX(b,e) NBK_FUNC_NAME(b,sse,e)
-#else
-#define NBK_FUNC_NAME_SSE_OR_AVX(b,e) NBK_FUNC_NAME(b,avx,e)
+#define NBK_FUNC_NAME_S128_OR_S256(b,e) NBK_FUNC_NAME(b,x86_simd128,e)
+#endif
+#ifdef GMX_MM256_HERE
+#define NBK_FUNC_NAME_S128_OR_S256(b,e) NBK_FUNC_NAME(b,x86_simd256,e)
 #endif
 
 static void
 #ifndef CALC_ENERGIES
-NBK_FUNC_NAME_SSE_OR_AVX(nbnxn_kernel,noener)
+NBK_FUNC_NAME_S128_OR_S256(nbnxn_kernel,noener)
 #else
 #ifndef ENERGY_GROUPS
-NBK_FUNC_NAME_SSE_OR_AVX(nbnxn_kernel,ener)
+NBK_FUNC_NAME_S128_OR_S256(nbnxn_kernel,ener)
 #else
-NBK_FUNC_NAME_SSE_OR_AVX(nbnxn_kernel,energrp)
+NBK_FUNC_NAME_S128_OR_S256(nbnxn_kernel,energrp)
 #endif
 #endif
 #undef NBK_FUNC_NAME
@@ -620,13 +623,13 @@ NBK_FUNC_NAME_SSE_OR_AVX(nbnxn_kernel,energrp)
 #define CHECK_EXCLS
             while (cjind < cjind1 && nbl->cj[cjind].excl != SIMD_MASK_ALL)
             {
-#include "nbnxn_kernel_sse_inner.h"
+#include "nbnxn_kernel_x86_simd_inner.h"
                 cjind++;
             }
 #undef CHECK_EXCLS
             for(; (cjind<cjind1); cjind++)
             {
-#include "nbnxn_kernel_sse_inner.h"
+#include "nbnxn_kernel_x86_simd_inner.h"
             }
 #undef HALF_LJ
 #undef CALC_COULOMB
@@ -637,13 +640,13 @@ NBK_FUNC_NAME_SSE_OR_AVX(nbnxn_kernel,energrp)
 #define CHECK_EXCLS
             while (cjind < cjind1 && nbl->cj[cjind].excl != SIMD_MASK_ALL)
             {
-#include "nbnxn_kernel_sse_inner.h"
+#include "nbnxn_kernel_x86_simd_inner.h"
                 cjind++;
             }
 #undef CHECK_EXCLS
             for(; (cjind<cjind1); cjind++)
             {
-#include "nbnxn_kernel_sse_inner.h"
+#include "nbnxn_kernel_x86_simd_inner.h"
             }
 #undef CALC_COULOMB
         }
@@ -652,13 +655,13 @@ NBK_FUNC_NAME_SSE_OR_AVX(nbnxn_kernel,energrp)
 #define CHECK_EXCLS
             while (cjind < cjind1 && nbl->cj[cjind].excl != SIMD_MASK_ALL)
             {
-#include "nbnxn_kernel_sse_inner.h"
+#include "nbnxn_kernel_x86_simd_inner.h"
                 cjind++;
             }
 #undef CHECK_EXCLS
             for(; (cjind<cjind1); cjind++)
             {
-#include "nbnxn_kernel_sse_inner.h"
+#include "nbnxn_kernel_x86_simd_inner.h"
             }
         }
 #undef CALC_LJ

src/mdlib/nbnxn_kernels/nbnxn_kernel_x86_simd128.c
@@ -46,7 +46,7 @@
 
 #if ( defined(GMX_IA32_SSE) || defined(GMX_X86_64_SSE) || defined(GMX_X86_64_SSE2) )
 
-#include "nbnxn_kernel_sse.h"
+#include "nbnxn_kernel_x86_simd128.h"
 
 /* Include all flavors of the 128-bit SSE or AVX kernel loops */
 
@@ -58,35 +58,35 @@
 /* Include the force+energy kernels */
 #define CALC_ENERGIES
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef CALC_ENERGIES
 
 /* Include the force+energygroups kernels */
 #define CALC_ENERGIES
 #define ENERGY_GROUPS
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef ENERGY_GROUPS
 #undef CALC_ENERGIES
 
 /* Include the force only kernels */
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 
 #undef CALC_COUL_RF
 
@@ -97,35 +97,35 @@
 /* Include the force+energy kernels */
 #define CALC_ENERGIES
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef CALC_ENERGIES
 
 /* Include the force+energygroups kernels */
 #define CALC_ENERGIES
 #define ENERGY_GROUPS
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef ENERGY_GROUPS
 #undef CALC_ENERGIES
 
 /* Include the force only kernels */
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 
 #undef CALC_COUL_PME
 
@@ -150,28 +150,28 @@ enum { coultRF, coultTAB, coultNR };
 
 
 static p_nbk_func_ener p_nbk_ener[coultNR][ljcrNR] =
-{ { nbnxn_kernel_sse_rf_comb_geom_ener,
-    nbnxn_kernel_sse_rf_comb_lb_ener,
-    nbnxn_kernel_sse_rf_comb_none_ener },
-  { nbnxn_kernel_sse_tab_comb_geom_ener,
-    nbnxn_kernel_sse_tab_comb_lb_ener,
-    nbnxn_kernel_sse_tab_comb_none_ener } };
+{ { nbnxn_kernel_x86_simd128_rf_comb_geom_ener,
+    nbnxn_kernel_x86_simd128_rf_comb_lb_ener,
+    nbnxn_kernel_x86_simd128_rf_comb_none_ener },
+  { nbnxn_kernel_x86_simd128_tab_comb_geom_ener,
+    nbnxn_kernel_x86_simd128_tab_comb_lb_ener,
+    nbnxn_kernel_x86_simd128_tab_comb_none_ener } };
 
 static p_nbk_func_ener p_nbk_energrp[coultNR][ljcrNR] =
-{ { nbnxn_kernel_sse_rf_comb_geom_energrp,
-    nbnxn_kernel_sse_rf_comb_lb_energrp,
-    nbnxn_kernel_sse_rf_comb_none_energrp },
-  { nbnxn_kernel_sse_tab_comb_geom_energrp,
-    nbnxn_kernel_sse_tab_comb_lb_energrp,
-    nbnxn_kernel_sse_tab_comb_none_energrp } };
+{ { nbnxn_kernel_x86_simd128_rf_comb_geom_energrp,
+    nbnxn_kernel_x86_simd128_rf_comb_lb_energrp,
+    nbnxn_kernel_x86_simd128_rf_comb_none_energrp },
+  { nbnxn_kernel_x86_simd128_tab_comb_geom_energrp,
+    nbnxn_kernel_x86_simd128_tab_comb_lb_energrp,
+    nbnxn_kernel_x86_simd128_tab_comb_none_energrp } };
 
 static p_nbk_func_noener p_nbk_noener[coultNR][ljcrNR] =
-{ { nbnxn_kernel_sse_rf_comb_geom_noener,
-    nbnxn_kernel_sse_rf_comb_lb_noener,
-    nbnxn_kernel_sse_rf_comb_none_noener },
-  { nbnxn_kernel_sse_tab_comb_geom_noener,
-    nbnxn_kernel_sse_tab_comb_lb_noener,
-    nbnxn_kernel_sse_tab_comb_none_noener } };
+{ { nbnxn_kernel_x86_simd128_rf_comb_geom_noener,
+    nbnxn_kernel_x86_simd128_rf_comb_lb_noener,
+    nbnxn_kernel_x86_simd128_rf_comb_none_noener },
+  { nbnxn_kernel_x86_simd128_tab_comb_geom_noener,
+    nbnxn_kernel_x86_simd128_tab_comb_lb_noener,
+    nbnxn_kernel_x86_simd128_tab_comb_none_noener } };
 
 #endif /* SSE */
 
@@ -243,15 +243,15 @@ static void reduce_group_energies(int ng,int ng_2log,
 }
 
 void
-nbnxn_kernel_sse(nbnxn_pairlist_set_t       *nbl_list,
-                 const nbnxn_atomdata_t     *nbat,
-                 const interaction_const_t  *ic,
-                 rvec                       *shift_vec, 
-                 int                        force_flags,
-                 gmx_bool                   clearF,
-                 real                       *fshift,
-                 real                       *Vc,
-                 real                       *Vvdw)
+nbnxn_kernel_x86_simd128(nbnxn_pairlist_set_t       *nbl_list,
+                         const nbnxn_atomdata_t     *nbat,
+                         const interaction_const_t  *ic,
+                         rvec                       *shift_vec, 
+                         int                        force_flags,
+                         gmx_bool                   clearF,
+                         real                       *fshift,
+                         real                       *Vc,
+                         real                       *Vvdw)
 #if ( defined(GMX_IA32_SSE) || defined(GMX_X86_64_SSE) || defined(GMX_X86_64_SSE2) )
 {
     int              nnbl;
@@ -371,6 +371,6 @@ nbnxn_kernel_sse(nbnxn_pairlist_set_t       *nbl_list,
 }
 #else
 {
-    gmx_incons("nbnxn_kernel_sse called while GROMACS was configured without SSE enabled");
+    gmx_incons("nbnxn_kernel_x86_simd128 called while GROMACS was configured without SSE enabled");
 }
 #endif

src/mdlib/nbnxn_kernels/nbnxn_kernel_x86_simd128.h
@@ -32,8 +32,8 @@
  * And Hey:
  * Gallium Rubidium Oxygen Manganese Argon Carbon Silicon
  */
-#ifndef _nbnxn_kernel_avx_h
-#define _nbnxn_kernel_avx_h
+#ifndef _nbnxn_kernel_x86_simd128_h
+#define _nbnxn_kernel_x86_simd128_h
 
 #include "typedefs.h"
 
@@ -41,17 +41,17 @@
 extern "C" {
 #endif
 
-/* Wrapper call for the non-bonded cell vs cell kernels */
+/* Wrapper call for the non-bonded cluster vs cluster kernels */
 void
-nbnxn_kernel_avx(nbnxn_pairlist_set_t       *nbl_list,
-                 const nbnxn_atomdata_t     *nbat,
-                 const interaction_const_t  *ic,
-                 rvec                       *shift_vec,
-                 int                        force_flags,
-                 gmx_bool                   clearF,
-                 real                       *fshift,
-                 real                       *Vc,
-                 real                       *Vvdw);
+nbnxn_kernel_x86_simd128(nbnxn_pairlist_set_t       *nbl_list,
+                         const nbnxn_atomdata_t     *nbat,
+                         const interaction_const_t  *ic,
+                         rvec                       *shift_vec,
+                         int                        force_flags,
+                         gmx_bool                   clearF,
+                         real                       *fshift,
+                         real                       *Vc,
+                         real                       *Vvdw);
 
 #ifdef __cplusplus
 }

src/mdlib/nbnxn_kernels/nbnxn_kernel_x86_simd256.c
@@ -46,7 +46,7 @@
 
 #ifdef GMX_AVX
 
-#include "nbnxn_kernel_avx.h"
+#include "nbnxn_kernel_x86_simd256.h"
 
 /* Include all flavors of the 256-bit AVX kernel loops */
 
@@ -58,35 +58,35 @@
 /* Include the force+energy kernels */
 #define CALC_ENERGIES
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef CALC_ENERGIES
 
 /* Include the force+energygroups kernels */
 #define CALC_ENERGIES
 #define ENERGY_GROUPS
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef ENERGY_GROUPS
 #undef CALC_ENERGIES
 
 /* Include the force only kernels */
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 
 #undef CALC_COUL_RF
 
@@ -97,35 +97,35 @@
 /* Include the force+energy kernels */
 #define CALC_ENERGIES
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef CALC_ENERGIES
 
 /* Include the force+energygroups kernels */
 #define CALC_ENERGIES
 #define ENERGY_GROUPS
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef ENERGY_GROUPS
 #undef CALC_ENERGIES
 
 /* Include the force only kernels */
 #define LJ_COMB_GEOM
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_GEOM
 #define LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 #undef LJ_COMB_LB
-#include "nbnxn_kernel_sse_outer.h"
+#include "nbnxn_kernel_x86_simd_outer.h"
 
 #undef CALC_COUL_PME
 
@@ -150,28 +150,28 @@ enum { coultRF, coultTAB, coultNR };
 
 
 static p_nbk_func_ener p_nbk_ener[coultNR][ljcrNR] =
-{ { nbnxn_kernel_avx_rf_comb_geom_ener,
-    nbnxn_kernel_avx_rf_comb_lb_ener,
-    nbnxn_kernel_avx_rf_comb_none_ener },
-  { nbnxn_kernel_avx_tab_comb_geom_ener,
-    nbnxn_kernel_avx_tab_comb_lb_ener,
-    nbnxn_kernel_avx_tab_comb_none_ener } };
+{ { nbnxn_kernel_x86_simd256_rf_comb_geom_ener,
+    nbnxn_kernel_x86_simd256_rf_comb_lb_ener,
+    nbnxn_kernel_x86_simd256_rf_comb_none_ener },
+  { nbnxn_kernel_x86_simd256_tab_comb_geom_ener,
+    nbnxn_kernel_x86_simd256_tab_comb_lb_ener,
+    nbnxn_kernel_x86_simd256_tab_comb_none_ener } };
 
 static p_nbk_func_ener p_nbk_energrp[coultNR][ljcrNR] =
-{ { nbnxn_kernel_avx_rf_comb_geom_energrp,
-    nbnxn_kernel_avx_rf_comb_lb_energrp,
-    nbnxn_kernel_avx_rf_comb_none_energrp },
-  { nbnxn_kernel_avx_tab_comb_geom_energrp,
-    nbnxn_kernel_avx_tab_comb_lb_energrp,
-    nbnxn_kernel_avx_tab_comb_none_energrp } };
+{ { nbnxn_kernel_x86_simd256_rf_comb_geom_energrp,
+    nbnxn_kernel_x86_simd256_rf_comb_lb_energrp,
+    nbnxn_kernel_x86_simd256_rf_comb_none_energrp },
+  { nbnxn_kernel_x86_simd256_tab_comb_geom_energrp,
+    nbnxn_kernel_x86_simd256_tab_comb_lb_energrp,
+    nbnxn_kernel_x86_simd256_tab_comb_none_energrp } };
 
 static p_nbk_func_noener p_nbk_noener[coultNR][ljcrNR] =
-{ { nbnxn_kernel_avx_rf_comb_geom_noener,
-    nbnxn_kernel_avx_rf_comb_lb_noener,
-    nbnxn_kernel_avx_rf_comb_none_noener },
-  { nbnxn_kernel_avx_tab_comb_geom_noener,
-    nbnxn_kernel_avx_tab_comb_lb_noener,
-    nbnxn_kernel_avx_tab_comb_none_noener } };
+{ { nbnxn_kernel_x86_simd256_rf_comb_geom_noener,
+    nbnxn_kernel_x86_simd256_rf_comb_lb_noener,
+    nbnxn_kernel_x86_simd256_rf_comb_none_noener },
+  { nbnxn_kernel_x86_simd256_tab_comb_geom_noener,
+    nbnxn_kernel_x86_simd256_tab_comb_lb_noener,
+    nbnxn_kernel_x86_simd256_tab_comb_none_noener } };
 
 #endif /* SSE */
 
@@ -243,15 +243,15 @@ static void reduce_group_energies(int ng,int ng_2log,
 }
 
 void
-nbnxn_kernel_avx(nbnxn_pairlist_set_t       *nbl_list,
-                 const nbnxn_atomdata_t     *nbat,
-                 const interaction_const_t  *ic,
-                 rvec                       *shift_vec, 
-                 int                        force_flags,
-                 gmx_bool                   clearF,
-                 real                       *fshift,
-                 real                       *Vc,
-                 real                       *Vvdw)
+nbnxn_kernel_x86_simd256(nbnxn_pairlist_set_t       *nbl_list,
+                         const nbnxn_atomdata_t     *nbat,
+                         const interaction_const_t  *ic,
+                         rvec                       *shift_vec, 
+                         int                        force_flags,
+                         gmx_bool                   clearF,
+                         real                       *fshift,
+                         real                       *Vc,
+                         real                       *Vvdw)
 #ifdef GMX_AVX
 {
     int              nnbl;
@@ -371,6 +371,6 @@ nbnxn_kernel_avx(nbnxn_pairlist_set_t       *nbl_list,
 }
 #else
 {
-    gmx_incons("nbnxn_kernel_sse called while GROMACS was configured without AVX enabled");
+    gmx_incons("nbnxn_kernel_x86_simd256 called while GROMACS was configured without AVX enabled");
 }
 #endif

src/mdlib/nbnxn_kernels/nbnxn_kernel_x86_simd256.h
@@ -32,8 +32,8 @@
  * And Hey:
  * Gallium Rubidium Oxygen Manganese Argon Carbon Silicon
  */
-#ifndef _nbnxn_kernel_sse_h
-#define _nbnxn_kernel_sse_h
+#ifndef _nbnxn_kernel_x86_simd256_h
+#define _nbnxn_kernel_x86_simd256_h
 
 #include "typedefs.h"
 
@@ -41,17 +41,17 @@
 extern "C" {
 #endif
 
-/* Wrapper call for the non-bonded cell vs cell kernels */
+/* Wrapper call for the non-bonded cluster vs cluster kernels */
 void
-nbnxn_kernel_sse(nbnxn_pairlist_set_t       *nbl_list,
-                 const nbnxn_atomdata_t     *nbat,
-                 const interaction_const_t  *ic,
-                 rvec                       *shift_vec,
-                 int                        force_flags,
-                 gmx_bool                   clearF,
-                 real                       *fshift,
-                 real                       *Vc,
-                 real                       *Vvdw);
+nbnxn_kernel_x86_simd256(nbnxn_pairlist_set_t       *nbl_list,
+                         const nbnxn_atomdata_t     *nbat,
+                         const interaction_const_t  *ic,
+                         rvec                       *shift_vec,
+                         int                        force_flags,
+                         gmx_bool                   clearF,
+                         real                       *fshift,
+                         real                       *Vc,
+                         real                       *Vvdw);
 
 #ifdef __cplusplus
 }

src/mdlib/sim_util.c
@@ -86,9 +86,9 @@
 #include "gmx_wallcycle.h"
 #include "genborn.h"
 #include "nbnxn_search.h"
-#include "nbnxn_kernels/nbnxn_kernel_sse.h"
-#include "nbnxn_kernels/nbnxn_kernel_avx.h"
 #include "nbnxn_kernels/nbnxn_kernel_ref.h"
+#include "nbnxn_kernels/nbnxn_kernel_x86_simd128.h"
+#include "nbnxn_kernels/nbnxn_kernel_x86_simd256.h"
 #include "nbnxn_kernels/nbnxn_kernel_gpu_ref.h"
 
 #ifdef GMX_LIB_MPI
@@ -618,28 +618,28 @@ static void do_nb_verlet(t_forcerec *fr,
             break;
         
         case nbk4xN_S128:
-            nbnxn_kernel_sse(&nbvg->nbl_lists,
-                             nbvg->nbat, ic,
-                             fr->shift_vec,
-                             flags,
-                             clearF,
-                             fr->fshift[0],
-                             enerd->grpp.ener[egCOULSR],
-                             fr->bBHAM ?
-                             enerd->grpp.ener[egBHAMSR] :
-                             enerd->grpp.ener[egLJSR]);
+            nbnxn_kernel_x86_simd128(&nbvg->nbl_lists,
+                                     nbvg->nbat, ic,
+                                     fr->shift_vec,
+                                     flags,
+                                     clearF,
+                                     fr->fshift[0],
+                                     enerd->grpp.ener[egCOULSR],
+                                     fr->bBHAM ?
+                                     enerd->grpp.ener[egBHAMSR] :
+                                     enerd->grpp.ener[egLJSR]);
             break;
         case nbk4xN_S256:
-            nbnxn_kernel_avx(&nbvg->nbl_lists,
-                             nbvg->nbat, ic,
-                             fr->shift_vec,
-                             flags,
-                             clearF,
-                             fr->fshift[0],
-                             enerd->grpp.ener[egCOULSR],
-                             fr->bBHAM ?
-                             enerd->grpp.ener[egBHAMSR] :
-                             enerd->grpp.ener[egLJSR]);
+            nbnxn_kernel_x86_simd256(&nbvg->nbl_lists,
+                                     nbvg->nbat, ic,
+                                     fr->shift_vec,
+                                     flags,
+                                     clearF,
+                                     fr->fshift[0],
+                                     enerd->grpp.ener[egCOULSR],
+                                     fr->bBHAM ?
+                                     enerd->grpp.ener[egBHAMSR] :
+                                     enerd->grpp.ener[egLJSR]);
             break;
 
         case nbk8x8x8_CUDA:

commit 4820a9daed92dc17d700f2e1cd1f9a004b85217a

fixed nbnxn sse double prec. compilation, broken in 224ec73c

src/mdlib/nbnxn_kernels/nbnxn_kernel_x86_simd_outer.h
@@ -63,12 +63,13 @@
 /* SSE single precision 4x4 kernel */
 #define SUM_SIMD(x) SUM_SIMD4(x)
 #define TAB_FDV0
-#endif
-#ifdef GMX_MM256_HERE
+#else
 /* SSE double precision 4x2 kernel */
 #define SUM_SIMD(x) (x[0]+x[1])
 #endif
-#else
+#endif
+
+#ifdef GMX_MM256_HERE
 #ifndef GMX_DOUBLE
 /* AVX single precision 4x8 kernel */
 #define SUM_SIMD(x) (x[0]+x[1]+x[2]+x[3]+x[4]+x[5]+x[6]+x[7])