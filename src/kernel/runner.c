a643e9c0ab1b8d82e50e2b4c37a391fa459ffd4a
Carsten Kutzner <ckutzne@gwdg.de>
2012-06-15 14:43:35 +0200

    Enforced rotation: PBC/restart fix
    ... for rot groups consisting of >1 molecule. This commit fixes a problem with checkpoint-restarts
    when using a rotation group consisting of more than a single molecule.
    
    Change-Id: I7378047b5cd0909b864a62200ded747682f627a1

--- src/kernel/runner.c
+++ src/kernel/runner.c
@@ -878,7 +878,7 @@ int mdrunner(int nthreads_requested, FILE *fplog,t_commrec *cr,int nfile,
         if (inputrec->bRot)
         {
            /* Initialize enforced rotation code */
-           init_rot(fplog,inputrec,nfile,fnm,cr,state->x,state->box,mtop,oenv,
+           init_rot(fplog,inputrec,nfile,fnm,cr,state->x,box,mtop,oenv,
                     bVerbose,Flags);
         }
 
