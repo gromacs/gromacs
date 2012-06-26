gromacs/gromacs

commit 697bcdcaf414f8800727ce1f3b67b11f290fdc6c

introduce gmx_omp wrapper for the OpenMP API
The gmx_omp_* OpenMP API wrappers should be used instead of directly
calling OpenMP functions. Therefore, gmx_omp.h should be included
instead of omp.h so OpenMP API functions don't need #ifdef-ing.

Additionally, moved GMX_OPENMP from command-line define to config.h.

Change-Id: If30f433946fef908c26a29f1bff3671580e90629

src/gmxlib/gmx_omp.c
@@ -1,0 +1,61 @@
+/* -*- mode: c; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; c-file-style: "stroustrup"; -*-
+ *
+ *
+ *                This source code is part of
+ *
+ *                 G   R   O   M   A   C   S
+ *
+ *          GROningen MAchine for Chemical Simulations
+ *
+ * Written by the Gromacs development team under coordination of
+ * David van der Spoel, Berk Hess, and Erik Lindahl.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org
+ *
+ * And Hey:
+ * Gnomes, ROck Monsters And Chili Sauce
+ */
+
+#ifdef HAVE_CONFIG
+#include "config.h"
+#endif
+
+#ifdef GMX_OPENMP
+#include <omp.h>
+#endif
+
+#include "gmx_omp.h"
+
+int gmx_omp_get_max_threads(void)
+{
+#ifdef GMX_OPENMP
+    return omp_get_max_threads();
+#else
+    return 1;
+#endif
+}
+
+
+int gmx_omp_get_thread_num(void)
+{
+#ifdef GMX_OPENMP
+    return omp_get_thread_num();
+#else
+    return 0;
+#endif
+}
+
+void gmx_omp_set_num_threads(int num_threads)
+{
+#ifdef GMX_OPENMP
+    omp_set_num_threads(num_threads);
+#else
+    return;
+#endif
+}

src/mdlib/pme.c
@@ -67,9 +67,7 @@
 #include "tmpi.h"
 #endif
 
-#ifdef GMX_OPENMP
-#include <omp.h>
-#endif
+#include "gmx_omp.h"
 
 #include <stdio.h>
 #include <string.h>

CMakeLists.txt
@@ -185,7 +185,6 @@ if(GMX_OPENMP)
     if(OPENMP_FOUND)
         set(GROMACS_C_FLAGS "${OpenMP_C_FLAGS} ${GROMACS_C_FLAGS}")
         set(GROMACS_CXX_FLAGS "${OpenMP_CXX_FLAGS} ${GROMACS_CXX_FLAGS}")
-        add_definitions(-DGMX_OPENMP)
     else(OPENMP_FOUND)
         message(WARNING
                 "Compiler not supporting OpenMP. This might hurt your performance a lot, "

include/gmx_omp.h
@@ -1,0 +1,48 @@
+/* -*- mode: c; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; c-file-style: "stroustrup"; -*-
+ *
+ *
+ *                This source code is part of
+ *
+ *                 G   R   O   M   A   C   S
+ *
+ *          GROningen MAchine for Chemical Simulations
+ *
+ * Written by the Gromacs development team under coordination of
+ * David van der Spoel, Berk Hess, and Erik Lindahl.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org
+ *
+ * And Hey:
+ * GROup of MAchos and Cynical Suckers
+ */
+
+#ifndef GMX_OMP_H
+#define GMX_OMP_H
+
+/* This module defines wrappers for OpenMP API functions and enables compiling
+ * code even when OpenMP is turned off in the build system.
+ * Therfore, OpenMP API functions should always be used through these wrappers
+ * and omp.h should never be directly included. Instead, this header should be
+ * used whnever OpenMP API functions are needed.
+ */
+
+/*! Sets the number of threads in subsequent parallel regions, unless overridden
+ *  by a num_threads clause. Acts as a wrapper for omp_get_max_threads(void). */
+int  gmx_omp_get_max_threads(void);
+
+/*! Returns the thread number of the thread executing within its thread team.
+ *  Acts as a warpper for omp_get_thread_num(void). */
+int  gmx_omp_get_thread_num(void);
+
+/*! Returns an integer that is equal to or greater than the number of threads
+ * that would be available if a parallel region without num_threads were
+ * defined at that point in the code. Acts as a wapepr for omp_set_num_threads(void). */
+void gmx_omp_set_num_threads(int num_threads);
+
+#endif /* GMX_OMP_H */

src/config.h.cmakein
@@ -131,8 +131,11 @@
    (MPI or thread_mpi) */
 #cmakedefine GMX_MPI
 
-/* Use threads for parallelization */
+/* Use threads_mpi for parallelization */
 #cmakedefine GMX_THREAD_MPI
+
+/* Use OpenMP multithreading */
+#cmakedefine GMX_OPENMP
 
 /* Use old threading (domain decomp force calc) code */
 #cmakedefine GMX_THREAD_SHM_FDECOMP 

src/kernel/runner.c
@@ -73,6 +73,8 @@
 #include "membed.h"
 #include "md_openmm.h"
 
+#include "gmx_omp.h"
+
 #ifdef GMX_LIB_MPI
 #include <mpi.h>
 #endif
@@ -86,10 +88,6 @@
 
 #ifdef GMX_OPENMM
 #include "md_openmm.h"
-#endif
-
-#ifdef GMX_OPENMP
-#include <omp.h>
 #endif
 
 
@@ -829,7 +827,7 @@ int mdrunner(int nthreads_requested, FILE *fplog,t_commrec *cr,int nfile,
             {
                 cpu_set_t mask;
                 CPU_ZERO(&mask);
-                core+=omp_get_thread_num();
+                core+=gmx_omp_get_thread_num();
                 CPU_SET(core,&mask);
                 sched_setaffinity((pid_t) syscall (SYS_gettid),sizeof(cpu_set_t),&mask);
             }

src/mdlib/fft5d.c
@@ -58,10 +58,11 @@
 #endif
 
 #ifdef GMX_OPENMP
+/* TODO: Do we still need this? Are we still planning ot use fftw + OpenMP? */
 #define FFT5D_THREADS
 #endif
 #ifdef FFT5D_THREADS
-#include <omp.h>
+#include "gmx_omp.h"
 /* requires fftw compiled with openmp */
 /* #define FFT5D_FFTW_THREADS (now set by cmake) */
 #endif

src/tools/geminate.c
@@ -52,13 +52,8 @@
 #include "smalloc.h"
 #include "vec.h"
 #include "geminate.h"
+#include "gmx_omp.h"
 
-#ifdef DOUSEOPENMP
-#define HAVE_OPENMP
-#endif
-#ifdef HAVE_OPENMP
-#include <omp.h>
-#endif
 
 /* The first few sections of this file contain functions that were adopted,
  * and to some extent modified, by Erik Marklund (erikm[aT]xray.bmc.uu.se,
@@ -473,13 +468,11 @@ static double eq10v2(double theoryCt[], double time[], int manytimes,
   part3 = gem_cxmul(gamma, gem_cxmul(gem_cxadd(alpha, beta) , gem_cxsub(alpha, beta)));  /* 3(1+2)(1-2) */
   part4 = gem_cxmul(gem_cxsub(gamma, alpha), gem_cxmul(gem_cxsub(alpha, beta), gem_cxsub(beta, gamma))); /* (3-1)(1-2)(2-3) */
 
-#ifdef HAVE_OPENMP
 #pragma omp parallel for				\
   private(i, tsqrt, oma, omb, omc, c1, c2, c3, c4),	\
   reduction(+:sumimaginary),				\
   default(shared),					\
   schedule(guided)
-#endif
   for (i=0; i<manytimes; i++){
     tsqrt = sqrt(time[i]);
     oma   = gem_comega(gem_cxrmul(alpha, tsqrt));
@@ -594,13 +587,11 @@ static double gemFunc_residual2(const gsl_vector *p, void *data)
   fixGemACF(GD->ctTheory, nFitPoints);
 
   /* Removing a bunch of points from the log-part. */
-#ifdef HAVE_OPENMP
 #pragma omp parallel for schedule(dynamic)	\
   firstprivate(nData, ctTheory, y, nFitPoints)	\
   private (i, iLog, r)			\
   reduction(+:residual2)			\
   default(shared)
-#endif
   for(i=0; i<nFitPoints; i++)
     {
       iLog = GD->logtime[i];
@@ -667,8 +658,8 @@ extern real fitGemRecomb(double *ct, double *time, double **ctFit,
 
 #ifdef HAVE_LIBGSL
 #ifdef HAVE_OPENMP
-  nThreads = omp_get_num_procs();
-  omp_set_num_threads(nThreads);
+  nThreads = gmx_omp_get_max_threads();
+  gmx_omp_set_num_threads(nThreads);
   fprintf(stdout, "We will be using %i threads.\n", nThreads);
 #endif
 

commit 00c34347f68e90878ce321d65fa387a841359bc6

fixes typo gmx_omp introduced by 697bcdc
Also removes an unnecessary include.

Change-Id: I8a92a5f1849d170951719b29b17e0ec6134ee556

src/gmxlib/gmx_omp.c
@@ -22,7 +22,7 @@
  * Gnomes, ROck Monsters And Chili Sauce
  */
 
-#ifdef HAVE_CONFIG
+#ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 

src/mdlib/pme.c
@@ -67,8 +67,6 @@
 #include "tmpi.h"
 #endif
 
-#include "gmx_omp.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <math.h>