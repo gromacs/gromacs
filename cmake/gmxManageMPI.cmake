gromacs/gromacs

commit 417d0affff5485d10f905aa3581f14602aca873a

Remove use of FindMPI.cmake
The functionality of FindMPI.cmake should not be needed for GROMACS,
since the wrapper compiler will do the complete job. This already
worked for Cray and BlueGene, and so far this new version is known
to work for OpenMPI wrapping icc and gcc.

CMake now warns the user about possible unsuitable versions of
OpenMPI and MVAPICH2.

The process of managing MPI is more modular than it used to be.

To use:

cmake .. -DGMX_MPI=ON -DCMAKE_C_COMPILER=`which mpicc`

or with bash

CC=mpicc cmake .. -DGMX_MPI=ON

and whatever else suits your setup.

Fixes #851 and #636.

Change-Id: Ibe41206bed8b70b83a25da1e4e29dd87b61ea17d

cmake/gmxManageMPI.cmake
@@ -1,0 +1,77 @@
+# Manage the MPI setup, assuming that CMAKE_C_COMPILER is an MPI
+# (wrapper) compiler.
+if(GMX_MPI)
+  if(GMX_THREADS)
+    set(GMX_THREADS OFF CACHE BOOL
+      "Thread-based parallelization conflicts with MPI." FORCE)
+  endif(GMX_THREADS)
+
+  # Test the CMAKE_C_COMPILER for being an MPI (wrapper) compiler
+  TRY_COMPILE(MPI_FOUND ${CMAKE_BINARY_DIR}
+    "${CMAKE_SOURCE_DIR}/cmake/TestMPI.c"
+    COMPILE_DEFINITIONS )
+
+  if(MPI_FOUND)
+    if(GMX_FAHCORE)
+      add_definitions( -DMPI ) #for FAHCORE
+    endif()
+    include(gmxTestMPI_IN_PLACE)
+    if (GMX_MPI_IN_PLACE)
+      gmx_test_mpi_in_place(MPI_IN_PLACE_EXISTS)
+    endif()
+
+    # Test for and warn about unsuitable MPI versions
+    exec_program(ompi_info
+      ARGS -v ompi full
+      OUTPUT_VARIABLE OPENMPI_TYPE
+      RETURN_VALUE OPENMPI_EXEC_RETURN)
+    if(OPENMPI_EXEC_RETURN EQUAL 0)
+      string(REGEX REPLACE ".*Open MPI: \([0-9]+\\.[0-9]*\\.?[0-9]*\).*" "\\1" OPENMPI_VERSION ${OPENMPI_TYPE})
+      if(OPENMPI_VERSION VERSION_LESS "1.4.1")
+        MESSAGE(WARNING "
+             CMake found OpenMPI version ${OPENMPI_VERSION} on your system.
+             There are known problems with GROMACS and OpenMPI version < 1.4.1.
+             Please consider updating your OpenMPI if your MPI wrapper compilers
+             are using the above OpenMPI version.")
+      endif()
+      unset(OPENMPI_VERSION)
+      unset(OPENMPI_TYPE)
+      unset(OPENMPI_EXEC_RETURN)
+    endif()
+    exec_program(mpiname
+      ARGS -n -v
+      OUTPUT_VARIABLE MVAPICH2_TYPE
+      RETURN_VALUE MVAPICH2_EXEC_RETURN)
+    if(MVAPICH2_EXEC_RETURN EQUAL 0)
+      string(REGEX MATCH "MVAPICH2" MVAPICH2_NAME ${MVAPICH2_TYPE})
+      # Want to check for MVAPICH2 in case some other library supplies mpiname
+      string(REGEX REPLACE "MVAPICH2 \([0-9]+\\.[0-9]*[a-z]?\\.?[0-9]*\)" "\\1" MVAPICH2_VERSION ${MVAPICH2_TYPE})
+      if(${MVAPICH2_NAME} STREQUAL "MVAPICH2" AND MVAPICH2_VERSION VERSION_LESS "1.5")
+        # This test works correctly even with 1.5a1
+        MESSAGE(WARNING "
+             CMake found MVAPICH2 version ${MVAPICH2_VERSION} on your system.
+             There are known problems with GROMACS and MVAPICH2 version < 1.5.
+             Please consider updating your MVAPICH2 if your MPI wrapper compilers
+             are using the above MVAPICH2 version.")
+      endif()
+      unset(MVAPICH2_VERSION)
+      unset(MVAPICH2_NAME)
+      unset(MVAPICH2_TYPE)
+      unset(MVAPICH2_EXEC_RETURN)
+    endif()
+
+  else(MPI_FOUND)
+    message(FATAL_ERROR "MPI support requested, but no MPI compiler found.")
+  endif(MPI_FOUND)
+
+  include(gmxTestCatamount)
+  gmx_test_catamount(GMX_CRAY_XT3)
+  if(GMX_CRAY_XT3)
+    set(PKG_CFLAGS "${PKG_CFLAGS} -DGMX_CRAY_XT3")
+    set(GMX_NO_SYSTEM 1)
+    set(GMX_NO_NICE 1)
+  endif(GMX_CRAY_XT3)
+
+  set(GMX_LIB_MPI 1)
+  set(PKG_CFLAGS "${PKG_CFLAGS} -DGMX_LIB_MPI")
+endif(GMX_MPI)

CMakeLists.txt
@@ -192,74 +192,7 @@ endif(GMX_POWERPC_INVSQRT)
 ########################################################################
 #Process MPI settings
 ########################################################################
-include(CheckCSourceCompiles) # for gmxTestMPI_IN_PLACE
-if(GMX_MPI)
-    if(GMX_THREADS)
-        #message(FATAL_ERROR "Thread-based parallelization conflicts with MPI.")
-        set(GMX_THREADS OFF CACHE BOOL 
-            "Thread-based parallelization conflicts with MPI." FORCE)
-    endif(GMX_THREADS)
-
-    # Work-around for Cray MPI
-    TRY_COMPILE(MPI_FOUND ${CMAKE_BINARY_DIR}
-      "${CMAKE_SOURCE_DIR}/cmake/TestMPI.c"
-      COMPILE_DEFINITIONS )
-    if(NOT MPI_FOUND)
-      # search for normal MPI installations
-      find_package(MPI)
-    endif(NOT MPI_FOUND)
-
-    if(MPI_FOUND)
-        set(GROMACS_C_FLAGS ${GROMACS_FLAGS} ${MPI_COMPILE_FLAGS})
-	set(GROMACS_LINKER_FLAGS ${GROMACS_LINKER_FLAGS} ${MPI_LINK_FLAGS})
-        
-        include_directories(${MPI_INCLUDE_PATH})
-        list(APPEND GMX_EXTRA_LIBRARIES ${MPI_LIBRARIES})
-        if(GMX_FAHCORE)
-            add_definitions( -DMPI ) #for FAHCORE
-        endif(GMX_FAHCORE)
-        include(gmxTestMPI_IN_PLACE)
-        if (GMX_MPI_IN_PLACE)
-            gmx_test_mpi_in_place(MPI_IN_PLACE_EXISTS)
-        endif (GMX_MPI_IN_PLACE)
-
-	# test for unsuitable versions of OpenMPI
-	exec_program(${MPIEXEC}
-	  ARGS --version
-	  OUTPUT_VARIABLE MPI_TYPE
-	  RETURN_VALUE MPI_EXEC_RETURN)
-	if(MPI_EXEC_RETURN EQUAL 0)
-	  if(MPI_TYPE MATCHES "Open MPI|OpenRTE")
-	    string(REGEX MATCH "[0-9]+\\.[0-9]*\\.?[0-9]*" MPI_VERSION ${MPI_TYPE})
-	    if(MPI_VERSION VERSION_LESS "1.4.1")
-	      MESSAGE(WARNING "
-            There are known problems with OpenMPI version < 1.4.1.
-            Please consider updating your OpenMPI.")
-	    endif(MPI_VERSION VERSION_LESS "1.4.1")
-	    unset(MPI_VERSION)
-	  else(MPI_TYPE MATCHES "Open MPI|OpenRTE")
-	    # This is not OpenMPI, so give the old generic warning message
-	    MESSAGE(WARNING "
-            There are known problems with some MPI implementations:
-                     MVAPICH2 version <= 1.4.1
-            Please consider updating your MPI if applicable.")
-	  endif(MPI_TYPE MATCHES "Open MPI|OpenRTE")
-	  unset(MPI_TYPE)
-	endif(MPI_EXEC_RETURN EQUAL 0)
-    else(MPI_FOUND)
-        message(FATAL_ERROR "MPI support requested, but no MPI compiler found.")
-    endif(MPI_FOUND)
-    include(gmxTestCatamount)
-    gmx_test_catamount(GMX_CRAY_XT3)
-    if(GMX_CRAY_XT3)
-        set(PKG_CFLAGS "${PKG_CFLAGS} -DGMX_CRAY_XT3")
-        set(GMX_NO_SYSTEM 1)
-        set(GMX_NO_NICE 1)
-    endif(GMX_CRAY_XT3)
-    set(GMX_LIB_MPI 1)
-    set(PKG_CFLAGS "${PKG_CFLAGS} -DGMX_LIB_MPI")
-endif(GMX_MPI)
-
+include(gmxManageMPI)
 
 #######################################################################
 # Check for options incompatible with OpenMM build                    #

admin/GerritBuild
@@ -3,9 +3,18 @@ if [ -n "${CMakeVersion}" ] ; then
    export PATH=$HOME/tools/cmake-${CMakeVersion}/bin:$PATH
 fi
 export CMAKE_LIBRARY_PATH=/usr/lib/atlas-base
-export CC=gcc-${CompilerVersion} 
-export CXX=g++-${CompilerVersion} 
-export FC=gfortran-${CompilerVersion} 
+if [ $GMX_MPI = on ]; then
+    export OMPI_CC=gcc-${CompilerVersion}
+    export OMPI_CXX=g++-${CompilerVersion}
+    export OMPI_FC=gfortran-${CompilerVersion}
+    export CC=mpicc
+    export CXX=mpic++
+    export FC=mpif90
+else
+    export CC=gcc-${CompilerVersion}
+    export CXX=g++-${CompilerVersion}
+    export FC=gfortran-${CompilerVersion}
+fi
 cmake -D GMX_DOUBLE=${GMX_DOUBLE} -D GMX_MPI=${GMX_MPI} -D GMX_OPENMP=${GMX_OPENMP} \
     -DGMX_EXTERNAL_BLAS=${GMX_EXTERNAL} -DGMX_EXTERNAL_LAPACK=${GMX_EXTERNAL} \
     -DGMX_DEFAULT_SUFFIX=off -DCMAKE_BUILD_TYPE=Debug . &&

cmake/gmxTestMPI_IN_PLACE.cmake
@@ -5,6 +5,7 @@
 #  VARIABLE will be set to true if MPI_IN_PLACE exists
 #
 
+include(CheckCSourceCompiles)
 MACRO(GMX_TEST_MPI_IN_PLACE VARIABLE)
     MESSAGE(STATUS "Checking for MPI_IN_PLACE")
 

commit 52b70307c6baf05de71a6f8967333ae3cdb9b3ce

Fix MPI build for MPI library without mpi wrapper
Some MPI library (e.g. MPICH and OpenMPI on Windows) don't
contain any MPI wrappers and thus 417d0afff broke the support for
those MPI libraries.

This change uses the FindMPI module if the compiler is not a
mpi wrapper and cmake >= 2.8.5. Requiring mpi wrappers for
cmake < 2.8.5 avoids reintroducing the problems of #851.

To avoid any confusion of different behavior with older and newer
versions of cmake, all documentation should recommend the mpi wrapper
approach, which works for all versions. The FindMPI approach should
only be discussed for advanced users (to support e.g. Windows).

As a side effect this change makes it more convinient to use with
cmake>=2.8.5. No need to speciy mpi wrapper and less problems with
nvcc.

Fixes #958

Change-Id: Ic53d8125c5a58edc6789fe16f2b710e7e2568d4f

cmake/gmxManageMPI.cmake
@@ -12,6 +12,20 @@ if(GMX_MPI)
     "${CMAKE_SOURCE_DIR}/cmake/TestMPI.c"
     COMPILE_DEFINITIONS )
 
+  # If CMAKE_C_COMPILER is not a MPI wrapper. Try to find MPI using cmake module as fall-back.
+  # MPI module from cmake >= 2.8.5 required for fall-back. Cmake <2.8.5 have to use mpi wrapper.
+  if(NOT MPI_FOUND AND NOT CMAKE_VERSION VERSION_LESS "2.8.5")
+      find_package(MPI)
+      if(MPI_C_FOUND) #false for cmake<2.8.5 because it does not have MPI_C_* variables
+	set(GROMACS_C_FLAGS ${GROMACS_FLAGS} ${MPI_C_COMPILE_FLAGS})
+	set(GROMACS_LINKER_FLAGS ${GROMACS_LINKER_FLAGS} ${MPI_C_LINK_FLAGS})
+        
+	include_directories(${MPI_C_INCLUDE_PATH})
+	list(APPEND GMX_EXTRA_LIBRARIES ${MPI_C_LIBRARIES})
+      endif()
+      set(MPI_FOUND ${MPI_C_FOUND})
+  endif()
+
   if(MPI_FOUND)
     include(gmxTestMPI_IN_PLACE)
     if (GMX_MPI_IN_PLACE)