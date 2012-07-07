8c87e60d8b91764b730452e16365e3c2862f67a3
Roland Schulz <roland@utk.edu>
2012-07-06 23:25:08 -0400

    Autogenerate gromacs.7 from programs.txt
    Change-Id: I958906dc9967155bccd3f43e5a6882632a829951

--- cmake/BuildManPages.cmake
+++ cmake/BuildManPages.cmake
@@ -1,7 +1,42 @@
+MACRO(TODAY RESULT)
+    IF(UNIX)
+        EXECUTE_PROCESS(COMMAND "date" "+%F" OUTPUT_VARIABLE ${RESULT} OUTPUT_STRIP_TRAILING_WHITESPACE)
+    ELSE()
+        set(${RESULT} "????-??-??")
+    ENDIF()
+ENDMACRO(TODAY)
+
 if(GMX_BUILD_MANPAGES)
     set(MAN1_PATH ${CMAKE_BINARY_DIR}/man/man1)
     file(MAKE_DIRECTORY ${MAN1_PATH})
+
+    #create gromacs.7
+    FILE(READ "${CMAKE_SOURCE_DIR}/admin/programs.txt" contents)
+
+    # Convert file contents into a CMake list. First escape ;
+    STRING(REGEX REPLACE ";" "\\\\;" contents "${contents}")
+    STRING(REGEX REPLACE "\n" ";" contents "${contents}")
+
+    set(PROGMANPAGES "")
+    foreach(line ${contents})
+        if (${line} MATCHES "^HEAD\\|")
+            string(REGEX REPLACE "^HEAD\\|" "" DESC ${line})
+            set(PROGMANPAGES "${PROGMANPAGES}.Sh \"${DESC}\"\n.IX Subsection \"${DESC}\"\n.Vb\n.ta 16n\n")
+        elseif(${line} MATCHES "^END$")
+            set(PROGMANPAGES "${PROGMANPAGES}.Ve\n")
+        elseif(${line} MATCHES "\\|")
+            string(REGEX REPLACE "\\|" "\t" line ${line})
+            set(PROGMANPAGES "${PROGMANPAGES}\\&  ${line}\n")
+        else()
+            message(WARNING "Incorrectly formated line \"${line}\" in programs.txt")
+        endif()
+    endforeach()
+    TODAY(TODAYS_DATE)
+    configure_file(${CMAKE_SOURCE_DIR}/man/man7/gromacs.7.cmakein ${CMAKE_BINARY_DIR}/man/man7/gromacs.7)
+    install(FILES ${CMAKE_BINARY_DIR}/man/man7/gromacs.7 DESTINATION
+        ${MAN_INSTALL_DIR}/man7)
 endif()
+
 function (gmx_add_man_page EXENAME)
     if(GMX_BUILD_MANPAGES)
         file(STRINGS ${CMAKE_SOURCE_DIR}/admin/programs.txt DESC 
