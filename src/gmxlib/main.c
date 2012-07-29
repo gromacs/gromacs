gromacs/gromacs

commit babdd87ca12628c62479bf118118c879c1cd9b69

now mdrun always initializes MPI
MPI was only initialized with more than one process. This required
checks everywhere. Now MPI is also initialized with one process.

Change-Id: I4d0f543d5c1ae5a6136a0ba34f0f83ac27604b14

src/gmxlib/main.c
@@ -89,50 +89,6 @@ gmx_ctime_r(const time_t *clock,char *buf, int n);
 
 #define BUFSIZE	1024
 
-/* this is not strictly thread-safe, but it's only written to at the beginning
-   of the simulation, once by each thread with the same value. We assume
-   that writing to an int is atomic.*/
-static gmx_bool parallel_env_val;
-#ifdef GMX_THREAD_MPI
-tMPI_Thread_mutex_t parallel_env_mutex=TMPI_THREAD_MUTEX_INITIALIZER;
-#endif
-
-
-/* returns 1 when running in a parallel environment, so could also be 1 if
-   mdrun was started with: mpirun -np 1.
-     
-   Use this function only to check whether a parallel environment has   
-   been initialized, for example when checking whether gmx_finalize()   
-   needs to be called. Use PAR(cr) to check whether the simulation actually
-   has more than one node/thread.  */
-gmx_bool gmx_parallel_env_initialized(void)
-{
-    gmx_bool ret;
-#ifdef GMX_THREAD_MPI
-    tMPI_Thread_mutex_lock(&parallel_env_mutex);
-#endif
-    ret=parallel_env_val;
-#ifdef GMX_THREAD_MPI
-    tMPI_Thread_mutex_unlock(&parallel_env_mutex);
-#endif
-    return ret;
-}
-
-static void set_parallel_env(gmx_bool val)
-{
-#ifdef GMX_THREAD_MPI
-    tMPI_Thread_mutex_lock(&parallel_env_mutex);
-#endif
-    if (!parallel_env_val)
-    {
-        /* we only allow it to be set, not unset */
-        parallel_env_val=val;
-    }
-#ifdef GMX_THREAD_MPI
-    tMPI_Thread_mutex_unlock(&parallel_env_mutex);
-#endif
-}
-
 
 static void par_fn(char *base,int ftp,const t_commrec *cr,
 		   gmx_bool bAppendSimId,gmx_bool bAppendNodeId,
@@ -527,39 +483,24 @@ t_commrec *init_par(int *argc,char ***argv_ptr)
 
     argv = *argv_ptr;
 
-#ifdef GMX_MPI
-#ifdef GMX_LIB_MPI
-    pe = TRUE;
-#ifdef GMX_CHECK_MPI_ENV
-    /* Do not use MPI calls when env.var. GMX_CHECK_MPI_ENV is not set */
-    if (getenv(GMX_CHECK_MPI_ENV) == NULL)
-        pe = FALSE;
-#endif /* GMX_CHECK_MPI_ENV */
-#endif /* GMX_LIB_MPI  */
-    set_parallel_env(pe);
-    if (pe) {
-        cr->sim_nodeid = gmx_setup(argc,argv,&cr->nnodes);
-    } else {
-        cr->nnodes     = 1;
-        cr->sim_nodeid = 0;
-    }
-#else /* GMX_MPI */
-    pe=FALSE;
-    set_parallel_env(pe);
-    cr->sim_nodeid   = 0;
-    cr->nnodes       = 1;
-#endif /* GMX_MPI */
+#if defined GMX_MPI && !defined GMX_THREAD_MPI
+    cr->sim_nodeid = gmx_setup(argc,argv,&cr->nnodes);
 
     if (!PAR(cr) && (cr->sim_nodeid != 0))
-        gmx_comm("(!PAR(cr) && (cr->sim_nodeid != 0))");
-
-    if (PAR(cr)) 
     {
-#ifdef GMX_MPI
-        cr->mpi_comm_mysim = MPI_COMM_WORLD;
-        cr->mpi_comm_mygroup = cr->mpi_comm_mysim;
-#endif /* GMX_MPI */
+        gmx_comm("(!PAR(cr) && (cr->sim_nodeid != 0))");
     }
+
+    cr->mpi_comm_mysim   = MPI_COMM_WORLD;
+    cr->mpi_comm_mygroup = cr->mpi_comm_mysim;
+#else
+    /* These should never be accessed */
+    cr->mpi_comm_mysim   = NULL;
+    cr->mpi_comm_mygroup = NULL;
+    cr->nnodes           = 1;
+    cr->sim_nodeid       = 0;
+#endif
+
     cr->nodeid = cr->sim_nodeid;
 
     cr->duty = (DUTY_PP | DUTY_PME);
@@ -567,7 +508,9 @@ t_commrec *init_par(int *argc,char ***argv_ptr)
     /* Communicate arguments if parallel */
 #ifndef GMX_THREAD_MPI
     if (PAR(cr))
+    {
         comm_args(cr,argc,argv_ptr);
+    }
 #endif /* GMX_THREAD_MPI */
 
 #ifdef GMX_MPI
@@ -603,8 +546,9 @@ t_commrec *init_par_threads(const t_commrec *cro)
     /* and we start setting our own thread-specific values for things */
     MPI_Initialized(&initialized);
     if (!initialized)
+    {
         gmx_comm("Initializing threads without comm");
-    set_parallel_env(TRUE);
+    }
     /* once threads will be used together with MPI, we'll
        fill the cr structure with distinct data here. This might even work: */
     cr->sim_nodeid = gmx_setup(0,NULL, &cr->nnodes);
@@ -618,21 +562,4 @@ t_commrec *init_par_threads(const t_commrec *cro)
 #else
     return NULL;
 #endif
-}
-
-
-t_commrec *init_cr_nopar(void)
-{
-    t_commrec *cr;
-
-    snew(cr,1);
-
-    cr->nnodes     = 1; 
-    /* cr->nthreads   = 1; */
-    cr->sim_nodeid = 0;
-    cr->nodeid     = 0;
-    /* cr->threadid   = 0; */
-    cr->duty       = (DUTY_PP | DUTY_PME);
-
-    return cr;
 }

src/tools/calcpot.c
@@ -229,7 +229,7 @@ FILE *init_calcpot(const char *log,const char *tpx,const char *table,
   FILE     *fplog;
   
   /* Initiate */
-  *cr = init_cr_nopar();
+  *cr = init_par(NULL,NULL);
   gmx_log_open(log,*cr,FALSE,0,&fplog);
 
   init_nrnb(&nrnb);

include/main.h
@@ -44,16 +44,6 @@
 extern "C" {
 #endif
 
-gmx_bool gmx_parallel_env_initialized(void); 
-/* 1 when running in a parallel environment, so could also be 1 if
-   mdrun was started with: mpirun -np 1.
-  
-   Use this function only to check whether a parallel environment has
-   been initialized, for example when checking whether gmx_finalize()
-   needs to be called. Use PAR(cr) to check whether the simulation actually
-   has more than one node/thread. */
-
-
 void gmx_log_open(const char *fn,const t_commrec *cr,
                           gmx_bool bMasterOnly, unsigned long Flags, FILE**);
 /* Open the log file, if necessary (nprocs > 1) the logfile name is
@@ -85,7 +75,7 @@ t_commrec *init_par(int *argc,char ***argv_ptr);
  * (see network.h). The command line arguments are communicated so that they can be
  * parsed on each processor.
  * Arguments are the number of command line arguments, and a pointer to the
- * array of argument strings.
+ * array of argument strings. Both are allowed to be NULL.
  */
 
 t_commrec *init_par_threads(const t_commrec *cro);
@@ -94,9 +84,6 @@ t_commrec *init_par_threads(const t_commrec *cro);
    the individual threads. Copies the original commrec to 
    thread-local versions (a small memory leak results because we don't 
    deallocate the old shared version).  */
-
-t_commrec *init_cr_nopar(void);
-/* Returns t_commrec for non-parallel functionality */
 
 #ifdef __cplusplus
 }

include/network.h
@@ -118,8 +118,7 @@ void gmx_sumd_sim(int nr,double r[],const gmx_multisim_t *ms);
 void gmx_abort(int nodeid,int nnodes,int errorno);
 /* Abort the parallel run */
 
-void gmx_finalize(void);
-
+void gmx_finalize_par(void);
 /* Finish the parallel run in an ordered manner */
 
 #ifdef GMX_DOUBLE

src/gmxlib/gmx_fatal.c
@@ -216,7 +216,6 @@ static void quit_gmx(const char *msg)
     }
 
 #ifndef GMX_THREAD_MPI
-    if (gmx_parallel_env_initialized())
     {
         int  nnodes;
         int  noderank;
@@ -225,20 +224,25 @@ static void quit_gmx(const char *msg)
         noderank = gmx_node_rank();
 
         if (nnodes > 1) 
+        {
             fprintf(stderr,"Error on node %d, will try to stop all the nodes\n",
                     noderank);
+        }
         gmx_abort(noderank,nnodes,-1);
     } 
-    else 
 #endif
+
+    if (debug)
     {
-        if (debug)
-            fflush(debug);
-        if (bDebugMode()) {
-            fprintf(stderr,"dump core (y/n):"); 
-            fflush(stderr);
-            if (toupper(getc(stdin))!='N') 
-                (void) abort(); 
+        fflush(debug);
+    }
+    if (bDebugMode())
+    {
+        fprintf(stderr,"dump core (y/n):"); 
+        fflush(stderr);
+        if (toupper(getc(stdin))!='N') 
+        {
+            (void) abort();
         }
     }
 
@@ -272,17 +276,21 @@ static void quit_gmx_noquit(const char *msg)
         perror(msg);
     }
 
-    if (!gmx_parallel_env_initialized())
+#ifdef GMX_THREAD_MPI
+    if (debug)
     {
-        if (debug)
-            fflush(debug);
-        if (bDebugMode()) {
-            fprintf(stderr,"dump core (y/n):"); 
-            fflush(stderr);
-            if (toupper(getc(stdin))!='N') 
-                (void) abort(); 
+        fflush(debug);
+    }
+    if (bDebugMode())
+    {
+        fprintf(stderr,"dump core (y/n):"); 
+        fflush(stderr);
+        if (toupper(getc(stdin))!='N') 
+        {
+            (void) abort(); 
         }
     }
+#endif
 
 #ifdef GMX_THREAD_MPI
     tMPI_Thread_mutex_unlock(&debug_mutex);
@@ -454,20 +462,17 @@ void gmx_fatal_collective(int f_errno,const char *file,int line,
     bFinalize = TRUE;
 
 #ifdef GMX_MPI
-    if (gmx_parallel_env_initialized())
+    /* Check if we are calling on all processes in MPI_COMM_WORLD */ 
+    if (cr != NULL)
     {
-        /* Check if we are calling on all processes in MPI_COMM_WORLD */ 
-        if (cr != NULL)
-        {
-            MPI_Comm_compare(cr->mpi_comm_mysim,MPI_COMM_WORLD,&result);
-        }
-        else
-        {
-            MPI_Comm_compare(dd->mpi_comm_all,MPI_COMM_WORLD,&result);
-        }
-        /* Any result except MPI_UNEQUAL allows us to call MPI_Finalize */
-        bFinalize = (result != MPI_UNEQUAL);
+        MPI_Comm_compare(cr->mpi_comm_mysim,MPI_COMM_WORLD,&result);
     }
+    else
+    {
+        MPI_Comm_compare(dd->mpi_comm_all,MPI_COMM_WORLD,&result);
+    }
+    /* Any result except MPI_UNEQUAL allows us to call MPI_Finalize */
+    bFinalize = (result != MPI_UNEQUAL);
 #endif
 
     if ((cr != NULL && MASTER(cr)  ) ||
@@ -501,28 +506,25 @@ void gmx_fatal_collective(int f_errno,const char *file,int line,
     }
 
 #ifdef GMX_MPI
-    if (gmx_parallel_env_initialized())
+    if (bFinalize)
     {
-        if (bFinalize)
-        {
-            /* Broadcast the fatal error number possibly modified
-             * on the master process, in case the user would like
-             * to use the return status on a non-master process.
-             * The master process in cr and dd always has global rank 0.
-             */
-            MPI_Bcast(&fatal_errno,sizeof(fatal_errno),MPI_BYTE,
-                      0,MPI_COMM_WORLD);
+        /* Broadcast the fatal error number possibly modified
+         * on the master process, in case the user would like
+         * to use the return status on a non-master process.
+         * The master process in cr and dd always has global rank 0.
+         */
+        MPI_Bcast(&fatal_errno,sizeof(fatal_errno),MPI_BYTE,
+                  0,MPI_COMM_WORLD);
 
-            /* Finalize nicely instead of aborting */
-            MPI_Finalize();
-        }
-        else
-        {
-            /* Let all other processes wait till the master has printed
-             * the error message and issued MPI_Abort.
-             */
-            MPI_Barrier(MPI_COMM_WORLD);
-        }
+        /* Finalize nicely instead of aborting */
+        MPI_Finalize();
+    }
+    else
+    {
+        /* Let all other processes wait till the master has printed
+         * the error message and issued MPI_Abort.
+         */
+        MPI_Barrier(MPI_COMM_WORLD);
     }
 #endif
 

src/gmxlib/network.c
@@ -738,18 +738,26 @@ void gmx_sumli_sim(int nr,gmx_large_int_t r[], const gmx_multisim_t *ms)
 }
 
 
-void gmx_finalize(void)
+void gmx_finalize_par(void)
 {
 #ifndef GMX_MPI
-  gmx_call("gmx_finalize");
+    /* Compiled without MPI, no MPI finalizing needed */
+    return;
 #else
-  int ret;
+    int initialized,finalized;
+    int ret;
 
-  /* just as a check; we don't want to finalize twice */
-  int finalized;
-  MPI_Finalized(&finalized);
-  if (finalized)
-      return;
+    MPI_Initialized(&initialized);
+    if (!initialized)
+    {
+        return;
+    }
+    /* just as a check; we don't want to finalize twice */
+    MPI_Finalized(&finalized);
+    if (finalized)
+    {
+	return;
+    }
 
   /* We sync the processes here to try to avoid problems
    * with buggy MPI implementations that could cause

src/gmxlib/statutil.c
@@ -821,10 +821,10 @@ void parse_common_args(int *argc,char *argv[],unsigned long Flags,
             gmx_cmd(argv[1]);
         }
     } 
-    if (bExit) {
-        if (gmx_parallel_env_initialized())
-            /*gmx_abort(gmx_node_rank(),gmx_node_num(),0);*/
-            gmx_finalize();
+    if (bExit)
+    {
+        gmx_finalize_par();
+
         exit(0);
     }
 #undef FF

src/kernel/md.c
@@ -923,11 +923,10 @@ double do_md(FILE *fplog,t_commrec *cr,int nfile,const t_filenm fnm[],
         {
             if (update_forcefield(fplog,
                                   nfile,fnm,fr,
-                                  mdatoms->nr,state->x,state->box)) {
-                if (gmx_parallel_env_initialized())
-                {
-                    gmx_finalize();
-                }
+                                  mdatoms->nr,state->x,state->box))
+            {
+                gmx_finalize_par();
+
                 exit(0);
             }
         }
@@ -1614,10 +1613,8 @@ double do_md(FILE *fplog,t_commrec *cr,int nfile,const t_filenm fnm[],
                                  f,NULL,xcopy,
                                  &(top_global->mols),mdatoms->massT,pres))
             {
-                if (gmx_parallel_env_initialized())
-                {
-                    gmx_finalize();
-                }
+                gmx_finalize_par();
+
                 fprintf(stderr,"\n");
                 exit(0);
             }

commit 17307b97663f01cdf7cd1faf0d5e445a3fde5f78

Fix Segfaults in Genion
Introduced by babdd87c and c7a82654f

Change-Id: Id42042966931f3b7dfbd2eebcde29d659f80c0f0
Fixes: 982

src/gmxlib/main.c
@@ -487,7 +487,7 @@ t_commrec *init_par(int *argc,char ***argv_ptr)
 
     snew(cr,1);
 
-    argv = *argv_ptr;
+    argv = argv_ptr ? *argv_ptr : NULL;
 
 #if defined GMX_MPI && !defined GMX_THREAD_MPI
     cr->sim_nodeid = gmx_setup(argc,argv,&cr->nnodes);

src/tools/calcpot.c
@@ -215,8 +215,8 @@ FILE *init_calcpot(const char *log,const char *tpx,const char *table,
 		   matrix box,rvec **x, const output_env_t oenv)
 {
   gmx_localtop_t *ltop;
-  double   t,t0,lam0;
-  real     lam;
+  double   t,t0;
+  real     lam[efptNR];
   int      fep_state;
   gmx_bool     bNEMD,bSA;
   int      traj=0,xtc_traj=0;
@@ -247,7 +247,7 @@ FILE *init_calcpot(const char *log,const char *tpx,const char *table,
   }
 
   clear_rvec(mutot);
-  init_md(fplog,*cr,inputrec,oenv,&t,&t0,&lam,&fep_state,&lam0,
+  init_md(fplog,*cr,inputrec,oenv,&t,&t0,lam,&fep_state,NULL,
 	  &nrnb,mtop,NULL,-1,NULL,NULL,NULL,
 	  force_vir,shake_vir,mutot,&bSA,NULL,NULL,0);
 