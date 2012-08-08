c20969c3a1e158da9e03ebec0cc19ae933922e8a
Teemu Murtola <teemu.murtola@gmail.com>
2012-08-08 07:02:38 +0300

    Improve the build system for gmx_header_config.h.
    - Configure gmx_header_config.h in the binary directory to make it work
      with multiple coexisting binary directories.
    - Or rather, configure a gmx_header_config_gen.h there, and include it
      using a relative path from a fixed gmx_header_config.h.  Add location
      of the generated file to include path.  This makes relative include
      paths still work in the source, as headers can continue to include
      gmx_header_config.h, which is still in the source tree.
    - Add Doxygen comments for the files, with an explicit todo item to try
      to get rid of the CMake-generated defines (or even better, all of
      them).
    - Make config.h include gmx_header_config.h to make it easier (and less
      error-prone) to move stuff between these two headers.  Remove
      unnecessary (after this change) #includes for gmx_header_config.h
      from source files.
    
    Fixes #991.
    
    Change-Id: Ib9686356c681b8fd2cce34511557566c9f4d20a2

---
+++ src/gromacs/utility/gmx_header_config.h
@@ -0,0 +1,18 @@
+/*! \brief
+ * Include file for configuration macros that affect installed headers.
+ *
+ * This include file (or rather, one that it includes) will configured by CMake
+ * and installed with GROMACS header files so that they can refer to a central
+ * location for #defines that will be available for builds of projects that
+ * depend on GROMACS.
+ *
+ * The actual defines are in gmx_header_config_gen.h to allow usage of relative
+ * include paths before installation.
+ *
+ * \todo
+ * It would be better to have the defines here such that they are not generated
+ * from CMake, but instead detected using #ifdefs (possible for some of the
+ * macros currently used).
+ * Even better would be to not have these defines at all.
+ */
+#include "gmx_header_config_gen.h"
