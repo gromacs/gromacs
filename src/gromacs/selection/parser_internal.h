85b98012a69510a37fb021ea7370fdaef469ef49
Teemu Murtola <teemu.murtola@gmail.com>
2012-08-17 14:13:40 +0300

    C++ classes for selection parser parameters.
    Converted t_selexpr_param to gmx::SelectionParserParameter and
    introduced smart pointers and standard containers to manage all memory
    related to these objects.  Solves several exception safety issues in the
    selection parser.
    
    Related to #655 and #880.
    
    Change-Id: Ia4c477f1313b49cd1da3b0407bec6e7a1a8b9296

--- src/gromacs/selection/parser_internal.h
+++ src/gromacs/selection/parser_internal.h
@@ -49,25 +49,17 @@
 
 #include "scanner.h"
 
-//! Helper method to reorder a list of parameter values and to count the values.
+//! Helper method to reorder a list of parameter values.
 static t_selexpr_value *
-process_value_list(t_selexpr_value *values, int *nr)
+process_value_list(t_selexpr_value *values)
 {
     t_selexpr_value *val, *pval, *nval;
 
     /* Count values (if needed) and reverse list */
-    if (nr)
-    {
-        *nr  = 0;
-    }
     pval = NULL;
     val  = values;
     while (val)
     {
-        if (nr)
-        {
-            ++*nr;
-        }
         nval = val->next;
         val->next = pval;
         pval = val;
@@ -76,27 +68,6 @@ process_value_list(t_selexpr_value *values, int *nr)
     values = pval;
 
     return values;
-}
-
-//! Helper method to reorder a list of parameters.
-static t_selexpr_param *
-process_param_list(t_selexpr_param *params)
-{
-    t_selexpr_param *par, *ppar, *npar;
-
-    /* Reverse list */
-    ppar = NULL;
-    par  = params;
-    while (par)
-    {
-        npar = par->next;
-        par->next = ppar;
-        ppar = par;
-        par = npar;
-    }
-    params = ppar;
-
-    return params;
 }
 
 //! Error handler needed by Bison.
@@ -134,45 +105,44 @@ yyerror(yyscan_t scanner, char const *s)
 //!\}
 
 /*! \brief
- * Retrieves a selection tree pointer from a semantic value.
+ * Retrieves a smart pointer from a semantic value.
  *
- * \param[in] src  Semantic value to get the tree from.
- * \returns   Pointer to the selection tree.
+ * \param[in] src  Semantic value to get the pointer from.
+ * \returns   Retrieved smart pointer.
  *
  * There should be no statements that may throw exceptions in actions before
- * this function has been called for all semantic values that have a tree
- * argument.  Together with set(), this function abstracts away exception
+ * this function has been called for all semantic values that have a smart
+ * pointer stored.  Together with set(), this function abstracts away exception
  * safety issues that arise from the use of a plain pointer for storing the
- * selection tree semantic values.
+ * semantic values.
  *
  * Does not throw.
  */
-static gmx::SelectionTreeElementPointer
-get(gmx::SelectionTreeElementPointer *src)
+template <typename PointerType> static
+PointerType get(PointerType *src)
 {
-    gmx::SelectionTreeElementPointer result;
+    PointerType result;
     if (src != NULL)
     {
         result.swap(*src);
         delete src;
     }
-    return result;
+    return move(result);
 }
 /*! \brief
- * Sets a selection tree pointer to a semantic value.
+ * Sets a smart pointer to a semantic value.
  *
- * \param[out] dest  Semantic value to set the tree to.
- * \param[in]  value Pointer to the selection tree to set.
+ * \param[out] dest  Semantic value to set (typically $$).
+ * \param[in]  value Pointer to put into the semantic value.
  * \throws     std::bad_alloc if out of memory.
  *
  * This should be the last statement before ::END_ACTION, except for a
  * possible ::CHECK_SEL.
  */
-static void
-set(gmx::SelectionTreeElementPointer *&dest,
-        const gmx::SelectionTreeElementPointer &value)
+template <typename PointerType> static
+void set(PointerType *&dest, PointerType value)
 {
-    dest = new gmx::SelectionTreeElementPointer(value);
+    dest = new PointerType(move(value));
 }
 /*! \brief
  * Checks that a valid tree was set.
