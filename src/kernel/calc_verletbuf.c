gromacs/gromacs

commit 1bbfc29327012680d79acaeee9a7cf5782badc29

added Verlet rlist recalculation in mdrun

src/kernel/calc_verletbuf.c
@@ -64,18 +64,30 @@ typedef struct
 } verletbuf_atomtype_t;
 
 
-void verletbuf_get_list_setup(verletbuf_list_setup_t *list_setup)
+void verletbuf_get_list_setup(gmx_bool bGPU,
+                              verletbuf_list_setup_t *list_setup)
 {
-    list_setup->cluster_size_i = NBNXN_CPU_CLUSTER_I_SIZE;
+    list_setup->cluster_size_i     = NBNXN_CPU_CLUSTER_I_SIZE;
+
+    if (bGPU)
+    {
+        list_setup->cluster_size_j = NBNXN_GPU_CLUSTER_SIZE;
+    }
+    else
+    {
 #ifndef GMX_X86_SSE2
-    list_setup->cluster_size_j = NBNXN_CPU_CLUSTER_I_SIZE;
+        list_setup->cluster_size_j = NBNXN_CPU_CLUSTER_I_SIZE;
 #else
-#ifndef GMX_DOUBLE
-    list_setup->cluster_size_j = 4;
+        int simd_width;
+
+#ifdef GMX_X86_AVX_256
+        simd_width = 256;
 #else
-    list_setup->cluster_size_j = 2;
+        simd_width = 128;
 #endif
+        list_setup->cluster_size_j = simd_width/(sizeof(real)*8);
 #endif
+    }
 }
 
 static void add_at(verletbuf_atomtype_t **att_p,int *natt_p,
@@ -436,6 +448,8 @@ static real surface_frac(int cluster_size,real particle_distance,real rlist)
         area_rel = 1.0 + d;
         break;
     case 4:
+    case 8:
+        /* We don't have a formula for 8 (yet), use 4 which is conservative */
         /* We assume a perfect, symmetric tetrahedron geometry.
          * The surface around a tetrahedron is too complex for a full
          * analytical solution, so we use a Taylor expansion.

src/kernel/calc_verletbuf.h
@@ -49,7 +49,8 @@ typedef struct
  * The setup with smallest cluster sizes is return, such that the Verlet
  * buffer size estimated with this setup will be conservative.
  */
-void verletbuf_get_list_setup(verletbuf_list_setup_t *list_setup);
+void verletbuf_get_list_setup(gmx_bool bGPU,
+			      verletbuf_list_setup_t *list_setup);
 
 
 /* Calculate the non-bonded pair-list buffer size for the Verlet list

src/kernel/grompp.c
@@ -1174,7 +1174,7 @@ static void set_verlet_buffer(const gmx_mtop_t *mtop,
                             &ls,&n_nonlin_vsite,&rlist_1x1);
 
     /* Set the pair-list buffer size in ir */
-    verletbuf_get_list_setup(&ls);
+    verletbuf_get_list_setup(FALSE,&ls);
     calc_verlet_buffer_size(mtop,det(box),ir,verletbuf_drift,
                             &ls,&n_nonlin_vsite,&ir->rlist);
 

src/kernel/runner.c
@@ -464,7 +464,7 @@ static void increase_nstlist(FILE *fp,t_commrec *cr,
         sscanf(env,"%d",&ir->nstlist);
     }
 
-    verletbuf_get_list_setup(&ls);
+    verletbuf_get_list_setup(TRUE,&ls);
 
     /* Allow rlist to make the list double the size of the cut-off sphere */
     rlist_inc = nbnxn_rlist_inc(NBNXN_GPU_CLUSTER_SIZE,mtop->natoms/det(box));
@@ -599,7 +599,7 @@ static void convert_to_verlet_scheme(FILE *fplog,
     {
         verletbuf_list_setup_t ls;
 
-        verletbuf_get_list_setup(&ls);
+        verletbuf_get_list_setup(FALSE,&ls);
         calc_verlet_buffer_size(mtop,box_vol,ir,ir->verletbuf_drift,&ls,
                                 NULL,&ir->rlist);
     }
@@ -1106,9 +1106,37 @@ int mdrunner(int nthreads_requested, FILE *fplog,t_commrec *cr,int nfile,
          * -- uncool, but there's no elegant workaround */
         gmx_bool bGPU = hwinfo->bCanUseGPU || (getenv("GMX_EMULATE_GPU") != NULL);
 
+        /* Check if we need to update rlist for the current run setup */
+        if (inputrec->verletbuf_drift > 0)
+        {
+            verletbuf_list_setup_t ls;
+            real rlist_new;
+
+            /* Here we assume CPU acceleration is on. But as currently
+             * calc_verlet_buffer_size gives the same results for 4x8 and 4x4
+             * and 4x2 gives a large buffer than 4x4, this is ok.
+             */
+            verletbuf_get_list_setup(bGPU,&ls);
+            
+            calc_verlet_buffer_size(mtop,det(box),inputrec,
+                                    inputrec->verletbuf_drift,&ls,
+                                    NULL,&rlist_new);
+            if (rlist_new != inputrec->rlist)
+            {
+                if (fplog != NULL)
+                {
+                    fprintf(fplog,"\nChanging rlist from %g to %g for non-bonded %dx%d atom kernels\n\n",
+                            inputrec->rlist,rlist_new,
+                            ls.cluster_size_i,ls.cluster_size_j);
+                }
+                inputrec->rlist     = rlist_new;
+                inputrec->rlistlong = rlist_new;
+            }
+        }
+
         if (bGPU)
         {
-            /* With GPUs disable PME nodes when the user request any */
+            /* With GPUs disable PME nodes when set to auto */
             if (cr->npmenodes < 0)
             {
                 cr->npmenodes = 0;

commit 06badaddd77415b1be70bb650cf56c87a7afea5c

fixed 4x8 rlist bug, introduced in 1bbfc293

src/kernel/calc_verletbuf.c
@@ -448,8 +448,6 @@ static real surface_frac(int cluster_size,real particle_distance,real rlist)
         area_rel = 1.0 + d;
         break;
     case 4:
-    case 8:
-        /* We don't have a formula for 8 (yet), use 4 which is conservative */
         /* We assume a perfect, symmetric tetrahedron geometry.
          * The surface around a tetrahedron is too complex for a full
          * analytical solution, so we use a Taylor expansion.
@@ -681,9 +679,12 @@ void calc_verlet_buffer_size(const gmx_mtop_t *mtop,real boxvol,
         /* Correct for the fact that we are using a Ni x Nj particle pair list
          * and not a 1 x 1 particle pair list. This reduces the drift.
          */
+        /* We don't have a formula for 8 (yet), use 4 which is conservative */
         nb_clust_frac_pairs_not_in_list_at_cutoff =
-            surface_frac(list_setup->cluster_size_i,particle_distance,rl)*
-            surface_frac(list_setup->cluster_size_j,particle_distance,rl);
+            surface_frac(min(list_setup->cluster_size_i,4),
+                         particle_distance,rl)*
+            surface_frac(min(list_setup->cluster_size_j,4),
+                         particle_distance,rl);
         drift *= nb_clust_frac_pairs_not_in_list_at_cutoff;
 
         /* Convert the drift to drift per unit time per atom */