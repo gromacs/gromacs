b68cc9803cc8ef5384e760256331b8dd41cd4617
Teemu Murtola <teemu.murtola@gmail.com>
2012-08-28 20:32:55 +0300

    [RFC] Add subsection depth to HelpWriterContext.
    Necessary for #969 for output formats where section titles need to be
    formatted differently at different depths.  Not yet used anywhere, but
    split into a separate commit to keep the main commit as simple as
    possible.  RFC because this may not be necessary, depending on what we
    decide to do with #969.
    
    Also reorganized the way help topic titles are written: HelpManager is
    now responsible for writing out the title for a topic.  This makes
    things behave the same as with HelpWriterContext::createSubsection().
    
    Part of #969.
    
    Change-Id: If7e0ee6d4c9dd313e1523fa457f64cf3c4f891f9

--- src/gromacs/selection/selhelp.cpp
+++ src/gromacs/selection/selhelp.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2009,2010,2011,2012, by the GROMACS development team, led by
+ * Copyright (c) 2009,2010,2011,2012,2013, by the GROMACS development team, led by
  * David van der Spoel, Berk Hess, Erik Lindahl, and including many
  * others, as listed in the AUTHORS file in the top-level source
  * directory and at http://www.gromacs.org.
@@ -544,7 +544,7 @@ void KeywordsHelpTopic::writeHelp(const HelpWriterContext &context) const
     // TODO: The markup here is not really appropriate, and printKeywordList()
     // still prints raw text, but these are waiting for discussion of the
     // markup format in #969.
-    writeBasicHelpTopic(context, *this, helpText());
+    context.writeTextBlock(helpText());
     context.writeTextBlock("[BR]");
 
     // Print the list of keywords
