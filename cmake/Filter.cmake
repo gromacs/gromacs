gromacs/gromacs

commit 26bd5f7ac7af14f438095990d84ab720c5012cb3

Build man-pages instead of distributing them in the source
One less thing to do manual for releasing the code. And thus
avoiding to accidental releasing which old version of man-pages.

Writing copyright notice to stderr for all programs (was inconsistent)
and caused copyright notice to be displayed during build.

The CPack source package continues to contain the man pages pre-build.
CPack verifies that they are genenerated before building the source
package.

Added .isreposource to distinquish code obtained from git from code
downloaded as CPack source package.

Related to #645, #854, #735
Fixes #878

Change-Id: I7d7145fb99d3f1991d0f3992ca4f3ea20668dd9a

cmake/Filter.cmake
@@ -1,0 +1,3 @@
+#Can be used to filter files at build time
+#Usage: cmake  -DINFILE=... -DOUTFILE=... [variables to replace] -P Filter.cmake
+configure_file(${INFILE} ${OUTFILE})

commit 4b7bc8fac727d9c5957078f2b2be4a7242e33531

Fixes too many backup files for *.nroff files
Problem introduced by 26bd5f7ac7af. nroff files are generated
automatically and one could get erorr message when 99 backup files
existed.

Change-Id: Ia0a5c7e04064a0d304266e1dc6af347d3e846864

cmake/Filter.cmake
@@ -1,3 +1,4 @@
 #Can be used to filter files at build time
 #Usage: cmake  -DINFILE=... -DOUTFILE=... [variables to replace] -P Filter.cmake
 configure_file(${INFILE} ${OUTFILE})
+file(REMOVE ${INFILE})