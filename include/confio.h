59d152bc8f774e02c1c5b18c73bf3291a7950e72
Sander Pronk <pronk@cbr.su.se>
2012-09-03 13:58:25 +0200

    Fixed .g96 trajectory reading.
    The line variable in .g96 configuration reading can now be made
    persistent - and is done so for reading .g96 trajectories.
    
    This commit fixes #781
    
    Change-Id: If2005a00bb925b41802c4b0dc92e66eb5635f22c

--- include/confio.h
+++ include/confio.h
@@ -50,14 +50,15 @@ void init_t_atoms(t_atoms *atoms, int natoms, gmx_bool bPdbinfo);
 /* allocate memory for the arrays, set nr to natoms and nres to 0
  * set pdbinfo to NULL or allocate memory for it */  
 
-int read_g96_conf(FILE *fp,const char *infile,t_trxframe *fr);
+int read_g96_conf(FILE *fp,const char *infile,t_trxframe *fr, char *line);
 /* read a Gromos96 coordinate or trajectory file,                       *
  * returns the number of atoms                                          *
  * sets what's in the frame in info                                     *  
  * read from fp, infile is only needed for error messages               *   
  * nwanted is the number of wanted coordinates,                         *
  * set this to -1 if you want to know the number of atoms in the file   *
- * title, atoms, x, v can all be NULL, in which case they won't be read */
+ * title, atoms, x, v can all be NULL, in which case they won't be read *
+ * line holds the previous line for trajectory reading                  */
 
 void write_g96_conf(FILE *out,t_trxframe *fr,int nindex,atom_id *index);
 /* write a Gromos96 coordinate file or trajectory frame *
