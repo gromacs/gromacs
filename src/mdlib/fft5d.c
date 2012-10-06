gromacs/gromacs

commit 23ca07b542457338e74d9559ddf1e54dc594fdf8

removed the extra fft5d transpose buffers with nthreads=1

src/mdlib/fft5d.c
@@ -403,13 +403,31 @@ fft5d_plan fft5d_plan_3d(int NG, int MG, int KG, MPI_Comm comm[2], int flags, t_
     if (!(flags&FFT5D_NOMALLOC)) { 
         lin = (t_complex*)gmx_calloc_aligned(sizeof(t_complex) * lsize);   
         lout = (t_complex*)gmx_calloc_aligned(sizeof(t_complex) * lsize); 
-        lout2 = (t_complex*)gmx_calloc_aligned(sizeof(t_complex) * lsize);
-        lout3 = (t_complex*)gmx_calloc_aligned(sizeof(t_complex) * lsize);
+        if (nthreads > 1)
+        {
+            /* We need extra transpose buffers to avoid OpenMP barriers */
+            lout2 = (t_complex*)gmx_calloc_aligned(sizeof(t_complex) * lsize);
+            lout3 = (t_complex*)gmx_calloc_aligned(sizeof(t_complex) * lsize);
+        }
+        else
+        {
+            /* We can reuse the buffers to avoid cache misses */
+            lout2 = lin;
+            lout3 = lout;
+        }
     } else {
         lin = *rlin;
         lout = *rlout;
-        lout2 = *rlout2;
-        lout3 = *rlout3;
+        if (nthreads > 1)
+        {
+            lout2 = *rlout2;
+            lout3 = *rlout3;
+        }
+        else
+        {
+            lout2 = lin;
+            lout3 = lout;
+        }
     }
 
     plan = (fft5d_plan)calloc(1,sizeof(struct fft5d_plan_t));
@@ -925,7 +943,7 @@ void fft5d_execute(fft5d_plan plan,int thread,fft5d_time times) {
         }
 #endif
 
-        if (bParallelDim) {
+        if (bParallelDim || plan->nthreads == 1) {
             fftout = lout;
         }
         else
@@ -1112,6 +1130,14 @@ llToAll
 
 void fft5d_destroy(fft5d_plan plan) {
     int s,t;
+
+    /* Note that we expect plan->lin and plan->lout to be freed elsewhere */
+    if (plan->nthreads > 1)
+    {
+        free(plan->lout2);
+        free(plan->lout3);
+    }
+
     for (s=0;s<3;s++)
     {
         if (plan->p1d[s])

commit cb951b7b911c5ed636bbc7c8ced715f8329896ec

Fix incorrect free
introduced by 23ca07b5 / 7b6508e8

Change-Id: I713827221923270f8ba94b3ed33ee126f3ce084c

src/mdlib/fft5d.c
@@ -1108,13 +1108,6 @@ llToAll
 void fft5d_destroy(fft5d_plan plan) {
     int s,t;
 
-    /* Note that we expect plan->lin and plan->lout to be freed elsewhere */
-    if (plan->nthreads > 1)
-    {
-        free(plan->lout2);
-        free(plan->lout3);
-    }
-
     for (s=0;s<3;s++)
     {
         if (plan->p1d[s])
@@ -1164,8 +1157,11 @@ void fft5d_destroy(fft5d_plan plan) {
     {
         sfree_aligned(plan->lin);
         sfree_aligned(plan->lout);
-        sfree_aligned(plan->lout2);
-        sfree_aligned(plan->lout3);
+        if (plan->nthreads > 1)
+        {
+            sfree_aligned(plan->lout2);
+            sfree_aligned(plan->lout3);
+        }
     }
     
 #ifdef FFT5D_THREADS