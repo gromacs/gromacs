bdb2d1cb6b71c539620ef9a33fa14018f7258827
Teemu Murtola <teemu.murtola@gmail.com>
2012-11-22 20:36:40 +0200

    Wrapper module to add old binaries into g_ana.
    Implement a module class that allows including old binaries (basically,
    anything that has a main()-like function and accepts a '-h' command-line
    argument) into g_ana.  As a proof-of-concept, move one tool (g_dist)
    into g_ana.  Will move most of the rest in a separate commit.
    
    To try to keep old functionality working, symbolic links are now created
    also into the build tree using the old binary names.  Also added that on
    Windows, copies of the g_ana binary are created instead of symbolic
    links.  Required some changes in the CreateLinks.cmake script.
    
    Only man page generation should break with this change as the old
    gmx_add_man_page() logic does not work as there is no target with the
    correct name to which add the post-build rule.
    
    Several TODO comments are left, but they can be resolved later.
    
    Related to #685.
    
    Change-Id: I7c826ab0604b2f3d9372f9962e0b0e30d655bcd6

--- src/programs/g_ana/legacymodules.h
+++ src/programs/g_ana/legacymodules.h
@@ -1,54 +1,55 @@
 /*
- * 
+ *
  *                This source code is part of
- * 
+ *
  *                 G   R   O   M   A   C   S
- * 
+ *
  *          GROningen MAchine for Chemical Simulations
- * 
- *                        VERSION 3.2.0
+ *
  * Written by David van der Spoel, Erik Lindahl, Berk Hess, and others.
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
- * Copyright (c) 2001-2004, The GROMACS development team,
+ * Copyright (c) 2001-2009, The GROMACS development team,
  * check out http://www.gromacs.org for more information.
 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * If you want to redistribute modifications, please consider that
  * scientific software is very special. Version control is crucial -
  * bugs must be traceable. We will be happy to consider code for
  * inclusion in the official distribution, but derived work must not
  * be called official GROMACS. Details are found in the README & COPYING
  * files - if they are missing, get the official version at www.gromacs.org.
- * 
+ *
  * To help us fund GROMACS development, we humbly ask that you cite
  * the papers on the package - you can find them in the top README file.
- * 
+ *
  * For more info, check our website at http://www.gromacs.org
- * 
- * And Hey:
- * Green Red Orange Magenta Azure Cyan Skyblue
  */
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
+/*! \internal \brief
+ * Declares registerLegacyModules().
+ *
+ * \author Teemu Murtola <teemu.murtola@cbr.su.se>
+ */
+#ifndef GMX_LEGACYMODULES_H
+#define GMX_LEGACYMODULES_H
 
-#include <gmx_ana.h>
-
-
-/* This is just a wrapper binary.
-* The code that used to be in g_dist.c is now in gmx_dist.c,
-* where the old main function is called gmx_dist().
-*/
-int
-main(int argc, char *argv[])
+namespace gmx
 {
-  gmx_dist(argc,argv);
-  return 0;
-}
+class CommandLineModuleManager;
+} // namespace gmx
 
+/*! \internal \brief
+ * Registers all legacy command-line modules.
+ *
+ * \param[in] manager  Command-line module manager to receive the modules.
+ * \throws    std::bad_alloc if out of memory.
+ *
+ * Registers all modules corresponding to pre-5.0 binaries such that
+ * they can be run through \p manager.
+ */
+void registerLegacyModules(gmx::CommandLineModuleManager *manager);
 
-  
+#endif
--- src/tools/gmx_dist.c
+++ src/tools/gmx_dist.c
@@ -133,9 +133,6 @@ int gmx_dist(int argc,char *argv[])
   };
 #define NFILE asize(fnm)
 
-
-  CopyRight(stderr,argv[0]);
-
   parse_common_args(&argc,argv,PCA_CAN_TIME | PCA_BE_NICE,
 		    NFILE,fnm,NPA,pa,asize(desc),desc,0,NULL,&oenv);
   
