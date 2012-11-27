d00e0e34d9023e56e9631825992710b818964282
Mark Abraham <mark.j.abraham@gmail.com>
2012-11-27 14:02:57 +0100

    Update copyright statements and change license to LGPL
    * I have followed http://www.gnu.org/licenses/gpl-howto.html
    * Copyright is asserted for 2012 (which is needed for the license to
      have any effect)
    * Mostly that meant amending the previous statements, most of which
      had no legal force anyway (e.g. using "2001-2004" requires that
      you spell out that you really mean every year in between is a
      copyrightable year)
    * Many files had no copyright on them; now they do
    * Licensing is now LGPL v2.1 (or later). Unfortunately a CPack
      limitation means we have to use just one file for all our
      licensing statements. That's not great, but it will do.
    * Previous copyright statements are intact
    * External code used under license retains that license statement
      in the code
    * Changes were made with a Perl script so this pain is
      not so bad next time
    * The copyright comment no longer names a specific version,
      so that we don't spam the git log for no real purpose.
    * Removed some remaining emacs editor lines, CVS tags and GROMACS
      acronym generator spam
    * Some code areas are not yet relicensed, pending anticipated
      updates e.g. AdResS kernels
    * Group kernels have been regenerated so that both the templates and
      the code all bear the copyright - this will continue to occur for
      new kernel sets
    * No duplicate copyright statements on essentially unmodified
      GPU memtest code used under license
    * No duplicate copyright statements on unmodified thread_mpi code
    * Don't bother licensing cmake/*.c files
    * Don't bother licensing anything in share subdirectory
    
    The following things we will fix after 4.6-beta1
    
    * TODO vectype_ops.cuh probably from CUDA SDK needs attribution
    * TODO fix licensing statements for things we have stolen from CMake
    * TODO fix things under admin, scripts
    
    Change-Id: I1c507b3feb9ec97d02c87d73cc66cfd5f7381c23

--- include/gmx_arpack.h
+++ include/gmx_arpack.h
@@ -1,32 +1,38 @@
 /*
- * 
+ * This file is part of the GROMACS molecular simulation package.
+ *
  * This file is part of Gromacs        Copyright (c) 1991-2004
  * David van der Spoel, Erik Lindahl, University of Groningen.
+ * Copyright (c) 2012, by the GROMACS development team, led by
+ * David van der Spoel, Berk Hess, Erik Lindahl, and including many
+ * others, as listed in the AUTHORS file in the top-level source
+ * directory and at http://www.gromacs.org.
  *
- * This file contains a subset of ARPACK functions to perform
- * diagonalization and SVD for sparse matrices in Gromacs.
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
  *
- * The code has been translated to C to avoid being dependent on
- * a Fotran compiler, and it has been made threadsafe by using 
- * additional workspace arrays to store data during reverse communication.
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You might prefer the original ARPACK library for general use, but
- * in case you want to this version can be redistributed freely, just
- * as the original library. However, please make clear that it is the
- * hacked version from Gromacs so any bugs are blamed on us and not
- * the original authors. You should also be aware that the double
- * precision work array workd needs to be of size (3*N+4) here
- * (4 more than the general library), and there is an extra argument
- * iwork, which should be an integer work array of length 80.
- * 
- * ARPACK was written by 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
  *
- *     Danny Sorensen               Phuong Vu
- *    Riconst chard Lehoucq              CRPC / Rice University
- *    Dept. of Computational &     Houston, Texas
- *    Applied Mathematics
- *    Rice University           
- *    Houston, Texas            
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
  */
 
 #ifndef _GMX_ARPACK_H
