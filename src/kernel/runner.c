102fa0d5869862ad63b483f921aea19507bd6db2
Berk Hess <hess@kth.se>
2012-11-29 22:44:15 +0100

    fixed issue with -multi and GPUs
    Several GPU related parts of the code did not take the multsim
    functionality and separate PME nodes into account.
    
    Change-Id: I4f6471dfda3d14ed7ca33205c56849a27564c742

--- src/kernel/runner.c
+++ src/kernel/runner.c
@@ -927,18 +927,9 @@ static void set_cpu_affinity(FILE *fplog,
             /* We need to determine a scan of the thread counts in this
              * compute node.
              */
-            int process_index;
             MPI_Comm comm_intra;
 
-            process_index = cr->nodeid_intra;
-            if (MULTISIM(cr))
-            {
-                /* To simplify the code, we shift process indices by nnodes.
-                 * There might be far less processes, but that doesn't matter.
-                 */
-                process_index += cr->ms->sim*cr->nnodes;
-            }
-            MPI_Comm_split(MPI_COMM_WORLD,gmx_hostname_num(),process_index,
+            MPI_Comm_split(MPI_COMM_WORLD,gmx_hostname_num(),cr->rank_intranode,
                            &comm_intra);
             MPI_Scan(&nthread_local,&thread_id_node,1,MPI_INT,MPI_SUM,comm_intra);
             /* MPI_Scan is inclusive, but here we need exclusive */
@@ -1677,8 +1668,8 @@ int mdrunner(gmx_hw_opt_t *hw_opt,
         gmx_setup_nodecomm(fplog,cr);
     }
 
-    /* Initialize per-node process ID and counters. */
-    gmx_init_intra_counters(cr);
+    /* Initialize per-phyiscal-node MPI process/thread ID and counters. */
+    gmx_init_intranode_counters(cr);
 
 #ifdef GMX_MPI
     md_print_info(cr,fplog,"Using %d MPI %s\n",
