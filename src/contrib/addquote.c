f7f9b1dfb0c46c848b303ffb5e70e0604f39dcbc
David van der Spoel <spoel@xray.bmc.uu.se>
2012-12-15 11:11:43 +0100

    removed some offensive quotes
    And added an extra powerful addquote program.
    Change-Id: Ibdad70d6ceb0caa52be351762704a677e9dddba8

--- src/contrib/addquote.c
+++ src/contrib/addquote.c
@@ -1,4 +1,5 @@
 /*
+ * $Id$
  * 
  *                This source code is part of
  * 
@@ -6,10 +7,10 @@
  * 
  *          GROningen MAchine for Chemical Simulations
  * 
- *                        VERSION 3.3.99_development_20071104
+ *                        VERSION 4.0.99
  * Written by David van der Spoel, Erik Lindahl, Berk Hess, and others.
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
- * Copyright (c) 2001-2006, The GROMACS development team,
+ * Copyright (c) 2001-2008, The GROMACS development team,
  * check out http://www.gromacs.org for more information.
 
  * This program is free software; you can redistribute it and/or
@@ -42,24 +43,66 @@
 #include "copyrite.h"
 #include "smalloc.h"
 
+void swap_str(char *str)
+{
+  int i;
+  for(i=0; (i<strlen(str)); i++)
+    str[i] = ~str[i];
+}
+
 void add_quote(char *q)
 {
   FILE *fp;
   int  i,n;
   char **str = NULL;
+  char c,*db   = "gurgle.dat";
+  
+  do {
+    fprintf(stderr,"Add quote '%s' (y/n)? ",q);
+    c = toupper(fgetc(stdin));
+  } while ((c != 'Y') && (c != 'N'));
+  if (c == 'Y') {
+    n = get_strings(db,&str);
+    srenew(str,n+1);
+    str[n] = strdup(q);
+    swap_str(str[n]);
+    n++;
+    fp = fopen(db,"w");
+    fprintf(fp,"%d\n",n);
+    for(i=0; (i<n); i++) 
+      fprintf(fp,"%s\n",str[i]);
+    fclose(fp);
+  }
+}
+
+void dump_quotes()
+{
+  FILE *fp;
+  int  i,j,n;
+  char **str = NULL;
   char *db   = "gurgle.dat";
   
   n = get_strings(db,&str);
-  srenew(str,n+1);
-  snew(str[n],strlen(q)+1);
-  for(i=0; (i<strlen(q)); i++)
-    str[n][i] = ~q[i];
-  str[n][i] = '\0';
-  n++;
+  for(j=0; (j<n); j++) {
+    swap_str(str[j]);
+    printf("%s\n",str[j]);
+  }
+}
+
+void undump_quotes(char *fn)
+{
+  FILE *fp;
+  int  i,j,n;
+  char **str = NULL;
+  char *db   = "gurgle.dat";
+
+  n = get_lines(fn,&str);  
   fp = fopen(db,"w");
   fprintf(fp,"%d\n",n);
-  for(i=0; (i<n); i++) 
-    fprintf(fp,"%s\n",str[i]);
+  for(j=0; (j<n); j++) {
+    swap_str(str[j]);
+    fprintf(fp,"%s\n",str[j]);
+  }
   fclose(fp);
 }
 
@@ -68,13 +111,40 @@ int main(int argc,char *argv[])
   int  i;
   char c;
   
-  for(i=1; (i<argc); i++) {
-    do {
-      fprintf(stderr,"Add quote '%s' (y/n)? ",argv[i]);
-      c = toupper(fgetc(stdin));
-    } while ((c != 'Y') && (c != 'N'));
-    if (c == 'Y') {
-      add_quote(argv[i]);
+  for(i=1; (i<argc); ) {
+    if (strcmp(argv[i],"-add") == 0) {
+      if (i < argc-1) {
+	add_quote(argv[i+1]);
+	i+=2;
+      }
+      else {
+	fprintf(stderr,"Expected argument after -add\n");
+	exit(1);
+      }
+    }
+    else if (strcmp(argv[i],"-dump") == 0) {
+      if (i < argc-1) {
+	dump_quotes();
+	i += 2;
+      }
+      else {
+	fprintf(stderr,"Expected argument after -dump\n");
+	exit(1);
+      }
+    }
+    else if (strcmp(argv[i],"-undump") == 0) {
+      if (i < argc-1) {
+	undump_quotes(argv[i+1]);
+	i += 2;
+      }
+      else {
+	fprintf(stderr,"Expected argument after -undump\n");
+	exit(1);
+      }
+    }
+    else {
+      fprintf(stderr,"WTF: %s\n",argv[i]);
+      i++;
     }
   }
   thanx(stdout);
