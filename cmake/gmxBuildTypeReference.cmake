b435e811505a62e1f8123c0748df3e517eca9cb1
Mark Abraham <mark.j.abraham@gmail.com>
2012-12-19 15:10:56 +0100

    Fix Reference build type
    This typo led to the Reference build claiming it was not building
    for thread-MPI, but actually it did. Pity we don't have a CMake
    syntax checker.
    
    Change-Id: I932794145471451e09028297cfbffa4a72e96f39

--- cmake/gmxBuildTypeReference.cmake
+++ cmake/gmxBuildTypeReference.cmake
@@ -45,7 +45,7 @@ if("${CMAKE_BUILD_TYPE}" STREQUAL "Reference")
     set(GMX_CPU_ACCELERATION "None" CACHE STRING "Disabled for regressiontests reference builds" FORCE)
     set(GMX_FFT_LIBRARY "fftpack" CACHE STRING "Use fftpack for regressiontests reference builds" FORCE)
     set(GMX_SOFTWARE_INVSQRT OFF CACHE BOOL "Disabled for regressiontests reference builds" FORCE)
-    set(GMX_THREAD_MPI OFF OFF CACHE BOOL "Disabled for regressiontests reference builds" FORCE)
+    set(GMX_THREAD_MPI OFF CACHE BOOL "Disabled for regressiontests reference builds" FORCE)
 
     if(NOT "${CMAKE_C_COMPILER_ID}" STREQUAL "GNU" OR NOT "${C_COMPILER_VERSION}" MATCHES "4.7")
         message(WARNING "Reference values for regressiontests should use Gromacs compiled with "
