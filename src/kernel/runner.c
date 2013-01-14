dd74bc81de3a0b65d2211e9640a0f0efe4dff2ad
Mark Abraham <mark.j.abraham@gmail.com>
2013-01-14 23:48:20 +0100

    Velocity-verlet integrators and v-rescale are broken
    Added fatal error and suggestion to upgrade GROMACS version.
    
    Fixes #1000
    
    Change-Id: I22653fafdfd0fb2f1fa772fbd95bc55fcf072d8c

--- src/kernel/runner.c
+++ src/kernel/runner.c
@@ -432,6 +432,10 @@ int mdrunner(int nthreads_requested, FILE *fplog,t_commrec *cr,int nfile,
     {
         gmx_fatal(FARGS, "Currently can't do velocity verlet with rerun in parallel.");
     }
+    if (EI_VV(inputrec->eI) && etcVRESCALE == inputrec->etc)
+    {
+        gmx_fatal(FARGS, "In GROMACS 4.5.x, velocity-Verlet integrators do not work with velocity-rescaling temperature coupling. They do work in 4.6.x. Please ugprade your GROMACS version.");
+    }
 
     /* A parallel command line option consistency check that we can
        only do after any threads have started. */
