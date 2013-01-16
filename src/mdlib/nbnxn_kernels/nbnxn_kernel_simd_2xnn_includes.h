95761d63fb705b4334acbe2ce6ae2f384b4bcadb
Mark Abraham <mark.j.abraham@gmail.com>
2013-01-16 01:55:31 +0100

    Fixing copyright issues and code contributors
    Added Mark Abraham, Christoph Junghans and
    Carsten Kutzner to the list of code contributors,
    per discussion with Erik and Berk.
    
    * Now that 2013 is a copyrightable year, we need to
      assert that copyright
    * No, we can't just say 1995-2013, unless we also say
      that every year in the range is a copyrightable year. I'm
      not interested in demonstrating after the fact that this is
      true for all the years before I was born. So we may as
      well start listing explicit years.
      Please read "howto (L)GPL" guides before arguing :-)
    * Removed erroneous GROMACS copyright statements on
      unmodified FindBLAS.cmake,FindLAPACK.cmake,
      FindOpenMP.cmake and slightly modified FindGit.cmake
    * Note Szilard's uncertainty about the origin of
      vectype_ops.cuh and thus its copyright status
    * TODO for 4.6.1 deal with material in admin and scripts
    
    References #818
    
    Change-Id: I946200651de7f05c2ff292fcded8f33fca7b356f

--- src/mdlib/nbnxn_kernels/nbnxn_kernel_simd_2xnn_includes.h
+++ src/mdlib/nbnxn_kernels/nbnxn_kernel_simd_2xnn_includes.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2009, The GROMACS Development Team
- * Copyright (c) 2012, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013, by the GROMACS development team, led by
  * David van der Spoel, Berk Hess, Erik Lindahl, and including many
  * others, as listed in the AUTHORS file in the top-level source
  * directory and at http://www.gromacs.org.
