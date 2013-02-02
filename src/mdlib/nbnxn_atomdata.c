f710c90fa0c98d9eff4bc53cedb306cc479be5eb
Mark Abraham <mark.j.abraham@gmail.com>
2013-02-02 12:53:19 +0100

    Fix and add Verlet kernel error messages
    Handles default cases better
    
    Change-Id: I45d8ebd73184630180e6ed87d54c3a8fc3cbef73

--- src/mdlib/nbnxn_atomdata.c
+++ src/mdlib/nbnxn_atomdata.c
@@ -357,7 +357,7 @@ void copy_rvec_to_nbat_real(const int *a, int na, int na_round,
             }
             break;
         default:
-            gmx_incons("Unsupported stride");
+            gmx_incons("Unsupported nbnxn_atomdata_t format");
     }
 }
 
@@ -1216,6 +1216,8 @@ nbnxn_atomdata_add_nbat_f_to_f_part(const nbnxn_search_t nbs,
                 }
             }
             break;
+    default:
+        gmx_incons("Unsupported nbnxn_atomdata_t format");
     }
 }
 
