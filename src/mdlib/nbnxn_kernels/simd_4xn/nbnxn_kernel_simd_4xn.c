d05a04892a5854cfb30ba30813ba959656b4b911
Mark Abraham <mark.j.abraham@gmail.com>
2013-02-25 21:00:44 +0100

    Split up Verlet SIMD kernels for faster compilation
    On a 48-core AMD machine using GCC 4.7 for an SSE2 build (i.e. 4xN
    Verlet kernels), the time for "make mdrun -j" went from 73 s to 34 s.
    
    On a more recent 8-core Intel machine using GCC 4.6.4 for AVX_256
    build (also 4xN), the time for "make mdrun -j" went from 81 s to 63 s.
    
    This could maybe go down a little further if there was a way to avoid
    compiling stub object files for the set of Verlet kernels that are not
    being used. That would require CMake duplicating the logic that
    selects which set of kernels should be used at run time.
    
    Also unwound some include file dependencies and removed repeated
    definitions of some supporting structures and functions.
    
    Change-Id: I0da1faf351defbe68d5ca43febcabddd93e21f0d

---
+++ src/mdlib/nbnxn_kernels/simd_4xn/nbnxn_kernel_simd_4xn.c
@@ -0,0 +1,148 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
+ * Copyright (c) 2001-2012, The GROMACS development team,
+ * check out http://www.gromacs.org for more information.
+ * Copyright (c) 2012,2013, by the GROMACS development team, led by
+ * David van der Spoel, Berk Hess, Erik Lindahl, and including many
+ * others, as listed in the AUTHORS file in the top-level source
+ * directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "typedefs.h"
+
+#ifdef GMX_NBNXN_SIMD_4XN
+
+/* Include the half-width SIMD macros */
+#ifdef GMX_NBNXN_HALF_WIDTH_SIMD
+#define GMX_USE_HALF_WIDTH_SIMD_HERE
+#endif
+#include "gmx_simd_macros.h"
+#include "gmx_simd_vec.h"
+
+/* Declare the extern function this file defines */
+#include "nbnxn_kernel_simd_4xn.h"
+
+#if !(GMX_SIMD_WIDTH_HERE == 2 || GMX_SIMD_WIDTH_HERE == 4 || GMX_SIMD_WIDTH_HERE == 8)
+#error "unsupported SIMD width"
+#endif
+
+/* Declare all the kernel functions */
+#include "nbnxn_kernel_simd_4xn_rf_comb_geom_ener.h"
+#include "nbnxn_kernel_simd_4xn_rf_comb_geom_energrp.h"
+#include "nbnxn_kernel_simd_4xn_rf_comb_geom_noener.h"
+#include "nbnxn_kernel_simd_4xn_rf_comb_lb_ener.h"
+#include "nbnxn_kernel_simd_4xn_rf_comb_lb_energrp.h"
+#include "nbnxn_kernel_simd_4xn_rf_comb_lb_noener.h"
+#include "nbnxn_kernel_simd_4xn_rf_comb_none_ener.h"
+#include "nbnxn_kernel_simd_4xn_rf_comb_none_energrp.h"
+#include "nbnxn_kernel_simd_4xn_rf_comb_none_noener.h"
+
+#include "nbnxn_kernel_simd_4xn_tab_comb_geom_ener.h"
+#include "nbnxn_kernel_simd_4xn_tab_comb_geom_energrp.h"
+#include "nbnxn_kernel_simd_4xn_tab_comb_geom_noener.h"
+#include "nbnxn_kernel_simd_4xn_tab_comb_lb_ener.h"
+#include "nbnxn_kernel_simd_4xn_tab_comb_lb_energrp.h"
+#include "nbnxn_kernel_simd_4xn_tab_comb_lb_noener.h"
+#include "nbnxn_kernel_simd_4xn_tab_comb_none_ener.h"
+#include "nbnxn_kernel_simd_4xn_tab_comb_none_energrp.h"
+#include "nbnxn_kernel_simd_4xn_tab_comb_none_noener.h"
+
+#include "nbnxn_kernel_simd_4xn_tab_twin_comb_geom_ener.h"
+#include "nbnxn_kernel_simd_4xn_tab_twin_comb_geom_energrp.h"
+#include "nbnxn_kernel_simd_4xn_tab_twin_comb_geom_noener.h"
+#include "nbnxn_kernel_simd_4xn_tab_twin_comb_lb_ener.h"
+#include "nbnxn_kernel_simd_4xn_tab_twin_comb_lb_energrp.h"
+#include "nbnxn_kernel_simd_4xn_tab_twin_comb_lb_noener.h"
+#include "nbnxn_kernel_simd_4xn_tab_twin_comb_none_ener.h"
+#include "nbnxn_kernel_simd_4xn_tab_twin_comb_none_energrp.h"
+#include "nbnxn_kernel_simd_4xn_tab_twin_comb_none_noener.h"
+
+#include "nbnxn_kernel_simd_4xn_ewald_comb_geom_ener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_comb_geom_energrp.h"
+#include "nbnxn_kernel_simd_4xn_ewald_comb_geom_noener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_comb_lb_ener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_comb_lb_energrp.h"
+#include "nbnxn_kernel_simd_4xn_ewald_comb_lb_noener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_comb_none_ener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_comb_none_energrp.h"
+#include "nbnxn_kernel_simd_4xn_ewald_comb_none_noener.h"
+
+#include "nbnxn_kernel_simd_4xn_ewald_twin_comb_geom_ener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_twin_comb_geom_energrp.h"
+#include "nbnxn_kernel_simd_4xn_ewald_twin_comb_geom_noener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_twin_comb_lb_ener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_twin_comb_lb_energrp.h"
+#include "nbnxn_kernel_simd_4xn_ewald_twin_comb_lb_noener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_twin_comb_none_ener.h"
+#include "nbnxn_kernel_simd_4xn_ewald_twin_comb_none_energrp.h"
+#include "nbnxn_kernel_simd_4xn_ewald_twin_comb_none_noener.h"
+
+/* Define the macros to support constructing the kernel function
+ * pointer lookup tables. */
+#define NBK_FN(elec, ljcomb, enertype) nbnxn_kernel_simd_4xn ## _ ## elec ## _comb_ ## ljcomb ## _ ## enertype
+
+/* Is there a better way to define/name this? Does it belong in
+ * gmx_simd_macros.h? */
+#define GMX_SIMD_J_UNROLL_SIZE 1
+
+#include "../simd_common/nbnxn_kernel_simd_common_preparation.c"
+
+#else
+
+#include "gmx_fatal.h"
+
+#endif /* GMX_NBNXN_SIMD_4XN */
+
+void
+nbnxn_kernel_simd_4xn(nbnxn_pairlist_set_t       *nbl_list,
+                      const nbnxn_atomdata_t     *nbat,
+                      const interaction_const_t  *ic,
+                      int                         ewald_excl,
+                      rvec                       *shift_vec,
+                      int                         force_flags,
+                      int                         clearF,
+                      real                       *fshift,
+                      real                       *Vc,
+                      real                       *Vvdw)
+{
+#ifdef GMX_NBNXN_SIMD_4XN
+#include "../simd_common/nbnxn_kernel_simd_common_kernel.c"
+#else
+    gmx_incons("nbnxn_kernel_simd_4xn called when its kernel path "
+               " is not enabled.");
+#endif
+}
+
+#undef NBK_FN
+#undef GMX_SIMD_J_UNROLL_SIZE
