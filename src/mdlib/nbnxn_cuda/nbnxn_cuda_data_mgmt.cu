d19017afc0b33a8e04384dfeeb53ba90b7ee5a54
Szilárd Páll <pszilard@cbr.su.se>
2013-03-07 03:50:47 +0100

    fix condition to switch to polling GPU wait
    Commit 84597ae introduced an incorrect conditional in the testing
    whether mdrun should/can switch to polling GPU wait.
    This minor bug resulted in swithching to polling wait even when this
    was unnecessary, something that is rather harmless.
    
    Change-Id: I9106cb34c9a8fde2f9b54045d9b03ee5f5d50d8b

--- src/mdlib/nbnxn_cuda/nbnxn_cuda_data_mgmt.cu
+++ src/mdlib/nbnxn_cuda/nbnxn_cuda_data_mgmt.cu
@@ -530,15 +530,16 @@ void nbnxn_cuda_init(FILE *fplog,
         else
         {
             /* Can/should turn of cudaStreamSynchronize wait only if
+             *   - driver is old
              *   - we're on x86/x86_64
              *   - atomics are available
              *   - GPUs are not being shared
-             *   - and driver is old. */
+             */
             nb->bUseStreamSync =
-                (bX86 && bTMPIAtomics && !gpu_info->bDevShare && bOldDriver) ?
-                true : false;
+                (bOldDriver && bX86 && bTMPIAtomics && !gpu_info->bDevShare) ?
+                false : true;
 
-            if (nb->bUseStreamSync)
+            if (!nb->bUseStreamSync)
             {
                 md_print_warn(fplog,
                               "NOTE: Using a GPU with ECC enabled and CUDA driver API version <5.0, known to\n"
