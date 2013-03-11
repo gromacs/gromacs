240c65b3790adaffe3c9724616d416f440879d91
Mark Abraham <mark.j.abraham@gmail.com>
2013-03-11 13:30:59 +0100

    Update management of linear algebra libraries
    Management of detection and/or linking to BLAS and LAPACK libraries is
    re-organized. The code has migrated to its own module. This will
    help future extension and maintenance. This version communicates
    things that are newsworthy and stays out of the way when nothing
    is changing.
    
    We no longer over-write the values specified by the user for
    GMX_EXTERNAL_(BLAS|LAPACK). Previously, this was used to signal
    whether detection succeeded, but that does not really get the job
    done. Instead, the user is notified that detection failed (repeatedly,
    if they deliberately set such an option on).
    
    Correct usage and expected behaviour in all cases is documented both
    in the code and the install guide.
    
    The user interface is pretty much unchanged. We still don't offer full
    configurability (e.g. MKL for FFTs must use MKL for linear algebra
    unless GMX_*_USER is used, and the only way to get MKL for linear
    algebra is to use it for FFTs). The size of any performance difference
    is probably very small, and if the user really needs mdrun with
    certain FFT and tools with certain linear algebra library, they can do
    two configurations. Note that mdrun never calls any linear algebra
    routines (tested empirically)!
    
    Expanded the solution of #771 by testing that the user supplied
    libraries that actually work. If not, we emit a warning and try to use
    them anyway.
    
    We also now check that MKL really does provide linear algebra
    routines, and fall back to the default treatment if it does not.
    
    Refs #771,#1186
    
    Change-Id: Ife5c59694e29a3ce73fc55975e26f6c083317d9b

---
+++ cmake/gmxManageBlasAndLapack.cmake
@@ -0,0 +1,162 @@
+# Helper macro for the function below. We treat BLAS and LAPACK the same
+# way, so there's no reason to duplicate the logic for them
+#
+# MESSSAGE_TEXT variable is accumulated while checks for these
+# libraries fail, but the message is only emitted if we are forced to
+# fall back on the internal version.
+#
+# Arguments should be:
+#     name                 "BLAS" or "LAPACK"
+#     function_in_library  the name of a function to use in a linking test of that library
+macro(manage_linear_algebra_library name function_in_library)
+    # Default behaviour is to try to use an external library, but fall
+    # back on the internal one if none is found.
+    set(GMX_EXTERNAL_${name}_DEFAULT ON)
+
+    set(_find_quietly FALSE)
+    set(_user_var_changed FALSE)
+    if(NOT DEFINED GMX_EXTERNAL_${name} OR
+       (GMX_EXTERNAL_${name} AND NOT "${GMX_${name}_USER}" STREQUAL "${GMX_${name}_USER_PREV}"))
+        set(_user_var_changed TRUE)
+    endif()
+    if(DEFINED GMX_EXTERNAL_${name} AND NOT _user_var_changed)
+        set(_find_quietly TRUE)
+    endif()
+
+    # We could consider printing status messages at the beginning and
+    # end, which would require caching whether the previous provider
+    # was user/MKL/external/internal. It's possible (we do it for
+    # FFT), but the number of times the user changes these is pretty
+    # low, so let's solve that one in master branch when we have
+    # better CMake gear to support it.
+    if(GMX_EXTERNAL_${name} OR NOT DEFINED GMX_EXTERNAL_${name})
+        set(GMX_${name}_USER_PREV ${GMX_${name}_USER} CACHE INTERNAL
+            "Previous value of GMX_${name}_USER (to detect changes)" FORCE)
+        set(_message_text)
+        # Look for user-specified libraries if external libraries have
+        # been specified (or specified by default).
+        if(GMX_${name}_USER)
+            set(_${name}_libraries ${GMX_${name}_USER})
+            set(GMX_${name}_FOUND 1)
+
+            if(NOT _find_quietly)
+                set(CMAKE_REQUIRED_LIBRARIES ${GMX_${name}_USER})
+                if(_user_var_changed)
+                    unset(_${name}_user_works CACHE)
+                endif()
+                message(STATUS "Checking that user ${name} library ${GMX_${name}_USER} works")
+                check_function_exists(${function_in_library} _${name}_user_works)
+                if(NOT _${name}_user_works)
+                    message(WARNING "GMX_${name}_USER library ${GMX_${name}_USER} was specified, but it may not provide ${name}. We are proceeding by assuming you know what you are doing and that linking F77-style to this library will work.")
+                endif()
+
+                if(HAVE_LIBMKL)
+                    message(STATUS "MKL and GMX_${name}_USER were both specified. Using the latter for ${name}.")
+                endif()
+            endif()
+        endif()
+
+        if(NOT GMX_${name}_FOUND AND HAVE_LIBMKL)
+            set(CMAKE_REQUIRED_LIBRARIES ${FFT_LINKER_FLAGS} ${FFT_LIBRARIES})
+            # This may also not work correctly if the user changes
+            # MKL_LIBRARIES after the first run. However,
+            # MKL_LIBRARIES is only needed for icc version < 11, so we
+            # can live with that.
+            check_function_exists(${function_in_library} _${name}_mkl_works)
+            if(_${name}_mkl_works)
+                # If we ever need to compile with MKL linear algebra and
+                # not with FFT supplied by MKL, uncomment the next line
+                # (and probably tweak other things).
+#                list(APPEND LINEAR_ALGEBRA_LIBRARIES ${FFT_LINKER_FLAGS} ${FFT_LIBRARIES})
+                set(GMX_${name}_FOUND 1)
+            else()
+                set(_message_text "Intel's MKL was specified, and it should provide ${name}, but it does not. ")
+            endif()
+        endif()
+
+        # If detection of ${name} has never run, or none of the preceding
+        # detection succeeded, try to detect ${name} in the CMake
+        # detection paths, etc.
+        if (NOT GMX_${name}_FOUND)
+            set(${name}_FIND_QUIETLY _find_quietly)
+            # Note that this finds all kinds of system libraries,
+            # including Apple's Accelerate Framework (and perhaps MKL for
+            # icc < 11).
+            find_package(${name})
+            if (${name}_FOUND)
+                set(_${name}_libraries ${${name}_LIBRARIES})
+                set(GMX_${name}_FOUND 1)
+            endif()
+        endif()
+
+        if (NOT GMX_${name}_FOUND)
+            # Couldn't find a linear algebra library, so default to the
+            # internal one.
+            set(GMX_EXTERNAL_${name}_DEFAULT OFF)
+
+            if (NOT _find_quietly)
+                message("${_message_text}A ${name} library was not found by CMake in the paths available to it. Falling back on the GROMACS internal version of the ${name} library instead. This is fine for normal usage.")
+            endif()
+        endif()
+    endif()
+
+    set(GMX_EXTERNAL_${name} ${GMX_EXTERNAL_${name}_DEFAULT} CACHE BOOL "Use a ${name} library that is external to GROMACS if possible (ON), or the internal GROMACS one (OFF)")
+    mark_as_advanced(GMX_EXTERNAL_${name})
+
+    if(GMX_EXTERNAL_${name})
+        if (NOT GMX_${name}_FOUND)
+            message(FATAL_ERROR "You have set GMX_EXTERNAL_${name}=ON to instruct GROMACS to use an external ${name} library, but no external library could be detected. ")
+        endif ()
+        # Actually trigger linking.
+        list(APPEND LINEAR_ALGEBRA_LIBRARIES ${_${name}_libraries})
+    else()
+        # Triggering the compilation of the internal version of the library is handled elsewhere.
+    endif()
+endmacro()
+
+# The default behaviour is to try to detect an "external" BLAS and/or
+# LAPACK, perhaps provided by a vendor, use those if found, and
+# otherwise fall back on the GROMACS internal implementations of
+# these. If the libraries are not in a standard location, the user can
+# indicate a search path with CMAKE_PREFIX_PATH.
+#
+# However, if we are using icc+mkl (so a build command that includes
+# -mkl), then it is probably painful to try to link some other BLAS or
+# LAPACK. In that case, we use the BLAS & LAPACK provided by MKL. In
+# principle, we could offer a more configurable behaviour if/when
+# there is need to (say) use vendor BLAS with MKL for FFTs.
+#
+# If the vendor BLAS and/or LAPACK have abnormal library names, then
+# the default searching procedure will fail (e.g. Redmine #771). The
+# GMX_(BLAS|LAPACK)_USER variables can be used to indicate the correct
+# libraries. If these do not work, a warning is emitted and we try to
+# use them anyway, assuming the user knows what they are doing.
+
+# Inputs:
+#     GMX_EXTERNAL_BLAS     user input about whether to detect BLAS
+#     GMX_EXTERNAL_LAPACK   user input about whether to detect LAPACK
+#     HAVE_LIBMKL           true if the build will link to MKL
+#     GMX_BLAS_USER         user input for BLAS libraries to use
+#     GMX_LAPACK_USER       user input for LAPACK libraries to use
+#
+# This function sets the following cache variables:
+#     GMX_EXTERNAL_BLAS     according to whether external BLAS is being used
+#     GMX_EXTERNAL_LAPACK   according to whether external LAPACK is being used
+# Their non-cached versions will normally be the same, except when the user
+# specified ON and no library was actually available.
+#
+# This function sets the following variables in its parent scope:
+#     LINEAR_ALGEBRA_LIBRARIES  will be appended as required to add libraries required for linear algebra
+#
+function(gmxManageBlasAndLapack)
+    include(CheckFunctionExists)
+
+    manage_linear_algebra_library(BLAS dgemm_)
+    set(BLAS_FIND_QUIETLY ON)
+    manage_linear_algebra_library(LAPACK cheev_)
+
+    # Propagate the new local value to the parent scope
+    set(LINEAR_ALGEBRA_LIBRARIES "${LINEAR_ALGEBRA_LIBRARIES}" PARENT_SCOPE)
+endfunction()
+
+gmxManageBlasAndLapack()
