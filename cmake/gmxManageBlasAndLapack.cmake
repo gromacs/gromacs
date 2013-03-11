ae4b8b8a81e3122725abb977bebd6ca450b4140a
Mark Abraham <mark.j.abraham@gmail.com>
2013-03-11 13:30:59 +0100

    Update management of linear algebra libraries
    CMake no longer over-writes the values specified by the user for
    GMX_EXTERNAL_(BLAS|LAPACK). This was only used to signal whether
    detection succeeded, and some other mechanism should be used for that.
    Once we have such conventions, maintenance and new features become
    easier to do.
    
    Management of detection and/or linking to BLAS and LAPACK libraries is
    re-organized. The code has migrated to its own module. This will
    help future extension and maintenance.
    
    Correct usage and expected behaviour in all cases is documented both
    in the code and the install guide.
    
    The user interface is pretty much unchanged. We still don't offer full
    configurability (e.g. MKL for FFTs must use MKL for linear algebra,
    and the only way to get MKL for linear algebra is to use it for FFTs).
    The size of any performance difference is probably very small, and if
    the user really needs mdrun with certain FFT and tools with certain
    linear algebra library, they can do two configurations. Note that
    mdrun never calls any linear algebra routines (tested empirically)!
    
    Expanded the solution of #771 by testing that the user supplied
    libraries that actually work. Also, the way GMX_USER_* interacts with
    GMX_EXTERNAL_* is now a bit different.
    
    Refs #771,#1186
    
    Change-Id: Ife5c59694e29a3ce73fc55975e26f6c083317d9b

---
+++ cmake/gmxManageBlasAndLapack.cmake
@@ -0,0 +1,110 @@
+# Helper macro for the function below. We treat BLAS and LAPACK the same
+# way, so there's no reason to duplicate the logic for them
+#
+# Arguments should be:
+#     name                 "BLAS" or "LAPACK"
+#     function_in_library  the name of a function to use in a linking test of that library
+macro(manage_linear_algebra_library name function_in_library)
+
+    if(HAVE_LIBMKL OR ACCELERATE_FRAMEWORK)
+
+        # In either case, we have BLAS/LAPACK routines already organized
+        # so no need to detect anything at all
+        set(HAVE_EXTERNAL_${name} TRUE)
+
+    else()
+
+        # Set up default behaviour of doing detection and assuming
+        # that an external library is not detected.
+        set(DETECT_EXTERNAL_${name} TRUE)
+        set(HAVE_EXTERNAL_${name} FALSE)
+
+        # First, check for user-specified libraries
+        if(GMX_${name}_USER)
+            set(CMAKE_REQUIRED_LIBRARIES ${GMX_${name}_USER})
+            check_function_exists(${function_in_library} ${name}_FOUND)
+            if (NOT ${name}_FOUND)
+                message(FATAL_ERROR "GMX_${name}_USER library ${GMX_${name}_USER} was specified, but it does not provide ${name}. Cannot proceed.")
+            endif()
+            list(APPEND LINEAR_ALGEBRA_LIBRARIES ${GMX_${name}_USER})
+            set(HAVE_EXTERNAL_${name} TRUE)
+            set(DETECT_EXTERNAL_${name} FALSE)
+        endif()
+
+        # If the user has set GMX_EXTERNAL_(BLAS|LAPACK) on, then
+        # we try to detect an external BLAS/LAPACK. If the user
+        # has set these off, then we do not attempt to detect,
+        # thus we fall back on the internal versions. In neither
+        # case do we override the user's choice about detection
+        # (i.e. no FORCE). If detection fails, a status message
+        # is issued that the fall-back is in use.
+        set(GMX_EXTERNAL_${name} ${DETECT_EXTERNAL_${name}} CACHE BOOL "Try to detect external ${name}, but fall back on built-in")
+        mark_as_advanced(GMX_EXTERNAL_${name})
+
+        if(GMX_EXTERNAL_${name})
+            set(${name}_FIND_QUIETLY ON)
+            find_package(${name})
+            if (${name}_FOUND)
+                list(APPEND LINEAR_ALGEBRA_LIBRARIES ${${name}_LIBRARIES})
+                set(HAVE_EXTERNAL_${name} TRUE)
+            else()
+                MESSAGE(STATUS "Detection of ${name} failed, using the GROMACS internal ${name} library instead.")
+            endif()
+        endif()
+
+    endif()
+
+    set(HAVE_EXTERNAL_${name} ${HAVE_EXTERNAL_${name}} PARENT_SCOPE)
+endmacro()
+
+# The default behaviour is to try to detect an "external" BLAS and/or
+# LAPACK, use those if found, and otherwise fall back on the GROMACS
+# internal implementations of these. If the libraries are not in a
+# standard location, the user can indicate a search path with
+# CMAKE_PREFIX_PATH.
+#
+# However, if we are using icc+mkl (so a build command that includes
+# -mkl), then it is probably painful to try to link some other BLAS or
+# LAPACK. In that case, we use the BLAS & LAPACK provided by
+# MKL. Likewise, if we are on Apple and have the Accelerate Framework
+# available. In principle, we could offer a more configurable
+# behaviour if/when there is need to (say) use vendor BLAS with MKL
+# for FFTs.
+#
+# If the vendor BLAS and/or LAPACK have abnormal library names, then
+# the default searching procedure will fail (e.g. Redmine #771). The
+# GMX_(BLAS|LAPACK)_USER variables can be used to indicate the correct
+# libraries. If these do not work, a fatal error will result.
+
+# Inputs:
+#     GMX_EXTERNAL_BLAS     user input about whether to detect BLAS
+#     GMX_EXTERNAL_LAPACK   user input about whether to detect LAPACK
+#     HAVE_LIBMKL           true if the build will link to MKL
+#     GMX_BLAS_USER         user input for BLAS libraries to use
+#     GMX_LAPACK_USER       user input for LAPACK libraries to use
+#
+# This function sets the following cache variables (but does not use FORCE):
+#     GMX_EXTERNAL_BLAS     according to whether detection took place
+#     GMX_EXTERNAL_LAPACK   according to whether detection took place
+#
+# This function sets the following variables in its parent scope:
+#     HAVE_EXTERNAL_BLAS        if an external BLAS will be used
+#     HAVE_EXTERNAL_LAPACK      if an external LAPACK will be used
+#     LINEAR_ALGEBRA_LIBRARIES  will be appended as required to add libraries required for linear algebra
+#
+function(gmxManageBlasAndLapack)
+    include(CheckFunctionExists)
+
+    if(APPLE)
+        find_library(ACCELERATE_FRAMEWORK Accelerate)
+        list(APPEND LINEAR_ALGEBRA_LIBRARIES ${ACCELERATE_FRAMEWORK})
+    endif(APPLE)
+
+    manage_linear_algebra_library(BLAS dgemm)
+    manage_linear_algebra_library(LAPACK cheev)
+
+    # Propagate the new local value to the parent scope
+    set(LINEAR_ALGEBRA_LIBRARIES "${LINEAR_ALGEBRA_LIBRARIES}" PARENT_SCOPE)
+endfunction()
+
+gmxManageBlasAndLapack()
