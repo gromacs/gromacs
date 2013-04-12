a7fee6e8fe04545e3fe70da650aebf2554d278f9
Berk Hess <hess@kth.se>
2013-04-12 19:11:09 +0200

    implemented plain-C SIMD macros for reference
    This is mainly code reorganization.
    Adds reference plain-C, slow, arbitrary width SIMD for testing.
    Adds FMA for gmx_calc_rsq_pr.
    Adds generic SIMD acceleration (also AVX or double) for pme solve.
    Moved SIMD vector operations to gmx_simd_vec.h
    The math functions invsqrt, inv, pmecorrF and pmecorrV have been
    copied from the x86 specific single/double files to generic files
    using the SIMD macros from gmx_simd_macros.h.
    Moved all architecture specific nbnxn_kernel_simd_utils code to
    separate files for each SIMD architecture and replaced all macros
    by inline functions.
    The SIMD reference nbnxn 2xnn kernels now support 16-wide SIMD.
    
    Refs #1173
    
    Change-Id: Ieda78cc3bcb499e8c17ef8ef539c49cbc2d6d74d

--- include/types/nb_verlet.h
+++ include/types/nb_verlet.h
@@ -46,6 +46,18 @@
 extern "C" {
 #endif
 
+
+/* For testing the reference plain-C SIMD kernels, uncomment the next lines,
+ * as well as the GMX_SIMD_REFERENCE_PLAIN_C define in gmx_simd_macros.h
+ * The actual SIMD width is set in gmx_simd_macros.h
+ * The 4xN reference kernels support 2-, 4- and 8-way SIMD.
+ * The 2x(N+N) reference kernels support 8- and 16-way SIMD.
+ */
+/* #define GMX_NBNXN_SIMD */
+/* #define GMX_NBNXN_SIMD_4XN */
+/* #define GMX_NBNXN_SIMD_2XNN */
+
+
 #ifdef GMX_X86_SSE2
 /* Use SIMD accelerated nbnxn search and kernels */
 #define GMX_NBNXN_SIMD
@@ -53,19 +65,13 @@ extern "C" {
 /* Uncomment the next line to use, slower, 128-bit SIMD with AVX-256 */
 /* #define GMX_NBNXN_HALF_WIDTH_SIMD */
 
-#if defined GMX_X86_AVX_256 && !defined GMX_NBNXN_HALF_WIDTH_SIMD
-#define GMX_NBNXN_SIMD_BITWIDTH  256
-#else
-#define GMX_NBNXN_SIMD_BITWIDTH  128
-#endif
-
 /* The nbnxn SIMD 4xN and 2x(N+N) kernels can be added independently.
  * Currently the 2xNN SIMD kernels only make sense with:
  *  8-way SIMD: 4x4 setup, works with AVX-256 in single precision
  * 16-way SIMD: 4x8 setup, not used, but most of the kernel code is there
  */
 #define GMX_NBNXN_SIMD_4XN
-#if GMX_NBNXN_SIMD_BITWIDTH == 256 && !defined GMX_DOUBLE
+#if defined GMX_X86_AVX_256 && !(defined GMX_DOUBLE || defined GMX_NBNXN_HALF_WIDTH_SIMD)
 #define GMX_NBNXN_SIMD_2XNN
 #endif
 
