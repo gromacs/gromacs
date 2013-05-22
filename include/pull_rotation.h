4da425087f165b30b25d772982e455845bac0fb0
Carsten Kutzner <ckutzne@gwdg.de>
2013-05-22 15:47:47 +0200

    Code cleanup in enforced rotation module, remove warnings.
    Change-Id: Iceb458b06f2c003c6325c77c3258edb112e449f6

--- include/pull_rotation.h
+++ include/pull_rotation.h
@@ -70,6 +70,7 @@ extern "C" {
  *                          of the rotation output files.
  * \param cr                Pointer to MPI communication data.
  * \param x                 The positions of all MD particles.
+ * \param box               The simulation box.
  * \param mtop              Molecular topology.
  * \param oenv              Needed to open the rotation output xvgr file.
  * \param Flags             Flags passed over from main, used to determine
@@ -134,11 +135,10 @@ extern real add_rot_forces(t_rot *rot, rvec f[], t_commrec *cr, gmx_large_int_t 
 
 /*! \brief Close the enforced rotation output files.
  *
- * \param fplog             General output file, normally md.log.
  * \param rot               Pointer to all the enforced rotation data.
  */
 GMX_LIBMD_EXPORT
-extern void finish_rot(FILE *fplog, t_rot *rot);
+extern void finish_rot(t_rot *rot);
 
 
 #ifdef __cplusplus
