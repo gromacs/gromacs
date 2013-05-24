02053b7c7940f2b3db6b8faf07f8d1978d9ece59
Mark Abraham <mark.j.abraham@gmail.com>
2013-05-24 16:50:37 +0200

    Work around for correct treatment of all-vs-all
    Neither C or accelerated all-vs-all kernels work in 4.6. The group
    SIMD kernels do not work either. The non-accelerated kernel is OK,
    though.
    
    Fixes #1249 (for certain values of "fixes"), refs #1095
    
    Change-Id: Id52c0ba110aa915e77859585c2cbfd4d529fe3ca

--- src/mdlib/forcerec.c
+++ src/mdlib/forcerec.c
@@ -2213,6 +2213,25 @@ void init_forcerec(FILE              *fp,
     fr->AllvsAll_work   = NULL;
     fr->AllvsAll_workgb = NULL;
 
+    /* All-vs-all kernels have not been implemented in 4.6, and
+     * the SIMD group kernels are also buggy in this case. Non-accelerated
+     * group kernels are OK. See Redmine #1249. */
+    if (fr->bAllvsAll)
+    {
+        fr->bAllvsAll = FALSE;
+        fr->use_cpu_acceleration = FALSE;
+        if (fp != NULL)
+        {
+            fprintf(fp,
+                    "\nYour simulation settings would have triggered the efficient all-vs-all\n"
+                    "kernels in GROMACS 4.5, but these have not been implemented in GROMACS\n"
+                    "4.6. Also, we can't use the accelerated SIMD kernels here because\n"
+                    "of an unfixed bug. The reference C kernels are correct, though, so\n"
+                    "we are proceeding by disabling all CPU architecture-specific\n"
+                    "(e.g. SSE2/SSE4/AVX) routines. If performance is important, please\n"
+                    "use GROMACS 4.5.7 or try cutoff-scheme = Verlet.\n\n");
+        }
+    }
 
     /* Neighbour searching stuff */
     fr->cutoff_scheme = ir->cutoff_scheme;
