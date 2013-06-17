4e6b680cb88167ef0ca0785608b151f65a5f537c
Mark Abraham <mark.j.abraham@gmail.com>
2013-06-17 16:32:48 +0200

    [WIP] Introduce tests for some SIMD functionality.
    This will detect various known compiler bugs with SIMD intrinsics, and
    permit TDD for new SIMD architectures.
    
    Most of the tests repetitively compare the output of the SIMD wrapper
    macros/functions with the reference SIMD implementation on random
    input, rather than caching known good results. That may or may not be
    a good idea in itself, but the machinery will serve the secondary
    purpose of allowing experimentation with rational-function
    approximations, etc.
    
    Changed the behaviour of gromacs/simd/macros.h and
    gromacs/simd/four_wide_macros.h so that they can be #included more
    than once.
    
    Broke up nbnxn_atomdata.c to be more test-friendly.
    
    TODO:
    
    See if we can avoid modifying gtest.
    
    Work out how to avoid recompiling all the content in base.h and
    general.h for almost all test fixtures.
    
    Expand NBNxN kernel tests to test all cases, via parameterized Google
    tests?  (see #1373)
    
    Test NBNxN kernels against reference implementation instead/also?
    
    Later, do similar stuff so that we can stop testing the group kernels
    in regressiontests.
    
    Decide if this commit should get split up (e.g. kernel tests separate)
    
    See if we can get the value-parameterized tests to generate
    better names for test fixtures and output files, since we have the
    matching enums already.
    
    Change reference-SIMD code path to be triggerable by a build type
    
    Other TODOs inline, no doubt!
    
    Fixes #1373
    
    Change-Id: I6c8e252075f57e4f7aa89a7830c9c04b3a278ea7

--- src/gromacs/mdlib/nbnxn_kernels/nbnxn_kernel_simd_utils_ref.h
+++ src/gromacs/mdlib/nbnxn_kernels/nbnxn_kernel_simd_utils_ref.h
@@ -38,7 +38,6 @@
 #define _nbnxn_kernel_simd_utils_ref_h_
 
 typedef gmx_simd_ref_epi32      gmx_simd_ref_exclfilter;
-typedef gmx_simd_ref_exclfilter gmx_exclfilter;
 static const int filter_stride = GMX_SIMD_EPI32_WIDTH/GMX_SIMD_WIDTH_HERE;
 
 /* Set the stride for the lookup of the two LJ parameters from their
@@ -57,13 +56,13 @@ static const int nbfp_stride = 4;
 /* float/double SIMD register type */
 typedef struct {
     real r[4];
-} gmx_mm_pr4;
+} gmx_simd_ref_pr4;
 
-static gmx_inline gmx_mm_pr4
+static gmx_inline gmx_simd_ref_pr4
 gmx_load_pr4(const real *r)
 {
-    gmx_mm_pr4 a;
-    int        i;
+    gmx_simd_ref_pr4 a;
+    int              i;
 
     for (i = 0; i < 4; i++)
     {
@@ -74,10 +73,9 @@ gmx_load_pr4(const real *r)
 }
 
 static gmx_inline void
-gmx_store_pr4(real *dest, gmx_mm_pr4 src)
+gmx_store_pr4(real *dest, gmx_simd_ref_pr4 src)
 {
-    gmx_mm_pr4 a;
-    int        i;
+    int              i;
 
     for (i = 0; i < 4; i++)
     {
@@ -85,11 +83,11 @@ gmx_store_pr4(real *dest, gmx_mm_pr4 src)
     }
 }
 
-static gmx_inline gmx_mm_pr4
-gmx_add_pr4(gmx_mm_pr4 a, gmx_mm_pr4 b)
+static gmx_inline gmx_simd_ref_pr4
+gmx_add_pr4(gmx_simd_ref_pr4 a, gmx_simd_ref_pr4 b)
 {
-    gmx_mm_pr4 c;
-    int        i;
+    gmx_simd_ref_pr4 c;
+    int              i;
 
     for (i = 0; i < 4; i++)
     {
@@ -114,13 +112,13 @@ typedef gmx_simd_ref_pr gmx_simd_ref_pr4;
 /* float/double SIMD register type */
 typedef struct {
     real r[GMX_SIMD_WIDTH_HERE/2];
-} gmx_mm_hpr;
+} gmx_simd_ref_hpr;
 
 /* Half-width SIMD operations */
 
 /* Load reals at half-width aligned pointer b into half-width SIMD register a */
 static gmx_inline void
-gmx_load_hpr(gmx_mm_hpr *a, const real *b)
+gmx_load_hpr(gmx_simd_ref_hpr *a, const real *b)
 {
     int i;
 
@@ -132,7 +130,7 @@ gmx_load_hpr(gmx_mm_hpr *a, const real *b)
 
 /* Set all entries in half-width SIMD register *a to b */
 static gmx_inline void
-gmx_set1_hpr(gmx_mm_hpr *a, real b)
+gmx_set1_hpr(gmx_simd_ref_hpr *a, real b)
 {
     int i;
 
@@ -170,7 +168,7 @@ gmx_loaddh_pr(gmx_simd_ref_pr *a, const real *b)
 
 /* Store half-width SIMD register b into half width aligned memory a */
 static gmx_inline void
-gmx_store_hpr(real *a, gmx_mm_hpr b)
+gmx_store_hpr(real *a, gmx_simd_ref_hpr b)
 {
     int i;
 
@@ -180,11 +178,11 @@ gmx_store_hpr(real *a, gmx_mm_hpr b)
     }
 }
 
-static gmx_inline gmx_mm_hpr
-gmx_add_hpr(gmx_mm_hpr a, gmx_mm_hpr b)
+static gmx_inline gmx_simd_ref_hpr
+gmx_add_hpr(gmx_simd_ref_hpr a, gmx_simd_ref_hpr b)
 {
-    gmx_mm_hpr c;
-    int        i;
+    gmx_simd_ref_hpr c;
+    int              i;
 
     for (i = 0; i < GMX_SIMD_WIDTH_HERE/2; i++)
     {
@@ -194,11 +192,11 @@ gmx_add_hpr(gmx_mm_hpr a, gmx_mm_hpr b)
     return c;
 }
 
-static gmx_inline gmx_mm_hpr
-gmx_sub_hpr(gmx_mm_hpr a, gmx_mm_hpr b)
+static gmx_inline gmx_simd_ref_hpr
+gmx_sub_hpr(gmx_simd_ref_hpr a, gmx_simd_ref_hpr b)
 {
-    gmx_mm_hpr c;
-    int        i;
+    gmx_simd_ref_hpr c;
+    int              i;
 
     for (i = 0; i < GMX_SIMD_WIDTH_HERE/2; i++)
     {
@@ -209,11 +207,11 @@ gmx_sub_hpr(gmx_mm_hpr a, gmx_mm_hpr b)
 }
 
 /* Sum over 4 half SIMD registers */
-static gmx_inline gmx_mm_hpr
+static gmx_inline gmx_simd_ref_hpr
 gmx_sum4_hpr(gmx_simd_ref_pr a, gmx_simd_ref_pr b)
 {
-    gmx_mm_hpr c;
-    int        i;
+    gmx_simd_ref_hpr c;
+    int              i;
 
     for (i = 0; i < GMX_SIMD_WIDTH_HERE/2; i++)
     {
@@ -228,11 +226,11 @@ gmx_sum4_hpr(gmx_simd_ref_pr a, gmx_simd_ref_pr b)
 }
 
 /* Sum the elements of halfs of each input register and store sums in out */
-static gmx_inline gmx_mm_pr4
-gmx_mm_transpose_sum4h_pr(gmx_simd_ref_pr a, gmx_simd_ref_pr b)
+static gmx_inline gmx_simd_ref_pr4
+gmx_simd_ref_transpose_sum4h_pr(gmx_simd_ref_pr a, gmx_simd_ref_pr b)
 {
-    gmx_mm_pr4 sum;
-    int        i;
+    gmx_simd_ref_pr4 sum;
+    int              i;
 
     sum.r[0] = 0;
     sum.r[1] = 0;
@@ -251,7 +249,7 @@ gmx_mm_transpose_sum4h_pr(gmx_simd_ref_pr a, gmx_simd_ref_pr b)
 }
 
 static gmx_inline void
-gmx_pr_to_2hpr(gmx_simd_ref_pr a, gmx_mm_hpr *b, gmx_mm_hpr *c)
+gmx_pr_to_2hpr(gmx_simd_ref_pr a, gmx_simd_ref_hpr *b, gmx_simd_ref_hpr *c)
 {
     int i;
 
@@ -262,7 +260,7 @@ gmx_pr_to_2hpr(gmx_simd_ref_pr a, gmx_mm_hpr *b, gmx_mm_hpr *c)
     }
 }
 static gmx_inline void
-gmx_2hpr_to_pr(gmx_mm_hpr a, gmx_mm_hpr b, gmx_simd_ref_pr *c)
+gmx_2hpr_to_pr(gmx_simd_ref_hpr a, gmx_simd_ref_hpr b, gmx_simd_ref_pr *c)
 {
     int i;
 
@@ -289,7 +287,7 @@ load_table_f(const real *tab_coul_F, gmx_simd_ref_epi32 ti_S, int *ti,
         ctab1_S->r[i] = tab_coul_F[ti_S.r[i]+1];
     }
 
-    *ctab1_S  = gmx_sub_pr(*ctab1_S, *ctab0_S);
+    *ctab1_S  = gmx_simd_ref_sub_pr(*ctab1_S, *ctab0_S);
 }
 
 static gmx_inline void
@@ -341,11 +339,11 @@ load_table_f_v(const real *tab_coul_FDV0,
 #endif
 
 /* Sum the elements within each input register and store the sums in out.
- * Note that 4/8-way SIMD requires gmx_mm_transpose_sum4_pr instead.
+ * Note that 4/8-way SIMD requires gmx_simd_ref_transpose_sum4_pr instead.
  */
 #if GMX_SIMD_WIDTH_HERE == 2
 static gmx_inline gmx_simd_ref_pr
-gmx_mm_transpose_sum2_pr(gmx_simd_ref_pr in0, gmx_simd_ref_pr in1)
+gmx_simd_ref_transpose_sum2_pr(gmx_simd_ref_pr in0, gmx_simd_ref_pr in1)
 {
     gmx_simd_ref_pr sum;
 
@@ -360,17 +358,17 @@ gmx_mm_transpose_sum2_pr(gmx_simd_ref_pr in0, gmx_simd_ref_pr in1)
 #if GMX_SIMD_WIDTH_HERE == 4
 static gmx_inline gmx_simd_ref_pr
 #else
-static gmx_inline gmx_mm_pr4
+static gmx_inline gmx_simd_ref_pr4
 #endif
-gmx_mm_transpose_sum4_pr(gmx_simd_ref_pr in0, gmx_simd_ref_pr in1,
-                         gmx_simd_ref_pr in2, gmx_simd_ref_pr in3)
+gmx_simd_ref_transpose_sum4_pr(gmx_simd_ref_pr in0, gmx_simd_ref_pr in1,
+                               gmx_simd_ref_pr in2, gmx_simd_ref_pr in3)
 {
 #if GMX_SIMD_WIDTH_HERE == 4
-    gmx_simd_ref_pr sum;
+    gmx_simd_ref_pr  sum;
 #else
-    gmx_mm_pr4      sum;
+    gmx_simd_ref_pr4 sum;
 #endif
-    int             i;
+    int              i;
 
     sum.r[0] = 0;
     sum.r[1] = 0;
@@ -396,11 +394,11 @@ gmx_mm_transpose_sum4_pr(gmx_simd_ref_pr in0, gmx_simd_ref_pr in1,
  * For this reference code we just use a plain-C sqrt.
  */
 static gmx_inline void
-gmx_mm_invsqrt2_pd(gmx_simd_ref_pr in0, gmx_simd_ref_pr in1,
-                   gmx_simd_ref_pr *out0, gmx_simd_ref_pr *out1)
+gmx_simd_ref_invsqrt2_pd(gmx_simd_ref_pr in0, gmx_simd_ref_pr in1,
+                         gmx_simd_ref_pr *out0, gmx_simd_ref_pr *out1)
 {
-    out0 = gmx_invsqrt_pr(in0);
-    out1 = gmx_invsqrt_pr(in1);
+    *out0 = gmx_simd_ref_rsqrt_pr(in0);
+    *out1 = gmx_simd_ref_rsqrt_pr(in1);
 }
 #endif
 
@@ -439,10 +437,6 @@ load_lj_pair_params2(const real *nbfp0, const real *nbfp1,
    interactions. The x86 code might use either integer- or real-type
    SIMD, but the reference code does not need to know. */
 
-#define gmx_load1_exclfilter(e)       gmx_simd_ref_load1_exclfilter(e)
-#define gmx_load_exclusion_filter(e)  gmx_simd_ref_load_exclusion_filter((int *) e)
-#define gmx_checkbitmask_pb(m0, m1)   gmx_simd_ref_checkbitmask_pb(m0, m1)
-
 static gmx_inline gmx_simd_ref_exclfilter
 gmx_simd_ref_load1_exclfilter(int src)
 {
@@ -458,7 +452,7 @@ gmx_simd_ref_load1_exclfilter(int src)
 }
 
 static gmx_inline gmx_simd_ref_exclfilter
-gmx_simd_ref_load_exclusion_filter(const int *src)
+gmx_simd_ref_load_exclusion_filter(const unsigned *src)
 {
     gmx_simd_ref_exclfilter a;
     int                     i;
