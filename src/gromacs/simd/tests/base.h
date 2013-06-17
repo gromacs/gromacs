04822292f512aa1c2dbd0eff0044ccc49070605d
Mark Abraham <mark.j.abraham@gmail.com>
2013-06-17 16:32:48 +0200

    [WIP] Introduce tests for some SIMD functionality.
    This will detect various known compiler bugs with SIMD intrinsics, and
    permit TDD for new SIMD architectures.
    
    Most of the tests repetitively compare the output of the SIMD wrapper
    macros/functions with the reference SIMD implementation on random
    input, rather than caching known good results. That may or may not be
    a good idea in itself, but the machinery will serve the secondary
    purpose of allowing experimentation with rational-function
    approximations, etc.
    
    Changed the behaviour of gromacs/simd/macros.h and
    gromacs/simd/four_wide_macros.h so that they can be #included more
    than once.
    
    Broke up nbnxn_atomdata.c to be more test-friendly.
    
    TODO:
    
    See if we can avoid modifying gtest.
    
    Work out how to avoid recompiling all the content in base.h and
    general.h for almost all test fixtures.
    
    Expand NBNxN kernel tests to test all cases, via parameterized Google
    tests?  (see #1373)
    
    Test NBNxN kernels against reference implementation instead/also?
    
    Later, do similar stuff so that we can stop testing the group kernels
    in regressiontests.
    
    Decide if this commit should get split up (e.g. kernel tests separate)
    
    See if we can get the value-parameterized tests to generate
    better names for test fixtures and output files, since we have the
    matching enums already.
    
    Change reference-SIMD code path to be triggerable by a build type
    
    Other TODOs inline, no doubt!
    
    Fixes #1373
    
    Change-Id: I6c8e252075f57e4f7aa89a7830c9c04b3a278ea7

---
+++ src/gromacs/simd/tests/base.h
@@ -0,0 +1,355 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2012, by the GROMACS development team, led by
+ * David van der Spoel, Berk Hess, Erik Lindahl, and including many
+ * others, as listed in the AUTHORS file in the top-level source
+ * directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Classes to support tests of SIMD functionality
+ *
+ * \author Mark Abraham <mark.j.abraham@gmail.com>
+ * \ingroup module_simd
+ */
+
+#ifndef _simd_tests_base_h_
+#define _simd_tests_base_h_
+
+#include "typedefs.h"
+#include "smalloc.h"
+#include "gromacs/legacyheaders/gmx_random.h"
+#include "testutils/testoptions.h"
+#include "gromacs/options/options.h"
+#include "gromacs/options/basicoptions.h"
+#include "utils.h"
+#include <maths.h>
+
+namespace SIMDTests
+{
+
+/*! We use some namespaces to be sure the test and reference versions
+   of function names and constants cannot collide.
+
+   Note that the declaration of the test functions should precede that
+   of the reference functions, so that macros.h defines
+   GMX_SIMD_WIDTH_HERE, and the reference code can know to use that
+   width. */
+#include "gromacs/simd/macros.h"
+
+namespace ReferenceFunctions
+{
+#define GMX_SIMD_REFERENCE_PLAIN_C
+#include "gromacs/simd/macros_ref.h"
+#undef GMX_SIMD_REFERENCE_PLAIN_C
+}
+
+/********************************************************************
+ * Tests for SIMD wrapper functions
+ */
+
+/*! \brief Base class to encapsulate all the SIMD support functions
+ * needed for testing SIMD wrapper functions. Not intended to be
+ * instantiated.
+ *
+ * Contains stubs for functions that re-wrap the SIMD wrapper
+ * functions. Declaring these is not really necessary, but they
+ * describe the interface to which the derived classes should adhere
+ * if they want to be called by the callFunction() functions in
+ * classes derived from SimdFunctionTestBase. These functions here
+ * could be made pure virtual, but there's no code that actually uses
+ * dynamic dispatch.
+ */
+template<class _realType, class _epiType, class _boolType>
+class SimdFunctionSetBase
+{
+    public:
+        //! Convenience typedef
+        typedef _realType realType;
+        //! Convenience typedef
+        typedef _epiType epiType;
+        //! Convenience typedef
+        typedef _boolType boolType;
+
+        //! Wrapper function to load a SIMD real
+        realType load_pr(const real *a);
+        //! Wrapper function to compare SIMD reals
+        boolType cmplt_pr(realType a, realType b);
+        //! Wrapper function to store a SIMD real
+        void store_pr(real *a, realType b);
+        //! Wrapper function to convert a SIMD real to a SIMD integer
+        epiType cvttpr_epi32(realType a);
+};
+
+namespace TestFunctions
+{
+
+/*! \brief Class to encapsulate all the support functions needed
+ * for testing SIMD wrapper functions on x86. */
+class x86SimdFunctionSet :
+    public SimdFunctionSetBase<gmx_mm_pr, gmx_epi32, gmx_mm_pb>
+{
+    public:
+        realType load_pr(const real *a)
+        {
+            return gmx_load_pr(a);
+        }
+
+        boolType cmplt_pr(realType a, realType b)
+        {
+            return gmx_cmplt_pr(a, b);
+        };
+
+        void store_pr(real *a, realType b)
+        {
+            return gmx_store_pr(a, b);
+        }
+
+        epiType
+        cvttpr_epi32(realType a)
+        {
+            return gmx_cvttpr_epi32(a);
+        }
+
+        static real getSimdBool(bool a)
+        {
+            BitManipulater manipulater;
+            manipulater.i = a ? simdTrue : simdFalse;
+            return manipulater.r;
+        }
+
+        static bool isSimdTrue(real a)
+        {
+            /* A direct comparison of the real is impossible, because
+               x86 SIMD true is a NaN, and that must compare false. */
+            BitManipulater manipulater;
+            manipulater.r = a;
+            return simdTrue == manipulater.i;
+        }
+
+        static bool isSimdFalse(real a)
+        {
+            /* A direct comparison of the real is impossible, because
+               x86 SIMD true is a NaN, and that must compare false. */
+            BitManipulater manipulater;
+            manipulater.r = a;
+            return simdFalse == manipulater.i;
+        }
+
+        //! Convenience function to map a value from one form of SIMD
+        //! boolean to that of the current hardware
+        /*
+           boolType mapSimdBooleanToHardwareSimdBoolean(boolType const &a)
+           {
+            return a;
+           };
+         */
+
+        static const UnsignedIntWithSizeOfReal simdTrue;
+        static const UnsignedIntWithSizeOfReal simdFalse;
+};
+
+#ifdef GMX_X86_SSE2
+
+//! Convenience typedef
+typedef x86SimdFunctionSet HardwareSimdFunctionSet;
+
+#endif  /* GMX_X86_SSE2 */
+
+/*! \brief Class to encapsulate all the support functions needed
+ * for testing SIMD wrapper functions on QPX (i.e. A2 core of BlueGene/Q). */
+class QpxSimdFunctionSet :
+    public SimdFunctionSetBase<gmx_mm_pr, gmx_epi32, gmx_mm_pb>
+{
+    public:
+        realType load_pr(const real *a)
+        {
+            return gmx_load_pr(a);
+        }
+
+        boolType cmplt_pr(realType a, realType b)
+        {
+            return gmx_cmplt_pr(a, b);
+        };
+
+        void store_pr(real *a, realType b)
+        {
+            return gmx_store_pr(a, b);
+        }
+
+        epiType
+        cvttpr_epi32(realType a)
+        {
+            return gmx_cvttpr_epi32(a);
+        }
+
+        static real getSimdBool(bool a)
+        {
+            return (real) (a ? simdTrue : simdFalse);
+        }
+
+        static bool isSimdTrue(real a)
+        {
+            return simdTrue == a;
+        }
+
+        static bool isSimdFalse(real a)
+        {
+            return simdFalse == a;
+        }
+
+        //! Convenience function to map a value from one form of SIMD
+        //! boolean to that of the current hardware
+        /*
+           boolType mapSimdBooleanToHardwareSimdBoolean(boolType const &a)
+           {
+            return a;
+           };
+         */
+
+        static const real simdTrue;
+        static const real simdFalse;
+};
+
+#ifdef GMX_CPU_ACCELERATION_IBM_QPX
+
+typedef TestFunctions::QpxSimdFunctionSet HardwareSimdFunctionSet;
+
+#endif  /* GMX_CPU_ACCELERATION_IBM_QPX */
+
+}
+
+namespace ReferenceFunctions
+{
+
+/*! \brief Class to encapsulate all the support functions needed
+ * for testing the reference implementation of the SIMD wrapper
+ * functions. */
+class ReferenceSimdFunctionSet :
+    public SimdFunctionSetBase<ReferenceFunctions::gmx_simd_ref_pr,
+                               ReferenceFunctions::gmx_simd_ref_epi32,
+                               ReferenceFunctions::gmx_simd_ref_pb>
+{
+    public:
+        realType load_pr(const real *a)
+        {
+            return ReferenceFunctions::gmx_simd_ref_load_pr(a);
+        }
+
+        boolType cmplt_pr(realType a, realType b)
+        {
+            return ReferenceFunctions::gmx_simd_ref_cmplt_pr(a, b);
+        };
+
+        void store_pr(real *a, realType b)
+        {
+            return ReferenceFunctions::gmx_simd_ref_store_pr(a, b);
+        }
+
+        epiType
+        cvttpr_epi32(realType a)
+        {
+            return ReferenceFunctions::gmx_simd_ref_cvttpr_epi32(a);
+        }
+};
+
+}
+
+/*! \brief Class for functor for templated aligned allocation for SIMD
+ *  tests
+ *
+ * \todo If/when we have containers of SIMD-aligned reals, use them
+ * here.
+ */
+template <class InputType>
+struct AlignedSimdAllocater
+{
+    AlignedSimdAllocater(size_t size) : size_(size) {};
+
+    void operator() (InputType * &pointer)
+    {
+        snew_aligned(pointer,
+                     GMX_SIMD_WIDTH_HERE * size_,
+                     GMX_SIMD_WIDTH_HERE * sizeof(InputType));
+    }
+
+    size_t size_;
+};
+
+/*! \brief Base class for test fixtures for SIMD functions
+ *
+ * \todo If/when we have containers of SIMD-aligned reals, use them
+ * here and in derived classes.
+ */
+class SimdFunctionTestBase :
+    public ::testing::Test
+{
+    public:
+        //! Convenience typedef
+        typedef ::testing::Test Parent;
+        //! Convenience typedef
+        typedef AlignedSimdAllocater<real> OutputAllocater;
+
+        SimdFunctionTestBase() :
+            Parent(), numOutputs_(0)
+        {
+            /* This can be set on the command line with
+               --gtest_random_seed=SEED. Zero is the GoogleTest
+               default that means "seed based on the current time." */
+            rng_ = gmx_rng_init(testing::UnitTest::GetInstance()->random_seed());
+        }
+
+        ~SimdFunctionTestBase()
+        {
+            gmx_rng_destroy(rng_);
+            sfree(referenceResult_);
+            sfree(testResult_);
+        }
+
+        //! Method object for reference SIMD functions
+        ReferenceFunctions::ReferenceSimdFunctionSet referenceSimd_;
+        //! Method object for hardware SIMD functions
+        TestFunctions::HardwareSimdFunctionSet       hardwareSimd_;
+
+        //! Number of output vectors from the SIMD function under test
+        size_t    numOutputs_;
+        //! Random number generator
+        gmx_rng_t rng_;
+        /*! Vector to store the concatenated output vectors from the
+           test function for later comparison. */
+        real     *testResult_;
+        /*! Vector to store the concatenated output vectors from the
+           reference function for later comparison. */
+        real     *referenceResult_;
+};
+
+}      // namespace
+
+#endif /* _simd_tests_base_h_ */
---
+++ src/gromacs/simd/tests/load.cpp
@@ -0,0 +1,91 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2012, by the GROMACS development team, led by
+ * David van der Spoel, Berk Hess, Erik Lindahl, and including many
+ * others, as listed in the AUTHORS file in the top-level source
+ * directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Tests for functionality that loads memory value(s) to a SIMD vector.
+ *
+ * \author Mark Abraham <mark.j.abraham@gmail.com>
+ * \ingroup module_simd
+ */
+
+#include "general.h"
+
+namespace SIMDTests
+{
+
+/* It's hard to test load and store independently, since you need one
+   of them to be in a position to test the other. If a test here fails
+   because of store_pr, so will almost all the others. If a test here
+   fails because of the load function under test, then those other
+   tests that don't load a real may still pass. */
+
+//! Typedef for the test fixture
+typedef SimdFunctionTest<real> SimdFunctionLoad;
+
+template<class SimdFunctionSet,
+         typename FunctionType> void
+callFunction(SimdFunctionSet     &simdFunctionSet,
+             FunctionType         function,
+             std::vector<real*>   inputs,
+             real                *result)
+{
+    typename SimdFunctionSet::realType result_pr;
+
+    result_pr = function(inputs[0]);
+    simdFunctionSet.store_pr(result, result_pr);
+}
+
+TEST_F(SimdFunctionLoad, gmx_load_pr_Works)
+{
+    prepare(1, 1);
+    RunTest(ReferenceFunctions::gmx_simd_ref_load_pr,
+            gmx_load_pr, real(0));
+}
+
+/* Wrapper function because the macro wrapped is defined with a
+   parameter. */
+static gmx_inline gmx_mm_pr gmx_load1_pr_wrapper(const real *a)
+{
+    return gmx_load1_pr(a);
+}
+
+TEST_F(SimdFunctionLoad, gmx_load1_pr_Works)
+{
+    prepare(1, 1);
+    RunTest(ReferenceFunctions::gmx_simd_ref_load1_pr,
+            gmx_load1_pr_wrapper, real(0));
+}
+
+} // namespace
