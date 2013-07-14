47264c9df6fae7db14876c7047032a80325f40f9
Alexey Shvetsov <alexxy@omrb.pnpi.spb.ru>
2013-07-14 16:57:39 +0400

    nb_kernel_sse2_single: clean up -Wunused-parameter warnings
    Clean up (most) of unused params
    
    Change-Id: I878b5c89378eec7f88304fda23dd8747a348a8a9
    Signed-off-by: Alexey Shvetsov <alexxy@omrb.pnpi.spb.ru>

--- src/gromacs/gmxlib/nonbonded/nb_kernel_sse2_single/nb_kernel_ElecEwSh_VdwNone_GeomW4W4_sse2_single.c
+++ src/gromacs/gmxlib/nonbonded/nb_kernel_sse2_single/nb_kernel_ElecEwSh_VdwNone_GeomW4W4_sse2_single.c
@@ -1401,7 +1401,7 @@ nb_kernel_ElecEwSh_VdwNone_GeomW4W4_F_sse2_single
                      rvec * gmx_restrict                    ff,
                      t_forcerec * gmx_restrict              fr,
                      t_mdatoms * gmx_restrict               mdatoms,
-                     nb_kernel_data_t * gmx_restrict        kernel_data,
+                     nb_kernel_data_t gmx_unused * gmx_restrict kernel_data,
                      t_nrnb * gmx_restrict                  nrnb)
 {
     /* Suffixes 0,1,2,3 refer to particle indices for waters in the inner or outer loop, or 
