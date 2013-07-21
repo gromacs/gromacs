828ad7f352d6a6193074b88fe8bd09b4336de046
Teemu Murtola <teemu.murtola@gmail.com>
2013-07-21 08:34:18 +0300

    Better command-line option handling for tests.
    The options are now registered using global initializers (somewhat ugly,
    but is the same mechanism as used by GTest for registering the test
    cases).  This makes them all available on startup in initTestUtils(),
    allowing it to print help text for them.  It also properly allows having
    multiple option definitions in the same binary.  This did compile with
    the previous approach as well, but didn't always work because of
    limitations in the command-line parser.
    
    The command-line parser now accepts, but does not require, a GNU-style
    -- prefix for command-line options, and gives an error for unknown
    options starting with --.  While at it, also made the parser accept
    options that start with digits, and even options consisting of only
    digits (the latter requires one to use a double dash).
    
    Add a helper for ignoring return values of
    __attribute__((warn_unused_result)) functions (works only for C++), and
    remove unused function from ngmx.cpp that had previously had a dummy
    variable for this purpose (the dummy variable was removed when
    converting to C++).
    
    Change-Id: Id1674510c4feb88ffc1d193274a6357cebef3916

--- src/gromacs/commandline/cmdlineparser.h
+++ src/gromacs/commandline/cmdlineparser.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013, by the GROMACS development team, led by
  * David van der Spoel, Berk Hess, Erik Lindahl, and including many
  * others, as listed in the AUTHORS file in the top-level source
  * directory and at http://www.gromacs.org.
@@ -94,26 +94,6 @@ class CommandLineParser
          * implemented.
          */
         void parse(int *argc, char *argv[]);
-        /*! \brief
-         * Parses the command line from a std::vector.
-         *
-         * \param[in] commandLine  Array of command-line strings.
-         * \throws  std::bad_alloc if out of memory.
-         * \throws  InvalidInputError if any errors were detected in the input.
-         *
-         * \p commandLine should relate to the standard \c argv array
-         * one-to-one.
-         *
-         * This method is provided for convenience for cases where the command
-         * line needs to be stored before parsing.
-         *
-         * Currently, the input parameters are not modified, but this may
-         * change if/when support for parsing only part of the options is
-         * implemented.
-         *
-         * \see parse(int *, char *[])
-         */
-        void parse(std::vector<std::string> *commandLine);
 
     private:
         class Impl;
