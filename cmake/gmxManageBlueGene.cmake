235c9b72cc387fb7f2d6368e67c73d7a321ee79d
Mark Abraham <mark.j.abraham@gmail.com>
2013-07-26 10:36:35 +0200

    Fixes for Bluegene/Q
    * Moved suffixing code to after where gmxManageBlueGene sets
      GMX_MPI. Since nothing depends on the suffixing until the
      calls to add_subdirectory(), this is safe. This change means
      that the binaries are built with the _mpi suffix like users
      have come to expect everywhere else.
    * Removed FORCE on set(GMX_MPI CACHE) for BlueGene/Q. It should be on
      by default and possible to turn off. This change makes it possible
      to build a ThreadMPI mdrun, which is (in principle) useful.
    
    Change-Id: I26cb782a1b47cf48f47b80ec8c8d0a53db338872

--- cmake/gmxManageBlueGene.cmake
+++ cmake/gmxManageBlueGene.cmake
@@ -60,7 +60,7 @@ set(GMX_GPU OFF CACHE BOOL "Cannot do GPU acceleration on BlueGene" FORCE)
 # certainly need proper MPI to use a whole chunk of the machine that
 # the scheduler will allocate.
 set(GMX_THREAD_MPI OFF CACHE BOOL "Thread-MPI generally not compatible with BlueGene, defaulting to disabled!")
-set(GMX_MPI ON CACHE BOOL "MPI is normally required on BlueGene" FORCE)
+set(GMX_MPI ON CACHE BOOL "MPI is normally required on BlueGene")
 
 # Access to /etc/passwd is not available on the back end of BlueGeneP
 # (at least), despite being detected by CMake. This can cause linker
