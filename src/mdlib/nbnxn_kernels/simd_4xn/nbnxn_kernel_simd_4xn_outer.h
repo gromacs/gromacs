eed3a6cb3843d81e2298b35fafc184acc8284eff
Mark Abraham <mark.j.abraham@gmail.com>
2013-08-19 18:33:55 +0200

    BlueGene/Q Verlet cut-off scheme kernels
    The kernels are implemented with either macros or small functions,
    which can be selected with a #define. A limitation in XLC prevents
    full inlining in the larger kernels, so we are stuck with macros until
    they fix it. bgclang might be able to use them effectively, however.
    
    It is always better to use the analytical Ewald correction.
    
    Other BG/Q considerations:
    
    Architecture detection now works on A2 core.
    
    Install guide updated.
    
    It is better to use intra-node communicators than not, and ranks
    within nodes are correctly detected via querying the BlueGene/Q API,
    since the hostname is not useful for the purpose.
    
    It is better to not set GMX_DD_SENDRECV2.
    
    In principle, we should version the type of variables and fields named
    d2, rl2, rbb2 in nbnxn_search*[ch] to be double on PowerPC and float
    everywhere else (each regardless of GROMACS target precision). This
    would mean that on PowerPC (where all flops take place in double
    precision with free precision-extension upon load) we can be both
    cache-efficient by storing bounding boxes in float, and flop-efficient
    by not having to generate a round-to-single instruction to compare the
    result of subc_bb_dist2_simd4 with the cut-off stored as a
    float. Still, a flop per bounding-box distance comparison will not
    break the bank.
    
    Enough bgclang support exists for the build to succeed, even with OpenMP,
    but a number of compiler issues have been reported on llvm-bgq-discuss.
    
    Change-Id: I98c5791ec3766cdbdcb8a8eb7418d00585727cc0

--- src/mdlib/nbnxn_kernels/simd_4xn/nbnxn_kernel_simd_4xn_outer.h
+++ src/mdlib/nbnxn_kernels/simd_4xn/nbnxn_kernel_simd_4xn_outer.h
@@ -75,10 +75,10 @@
 #if UNROLLJ >= 4
     /* We use an i-force SIMD register width of 4 */
 #if UNROLLJ == 4
-#define gmx_mm_pr4     gmx_mm_pr
-#define gmx_load_pr4   gmx_load_pr
-#define gmx_store_pr4  gmx_store_pr
-#define gmx_add_pr4    gmx_add_pr
+#define gmx_mm_pr4    gmx_mm_pr
+#define gmx_load_pr4  gmx_load_pr
+#define gmx_store_pr4 gmx_store_pr
+#define gmx_add_pr4   gmx_add_pr
 #else
     /* The pr4 stuff is defined in nbnxn_kernel_simd_utils.h */
 #endif
@@ -100,16 +100,16 @@
     unsigned      *exclusion_filter;
     gmx_exclfilter filter_S0, filter_S1, filter_S2, filter_S3;
 
-    gmx_mm_pr  zero_S = gmx_set1_pr(0);
+    gmx_mm_pr      zero_S = gmx_set1_pr(0.0);
 
-    gmx_mm_pr  one_S  = gmx_set1_pr(1.0);
-    gmx_mm_pr  iq_S0  = gmx_setzero_pr();
-    gmx_mm_pr  iq_S1  = gmx_setzero_pr();
-    gmx_mm_pr  iq_S2  = gmx_setzero_pr();
-    gmx_mm_pr  iq_S3  = gmx_setzero_pr();
-    gmx_mm_pr  mrc_3_S;
+    gmx_mm_pr      one_S  = gmx_set1_pr(1.0);
+    gmx_mm_pr      iq_S0  = gmx_setzero_pr();
+    gmx_mm_pr      iq_S1  = gmx_setzero_pr();
+    gmx_mm_pr      iq_S2  = gmx_setzero_pr();
+    gmx_mm_pr      iq_S3  = gmx_setzero_pr();
+    gmx_mm_pr      mrc_3_S;
 #ifdef CALC_ENERGIES
-    gmx_mm_pr  hrc_3_S, moh_rc_S;
+    gmx_mm_pr      hrc_3_S, moh_rc_S;
 #endif
 
 #ifdef CALC_COUL_TAB
