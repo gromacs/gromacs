b84863b9196da6582492bedf69d18a037b04fd67
Berk Hess <hess@kth.se>
2013-08-21 17:30:36 +0200

    [WIP] intermolecular bonded interaction support added
    The .top file can now contain an [ intermolecular_interactions ]
    directive, after which bonded interactions can be entered using
    global atom indices.
    
    NOTE: This patch is supposed to be fully functional, but some extra
    coding or error checking might be required for exotic cases.
    
    Change-Id: I383287dd0729fef1c54f27a4fe9f5d628445549c

--- src/gromacs/gmxlib/tpxio.c
+++ src/gromacs/gmxlib/tpxio.c
@@ -73,7 +73,7 @@
 static const char *tpx_tag = TPX_TAG_RELEASE;
 
 /* This number should be increased whenever the file format changes! */
-static const int tpx_version = 92;
+static const int tpx_version = 93;
 
 /* This number should only be increased when you edit the TOPOLOGY section
  * or the HEADER of the tpx format.
@@ -85,7 +85,7 @@ static const int tpx_version = 92;
  * to the end of the tpx file, so we can just skip it if we only
  * want the topology.
  */
-static const int tpx_generation = 25;
+static const int tpx_generation = 26;
 
 /* This number should be the most recent backwards incompatible version
  * I.e., if this number is 9, we cannot read tpx version 9 with this code.
@@ -2797,6 +2797,23 @@ static void do_mtop(t_fileio *fio, gmx_mtop_t *mtop, gmx_bool bRead,
         mtop->molblock[0].natoms_mol = mtop->moltype[0].atoms.nr;
         mtop->molblock[0].nposres_xA = 0;
         mtop->molblock[0].nposres_xB = 0;
+    }
+
+    if (file_version >= 93)
+    {
+        gmx_fio_do_gmx_bool(fio, mtop->bIntermolecularInteractions);
+        if (mtop->bIntermolecularInteractions)
+        {
+            if (bRead)
+            {
+                snew(mtop->intermolecular_ilist, F_NRE);
+            }
+            do_ilists(fio, mtop->intermolecular_ilist, bRead, file_version);
+        }
+    }
+    else
+    {
+        mtop->bIntermolecularInteractions = FALSE;
     }
 
     do_atomtypes (fio, &(mtop->atomtypes), bRead, file_version);
