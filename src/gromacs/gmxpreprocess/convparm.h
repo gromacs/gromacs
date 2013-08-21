28e04be86f0791f19a55b2d8d27294454c352d88
Berk Hess <hess@kth.se>
2013-08-21 17:30:36 +0200

    Intermolecular bonded interaction support added
    The .top file can now contain an [ intermolecular_interactions ]
    directive, after which bonded interactions can be entered using
    global atom indices.
    Added a molecule defition section to the topologies chapter in the
    manual. A description of the moleculetype directive was missing.
    This section has a subsection on intermolecular interactions.
    
    Change-Id: I383287dd0729fef1c54f27a4fe9f5d628445549c

--- src/gromacs/gmxpreprocess/convparm.h
+++ src/gromacs/gmxpreprocess/convparm.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -46,7 +46,9 @@ extern "C" {
 #endif
 
 void convert_params(int atnr, t_params nbtypes[],
-                    t_molinfo *mi, int comb, double reppow, real fudgeQQ,
+                    t_molinfo *mi,
+                    t_molinfo *intermolecular_interactions,
+                    int comb, double reppow, real fudgeQQ,
                     gmx_mtop_t *mtop);
 
 #ifdef __cplusplus
--- src/gromacs/gmxpreprocess/grompp-impl.h
+++ src/gromacs/gmxpreprocess/grompp-impl.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -165,6 +165,7 @@ typedef enum {
     d_orientation_restraints,
     d_dihedral_restraints,
     d_cmap,
+    d_intermolecular_interactions,
     d_maxdir,
     d_invalid,
     d_none
