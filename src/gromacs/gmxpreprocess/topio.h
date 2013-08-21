bbeb9434a44917d9aa24a6ed6a3652c03bb72cdb
Berk Hess <hess@kth.se>
2013-08-21 17:30:36 +0200

    Intermolecular bonded interaction support added
    The .top file can now contain an [ intermolecular_interactions ]
    directive, after which bonded interactions can be entered using
    global atom indices.
    
    Change-Id: I383287dd0729fef1c54f27a4fe9f5d628445549c

--- src/gromacs/gmxpreprocess/topio.h
+++ src/gromacs/gmxpreprocess/topio.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2012,2014, by the GROMACS development team, led by
+ * Copyright (c) 2012,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -63,12 +63,12 @@ char **do_top(gmx_bool         bVerbose,
               gpp_atomtype_t   atype,
               int             *nrmols,
               t_molinfo      **molinfo,
+              t_molinfo      **intermolecular_interactions,
               t_inputrec      *ir,
               int             *nmolblock,
               gmx_molblock_t **molblock,
               gmx_bool         bGB,
               warninp_t        wi);
-
 
 /* This routine expects sys->molt[m].ilist to be of size F_NRE and ordered. */
 void generate_qmexcl(gmx_mtop_t *sys, t_inputrec *ir, warninp_t    wi);
