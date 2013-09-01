7115b05a51434e4204cde6d9162f8f0b32fbe06b
Mark Abraham <mark.j.abraham@gmail.com>
2013-09-01 11:56:40 +0200

    Still trying

--- rules.pl
+++ rules.pl
@@ -3,11 +3,11 @@ submit_rule(submit(V, CR)) :-
     gerrit:max_with_block(-2, 2, 'Verified', V).
 
 submit_rule(submit(WIP)) :-
-    gerrit:commit_message_matches('\[WIP\]'),
+    gerrit:commit_message_matches('\\\[WIP\\\]'),
     !,
     WIP = label('Commit-Message-does-not-include-[WIP]', need(_)).
 
 submit_rule(submit(RFC)) :-
-    gerrit:commit_message_matches('\[RFC\]'),
+    gerrit:commit_message_matches('\\\[RFC\\\]'),
     !,
     RFC = label('Commit-Message-does-not-include-[RFC]', need(_)).
