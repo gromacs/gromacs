8bd5b6a8863f7a9801d81a75af85ce8166844643
Mark Abraham <mark.j.abraham@gmail.com>
2013-09-01 13:18:08 +0200

    Another fresh attempt

--- rules.pl
+++ rules.pl
@@ -5,7 +5,7 @@ submit_rule(submit(RFC)) :-
     RFC = label('Commit-Message-does-not-include-[RFC]', need(_)).
 
 submit_rule(submit(CR, V)) :-
-    not(gerrit:commit_message_matches('\\\[WIP\\\]')),
-    not(gerrit:commit_message_matches('\\\[RFC\\\]')),
+    gerrit:commit_message_matches('(?!\\\[WIP\\\])'),
+    gerrit:commit_message_matches('(?!\\\[RFC\\\])'),
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
     gerrit:max_with_block(-2, 2, 'Verified', V).
