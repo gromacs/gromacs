e9ce89bbad5d5487ce785f292f733850257cfdaf
Mark Abraham <mark.j.abraham@gmail.com>
2013-09-01 12:38:45 +0200

    Trying to disable pointless voting

--- rules.pl
+++ rules.pl
@@ -2,10 +2,10 @@ submit_rule(submit(V, CR)) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
     gerrit:max_with_block(-2, 2, 'Verified', V).
 
-submit_rule(submit()) :-
+submit_rule(submit(WIP)) :-
     not(gerrit:commit_message_matches('\\\[WIP\\\]')),
     WIP = label('Commit-Message-does-not-include-[WIP]', need(_)).
 
-submit_rule(submit()) :-
+submit_rule(submit(RFC)) :-
     not(gerrit:commit_message_matches('\\\[RFC\\\]')),
     RFC = label('Commit-Message-does-not-include-[RFC]', need(_)).
