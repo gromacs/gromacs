a13a4f70cb3dffdb3116c207d16e44b30d05d7e8
Teemu Murtola <teemu.murtola@gmail.com>
2013-09-01 20:23:50 +0300

    Disallow submission of RFC/WIP changes.

--- rules.pl
+++ rules.pl
@@ -1,12 +1,11 @@
-submit_rule(submit(CR, V)) :-
+submit_rule(submit(CR, V, RFC)) :-
     base(CR, V),
-    gerrit:commit_message_matches('(?!\\\[WIP\\\])'),
-    gerrit:commit_message_matches('(?!\\\[RFC\\\])'),
-    !.
+    gerrit:commit_message_matches('(\\\[WIP\\\]|\\\[RFC\\\])'),
+    !,
+    RFC = label('Not-RFC-or-WIP', need(_)).
 
-submit_rule(submit(CR, V, NoSubmitTags)) :-
-    base(CR, V),
-    NoSubmitTags = label('Commit-Message-does-not-include-[WIP]-or-[RFC]', need(_)).
+submit_rule(submit(CR, V)) :-
+    base(CR, V).
 
 base(CR, V) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
