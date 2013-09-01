3f53efb429b3b45bf7ee0ed94964c1e9033c66ca
Mark Abraham <mark.j.abraham@gmail.com>
2013-09-01 13:50:20 +0200

    Yet another attempt

--- rules.pl
+++ rules.pl
@@ -1,12 +1,11 @@
-submit_rule(submit(CR, V, NoSubmitTags)) :-
+submit_rule(submit(CR, V)) :-
     base(CR, V),
     gerrit:commit_message_matches('(?!\\\[WIP\\\])|(?!\\\[RFC\\\])'),
-    NoSubmitTags = label('Commit-Message-does-not-include-[WIP]-or-[RFC]', ok(_)),
     !.
 
-submit_rule(submit(CR, V, NoSubmitTags)) :-
+submit_rule(submit(CR, V)) :-
     base(CR, V),
-    NoSubmitTags = label('Commit-Message-does-not-include-[WIP]-or-[RFC]', need(_)).
+    label('Commit-Message-does-not-include-[WIP]-or-[RFC]', need(_)).
 
 base(CR, V) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
