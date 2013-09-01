d992bb7f64fff1aea5203cc07a6951a8d2bd9295
Mark Abraham <mark.j.abraham@gmail.com>
2013-09-01 11:56:40 +0200

    Again reverting to sort-of-good 7115b05

--- rules.pl
+++ rules.pl
@@ -1,5 +1,13 @@
 submit_rule(submit(CR, V)) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-    gerrit:max_with_block(-2, 2, 'Verified', V),
-    not(gerrit:commit_message_matches('\\\[WIP\\\]')),
-    not(gerrit:commit_message_matches('\\\[RFC\\\]')).
+    gerrit:max_with_block(-2, 2, 'Verified', V).
+
+submit_rule(submit(WIP)) :-
+    gerrit:commit_message_matches('\\\[WIP\\\]'),
+    !,
+    WIP = label('Commit-Message-does-not-include-[WIP]', need(_)).
+
+submit_rule(submit(RFC)) :-
+    gerrit:commit_message_matches('\\\[RFC\\\]'),
+    !,
+    RFC = label('Commit-Message-does-not-include-[RFC]', need(_)).
