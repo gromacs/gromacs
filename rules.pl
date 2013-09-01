a4e50ac85f1d8ae0e35aa2fd933e5e78b0f84250
Mark Abraham <mark.j.abraham@gmail.com>
2013-09-01 12:56:26 +0200

    Fresh attempt at stopping pointless voting

--- rules.pl
+++ rules.pl
@@ -1,7 +1,16 @@
-submit_rule(submit(V, CR)) :-
+submit_rule(submit(CR, V)) :-
+    base(CR, V),
+    not(gerrit:commit_message_matches('\\\[WIP\\\]')),
+    !.
+
+submit_rule(submit(CR, V)) :-
+    base(CR, V),
+    not(gerrit:commit_message_matches('\\\[RFC\\\]')),
+    !.
+
+submit_rule(submit(CR, V)) :-
+    base(CR, V).
+
+base(CR, V) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
     gerrit:max_with_block(-2, 2, 'Verified', V),
-    not(gerrit:commit_message_matches('\\\[WIP\\\]')),
-    WIP = label('Commit-Message-does-not-include-[WIP]', need(_)),
-    not(gerrit:commit_message_matches('\\\[RFC\\\]')),
-    RFC = label('Commit-Message-does-not-include-[RFC]', need(_)).
