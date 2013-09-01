35cb0ed542a2cb34204a4f0861fe3be9c521ad26
Teemu Murtola <teemu.murtola@gmail.com>
2013-09-01 20:47:51 +0300

    Revert back to a somewhat working version.

--- rules.pl
+++ rules.pl
@@ -2,11 +2,10 @@ submit_rule(submit(CR, V, RFC)) :-
     base(CR, V),
     gerrit:commit_message_matches('\\\[WIP\\\]|\\\[RFC\\\]'),
     !,
-    RFC = label('RFC-or-WIP', ok(_)).
+    RFC = label('Not-RFC-or-WIP', need(_)).
 
-submit_rule(submit(CR, V, RFC)) :-
-    base(CR, V),
-    RFC = label('RFC-or-WIP', need(_)).
+submit_rule(submit(CR, V)) :-
+    base(CR, V).
 
 base(CR, V) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
