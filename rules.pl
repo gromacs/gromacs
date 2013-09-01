2e47a5df3346e7da6ad0b9d3581c45030b0f6ffe
Mark Abraham <mark.j.abraham@gmail.com>
2013-09-01 11:42:27 +0200

    Disallow submission of commits with messages containing 'WIP'

--- rules.pl
+++ rules.pl
@@ -1,3 +1,7 @@
 submit_rule(submit(V, CR)) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
     gerrit:max_with_block(-2, 2, 'Verified', V).
+
+submit_rule(submit(WIP)) :-
+    gerrit:commit_message_matches('WIP'),
+    WIP = label('Commit-Message-includes-WIP', reject(_)).
