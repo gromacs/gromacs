a4d07fcc0d1e0434ae57000d803815711bdd206c
Teemu Murtola <teemu.murtola@gmail.com>
2013-09-01 20:59:49 +0300

    Revert to the original configuration.
    I'm quite confident that the rules themselves work correctly in 35cb0ed,
    but that still shows the extra labels in
    1) the main page, and
    2) as a grayed out voting category in RFC/WIP changes
    (but not elsewhere).

--- rules.pl
+++ rules.pl
@@ -1,12 +1,3 @@
-submit_rule(submit(CR, V, RFC)) :-
-    base(CR, V),
-    gerrit:commit_message_matches('\\\[WIP\\\]|\\\[RFC\\\]'),
-    !,
-    RFC = label('Not-RFC-or-WIP', need(_)).
-
 submit_rule(submit(CR, V)) :-
-    base(CR, V).
-
-base(CR, V) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
     gerrit:max_with_block(-2, 2, 'Verified', V).
