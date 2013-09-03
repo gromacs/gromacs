7778280525c15677ff9b31427fb330a56b10819f
Teemu Murtola <teemu.murtola@gmail.com>
2013-09-03 07:11:38 +0300

    Man page export from the wrapper binary.
    Implement nroff output format for 'gmx help -export'.
    Move the header/footer generation out of wman.cpp.
    The generation is now off by default.  Can be reinstantiated if someone
    comes with a good approach for the case where the compiled binaries
    don't run on the build host (e.g., compiling AVX256 binaries on a SSE4.1
    host).
    
    Related to #685 and #969.
    
    Change-Id: I17725e3a487470bea8f6db2a59da31139e70621e

--- CPackInit.cmake
+++ CPackInit.cmake
@@ -1,10 +1,10 @@
 #TODO: add check that source doesn't contain any untracked files
 if(CPACK_SOURCE_PACKAGE_FILE_NAME) #building source package
     get_filename_component(CMAKE_BINARY_DIR ${CPACK_OUTPUT_CONFIG_FILE} PATH)
-    if(NOT EXISTS "${CMAKE_BINARY_DIR}/man/man1/ngmx.1")
-        message(FATAL_ERROR 
-            "To create a complete source package all man pages need to be generated."
-            "The man pages are automatically built together with the binaries. "
-            "Make sure to build all binaries (e.g. GMX_X11=on).")
+    if(NOT EXISTS "${CMAKE_BINARY_DIR}/share/man/man1/gmx-view.1")
+        message(FATAL_ERROR
+            "To create a complete source package all man pages need to be generated. "
+            "You need to run 'make man' or set GMX_BUILD_MANPAGES=ON to get "
+            "them automatically built together with the binaries.")
     endif()
 endif()
