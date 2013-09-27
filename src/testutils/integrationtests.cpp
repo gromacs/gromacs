76fa32719dd7a0c4a5daa0d8c28ca5561234e371
Mark Abraham <mark.j.abraham@gmail.com>
2013-09-27 19:09:07 +0200

    Add machinery for integration tests
    This code permits the construction of script-like test cases. Examples
    can be seen in src/programs/mdrun/tests/rerun.cpp and
    src/gromacs/gmxana/legacytests/gmx_traj_tests.cpp.
    
    Initially, this will permit us to check things like "this combination
    of inputs to grompp led correctly to error/success in grompp or mdrun,
    and did not segfault." This will permit us to enumerate test cases
    over ranges of legal inputs to .mdp in a more automated fashion, using
    things like the enumerations in the source code, without needing to
    store and maintain all the input cases. This is particularly valuable
    for detecting unknown side effects when refactoring (e.g. removing
    PD).
    
    Also permits test cases like CheckMdrunCanWriteCheckpoint.
    
    Reporting of failures when run under MPI is a long way from perfect.
    Only rank 0 can be allowed to write to stdout and the GTest XML output
    file. On stdout, ranks of failing tests and their expected and actual
    values are reported. In the XML, only the ranks are reported. Fixing
    this later would require serializing in rank > 0 either the
    TestPartResult object, or its result string, and sending those to rank
    0 for processing.
    
    Later, it would be good to refactor these tests so that we do not have
    to do things like disk I/O for .tpr files, so they run faster and
    we can have more of them.
    
    Also later, machinery to get things like energies and forces from
    mdrun for sanity checks would be great, too, so that we can prevent
    bugs like "this combination of inputs incorrectly computes zero
    energies" when somebody forgets to extend a switch statement, etc.
    
    Change-Id: I8072355b44c12d4a47c181734c83c961fd306666

---
+++ src/testutils/integrationtests.cpp
@@ -0,0 +1,100 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2013, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Implements classes in integrationtests.h.
+ *
+ * \author Mark Abraham <mark.j.abraham@gmail.com>
+ * \ingroup module_integration_tests
+ */
+#include "integrationtests.h"
+
+#include "testutils/testoptions.h"
+#include "gromacs/utility/stringutil.h"
+#include "gromacs/utility/exceptions.h"
+#include "gromacs/options/options.h"
+#include "gromacs/options/basicoptions.h"
+#include "gromacs/utility/file.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+namespace gmx
+{
+namespace test
+{
+
+/********************************************************************
+ * IntegrationTestFixture
+ */
+
+std::string IntegrationTestFixture::maxBackup_s("-1");
+
+GMX_TEST_OPTIONS(IntegrationTestOptions, options)
+{
+    options->addOption(StringOption("max-backup")
+                           .store(&IntegrationTestFixture::maxBackup_s)
+                           .description("Maximum number of backup files of old test output to write (-1 prevents backups being created)"));
+}
+
+IntegrationTestFixture::IntegrationTestFixture()
+{
+    // TODO fix this when we have an encapsulation layer for handling
+    // environment variables
+#ifdef GMX_NATIVE_WINDOWS
+    _putenv_s("GMX_MAXBACKUP", maxBackup_s.c_str());
+#else
+    setenv("GMX_MAXBACKUP", maxBackup_s.c_str(), true);
+#endif
+}
+
+IntegrationTestFixture::~IntegrationTestFixture()
+{
+}
+
+void
+IntegrationTestFixture::redirectStringToStdin(const char* theString)
+{
+    std::string fakeStdin("fake-stdin");
+    gmx::File::writeFileFromString(fakeStdin, theString);
+    if (NULL == std::freopen(fakeStdin.c_str(), "r", stdin))
+    {
+        GMX_THROW_WITH_ERRNO(FileIOError("Failed to redirect a string to stdin"),
+                             "freopen",
+                             errno);
+    }
+}
+
+} // namespace test
+} // namespace gmx
--- src/testutils/mpi-printer.h
+++ src/testutils/mpi-printer.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012,2013, by the GROMACS development team, led by
+ * Copyright (c) 2013, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -32,35 +32,36 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the research papers on the package. Check out http://www.gromacs.org.
  */
-/*! \libinternal \file
+/*! \internal \file
  * \brief
- * main() for unit tests that use \ref module_testutils.
+ * Declares gmx::test::initMPIOutput().
  *
- * \author Teemu Murtola <teemu.murtola@gmail.com>
  * \ingroup module_testutils
  */
-#include <gtest/gtest.h>
+#ifndef GMX_TESTUTILS_MPI_PRINTER_H
+#define GMX_TESTUTILS_MPI_PRINTER_H
 
-#include "testutils/testoptions.h"
-
-#ifndef TEST_DATA_PATH
-//! Path to test input data directory (needs to be set by the build system).
-#define TEST_DATA_PATH 0
-#endif
-
-#ifndef TEST_TEMP_PATH
-//! Path to test output temporary directory (needs to be set by the build system).
-#define TEST_TEMP_PATH 0
-#endif
-
-/*! \brief
- * Initializes unit testing for \ref module_testutils.
- */
-int main(int argc, char *argv[])
+namespace gmx
 {
-    // Calls ::testing::InitGoogleMock()
-    ::gmx::test::initTestUtils(TEST_DATA_PATH, TEST_TEMP_PATH, &argc, &argv);
-    int errcode = RUN_ALL_TESTS();
-    ::gmx::test::finalizeTestUtils();
-    return errcode;
-}
+namespace test
+{
+
+//! \cond internal
+/*! \brief
+ * Customizes test output and test failure handling for MPI runs.
+ *
+ * Only one rank should report the test result. Errors detected on a
+ * subset of ranks need to be reported individually, and as an overall
+ * failure.
+ *
+ * On non-MPI builds, does nothing.
+ *
+ * \ingroup module_testutils
+ */
+void initMPIOutput();
+//! \endcond
+
+} // namespace test
+} // namespace gmx
+
+#endif
