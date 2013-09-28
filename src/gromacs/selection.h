737b475582b3fa3bd9914131a49bd72392351d9f
Teemu Murtola <teemu.murtola@gmail.com>
2013-09-28 22:59:01 +0300

    Overview Doxygen pages for analysis framework.
    Main content:
    - General overview page for the whole framework.
    - Reorganized the way the documentation for the analysis template is
      generated.
    - Separate page for describing the concepts for the analysisdata module
      in more detail.
    - Add missing installation rule for analysisdata/modules/lifetime.h.
    
    The module pages could now use some restructuring to reference the
    information here and provide relevant details instead of duplicating any
    of it.  Left for later.
    
    Change-Id: I159a2d913fe6f9114c60804d30fa3714b23df262

--- src/gromacs/selection.h
+++ src/gromacs/selection.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013, by the GROMACS development team, led by
  * David van der Spoel, Berk Hess, Erik Lindahl, and including many
  * others, as listed in the AUTHORS file in the top-level source
  * directory and at http://www.gromacs.org.
@@ -32,7 +32,7 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the research papers on the package. Check out http://www.gromacs.org.
  */
-/*! \defgroup module_selection Parsing and Evaluation of Analysis Selections
+/*! \defgroup module_selection Parsing and Evaluation of Analysis Selections (selection)
  * \ingroup group_analysismodules
  * \brief
  * Provides functionality for initializing and evaluating selections.
