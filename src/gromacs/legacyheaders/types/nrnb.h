4284594c87a19952c04a7e35ea88d55a679177fe
David van der Spoel <spoel@xray.bmc.uu.se>
2013-10-01 14:11:51 +0200

    Implementation of WAXS/SAXS refinement in MD
    This code implements a module for refining protein structures with
    respect to WAXS/SAXS data. The Debye equation is used to compute
    the theoretical scattering Scalc(q) and the energy function is
    Ewaxs = Kwaxs/2 Sum_q (Sobs(q) - Scalc(q))^2
    More details have been added to the manual. In addition preparation
    tools are implemented in gmx genrestr and an analysis tool
    is in gmx waxsdebye.  Data files are implemented using XML.
    Added eftXML to options framework.
    
    Change-Id: Ie102e1fc565cb3a70b40e8884718e7854c1f3674

--- src/gromacs/legacyheaders/types/nrnb.h
+++ src/gromacs/legacyheaders/types/nrnb.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -110,7 +110,7 @@ enum
     eNR_RB,                   eNR_FOURDIH,              eNR_TABDIHS,
     eNR_DISRES,               eNR_ORIRES,               eNR_DIHRES,
     eNR_POSRES,               eNR_FBPOSRES,
-    eNR_ANGRES,               eNR_ANGRESZ,
+    eNR_ANGRES,               eNR_ANGRESZ,              eNR_WAXS_DEBYE,
     eNR_MORSE,                eNR_CUBICBONDS,           eNR_WALLS,
     eNR_POLARIZE,             eNR_ANHARM_POL,
     eNR_WPOL,                 eNR_THOLE,                eNR_VIRIAL,
