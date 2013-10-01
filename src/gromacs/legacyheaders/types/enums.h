f7366ca5c47427aa29b498fabca6dbc7c95f17d6
David van der Spoel <spoel@xray.bmc.uu.se>
2013-10-01 14:11:51 +0200

    [RFC] Implementation of WAXS/SAXS refinement in MD
    This code implements a module for refining protein structures with
    respect to WAXS/SAXS data. The Debye equation is used to compute
    the theoretical scattering Scalc(q) and the energy function is
    Ewaxs = Kwaxs/2 Sum_q (Sobs(q) - Scalc(q))^2
    More details will be added to the manual. In addition preparation
    tools are implemented in gmx genrestr and a framework for analysis
    is in gmx waxsdebye.  Data files are implemented using XML.
    Added eftXML top options framework.
    
    Change-Id: Ie102e1fc565cb3a70b40e8884718e7854c1f3674

--- src/gromacs/legacyheaders/types/enums.h
+++ src/gromacs/legacyheaders/types/enums.h
@@ -77,6 +77,11 @@ enum {
     epctSURFACETENSION, epctNR
 };
 
+/* For WAXS/SAXS refinement */
+enum {
+    eWaxsNO, eWaxsDebye, eWaxsFull, eWaxsNR
+};
+
 enum {
     erscNO, erscALL, erscCOM, erscNR
 };
