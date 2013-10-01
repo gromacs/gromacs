f6b601518d91893585e610c8259f9257592d66cc
Magnus Lundborg <lundborg.magnus@gmail.com>
2013-10-01 10:11:00 +0200

    Added TNG library and made GROMACS write basic TNG
    Change-Id: Ied156d8ad728711e08a0c61db5ee3472b0df39d5

--- src/gromacs/legacyheaders/types/filenm.h
+++ src/gromacs/legacyheaders/types/filenm.h
@@ -41,7 +41,11 @@ extern "C" {
 /* this enum should correspond to the array deffile in gmxlib/filenm.c */
 enum {
     efMDP,
-    efTRX, efTRO, efTRN, efTRR, efTRJ, efXTC, efG87,
+    efTRX, efTRO, efTRN, efTRR, efTRJ, efXTC,
+#ifdef GMX_USE_TNG
+    efTNG,
+#endif
+    efG87,
     efEDR,
     efSTX, efSTO, efGRO, efG96, efPDB, efBRK, efENT, efESP, efPQR, efXYZ,
     efCPT,
