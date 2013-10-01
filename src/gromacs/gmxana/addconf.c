c5374b3af306606a677706a0f1af73ef2b05b77e
David van der Spoel <spoel@xray.bmc.uu.se>
2013-10-01 14:11:51 +0200

    [RFC] Implementation of WAXS/SAXS refinement in MD
    This code implements a module for refining protein structures with
    respect to WAXS/SAXS data. The Debye equation is used to compute
    the theoretical scattering Scalc(q) and the energy function is
    Ewaxs = Kwaxs/2 Sum_q (Sobs(q) - Scalc(q))^2
    More details will be added to the manual. In addition preparation
    tools are implemented in gmx genrestr and a framework for analysis
    is in gmx waxsdebye.  Data files are implemented using XML.
    Added eftXML top options framework.
    
    Change-Id: Ie102e1fc565cb3a70b40e8884718e7854c1f3674

--- src/gromacs/gmxana/addconf.c
+++ src/gromacs/gmxana/addconf.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -297,7 +297,7 @@ void do_nsgrid(FILE *fp, gmx_bool bVerbose,
     snew(ir->fepvals, 1);
     printf("Neighborsearching with a cut-off of %g\n", rlong);
     init_forcerec(stdout, oenv, fr, NULL, ir, mtop, cr, box,
-                  NULL, NULL, NULL, NULL, NULL, TRUE, -1);
+                  NULL, 0, NULL, TRUE, -1);
     if (debug)
     {
         pr_forcerec(debug, fr);
