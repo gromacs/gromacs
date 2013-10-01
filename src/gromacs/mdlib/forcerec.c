21f60842a8affedaacf14326adaa0be13ff8fff1
David van der Spoel <spoel@xray.bmc.uu.se>
2013-10-01 14:11:51 +0200

    [RFC] Implementation of WAXS/SAXS refinement in MD
    This code implements a module for refining protein structures with
    respect to WAXS/SAXS data. The Debye equation is used to compute
    the theoretical scattering Scalc(q) and the energy function is
    Ewaxs = Kwaxs/2 Sum_q (Sobs(q) - Scalc(q))^2
    More details will be added to the manual. In addition preparation
    tools are implemented in gmx genrestr and an analysis tool
    is in gmx waxsdebye.  Data files are implemented using XML.
    Added eftXML to options framework.
    
    Change-Id: Ie102e1fc565cb3a70b40e8884718e7854c1f3674

--- src/gromacs/mdlib/forcerec.c
+++ src/gromacs/mdlib/forcerec.c
@@ -70,6 +70,7 @@
 #include "gromacs/legacyheaders/gmx_omp_nthreads.h"
 #include "gromacs/legacyheaders/gmx_detect_hardware.h"
 #include "gromacs/legacyheaders/inputrec.h"
+#include "gromacs/waxsdebye/waxs_debye_force_c.h"
 
 #include "gromacs/pbcutil/ishift.h"
 #include "gromacs/pbcutil/pbc.h"
@@ -309,7 +310,7 @@ check_solvent_cg(const gmx_moltype_t    *molt,
     j1     = molt->cgs.index[cg0+1];
 
     /* Number of atoms in our molecule */
-    nj     = j1 - j0;
+    nj = j1 - j0;
 
     if (debug)
     {
@@ -762,8 +763,8 @@ static cginfo_mb_t *init_cginfo_mb(FILE *fplog, const gmx_mtop_t *mtop,
                     /* Check VDW and electrostatic interactions */
                     bHaveVDW = bHaveVDW || (type_VDW[molt->atoms.atom[ai].type] ||
                                             type_VDW[molt->atoms.atom[ai].typeB]);
-                    bHaveQ  = bHaveQ    || (molt->atoms.atom[ai].q != 0 ||
-                                            molt->atoms.atom[ai].qB != 0);
+                    bHaveQ = bHaveQ    || (molt->atoms.atom[ai].q != 0 ||
+                                           molt->atoms.atom[ai].qB != 0);
 
                     bHavePerturbedAtoms = bHavePerturbedAtoms || (PERTURBED(molt->atoms.atom[ai]) != 0);
 
@@ -2067,11 +2068,11 @@ init_interaction_const(FILE                       *fp,
         ic->k_rf       = 0;
         if (fr->coulomb_modifier == eintmodPOTSHIFT)
         {
-            ic->c_rf   = 1/ic->rcoulomb;
+            ic->c_rf = 1/ic->rcoulomb;
         }
         else
         {
-            ic->c_rf   = 0;
+            ic->c_rf = 0;
         }
     }
 
@@ -2315,11 +2316,9 @@ void init_forcerec(FILE              *fp,
                    const gmx_mtop_t  *mtop,
                    const t_commrec   *cr,
                    matrix             box,
-                   const char        *tabfn,
-                   const char        *tabafn,
-                   const char        *tabpfn,
-                   const char        *tabbfn,
                    const char        *nbpu_opt,
+                   int                nfile,
+                   const t_filenm     fnm[],
                    gmx_bool           bNoSolvOpt,
                    real               print_force)
 {
@@ -2333,6 +2332,10 @@ void init_forcerec(FILE              *fp,
     gmx_bool       bFEP_NonBonded;
     t_nblists     *nbl;
     int           *nm_ind, egp_flags;
+    const char    *tabfn  = opt2fn("-table", nfile, fnm);
+    const char    *tabafn = opt2fn("-tabletf", nfile, fnm);
+    const char    *tabpfn = opt2fn("-tablep", nfile, fnm);
+    const char    *tabbfn = opt2fn("-tableb", nfile, fnm);
 
     if (fr->hwinfo == NULL)
     {
@@ -2479,7 +2482,7 @@ void init_forcerec(FILE              *fp,
 
     if (bGenericKernelOnly == TRUE)
     {
-        bNoSolvOpt         = TRUE;
+        bNoSolvOpt = TRUE;
     }
 
     if ( (getenv("GMX_DISABLE_SIMD_KERNELS") != NULL) || (getenv("GMX_NOOPTIMIZEDKERNELS") != NULL) )
@@ -2659,8 +2662,8 @@ void init_forcerec(FILE              *fp,
     }
 
     /* These start out identical to ir, but might be altered if we e.g. tabulate the interaction in the kernel */
-    fr->nbkernel_elec_modifier    = fr->coulomb_modifier;
-    fr->nbkernel_vdw_modifier     = fr->vdw_modifier;
+    fr->nbkernel_elec_modifier = fr->coulomb_modifier;
+    fr->nbkernel_vdw_modifier  = fr->vdw_modifier;
 
     fr->rvdw             = cutoff_inf(ir->rvdw);
     fr->rvdw_switch      = ir->rvdw_switch;
@@ -2677,11 +2680,11 @@ void init_forcerec(FILE              *fp,
         fr->bvdwtab    = ((fr->vdwtype != evdwCUT || !gmx_within_tol(fr->reppow, 12.0, 10*GMX_DOUBLE_EPS))
                           && !EVDW_PME(fr->vdwtype));
         /* We have special kernels for standard Ewald and PME, but the pme-switch ones are tabulated above */
-        fr->bcoultab   = !(fr->eeltype == eelCUT ||
-                           fr->eeltype == eelEWALD ||
-                           fr->eeltype == eelPME ||
-                           fr->eeltype == eelRF ||
-                           fr->eeltype == eelRF_ZERO);
+        fr->bcoultab = !(fr->eeltype == eelCUT ||
+                         fr->eeltype == eelEWALD ||
+                         fr->eeltype == eelPME ||
+                         fr->eeltype == eelRF ||
+                         fr->eeltype == eelRF_ZERO);
 
         /* If the user absolutely wants different switch/shift settings for coul/vdw, it is likely
          * going to be faster to tabulate the interaction than calling the generic kernel.
@@ -3142,7 +3145,6 @@ void init_forcerec(FILE              *fp,
         if (ir->adress->n_tf_grps > 0)
         {
             make_adress_tf_tables(fp, oenv, fr, ir, tabfn, mtop, box);
-
         }
         else
         {
@@ -3251,6 +3253,26 @@ void init_forcerec(FILE              *fp,
     if (ir->eDispCorr != edispcNO)
     {
         calc_enervirdiff(fp, ir->eDispCorr, fr);
+    }
+
+    /* Initiate the waxs data structure if needed */
+    {
+        const char *waxs_ref = opt2fn_null("-waxs_ref", nfile, fnm);
+        if (NULL != waxs_ref)
+        {
+            fr->wdf = waxs_debye_force_init(fp,
+                                            opt2fn_null("-sfac", nfile, fnm),
+                                            waxs_ref,
+                                            opt2fn_null("-waxs_diff", nfile, fnm),
+                                            opt2fn("-waxs_out", nfile, fnm),
+                                            opt2fn("-waxs_alpha", nfile, fnm),
+                                            cr,
+                                            ir);
+        }
+        else
+        {
+            fr->wdf = NULL;
+        }
     }
 }
 
---
+++ src/gromacs/waxsdebye/tests/scattering_factors.cpp
@@ -0,0 +1,227 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Implements test routines from scattering factor IO
+ *
+ * \author Daccid van der Spoel
+ * \ingroup module_selection
+ */
+#include "gmxpre.h"
+
+#include <cstring>
+
+#include "gromacs/waxsdebye/scattering_factors.h"
+#include "gromacs/utility/gmxassert.h"
+
+namespace gmx
+{
+namespace
+{
+
+class ScatteringFactorTableTest : public ::testing::Test
+{
+
+    protected:
+        static ScatteringFactorTable sft;
+
+        //static init, only run once every test.
+        static void SetUpTestCase(const char *datafile)
+        {
+            sft.read(datafile);
+        }
+
+        gmx::test::TestReferenceData    refData;
+        gmx::test::TestReferenceChecker checker;
+
+
+        ScatteringFactorTableTest( )
+            : refData(gmx::test::erefdataCreateMissing), checker(refData.rootChecker())
+        {
+
+#ifdef GMX_DOUBLE
+            checker.setDefaultTolerance(gmx::test::relativeRealTolerance(1, 20000));
+#else
+            checker.setDefaultTolerance(gmx::test::relativeRealTolerance(1, 5000));
+#endif
+        }
+
+        static void TearDownTestCase()
+        {
+        }
+
+        void test(const char *datafile, real result[])
+        {
+            int     nfitparm = effnNparams(type);
+            int     testType = getTestType(type);
+            real  * values   = new real[data[0]->getNrLines()];
+            for (int i = 0; i < data[0]->getNrLines(); i++)
+            {
+                values[i] = data[testType]->getValue(i);
+
+            }
+            //init data and cheker might be a beter plays to init them
+
+            testResult(result, nfitparm);
+            delete [] values;
+        }
+
+        void testResult(real result[], int nrFitParam)
+        {
+
+            std::string testName = "result";
+            checker.checkSequenceArray(nrFitParam, result, testName.c_str());
+
+        }
+
+
+
+
+
+        int getTestType(unsigned long type)
+        {
+            /*
+               type 0 exp test
+               type 1 PRES test
+               type 2 ERF test
+             */
+            switch (type)
+            {
+                case effnEXP1:
+                    return 0;
+                case effnEXP2:
+                    return 0;
+                case effnEXP3:
+                    return 0;
+                case effnEXP5:
+                    return 0;
+                case effnEXP7:
+                    return 0;
+                case effnEXP9:
+                    return 0;
+                case effnVAC:
+                    return 0;
+                case effnERF:
+                    return 0;
+                case effnERREST:
+                    return 2;
+                case effnPRES:
+                    return 1;
+                default:
+                    GMX_RELEASE_ASSERT(true, "Invalid expfit option");
+                    return -1;
+            }
+        }
+
+
+        std::string convertInt(int number)
+        {
+            std::stringstream ss; //create a stringstream
+            ss << number;         //add number to the stream
+            return ss.str();      //return a string with the contents of the stream
+        }
+
+};
+
+TEST_F (ScatteringFactorTableTest, MartiniDS)
+{
+    const char *sfactorFile = "sfactor_martini_ds_Fourier.xml";
+    test(datafile, param);
+}
+
+TEST_F (ScatteringFactorTableTest, EffnEXP2)
+{
+    real  param[] = {35, 0.5};
+    test(effnEXP2, param);
+}
+
+TEST_F (ScatteringFactorTableTest, EffnEXP3)
+{
+    real param[] = {45, 0.5, 5};
+    test(effnEXP3, param);
+}
+
+TEST_F (ScatteringFactorTableTest, EffnEXP5)
+{
+    real  param[] = {0.5, 5, 0.5, 50, 0.002};
+    test(effnEXP5, param);
+}
+
+TEST_F (ScatteringFactorTableTest, EffnEXP7)
+{
+    real  param[] = {0.5, 5, -0.02, 0.5, 0.5, 50, -0.002};
+    test(effnEXP7, param);
+}
+
+TEST_F (ScatteringFactorTableTest, EffnEXP9)
+{
+    real  param[] = {0.18, 800, -0.2, 161, 0.7, 60, 0.5, 5, 0.1};
+    test(effnEXP9, param);
+}
+
+TEST_F (ScatteringFactorTableTest, EffnERF)
+{
+    real  param[] = {0.5, 0.5, 0.5, 1};
+    test(effnERF, param);
+}
+
+TEST_F (ScatteringFactorTableTest, EffnERREST) {
+    real  param[] = {0.5, 0.7, 0.3};
+    test(effnERREST, param);
+}
+
+TEST_F (ScatteringFactorTableTest, EffnVAC)
+{
+    real param[] = {0.5, 0.05};
+    test(effnVAC, param);
+}
+
+TEST_F (ScatteringFactorTableTest, EffnPRES)
+{
+    real param[] = {0, 10, 4, 1, 0.5, 1};
+    test(effnPRES, param);
+}
+
+}
+
+TEST_F(ScatteringFactorTableTest, FileIO)
+{
+
+    sft.write("sfactor_martini_ds.out");
+}
+
+}
+
+}
