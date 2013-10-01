3275c4d8dbc9ab25f81ef8856b59eb40e0c2a76f
Magnus Lundborg <lundborg.magnus@gmail.com>
2013-10-01 10:11:00 +0200

    [WIP] TNG writing and reading from mdrun and some tools.
    Reading from e.g. gmxdump, trjconv, trjcat.
    Writing from e.g. mdrun, trjconv, trjcat.
    Some other tools should now work as well, e.g., g_rms.
    
    Tools such as trjconv, trjcat, gmxcheck and gmxdump have partial
    support for TNG available. Full support for these tools (even only for
    the subset of TNG features that GROMACS might want to use right now)
    is currently difficult, because the code and data for existing
    trajectory writing is a mess of excess functionality.  This will need
    to be revisited when (at least) trjconv is broken up into more parts.
    
    TNG is never written by default, but is available as trajectory
    output.
    
    Renamed status variable in gmx_trjconv.c to better reflect its usage
    as an opaque trajectory-reading-management object.
    
    Encapsulated gmx_mdoutf_t
    
    Moved content of trajectory_writing_low_level.c to mdoutf.c
    
    Windows build is known to be broken unless you explicitly specify
    non-.tng file extensions for trajectory files. To be fixed after the
    first beta with TNG. If TNG is disabled TNG tests are not run.
    
    Changed .mdp field and code variable names that were specific to XTC
    to be more generic, now that there is more than one way to write a
    trajectory file with lossy compression. Other associated documentation
    updates also.
    
    TODO see if mdrun -rerun works
    
    Change-Id: Ied156d8ad728711e08a0c61db5ee3472b0df39d5

---
+++ src/gromacs/fileio/tngio_for_tools.h
@@ -0,0 +1,123 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2013, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+#ifndef GMX_FILEIO_TNGIO_FOR_TOOLS_H
+#define GMX_FILEIO_TNGIO_FOR_TOOLS_H
+
+#define TNG_TRAJ_POSITIONS              0x0000000010000001LL
+
+#include "gromacs/legacyheaders/typedefs.h"
+#include "trxio.h"
+#include "../../external/tng_io/include/tng_io_fwd.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#if 0
+}
+#endif
+
+/*! \brief Prepare to write TNG output from trajectory conversion tools */
+void prepare_tng_writing(const char              *filename,
+                         char                     mode,
+                         tng_trajectory_t        *in,
+                         tng_trajectory_t        *out,
+                         int                      nAtoms,
+                         const gmx_mtop_t        *mtop,
+                         const atom_id           *index,
+                         const char              *indexGroupName);
+
+/*! \brief Open a TNG input file */
+void open_tng_for_reading(const char              *fn,
+                          tng_trajectory_t        *input);
+
+/*! \brief Write a trxframe to a TNG file
+ *
+ * \param natoms Number of atoms to actually write
+ *
+ * The natoms field in frame is the number of atoms in the system. The
+ * parameter natoms supports writing an index-group subset of the
+ * atoms.
+ */
+void write_tng_from_trxframe(tng_trajectory_t        output,
+                             t_trxframe             *frame,
+                             int                     natoms);
+
+/*! \brief Creates a molecule containing only the indexed atoms and sets
+ * the number of all other molecules to 0. Works similar to a
+ * selection group. */
+void setup_atom_subgroup(tng_trajectory_t tng,
+                         const int        nind,
+                         const atom_id   *ind,
+                         const char      *name);
+
+/*! \brief Read the first/next TNG frame. */
+gmx_bool read_next_tng_frame(tng_trajectory_t            input,
+                             t_trxframe                 *fr,
+                             gmx_int64_t                *requestedIds,
+                             int                         numRequestedIds);
+
+/*! \brief Print the molecule system to stream */
+void print_tng_molecule_system(tng_trajectory_t input,
+                               FILE            *stream);
+
+/*! \brief Get a list of block IDs present in the next frame with data. */
+gmx_bool get_tng_data_block_types_of_next_frame(tng_trajectory_t     input,
+                                                int                  frame,
+                                                int                  nRequestedIds,
+                                                gmx_int64_t         *requestedIds,
+                                                gmx_int64_t         *nextFrame,
+                                                gmx_int64_t         *nBlocks,
+                                                gmx_int64_t        **blockIds);
+
+/*! \brief Get data of the next frame with data from the data block
+ * with the specified block ID. */
+gmx_bool get_tng_data_next_frame_of_block_type(tng_trajectory_t     input,
+                                               gmx_int64_t          blockId,
+                                               real               **values,
+                                               gmx_int64_t         *frameNumber,
+                                               double              *frameTime,
+                                               gmx_int64_t         *nValuesPerFrame,
+                                               gmx_int64_t         *nAtoms,
+                                               real                *prec,
+                                               char                *name,
+                                               int                  maxLen,
+                                               gmx_bool            *bOK);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
