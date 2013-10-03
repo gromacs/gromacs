f6ea7cd3c4cf7ab3f3e8370977103a419740df1c
Mark Abraham <mark.j.abraham@gmail.com>
2013-10-03 20:00:57 +0200

    Renamed and removed last references to GMX_CRAY_XT3
    These systems were around in 2006, so probably out of commission by
    now. XT5 had replaced catamount with something else. Not sure about
    XT4, which might still be around. In any case, the machinery to test
    for Cray's catamount should be named more generally, and spam the
    CMake status and cache less.
    
    Change-Id: Ic23b195f500662a6a0c4e27b4dc1c9c647b03fc1

--- cmake/gmxTestCatamount.cmake
+++ cmake/gmxTestCatamount.cmake
@@ -6,25 +6,7 @@
 #
 
 MACRO(GMX_TEST_CATAMOUNT VARIABLE)
-    IF(NOT DEFINED ${VARIABLE})
-
-        MESSAGE(STATUS "Checking for CRAY XT Catamount compile")
-
-	# First check without any special flags
-        TRY_COMPILE(CATAMOUNT_COMPILE_OK "${CMAKE_BINARY_DIR}"    
-                    "${CMAKE_SOURCE_DIR}/cmake/TestCatamount.c")
-
-        if(CATAMOUNT_COMPILE_OK)
-	    MESSAGE(STATUS "Checking for CRAY XT Catamount target - yes")			
-        else(CATAMOUNT_COMPILE_OK)
-            MESSAGE(STATUS "Checking for CRAY XT Catamount target - no")
-      	endif(CATAMOUNT_COMPILE_OK)
-
-        set(${VARIABLE} ${CATAMOUNT_COMPILE_OK} CACHE INTERNAL 
-            "Result of test for CRAY XT Catamount target" FORCE)
-        
-    ENDIF(NOT DEFINED ${VARIABLE})
+    TRY_COMPILE(CATAMOUNT_COMPILE_OK "${CMAKE_BINARY_DIR}"
+        "${CMAKE_SOURCE_DIR}/cmake/TestCatamount.c")
+    set(${VARIABLE} ${CATAMOUNT_COMPILE_OK} PARENT_SCOPE)
 ENDMACRO(GMX_TEST_CATAMOUNT VARIABLE)
-
-
-
