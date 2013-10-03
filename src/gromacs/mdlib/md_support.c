4478eb11fed765bbc64df7082229b57020967d0e
Mark Abraham <mark.j.abraham@gmail.com>
2013-10-03 21:05:00 +0200

    Removed heuristic group-scheme neighbour-list updates
    -1 == nstlist used to trigger the use of an algorithm that did
    neighbour searching only when particles had moved far enough that
    there might be a need to update the list. This supported obtaining
    better energy conservation with the group cut-off scheme. It has been
    superseded by the Verlet cut-off scheme.
    
    Part of #1292
    
    Change-Id: I074790c1c5670e874b8b587a1d46e62e2edc8961

--- src/gromacs/mdlib/md_support.c
+++ src/gromacs/mdlib/md_support.c
@@ -52,7 +52,7 @@
 #include "gromacs/timing/wallcycle.h"
 
 /* Is the signal in one simulation independent of other simulations? */
-gmx_bool gs_simlocal[eglsNR] = { TRUE, FALSE, FALSE, TRUE };
+gmx_bool gs_simlocal[eglsNR] = { FALSE, FALSE, TRUE };
 
 /* check which of the multisim simulations has the shortest number of
    steps and return that number of nsteps */
