7a2a9e329572c6cd4bce297d47fce1620c1105b9
Teemu Murtola <teemu.murtola@gmail.com>
2013-10-07 06:59:58 +0300

    Add some missing copyright headers.
    Went through files that were modified by
      git ls-tree --name-only -r HEAD | git check-attr filter --stdin | \
      sed -Ene '/(copyright|uncrustify)$/ {s/:.*//;p;}' | \
      grep -v '_kernel_' | xargs admin/copyright.py --add-missing
    and updated the copyright years for those where I know the history or
    could easily dig it out.  Mostly this is CMake build system files, which
    came to existence at earliest 2009, so it wasn't too hard to dig out
    when the code was actually introduced/changed, using git log and
    git log --follow.
    
    Part of #818.
    
    Change-Id: Ibba6d8bacc700efd76b7bc429228d751b8e17a27

--- cmake/gmxTestInlineASM.cmake
+++ cmake/gmxTestInlineASM.cmake
@@ -1,3 +1,37 @@
+#
+# This file is part of the GROMACS molecular simulation package.
+#
+# Copyright (c) 2009,2010,2012,2013, by the GROMACS development team, led by
+# Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+# and including many others, as listed in the AUTHORS file in the
+# top-level source directory and at http://www.gromacs.org.
+#
+# GROMACS is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# GROMACS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with GROMACS; if not, see
+# http://www.gnu.org/licenses, or write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+#
+# If you want to redistribute modifications to GROMACS, please
+# consider that scientific software is very special. Version
+# control is crucial - bugs must be traceable. We will be happy to
+# consider code for inclusion in the official distribution, but
+# derived work must not be called official GROMACS. Details are found
+# in the README & COPYING files - if they are missing, get the
+# official version at http://www.gromacs.org.
+#
+# To help us fund GROMACS development, we humbly ask that you cite
+# the research papers on the package. Check out http://www.gromacs.org.
+
 # - Define macro to check GCC x86 inline ASM support
 #
 #  GMX_TEST_INLINE_ASM_GCC_X86(VARIABLE)
@@ -6,10 +40,10 @@
 
 MACRO(GMX_TEST_INLINE_ASM_GCC_X86 VARIABLE)
     IF(NOT DEFINED ${VARIABLE})
-        
+
         MESSAGE(STATUS "Checking for GCC x86 inline asm")
 
-        TRY_COMPILE(${VARIABLE} "${CMAKE_BINARY_DIR}"    
+        TRY_COMPILE(${VARIABLE} "${CMAKE_BINARY_DIR}"
                     "${CMAKE_SOURCE_DIR}/cmake/TestInlineASM_gcc_x86.c"
                     OUTPUT_VARIABLE INLINE_ASM_COMPILE_OUTPUT)
 
@@ -19,7 +53,7 @@ MACRO(GMX_TEST_INLINE_ASM_GCC_X86 VARIABLE)
         else(${VARIABLE})
             MESSAGE(STATUS "Checking for GCC x86 inline asm - not supported")
             set(${VARIABLE} 0 CACHE INTERNAL "Result of test for GCC x86 inline asm" FORCE)
-      	endif(${VARIABLE})
+        endif(${VARIABLE})
 
     ENDIF(NOT DEFINED ${VARIABLE})
 ENDMACRO(GMX_TEST_INLINE_ASM_GCC_X86 VARIABLE)
