c245b1f9060ad610343ea484fef273f06285d2df
Roland Schulz <roland@utk.edu>
2013-10-09 14:16:52 -0400

    Fix SIMD C reference nbnxn kernels
    Got broken by ace006a86 and 022581b388.
    An additional fix for nbnxn 4x8 reference code, broken by c0cf8ce,
    is in a separate patch.
    Also changed the AVX256 double precision nbfp_stride from 4 to 2.
    
    Change-Id: If3b3291a7ff765acc19c29f834e856cc9798d47e

--- src/mdlib/nbnxn_kernels/nbnxn_kernel_simd_utils.h
+++ src/mdlib/nbnxn_kernels/nbnxn_kernel_simd_utils.h
@@ -56,6 +56,20 @@
 
 #ifdef GMX_SIMD_REFERENCE_PLAIN_C
 
+/* Set the stride for the lookup of the two LJ parameters from their
+ * (padded) array.
+ * Note that currently only arrays with stride 2 and 4 are available.
+ * Since the reference code does not require alignment, we can always use 2.
+ */
+static const int nbfp_stride = 2;
+
+/* Align a stack-based thread-local working array. */
+static gmx_inline int *
+prepare_table_load_buffer(const int *array)
+{
+    return NULL;
+}
+
 #include "nbnxn_kernel_simd_utils_ref.h"
 
 #else /* GMX_SIMD_REFERENCE_PLAIN_C */
@@ -64,10 +78,9 @@
 /* Include x86 SSE2 compatible SIMD functions */
 
 /* Set the stride for the lookup of the two LJ parameters from their
-   (padded) array. Only strides of 2 and 4 are currently supported. */
-#if defined GMX_NBNXN_SIMD_2XNN
-static const int nbfp_stride = 4;
-#elif defined GMX_DOUBLE
+ * (padded) array. We use the minimum supported SIMD memory alignment.
+ */
+#if defined GMX_DOUBLE
 static const int nbfp_stride = 2;
 #else
 static const int nbfp_stride = 4;
