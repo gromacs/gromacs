4e1401ae312d956771682a3c320d9feeb8be498c
Teemu Murtola <teemu.murtola@gmail.com>
2013-10-26 08:25:28 +0300

    [RFC] Convert gmx-sas to the C++ framework
    An example of converting a non-trivial analysis tool to the new
    framework.  Most functionality is intact, but the command-line interface
    is changed a bit.  Changes in behavior:
     - Writing a position restraint file is not implemented.  Would not be
       very difficult to add.
     - Solvation free energy estimates are written into a separate file
       instead of the main output file, and only if requested with an -odg
       option.
     - The calculation group is specified as a selection with -surface.
       Can be a dynamic selection.  The total area of this group is always
       written out.
     - There can be 0..* extra output groups, specified as selections with
       -output.  All can be dynamic, and must always be a subset of the
       calculation group.
     - There is no longer a separate concept of hydrophilic and hydrophobic
       surface area.  The user can trivially calculate such things by
       providing two different output groups.  The old hydrophobicity
       definition is easily expressed as "... and charge {-0.2 to 0.2}".
     - tpr file is no longer required for most of the output.
     - Output precision is different from the old tool.  The current fixed
       to three decimal places should be sufficient for most uses, but could
       be considered to use %g for formatting if really necessary.
    
    Future TODO:
     - Improve X axis labels so that they would again provide real
       atom/residue numbers instead of internal indices.
     - Consider if the legends on the plots could be improved.
    
    RFC since the old tool should be removed to make Jenkins pass.  The new
    tests still need some verification before that can be done, but uploaded
    an intermediate version to check coverage and any possible issues.
    Currently, it should produce the same results as the old tool, at least
    for the total/atom/residue areas.
    
    Part of #665.
    
    Change-Id: Iee60c13b927b3b63b6e218164cd961971f2f3fce

--- src/testutils/testfilemanager.h
+++ src/testutils/testfilemanager.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -111,6 +111,8 @@ class TestFileManager
          * same test.
          */
         std::string getTemporaryFilePath(const char *suffix);
+        //! \copydoc TestFileManager::getTemporaryFilePath(const char *)
+        std::string getTemporaryFilePath(const std::string &suffix);
 
         /*! \brief
          * Creates a file name for use within a single unit test.
