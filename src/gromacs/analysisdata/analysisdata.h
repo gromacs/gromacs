844bfdd77ef08d9b3f8fecff8635ef896b8c76ea
Teemu Murtola <teemu.murtola@gmail.com>
2013-10-26 08:25:28 +0300

    [RFC] Convert gmx-sas to the C++ framework
    An example of converting a non-trivial analysis tool to the new
    framework.  Most functionality is intact, but the command-line interface
    is changed a bit.  Changes in behavior:
     - Writing a position restraint file is not implemented.  Would not be
       very difficult to add.
     - Solvation free energy estimates are written into a separate file
       instead of the main output file, and only if requested with an -odg
       option.
     - The calculation group is specified as a selection with -surface.
       Can be a dynamic selection.  The total area of this group is always
       written out.
     - There can be 0..* extra output groups, specified as selections with
       -output.  All can be dynamic, and must always be a subset of the
       calculation group.
     - There is no longer a separate concept of hydrophilic and hydrophobic
       surface area.  The user can trivially calculate such things by
       providing two different output groups.  The old hydrophobicity
       definition is easily expressed as "... and charge {-0.2 to 0.2}".
    
    RFC since the old tool should be removed to make Jenkins pass.  And it
    would be good to add some unit tests.  Currently, it should produce the
    same results as the old tool, at least for the total/atom/residue areas,
    except that the output precision is different and some X axis labels are
    suboptimal/less clear in the plots (could be improved separately).
    
    Part of #665.
    
    Change-Id: Iee60c13b927b3b63b6e218164cd961971f2f3fce

--- src/gromacs/analysisdata/analysisdata.h
+++ src/gromacs/analysisdata/analysisdata.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012,2013, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -241,6 +241,9 @@ class AnalysisDataHandle
          * Does not throw.
          */
         AnalysisDataHandle();
+
+        //! Returns whether this data handle is valid.
+        bool isValid() const { return impl_ != NULL; }
 
         /*! \brief
          * Start data for a new frame.
--- src/gromacs/trajectoryanalysis/analysismodule.h
+++ src/gromacs/trajectoryanalysis/analysismodule.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -108,6 +108,8 @@ class TrajectoryAnalysisModuleData
          *
          * \p data should have previously been registered with
          * TrajectoryAnalysisModule::registerAnalysisDataset().
+         * If \p data has zero columns in all data sets, the returned data
+         * handle is invalid.
          *
          * Does not throw.
          */
