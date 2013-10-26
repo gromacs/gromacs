6fcb0df7f028f29b567e463cc3a05a7e89004ae6
Teemu Murtola <teemu.murtola@gmail.com>
2013-10-26 08:25:28 +0300

    Convert gmx-sas to the C++ framework
    An example of converting a non-trivial analysis tool to the new
    framework.  Most functionality is intact, but the command-line interface
    is changed a bit.  Changes in behavior:
     - Writing a position restraint file is not implemented.  Would not be
       very difficult to add.
     - Solvation free energy estimates are written into a separate file
       instead of the main output file, and only if requested with an -odg
       option.
     - The calculation group is specified as a selection with -surface.
       Can be a dynamic selection.  The total area of this group is always
       written out.
     - There can be 0..* extra output groups, specified as selections with
       -output.  All can be dynamic, and must always be a subset of the
       calculation group.
     - There is no longer a separate concept of hydrophilic and hydrophobic
       surface area.  The user can trivially calculate such things by
       providing two different output groups.  The old hydrophobicity
       definition is easily expressed as "... and charge {-0.2 to 0.2}".
     - tpr file is no longer required for most of the output.
     - Output precision is different from the old tool.  The current
       precision (fixed to three decimal places) should be sufficient for
       most uses, but it could be considered to use %g for formatting like
       the old tool if really necessary.
    
    Future TODOs:
     - Consider if the legends on the plots could be improved.
     - Add unit tests for the actual surface area calculation routine.
    
    Part of #665.
    
    Change-Id: Iee60c13b927b3b63b6e218164cd961971f2f3fce

--- src/gromacs/trajectoryanalysis/modules/nsc.c
+++ src/gromacs/trajectoryanalysis/modules/nsc.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2007, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -61,7 +61,6 @@
 
 real   *xpunsp = NULL;
 real    del_cube;
-int    *ico_wk = NULL, *ico_pt = NULL;
 int     n_dot, ico_cube, last_n_dot = 0, last_densit = 0, last_unsp = 0;
 int     last_cubus = 0;
 
@@ -530,6 +529,7 @@ int unsp_type(int densit)
 
 int make_unsp(int densit, int mode, int * num_dot, int cubus)
 {
+    int  *ico_wk, *ico_pt;
     int   ndot, ico_cube_cb, i, j, k, l, ijk, tn, tl, tl2;
     real *xus;
     int  *work;
@@ -538,10 +538,6 @@ int make_unsp(int densit, int mode, int * num_dot, int cubus)
     if (xpunsp)
     {
         free(xpunsp);
-    }
-    if (ico_wk)
-    {
-        free(ico_wk);
     }
 
     k = 1; if (mode < 0)
@@ -647,7 +643,8 @@ int make_unsp(int densit, int mode, int * num_dot, int cubus)
             } /* cycle k */
         }     /* cycle j */
     }         /* cycle i */
-    free(work);
+    sfree(ico_wk);
+    sfree(work);
 
     return 0;
 }
@@ -660,31 +657,32 @@ typedef struct _stwknb {
     real dot;
 } Neighb;
 
-int nsc_dclm_pbc(rvec *coords, real *radius, int nat,
+int nsc_dclm_pbc(const rvec *coords, real *radius, int nat,
                  int  densit, int mode,
                  real *value_of_area, real **at_area,
                  real *value_of_vol,
                  real **lidots, int *nu_dots,
                  atom_id index[], int ePBC, matrix box)
 {
-    int      iat, i, ii, iii, ix, iy, iz, ixe, ixs, iye, iys, ize, izs, i_ac;
-    int      jat, j, jj, jjj, jx, jy, jz;
-    int      distribution;
-    int      l;
-    int      maxnei, nnei, last, maxdots = 0;
-    int     *wkdot = NULL, *wkbox = NULL, *wkat1 = NULL, *wkatm = NULL;
-    Neighb  *wknb, *ctnb;
-    int      iii1, iii2, iiat, lfnr = 0, i_at, j_at;
-    real     dx, dy, dz, dd, ai, aisq, ajsq, aj, as, a;
-    real     xi, yi, zi, xs = 0., ys = 0., zs = 0.;
-    real     dotarea, area, vol = 0.;
-    real    *xus, *dots = NULL, *atom_area = NULL;
-    int      nxbox, nybox, nzbox, nxy, nxyz;
-    real     xmin = 0, ymin = 0, zmin = 0, xmax, ymax, zmax, ra2max, d, *pco;
+    int         iat, i, ii, iii, ix, iy, iz, ixe, ixs, iye, iys, ize, izs, i_ac;
+    int         jat, j, jj, jjj, jx, jy, jz;
+    int         distribution;
+    int         l;
+    int         maxnei, nnei, last, maxdots = 0;
+    int        *wkdot = NULL, *wkbox = NULL, *wkat1 = NULL, *wkatm = NULL;
+    Neighb     *wknb, *ctnb;
+    int         iii1, iii2, iiat, lfnr = 0, i_at, j_at;
+    real        dx, dy, dz, dd, ai, aisq, ajsq, aj, as, a;
+    real        xi, yi, zi, xs = 0., ys = 0., zs = 0.;
+    real        dotarea, area, vol = 0.;
+    real       *xus, *dots = NULL, *atom_area = NULL;
+    int         nxbox, nybox, nzbox, nxy, nxyz;
+    real        xmin = 0, ymin = 0, zmin = 0, xmax, ymax, zmax, ra2max, d;
+    const real *pco;
     /* Added DvdS 2006-07-19 */
-    t_pbc    pbc;
-    rvec     ddx, *x = NULL;
-    int      iat_xx, jat_xx;
+    t_pbc       pbc;
+    rvec        ddx, *x = NULL;
+    int         iat_xx, jat_xx;
 
     distribution = unsp_type(densit);
     if (distribution != -last_unsp || last_cubus != 4 ||
