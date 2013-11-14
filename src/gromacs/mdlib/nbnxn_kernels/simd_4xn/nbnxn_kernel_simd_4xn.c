39e5bbce422cbc23881ffd2c7ba4222981cd6efb
Berk Hess <hess@kth.se>
2013-11-14 09:59:50 +0100

    Implemented nbnxn LJ switch functions
    The nbnxn C reference and SIMD kernels now support force and
    potential switching, which are selected through vdw_modifier.
    Both use analytical functions, since tables are expensive with SIMD.
    Made the nbnxn kernel naming consistent with the group scheme.
    Corresponding CUDA kernels will be added soon.
    
    Change-Id: I52a1cda5ec7c3bdce6f3f2735ff7b13303ea5880

--- src/gromacs/mdlib/nbnxn_kernels/simd_4xn/nbnxn_kernel_simd_4xn.c
+++ src/gromacs/mdlib/nbnxn_kernels/simd_4xn/nbnxn_kernel_simd_4xn.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -48,9 +48,9 @@
 #ifdef GMX_NBNXN_HALF_WIDTH_SIMD
 #define GMX_USE_HALF_WIDTH_SIMD_HERE
 #endif
+
 #include "gromacs/simd/macros.h"
 #include "gromacs/simd/vector_operations.h"
-
 #if !(GMX_SIMD_WIDTH_HERE == 2 || GMX_SIMD_WIDTH_HERE == 4 || GMX_SIMD_WIDTH_HERE == 8)
 #error "unsupported SIMD width"
 #endif
@@ -60,6 +60,7 @@
 #include "../nbnxn_kernel_common.h"
 #include "gmx_omp_nthreads.h"
 #include "types/force_flags.h"
+#include "gmx_fatal.h"
 
 /*! \brief Kinds of electrostatic treatments in SIMD Verlet kernels
  */
@@ -67,91 +68,124 @@ enum {
     coultRF, coultTAB, coultTAB_TWIN, coultEWALD, coultEWALD_TWIN, coultNR
 };
 
-/* Declare and define the kernel function pointer lookup tables. */
-static p_nbk_func_ener p_nbk_ener[coultNR][ljcrNR] =
+/* Declare and define the kernel function pointer lookup tables.
+ * The minor index of the array goes over both the LJ combination rules,
+ * which is only supported by plain cut-off, and the LJ switch functions.
+ */
+static p_nbk_func_noener p_nbk_noener[coultNR][ljcrNR+2] =
 {
     {
-        nbnxn_kernel_simd_4xn_rf_comb_geom_ener,
-        nbnxn_kernel_simd_4xn_rf_comb_lb_ener,
-        nbnxn_kernel_simd_4xn_rf_comb_none_ener,
+        nbnxn_kernel_ElecRF_VdwLJCombGeom_F_4xn,
+        nbnxn_kernel_ElecRF_VdwLJCombLB_F_4xn,
+        nbnxn_kernel_ElecRF_VdwLJ_F_4xn,
+        nbnxn_kernel_ElecRF_VdwLJFSw_F_4xn,
+        nbnxn_kernel_ElecRF_VdwLJPSw_F_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_tab_comb_geom_ener,
-        nbnxn_kernel_simd_4xn_tab_comb_lb_ener,
-        nbnxn_kernel_simd_4xn_tab_comb_none_ener,
+        nbnxn_kernel_ElecQSTab_VdwLJCombGeom_F_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJCombLB_F_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJ_F_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJFSw_F_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJPSw_F_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_tab_twin_comb_geom_ener,
-        nbnxn_kernel_simd_4xn_tab_twin_comb_lb_ener,
-        nbnxn_kernel_simd_4xn_tab_twin_comb_none_ener,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJCombGeom_F_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJCombLB_F_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJ_F_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJFSw_F_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJPSw_F_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_ewald_comb_geom_ener,
-        nbnxn_kernel_simd_4xn_ewald_comb_lb_ener,
-        nbnxn_kernel_simd_4xn_ewald_comb_none_ener,
+        nbnxn_kernel_ElecEw_VdwLJCombGeom_F_4xn,
+        nbnxn_kernel_ElecEw_VdwLJCombLB_F_4xn,
+        nbnxn_kernel_ElecEw_VdwLJ_F_4xn,
+        nbnxn_kernel_ElecEw_VdwLJFSw_F_4xn,
+        nbnxn_kernel_ElecEw_VdwLJPSw_F_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_ewald_twin_comb_geom_ener,
-        nbnxn_kernel_simd_4xn_ewald_twin_comb_lb_ener,
-        nbnxn_kernel_simd_4xn_ewald_twin_comb_none_ener,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJCombGeom_F_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJCombLB_F_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJ_F_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJFSw_F_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJPSw_F_4xn,
     },
 };
 
-static p_nbk_func_ener p_nbk_energrp[coultNR][ljcrNR] =
+static p_nbk_func_ener p_nbk_ener[coultNR][ljcrNR+2] =
 {
     {
-        nbnxn_kernel_simd_4xn_rf_comb_geom_energrp,
-        nbnxn_kernel_simd_4xn_rf_comb_lb_energrp,
-        nbnxn_kernel_simd_4xn_rf_comb_none_energrp,
+        nbnxn_kernel_ElecRF_VdwLJCombGeom_VF_4xn,
+        nbnxn_kernel_ElecRF_VdwLJCombLB_VF_4xn,
+        nbnxn_kernel_ElecRF_VdwLJ_VF_4xn,
+        nbnxn_kernel_ElecRF_VdwLJFSw_VF_4xn,
+        nbnxn_kernel_ElecRF_VdwLJPSw_VF_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_tab_comb_geom_energrp,
-        nbnxn_kernel_simd_4xn_tab_comb_lb_energrp,
-        nbnxn_kernel_simd_4xn_tab_comb_none_energrp,
+        nbnxn_kernel_ElecQSTab_VdwLJCombGeom_VF_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJCombLB_VF_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJ_VF_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJFSw_VF_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJPSw_VF_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_tab_twin_comb_geom_energrp,
-        nbnxn_kernel_simd_4xn_tab_twin_comb_lb_energrp,
-        nbnxn_kernel_simd_4xn_tab_twin_comb_none_energrp,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJCombGeom_VF_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJCombLB_VF_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJ_VF_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJFSw_VF_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJPSw_VF_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_ewald_comb_geom_energrp,
-        nbnxn_kernel_simd_4xn_ewald_comb_lb_energrp,
-        nbnxn_kernel_simd_4xn_ewald_comb_none_energrp,
+        nbnxn_kernel_ElecEw_VdwLJCombGeom_VF_4xn,
+        nbnxn_kernel_ElecEw_VdwLJCombLB_VF_4xn,
+        nbnxn_kernel_ElecEw_VdwLJ_VF_4xn,
+        nbnxn_kernel_ElecEw_VdwLJFSw_VF_4xn,
+        nbnxn_kernel_ElecEw_VdwLJPSw_VF_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_ewald_twin_comb_geom_energrp,
-        nbnxn_kernel_simd_4xn_ewald_twin_comb_lb_energrp,
-        nbnxn_kernel_simd_4xn_ewald_twin_comb_none_energrp,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJCombGeom_VF_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJCombLB_VF_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJ_VF_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJFSw_VF_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJPSw_VF_4xn,
     },
 };
 
-static p_nbk_func_noener p_nbk_noener[coultNR][ljcrNR] =
+static p_nbk_func_ener p_nbk_energrp[coultNR][ljcrNR+2] =
 {
     {
-        nbnxn_kernel_simd_4xn_rf_comb_geom_noener,
-        nbnxn_kernel_simd_4xn_rf_comb_lb_noener,
-        nbnxn_kernel_simd_4xn_rf_comb_none_noener,
+        nbnxn_kernel_ElecRF_VdwLJCombGeom_VgrpF_4xn,
+        nbnxn_kernel_ElecRF_VdwLJCombLB_VgrpF_4xn,
+        nbnxn_kernel_ElecRF_VdwLJ_VgrpF_4xn,
+        nbnxn_kernel_ElecRF_VdwLJFSw_VgrpF_4xn,
+        nbnxn_kernel_ElecRF_VdwLJPSw_VgrpF_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_tab_comb_geom_noener,
-        nbnxn_kernel_simd_4xn_tab_comb_lb_noener,
-        nbnxn_kernel_simd_4xn_tab_comb_none_noener,
+        nbnxn_kernel_ElecQSTab_VdwLJCombGeom_VgrpF_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJCombLB_VgrpF_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJ_VgrpF_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJFSw_VgrpF_4xn,
+        nbnxn_kernel_ElecQSTab_VdwLJPSw_VgrpF_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_tab_twin_comb_geom_noener,
-        nbnxn_kernel_simd_4xn_tab_twin_comb_lb_noener,
-        nbnxn_kernel_simd_4xn_tab_twin_comb_none_noener,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJCombGeom_VgrpF_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJCombLB_VgrpF_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJ_VgrpF_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJFSw_VgrpF_4xn,
+        nbnxn_kernel_ElecQSTabTwinCut_VdwLJPSw_VgrpF_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_ewald_comb_geom_noener,
-        nbnxn_kernel_simd_4xn_ewald_comb_lb_noener,
-        nbnxn_kernel_simd_4xn_ewald_comb_none_noener,
+        nbnxn_kernel_ElecEw_VdwLJCombGeom_VgrpF_4xn,
+        nbnxn_kernel_ElecEw_VdwLJCombLB_VgrpF_4xn,
+        nbnxn_kernel_ElecEw_VdwLJ_VgrpF_4xn,
+        nbnxn_kernel_ElecEw_VdwLJFSw_VgrpF_4xn,
+        nbnxn_kernel_ElecEw_VdwLJPSw_VgrpF_4xn,
     },
     {
-        nbnxn_kernel_simd_4xn_ewald_twin_comb_geom_noener,
-        nbnxn_kernel_simd_4xn_ewald_twin_comb_lb_noener,
-        nbnxn_kernel_simd_4xn_ewald_twin_comb_none_noener,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJCombGeom_VgrpF_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJCombLB_VgrpF_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJ_VgrpF_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJFSw_VgrpF_4xn,
+        nbnxn_kernel_ElecEwTwinCut_VdwLJPSw_VgrpF_4xn,
     },
 };
 
@@ -217,7 +251,7 @@ nbnxn_kernel_simd_4xn(nbnxn_pairlist_set_t      gmx_unused *nbl_list,
 {
     int                nnbl;
     nbnxn_pairlist_t **nbl;
-    int                coult;
+    int                coult, ljtreatment = 0;
     int                nb;
 
     nnbl = nbl_list->nnbl;
@@ -253,6 +287,23 @@ nbnxn_kernel_simd_4xn(nbnxn_pairlist_set_t      gmx_unused *nbl_list,
         }
     }
 
+    switch (ic->vdw_modifier)
+    {
+        case eintmodNONE:
+        case eintmodPOTSHIFT:
+            ljtreatment = nbat->comb_rule;
+            break;
+        /* Switch functions follow after cut-off combination rule kernels */
+        case eintmodFORCESWITCH:
+            ljtreatment = ljcrNR;
+            break;
+        case eintmodPOTSWITCH:
+            ljtreatment = ljcrNR + 1;
+            break;
+        default:
+            gmx_incons("Unsupported VdW interaction modifier");
+    }
+
 #pragma omp parallel for schedule(static) num_threads(gmx_omp_nthreads_get(emntNonbonded))
     for (nb = 0; nb < nnbl; nb++)
     {
@@ -283,11 +334,11 @@ nbnxn_kernel_simd_4xn(nbnxn_pairlist_set_t      gmx_unused *nbl_list,
         if (!(force_flags & GMX_FORCE_ENERGY))
         {
             /* Don't calculate energies */
-            p_nbk_noener[coult][nbat->comb_rule](nbl[nb], nbat,
-                                                 ic,
-                                                 shift_vec,
-                                                 out->f,
-                                                 fshift_p);
+            p_nbk_noener[coult][ljtreatment](nbl[nb], nbat,
+                                             ic,
+                                             shift_vec,
+                                             out->f,
+                                             fshift_p);
         }
         else if (out->nV == 1)
         {
@@ -295,13 +346,13 @@ nbnxn_kernel_simd_4xn(nbnxn_pairlist_set_t      gmx_unused *nbl_list,
             out->Vvdw[0] = 0;
             out->Vc[0]   = 0;
 
-            p_nbk_ener[coult][nbat->comb_rule](nbl[nb], nbat,
-                                               ic,
-                                               shift_vec,
-                                               out->f,
-                                               fshift_p,
-                                               out->Vvdw,
-                                               out->Vc);
+            p_nbk_ener[coult][ljtreatment](nbl[nb], nbat,
+                                           ic,
+                                           shift_vec,
+                                           out->f,
+                                           fshift_p,
+                                           out->Vvdw,
+                                           out->Vc);
         }
         else
         {
@@ -317,13 +368,13 @@ nbnxn_kernel_simd_4xn(nbnxn_pairlist_set_t      gmx_unused *nbl_list,
                 out->VSc[i] = 0;
             }
 
-            p_nbk_energrp[coult][nbat->comb_rule](nbl[nb], nbat,
-                                                  ic,
-                                                  shift_vec,
-                                                  out->f,
-                                                  fshift_p,
-                                                  out->VSvdw,
-                                                  out->VSc);
+            p_nbk_energrp[coult][ljtreatment](nbl[nb], nbat,
+                                              ic,
+                                              shift_vec,
+                                              out->f,
+                                              fshift_p,
+                                              out->VSvdw,
+                                              out->VSc);
 
             reduce_group_energies(nbat->nenergrp, nbat->neg_2log,
                                   out->VSvdw, out->VSc,
