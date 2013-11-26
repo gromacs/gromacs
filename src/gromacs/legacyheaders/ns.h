0e18782401448b5cac7d5cbc7852b07e73802e04
Berk Hess <hess@kth.se>
2013-11-26 08:52:16 +0100

    Free-energy now works with the Verlet scheme
    Implemented perturbed non-bonded interactions with the Verlet
    by masking them from the nbnxn pair lists and storing them in an
    old-fashioned group neighbor list. This way we keep a single
    free energy kernel.
    The free energy kernel uses OpenMP atomic for reductions.
    
    Also fixed grompp setting nstcalcenergy=0 with nstenergy=0.
    
    Change-Id: I5a6a7e84b46e06250d141e2e08cb3a11077cddab

--- src/gromacs/legacyheaders/ns.h
+++ src/gromacs/legacyheaders/ns.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -99,6 +99,9 @@ int read_nblist(FILE *in, FILE *out, int **mat, int natoms, gmx_bool bSymm);
 int natoms_beyond_ns_buffer(t_inputrec *ir, t_forcerec *fr, t_block *cgs,
                             matrix scale_tot, rvec *x);
 /* Returns the number of atoms that moved beyond the ns buffer */
+
+void reallocate_nblist(t_nblist *nl);
+/* List reallocation, only exported for Verlet scheme use with FEP */
 
 #ifdef __cplusplus
 }
