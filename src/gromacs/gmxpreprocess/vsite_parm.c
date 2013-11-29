6d036c381a558d44acd96d39966ba4ec25a8ca70
Alexey Shvetsov <alexxy@omrb.pnpi.spb.ru>
2013-11-29 22:54:50 +0400

    Fix -Wmaybe-uninitialized warnings in some files
    Change-Id: Ica4b1d17039e23bc2274a20fe5abca18295a7593
    Signed-off-by: Alexey Shvetsov <alexxy@omrb.pnpi.spb.ru>

--- src/gromacs/gmxpreprocess/vsite_parm.c
+++ src/gromacs/gmxpreprocess/vsite_parm.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1406,7 +1406,7 @@ static void clean_vsite_dihs(t_params *plist, t_pindex pindex[],
 {
     int          ftype, i, parnr, k, l, m, n, nvsite, kept_i, vsnral;
     atom_id      atom, constr;
-    atom_id      vsiteatoms[4];
+    atom_id      vsiteatoms[4] = { 0 }; /* init to zero to make gcc4.8 happy */
     gmx_bool     bKeep, bUsed, bPresent;
     t_params    *ps;
 
