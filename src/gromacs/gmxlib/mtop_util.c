ac25fa90d70c18156f5eebf6d7aac29354873e40
Teemu Murtola <teemu.murtola@gmail.com>
2013-11-30 13:50:39 +0200

    More copyright header updates
    Remove old copyrights from files added recently (for 4.6 or somewhat
    earlier, plus some selected files where it was easier to enumerate all
    years than to adapt the script to handle their existing copyrights) and
    make the copyright headers follow the new format.  Put best-guess
    estimates for the copyright years based on when the files have been
    modified.  No effort was spent on trying to track content beyond git
    rename detection, so some content may originate from earlier than the
    first copyright year mentioned, but this is hopefully not a big deal.
    
    Part of #818.
    
    Change-Id: I44e1eff552bff47a2ae10b3181960a8a2a6753df

--- src/gromacs/gmxlib/mtop_util.c
+++ src/gromacs/gmxlib/mtop_util.c
@@ -1,8 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 1991-2008 David van der Spoel, Erik Lindahl, Berk Hess, University of Groningen.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2008,2009,2010,2012,2013, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
