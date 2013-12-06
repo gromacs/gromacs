d0c123783de7721c371e8dcc1812f248ada993a5
ngoga <n.goga@rug.nl>
2013-12-06 10:30:15 +0100

    Default SD integrator is now sd1 by Nicu Goga
    The default SD integrator is now sd1, sd2 is deprecated. grompp
    and mdrun both warn about sd2, and recommend sd.
    
    The new sd1 integrator was developed by Herman Berendsen and
    implemented by Nicu Goga.
    Without constraints the sd1 integrator is nearly unchanged.
    With constraints the new sd1 integrator is now as accurate as normal
    MD with a leap-frog integrator, whereas the sd2 integrator will give
    a slightly too high temperature.
    
    Fixes #1496
    
    Change-Id: I02b4537b05742ed499a84b625c4d4bf8994c0304

--- src/gromacs/gmxlib/names.c
+++ src/gromacs/gmxlib/names.c
@@ -55,7 +55,7 @@ const char *ens_names[ensNR+1] =
 
 const char *ei_names[eiNR+1] =
 {
-    "md", "steep", "cg", "bd", "sd", "nm", "l-bfgs", "tpi", "tpic", "sd1", "md-vv", "md-vv-avek", NULL
+    "md", "steep", "cg", "bd", "sd2", "nm", "l-bfgs", "tpi", "tpic", "sd", "md-vv", "md-vv-avek", NULL
 };
 
 const char *bool_names[BOOL_NR+1] =
