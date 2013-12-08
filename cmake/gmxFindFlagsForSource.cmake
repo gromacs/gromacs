cff9d5085c40ecda2640ad28e5cb6f836ccdf9fb
Erik Lindahl <erik@kth.se>
2013-12-08 20:23:38 +0100

    Added AVX2 detection to cmake and cleanup of cmake code
    The main CMakeLists.txt has been cleaned up by moving
    both all tests for buggy compilers and the CPU SIMD acceleration
    tests to separate modules in the cmake directory, and to reduce
    the size further the same was done for the configurations for
    FFT libraries, OpenMP, and shared libraries.
    AVX2 detection has been added, and Acceleration detection has
    been improved by actually testing SSE/AVX/AVX2 codes with the
    flags, rather than first testing flags and then code (since
    some compilers silently accept illegal flags). Finally, cmake
    now detects options to use the clang built-in assembler on OS X
    together with the gcc compiler, which makes it possible to use
    AVX/AVX2 in combination with OpenMP support on OS X using free
    compilers.
    
    Change-Id: I6f9de80a87283cec4cf4211c4d0068d32315ba90

---
+++ cmake/gmxFindFlagsForSource.cmake
@@ -0,0 +1,96 @@
+#
+# This file is part of the GROMACS molecular simulation package.
+#
+# Copyright (c) 2013, by the GROMACS development team, led by
+# Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+# and including many others, as listed in the AUTHORS file in the
+# top-level source directory and at http://www.gromacs.org.
+#
+# GROMACS is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# GROMACS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with GROMACS; if not, see
+# http://www.gnu.org/licenses, or write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+#
+# If you want to redistribute modifications to GROMACS, please
+# consider that scientific software is very special. Version
+# control is crucial - bugs must be traceable. We will be happy to
+# consider code for inclusion in the official distribution, but
+# derived work must not be called official GROMACS. Details are found
+# in the README & COPYING files - if they are missing, get the
+# official version at http://www.gromacs.org.
+#
+# To help us fund GROMACS development, we humbly ask that you cite
+# the research papers on the package. Check out http://www.gromacs.org.
+
+
+# Helper routine to find flag (from a list) to compile a specific C source.
+# VARIABLE            This will be set when we have found a flag that works
+# DESCRIPTION         Text string describing what flag we are trying to find
+# SOURCE              Source code to test
+#                     The compiler is chosen based on the extension of this file
+# FLAGSVAR            Variable (string) to which we should add the correct flag
+# Args 5 through N    Multiple strings with acceleration flags to test
+MACRO(GMX_FIND_CFLAG_FOR_SOURCE VARIABLE DESCRIPTION SOURCE CFLAGSVAR)
+    IF(NOT DEFINED ${VARIABLE})
+        set(SAVED_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+        # Insert a blank element last in the list (try without any flags too)
+        # This must come last, since some compilers (Intel) might try to emulate
+        # emulate AVX instructions with SSE4.1 otherwise.
+        foreach(_testflag ${ARGN} "")
+            message(STATUS "Try ${DESCRIPTION} = [${_testflag}]")
+            set(CMAKE_REQUIRED_FLAGS "${${CFLAGSVAR}} ${_testflag}")
+            check_c_source_compiles("${SOURCE}" ${VARIABLE})
+            if(${VARIABLE})
+                set(${VARIABLE}_FLAG "${_testflag}" 
+                    CACHE INTERNAL "${DESCRIPTION}")
+                break()
+            endif(${VARIABLE})
+        endforeach()
+        set(CMAKE_REQUIRED_FLAGS "${SAVED_CMAKE_REQUIRED_FLAGS}")
+    ENDIF(NOT DEFINED ${VARIABLE})
+    IF (${VARIABLE})
+        SET (${CFLAGSVAR} "${${CFLAGSVAR}} ${${VARIABLE}_FLAG}")
+    ENDIF (${VARIABLE})
+ENDMACRO(GMX_FIND_CFLAG_FOR_SOURCE VARIABLE DESCRIPTION SOURCE CFLAGSVAR)
+
+
+# Helper routine to find flag (from list) to compile a specific C++ source.
+# VARIABLE            This will be set when we have found a flag that works
+# DESCRIPTION         Text string describing what flag we are trying to find
+# SOURCE              Source code to test
+#                     The compiler is chosen based on the extension of this file
+# FLAGSVAR            Variable (string) to which we should add the correct flag
+# Args 5 through N    Multiple strings with acceleration flags to test
+MACRO(GMX_FIND_CXXFLAG_FOR_SOURCE VARIABLE DESCRIPTION SOURCE CXXFLAGSVAR)
+    IF(NOT DEFINED ${VARIABLE})
+        set(SAVED_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+        # Insert a blank element last in the list (try without any flags too)
+        # This must come last, since some compilers (Intel) might try to
+        # emulate AVX instructions with SSE4.1 otherwise.
+        foreach(_testflag ${ARGN} "")
+            message(STATUS "Try ${DESCRIPTION} = [${_testflag}]")
+            set(CMAKE_REQUIRED_FLAGS "${${CXXFLAGSVAR}} ${_testflag}")
+            check_cxx_source_compiles("${SOURCE}" ${VARIABLE})
+            if(${VARIABLE})
+                set(${VARIABLE}_FLAG "${_testflag}" 
+                    CACHE INTERNAL "${DESCRIPTION}")
+                break()
+            endif(${VARIABLE})
+        endforeach()
+        set(CMAKE_REQUIRED_FLAGS "${SAVED_CMAKE_REQUIRED_FLAGS}")
+    ENDIF(NOT DEFINED ${VARIABLE})
+    IF (${VARIABLE})
+        SET (${CXXFLAGSVAR} "${${CXXFLAGSVAR}} ${${VARIABLE}_FLAG}")
+    ENDIF (${VARIABLE})
+ENDMACRO(GMX_FIND_CXXFLAG_FOR_SOURCE VARIABLE DESCRIPTION SOURCE CXXFLAGSVAR)
+
