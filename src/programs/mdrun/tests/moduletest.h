233c70a6a39a3ee93d2e18d577bd9658733d9e15
Mark Abraham <mark.j.abraham@gmail.com>
2013-12-10 18:02:52 +1100

    Add integration tests for replica exchange
    Only NVT and NPT tested so far.
    
    Added new convenience class for a value-parameterized mdrun
    integration-test fixture.
    
    Change-Id: I25211277d6979909f67378dc578e3d388a8abeda

--- src/programs/mdrun/tests/moduletest.h
+++ src/programs/mdrun/tests/moduletest.h
@@ -93,7 +93,7 @@ class MdrunTestFixture : public IntegrationTestFixture
         void useStringAsMdpFile(const char *mdpString);
         //! Use a standard .top and .gro file as input to grompp
         void useTopAndGroFromDatabase(const char *name);
-        //! Calls grompp to prepare for the mdrun test
+        //! Calls grompp (on rank 0) to prepare for the mdrun test
         int callGrompp();
         //! Calls grompp (on this rank) to prepare for the mdrun test
         int callGromppOnThisRank();
@@ -124,6 +124,12 @@ class MdrunTestFixture : public IntegrationTestFixture
         std::string logFileName;
         std::string edrFileName;
         //@}
+};
+
+/*! \brief Parameterized test fixture for mdrun integration tests */
+class ParameterizedMdrunTestFixture : public gmx::test::MdrunTestFixture,
+                                      public ::testing::WithParamInterface<const char *>
+{
 };
 
 } // namespace test
