26a8f88a19d3f6bcd5f3182b49db32116502447a
Teemu Murtola <teemu.murtola@gmail.com>
2013-12-21 20:33:44 +0200

    Unit tests for parse_common_args()
    Add unit tests for most of the basic command line parsing functionality
    in parse_common_args(), including the file name handling.
    
    To support these tests, extend arrayref.h to also include ArrayRef,
    which provides a mutable wrapper for a C array.  This is complete enough
    for the purpose of the tests, but may need additional work for more
    general use.  Also fix some documentation typos noticed in the existing
    code.
    
    Also, fix unintuitive behavior of etTIME options that was revealed by
    the tests.
    
    Change-Id: I4b8b3ad92b0efe24fdf478cd39ba246692b036c7

--- src/gromacs/commandline/pargs.cpp
+++ src/gromacs/commandline/pargs.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -866,7 +866,7 @@ gmx_bool parse_common_args(int *argc, char *argv[], unsigned long Flags,
 
     for (i = 0; i < npall; i++)
     {
-        if ((all_pa[i].type == etTIME) && (*all_pa[i].u.r >= 0))
+        if (all_pa[i].type == etTIME && all_pa[i].bSet)
         {
             *all_pa[i].u.r *= output_env_get_time_invfactor(*oenv);
         }
