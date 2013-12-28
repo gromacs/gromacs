8d44ebf147bedc34e39d38531ca53d212bbd997f
Teemu Murtola <teemu.murtola@gmail.com>
2013-12-28 08:35:26 +0200

    Simplify ProgramInfo
    Remove realBinaryName() and invariantBinaryName() from ProgramInfo, as
    they were only necessary for the command line module manager.  Instead,
    pass the binary name to CommandLineModuleManager constructor and move
    the invariant name logic into the manager.  This removes unnecessary
    functionality from ProgramInfo, allowing to simplify it further.
    
    Change-Id: I2f18dffcb5dde8e2101376444435bb13daf8c236

--- src/programs/gmx/gmx.cpp
+++ src/programs/gmx/gmx.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012,2013, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -48,10 +48,10 @@
 int
 main(int argc, char *argv[])
 {
-    gmx::ProgramInfo &info = gmx::init("gmx", &argc, &argv);
+    gmx::ProgramInfo &info = gmx::init(&argc, &argv);
     try
     {
-        gmx::CommandLineModuleManager manager(&info);
+        gmx::CommandLineModuleManager manager("gmx", &info);
         registerTrajectoryAnalysisModules(&manager);
         registerLegacyModules(&manager);
         manager.addHelpTopic(gmx::SelectionCollection::createDefaultHelpTopic());
