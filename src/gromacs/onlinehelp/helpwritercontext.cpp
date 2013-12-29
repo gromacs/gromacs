558f7c13ec1f1859b82f9a6d75ee248ff23e7caa
Teemu Murtola <teemu.murtola@gmail.com>
2013-12-29 13:46:11 +0200

    Refactor ProgramInfo use
    Instead of having ProgramInfo in src/gromacs/utility/, declare only
    a program context interface there, and provide simple setters and
    accessors for it.  Move all the logic related to actually providing the
    values (in particular, the location of the binary) into
    src/gromacs/commandline/, which provides an implementation for the new
    interface.
    
    This provides two main benefits:
     - The responsibilities of the different modules are clearer, and there
       is no complex logic related to command line programs in the
       bottom layer.
     - The API provided for external code for initializing the library is a
       lot cleaner.  They can either choose to use all the command line
       logic by using gmx::initForCommandLine(), or provide their own
       implementation for the program context, and set it directly with
       gmx::setProgramContext().
    
    Thread safety guarantees have changed somewhat; the implementation is
    much simpler this way, and I don't think we need to be 100% thread safe
    in methods such as setProgramContext() (gmx::init() isn't, either).
    
    Change-Id: I2b89aeb3833e7e40e5dd2322745efbc44581e15b

--- src/gromacs/onlinehelp/helpwritercontext.cpp
+++ src/gromacs/onlinehelp/helpwritercontext.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -52,7 +52,7 @@
 #include "gromacs/utility/exceptions.h"
 #include "gromacs/utility/file.h"
 #include "gromacs/utility/gmxassert.h"
-#include "gromacs/utility/programinfo.h"
+#include "gromacs/utility/programcontext.h"
 #include "gromacs/utility/stringutil.h"
 
 namespace gmx
@@ -524,7 +524,7 @@ class HelpWriterContext::Impl
 
 void HelpWriterContext::Impl::initDefaultReplacements()
 {
-    const char *program = ProgramInfo::getInstance().programName().c_str();
+    const char *program = getProgramContext().programName();
     addReplacement("[PROGRAM]", program);
 }
 
