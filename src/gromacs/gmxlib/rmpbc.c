ccb0ab702acf2dd451b72f08043d7cfb919f3d65
Teemu Murtola <teemu.murtola@gmail.com>
2014-01-04 20:59:13 +0200

    Move gstat.h to gmxana/
    Remove unnecessary inclusions of gstat.h from outside gmxana/.
    Now that the header is no longer used outside gmxana/, move it into that
    directory.  Still install the header, since it contains a wide mixture
    of stuff, some of which may actually be used by user tools.
    
    This cleans up the include dependency graph considerably, since the
    legacyheaders/ directory no longer depends on commandline/.  This was a
    source of a cyclic dependency that made the graph layout very confusing.
    
    Change-Id: I3843f2773d6b393e4d260e84478ab559ab8e00d7

--- src/gromacs/gmxlib/rmpbc.c
+++ src/gromacs/gmxlib/rmpbc.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -43,9 +43,10 @@
 #include "smalloc.h"
 #include "mshift.h"
 #include "pbc.h"
-#include "gstat.h"
-#include "gromacs/fileio/futil.h"
+#include "rmpbc.h"
 #include "vec.h"
+
+#include "gromacs/fileio/futil.h"
 
 typedef struct {
     int      natoms;
