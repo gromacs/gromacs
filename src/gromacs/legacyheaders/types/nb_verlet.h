de83f42dd53608f90cb6f6bb7e52225e38c3618e
Roland Schulz <roland@utk.edu>
2014-01-10 17:42:02 -0500

    Add cmake option for Reference kernel
    Change-Id: I6b6cb9c7eb575d8511dccd8e0bf410f44383259d

--- src/gromacs/legacyheaders/types/nb_verlet.h
+++ src/gromacs/legacyheaders/types/nb_verlet.h
@@ -43,18 +43,6 @@
 extern "C" {
 #endif
 
-
-/* For testing the reference plain-C SIMD kernels, uncomment the next lines,
- * as well as the GMX_SIMD_REFERENCE_PLAIN_C define in gromacs/simd/macros.h
- * The actual SIMD width is set in gromacs/simd/macros.h
- * The 4xN reference kernels support 2-, 4- and 8-way SIMD.
- * The 2x(N+N) reference kernels support 8- and 16-way SIMD.
- */
-/* #define GMX_NBNXN_SIMD */
-/* #define GMX_NBNXN_SIMD_4XN */
-/* #define GMX_NBNXN_SIMD_2XNN */
-
-
 #if (defined GMX_X86_SSE2) || (defined GMX_CPU_ACCELERATION_IBM_QPX)
 /* Use SIMD accelerated nbnxn search and kernels */
 #define GMX_NBNXN_SIMD
