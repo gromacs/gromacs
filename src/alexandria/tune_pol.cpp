2ffad9855fb113e03142550a984957e911a441d6
David van der Spoel <spoel@xray.bmc.uu.se>
2014-01-10 16:28:58 +0100

    updates
    Change-Id: Ia2522a0bd5d82c607e76a5ca44a89cb2b33b03bd

--- src/alexandria/tune_pol.cpp
+++ src/alexandria/tune_pol.cpp
@@ -558,6 +558,9 @@ int alex_tune_pol(int argc, char *argv[])
         "atomtypes for which the polarizability is zero, and for which",
         "there is sufficient data in the input data file ([TT]-f[tt] option)",
         "will be used in the least-squares fit (done by matrix inversion).[PAR]"
+        "Bootstrapping can be used to estimate the error in the resulting parameters",
+        "and this will be stored in the resulting file such that programs",
+        "using the parameters can estimate an error in the polarizability.[PAR]",
         "A selection of molecules into a training set and a test set (or ignore set)",
         "can be made using option [TT]-sel[tt]. The format of this file is:[BR]",
         "iupac|Train[BR]",
