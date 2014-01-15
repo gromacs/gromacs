cb9ae5e0754e3e1543127ab6d6c9f6a5ba1bd11b
Teemu Murtola <teemu.murtola@gmail.com>
2014-01-15 21:19:25 +0200

    Fix build with GMX_BUILD_UNITTESTS=OFF
    Don't build the object libraries used for unit tests either if the unit
    tests are disabled.  Added a helper macro for this as well.
    
    Change-Id: I7b9f899dd2ee8adef261e49d3f8100530487ed54

--- src/testutils/TestMacros.cmake
+++ src/testutils/TestMacros.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2011,2012,2013, by the GROMACS development team, led by
+# Copyright (c) 2011,2012,2013,2014, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -31,6 +31,13 @@
 #
 # To help us fund GROMACS development, we humbly ask that you cite
 # the research papers on the package. Check out http://www.gromacs.org.
+
+function (gmx_add_unit_test_object_library NAME)
+    if (GMX_BUILD_UNITTESTS AND BUILD_TESTING)
+        include_directories(${GMOCK_INCLUDE_DIRS})
+        add_library(${NAME} OBJECT ${ARGN})
+    endif ()
+endfunction ()
 
 function (gmx_build_unit_test NAME EXENAME)
     if (GMX_BUILD_UNITTESTS AND BUILD_TESTING)
