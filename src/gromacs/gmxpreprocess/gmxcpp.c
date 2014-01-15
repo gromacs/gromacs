95dd4a757f585f0c4bbdc7f2467d06d81803d884
Mark Abraham <mark.j.abraham@gmail.com>
2014-01-15 18:02:33 +0100

    Clean up after gmxcpp
    Using grompp in integration tests with potentially multiple different
    calls to gmx_grompp per executable requires that we start from a clean
    state every time we do such a call. Old code was re-using -I paths,
    and both -D and #defines, which was breaking some tests I am working
    on.
    
    Also added some other done_* calls on the general theory of trying not
    to be evil.
    
    Change-Id: I6016bc1786c8ecc9303eeee2ad4a83975117ca5f

--- src/gromacs/gmxpreprocess/gmxcpp.c
+++ src/gromacs/gmxpreprocess/gmxcpp.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -173,6 +173,18 @@ static void add_include(const char *include)
     }
 }
 
+static void done_includes()
+{
+    int i;
+    for (i = 0; (i < nincl); i++)
+    {
+        sfree(incl[i]);
+    }
+    sfree(incl);
+    incl  = NULL;
+    nincl = 0;
+}
+
 static void add_define(const char *name, const char *value)
 {
     int  i;
@@ -207,6 +219,19 @@ static void add_define(const char *name, const char *value)
     {
         defs[i].def  = NULL;
     }
+}
+
+static void done_defines()
+{
+    int i;
+    for (i = 0; (i < ndef); i++)
+    {
+        sfree(defs[i].name);
+        sfree(defs[i].def);
+    }
+    sfree(defs);
+    defs = NULL;
+    ndef = 0;
 }
 
 /* Open the file to be processed. The handle variable holds internal
@@ -748,6 +773,12 @@ int cpp_close_file(gmx_cpp_t *handlep)
     }
 
     return eCPP_OK;
+}
+
+void cpp_done()
+{
+    done_includes();
+    done_defines();
 }
 
 /* Return a string containing the error message coresponding to status
