4f23839d306fe03256fc8a76fb053e0de309f028
David van der Spoel <spoel@xray.bmc.uu.se>
2014-01-19 08:51:06 +0100

    Moved random number generator code to separate dir.
    GROMACS sports two random number generators for historical
    reason. The oldest one is phased out in this patch.
    Changing the random number
    generator means that e.g. replica exchange simulations will
    not be reproducible (if they ever were in practice).
    
    Change-Id: If24f1019fba9b9412713b5fe04b5c0a8a7638468

--- src/gromacs/selection/tests/nbsearch.cpp
+++ src/gromacs/selection/tests/nbsearch.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -52,7 +52,7 @@
 #include <set>
 #include <vector>
 
-#include "gromacs/legacyheaders/gmx_random.h"
+#include "gromacs/random/random.h"
 #include "gromacs/legacyheaders/pbc.h"
 #include "gromacs/legacyheaders/smalloc.h"
 #include "gromacs/legacyheaders/vec.h"
