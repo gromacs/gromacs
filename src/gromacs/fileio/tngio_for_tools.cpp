4bc613eadf6b5790887fa5c759a91d305858eb57
Mark Abraham <mark.j.abraham@gmail.com>
2014-01-20 18:57:55 +0100

    Improved dependencies on maths includes
    Our physics.h should have a dependency on our maths header for
    PI.
    
    Made explicit some dependencies on system math.h that were being
    covered transitively.
    
    Change-Id: I5a35ff10860a84688bfe73260acbd63166f4caec

--- src/gromacs/fileio/tngio_for_tools.cpp
+++ src/gromacs/fileio/tngio_for_tools.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -37,6 +37,8 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+
+#include <math.h>
 
 #include "tngio.h"
 #include "trx.h"
