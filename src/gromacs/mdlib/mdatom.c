gromacs/gromacs

commit 1c4ab8dc85d1b81954671d9d0071d8f84e026aff

Calculating LJ-interactions using Particle mesh Ewald
The calls to ewald_LRcorrection and gmx_pme_do in force.c have been
changed to also handle LJ-PME. The unused implementation with
tables in ewald_LRcorrection have been removed, but would in principle
be faster. But since the Verlet kernels won't call the routine and
group kernels are on a limited lifespan we do not do this optimization.

The switch-statement over the different electrostatic interactions in
force.c are now handled with if-statements.

The grids in PME have been modified to now be larger "arrays of grids",
where the first two are reserved for electrostatics(A and B-state) and
the remaining ones are for LJ-PME.

The code in gmx_pme_do have been changed to also handle LJ-PME.
Previous loop over 2 states in electrostatic PME now also loops
over two additional states for LJ-PME with geometric combination
rules. At the end it contains a separate part which loops over
the various terms for the case with LB combination rules.

Added a new enum for the parameters to be communicated in pme_pp.c

A lot of small changes in various other files mostly to fix
variable-names and small LJ-PME related details. Kernel-generating
scripts for AVX are modified to access fr->ewaldcoeff_q instead of
fr->ewaldcoeff, which doesn't exist any more.

Turning on LJ-PME in simulations is just a matter of setting
vdw-type = PME in the .mdp-file and setting "lj-pme-comb-rule"
to either "LB" or "geometric"

Manual updated with new section called "Long-range Wan der
Waals interactions" which includes the old "Dispersion correction"
and a section for LJ-PME.

Uncrustified files and updated copyright headers

TODO:
1. Full functionality for LJ-PME with free energy calculations, the basic
   framework is there but currently it gives a gmx_fatal
2. LJ-PME with verlet-kernels, currently gives gmx_fatal
3. Integrate with PME tuning

Change-Id: I6395b0a6bcb887b30311143415fde5496c6f3d38

src/gromacs/mdlib/mdatom.c
@@ -1,43 +1,45 @@
-/* -*- mode: c; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; c-file-style: "stroustrup"; -*-
+/*
+ * This file is part of the GROMACS molecular simulation package.
  *
- *
- *                This source code is part of
- *
- *                 G   R   O   M   A   C   S
- *
- *          GROningen MAchine for Chemical Simulations
- *
- *                        VERSION 3.2.0
- * Written by David van der Spoel, Erik Lindahl, Berk Hess, and others.
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
- * Copyright (c) 2001-2004, The GROMACS development team,
- * check out http://www.gromacs.org for more information.
-
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
+ * Copyright (c) 2001-2004, The GROMACS development team.
+ * Copyright (c) 2013, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
  * of the License, or (at your option) any later version.
  *
- * If you want to redistribute modifications, please consider that
- * scientific software is very special. Version control is crucial -
- * bugs must be traceable. We will be happy to consider code for
- * inclusion in the official distribution, but derived work must not
- * be called official GROMACS. Details are found in the README & COPYING
- * files - if they are missing, get the official version at www.gromacs.org.
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
  *
  * To help us fund GROMACS development, we humbly ask that you cite
- * the papers on the package - you can find them in the top README file.
- *
- * For more info, check our website at http://www.gromacs.org
- *
- * And Hey:
- * GROwing Monsters And Cloning Shrimps
+ * the research papers on the package. Check out http://www.gromacs.org.
  */
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
 
 #include "typedefs.h"
+#include "maths.h"
 #include "mdatoms.h"
 #include "smalloc.h"
 #include "main.h"
@@ -82,6 +84,10 @@ t_mdatoms *init_mdatoms(FILE *fp, gmx_mtop_t *mtop, gmx_bool bFreeEnergy)
             {
                 md->nChargePerturbed++;
             }
+            if (atom->typeB != atom->type)
+            {
+                md->nTypePerturbed++;
+            }
         }
 
         tmA += atom->m;
@@ -111,6 +117,7 @@ void atoms2md(gmx_mtop_t *mtop, t_inputrec *ir,
     gmx_mtop_atomlookup_t alook;
     int                   i;
     t_grpopts            *opts;
+    real                  c6, c12;
     gmx_groups_t         *groups;
     gmx_molblock_t       *molblock;
 
@@ -145,9 +152,15 @@ void atoms2md(gmx_mtop_t *mtop, t_inputrec *ir,
         srenew(md->massT, md->nalloc);
         srenew(md->invmass, md->nalloc);
         srenew(md->chargeA, md->nalloc);
+        srenew(md->c6A, md->nalloc);
+        srenew(md->sigmaA, md->nalloc);
+        srenew(md->sigma3A, md->nalloc);
         if (md->nPerturbed)
         {
             srenew(md->chargeB, md->nalloc);
+            srenew(md->c6B, md->nalloc);
+            srenew(md->sigmaB, md->nalloc);
+            srenew(md->sigma3B, md->nalloc);
         }
         srenew(md->typeA, md->nalloc);
         if (md->nPerturbed)
@@ -298,13 +311,38 @@ void atoms2md(gmx_mtop_t *mtop, t_inputrec *ir,
         {
             md->invmass[i]    = 1.0/mA;
         }
-        md->chargeA[i]  = atom->q;
-        md->typeA[i]    = atom->type;
+        md->chargeA[i]      = atom->q;
+        md->typeA[i]        = atom->type;
+        c6                  = mtop->ffparams.iparams[atom->type*(mtop->ffparams.atnr+1)].lj.c6;
+        c12                 = mtop->ffparams.iparams[atom->type*(mtop->ffparams.atnr+1)].lj.c12;
+        md->c6A[i]          = sqrt(c6);
+        if (c6 == 0.0 || c12 == 0.0)
+        {
+            md->sigmaA[i]     = 1.0;
+        }
+        else
+        {
+            md->sigmaA[i]     = pow(c12/c6, 1.0/6.0);
+        }
+        md->sigma3A[i] = 1/(md->sigmaA[i]*md->sigmaA[i]*md->sigmaA[i]);
+
         if (md->nPerturbed)
         {
             md->chargeB[i]    = atom->qB;
             md->typeB[i]      = atom->typeB;
+            c6                = mtop->ffparams.iparams[atom->typeB*(mtop->ffparams.atnr+1)].lj.c6;
+            c12               = mtop->ffparams.iparams[atom->typeB*(mtop->ffparams.atnr+1)].lj.c12;
+            md->c6B[i]        = sqrt(c6);
+            if (c6 == 0.0 || c12 == 0.0)
+            {
+                md->sigmaB[i]   = 1.0;
+            }
+            else
+            {
+                md->sigmaB[i]   = pow(c12/c6, 1.0/6.0);
+            }
             md->bPerturbed[i] = PERTURBED(*atom);
+            md->sigma3B[i]    = 1/(md->sigmaB[i]*md->sigmaB[i]*md->sigmaB[i]);
         }
         md->ptype[i]    = atom->ptype;
         if (md->cTC)

commit 1a25b55317ae2fc556cbb65c20aa9fb372ca9c5d

Fix race conditions
progBal problem didn't cause probles because the value written
to the same value on each thread. mdatom.c (c6/c12) problem
was introduced in commit 1c4ab8dc85d1b.

Change-Id: Ia61d214f82771b181308573e4ae1adac94ce5ffa

src/gromacs/mdlib/mdatom.c
@@ -225,7 +225,7 @@ void atoms2md(gmx_mtop_t *mtop, t_inputrec *ir,
 
     alook = gmx_mtop_atomlookup_init(mtop);
 
-#pragma omp parallel for num_threads(gmx_omp_nthreads_get(emntDefault)) schedule(static)
+#pragma omp parallel for num_threads(gmx_omp_nthreads_get(emntDefault)) schedule(static), private(c6,c12)
     for (i = 0; i < md->nr; i++)
     {
         int      g, ag, molb;

src/gromacs/mdlib/nbnxn_search.c
@@ -5048,6 +5048,11 @@ void nbnxn_make_pairlist(const nbnxn_search_t  nbs,
                 ci_block = get_ci_block_size(gridi, nbs->DomDec, nnbl);
             }
 
+            /* With GPU: generate progressively smaller lists for
+             * load balancing for local only or non-local with 2 zones.
+             */
+            progBal = (LOCAL_I(iloc) || nbs->zones->n <= 2);
+
 #pragma omp parallel for num_threads(nnbl) schedule(static)
             for (th = 0; th < nnbl; th++)
             {
@@ -5064,11 +5069,6 @@ void nbnxn_make_pairlist(const nbnxn_search_t  nbs,
                 {
                     clear_pairlist(nbl[th]);
                 }
-
-                /* With GPU: generate progressively smaller lists for
-                 * load balancing for local only or non-local with 2 zones.
-                 */
-                progBal = (LOCAL_I(iloc) || nbs->zones->n <= 2);
 
                 /* Divide the i super cell equally over the nblists */
                 nbnxn_make_pairlist_part(nbs, gridi, gridj,