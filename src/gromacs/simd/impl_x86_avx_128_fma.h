8e0586a89586bbb77a16177e9f8d26489914e90a
Erik Lindahl <erik@kth.se>
2014-01-22 18:30:10 +0100

    Redesigned SIMD module and unit tests.
    Second part of the commit (after renaming), which contains
    the actual functionality changes. This new version
    implements a complete interface layer that
    is architecture-agnostic, while each architecture has a
    separate implementation file with both single, double and
    simd4 versions (that can be used simultaneously). simd.h
    contains a number of defines that describe the capabilities
    of the instruction set, and there is a new documentation
    module for Doxygen. This will be used in two upcoming patches
    for redesigned group and verlet kernels, respectively. With
    these, we aim to remove all architecture-specific SIMD code
    from the rest of Gromacs.
    All SIMD math functions have been redesigned so they work even
    with instruction sets that do not support integers (and even
    on sets that do not support logical operations), and
    accuracy has been improved for double precision sincos() by
    removing the table implementation. To try to reduce
    the size of this relatively large patch I have kept a few
    header files (in particular the math files in gromacs/simd)
    to avoid touching all the group kernels. The implementations
    for QPX and MIC versions still need to be tested better, but
    since we cannot run the unit tests on those right now (We will
    soon have a MIC card in Jenkins), we wil do that with the
    kernel modifications. With this new kernel module, 256-bit
    AVX2 SIMD acceleration will now automatically be enabled for
    the verlet kernels. Group kernels will use AVX_256 in this case.
    
    Change-Id: I643da75f346f120500682bcc4bcc1333a635db70

---
+++ src/gromacs/simd/impl_x86_avx_128_fma.h
@@ -0,0 +1,133 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
+ * Copyright (c) 2001-2012, The GROMACS Development Team.
+ * Copyright (c) 2012,2013,2014, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+#ifndef GMX_SIMD_IMPL_X86_AVX_128_FMA_H
+#define GMX_SIMD_IMPL_X86_AVX_128_FMA_H
+
+#ifndef GMX_SIMD_SIMD_H
+#    error "Never include architecture-specific simd headers directly; use simd.h."
+#endif
+
+#include <math.h>
+#include <immintrin.h>
+#include <x86intrin.h>
+
+/* x86 128-bit AVX with FMA SIMD instruction wrappers
+ *
+ * Please see documentation in gromacs/simd/simd.h for details
+ */
+
+/* Inherit parts of AVX_128_FMA from SSE4.1 */
+#include "impl_x86_sse4_1.h"
+/* Increment over SSE4.1 capabilities */
+#define GMX_SIMD_X86_AVX_128_FMA_OR_HIGHER
+
+/* Override some capability definitions for things added in AVX over SSE4.1 */
+#define GMX_SIMD_HAVE_FMA
+#define GMX_SIMD_HAVE_FRACTION
+#define GMX_SIMD4_HAVE_DOUBLE  /* We can use 256-bit operations for this */
+
+/* SINGLE */
+#undef  gmx_simd_fmadd_ps
+#define gmx_simd_fmadd_ps           _mm_macc_ps
+#undef  gmx_simd_fmsub_ps
+#define gmx_simd_fmsub_ps(a, b, c)    _mm_msub_ps
+#undef  gmx_simd_fnmadd_ps
+#define gmx_simd_fnmadd_ps(a, b, c)   _mm_nmacc_ps
+#undef  gmx_simd_fnmsub_ps
+#define gmx_simd_fnmsub_ps(a, b, c)   _mm_nmsub_ps
+#undef  gmx_simd_fraction_f
+#define gmx_simd_fraction_f         _mm_frcz_ps
+
+/* DOUBLE */
+#undef  gmx_simd_fmadd_pd
+#define gmx_simd_fmadd_pd            _mm_macc_pd
+#undef  gmx_simd_fmsub_pd
+#define gmx_simd_fmsub_pd(a, b, c)     _mm_msub_pd
+#undef  gmx_simd_fnmadd_pd
+#define gmx_simd_fnmadd_pd(a, b, c)    _mm_nmacc_pd
+#undef  gmx_simd_fnmsub_pd
+#define gmx_simd_fnmsub_pd(a, b, c)    _mm_nmsub_pd
+#undef  gmx_simd_fraction_d
+#define gmx_simd_fraction_d          _mm_frcz_pd
+
+/* Even if the _main_ SIMD implementation for this architecture file corresponds
+ * to 128-bit AVX (since it will be faster), the 256-bit operations will always
+ * be available in AVX, so we can use them for double precision SIMD4!
+ */
+/* SIMD4 Double precision floating point */
+#define gmx_simd4_double_t               __m256d
+#define gmx_simd4_load_d                 _mm256_load_pd
+#define gmx_simd4_load1_d                _mm256_load1_pd
+#define gmx_simd4_set1_d                 _mm256_set1_pd
+#define gmx_simd4_store_d                _mm256_store_pd
+#define gmx_simd4_loadu_d                _mm256_loadu_pd
+#define gmx_simd4_storeu_d               _mm256_storeu_pd
+#define gmx_simd4_setzero_d              _mm256_setzero_pd
+#define gmx_simd4_add_d                  _mm256_add_pd
+#define gmx_simd4_sub_d                  _mm256_sub_pd
+#define gmx_simd4_mul_d                  _mm256_mul_pd
+#define gmx_simd4_fmadd_d                _mm256_macc_pd
+#define gmx_simd4_fmsub_d                _mm256_msub_pd
+#define gmx_simd4_fnmadd_d               _mm256_nmacc_pd
+#define gmx_simd4_fnmsub_d               _mm256_nmsub_pd
+#define gmx_simd4_and_d                  _mm256_and_pd
+#define gmx_simd4_andnot_d               _mm256_andnot_pd
+#define gmx_simd4_or_d                   _mm256_or_pd
+#define gmx_simd4_xor_d                  _mm256_xor_pd
+#define gmx_simd4_rsqrt_d(x)             _mm256_cvtps_pd(_mm_rsqrt_ps(_mm256_cvtpd_ps(x)))
+#define gmx_simd4_fabs_d(x)              _mm256_andnot_pd(_mm256_set1_pd(-0.0), x)
+#define gmx_simd4_fneg_d(x)              _mm256_xor_pd(x, _mm256_set1_pd(-0.0))
+#define gmx_simd4_max_d                  _mm256_max_d
+#define gmx_simd4_min_d                  _mm256_min_d
+#define gmx_simd4_round_d(x)             _mm256_round_pd(x, _MM_FROUND_NINT)
+#define gmx_simd4_trunc_d(x)             _mm256_round_pd(x, _MM_FROUND_TRUNC)
+/* SIMD4 booleans corresponding to double */
+#define gmx_simd4_dbool_t                __m256
+#define gmx_simd4_cmpeq_d                _mm256_cmpeq_pd
+#define gmx_simd4_cmplt_d                _mm256_cmplt_pd
+#define gmx_simd4_cmple_d                _mm256_cmple_pd
+#define gmx_simd4_and_db                 _mm256_and_pd
+#define gmx_simd4_or_db                  _mm256_or_pd
+#define gmx_simd4_anytrue_db             _mm256_movemask_pd
+#define gmx_simd4_blendzero_d            _mm256_and_pd
+#define gmx_simd4_blendv_d               _mm256_blendv_pd
+/* SIMD4 float/double conversion */
+#define gmx_simd4_cvt_f2d                _mm256_cvtps_pd
+#define gmx_simd4_cvt_d2f                _mm256_cvtpd_ps
+
+#endif /* GMX_SIMD_IMPL_X86_AVX_128_FMA_H */
