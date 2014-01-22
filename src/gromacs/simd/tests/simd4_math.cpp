9ca8009be0b8ce9a2eb48b05ccca1ca65fc8af94
Erik Lindahl <erik@kth.se>
2014-01-22 18:30:10 +0100

    Redesigned SIMD module and unit tests.
    Second part of the commit (after renaming), which contains
    the actual functionality changes. This new version
    implements a complete interface layer that
    is architecture-agnostic, while each architecture has a
    separate implementation file with both single, double and
    simd4 versions (that can be used simultaneously). simd.h
    contains a number of defines that describe the capabilities
    of the instruction set, and there is a new documentation
    module for Doxygen. This will be used in two upcoming patches
    for redesigned group and verlet kernels, respectively. With
    these, we aim to remove all architecture-specific SIMD code
    from the rest of Gromacs.
    All SIMD math functions have been redesigned so they work even
    with instruction sets that do not support integers (and even
    on sets that do not support logical operations), and
    accuracy has been improved for double precision sincos() by
    removing the table implementation. To try to reduce
    the size of this relatively large patch I have kept a few
    header files (in particular the math files in gromacs/simd)
    to avoid touching all the group kernels. The implementations
    for QPX and MIC versions still need to be tested better, but
    since we cannot run the unit tests on those right now (We will
    soon have a MIC card in Jenkins), we wil do that with the
    kernel modifications. With this new kernel module, 256-bit
    AVX2 SIMD acceleration will now automatically be enabled for
    the verlet kernels. Group kernels will use AVX_256 in this case.
    
    Change-Id: I643da75f346f120500682bcc4bcc1333a635db70

---
+++ src/gromacs/simd/tests/simd4_math.cpp
@@ -0,0 +1,65 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2012,2013,2014, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <vector>
+
+#include "gromacs/simd/simd_math.h"
+#include "gromacs/math/utilities.h"
+
+#include "simd4_util.h"
+
+namespace simd4Test
+{
+
+#ifdef GMX_SIMD4_HAVE_REAL
+
+// Helper function wrapper
+real ref_invsqrt(real x)
+{
+    return 1.0/sqrt(x);
+}
+
+TEST(Simd4TestMath, gmxSimd4InvsqrtR)
+{
+    std::pair<real, real>  range(1e-10, 1e10);
+    GMX_ASSERT_SIMD4_FUNC_NEAR(ref_invsqrt, gmx_simd4_invsqrt_r, range, defaultTol, 0);
+}
+
+#endif
+
+}
