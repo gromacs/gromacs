b53c7695e487d882f0a68a3747294c3b847c4e4a
Mark Abraham <mark.j.abraham@gmail.com>
2014-02-04 16:29:15 +0100

    Move orientation restraints checks inside init function
    This removes clutter from runner(), much like init_disre() already
    does. Removed duplicate check for the number of orientation restraints
    being zero. Removed an unused function parameter.
    
    Change-Id: Icc6a08a2ed4af010e65bfe1f5e49b6c229454df1

--- src/gromacs/gmxlib/orires.c
+++ src/gromacs/gmxlib/orires.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -53,30 +53,43 @@
 void init_orires(FILE *fplog, const gmx_mtop_t *mtop,
                  rvec xref[],
                  const t_inputrec *ir,
-                 const gmx_multisim_t *ms, t_oriresdata *od,
-                 t_state *state)
+                 const t_commrec *cr, t_oriresdata *od,
+                 t_state *state, gmx_bool bIsParticleDecomposition)
 {
-    int                     i, j, d, ex, nmol, nr, *nr_ex;
+    int                     i, j, d, ex, nmol, *nr_ex;
     double                  mtot;
     rvec                    com;
     gmx_mtop_ilistloop_t    iloop;
     t_ilist                *il;
     gmx_mtop_atomloop_all_t aloop;
     t_atom                 *atom;
+    const gmx_multisim_t   *ms;
+
+    od->nr = gmx_mtop_ftype_count(mtop, F_ORIRES);
+    if (0 == od->nr)
+    {
+        /* Not doing orientation restraints */
+        return;
+    }
+
+    if (PAR(cr) && !bIsParticleDecomposition)
+    {
+        gmx_fatal(FARGS, "Orientation restraints do not work (yet) with domain decomposition, use particle decomposition (mdrun option -pd)");
+    }
+    /* Orientation restraints */
+    if (!MASTER(cr))
+    {
+        /* Nothing to do */
+        return;
+    }
+    ms = cr->ms;
 
     od->fc  = ir->orires_fc;
     od->nex = 0;
     od->S   = NULL;
-
     od->M   = NULL;
     od->eig = NULL;
     od->v   = NULL;
-
-    od->nr = gmx_mtop_ftype_count(mtop, F_ORIRES);
-    if (od->nr == 0)
-    {
-        return;
-    }
 
     nr_ex = NULL;
 
