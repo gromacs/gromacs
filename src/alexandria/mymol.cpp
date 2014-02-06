7301f2eeb5849010b75ebc35364be05cd6902f25
David van der Spoel <spoel@xray.bmc.uu.se>
2014-02-06 21:46:38 +0100

    Updaye
    Change-Id: If1b1c531fbe0ef17ae6176930afeb0f1aaae64f0

--- src/alexandria/mymol.cpp
+++ src/alexandria/mymol.cpp
@@ -1108,75 +1108,77 @@ immStatus MyMol::GenerateCharges(gmx_poldata_t pd,
 
     if (immOK == imm)
     {
-        if (gmx_resp_add_atom_info(gr_, &topology_->atoms, pd))
+        switch (iModel)
         {
-            gmx_resp_add_atom_symmetry(gr_, symmetric_charges_);
-            gmx_resp_update_atomtypes(gr_, &(topology_->atoms));
-            gmx_resp_summary(debug, gr_, symmetric_charges_);
-            gmx_resp_add_atom_coords(gr_, x_);
+        case eqgRESP:
+        case eqgRESPG:
+            if (gmx_resp_add_atom_info(gr_, &topology_->atoms, pd))
+            {
+                gmx_resp_add_atom_symmetry(gr_, symmetric_charges_);
+                gmx_resp_update_atomtypes(gr_, &(topology_->atoms));
+                gmx_resp_summary(debug, gr_, symmetric_charges_);
+                gmx_resp_add_atom_coords(gr_, x_);
+                /* Even if we get the right LoT it may still not have
+                 * the ESP
+                 */
+                CalculationIterator ci = GetLotPropType(lot,
+                                                        MPO_POTENTIAL,
+                                                        NULL);
+                if (ci != EndCalculation())
+                {
+                    //printf("There are %d potential points\n",ci->NPotential());
+                    for (ElectrostaticPotentialIterator epi = ci->BeginPotential(); (epi < ci->EndPotential()); epi++)
+                    {
+                        /* Maybe not convert to gmx ? */
+                        int xu = string2unit(epi->GetXYZunit().c_str());
+                        int vu = string2unit(epi->GetVunit().c_str());
+                        if (-1 == xu)
+                        {
+                            xu = eg2cAngstrom;
+                        }
+                        if (-1 == vu)
+                        {
+                            vu = eg2cHartree_e;
+                        }
+                        gmx_resp_add_point(gr_,
+                                           convert2gmx(epi->GetX(), xu),
+                                           convert2gmx(epi->GetY(), xu),
+                                           convert2gmx(epi->GetZ(), xu),
+                                           convert2gmx(epi->GetV(), vu));
+                    }
+                }
+            }
+            break;
+        case eqgESP:
+            fprintf(stderr, "ESP not implemented right now\n");
+            break;
+        case eqgNone:
+            /* Check which algorithm to use for charge generation */
+            strcpy(qgen_msg, "");
+            printf("Using zero charges!\n");
+            for (i = 0; (i < topology_->atoms.nr); i++)
+            {
+                topology_->atoms.atom[i].q  = topology_->atoms.atom[i].qB = 0;
+            }
+            eQGEN = eQGEN_OK;
+            break;
+        default:
+            qgen_ = gentop_qgen_init(pd, &topology_->atoms, ap, x_, iModel, hfac, GetCharge(), epsr);
             
-            /* Even if we get the right LoT it may still not have
-             * the ESP
-             */
-            CalculationIterator ci = GetLotPropType(lot,
-                                                    MPO_POTENTIAL,
-                                                    NULL);
-            if (ci != EndCalculation())
+            if (NULL == qgen_)
             {
-                //printf("There are %d potential points\n",ci->NPotential());
-                for (ElectrostaticPotentialIterator epi = ci->BeginPotential(); (epi < ci->EndPotential()); epi++)
-                {
-                    /* Maybe not convert to gmx ? */
-                    int xu = string2unit(epi->GetXYZunit().c_str());
-                    int vu = string2unit(epi->GetVunit().c_str());
-                    if (-1 == xu)
-                    {
-                        xu = eg2cAngstrom;
-                    }
-                    if (-1 == vu)
-                    {
-                        vu = eg2cHartree_e;
-                    }
-                    gmx_resp_add_point(gr_,
-                                       convert2gmx(epi->GetX(), xu),
-                                       convert2gmx(epi->GetY(), xu),
-                                       convert2gmx(epi->GetZ(), xu),
-                                       convert2gmx(epi->GetV(), vu));
-                }
+                gmx_fatal(FARGS, "Can not generate charges for %s. Probably due to issues with atomtype detection or support.\n", GetMolname().c_str());
             }
-        }
-        if ( 0 ) {
-            switch (iModel) 
-            {
-            case eqgRESP:
-            case eqgNone:
-                /* Check which algorithm to use for charge generation */
-                strcpy(qgen_msg, "");
-                printf("Using zero charges!\n");
-                for (i = 0; (i < topology_->atoms.nr); i++)
-                {
-                    topology_->atoms.atom[i].q  = topology_->atoms.atom[i].qB = 0;
-                }
-                eQGEN = eQGEN_OK;
-                break;
-            default:
-                qgen_ = gentop_qgen_init(pd, &topology_->atoms, ap, x_, iModel, hfac, GetCharge(), epsr);
-                
-                if (NULL == qgen_)
-                {
-                    gmx_fatal(FARGS, "Can not generate charges for %s. Probably due to issues with atomtype detection or support.\n", GetMolname().c_str());
-                }
-                eQGEN = generate_charges(NULL,
-                                         qgen_, gr_, GetMolname().c_str(), 
+            eQGEN = generate_charges(NULL,
+                                     qgen_, NULL, GetMolname().c_str(), 
                                      pd, &topology_->atoms, 0.0001,
-                                         10000, 1, ap);
-                qgen_message(qgen_, sizeof(qgen_msg), qgen_msg, gr_);
-                if (eQGEN_OK != eQGEN)
-                {
-                    imm = immChargeGeneration;
-                }
-                break;
+                                     10000, 1, ap);
+            qgen_message(qgen_, sizeof(qgen_msg), qgen_msg, gr_);
+            if (eQGEN_OK != eQGEN)
+            {
+                imm = immChargeGeneration;
             }
+            break;
         }
     }
     return imm;
