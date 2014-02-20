9b4b94a7f9fc016393b50ea44b55edbb23a63688
Rossen Apostolov <rossen@kth.se>
2014-02-20 16:34:45 +0100

    Removed the deprecated gmx.ff and gmx2.ff.
    Also fixed use of tip3p.itp in an integration test.
    
    There are still a few places in the manual that need to be fixed.
    
    Change-Id: I5c265470c1b73cb755918c5a000208da3d1e7b22

--- src/gromacs/gmxpreprocess/genhydro.c
+++ src/gromacs/gmxpreprocess/genhydro.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -813,7 +813,7 @@ int protonate(t_atoms **atomsptr, rvec **xptr, t_protonate *protdata)
         }
 
         /* set forcefield to use: */
-        strcpy(protdata->FF, "gmx2.ff");
+        strcpy(protdata->FF, "oplsaa.ff");
 
         /* get the databases: */
         protdata->nah = read_h_db(protdata->FF, &protdata->ah);
