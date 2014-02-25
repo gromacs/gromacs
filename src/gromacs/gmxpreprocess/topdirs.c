ca129b8423dd0faaf128c13e8470fcf4280011d9
ngoga <n.goga@rug.nl>
2014-02-25 17:22:03 +0100

    Martini CG - Combined angle-torsion potentials
    - Potential important for Martini
    - Most Important diffence: bondfree.c
    - Rest of files: definition of types
    - Different MD groups want to have it
    - Avoid the numerical instabilities
    of having three beads in a row and
    therefore an undefined dihedral
    angle. This might not happen easily
    at the atomistic level, but for coarse-grain
    models this is a real pain.
    - Paper at
    http://md.chem.rug.nl/~monica/Bulacu_Marrink_2013.pdf
    
    Paper published.
    Change-Id: I8ed470c464d843eb0218c87aeea3a44c3a1f64f8

--- src/gromacs/gmxpreprocess/topdirs.c
+++ src/gromacs/gmxpreprocess/topdirs.c
@@ -144,6 +144,8 @@ int ifunc_index(directive d, int type)
                     return F_TABANGLES;
                 case 9:
                     return F_LINEAR_ANGLES;
+                case 10:
+                    return F_RESTRANGLES;
                 default:
                     gmx_fatal(FARGS, "Invalid angle type %d", type);
                     break;
@@ -184,6 +186,10 @@ int ifunc_index(directive d, int type)
                     return F_TABDIHS;
                 case 9:
                     return F_PDIHS; /* proper dihedrals where we allow multiple terms over single bond */
+                case 10:
+                    return F_RESTRDIHS;
+                case 11:
+                    return F_CBTDIHS;
                 default:
                     gmx_fatal(FARGS, "Invalid dihedral type %d", type);
             }
