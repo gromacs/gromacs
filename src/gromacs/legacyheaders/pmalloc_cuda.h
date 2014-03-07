b261e6d91e76e2ec4e5b8d0d1c3a6041894d5243
Roland Schulz <roland@utk.edu>
2014-03-06 19:28:39 -0500

    Fix dependencies on config.h for typedefs.h
    The include in simple.h hid other dependencies and was only
    used for gmx_inline and gmx_restrict. Defining those
    in just one place simplifies it.
    
    GMX_MPI and GMX_THREAD_MPI is moved to gmx_header_config so
    that it can be used by gmxmpi.h. gmxmpi assumed that config.h
    was included prior to it being included.
    
    Change-Id: I85f2ee893af7898bc0cd7c73ef12610b3df34be6

--- src/gromacs/legacyheaders/pmalloc_cuda.h
+++ src/gromacs/legacyheaders/pmalloc_cuda.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -35,6 +35,12 @@
 
 #ifndef PMALLOC_CUDA_H
 #define PMALLOC_CUDA_H
+
+/* TODO: HAVE_CONFIG_H dependence should be removed or made
+   a non public header */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include "types/simple.h"
 
