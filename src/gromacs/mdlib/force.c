cfc39bcf013cfa147a3c0e122d046c8759c8e322
Christian Wennberg <christian.wennberg@scilifelab.se>
2014-03-11 15:44:45 +0100

    Free-energy support for LJ-PME
    Free energy calculations is now supported together with
    LJ-PME.
    
    Change-Id: I8f30bbf565d06bb10a5b6be37a3680fbd5cef6af

--- src/gromacs/mdlib/force.c
+++ src/gromacs/mdlib/force.c
@@ -535,16 +535,17 @@ void do_force_lowlevel(FILE       *fplog,   gmx_int64_t step,
                     }
                     *dvdlt_q  = 0;
                     *dvdlt_lj = 0;
+
                     ewald_LRcorrection(fr->excl_load[t], fr->excl_load[t+1],
                                        cr, t, fr,
                                        md->chargeA,
                                        md->nChargePerturbed ? md->chargeB : NULL,
                                        md->sqrt_c6A,
-                                       md->nChargePerturbed ? md->sqrt_c6B : NULL,
+                                       md->nTypePerturbed ? md->sqrt_c6B : NULL,
                                        md->sigmaA,
-                                       md->nChargePerturbed ? md->sigmaB : NULL,
+                                       md->nTypePerturbed ? md->sigmaB : NULL,
                                        md->sigma3A,
-                                       md->nChargePerturbed ? md->sigma3B : NULL,
+                                       md->nTypePerturbed ? md->sigma3B : NULL,
                                        ir->cutoff_scheme != ecutsVERLET,
                                        excl, x, bSB ? boxs : box, mu_tot,
                                        ir->ewald_geometry,
