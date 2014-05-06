c289d8f778bcbd25dbcfbcd66115055168381914
Rossen Apostolov <rossen@kth.se>
2014-05-06 16:23:25 +0200

    Normal modes don't work currently with virtual sites or shells.
    Refs #879.
    
    Change-Id: I1c45b5a4b4c97feff222dccbbbb884e0153ad0c5

--- src/kernel/md.c
+++ src/kernel/md.c
@@ -348,6 +348,18 @@ double do_md(FILE *fplog, t_commrec *cr, int nfile, const t_filenm fnm[],
                                   (DOMAINDECOMP(cr) && !MASTER(cr))) ?
                                  NULL : state_global->x);
 
+    if (shellfc && ir->eI == eiNM)
+    {
+        /* Currently shells don't work with Normal Modes */
+        gmx_fatal(FARGS, "Normal Mode analysis is not supported with shells.\nIf you'd like to help with adding support, we have an open discussion at http://redmine.gromacs.org/issues/879\n");
+    }
+
+    if (vsite && ir->eI == eiNM)
+    {
+        /* Currently virtual sites don't work with Normal Modes */
+        gmx_fatal(FARGS, "Normal Mode analysis is not supported with virtual sites.\nIf you'd like to help with adding support, we have an open discussion at http://redmine.gromacs.org/issues/879\n");
+    }
+
     if (DEFORM(*ir))
     {
 #ifdef GMX_THREAD_MPI
