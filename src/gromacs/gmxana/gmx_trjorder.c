60ddcf9f85677885f3c23040f05064fa7e274d40
Mark Abraham <mark.j.abraham@gmail.com>
2014-05-21 19:47:52 +0200

    Move tools code to where it is used
    All the actual clients of calch.c and princ.c were respectively
    in gmxpreprocess and gmxana, so it makes more sense for them
    to be there, together with their headers. Removed some
    unneccesary #includes of those headers.
    
    Change-Id: I47f6d2f1e3a4670ebba907b874590e362f070eac

--- src/gromacs/gmxana/gmx_trjorder.c
+++ src/gromacs/gmxana/gmx_trjorder.c
@@ -52,7 +52,7 @@
 #include "index.h"
 #include "mshift.h"
 #include "gromacs/fileio/xvgr.h"
-#include "princ.h"
+
 #include "gromacs/pbcutil/rmpbc.h"
 #include "txtdump.h"
 #include "gromacs/fileio/tpxio.h"
