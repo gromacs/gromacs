3791f5ca4939ea039dc02006507dcab09590a7a2
David van der Spoel <spoel@xray.bmc.uu.se>
2014-05-21 16:38:25 +0200

    Reorganizing analysis of correlation functions.
    Moved routines computing correlation functions and
    fitting to those in a special subdirectory not
    dependent on gmxana. The autocorrelation now functions
    as it should (previously there were issues with long
    time correlations).
    
    Replaced Levenberg-Marquardt algorithm by another implementation
    from http://apps.jcns.fz-juelich.de/doku/sc/lmfit,
    under a FreeBSD license. This code is in src/external/lmfit.
    
    Added Doxygen comments to all headers.
    
    Fixed wordcount algorithm in xvgr.c
    
    The routines many_auto_correl and many_cross_correl
    are optimized using OpenMP.
    
    Change-Id: Ifbe003437db36e35d606cd942bb38deeef86ea64
    TODO: Implement effective use of the parallel function.

---
+++ src/gromacs/correlationfunctions/tests/correlationDataSet.h
@@ -0,0 +1,111 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2014, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Declares helper class for autocorrelation tests
+ *
+ * \author Anders G&auml;rden&auml;s <anders.gardenas@gmail.com>
+ * \ingroup module_correlationfunctions
+ */
+#ifndef GMX_CORRELATIONDATASET_H
+#define GMX_CORRELATIONDATASET_H
+
+#include <vector>
+#include <string>
+#include "gromacs/utility/real.h"
+
+class CorrelationDataSet
+{
+    std::vector<real> values;
+
+    int               nrLines;
+    real              startTime;
+    real              endTime;
+    real              dt;
+    int               nrColums;
+
+    public:
+
+/*! \brief
+ * Constructor
+ * \param[in] fileName containing function to test. *.xvg
+ * \param[in] dim the dimension of the .xvg file, the nummber of colums to read
+ */
+        CorrelationDataSet(std::string fileName, int dim);
+
+/*! \brief
+ * Return a value at an index
+ * \param[in] x the index of the value
+ */
+        real getValue(int x);
+
+
+/*! \brief
+ * Return the nummber of colums
+ */
+        int getNrColums();
+
+/*! \brief
+ * Return the nummber of Lines
+ */
+        int getNrLines();
+
+/*! \brief
+ * Return the time witch the function starts at
+ */
+        real getStartTime();
+
+/*! \brief
+ * Return the time the function ends at
+ */
+        real getEndTime();
+
+/*! \brief
+ * return delta time
+ */
+        real getDt();
+
+/*! \brief
+ * Destructor
+ */
+        ~CorrelationDataSet()
+        {
+
+        }
+
+
+};
+
+#endif
