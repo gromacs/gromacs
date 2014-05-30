78273382d66a7a216bb0647d7bf198bc36c7e7b8
Roland Schulz <roland@utk.edu>
2014-05-30 01:02:23 -0400

    Used IWYU to partially clean up some includes
    - Used trunk version
    - Works well for C and simple C++ files
      - Does not yet work without manual reverting some changes for more complicated
         files e.g. options.cpp (see issues 105, 111, 135, and 138)
    - IWYU doesn't check for conditional compiled code. I ran for
      pme.c and wallcycle.c with -DDEBUG_PME/_WCYCLE
    
    Given that one needs to fix a few issues and have to check for #ifdefs it is
    still manual work, but I think quite a bit faster and more complete than doing
    it manually.
    
    Change-Id: I7c5569693a4b84f481f0f7afd85f0f01c33295cf

--- src/gromacs/legacyheaders/types/group.h
+++ src/gromacs/legacyheaders/types/group.h
@@ -37,7 +37,9 @@
 #ifndef GMX_LEGACYHEADERS_TYPES_GROUP_H
 #define GMX_LEGACYHEADERS_TYPES_GROUP_H
 
-#include "gromacs/legacyheaders/types/simple.h"
+#include "gromacs/math/vectypes.h"
+#include "gromacs/utility/basedefinitions.h"
+#include "gromacs/utility/real.h"
 
 #ifdef __cplusplus
 extern "C" {
