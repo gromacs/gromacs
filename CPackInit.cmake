9e9b4823a49f79d9300f5480f29d0c1fca967fe9
Mark Abraham <mark.j.abraham@gmail.com>
2014-06-05 19:20:56 +0200

    Convert install guide to markdown
    Adds `make install-guide` to make both a plain-text INSTALL for the
    tarball, and a monolithic HTML install guide for uploading to the
    website. Intended for processing with pandoc, but I don't think
    there's any pandoc-specific extensions used. Naturally, if the build
    system doesn't have pandoc, then this make target is not available,
    just like the other documentation features.
    
    This now does some variable interpolation to keep accuracy up without
    manual maintenance, so setting some such variables gets changed a bit.
    
    Minor textual changes to the install guide. This is overdue for a
    review, anyway!
    
    Change-Id: Iff8e165759fbf8cc5199c8447e912403b8a79a17

--- CPackInit.cmake
+++ CPackInit.cmake
@@ -36,11 +36,13 @@
 if(CPACK_SOURCE_PACKAGE_FILE_NAME) #building source package
     get_filename_component(CMAKE_BINARY_DIR ${CPACK_OUTPUT_CONFIG_FILE} PATH)
     if (NOT EXISTS "${CMAKE_BINARY_DIR}/share/man/man1/gmx-view.1" OR
+        NOT EXISTS "${CMAKE_BINARY_DIR}/INSTALL" OR
         NOT EXISTS "${CMAKE_BINARY_DIR}/share/html/final/online.html")
         message(FATAL_ERROR
             "To create a complete source package all man and HTML pages need "
-            "to be generated. "
+            "to be generated, and the INSTALL file generated. "
             "You need to run 'make man html' or set GMX_BUILD_HELP=ON to get "
-            "them automatically built together with the binaries.")
+            "man and HTML automatically built together with the binaries. "
+            "'make install-guide' is needed for the latter.")
     endif()
 endif()
