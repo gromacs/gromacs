ac57b327fd8d5b11c4442d41da522cc2914bec6f
Magnus Lundborg <lundborg.magnus@gmail.com>
2014-06-16 17:15:08 +0200

    Bumped the TNG library to the latest version.
    This is commit 5f37b8a83c0fcbd5887f42dbbf003ad986df352a
    in the TNG repository.
    
    Change-Id: I1f5a9edbaf1588b6d1cee6cf86a7ea254aad23a1

--- src/external/tng_io/src/tests/tng_io_read_pos_util.c
+++ src/external/tng_io/src/tests/tng_io_read_pos_util.c
@@ -2,8 +2,6 @@
  *
  *  The high-level API of the TNG API is used where appropriate.
  *
- *                      VERSION 1.0
- *
  * Written by Magnus Lundborg
  * Copyright (c) 2012-2013, The GROMACS development team.
  * Check out http://www.gromacs.org for more information.
