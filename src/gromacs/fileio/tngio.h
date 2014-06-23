5832eeb0f11802da87a6e9c41ff475672e9479da
Magnus Lundborg <lundborg.magnus@gmail.com>
2014-06-23 12:07:23 +0200

    Find installed TNG version.
    Make it possible to use external TNG or compile the version
    shipped with GROMACS (default). Moved TNG handling into
    src/gromacs/CMakeLists.txt in anticipation of future
    developments.
    
    Bumped the version of the TNG library. Corresponds to commit
    8f47720452a3bade2aa0a76ed2576d12f7533f7f in the TNG repository.
    
    Change-Id: I7ac9b578e45b8e75c67c9df9440eed968a3a9371

--- src/gromacs/fileio/tngio.h
+++ src/gromacs/fileio/tngio.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -37,7 +37,7 @@
 #define GMX_FILEIO_TNGIO_H
 
 #include "gromacs/legacyheaders/typedefs.h"
-#include "../../external/tng_io/include/tng_io_fwd.h"
+#include "tng/tng_io_fwd.h"
 
 #ifdef __cplusplus
 extern "C" {
