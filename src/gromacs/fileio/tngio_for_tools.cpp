89d9dcdee5e252b9ddc00c15145a20a3cb948d45
Magnus Lundborg <lundborg.magnus@gmail.com>
2014-06-23 12:07:23 +0200

    Update TNG build system
    GROMACS can now find and use a copy of TNG installed on the system.
    The default is still to compile the version shipped with GROMACS.
    That compilation is now handled using the BuildTNG.cmake script, so
    there is no duplication between the GROMACS and TNG CMake code that
    builds TNG, and the result is fully portable with all supported
    environments and CMake versions.
    
    Moved TNG handling into src/gromacs/CMakeLists.txt where it is
    actually used.
    
    Bumped the version of the TNG library to 1.6. Corresponds to commit
    ba9aea42b01 in the TNG repository.
    
    Renamed md5 things with GROMACS prefixes, so that there is no internal
    name clash with the md5 things in TNG, and no client of libgromacs can
    get unexpected behaviour.
    
    Updated GMX_USE_TNG=off to work, and silenced unused-parameter
    warnings associated with that setting.
    
    Refs #1453 (partial fix)
    Fixes #1520
    
    Change-Id: I7ac9b578e45b8e75c67c9df9440eed968a3a9371

--- src/gromacs/fileio/tngio_for_tools.cpp
+++ src/gromacs/fileio/tngio_for_tools.cpp
@@ -44,7 +44,7 @@
 #include "trx.h"
 
 #ifdef GMX_USE_TNG
-#include "../../external/tng_io/include/tng_io.h"
+#include "tng/tng_io.h"
 #endif
 
 #include "gromacs/legacyheaders/types/atoms.h"
@@ -171,6 +171,9 @@ void gmx_prepare_tng_writing(const char              *filename,
     GMX_UNUSED_VALUE(input);
     GMX_UNUSED_VALUE(output);
     GMX_UNUSED_VALUE(nAtoms);
+    GMX_UNUSED_VALUE(mtop);
+    GMX_UNUSED_VALUE(index);
+    GMX_UNUSED_VALUE(indexGroupName);
 #endif
 }
 
@@ -201,6 +204,7 @@ void gmx_write_tng_from_trxframe(tng_trajectory_t        output,
 #else
     GMX_UNUSED_VALUE(output);
     GMX_UNUSED_VALUE(frame);
+    GMX_UNUSED_VALUE(natoms);
 #endif
 }
 
@@ -427,7 +431,7 @@ gmx_bool gmx_read_next_tng_frame(tng_trajectory_t            input,
     void                   *values        = NULL;
     double                  frameTime     = -1.0;
     int                     size, blockDependency;
-    float                   prec;
+    double                  prec;
     const int               defaultNumIds = 5;
     static gmx_int64_t      fallbackRequestedIds[defaultNumIds] =
     {
@@ -617,6 +621,7 @@ gmx_bool gmx_read_next_tng_frame(tng_trajectory_t            input,
     GMX_UNUSED_VALUE(input);
     GMX_UNUSED_VALUE(fr);
     GMX_UNUSED_VALUE(requestedIds);
+    GMX_UNUSED_VALUE(numRequestedIds);
     return FALSE;
 #endif
 }
@@ -778,7 +783,7 @@ gmx_bool gmx_get_tng_data_next_frame_of_block_type(tng_trajectory_t     input,
     gmx_int64_t         codecId;
     int                 blockDependency;
     void               *data = 0;
-    float               localPrec;
+    double              localPrec;
 
     stat = tng_data_block_name_get(input, blockId, name, maxLen);
     if (stat != TNG_SUCCESS)
