059540ac7302b450ad49d526efff98964bd62e43
Teemu Murtola <teemu.murtola@gmail.com>
2014-06-26 14:59:07 +0300

    [WIP] Convert gmx rdf to the C++ framework
    Convert the RDF calculation tool to the new framework.  Main changed
    beyond mechanical conversion:
     - Remove -com and -rdf options in favor of using selections directly.
       Both functionality, and a lot more, is now directly supported with
       proper selections.
     - Remove -hq and -fade options, as both are just post-processing that
       can be trivially done based on the output xvg file alone.
     - Normalization is reworked to work with dynamic selections.
     - Add an -rmax parameter that allows specifying an upper cutoff for the
       RDF.  Combined with the use of the neighborhood search routines, this
       potentially makes the tool a lot faster if the RDF is not required up
       to half the box length.
    
    Supporting changes:
     - Add support for cumulative histograms in AnalysisData.
    
    WIP because of:
     - Haven't tested this at all yet, beyond compiling.
     - Some cleanup would be nice, as well as addressing the TODOs.
     - Manual needs to be checked (it has some stuff on RDFs).
     - Unit tests are missing for the tool and for other changed code.
    
    Change-Id: Ieed19fc39a84e299291232190333652ced26abee

--- src/gromacs/analysisdata/modules/histogram.cpp
+++ src/gromacs/analysisdata/modules/histogram.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012,2013, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013,2014, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -338,7 +338,7 @@ AbstractAverageHistogram::normalizeProbability()
 {
     for (int c = 0; c < columnCount(); ++c)
     {
-        real sum = 0;
+        double sum = 0;
         for (int i = 0; i < rowCount(); ++i)
         {
             sum += value(i, c).value();
@@ -346,6 +346,20 @@ AbstractAverageHistogram::normalizeProbability()
         if (sum > 0.0)
         {
             scaleSingle(c, 1.0 / (sum * xstep()));
+        }
+    }
+}
+
+void
+AbstractAverageHistogram::makeCumulative()
+{
+    for (int c = 0; c < columnCount(); ++c)
+    {
+        double sum = 0;
+        for (int i = 0; i < rowCount(); ++i)
+        {
+            sum += value(i, c).value();
+            value(i, c).setValue(sum);
         }
     }
 }
