062a7d6c67daa2b7c3a172b5e27d1c9d0b86e68e
Mark Abraham <mark.j.abraham@gmail.com>
2014-07-05 14:24:28 +0200

    Fix tarball and install, particularly for INSTALL
    Getting the INSTALL file generated from the markdown, into the tarball
    and not otherwise installed is a bit tricky. I did this manually for
    5.0, but this automates things. I don't know why CPack needs
    declarations of intent to install in two places, nor whether we really
    need the "final" subdirectory, but it is consistent with how we
    generate the other online HTML.
    
    Added more CPack-time checks that the tarball will be complete.
    
    Prevented installation of .gitattributes.
    
    Change-Id: I77c645faf7b292a085165bdd1598741dae6972a5

--- CPackInit.cmake
+++ CPackInit.cmake
@@ -36,8 +36,10 @@
 if(CPACK_SOURCE_PACKAGE_FILE_NAME) #building source package
     get_filename_component(CMAKE_BINARY_DIR ${CPACK_OUTPUT_CONFIG_FILE} PATH)
     if (NOT EXISTS "${CMAKE_BINARY_DIR}/share/man/man1/gmx-view.1" OR
-        NOT EXISTS "${CMAKE_BINARY_DIR}/INSTALL" OR
-        NOT EXISTS "${CMAKE_BINARY_DIR}/share/html/final/online.html")
+        NOT EXISTS "${CMAKE_BINARY_DIR}/install-guide/final/INSTALL" OR
+        NOT EXISTS "${CMAKE_BINARY_DIR}/share/html/final/online.html" OR
+        NOT EXSITS "${CMAKE_BINARY_DIR}/src/programs/completion/gmx-completion.bash" OR
+        NOT EXISTS "${CMAKE_BINARY_DIR}/share/html/final/programs/gmx-view.html")
         message(FATAL_ERROR
             "To create a complete source package all man and HTML pages need "
             "to be generated, and the INSTALL file generated. "
