ea3e8aa8bfdd6865973d09ab852a9c64a6a1d180
Erik Lindahl <erik@kth.se>
2014-07-07 13:43:58 +0200

    Make IEEE754 check more robust
    This change should prevent compilers from
    optimizing away the structure used for checking
    strings in the binary by also using it to produce
    a return values. There are now also clearer
    warnings, and if IEEE754 cannot be detected we
    fall by to using the OS versions of erf/erfc()
    in double precision. Fixes compiles on Power7.
    
    Change-Id: I8ed834d5b87baf33e03725d2ab93a60a76c34a12

--- cmake/TestFloatFormat.c
+++ cmake/TestFloatFormat.c
@@ -3,8 +3,10 @@ main()
 {
   /* Check that a double is 8 bytes - compilation dies if it isnt */
   extern char xyz [sizeof(double) == 8 ? 1 : -1];
+  int i;
+  double d;
 
-  double abc [] = {
+  double abc [10] = {
     /* Zero-terminated strings encoded as floating-point numbers */
     /* "GROMACSX" in ascii    */
     (double)  3.80279098314984902657e+35 , (double) 0.0,
@@ -18,5 +20,13 @@ main()
     (double) -5.22995989424860458374e+10 , (double) 0.0,
   };
 
-  return 0;
+  /* Make sure some compilers do not optimize away the entire structure
+   * with floating-point data by using it to produce a return value.
+   */
+  for(i=0,d=0;i<10;i++)
+  {
+    d+=abc[i];
+  }
+
+  return (d==12345.0);
 }
