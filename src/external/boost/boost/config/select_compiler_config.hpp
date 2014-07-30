8b7f2d16750c2c433c4aaebeaf4123a1fccc10f4
Erik Lindahl <erik@kth.se>
2014-07-30 14:34:32 +0200

    Improve Gromacs portability
    This patch addresses a few general portability issues
    that we have experienced at least on Fujitsu compilers
    (K computer) and (partly) for Pathscale x86 compilers.
    - I have added a generic compiler definition fallback
      for boost, so it will work on unidentified compilers.
    - There is now  a define for gmock-1.7.0 to handle
      the case where wcscasecmp() is not available. We use
      this by default since it is not important for us.
    - If we do not find atomics for sparc64 we now warn and
      fall back on mutexes rather than the compile dying.
    - The compiler flag checks have been updated from
      cmake-3.0, and more warning checks added.
    - Since some non-GCC compilers set _GNUC_ for
      compatibilty, we do not assume all such compilers
      support tr1, but use the internal tr1/tuple definition
      in Gmock by default.
    - Before using the sched.h affinity functions, we now
      check that all the functions we need are supported,
      rather than just one of them.
    
    Change-Id: I31b7f0f46a9d03a627c73a0789b4a9b24fa860ee

--- src/external/boost/boost/config/select_compiler_config.hpp
+++ src/external/boost/boost/config/select_compiler_config.hpp
@@ -106,9 +106,10 @@
 //  example) also #define _MSC_VER
 #   define BOOST_COMPILER_CONFIG "boost/config/compiler/visualc.hpp"
 
-#elif defined (BOOST_ASSERT_CONFIG)
-// this must come last - generate an error if we don't
-// recognise the compiler:
-#  error "Unknown compiler - please configure (http://www.boost.org/libs/config/config.htm#configuring) and report the results to the main boost mailing list (http://www.boost.org/more/mailing_lists.htm#main)"
+#else
+
+#   warning Unknown boost compiler; using generic settings not tested for this compiler.
+#   define BOOST_COMPILER_CONFIG "boost/config/compiler/generic.hpp"
+
 
 #endif
