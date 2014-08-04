40034dc1a40ac2bba65fee8c5fee48b7db4ae3d1
Berk Hess <hess@kth.se>
2014-08-04 17:51:03 +0200

    Improve DLB+PME tuning with GPUs
    With GPUs and the DD DLB can quickly limit the PME load balancing
    room too much. In such cases (and only with DLB=auto) we now first
    do PME load balancing without DLB and then, if DLB gets turned on,
    a second round of PME load balancing.
    
    Also fixed that when DLB limited the tuning, the fastest choice was
    reset, which would often lead to stronger limitations.
    
    Change-Id: I0087e6b8512d5574d8d0fa2db82e6e38279a82f1

--- src/gromacs/domdec/domdec.cpp
+++ src/gromacs/domdec/domdec.cpp
@@ -301,6 +301,8 @@ typedef struct gmx_domdec_comm
     int      eDLB;
     /* Is eDLB=edlbAUTO locked such that we currently can't turn it on? */
     gmx_bool bDLB_locked;
+    /* With eDLB=edlbAUTO, should we check if to DLB on at the next DD? */
+    gmx_bool bCheckWhetherToTurnDlbOn;
     /* Are we actually using DLB? */
     gmx_bool bDynLoadBal;
 
@@ -6703,8 +6705,9 @@ gmx_domdec_t *init_domain_decomposition(FILE *fplog, t_commrec *cr,
     /* Initialize to GPU share count to 0, might change later */
     comm->nrank_gpu_shared = 0;
 
-    comm->eDLB        = check_dlb_support(fplog, cr, dlb_opt, comm->bRecordLoad, Flags, ir);
-    comm->bDLB_locked = FALSE;
+    comm->eDLB                     = check_dlb_support(fplog, cr, dlb_opt, comm->bRecordLoad, Flags, ir);
+    comm->bDLB_locked              = FALSE;
+    comm->bCheckWhetherToTurnDlbOn = TRUE;
 
     comm->bDynLoadBal = (comm->eDLB == edlbYES);
     if (fplog)
@@ -7589,7 +7592,7 @@ gmx_bool change_dd_cutoff(t_commrec *cr, t_state *state, t_inputrec *ir,
     return bCutoffAllowed;
 }
 
-void change_dd_dlb_cutoff_limit(t_commrec *cr)
+void set_dd_dlb_max_cutoff(t_commrec *cr, real cutoff)
 {
     gmx_domdec_comm_t *comm;
 
@@ -7599,7 +7602,18 @@ void change_dd_dlb_cutoff_limit(t_commrec *cr)
     comm->bPMELoadBalDLBLimits = TRUE;
 
     /* Change the cut-off limit */
-    comm->PMELoadBal_max_cutoff = comm->cutoff;
+    comm->PMELoadBal_max_cutoff = cutoff;
+
+    if (debug)
+    {
+        fprintf(debug, "PME load balancing set a limit to the DLB staggering such that a %f cut-off will continue to fit\n",
+                comm->PMELoadBal_max_cutoff);
+    }
+}
+
+gmx_bool dd_dlb_is_on(const gmx_domdec_t *dd)
+{
+    return dd->comm->bDynLoadBal;
 }
 
 gmx_bool dd_dlb_is_locked(const gmx_domdec_t *dd)
@@ -7612,8 +7626,41 @@ void dd_dlb_set_lock(gmx_domdec_t *dd, gmx_bool bValue)
     /* We can only lock the DLB when it is set to auto, otherwise don't lock */
     if (dd->comm->eDLB == edlbAUTO)
     {
-        dd->comm->bDLB_locked = bValue;
+        dd->comm->bDLB_locked              = bValue;
+        dd->comm->bCheckWhetherToTurnDlbOn = TRUE;
     }
+}
+
+/* Returns if we should collect the load imbalance data, so we can check
+ * if to trigger dynamic load balancing. This function should not be called
+ * multiple times per step, since it might change comm->bCheckWhetherToTurnDlbOn
+ * which is used to avoid collecting the timing data at every repartitioning.
+ */
+static gmx_bool get_bCheckWhetherToTurnDlbOn(gmx_domdec_t *dd)
+{
+    gmx_domdec_comm_t *comm = dd->comm;
+    gmx_bool           bCheckWhetherToTurnDlbOn;
+
+    if (comm->eDLB == edlbAUTO && !comm->bDynLoadBal && !dd_dlb_is_locked(dd))
+    {
+        /* Check if we should use DLB at the second partitioning
+         * (or after locking) and every 100 partitionings,
+         * so the extra communication cost is negligible.
+         */
+        const int nddp_chk_dlb = 100;
+
+        bCheckWhetherToTurnDlbOn
+            = (comm->bCheckWhetherToTurnDlbOn ||
+               comm->n_load_have % nddp_chk_dlb == nddp_chk_dlb - 1);
+
+        comm->bCheckWhetherToTurnDlbOn = FALSE;
+    }
+    else
+    {
+        bCheckWhetherToTurnDlbOn = FALSE;
+    }
+
+    return bCheckWhetherToTurnDlbOn;
 }
 
 static void merge_cg_buffers(int ncell,
@@ -9323,7 +9370,7 @@ void dd_partition_system(FILE                *fplog,
     gmx_int64_t        step_pcoupl;
     rvec               cell_ns_x0, cell_ns_x1;
     int                i, n, ncgindex_set, ncg_home_old = -1, ncg_moved, nat_f_novirsum;
-    gmx_bool           bBoxChanged, bNStGlobalComm, bDoDLB, bCheckDLB, bTurnOnDLB, bLogLoad;
+    gmx_bool           bBoxChanged, bNStGlobalComm, bDoDLB, bCheckWhetherToTurnDlbOn, bTurnOnDLB, bLogLoad;
     gmx_bool           bRedist, bSortCG, bResortAll;
     ivec               ncells_old = {0, 0, 0}, ncells_new = {0, 0, 0}, np;
     real               grid_density;
@@ -9384,20 +9431,7 @@ void dd_partition_system(FILE                *fplog,
     /* Check if we have recorded loads on the nodes */
     if (comm->bRecordLoad && dd_load_count(comm) > 0)
     {
-        if (comm->eDLB == edlbAUTO && !comm->bDynLoadBal && !dd_dlb_is_locked(dd))
-        {
-            /* Check if we should use DLB at the second partitioning
-             * and every 100 partitionings,
-             * so the extra communication cost is negligible.
-             */
-            const int nddp_chk_dlb = 100;
-            bCheckDLB = (comm->n_load_collect == 0 ||
-                         comm->n_load_have % nddp_chk_dlb == nddp_chk_dlb - 1);
-        }
-        else
-        {
-            bCheckDLB = FALSE;
-        }
+        bCheckWhetherToTurnDlbOn = get_bCheckWhetherToTurnDlbOn(dd);
 
         /* Print load every nstlog, first and last step to the log file */
         bLogLoad = ((ir->nstlog > 0 && step % ir->nstlog == 0) ||
@@ -9408,7 +9442,7 @@ void dd_partition_system(FILE                *fplog,
         /* Avoid extra communication due to verbose screen output
          * when nstglobalcomm is set.
          */
-        if (bDoDLB || bLogLoad || bCheckDLB ||
+        if (bDoDLB || bLogLoad || bCheckWhetherToTurnDlbOn ||
             (bVerbose && (ir->nstlist == 0 || nstglobalcomm <= ir->nstlist)))
         {
             get_load_distribution(dd, wcycle);
@@ -9425,7 +9459,7 @@ void dd_partition_system(FILE                *fplog,
             }
             comm->n_load_collect++;
 
-            if (bCheckDLB)
+            if (bCheckWhetherToTurnDlbOn)
             {
                 /* Since the timings are node dependent, the master decides */
                 if (DDMASTER(dd))
