6ac96cb7a674ce21cbee5a03fcb4df610b43f6cc
Berk Hess <hess@kth.se>
2014-08-05 12:07:36 +0200

    enabled 1 PP + 1 PME node
    Change-Id: I18a4c2bac71f1b5b81d9d374b212bfb9edc7a1e8

--- src/gromacs/domdec/domdec_setup.cpp
+++ src/gromacs/domdec/domdec_setup.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2008,2009,2010,2011,2012,2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2008,2009,2010,2011,2012,2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -721,11 +721,6 @@ real dd_choose_grid(FILE *fplog,
         {
             if (cr->npmenodes > 0)
             {
-                if (cr->nnodes <= 2)
-                {
-                    gmx_fatal(FARGS,
-                              "Cannot have separate PME ranks with 2 or fewer ranks");
-                }
                 if (cr->npmenodes >= cr->nnodes)
                 {
                     gmx_fatal(FARGS,
