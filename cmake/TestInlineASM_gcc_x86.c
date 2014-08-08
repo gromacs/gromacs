7f16b6e6e6ec4940abc41a78e1311db382e5a928
Erik Lindahl <erik@kth.se>
2014-08-08 23:37:49 +0200

    Replace __LP64__ with check for pointer size
    The __LP64__ define is not set e.g. on PGI compilers,
    so instead we use a portable way of checking for
    pointer size based on defines from stdint.h.
    
    Change-Id: Ib2dd6e45c2b168add07f5478d59aec487a91ab9e

--- cmake/TestInlineASM_gcc_x86.c
+++ cmake/TestInlineASM_gcc_x86.c
@@ -5,17 +5,17 @@ main()
   unsigned int level = 0;
 
   /* Test gcc inline asm for x86 */
-#if defined (__LP64__) || defined (_M_X64)
-    __asm__("push %%rbx       \n\t"
-            "cpuid            \n\t"
-            "movl %%ebx, %1   \n\t"
-            "pop %%rbx        \n\t"
-            : "=a"(_eax), "=r"(_ebx), "=c"(_ecx), "=d"(_edx) : "0"(level));
-#else
+#if (UINTPTR_MAX==UINT32_MAX)
     __asm__("push %%ebx       \n\t"
             "cpuid            \n\t"
             "movl %%ebx, %1   \n\t"
             "pop %%ebx        \n\t"
+            : "=a"(_eax), "=r"(_ebx), "=c"(_ecx), "=d"(_edx) : "0"(level));
+#else
+    __asm__("push %%rbx       \n\t"
+            "cpuid            \n\t"
+            "movl %%ebx, %1   \n\t"
+            "pop %%rbx        \n\t"
             : "=a"(_eax), "=r"(_ebx), "=c"(_ecx), "=d"(_edx) : "0"(level));
 #endif
 
