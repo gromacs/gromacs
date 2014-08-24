9e6763e16767e3ca5bee7acc6f469fbb557045b3
Roland Schulz <roland@utk.edu>
2014-08-24 18:08:27 -0400

    Make nbnxn generator match generated files
    Change-Id: Ibc603dd0c85291f0c8fede394d9fa35b39c3a0a4

--- src/gromacs/mdlib/nbnxn_kernels/nbnxn_kernel_file_generator/make_verlet_simd_kernel_files.py
+++ src/gromacs/mdlib/nbnxn_kernels/nbnxn_kernel_file_generator/make_verlet_simd_kernel_files.py
@@ -163,6 +163,7 @@ for type in VerletKernelTypeDict:
     KernelNamePrefix = 'nbnxn_kernel'
     KernelsName = "{0}_simd_{1}".format(KernelNamePrefix,type)
     KernelsHeaderFileName = "{0}.h".format(KernelsName,type)
+    KernelsHeaderPathName = "gromacs/mdlib/nbnxn_kernels/simd_{0}/{1}".format(type,KernelsHeaderFileName)
     KernelFunctionLookupTable = {}
     KernelDeclarations = ''
     KernelTemplate = read_kernel_template("{0}_kernel.c.pre".format(KernelsName))
@@ -189,7 +190,7 @@ for type in VerletKernelTypeDict:
                                            ElectrostaticsDict[elec]['define'],
                                            VdwTreatmentDict[ljtreat]['define'],
                                            EnergiesComputationDict[ener]['define'],
-                                           KernelsHeaderFileName,
+                                           KernelsHeaderPathName,
                                            KernelName,
                                            " " * (len(KernelName) + 1),
                                            VerletKernelTypeDict[type]['UnrollSize'],
@@ -205,7 +206,7 @@ for type in VerletKernelTypeDict:
 
     # Write the header file that declares all the kernel
     # functions for this type
-    with open('{0}/{1}'.format(DirName,KernelsHeaderFileName),'w') as fp:
+    with open('../../../../{0}'.format(KernelsHeaderFileName),'w') as fp:
         fp.write(FileHeader.format(type))
         fp.write(KernelsHeaderTemplate
                  .format(KernelsName,
