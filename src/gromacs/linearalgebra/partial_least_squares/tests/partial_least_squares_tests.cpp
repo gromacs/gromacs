27dd39ea65ff532225b9dbfcc960cec184c53c54
Jan Henning Peters <jpeters@gwdg.de>
2014-09-02 16:01:58 +0200

    Added Partial Least Squares Method
    as recommended in the change adding the  Functional Mode Analysis
    (FMA) trajectory analysis module
    (https://gerrit.gromacs.org/#/c/3277/15), this is an independent
    change containing PLS. The linear algebra functions required
    for it's implementation are taken from the patch
    Linear algebra on IrregArray2D and std::vector.
    
    Implementation of PLS according to:
    
    Denham, M. C. 1995. “Implementing Partial Least Squares.”
    Statistics and Computing 5 (3): 191–202. doi:10.1007/BF00142661.
    
    Change-Id: I0207bf6932078d948f185fd875ab37c3642af3a5

---
+++ src/gromacs/linearalgebra/partial_least_squares/tests/partial_least_squares_tests.cpp
@@ -0,0 +1,81 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+   \brief
+   Tests for partial least squares function
+
+   \author Berenger Bramas <berenger.bramas@mpcdf.mpg.de>
+ */
+#include "gmxpre.h"
+
+#include <gtest/gtest.h>
+#include <vector>
+
+#include "gromacs/linearalgebra/partial_least_squares/partial_least_squares.h"
+
+namespace gmx
+{
+
+namespace partial_least_squares_tests
+{
+
+namespace
+{
+
+TEST(PartialLeastSquares, SimpleTest){
+    const size_t            n = 10;
+    const int               k = 10;
+    std::vector<real>       y(n, 1);
+    gmx::IrregArray2D<real> X(int(0), n-1, int(0), k-1);
+    gmx::IrregArray2D<real> W(int(0), n-1, int(0), k-1);
+    std::vector<real>       q(n);
+
+    for (int idx = 0; idx < n; ++idx)
+    {
+        X(idx, idx) = idx;
+    }
+
+    const int               retValue = pls_denham(X, y, n, k, &W, &q);
+    EXPECT_EQ(retValue, 0);
+}
+
+TEST(PartialLeastSquares, ParametersFailureTest){
+}
+
+} // namespace
+
+} // namespace partial_least_squares_tests
+
+} // namespace gmx
