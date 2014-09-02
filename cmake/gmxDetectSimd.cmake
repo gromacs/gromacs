58e113fcbcd87208d7734370f19d3d2154fd8379
Erik Lindahl <erik@kth.se>
2014-09-02 08:36:34 +0200

    Fixed string var expansion in CMake due to CMP0053
    CMake policy 0053 means they are deprecating the usage of
    @VAR@ in CMakefiles, so CMake-3.0.2 has started to
    issue big warnings about this. Replace them with ${VAR}.
    
    Change-Id: I4e63a4eb038edbc27019bfb89c7106e97aa194c2

--- cmake/gmxDetectSimd.cmake
+++ cmake/gmxDetectSimd.cmake
@@ -62,7 +62,7 @@ function(gmx_suggest_x86_simd _suggested_simd)
     message(STATUS "Detecting best SIMD instructions for this CPU")
 
     # Get CPU SIMD properties information
-    set(_compile_definitions "@GCC_INLINE_ASM_DEFINE@ -I${CMAKE_SOURCE_DIR}/src/gromacs/legacyheaders -DGMX_CPUID_STANDALONE")
+    set(_compile_definitions "${GCC_INLINE_ASM_DEFINE} -I${CMAKE_SOURCE_DIR}/src/gromacs/legacyheaders -DGMX_CPUID_STANDALONE")
     if(GMX_TARGET_X86)
         set(_compile_definitions "${_compile_definitions} -DGMX_TARGET_X86")
     endif()
@@ -84,10 +84,10 @@ function(gmx_suggest_x86_simd _suggested_simd)
         set(OUTPUT_TMP "None")
     endif(NOT GMX_CPUID_COMPILED)
 
-    string(STRIP "@OUTPUT_TMP@" OUTPUT_SIMD)
+    string(STRIP "${OUTPUT_TMP}" OUTPUT_SIMD)
 
-    set(${_suggested_simd} "@OUTPUT_SIMD@" PARENT_SCOPE)
-    message(STATUS "Detected best SIMD instructions for this CPU - @OUTPUT_SIMD@")
+    set(${_suggested_simd} "${OUTPUT_SIMD}" PARENT_SCOPE)
+    message(STATUS "Detected best SIMD instructions for this CPU - ${OUTPUT_SIMD}")
 endfunction()
 
 function(gmx_detect_simd _suggested_simd)
