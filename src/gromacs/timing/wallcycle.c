874629207f105997034e55c70aacceea9a971e9f
Berk Hess <hess@kth.se>
2014-09-11 15:02:14 +0200

    Added GPU local wait to load balancing
    The GPU local wait time was not included in the load imbalance timing.
    This could lead to incorrect load imbalance reporting and dynamic
    load balancing, especially with CUDA stream priorities.
    Note that we can't accurately measure the GPU time, when it finishes
    during dd_move_f.
    
    Change-Id: If4bb0f867593450cc025c8593672b0569397b1d6

--- src/gromacs/timing/wallcycle.c
+++ src/gromacs/timing/wallcycle.c
@@ -98,7 +98,7 @@ static const char *wcn[ewcNR] =
     "DD comm. bounds", "Vsite constr.", "Send X to PME", "Neighbor search", "Launch GPU ops.",
     "Comm. coord.", "Born radii", "Force", "Wait + Comm. F", "PME mesh",
     "PME redist. X/F", "PME spread/gather", "PME 3D-FFT", "PME 3D-FFT Comm.", "PME solve LJ", "PME solve Elec",
-    "PME wait for PP", "Wait + Recv. PME F", "Wait GPU nonlocal", "Wait GPU local", "NB X/F buffer ops.",
+    "PME wait for PP", "Wait + Recv. PME F", "Wait GPU nonlocal", "Wait GPU local", "Wait GPU loc. est.", "NB X/F buffer ops.",
     "Vsite spread", "COM pull force",
     "Write traj.", "Update", "Constraints", "Comm. energies",
     "Enforced rotation", "Add rot. forces", "Coordinate swapping", "IMD", "Test"
@@ -386,6 +386,12 @@ void wallcycle_sum(t_commrec *cr, gmx_wallcycle_t wc)
     snew(wc->cycles_sum, ewcNR+ewcsNR);
 
     wcc = wc->wcc;
+
+    /* The GPU wait estimate counter is used for load balancing only
+     * and will mess up the total due to double counting: clear it.
+     */
+    wcc[ewcWAIT_GPU_NB_L_EST].n = 0;
+    wcc[ewcWAIT_GPU_NB_L_EST].c = 0;
 
     for (i = 0; i < ewcNR; i++)
     {
