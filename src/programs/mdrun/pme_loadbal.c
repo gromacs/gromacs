70a8656083fa7a22fbc33b5911f6b5c1b9f192ad
Berk Hess <hess@kth.se>
2014-09-26 20:47:47 +0200

    Avoid PME tuning decreasing rcoulomb
    With mdrun input with the grid much finer than fourier-spacing,
    PME tuning could reduce rcoulomb below the starting value.
    Fixes #1606.
    
    Change-Id: I3ae7fe38a4240251aadb96336199b306fc7dce40

--- src/programs/mdrun/pme_loadbal.c
+++ src/programs/mdrun/pme_loadbal.c
@@ -261,6 +261,15 @@ static gmx_bool pme_loadbal_increase_cutoff(pme_load_balancing_t  pme_lb,
     while (sp <= 1.001*pme_lb->setup[pme_lb->cur].spacing || !grid_ok);
 
     set->rcut_coulomb = pme_lb->cut_spacing*sp;
+    if (set->rcut_coulomb < pme_lb->rcut_coulomb_start)
+    {
+        /* This is unlikely, but can happen when e.g. continuing from
+         * a checkpoint after equilibration where the box shrank a lot.
+         * We want to avoid rcoulomb getting smaller than rvdw
+         * and there might be more issues with decreasing rcoulomb.
+         */
+        set->rcut_coulomb = pme_lb->rcut_coulomb_start;
+    }
 
     if (pme_lb->cutoff_scheme == ecutsVERLET)
     {
