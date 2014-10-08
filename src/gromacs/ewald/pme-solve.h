683d161bda8f6483f9b80ceb3f93d73d8e17dcf5
Mark Abraham <mark.j.abraham@gmail.com>
2014-10-08 19:38:59 +0200

    Split pme.c into multiple files
    Hopefully, this will guide the way for some more internal structure,
    so that more structs can become opaque, etc.
    
    Made pme_spline_work_t into opaque struct pme_spline_work. Changed
    usage of gmx_pme_t to struct gmx_pme_t, per policy.
    
    Renamed parameters of reuse_pmegrids to avoid "new" being interpreted
    by uncrustify as a C++ symbol (also needed for subsequent C++ use).
    
    Noted a TODO for possible future consolidation of serial and threaded
    implementations.
    
    Change-Id: I833b8d3e1ec746435de44ba93518e13eb3d5c76b

---
+++ src/gromacs/ewald/pme-solve.h
@@ -0,0 +1,67 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2014,2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#ifndef GMX_EWALD_PME_SOLVE_H
+#define GMX_EWALD_PME_SOLVE_H
+
+#include "gromacs/math/gmxcomplex.h"
+#include "gromacs/utility/basedefinitions.h"
+#include "gromacs/utility/real.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct gmx_pme_t;
+
+void get_pme_ener_vir_q(const struct gmx_pme_t *pme, int nthread,
+                        real *mesh_energy, matrix vir);
+
+void get_pme_ener_vir_lj(const struct gmx_pme_t *pme, int nthread,
+                         real *mesh_energy, matrix vir);
+
+int solve_pme_yzx(struct gmx_pme_t *pme, t_complex *grid,
+                  real ewaldcoeff, real vol,
+                  gmx_bool bEnerVir,
+                  int nthread, int thread);
+
+int solve_pme_lj_yzx(struct gmx_pme_t *pme, t_complex **grid, gmx_bool bLB,
+                     real ewaldcoeff, real vol,
+                     gmx_bool bEnerVir, int nthread, int thread);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
