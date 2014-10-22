2b05efec8e1989b62d5c4a8f3e7b2f14db97d54d
Roland Schulz <roland@utk.edu>
2014-10-21 21:23:29 -0400

    Don't set CFLAGS for Phi in toolchain
    The recommended way to set those flags is with CMAKE_*_FLAGS_INIT but
    that is only passible in a platform file because those set in a toolchain
    file get overwritten.
    
    This fixes that extra CFLAGS passed on the command line don't get ignored.
    Also move setting of fftw out of platform file because those should
    be project independent.
    
    Change-Id: I5f4d70efd8369bdef5f5060959e3568026d6820c

--- cmake/Platform/Linux-Intel-C-XeonPhi.cmake
+++ cmake/Platform/Linux-Intel-C-XeonPhi.cmake
@@ -31,9 +31,5 @@
 #
 # To help us fund GROMACS development, we humbly ask that you cite
 # the research papers on the package. Check out http://www.gromacs.org.
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR XeonPhi)
-set(CMAKE_C_COMPILER "icc")
-set(CMAKE_CXX_COMPILER "icpc")
-set(MPI_C_COMPILER "mpiicc")
-set(MPI_CXX_COMPILER "mpiicpc")
+set(CMAKE_C_FLAGS_INIT "-mmic")
+set(CMAKE_SYSTEM_PREFIX_PATH  "${_CMAKE_INSTALL_DIR}" "${CMAKE_INSTALL_PREFIX}") #remove /usr and /usr/local
