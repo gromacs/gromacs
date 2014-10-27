ca8160f46ff7bb825e5834cb32520d4632eb6ec7
Justin Lemkul <jalemkul@vt.edu>
2014-10-27 13:55:27 -0400

    Comment cleanup.
    Change-Id: I8cd8d7133b7394398a1dc4e2fd268d0027da6f9f

--- src/gromacs/gmxpreprocess/gen_maxwell_velocities.c
+++ src/gromacs/gmxpreprocess/gen_maxwell_velocities.c
@@ -87,12 +87,9 @@ static void low_mspeed(real tempi,
                 for (m = 0; (m < DIM); m++)
                 {
                     v[i][m] = sd*gmx_rng_gaussian_real(rng);
-                    /* TODO: does this belong here? */
-                    /* Drude T is not an absolute T and should not affect system T */
-                    /* ekin   += 0.5*mass*v[i][m]*v[i][m]; */
+                    /* Drudes do not contribute to Ekin or nrdf for the purpose of
+                     * the calculations done here */
                 }
-                /* TODO: likewise, does this belong here? */
-                /* nrdf += DIM; */
             }
         }
     }
