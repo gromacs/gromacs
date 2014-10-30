e47a4d59d83ecb8cec1826181059f70a2e37a8b6
Berk Hess <hess@kth.se>
2014-10-30 12:05:48 +0100

    DD halo communication is now direct
    The DD halo communication for the Verlet cut-off scheme has been
    changed from pulses along the three grid directions to direct
    communication with the (up to) 7 neighbors in the eighth shell.
    The new halo code is all in domdec_halo.cpp.
    The old pulsed code in domdec.c is stil used with the group cut-off
    scheme and should be removed when the group scheme is removed.
    
    Moved the common data used by multiple domdec source files to
    domdec_internal.h with documentation.
    
    Communicate instead of looking up cginfo in DD redist (also helps
    the group scheme) and in the new halo communication.
    
    Decreased the default minimum DLB margin, mdrun option -dlb,
    from 0.8 to 0.9 to avoid skipping convenient DD grids.
    
    The only disadvantage of (the current implemenation of) direct
    communication is that when only communicating with direct neighbors,
    the DD cells can not be smaller than rlist. This decreases the maximum
    number of MPI ranks that can be used for parallelization. But one can
    usually always increase parallelization by adding more OpenMP threads.
    The optimal performance is in most cases achieved at cell sizes
    larger than rlist.
    
    Advantages:
    * Faster halo communication, both shmem and over network.
    * Part of the non-local gridding is removed.
    * Much simpler halo communication code.
    * The shape of non-local grid cells is now similar to local ones,
      which provides more uniform list buffering (but adds some extra
      zero interactions compared to the previous setup).
    * The GPU local kernel time estimation has been removed, as the wait
      for the non-blocking force communication is now after the GPU wait.
    * Forces the user to use OpenMP at high parallelization; the error
      message for too many MPI ranks now suggests to use OpenMP.
    
    Preparation for:
    * No PBC support for the Verlet cut-off scheme.
    * More task parallelization.
    * RDMA communication.
    * Fast Multipole Method Electrostatics.
    
    Change-Id: Ia382479bc3491fce40b19f19a698375cbc395d48

--- src/programs/mdrun/runner.h
+++ src/programs/mdrun/runner.h
@@ -71,6 +71,7 @@ namespace gmx
  * \param[in] ddxyz    Division of sub-boxes over processors for
  *                     use in domain decomposition parallellization
  * \param[in] dd_node_order
+ * \param[in] npme     The number of separate PME ranks requested
  * \param[in] rdd      The maximum distance for bonded interactions with DD (nm)
  * \param[in] rconstr  Maximum distance for P-LINCS (nm)
  * \param[in] dddlb_opt File name for debugging
@@ -96,7 +97,8 @@ namespace gmx
 int mdrunner(gmx_hw_opt_t *hw_opt,
              FILE *fplog, struct t_commrec *cr, int nfile,
              const t_filenm fnm[], const output_env_t oenv, gmx_bool bVerbose,
-             gmx_bool bCompact, int nstglobalcomm, ivec ddxyz, int dd_node_order,
+             gmx_bool bCompact, int nstglobalcomm, ivec ddxyz,
+             int dd_node_order, int npme,
              real rdd, real rconstr, const char *dddlb_opt, real dlb_scale,
              const char *ddcsx, const char *ddcsy, const char *ddcsz,
              const char *nbpu_opt, int nstlist_cmdline,
