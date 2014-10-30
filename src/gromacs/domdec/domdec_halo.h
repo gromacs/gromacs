7ffb618fcbdd2f60238506ebe8e20ad6cd36bc03
Berk Hess <hess@kth.se>
2014-10-30 12:05:48 +0100

    DD halo communication is now direct
    The DD halo communication for the Verlet cut-off scheme has been
    changed from pulses along the three grid directions to direct
    communication with the (up to) 7 neighbors in the eighth shell.
    The new halo code is all in domdec_halo.cpp.
    The old pulsed code in domdec.c is stil used with the group cut-off
    scheme and should be removed when the group scheme is removed.
    
    Moved the common data used by multiple domdec source files to
    domdec_internal.h with documentation.
    
    Communicate instead of looking up cginfo in DD redist (also helps
    the group scheme) and in the new halo communication.
    
    Decreased the default minimum DLB margin, mdrun option -dlb,
    from 0.8 to 0.9 to avoid skipping convenient DD grids.
    
    The only disadvantage of (the current implemenation of) direct
    communication is that when only communicating with direct neighbors,
    the DD cells can not be smaller than rlist. This decreases the maximum
    number of MPI ranks that can be used for parallelization. But one can
    usually always increase parallelization by adding more OpenMP threads.
    The optimal performance is in most cases achieved at cell sizes
    larger than rlist.
    
    Advantages:
    * Faster halo communication, both shmem and over network.
    * Part of the non-local gridding is removed.
    * Much simpler halo communication code.
    * The shape of non-local grid cells is now similar to local ones,
      which provides more uniform list buffering (but adds some extra
      zero interactions compared to the previous setup).
    * The GPU local kernel time estimation has been removed, as the wait
      for the non-blocking force communication is now after the GPU wait.
    * Forces the user to use OpenMP at high parallelization; the error
      message for too many MPI ranks now suggests to use OpenMP.
    
    Preparation for:
    * No PBC support for the Verlet cut-off scheme.
    * More task parallelization.
    * RDMA communication.
    * Fast Multipole Method Electrostatics.
    
    Change-Id: Ia382479bc3491fce40b19f19a698375cbc395d48

---
+++ src/gromacs/domdec/domdec_halo.h
@@ -0,0 +1,189 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2014,2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \libinternal \file
+ *  \brief
+ * Halo communication for the force calculation.
+ *
+ * This file contains functions to set up the halo communication
+ * for the eighth shell domain decomposition and to execute
+ * the halo communication.
+ * Only nearest neighbor communication is supported, which means that
+ * each rank/domain sends and receives from 1, 3 and 7 ranks with 1D, 2D
+ * and 3D decomposition respectively. All communication is non-blocking.
+ *
+ * \inlibraryapi
+ * \author Berk Hess <hess@kth.se>
+ * \ingroup module_domdec
+ */
+
+#ifndef GMX_DOMDEC_DOMDEC_HALO_H
+#define GMX_DOMDEC_DOMDEC_HALO_H
+
+#include "gromacs/legacyheaders/types/commrec.h"
+#include "gromacs/legacyheaders/types/forcerec.h"
+
+/*! \brief Initiate a non-blocking halo coordinate receive.
+ *
+ * Iniatiate the non-blocking receive of the halo coordinates \p x.
+ * Should be called as early as possible for fast communication.
+ *
+ * \param[in]  dd Pointer to the domain decomposition setup.
+ * \param[out] x  Pointer to the start of the coordinate array.
+ */
+void dd_halo_initiate_recv_x(gmx_domdec_t *dd, rvec *x);
+
+/*! \brief Initiate a non-blocking halo coordinate send.
+ *
+ * Initiate the non-blocking send of the halo coordinates \p x.
+ * Should be called as early as possible when \p x is ready.
+ *
+ * \param[in]  dd  Pointer to the domain decomposition setup.
+ * \param[in]  box Unit-cell matrix.
+ * \param[in]  x   Pointer to the start of the coordinate array.
+ */
+void dd_halo_initiate_send_x(gmx_domdec_t *dd, matrix box, rvec x[]);
+
+/*! \brief Ensures that the coordinates have been received.
+ *
+ * Needs to be called after dd_halo_initiate_recv_x.
+ * Call as late as possible.
+ *
+ * \param[in]  dd Pointer to the domain decomposition setup.
+ */
+void dd_halo_complete_recv_x(gmx_domdec_t *dd);
+
+/*! \brief Ensures that the coordinates have been received.
+ *
+ * Needs to be called after dd_halo_initiate_send_x.
+ * Call as late as possible.
+ *
+ * \param[in]  dd Pointer to the domain decomposition setup.
+ */
+void dd_halo_complete_send_x(gmx_domdec_t *dd);
+
+/*! \brief Send and receive the halo coordinates.
+ *
+ * Internally calls:
+ * \p dd_halo_initiate_recv_x
+ * \p dd_halo_initiate_send_x
+ * \p dd_halo_complete_recv_x
+ * \p dd_halo_complete_send_x
+ * Only call this for infrequent use. For frequent use calls the four
+ * functions separately and put computation between the initiate
+ * and complete calls to overlap calculation and communication.
+ *
+ * \param[in]     dd  Pointer to the domain decomposition setup.
+ * \param[in]     box Unit-cell matrix.
+ * \param[in,out] x   Pointer to the start of the coordinate array.
+ */
+void dd_halo_move_x(gmx_domdec_t *dd, matrix box, rvec *x);
+
+/*! \brief Initiate a non-blocking halo force receive.
+ *
+ * Iniatiate the non-blocking receive of the halo forces.
+ * Should be called as early as possible for fast communication.
+ *
+ * \param[in]  dd Pointer to the domain decomposition setup.
+ */
+void dd_halo_initiate_recv_f(gmx_domdec_t *dd);
+
+/*! \brief Initiate a non-blocking halo force send.
+ *
+ * Initiate the non-blocking send of the halo forces \p f
+ * Should be called as early as possible when \p f is ready.
+ *
+ * \param[in]  dd  Pointer to the domain decomposition setup.
+ * \param[in]  f   Pointer to the start of the force array.
+ */
+void dd_halo_initiate_send_f(gmx_domdec_t *dd, rvec *f);
+
+/*! \brief Ensures that the forces have been received and reduces the forces.
+ *
+ * If \p fshift!=NULL, also updates the shift forces \p fshift.
+ * Needs to be called after dd_halo_initiate_recv_f.
+ * Call as late as possible.
+ *
+ * \param[in]     dd     Pointer to the domain decomposition setup.
+ * \param[in,out] f      Pointer to the start of the force array.
+ * \param[in,out] fshift Pointer to the start of the shift force array.
+ */
+void dd_halo_complete_recv_f(gmx_domdec_t *dd, rvec *f, rvec *fshift);
+
+/*! \brief Completes the force send.
+ *
+ * Needs to be called after dd_halo_initiate_send_f.
+ * Call as late as possible.
+ *
+ * \param[in]  dd Pointer to the domain decomposition setup.
+ */
+void dd_halo_complete_send_f(gmx_domdec_t *dd);
+
+/*! \brief Communicates and reduces the halo forces.
+ *
+ * If \p fshift!=NULL, also updates the shift forces.
+ * Internally calls:
+ * \p dd_halo_initiate_recv_f
+ * \p dd_halo_initiate_send_f
+ * \p dd_halo_complete_recv_f
+ * \p dd_halo_complete_send_f
+ * Only call this for infrequent use. For frequent use calls the four
+ * functions separately and put computation between the initiate
+ * and complete calls to overlap calculation and communication.
+ *
+ * \param[in]     dd     Pointer to the domain decomposition setup.
+ * \param[in,out] f      Pointer to the start of the force array.
+ * \param[in,out] fshift Pointer to the start of the shift force array.
+ */
+void dd_halo_move_f(gmx_domdec_t *dd, rvec *f, rvec *fshift);
+
+/*! \brief Set up the eighth-shell halo coordinate/force communcation
+ *
+ * Set up the eighth-shell halo communcation for non-bonded + bonded
+ * interactions
+ * \p bCellsChanged indicates if the domain decomposition cells changed,
+ * either due to dynamic load balancing or pressure scaling.
+ *
+ * \param[in,out] dd     Pointer to the domain decomposition setup.
+ * \param[in]     box    Unit-cell matrix.
+ * \param[in]     ddbox  Domain decomposition unit-cell and PBC data.
+ * \param[in]     fr     Pointr to the force record struct.
+ * \param[in]     bCellsChanged Tells if the domain decomposition cells changed.
+ */
+void setup_halo_communication(gmx_domdec_t *dd,
+                              const matrix box, const gmx_ddbox_t *ddbox,
+                              t_forcerec *fr,
+                              gmx_bool bCellsChanged);
+
+#endif
