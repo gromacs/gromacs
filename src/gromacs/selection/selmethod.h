35b614965754ca4e39ee05e080d4ad1d20c9d5c2
Teemu Murtola <teemu.murtola@gmail.com>
2014-10-31 20:34:04 +0200

    Refactor position-evaluated selection keyword handling
    Now the keywords themselves only return one value per position, not one
    value per atom.  The expansion to atoms is instead done by the caller in
    evaluate.cpp.  This makes it easier to reuse the methods for
    implementing syntax like "z of ...", where the expansion is not
    appropriate.
    
    Also, add an additional unit test for code around this path.
    
    Change-Id: Ida74244fe1c76eaf89c3d2e1ca9b6f6784b0476d

--- src/gromacs/selection/selmethod.h
+++ src/gromacs/selection/selmethod.h
@@ -578,13 +578,14 @@ typedef void  (*sel_updatefunc)(t_topology *top, t_trxframe *fr, t_pbc *pbc,
  * \param      data Internal data structure from sel_datafunc().
  * \returns    0 on success, a non-zero error code on error.
  *
- * This function should evaluate the method for each position in \p g,
+ * This function should evaluate the method for each position in \p pos,
  * and write the output values to \p out. The pointer in the union \p out->u
  * that corresponds to the type of the method should be used.
  * Enough memory has been allocated to store the output values.
  * The number of values in \p out should also be updated if necessary.
- * However, \ref POS_VALUE or \ref GROUP_VALUE methods should not touch
- * \p out->nr (it should be 1 anyways).
+ * \ref POS_VALUE or \ref GROUP_VALUE methods should not touch
+ * \p out->nr (it should be 1 anyways).  For other types of methods, the number
+ * of output values should equal the number of positions in \p pos.
  *
  * For \ref STR_VALUE methods, the pointers stored in \p out->s are discarded
  * without freeing; it is the responsibility of this function to provide
