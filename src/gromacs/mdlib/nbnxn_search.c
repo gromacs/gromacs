7ab772bb4e111616b278bb344addbbac66acc82f
Berk Hess <hess@kth.se>
2014-11-03 17:25:51 +0100

    Improved the intra-GPU load balancing
    The splitting of the pair list to improve load balancing on the GPU
    was based on the number of generated lists. But this number can be
    high(er) due to small lists before splitting. This lead to too few
    lists for small systems and too many for large systems.
    Now the splitting is based on the number of pairs in the list up till
    now. This produces much more stable results.
    
    Change-Id: Id2210171a409ef1a27f7dc919fe806f0fe4d869c

--- src/gromacs/mdlib/nbnxn_search.c
+++ src/gromacs/mdlib/nbnxn_search.c
@@ -3941,10 +3941,11 @@ static void close_ci_entry_simple(nbnxn_pairlist_t *nbl)
  * both on nthread and our own thread index.
  */
 static void split_sci_entry(nbnxn_pairlist_t *nbl,
-                            int nsp_max_av, gmx_bool progBal, int nc_bal,
+                            int nsp_target_av,
+                            gmx_bool progBal, int nsp_tot_est,
                             int thread, int nthread)
 {
-    int nsci_est;
+    int nsp_est;
     int nsp_max;
     int cj4_start, cj4_end, j4len, cj4;
     int sci;
@@ -3956,17 +3957,20 @@ static void split_sci_entry(nbnxn_pairlist_t *nbl,
         /* Estimate the total numbers of ci's of the nblist combined
          * over all threads using the target number of ci's.
          */
-        nsci_est = nc_bal*thread/nthread + nbl->nsci;
+        nsp_est = (nsp_tot_est*thread)/nthread + nbl->nci_tot;
 
         /* The first ci blocks should be larger, to avoid overhead.
          * The last ci blocks should be smaller, to improve load balancing.
+         * The factor 3/2 makes the first block 3/2 times the target average
+         * and ensures that the total number of blocks end up equal to
+         * the that with blocks of equal nsp_target_av size.
          */
         nsp_max = max(1,
-                      nsp_max_av*nc_bal*3/(2*(nsci_est - 1 + nc_bal)));
+                      nsp_target_av*nsp_tot_est*3/(2*(nsp_est + nsp_tot_est)));
     }
     else
     {
-        nsp_max = nsp_max_av;
+        nsp_max = nsp_target_av;
     }
 
     cj4_start = nbl->sci[nbl->nsci-1].cj4_ind_start;
@@ -4033,7 +4037,7 @@ static void split_sci_entry(nbnxn_pairlist_t *nbl,
 /* Clost this super/sub list i entry */
 static void close_ci_entry_supersub(nbnxn_pairlist_t *nbl,
                                     int nsp_max_av,
-                                    gmx_bool progBal, int nc_bal,
+                                    gmx_bool progBal, int nsp_tot_est,
                                     int thread, int nthread)
 {
     int j4len, tlen;
@@ -4056,7 +4060,8 @@ static void close_ci_entry_supersub(nbnxn_pairlist_t *nbl,
         if (nsp_max_av > 0)
         {
             /* Measure the size of the new entry and potentially split it */
-            split_sci_entry(nbl, nsp_max_av, progBal, nc_bal, thread, nthread);
+            split_sci_entry(nbl, nsp_max_av, progBal, nsp_tot_est,
+                            thread, nthread);
         }
     }
 }
@@ -4328,10 +4333,12 @@ static real nonlocal_vol2(const gmx_domdec_zones_t *zones, rvec ls, real r)
 }
 
 /* Estimates the average size of a full j-list for super/sub setup */
-static int get_nsubpair_max(const nbnxn_search_t nbs,
-                            int                  iloc,
-                            real                 rlist,
-                            int                  min_ci_balanced)
+static void get_nsubpair_target(const nbnxn_search_t  nbs,
+                                int                   iloc,
+                                real                  rlist,
+                                int                   min_ci_balanced,
+                                int                  *nsubpair_target,
+                                int                  *nsubpair_tot_est)
 {
     const nbnxn_grid_t *grid;
     rvec                ls;
@@ -4391,27 +4398,32 @@ static int get_nsubpair_max(const nbnxn_search_t nbs,
     if (min_ci_balanced <= 0 || grid->nc >= min_ci_balanced || grid->nc == 0)
     {
         /* We don't need to worry */
-        nsubpair_max = -1;
+        *nsubpair_target = -1;
     }
     else
     {
-        /* Thus the (average) maximum j-list size should be as follows */
-        nsubpair_max = max(1, (int)(nsp_est/min_ci_balanced+0.5));
+        /* Thus the (average) maximum j-list size should be as follows.
+         * Since there is overhead, we shouldn't make the lists too small
+         * and we can't chop up j-groups, so we set a full jgroup of pairs
+         * as the minimum target size.
+         */
+        *nsubpair_target = max(GPU_NSUBCELL*NBNXN_GPU_JGROUP_SIZE,
+                               (int)(nsp_est/min_ci_balanced+0.5));
+
+        if (debug)
+        {
+            fprintf(debug, "nbl nsp estimate %.1f, nsubpair_target %d\n",
+                    nsp_est, *nsubpair_target);
+        }
 
         /* Since the target value is a maximum (this avoids high outliers,
          * which lead to load imbalance), not average, we add half the
          * number of pairs in a cj4 block to get the average about right.
          */
-        nsubpair_max += GPU_NSUBCELL*NBNXN_GPU_JGROUP_SIZE/2;
+        *nsubpair_target += GPU_NSUBCELL*NBNXN_GPU_JGROUP_SIZE/2;
     }
 
-    if (debug)
-    {
-        fprintf(debug, "nbl nsp estimate %.1f, nsubpair_max %d\n",
-                nsp_est, nsubpair_max);
-    }
-
-    return nsubpair_max;
+    *nsubpair_tot_est = nsp_est;
 }
 
 /* Debug list print function */
@@ -4814,7 +4826,7 @@ static void nbnxn_make_pairlist_part(const nbnxn_search_t nbs,
                                      gmx_bool bFBufferFlag,
                                      int nsubpair_max,
                                      gmx_bool progBal,
-                                     int min_ci_balanced,
+                                     int nsubpair_tot_est,
                                      int th, int nth,
                                      nbnxn_pairlist_t *nbl,
                                      t_nblist *nbl_fep)
@@ -5408,7 +5420,7 @@ static void nbnxn_make_pairlist_part(const nbnxn_search_t nbs,
                     {
                         close_ci_entry_supersub(nbl,
                                                 nsubpair_max,
-                                                progBal, min_ci_balanced,
+                                                progBal, nsubpair_tot_est,
                                                 th, nth);
                     }
                 }
@@ -5600,7 +5612,7 @@ void nbnxn_make_pairlist(const nbnxn_search_t  nbs,
     nbnxn_grid_t      *gridi, *gridj;
     gmx_bool           bGPUCPU;
     int                nzi, zi, zj0, zj1, zj;
-    int                nsubpair_max;
+    int                nsubpair_target, nsubpair_tot_est;
     int                th;
     int                nnbl;
     nbnxn_pairlist_t **nbl;
@@ -5670,11 +5682,13 @@ void nbnxn_make_pairlist(const nbnxn_search_t  nbs,
 
     if (!nbl_list->bSimple && min_ci_balanced > 0)
     {
-        nsubpair_max = get_nsubpair_max(nbs, iloc, rlist, min_ci_balanced);
+        get_nsubpair_target(nbs, iloc, rlist, min_ci_balanced,
+                            &nsubpair_target, &nsubpair_tot_est);
     }
     else
     {
-        nsubpair_max = 0;
+        nsubpair_target  = 0;
+        nsubpair_tot_est = 0;
     }
 
     /* Clear all pair-lists */
@@ -5751,8 +5765,8 @@ void nbnxn_make_pairlist(const nbnxn_search_t  nbs,
                                          nb_kernel_type,
                                          ci_block,
                                          nbat->bUseBufferFlags,
-                                         nsubpair_max,
-                                         progBal, min_ci_balanced,
+                                         nsubpair_target,
+                                         progBal, nsubpair_tot_est,
                                          th, nnbl,
                                          nbl[th],
                                          nbl_list->nbl_fep[th]);
