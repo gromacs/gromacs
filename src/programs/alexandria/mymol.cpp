97d422583d524b32e66664910367f210f2e7c1a1
David van der Spoel <spoel@xray.bmc.uu.se>
2014-11-07 20:20:02 +0100

    updated
    Change-Id: I6b8f264207b3ac7d01246b9cd5bb60ea2c8eadf9

--- src/programs/alexandria/mymol.cpp
+++ src/programs/alexandria/mymol.cpp
@@ -1,22 +1,3 @@
-/*
- * This source file is part of the Aleandria project.
- *
- * Copyright (C) 2014 David van der Spoel and Paul J. van Maaren
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
 /*! \internal \brief
  * Implements part of the alexandria program.
  * \author David van der Spoel <david.vanderspoel@icm.uu.se>
@@ -1484,9 +1465,9 @@ static void write_top2(FILE *out, char *molname,
     }
 }
 
-void MyMol::PrintTopology(const char *fn, 
-                          ChargeGenerationModel iModel, 
-                          bool bVerbose)
+void MyMol::PrintTopology(const char           *fn,
+                          ChargeGenerationModel iModel,
+                          bool                  bVerbose)
 {
     FILE   *fp;
     t_mols  printmol;
