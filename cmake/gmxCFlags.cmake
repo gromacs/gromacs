f13ff92626b6de947d68f3f0fe325eeb84057e13
Mark Abraham <mark.j.abraham@gmail.com>
2014-11-18 16:32:52 +0000

    Fix static-linking issues with icc on Cray
    CMake is hard-coded to use -rdynamic for
    CMAKE_SHARED_LIBRARY_LINK_{C,CXX}_FLAGS, which can be avoided
    (http://www.cmake.org/Bug/view.php?id=9985), and is necessary to avoid
    because it is used in Source/cmLocalGenerator.cxx by
    OutputLinkLibraries(), even when there are no libraries to link. I
    have proposed a change at
    http://public.kitware.com/pipermail/cmake/2014-November/059154.html,
    but even if accepted, we'll have to work around these issues until we
    require CMake at least > 3.1.
    
    However, the work-around of setting those variables in the cache to be
    empty (which GMX_LINK_STATIC_BINARIES does) does not take effect early
    enough to work in the CMake tests for whether the compiler is
    functional. Those work OK for gcc, but fail completely in the case of
    icc when the Cray wrapper compiler gets confused by the -rdynamic.
    
    The only mechanism that can act early enough to trigger the use of a
    modified platform file is a toolchain file, which will make CMake
    think we are cross-compiling, so the try_run() use for CPU detection
    cannot work.
    
    This patch implements a toolchain for the "Cray-static" platform, and
    adapts a suitably renamed copy of the standard Linux-Intel.cmake
    platform file (lifted from CMake 3.0.2), and modifies it to avoid the
    use of -rdynamic (only).
    
    Updated the install guide to direct the use of the new toolchain
    file in that case.
    
    Suppressed warnings about AVX compiler flags helpfully added by
    Cray when compiling with icc.
    
    Refs #1641
    
    Change-Id: Id5e9f6594bb481617e58d1ed4e79fdc692689ece

--- cmake/gmxCFlags.cmake
+++ cmake/gmxCFlags.cmake
@@ -176,9 +176,11 @@ MACRO(gmx_c_flags)
 #2557: comparison between signed and unsigned operands
 #3280: declaration hides member ".."
 #3346: dynamic exception specifications are deprecated
+#10120: overriding '-xCORE-AVX-I' with '-mavx' (apparently Cray wrapper compilers like trying to be helpful here)
+#10121: overriding '-xCORE-AVX-I' with '-march=core-avx2' (same)
 #11074: Inlining inhibited by limit max-size(/max-total-size)
 #11076: To get full report use -opt-report=3 -opt-report-phase ipo (shown for previous remark)
-            GMX_TEST_CFLAG(CFLAGS_WARN "-w3 -wd111 -wd177 -wd181 -wd193 -wd271 -wd304 -wd383 -wd424 -wd444 -wd522 -wd593 -wd869 -wd981 -wd1418 -wd1419 -wd1572 -wd1599 -wd2259 -wd2415 -wd2547 -wd2557 -wd3280 -wd3346 -wd11074 -wd11076" GMXC_CFLAGS)
+            GMX_TEST_CFLAG(CFLAGS_WARN "-w3 -wd111 -wd177 -wd181 -wd193 -wd271 -wd304 -wd383 -wd424 -wd444 -wd522 -wd593 -wd869 -wd981 -wd1418 -wd1419 -wd1572 -wd1599 -wd2259 -wd2415 -wd2547 -wd2557 -wd3280 -wd3346 -wd10120 -wd10121 -wd11074 -wd11076" GMXC_CFLAGS)
             GMX_TEST_CFLAG(CFLAGS_STDGNU "-std=gnu99" GMXC_CFLAGS)
             GMX_TEST_CFLAG(CFLAGS_OPT "-ip -funroll-all-loops -alias-const -ansi-alias" GMXC_CFLAGS_RELEASE)
         else()
@@ -206,7 +208,7 @@ MACRO(gmx_c_flags)
 #All but the following warnings are identical for the C-compiler (see above)
 #1782: #pragma once is obsolete
 #2282: unrecognized GCC pragma
-            GMX_TEST_CXXFLAG(CXXFLAGS_WARN "-w3 -wd111 -wd177 -wd181 -wd193 -wd271 -wd304 -wd383 -wd424 -wd444 -wd522 -wd593 -wd869 -wd981 -wd1418 -wd1419 -wd1572 -wd1599 -wd2259 -wd2415 -wd2547 -wd2557 -wd3280 -wd3346 -wd11074 -wd11076 -wd1782 -wd2282" GMXC_CXXFLAGS)
+            GMX_TEST_CXXFLAG(CXXFLAGS_WARN "-w3 -wd111 -wd177 -wd181 -wd193 -wd271 -wd304 -wd383 -wd424 -wd444 -wd522 -wd593 -wd869 -wd981 -wd1418 -wd1419 -wd1572 -wd1599 -wd2259 -wd2415 -wd2547 -wd2557 -wd3280 -wd3346 -wd10120 -wd10121 -wd11074 -wd11076 -wd1782 -wd2282" GMXC_CXXFLAGS)
             GMX_TEST_CXXFLAG(CXXFLAGS_OPT "-ip -funroll-all-loops -alias-const -ansi-alias" GMXC_CXXFLAGS_RELEASE)
         else()
             if(NOT GMX_OPENMP)
