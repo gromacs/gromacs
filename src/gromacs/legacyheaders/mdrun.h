523f2d5b0bc2f90bf7220b113c18169474ec973c
Mark Abraham <mark.j.abraham@gmail.com>
2014-11-25 21:23:37 +0100

    Move mdrun signalling code
    No functionality changes here, I just put some related functionality
    into functions in their own source file to
    * simplify later removing inter-simulation signalling, and
    * prepare for further encapsulation later.
    
    Updated struct name and usage for new style. Updated include guards.
    Removed now-useless C++-compilation guard from header file. Added
    basic Doxygen.
    
    Change-Id: I3ea1e2f77a1cdb6608bba38f08a430cc3b8d7c9a

--- src/gromacs/legacyheaders/mdrun.h
+++ src/gromacs/legacyheaders/mdrun.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -50,7 +50,6 @@
 #include "gromacs/legacyheaders/update.h"
 #include "gromacs/legacyheaders/vcm.h"
 #include "gromacs/legacyheaders/vsite.h"
-#include "gromacs/legacyheaders/types/globsig.h"
 #include "gromacs/legacyheaders/types/membedt.h"
 #include "gromacs/timing/wallcycle.h"
 
