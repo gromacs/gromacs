637ef9fd764c89a45f2bed82025fd4ce416fa897
David van der Spoel <spoel@xray.bmc.uu.se>
2014-12-14 20:45:23 +0100

    Framework for analyzing energy files.
    Development of classes for analyzing energies from simulations.
    The code is similar to the trajectoryanalysis module.
    The different calculations are split off into separate modules called using:
    gmx viscosity
    gmx energy
    gmx fluctprops
    gmx dhdl
    
    The calculation of free energy differences based on two energy files has been
    deprecated (old option -fee).
    
    The NMR tools have been implemented in a stripped down version of the old
    gmx energy in this patch https://gerrit.gromacs.org/#/c/5179/
    
    The Options class is used rather than old-style pargs and the user interface is
    built upon ICommandLineOptionsModule.
    
    Change-Id: If968cbe8a4462805c2caf7c192f6d085a63388ff

---
+++ src/gromacs/energyanalysis/energyanalysisrunner.h
@@ -0,0 +1,129 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2014,2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \libinternal \file
+ * \brief
+ * Declares gmx::energyanalysis::EnergyHandler
+ *
+ * \author David van der Spoel <david.vanderspoel@icm.uu.se>
+ * \ingroup module_energyanalysis
+ */
+#ifndef GMX_ENERGYANALYSIS_HANDLER_H
+#define GMX_ENERGYANALYSIS_HANDLER_H
+
+#include <string>
+#include <vector>
+
+#include "gromacs/analysisdata/modules/plot.h"
+#include "gromacs/commandline/cmdlineoptionsmodule.h"
+#include "gromacs/fileio/oenv.h"
+#include "gromacs/options/timeunitmanager.h"
+
+#include "analysismodule.h"
+
+namespace gmx
+{
+
+namespace energyanalysis
+{
+
+/*! \libinternal
+ * \brief
+ * Class doing the actual reading of an energy file.
+ *
+ * Reading of the file (and hence dealing with underlying file formats)
+ * is concentrated in this class and results are passed on to the
+ * different energy analysis tools.
+ */
+class EnergyAnalysisRunner : public ICommandLineOptionsModule
+{
+    private:
+        //! The energy files
+        std::vector<std::string>                 fnEnergy_;
+        //! Start time of the analysis
+        double                                   t0_;
+        //! End time of the analysis
+        double                                   t1_;
+        //! Skipping time of the analysis
+        double                                   tDelta_;
+        //! Do we want to view the output?
+        bool                                     bView_;
+        //! Do we want verbose output?
+        bool                                     bVerbose_;
+        //! Output environment for xvg writing etcetera
+        gmx_output_env_t                        *oenv_;
+        //! Module that does all the work
+        EnergyAnalysisModulePointer              module_;
+        //! Check whether time is within range
+        int checkTime(double t);
+        //! Global plotting settings for the analysis module.
+        AnalysisDataPlotSettings                 plotSettings_;
+        //! Energy names and units stored for checking multiple files
+        std::vector<energyNameUnit>              eNU_;
+        //! To be able to change time units on the command line
+        boost::shared_ptr<TimeUnitBehavior>      timeUnitBehavior_;
+    public:
+        /*! \brief
+         * Initiate local variables and register the analysis module
+         *
+         * \param[in] module Pointer to the actual analysis to be performed
+         */
+        EnergyAnalysisRunner(EnergyAnalysisModulePointer module);
+
+        virtual void init(CommandLineModuleSettings * /*settings*/) {};
+
+        virtual void initOptions(IOptionsContainer                 *options,
+                                 ICommandLineOptionsModuleSettings *settings);
+        /*! \brief
+         * Called when the last options have been processed
+         */
+        virtual void optionsFinished() {}
+
+        /*! \brief
+         * Read the files and call the tools to analyze them.
+         *
+         * The files are read
+         * sequentially and tools have to be able to deal with this.
+         * \return 0 if everything went smoothly
+         */
+        virtual int run();
+};
+
+//! Type to make sure these pointers can not be copied
+typedef std::unique_ptr<EnergyAnalysisRunner> EnergyAnalysisRunnerPointer;
+
+}   // namespace energyanalysis
+
+}
+#endif
