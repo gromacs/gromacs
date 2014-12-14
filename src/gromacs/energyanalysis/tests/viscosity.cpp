62f743264e4d1d80f9b1de7be17fcee2276184b2
David van der Spoel <spoel@xray.bmc.uu.se>
2014-12-14 20:45:23 +0100

    Framework for analyzing energy files.
    Development of classes for analyzing energies from simulations.
    The code is similar to the trajectoryanalysis module.
    The different calculations are split off into separate modules called using:
    gmx viscosity
    gmx energy
    gmx fluctprops
    gmx dhdl
    
    Selection of energies is case insensitive and first substring matches the
    selection. One can use e.g.:
      gmx energy -f ener -term 'pot kin'
    to get the Potential and Kinetic energies. In addition numbers and strings
    can be mixed if one would be so inclined.
    
    The calculation of free energy differences based on two energy files has been
    deprecated (old option -fee).
    
    The NMR tools are implemented in a stripped down version of the old
    gmx energy that is now called gmx nmr.
    
    The Options class is used rather than old-style pargs and the user interface is
    built upon ICommandLineOptionsModule. Proper tests are implemented. Memory
    leak in inputrec.cpp was fixed (that had not been detected before).
    
    Related to #2071 as it is important to be able to analyze simulations reliably.
    Related to #2097, cleaning up energy stuff.
    
    Change-Id: If968cbe8a4462805c2caf7c192f6d085a63388ff

---
+++ src/gromacs/energyanalysis/tests/viscosity.cpp
@@ -0,0 +1,125 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2014,2015,2016,2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Tests for analysis of energy files
+ *
+ * \author David van der Spoel <david.vanderspoel@icm.uu.se>
+ */
+#include "gmxpre.h"
+
+#include "gromacs/energyanalysis/modules/viscosity.h"
+
+#include <cstdlib>
+
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "gromacs/energyanalysis/energyanalysisrunner.h"
+#include "gromacs/energyanalysis/modules/dhdl.h"
+//#include "gromacs/utility/filestream.h"
+//#include "gromacs/utility/snprintf.h"
+
+#include "testutils/cmdlinetest.h"
+#include "testutils/refdata.h"
+#include "testutils/testasserts.h"
+#include "testutils/textblockmatchers.h"
+#include "testutils/xvgtest.h"
+
+namespace gmx
+{
+
+namespace energyanalysis
+{
+
+namespace
+{
+
+using test::CommandLine;
+
+class ViscosityTest : public gmx::test::CommandLineTestBase
+{
+    public:
+        ViscosityTest()
+        {
+            setInputFile("-f", "ener.edr");
+        }
+
+        /*! \brief Run the ViscosityTest
+         *
+         * \param[in] option      The command line option
+         * \param[in] filename    The corresponding output file
+         */
+        void runTest(const char *option, const char *filename)
+        {
+            const char *const command[] = { "viscosity"  };
+            double            tolerance = 1e-4;
+            test::XvgMatch    xvg;
+            setOutputFile(option, filename,
+                          xvg.tolerance(gmx::test::relativeToleranceAsFloatingPoint(1, tolerance)));
+            CommandLine &cmdline = commandLine();
+            CommandLine  args    = CommandLine(command);
+            cmdline.merge(args);
+
+            rootChecker().checkString(args.toString(), "CommandLine");
+            ICommandLineOptionsModulePointer runner(EnergyAnalysisRunner::createModule(ViscosityInfo::create()));
+
+            ASSERT_EQ(0, gmx::test::CommandLineTestHelper::runModuleDirect(std::move(runner), &cmdline));
+
+            checkOutputFiles();
+        }
+};
+
+TEST_F(ViscosityTest, EinsteinViscosity)
+{
+    runTest("-evis", "einstein.xvg");
+}
+
+TEST_F(ViscosityTest, EinsteinViscosityIntegral)
+{
+    runTest("-eivis", "einsteinintegral.xvg");
+}
+
+TEST_F(ViscosityTest, FanourgakisViscosity)
+{
+    runTest("-pcorr", "presscorr.xvg");
+}
+
+}
+
+} // namespace energyanalysis
+
+} // namespace gmx
