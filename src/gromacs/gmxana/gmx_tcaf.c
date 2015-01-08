d199e720131f11c02eeb52172169de23d2f42581
David van der Spoel <spoel@xray.bmc.uu.se>
2015-01-08 11:16:27 +0100

    Updated exponential fitting to make it robust.
    The exponential fitting code was not robust under different
    data. This is improved by ensuring that e.g. time constants
    are always positive. For multi-exponential fits the time constants
    are guaranteed to be in increasing order.
    
    General cleanups of the code were done.
    
    TODO:
    - Add manual description
    - Allow fitting with fixed parameters again.
    
    Change-Id: Ib72fccf7f85742afeeb3fc0fd6fbd44c1c47795a

--- src/gromacs/gmxana/gmx_tcaf.c
+++ src/gromacs/gmxana/gmx_tcaf.c
@@ -208,7 +208,7 @@ static void process_tcaf(int nframes, real dt, int nkc, real **tc, rvec *kfac,
         fitparms[0]  = 1;
         fitparms[1]  = 1;
         do_lmfit(ncorr, tcaf[k], sig, dt, 0, 0, ncorr*dt,
-                 oenv, bDebugMode(), effnVAC, fitparms, 0);
+                 oenv, bDebugMode(), effnVAC, fitparms, 0, NULL);
         eta = 1000*fitparms[1]*rho/
             (4*fitparms[0]*PICO*norm2(kfac[k])/(NANO*NANO));
         fprintf(stdout, "k %6.3f  tau %6.3f  eta %8.5f 10^-3 kg/(m s)\n",
@@ -233,7 +233,7 @@ static void process_tcaf(int nframes, real dt, int nkc, real **tc, rvec *kfac,
             fitparms[0]  = 1;
             fitparms[1]  = 1;
             do_lmfit(ncorr, tcafc[k], sig, dt, 0, 0, ncorr*dt,
-                     oenv, bDebugMode(), effnVAC, fitparms, 0);
+                     oenv, bDebugMode(), effnVAC, fitparms, 0, NULL);
             eta = 1000*fitparms[1]*rho/
                 (4*fitparms[0]*PICO*norm2(kfac[kset_c[k]])/(NANO*NANO));
             fprintf(stdout,
