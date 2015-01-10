efdb496702a3c70526261f765a68d9331d6d5e63
anca <anca@streamcomputing.eu>
2015-01-10 23:41:39 +0200

    Implement OpenCL support
    StreamComputing (http://www.streamcomputing.eu) has implemented the
    short-ranged non-bonded interaction accleration features previously
    accelerated with CUDA using OpenCL 1.1. Supported devices include
    GCN-based AMD GPUs and NVIDIA GPUs.
    
    Compilation requires an OpenCL SDK installed. This is included in
    the CUDA SDK in that case.
    
    The overall project is not complete, but Gromacs runs correctly on
    supported devices. It only runs fast on AMD devices, because of a
    limitation in the Nvidia driver. A list of known TODO items can be
    found in docs/OpenCLTODOList.txt. Only devices with a warp/wavefront
    size that is a multiple of 32 are compatible with the implementation.
    
    Known issues include that tabulated Ewald kernels do not work (but the
    analytical kernels are on by default, as with CUDA), and the blocking
    behaviour of clEnqueue in Nvidia drivers means no overlap of CPU and
    GPU computation occurs.
    
    There is some duplication between the two GPU implementations, but
    the active development expected for both of them suggests it is
    not worthwhile consolidating the implementations more closely.
    
    Change-Id: Ideaf16929028eb60e785feb8298c08e917394d0f

--- src/gromacs/gmxlib/gmx_detect_hardware.cpp
+++ src/gromacs/gmxlib/gmx_detect_hardware.cpp
@@ -196,13 +196,23 @@ makeGpuUsageReport(const gmx_gpu_info_t *gpu_info,
     }
 
     {
-        std::vector<int>   gpuIdsInUse;
+#ifdef GMX_USE_OPENCL
+        std::vector<char*> gpuNamesInUse;
+        for (int i = 0; i < ngpu_use; i++)
+        {
+            gpuNamesInUse.push_back(get_ocl_gpu_device_name(gpu_info, gpu_opt, i));
+        }
+        std::string gpuIdsString =
+            formatAndJoin(gpuNamesInUse, ",", gmx::StringFormatter("%s"));
+#else
+        std::vector<int> gpuIdsInUse;
         for (int i = 0; i < ngpu_use; i++)
         {
             gpuIdsInUse.push_back(get_cuda_gpu_device_id(gpu_info, gpu_opt, i));
         }
         std::string gpuIdsString =
             formatAndJoin(gpuIdsInUse, ",", gmx::StringFormatter("%d"));
+#endif
         int         numGpusInUse = gmx_count_gpu_dev_unique(gpu_info, gpu_opt);
         bool        bPluralGpus  = numGpusInUse > 1;
 
@@ -508,7 +518,11 @@ static int gmx_count_gpu_dev_unique(const gmx_gpu_info_t *gpu_info,
      * to 1 indicates that the respective GPU was selected to be used. */
     for (i = 0; i < gpu_opt->n_dev_use; i++)
     {
+#ifdef GMX_USE_OPENCL
+        uniq_ids[i] = 1;
+#else
         uniq_ids[get_cuda_gpu_device_id(gpu_info, gpu_opt, i)] = 1;
+#endif
     }
     /* Count the devices used. */
     for (i = 0; i < ngpu; i++)
@@ -1027,6 +1041,27 @@ void gmx_print_detected_hardware(FILE *fplog, const t_commrec *cr,
     check_use_of_rdtscp_on_this_cpu(fplog, cr, hwinfo);
 }
 
+//! \brief Return if any GPU ID (e.g in a user-supplied string) is repeated
+static gmx_bool anyGpuIdIsRepeated(const gmx_gpu_opt_t *gpu_opt)
+{
+    /* Loop over IDs in the string */
+    for (int i = 0; i < gpu_opt->n_dev_use - 1; ++i)
+    {
+        /* Look for the ID in location i in the following part of the
+           string */
+        for (int j = i + 1; j < gpu_opt->n_dev_use; ++j)
+        {
+            if (gpu_opt->dev_use[i] == gpu_opt->dev_use[j])
+            {
+                /* Same ID found in locations i and j */
+                return TRUE;
+            }
+        }
+    }
+
+    return FALSE;
+}
+
 void gmx_parse_gpu_ids(gmx_gpu_opt_t *gpu_opt)
 {
     char *env;
@@ -1055,7 +1090,12 @@ void gmx_parse_gpu_ids(gmx_gpu_opt_t *gpu_opt)
         parse_digits_from_plain_string(env,
                                        &gpu_opt->n_dev_use,
                                        &gpu_opt->dev_use);
-
+#ifdef GMX_USE_OPENCL
+        if (anyGpuIdIsRepeated(gpu_opt))
+        {
+            gmx_fatal(FARGS, "The OpenCL implementation only supports using exactly one PP rank per GPU");
+        }
+#endif
         if (gpu_opt->n_dev_use == 0)
         {
             gmx_fatal(FARGS, "Empty GPU ID string encountered.\n%s\n",
@@ -1158,7 +1198,14 @@ static void set_gpu_ids(gmx_gpu_opt_t *gpu_opt, int nrank, int rank)
     {
         if (nrank % gpu_opt->n_dev_compatible == 0)
         {
+#ifdef GMX_USE_OPENCL
+            /* Our current OpenCL implementation only supports using
+             * exactly one PP rank per GPU, so sharing is
+             * impossible */
+            nshare = 1;
+#else
             nshare = nrank/gpu_opt->n_dev_compatible;
+#endif
         }
         else
         {
