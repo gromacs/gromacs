b8ef4e43763c51961008e8549e9d3714045f3ef6
Mark Abraham <mark.j.abraham@gmail.com>
2015-01-18 21:18:52 +0100

    Add checks for correct use of simd.h
    Fixed actual performance bugs for TPI, and PME NBNXN flavour selection
    on FMA hardware where both 4xn and 2xnn were supported (ie. AMD and
    Haswell).
    
    Fixed issues where simd.h was being acquired transitively. We should
    not rely on transitive #includes, because they're too easy to break
    under maintenance.
    
    Renamed tests/simd.h to make it easier to implement the checks because
    only one file has the basename 'simd.h'.  This was problematic when
    tests/simd.h had to include the real one.
    
    Noted the existence of the new checker in the developer SIMD docs.
    
    Fixes #1673
    
    Change-Id: Id1858ba97f6fe12177c0290e2f65dbbabcc41e2f

--- src/gromacs/simd/tests/simd_vector_operations.cpp
+++ src/gromacs/simd/tests/simd_vector_operations.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -39,7 +39,7 @@
 #include "gromacs/simd/simd.h"
 #include "gromacs/simd/vector_operations.h"
 
-#include "simd.h"
+#include "simd_test.h"
 
 namespace gmx
 {
