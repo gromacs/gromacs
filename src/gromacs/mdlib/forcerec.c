3df2b06e2896161e5f501950b6a065e2375559e2
Mark Abraham <mark.j.abraham@gmail.com>
2015-01-19 11:55:35 +0100

    Fix use of simd.h
    TPI correctness check was inactive.
    
    Use of SIMD preprocessor symbols in forcerec.c was OK because it got
    simd.h from nbnxn_simd.h, but also fixed the erroneous use of
    transitive inclusion of simd.h just in case that helps someone.
    
    Fixes #1673
    
    Change-Id: Iaf42a4ec420139485de3e509662ca0892fa662c5

--- src/gromacs/mdlib/forcerec.c
+++ src/gromacs/mdlib/forcerec.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -75,6 +75,7 @@
 #include "gmx_omp_nthreads.h"
 #include "gmx_detect_hardware.h"
 #include "inputrec.h"
+#include "gromacs/simd/simd.h"
 
 #include "types/nbnxn_cuda_types_ext.h"
 #include "gpu_utils.h"
