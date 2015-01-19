88d35168596ec5465d3890a49763c2ec9c825566
Mark Abraham <mark.j.abraham@gmail.com>
2015-01-19 12:20:28 +0100

    Change some listed-forces headers to be library-only
    Some analysis tools call functions declared here, so they can't be
    module-only (yet). Clients of libgromacs should only use functions
    from listed-forces.h.
    
    Moved ftype_is_bonded_potential() accordingly.
    
    This will also make it easier to re-instate SIMD support for angles
    and dihedrals, because there has to be an intra-module header that
    declares the SIMD versions of those functions, and it cannot also be
    an installed header if it depends on simd.h.
    
    Refs #1673
    
    Change-Id: Ia2815b57f712fbeb1e294dbea3231c6c9bfc32dc

--- src/gromacs/listed-forces/bonded.h
+++ src/gromacs/listed-forces/bonded.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -34,13 +34,13 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the research papers on the package. Check out http://www.gromacs.org.
  */
-/*! \file
+/*! \libinternal \file
  *
  * \brief This file contains declarations necessary for low-level
  * functions for computing energies and forces for bonded interactions.
  *
  * \author Mark Abraham <mark.j.abraham@gmail.com>
- * \inpublicapi
+ * \inlibraryapi
  * \ingroup module_listed-forces
  */
 
@@ -66,15 +66,6 @@ extern "C" {
 struct gmx_wallcycle;
 struct t_graph;
 struct t_pbc;
-
-/*! \brief Return whether this is an interaction that actually
- * calculates a potential and works on multiple atoms (not e.g. a
- * connection or a position restraint).
- *
- * \todo This function could go away when idef is not a big bucket of
- * everything. */
-gmx_bool
-ftype_is_bonded_potential(int ftype);
 
 /*! \brief Calculate bond-angle. No PBC is taken into account (use mol-shift) */
 real bond_angle(const rvec xi, const rvec xj, const rvec xk,
