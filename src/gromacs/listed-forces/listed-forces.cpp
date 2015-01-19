44cc879ce06760d1c01e26156a2916c363877327
Mark Abraham <mark.j.abraham@gmail.com>
2015-01-19 12:20:28 +0100

    Change some listed-forces headers to be library-only
    Some analysis tools call functions declared here, so they can't be
    module-only (yet). Clients of libgromacs should only use functions
    from listed-forces.h.
    
    Moved ftype_is_bonded_potential() accordingly.
    
    This will also make it easier to re-instate SIMD support for angles
    and dihedrals, because there has to be an intra-module header that
    declares the SIMD versions of those functions, and it cannot also be
    an installed header if it depends on simd.h.
    
    Refs #1673
    
    Change-Id: Ia2815b57f712fbeb1e294dbea3231c6c9bfc32dc

--- src/gromacs/listed-forces/listed-forces.cpp
+++ src/gromacs/listed-forces/listed-forces.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -342,6 +342,15 @@ calc_one_bond(int thread,
 }
 
 } // namespace
+
+gmx_bool
+ftype_is_bonded_potential(int ftype)
+{
+    return
+        (interaction_function[ftype].flags & IF_BOND) &&
+        !(ftype == F_CONNBONDS || ftype == F_POSRES || ftype == F_FBPOSRES) &&
+        (ftype < F_GB12 || ftype > F_GB14);
+}
 
 void calc_listed(const gmx_multisim_t *ms,
                  const t_idef *idef,
