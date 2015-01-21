b3ecb07ffb4d50ef37fc8db4d9bde54a80925e40
John Baxter <automata@unm.edu>
2015-01-21 16:04:31 -0700

    Changed "struct swap" to "struct t_swap" Resolves redmine.gromacs.org/issues/1674 Fixes #1674
    Change-Id: Ia3917dd2ee9d30d2d280bd00fba8953b2eb5b432

--- src/gromacs/legacyheaders/types/inputrec.h
+++ src/gromacs/legacyheaders/types/inputrec.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -283,7 +283,7 @@ typedef struct {
 } t_IMD;
 
 /* Abstract types for position swapping only defined in swapcoords.c */
-typedef struct swap *gmx_swapcoords_t;
+typedef struct t_swap *gmx_swapcoords_t;
 
 typedef struct {
     int              nstswap;           /* Every how many steps a swap is attempted?    */
