f26919dd531fa430965a04d4defaf77724ebe8c2
Szilárd Páll <pszilard@kth.se>
2015-01-23 15:59:04 +0100

    define CUDA version and texobj support in config.h
    CUDA_VERSION from cuda.h has been used to check CUDA runtime API feature
    compatibility which is technically incorrect as this macro represents
    the driver API version - although this is identical to the runtime API
    version (CUDART_VERSION in cuda_runtime_api.h) in all existing toolkit
    releases.
    
    To fix this inconsistency and remove the explicit dependency on a CUDA
    header, this change replaces all occurrences of CUDA_VERSION with the
    GMX_CUDA_VERSION macro. This is generated from the FindCUDA-detected
    major+minor version numbers (nvcc version). This should be equal to the
    CUDART_VERSION macro's content. This we now ensure through a consistency
    check at cmake-time. Implicitly, this check also ensures that FindCUDA
    detected a consistent set of CUDA components (compiler and headers).
    
    Additionally, also moved the declaration of the (former)
    TEXOBJ_SUPPORTED macro to config.h
    
    The advantage of moving these macros to config.h is that it eliminates
    the risk of missing include or wrong include order.
    
    Also removed some deprecated code used with CUDA <v4.0.
    
    Change-Id: I933bccd48540ba618db2b959764e5ac566e7e12e

--- src/gromacs/gmxlib/cuda_tools/copyrite_gpu.cu
+++ src/gromacs/gmxlib/cuda_tools/copyrite_gpu.cu
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -36,9 +36,6 @@
 #include "gmxpre.h"
 
 #include <stdio.h>
-
-#include <cuda.h>
-#include <cuda_runtime_api.h>
 
 #include "buildinfo.h"
 
