fb85fe5364e6fe9d92c0e07c009d752bbca05213
John Eblen <jeblen@acm.org>
2015-02-24 12:54:51 -0800

    New functionality for time and cycle accounting
    Allow incrementing cycle count without calling start/stop
    (useful if cycle counts come from external device)
    
    Allow stopping and resuming time accounting
    
    Change-Id: Ic32de40875315d387423f9763361be364698fa76

--- src/gromacs/timing/wallcycle.h
+++ src/gromacs/timing/wallcycle.h
@@ -89,6 +89,9 @@ void wallcycle_start_nocount(gmx_wallcycle_t wc, int ewc);
 double wallcycle_stop(gmx_wallcycle_t wc, int ewc);
 /* Stop the cycle count for ewc, returns the last cycle count */
 
+void wallcycle_add(gmx_wallcycle_t wc, int ewc, gmx_uint64_t cycles, int steps);
+/* Increment cycle count and steps for ewc without having to use start and stop */
+
 void wallcycle_reset_all(gmx_wallcycle_t wc);
 /* Resets all cycle counters to zero */
 
@@ -110,6 +113,9 @@ void wallcycle_sub_start(gmx_wallcycle_t wc, int ewcs);
 
 void wallcycle_sub_stop(gmx_wallcycle_t wc, int ewcs);
 /* Stop the sub cycle count for ewcs */
+
+void wallcycle_sub_add(gmx_wallcycle_t wc, int ewcs, gmx_uint64_t cycles, int steps);
+/* Increment cycle count and steps for ewcs without having to use start and stop */
 
 #ifdef __cplusplus
 }
