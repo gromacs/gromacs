a81810e232bcbbe9335fe82c06b4005bdba8ac92
David van der Spoel <spoel@xray.bmc.uu.se>
2015-04-08 16:13:37 +0200

    Fixed incorrect output of gb_algorith as sa_algorithm.
    Change-Id: I96e51e7c9ac931b7635573a38cd700c61bc596e4

--- src/gromacs/gmxlib/txtdump.c
+++ src/gromacs/gmxlib/txtdump.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -964,7 +964,7 @@ void pr_inputrec(FILE *fp, int indent, const char *title, t_inputrec *ir,
         PR("gb-obc-beta", ir->gb_obc_beta);
         PR("gb-obc-gamma", ir->gb_obc_gamma);
         PR("gb-dielectric-offset", ir->gb_dielectric_offset);
-        PS("sa-algorithm", ESAALGORITHM(ir->gb_algorithm));
+        PS("sa-algorithm", ESAALGORITHM(ir->sa_algorithm));
         PR("sa-surface-tension", ir->sa_surface_tension);
 
         /* Options for weak coupling algorithms */
