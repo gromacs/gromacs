3fae82660c0be31dc50219c1adca18bace3d6845
Viveca Lindahl <vivecal@kth.se>
2015-04-16 01:04:58 +0200

    Added pull coordinate geometry angle.
    Some changes were made to the pull output order and naming.
    Replaced pull-print-com1,2 mdp option with pull-print-com.
    
    Change-Id: Ie3ab77f6f2e9f35b3afe1a99414a9c0b2bafb991

--- src/gromacs/pulling/pull_internal.h
+++ src/gromacs/pulling/pull_internal.h
@@ -88,13 +88,15 @@ typedef struct
 
     double        value_ref; /* The reference value, usually init+rate*t */
     double        value;     /* The current value of the coordinate */
-    dvec          dr;        /* The distance from the reference group */
+    dvec          dr01;      /* The direction vector of group 1 relative to group 0 */
+    dvec          dr23;      /* The direction vector of group 3 relative to group 2 */
     rvec          vec;       /* The pull direction */
     double        vec_len;   /* Length of vec for direction-relative */
     dvec          ffrad;     /* conversion factor from vec to radial force */
     double        cyl_dev;   /* The deviation from the reference position */
     double        f_scal;    /* Scalar force for directional pulling */
-    dvec          f;         /* force due to the pulling/constraining */
+    dvec          f01;       /* Force due to the pulling/constraining for groups 0, 1 */
+    dvec          f23;       /* Force for groups 2 and 3 */
 }
 pull_coord_work_t;
 
@@ -104,6 +106,7 @@ struct pull_t
 
     gmx_bool           bPotential;   /* Are there coordinates with potential? */
     gmx_bool           bConstraint;  /* Are there constrained coordinates? */
+    gmx_bool           bAngle;       /* Are there angle geometry coordinates? */
 
     int                ePBC;         /* the boundary conditions */
     int                npbcdim;      /* do pbc in dims 0 <= dim < npbcdim */
