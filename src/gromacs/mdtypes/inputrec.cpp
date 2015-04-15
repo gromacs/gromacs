5d119573dd128e94785b4a38b65788b5f06748c2
Viveca Lindahl <vivecal@kth.se>
2015-04-16 01:04:58 +0200

    Added pull coordinate geometry angle.
    Some changes were made to the pull output order and naming.
    Replaced pull-print-com1,2 mdp option with pull-print-com.
    A new subsection was added to the docs explaining the new geometry.
    
    Change-Id: Ie3ab77f6f2e9f35b3afe1a99414a9c0b2bafb991

--- src/gromacs/mdtypes/inputrec.cpp
+++ src/gromacs/mdtypes/inputrec.cpp
@@ -43,6 +43,7 @@
 
 #include <algorithm>
 
+#include "gromacs/math/units.h"
 #include "gromacs/math/vecdump.h"
 #include "gromacs/mdtypes/md_enums.h"
 #include "gromacs/mdtypes/pull-params.h"
@@ -544,8 +545,16 @@ static void pr_pull_coord(FILE *fp, int indent, int c, const t_pull_coord *pcrd)
     pr_rvec(fp, indent, "origin", pcrd->origin, DIM, TRUE);
     pr_rvec(fp, indent, "vec", pcrd->vec, DIM, TRUE);
     PS("start", EBOOL(pcrd->bStart));
-    PR("init", pcrd->init);
-    PR("rate", pcrd->rate);
+    if (pcrd->eGeom == epullgANGLE)
+    {
+        PR("init", pcrd->init*RAD2DEG);
+        PR("rate", pcrd->rate*RAD2DEG);
+    }
+    else
+    {
+        PR("init", pcrd->init);
+        PR("rate", pcrd->rate);
+    }
     PR("k", pcrd->k);
     PR("kB", pcrd->kB);
 }
@@ -665,8 +674,7 @@ static void pr_pull(FILE *fp, int indent, const pull_params_t *pull)
 
     PR("pull-cylinder-r", pull->cylinder_r);
     PR("pull-constr-tol", pull->constr_tol);
-    PS("pull-print-COM1", EBOOL(pull->bPrintCOM1));
-    PS("pull-print-COM2", EBOOL(pull->bPrintCOM2));
+    PS("pull-print-COM", EBOOL(pull->bPrintCOM));
     PS("pull-print-ref-value", EBOOL(pull->bPrintRefValue));
     PS("pull-print-components", EBOOL(pull->bPrintComp));
     PI("pull-nstxout", pull->nstxout);
