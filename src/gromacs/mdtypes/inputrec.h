b22f00f31ae75d94ba963fbc464051475fed4623
Berk Hess <hess@kth.se>
2015-04-17 20:10:08 +0200

    Pull group count for coords stored in tpr
    Added a parameter ngroup to the pull coord parameters. This is now
    also stored in the tpr file. This simplifies code in several places,
    since we now only need to determine ngroup is one place. This change
    also makes the pull geometry forward compatible, which is useful
    since it avoid bumping tpx_version with every new geometry and
    we expect that users want to experiment with new geometries.
    
    Change-Id: I7fc0032e439a5ac329f3260d5a6c775295a61bfc

--- src/gromacs/mdtypes/inputrec.h
+++ src/gromacs/mdtypes/inputrec.h
@@ -110,9 +110,10 @@ typedef struct {
 } t_pull_group;
 
 typedef struct {
-    int         group[4];   /* The pull groups, index in group in t_pull */
     int         eType;      /* The pull type: umbrella, constraint, ... */
     int         eGeom;      /* The pull geometry */
+    int         ngroup;     /* The number of groups, depends on eGeom */
+    int         group[4];   /* The pull groups: indices into the group arrays in pull_t and pull_params_t, ngroup indices are used */
     ivec        dim;        /* Used to select components for constraint */
     rvec        origin;     /* The origin for the absolute reference */
     rvec        vec;        /* The pull vector, direction or position */
