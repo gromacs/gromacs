2a51cc64177a33123c9693afde51317beae665d2
Erik Lindahl <erik@kth.se>
2015-04-23 12:07:55 +0200

    Extended SIMD implementation for SSE2, SSE4.1 and 128-bit AVX.
    This adds the same functionality that was previously written to the
    reference SIMD implementation. This includes all the 128-bit x86
    flavors, since SSE4.1 and AVX-128 only overrides a few SSE2
    instructions/functions. For the most performance-sensitive
    functions I expect that we will later test a few different
    alternative implementations once we can benchmark the routines
    inside actual kernels using them. Thus, for now we only care
    about correctness and that it is not obviously worse than some
    other way of implementing the functionality.
    
    Change-Id: I59d5741df345b38745f9a6d1ea3a4d27b0a66034

--- src/gromacs/simd/impl_x86_avx_128_fma/impl_x86_avx_128_fma.h
+++ src/gromacs/simd/impl_x86_avx_128_fma/impl_x86_avx_128_fma.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -58,9 +58,20 @@
 #define GMX_SIMD_HAVE_FRACTION
 #define GMX_SIMD4_HAVE_DOUBLE  /* We can use 256-bit operations for this */
 
+
+/* Work around gcc bug with wrong type for mask formal parameter to maskload/maskstore */
+#ifdef GMX_SIMD_X86_AVX_GCC_MASKLOAD_BUG
+#    define gmx_mm_maskload_ps(mem, mask)       _mm_maskload_ps((mem), _mm_castsi128_ps(mask))
+#    define gmx_mm_maskstore_ps(mem, mask, x)   _mm_maskstore_ps((mem), _mm_castsi128_ps(mask), (x))
+#else
+#    define gmx_mm_maskload_ps(mem, mask)       _mm_maskload_ps((mem), (mask))
+#    define gmx_mm_maskstore_ps(mem, mask, x)   _mm_maskstore_ps((mem), (mask), (x))
+#endif
+
+
 /* SINGLE */
 #undef  gmx_simd_fmadd_ps
-#define gmx_simd_fmadd_ps           _mm_macc_ps
+#define gmx_simd_fmadd_ps             _mm_macc_ps
 #undef  gmx_simd_fmsub_ps
 #define gmx_simd_fmsub_ps(a, b, c)    _mm_msub_ps
 #undef  gmx_simd_fnmadd_ps
@@ -68,11 +79,18 @@
 #undef  gmx_simd_fnmsub_ps
 #define gmx_simd_fnmsub_ps(a, b, c)   _mm_nmsub_ps
 #undef  gmx_simd_fraction_f
-#define gmx_simd_fraction_f         _mm_frcz_ps
+#define gmx_simd_fraction_f           _mm_frcz_ps
+#undef  gmx_simd_gather_load_3_transpose_f
+#define gmx_simd_gather_load_3_transpose_f      gmx_simd_gather_load_3_transpose_f_avx_128_fma
+#undef  gmx_simd_transpose_scatter_store_3_f
+#define gmx_simd_transpose_scatter_store_3_f    gmx_simd_transpose_scatter_store_3_f_avx_128_fma
+#undef  gmx_simd_expand_scalars_to_triplets_f
+#define gmx_simd_expand_scalars_to_triplets_f   gmx_simd_expand_scalars_to_triplets_f_avx_128_fma
+
 
 /* DOUBLE */
 #undef  gmx_simd_fmadd_pd
-#define gmx_simd_fmadd_pd            _mm_macc_pd
+#define gmx_simd_fmadd_pd              _mm_macc_pd
 #undef  gmx_simd_fmsub_pd
 #define gmx_simd_fmsub_pd(a, b, c)     _mm_msub_pd
 #undef  gmx_simd_fnmadd_pd
@@ -80,7 +98,9 @@
 #undef  gmx_simd_fnmsub_pd
 #define gmx_simd_fnmsub_pd(a, b, c)    _mm_nmsub_pd
 #undef  gmx_simd_fraction_d
-#define gmx_simd_fraction_d          _mm_frcz_pd
+#define gmx_simd_fraction_d            _mm_frcz_pd
+#undef  gmx_simd_expand_scalars_to_triplets_d
+#define gmx_simd_expand_scalars_to_triplets_d   gmx_simd_expand_scalars_to_triplets_d_avx_128_fma
 
 /* Even if the _main_ SIMD implementation for this architecture file corresponds
  * to 128-bit AVX (since it will be faster), the 256-bit operations will always
@@ -114,6 +134,7 @@
 #define gmx_simd4_round_d(x)             _mm256_round_pd(x, _MM_FROUND_NINT)
 #define gmx_simd4_trunc_d(x)             _mm256_round_pd(x, _MM_FROUND_TRUNC)
 #define gmx_simd4_dotproduct3_d          gmx_simd4_dotproduct3_d_avx_128_fma
+#define gmx_simd4_transpose_d            gmx_simd4_transpose_d_avx_128_fma
 /* SIMD4 booleans corresponding to double */
 #define gmx_simd4_dbool_t                __m256d
 #define gmx_simd4_cmpeq_d(a, b)           _mm256_cmp_pd(a, b, _CMP_EQ_OQ)
@@ -130,11 +151,85 @@
 #define gmx_simd4_cvt_f2d                _mm256_cvtps_pd
 #define gmx_simd4_cvt_d2f                _mm256_cvtpd_ps
 
+
+
+/****************************************************
+ * Single precision higher-level utility functions  *
+ ****************************************************/
+static gmx_inline void gmx_simdcall
+gmx_simd_gather_load_3_transpose_f_avx_128_fma(const float * base, const gmx_int32_t offset[],
+                                               gmx_simd_float_t * v0, gmx_simd_float_t * v1, gmx_simd_float_t * v2)
+{
+    __m128  t1, t2, t3, t4;
+    __m128i mask = _mm_set_epi32(0, -1, -1, -1);
+    t1             = gmx_mm_maskload_ps(base + 3 * offset[0], mask);
+    t2             = gmx_mm_maskload_ps(base + 3 * offset[1], mask);
+    t3             = gmx_mm_maskload_ps(base + 3 * offset[2], mask);
+    t4             = gmx_mm_maskload_ps(base + 3 * offset[3], mask);
+    _MM_TRANSPOSE4_PS(t1, t2, t3, t4);
+    *v0           = t1;
+    *v1           = t2;
+    *v2           = t3;
+}
+
+static gmx_inline void gmx_simdcall
+gmx_simd_transpose_scatter_store_3_f_avx_128_fma(float * base, const gmx_int32_t offset[],
+                                                 gmx_simd_float_t v0, gmx_simd_float_t v1, gmx_simd_float_t v2)
+{
+    __m128  t3   = _mm_setzero_ps();
+    __m128i mask = _mm_set_epi32(0, -1, -1, -1);
+    _MM_TRANSPOSE4_PS(v0, v1, v2, t3);
+    gmx_mm_maskstore_ps(base + 3 * offset[0], mask, v0);
+    gmx_mm_maskstore_ps(base + 3 * offset[1], mask, v1);
+    gmx_mm_maskstore_ps(base + 3 * offset[2], mask, v2);
+    gmx_mm_maskstore_ps(base + 3 * offset[3], mask, t3);
+
+}
+
+/* In the old group kernels we found it more efficient to transpose the data to store rather
+ * than using maskload and maskstore. It might be worth to test again, but for now we assume
+ * this is still the case, and rely on those version inherited from the SSE2 header.
+ *
+ * It is also worth testing if changing _mm_shuffle_ps() to _mm_permute_ps() could improve
+ * throughput just-so-slightly.
+ */
+
+static gmx_inline void gmx_simdcall
+gmx_simd_expand_scalars_to_triplets_f_avx_128_fma(gmx_simd_float_t   scalar,
+                                                  gmx_simd_float_t * triplets0,
+                                                  gmx_simd_float_t * triplets1,
+                                                  gmx_simd_float_t * triplets2)
+{
+    *triplets0 = _mm_permute_ps(scalar, _MM_SHUFFLE(1, 0, 0, 0));
+    *triplets1 = _mm_permute_ps(scalar, _MM_SHUFFLE(2, 2, 1, 1));
+    *triplets2 = _mm_permute_ps(scalar, _MM_SHUFFLE(3, 3, 3, 2));
+}
+
+/* Permute might be a small help for gmx_simd_reduce_incr_4_return_sum_f(). Test in kernels later. */
+
+
+
+/****************************************************
+ * Double precision higher-level utility functions  *
+ ****************************************************/
+static gmx_inline void gmx_simdcall
+gmx_simd_expand_scalars_to_triplets_d_avx_128_fma(gmx_simd_double_t   scalar,
+                                                  gmx_simd_double_t * triplets0,
+                                                  gmx_simd_double_t * triplets1,
+                                                  gmx_simd_double_t * triplets2)
+{
+    *triplets0 = _mm_permute_pd(scalar, _MM_SHUFFLE2(0, 0));
+    *triplets1 = _mm_permute_pd(scalar, _MM_SHUFFLE2(1, 0));
+    *triplets2 = _mm_permute_pd(scalar, _MM_SHUFFLE2(1, 1));
+}
+
+
 static gmx_inline double gmx_simdcall
 gmx_simd4_reduce_d_avx_128_fma(__m256d a)
 {
     double  f;
     __m128d a0, a1;
+    /* test with shuffle & add as an alternative to hadd later */
     a  = _mm256_hadd_pd(a, a);
     a0 = _mm256_castpd256_pd128(a);
     a1 = _mm256_extractf128_pd(a, 0x1);
@@ -157,5 +252,21 @@ gmx_simd4_dotproduct3_d_avx_128_fma(__m256d a, __m256d b)
     _mm_store_sd(&d, tmp1);
     return d;
 }
+
+static gmx_inline void gmx_simdcall
+gmx_simd4_transpose_d_avx_128_fma(gmx_simd4_double_t * v0, gmx_simd4_double_t * v1,
+                                  gmx_simd4_double_t * v2, gmx_simd4_double_t * v3)
+{
+    __m256d t1, t2, t3, t4;
+    t1  = _mm256_unpacklo_pd(*v0, *v1);
+    t2  = _mm256_unpackhi_pd(*v0, *v1);
+    t3  = _mm256_unpacklo_pd(*v2, *v3);
+    t4  = _mm256_unpackhi_pd(*v2, *v3);
+    *v0 = _mm256_permute2f128_pd(t1, t3, 0x20);
+    *v1 = _mm256_permute2f128_pd(t2, t4, 0x20);
+    *v2 = _mm256_permute2f128_pd(t1, t3, 0x31);
+    *v3 = _mm256_permute2f128_pd(t2, t4, 0x31);
+}
+
 
 #endif /* GMX_SIMD_IMPL_X86_AVX_128_FMA_H */
