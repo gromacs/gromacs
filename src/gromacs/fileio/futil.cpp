95c7de012d45b9e819e41cb99c8d5c66420550f6
Olivier Fisette <ofisette@gmail.com>
2015-04-26 15:54:27 +0200

    Fix error handling in gmx_tmpnam
    The return code of mkstemp was being mis-used for error handling.
    This could explain some long-standing issues with (e.g.) DSSP
    mysteriously not working even when the user had done everything right.
    
    Fixes #1717
    
    Change-Id: I72b385a751b99c3f49d99a14bfc6964ad776c22d

--- src/gromacs/fileio/futil.cpp
+++ src/gromacs/fileio/futil.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -851,19 +851,10 @@ void gmx_tmpnam(char *buf)
 #else
     int fd = mkstemp(buf);
 
-    switch (fd)
+    if (fd < 0)
     {
-        case EINVAL:
-            gmx_fatal(FARGS, "Invalid template %s for mkstemp", buf);
-            break;
-        case EEXIST:
-            gmx_fatal(FARGS, "mkstemp created existing file", buf);
-            break;
-        case EACCES:
-            gmx_fatal(FARGS, "Permission denied for opening %s", buf);
-            break;
-        default:
-            break;
+        gmx_fatal(FARGS, "Creating temporary file %s: %s", buf,
+                  strerror(errno));
     }
     close(fd);
 #endif
