dd4aa9d377e2bfb3d13de03b418f9c800cc3f820
Erik Lindahl <erik@kth.se>
2015-04-28 17:02:39 -0400

    Extended SIMD implementation for 256-bit AVX and AVX2
    Added support for nonbonded kernel utilities,
    gather-transpose and transpose-store operations. Most
    routines have been optimized to reduce cross-lane
    shuffling which is more expensive on Haswell (AVX2).
    
    Change-Id: Ia9b3733051410355ed9d4a68cbdb9646c9d3e41d

--- src/gromacs/simd/impl_x86_avx2_256/impl_x86_avx2_256_util_double.h
+++ src/gromacs/simd/impl_x86_avx2_256/impl_x86_avx2_256_util_double.h
@@ -33,36 +33,11 @@
  * the research papers on the package. Check out http://www.gromacs.org.
  */
 
-#ifndef GMX_SIMD_IMPL_X86_AVX_256_COMMON_H
-#define GMX_SIMD_IMPL_X86_AVX_256_COMMON_H
+#ifndef GMX_SIMD_IMPL_X86_AVX2_256_UTIL_DOUBLE_H
+#define GMX_SIMD_IMPL_X86_AVX2_256_UTIL_DOUBLE_H
 
-/* Capability definitions for 256-bit AVX - no inheritance from SSE */
-#define GMX_SIMD                          1
-#define GMX_SIMD_HAVE_FLOAT               1
-#define GMX_SIMD_HAVE_DOUBLE              1
-#define GMX_SIMD_HAVE_LOADU               1
-#define GMX_SIMD_HAVE_STOREU              1
-#define GMX_SIMD_HAVE_LOGICAL             1
-#define GMX_SIMD_HAVE_FMA                 0
-#define GMX_SIMD_HAVE_FRACTION            0
-#define GMX_SIMD_HAVE_FINT32              1
-#define GMX_SIMD_HAVE_FINT32_EXTRACT      1  /* Emulated */
-#define GMX_SIMD_HAVE_FINT32_LOGICAL      0  /* AVX1 cannot do 256-bit int shifts */
-#define GMX_SIMD_HAVE_FINT32_ARITHMETICS  0  /* AVX1 cannot do 256-bit int +,-,*  */
-#define GMX_SIMD_HAVE_DINT32              1
-#define GMX_SIMD_HAVE_DINT32_EXTRACT      1  /* Native, dint uses 128-bit SIMD    */
-#define GMX_SIMD_HAVE_DINT32_LOGICAL      1
-#define GMX_SIMD_HAVE_DINT32_ARITHMETICS  1
-#define GMX_SIMD4_HAVE_FLOAT              1
-#define GMX_SIMD4_HAVE_DOUBLE             1
+#include <immintrin.h>
 
-/* Implementation details */
-#define GMX_SIMD_FLOAT_WIDTH              8
-#define GMX_SIMD_DOUBLE_WIDTH             4
-#define GMX_SIMD_FINT32_WIDTH             8
-#define GMX_SIMD_DINT32_WIDTH             4
-#define GMX_SIMD4_WIDTH                   4
-#define GMX_SIMD_RSQRT_BITS              11
-#define GMX_SIMD_RCP_BITS                11
+#include "gromacs/simd/impl_x86_avx_256/impl_x86_avx_256_util_double.h"
 
-#endif /* GMX_SIMD_IMPL_X86_AVX_256_COMMON_H */
+#endif // GMX_SIMD_IMPL_X86_AVX2_256_UTIL_DOUBLE_H
