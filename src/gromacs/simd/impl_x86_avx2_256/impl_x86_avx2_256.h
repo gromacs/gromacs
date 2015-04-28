c31711be429e60d3472a0bcfc94c26798b2ff3d5
Erik Lindahl <erik@kth.se>
2015-04-28 17:02:39 -0400

    Extended SIMD implementation for 256-bit AVX and AVX2
    Added support for nonbonded kernel utilities,
    gather-transpose and transpose-store operations. Most
    routines have been optimized to reduce cross-lane
    shuffling which is more expensive on Haswell (AVX2).
    
    Change-Id: Ia9b3733051410355ed9d4a68cbdb9646c9d3e41d

--- src/gromacs/simd/impl_x86_avx2_256/impl_x86_avx2_256.h
+++ src/gromacs/simd/impl_x86_avx2_256/impl_x86_avx2_256.h
@@ -36,9 +36,13 @@
 #ifndef GMX_SIMD_IMPL_X86_AVX2_256_H
 #define GMX_SIMD_IMPL_X86_AVX2_256_H
 
+#include "impl_x86_avx2_256_definitions.h"
+#include "impl_x86_avx2_256_general.h"
 #include "impl_x86_avx2_256_simd4_double.h"
 #include "impl_x86_avx2_256_simd4_float.h"
 #include "impl_x86_avx2_256_simd_double.h"
 #include "impl_x86_avx2_256_simd_float.h"
+#include "impl_x86_avx2_256_util_double.h"
+#include "impl_x86_avx2_256_util_float.h"
 
-#endif /* GMX_SIMD_IMPL_X86_AVX2_256_H */
+#endif // GMX_SIMD_IMPL_X86_AVX2_256_H
