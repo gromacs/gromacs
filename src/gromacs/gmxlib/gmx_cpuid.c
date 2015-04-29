afbeb677cb5722243d2f39a383b8769a9bf96c17
Berk Hess <hess@kth.se>
2015-04-29 12:13:00 +0200

    Hardware reporting now covers all nodes
    The CPU and GPU hardware reporting and checking were only printed
    for the node(s) of the master rank(s) of each simulation.
    Now the most important CPU and GPU information is reduced over
    MPI_COMM_WORLD. These results are printed and checks/warnings are
    now based on this.
    Because this is now system wide, the printing is moved up to directly
    after the detection and GPU info is printed together with CPU info.
    The Gromacs reference printing is moved to after hardware printing.
    
    Refs #1643.
    
    Change-Id: I974871c9a293a922dd1db9dcf633d485f62996ae

--- src/gromacs/gmxlib/gmx_cpuid.c
+++ src/gromacs/gmxlib/gmx_cpuid.c
@@ -255,6 +255,13 @@ gmx_cpuid_is_intel_nehalem  (const gmx_cpuid_t          cpuid)
 }
 
 
+int
+gmx_cpuid_ncores            (const gmx_cpuid_t          cpuid)
+{
+    return cpuid->npackages*cpuid->ncores_per_package;
+}
+
+
 /* What type of SIMD was compiled in, if any? */
 #ifdef GMX_SIMD_X86_AVX_512ER
 static const enum gmx_cpuid_simd compiled_simd = GMX_CPUID_SIMD_X86_AVX_512ER;
@@ -287,6 +294,13 @@ static const enum gmx_cpuid_simd compiled_simd = GMX_CPUID_SIMD_REFERENCE;
 #else
 static const enum gmx_cpuid_simd compiled_simd = GMX_CPUID_SIMD_NONE;
 #endif
+
+
+enum gmx_cpuid_simd
+gmx_compiled_simd()
+{
+    return compiled_simd;
+}
 
 
 #ifdef GMX_CPUID_X86
@@ -1174,19 +1188,19 @@ gmx_cpuid_formatstring       (gmx_cpuid_t              cpuid,
 
 #ifdef _MSC_VER
     _snprintf(str, n,
-              "Vendor: %s\n"
-              "Brand:  %s\n"
-              "Family: %2d  Model: %2d  Stepping: %2d\n"
-              "Features:",
+              "  CPU vendor: %s\n"
+              "  CPU brand:  %s\n"
+              "  CPU family: %2d  model: %2d  stepping: %2d\n"
+              "  CPU features:",
               gmx_cpuid_vendor_string[gmx_cpuid_vendor(cpuid)],
               gmx_cpuid_brand(cpuid),
               gmx_cpuid_family(cpuid), gmx_cpuid_model(cpuid), gmx_cpuid_stepping(cpuid));
 #else
     snprintf(str, n,
-             "Vendor: %s\n"
-             "Brand:  %s\n"
-             "Family: %2d  Model: %2d  Stepping: %2d\n"
-             "Features:",
+             "  CPU vendor: %s\n"
+             "  CPU brand:  %s\n"
+             "  CPU family: %2d  model: %2d  stepping: %2d\n"
+             "  CPU features:",
              gmx_cpuid_vendor_string[gmx_cpuid_vendor(cpuid)],
              gmx_cpuid_brand(cpuid),
              gmx_cpuid_family(cpuid), gmx_cpuid_model(cpuid), gmx_cpuid_stepping(cpuid));
@@ -1309,32 +1323,13 @@ gmx_cpuid_simd_suggest  (gmx_cpuid_t                 cpuid)
 
 
 int
-gmx_cpuid_simd_check(gmx_cpuid_t   cpuid,
-                     FILE *        log,
-                     int           print_to_stderr)
+gmx_cpuid_simd_check(enum gmx_cpuid_simd  simd_suggest,
+                     FILE *               log,
+                     int                  print_to_stderr)
 {
-    int                           rc;
-    char                          str[1024];
-    enum gmx_cpuid_simd           simd;
+    int  rc;
 
-    simd = gmx_cpuid_simd_suggest(cpuid);
-
-    rc = (simd != compiled_simd);
-
-    gmx_cpuid_formatstring(cpuid, str, 1023);
-    str[1023] = '\0';
-
-    if (log != NULL)
-    {
-        fprintf(log,
-                "\nDetecting CPU SIMD instructions.\nPresent hardware specification:\n"
-                "%s"
-                "SIMD instructions most likely to fit this hardware: %s\n"
-                "SIMD instructions selected at GROMACS compile time: %s\n\n",
-                str,
-                gmx_cpuid_simd_string[simd],
-                gmx_cpuid_simd_string[compiled_simd]);
-    }
+    rc = (simd_suggest != compiled_simd);
 
     if (rc != 0)
     {
@@ -1343,14 +1338,14 @@ gmx_cpuid_simd_check(gmx_cpuid_t   cpuid,
             fprintf(log, "\nBinary not matching hardware - you might be losing performance.\n"
                     "SIMD instructions most likely to fit this hardware: %s\n"
                     "SIMD instructions selected at GROMACS compile time: %s\n\n",
-                    gmx_cpuid_simd_string[simd],
+                    gmx_cpuid_simd_string[simd_suggest],
                     gmx_cpuid_simd_string[compiled_simd]);
         }
         if (print_to_stderr)
         {
             fprintf(stderr, "Compiled SIMD instructions: %s (Gromacs could use %s on this machine, which is better)\n",
                     gmx_cpuid_simd_string[compiled_simd],
-                    gmx_cpuid_simd_string[simd]);
+                    gmx_cpuid_simd_string[simd_suggest]);
         }
     }
     return rc;
