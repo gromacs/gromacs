56a67d7cce2acc2feee9b7fe3c7e503010f5032b
Berk Hess <hess@kth.se>
2015-05-13 21:54:38 +0200

    Fixed CUDA error with empty domains
    Recent commit fc8a5624 introduced empty CUDA kernel calls when there
    are empty domains. This seems not be allowed by CUDA (we get errors).
    Fixed #1734. Refs #1721.
    
    Change-Id: Ifd32a55c8d6756c93a0fcaba29983ae326abc569

--- src/mdlib/nbnxn_cuda/nbnxn_cuda.cu
+++ src/mdlib/nbnxn_cuda/nbnxn_cuda.cu
@@ -292,7 +292,8 @@ void nbnxn_cuda_launch_kernel(nbnxn_cuda_ptr_t cu_nb,
        we always call the local kernel, the local x+q copy and later (not in
        this function) the stream wait, local f copyback and the f buffer
        clearing. All these operations, except for the local interaction kernel,
-       are needed for the non-local interactions. */
+       are needed for the non-local interactions. The skip of the local kernel
+       call is taken care of later in this function. */
     if (iloc == eintNonlocal && plist->nsci == 0)
     {
         return;
@@ -341,6 +342,12 @@ void nbnxn_cuda_launch_kernel(nbnxn_cuda_ptr_t cu_nb,
     {
         stat = cudaEventRecord(t->stop_nb_h2d[iloc], stream);
         CU_RET_ERR(stat, "cudaEventRecord failed");
+    }
+
+    if (plist->nsci == 0)
+    {
+        /* Don't launch an empty local kernel (not allowed with CUDA) */
+        return;
     }
 
     /* beginning of timed nonbonded calculation section */
