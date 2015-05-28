3e90ff1caf3ccbeb1c62b023a804229a3a774595
Berk Hess <hess@kth.se>
2015-05-28 11:23:21 +0200

    Fix for GMX_FORCE_NONBONDED off with nbnxn
    When the GMX_FORCE_NONBONDED force flag was off (e.g. by setting
    the GMX_NO_NONBONDED env.var.), the nbnxn kernels were skipped,
    but the force reduction was not, which caused unitialized memory
    to be added to the forces.
    
    Change-Id: Iacc441883da1c4901273e1d7e3a88078fc02f032

--- src/gromacs/mdlib/sim_util.cpp
+++ src/gromacs/mdlib/sim_util.cpp
@@ -1198,7 +1198,7 @@ void do_force_cutsVERLET(FILE *fplog, t_commrec *cr,
         }
     }
 
-    if (!bUseOrEmulGPU || bDiffKernels)
+    if ((!bUseOrEmulGPU || bDiffKernels) && (flags & GMX_FORCE_NONBONDED))
     {
         int aloc;
 
@@ -1274,7 +1274,7 @@ void do_force_cutsVERLET(FILE *fplog, t_commrec *cr,
         do_flood(cr, inputrec, x, f, ed, box, step, bNS);
     }
 
-    if (bUseOrEmulGPU && !bDiffKernels)
+    if ((bUseOrEmulGPU && !bDiffKernels) && (flags & GMX_FORCE_NONBONDED))
     {
         /* wait for non-local forces (or calculate in emulation mode) */
         if (DOMAINDECOMP(cr))
@@ -1338,7 +1338,7 @@ void do_force_cutsVERLET(FILE *fplog, t_commrec *cr,
         wallcycle_stop(wcycle, ewcMOVEF);
     }
 
-    if (bUseOrEmulGPU)
+    if (bUseOrEmulGPU && (flags & GMX_FORCE_NONBONDED))
     {
         /* wait for local forces (or calculate in emulation mode) */
         if (bUseGPU)
