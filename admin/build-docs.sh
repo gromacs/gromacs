d50a19d06284b9f11f31975c4d7e22361161c9bc
Teemu Murtola <teemu.murtola@gmail.com>
2015-06-04 08:48:06 +0300

    WIP Improve Jenkins docs build
    - Use GMX_COMPACT_DOXYGEN=ON for per-patch set builds to save disk space
      and some running time.
    - Build also the man and install-guide targets, which were previously
      missing.
    - Make the build fail if it actually fails, and unstable if it just
      produces warnings.
    - Make the end of the console output contain some useful information on
      what exactly failed.  This gets put into fail-reason.log, which could
      potentially be used otherwise as well.
    
    Change-Id: Ice7622d7760c3dbd74f7b5b25700482d0a08e06b

--- admin/build-docs.sh
+++ admin/build-docs.sh
@@ -52,6 +52,7 @@
 #  * stdout (any instance of "FAILED" marks the build unstable),
 #  * HTML documentation produced at build/docs/html/, and
 #  * log files under build/docs/logs/:
+#     - fail-reason.log contains a short description of what failed/was unstable
 #     - all .log files from this directory are published as Jenkins artifacts
 #     - all .log files under doxygen/ subdir are scanned for Doxygen warnings
 #     - all .log files under sphinx/ subdir are scanned for Sphinx warnings
@@ -77,6 +78,24 @@ case "$1" in
 esac
 shift
 
+FAILED=
+function report_failure() {
+    echo "$1"
+    echo "$1" >> docs/logs/fail-reason.log
+    FAILED=1
+}
+function report_unstable() {
+    echo "FAILED: $1"
+    echo "$1" >> docs/logs/fail-reason.log
+}
+function exit_if_failed() {
+    if [[ $FAILED ]] ; then
+        echo "Documentation build FAILED:"
+        cat docs/logs/fail-reason.log
+        exit 1
+    fi
+}
+
 set -x
 
 srcdir=`git rev-parse --show-toplevel`
@@ -91,35 +110,56 @@ rm -rf build
 mkdir build
 cd build
 
+# Copy the output logs to a static hierarchy to allow changing the exact file
+# names and adding new logs without changing the Jenkins job configuration.
+mkdir docs
+mkdir docs/logs
+
+cmake_args="-DGMX_BUILD_HELP=ON -DGMX_BUILD_MANUAL=ON"
+if [[ $GERRIT_MODE ]] ; then
+    cmake_args+=" -DGMX_COMPACT_DOXYGEN=ON"
+fi
 # Make a configuration that is fast, just for building docs.
-cmake ..  "$@" \
-    -DGMX_BUILD_HELP=yes -DGMX_BUILD_MANUAL=yes \
-    -DCMAKE_BUILD_TYPE=Debug -DGMX_OPENMP=off -DGMX_SIMD=None -DGMX_GPU=no
+cmake_args+=" -DCMAKE_BUILD_TYPE=Debug -DGMX_OPENMP=OFF -DGMX_SIMD=None -DGMX_GPU=OFF"
+cmake ..  ${cmake_args} "$@" || report_failure "CMake configuration failed"
+exit_if_failed
 
 # Need to make gmx to export rst help.
-make gmx -j 2 || echo "FAILED to make gmx"
+make gmx -j 2 || report_failure "Building gmx failed"
 
-# webpage target makes all the documentation components.
-make webpage || echo "FAILED to make webpage"
+# Do various parts individually to report the errors.
+make manual || report_failure "PDF manual failed to build"
+cp docs/manual/gromacs.log docs/logs/
+grep "LaTeX Warning: Reference .* on page .* undefined" docs/logs/gromacs.log && report_unstable "undefined references in manual"
 
-# Ideally, we would also make the other components individually, to check
-# that the targets still work, but most of them duplicate the build
-
-grep "LaTeX Warning: Reference .* on page .* undefined" docs/manual/gromacs.log && echo "FAILED - undefined references in manual"
+make doxygen-all || report_failure "Doxygen documentation failed to build"
 
 # run check-source
 if [[ $GERRIT_MODE ]] ; then
-    make check-source || echo "FAILED: check-source found errors"
+    make check-source || report_failure "check-source failed to run"
 fi
-
-# Copy the output logs to a static hierarchy to allow changing the exact file
-# names and adding new logs without changing the Jenkins job configuration.
-mkdir docs/logs
-cp docs/manual/manual.log docs/logs/
 mkdir docs/logs/doxygen
 cp docs/doxygen/*.log docs/logs/doxygen/
+# TODO: Check also the other logs.
+[[ -s docs/logs/check-source.log ]] && report_unstable "check-source found issues"
+exit_if_failed
+
+# Generate Sphinx input.
+make sphinx-input || report_failure "Generating Sphinx input failed"
+make sphinx-programs || report_failure "Running gmx help -export rst failed"
+exit_if_failed
+
+# Run various Sphinx commands
+make webpage-sphinx || report_failure "HTML generation with Sphinx failed"
+make man || report_failure "Man page generation with Sphinx failed"
+make install-guide || report_failure "INSTALL generation with Sphinx failed"
 mkdir docs/logs/sphinx
 cp docs/sphinx-*.log docs/logs/sphinx/
+# TODO: Report unstability reason if any of these logs are non-empty.
+exit_if_failed
+
+# webpage target makes some final work for the documentation.
+make webpage || report_failure "webpage target failed to build"
 
 cd ..
 
@@ -133,6 +173,7 @@ else
     echo "No build/docs/html/index.html was made; can't check links!"
 fi
 
+exit_if_failed
 # Exit with zero code to avoid failing builds because of whatever was the last
 # command executed.
 exit 0
