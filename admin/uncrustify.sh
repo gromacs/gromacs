2fd5cf0509e9c942f751763b58d27bf4be701171
Teemu Murtola <teemu.murtola@gmail.com>
2015-06-05 22:25:13 +0300

    Improve uncrustify Jenkins builds
    Add a script to wrap uncrustify.sh for Jenkins, and make it produce
    additional text files that can more easily be used for further
    processing in Jenkins.
    
    Change-Id: I3c25760eec4c37a07a7889f471eb5b61f9932a9c

--- admin/uncrustify.sh
+++ admin/uncrustify.sh
@@ -42,7 +42,8 @@
 # Parse command-line arguments
 function usage() {
     echo "usage: uncrustify.sh [-f|--force] [--rev=REV]"
-    echo "           [--uncrustify=(off|check)] [--copyright=<cmode>] [<action>]"
+    echo "           [--uncrustify=(off|check)] [--copyright=<cmode>]"
+    echo "           [--warnings=<file>] [<action>]"
     echo "<action>: (check*|diff|update)[-(index|workdir*)] (*=default)"
     echo "<cmode>:  off|add|update*|replace|full"
 }
@@ -53,6 +54,7 @@ baserev="HEAD"
 force=
 uncrustify_mode=check
 copyright_mode=update
+warning_file=
 for arg in "$@" ; do
     if [[ "$arg" == "check-index" || "$arg" == "check-workdir" || \
           "$arg" == "diff-index" || "$arg" == "diff-workdir" || \
@@ -77,6 +79,8 @@ for arg in "$@" ; do
         force=1
     elif [[ "$arg" == --rev=* ]] ; then
         baserev=${arg#--rev=}
+    elif [[ "$arg" == --warnings=* ]] ; then
+        warning_file=${arg#--warnings=}
     elif [[ "$arg" == "-h" || "$arg" == "--help" ]] ; then
         usage
         exit 0
@@ -272,7 +276,7 @@ elif [[ $action == update-workdir ]] ; then
 fi
 
 # Report what was done
-sort $tmpdir/messages
+sort $tmpdir/messages | tee $srcdir/$warning_file
 
 rm -rf $tmpdir
 exit $changes
