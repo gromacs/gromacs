8aeafd51350302ed9ec815cf414b0ab4f7469516
mohammad-ghahremanpour <ghahremanpour.gro@gmail.com>
2015-06-12 15:42:42 +0200

    gen_table
    Change-Id: I5c2dd411ea9887af93754b1a33e864e7452df3c6

--- src/programs/alexandria/gen_table.cpp
+++ src/programs/alexandria/gen_table.cpp
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 /*! \internal \brief
- * Implements part of the alexandria program.
+ * Implements part of the alexandria program
  * \author David van der Spoel <david.vanderspoel@icm.uu.se>
  */
 #include "gmxpre.h"
