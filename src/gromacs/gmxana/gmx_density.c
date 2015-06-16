3e9a0a12522f9d6adbbdac6271cd97b62a29bdd7
Erik Lindahl <erik@kth.se>
2015-06-17 00:15:18 +0200

    Fix incorrect shift with g_density -center
    Fixes #1698.
    
    Change-Id: I22be246eded9c1fc6f421f14eb3938584afed0de

--- src/gromacs/gmxana/gmx_density.c
+++ src/gromacs/gmxana/gmx_density.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -159,7 +159,7 @@ void center_coords(t_atoms *atoms, atom_id *index_center, int ncenter,
         com[m] /= tmass;
     }
     calc_box_center(ecenterDEF, box, box_center);
-    rvec_sub(box_center, com, shift);
+    rvec_sub(com, box_center, shift);
 
     /* Important - while the center was calculated based on a group, we should move all atoms */
     for (i = 0; (i < atoms->nr); i++)
