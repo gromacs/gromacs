00dfed87da10cc1ab3e849f26ff950a0ef18e831
Erik Lindahl <erik@kth.se>
2015-06-17 00:18:27 +0200

    Fix argument order error for g_rmsdist
    Incorrect argument order to calc_rms() caused
    command options to be permuted.
    
    Fixes #1709.
    
    Change-Id: I82042dac463c8bb305ab3d3bc2a122b5409c55ef

--- src/gromacs/gmxana/gmx_rmsdist.c
+++ src/gromacs/gmxana/gmx_rmsdist.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -804,7 +804,7 @@ int gmx_rmsdist(int argc, char *argv[])
 
     close_trj(status);
 
-    calc_rms(isize, teller, dtot, dtot2, mean, &meanmax, rms, &rmsmax, rmsc, &rmscmax);
+    calc_rms(isize, teller, dtot, dtot2, rms, &rmsmax, rmsc, &rmscmax, mean, &meanmax);
     fprintf(stderr, "rmsmax = %g, rmscmax = %g\n", rmsmax, rmscmax);
 
     if (bNMR)
