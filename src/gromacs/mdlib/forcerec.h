8c1f18267c2d4c18a894d0db1ffccf52aaf0025b
Berk Hess <hess@kth.se>
2015-06-18 10:46:53 +0200

    WIP: Quadratic spline table can read from table
    This generalizes the quadratic spline table generation function.
    It can now take its input not only from an analytical function,
    but also from a table with potential values and optionally force
    values. Currently this new functionally is not used.
    
    The routine is converted to C++ and a few tests have been added.
    A new InteractionTables class is introduced and passed around
    leading to that a few more c files are converted to C++.
    
    Change-Id: I41aaf1f7c7994888967be2f40f51d548b4e69169

---
+++ src/gromacs/mdlib/forcerec.h
@@ -0,0 +1,83 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
+ * Copyright (c) 2001-2004, The GROMACS development team.
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+#ifndef GMX_MDLIB_FORCEREC_H
+#define GMX_MDLIB_FORCEREC_H
+
+#include "gromacs/legacyheaders/genborn.h"
+#include "gromacs/legacyheaders/network.h"
+#include "gromacs/legacyheaders/tgroup.h"
+#include "gromacs/legacyheaders/typedefs.h"
+#include "gromacs/legacyheaders/vsite.h"
+#include "gromacs/legacyheaders/types/force_flags.h"
+#include "gromacs/tables/tables.h"
+#include "gromacs/timing/wallcycle.h"
+
+void init_interaction_const_tables(FILE                   *fp,
+                                   interaction_const_t    *ic,
+                                   gmx::InteractionTables *it,
+                                   real                    rtab);
+/* Initializes the tables in the interaction constant data structure. */
+
+void init_forcerec(FILE                   *fplog,
+                   const output_env_t      oenv,
+                   t_forcerec             *fr,
+                   t_fcdata               *fcd,
+                   const t_inputrec       *ir,
+                   const gmx_mtop_t       *mtop,
+                   const t_commrec        *cr,
+                   matrix                  box,
+                   const char             *tabfn,
+                   const char             *tabafn,
+                   const char             *tabpfn,
+                   const char             *tabbfn,
+                   const char             *nbpu_opt,
+                   gmx::InteractionTables *interaction_tables,
+                   gmx_bool                bNoSolvOpt,
+                   real                    print_force);
+/* The Force rec struct must be created with mk_forcerec
+ * The gmx_booleans have the following meaning:
+ * bSetQ:    Copy the charges [ only necessary when they change ]
+ * bMolEpot: Use the free energy stuff per molecule
+ * print_force >= 0: print forces for atoms with force >= print_force
+ */
+
+void forcerec_set_excl_load(t_forcerec           *fr,
+                            const gmx_localtop_t *top);
+/* Set the exclusion load for the local exclusions and possibly threads */
+
+#endif
