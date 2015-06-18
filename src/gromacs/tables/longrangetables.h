d53efe9ac133fbc91c7151971eb9aed18d1862a0
Berk Hess <hess@kth.se>
2015-06-18 10:46:53 +0200

    WIP: Quadratic spline table can read from table
    This generalizes the quadratic spline table generation function.
    It can now take its input not only from an analytical function,
    but also from a table with potential values and optionally force
    values. Currently this new functionally is not used.
    
    The routine is converted to C++ and a few tests have been added.
    A new InteractionTables class is introduced and passed around
    leading to that a few more c files are converted to C++.
    
    Change-Id: I41aaf1f7c7994888967be2f40f51d548b4e69169

---
+++ src/gromacs/tables/longrangetables.h
@@ -0,0 +1,201 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2012,2014,2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#ifndef GMX_TABLES_TABLES_H
+#define GMX_TABLES_TABLES_H
+
+#include <cstdlib>
+
+#include <vector>
+
+#include "gromacs/tables/splinetable.h"
+
+/*! \libinternal \brief
+ * Function pointer to fill table.
+ *
+ * Function pointer used to tell the table filling routine whether it
+ * should calculate the grid contribution for electrostatics or LJ.
+ * The first parameters is an array of constant used to evaluate
+ * the function, the second parameter is the distance.
+ */
+typedef double (*interaction_potential_function)(const double *, double);
+
+/*! \libinternal\brief
+ * Return the long range energy for Ewald Coulomb
+ *
+ * \param[in] params Parameters to the long range function
+ * \param[in] r      Distance
+ * \return Long range energy
+ */
+double v_q_ewald_lr(const double *params, double r);
+
+/*! \libinternal\brief
+ * Return the long range energy for Ewald Lennard Jones (r^{-6} only)
+ *
+ * \param[in] params Parameters to the long range function
+ * \param[in] r      Distance
+ * \return Long range energy
+ */
+double v_lj_ewald_lr(const double *params, double r);
+
+namespace gmx
+{
+/*! \libinternal\brief Class containing long range interaction tables.
+ *
+ * This class generates input for quadratic spline table interpolation.
+ * The force table table_f is always generated. The potential and combined
+ * tables table_v and table_fdv0 are not generated when NULL is passed.
+ * Input can be generated using either an analytical function, passed
+ * as function pointer v_could, v_vdw with parameters params_coul and
+ * params_vdw or using table inputs.
+ * Table input can be passed as a potential alone, using table_v_coul
+ * and table_v_vdw or
+ * combined with a force table(s) table_f_coul, table_f_vdw.
+ * A stride is used (which can be 1):
+ * V(i*dx) = table_in_v(i*stride*dx). When no force table input is passed,
+ * the stride has to be a multiple of 2.
+ * Note that the table input should be smooth, e.g. not containing noise
+ * from an (iterative) Boltzmann inversion procedure.
+ *
+ * The scale (1/spacing) for third order spline interpolation
+ * of the Ewald mesh contribution which needs to be subtracted
+ * from the non-bonded interactions.
+ * Since there is currently only one spacing for Coulomb and LJ,
+ * the finest spacing is used if both Ewald types are present.
+ *
+ * The class implements completely separate tables for the energy
+ * and force for both Coulomb and Van der Waals and a combined
+ * table which is more efficient to evaluate in the mdrun kernels.
+ *
+ * The current setup uses the same spacing to provide slightly
+ * faster kernels with both Coulomb and LJ Ewald, especially
+ * when interleaving both tables (currently not implemented).
+ */
+class LongRangeInteractionTables
+{
+    public:
+        /*! \brief Constructor
+         *
+         * \param[in] type_coul       The electrostatics type
+         * \param[in] ewaldcoeff_coul The coefficient for Coulomb
+         * \param[in] r_coul          The Coulomb cut-off (switching distance)
+         * \param[in] params_coul     Parameters for the function v_qq
+         * \param[in] v_coul          A function pointer that can be used
+         *                            for computing energies (may be NULL)
+         * \param[in] table_v_coul    Table containing energies for Coulomb
+         *                            (may be NULL)
+         * \param[in] table_f_coul    Table containing forces for Coulomb
+         *                            (may be NULL)
+         * \param[in] type_vdw        The Van der Waals type
+         * \param[in] ewaldcoeff_vdw  The coefficient for Van der Waals
+         * \param[in] r_vdw           The Van der Waals cut-off (switching distance)
+         * \param[in] params_vdw      Parameters for the function v_vdw
+         * \param[in] v_vdw           A function pointer that can be used as an
+         * \param[in] table_v_vdw     Table containing energies for Van der
+         *                            Waals (may be NULL)
+         * \param[in] table_f_vdw     Table containing forces for Van der
+         *                            Waals (may be NULL)
+         * \param[in] table_length    Length of input tables.
+         * \param[in] table_dx        Spacing between data points in
+         *                            input tables.
+         */
+        LongRangeInteractionTables(int                               type_coul,
+                                   double                            ewaldcoeff_coul,
+                                   double                            r_coul,
+                                   double                           *params_coul,
+                                   interaction_potential_function    v_coul,
+                                   const double                     *table_v_coul,
+                                   const double                     *table_f_coul,
+                                   int                               type_vdw,
+                                   double                            ewaldcoeff_vdw,
+                                   double                            r_vdw,
+                                   double                           *params_vdw,
+                                   interaction_potential_function    v_vdw,
+                                   const double                     *table_v_vdw,
+                                   const double                     *table_f_vdw,
+                                   unsigned int                      table_length,
+                                   double                            table_dx);
+
+        /*! \brief Copy Constructor since Jenkins wants one */
+        LongRangeInteractionTables(const LongRangeInteractionTables &L);
+        /*! \brief Return table spacing (nm) */
+        double tabSpacing() const { return tabSpacing_; }
+        /*! \brief Return number of points in the table */
+        unsigned int tabSize() const { return tabSize_; }
+        //! Return the Coulomb force + energy table
+        const std::vector<double> &coulombFDV0() const { return tableCoulomb_->tableFDV0(); }
+        //! Return the Van der Waals force + energy table
+        const std::vector<double> &vdwFDV0() const { return tableVdw_->tableFDV0(); }
+    private:
+        /*! \brief Internal function to compute tabSpacing_ and tabLength_
+         *
+         * \param[in] type_coul       The electrostatics type
+         * \param[in] ewaldcoeff_coul The coefficient for Coulomb
+         * \param[in] r_coul          The Coulomb cut-off (switching distance)
+         * \param[in] type_vdw        The Van der Waals type
+         * \param[in] ewaldcoeff_vdw  The coefficient for Van der Waals
+         * \param[in] r_vdw           The Van der Waals cut-off (switching distance)
+         */
+        void initTabScaleLength(int    type_coul,
+                                double ewaldcoeff_coul,
+                                double r_coul,
+                                int    type_vdw,
+                                double ewaldcoeff_vdw,
+                                double r_vdw);
+        //! Table scaling (nm)
+        double              tabSpacing_;
+        //! Table size in number of points
+        unsigned int        tabSize_;
+        /*! \brief Combined array class for Coulomb
+         *
+         * Coulomb force+energy table, size of array is 4 times longer
+         * than the other tables.
+         * Quadruplets are: F[i], F[i+1]-F[i], V[i], 0,
+         * this is used with single precision x86 SIMD for aligned loads
+         */
+        SplineTable         *tableCoulomb_;
+
+        /*! \brief Combined array class for Van der Waals
+         *
+         * Vdw force+energy table for LJ-PME, size of array is 4 times longer
+         * than the other tables.
+         * Quadruplets are: F[i], F[i+1]-F[i], V[i], 0, this is used with
+         * single precision x86 SIMD for aligned loads
+         */
+        SplineTable         *tableVdw_;
+};
+
+}      // namespace gmx
+
+#endif /* GMX_TABLES_TABLES_H */
---
+++ src/gromacs/tables/splinetable.h
@@ -0,0 +1,108 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2012,2014,2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#ifndef GMX_TABLES_SPLINETABLE_H
+#define GMX_TABLES_SPLINETABLE_H
+
+#include <cstdlib>
+
+#include <vector>
+
+namespace gmx
+{
+/*! \libinternal\brief Class containing one spline interaction table.
+ *
+ * This class generates input for quadratic spline table interpolation
+ * from input tables.
+ * If both energy and force tables are input the number of output
+ * points is equal to the input.
+ * When no force table input is passed the number of points in the
+ * table is halved because an extra interpolation is needed.
+ * In general it is more accurate to provide both energy and force tables.
+ *
+ * Functions using the tables should use methods tabSpacing() and tabSize()
+ * to get the actual spacing and number of points.
+ *
+ * Note that the table input should be smooth, e.g. not containing noise
+ * from an (iterative) Boltzmann inversion procedure.
+ *
+ * The class implements completely separate tables for the energy
+ * and force for both Coulomb and Van der Waals and a combined
+ * table which is more efficient to evaluate in the mdrun kernels.
+ */
+class SplineTable
+{
+    public:
+        /*! \brief Constructor
+         *
+         * \param[in] ntable     Number of data points in the table
+         * \param[in] dx         Space between data points
+         * \param[in] table_v    Table containing energies
+         * \param[in] table_f    Table containing forces (may be NULL)
+         */
+        SplineTable(unsigned int  ntable,
+                    double        dx,
+                    const double *table_v,
+                    const double *table_f);
+
+        /*! \brief Return the force at a distance r */
+        double force(double r);
+
+        /*! \brief Return the energy at a distance r */
+        double energy(double r);
+
+        /*! \brief Return table spacing (nm) */
+        double tabSpacing() const { return tabSpacing_; }
+
+        /*! \brief Return number of points in the table */
+        unsigned int tabSize() const { return tableFDV0_.size()/4; }
+
+        //! Return the force + energy table
+        const std::vector<double> &tableFDV0() const { return tableFDV0_; }
+    private:
+        //! Table scaling (nm)
+        double              tabSpacing_;
+        /*! \brief Combined array for force and energy
+         *
+         * Force+energy table, size of array is 4 times longer
+         * than the other tables.
+         * Quadruplets are: F[i], F[i+1]-F[i], V[i], 0,
+         * this is used with single precision x86 SIMD for aligned loads
+         */
+        std::vector<double> tableFDV0_;
+};
+
+}      // namespace gmx
+
+#endif /* GMX_TABLES_TABLES_H */
