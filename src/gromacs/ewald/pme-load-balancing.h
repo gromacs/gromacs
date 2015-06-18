0cd5dfa525c65161b0d173278227d4d632a0656a
Berk Hess <hess@kth.se>
2015-06-18 10:46:53 +0200

    WIP: Quadratic spline table can read from table
    This generalizes the quadratic spline table generation function.
    It can now take its input not only from an analytical function,
    but also from a table with potential values and optionally force
    values. Currently this new functionally is not used.
    
    The routine is converted to C++ and a few tests have been added.
    A new InteractionTables class is introduced and passed around
    leading to that a few more c files are converted to C++.
    
    Change-Id: I41aaf1f7c7994888967be2f40f51d548b4e69169

--- src/gromacs/ewald/pme-load-balancing.h
+++ src/gromacs/ewald/pme-load-balancing.h
@@ -51,6 +51,7 @@
 #include "gromacs/legacyheaders/types/inputrec.h"
 #include "gromacs/legacyheaders/types/interaction_const.h"
 #include "gromacs/legacyheaders/types/state.h"
+#include "gromacs/tables/tables.h"
 #include "gromacs/timing/wallcycle.h"
 
 /*! \brief Object to manage PME load balancing */
@@ -84,17 +85,18 @@ void pme_loadbal_init(pme_load_balancing_t     **pme_lb_p,
  * Should be called after the ewcSTEP cycle counter has been stopped.
  * Returns if the load balancing is printing to fp_err.
  */
-void pme_loadbal_do(pme_load_balancing_t *pme_lb,
-                    t_commrec            *cr,
-                    FILE                 *fp_err,
-                    FILE                 *fp_log,
-                    t_inputrec           *ir,
-                    t_forcerec           *fr,
-                    t_state              *state,
-                    gmx_wallcycle_t       wcycle,
-                    gmx_int64_t           step,
-                    gmx_int64_t           step_rel,
-                    gmx_bool             *bPrinting);
+void pme_loadbal_do(pme_load_balancing_t   *pme_lb,
+                    t_commrec              *cr,
+                    FILE                   *fp_err,
+                    FILE                   *fp_log,
+                    t_inputrec             *ir,
+                    t_forcerec             *fr,
+                    t_state                *state,
+                    gmx::InteractionTables *interaction_tables,
+                    gmx_wallcycle_t         wcycle,
+                    gmx_int64_t             step,
+                    gmx_int64_t             step_rel,
+                    gmx_bool               *bPrinting);
 
 /*! \brief Finish the PME load balancing and print the settings when fplog!=NULL */
 void pme_loadbal_done(pme_load_balancing_t *pme_lb,
