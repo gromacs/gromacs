28dfa15c7ea92a511ce6eca9543b2fc9d22e68ab
Szilard Pall <pall.szilard@gmail.com>
2015-06-21 01:23:44 +0200

    avoid CPU oversubscription with GPUs
    If the user specifies the number of OpenMP threads on the command line N
    such that N*#GPU > #hardware threads, mdrun will determine the number of
    thread-MPI ranks to start based on the number of GPUs without taking
    into account the total number of threads that such a run would use. This
    will result in CPU oversubscription.
    
    This change will reduce the number of tMPI threads started to prevent
    oversubscription.
    
    Fixes #1338
    
    Change-Id: I6bceaa2230686fee2e410caf8ae97c43847a5c28

--- src/programs/mdrun/resource-division.cpp
+++ src/programs/mdrun/resource-division.cpp
@@ -192,6 +192,19 @@ static int get_tmpi_omp_thread_division(const gmx_hw_info_t *hwinfo,
     if (ngpu > 0)
     {
         nrank = ngpu;
+
+        /* When the user sets nthreads_omp, we can end up oversubscribing CPU cores
+         * if we simply start as many ranks as GPUs. To avoid this, we start as few
+         * tMPI ranks as necessary to avoid oversubscription and instead leave GPUs idle.
+         * If the user does not set the number of OpenMP threads, nthreads_omp==0 and
+         * this code has no effect.
+         */
+        GMX_RELEASE_ASSERT(hw_opt->nthreads_omp >= 0, "nthreads_omp is negative, but previous checks should have prevented this");
+        while (nrank*hw_opt->nthreads_omp > hwinfo->nthreads_hw_avail && nrank > 1)
+        {
+            nrank--;
+        }
+
         if (nthreads_tot < nrank)
         {
             /* #thread < #gpu is very unlikely, but if so: waste gpu(s) */
@@ -627,7 +640,13 @@ void check_and_update_hw_opt_1(gmx_hw_opt_t    *hw_opt,
 {
     /* Currently hw_opt only contains default settings or settings supplied
      * by the user on the command line.
-     * Check for OpenMP settings stored in environment variables, which can
+     */
+    if (hw_opt->nthreads_omp < 0)
+    {
+        gmx_fatal(FARGS, "The number of OpenMP threads supplied on the command line is %d, which is negative and not allowed", hw_opt->nthreads_omp);
+    }
+
+    /* Check for OpenMP settings stored in environment variables, which can
      * potentially be different on different MPI ranks.
      */
     gmx_omp_nthreads_read_env(&hw_opt->nthreads_omp, SIMMASTER(cr));
