91f3405aca6722d8203218576aceb905f2741c0d
Erik Lindahl <erik@kth.se>
2015-06-22 08:18:53 -0400

    Fix binary exact continuation for trajectories
    The initial call to compute_globals() after
    continuation would remove COM motion, which meant
    trajectories would not be exactly (binary)
    identical to a single trajectori - it was likely
    introduced with the velocity verlet code.
    
    Refs #1342.
    
    Change-Id: I061648774c7625810a8b693c631c61ae377ba297

--- src/kernel/md.c
+++ src/kernel/md.c
@@ -619,9 +619,15 @@ double do_md(FILE *fplog, t_commrec *cr, int nfile, const t_filenm fnm[],
         nstfep = gmx_greatest_common_divisor(repl_ex_nst,nstfep);
     }
 
-    /* I'm assuming we need global communication the first time! MRS */
+    /* Be REALLY careful about what flags you set here. You CANNOT assume
+     * this is the first step, since we might be restarting from a checkpoint,
+     * and in that case we are likely at an odd step where we should not
+     * be doing any comm removal or constraints.
+     */
+    bStopCM = (ir->comm_mode != ecmNO && do_per_step(ir->init_step, ir->nstcomm));
+
     cglo_flags = (CGLO_TEMPERATURE | CGLO_GSTAT
-                  | ((ir->comm_mode != ecmNO) ? CGLO_STOPCM : 0)
+                  | (bStopCM ? CGLO_STOPCM : 0)
                   | (bVV ? CGLO_PRESSURE : 0)
                   | (bVV ? CGLO_CONSTRAINT : 0)
                   | (bRerunMD ? CGLO_RERUNMD : 0)
