d965f37af7ca25b2823965d1bf432fddcd514da5
Peter Kasson <kasson@gmail.com>
2015-06-22 17:32:26 -0400

    Prepend pullx_ and pullf_ to pull files when -deffnm used.
    Changes -deffnm behavior for pulling so that the pullx and pullf
    files don't collide. Previously, this resulted in one being
    backed up and checkpoint restarts failing when -deffnm was used.
    
    Additionally return fatal error if -px or -pf set and output files
    collide.
    
    Fix is now localized to the pull code.
    
    Fixes #942.
    
    Change-Id: I27b8b4ced0f307905e2c2ea4fb260376dd25dc32

--- src/gromacs/pulling/pull.cpp
+++ src/gromacs/pulling/pull.cpp
@@ -1867,6 +1867,39 @@ init_pull(FILE *fplog, const pull_params_t *pull_params, const t_inputrec *ir,
     pull->out_f = NULL;
     if (bOutFile)
     {
+        /* Check for px and pf filename collision */
+        std::string px_filename, pf_filename;
+        std::string px_prepended = std::string("pullx_");
+        std::string pf_prepended = std::string("pullf_");
+        try
+        {
+            px_filename  = std::string(opt2fn("-px", nfile, fnm));
+            pf_filename  = std::string(opt2fn("-pf", nfile, fnm));
+        }
+        GMX_CATCH_ALL_AND_EXIT_WITH_FATAL_ERROR;
+
+
+        if (pull->params.nstxout && pull->params.nstfout &&
+            !opt2bSet("-px", nfile, fnm) && !opt2bSet("-pf", nfile, fnm) &&
+            px_filename == pf_filename)
+        {
+            /* We are writing both pull files but neither is set directly. */
+            fprintf(stderr, "Prepending to prevent pull file duplication\n");
+            px_prepended += px_filename;
+            pf_prepended += pf_filename;
+            pull->out_x   = open_pull_out(px_prepended.c_str(), pull, oenv,
+                                          TRUE, Flags);
+            pull->out_f = open_pull_out(pf_prepended.c_str(), pull, oenv,
+                                        FALSE, Flags);
+            return pull;
+        }
+        else if (pull->params.nstxout && pull->params.nstfout &&
+                 (px_filename == pf_filename))
+        {
+            /* If one of -px and -pf is set but the filenames are identical: */
+            gmx_fatal(FARGS, "Identical pull_x and pull_f output filenames %s",
+                      px_filename.c_str());
+        }
         if (pull->params.nstxout > 0)
         {
             pull->out_x = open_pull_out(opt2fn("-px", nfile, fnm), pull, oenv,
