1227f60ee4ab6f3c8af0bc96e5f9e57679d9df23
Mark Abraham <mark.j.abraham@gmail.com>
2015-06-25 22:46:51 +0200

    Fix all use of nbnxn_simd.h
    Added nbnxn_simd.h to the set of SIMD headers for which we do
    per-commit testing for correct use.
    
    Fixed a bunch of files that were acquiring the dependency
    transitively, which is error-prone.
    
    Change-Id: Ic8fb462c7790a5e723f901700dd06fd09543e58a

--- src/gromacs/mdlib/nbnxn_kernels/simd_4xn/nbnxn_kernel_ElecQSTab_VdwLJEwCombGeom_VF_4xn.c
+++ src/gromacs/mdlib/nbnxn_kernels/simd_4xn/nbnxn_kernel_ElecQSTab_VdwLJEwCombGeom_VF_4xn.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -44,6 +44,8 @@
  * function definition when the kernel will never be called.
  */
 #include "gmxpre.h"
+
+#include "gromacs/mdlib/nbnxn_simd.h"
 
 #define GMX_SIMD_J_UNROLL_SIZE 1
 #include "gromacs/mdlib/nbnxn_kernels/simd_4xn/nbnxn_kernel_simd_4xn.h"
