c1da1c9bdd3b1e43aa2349265e07d6bc7d088d31
Berk Hess <hess@kth.se>
2015-07-02 16:03:34 +0200

    Fix DD DLB state issue
    The introduction of DLB locking for PME load balancing added another
    DLB state, which was stored in a third variable. These variables
    were not always all properly checked. Simplified the code by merging
    these three state variables into one. In added there was a fourth
    variable (bGridJump) is gmx_domdec_t, this is replaced by calls to
    a functions returning is DLB is on.
    
    Refs #1760.
    
    Change-Id: I80d499149e4e5bfd689e76208384a8ba61e2842a

--- src/gromacs/mdlib/nsgrid.c
+++ src/gromacs/mdlib/nsgrid.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -247,7 +247,7 @@ static void set_grid_sizes(matrix box, rvec izones_x0, rvec izones_x1, real rlis
              * direction has uniform DD cell boundaries.
              */
             bDDRect = !(ddbox->tric_dir[i] ||
-                        (dd->bGridJump && i != dd->dim[0]));
+                        (dd_dlb_is_on(dd) && i != dd->dim[0]));
 
             radd = rlist;
             if (i >= ddbox->npbcdim &&
