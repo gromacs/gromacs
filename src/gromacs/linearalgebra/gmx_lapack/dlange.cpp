eb125f0c855ed9096eac4286241db99776f19018
Erik Lindahl <erik@kth.se>
2015-07-08 22:20:42 +0200

    Moved linear algebra sources to C++
    Introduced std:: prefix for math, ctype and string functions. BLAS/LAPACK
    source have also been updated and fortran-style checks for NaN replaced
    with std::isnan(), and unused variables removed. The only reason for
    moving BLAS/LAPACK to C++ is that it will eventually help us avoid the C
    compiler entirely in Gromacs.
    
    Change-Id: I5432c22228d4df385edad9076f88c8f14a0c94c6

---
+++ src/gromacs/linearalgebra/gmx_lapack/dlange.cpp
@@ -0,0 +1,71 @@
+#include <cctype>
+#include <cmath>
+#include "../gmx_lapack.h"
+
+
+double
+F77_FUNC(dlange,DLANGE)(const char *norm,
+	int *m,
+	int *n,
+	double *a,
+	int *lda,
+	double *work)
+{
+  const char ch=std::toupper(*norm);
+  double dtemp,sum,max,val,scale;
+  int i,j;
+
+  switch(ch) {
+  case 'M':
+    max = 0.0;
+    for(j=0;j<*n;j++)
+      for(i=0;i<*m;i++) {
+	dtemp = std::abs(a[j*(*lda)+i]);
+	if(dtemp>max)
+	  max = dtemp;
+      }
+    val = max;
+    break;
+
+  case 'O':
+  case '1':
+    max = 0.0;
+    for(j=0;j<*n;j++) {
+      sum = 0.0;
+      for(i=0;i<*m;i++) 
+	sum += std::abs(a[j*(*lda)+i]);
+      if(sum>max)
+	max = sum;
+    }
+    val = max;
+    break;
+
+  case 'I':
+    for(i=0;i<*m;i++)
+      work[i] = 0.0;
+    for(j=0;j<*n;j++)
+      for(i=0;i<*m;i++)
+	work[i] += std::abs(a[j*(*lda)+i]);
+    max = 0;
+    for(i=0;i<*m;i++)
+      if(work[i]>max)
+	max=work[i];
+    val = max;
+    break;
+
+  case 'F':
+  case 'E':
+    scale = 0.0;
+    sum   = 1.0;
+    i = 1;
+    for(j=0;j<*n;j++) 
+      F77_FUNC(dlassq,DLASSQ)(m,&(a[j*(*lda)+0]),&i,&scale,&sum);
+    val = scale* std::sqrt(sum);
+    break;
+
+  default:
+    val = 0.0;
+    break;
+  }
+  return val;
+}
