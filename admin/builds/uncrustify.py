ea5242ffcef441b8fa610c67a135b628b56508d6
Teemu Murtola <teemu.murtola@gmail.com>
2015-07-10 19:47:48 +0300

    Uncrustify build with new releng script
    Also, make uncrustify.sh --warnings= flag work properly with absolute
    path names.
    
    Change-Id: I23ad807183e2d9b5b03f0a5c1bd2b9f5cd6992fc

---
+++ admin/builds/uncrustify.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+def do_build(context):
+    os.environ['UNCRUSTIFY'] = context.env.get_uncrustify_command()
+    # TODO: Change uncrustify.sh such that it allows providing a full path for
+    # this file.
+    warning_log = 'uncrustify.log'
+    cmd = ['admin/uncrustify.sh', 'check', '--rev=HEAD^', '--warnings=' + warning_log]
+    ret = subprocess.call(cmd)
+    if ret == 1:
+        context.publish_logs([warning_log], category='uncrustify')
+        with open(warning_log, 'r') as f:
+            warnings = f.readlines()
+        if len(warnings) <= 5:
+            details = [x.rstrip() for x in warnings]
+        else:
+            uncrustify_count = 0
+            cpyear_count = 0
+            cpheader_count = 0
+            for w in warnings:
+                if 'needs uncrustify' in w:
+                    uncrustify_count += 1
+                if 'copyright year' in w:
+                    cpyear_count += 1
+                if 'copyright header' in w:
+                    cpheader_count += 1
+            details = []
+            if uncrustify_count > 0:
+                details.append('formatting issues in {0} files'.format(uncrustify_count))
+            if cpyear_count > 0:
+                details.append('copyright year missing in {0} files'.format(cpyear_count))
+            if cpheader_count > 0:
+                details.append('copyright header issues in {0} files'.format(cpheader_count))
+        context.mark_unstable(reason='uncrustify.sh found issues', details=details)
+    elif ret != 0:
+        raise BuildError('uncrustify.sh failed to run')
