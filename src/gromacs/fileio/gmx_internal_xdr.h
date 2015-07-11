fd06020921530b7fbff49daa870d33148458e82e
Erik Lindahl <erik@kth.se>
2015-07-11 22:51:14 +0200

    Move fileio sources to C++
    String functions have been moved into std namespace,
    unused variables removed. The internal XDR implementation has
    been modified to work in C++, and while doing so it has been
    renamed to gmx_internal_xdr.cpp to avoid confusion with the
    external system implementations.
    
    Change-Id: Iadaf812da88ba24530b637b8e3259fd3cac417e0

--- src/gromacs/fileio/gmx_internal_xdr.h
+++ src/gromacs/fileio/gmx_internal_xdr.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -38,10 +38,9 @@
 #ifndef GMX_FILEIO_GMX_SYSTEM_XDR_H
 #define GMX_FILEIO_GMX_SYSTEM_XDR_H
 
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
 
 /*
  * This header file is ONLY used on windows systems, since these do
