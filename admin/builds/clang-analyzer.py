0d569aad6f7af0aa756f21f931728566f21cc39e
Teemu Murtola <teemu.murtola@gmail.com>
2015-07-12 07:05:02 +0300

    Use releng for clang static analyzer
    As with cppcheck, by itself this does not add any functionality, but
    makes it easier to do so in the future.
    
    Change-Id: I396305cc47d45514d751b5aec84c4aaadbd6571c

---
+++ admin/builds/clang-analyzer.py
@@ -0,0 +1,50 @@
+#
+# This file is part of the GROMACS molecular simulation package.
+#
+# Copyright (c) 2015, by the GROMACS development team, led by
+# Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+# and including many others, as listed in the AUTHORS file in the
+# top-level source directory and at http://www.gromacs.org.
+#
+# GROMACS is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# GROMACS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with GROMACS; if not, see
+# http://www.gnu.org/licenses, or write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+#
+# If you want to redistribute modifications to GROMACS, please
+# consider that scientific software is very special. Version
+# control is crucial - bugs must be traceable. We will be happy to
+# consider code for inclusion in the official distribution, but
+# derived work must not be called official GROMACS. Details are found
+# in the README & COPYING files - if they are missing, get the
+# official version at http://www.gromacs.org.
+#
+# To help us fund GROMACS development, we humbly ask that you cite
+# the research papers on the package. Check out http://www.gromacs.org.
+
+def do_build(context):
+    cmake_opts = {
+            'CMAKE_BUILD_TYPE': 'Debug',
+            'GMX_GPU': 'OFF',
+            'GMX_OPENMP': 'OFF',
+            'GMX_SIMD': 'None',
+            'GMX_FFT_LIBRARY': 'fftpack'
+        }
+
+    context.env.init_clang_analyzer(clang_version='3.4')
+    context.run_cmake(cmake_opts)
+    # This does not build the tests, so they are not analyzed.
+    # But since this has been the case for at least a year, we probably need to
+    # fix a few issues before turning it back on...
+    context.build_target(target=None)
+    context.process_clang_analyzer_results()
