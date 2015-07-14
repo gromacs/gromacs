b6d3ad6e9a25e85b380e3aac1e0d609e79b5ac7a
James W. Barnett <jbarnet4@tulane.edu>
2015-07-14 18:24:40 -0500

    Fix zsh shell completions.
    zsh completions when gmx prefix was used was broken. Use zsh builtin
    bashcompinit to use the bash completions. Remove 'shopt -s extglob'
    from gmx-completion.bash since it already executed in GMXRC.bash and
    is incompatible with zsh. gmx-completion.bash and gmx-completion-gmx.bash can
    now be used for completions in both bash and zsh.
    
    Change-Id: Ib8f3cf0535d39e91a6b31933f41aa7548755b351

--- src/gromacs/commandline/shellcompletions.cpp
+++ src/gromacs/commandline/shellcompletions.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -218,7 +218,6 @@ File *ShellCompletionWriter::outputFile()
 void ShellCompletionWriter::startCompletions()
 {
     impl_->file_.reset(new File(impl_->binaryName_ + "-completion.bash", "w"));
-    impl_->file_->writeLine("shopt -s extglob");
 }
 
 void ShellCompletionWriter::writeModuleCompletions(
