ebc9e231a66527159a28bc503d1512d774f3f60e
Erik Lindahl <erik@kth.se>
2015-08-12 18:02:42 +0200

    Random engines & distributions as proper C++11 classes
    This change implements the ThreeFry2x64 random engine with flexible
    number of encryption rounds and internal counter bits. The class is
    compatible with the C++11 random number generators, and the GROMACS
    tabulated normal distribution has likewise been turned into a
    random distribution compatible with C++11, meaning they can be used in
    almost any combination with the standard library distributions.
    - The ThreeFry2x64 implementation uses John Salmon's idea of a template-
      selected internal counter so a number of bits are reserved to generate
      an arbitrary random stream. This makes it possible to use ThreeFry as
      a normal random engine, and even in counter mode it is possible to
      draw an arbitrary amount of random numbers before restarting counters.
    - Both accurate (20-round) and fast (13-round) versions are available.
    - There is a gmx::DefaultRandomEngine when we don't care about details.
    - gmx::GammaDistribution has been added to work around bugs in
      libstdc++-4.4.7 headers, and to avoid getting different results
      for libstdc++ vs. libc++.
    - Custom Uniform, normal, and exponential distributions have been added
      to make all results reproducible across platforms since stdlibc++ and
      libc++ do not use the same generating algorithms.
    - Code using random numbers has been updated, but no changes have been
      made to turn random seeds into 64bits yet.
    
    Change-Id: I47a04d03e2f264e1a6ef0aa0a2174cb464ed9af7

---
+++ src/gromacs/random/tests/uniformrealdistribution.cpp
@@ -0,0 +1,139 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief Tests for GROMACS uniform real distribution
+ *
+ * \author Erik Lindahl <erik.lindahl@gmail.com>
+ * \ingroup module_random
+ */
+#include "gmxpre.h"
+
+#include "gromacs/random/uniformrealdistribution.h"
+
+#include <gtest/gtest.h>
+
+#include "gromacs/random/threefry.h"
+
+#include "testutils/refdata.h"
+#include "testutils/testasserts.h"
+
+namespace gmx
+{
+
+namespace
+{
+
+TEST(UniformRealDistributionTest, GenerateCanonical)
+{
+    gmx::test::TestReferenceData        data;
+    gmx::test::TestReferenceChecker     checker(data.rootChecker());
+
+    gmx::ThreeFry2x64<8>                rng(123456, gmx::RandomDomain::Other);
+    std::vector<real>                   result;
+
+    for (int i = 0; i < 10; i++)
+    {
+        result.push_back(gmx::generateCanonical<real, std::numeric_limits<real>::digits>(rng));
+    }
+    checker.checkSequence(result.begin(), result.end(), "GenerateCanonical");
+}
+
+TEST(UniformRealDistributionTest, Output)
+{
+    gmx::test::TestReferenceData            data;
+    gmx::test::TestReferenceChecker         checker(data.rootChecker());
+
+    gmx::ThreeFry2x64<8>                    rng(123456, gmx::RandomDomain::Other);
+    gmx::UniformRealDistribution<real>      dist(1.0, 10.0);
+    std::vector<real>                       result;
+
+    for (int i = 0; i < 10; i++)
+    {
+        result.push_back(dist(rng));
+    }
+    checker.checkSequence(result.begin(), result.end(), "UniformRealDistribution");
+}
+
+TEST(UniformRealDistributionTest, Logical)
+{
+    gmx::ThreeFry2x64<8>                rng(123456, gmx::RandomDomain::Other);
+    gmx::UniformRealDistribution<real>  distA(2.0, 5.0);
+    gmx::UniformRealDistribution<real>  distB(2.0, 5.0);
+    gmx::UniformRealDistribution<real>  distC(3.0, 5.0);
+    gmx::UniformRealDistribution<real>  distD(2.0, 4.0);
+
+    EXPECT_EQ(distA, distB);
+    EXPECT_NE(distA, distC);
+    EXPECT_NE(distA, distD);
+}
+
+
+TEST(UniformRealDistributionTest, Reset)
+{
+    gmx::ThreeFry2x64<8>                           rng(123456, gmx::RandomDomain::Other);
+    gmx::UniformRealDistribution<real>             distA(2.0, 5.0);
+    gmx::UniformRealDistribution<real>             distB(2.0, 5.0);
+    gmx::UniformRealDistribution<>::result_type    valA, valB;
+
+    valA = distA(rng);
+
+    distB(rng);
+    rng.restart();
+    distB.reset();
+
+    valB = distB(rng);
+
+    EXPECT_EQ(valA, valB);
+}
+
+TEST(UniformRealDistributionTest, AltParam)
+{
+    gmx::ThreeFry2x64<8>                            rngA(123456, gmx::RandomDomain::Other);
+    gmx::ThreeFry2x64<8>                            rngB(123456, gmx::RandomDomain::Other);
+    gmx::UniformRealDistribution<real>              distA(2.0, 5.0);
+    gmx::UniformRealDistribution<real>              distB; // default parameters
+    gmx::UniformRealDistribution<real>::param_type  paramA(2.0, 5.0);
+
+    EXPECT_NE(distA(rngA), distB(rngB));
+    rngA.restart();
+    rngB.restart();
+    distA.reset();
+    distB.reset();
+    EXPECT_EQ(distA(rngA), distB(rngB, paramA));
+}
+
+}      // namespace anonymous
+
+}      // namespace gmx
