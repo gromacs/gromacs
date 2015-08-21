9e5a08e1fa72c2bf472ed28d1e66845e35ce5483
J. Baxter <automata@unm.edu>
2015-08-21 14:29:59 -0600

    Created a header file for minimize.cpp
    Rationale:
    I am working on a custom integrator for a personal project.
    It doesn't belong in minimize.cpp because it isn't purely a
    energy minimization method; however, it requires energy and
    force evaluations like the energy minimization integrators.
    I chose to make a header file for minimize.cpp to allow the
    em_state_t struct and non-integrator functions to be used
    beyond the minimize.cpp source file. I have also moved all
    but the integrator functions into the gmx:: namespace.
    
    Changes:
    The em_state_t struct and previously static functions found
    in minimize.cpp (all except do_cg, do_lbfgs, do_steep, and
    do_nm) are now part of the gmx:: namespace, and are exposed
    through minimize.h, a new header file. Calls and variable
    declarations within the do_* integrator functions have been
    prepended with gmx:: as necessary to reflect this change.
    No changes have been made to function logic or behavior.
    
    Question:
    Would it be preferable to place these in a deeper namespace
    such as gmx::minimize::, since there are a few generic
    function names (e.g. warn_step, evaluate_energy)?
    I can make this change if it is desired.
    
    Update, Patch Set 2:
    Applied uncrustify as requested by Jenkins Buildbot.
    
    Update, Patch Set 3:
    Added in the missing include guard for minimize.h,
    moved includes back into minimize.cpp where possible,
    removed from minimize.cpp several redundant includes.
    
    Change-Id: I978e7d1fe36b95c1779c086b02444d8ddfbebea5

---
+++ src/gromacs/mdlib/minimize.h
@@ -0,0 +1,156 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
+ * Copyright (c) 2001-2004, The GROMACS development team.
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+#ifndef GMX_MINIMIZE_H
+#define GMX_MINIMIZE_H
+
+#include "gmxpre.h"
+#include "config.h"
+#include "gromacs/legacyheaders/sim_util.h"
+#include "gromacs/legacyheaders/txtdump.h"
+#include "gromacs/legacyheaders/types/state.h"
+#include "gromacs/legacyheaders/vsite.h"
+#include "gromacs/timing/wallcycle.h"
+#include "gromacs/timing/walltime_accounting.h"
+
+namespace gmx
+{
+
+typedef struct {
+    t_state  s;
+    rvec    *f;
+    real     epot;
+    real     fnorm;
+    real     fmax;
+    int      a_fmax;
+} em_state_t;
+
+em_state_t *init_em_state();
+
+void print_em_start(FILE                     *fplog,
+                    t_commrec                *cr,
+                    gmx_walltime_accounting_t walltime_accounting,
+                    gmx_wallcycle_t           wcycle,
+                    const char               *name);
+
+void em_time_end(gmx_walltime_accounting_t walltime_accounting,
+                 gmx_wallcycle_t           wcycle);
+
+void sp_header(FILE *out, const char *minimizer, real ftol, int nsteps);
+
+void warn_step(FILE *fp, real ftol, gmx_bool bLastStep, gmx_bool bConstrain);
+
+void print_converged(FILE *fp, const char *alg, real ftol,
+                     gmx_int64_t count, gmx_bool bDone, gmx_int64_t nsteps,
+                     real epot, real fmax, int nfmax, real fnorm);
+
+void get_f_norm_max(t_commrec *cr,
+                    t_grpopts *opts, t_mdatoms *mdatoms, rvec *f,
+                    real *fnorm, real *fmax, int *a_fmax);
+
+void get_state_f_norm_max(t_commrec *cr,
+                          t_grpopts *opts, t_mdatoms *mdatoms,
+                          em_state_t *ems);
+
+void init_em(FILE *fplog, const char *title,
+             t_commrec *cr, t_inputrec *ir,
+             t_state *state_global, gmx_mtop_t *top_global,
+             em_state_t *ems, gmx_localtop_t **top,
+             rvec **f, rvec **f_global,
+             t_nrnb *nrnb, rvec mu_tot,
+             t_forcerec *fr, gmx_enerdata_t **enerd,
+             t_graph **graph, t_mdatoms *mdatoms, gmx_global_stat_t *gstat,
+             gmx_vsite_t *vsite, gmx_constr_t constr,
+             int nfile, const t_filenm fnm[],
+             gmx_mdoutf_t *outf, t_mdebin **mdebin,
+             int imdport, unsigned long gmx_unused Flags,
+             gmx_wallcycle_t wcycle);
+
+void finish_em(t_commrec *cr, gmx_mdoutf_t outf,
+               gmx_walltime_accounting_t walltime_accounting,
+               gmx_wallcycle_t wcycle);
+
+void swap_em_state(em_state_t *ems1, em_state_t *ems2);
+
+void copy_em_coords(em_state_t *ems, t_state *state);
+
+void write_em_traj(FILE *fplog, t_commrec *cr,
+                   gmx_mdoutf_t outf,
+                   gmx_bool bX, gmx_bool bF, const char *confout,
+                   gmx_mtop_t *top_global,
+                   t_inputrec *ir, gmx_int64_t step,
+                   em_state_t *state,
+                   t_state *state_global, rvec *f_global);
+
+void do_em_step(t_commrec *cr, t_inputrec *ir, t_mdatoms *md,
+                gmx_bool bMolPBC,
+                em_state_t *ems1, real a, rvec *f, em_state_t *ems2,
+                gmx_constr_t constr, gmx_localtop_t *top,
+                t_nrnb *nrnb, gmx_wallcycle_t wcycle,
+                gmx_int64_t count);
+
+void em_dd_partition_system(FILE *fplog, int step, t_commrec *cr,
+                            gmx_mtop_t *top_global, t_inputrec *ir,
+                            em_state_t *ems, gmx_localtop_t *top,
+                            t_mdatoms *mdatoms, t_forcerec *fr,
+                            gmx_vsite_t *vsite, gmx_constr_t constr,
+                            t_nrnb *nrnb, gmx_wallcycle_t wcycle);
+
+void evaluate_energy(FILE *fplog, t_commrec *cr,
+                     gmx_mtop_t *top_global,
+                     em_state_t *ems, gmx_localtop_t *top,
+                     t_inputrec *inputrec,
+                     t_nrnb *nrnb, gmx_wallcycle_t wcycle,
+                     gmx_global_stat_t gstat,
+                     gmx_vsite_t *vsite, gmx_constr_t constr,
+                     t_fcdata *fcd,
+                     t_graph *graph, t_mdatoms *mdatoms,
+                     t_forcerec *fr, rvec mu_tot,
+                     gmx_enerdata_t *enerd, tensor vir, tensor pres,
+                     gmx_int64_t count, gmx_bool bFirst);
+
+double reorder_partsum(t_commrec *cr, t_grpopts *opts, t_mdatoms *mdatoms,
+                       gmx_mtop_t *mtop,
+                       em_state_t *s_min, em_state_t *s_b);
+
+real pr_beta(t_commrec *cr, t_grpopts *opts, t_mdatoms *mdatoms,
+             gmx_mtop_t *mtop,
+             em_state_t *s_min, em_state_t *s_b);
+
+} /* namespace gmx */
+
+#endif
