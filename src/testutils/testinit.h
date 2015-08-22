c8047a4b982ad532d02ac1a4f67bcc4063d23b21
Teemu Murtola <teemu.murtola@gmail.com>
2015-08-22 17:22:19 +0300

    Disable multiple ranks for non-MPI unit tests
    Make tests not explicitly declared in CMake as supporting MPI only run
    on a single MPI rank.  This is already the case for the 'test' and
    'check' targets, but manually it is possible to run them with multiple
    ranks.  Now all other ranks except the master will exit, and only master
    will run the tests.  This avoids potential race conditions in tests not
    designed for concurrent execution, e.g., related to file system access.
    Some cleanup of the related CMake macros.
    
    Closes #1795
    
    Change-Id: I69e88ba3419cce96eb5b0c7e145643accc65533d

--- src/testutils/testinit.h
+++ src/testutils/testinit.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -60,7 +60,8 @@ namespace test
  *
  * \ingroup module_testutils
  */
-void initTestUtils(const char *dataPath, const char *tempPath, int *argc, char ***argv);
+void initTestUtils(const char *dataPath, const char *tempPath, bool usesMpi,
+                   int *argc, char ***argv);
 
 /*! \internal
  * \brief
