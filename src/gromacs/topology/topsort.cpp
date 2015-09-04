417e65c36316662c708588e56f626f5697024a69
Rossen Apostolov <rossen@kth.se>
2015-09-04 18:17:04 +0200

    Converted topology to C++.
    Removed a few unused variables.
    
    Change-Id: I6185999027137c4c8a29da23b0a24c1447790765

--- src/gromacs/topology/topsort.cpp
+++ src/gromacs/topology/topsort.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2008, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -206,7 +206,6 @@ void gmx_sort_ilist_fe(t_idef *idef, const real *qA, const real *qB)
     t_iparams *iparams;
     t_ilist   *ilist;
     t_iatom   *iatoms;
-    gmx_bool   bPert;
     t_iatom   *iabuf;
     int        iabuf_nalloc;
 
