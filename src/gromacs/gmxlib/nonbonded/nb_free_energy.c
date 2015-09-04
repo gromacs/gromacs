0a940fb8e62dd161bdc038f3cf5f6e67518df683
Mark Abraham <mark.j.abraham@gmail.com>
2015-09-04 15:47:30 +0200

    Hard-code fewer things in table code
    Attempts to extend the table support to extend to the Verlet scheme
    have broken several parts of the code that magically depend on etiNR
    having the value 3. It's not worth fixing e.g. all the group-scheme
    kernels, but some parts of the code that might outlive the group
    scheme are worth some clarifying maintenance.
    
    In principle, this change might make short-range free-energy and 1-4
    kernels run slightly slower, but these are not important enough to
    worry about here, given the broader issues.
    
    Change-Id: Id73a4306e1b9bbe0b419aceafa6d5de89782fb85

--- src/gromacs/gmxlib/nonbonded/nb_free_energy.c
+++ src/gromacs/gmxlib/nonbonded/nb_free_energy.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -335,7 +335,7 @@ gmx_nb_free_energy_kernel(const t_nblist * gmx_restrict    nlist,
         tabscale         = kernel_data->table_elec_vdw->scale;
         VFtab            = kernel_data->table_elec_vdw->data;
         /* we always use the combined table here */
-        tab_elemsize     = 12;
+        tab_elemsize     = kernel_data->table_elec_vdw->stride;
     }
 
     for (n = 0; (n < nri); n++)
