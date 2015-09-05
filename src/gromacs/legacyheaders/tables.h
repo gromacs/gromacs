031209b96b1fe8ff6ca00e37f178df8d3d709806
Alfredo Metere <alfredometere2@gmail.com>
2015-09-05 19:58:58 +0200

    CUDA USER tabulated potential Take 4.21
    All columns of table.xvg can now be read.
    Minor and major bugs fixed.
    
    The table.xvg file must be of 9 columns.
    The first 7 columns are read according to the manual p.177
    The additional 2 columns are generic tables.
    It means that they are used as they are by mdrun, with
    no further modification.
    They represent the potential V(r) and the force modulus F(r)
    with r being the interparticle distance.
    
    To use generic tables, GMX_NO_TABLE_COEFFS environment
    variable must be set.
    Example:
    foo@bar$ GMX_NO_TABLE_COEFFS=1 mdrun -v ...
    
    1) Fixed enums in tables.c and put an intuitive name to them.
    2) Fixed passing the wrong force value to quadratic spline
       interpolation function.
    3) Fixed force calculation.
       (used F_invr += ... instead of F_invr = ...)
    4) Now tables.c make_tables_Verlet() reads the tables into
       an array declared and allocated as 'double' instead of 'real'.
    5) Fixed a plus-for-a-minus sign typo in the quadratic spline interpolation
       functions. Now it works beautifully.
    6) Introduced an gmx_fatal for attempting to use tables with less or more
       than 1000 points with the Verlet cutoff-scheme and CUDA tables.
    
    Test files can be downloaded here:
    https://www.dropbox.com/s/o1bs4bqtarncm3c/testfiles.tar?dl=0
    
    Change-Id: Ie9ccc2e096cc4cdc3580d1a334b484d0a2a36c5d

--- src/gromacs/legacyheaders/tables.h
+++ src/gromacs/legacyheaders/tables.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2014, by the GROMACS development team, led by
+ * Copyright (c) 2012,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -45,18 +45,27 @@ extern "C" {
 }
 #endif
 
-typedef double (*real_space_grid_contribution_computer)(double, double);
+typedef double (*interaction_potential_function)(const double *, double);
 /* Function pointer used to tell table_spline3_fill_ewald_lr whether it
  * should calculate the grid contribution for electrostatics or LJ.
  */
 
-void table_spline3_fill_ewald_lr(real                                 *table_F,
-                                 real                                 *table_V,
-                                 real                                 *table_FDV0,
-                                 int                                   ntab,
-                                 double                                dx,
-                                 real                                  beta,
-                                 real_space_grid_contribution_computer v_lr);
+void table_spline3_fill_ewald_lr(real                           *table_F,
+                                 real                           *table_V,
+                                 real                           *table_FDV0,
+                                 int                             ntab,
+                                 double                          dx,
+                                 real                            beta,
+                                 interaction_potential_function  v_lr);
+
+void table_spline3_fill_Verlet(real                               *table_F,
+                               real                               *table_V,
+                               real                               *table_FDV0,
+                               int                                 ntab,
+                               double                              dx,
+                               real                               *table_in_f,
+                               real                               *table_in_v,
+                               int                                 table_in_size);
 /* Fill tables of ntab points with spacing dr with the ewald long-range
  * (mesh) force.
  * There are three separate tables with format FDV0, F, and V.
@@ -65,14 +74,34 @@ void table_spline3_fill_ewald_lr(real                                 *table_F,
  * The force can then be interpolated linearly.
  */
 
+
+static void table_spline3_fill(real                           *table_f,
+                               real                           *table_v,
+                               real                           *table_fdv0,
+                               int                             ntab,
+                               double                          dx,
+                               interaction_potential_function  v_ana,
+                               const double                   *params,
+                               const double                   *table_in_f,
+                               const double                   *table_in_v,
+                               int                             table_in_size,
+                               int                             stride);
+
+
 real ewald_spline3_table_scale(const interaction_const_t *ic);
 /* Return the scaling for the Ewald quadratic spline tables. */
 
-double v_q_ewald_lr(double beta, double r);
+real nb_spline3_table_scale(const interaction_const_t *i, real rtab);
+/* Generic scaling function for User quadratic spline tables */
+
+double v_q_ewald_lr(const double *beta, double r);
 /* Return the real space grid contribution for Ewald*/
 
-double v_lj_ewald_lr(double beta, double r);
+double v_lj_ewald_lr(const double *beta, double r);
 /* Return the real space grid contribution for LJ-Ewald*/
+
+double v_user(const double *beta, double r);
+
 
 #ifdef __cplusplus
 }
