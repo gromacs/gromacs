a02c6384830cbc3f8fd79e94edb610c18c695e4f
Teemu Murtola <teemu.murtola@gmail.com>
2015-09-06 07:19:35 +0300

    [WIP] Build script for coverage
    Logic is for now mostly the same as in the combination of the old
    coverage builds, except that the build is always done with
    GMX_SIMD=None so that build slave properties should not affect the
    results.
    
    Change-Id: Idd59cb25b46b0ed2b26dedb3f143d6c336f0d9e1

---
+++ admin/builds/coverage.py
@@ -0,0 +1,66 @@
+#
+# This file is part of the GROMACS molecular simulation package.
+#
+# Copyright (c) 2015, by the GROMACS development team, led by
+# Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+# and including many others, as listed in the AUTHORS file in the
+# top-level source directory and at http://www.gromacs.org.
+#
+# GROMACS is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# GROMACS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with GROMACS; if not, see
+# http://www.gnu.org/licenses, or write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+#
+# If you want to redistribute modifications to GROMACS, please
+# consider that scientific software is very special. Version
+# control is crucial - bugs must be traceable. We will be happy to
+# consider code for inclusion in the official distribution, but
+# derived work must not be called official GROMACS. Details are found
+# in the README & COPYING files - if they are missing, get the
+# official version at http://www.gromacs.org.
+#
+# To help us fund GROMACS development, we humbly ask that you cite
+# the research papers on the package. Check out http://www.gromacs.org.
+
+import os
+
+extra_projects = [Project.REGRESSIONTESTS]
+
+def do_build(context):
+    context.env.init_gcc(version='4.6')
+
+    cmake_opts=dict()
+    cmake_opts['CMAKE_BUILD_TYPE'] = 'Debug'
+    cmake_opts['GMX_BUILD_FOR_COVERAGE'] = 'ON'
+    cmake_opts['CMAKE_C_FLAGS'] = '--coverage -g'
+    cmake_opts['CMAKE_CXX_FLAGS'] = '--coverage -g'
+    cmake_opts['GMX_GPU'] = 'OFF'
+    # Ideally, this would be Reference, but running the regression tests is way
+    # too slow that way...
+    cmake_opts['GMX_SIMD'] = 'None'
+
+    os.environ['GMX_NO_TERM'] = '1'
+
+    context.run_cmake(cmake_opts)
+    context.build_target(target=None)
+    context.build_target(target='tests')
+
+    context.run_ctest(args=['--output-on-failure'])
+
+    gromacs_dir = context.workspace.get_project_dir(Project.GROMACS)
+    os.environ['PATH'] += os.path.pathsep + os.path.join(gromacs_dir, 'bin')
+    os.chdir(context.workspace.get_project_dir(Project.REGRESSIONTESTS))
+    cmd = ['perl', 'gmxtest.pl', '-xml', 'all', '-nt', '2']
+    context.run_cmd_with_env(cmd, failure_message='Regression tests failed to execute')
+
+    context.process_coverage_results(exclude=['^src/external/'])
