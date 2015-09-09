9c0d348ce2c9c9fbb17b20ca1c890e89dd7ebc13
Roland Schulz <roland@utk.edu>
2015-09-09 12:23:32 -0400

    Bump required CUDA version
    Change-Id: I937dc0bdf8b6663fb89998eb750d3759620486e5

--- src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda.cu
+++ src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda.cu
@@ -289,15 +289,12 @@ static inline int calc_shmem_required(const int num_threads_z, gmx_device_info_t
     shmem  = NCL_PER_SUPERCL * CL_SIZE * sizeof(float4);
     /* cj in shared memory, for each warp separately */
     shmem += num_threads_z * 2 * NBNXN_GPU_JGROUP_SIZE * sizeof(int);
-    /* CUDA versions below 4.2 won't generate code for sm>=3.0 */
-#if GMX_CUDA_VERSION >= 4200
     if (dinfo->prop.major >= 3)
     {
         /* i-atom types in shared memory */
         shmem += NCL_PER_SUPERCL * CL_SIZE * sizeof(int);
     }
     if (dinfo->prop.major < 3)
-#endif
     {
         /* force reduction buffers in shared memory */
         shmem += CL_SIZE * CL_SIZE * 3 * sizeof(float);
