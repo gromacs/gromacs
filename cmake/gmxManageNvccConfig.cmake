9cfd530391969e2178820b59c6f5d30865660492
Roland Schulz <roland@utk.edu>
2015-09-09 16:01:36 -0400

    Manual propagate CUDA host flags
    Required to remove C++11 flag.
    
    Fixes #1390
    
    Change-Id: I857a4c441e0b3c6b72ac776338d48e165830f1a8

--- cmake/gmxManageNvccConfig.cmake
+++ cmake/gmxManageNvccConfig.cmake
@@ -212,5 +212,21 @@ if (CMAKE_VERSION VERSION_LESS "2.8.10")
 endif()
 list(APPEND GMX_CUDA_NVCC_FLAGS "${CUDA_HOST_COMPILER_OPTIONS}")
 
+# When CUDA 6.5 is required we should use C++11 also for CUDA and also propagate
+# the C++11 flag to CUDA. Then we can use the solution implemented in FindCUDA
+# (starting with 3.3 - can be backported). For now we need to remove the C++11
+# flag which means we need to manually propagate all other flags.
+string(REGEX REPLACE "[-]+std=c\\+\\+0x" "" _CMAKE_CXX_FLAGS_NOCXX11 "${CMAKE_CXX_FLAGS}")
+
 # finally set the damn flags
-set(CUDA_NVCC_FLAGS "${GMX_CUDA_NVCC_FLAGS}" CACHE STRING "Compiler flags for nvcc." FORCE)
+if(CUDA_PROPAGATE_HOST_FLAGS)
+    set(CUDA_PROPAGATE_HOST_FLAGS OFF)
+    string(REPLACE " " "," _flags "${_CMAKE_CXX_FLAGS_NOCXX11}")
+    set(CUDA_NVCC_FLAGS "${GMX_CUDA_NVCC_FLAGS};-Xcompiler;${_flags}" CACHE STRING "Compiler flags for nvcc." FORCE)
+    foreach(_config ${CMAKE_CONFIGURATION_TYPES})
+        string(REPLACE " " "," _flags "${CMAKE_CXX_FLAGS_${_config}}")
+        set(CUDA_NVCC_FLAGS_${_config} "-Xcompiler;${_flags}" CACHE STRING "Compiler flags for nvcc." FORCE)
+    endforeach()
+else()
+    set(CUDA_NVCC_FLAGS "${GMX_CUDA_NVCC_FLAGS}" CACHE STRING "Compiler flags for nvcc." FORCE)
+endif()
