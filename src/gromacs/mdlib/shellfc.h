b93b0264bad2a9c1d99fe527f9424047d7c5a5e8
David van der Spoel <spoel@xray.bmc.uu.se>
2015-09-30 20:06:13 +0200

    Made normal modes work with shells and vsites
    Implement shells and vsites in normal mode analysis (do_nm routine)
    and analysis of eigenvalues and frequencies. The normal mode analysis
    is done on real atoms only and the shells are minimized at each step
    of the analysis.
    
    Fixes #879
    
    Change-Id: Iac127f51ef4ebd246c9d2417e039778b5952122f

--- src/gromacs/mdlib/shellfc.h
+++ src/gromacs/mdlib/shellfc.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2008, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -58,8 +58,7 @@ struct t_state;
 gmx_shellfc_t *init_shell_flexcon(FILE *fplog,
                                   gmx_mtop_t *mtop, int nflexcon,
                                   int nstcalcenergy,
-                                  bool usingDomainDecomposition,
-                                  bool doingNormalModeAnalysis);
+                                  bool usingDomainDecomposition);
 
 /* Get the local shell with domain decomposition */
 void make_local_shells(t_commrec *cr, t_mdatoms *md,
