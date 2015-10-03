8a4d326e30985ac36be0f022c53413b7a2044084
Teemu Murtola <teemu.murtola@gmail.com>
2015-10-03 07:12:14 +0300

    Support replacing solvent in insert-molecules
    Make it possible to specify the solvent (or other set of atoms) with
    -replace (as a selection) for gmx insert-molecules, and make the tool
    replace residues from this set with the inserted molecules, instead of
    not inserting there.  It is assumed that the solvent consists of
    single-residue molecules, since molecule information would require a tpr
    input, which might not be commonly available when preparing the system.
    
    Add a basic test for the functionality.
    
    Change-Id: I3a60cdcec9a1675a116c13d83465b07a69b3388d

--- src/gromacs/topology/atomsbuilder.h
+++ src/gromacs/topology/atomsbuilder.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -92,6 +92,8 @@ class AtomsRemover
     public:
         explicit AtomsRemover(const t_atoms &atoms);
         ~AtomsRemover();
+
+        void refreshAtomCount(const t_atoms &atoms);
 
         void markAll();
         void markResidue(const t_atoms &atoms, int atomIndex, bool bStatus);
