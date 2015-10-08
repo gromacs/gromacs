2cc8a0f71ea1410e14c9b0810d0526b381b30d86
Erik Lindahl <erik@kth.se>
2015-10-08 18:28:49 +0200

    C++ classes for cpuid and hardware topology
    Refactored old cpuid into classes for cpuid and hardware
    topology detection, and moved SIMD architecture selection
    routines to the SIMD module. This will make it possible
    to introduce hwloc transparently to get more advanced
    topology information from the hardware topology class.
    Since forcerec.h is still included by a few C files, the
    previous cpuid datatype has been turned into pointers
    to forward-declared non-existing structs, which causes
    snew/sfree to choke on them. Internally they are instead
    properly new:d/delete:d in the C++ code.
    
    Change-Id: I378ec5dc6c32883c5d89f1cfeecea93a3b8b0e6b

---
+++ src/gromacs/hardware/hardwaretopology.h
@@ -0,0 +1,183 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \libinternal \file
+ * \brief
+ * Declares gmx::HardwareTopology
+ *
+ * \author Erik Lindahl <erik.lindahl@gmail.com>
+ * \inlibraryapi
+ * \ingroup module_hardware
+ */
+#ifndef GMX_HARDWARE_HARDWARETOPOLOGY_H
+#define GMX_HARDWARE_HARDWARETOPOLOGY_H
+
+#include <vector>
+
+#include "gromacs/hardware/cpuinfo.h"
+
+
+namespace gmx
+{
+
+/*! \libinternal \brief Information about sockets, cores, hardware threads, numa, and caches
+ *
+ * This class is the main GROMACS interface to provide information about the
+ * hardware of the system we are running on. Internally, it uses either
+ * hwloc for full or almost-full information, or a fallback implementation
+ * that relies on CpuInfo on x86.
+ *
+ * You should always use this class (rather than CpuInfo directly) to query
+ * the hardware layout in user code. Note that you cannot rely on any
+ * information being present, but you must check with the supportLevel()
+ * method before trying to access any information.
+ */
+class HardwareTopology
+{
+    public:
+
+        /*! \brief Amount of topology information present (incremental) */
+        enum class SupportLevel
+        {
+            None,                  //!< No hardware information whatsoever. Sorry.
+            LogicalProcessorCount, //!< Only machine().logicalProcessorCount is valid
+            Basic,                 //!< Socket, core and hardware thread info
+            Full,                  //!< Cache, memory and numa node info
+            FullWithDevices        //!< Information about devices on the PCI bus
+        };
+
+        // For now the structures describing the machine are very basic, but they
+        // will grow to include e.g. cache and core-group information in the future.
+
+        /*! \libinternal \brief Information about a single hardware thread in a core */
+        struct HWThread
+        {
+            int                    logicalProcessorId; //!< Id of the operating system logical processor
+        };
+
+        /*! \libinternal \brief Information about a single core in a socket */
+        struct Core
+        {
+            std::vector<HWThread>  hwThreads;       //!< All the hardware threads in this core
+        };
+
+        /*! \libinternal \brief Information about a single core in the system */
+        struct Socket
+        {
+            std::vector<Core>      cores;          //!< All the cores in this socket
+        };
+
+        /*! \libinternal \brief Information about socket, core and hwthread for a logical processor */
+        struct LogicalProcessor
+        {
+            int                socket;   //!< Index of socket in machine
+            int                core;     //!< Index of core in socket
+            int                hwThread; //!< Index of hardware thread in core
+        };
+
+        /*! \libinternal \brief Hardware topology information about the entire machine
+         *
+         * The machine structure is a tree with top-down information about all
+         * sockets, cores, and hardware threads in the system. For example, an
+         * operating system logical processor index can be found as
+         * machine.socket[0].core[1].hwthread[2].logicalProcessorId.
+         * In some cases you might need the opposite lookup, i.e. the physical
+         * hardware data for a specific logical processor. This is present in the
+         * logicalProcessor vector for convenience.
+         *
+         * \note The logicalProcessor vector will only have non-zero length if the
+         *       support level is SupportLevel::Basic or higher. You cannot use the
+         *       size of this vector to query the number of logical processors on
+         *       lower support levels.
+         */
+        struct Machine
+        {
+            int                            logicalProcessorCount; //!< Number of logical processors in system
+            std::vector<LogicalProcessor>  logicalProcessors;     //!< Map logical processors to socket/core
+            std::vector<Socket>            sockets;               //!< All the sockets in the system
+        };
+
+    public:
+
+        /*! \brief Detects the hardware topology.
+         */
+        static HardwareTopology detect();
+
+        /*! \brief Check what topology information that is available and valid
+         *
+         *  The amount of hardware topology information that can be detected depends
+         *  on both the hardware and whether GROMACS was linked with the external
+         *  hwloc library. You cannot assume that any information is present,
+         *  although we can almost always provide the number of logical processors.
+         *  On x86 we can usually get basic information about how sockets, cores
+         *  and hardware threads are ordered even without hwloc.
+         *  With the hwloc library we can usually also get information about cache,
+         *  memory and concepts such as core groups and ccNUMA nodes.
+         *  Finally, if hwloc was built with support for libpci we can also
+         *  detect how the PCI devices are connected.
+         */
+        SupportLevel
+        supportLevel() const { return supportLevel_; }
+
+        /*! \brief Return the machine topology tree
+         *
+         *  You can always call this routine, but be aware that some or all contents
+         *  will not be valid unless supportLevel() returns a sufficient level.
+         *
+         *  - With SupportLevel::LogicalProcessorCount, only the field
+         *    machine.logicalProcessorCount is valid.
+         *  - With SupportLevel::Basic, you can access the vectors of sockets,
+         *    cores, and hardware threads, and query what logical processorId
+         *    each hardware thread corresponds to.
+         *  - SupportLevel::Full adds cache, memory and ccNUMA information.
+         *  - SupportLevel::FullWithDevices also adds the PCI express bus.
+         *
+         *  While data that is not valid has been initialized to special values,
+         *  you should not rely on those but query the supportLevel() method before
+         *  accessing it.
+         */
+        const Machine &
+        machine() const { return machine_; }
+
+    private:
+
+        HardwareTopology();
+
+        SupportLevel        supportLevel_; //!< Available topology information
+        Machine             machine_;      //!< The machine map
+};
+
+}
+
+#endif // GMX_HARDWARE_HARDWARETOPOLOGY_H
---
+++ src/gromacs/hardware/tests/cpuinfo.cpp
@@ -0,0 +1,88 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Tests for gmx::CpuInfo
+ *
+ * \author Erik Lindahl <erik.lindahl@gmail.com>
+ * \ingroup module_hardware
+ */
+#include "gmxpre.h"
+
+#include "gromacs/hardware/cpuinfo.h"
+
+#include "config.h"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+
+TEST(CpuInfoTest, SupportLevel)
+{
+    // There is no way we can compare to any reference data since that
+    // depends on the architecture, but we can at least make sure that it
+    // works to execute the tests
+
+    gmx::CpuInfo c(gmx::CpuInfo::detect());
+
+    std::string  commonMsg =
+        "\nGROMACS might still work, but it will likely hurt your performance."
+        "\nPlease mail gmx-developers@gromacs.org so we can try to fix it.";
+
+    // It is not the end of the world if any of these tests fail (Gromacs will
+    // work fine without cpuinfo), but we might as well flag it so we add it to
+    // our detection code
+    EXPECT_GT(c.supportLevel(), gmx::CpuInfo::SupportLevel::None)
+    << "No CPU information at all could be detected. " << commonMsg << std::endl;
+
+#if defined __powerpc__ || defined __ppc__ || defined __PPC__  || defined __arm__ || defined __arm || defined GMX_TARGET_X86
+    EXPECT_GE(c.supportLevel(), gmx::CpuInfo::SupportLevel::Features)
+    << "No CPU features could be detected. " << commonMsg << std::endl;
+#endif
+
+    if (c.supportLevel() >= gmx::CpuInfo::SupportLevel::LogicalProcessorInfo)
+    {
+        // Make sure assigned numbers are reasonable if we have them
+        for (auto &l : c.logicalProcessors())
+        {
+            EXPECT_GE(l.socket,   0) << "Impossible socket index for logical processor. " << commonMsg << std::endl;
+            EXPECT_GE(l.core,     0) << "Impossible core index for logical processor. " << commonMsg << std::endl;
+            EXPECT_GE(l.hwThread, 0) << "Impossible hwthread index for logical processor. " << commonMsg << std::endl;
+        }
+    }
+}
+
+} // namespace
