1624db8a93453dd3a8db4f5f607a5229a7d5ece9
Thomas Ullmann <thomas.ullmann@mpibpc.mpg.de>
2015-11-04 04:34:40 +0100

    A C++ replacement for the symbol table
    Function
    ========
    
    Class shared_string can be used pretty much like std::string.
    Instances of the class that represent the same string share
    the same character array as with tzhe current symbol table
    t_symtab found in src/gromacs/topology. The symbol table
    is currently, e.g., used for easy global replacement of
    common residue or atom names in system preparation, for
    example in pdb2gmx.
    
    additional features:
    - global replacement of names is now even easier
      and can be done separately for groups of strings
    - automatic memory management, unused entries are automatically deallocated
    - lock-based thread-safety
    
    Purpose
    =======
    
    For the lambda-dynamics/FMM method a new topology with local
    alternatives will be needed (e.g., protein sidechain protonation
    states, or at the extreme drug molecules with different substituents
    or entirely different structures). This requirement will force
    us to rewrite at least parts of the code for topology reading
    and generation. This more general topology will be needed for
    purposes other than the lambda-dynamics/FMM too, especially in
    general free energy calculations with N end states.
    
    There are already some Redmine issues on the topic (refs #1653, #1652 and #1332)
    
    Details
    =======
    
    We plan to use shared string for representing residue and atom
    names, that may have to be renamed in a program as currently done
    with t_symtab.
    
    See the unit test in
    src/gromacs/utility/data_structures/tests/shared_string.cpp
    for usage examples and performance tests.
    
    Change-Id: I68814357b5959ea691bb508fd13aeb8eba4f1fff

---
+++ src/gromacs/utility/data_structures/tests/data_struct_test_commons.cpp
@@ -0,0 +1,85 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+   \brief
+   implementation file for data_struct_test_commons.h
+
+   \author R. Thomas Ullmann <thomas.ullmann@mpibpc.mpg.de>
+
+   \ingroup module_utility
+ */
+#include "gmxpre.h"
+
+#include <fstream>
+#include <iomanip>  // ::std::setw, ::std::setfill
+#include <ios>      // ::std::left, ::std::rightusing debug;
+#include <iostream> // ::debug
+#include <ostream>  // ::std::ostream (<<)
+#include <stdexcept>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "gromacs/options/basicoptions.h"
+#include "gromacs/options/options.h"
+#include "gromacs/utility/path.h"
+
+#include "testutils/refdata.h"
+#include "testutils/stringtest.h"
+#include "testutils/testfilemanager.h"
+#include "testutils/testoptions.h"
+
+namespace gmx
+{
+
+namespace data_struct_test
+{
+
+
+// flag that enables (true) or disables (false) debug output
+// can be set to via commandline option flag -[no]debug
+//! \cond
+bool g_debugBool = false;
+GMX_TEST_OPTIONS(MyTestOptions, options)
+{
+    options->addOption(gmx::BooleanOption("debug").store(&g_debugBool)
+                           .description("Print additional debug output if available."));
+}
+//! \endcond
+
+
+} // namespace data_struct_test
+
+} // namespace gmx
