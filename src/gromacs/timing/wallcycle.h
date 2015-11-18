33081394f0108c7c2fe43ec538f2d180b737da0a
Mark Abraham <mark.j.abraham@gmail.com>
2015-11-18 20:05:46 +0100

    Split wallcycle interface
    Separated header for components that relate to accumulating counts
    from end-of-run reporting. The former benefits from exposing a minimal
    interface, e.g. so CUDA code isn't potentially exposed to C++11
    headers.
    
    Refs #1745
    
    Change-Id: I70517ac104f002b7b4e61a3aaa26acff5193361c

--- src/gromacs/timing/wallcycle.h
+++ src/gromacs/timing/wallcycle.h
@@ -42,12 +42,9 @@
 
 #include <stdio.h>
 
-#include <array>
-
 #include "gromacs/utility/basedefinitions.h"
 
 typedef struct gmx_wallcycle *gmx_wallcycle_t;
-typedef struct gmx_wallclock_gpu_t gmx_wallclock_gpu_t;
 struct t_commrec;
 
 enum {
@@ -103,19 +100,6 @@ void wallcycle_reset_all(gmx_wallcycle_t wc);
 
 void wallcycle_scale_by_num_threads(gmx_wallcycle_t wc, bool isPmeRank, int nthreads_pp, int nthreads_pme);
 /* Scale the cycle counts to reflect how many threads run for that number of cycles */
-
-typedef std::array<double, ewcNR+ewcsNR> WallcycleCounts;
-/* Convenience typedef */
-
-WallcycleCounts wallcycle_sum(struct t_commrec *cr, gmx_wallcycle_t wc);
-/* Return a vector of the sum of cycle counts over the nodes in
-   cr->mpi_comm_mysim. */
-
-void wallcycle_print(FILE *fplog, int nnodes, int npme,
-                     int nth_pp, int nth_pme, double realtime,
-                     gmx_wallcycle_t wc, const WallcycleCounts &cyc_sum,
-                     struct gmx_wallclock_gpu_t *gpu_t);
-/* Print the cycle and time accounting */
 
 gmx_int64_t wcycle_get_reset_counters(gmx_wallcycle_t wc);
 /* Return reset_counters from wc struct */
