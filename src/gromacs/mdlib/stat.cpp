4243ea9c5fc41f8702aece2d05b6da5c82356cbb
Mark Abraham <mark.j.abraham@gmail.com>
2015-11-23 17:01:00 +0100

    Stop global communication depending on integrator
    In 488464e7, we removed the iterative case for the md-vv
    integrator. This exposed a bug that I suspect has been there ever
    since the first implementation of md-vv. Whether we do reduction on
    the force virial should depend only on whether the main loop asked for
    it, because we're planning to calculate quantities that need it,
    e.g. pressure. Further discussion on Redmine.
    
    Fixes #1858
    
    Change-Id: I0fbce62a9732dac186aec687445dcb848151c4fd

--- src/gromacs/mdlib/stat.cpp
+++ src/gromacs/mdlib/stat.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -224,7 +224,7 @@ void global_stat(FILE *fplog, gmx_global_stat_t gs,
     }
     where();
 
-    if (bPres || !bVV)
+    if (bPres)
     {
         ifv = add_binr(rb, DIM*DIM, fvir[0]);
     }
@@ -333,7 +333,7 @@ void global_stat(FILE *fplog, gmx_global_stat_t gs,
             where();
         }
     }
-    if (bPres || !bVV)
+    if (bPres)
     {
         extract_binr(rb, ifv, DIM*DIM, fvir[0]);
     }
