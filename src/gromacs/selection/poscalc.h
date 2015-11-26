5508d5c6fd4d1d67ccdb4599d493c9b6d5539222
Teemu Murtola <teemu.murtola@gmail.com>
2015-11-26 21:52:50 +0200

    Support arbitrary subset trajectories in poscalc.*
    - Make low-level selection code work with trajectories that contain an
      arbitrary subset of the topology atoms, instead of just the first N.
    - Add an entry to t_trxframe to represent such trajectories.
    - Add some basic tests.  Negative cases crash with an assert, so not
      tested (should get caught higher up).
    
    This should cover all of selection evaluation, but various consistency
    checks need to be adjusted as well (will be done in a child change).
    
    Related to #1861.
    
    Change-Id: I727c79a38e39fb9142976875ab3311e7b1172a55

--- src/gromacs/selection/poscalc.h
+++ src/gromacs/selection/poscalc.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2009,2010,2011,2012,2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2009,2010,2011,2012,2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -303,17 +303,18 @@ class PositionCalculationCollection
         /*! \brief
          * Initializes a position calculation collection for a new frame.
          *
-         * Clears the evaluation flag for all calculations.
+         * \param[in] fr  Frame to initialize evaluation for.
+         *
          * Should be called for each frame before calling
          * gmx_ana_poscalc_update().
          *
          * This function calls initEvaluation() automatically if it has not
          * been called earlier.
          *
-         * Does not throw, but may throw if initEvaluation() is changed to
-         * throw.
+         * Does not currently throw, but may throw std::bad_alloc in the
+         * future.
          */
-        void initFrame();
+        void initFrame(const t_trxframe *fr);
 
     private:
         class Impl;
