19f32281c4adf947e956f0d274bd25f153d28a98
Mark Abraham <mark.j.abraham@gmail.com>
2015-11-27 21:17:39 +0100

    Re-implemented mdrun -rerun tests
    Added infrastructure to read .edr files and return the values of
    selected fields, so that they can be compared. The rerun test can now
    check that the energies produced by a rerun actually match those
    produced by a normal mdrun.
    
    This test running in parallel would have prevented #1858.
    
    Refs #1858
    
    Change-Id: I7f99eba36184f564108c38865fe219f1bca55354

---
+++ src/programs/mdrun/tests/energyreader.h
@@ -0,0 +1,173 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2015, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+/*! \internal \file
+ * \brief Interfaces of related classes for tests that want to
+ * inspect energies produced by mdrun.
+ *
+ * \author Mark Abraham <mark.j.abraham@gmail.com>
+ * \ingroup module_mdrun_integration_tests
+ */
+#ifndef GMX_PROGRAMS_MDRUN_TESTS_ENERGYREADER_H
+#define GMX_PROGRAMS_MDRUN_TESTS_ENERGYREADER_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "gromacs/fileio/enxio.h"
+#include "gromacs/utility/scoped_cptr.h"
+
+#include "testutils/testasserts.h"
+
+namespace gmx
+{
+namespace test
+{
+
+class EnergyFrameReader;
+
+/*! \internal
+ * \brief Manages opening an .edr file and reading required energy fields from it safely.
+ *
+ * This class manages resources for reading .edr file frames, to
+ * ensure that objects of EnergyFrameReader and EnergyFrameInfo can be
+ * used with as much exception safety as possible (given the
+ * limitations of the legacy API it wraps).
+ *
+ * As such, it has sole responsibility for making EnergyFrameReader objects. */
+class EnergyFileReader
+{
+    public:
+        /*! \brief Constructor, taking the name of the .edr file to read. */
+        explicit EnergyFileReader(const std::string &filename);
+        /*! \brief Open the file and return an object that can read the required fields from an .edr file.
+         *
+         * \param[in] requiredEnergyFieldNames Names of the energy fields that the caller requires to
+         *                                     be present for an .edr file frame to be considered valid
+         * \throws    FileIOError              If the .edr file cannot be opened
+         * \throws    APIError                 Upon any subsequent call to this function
+         * \throws    APIError                 If any required energy field is not present in the file
+         * \throws    std::bad_alloc           When out of memory */
+        EnergyFrameReader openToReadFields(const std::vector<std::string> &requiredEnergyFieldNames);
+
+    private:
+        //! Name of .edr file.
+        std::string                       filename_;
+        //! Legacy file object managed for exception safety.
+        scoped_cptr<ener_file, close_enx> energyFile_;
+        //! Object to contain data read from .edr file frames, used by associated EnergyFrameReader object.
+        t_enxframe frame_;
+        //! RAII-aware handle to contents of .edr file frames, to permit exception-safe use of EnergyFrameReader.
+        scoped_cptr<t_enxframe, free_enxframe> frameContents_;
+        // Multiple owners of these resources isn't very sensible, so prevent it
+        GMX_DISALLOW_COPY_AND_ASSIGN(EnergyFileReader);
+};
+
+class EnergyFrameInfo;
+
+/*! \internal
+ * \brief Manages returning an EnergyFrameInfo object containing
+ * required energy field values read from successive frames of an .edr
+ * file.
+ *
+ * The scope of such an object should not exceed that of the
+ * EnergyFileReader object that created it. */
+class EnergyFrameReader
+{
+    public:
+        /*! \brief Read the next frame from the file.
+         *
+         * \throws InternalError   if the index for a required energy field is invalid for the current .edr file frame
+         * \throws std::bad_alloc  when out of memory */
+        EnergyFrameInfo readNextFrame();
+    private:
+        /*! \brief Constructor, accessible only to EnergyFileReader
+         *
+         * \param[in] indicesOfEnergyFields  Looks up energy fields by name to get the index into a t_enxframe structure read by the legacy API.
+         * \param[in] energyFile             Energy file object from which to read frames
+         * \param[in] frame                  Handle to object into the legacy energy-frame reading API stores temporary data */
+        explicit EnergyFrameReader(std::map<std::string, int> indicesOfEnergyFields,
+                                   ener_file *energyFile,
+                                   t_enxframe *frame);
+
+    private:
+        //! Convert energy field name to its index within a t_enxframe.
+        std::map<std::string, int> indicesOfEnergyFields_;
+        //! Handle to open energy file
+        ener_file                 *energyFile_;
+        //! Handle to object with data read from the current .edr file frame.
+        t_enxframe                *frame_;
+
+        friend class EnergyFileReader;
+};
+
+/*! \internal
+ * \brief Contains the content of an .edr frame read by an EnergyFrameReader
+ *
+ * Objects of this type are intended to be constructed by
+ * EnergyFrameReader objects. */
+class EnergyFrameInfo
+{
+    public:
+        //! Constructor
+        EnergyFrameInfo() : values_(), step_(), time_(), isValid_(false) {};
+        /*! \brief Return string that helps users identify this frame, containing time and step number.
+         *
+         * \throws APIError  if no valid frame has been read. */
+        std::string getFrameName() const;
+        //! Returns whether this frame contains valid data
+        operator bool() const;
+        /*! \brief Return the value read for energy \c name.
+         *
+         * \throws APIError  if no valid frame has been read, or \c name was not registered with \c EnergyFileReader. */
+        real getValue(const std::string &name) const;
+    private:
+        //! Container for energy values, indexed by name
+        std::map<std::string, real> values_;
+        //! Step number read from the .edr file frame
+        gmx_int64_t                 step_;
+        //! Time read from the .edr file frame
+        double time_;
+        //! True if this object contains valid .edr data
+        bool   isValid_;
+
+        friend class EnergyFrameReader;
+};
+
+} // namespace
+} // namespace
+
+#endif
--- src/programs/mdrun/tests/rerun.cpp
+++ src/programs/mdrun/tests/rerun.cpp
@@ -42,70 +42,249 @@
  */
 #include "gmxpre.h"
 
-#include "config.h"
+#include <array>
+#include <string>
+#include <vector>
 
+#include <tuple>
 #include <gtest/gtest.h>
 
 #include "gromacs/options/filenameoption.h"
+#include "gromacs/topology/idef.h"
+#include "gromacs/topology/ifunc.h"
+#include "gromacs/utility/exceptions.h"
+#include "gromacs/utility/stringutil.h"
 
 #include "testutils/cmdlinetest.h"
+#include "testutils/refdata.h"
+#include "testutils/testasserts.h"
 
+#include "energyreader.h"
 #include "moduletest.h"
 
+namespace gmx
+{
+namespace test
+{
 namespace
 {
 
-//! Test fixture for mdrun -rerun
-class MdrunRerun : public gmx::test::MdrunTestFixture,
-                   public ::testing::WithParamInterface<const char *>
+/*! \brief Test fixture for mdrun -rerun
+ *
+ * This test ensures mdrun can run a simulation, writing a trajectory
+ * and matching energies, and reproduce the same energies from a rerun
+ * to within a tight tolerance. It says nothing about whether an mdrun
+ * rerun can reproduce energies from a trajectory generated with older
+ * code, since that is not a useful property. Whether mdrun produced
+ * correct energies then and now needs different kinds of testing, but
+ * if true, this test ensures the rerun has the expected property.
+ *
+ * Reproducing the same energies is currently only meaningful for
+ * integration without thermostats or barostats, however the present
+ * form of the test infrastructure has in-principle support for such,
+ * if that is ever needed.
+ *
+ * \todo Add virtual sites peptide-in-water input case, so that
+ * is also tested. */
+class MdrunRerunCorrectlyReproduces : public MdrunTestFixture,
+                                      public ::testing::WithParamInterface<std::tuple<const char *> >
 {
+    public:
+        //! Run a normal mdrun, its rerun, and compare the results
+        void runTest(std::vector<std::string> namesOfRequiredFields,
+                     FloatingPointTolerance   testTolerance)
+        {
+            EXPECT_EQ(0, runner_.callGrompp());
+
+            // prepare some names for files to use with the two mdrun calls
+            std::string rerunTrajectoryFileName = fileManager_.getTemporaryFilePath("normal.trr");
+            std::string normalRunEdrFileName    = fileManager_.getTemporaryFilePath("normal.edr");
+            std::string rerunEdrFileName        = fileManager_.getTemporaryFilePath("rerun.edr");
+
+            // do a normal mdrun
+            {
+                runner_.fullPrecisionTrajectoryFileName_ = rerunTrajectoryFileName;
+                runner_.edrFileName_                     = normalRunEdrFileName;
+                CommandLine normalRunCaller;
+                normalRunCaller.append("mdrun");
+                ASSERT_EQ(0, runner_.callMdrun(normalRunCaller));
+            }
+
+            // do a rerun on the .trr just produced
+            {
+                runner_.fullPrecisionTrajectoryFileName_.clear();
+                runner_.edrFileName_ = rerunEdrFileName;
+                CommandLine rerunCaller;
+                rerunCaller.append("mdrun");
+                rerunCaller.addOption("-rerun", rerunTrajectoryFileName);
+                ASSERT_EQ(0, runner_.callMdrun(rerunCaller));
+            }
+
+            // Prepare objects to read the energy files
+            EnergyFileReader  normalRunFile(normalRunEdrFileName);
+            EnergyFileReader  rerunFile(rerunEdrFileName);
+
+            EnergyFrameReader normalRun(normalRunFile.openToReadFields(namesOfRequiredFields));
+            EnergyFrameReader rerun    (rerunFile.openToReadFields(namesOfRequiredFields));
+
+            do
+            {
+                EnergyFrameInfo normalRunFrame = normalRun.readNextFrame();
+                EnergyFrameInfo rerunFrame     = rerun.readNextFrame();
+                if (normalRunFrame && rerunFrame)
+                {
+                    for (auto const &name : namesOfRequiredFields)
+                    {
+                        EXPECT_REAL_EQ_TOL(normalRunFrame.getValue(name), rerunFrame.getValue(name), testTolerance)
+                        << name << " didn't match between normal run " << normalRunFrame.getFrameName() << " and rerun " << rerunFrame.getFrameName();
+                    }
+                }
+                else
+                {
+                    // At least one file is out of frames. Report if the two
+                    // files had different numbers of frames.
+                    if (normalRunFrame != rerunFrame)
+                    {
+                        EXPECT_FALSE(rerunFrame) << "rerun energy file had at least one extra frame";
+                        EXPECT_FALSE(normalRunFrame) << "normal run energy file had at least one extra frame";
+                    }
+                    break;
+                }
+            }
+            while (true);
+        }
 };
 
-/* Among other things, this test ensures mdrun can read a trajectory. */
-TEST_P(MdrunRerun, WithDifferentInputFormats)
+TEST_P(MdrunRerunCorrectlyReproduces, ArgonSimulation)
 {
-    runner_.useEmptyMdpFile();
-    runner_.useTopGroAndNdxFromDatabase("spc2");
-    EXPECT_EQ(0, runner_.callGrompp());
+    const char *integrator      = std::get<0>(GetParam());
 
-    std::string rerunFileName = fileManager_.getInputFilePath(GetParam());
+    /* Do a simulation on argon
+     * - writing frames from different kinds of steps: starting, ending, intermediate NS, intermediate non-NS
+     * - with other steps between frame-writing steps
+     * - with enough buffer that the rerun will compute the same potential energy even though it does NS every frame
+     * - can generalize to using thermostats and barostats if that's ever useful
+     * - without constraints
+     * whose rerun will match with a tight tolerance.
+     */
+    runner_.useStringAsMdpFile(formatString("rcoulomb = 1.0\n"
+                                            "rvdw = 1.0\n"
+                                            "rlist = -1\n"
+                                            "bd-fric = 100\n"
+                                            "cutoff-scheme = Verlet\n"
+                                            "verlet-buffer-tolerance = 0.000001\n"
+                                            "nsteps = 20\n"
+                                            "nstcalcenergy = -1\n"
+                                            "nstenergy = 5\n"
+                                            "nstlist = 10\n"
+                                            "nstxout = 5\n"
+                                            "nstvout = 5\n"
+                                            "integrator = %s\n"
+                                            "ld-seed = 234262\n"
+                                            "tcoupl = no\n"
+                                            "ref-t = 298.0\n"
+                                            "tau-t = 1.0\n"
+                                            "tc-grps = System\n"
+                                            "pcoupl = no\n"
+                                            "ref-p = 1\n"
+                                            "compressibility = 5e-5\n",
+                                            integrator));
 
-    ::gmx::test::CommandLine rerunCaller;
-    rerunCaller.append("mdrun");
-    rerunCaller.addOption("-rerun", rerunFileName);
-    ASSERT_EQ(0, runner_.callMdrun(rerunCaller));
+    // make the tpr file
+    runner_.useTopGroAndNdxFromDatabase("argon");
+
+    // Describe the energy-file fields that we wish to compare between
+    // the two runs.
+    std::vector<std::string> namesOfRequiredFields({{interaction_function[F_EPOT].longname,
+                                                     interaction_function[F_EKIN].longname,
+                                                     interaction_function[F_ETOT].longname,
+                                                     interaction_function[F_TEMP].longname,
+                                                     interaction_function[F_PRES].longname }});
+
+    /* NOTE This is an arbitrary-but-sufficient choice for the
+     * tolerance. Rerun does pair search every frame, so it cannot in
+     * general exactly reproduce quantities from a normal run. (Nor
+     * does it reproduce pair-search frames exactly, either). */
+    FloatingPointTolerance testTolerance = relativeToleranceAsPrecisionDependentUlp(1.0, 120, 1.2e5);
+
+    runTest(namesOfRequiredFields, testTolerance);
 }
 
-/*! \brief Helper array of input files present in the source repo
- * database. These all have two identical frames of two SPC water
- * molecules, which were generated via trjconv from the .gro
- * version. */
-const char *trajectoryFileNames[] = {
-    "../../../gromacs/gmxana/legacytests/spc2-traj.trr",
-#if defined GMX_USE_TNG && HAVE_ZLIB
-    "../../../gromacs/gmxana/legacytests/spc2-traj.tng",
-#endif
-    "../../../gromacs/gmxana/legacytests/spc2-traj.xtc",
-    "../../../gromacs/gmxana/legacytests/spc2-traj.gro",
-    "../../../gromacs/gmxana/legacytests/spc2-traj.pdb",
-    "../../../gromacs/gmxana/legacytests/spc2-traj.g96"
-};
-// TODO later. Find a better way to manage this file database and
-// these string arrays that index it
+TEST_P(MdrunRerunCorrectlyReproduces, WaterSimulation)
+{
+    const char *integrator      = std::get<0>(GetParam());
 
-#ifdef __INTEL_COMPILER
-#pragma warning( disable : 177 )
-#endif
+    /* Do a simulation
+     * - writing frames from different kinds of steps: starting, ending, intermediate NS, intermediate non-NS
+     * - with other steps between frame-writing steps
+     * - with enough buffer that the rerun will compute the same potential energy even though it does NS every frame
+     * - can generalize to using thermostats and barostats if that's ever useful
+     * - with constraints (ie. SETTLE)
+     * whose rerun will match with a tight tolerance.
+     */
+    runner_.useStringAsMdpFile(formatString("rcoulomb = 0.8\n"
+                                            "rvdw = 0.8\n"
+                                            "rlist = -1\n"
+                                            "bd-fric = 1000\n"
+                                            "cutoff-scheme = Verlet\n"
+                                            "verlet-buffer-tolerance = 0.000001\n"
+                                            "nsteps = 20\n"
+                                            "nstcalcenergy = -1\n"
+                                            "nstenergy = 5\n"
+                                            "nstlist = 10\n"
+                                            "nstxout = 5\n"
+                                            "nstvout = 5\n"
+                                            "integrator = %s\n"
+                                            "ld-seed = 234262\n"
+                                            "tcoupl = no\n"
+                                            "ref-t = 298.0\n"
+                                            "tau-t = 1.0\n"
+                                            "tc-grps = System\n"
+                                            "pcoupl = no\n"
+                                            "ref-p = 1\n"
+                                            "compressibility = 5e-5\n",
+                                            integrator));
 
-INSTANTIATE_TEST_CASE_P(NoFatalErrorFrom,
-                        MdrunRerun,
-                            ::testing::ValuesIn(gmx::ArrayRef<const char*>(trajectoryFileNames)));
+    // make the tpr file
+    runner_.useTopGroAndNdxFromDatabase("spc216");
+
+    // Describe the energy-file fields that we wish to compare between
+    // the two runs. We do not compare pressure, because with
+    // constraints the non-search steps need a much larger tolerance,
+    // and per Redmine 1858 we should stop computing pressure in
+    // reruns anyway.
+    std::vector<std::string> namesOfRequiredFields({{interaction_function[F_EPOT].longname,
+                                                     interaction_function[F_EKIN].longname,
+                                                     interaction_function[F_ETOT].longname,
+                                                     interaction_function[F_TEMP].longname }});
+
+    /* NOTE This is an arbitrary-but-sufficient choice for the
+     * tolerance. Rerun does pair search every frame, so it cannot in
+     * general exactly reproduce quantities from a normal run. (Nor
+     * does it reproduce pair-search frames exactly, either). */
+    FloatingPointTolerance testTolerance = relativeToleranceAsPrecisionDependentUlp(1.0, 30, 1.2e5);
+
+    if (0 != std::strcmp("bd", integrator))
+    {
+        runTest(namesOfRequiredFields, testTolerance);
+    }
+    else
+    {
+        // TODO we can start testing bd integrator when we stop
+        // expecting rerun to reproduce KE, per Redmine 1858
+        fprintf(stderr, "bd + constraints rerun not tested because of known bug with first-frame KE\n");
+    };
+}
+
+INSTANTIATE_TEST_CASE_P(WithVariousIntegrators,
+                        MdrunRerunCorrectlyReproduces,
+                            ::testing::Values("md", "md-vv", "bd", "sd"));
 
 /*! \todo Add other tests for mdrun -rerun, e.g.
  *
- * - RerunReproducesRunWhenRunOnlyWroteEnergiesOnNeighborSearchSteps
- *   (e.g. do such a run, do a rerun, call gmxcheck)
  * - TpiExitsNormally (since it uses the -rerun machinery)
  */
 
+} // namespace
+} // namespace
 } // namespace
