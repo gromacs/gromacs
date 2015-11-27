5fe5e3658e52dc43e3d25fbc42e9b3bf7f1b337e
Alexey Shvetsov <alexxy@omrb.pnpi.spb.ru>
2015-11-27 14:56:43 +0300

    [RFC] Add basic operations for RVec
    1. Overload +,-,+=,-= operations for BasicVector
    2. Add class methods for dot,cross products,
    scale, unitv, length, sqlength functions
    3. Add out of class funtions for same operations as class methods
    4. Add .toRVec, .toDVec methods
    
    Change-Id: I9d0f4490ae51d12842e0f22e9b24b46754fb4f79
    Signed-off-by: Alexey Shvetsov <alexxy@omrb.pnpi.spb.ru>

--- src/gromacs/math/tests/vectypes.cpp
+++ src/gromacs/math/tests/vectypes.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -57,6 +57,8 @@ namespace
 {
 
 using gmx::RVec;
+using gmx::DVec;
+using gmx::IVec;
 
 TEST(RVecTest, CanBeStoredInVector)
 {
@@ -115,17 +117,235 @@ TEST(RVecTest, WorksAs_rvec_Array)
 }
 
 /*! \brief
- * Helper function for testing RVec to rvec conversions.
+ * Test overloaded math operations
  */
-const rvec *testFunction(const rvec &x)
+
+TEST(RVecTest, CanAssignRVecToRvec)
+{
+    RVec a(1, 2, 3);
+    RVec b;
+    b = a;
+    EXPECT_EQ(1, b[XX]);
+    EXPECT_EQ(2, b[YY]);
+    EXPECT_EQ(3, b[ZZ]);
+}
+
+TEST(RVecTest, CanAddRVecToRvec)
+{
+    RVec a(1, 2, 3);
+    RVec b(3, 2, 1);
+    RVec c;
+    c = a + b;
+    EXPECT_EQ(4, c[XX]);
+    EXPECT_EQ(4, c[YY]);
+    EXPECT_EQ(4, c[ZZ]);
+}
+
+TEST(RVecTest, CanAddAssignRVecToRvec)
+{
+    RVec a(1, 2, 3);
+    RVec b(3, 2, 1);
+    a += b;
+    EXPECT_EQ(4, a[XX]);
+    EXPECT_EQ(4, a[YY]);
+    EXPECT_EQ(4, a[ZZ]);
+}
+
+
+TEST(RVecTest, CanSubstractRVecFromRvec)
+{
+    RVec a(1, 2, 3);
+    RVec b(3, 2, 1);
+    RVec c;
+    c = b - a;
+    EXPECT_EQ(2, c[XX]);
+    EXPECT_EQ(0, c[YY]);
+    EXPECT_EQ(-2, c[ZZ]);
+}
+
+TEST(RVecTest, CanSubstractAssignRVecFromRvec)
+{
+    RVec a(1, 2, 3);
+    RVec b(3, 2, 1);
+    RVec c;
+    b -= a;
+    EXPECT_EQ(2, b[XX]);
+    EXPECT_EQ(0, b[YY]);
+    EXPECT_EQ(-2, b[ZZ]);
+}
+
+TEST(RVecTest, CanDotProductRVecByRvec)
+{
+    RVec  a(1, 2, 3);
+    RVec  b(3, 2, 1);
+    float c;
+    c = a.dot(b);
+    EXPECT_EQ(10, c);
+}
+
+TEST(RVecTest, CanCrossProductRVecByRvec)
+{
+    RVec a(1, 2, 3);
+    RVec b(3, 2, 1);
+    RVec c;
+    c = a.cross(b);
+    EXPECT_EQ(-4, c[XX]);
+    EXPECT_EQ(8, c[YY]);
+    EXPECT_EQ(-4, c[ZZ]);
+}
+
+/*! \brief
+ * Test for inplace operations imported from vec.h
+ */
+
+TEST(RVecTest, CanScaleRVecInplace)
+{
+    RVec a(1, 2, 3);
+    real b = 2.0;
+    RVec c;
+    c = a.scale(b);
+    EXPECT_EQ(2, c[XX]);
+    EXPECT_EQ(4, c[YY]);
+    EXPECT_EQ(6, c[ZZ]);
+}
+
+TEST(RVecTest, CanDoUnitvFromRVec)
+{
+    RVec a(3, 0, 0);
+    RVec b;
+    b = a.unitv();
+    EXPECT_EQ(1, b[XX]);
+    EXPECT_EQ(0, b[YY]);
+    EXPECT_EQ(0, b[ZZ]);
+}
+
+TEST(RVecTest, CanSqLengthOfRVec)
+{
+    RVec a(1, 2, 2);
+    real b;
+    b = a.sqlength();
+    EXPECT_EQ(9, b);
+}
+
+TEST(RVecTest, CanLengthOfRVec)
+{
+    RVec a(1, 2, 2);
+    real b;
+    b = a.length();
+    EXPECT_EQ(3, b);
+}
+
+TEST(RVecTest, CanCastToRVec)
+{
+    DVec a(1, 2, 2);
+    RVec b;
+    b = a.toRVec();
+    EXPECT_EQ(1, b[XX]);
+    EXPECT_EQ(2, b[YY]);
+    EXPECT_EQ(2, b[ZZ]);
+}
+
+TEST(RVecTest, CanCastToDVec)
+{
+    RVec a(1, 2, 2);
+    DVec b;
+    b = a.toDVec();
+    EXPECT_EQ(1, b[XX]);
+    EXPECT_EQ(2, b[YY]);
+    EXPECT_EQ(2, b[ZZ]);
+}
+
+
+/*! \brief
+ * Tests for out of class functions
+ */
+TEST(RVecTest, CanScaleRVec)
+{
+    RVec a(1, 2, 3);
+    real b = 2.0;
+    RVec c;
+    c = gmx::scale(a, b);
+    EXPECT_EQ(2, c[XX]);
+    EXPECT_EQ(4, c[YY]);
+    EXPECT_EQ(6, c[ZZ]);
+}
+
+TEST(RVecTest, CanGetUnitvFromRVec)
+{
+    RVec a(3, 0, 0);
+    RVec b;
+    b = gmx::unitv(a);
+    EXPECT_EQ(1, b[XX]);
+    EXPECT_EQ(0, b[YY]);
+    EXPECT_EQ(0, b[ZZ]);
+}
+
+TEST(RVecTest, CanGetSqLengthOfRVec)
+{
+    RVec a(1, 2, 2);
+    real b;
+    b = gmx::sqlength(a);
+    EXPECT_EQ(9, b);
+}
+
+TEST(RVecTest, CanGetLengthOfRVec)
+{
+    RVec a(1, 2, 2);
+    real b;
+    b = gmx::length(a);
+    EXPECT_EQ(3, b);
+}
+
+TEST(RVecTest, CanDoCrossProductOfRVec)
+{
+    RVec a(1, 2, 3);
+    RVec b(3, 2, 1);
+    RVec c;
+    c = gmx::cross(a, b);
+    EXPECT_EQ(-4, c[XX]);
+    EXPECT_EQ(8, c[YY]);
+    EXPECT_EQ(-4, c[ZZ]);
+}
+
+TEST(RVecTest, CanDoDotProductOfRVec)
+{
+    RVec  a(1, 2, 3);
+    RVec  b(3, 2, 1);
+    float c;
+    c = gmx::dot(a, b);
+    EXPECT_EQ(10, c);
+}
+
+
+/*! \brief
+ * Helper function for testing DVec to dvec conversions.
+ */
+const dvec *testFunction(const dvec &x)
 {
     return &x;
 }
 
-TEST(RVecTest, WorksAs_rvec_Reference)
+TEST(RVecTest, WorksAs_dvec_Reference)
 {
-    RVec        v(1, 2, 3);
-    const rvec *r = testFunction(v.as_vec());
+    DVec        v(1, 2, 3);
+    const dvec *r = testFunction(v.as_vec());
+    EXPECT_EQ(1, r[0][XX]);
+    EXPECT_EQ(2, r[0][YY]);
+    EXPECT_EQ(3, r[0][ZZ]);
+}
+
+/*! \brief
+ * Helper function for testing IVec to ivec conversions.
+ */
+const ivec *testFunction(const ivec &x)
+{
+    return &x;
+}
+
+TEST(RVecTest, WorksAs_ivec_Reference)
+{
+    IVec        v(1, 2, 3);
+    const ivec *r = testFunction(v.as_vec());
     EXPECT_EQ(1, r[0][XX]);
     EXPECT_EQ(2, r[0][YY]);
     EXPECT_EQ(3, r[0][ZZ]);
