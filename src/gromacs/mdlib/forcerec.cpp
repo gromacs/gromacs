eddbe9076da0288f22f2bb7d74281f8e97d7d05c
Berk Hess <hess@kth.se>
2015-11-27 15:38:32 +0100

    Allow rcoulomb>rvdw with PME
    We have Coulomb PME + LJ cut-off kernels that support rcoulomb>rvdw
    or PME load balancing. Now we allow this setup to be chosen also
    through mdp options. This is mainly a straightforward change, only
    the Verlet buffer calculation needed to be adapted to support
    different buffer lengths for LJ and Coulomb.
    
    Change-Id: I9d8176656435fbdb4ec48b3dd2c7128d13c5ef07

--- src/gromacs/mdlib/forcerec.cpp
+++ src/gromacs/mdlib/forcerec.cpp
@@ -87,6 +87,7 @@
 #include "gromacs/topology/mtop_util.h"
 #include "gromacs/utility/cstringutil.h"
 #include "gromacs/utility/fatalerror.h"
+#include "gromacs/utility/gmxassert.h"
 #include "gromacs/utility/smalloc.h"
 
 #include "nbnxn_gpu_jit_support.h"
@@ -1862,7 +1863,7 @@ static void init_ewald_f_table(interaction_const_t *ic,
     sfree_aligned(ic->tabq_vdw_F);
     sfree_aligned(ic->tabq_vdw_V);
 
-    if (ic->eeltype == eelEWALD || EEL_PME(ic->eeltype))
+    if (EEL_PME_EWALD(ic->eeltype))
     {
         /* Create the original table data in FDV0 */
         snew_aligned(ic->tabq_coul_FDV0, ic->tabq_size*4, 32);
@@ -1886,7 +1887,7 @@ void init_interaction_const_tables(FILE                *fp,
                                    interaction_const_t *ic,
                                    real                 rtab)
 {
-    if (ic->eeltype == eelEWALD || EEL_PME(ic->eeltype) || EVDW_PME(ic->vdwtype))
+    if (EEL_PME_EWALD(ic->eeltype) || EVDW_PME(ic->vdwtype))
     {
         init_ewald_f_table(ic, rtab);
 
@@ -2625,7 +2626,7 @@ void init_forcerec(FILE              *fp,
     fr->rcoulomb         = cutoff_inf(ir->rcoulomb);
     fr->rcoulomb_switch  = ir->rcoulomb_switch;
 
-    fr->bEwald     = (EEL_PME(fr->eeltype) || fr->eeltype == eelEWALD);
+    fr->bEwald     = EEL_PME_EWALD(fr->eeltype);
 
     fr->reppow     = mtop->ffparams.reppow;
 
@@ -3160,9 +3161,14 @@ void init_forcerec(FILE              *fp,
 
     if (fr->cutoff_scheme == ecutsVERLET)
     {
-        if (ir->rcoulomb != ir->rvdw)
+        /* We have PME+LJcutoff kernels for rcoulomb>rvdw */
+        if (EEL_PME_EWALD(ir->coulombtype) && ir->vdwtype == eelCUT)
         {
-            gmx_fatal(FARGS, "With Verlet lists rcoulomb and rvdw should be identical");
+            GMX_RELEASE_ASSERT(ir->rcoulomb >= ir->rvdw, "With Verlet lists and PME we should have rcoulomb>=rvdw");
+        }
+        else
+        {
+            GMX_RELEASE_ASSERT(ir->rcoulomb == ir->rvdw, "With Verlet lists and no PME rcoulomb and rvdw should be identical");
         }
 
         init_nb_verlet(fp, &fr->nbv, bFEP_NonBonded, ir, fr, cr, nbpu_opt);
