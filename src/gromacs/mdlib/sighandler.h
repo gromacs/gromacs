c14ce002a4e309efbc6379e56468cae01e3d1e0b
Teemu Murtola <teemu.murtola@gmail.com>
2015-12-13 12:20:21 +0200

    Move some mdrun code out of gmxlib/
    - Move sighandler.* to mdlib, since it is specific to mdrun.
    - Move thread_affinity.* to mdrunutility, since it is only used from
      runner.cpp.  Convert existing comments to rudimentary Doxygen
      documentation to avoid check-source and Doxygen warnings.
    
    Change-Id: I83d431497480bcfa2b2edf8f347708a0c1832005

--- src/gromacs/mdlib/sighandler.h
+++ src/gromacs/mdlib/sighandler.h
@@ -34,10 +34,8 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the research papers on the package. Check out http://www.gromacs.org.
  */
-#ifndef GMX_GMXLIB_SIGHANDLER_H
-#define GMX_GMXLIB_SIGHANDLER_H
-
-#include <signal.h>
+#ifndef GMX_MDLIB_SIGHANDLER_H
+#define GMX_MDLIB_SIGHANDLER_H
 
 #include "gromacs/utility/basedefinitions.h"
 
