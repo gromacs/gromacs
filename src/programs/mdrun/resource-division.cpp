727c409dfd112745a92a270599b13ad21a1a410b
Teemu Murtola <teemu.murtola@gmail.com>
2015-12-14 20:25:41 +0200

    Move hardware detection to hardware/
    This may not be the best place for all of the code, since there are
    also, e.g., functions that return information about implementation
    limitations of mdrun, and for checking mdrun input parameter
    consistency.  But it would make sense for as much of the actual
    detection code as possible to be in a single location, and this would be
    the most natural place.
    
    There are several cyclic dependencies suppressed for now to get forward
    with breaking up gmxlib/.
    
    Change-Id: Ie73d0a18530082d3a2b9f26a655706b33bf58a8a

--- src/programs/mdrun/resource-division.cpp
+++ src/programs/mdrun/resource-division.cpp
@@ -44,10 +44,10 @@
 
 #include <algorithm>
 
-#include "gromacs/gmxlib/gmx_detect_hardware.h"
 #include "gromacs/gmxlib/gmx_omp_nthreads.h"
 #include "gromacs/gmxlib/md_logging.h"
 #include "gromacs/hardware/cpuinfo.h"
+#include "gromacs/hardware/detecthardware.h"
 #include "gromacs/hardware/gpu_hw_info.h"
 #include "gromacs/hardware/hardwaretopology.h"
 #include "gromacs/hardware/hw_info.h"
