ee318d0f80c050e63d78f8072fc8281d3ddd4d61
Mark Abraham <mark.j.abraham@gmail.com>
2015-12-29 18:39:44 +0100

    Re-implemented mdrun -rerun tests
    Used the new infrastructure to let the rerun test check that the
    energies and forces produced by a rerun actually match those produced
    by a normal mdrun. Such a test running in parallel would have
    prevented issue 1868, and will help checking that any changed or new
    integrator functionality does the job that it should do.
    
    Stopped passing a std::pair into compareFrames. It's important that
    the reader return two frames that it knows matches, but that's not
    important to enforce further along.
    
    Refs #1868
    
    Change-Id: I7f99eba36184f564108c38865fe219f1bca55354

--- src/programs/mdrun/tests/mdruncomparisonfixture.cpp
+++ src/programs/mdrun/tests/mdruncomparisonfixture.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2016, by the GROMACS development team, led by
+ * Copyright (c) 2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -237,15 +237,29 @@ void MdrunComparisonFixture::prepareMdpFile(const MdpFieldValues &mdpFieldValues
                                             mdpFieldValues.at("other").c_str()));
 }
 
-void MdrunComparisonFixture::runTest(const char            *simulationName,
-                                     const char            *integrator,
-                                     const char            *tcoupl,
-                                     const char            *pcoupl,
-                                     FloatingPointTolerance tolerance)
+void MdrunComparisonFixture::runGrompp(const CommandLine     &gromppCallerRef,
+                                       const char            *simulationName,
+                                       const char            *integrator,
+                                       const char            *tcoupl,
+                                       const char            *pcoupl)
+{
+    /* Note that the database lookups will throw std::out_of_range if
+     * the simulationName key is not found */
+    runner_.useTopGroAndNdxFromDatabase(simulationName);
+    auto mdpFieldValues = prepareMdpFieldValues(simulationName);
+    prepareMdpFile(mdpFieldValues, integrator, tcoupl, pcoupl);
+    EXPECT_EQ(0, runner_.callGrompp(gromppCallerRef));
+    gromppWasRun_ = true;
+}
+
+void MdrunComparisonFixture::runGrompp(const char            *simulationName,
+                                       const char            *integrator,
+                                       const char            *tcoupl,
+                                       const char            *pcoupl)
 {
     CommandLine caller;
     caller.append("grompp");
-    runTest(caller, simulationName, integrator, tcoupl, pcoupl, tolerance);
+    runGrompp(caller, simulationName, integrator, tcoupl, pcoupl);
 }
 
 } // namespace test
