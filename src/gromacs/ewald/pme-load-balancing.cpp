98d71a8b422487549ac74ddd4f3f822ae21f6bcd
Berk Hess <hess@kth.se>
2016-01-04 15:06:46 +0100

    Fix PME load balancing skipping a setup
    During stage 0 of the PME tuning we only time every second setup.
    In stage 1 only a reduced range of setups is considered. If the tpr
    setup (setup index 0) was not part of this reduced range, the first
    setup in the range would never be timed. This could lead to choosing
    a non-bonded cut-off that was longer than optimal.
    
    Fixes #1880
    
    Also fixed two conditionals (but this had no effect on the settings).
    
    Change-Id: Id9f361d23e3294e41553b39ae8e8f37ceb80bbe5

--- src/gromacs/ewald/pme-load-balancing.cpp
+++ src/gromacs/ewald/pme-load-balancing.cpp
@@ -504,6 +504,9 @@ static void print_loadbal_limited(FILE *fp_err, FILE *fp_log,
  * In this stage, only reasonably fast setups are run again. */
 static void switch_to_stage1(pme_load_balancing_t *pme_lb)
 {
+    /* Increase start until we find a setup that is not slower than
+     * maxRelativeSlowdownAccepted times the fastest setup.
+     */
     pme_lb->start = pme_lb->lower_limit;
     while (pme_lb->start + 1 < pme_lb->n &&
            (pme_lb->setup[pme_lb->start].count == 0 ||
@@ -512,11 +515,17 @@ static void switch_to_stage1(pme_load_balancing_t *pme_lb)
     {
         pme_lb->start++;
     }
-    while (pme_lb->start > 0 && pme_lb->setup[pme_lb->start - 1].cycles == 0)
+    /* While increasing start we might have skipped setups that we never timed,
+     * include the setups down to the setup that we actually timed, so we will
+     * time those during stage 1.
+     */
+    while (pme_lb->start > pme_lb->lower_limit &&
+           pme_lb->setup[pme_lb->start - 1].count == 0)
     {
         pme_lb->start--;
     }
 
+    /* Decrease end only with setups that we timed and that are slow. */
     pme_lb->end = pme_lb->n;
     if (pme_lb->setup[pme_lb->end - 1].count > 0 &&
         pme_lb->setup[pme_lb->end - 1].cycles >
@@ -528,7 +537,7 @@ static void switch_to_stage1(pme_load_balancing_t *pme_lb)
     pme_lb->stage = 1;
 
     /* Next we want to choose setup pme_lb->end-1, but as we will decrease
-     * pme_ln->cur by one right after returning, we set cur to end.
+     * pme_lb->cur by one right after returning, we set cur to end.
      */
     pme_lb->cur = pme_lb->end;
 }
@@ -736,8 +745,11 @@ pme_load_balance(pme_load_balancing_t      *pme_lb,
          */
         do
         {
-            pme_lb->cur--;
-            if (pme_lb->cur == pme_lb->start)
+            if (pme_lb->cur > pme_lb->start)
+            {
+                pme_lb->cur--;
+            }
+            else
             {
                 pme_lb->stage++;
 
