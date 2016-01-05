bb2235b88a00c6933705a289703c080ac6358b57
Berk Hess <hess@kth.se>
2016-01-05 10:33:27 +0100

    Update -ntomp_pme -npme check
    During the domain decomposition initialization update we forgot to
    replace cr->npmenodes by npme in check_and_update_hw_opt_1.
    
    Change-Id: If8d303a189957bb4e02f8f809b849c85abd12223

--- src/programs/mdrun/resource-division.cpp
+++ src/programs/mdrun/resource-division.cpp
@@ -622,7 +622,8 @@ static void print_hw_opt(FILE *fp, const gmx_hw_opt_t *hw_opt)
 
 /* Checks we can do when we don't (yet) know the cut-off scheme */
 void check_and_update_hw_opt_1(gmx_hw_opt_t    *hw_opt,
-                               const t_commrec *cr)
+                               const t_commrec *cr,
+                               int              nPmeRanks)
 {
     /* Currently hw_opt only contains default settings or settings supplied
      * by the user on the command line.
@@ -656,7 +657,7 @@ void check_and_update_hw_opt_1(gmx_hw_opt_t    *hw_opt,
 
         if (hw_opt->nthreads_omp_pme >= 1 &&
             hw_opt->nthreads_omp_pme != hw_opt->nthreads_omp &&
-            cr->npmenodes <= 0)
+            nPmeRanks <= 0)
         {
             /* This can result in a fatal error on many MPI ranks,
              * but since the thread count can differ per rank,
