21cae64f47d88c6f9df6fbd8f70af6954254e434
Szilárd Páll <pall.szilard@gmail.com>
2016-01-19 23:29:45 +0100

    GPU code reorganization and tweaks
    - Moved the warp size macro to the new arch utils header and converted
      it to static const;
    - Changes some awkward macro names in CUDA and OpenCL;
    - Change the inline modifier of a number of CUDA functions to
      __forceinline__ which means unconditional inlining.
    
    Change-Id: I5b748a920927f9a51ada14e3d6e65b6b4d4dbd43

--- src/gromacs/gpu_utils/vectype_ops.cuh
+++ src/gromacs/gpu_utils/vectype_ops.cuh
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2015, by the GROMACS development team, led by
+ * Copyright (c) 2012,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -37,67 +37,67 @@
 #define VECTYPE_OPS_CUH
 
 /**** float3 ****/
-inline __host__ __device__ float3 make_float3(float s)
+__forceinline__ __host__ __device__ float3 make_float3(float s)
 {
     return make_float3(s, s, s);
 }
-inline __host__ __device__ float3 make_float3(float4 a)
+__forceinline__ __host__ __device__ float3 make_float3(float4 a)
 {
     return make_float3(a.x, a.y, a.z);
 }
-inline __host__ __device__ float3 operator-(float3 &a)
+__forceinline__ __host__ __device__ float3 operator-(float3 &a)
 {
     return make_float3(-a.x, -a.y, -a.z);
 }
-inline __host__ __device__ float3 operator+(float3 a, float3 b)
+__forceinline__ __host__ __device__ float3 operator+(float3 a, float3 b)
 {
     return make_float3(a.x + b.x, a.y + b.y, a.z + b.z);
 }
-inline __host__ __device__ float3 operator-(float3 a, float3 b)
+__forceinline__ __host__ __device__ float3 operator-(float3 a, float3 b)
 {
     return make_float3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
-inline __host__ __device__ float3 operator*(float3 a, float k)
+__forceinline__ __host__ __device__ float3 operator*(float3 a, float k)
 {
     return make_float3(k * a.x, k * a.y, k * a.z);
 }
-inline __host__ __device__ float3 operator*(float k, float3 a)
+__forceinline__ __host__ __device__ float3 operator*(float k, float3 a)
 {
     return make_float3(k * a.x, k * a.y, k * a.z);
 }
-inline __host__ __device__ void operator += (float3 &a, float3 b)
+__forceinline__ __host__ __device__ void operator += (float3 &a, float3 b)
 {
     a.x += b.x; a.y += b.y; a.z += b.z;
 }
-inline __host__ __device__ void operator += (float3 &a, float4 b)
+__forceinline__ __host__ __device__ void operator += (float3 &a, float4 b)
 {
     a.x += b.x; a.y += b.y; a.z += b.z;
 }
-inline __host__ __device__ void operator -= (float3 &a, float3 b)
+__forceinline__ __host__ __device__ void operator -= (float3 &a, float3 b)
 {
     a.x -= b.x; a.y -= b.y; a.z -= b.z;
 }
-inline __host__ __device__ float norm(float3 a)
+__forceinline__ __host__ __device__ float norm(float3 a)
 {
     return sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
 }
-inline __host__ __device__ float norm2(float3 a)
+__forceinline__ __host__ __device__ float norm2(float3 a)
 {
     return (a.x * a.x + a.y * a.y + a.z * a.z);
 }
-inline __host__ __device__ float dist3(float3 a, float3 b)
+__forceinline__ __host__ __device__ float dist3(float3 a, float3 b)
 {
     return norm(b - a);
 }
-inline __host__ __device__ float3 operator*(float3 a, float3 b)
+__forceinline__ __host__ __device__ float3 operator*(float3 a, float3 b)
 {
     return make_float3(a.x * b.x, a.y * b.y, a.z * b.z);
 }
-inline __host__ __device__ void operator *= (float3 &a, float3 b)
+__forceinline__ __host__ __device__ void operator *= (float3 &a, float3 b)
 {
     a.x *= b.x; a.y *= b.y; a.z *= b.z;
 }
-inline __device__ void atomicAdd(float3 *addr, float3 val)
+__forceinline__ __device__ void atomicAdd(float3 *addr, float3 val)
 {
     atomicAdd(&addr->x, val.x);
     atomicAdd(&addr->y, val.y);
@@ -106,49 +106,49 @@ inline __device__ void atomicAdd(float3 *addr, float3 val)
 /****************************************************************/
 
 /**** float4 ****/
-inline __host__ __device__ float4 make_float4(float s)
+__forceinline__ __host__ __device__ float4 make_float4(float s)
 {
     return make_float4(s, s, s, s);
 }
-inline __host__ __device__ float4 make_float4(float3 a)
+__forceinline__ __host__ __device__ float4 make_float4(float3 a)
 {
     return make_float4(a.x, a.y, a.z, 0.0f);
 }
-inline __host__ __device__ float4 operator+(float4 a, float4 b)
+__forceinline__ __host__ __device__ float4 operator+(float4 a, float4 b)
 {
     return make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
 }
-inline __host__ __device__ float4 operator+(float4 a, float3 b)
+__forceinline__ __host__ __device__ float4 operator+(float4 a, float3 b)
 {
     return make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w);
 }
-inline __host__ __device__ float4 operator-(float4 a, float4 b)
+__forceinline__ __host__ __device__ float4 operator-(float4 a, float4 b)
 {
     return make_float4(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
 }
-inline __host__ __device__ float4 operator*(float4 a, float k)
+__forceinline__ __host__ __device__ float4 operator*(float4 a, float k)
 {
     return make_float4(k * a.x, k * a.y, k * a.z, k * a.w);
 }
-inline __host__ __device__ void operator += (float4 &a, float4 b)
+__forceinline__ __host__ __device__ void operator += (float4 &a, float4 b)
 {
     a.x += b.x; a.y += b.y; a.z += b.z; a.w += b.w;
 }
-inline __host__ __device__ void operator += (float4 &a, float3 b)
+__forceinline__ __host__ __device__ void operator += (float4 &a, float3 b)
 {
     a.x += b.x; a.y += b.y; a.z += b.z;
 }
-inline __host__ __device__ void operator -= (float4 &a, float3 b)
+__forceinline__ __host__ __device__ void operator -= (float4 &a, float3 b)
 {
     a.x -= b.x; a.y -= b.y; a.z -= b.z;
 }
 
-inline __host__ __device__ float norm(float4 a)
+__forceinline__ __host__ __device__ float norm(float4 a)
 {
     return sqrt(a.x * a.x + a.y * a.y + a.z * a.z + a.w * a.w);
 }
 
-inline __host__ __device__ float dist3(float4 a, float4 b)
+__forceinline__ __host__ __device__ float dist3(float4 a, float4 b)
 {
     return norm(b - a);
 }
