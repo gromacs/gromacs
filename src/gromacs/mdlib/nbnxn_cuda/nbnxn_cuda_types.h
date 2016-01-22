c0e86ceac0cdbe879995ffc3e59c37a6dff12376
Berk Hess <hess@kth.se>
2016-01-22 11:51:26 +0100

    Replaced literals in nbnxn lists by macros
    Several literal used for array sizes in nbnxn_pairlist.h and
    nbnxn_search.cpp have been replaced by macros or static const int.
    The GPU code should assert that these values match the warp size.
    
    Change-Id: I467e7ac32c45b4208bfeeef6d5c031e32873f4ce

--- src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda_types.h
+++ src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda_types.h
@@ -55,10 +55,10 @@
 
 /* TODO: consider moving this to kernel_utils */
 /* Convenience defines */
-/*! \brief number of clusters per supercluster. */
-#define NCL_PER_SUPERCL         (NBNXN_GPU_NCLUSTER_PER_SUPERCLUSTER)
+/*! \brief number of clusters per supercluster = pair search cell. */
+static const int NCL_PER_SUPERCL = nbnxn_gpu_ncluster_per_cell;
 /*! \brief cluster size = number of atoms per cluster. */
-#define CL_SIZE                 (NBNXN_GPU_CLUSTER_SIZE)
+static const int CL_SIZE         = nbnxn_gpu_cluster_size;
 
 #ifdef __cplusplus
 extern "C" {
