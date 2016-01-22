2647b46ac9b81a40865dbbf581fc77d70e50aa0b
Berk Hess <hess@kth.se>
2016-01-22 11:51:26 +0100

    Replaced literals in nbnxn lists by ints
    Several literals used for array sizes in nbnxn_pairlist.h and
    nbnxn_search.cpp have been replaced by static const int.
    The GPU code should assert that these values match the warp size.
    Removed the incorrectly used macro NBNXN_PBB_SIMD4.
    
    Change-Id: I467e7ac32c45b4208bfeeef6d5c031e32873f4ce

--- src/gromacs/mdlib/nbnxn_atomdata.cpp
+++ src/gromacs/mdlib/nbnxn_atomdata.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1010,7 +1010,7 @@ static void nbnxn_atomdata_mask_fep(nbnxn_atomdata_t    *nbat,
         }
         else
         {
-            nsubc = GPU_NSUBCELL;
+            nsubc = gpu_ncluster_per_cell;
         }
 
         int c_offset = grid->cell0*grid->na_sc;
