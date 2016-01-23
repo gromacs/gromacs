0f34db657e35abbeef924d790b49bb1ee169edd9
Teemu Murtola <teemu.murtola@gmail.com>
2016-01-23 08:10:45 +0200

    Handle pdbinfo in AtomsBuilder
    This makes gmx solvate and gmx insert-molecules work when both -f and -o
    are .pdb files (previously, the pdbinfo got out-of-sync when the atoms
    were added/removed).
    
    Centralize the defaults for pdbinfo to atoms.cpp.
    
    Fixes #1887.
    
    Change-Id: Ia7e7d991cc35e701729904b8319c53283479d0b1

--- src/gromacs/fileio/pdbio.h
+++ src/gromacs/fileio/pdbio.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -41,6 +41,7 @@
 #include <stdio.h>
 
 #include "gromacs/math/vectypes.h"
+#include "gromacs/topology/atoms.h"
 #include "gromacs/utility/basedefinitions.h"
 #include "gromacs/utility/real.h"
 
@@ -54,15 +55,6 @@ struct t_symtab;
 struct t_topology;
 
 typedef struct gmx_conect_t *gmx_conect;
-
-/* Enumerated type for pdb records. The other entries are ignored
- * when reading a pdb file
- */
-enum PDB_record {
-    epdbATOM,   epdbHETATM, epdbANISOU, epdbCRYST1, epdbCOMPND,
-    epdbMODEL,  epdbENDMDL, epdbTER,    epdbHEADER, epdbTITLE, epdbREMARK,
-    epdbCONECT, epdbNR
-};
 
 /* Write a PDB line with an ATOM or HETATM record directly to a file pointer.
  *
