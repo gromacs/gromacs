8037b47587bb4d024195e01b2a0457c1130949b2
Mark Abraham <mark.j.abraham@gmail.com>
2016-01-26 17:44:48 +0100

    Refactor GMX_GPU handling in cmake
    This should be almost transparent to users.
    
    The key changes are to use the trivalue option functionality for
    auto/on/off, and thus we never want to change the value of the cache
    variable that we expect the user might set. Instead, we use the
    GMX_GPU_AUTO and GMX_GPU_FORCE variables that the trivalue option sets
    up, perhaps along with the results of the detection, to set
    GMX_USE_CUDA suitably or give a fatal error. Note that none of these
    variables needs to be put in the cache.
    
    Moved the (duplicated) OpenMP warning to the end of CMakeLists.txt,
    which removes the need to have and call gmx_gpu_setup() to do
    two-stage configuration of GPU support.
    
    Used early return() functions to simplify gmxManageGPU.cmake, and
    added comments to clarify its functionality.
    
    Simplified the construction of CUDA_NOTFOUND_MESSAGE, and documented
    there that it is sometimes used and the end of the main CMakeLists.txt
    (so that the error text is at the bottom of the terminal output).
    
    GMX_GPU is no longer required to be set for an OpenCL build (but
    causes no problems if it is set), and documented accordingly.
    
    Change-Id: I7df81dea738da3ec9cd3971ad3507298a9f97dff

--- cmake/gmxDetectGpu.cmake
+++ cmake/gmxDetectGpu.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012, by the GROMACS development team, led by
+# Copyright (c) 2012,2016, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -218,4 +218,4 @@ macro(gmx_detect_gpu)
         endif()
 
     endif (NOT DEFINED GMX_DETECT_GPU_COUNT OR NOT DEFINED GMX_DETECT_GPU_INFO)
-endmacro(gmx_detect_gpu)
+endmacro()
