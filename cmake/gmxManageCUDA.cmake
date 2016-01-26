986ac8048064bb41874bd38043d3f07b178e9f12
Mark Abraham <mark.j.abraham@gmail.com>
2016-01-26 17:44:48 +0100

    Refactor GMX_GPU handling in cmake
    This should be almost transparent to users.
    
    The key changes are to use the trivalue option functionality for
    auto/on/off, and thus we never want to change the value of the cache
    variable that we expect the user might set. Instead, we use the
    GMX_GPU_AUTO and GMX_GPU_FORCE variables that the trivalue option sets
    up, perhaps along with the results of the detection, to set
    GMX_USE_CUDA suitably or give a fatal error. Note that none of these
    variables needs to be put in the cache.
    
    Moved the (duplicated) OpenMP warning to the end of CMakeLists.txt,
    which removes the need to have and call gmx_gpu_setup() to do
    two-stage configuration of GPU support.
    
    Used early return() functions to simplify gmxManageGPU.cmake, and
    added comments to clarify its functionality.
    
    Renamed GMX_GPU_DETECTION_DONE to GMX_MANAGE_CUDA_DONE to reflect
    that it is used for multiple purposes to reflect whether or
    not there has been a non-fatal completion of gmxManageCUDA.cmake.
    
    Simplified the construction of CUDA_NOTFOUND_MESSAGE, and documented
    there that it is sometimes issued at the end of the main
    CMakeLists.txt (so that the error text is at the bottom of the
    terminal output).
    
    GMX_GPU is no longer required to be set for an OpenCL build (but
    causes no problems if it is set), and documented accordingly.
    
    Removed some logic that set CUDA_FOUND if CUDA_TOOLKIT_ROOT_DIR
    existed, since we encourage the user to set the latter (so it might be
    wrong), and we should anyway be relying on FindCUDA to set CUDA_FOUND.
    CUDA_TOOLKIT_ROOT_DIR now toggles between advanced and normal according
    to whether a CUDA build will occur.
    
    Fixes #1985
    
    Change-Id: I7df81dea738da3ec9cd3971ad3507298a9f97dff

---
+++ cmake/gmxManageCUDA.cmake
@@ -0,0 +1,275 @@
+#
+# This file is part of the GROMACS molecular simulation package.
+#
+# Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+# Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+# and including many others, as listed in the AUTHORS file in the
+# top-level source directory and at http://www.gromacs.org.
+#
+# GROMACS is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# GROMACS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with GROMACS; if not, see
+# http://www.gnu.org/licenses, or write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+#
+# If you want to redistribute modifications to GROMACS, please
+# consider that scientific software is very special. Version
+# control is crucial - bugs must be traceable. We will be happy to
+# consider code for inclusion in the official distribution, but
+# derived work must not be called official GROMACS. Details are found
+# in the README & COPYING files - if they are missing, get the
+# official version at http://www.gromacs.org.
+#
+# To help us fund GROMACS development, we humbly ask that you cite
+# the research papers on the package. Check out http://www.gromacs.org.
+
+# GMX_GPU has already been analysed to be on, auto or off.
+# In the latter cases, GMX_GPU_AUTO or GMX_GPU_FORCE is true.
+#
+# Off does nothing.
+# On requires that the build is not double precision, and that
+#   CUDA is found (but does not require finding a GPU).
+# Auto means we'll
+# - not build for CUDA if the build is double precision
+# - search for CUDA and set GMX_USE_CUDA=ON we find it.
+# - check whether GPUs are present, and issue a warning
+#   if CUDA is not found but GPUs were detected.
+
+include(CMakeDependentOption)
+include(gmxOptionUtilities)
+
+if (NOT GMX_GPU)
+    # The user turned GPU support off, there's nothing further to do
+    return()
+endif()
+# After this point, we know GMX_GPU was ON or AUTO, thus
+# exactly one of GMX_GPU_FORCE or GMX_GPU_AUTO is true.
+
+if(GMX_GPU_FORCE AND GMX_DOUBLE)
+    message(FATAL_ERROR "GPU acceleration is not available in double precision!")
+endif()
+
+if(GMX_GPU_AUTO AND GMX_DOUBLE)
+    message(WARNING "GPU acceleration is not available in double precision, disabled!")
+    set(GMX_USE_CUDA OFF)
+    return()
+endif()
+
+# Set up a variable that can later be changed to reflect whether
+# gmxManageCUDA has already been run for this build without exiting
+# with a fatal error.
+if (NOT GMX_MANAGE_CUDA_DONE)
+    set(GMX_MANAGE_CUDA_DONE FALSE CACHE INTERNAL "Whether gmxManageCUDA has already been run in this or a previous pass of CMake.")
+endif()
+
+# detect GPUs in the build host machine
+if (NOT GMX_MANAGE_CUDA_DONE)
+    include(gmxDetectGpu)
+    gmx_detect_gpu()
+endif()
+
+# We need to call find_package even when we've already done the detection/setup
+if(NOT GMX_GPU_FORCE AND NOT GMX_DETECT_GPU_AVAILABLE)
+    # Stay quiet when detection has occured and found no GPU.
+    # (Noise is acceptable when there is a GPU, or the user
+    # required one.)
+    set(FIND_CUDA_QUIETLY QUIET)
+endif()
+
+# CMake tries to use the static cuda runtime by default,
+# but this leads to unusable GPU builds on OS X.
+if(APPLE)
+    set(CUDA_USE_STATIC_CUDA_RUNTIME OFF CACHE STRING "Use the static version of the CUDA runtime library if available")
+endif()
+
+find_package(CUDA ${REQUIRED_CUDA_VERSION} ${FIND_CUDA_QUIETLY})
+
+# Annoyingly enough, FindCUDA leaves a few variables behind as
+# non-advanced.  We need to mark these advanced outside the
+# conditional, otherwise, if the user turns GMX_GPU=OFF, these
+# variables will be left behind in the non-advanced cache.
+mark_as_advanced(CUDA_SDK_ROOT_DIR
+                 CUDA_USE_STATIC_CUDA_RUNTIME
+                 CUDA_dl_LIBRARY CUDA_rt_LIBRARY
+                 )
+
+# Depending on the current values of GMX_GPU_* and perhaps CUDA_FOUND:
+# - GMX_GPU=OFF         : Skip this detection/setup (see above return() statement)
+# - GMX_GPU_AUTO =TRUE,
+#   CUDA_FOUND   =FALSE : Don't attempt a GPU build, but assemble
+#                         a warning message which will be issued at the end of the
+#                         configuration if GPU(s) are found below.
+# - GMX_GPU_FORCE=TRUE,
+#   CUDA_FOUND   =FALSE : The user requested GPU build and this requires CUDA, so we will
+#                         fail if it is not available.
+# - GMX_GPU_AUTO =TRUE,
+#   CUDA_FOUND   =TRUE  : Go ahead and set GMX_USE_CUDA.
+# - GMX_GPU_FORCE=TRUE,
+#   CUDA_FOUND   =TRUE  : Go ahead and set GMX_USE_CUDA.
+
+if (NOT CUDA_FOUND)
+    if (GMX_DETECT_GPU_AVAILABLE)
+        # assemble warning/error message
+        set(CUDA_NOTFOUND_MESSAGE "mdrun supports native GPU acceleration on NVIDIA hardware with compute capability >= ${REQUIRED_CUDA_COMPUTE_CAPABILITY} (Fermi or later). This requires the NVIDIA CUDA toolkit, which was not found. Its location can be hinted by setting the CUDA_TOOLKIT_ROOT_DIR CMake option (does not work as an environment variable). The typical location would be /usr/local/cuda[-version]. Note that CPU or GPU acceleration can be selected at runtime.
+
+${GMX_DETECT_GPU_COUNT} NVIDIA GPU(s) found in the system")
+
+        # append GPU names
+        if (NOT GMX_DETECT_GPU_INFO STREQUAL "")
+            set(CUDA_NOTFOUND_MESSAGE "${CUDA_NOTFOUND_MESSAGE}:")
+            foreach(gpu ${GMX_DETECT_GPU_INFO})
+                set(CUDA_NOTFOUND_MESSAGE "${CUDA_NOTFOUND_MESSAGE}
+${gpu}")
+            endforeach()
+        endif()
+
+        # TODO remove the second part of the message when we'll have compute
+        # capability information from the detection.
+        set(CUDA_NOTFOUND_MESSAGE "${CUDA_NOTFOUND_MESSAGE}
+Compute capability information not available, consult the NVIDIA website:
+https://developer.nvidia.com/cuda-gpus")
+    else()
+        # Don't issue warnings if we found neither CUDA or a GPU
+        unset(CUDA_NOTFOUND_MESSAGE)
+    endif()
+
+    if (GMX_GPU_AUTO)
+        message(STATUS "No compatible CUDA toolkit found (v5.0+), disabling native GPU acceleration")
+        set(GMX_USE_CUDA OFF)
+        # A possible warning message constructed from
+        # CUDA_NOTFOUND_MESSAGE is issued at the end of the main
+        # CMakeLists.txt, unless we already did this in a previous
+        # pass of CMake.
+        if (GMX_MANAGE_CUDA_DONE)
+            unset(CUDA_NOTFOUND_MESSAGE)
+        endif()
+        return()
+    endif()
+    if (GMX_GPU_FORCE)
+        # the user requested CUDA, but it wasn't found
+        message(FATAL_ERROR "${CUDA_NOTFOUND_MESSAGE}")
+    endif()
+else()
+    if (NOT GMX_MANAGE_CUDA_DONE)
+        # Only show this message on the first pass
+        message(STATUS "Enabling native CUDA GPU acceleration")
+    endif()
+    set(GMX_USE_CUDA ON)
+endif()
+
+# After here, we've found CUDA and are building with it.
+
+# Try to find NVML if a GPU accelerated binary should be build.
+if (DEFINED NVML_LIBRARY)
+    set(NVML_FIND_QUIETLY TRUE)
+endif()
+find_package(NVML)
+option(GMX_USE_NVML "Use NVML support for better CUDA performance" ${NVML_FOUND})
+mark_as_advanced(GMX_USE_NVML)
+if(GMX_USE_NVML)
+    if(NVML_FOUND)
+        include_directories(SYSTEM ${NVML_INCLUDE_DIR})
+        set(HAVE_NVML 1)
+        list(APPEND GMX_EXTRA_LIBRARIES ${NVML_LIBRARY})
+    else()
+        message(FATAL_ERROR "NVML support was required, but was not detected. Please consult the install guide.")
+    endif()
+endif()
+
+# Try to execute ${CUDA_NVCC_EXECUTABLE} --version and set the output
+# (or an error string) in the argument variable.
+# Note that semicolon is used as separator for nvcc.
+#
+# Parameters:
+#   COMPILER_INFO   - [output variable] string with compiler path, ID and
+#                     some compiler-provided information
+#   COMPILER_FLAGS  - [output variable] flags for the compiler
+#
+macro(get_cuda_compiler_info COMPILER_INFO COMPILER_FLAGS)
+    if(NOT CUDA_NVCC_EXECUTABLE)
+        message(FATAL_ERROR "Need nvcc executable to get CUDA compiler information")
+    endif()
+
+    # Get the nvcc version string. This is multi-line, but since it is only 4 lines
+    # and might change in the future it is better to store than trying to parse out
+    # the version from the current format.
+    execute_process(COMMAND ${CUDA_NVCC_EXECUTABLE} --version
+        RESULT_VARIABLE _nvcc_version_res
+        OUTPUT_VARIABLE _nvcc_version_out
+        ERROR_VARIABLE  _nvcc_version_err
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if (${_nvcc_version_res} EQUAL 0)
+        # Fix multi-line mess: Replace newline with ";" so we can use it in a define
+        string(REPLACE "\n" ";" _nvcc_info_singleline ${_nvcc_version_out})
+        SET(${COMPILER_INFO} "${CUDA_NVCC_EXECUTABLE} ${_nvcc_info_singleline}")
+        string(TOUPPER ${CMAKE_BUILD_TYPE} _build_type)
+        SET(_compiler_flags "${CUDA_NVCC_FLAGS_${_build_type}}")
+        if(CUDA_PROPAGATE_HOST_FLAGS)
+            string(REGEX REPLACE "[ ]+" ";" _cxx_flags_nospace "${BUILD_CXXFLAGS}")
+        endif()
+        SET(${COMPILER_FLAGS} "${CUDA_NVCC_FLAGS}${CUDA_NVCC_FLAGS_${_build_type}}; ${_cxx_flags_nospace}")
+    else()
+        SET(${COMPILER_INFO} "N/A")
+        SET(${COMPILER_FLAGS} "N/A")
+    endif()
+endmacro ()
+
+if (NOT_CUDA_NVCC_EXECUTABLE)
+    message(FATAL_ERROR "nvcc is required for a CUDA build, please set CUDA_TOOLKIT_ROOT_DIR appropriately")
+endif()
+
+# set up nvcc options
+include(gmxManageNvccConfig)
+
+gmx_check_if_changed(_cuda_version_changed CUDA_VERSION)
+
+# Generate CUDA RT API version string which will end up in config.h
+# We do this because nvcc is silly enough to not define its own version
+# (which should match the CUDA runtime API version AFAICT) and we want to
+# avoid creating the fragile dependency on cuda_runtime_api.h.
+#
+# NOTE: CUDA v7.5 is expected to have nvcc define it own version, so in the
+# future we should switch to using that version string instead of our own.
+MATH(EXPR GMX_CUDA_VERSION "${CUDA_VERSION_MAJOR}*1000 + ${CUDA_VERSION_MINOR}*10")
+if (_cuda_version_changed)
+    # check the generated CUDA API version against the one present in cuda_runtime_api.h
+
+    try_compile(_get_cuda_version_compile_res
+        ${CMAKE_BINARY_DIR}
+        ${CMAKE_SOURCE_DIR}/cmake/TestCUDAVersion.c
+        COMPILE_DEFINITIONS "-DGMX_CUDA_VERSION=${GMX_CUDA_VERSION}"
+        CMAKE_FLAGS "-DINCLUDE_DIRECTORIES=${CUDA_TOOLKIT_INCLUDE}"
+        OUTPUT_VARIABLE _get_cuda_version_compile_out)
+
+    if (NOT _get_cuda_version_compile_res)
+        if (_get_cuda_version_compile_out MATCHES "CUDA version mismatch")
+            message(FATAL_ERROR "The CUDA API version generated internally from the compiler version does not match the version reported by cuda.h. This means either that the CUDA detection picked up mismatching nvcc and the CUDA headers (likely not part of the same toolkit installation) or that there is an error in the internal version generation. If you are sure that it is not the former causing the error (check the relevant cache variables), define the GMX_CUDA_VERSION cache variable to work around the error.")
+        else()
+            message(FATAL_ERROR "Could not detect CUDA runtime API version")
+        endif()
+    endif()
+endif()
+
+# We need to use single compilation unit for kernels:
+# - when compiling for CC 2.x devices where buggy kernel code is generated
+gmx_check_if_changed(_gmx_cuda_target_changed GMX_CUDA_TARGET_SM GMX_CUDA_TARGET_COMPUTE CUDA_NVCC_FLAGS)
+# Set this property if the target has changed, or it's the first time we're here
+if(_gmx_cuda_target_changed OR NOT GMX_MANAGE_CUDA_DONE)
+    if((NOT GMX_CUDA_TARGET_SM AND NOT GMX_CUDA_TARGET_COMPUTE) OR
+            (GMX_CUDA_TARGET_SM MATCHES "2[01]" OR GMX_CUDA_TARGET_COMPUTE MATCHES "2[01]"))
+        message(STATUS "Enabling single compilation unit for the CUDA non-bonded module. Multiple compilation units are not compatible with CC 2.x devices, to enable the feature specify only CC >=3.0 target architectures in GMX_CUDA_TARGET_SM/GMX_CUDA_TARGET_COMPUTE.")
+        set_property(CACHE GMX_CUDA_NB_SINGLE_COMPILATION_UNIT PROPERTY VALUE ON)
+    else()
+        message(STATUS "Enabling multiple compilation units for the CUDA non-bonded module.")
+        set_property(CACHE GMX_CUDA_NB_SINGLE_COMPILATION_UNIT PROPERTY VALUE OFF)
+    endif()
+endif()
