e2f9fb3b9d3f3d3d78e0b479f57f0a4cb9e11af8
Berk Hess <hess@kth.se>
2016-01-27 14:16:04 +0100

    Replaced nbnxn search+grid simd macros
    Replaced macros in the nbnxn search and grid code by templates,
    functions and static const int.
    Moved macros and constants only used in search+grid from
    nbnxn_consts.h to nbnxn_internal.h.
    Converted all switch macros in nbnxn_internal.h to 0/1.
    
    Change-Id: Iec6fcc7c4aca5c2a0276781048f851dfb709fdc5

--- src/gromacs/mdlib/nbnxn_consts.h
+++ src/gromacs/mdlib/nbnxn_consts.h
@@ -59,21 +59,6 @@ extern "C" {
 #endif
 
 
-/* Strides for x/f with xyz and xyzq coordinate (and charge) storage */
-#define STRIDE_XYZ   3
-#define STRIDE_XYZQ  4
-/* Size of packs of x, y or z with SSE/AVX packed coords/forces */
-#define PACK_X4      4
-#define PACK_X8      8
-/* Strides for a pack of 4 and 8 coordinates/forces */
-#define STRIDE_P4    (DIM*PACK_X4)
-#define STRIDE_P8    (DIM*PACK_X8)
-
-/* Index of atom a into the SSE/AVX coordinate/force array */
-#define X4_IND_A(a)  (STRIDE_P4*((a) >> 2) + ((a) & (PACK_X4 - 1)))
-#define X8_IND_A(a)  (STRIDE_P8*((a) >> 3) + ((a) & (PACK_X8 - 1)))
-
-
 /* Cluster-pair Interaction masks for 4xN and 2xNN kernels.
  * Bit i*CJ_SIZE + j tells if atom i and j interact.
  */
