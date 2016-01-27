8c592fa083c5ad3cb48af61e6c8ebbdf4d545835
Mark Abraham <mark.j.abraham@gmail.com>
2016-01-27 15:14:19 +0100

    Fix grompp not warning when .mdp values have wrong types
    With old code, e.g. init_lambda_state = 0.35 was silently
    interpreted as 0.
    
    Added some comments and TODOs.
    
    Added unit tests for some simple parts of the the functionality that
    needed to be fixed. Extended the interface of warninp_t to permit
    sound testing.
    
    Fixes #1893
    
    Change-Id: I425798daa8ed3a391cde49ba4a2d3d352d682f8f

--- src/gromacs/gmxlib/warninp.c
+++ src/gromacs/gmxlib/warninp.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -177,6 +177,11 @@ void check_warning_error(warninp_t wi, int f_errno, const char *file, int line)
     }
 }
 
+gmx_bool warning_errors_exist(warninp_t wi)
+{
+    return (wi->nwarn_error > 0);
+}
+
 void done_warning(warninp_t wi, int f_errno, const char *file, int line)
 {
     print_warn_count("note", wi->nwarn_note);
@@ -192,6 +197,11 @@ void done_warning(warninp_t wi, int f_errno, const char *file, int line)
                   wi->nwarn_warn, ShortProgram());
     }
 
+    free_warning(wi);
+}
+
+void free_warning(warninp_t wi)
+{
     sfree(wi);
 }
 
