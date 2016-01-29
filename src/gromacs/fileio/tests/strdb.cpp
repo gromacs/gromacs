984372409613f190bbc3e11d3f4410706fff5d0b
Mark Abraham <mark.j.abraham@gmail.com>
2016-01-29 16:40:32 +0100

    Add tests and comments on get_a_line
    Change-Id: Ic551adaccac24d5863c66fba11c0aaf42a6111e7

---
+++ src/gromacs/fileio/tests/strdb.cpp
@@ -0,0 +1,104 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Tests utilities for strdb routines
+ *
+ * \author Mark Abraham <mark.j.abraham@gmail.com>
+ */
+#include "gmxpre.h"
+
+#include "../strdb.h"
+
+#include <stdio.h>
+
+#include <gtest/gtest.h>
+
+#include "gromacs/utility/scoped_cptr.h"
+#include "gromacs/utility/cstringutil.h"
+#include "gromacs/utility/smalloc.h"
+
+namespace gmx
+{
+namespace testing
+{
+
+class get_a_lineTest : public ::testing::Test
+{
+    public:
+        get_a_lineTest() {}
+
+        void runTest(const char *input, const char *expectedOutput)
+        {
+            std::string lineToParse(input), output(input);
+
+            char       *buffer = const_cast<char *>(lineToParse.c_str());
+            // Trust fmemopen not to break buffer
+            FILE       *memstream = fmemopen(buffer, lineToParse.size()+1, "r");
+
+            buffer = const_cast<char *>(output.c_str());
+            // Trust get_a_line not to break buffer
+            get_a_line(memstream, buffer, output.size()+1);
+            fclose(memstream);
+
+            ASSERT_STREQ(expectedOutput, output.c_str());
+        }
+};
+
+TEST_F(get_a_lineTest, ParsesValidSingleLines)
+{
+    runTest("mdp-option = 1\n", "mdp-option = 1");
+    runTest("mdp-option = 1", "mdp-option = 1");
+    runTest("  mdp-option = 1\n", "mdp-option = 1");
+    runTest("  mdp-option = 1", "mdp-option = 1");
+    runTest("mdp-option = 1;comment\n", "mdp-option = 1");
+    runTest("mdp-option = 1; comment\n", "mdp-option = 1");
+    runTest("mdp-option = 1  ; comment\n", "mdp-option = 1");
+    runTest("  mdp-option = 1  ; comment\n", "mdp-option = 1");
+    runTest(";mdp-option = 1  ; comment\n", "");
+    runTest("", "");
+    runTest("   ", "   "); // Can't skip this line because there is no next line
+    runTest("\n", "");
+}
+
+TEST_F(get_a_lineTest, ParsesValidMultipleLines)
+{
+    runTest("mdp-option = 1\nmdp-option = 2\n", "mdp-option = 1");
+    runTest("\nmdp-option = 1\n", "mdp-option = 1");
+    runTest("   \nmdp-option = 1\n", "mdp-option = 1");
+}
+
+} // namespace
+} // namespace
--- src/gromacs/utility/strdb.cpp
+++ src/gromacs/utility/strdb.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -56,6 +56,7 @@ gmx_bool get_a_line(FILE *fp, char line[], int n)
 
     do
     {
+        // Try to read a line
         if (!fgets(line0, n+1, fp))
         {
             sfree(line0);
@@ -64,26 +65,35 @@ gmx_bool get_a_line(FILE *fp, char line[], int n)
         dum = std::strchr(line0, '\n');
         if (dum)
         {
+            // A newline was found, so make that the end of the string
             dum[0] = '\0';
         }
         else if (static_cast<int>(std::strlen(line0)) == n)
         {
+            // No newline or null terminator was found in the string
             fprintf(stderr, "Warning: line length exceeds buffer length (%d), data might be corrupted\n", n);
             line0[n-1] = '\0';
         }
         else
         {
+            // No newline was found in the string
             fprintf(stderr, "Warning: file does not end with a newline, last line:\n%s\n",
                     line0);
         }
+        // Find the first semicolon, which would start a comment
         dum = std::strchr(line0, ';');
         if (dum)
         {
+            // Found a comment, so delete the rest of the line
             dum[0] = '\0';
         }
+        // Copy the edited version of string into the return buffer
         std::strncpy(line, line0, n);
+        // Set dum to the start of the edited version of the string
         dum = line0;
+        // If the line originally contained at most whitespace, organize to skip it
         ltrim(dum);
+        // Loop to get the next line if this line parsed equivalent to a blank line
     }
     while (dum[0] == '\0');
 
