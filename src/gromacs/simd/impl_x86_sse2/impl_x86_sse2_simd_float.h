df3918e5f79913918fdfdb6b12902a457ca2ff06
Berk Hess <hess@kth.se>
2016-02-03 14:53:13 +0100

    [WIP] Added scalars to SIMD load proxies
    To be able to use the same templates for SIMD and scalar code, we need
    to be able to use load() for scalar equivalents of SIMD registers.
    To achieve this, the load() functions for SimdFloat and SimdDouble
    in the implementations needed to be renamed.
    
    TODO: Implementations currently only updated for SSE and AVX.
    
    Change-Id: Ia6490eb2b222ca2cf3c2ac21ae692c73154aef8b

--- src/gromacs/simd/impl_x86_sse2/impl_x86_sse2_simd_float.h
+++ src/gromacs/simd/impl_x86_sse2/impl_x86_sse2_simd_float.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -99,7 +99,7 @@ class SimdFIBool
 };
 
 static inline SimdFloat gmx_simdcall
-load(const float *m)
+loadF(const float *m)
 {
     assert(std::size_t(m) % 16 == 0);
     return {
