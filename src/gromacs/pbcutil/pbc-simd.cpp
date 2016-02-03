1452720c49f693d7d5d371a3292a5cd58d8ef7db
Berk Hess <hess@kth.se>
2016-02-03 13:24:02 +0100

    Add SIMD version of pbc_dx_aiuc
    Also support PBC struct with no PBC in set_pbc_simd and NULL box
    pointer to set_pbc.
    
    Change-Id: I4dc0829c2160a2e14720e5d69027385d919c149d

--- src/gromacs/pbcutil/pbc-simd.cpp
+++ src/gromacs/pbcutil/pbc-simd.cpp
@@ -54,7 +54,7 @@ void set_pbc_simd(const t_pbc gmx_unused *pbc,
                   real gmx_unused        *pbc_simd)
 {
 #if GMX_SIMD_HAVE_REAL
-    if (pbc != NULL)
+    if (pbc != NULL && pbc->ePBC != epbcNONE)
     {
         rvec inv_box_diag = {0, 0, 0};
 
@@ -75,6 +75,7 @@ void set_pbc_simd(const t_pbc gmx_unused *pbc,
     }
     else
     {
+        /* Setting inv_box_diag to zero leads to no PBC being applied */
         for (int i = 0; i < (DIM + DIM*(DIM+1)/2); i++)
         {
             store(pbc_simd + i*GMX_SIMD_REAL_WIDTH, SimdReal(0));
