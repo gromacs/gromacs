1ab2eb0130d34d9db1c1463ff6358598766b632e
Mark Abraham <mark.j.abraham@gmail.com>
2016-02-12 10:18:49 +0100

    Improved some mdrun integration tests
    Made some better naming.
    
    Avoided using the whole of OPLS/AA just to get water parameters, which
    may have been unstable when calling grompp many times in a binary, for
    reasons unknown.
    
    Removed warning suppressions for unsupported icc compiler versions.
    
    Change-Id: I5107fca64f6a73e3c0b85146f28347453ded705a

--- src/programs/mdrun/tests/trajectory_writing.cpp
+++ src/programs/mdrun/tests/trajectory_writing.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -129,17 +129,13 @@ TEST_P(NptTrajectories, WithDifferentPcoupl)
     runTest();
 }
 
-#ifdef __INTEL_COMPILER
-#pragma warning( disable : 177 )
-#endif
-
 // TODO Consider spamming more of the parameter space when we don't
 // have to write .mdp and .tpr files to do it.
-INSTANTIATE_TEST_CASE_P(CanWrite,
+INSTANTIATE_TEST_CASE_P(MdrunCanWrite,
                         Trajectories,
                             ::testing::Values("1", "2", "3"));
 
-INSTANTIATE_TEST_CASE_P(CanWrite,
+INSTANTIATE_TEST_CASE_P(MdrunCanWrite,
                         NptTrajectories,
                             ::testing::Values("no", "Berendsen", "Parrinello-Rahman"));
 
