8644b29882142b14b18a68e4a73356fcf64c1aaf
Erik Lindahl <erik@kth.se>
2016-02-21 22:31:01 +0100

    New class for quadratic spline interpolation table
    Tables can be constructed either from analytical
    functions or numerical vector input, and they can contain
    an arbitrary number of functions as multi-tables.
    Spacing is optimized to achieve the requested precision.
    To make the class more generic it works with functions and
    derivatives, and avoids worrying about swapping signs to get
    forces. The interpolation functions are templated and will also
    work with SIMD, without exposing any table internals.
    
    Change-Id: If75f52b0601ad4b396d3cef74b5bbbe81ba91753

--- src/gromacs/utility/errorcodes.cpp
+++ src/gromacs/utility/errorcodes.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -64,6 +64,7 @@ const char *const error_names[] =
     "System I/O error",
     "Error in user input",
     "Inconsistency in user input",
+    "Requested tolerance cannot be achieved",
     "Simulation instability detected",
 
     "Feature not implemented",
