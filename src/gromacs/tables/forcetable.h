9a14454a42a6dab6d714aa6f2400bd6ce8fe4903
Erik Lindahl <erik@kth.se>
2016-02-21 22:31:01 +0100

    New class for quadratic spline interpolation table
    Tables can be constructed either from analytical
    functions or numerical vector input, and they can contain
    an arbitrary number of functions as multi-tables.
    Spacing is optimized to achieve the requested precision.
    To make the class more generic it works with functions and
    derivatives, and avoids worrying about swapping signs to get
    forces. The interpolation functions are templated and will also
    work with SIMD, without exposing any table internals.
    
    Change-Id: If75f52b0601ad4b396d3cef74b5bbbe81ba91753

--- src/gromacs/tables/forcetable.h
+++ src/gromacs/tables/forcetable.h
@@ -35,6 +35,15 @@
 #ifndef GMX_TABLES_FORCETABLE_H
 #define GMX_TABLES_FORCETABLE_H
 
+/*! \libinternal \file
+ * \brief
+ * Old routines for table generation (will eventually be replaced)
+ *
+ * \inlibraryapi
+ * \author Erik Lindahl <erik.lindahl@gmail.com>
+ * \ingroup module_tables
+ */
+
 #include <cstdio>
 
 #include "gromacs/mdtypes/fcdata.h"
@@ -42,19 +51,44 @@
 #include "gromacs/mdtypes/interaction_const.h"
 #include "gromacs/utility/real.h"
 
+/*! \brief Flag to select user tables for make_tables */
 #define GMX_MAKETABLES_FORCEUSER  (1<<0)
+/*! \brief Flag to only make 1,4 pair tables for make_tables */
 #define GMX_MAKETABLES_14ONLY     (1<<1)
 
-/* Index in the tables that says which function to use */
+/*! \brief Enumerated type to describe the interaction types in a table */
 enum {
-    etiCOUL, etiLJ6, etiLJ12, etiNR
+    etiCOUL,  //!< Coulomb
+    etiLJ6,   //!< Dispersion
+    etiLJ12,  //!< Repulsion
+    etiNR     //!< Total number of interaction types
 };
 
-typedef double (*real_space_grid_contribution_computer)(double, double);
-/* Function pointer used to tell table_spline3_fill_ewald_lr whether it
+/*! \brief Function pointer to calculate the grid contribution for coulomb/LJ
+ *
+ * Used to tell table_spline3_fill_ewald_lr whether it
  * should calculate the grid contribution for electrostatics or LJ.
  */
+typedef double (*real_space_grid_contribution_computer)(double, double);
 
+
+/*! \brief Fill tables with the Ewald long-range force interaction
+ *
+ * Fill tables of ntab points with spacing dr with the ewald long-range
+ * (mesh) force.
+ * There are three separate tables with format FDV0, F, and V.
+ * This function interpolates the Ewald mesh potential contribution
+ * with coefficient beta using a quadratic spline.
+ * The force can then be interpolated linearly.
+ *
+ * \param table_F    Force table
+ * \param table_V    Potential table
+ * \param table_FDV0 Combined table optimized for SIMD loads
+ * \param ntab       Number of points in tables
+ * \param dx         Spacing
+ * \param beta       Ewald splitting paramter
+ * \param v_lr       Pointer to function calculating real-space grid contribution
+ */
 void table_spline3_fill_ewald_lr(real                                 *table_F,
                                  real                                 *table_V,
                                  real                                 *table_FDV0,
@@ -62,34 +96,58 @@ void table_spline3_fill_ewald_lr(real                                 *table_F,
                                  double                                dx,
                                  real                                  beta,
                                  real_space_grid_contribution_computer v_lr);
-/* Fill tables of ntab points with spacing dr with the ewald long-range
- * (mesh) force.
- * There are three separate tables with format FDV0, F, and V.
- * This function interpolates the Ewald mesh potential contribution
- * with coefficient beta using a quadratic spline.
- * The force can then be interpolated linearly.
+
+/*! \brief Compute scaling for the Ewald quadratic spline tables.
+ *
+ * \param ic  Pointer to interaction constant structure
+ * \return The scaling factor
  */
-
 real ewald_spline3_table_scale(const interaction_const_t *ic);
-/* Return the scaling for the Ewald quadratic spline tables. */
 
+/*! \brief Return the real space grid contribution for Ewald
+ *
+ *  \param beta  Ewald splitting parameter
+ *  \param r     Distance for which to calculate the real-space contrib
+ *  \return      Real space grid contribution for Ewald electrostatics
+ */
 double v_q_ewald_lr(double beta, double r);
-/* Return the real space grid contribution for Ewald*/
 
+/*! \brief Return the real space grid contribution for LJ-Ewald
+ *
+ *  \param beta  Ewald splitting parameter
+ *  \param r     Distance for which to calculate the real-space contrib
+ *  \return      Real space grid contribution for Ewald Lennard-Jones interaction
+ */
 double v_lj_ewald_lr(double beta, double r);
-/* Return the real space grid contribution for LJ-Ewald*/
 
+/*! \brief Return tables for inner loops.
+ *
+ * \param fp     Log file pointer
+ * \param fr     Force record
+ * \param fn     File name from which to read user tables
+ * \param rtab   Largest interaction distance to tabulate
+ * \param flags  Flags to select table settings
+ *
+ * \return Pointer to inner loop table structure
+ */
 t_forcetable *make_tables(FILE *fp,
                           const t_forcerec *fr,
                           const char *fn, real rtab, int flags);
-/* Return tables for inner loops. */
 
-bondedtable_t make_bonded_table(FILE *fplog, char *fn, int angle);
-/* Return a table for bonded interactions,
- * angle should be: bonds 0, angles 1, dihedrals 2
+/*! \brief Return a table for bonded interactions,
+ *
+ * \param  fplog   Pointer to log file
+ * \param  fn      File name
+ * \param  angle   Type of angle: bonds 0, angles 1, dihedrals 2
+ * \return New bonded table datatype
  */
+bondedtable_t make_bonded_table(FILE *fplog, char *fn, int angle);
 
-/* Return a table for GB calculations */
+/*! \brief Return a table for GB calculations
+ *
+ * \param fr   Force record
+ * \return     Pointer to new gb table structure
+ */
 t_forcetable *make_gb_table(const t_forcerec              *fr);
 
 /*! \brief Construct and return tabulated dispersion and repulsion interactions
