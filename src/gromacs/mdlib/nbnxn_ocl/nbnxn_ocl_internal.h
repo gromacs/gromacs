5874c665e43eebd9b0a22b2c4671518816c7b580
Szilárd Páll <pall.szilard@gmail.com>
2016-03-01 21:30:18 +0100

    LJ combination rule kernels for OpenCL
    The current implementation enables combination rules for both AMD and
    NVIDIA OpenCL (also ports the changes to the "nowarp" test/CPU kernel).
    
    Like in the CUDA implementation, all kernels support it, but only for
    plain cut-off are combination rules used.
    
    Notes:
    - On AMD tested on Hawaii, Fiji, Spectre and Oland devices;
      combination rules in all cases improve performance, although combined
      with the i-prefetching, the improvement is typically only ~10%.
    - On NVIDIA tested on Kepler and Maxwell; in most cases the combination
      rule kernels are fastest.
      However, with certain inputs these kernels are 25% slower on Maxwell
      (e.g. pure water box, cut-off LJ, pot shift), but not on Kepler.
      This is likely a compiler mis-optimization, so we'll just leave the
      defaults the same as AMD.
    
    Change-Id: I05396e000cdf93c1d872729e6b477192af152495

---
+++ src/gromacs/mdlib/nbnxn_ocl/nbnxn_ocl_internal.h
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ *  \brief Internal API of the OpenCL non-bonded module.
+ *
+ *  \author Szilárd Páll <pall.szilard@gmail.com>
+ *  \ingroup module_mdlib
+ */
+
+#include "gmxpre.h"
+
+#include "nbnxn_ocl_types.h"
+
+#ifndef NBNXN_OCL_INTERNAL_H
+#define NBNXN_OCL_INTERNAL_H
+
+/*! \brief Returns true if LJ combination rules are used in the non-bonded kernels.
+ *
+ *  \param[in] vdwType The VdW interaction/implementation type as defined by evdwOcl in nbnxn_ocl_types.h.
+ *  \returns           True if combination rules are used by the run
+ */
+bool useLjCombRule(int vdwType);
+
+#endif /* NBNXN_OCL_INTERNAL_H */
