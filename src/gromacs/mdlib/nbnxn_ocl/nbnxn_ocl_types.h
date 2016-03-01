f46b7214b95ff8ef237b82dd8555e915ebf0da02
Szilárd Páll <pall.szilard@gmail.com>
2016-03-01 21:30:18 +0100

    [WIP] LJ combination rule kernels for OpenCL
    The current implementation enables combination rules for both AMD and
    NVIDIA OpenCL.
    
    This change also restores the i-atom type prefetch and also enables it
    for the LJ parameter lookup.
    
    TODO:
    - test on more architectures, currently tested on AMD Fiji and Hawaii
    - need more tests on NVIDIA, with current compilers on Fermi both LJ
      combination rules and prefetch improves performance dramatically (20%
      and 10%, resp.), but on Maxwell both tweaks deteriorate performance.
    
    Change-Id: I05396e000cdf93c1d872729e6b477192af152495

--- src/gromacs/mdlib/nbnxn_ocl/nbnxn_ocl_types.h
+++ src/gromacs/mdlib/nbnxn_ocl/nbnxn_ocl_types.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -97,8 +97,13 @@ enum eelOcl {
  * should match the order of enumerated types below.
  */
 enum evdwOcl {
-    evdwOclCUT, evdwOclFSWITCH, evdwOclPSWITCH, evdwOclEWALDGEOM, evdwOclEWALDLB, evdwOclNR
+    evdwOclCUT, evdwOclCUTCOMBGEOM, evdwOclCUTCOMBLB, evdwOclFSWITCH, evdwOclPSWITCH, evdwOclEWALDGEOM, evdwOclEWALDLB, evdwOclNR
 };
+
+// FIXME
+// #if defined LJ_COMB_GEOM || defined LJ_COMB_LB
+// #define LJ_COMB
+// #endif
 
 /*! \internal
  * \brief Staging area for temporary data downloaded from the GPU.
@@ -135,6 +140,7 @@ typedef struct cl_atomdata
 
     int         ntypes;              /**< number of atom types                           */
     cl_mem      atom_types;          /**< int buffer with atom type indices, size natoms */
+    cl_mem      lj_comb;             /**< float2 buffer with sqrt(c6),sqrt(c12), size natoms */
 
     cl_mem      shift_vec;           /**< float3 buffer with shifts values               */
     size_t      shift_vec_elem_size; /**< Size in bytes for one element of shift_vec buffer */
