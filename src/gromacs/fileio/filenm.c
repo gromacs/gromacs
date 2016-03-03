be274f84be2e3e98ea4fa67b3f6e6ace438cf660
Mark Abraham <mark.j.abraham@gmail.com>
2016-03-03 16:04:16 +0100

    Fix reading of bonded tables
    The old approach specified only a filename prefix, and init_forcerec
    would expect to be able to append _b0 to generate the filename for the
    zeroth bonded table, etc. This failed a check in early command-line
    parsing when no file that matched just the prefix could be found.
    Pairs are unaffected because there's only one table. Short-ranged
    energy-group tables are not affected because they require that the
    file with default name exists.
    
    Command-line parsing now requires -tableb to name all tabulated bonded
    interaction table files explicitly, and does the normal check that
    they exist. init_forcerec will find any name in that list that matches
    the required pattern. The concept of a default filename prefix for
    tabulated bonded interactions no longer applies, but the old
    'table_b0.xvg' naming still works. Added some comments and Doxygen to
    explain what the old code was doing. Amended the user documentation
    suitably.
    
    One could add further checks e.g. that there are no table filenames
    provided that are unused by the topology, but it is likely the need
    for such checks will go away when the topology will name the files
    directly.
    
    Add some integration tests to show the functionality no longer
    produces an error. Infrastructure to actually verify energies and
    forces is waiting for review in Gerrit.
    
    Expanded some string- and filename-handling infrastructure to meet
    the new needs.
    
    Fixes #1913
    
    Change-Id: If0a7cd69a40eb95fbda20eaaf4c3002782772f0e

--- src/gromacs/fileio/filenm.c
+++ src/gromacs/fileio/filenm.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -359,16 +359,25 @@ int fn2ftp(const char *fn)
     return i;
 }
 
-const char *opt2fn(const char *opt, int nfile, const t_filenm fnm[])
+const t_filenm *getFilenm(const char *opt, int nfile, const t_filenm fnm[])
 {
-    int i;
-
-    for (i = 0; (i < nfile); i++)
+    for (int i = 0; (i < nfile); i++)
     {
         if (strcmp(opt, fnm[i].opt) == 0)
         {
-            return fnm[i].fns[0];
+            return &fnm[i];
         }
+    }
+
+    return NULL;
+}
+
+const char *opt2fn(const char *opt, int nfile, const t_filenm fnm[])
+{
+    const t_filenm *f = getFilenm(opt, nfile, fnm);
+    if (f != NULL)
+    {
+        return f->fns[0];
     }
 
     fprintf(stderr, "No option %s\n", opt);
