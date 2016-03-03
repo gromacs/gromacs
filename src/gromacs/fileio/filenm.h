1e8dec802b86078cd3710d06fc09309b1a5f7f28
Mark Abraham <mark.j.abraham@gmail.com>
2016-03-03 16:04:16 +0100

    Fix reading of bonded tables
    The old approach specified only a filename prefix, and init_forcerec
    would expect to be able to append _b0 to generate the filename for the
    zeroth bonded table, etc. This failed a check in early command-line
    parsing when no file that matched just the prefix could be found.
    Pairs are unaffected because there's only one table. Short-ranged
    energy-group tables are not affected because they require that the
    file with default name exists.
    
    Command-line parsing now requires -tableb to name all tabulated bonded
    interaction table files explicitly, and does the normal check that
    they exist. init_forcerec will find any name in that list that matches
    the required pattern. The concept of a default filename prefix for
    tabulated bonded interactions no longer applies, but the old
    'table_b0.xvg' naming still works. Added some comments and Doxygen to
    explain what the old code was doing. Amended the user documentation
    suitably.
    
    One could add further checks e.g. that there are no table filenames
    provided that are unused by the topology, but it is likely the need
    for such checks will go away when the topology will name the files
    directly.
    
    Add some integration tests to show the functionality no longer
    produces an error. Infrastructure to actually verify energies and
    forces is waiting for review in Gerrit.
    
    Expanded some string- and filename-handling infrastructure to meet
    the new needs.
    
    Improved the robustness of the check that the derivative column is
    approximately the negative of the numerical derivative of the value
    column.
    
    Fixes #1913
    
    Change-Id: If0a7cd69a40eb95fbda20eaaf4c3002782772f0e

--- src/gromacs/fileio/filenm.h
+++ src/gromacs/fileio/filenm.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -136,6 +136,12 @@ int opt2fns(char **fns[], const char *opt, int nfile,
             const t_filenm fnm[]);
 /* Return the filenames belonging to cmd-line option opt, or NULL when
  * no such option. */
+
+/*! \brief
+ * Return a pointer to the t_filenm data structure of filenames belonging to
+ * command-line option opt, or NULL when no such option was used.
+ */
+const t_filenm *getFilenm(const char *opt, int nfile, const t_filenm fnm[]);
 
 #define opt2FILE(opt, nfile, fnm, mode) gmx_ffopen(opt2fn(opt, nfile, fnm), mode)
 /* Return a file pointer from the filename (see above) */
