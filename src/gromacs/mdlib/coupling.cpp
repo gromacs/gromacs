5e7178f07cb86044cc1c679a84817e9f9f31bc94
Berk Hess <hess@kth.se>
2016-03-17 22:18:01 +0100

    Correct nrdf for 1D/2D systems
    With COMM removal, grompp would subtract degrees of freedom also for
    VCM groups with fully frozen dimensions, i.e. 1D/2D systems.
    Also fixed division by zero for groups with #DOF=0 with VV.
    
    Fixes #1923.
    
    Change-Id: I0ba2535df0495947d9bbb6ee1ef29f519635c221

--- src/gromacs/mdlib/coupling.cpp
+++ src/gromacs/mdlib/coupling.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1029,7 +1029,7 @@ extern void init_npt_masses(t_inputrec *ir, t_state *state, t_extmass *MassQ, gm
         /* now, set temperature variables */
         for (i = 0; i < ngtc; i++)
         {
-            if ((opts->tau_t[i] > 0) && (opts->ref_t[i] > 0))
+            if (opts->tau_t[i] > 0 && opts->ref_t[i] > 0 && opts->nrdf[i] > 0)
             {
                 reft = std::max<real>(0, opts->ref_t[i]);
                 nd   = opts->nrdf[i];
