89697c32ca94318548d297488300625207845fb2
Mark Abraham <mark.j.abraham@gmail.com>
2016-03-18 23:53:16 +0100

    Clean up vsite code
    No functionality changes. Added some const correctness. Reduced the
    scope of a lot of variables.
    
    Change-Id: Ia6caba6686ecf9348223932f7cc812e601d0109c

--- src/gromacs/mdlib/vsite.h
+++ src/gromacs/mdlib/vsite.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -93,11 +93,12 @@ void construct_vsites_mtop(gmx_vsite_t *vsite,
  * This function assumes that all molecules are whole.
  */
 
-void spread_vsite_f(gmx_vsite_t *vsite,
-                    rvec x[], rvec f[], rvec *fshift,
+void spread_vsite_f(const gmx_vsite_t *vsite,
+                    const rvec x[],
+                    rvec f[], rvec *fshift,
                     gmx_bool VirCorr, matrix vir,
-                    t_nrnb *nrnb, t_idef *idef,
-                    int ePBC, gmx_bool bMolPBC, t_graph *g, matrix box,
+                    t_nrnb *nrnb, const t_idef *idef,
+                    int ePBC, gmx_bool bMolPBC, const t_graph *g, const matrix box,
                     t_commrec *cr);
 /* Spread the force operating on the vsite atoms on the surrounding atoms.
  * If fshift!=NULL also update the shift forces.
