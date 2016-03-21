747490998053bd1757332dc978c53af25a43414f
Mark Abraham <mark.j.abraham@gmail.com>
2016-03-21 07:05:40 +0100

    Add test and improved docs for custom checkSequence
    I tried to use this, but ran into problems if I passed an id that was
    not NULL to the compound checker. Passing NULL is the behaviour for
    the implementation of checkSequenceArray, and that seems to be
    appropriate here, too.
    
    Change-Id: I9b02e4632085ad995ef67bbb52959f1533d56c34

--- src/testutils/refdata.h
+++ src/testutils/refdata.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2011,2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2011,2012,2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -457,7 +457,7 @@ class TestReferenceChecker
          * values of the sequence are checked.  Calls \c checkItem(&compound, *i)
          * with that compound for each iterator \c i in the range [begin, end).
          * \p checkItem should use the various check methods in the passed
-         * checker to check each value.
+         * checker to check each value, passing NULL for the id value.
          *
          * This method can be used to check a sequence made of compound types.
          * Typically \p checkItem will create a compound within the passed
