8e6f25609dfb01f7326d1b0eb898c73f62cfb7d5
Viveca Lindahl <vivecalindahl@gmail.com>
2016-03-28 14:31:42 +0200

    Add reading and writing to AWH module.
    This change adds IO to the AWH module. AWH writes coordinate
    free energies and distributions to an energy file block. The
    reading is handled by a new tool gmx awh.
    
    Change-Id: Ie30991bca376c2a648371db771fc5dfd8fca3715

--- src/gromacs/mdlib/mdebin.cpp
+++ src/gromacs/mdlib/mdebin.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -42,6 +42,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "gromacs/awh/awh.h"
 #include "gromacs/fileio/enxio.h"
 #include "gromacs/fileio/gmxfio.h"
 #include "gromacs/fileio/xvgr.h"
@@ -1245,7 +1246,7 @@ void print_ebin(ener_file_t fp_ene, gmx_bool bEne, gmx_bool bDR, gmx_bool bOR,
                 gmx_int64_t step, double time,
                 int mode,
                 t_mdebin *md, t_fcdata *fcd,
-                gmx_groups_t *groups, t_grpopts *opts)
+                gmx_groups_t *groups, t_grpopts *opts, const awh_t *awh)
 {
     /*static char **grpnms=NULL;*/
     char         buf[246];
@@ -1358,6 +1359,12 @@ void print_ebin(ener_file_t fp_ene, gmx_bool bEne, gmx_bool bDR, gmx_bool bOR,
                 if (md->dhc)
                 {
                     mde_delta_h_coll_reset(md->dhc);
+                }
+
+                /* AWH bias blocks. */
+                if (awh != NULL)  // TODO: add boolean in t_mdebin. See in mdebin.h.
+                {
+                    write_awh_to_energyframe(&fr, awh);
                 }
 
                 /* do the actual I/O */
