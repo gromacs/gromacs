070f1798c93df66e0ef3bcd17880eca7f7aa0a6f
Viveca Lindahl <vivecalindahl@gmail.com>
2016-03-28 14:34:35 +0200

    Add force correlation to AWH module.
    This change adds the calculation of force correlation statistics
    during an AWH biased simulation.
    The main quantity of interest is the time-integrated force
    correlation, also known as the friction tensor
    (see e.g. http://dx.doi.org/10.1103/PhysRevLett.108.190602).
    The friction tensor defines a metric on the coordinate space and
    the local volume element of this metric is a useful measure for
    determining which regions need more or less sampling.
    
    Change-Id: I164be4665004dea5b250e3c7ac135ac1c1cbd783

--- src/gromacs/awh/awh-update.cpp
+++ src/gromacs/awh/awh-update.cpp
@@ -59,6 +59,7 @@
 #include "gromacs/utility/gmxassert.h"
 #include "gromacs/utility/smalloc.h"
 
+#include "correlation.h"
 #include "data-writer.h"
 #include "grid.h"
 #include "history.h"
@@ -1811,12 +1812,46 @@ static void sample_pmf(awh_bias_t *awh_bias)
 }
 
 /*! \brief
+ * Collect samples for the force correlation analysis.
+ *
+ * \param[in,out] forcecorr   The force correlation grid.
+ * \param[in]     awh_bias    AWH bias.
+ * \param[in]     t           Time.
+ */
+static void update_force_correlation(correlation_grid_t *forcecorr, const awh_bias_t *awh_bias, double t)
+{
+    for (int n = 0; n < awh_bias->grid->point[awh_bias->coord_value_index].nneighbors; n++)
+    {
+        int      m_neighbor;
+        double   weight_neighbor;
+        awh_dvec force_from_neighbor;
+
+        weight_neighbor = awh_bias->prob_weight_neighbor[n];
+        m_neighbor      = awh_bias->grid->point[awh_bias->coord_value_index].neighbor[n];
+
+        /* Add the force data of this neighbor point. Note: the sum of these forces is the convolved force.
+
+           We actually add the force normalized by beta which has the units of 1/length. This means that the
+           resulting correlation time integral is directly in units of friction time/length^2 which is really what
+           we're interested in. */
+        calcUmbrellaForceAndPotential(awh_bias, awh_bias->betak, m_neighbor, force_from_neighbor);
+
+        /* Note: we might want to give a whole list of data to add instead and have this loop in the data adding function */
+        add_data_to_correlation_matrix(forcecorr, m_neighbor, weight_neighbor, force_from_neighbor, t);
+    }
+}
+
+/*! \brief
  * Sample observables for future updates or analysis.
  *
  * \param[in,out] awh_bias       AWH bias.
+ * \param[in] t                  Time.
  */
-static void do_sampling(awh_bias_t *awh_bias)
+static void do_sampling(awh_bias_t *awh_bias, double t)
 {
+    /* Force correlation */
+    update_force_correlation(awh_bias->forcecorr, awh_bias, t);
+
     /* Sampling-based deconvolution extracting the PMF */
     sample_pmf(awh_bias);
 
@@ -1863,7 +1898,7 @@ static void do_awh_bias_step(awh_bias_t *awh_bias,
 
         if (step > 0 && do_at_step(awh_bias->nstsample_coord, step))
         {
-            do_sampling(awh_bias);
+            do_sampling(awh_bias, t);
         }
     }
 
