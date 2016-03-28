466c54a810fa0aee26fa6d8f376c43e0ee4e4ef3
Viveca Lindahl <vivecalindahl@gmail.com>
2016-03-28 14:31:42 +0200

    Add reading and writing to AWH module.
    This change adds IO to the AWH module. AWH writes coordinate
    free energies and distributions to an energy file block. The
    reading is handled by a new tool gmx awh.
    
    Change-Id: Ie30991bca376c2a648371db771fc5dfd8fca3715

--- src/gromacs/mdlib/mdebin.h
+++ src/gromacs/mdlib/mdebin.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -48,6 +48,7 @@ struct gmx_constr;
 struct gmx_ekindata_t;
 struct gmx_mtop_t;
 struct gmx_output_env_t;
+struct awh_t;
 struct t_expanded;
 struct t_fcdata;
 struct t_grpopts;
@@ -153,7 +154,8 @@ void print_ebin(ener_file_t fp_ene, gmx_bool bEne, gmx_bool bDR, gmx_bool bOR,
                 gmx_int64_t step, double time,
                 int mode,
                 t_mdebin *md, t_fcdata *fcd,
-                gmx_groups_t *groups, t_grpopts *opts);
+                gmx_groups_t *groups, t_grpopts *opts,
+                const awh_t *awh);
 
 
 
