f5332913c0b16b19672a452bb55f90309807aea7
Viveca Lindahl <vivecalindahl@gmail.com>
2016-03-28 14:27:33 +0200

    Add AWH biasing module.
    The AWH (Accelerated weight histogram) method is an adaptive biasing
    method used for overcoming free energy barriers and calculating
    free energies (see http://dx.doi.org/10.1063/1.4890371). Although
    AWH can in general bias any system parameter, this change only
    implements biasing of reaction coordinates. The actual force
    distribution and coordinate handling is taken care of by the pull
    code. AWH interacts with the pull code by registering itself as
    the external potential module for the coordinate that should be
    AWH biased. The AWH code sets the potential and force for those
    coordinates.
    
    Change-Id: I202f58f7042e8e63c9d708fdcaca6da7e8a4022e

---
+++ src/gromacs/awh/bias.h
@@ -0,0 +1,275 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2015,2016,2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+/*! \libinternal \file
+ *
+ * \brief
+ * Declares Bias class and its helpers.
+ *
+ * \author Viveca Lindahl
+ * \author Berk Hess <hess@kth.se>
+ * \ingroup module_awh
+ */
+
+#ifndef GMX_AWH_BIAS_H
+#define GMX_AWH_BIAS_H
+
+#include <memory>
+#include <vector>
+
+#include "gromacs/math/vectypes.h"
+#include "gromacs/utility/basedefinitions.h"
+
+//! The maximum dimensionality of the AWH coordinate.
+static const int c_biasMaxNumDim = 4;
+
+//! A real vector in AWH coordinate space.
+typedef double awh_dvec[c_biasMaxNumDim];
+
+//! An integer vector in AWH coordinate space.
+typedef int awh_ivec[c_biasMaxNumDim];
+
+struct AwhBiasHistory;
+struct AwhHistory;
+struct AwhPointStateHistory;
+struct awh_bias_params_t;
+struct awh_params_t;
+struct BiasParams;
+struct BiasState;
+struct gmx_multisim_t;
+class PointState;
+struct t_commrec;
+class Grid;
+struct t_inputrec;
+struct t_mdatoms;
+struct t_pbc;
+
+/*! \cond INTERNAL */
+
+//! Constant parameters for each dimension of the coordinate.
+struct DimParams
+{
+    double                  betak;                    /**< Inverse variance (1/nm^2) for each coordinate dimension. */
+    double                  k;                        /**< Force constant (kJ/mol/nm^2) for each coordinate dimension. */
+    int                     pullCoordIndex;           /**< Indices of the pull coordinates for each coordinate dimension. */
+    double                  userCoordUnitsToInternal; /**< Conversion factor coordinate units. */
+
+    /*! \brief Convert internal coordinate units to external, user coordinate units.
+     *
+     * \param[in] value               Value to convert.
+     * \returns the converted value.
+     */
+    double scaleInternalToUserInput(double value) const
+    {
+        return value/userCoordUnitsToInternal;
+    }
+
+    /*! \brief Convert external, user coordinate units to internal coordinate units.
+     *
+     * \param[in] value               Value to convert.
+     * \returns the converted value.
+     */
+    double scaleUserInputToInternal(double value) const
+    {
+        return value*userCoordUnitsToInternal;
+    }
+};
+
+//! Constant parameters for the method.
+struct BiasParams
+{
+    double    invBeta;               /**< 1/beta = kT */
+    int       nstsample_coord;       /**< Number of steps per coordinate value sample. */
+    int       nstupdate_free_energy; /**< Number of steps per free energy update. */
+    int       nstupdate_target;      /**< Number of steps per updating the target distribution. */
+    int       eTarget;               /**< Type of target distribution. */
+    double    target_param;          /**< Target distribution parameter (meaning depends on eTarget). */
+    bool      idealWeighthistUpdate; /**< Update reference weighthistogram using the target distribution? Otherwise use the realized distribution. */
+    double    update_weight;         /**< The probability weight accumulated for each update. */
+    double    localWeightScaling;    /**< Scaling factor applied to a sample before adding it to the reference weight histogram (= 1, usually). */
+    double    histSizeInitial;       /**< Initial reference weight histogram size. */
+    int       numSharedUpdate;       /**< The number of (multi-)simulations sharing the bias update */
+    awh_ivec  coverRadius;           /**< The radius (in points) that needs to be sampled around a point before it is considered covered. */
+
+    /*! \brief
+     * Check if the AWH constant parameters permit skipping updates.
+     *
+     * Generally, we can skip (postpone) updates of points that are non-local
+     * at the time of the update if we for later times, when the points
+     * with skipped updates have become local, know exactly how to apply
+     * the previous updates. The free energy updates only depend
+     * on local sampling, but the histogram rescaling factors
+     * generally depend on the histogram size (all samples).
+     * If the histogram size is kept constant or the scaling factors
+     * are trivial, this is not a problem. However, if the histogram growth
+     * is scaled down by some factor the size at the time of the update
+     * needs to be known. It would be fairly simple to, for a deterministically
+     * growing histogram, backtrack and calculate this value, but currently
+     * we just disallow this case. This is not a restriction because it
+     * only affects the local Boltzmann target type for which every update
+     * is currently anyway global because the target is always updated globally.
+     *
+     * \returns true when we can skip updates.
+     */
+    bool canSkipUpdates() const
+    {
+        return localWeightScaling == 1;
+    }
+};
+
+//! Variables defining the global state of a bias.
+struct BiasState
+{
+    awh_dvec  coordValue;             /**< Current coordinate value in (nm or rad) */
+    int       gridpointIndex;         /**< The grid point index for the current coordinate value */
+    int       refGridpoint;           /**< Index for the current reference grid point (for umbrella potential type) */
+    awh_ivec  origin_updatelist;      /**< The origin of the subgrid that has been touched since last update. */
+    awh_ivec  end_updatelist;         /**< The end of the subgrid that has been touched since last update. */
+    bool      in_initial;             /**< True if in the intial stage. */
+    bool      equilibrateHistogram;   /**< True if samples are kept from accumulating until the sampled distribution is close enough to the target. */
+    double    histSize;               /**< Size of reference weight histogram. */
+    double    scaledSampleWeight;     /**< The log of the current sample weight, scaled because of the histogram rescaling. */
+    double    maxScaledSampleWeight;  /**< Maximum sample weight obtained for previous (smaller) histogram sizes. */
+};
+
+//! An AWH bias, i.e. all data associated with biasing reaction coordinates that contribute to one histogram.
+struct Bias
+{
+    public:
+        /*! \brief
+         * Constructor.
+         *
+         * \param[in,out] fplog              Log file.
+         * \param[in] ir                     Input parameters.
+         * \param[in] cr                     Struct for communication.
+         * \param[in] biasIndexInCollection  Index of the bias in collection.
+         * \param[in] awhParams              AWH parameters.
+         * \param[in] awhBiasParams          Bias parameters.
+         */
+        Bias(FILE                     *fplog,
+             const t_inputrec         *ir,
+             const t_commrec          *cr,
+             int                       biasIndexInCollection,
+             const awh_params_t       &awhParams,
+             const awh_bias_params_t  &awhBiasParams);
+
+        /*! \brief
+         * Update the coordinate value with the corresponding pull coordinate value.
+         *
+         * \param[in, out] pull_work  Pull struct.
+         * \param[in] pbc             Struct with information about periodic boundary conditions.
+         */
+        void setCoordValue(struct pull_t *pull_work, const t_pbc *pbc);
+
+        /*! \brief
+         * Do what the AWH bias needs to do every step.
+         *
+         * The main objectives of the AWH bias step is to 1) set the bias force
+         * and potential; 2) update the free energy and bias; and 3) sample
+         * observables (e.g. the PMF).
+         *
+         * \param[in] convolveForce      True if the bias force is convolved, otherwise apply single umbrella force.
+         * \param[in] probWeightNeighbor Working arrary to store weights of neighbors.
+         * \param[out]    biasForce      The AWH bias force.
+         * \param[in,out] awhPotential   Bias potential from all AWH biases.
+         * \param[in,out] potential_jump Change in bias potential for all AWH biases.
+         * \param[in] ms                 Struct for multi-simulation communication.
+         * \param[in] t                  Time.
+         * \param[in] step               Time step.
+         * \param[in] seed               Random seed.
+         * \param[in,out] fplog          Log file.
+         */
+        void doStep(bool convolveForce,
+                    std::vector<double> *probWeightNeighbor,
+                    awh_dvec biasForce,
+                    double *awhPotential, double *potential_jump,
+                    const gmx_multisim_t *ms,
+                    double t, gmx_int64_t step, int seed,
+                    FILE *fplog);
+
+        /*! \brief
+         * Set pull forces for the pull coordinates the bias is responsible for.
+         *
+         * \param[in] biasForce         The AWH bias force.
+         * \param[in,out] pull_work     Pull struct.
+         * \param[in] mdatoms           Atom properties.
+         * \param[in,out] force         Forces.
+         * \param[in,out] virial        Virial tensor.
+         */
+        void applyForceToPullCoords(const awh_dvec    biasForce,
+                                    struct pull_t    *pull_work,
+                                    const t_mdatoms  *mdatoms,
+                                    rvec             *force,
+                                    tensor            virial) const;
+
+        /*! \brief
+         * Update the bias history with a new state.
+         *
+         * \param[in,out] biasHistory  Bias history struct.
+         */
+        void updateHistory(AwhBiasHistory *biasHistory) const;
+
+        /*! \brief
+         * Restore the bias state from history.
+         *
+         * \param[in] biasHistory  Bias history struct.
+         */
+        void restoreStateFromHistory(const AwhBiasHistory *biasHistory);
+
+        /*! \brief
+         * Broadcast the bias data over the MPI ranks in this simulation.
+         *
+         * \param[in] cr  Struct for communication.
+         */
+        void broadcast(const t_commrec *cr);
+
+        /* Data members. */
+    public:
+        int                     biasIndex;                  /**< The index of this bias in AwhBiasCollection */
+        int                     ndim;                       /**< Dimensionality of the AWH coordinate. */
+        std::unique_ptr<Grid>   grid;                       /**< The multidimensional grid organizing the coordinate point locations. */
+        std::vector<PointState> pointState;                 /**< Grid point states. */
+        std::vector<int>        updateList;                 /**< List of points for update for temporary use. */
+
+        DimParams               dimParams[c_biasMaxNumDim]; /**< Parameters for each dimension. */
+
+        BiasParams              params;                     /**< Constant parameters for the method. */
+
+        BiasState               state;                      /**< The global state. */
+};
+
+/*! \endcond */
+
+#endif  /* GMX_AWH_BIAS_H */
