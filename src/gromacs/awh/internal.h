09d747f59e69edcab5c4457db6adb7ac1ba94f0a
Viveca Lindahl <vivecalindahl@gmail.com>
2016-03-28 14:37:58 +0200

    Add bias replica exchange option for AWH module.
    For multisimulation runs it is now possible to have AWH biased
    replicas exchange coordinates. This can be useful in cases when
    the AWH coordinate interval is partitioned into subintervals (e.g.
    because the diffusion is slow compared to the interval length). If
    there is overlap between subintervals, exchanges in overlap regions
    allow coordinates to mix across the whole interval while still
    keeping continuous trajectories in non-overlap regions.
    
    Change-Id: I0353dee67564969dd13faf2016bd1f6ce3aa1ae2

--- src/gromacs/awh/internal.h
+++ src/gromacs/awh/internal.h
@@ -127,12 +127,14 @@ double scaleUserInputToInternal(const awh_bias_params_t *awh_bias_params, int di
  * \param[in]     ir              General input parameters.
  * \param[in]     cr              Struct for communication (or NULL).
  * \param[in]     awh_params      AWH input parameters.
+ * \param[in] nstreplica_exchange Replica exchange step interval.
  * \returns the initialized AWH struct.
  */
 awh_t *init_awh(FILE                    *fplog,
                 const t_inputrec        *ir,
                 const t_commrec         *cr,
-                const awh_params_t      *awh_params);
+                const awh_params_t      *awh_params,
+                int                      nstreplica_exchange);
 
 /*! \brief Register the AWH biased coordinates with pull.
  *
