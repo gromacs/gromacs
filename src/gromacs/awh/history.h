5d2fcc7e37796fb879009c1ea7863cd990db9bdf
Viveca Lindahl <vivecalindahl@gmail.com>
2016-03-28 14:27:33 +0200

    Add AWH biasing module.
    The AWH (Accelerated weight histogram) method is an adaptive biasing
    method used for overcoming free energy barriers and calculating
    free energies (see http://dx.doi.org/10.1063/1.4890371). Although
    AWH can in general bias any system parameter, this change only
    implements biasing of reaction coordinates. The actual force
    distribution and coordinate handling is taken care of by the pull
    code. AWH interacts with the pull code by registering itself as
    the external potential module for the coordinate that should be
    AWH biased. The AWH code sets the potential and force for those
    coordinates.
    
    Change-Id: I202f58f7042e8e63c9d708fdcaca6da7e8a4022e

---
+++ src/gromacs/awh/history.h
@@ -0,0 +1,78 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2015,2016,2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+/*! \internal \file
+ *
+ * \brief
+ * Contains datatypes and function declarations needed by AWH to
+ * have its data checkpointed.
+ *
+ * \author Viveca Lindahl
+ * \author Berk Hess <hess@kth.se>
+ * \ingroup module_awh
+ */
+
+#ifndef GMX_AWH_HISTORY_H
+#define GMX_AWH_HISTORY_H
+
+#include "gromacs/utility/basedefinitions.h"
+
+class AwhBiasCollection;
+struct AwhHistory;
+struct t_commrec;
+
+
+/*! \brief
+ * Allocate and initialize an AWH history with the given AWH state.
+ *
+ * This function would be called at the start of a new simulation.
+ * Note that no data only constant data will be initialized here.
+ * History data is set by update_awh_history.
+ *
+ * \param[in,out] awhHistory  AWH bias history to initialize.
+ * \param[in] awh             AWH state to initialize with.
+ */
+void initAwhHistoryFromState(AwhHistory *awhHistory, const AwhBiasCollection *awh);
+
+/*! \brief
+ * Update the AWH bias history for checkpointing.
+ *
+ * \param[in,out] awhHistory  AWH bias history to set.
+ * \param[in] awh             AWH state to read.
+ */
+void updateAwhHistory(AwhHistory               *awhHistory,
+                      const AwhBiasCollection  *awh);
+
+#endif
