86d225862434bc2a12a3e1e3d313c61970928d49
Mark Abraham <mark.j.abraham@gmail.com>
2016-03-30 05:38:46 +0200

    Facilitate linking with libcxx
    Using recent clang static analyzer versions seems to be easier with
    libcxx, which we should anyway support for building and testing.
    
    Introduced GMX_STDLIB_CXX_FLAGS, so that all the linked executables
    can have their sources compiled with any compiler flag that might be
    required. Alternatives like requiring the user to modify
    CMAKE_CXX_FLAGS, or adding COMPILE_FLAGS properties to targets didn't
    seem great. The latter also triggers clang to issue warnings for
    source files that are still C (group kernels and TNG).
    
    Introduced GMX_STDLIB_LIBRARIES, so that linking can proceed
    correctly.
    
    For example, the check for C++11 support needs to be passed a library
    to link during the try_compile(), and the only reliable way for the
    user to do that before this patch was to add the linker flag to
    CMAKE_CXX_FLAGS, which then leads to clang warning about the unused
    linker flag as it compiles each source file. The GMX_STDLIB_*
    mechanisms probably also permit users to build against different
    versions of GNU libstdc++, which may be useful on distributions like
    CentOS, because CMake has no mechanism at all for this.
    
    Updated the install guide to clarify how to choose a standard library
    in the various cases. Updated the guide for using GROMACS as a library,
    and the template README.
    
    Fixed issue where the template did not have C++11 compiler flags
    propagated properly. The template now builds correctly, via both
    Makefile.pkg and cmake, both with normal default libstdc++ and libc++
    selected via this mechanism and propagated to the installed build
    system for the template.
    
    Fixed issue where SIMD suggestion would produce a garbage suggestion
    when the linking failed and OUTPUT_SIMD was left unset.
    
    Change-Id: Ieef3b47de5c1a00a203baa1b34ebf70535cf5ff0

--- cmake/gmxTestCXX11.cmake
+++ cmake/gmxTestCXX11.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012,2013,2014,2015, by the GROMACS development team, led by
+# Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -45,7 +45,13 @@ MACRO(GMX_TEST_CXX11 VARIABLE FLAG)
     if(NOT CXXFLAG_STD_CXX0X)
         set(CXX11_FLAG "")
     endif()
-    set(CMAKE_REQUIRED_FLAGS "${CXX11_FLAG}")
+    if (GMX_STDLIB_CXX_FLAGS)
+        CHECK_CXX_COMPILER_FLAG("${GMX_STDLIB_CXX_FLAGS}" CXXFLAG_STDLIB)
+    else()
+        set(CXXFLAG_STDLIB "")
+    endif()
+    set(CMAKE_REQUIRED_FLAGS "${CXX11_FLAG} ${CXXFLAG_STDLIB}")
+    set(CMAKE_REQUIRED_LIBRARIES "${GMX_STDLIB_LIBRARIES}")
     check_cxx_source_compiles(
 "#include <map>
 #include <memory>
