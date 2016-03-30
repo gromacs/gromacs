a075215407a8a63f097b4ae2a21f2030b6f8c9c9
Mark Abraham <mark.j.abraham@gmail.com>
2016-03-30 05:38:46 +0200

    Facilitate linking with libcxx
    Using recent clang static analyzer versions seems to be easier with
    libcxx, which we should anyway support for building and testing.
    
    In particular, in this case the check for C++11 support needs to be
    passed a library to link during the try_compile(), and the only
    reliable way for the user to do that before this patch was to add the
    linker flag to CMAKE_CXX_FLAGS, which then leads to clang warning
    about the unused linker flag as it compiles each source file. The
    GMX_STDLIB mechanism probably also permits users to build against
    different versions of GNU libstdc++, which is useful because CMake has
    no mechanism at all for this.
    
    Updated the install guide to clarify how to choose a standard library
    in the various cases.
    
    Change-Id: Ieef3b47de5c1a00a203baa1b34ebf70535cf5ff0

--- cmake/gmxTestCXX11.cmake
+++ cmake/gmxTestCXX11.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012,2013,2014,2015, by the GROMACS development team, led by
+# Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -46,6 +46,7 @@ MACRO(GMX_TEST_CXX11 VARIABLE FLAG)
         set(CXX11_FLAG "")
     endif()
     set(CMAKE_REQUIRED_FLAGS "${CXX11_FLAG}")
+    set(CMAKE_REQUIRED_LIBRARIES "${GMX_STDLIB}")
     check_cxx_source_compiles(
 "#include <map>
 #include <memory>
