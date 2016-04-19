fa7d0e1f34c8a9a0a6ac20d5b9f2848d1ab28fb4
Mark Abraham <mark.j.abraham@gmail.com>
2016-04-19 02:02:13 +0200

    Fix parse_digits_from_string
    New GPU-passing functionality wasn't checked for empty string like the
    functionality that it extended is checked for. Noted TODO for future.
    
    Change-Id: I587e526802a4e880fa966836b555fb07e2b3ad30

--- src/gromacs/utility/cstringutil.cpp
+++ src/gromacs/utility/cstringutil.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -543,6 +543,14 @@ char *gmx_step_str(gmx_int64_t i, char *buf)
 
 void parse_digits_from_string(const char *digitstring, int *ndigits, int **digitlist)
 {
+    /* TODO use std::string, once gmx_gpu_opt_t is ready for it */
+    if (NULL == digitstring)
+    {
+        *ndigits   = 0;
+        *digitlist = NULL;
+        return;
+    }
+
     if (strstr(digitstring, ",") != NULL)
     {
         parse_digits_from_csv_string(digitstring, ndigits, digitlist);
