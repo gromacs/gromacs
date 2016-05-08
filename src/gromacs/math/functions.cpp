456108812cf064b71f97322c59d43b7439acfab9
Erik Lindahl <erik@kth.se>
2016-05-08 22:49:36 +0200

    Fix precision loss in tabulated normal distribution
    The tabulated normal distribution was generated by
    simply integrating a gaussian function, which led to
    severe loss-of-precision for double precision tables.
    Fixed by implementing proper inverse error functions
    in the math module, and using this to fill the table
    directly.
    
    Fixes #1930.
    
    Change-Id: I74b72b4a6d36f1eb382c6456501ce5644c92725e

--- src/gromacs/math/functions.cpp
+++ src/gromacs/math/functions.cpp
@@ -51,11 +51,13 @@
 #include <cstdint>
 
 #include <array>
+#include <limits>
 
 #if GMX_NATIVE_WINDOWS
 #    include <intrin.h> // _BitScanReverse, _BitScanReverse64
 #endif
 
+#include "gromacs/math/utilities.h"
 #include "gromacs/utility/gmxassert.h"
 
 namespace gmx
@@ -176,6 +178,97 @@ greatestCommonDivisor(std::int64_t   p,
         p                = tmp;
     }
     return p;
+}
+
+double
+erfinv(double x)
+{
+    double xabs = std::abs(x);
+
+    if (xabs > 1.0)
+    {
+        return std::nan("");
+    }
+
+    if (x == 1.0)
+    {
+        return std::numeric_limits<double>::infinity();
+    }
+
+    if (x == -1.0)
+    {
+        return -std::numeric_limits<double>::infinity();
+    }
+
+    double res;
+
+    if (xabs <= 0.7)
+    {
+        // Rational approximation in range [0,0.7]
+        double z = x*x;
+        double P = (((-0.140543331 * z + 0.914624893) * z - 1.645349621) * z + 0.886226899);
+        double Q = ((((0.012229801 * z - 0.329097515) * z + 1.442710462) * z - 2.118377725) * z + 1.0);
+        res = x * P/Q;
+    }
+    else
+    {
+        // Rational approximation in range [0.7,1)
+        double z = std::sqrt(-std::log((1.0 - std::abs(x))/2.0));
+        double P = ((1.641345311 * z + 3.429567803) * z - 1.624906493) * z - 1.970840454;
+        double Q = (1.637067800 * z + 3.543889200) * z + 1.0;
+        res = std::copysign(1.0, x) * P/Q;
+    }
+
+    // Double precision requires two N-R iterations
+    res = res - (std::erf(res) - x)/( (2.0/std::sqrt(M_PI))*std::exp(-res*res));
+    res = res - (std::erf(res) - x)/( (2.0/std::sqrt(M_PI))*std::exp(-res*res));
+
+    return res;
+}
+
+float
+erfinv(float x)
+{
+    float xabs = std::abs(x);
+
+    if (xabs > 1.0f)
+    {
+        return std::nan("");
+    }
+
+    if (x == 1.0f)
+    {
+        return std::numeric_limits<float>::infinity();
+    }
+
+    if (x == -1.0f)
+    {
+        return -std::numeric_limits<float>::infinity();
+    }
+
+    float res;
+
+    if (xabs <= 0.7f)
+    {
+        // Rational approximation in range [0,0.7]
+        float z = x*x;
+        float P = (((-0.140543331f * z + 0.914624893f) * z - 1.645349621f) * z + 0.886226899f);
+        float Q = ((((0.012229801f * z - 0.329097515f) * z + 1.442710462f) * z - 2.118377725f) * z + 1.0f);
+        res = x * P/Q;
+    }
+    else
+    {
+        // Rational approximation in range [0.7,1)
+        float z = std::sqrt(-std::log((1.0 - std::abs(x))/2.0f));
+        float P = ((1.641345311f * z + 3.429567803f) * z - 1.624906493f) * z - 1.970840454f;
+        float Q = (1.637067800f * z + 3.543889200f) * z + 1.0f;
+        res = std::copysign(1.0f, x) * P/Q;
+    }
+
+    // Single N-R iteration sufficient for single precision
+    res = res - (std::erf(res) - x)/( (2.0f/std::sqrt(M_PI))*std::exp(-res*res));
+
+    return res;
 }
 
 } // namespace gmx
