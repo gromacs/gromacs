acd1df190381b1fe53f534480beac4b175f32b45
Erik Lindahl <erik@kth.se>
2016-05-11 00:19:49 +0200

    Fixed large file issue on 32-bit platforms
    At some point gcc started to issue a warning
    instead of a fatal error for the checking
    code; fixed to really generate an error now.
    
    Fixes #1834.
    
    Change-Id: I5827f358b22de516becaac02d81229b82b02297b

--- cmake/TestFileOffsetBits.c
+++ cmake/TestFileOffsetBits.c
@@ -3,8 +3,7 @@
 int main()
 {
   /* Cause a compile-time error if off_t is smaller than 64 bits */
-#define LARGE_OFF_T (((off_t) 1 << 62) - 1 + ((off_t) 1 << 62))
-  int off_t_is_large[ (LARGE_OFF_T % 2147483629 == 721 && LARGE_OFF_T % 2147483647 == 1) ? 1 : -1 ];  
+  int off_t_is_large[sizeof(off_t)-7];
   return off_t_is_large[0];
 }
 
