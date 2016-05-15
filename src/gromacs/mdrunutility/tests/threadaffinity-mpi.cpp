98a534019e89f72b8e292337d00299516d902a14
Teemu Murtola <teemu.murtola@gmail.com>
2016-05-15 10:23:14 +0300

    Some multi-rank thread affinity tests
    Add tests for setting the thread affinity on multiple MPI ranks.  Some
    of the error messages still do not work as they should (one such case
    triggered by the non-master-only pinning test), and there are some
    corner cases that deadlock (those are not tested yet).  These will be
    fixed and additional tests added in a separate change.
    
    Allow simulating multi-node environments, including heterogeneous nodes,
    with both MPI flavors.
    
    Change-Id: I69628d8e3d8f2d87cd40f8393da4b251ce1cba94

---
+++ src/gromacs/mdrunutility/tests/threadaffinity-mpi.cpp
@@ -0,0 +1,153 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#include "gmxpre.h"
+
+#include "config.h"
+
+#include <array>
+
+#include <gtest/gtest.h>
+
+#include "gromacs/utility/basenetwork.h"
+
+#include "testutils/mpitest.h"
+
+#include "threadaffinitytest.h"
+
+namespace
+{
+
+using gmx::test::ThreadAffinityTestHelper;
+
+typedef gmx::test::ThreadAffinityTestBase ThreadAffinityMultiRankTest;
+
+TEST_F(ThreadAffinityMultiRankTest, PinsWholeNode)
+{
+    GMX_MPI_TEST(4);
+    ThreadAffinityTestHelper helper;
+    helper.setLogicalProcessorCount(4);
+    helper.expectAffinitySet(gmx_node_rank());
+    helper.setAffinity(this, 1);
+}
+
+TEST_F(ThreadAffinityMultiRankTest, PinsWithOffsetAndStride)
+{
+    GMX_MPI_TEST(4);
+    setAffinityOption(threadaffON);
+    setOffsetAndStride(1, 2);
+    ThreadAffinityTestHelper helper;
+    helper.setLogicalProcessorCount(8);
+    helper.expectAffinitySet(1 + 2*gmx_node_rank());
+    helper.setAffinity(this, 1);
+}
+
+TEST_F(ThreadAffinityMultiRankTest, PinsTwoNodes)
+{
+    GMX_MPI_TEST(4);
+    ThreadAffinityTestHelper helper;
+    helper.setPhysicalNodeId(gmx_node_rank()/2);
+    helper.setLogicalProcessorCount(2);
+    helper.expectAffinitySet(gmx_node_rank()%2);
+    helper.setAffinity(this, 1);
+}
+
+TEST_F(ThreadAffinityMultiRankTest, DoesNothingWhenDisabled)
+{
+    GMX_MPI_TEST(4);
+    setAffinityOption(threadaffOFF);
+    ThreadAffinityTestHelper helper;
+    helper.setLogicalProcessorCount(4);
+    helper.setAffinity(this, 1);
+}
+
+TEST_F(ThreadAffinityMultiRankTest, HandlesTooManyThreadsWithAuto)
+{
+    GMX_MPI_TEST(4);
+    ThreadAffinityTestHelper helper;
+    helper.setLogicalProcessorCount(6);
+    helper.setAffinity(this, 2);
+}
+
+TEST_F(ThreadAffinityMultiRankTest, HandlesTooManyThreadsWithForce)
+{
+    GMX_MPI_TEST(4);
+    setAffinityOption(threadaffON);
+    ThreadAffinityTestHelper helper;
+    helper.setLogicalProcessorCount(6);
+    helper.setAffinity(this, 2);
+}
+
+class ThreadAffinityHeterogeneousNodesTest : public gmx::test::ThreadAffinityTestBase
+{
+    public:
+        int currentNode() const { return gmx_node_rank() / 2; }
+        int indexInNode() const { return gmx_node_rank() % 2; }
+
+        void setupNodes(ThreadAffinityTestHelper *helper, std::array<int, 2> cores)
+        {
+            const int node = currentNode();
+            helper->setPhysicalNodeId(node);
+            helper->setLogicalProcessorCount(cores[node]);
+        }
+        void expectNodeAffinitySet(ThreadAffinityTestHelper *helper, int node, int core)
+        {
+            if (currentNode() == node)
+            {
+                helper->expectAffinitySet(core);
+            }
+        }
+};
+
+TEST_F(ThreadAffinityHeterogeneousNodesTest, PinsOnMasterOnly)
+{
+    GMX_MPI_TEST(4);
+    setAffinityOption(threadaffON);
+    ThreadAffinityTestHelper helper;
+    setupNodes(&helper, {{2, 1}});
+    expectNodeAffinitySet(&helper, 0, indexInNode());
+    helper.setAffinity(this, 1);
+}
+
+TEST_F(ThreadAffinityHeterogeneousNodesTest, PinsOnNonMasterOnly)
+{
+    GMX_MPI_TEST(4);
+    setAffinityOption(threadaffON);
+    ThreadAffinityTestHelper helper;
+    setupNodes(&helper, {{1, 2}});
+    expectNodeAffinitySet(&helper, 1, indexInNode());
+    helper.setAffinity(this, 1);
+}
+
+} // namespace
