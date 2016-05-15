9d909d2f39f7746701dabb53b8482ff4dc552b4a
Teemu Murtola <teemu.murtola@gmail.com>
2016-05-15 10:23:14 +0300

    Multi-rank thread affinity tests
    Add tests for setting the thread affinity on multiple MPI ranks,
    including various heterogeneous run setups where thread affinity setting
    fails only on a subset of the nodes.
    
    Allow simulating multi-node environments, including heterogeneous nodes,
    with both MPI flavors.
    
    Change-Id: I69628d8e3d8f2d87cd40f8393da4b251ce1cba94

--- src/gromacs/mdrunutility/tests/threadaffinitytest.cpp
+++ src/gromacs/mdrunutility/tests/threadaffinitytest.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2016, by the GROMACS development team, led by
+ * Copyright (c) 2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -52,7 +52,7 @@ namespace test
 {
 
 MockThreadAffinityAccess::MockThreadAffinityAccess()
-    : supported_(true)
+    : supported_(true), physicalNodeId_(0)
 {
     using ::testing::_;
     using ::testing::Return;
