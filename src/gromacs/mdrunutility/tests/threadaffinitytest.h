fcc0cebd23f1fff5f988ae9023e73996f927f72b
Teemu Murtola <teemu.murtola@gmail.com>
2016-05-15 10:23:14 +0300

    Multi-rank thread affinity tests
    Add tests for setting the thread affinity on multiple MPI ranks,
    including various heterogeneous run setus where thread affinity setting
    fails only on a subset of the nodes.
    
    Allow simulating multi-node environments, including heterogeneous nodes,
    with both MPI flavors.
    
    Change-Id: I69628d8e3d8f2d87cd40f8393da4b251ce1cba94

--- src/gromacs/mdrunutility/tests/threadaffinitytest.h
+++ src/gromacs/mdrunutility/tests/threadaffinitytest.h
@@ -59,12 +59,15 @@ class MockThreadAffinityAccess : public IThreadAffinityAccess
         ~MockThreadAffinityAccess();
 
         void setSupported(bool supported) { supported_ = supported; }
+        void setPhysicalNodeId(int nodeId) { physicalNodeId_ = nodeId; }
 
         virtual bool isThreadAffinitySupported() const { return supported_; }
+        virtual int physicalNodeId() const { return physicalNodeId_; }
         MOCK_METHOD1(setCurrentThreadAffinityToCore, bool(int core));
 
     private:
         bool supported_;
+        int  physicalNodeId_;
 };
 
 class ThreadAffinityTestHelper
@@ -85,6 +88,11 @@ class ThreadAffinityTestHelper
         {
             hwOpt_->core_pinning_offset = offset;
             hwOpt_->core_pinning_stride = stride;
+        }
+
+        void setPhysicalNodeId(int nodeId)
+        {
+            affinityAccess_.setPhysicalNodeId(nodeId);
         }
 
         void setLogicalProcessorCount(int logicalProcessorCount);
