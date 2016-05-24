adf50a84acb474f53b777a9a795c29f25afe9a23
Erik Lindahl <erik@kth.se>
2016-05-25 01:04:50 +0200

    Prevent fragile use cases of checkpoint appending
    There are way too many ways we allow runs to be continued
    and extended. To avoid mistakes with file appending when
    restarting from checkpoints, we now require that all
    previous files must be present (unless -noappend is used),
    and the file names must match the ones used in the
    previous run.
    
    Fixes #1777.
    
    Change-Id: Id9e89773a4a9214be6dbb76676c526e98e12bd37

--- src/programs/mdrun/mdrun.cpp
+++ src/programs/mdrun/mdrun.cpp
@@ -299,7 +299,7 @@ int gmx_mdrun(int argc, char *argv[])
     real              rdd                   = 0.0, rconstr = 0.0, dlb_scale = 0.8, pforce = -1;
     char             *ddcsx                 = NULL, *ddcsy = NULL, *ddcsz = NULL;
     real              cpt_period            = 15.0, max_hours = -1;
-    gmx_bool          bTryToAppendFiles     = TRUE;
+    gmx_bool          bAppendFiles          = TRUE;
     gmx_bool          bKeepAndNumCPT        = FALSE;
     gmx_bool          bResetCountersHalfWay = FALSE;
     gmx_output_env_t *oenv                  = NULL;
@@ -380,7 +380,7 @@ int gmx_mdrun(int argc, char *argv[])
           "Checkpoint interval (minutes)" },
         { "-cpnum",   FALSE, etBOOL, {&bKeepAndNumCPT},
           "Keep and number checkpoint files" },
-        { "-append",  FALSE, etBOOL, {&bTryToAppendFiles},
+        { "-append",  FALSE, etBOOL, {&bAppendFiles},
           "Append to previous output files when continuing from checkpoint instead of adding the simulation part number to all file names" },
         { "-nsteps",  FALSE, etINT64, {&nsteps},
           "Run this number of steps, overrides .mdp file option (-1 means infinite, -2 means use mdp option, smaller is invalid)" },
@@ -416,7 +416,7 @@ int gmx_mdrun(int argc, char *argv[])
     unsigned long   Flags;
     ivec            ddxyz;
     int             dd_rank_order;
-    gmx_bool        bDoAppendFiles, bStartFromCpt;
+    gmx_bool        bStartFromCpt;
     FILE           *fplog;
     int             rc;
     char          **multidir = NULL;
@@ -488,8 +488,19 @@ int gmx_mdrun(int argc, char *argv[])
 #endif
     }
 
-    handleRestart(cr, bTryToAppendFiles, NFILE, fnm,
-                  &bDoAppendFiles, &bStartFromCpt);
+    if (!opt2bSet("-cpi", NFILE, fnm))
+    {
+        if (opt2parg_bSet("-append", asize(pa), pa))
+        {
+            gmx_fatal(FARGS, "GROMACS can only append to files when restarting from a checkpoint.");
+        }
+        else
+        {
+            bAppendFiles = FALSE;
+        }
+    }
+
+    handleRestart(cr, bAppendFiles, NFILE, fnm, &bStartFromCpt);
 
     Flags = opt2bSet("-rerun", NFILE, fnm) ? MD_RERUN : 0;
     Flags = Flags | (bDDBondCheck  ? MD_DDBONDCHECK  : 0);
@@ -498,7 +509,7 @@ int gmx_mdrun(int argc, char *argv[])
     Flags = Flags | (bConfout      ? MD_CONFOUT      : 0);
     Flags = Flags | (bRerunVSite   ? MD_RERUN_VSITE  : 0);
     Flags = Flags | (bReproducible ? MD_REPRODUCIBLE : 0);
-    Flags = Flags | (bDoAppendFiles ? MD_APPENDFILES  : 0);
+    Flags = Flags | (bAppendFiles  ? MD_APPENDFILES  : 0);
     Flags = Flags | (opt2parg_bSet("-append", asize(pa), pa) ? MD_APPENDFILESSET : 0);
     Flags = Flags | (bKeepAndNumCPT ? MD_KEEPANDNUMCPT : 0);
     Flags = Flags | (bStartFromCpt ? MD_STARTFROMCPT : 0);
@@ -511,7 +522,7 @@ int gmx_mdrun(int argc, char *argv[])
     /* We postpone opening the log file if we are appending, so we can
        first truncate the old log file and append to the correct position
        there instead.  */
-    if (MASTER(cr) && !bDoAppendFiles)
+    if (MASTER(cr) && !bAppendFiles)
     {
         gmx_log_open(ftp2fn(efLOG, NFILE, fnm), cr,
                      Flags & MD_APPENDFILES, &fplog);
@@ -535,7 +546,7 @@ int gmx_mdrun(int argc, char *argv[])
 
     /* Log file has to be closed in mdrunner if we are appending to it
        (fplog not set here) */
-    if (MASTER(cr) && !bDoAppendFiles)
+    if (MASTER(cr) && !bAppendFiles)
     {
         gmx_log_close(fplog);
     }
