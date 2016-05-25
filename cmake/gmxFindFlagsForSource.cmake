8d223faa4b9684a390bf48ce6c23c4f0de1909ce
Mark Abraham <mark.j.abraham@gmail.com>
2016-05-25 08:40:10 +0200

    Fix SIMD configuration management
    Subsequent runs of cmake gave inconsistent diagnostic messages because
    SUGGEST_BINUTILS_UPDATE was not set on subsequent runs because we were
    caching the result of logic, as well as caching the results of
    compilation tests. This made life confusing, e.g. when compiling with
    gcc on MacOS with clang assembler not available.
    
    Instead, we now re-run the fast logic (quietly, if this is a
    subsequent run).
    
    Improved the handling of ${VARIABLE}, because there was no need to use
    FORCE because the semantics of an unset variable in CMake just work.
    There was also no need for such variables to be put into the cache,
    and we were using one more variable than we needed to use. This meant
    it was no longer worth implementing the redundant hints about perhaps
    updating the binutils package, nor suppressing the redundant special
    status-line output.
    
    Noted some TODOs for future simplification. Changed the use of SIMD to
    SOURCE, since this utility code doesn't have to relate to SIMD flags.
    
    Change-Id: Id9605ccff0903c55e2621ddd8af10c8da523bebe

--- cmake/gmxFindFlagsForSource.cmake
+++ cmake/gmxFindFlagsForSource.cmake
@@ -33,105 +33,107 @@
 # the research papers on the package. Check out http://www.gromacs.org.
 
 # Helper routine to find flag (from a list) to compile a specific C source.
-# VARIABLE            This will be set when we have found a flag that works
-# DESCRIPTION         Text string describing what flag we are trying to find
+# VARIABLE            Name of variable to set in the parent scope to true if
+#                     we have found a flag that works (which could be "")
 # SOURCE              Source code to test
 #                     The compiler is chosen based on the extension of this file
-# FLAGSVAR            Variable (string) to which we should add the correct flag
-# Args 5 through N    Multiple strings with optimization flags to test
+# CFLAGSVAR           Any flags already known to be needed by the toolchain,
+#                     to which the first useful flag will be appended
+# Args 5 through N    Multiple strings with compiler flags to test
 #
-# If a compile flag is found, but the project in check_c_source_compiles
+# If a compiler flag is found, but the project in check_c_source_compiles
 # fails to build, sets SUGGEST_BINUTILS_UPDATE in parent scope to suggest
 # that the calling code tell the user about this issue if needed.
-FUNCTION(GMX_FIND_CFLAG_FOR_SOURCE VARIABLE DESCRIPTION SOURCE CFLAGSVAR)
-    IF(NOT DEFINED ${VARIABLE})
+#
+# Note that if you update this function, you probably want to make
+# matching changes to the C++ version below.
+FUNCTION(GMX_FIND_CFLAG_FOR_SOURCE VARIABLE SOURCE CFLAGSVAR)
+    # TODO This is just here to add a level of indentation, remove in master branch
+    if(TRUE)
         # Insert a blank element last in the list (try without any flags too)
         # This must come last, since some compilers (Intel) might try to emulate
         # emulate AVX instructions with SSE4.1 otherwise.
         foreach(_testflag ${ARGN} "")
-            message(STATUS "Try ${DESCRIPTION} = [${_testflag}]")
+            # TODO This sets up implicit context for both the compilation
+            # checks below, but the implementation could be improved to
+            # make this more explicit, or cache such state.
             set(CMAKE_REQUIRED_FLAGS "${${CFLAGSVAR}} ${_testflag}")
             # make valid variable names from the flag string: replace all non-alphanumerical chars
             string(REGEX REPLACE "[^a-zA-Z0-9]+" "_" COMPILE_FLAG_VARIABLE "C_FLAG_${_testflag}")
-            string(REGEX REPLACE "[^a-zA-Z0-9]+" "_" COMPILE_SIMD_VARIABLE "C_SIMD_COMPILES_FLAG_${_testflag}")
+            string(REGEX REPLACE "[^a-zA-Z0-9]+" "_" COMPILE_SOURCE_VARIABLE "C_SOURCE_COMPILES_FLAG_${_testflag}")
 
             # Check that the flag itself is fine, and that is does not generate warnings either
             check_c_compiler_flag("${_testflag}" ${COMPILE_FLAG_VARIABLE})
 
             if(${COMPILE_FLAG_VARIABLE})
-                # Check that we can compile SIMD source (this does not catch compiler warnings)
-                check_c_source_compiles("${SOURCE}" ${COMPILE_SIMD_VARIABLE})
-                if(NOT ${COMPILE_SIMD_VARIABLE})
-                    message(STATUS "Compiler flag was valid, but executable did not build - perhaps update the binutils package")
+                # Check that we can compile source (this does not catch compiler warnings)
+                check_c_source_compiles("${SOURCE}" ${COMPILE_SOURCE_VARIABLE})
+                if(NOT ${COMPILE_SOURCE_VARIABLE})
                     set(SUGGEST_BINUTILS_UPDATE 1 PARENT_SCOPE)
                 endif()
             endif()
 
-            if(${COMPILE_FLAG_VARIABLE} AND ${COMPILE_SIMD_VARIABLE})
-                set(${VARIABLE}_FLAG "${_testflag}" CACHE INTERNAL "${DESCRIPTION}")
-                set(${VARIABLE} 1 CACHE INTERNAL "Result of test for ${DESCRIPTION}" FORCE)
+            if(${COMPILE_FLAG_VARIABLE} AND ${COMPILE_SOURCE_VARIABLE})
+                set(${VARIABLE} 1 PARENT_SCOPE)
+                set(${CFLAGSVAR} "${${CFLAGSVAR}} ${_testflag}" PARENT_SCOPE)
                 break()
-            else()
-                set(${VARIABLE} 0 CACHE INTERNAL "Result of test for ${DESCRIPTION}" FORCE)
             endif()
         endforeach()
-    ENDIF()
-
-    IF (${VARIABLE})
-        SET (${CFLAGSVAR} "${${CFLAGSVAR}} ${${VARIABLE}_FLAG}" PARENT_SCOPE)
-    ENDIF ()
-ENDFUNCTION(GMX_FIND_CFLAG_FOR_SOURCE VARIABLE DESCRIPTION SOURCE CFLAGSVAR)
+        # If no flag has been found, then leaving ${VARIABLE} unset
+        # will be interpreted by CMake as false.
+    endif()
+ENDFUNCTION()
 
 
 # Helper routine to find flag (from list) to compile a specific C++ source.
-# VARIABLE            This will be set when we have found a flag that works
-# DESCRIPTION         Text string describing what flag we are trying to find
+# VARIABLE            Name of variable to set in the parent scope to true if
+#                     we have found a flag that works (which could be "")
 # SOURCE              Source code to test
 #                     The compiler is chosen based on the extension of this file
-# FLAGSVAR            Variable (string) to which we should add the correct flag
-# Args 5 through N    Multiple strings with optimization flags to test
+# CXXFLAGSVAR         Any flags already known to be needed by the toolchain,
+#                     to which the first useful flag will be appended
+# Args 5 through N    Multiple strings with compiler flags to test
 #
-# If a compile flag is found, but the project in check_cxx_source_compiles
+# If a compiler flag is found, but the project in check_cxx_source_compiles
 # fails to build, sets SUGGEST_BINUTILS_UPDATE in parent scope to suggest
 # that the calling code tell the user about this issue if needed.
-FUNCTION(GMX_FIND_CXXFLAG_FOR_SOURCE VARIABLE DESCRIPTION SOURCE CXXFLAGSVAR)
-
-    IF(NOT DEFINED ${VARIABLE})
+#
+# Note that if you update this function, you probably want to make
+# matching changes to the C version above.
+FUNCTION(GMX_FIND_CXXFLAG_FOR_SOURCE VARIABLE SOURCE CXXFLAGSVAR)
+    # TODO This is just here to add a level of indentation, remove in master branch
+    if(TRUE)
         # Insert a blank element last in the list (try without any flags too)
         # This must come last, since some compilers (Intel) might try to
         # emulate AVX instructions with SSE4.1 otherwise.
         foreach(_testflag ${ARGN} "")
-            message(STATUS "Try ${DESCRIPTION} = [${_testflag}]")
+            # TODO This sets up implicit context for both the compilation
+            # checks below, but the implementation could be improved to
+            # make this more explicit, or cache such state.
             set(CMAKE_REQUIRED_FLAGS "${${CXXFLAGSVAR}} ${_testflag}")
             # make valid variable names from the flag string: replace all non-alphanumerical chars
             string(REGEX REPLACE "[^a-zA-Z0-9]+" "_" COMPILE_FLAG_VARIABLE "CXX_FLAG_${_testflag}")
-            string(REGEX REPLACE "[^a-zA-Z0-9]+" "_" COMPILE_SIMD_VARIABLE "CXX_SIMD_COMPILES_FLAG_${_testflag}")
+            string(REGEX REPLACE "[^a-zA-Z0-9]+" "_" COMPILE_SOURCE_VARIABLE "CXX_SOURCE_COMPILES_FLAG_${_testflag}")
             
             # Check that the flag itself is fine, and that is does not generate warnings either
             check_cxx_compiler_flag("${_testflag}" ${COMPILE_FLAG_VARIABLE})
 
             if(${COMPILE_FLAG_VARIABLE})
-                # Check that we can compile SIMD source (this does not catch compiler warnings)
-                check_cxx_source_compiles("${SOURCE}" ${COMPILE_SIMD_VARIABLE})
-                if(NOT ${COMPILE_SIMD_VARIABLE})
-                    message(STATUS "Compiler flag was valid, but executable did not build - perhaps update the binutils package")
+                # Check that we can compile source (this does not catch compiler warnings)
+                check_cxx_source_compiles("${SOURCE}" ${COMPILE_SOURCE_VARIABLE})
+                if(NOT ${COMPILE_SOURCE_VARIABLE})
                     set(SUGGEST_BINUTILS_UPDATE 1 PARENT_SCOPE)
                 endif()
             endif()
 
-            if(${COMPILE_FLAG_VARIABLE} AND ${COMPILE_SIMD_VARIABLE})
-                set(${VARIABLE}_FLAG "${_testflag}" CACHE INTERNAL "${DESCRIPTION}")
-                set(${VARIABLE} 1 CACHE INTERNAL "Result of test for ${DESCRIPTION}" FORCE)
+            if(${COMPILE_FLAG_VARIABLE} AND ${COMPILE_SOURCE_VARIABLE})
+                set(${VARIABLE} 1 PARENT_SCOPE)
+                set(${CXXFLAGSVAR} "${${CXXFLAGSVAR}} ${_testflag}" PARENT_SCOPE)
                 break()
-            else()
-                set(${VARIABLE} 0 CACHE INTERNAL "Result of test for ${DESCRIPTION}" FORCE)
             endif()
         endforeach()
-    ENDIF()
-
-    IF (${VARIABLE})
-        SET (${CXXFLAGSVAR} "${${CXXFLAGSVAR}} ${${VARIABLE}_FLAG}" PARENT_SCOPE)
-    ENDIF ()
-
-ENDFUNCTION(GMX_FIND_CXXFLAG_FOR_SOURCE VARIABLE DESCRIPTION SOURCE CXXFLAGSVAR)
+        # If no flag has been found, then leaving ${VARIABLE} unset
+        # will be interpreted by CMake as false.
+    endif()
+ENDFUNCTION()
 
