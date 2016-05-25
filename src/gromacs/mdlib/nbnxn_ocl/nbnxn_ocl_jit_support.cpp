25d5aebacc285d0a452d2e21c130eaad174e3dab
Berk Hess <hess@kth.se>
2016-05-25 15:19:02 +0200

    Avoid numerical overflow with overlapping atoms
    The verlet kernels did not allow overlapping atoms, even if they were
    not interacting (in contrast to the group kernels). Fixed by clamping
    the interaction distance so it can not become smaller than ~6e-4
    in single and ~1e-18 in double, and when this number is later
    multiplied by zero parameters it will not influence forces. The
    clamping should never affect normal interactions; we would previously
    crash for distances that were this small.
    On Haswell, RF and PME kernels get 3% and 1% slower, respectively.
    On CUDA, RF and PME kernels get 1% and 2% faster, respectively.
    
    Fixes #1958.
    
    Change-Id: I83b88f0e9ca34dc151a8b907f334a95a1a4301cc

--- src/gromacs/mdlib/nbnxn_ocl/nbnxn_ocl_jit_support.cpp
+++ src/gromacs/mdlib/nbnxn_ocl/nbnxn_ocl_jit_support.cpp
@@ -199,14 +199,14 @@ nbnxn_gpu_compile_kernels(gmx_nbnxn_ocl_t *nb)
      * in the JIT compilation that happens at runtime.
      */
     sprintf(runtime_consts,
-            "-DCENTRAL=%d -DNBNXN_GPU_NCLUSTER_PER_SUPERCLUSTER=%d -DNBNXN_GPU_CLUSTER_SIZE=%d -DNBNXN_GPU_JGROUP_SIZE=%d -DNBNXN_AVOID_SING_R2_INC=%s %s",
-            CENTRAL,                                    /* Defined in ishift.h */
-            c_nbnxnGpuNumClusterPerSupercluster,        /* Defined in nbnxn_pairlist.h */
-            c_nbnxnGpuClusterSize,                      /* Defined in nbnxn_pairlist.h */
-            c_nbnxnGpuJgroupSize,                       /* Defined in nbnxn_pairlist.h */
-            STRINGIFY_MACRO(NBNXN_AVOID_SING_R2_INC)    /* Defined in nbnxn_consts.h */
-                                                        /* NBNXN_AVOID_SING_R2_INC passed as string to avoid
-                                                           floating point representation problems with sprintf */
+            "-DCENTRAL=%d -DNBNXN_GPU_NCLUSTER_PER_SUPERCLUSTER=%d -DNBNXN_GPU_CLUSTER_SIZE=%d -DNBNXN_GPU_JGROUP_SIZE=%d -DNBNXN_MIN_RSQ=%s %s",
+            CENTRAL,                             /* Defined in ishift.h */
+            c_nbnxnGpuNumClusterPerSupercluster, /* Defined in nbnxn_pairlist.h */
+            c_nbnxnGpuClusterSize,               /* Defined in nbnxn_pairlist.h */
+            c_nbnxnGpuJgroupSize,                /* Defined in nbnxn_pairlist.h */
+            STRINGIFY_MACRO(NBNXN_MIN_RSQ)       /* Defined in nbnxn_consts.h */
+                                                 /* NBNXN_MIN_RSQ passed as string to avoid
+                                                    floating point representation problems with sprintf */
             , (nb->bPrefetchLjParam) ? "-DIATYPE_SHMEM" : ""
             );
 
