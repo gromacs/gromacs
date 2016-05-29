fdb3005bcd3760c2925baf3611bf92cda50bb921
Teemu Murtola <teemu.murtola@gmail.com>
2016-05-29 07:22:05 +0300

    [RFC] Basic C++ logging interface
    Add C++ classes to replace md_logging.*, as well as some other cases in
    the future.  Split the implementation to a separate change; child
    change(s) will actually take this into use once the interface is final.
    
    RFC because documentation is still missing.
    
    Change-Id: I8fc8550194853a5387057fdb7772f882fc061708

--- src/gromacs/utility/stringutil.h
+++ src/gromacs/utility/stringutil.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2011,2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2011,2012,2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -43,6 +43,7 @@
 #ifndef GMX_UTILITY_STRINGUTIL_H
 #define GMX_UTILITY_STRINGUTIL_H
 
+#include <cstdarg>
 #include <cstring>
 
 #include <string>
@@ -167,6 +168,7 @@ std::string stripString(const std::string &str);
  * supported.
  */
 std::string formatString(const char *fmt, ...);
+std::string formatStringV(const char *fmt, va_list ap);
 
 /*! \brief Function object that wraps a call to formatString() that
  * expects a single conversion argument, for use with algorithms. */
