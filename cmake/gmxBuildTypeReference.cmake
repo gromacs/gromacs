3a89e4010e097239c2c5b4eebdd20e56794fff64
Mark Abraham <mark.j.abraham@gmail.com>
2016-06-01 13:38:28 +0200

    Make more CMake variables advanced
    This keeps the ccmake display down to things users are reasonably
    likely to want to change.
    
    Change-Id: Ia78387bb32f5d6c7f6b82e453b155fdf657a044a

--- cmake/gmxBuildTypeReference.cmake
+++ cmake/gmxBuildTypeReference.cmake
@@ -37,7 +37,7 @@
 set( CMAKE_CXX_FLAGS_REFERENCE "-O0 -g" CACHE STRING "C++ flags for regressiontests reference runs." FORCE)
 set( CMAKE_C_FLAGS_REFERENCE "-O0 -g" CACHE STRING "C flags for regressiontests reference runs." FORCE)
 set( CMAKE_EXE_LINKER_FLAGS_REFERENCE "" CACHE STRING "Linker flags for regressiontests reference runs.")
-mark_as_advanced( CMAKE_CXX_FLAGS_REFERENCE CMAKE_C_FLAGS_REFERENCE)
+mark_as_advanced( CMAKE_CXX_FLAGS_REFERENCE CMAKE_C_FLAGS_REFERENCE CMAKE_EXE_LINKER_FLAGS_REFERENCE)
 
 # turn off all fancy options for the regressiontests reference build
 if("${CMAKE_BUILD_TYPE}" STREQUAL "Reference")
