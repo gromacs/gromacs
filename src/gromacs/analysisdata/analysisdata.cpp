fe5845be298114c1e3009f2db313c7315bef57f9
Teemu Murtola <teemu.murtola@gmail.com>
2016-06-04 21:09:22 +0300

    [WIP] Add selection-enabled gmx traj
    For now, this tool only plots coordinates, velocities, and forces for
    selections, so it should provide a full replacement for -ox, -ov, -of,
    -com, and -mol from gmx traj.
    
    WIP because tests are missing.
    
    Change-Id: Ia3b0e77353d9183ffc049d1e1fb0253e30332664

--- src/gromacs/analysisdata/analysisdata.cpp
+++ src/gromacs/analysisdata/analysisdata.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -280,14 +280,15 @@ AnalysisDataHandle::setPoint(int column, real value, real error, bool bPresent)
 
 
 void
-AnalysisDataHandle::setPoints(int firstColumn, int count, const real *values)
+AnalysisDataHandle::setPoints(int firstColumn, int count, const real *values,
+                              bool bPresent)
 {
     GMX_RELEASE_ASSERT(impl_ != NULL, "Invalid data handle used");
     GMX_RELEASE_ASSERT(impl_->currentFrame_ != NULL,
                        "setPoints() called without calling startFrame()");
     for (int i = 0; i < count; ++i)
     {
-        impl_->currentFrame_->setValue(firstColumn + i, values[i]);
+        impl_->currentFrame_->setValue(firstColumn + i, values[i], bPresent);
     }
 }
 
--- src/gromacs/trajectoryanalysis/modules/distance.cpp
+++ src/gromacs/trajectoryanalysis/modules/distance.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -345,7 +345,7 @@ Distance::analyzeFrame(int frnr, const t_trxframe &fr, t_pbc *pbc,
             real dist     = norm(dx);
             bool bPresent = p1.selected() && p2.selected();
             distHandle.setPoint(n, dist, bPresent);
-            xyzHandle.setPoints(n*3, 3, dx);
+            xyzHandle.setPoints(n*3, 3, dx, bPresent);
         }
     }
     distHandle.finishFrame();
