fbc99c75abe169b792f53ddceecfe1e9226d199f
Vedran Miletić <vedran@miletic.net>
2016-06-21 15:53:42 +0200

    Prevent writing to unallocated memory in mk_specbonds
    Stop creating new specbonds after at most nspec bonds have been created
    to prevent writing to unallocated memory.
    
    Change-Id: I53f9d20059915e7fba8767b92d92fa751e9165e3

--- src/gromacs/gmxpreprocess/specbond.c
+++ src/gromacs/gmxpreprocess/specbond.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -329,7 +329,8 @@ int mk_specbonds(t_atoms *pdba, rvec x[], gmx_bool bInteractive,
             for (j = i+1; (j < nspec); j++)
             {
                 aj = sgp[j];
-                if (is_bond(nsb, sb, pdba, ai, aj, d[i][j], &index_sb, &bSwap))
+                /* Ensure creation of at most nspec special bonds to avoid overflowing bonds[] */
+                if (nbonds < nspec && is_bond(nsb, sb, pdba, ai, aj, d[i][j], &index_sb, &bSwap))
                 {
                     fprintf(stderr, "%s %s-%d %s-%d and %s-%d %s-%d%s",
                             bInteractive ? "Link" : "Linking",
