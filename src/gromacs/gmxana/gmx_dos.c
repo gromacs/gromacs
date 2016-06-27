e65c43da16aac8411a27ac1952d1e035c2a095ad
David van der Spoel <spoel@xray.bmc.uu.se>
2016-06-27 14:47:24 +0200

    Made gmx dos work again.
    Due to an error in the index handling gmx dos always stopped with a fatal
    error.
    
    Fixes #1996
    
    Change-Id: Iba7685c1e1b86acc92427902a2187eb4e6c9f260

--- src/gromacs/gmxana/gmx_dos.c
+++ src/gromacs/gmxana/gmx_dos.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2011,2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2011,2012,2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -89,7 +89,7 @@ static int calcMoleculesInIndexGroup(t_block *mols, int natoms, atom_id *index, 
                 gmx_fatal(FARGS, "The index group does not consist of whole molecules");
             }
             i++;
-            if (i == natoms)
+            if (i > natoms)
             {
                 gmx_fatal(FARGS, "Index contains atom numbers larger than the topology");
             }
