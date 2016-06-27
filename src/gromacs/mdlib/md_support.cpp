a16cb590133893e9ec5162889f8d23680b01e644
Michael Shirts <michael.shirts@virginia.edu>
2016-06-26 22:19:26 -0600

    Fix for incorrectly setting expanded ensemble when fep_state = 0
    Expanded ensemble lambda values were not being copied over when the
    proposed fep_state was 0.  This commit fixes that by removing a
    a conditonal that was incorrectly added.
    
    Fixes #1995
    
    Change-Id: I3d49b0936d973fb70a9a79799743f5069ba4fff4

--- src/gromacs/mdlib/md_support.cpp
+++ src/gromacs/mdlib/md_support.cpp
@@ -526,15 +526,12 @@ void set_current_lambdas(gmx_int64_t step, t_lambda *fepvals, gmx_bool bRerunMD,
         }
         else
         {
-            if (state->fep_state > 0)
-            {
-                state_global->fep_state = state->fep_state; /* state->fep is the one updated by bExpanded */
-                for (i = 0; i < efptNR; i++)
-                {
-                    state_global->lambda[i] = fepvals->all_lambda[i][state_global->fep_state];
-                }
-            }
-        }
+			state_global->fep_state = state->fep_state; /* state->fep_state is the one updated by bExpanded */
+			for (i = 0; i < efptNR; i++)
+			{
+				state_global->lambda[i] = fepvals->all_lambda[i][state_global->fep_state];
+			}
+		}
     }
     for (i = 0; i < efptNR; i++)
     {
