16c554d300e9b2aa4c420354aac59d66912f2de1
Christian Blau <cblau@gwdg.de>
2016-07-07 16:11:32 +0200

    Add LocalAtomSet for handling atom indices.
    Moves reading and updating indices for a set of atoms to a
    single place. Pulling, IMD, rotation enforcement, essential
    dynamics etc. all use their own methods triggered in domain
    decompostion to handle atom indices in parallel simulations.
    This class works towards removing the exposed hooks to all
    these modules in domain decomposition.
    
    External potentials will use LocalAtomSet to access atom
    subsets to make handling of atom indices not the task of individual
    external potentials, but of an parallel-Atom API instead.
    
    Related to #1972 .
    
    Change-Id: I69460963927249c7d713e76f166e6c87122c68de

---
+++ src/gromacs/domdec/localatomset.h
@@ -0,0 +1,133 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \libinternal \brief
+ * Declares gmx::LocalAtomSet.
+ *
+ * \author Christian Blau <cblau@gwdg.de>
+ * \ingroup module_domdec
+ */
+#ifndef GMX_LOCALATOMSET_H
+#define GMX_LOCALATOMSET_H
+
+#include <memory>
+#include <vector>
+
+#include "gromacs/utility/classhelpers.h"
+
+struct gmx_ga2la_t;
+
+namespace gmx
+{
+
+/*! \libinternal \brief
+ * A local atom set is a collection of local, global and collective indices of atoms.
+ *
+ *  Local atom sets are constructed, accessed and destroyed by LocalAtomSetManager.
+ */
+class LocalAtomSet
+{
+    public:
+
+        /* Only LocalAtomSetManger may create, initialize and destroy group indexs and
+         * trigger index updates. */
+        friend class LocalAtomSetManager;
+
+        /* \brief Provide a move constructor so we can store LocalAtomSets in a
+         * std::vector, yet PIMPL.
+         * \param[in] other Atom set to be move-constructed.
+         */
+        LocalAtomSet(LocalAtomSet &&other);
+
+        /* \brief Provide a destructor so we can store LocalAtomSets in a
+         * std::vector, yet PIMPL. */
+        ~LocalAtomSet();
+
+        /*! \brief Maps indices on node (0..num_atoms_local_) to global atom indicices.
+         *
+         * \returns the collective index.
+         */
+        const std::vector<int> &collectiveIndex() const;
+
+        /*! \brief
+         * Global indices of the atoms in this set.
+         *
+         * \returns the global index.
+         */
+        const std::vector<int> &globalIndex() const;
+
+        /*! \brief
+         * Local indices of the atoms.
+         *
+         * Access,e.g., the i-th local atom coordinate of this set by x[local_index_[i]].
+         * Constructed and updated every domain-decomposition step
+         *
+         * \returns the local index.
+         */
+        const std::vector<int> &localIndex() const;
+        /*! \brief The number of atoms from this group index on this node.*/
+        size_t numAtomsLocal() const;
+        /*! \brief The number of all atoms from this group index on all nodes together. */
+        size_t numAtomsGlobal() const;
+    private:
+
+        /*! \brief Only LocalAtomSetManager may construct an atom set with an index group.
+         *
+         * If called within a parallel simulation, initialization shall be finished
+         * during domain decomposition with setLocalAndCollectiveIndices.
+         *
+         * \param[in] number_of_atoms The total number of atoms of this atom set.
+         * \param[in] index An global integer index of the atoms in the index set.
+         * \param[in] bParallel True if simulation is run in parallel
+         */
+        explicit LocalAtomSet(const int number_of_atoms, const int *index, bool bParallel);
+
+        /*! \brief Sets the local and collective indices from a lookup in ga2la.
+         *
+         * This makes only sense when atoms are indeed distributed over nodes,
+         * otherwise local indices equal global indices and are set in init.
+         *
+         * \param[in] ga2la lookup table that reports if an atom is local.
+         */
+        void setLocalAndCollectiveIndices(const gmx_ga2la_t  *ga2la);
+
+        class Impl;
+        PrivateImplPointer<Impl> impl_;
+
+};
+
+} // namespace gmx
+
+#endif
