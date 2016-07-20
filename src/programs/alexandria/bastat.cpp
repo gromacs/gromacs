714528e025c49a2950cafc235a8cce5d6616fbd4
mohammad-ghahremanpour <ghahremanpour.gro@gmail.com>
2016-07-20 11:51:52 +0200

    updates
    Change-Id: Ie3cc10d8b0f4165bd28dc0b270a81b77ec58356a

--- src/programs/alexandria/bastat.cpp
+++ src/programs/alexandria/bastat.cpp
@@ -442,6 +442,7 @@ void update_pd(FILE *fp, t_bonds *b, Poldata &pd,
 
     for (auto &i : b->angle)
     {
+
         gmx_stats_get_average(i.lsq, &av);
         gmx_stats_get_sigma(i.lsq, &sig);
         gmx_stats_get_npoints(i.lsq, &N);
