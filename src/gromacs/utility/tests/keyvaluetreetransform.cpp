997dec08c402f635415002c67bb6edfbc4464ba9
Teemu Murtola <teemu.murtola@gmail.com>
2016-07-26 16:05:13 +0300

    Basic support for transforming KeyValueTrees
    Add support for writing transforms from one KeyValueTree to another.
    For now, only a small subset of simple transforms is supported, but this
    should be mostly sufficient for a proof-of-concept for MDP processing on
    the electric field code, and new cases can be relatively easily added.
    
    These transforms can support two use cases: specifying how a legacy MDP
    file maps to a more structured format, and for supporting changes from
    version to version in the structured format.  However, versioning for
    the structured format (in particular, if/when it will be necessary to
    recognize the version from input) likely still needs some work.
    
    There is not much in terms of consistency checks, error handling, or
    documentation, but that is better to add once using code exists.
    
    Change-Id: Ie2570eee8a62c2d64b79e00925bbf1e40b62b261

---
+++ src/gromacs/utility/tests/keyvaluetreetransform.cpp
@@ -0,0 +1,143 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#include "gmxpre.h"
+
+#include "gromacs/utility/keyvaluetreetransform.h"
+
+#include <string>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "gromacs/utility/keyvaluetree.h"
+#include "gromacs/utility/keyvaluetreebuilder.h"
+#include "gromacs/utility/strconvert.h"
+#include "gromacs/utility/stringutil.h"
+
+#include "testutils/refdata.h"
+
+namespace
+{
+
+class TreeValueTransformTest : public ::testing::Test
+{
+    public:
+        void testTransform(const gmx::KeyValueTreeObject      &input,
+                           const gmx::KeyValueTreeTransformer &transform)
+        {
+            gmx::KeyValueTreeObject         result = transform.transform(input);
+
+            gmx::test::TestReferenceData    data;
+            gmx::test::TestReferenceChecker checker(data.rootChecker());
+            checker.checkKeyValueTreeObject(input, "Input");
+            checker.checkKeyValueTreeObject(result, "Tree");
+        }
+};
+
+TEST_F(TreeValueTransformTest, SimpleTransforms)
+{
+    gmx::KeyValueTreeBuilder     builder;
+    builder.rootObject().addValue<std::string>("a", "1");
+    builder.rootObject().addValue<std::string>("b", "2");
+    gmx::KeyValueTreeObject      input = builder.build();
+
+    gmx::KeyValueTreeTransformer transform;
+    transform.rules()->addRule()
+        .from<std::string>("/a").to<int>("/i").transformWith(&gmx::fromStdString<int>);
+    transform.rules()->addRule()
+        .from<std::string>("/b").to<int>("/j").transformWith(&gmx::fromStdString<int>);
+
+    testTransform(input, transform);
+}
+
+TEST_F(TreeValueTransformTest, SimpleTransformsToObject)
+{
+    gmx::KeyValueTreeBuilder     builder;
+    builder.rootObject().addValue<std::string>("a", "1");
+    builder.rootObject().addValue<std::string>("b", "2");
+    gmx::KeyValueTreeObject      input = builder.build();
+
+    gmx::KeyValueTreeTransformer transform;
+    transform.rules()->addRule()
+        .from<std::string>("/a").to<int>("/foo/i").transformWith(&gmx::fromStdString<int>);
+    transform.rules()->addRule()
+        .from<std::string>("/b").to<int>("/foo/j").transformWith(&gmx::fromStdString<int>);
+
+    testTransform(input, transform);
+}
+
+
+TEST_F(TreeValueTransformTest, ObjectFromString)
+{
+    gmx::KeyValueTreeBuilder     builder;
+    builder.rootObject().addValue<std::string>("a", "1 2");
+    gmx::KeyValueTreeObject      input = builder.build();
+
+    gmx::KeyValueTreeTransformer transform;
+    transform.rules()->addRule()
+        .from<std::string>("/a").toObject("/foo").transformWith(
+            [] (gmx::KeyValueTreeObjectBuilder *builder, const std::string &value)
+            {
+                std::vector<std::string> values = gmx::splitString(value);
+                builder->addValue<int>("a", gmx::fromString<int>(values[0]));
+                builder->addValue<int>("b", gmx::fromString<int>(values[1]));
+            });
+
+    testTransform(input, transform);
+}
+
+TEST_F(TreeValueTransformTest, ObjectFromMultipleStrings)
+{
+    gmx::KeyValueTreeBuilder     builder;
+    builder.rootObject().addValue<std::string>("a", "1");
+    builder.rootObject().addValue<std::string>("b", "2 3");
+    gmx::KeyValueTreeObject      input = builder.build();
+
+    gmx::KeyValueTreeTransformer transform;
+    transform.rules()->addRule()
+        .from<std::string>("/a").to<int>("/foo/a").transformWith(&gmx::fromStdString<int>);
+    transform.rules()->addRule()
+        .from<std::string>("/b").toObject("/foo").transformWith(
+            [] (gmx::KeyValueTreeObjectBuilder *builder, const std::string &value)
+            {
+                std::vector<std::string> values = gmx::splitString(value);
+                builder->addValue<int>("b", gmx::fromString<int>(values[0]));
+                builder->addValue<int>("c", gmx::fromString<int>(values[1]));
+            });
+
+    testTransform(input, transform);
+}
+
+} // namespace
