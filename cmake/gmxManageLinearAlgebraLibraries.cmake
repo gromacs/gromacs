a571b323c8f4eb53ba44b85f6994c33b19567a82
Erik Lindahl <erik@kth.se>
2016-08-05 12:05:26 +0200

    Bump oldest cmake, compiler and CUDA versions required
    For release 2017 we now require gcc-4.8.1, clang-3.3 and icc-14, so we
    can rely on full C++11 support. We now also require CUDA-6.5 and
    CMake-3.4.3. This in turn means we can remove some older cmake tests,
    and since many users won't read the install guide there are now
    version tests that produce fatal errors during CMake configuration for
    compiler versions we know are too old.
    
    Various hacks and workarounds in source and build system can
    now be removed.
    
    Fixes #2012.
    
    Change-Id: I36e02379a985b22c72f0f06481c65cae6e780c02

--- cmake/gmxManageLinearAlgebraLibraries.cmake
+++ cmake/gmxManageLinearAlgebraLibraries.cmake
@@ -89,7 +89,7 @@ macro(manage_linear_algebra_library name function_in_library)
             set(CMAKE_REQUIRED_FLAGS "${FFT_LINKER_FLAGS}")
             # This may also not work correctly if the user changes
             # MKL_LIBRARIES after the first run. However,
-            # MKL_LIBRARIES is only needed for icc version < 11, or
+            # MKL_LIBRARIES is only needed
             # for trying to use MKL with a non-Intel compiler, and we
             # can live with that for now.
             check_function_exists(${function_in_library} _${name}_mkl_works)
@@ -110,8 +110,7 @@ macro(manage_linear_algebra_library name function_in_library)
         if (NOT _library_was_found)
             set(${name}_FIND_QUIETLY ${_find_quietly})
             # Note that this finds all kinds of system libraries,
-            # including Apple's Accelerate Framework (and perhaps MKL for
-            # icc < 11).
+            # including Apple's Accelerate Framework
             find_package(${name})
             if (${name}_FOUND)
                 set(_libraries_to_link ${${name}_LIBRARIES})
