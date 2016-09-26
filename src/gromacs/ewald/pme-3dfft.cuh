297af93c0646bebfa18d79d304c893ea356a47eb
Aleksei Iupinov <a.yupinov@gmail.com>
2016-09-26 12:53:29 +0200

    PME GPU/CUDA data framework.
    This patch adds most of the PME GPU data handling routines
    (pme-gpu.h/cpp / pme.cu(h) / pme-gpu-internal.h/cpp),
    data structures used on host and on device
    (pme-gpu-types.h / pme.cuh).
    The main PME GPU body will later live in pme-gpu.cpp as well.
    The PME GPU kernels and their host code will live in separate files.
    There is also cuFFT code (pme-3dfft.cu(h)) and
    optional CUDA timing events (pme-timings.cu(h)) included.
    Currently this is a dead code, PME GPU is not actually getting initialized
    as the corresponding boolean is always false which is asserted.
    
    Change-Id: I9b03f54a2412885e25ea27f17bf2dca1b01f9f78

---
+++ src/gromacs/ewald/pme-3dfft.cuh
@@ -0,0 +1,100 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+/*! \libinternal \file
+ *  \brief Defines the CUDA 3D-FFT functions for PME.
+ *
+ *  \author Aleksei Iupinov <a.yupinov@gmail.com>
+ */
+
+#ifndef PME3DFFT_CUH
+#define PME3DFFT_CUH
+
+#include <cufft.h>                  // for the cufft types
+
+#include "gromacs/fft/fft.h"        // for the enum gmx_fft_direction
+#include "gromacs/math/vectypes.h"  // for the ivec
+
+struct pme_gpu_t;
+
+/*! \brief \internal A 3D FFT class for performing R2C/C2R transforms */
+class parallel_3dfft_gpu_t
+{
+    ivec          nDataReal_;
+    ivec          sizeReal_;
+    ivec          sizeComplex_;
+
+    cufftHandle   planR2C_;
+    cufftHandle   planC2R_;
+    cufftReal    *realGrid_;
+    cufftComplex *complexGrid_;
+
+    /* unused */
+    ivec          localOffset_;
+    public:
+        /*! \brief
+         * Constructs CUDA FFT plans for performing 3D FFT on a PME grid.
+         *
+         * \param[in] pmeGPU                  The PME GPU structure.
+         */
+        parallel_3dfft_gpu_t(const pme_gpu_t *pmeGPU);
+        /*! \brief Destroys CUDA FFT plans. */
+        ~parallel_3dfft_gpu_t();
+        /*! \brief
+         * Returns the grid dimensions of the local real-space grid.
+         *
+         * \param[out]   localNData           The numbers of real elements in the local grid.
+         * \param[out]   localOffset          The offsets of the local grid.
+         * \param[out]   localSize            The numbers of real elements (with padding) in the local grid.
+         */
+        void get_real_limits(ivec localNData, ivec localOffset, ivec localSize);
+        /*! \brief
+         * Returns the grid dimensions of the local complex grid.
+         *
+         * \param[out]   localNData           The numbers of complex elements in the local grid.
+         * \param[out]   localOffset          The offsets of the local grid.
+         * \param[out]   localSize            The numbers of complex elements (with padding) in the local grid.
+         */
+        void get_complex_limits(ivec localNData, ivec localOffset, ivec localSize);
+        /*! \brief
+         * Performs the 3D FFT.
+         *
+         * \param[in] dir                     The transform direction.
+         * \returns                           The cuFFT result code (0 if no error).
+         */
+        cufftResult_t perform_3dfft(gmx_fft_direction dir);
+};
+
+#endif // PME3DFFT_CUH
