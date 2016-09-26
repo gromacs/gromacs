f91b8f9e3a9a373b87a8742f275855b0f9cff17a
Aleksei Iupinov <a.yupinov@gmail.com>
2016-09-26 12:53:29 +0200

    PME GPU/CUDA data framework.
    This patch adds most of the PME GPU data handling routines
    (pme.h and pme-gpu.cpp / pme.cu(h) / pme-gpu-internal.h/cpp),
    data structures used on host and on device
    (pme-gpu-types.h / pme.cuh).
    The PME GPU kernels and their host code will live in separate files.
    There is also cuFFT code (pme-3dfft.cu(h)) and
    optional CUDA timing events (pme-timings.cu(h)) included.
    Currently this is a dead code, PME GPU is not actually getting initialized
    as the corresponding boolean is always false which is asserted.
    
    Change-Id: I9b03f54a2412885e25ea27f17bf2dca1b01f9f78

---
+++ src/gromacs/ewald/pme-timings.cuh
@@ -0,0 +1,115 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016,2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+/*! \libinternal \file
+ *  \brief Defines the GPU timing event class in CUDA, and the PME GPU timing functions.
+ *
+ *  \author Aleksei Iupinov <a.yupinov@gmail.com>
+ */
+
+#ifndef GMX_EWALD_PME_TIMINGS_CUH
+#define GMX_EWALD_PME_TIMINGS_CUH
+
+#include "gromacs/timing/gpu_timing.h"       // for the PME GPU timing events enum
+#include "gromacs/utility/basedefinitions.h"
+
+struct pme_gpu_t;
+
+/*! \libinternal
+ * \brief
+ * This is a GPU timing class, based on CUDA events.
+ * It should allow for host-side tracking of the accumulated execution timespans in the CUDA code.
+ * (Measuring kernel or transfers duration).
+ * Note however, that the data reported by CUDA events is not correct with multiple CUDA streams (e.g. PME and NB).
+ * The reason is that currently the underlying CUDA events use the host-side queue.
+ *
+ * \ingroup module_ewald
+ */
+class pme_gpu_timing
+{
+    //! Tells if the object was properly initialized. TODO: mark the object as not copyable instead?
+    bool          initialized_;
+    //! The underlying synchronization event pair - the beginning and the end of the timespan
+    cudaEvent_t   eventStart_, eventStop_;
+    //! The number of times the timespan was measured
+    unsigned int  callCount_;
+    //! The accumulated duration of the timespans measured (milliseconds)
+    double        totalMilliseconds_;
+
+    public:
+        pme_gpu_timing();
+        ~pme_gpu_timing();
+
+        /*! \brief
+         * To be called before the kernel/transfer launch.
+         *
+         * \param[in] s   The CUDA stream where the event being measured takes place.
+         */
+        void startRecording(cudaStream_t s);
+        /*! \brief
+         * To be called after the kernel/transfer launch.
+         *
+         * \param[in] s   The CUDA stream where the event being measured took place.
+         */
+        void stopRecording(cudaStream_t s);
+        /*! \brief To be called after stop_recording and the CUDA stream of the event has been synchronised. */
+        void update();
+        /*! \brief Enables the timing event once. */
+        void enable();
+        /*! \brief Resets the timing event total time/call count. */
+        void reset();
+        /*! \brief Gets total time recorded. */
+        float getTotalTimeMilliseconds();
+        /*! \brief Gets total call count recorded. */
+        unsigned int getCallCount();
+};
+
+/*! \libinternal \brief
+ * Starts timing the certain PME GPU stage during a single step (if timings are enabled).
+ *
+ * \param[in] pmeGPU         The PME GPU data structure.
+ * \param[in] PMEStageId     The PME GPU stage gtPME_ index from the enum in src/gromacs/timing/gpu_timing.h
+ */
+void pme_gpu_start_timing(const pme_gpu_t *pmeGPU, size_t PMEStageId);
+
+/*! \libinternal \brief
+ * Stops timing the certain PME GPU stage during a single step (if timings are enabled).
+ *
+ * \param[in] pmeGPU         The PME GPU data structure.
+ * \param[in] PMEStageId     The PME GPU stage gtPME_ index from the enum in src/gromacs/timing/gpu_timing.h
+ */
+void pme_gpu_stop_timing(const pme_gpu_t *pmeGPU, size_t PMEStageId);
+
+#endif
