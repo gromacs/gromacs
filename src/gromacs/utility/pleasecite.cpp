d5244f0437a0bcd46be828bc0c69e57999f7b7c7
Philip Loche <philip-loche@hotmail.de>
2016-09-28 13:49:13 +0200

    EW3DC for non-neutral systems
    Add the corrections to force and energy according to
    Ballenegger, Arnold, and Cerdà, J. Chem. Phys. 131, 094107 2009
    (http://dx.doi.org/10.1063/1.3216473).
    Added a warning in grompp for charged systems with Ewald.
    
    Added a ref to Hub2014a too to help users change their mind.
    
    Change-Id: I96a2ab51fede9b448d4c725d703faf71342e7346

--- src/gromacs/utility/pleasecite.cpp
+++ src/gromacs/utility/pleasecite.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -366,6 +366,16 @@ void please_cite(FILE *fp, const char *key)
           "GROMACS: High performance molecular simulations through multi-level parallelism from laptops to supercomputers",
           "SoftwareX",
           1, 2015, "19-25" },
+        { "Ballenegger2009",
+          "V. Ballenegger, A. Arnold, J. J. Cerdà",
+          "Simulations of non-neutral slab systems with long-range electrostatic interactions in two-dimensional periodic boundary conditions",
+          "J. Chem. Phys",
+          131, 2009, "094107" },
+        { "Hub2014a",
+          "J. S. Hub, B. L. de Groot, H. Grubmueller, G. Groenhof",
+          "Quantifying Artifacts in Ewald Simulations of Inhomogeneous Systems with a Net Charge",
+          "J. Chem. Theory Comput.",
+          10, 2014, "381-393" },
     };
 #define NSTR (int)asize(citedb)
 
