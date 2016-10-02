18ea4df9c77f6b6b91029b863b60af360f8f46a2
Teemu Murtola <teemu.murtola@gmail.com>
2016-10-02 21:26:16 +0300

    Improved error handling for KeyValueTree operations
    Support an error handler for operations involving KeyValueTrees to allow
    for better error messages during mdp parsing.
    
    Change-Id: I3738b956baafe7a4ecb96ec6f9e4729541d74bf8

--- src/gromacs/utility/keyvaluetree.h
+++ src/gromacs/utility/keyvaluetree.h
@@ -58,6 +58,32 @@ namespace gmx
 class KeyValueTreeArray;
 class KeyValueTreeObject;
 
+class KeyValueTreePath
+{
+    public:
+        KeyValueTreePath() = default;
+        KeyValueTreePath(const std::string &path);
+
+        void append(const std::string &key) { path_.push_back(key); }
+        void pop_back() { return path_.pop_back(); }
+        std::string pop_last()
+        {
+            std::string result = std::move(path_.back());
+            path_.pop_back();
+            return result;
+        }
+
+        bool empty() const { return path_.empty(); }
+        size_t size() const { return path_.size(); }
+        const std::string &operator[](int i) const { return path_[i]; }
+        const std::vector<std::string> &elements() const { return path_; }
+
+        std::string toString() const;
+
+    private:
+        std::vector<std::string> path_;
+};
+
 class KeyValueTreeValue
 {
     public:
