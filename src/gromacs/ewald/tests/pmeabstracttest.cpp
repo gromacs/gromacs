d70efe5413de890935252b1140db27603bf88235
Aleksei Iupinov <a.yupinov@gmail.com>
2016-10-10 12:23:10 +0200

    PME B-Spline moduli computation unit test
    Ewald module is currently not covered by tests at all.
    As unit testing is needed both for the GPU and CPU codepaths,
    this first patch introduces small abstract classes for testing PME stages
    to fail gracefully (gmx_fatal) or reproducing results correctly.
    These 2 types of tests are implemented as a proof of concept
    for the smallest part of PME - B-spline moduli precomputation.
    What is of interest is how much valid inputs we want to test
    (currently it iterates over orders from 3 to 8/12, PME or P3M, 2 sample grid sizes).
    I am also not sure whether and which checker tolerance should be selected.
    
    Change-Id: I6be93f94b6b17e901667bbd3d39c967be6cbe77e

---
+++ src/gromacs/ewald/tests/pmeabstracttest.cpp
@@ -0,0 +1,80 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Implements abstract base classes for PME tests.
+ *
+ * \author Aleksei Iupinov <a.yupinov@gmail.com>
+ * \ingroup module_ewald
+ */
+#include "gmxpre.h"
+
+#include "pmeabstracttest.h"
+
+#include <gmock/gmock.h>
+
+#include "gromacs/ewald/pme.h"
+#include "gromacs/mdrunutility/mdmodules.h"
+#include "gromacs/utility/scoped_cptr.h"
+
+#include "testutils/refdata.h"
+
+/*! \brief PME destructor function wrapper for the safe pointer. */
+void pme_free_wrapper(gmx_pme_t *pme)
+{
+    if (pme)
+    {
+        gmx_pme_destroy(&pme);
+    }
+}
+
+void AbstractPmeTestBase::Init()
+{
+    gmx_pme_t *pmeDataRaw = NULL;
+    gmx_pme_init(&pmeDataRaw, NULL, 1, 1, const_cast<t_inputrec *>(inputRec_),
+                 0, FALSE, FALSE, TRUE, 0.0, 0.0, 1);
+    pme_.reset(pmeDataRaw); // taking ownership of the non-const pointer
+}
+
+void AbstractPmeDeathTestBase::Check()
+{
+    EXPECT_DEATH(RunTest(), "Fatal error");
+}
+
+void AbstractPmeCorrectnessTestBase::Check()
+{
+    RunTest();
+    CheckOutputs();
+}
