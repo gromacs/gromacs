04eb136f7ad7652dfa721b7582233348ba05baeb
Teemu Murtola <teemu.murtola@gmail.com>
2016-10-11 22:30:47 +0300

    [RFC] Generic tpr handling for electric field
    Remove special code to store inputrec values to tpr files from
    IInputRecExtension, and instead store them as a generic KeyValueTree.
    
    RFC because there are a few discussion points related to forward
    compatibility that would need clarification:
     - Currently, default values for any options not specified in the mdp
       file are stored in the tpr file.  This means that it is not possible
       (without quite a bit of extra work, and extra information stored in
       the tpr file) to know in the modules whether some value has been set
       by the user or whether it comes from a default.  Is this a problem,
       and if it is, how should we deal with it?
     - Currently, there is no support for changing the storage format, other
       than by adding new options.  We may not need this immediately, and I
       think it should not be any extra work to introduce it later (rather
       than right now), but this needs to be considered.  With this
       approach, tpr backwards compatibility is essentially the same as mdp
       backwards compatibility.
     - The current structure in the electric field code may not be ideal for
       mdp input; we might want to, e.g., put the values under a common
       'applied-forces' section, instead of having them at the top level.
       This would benefit from some planning for the overall mdp structure
       for the future.
    
    Change-Id: I0a0f552041b2a7401bc82972c9648d1f3a64af34

--- src/programs/mdrun/runner.cpp
+++ src/programs/mdrun/runner.cpp
@@ -778,6 +778,7 @@ int mdrunner(gmx_hw_opt_t *hw_opt,
     {
         /* Read (nearly) all data required for the simulation */
         read_tpx_state(ftp2fn(efTPR, nfile, fnm), inputrec, state, mtop);
+        mdModules.assignOptionsToModulesFromInputrec();
 
         if (inputrec->cutoff_scheme == ecutsVERLET)
         {
