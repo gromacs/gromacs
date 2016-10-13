89b790b542be1c8ed9ce792151ca2dd680cf8c63
mohammad-ghahremanpour <ghahremanpour.gro@gmail.com>
2016-10-13 12:02:25 +0200

    A new modifed Buckingham potential is implemented in nb-generic.
    The potential is called Wang-Buckingham published in
    JCTC  Volume: 9  Page: 452  Year: 2012.
    
    Change-Id: I3cff0b54f81c267713190e9a5793d01168a7b7d4

--- src/gromacs/topology/ifunc.cpp
+++ src/gromacs/topology/ifunc.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -124,6 +124,7 @@ const t_interaction_function interaction_function[F_NRE] =
     def_bondedz ("LJC_NB",   "LJC Pairs NB",    2, 4, 0,  eNR_NB14,   unimplemented ),
     def_nb      ("LJ_SR",    "LJ (SR)",         2, 2                                ),
     def_nb      ("BHAM",     "Buck.ham (SR)",   2, 3                                ),
+    def_nb      ("WBHAM",    "Wang.Buck.ham (SR)",   2, 3                           ),
     def_nofc    ("LJ_LR",    "LJ (unused)"                                          ),
     def_nofc    ("BHAM_LR",  "B.ham (unused)"                                       ),
     def_nofc    ("DISPCORR", "Disper. corr."                                        ),
