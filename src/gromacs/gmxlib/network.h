380ba1f8bbdf213ae3d23ad17cca59131d6220d8
David van der Spoel <spoel@xray.bmc.uu.se>
2016-10-14 08:41:25 +0200

    Fixes some memory leaks associated with testing applied forces
    Fixes memory leaks associated with the electric
    field test. Implemented a done_commrec function.
    There may be need for a done_domdec as well but
    that falls outside the scope of this patch.
    
    Change-Id: Ib86629bb7959b064b6ac3a57d4bb96e2dd3bfe94

--- src/gromacs/gmxlib/network.h
+++ src/gromacs/gmxlib/network.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -52,6 +52,9 @@ struct t_filenm;
 
 struct t_commrec *init_commrec(void);
 /* Allocate, initialize and return the commrec. */
+
+void done_commrec(struct t_commrec **cr);
+/* Free memory associated with the commrec. */
 
 struct t_commrec *reinitialize_commrec_for_this_thread(const struct t_commrec *cro);
 /* Initialize communication records for thread-parallel simulations.
