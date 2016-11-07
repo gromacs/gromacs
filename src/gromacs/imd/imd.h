56cff4f866896fdbc6b35b9aabf501f7c9ca896a
Mark Abraham <mark.j.abraham@gmail.com>
2016-11-07 16:59:59 +0100

    Removed init_state
    Made a simple zero-initializing constructor for t_state and the
    structs of some of its members. Called them classes. Later, we might
    prefer to require explicit initialization with actual values, so that
    tools can detect the use of uninitialized values and find our bugs,
    but for now having a constructor is a useful initial step in that
    direction.
    
    Extracted some new functions that cover some of the incidental
    functionality that was also present in init_state.
    
    Made state.lambda a std::array, thereby removing the need to consider
    resizing it, and converted client code to be passed an ArrayRef rather
    than hard-code the name of the specific container. This caters for
    convenient future refactoring of the underlying storage, and sometimes
    needing to implicitly know what the size of the container is.
    
    Passing an ArrayRef by value is consistent with the CppCoreGuidelines,
    but has potential for performance impact. Doing this means that a
    caller pushes onto the stack a copy of the object (containing two
    pointers), rather than previous idioms such as pointer + size, or
    pointer + implicit constant size from an enum, or pointer + implicit
    size in some other parameter. This could mean an extra argument is
    pushed to the stack for the function call, compared with the
    alternatives of pushing a pointer to data, pointer to container, or
    pointer to ArrayRef. In all cases, the caller has to load the pointer
    value via an offset that is known to the compiler, so that aspect is
    probably irrelevant. So, we would probably prefer to avoid calling
    functions that take such parameters in a tight loop, or where multiple
    containers share a common size. But the uses in this patch seem to be
    of sufficiently high level to be an acceptable trade of possible
    performance for improved maintainability.
    
    Change-Id: I17e7d83cfc89566f76fa9949c425b950ad6aef62

--- src/gromacs/imd/imd.h
+++ src/gromacs/imd/imd.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -78,7 +78,7 @@ struct t_filenm;
 struct t_gmx_IMD;
 struct t_IMD;
 struct t_inputrec;
-struct t_state;
+class t_state;
 
 static const char IMDstr[] = "IMD:";  /**< Tag output from the IMD module with this string. */
 
