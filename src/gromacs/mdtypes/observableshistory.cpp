f98be12c79bf76ff9b9b3ca6889add1bd94b9dc8
Magnus Lundborg <lundborg.magnus@gmail.com>
2016-11-17 16:48:57 +0100

    Enable output of average pull force and positions.
    Write pull data to checkpoints to keep a proper average.
    Add option whether to write instantaneous or average pull
    coordinates and forces, respectively.
    
    Fixes #2626
    
    Change-Id: Id0268e00486028f497463dc13ae8d65a6c5df325

--- src/gromacs/mdtypes/observableshistory.cpp
+++ src/gromacs/mdtypes/observableshistory.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2017, by the GROMACS development team, led by
+ * Copyright (c) 2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -39,6 +39,7 @@
 
 #include "gromacs/mdtypes/edsamhistory.h"
 #include "gromacs/mdtypes/energyhistory.h"
+#include "gromacs/mdtypes/pullhistory.h"
 #include "gromacs/mdtypes/swaphistory.h"
 
 ObservablesHistory::ObservablesHistory()  = default;
