4a33c592de2653bde5a593e2381e48ced9678674
Roland Schulz <roland.schulz@intel.com>
2016-11-24 22:04:08 -0800

    Add create_unique_with_deleter
    A more flexible alternative to unique_cptr. Takes also
    non-void deleter functions or lambdas as argument.
    
    Change-Id: I25c1d7d9ed63556cc42230f3da578dc449116d78

--- src/gromacs/utility/unique_cptr.h
+++ src/gromacs/utility/unique_cptr.h
@@ -64,12 +64,17 @@ struct functor_wrapper {
     void operator()(T* t) { D(t); }
 };
 
-//! unique_ptr which takes function pointer as template argument
+//! unique_ptr which takes function pointer (has to return void) as template argument
 template<typename T, void D(T *) = sfree_wrapper>
 using unique_cptr                = std::unique_ptr<T, functor_wrapper<T, D> >;
 
 //! Simple guard which calls sfree. See unique_cptr for details.
 typedef unique_cptr<void> sfree_guard;
+
+
+//! Create unique_ptr with any deleter function or lambda
+template<typename T, typename D>
+std::unique_ptr<T, D> create_unique_with_deleter(T *t, D d) { return std::unique_ptr<T, D>(t, d); }
 
 }      // namespace gmx
 
