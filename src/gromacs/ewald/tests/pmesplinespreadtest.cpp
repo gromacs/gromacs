ffd3410eeaffe6558844dcb0f69efab13e64b388
Aleksei Iupinov <a.yupinov@gmail.com>
2016-12-02 14:46:49 +0100

    PME spline computation and charge spreading tests
    Unit tests for PME spline parameter calculation and charge spreading on CPU.
    Test 2 grid sizes, PME orders of 4 and 5, normal and triclinic boxes,
    configurations of 1, 2 and 13 atoms.
    
    Change-Id: Ie2ece38c407ad957991f428ec2600c33bed9b78a

---
+++ src/gromacs/ewald/tests/pmesplinespreadtest.cpp
@@ -0,0 +1,286 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Implements PME spline computation and charge spreading tests.
+ *
+ * \author Aleksei Iupinov <a.yupinov@gmail.com>
+ * \ingroup module_ewald
+ */
+
+#include "gmxpre.h"
+
+#include <string>
+
+#include <gmock/gmock.h>
+
+#include "gromacs/mdrunutility/mdmodules.h"
+#include "gromacs/mdtypes/inputrec.h"
+#include "gromacs/utility/stringutil.h"
+
+#include "testutils/refdata.h"
+#include "testutils/testasserts.h"
+
+#include "pmetestcommon.h"
+
+namespace gmx
+{
+namespace test
+{
+namespace
+{
+/*! \brief Convenience typedef of input parameters - unit cell box, PME interpolation order, grid dimensions,
+ * particle coordinates, particle charges, PME code path being tested
+ * TODO: consider inclusion of local grid offsets/sizes or PME nodes counts to test the PME DD
+ */
+typedef std::tuple<Matrix3x3, int, IVec, CoordinatesVector, ChargesVector, PmeCodePath> SplineAndSpreadInputParameters;
+
+/*! \brief Test fixture for testing both atom spline parameter computation and charge spreading.
+ * These 2 stages of PME are tightly coupled in the code.
+ */
+class PmeSplineAndSpreadTest : public ::testing::TestWithParam<SplineAndSpreadInputParameters>
+{
+    private:
+        //! Environment for getting the t_inputrec structure easily
+        MDModules mdModules_;
+
+    public:
+        //! Default constructor
+        PmeSplineAndSpreadTest() = default;
+        //! The test
+        void RunTest()
+        {
+            /* Getting the input */
+            Matrix3x3               box;
+            int                     pmeOrder;
+            IVec                    gridSize;
+            CoordinatesVector       coordinates;
+            ChargesVector           charges;
+            PmeCodePath             mode;
+
+            std::tie(box, pmeOrder, gridSize, coordinates, charges, mode) = GetParam();
+            const size_t atomCount = coordinates.size();
+
+            /* Describing the test uniquely in case it fails */
+            SCOPED_TRACE(formatString("Testing spline computation and spreading for PME grid size %d %d %d"
+                                      ", order %d, %zu atoms",
+                                      gridSize[XX], gridSize[YY], gridSize[ZZ],
+                                      pmeOrder,
+                                      atomCount));
+
+            /* Storing the input where it's needed */
+            t_inputrec *inputRec  = mdModules_.inputrec();
+            inputRec->nkx         = gridSize[XX];
+            inputRec->nky         = gridSize[YY];
+            inputRec->nkz         = gridSize[ZZ];
+            inputRec->pme_order   = pmeOrder;
+            inputRec->coulombtype = eelPME;
+
+            PmeSafePointer  pmeSafe = PmeInitWithAtoms(inputRec, coordinates, charges, box);
+
+            /* Running the test */
+            const bool computeSplines = true, spreadCharges = true;
+            PmePerformSplineAndSpread(pmeSafe, mode, computeSplines, spreadCharges);
+
+            /* Outputs correctness check */
+            /* All tolerances were picked empirically for single precision on CPU */
+
+            TestReferenceData    refData;
+            TestReferenceChecker checker(refData.rootChecker());
+
+            if (computeSplines)
+            {
+                SplineParamsVector    splineValues, splineDerivatives;
+                CoordinatesVector     fractCoordinates;
+                GridLineIndicesVector gridLineIndices;
+                PmeFetchOutputsSpline(pmeSafe, mode, fractCoordinates, splineValues, splineDerivatives, gridLineIndices);
+
+                /* Spline data - values and derivatives */
+                const auto            ulpToleranceSplines = 40;
+                checker.setDefaultTolerance(relativeToleranceAsUlp(1.0, ulpToleranceSplines));
+                checker.checkSequence(splineValues.begin(), splineValues.end(), "Spline values");
+                checker.checkSequence(splineDerivatives.begin(), splineDerivatives.end(), "Spline derivatives");
+
+                /* Fractional particle coordinates */
+                const auto ulpToleranceFractCoords = 24;
+                checker.setDefaultTolerance(relativeToleranceAsUlp(1.0, ulpToleranceFractCoords));
+                checker.checkSequence(fractCoordinates.begin(), fractCoordinates.end(), "Fractional coordinates");
+
+                /* Particle gridline indices */
+                checker.checkSequence(gridLineIndices.begin(), gridLineIndices.end(), "Gridline indices");
+            }
+
+            if (spreadCharges)
+            {
+                SparseGridValues nonZeroGridValues;
+                PmeFetchOutputsSpread(pmeSafe, mode, nonZeroGridValues);
+
+                /* The wrapped grid */
+                TestReferenceChecker gridValuesChecker(checker.checkCompound("NonZeroGridValues", "RealSpaceGrid"));
+                const auto           ulpTolerance = 48;
+                gridValuesChecker.setDefaultTolerance(relativeToleranceAsUlp(1.0, ulpTolerance));
+                for (const auto &point : nonZeroGridValues)
+                {
+                    std::string valueId = formatString("Cell %d %d %d", point.first[XX], point.first[YY], point.first[ZZ]);
+                    gridValuesChecker.checkReal(point.second, valueId.c_str());
+                }
+            }
+        }
+};
+
+
+/*! \brief Test for PME B-spline moduli computation */
+TEST_P(PmeSplineAndSpreadTest, ReproducesOutputs)
+{
+    EXPECT_NO_THROW(RunTest());
+}
+
+/* Valid input instances */
+//! A couple of valid inputs for boxes.
+static std::vector<Matrix3x3> const sampleBoxes
+{
+    // normal box
+    Matrix3x3 {{
+                   8.0f, 0.0f, 0.0f,
+                   0.0f, 3.4f, 0.0f,
+                   0.0f, 0.0f, 2.0f
+               }},
+    // triclinic box
+    Matrix3x3 {{
+                   7.0f, 0.0f, 0.0f,
+                   0.0f, 2.1f, 0.0f,
+                   6.0f, 5.0f, 3.2f
+               }},
+};
+
+//! A couple of valid inputs for grid sizes.
+static std::vector<IVec> const sampleGridSizes
+{
+    IVec {
+        16, 12, 14
+    },
+    IVec {
+        9, 7, 11
+    }
+};
+
+//! Random charges
+static ChargesVector const sampleChargesFull
+{
+    4.95f, 3.11f, 3.97f, 1.08f, 2.09f, 1.1f, 4.13f, 3.31f, 2.8f, 5.83f, 5.09f, 6.1f, 2.86f, 0.24f, 5.76f, 5.19f, 0.72f
+};
+//! 1 charge
+static ChargesVector const sampleCharges1(sampleChargesFull.begin(), sampleChargesFull.begin() + 1);
+//! 2 charges
+static ChargesVector const sampleCharges2(sampleChargesFull.begin() + 1, sampleChargesFull.begin() + 3);
+//! 13 charges
+static ChargesVector const sampleCharges13(sampleChargesFull.begin() + 3, sampleChargesFull.begin() + 16);
+
+//! Random coordinate vectors
+static CoordinatesVector const sampleCoordinatesFull
+{
+    {
+        1.59f, 1.37f, 0.95f
+    }, {
+        16.0f, 1.02f, 0.22f  // 2 box lengths in x
+    }, {
+        0.03f, 1.6f, 0.22f
+    }, {
+        0.33f, 0.92f, 1.56f
+    }, {
+        1.16f, 0.75f, 0.39f
+    }, {
+        0.49f, 1.6f, 1.12f
+    }, {
+        16.0001f, 1.52f, 1.19f  // >2 box lengths in x
+    }, {
+        1.43f, 1.1f, 0.08f
+    }, {
+        -1.08f, 1.19f, 0.08f   // negative x
+    }, {
+        1.6f, 0.93f, 0.53f
+    }, {
+        1.32f, 1.48f, 0.16f
+    }, {
+        0.87f, 0.0f, 0.33f
+    }, {
+        0.95f, 0.13f, 0.48f
+    }, {
+        1.23f, 0.91f, 0.68f
+    }, {
+        0.19f, 1.45f, 0.94f
+    }, {
+        1.28f, 0.46f, 0.38f
+    }, {
+        1.21f, 0.23f, 1.0f
+    }
+};
+//! 1 coordinate vector
+static CoordinatesVector const sampleCoordinates1(sampleCoordinatesFull.begin(), sampleCoordinatesFull.begin() + 1);
+//! 2 coordinate vectors
+static CoordinatesVector const sampleCoordinates2(sampleCoordinatesFull.begin() + 1, sampleCoordinatesFull.begin() + 3);
+//! 13 coordinate vectors
+static CoordinatesVector const sampleCoordinates13(sampleCoordinatesFull.begin() + 3, sampleCoordinatesFull.begin() + 16);
+
+/*! \brief Instantiation of the PME spline computation test with valid input and 1 atom */
+INSTANTIATE_TEST_CASE_P(SaneInput1, PmeSplineAndSpreadTest, ::testing::Combine(
+                                    ::testing::ValuesIn(sampleBoxes),
+                                    ::testing::Range(4, 5 + 1),
+                                    ::testing::ValuesIn(sampleGridSizes),
+                                    ::testing::Values(sampleCoordinates1),
+                                    ::testing::Values(sampleCharges1),
+                                    ::testing::Values(PmeCodePath::CPU)
+                                ));
+/*! \brief Instantiation of the PME spline computation test with valid input and 2 atoms */
+INSTANTIATE_TEST_CASE_P(SaneInput2, PmeSplineAndSpreadTest, ::testing::Combine(
+                                    ::testing::ValuesIn(sampleBoxes),
+                                    ::testing::Range(4, 5 + 1),
+                                    ::testing::ValuesIn(sampleGridSizes),
+                                    ::testing::Values(sampleCoordinates2),
+                                    ::testing::Values(sampleCharges2),
+                                    ::testing::Values(PmeCodePath::CPU)
+                                ));
+/*! \brief Instantiation of the PME spline computation test with valid input and 13 atoms */
+INSTANTIATE_TEST_CASE_P(SaneInput13, PmeSplineAndSpreadTest, ::testing::Combine(
+                                    ::testing::ValuesIn(sampleBoxes),
+                                    ::testing::Range(4, 5 + 1),
+                                    ::testing::ValuesIn(sampleGridSizes),
+                                    ::testing::Values(sampleCoordinates13),
+                                    ::testing::Values(sampleCharges13),
+                                    ::testing::Values(PmeCodePath::CPU)
+                                ));
+}
+}
+}
--- src/gromacs/ewald/tests/pmetestcommon.cpp
+++ src/gromacs/ewald/tests/pmetestcommon.cpp
@@ -44,20 +44,158 @@
 #include "pmetestcommon.h"
 
 #include "gromacs/ewald/pme.h"
+#include "gromacs/ewald/pme-grid.h"
+#include "gromacs/ewald/pme-internal.h"
+#include "gromacs/ewald/pme-spread.h"
+#include "gromacs/math/invertmatrix.h"
+#include "gromacs/utility/exceptions.h"
+#include "gromacs/utility/gmxassert.h"
 
+namespace gmx
+{
 //! PME initialization - internal
-pmeSafePointer PmeInitInternal(const t_inputrec *inputRec, size_t atomCount)
+PmeSafePointer PmeInitInternal(const t_inputrec *inputRec, size_t atomCount)
 {
     gmx_pme_t *pmeDataRaw = NULL;
     gmx_pme_init(&pmeDataRaw, NULL, 1, 1, inputRec,
                  atomCount, FALSE, FALSE, TRUE, 0.0, 0.0, 1);
-    pmeSafePointer pme(pmeDataRaw, gmx_pme_destroy); // taking ownership
+    PmeSafePointer pme(pmeDataRaw, gmx_pme_destroy); // taking ownership
     return pme;
 }
 
 //! Simple PME initialization based on input, no atom data
-pmeSafePointer PmeInitEmpty(const t_inputrec *inputRec)
+PmeSafePointer PmeInitEmpty(const t_inputrec *inputRec)
 {
     return PmeInitInternal(inputRec, 0);
     // hiding the fact that PME actually needs to know the number of atoms in advance
+}
+
+//! PME initialization with atom data and system box
+PmeSafePointer PmeInitWithAtoms(const t_inputrec        *inputRec,
+                                const CoordinatesVector &coordinates,
+                                const ChargesVector     &charges,
+                                const Matrix3x3          box
+                                )
+{
+    const size_t    atomCount = coordinates.size();
+    GMX_RELEASE_ASSERT(atomCount == charges.size(), "Mismatch in atom data");
+    PmeSafePointer  pmeSafe = PmeInitInternal(inputRec, atomCount);
+    pme_atomcomm_t *atc     = &(pmeSafe->atc[0]);
+    atc->x           = const_cast<rvec *>(as_rvec_array(coordinates.data()));
+    atc->coefficient = const_cast<real *>(charges.data());
+    /* With decomposition there would be more boilerplate atc code here, e.g. do_redist_pos_coeffs */
+
+    // TODO get rid of this with proper matrix type
+    matrix boxTemp;
+    for (int i = 0; i < DIM; i++)
+    {
+        for (int j = 0; j < DIM; j++)
+        {
+            boxTemp[i][j] = box[i * DIM + j];
+        }
+    }
+    gmx::invertBoxMatrix(boxTemp, pmeSafe->recipbox);
+
+    return pmeSafe;
+}
+
+//! PME spline calculation and charge spreading
+void PmePerformSplineAndSpread(const PmeSafePointer &pmeSafe, PmeCodePath mode,
+                               bool computeSplines, bool spreadCharges)
+{
+    gmx_pme_t      *pmeUnsafe                    = pmeSafe.get();
+    pme_atomcomm_t *atc                          = &(pmeSafe->atc[0]);
+    const size_t    gridIndex                    = 0;
+    const bool      computeSplinesForZeroCharges = true;
+    real           *fftgrid                      = spreadCharges ? pmeSafe->fftgrid[gridIndex] : nullptr;
+    switch (mode)
+    {
+        case PmeCodePath::CPU:
+            spread_on_grid(pmeUnsafe, atc, &pmeSafe->pmegrid[gridIndex], computeSplines, spreadCharges,
+                           fftgrid, computeSplinesForZeroCharges, gridIndex);
+            if (spreadCharges && !pmeSafe->bUseThreads)
+            {
+                wrap_periodic_pmegrid(pmeUnsafe, pmeSafe->pmegrid[gridIndex].grid.grid);
+                copy_pmegrid_to_fftgrid(pmeUnsafe, pmeSafe->pmegrid[gridIndex].grid.grid, fftgrid, gridIndex);
+            }
+            break;
+
+        default:
+            GMX_THROW(gmx::InternalError("Test not implemented for this mode"));
+            // TODO: there could be 2 GPU modes - separate spline and spread, and fused.
+            // In the fused case everything faster in a single kernel.
+            // Everything still ends up on the host in the end - have to make sure of that!
+    }
+}
+
+//! Fetching the spline computation outputs of PmePerformSplineAndSpread()
+void PmeFetchOutputsSpline(const PmeSafePointer &pmeSafe, PmeCodePath mode,
+                           CoordinatesVector &fractCoordinates,
+                           SplineParamsVector &splineValues,
+                           SplineParamsVector &splineDerivatives,
+                           GridLineIndicesVector &gridLineIndices)
+
+{
+    const pme_atomcomm_t *atc         = &(pmeSafe->atc[0]);
+    const size_t          atomCount   = atc->n;
+    const size_t          pmeOrder    = pmeSafe->pme_order;
+    const size_t          threadIndex = 0; // relying on running single threaded on CPU
+    switch (mode)
+    {
+        case PmeCodePath::CPU:
+            fractCoordinates.assign(atc->fractx, atc->fractx + atomCount);
+            gridLineIndices.assign(atc->idx, atc->idx + atomCount);
+            // spline values - XX...XXYY...YYZZ...ZZ
+            splineValues.clear();
+            splineDerivatives.clear();
+            for (int i = 0; i < DIM; i++)
+            {
+                splineValues.insert(splineValues.end(), atc->spline[threadIndex].theta[i],
+                                    atc->spline[threadIndex].theta[i] + atomCount * pmeOrder);
+                splineDerivatives.insert(splineDerivatives.end(), atc->spline[threadIndex].dtheta[i],
+                                         atc->spline[threadIndex].dtheta[i] + atomCount * pmeOrder);
+            }
+            break;
+
+        default:
+            GMX_THROW(gmx::InternalError("Test not implemented for this mode"));
+    }
+}
+
+//! Fetching the spreading output of PmePerformSplineAndSpread()
+void PmeFetchOutputsSpread(const PmeSafePointer &pmeSafe, PmeCodePath mode,
+                           SparseGridValues &gridValues)
+{
+    const size_t          gridIndex = 0;
+    IVec                  gridSize, gridOffsetUnused, paddedGridSize;
+
+    switch (mode)
+    {
+        case PmeCodePath::CPU:
+            gridValues.clear();
+
+            gmx_parallel_3dfft_real_limits(pmeSafe->pfft_setup[gridIndex], gridSize, gridOffsetUnused, paddedGridSize);
+            for (int ix = 0; ix < gridSize[XX]; ix++)
+            {
+                for (int iy = 0; iy < gridSize[YY]; iy++)
+                {
+                    for (int iz = 0; iz < gridSize[ZZ]; iz++)
+                    {
+                        const size_t gridValueIndex = (ix * paddedGridSize[YY] + iy) * paddedGridSize[ZZ] + iz;
+                        const real   value          = pmeSafe->fftgrid[gridIndex][gridValueIndex];
+                        if (value != 0.0)
+                        {
+                            IVec key = {ix, iy, iz};
+                            gridValues[key] = value;
+                        }
+                    }
+                }
+            }
+            break;
+
+        default:
+            GMX_THROW(gmx::InternalError("Test not implemented for this mode"));
+    }
+}
+
 }
