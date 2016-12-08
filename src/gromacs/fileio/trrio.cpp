gromacs/gromacs

commit f7d4d019eecd733a56cbf4eeb9c936c6bd775eb1

Fix trr magic number reading
The trr header-reading routine returned an "OK" value if the magic
number was wrong, which might lead to chaotic results everywhere,
because checking of return values tends not to happen (even when
they're right). Other GROMACS magic-number reading routines tend to
give a fatal error if the the number is wrong, e.g. by reading a file
written in wrong endianness. (This should never be a thing for XDR
files, which are defined to be big endian, but such code has existed.)

This change adds/restores that behaviour to trr reading, along
with separating the behaviour of failing to read a magic integer
from reading one that doesn't match.

Fixes #1926

Change-Id: I3cdd8ae9172e3b95fc232d8fa31a442d239233db

src/gromacs/fileio/trrio.cpp
@@ -48,7 +48,6 @@
 #include "gromacs/utility/smalloc.h"
 
 #define BUFSIZE     128
-#define GROMACS_MAGIC   1993
 
 static int nFloatSize(gmx_trr_header_t *sh)
 {
@@ -86,15 +85,24 @@ static int nFloatSize(gmx_trr_header_t *sh)
 static gmx_bool
 do_trr_frame_header(t_fileio *fio, bool bRead, gmx_trr_header_t *sh, gmx_bool *bOK)
 {
-    int             magic  = GROMACS_MAGIC;
-    static gmx_bool bFirst = TRUE;
+    const int       magicValue = 1993;
+    int             magic      = magicValue;
+    static gmx_bool bFirst     = TRUE;
     char            buf[256];
 
     *bOK = TRUE;
 
-    if (!gmx_fio_do_int(fio, magic) || magic != GROMACS_MAGIC)
+    if (!gmx_fio_do_int(fio, magic))
     {
-        return FALSE;
+        // Failed to read an integer, which should be the magic number
+        *bOK = FALSE;
+        return *bOK;
+    }
+    if (magic != magicValue)
+    {
+        *bOK = FALSE;
+        gmx_fatal(FARGS, "Failed to find GROMACS magic number in trr frame header, so this is not a trr file!\n");
+        return *bOK;
     }
 
     if (bRead)

commit 40ad943792299ab9cf5dfd33ef3b4c4aade8d56f

Fix logic of TRR reading
Commit f7d4d019 introduced a bug where TRR reading reaching the end of
the file was indistinguishable from a reading error or a magic-number
error. This is now fixed, restoring the end-of-file behaviour that
existed before f7d4d019, while retaining the wrong-magic-number
behaviour that it introduced.

Refs #1926

Change-Id: Ic8540846c481f022bc6ae7b774794792c8c7a523

src/gromacs/fileio/trrio.cpp
@@ -82,6 +82,11 @@ static int nFloatSize(gmx_trr_header_t *sh)
     return nflsize;
 }
 
+/* Returns whether a valid frame header was read. Upon exit, *bOK is
+   TRUE if a normal outcome resulted. Usually that is the same thing,
+   but encountering the end of the file before reading the magic
+   integer is a normal outcome for TRR reading, and it does not
+   produce a valid frame header, so the values differ in that case. */
 static gmx_bool
 do_trr_frame_header(t_fileio *fio, bool bRead, gmx_trr_header_t *sh, gmx_bool *bOK)
 {
@@ -94,9 +99,9 @@ do_trr_frame_header(t_fileio *fio, bool bRead, gmx_trr_header_t *sh, gmx_bool *b
 
     if (!gmx_fio_do_int(fio, magic))
     {
-        // Failed to read an integer, which should be the magic number
-        *bOK = FALSE;
-        return *bOK;
+        /* Failed to read an integer, which should be the magic
+           number, which means we've reached the end of the file. */
+        return FALSE;
     }
     if (magic != magicValue)
     {