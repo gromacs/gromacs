7c822eeef99c689e74cc9aecd59047846710f5a7
Mark Abraham <mark.j.abraham@gmail.com>
2016-12-21 16:19:22 +1100

    Modernize read_inpfile
    Used std::string and gmx::TextReader to simplify this. It is likely no
    longer as efficient because it now makes some std::string objects, but
    this is not a significant part of the execution time of e.g. grompp.
    
    Move COMMENTSIGN to cstringutil.cpp, now that this is an
    implementation detail of string-handling code, rather than also used
    in readinp.cpp.
    
    Moved responsibility for stripping of comments to TextReader, reworked
    its whitespace-trimming behaviour, and introduced some tests for that
    class.
    
    Introduced some TODO items to reconsider multiple behaviours where
    read_inpfile ignores what could be malformed input. It is used for
    grompp, xpm2ps and membed, however, so it is not immediately clear
    what fixes might be appropriate, and we might anyway remove this
    code shortly.
    
    Introduced catches for exceptions that might be thrown while calling
    read_inpfile (and related code that might change soon).
    
    Added tests for newly introduced splitting functionality that
    supports read_inpfile.
    
    Refs #2074
    
    Change-Id: Id9c46d60a3ec7ecdcdb9529bba2fdb68ce241914

--- src/gromacs/commandline/cmdlinehelpmodule.cpp
+++ src/gromacs/commandline/cmdlinehelpmodule.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -553,7 +553,8 @@ HelpExportReStructuredText::HelpExportReStructuredText(
 {
     TextReader   linksFile("links.dat");
     std::string  line;
-    while (linksFile.readLineTrimmed(&line))
+    linksFile.trimTrailingWhiteSpace(true);
+    while (linksFile.readLine(&line))
     {
         links_.addLink("[REF]." + line + "[ref]",
                        formatString(":ref:`.%s <%s>`", line.c_str(), line.c_str()),
