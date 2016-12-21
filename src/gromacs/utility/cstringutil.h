13e7c07218ad86ae85d256075ee7150d514ae1c4
Mark Abraham <mark.j.abraham@gmail.com>
2016-12-21 16:19:22 +1100

    Modernize read_inpfile
    Used std::string and gmx::TextReader to simplify this. It is likely no
    longer as efficient because it now makes several std::string objects
    per line, but this is not a significant part of the execution time of
    e.g. grompp.
    
    Move COMMENTSIGN to cstringutil.cpp, now that this is an
    implementation detail of string-handling code, rather than also used
    in readinp.cpp.
    
    Moved responsibility for stripping of comments to TextReader, reworked
    its whitespace-trimming behaviour, and introduced some tests for that
    class.
    
    Introduced some TODO items to reconsider multiple behaviours where
    read_inpfile ignores what could be malformed input. It is used for
    grompp, xpm2ps and membed, however, so it is not immediately clear
    what fixes might be appropriate, and we might anyway remove this
    code shortly.
    
    Introduced catches for exceptions that might be thrown while calling
    read_inpfile (and related code that might change soon).
    
    Added tests for newly introduced split-and-trim functionality that
    supports read_inpfile.
    
    Refs #2074
    
    Change-Id: Id9c46d60a3ec7ecdcdb9529bba2fdb68ce241914

--- src/gromacs/utility/cstringutil.h
+++ src/gromacs/utility/cstringutil.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -56,8 +56,6 @@ extern "C" {
 
 /** Continuation character. */
 #define CONTINUE    '\\'
-/** Comment sign to use. */
-#define COMMENTSIGN ';'
 /** Standard size for char* string buffers. */
 #define STRLEN 4096
 
@@ -77,7 +75,7 @@ int continuing(char *s);
  */
 char *fgets2(char *s, int n, FILE *stream);
 
-/** Remove portion of a line after a ::COMMENTSIGN.  */
+/** Remove portion of a line after a ';' comment sign.  */
 void strip_comment(char *line);
 
 /** Make a string uppercase. */
