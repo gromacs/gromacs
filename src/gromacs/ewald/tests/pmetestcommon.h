ae9c34ab3c8b281d68fd928af7b7c12f76cc35bf
Aleksei Iupinov <a.yupinov@gmail.com>
2016-12-30 00:21:59 +0100

    PME force gathering tests
    Unit tests for PME force gathering on CPU.
    Test 2 grid sizes, PME orders from 3 to 5, normal and triclinic boxes,
    configurations of 1, 2 and 13 atoms, with and without input forces reduction.
    Forces gathered from the grid are tested as outputs.
    
    Change-Id: I1b624e0019f2c8401084db5af4dab7c0efc932fb

--- src/gromacs/ewald/tests/pmetestcommon.h
+++ src/gromacs/ewald/tests/pmetestcommon.h
@@ -61,9 +61,11 @@ namespace test
 //! A safe pointer type for PME.
 typedef gmx::unique_cptr<gmx_pme_t, gmx_pme_destroy> PmeSafePointer;
 //! Charges
-typedef std::vector<real> ChargesVector;
+typedef ConstArrayRef<real> ChargesVector;
 //! Coordinates
 typedef std::vector<RVec> CoordinatesVector;
+//! Forces
+typedef ArrayRef<RVec> ForcesVector;
 //! Gridline indices
 typedef ConstArrayRef<IVec> GridLineIndicesVector;
 //! Type of spline data
@@ -75,15 +77,32 @@ enum class PmeSplineDataType
 /*! \brief Spline parameters (theta or dtheta).
  * A reference to a single dimension's spline data; this means (atomCount * pmeOrder) values or derivatives.
  */
-typedef ConstArrayRef<real> SplineParamsVector;
-//! Non-zero grid values; keys are string representations of the cells' 3d indices (IVec)
-typedef std::map<std::string, real> SparseGridValues;
+typedef ConstArrayRef<real> SplineParamsDimVector;
+/*! \brief Spline parameters (theta or dtheta) in all 3 dimensions
+ */
+typedef std::array<SplineParamsDimVector, DIM> SplineParamsVector;
+
+//! Non-zero grid values for test input; keys are 3d indices (IVec)
+template<typename ValueType>using SparseGridValuesInput = std::map<IVec, ValueType>;
+//! Non-zero real grid values
+typedef SparseGridValuesInput<real> SparseRealGridValuesInput;
+//! Non-zero grid values for test output; keys are string representations of the cells' 3d indices (IVec); this allows for better sorting.
+template<typename ValueType>using SparseGridValuesOutput = std::map<std::string, ValueType>;
+//! Non-zero real grid values
+typedef SparseGridValuesOutput<real> SparseRealGridValuesOutput;
+
 //! TODO: make proper C++ matrix for the whole Gromacs, get rid of this
 typedef std::array<real, DIM * DIM> Matrix3x3;
 //! PME code path being tested
 enum class CodePath
 {
     CPU,    // serial CPU code
+};
+//! PME gathering input forces treatment
+enum class PmeGatherInputHandling
+{
+    Overwrite,
+    ReduceWith,
 };
 
 // PME stages
@@ -99,18 +118,31 @@ PmeSafePointer pmeInitWithAtoms(const t_inputrec        *inputRec,
 //! PME spline computation and charge spreading
 void pmePerformSplineAndSpread(gmx_pme_t *pme, CodePath mode,
                                bool computeSplines, bool spreadCharges);
+//! PME force gathering
+void pmePerformGather(gmx_pme_t *pme, CodePath mode,
+                      PmeGatherInputHandling inputTreatment, ForcesVector &forces);
+
+// PME state setters
+
+//! Setting atom spline values or derivatives to be used in spread/gather
+void pmeSetSplineData(const gmx_pme_t *pme, CodePath mode,
+                      const SplineParamsDimVector &splineValues, PmeSplineDataType type, int dimIndex);
+//! Setting gridline indices be used in spread/gather
+void pmeSetGridLineIndices(const gmx_pme_t *pme, CodePath mode,
+                           const GridLineIndicesVector &gridLineIndices);
+//! Setting real grid to be used in gather
+void pmeSetRealGrid(const gmx_pme_t *pme, CodePath mode,
+                    const SparseRealGridValuesInput &gridValues);
 
 // PME state getters
 
 //! Getting the single dimension's spline values or derivatives
-SplineParamsVector pmeGetSplineData(const gmx_pme_t *pme, CodePath mode,
-                                    PmeSplineDataType type, int dimIndex);
-
+SplineParamsDimVector pmeGetSplineData(const gmx_pme_t *pme, CodePath mode,
+                                       PmeSplineDataType type, int dimIndex);
 //! Getting the gridline indices
 GridLineIndicesVector pmeGetGridlineIndices(const gmx_pme_t *pme, CodePath mode);
-
 //! Getting the real grid (spreading output of PmePerformSplineAndSpread())
-SparseGridValues pmeGetRealGrid(const gmx_pme_t *pme, CodePath mode);
+SparseRealGridValuesOutput pmeGetRealGrid(const gmx_pme_t *pme, CodePath mode);
 
 }
 }
