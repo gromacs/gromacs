c0e7b5ce3e733582990b70332e1d377f07b681bb
Mark Abraham <mark.j.abraham@gmail.com>
2017-01-01 10:13:13 +0000

    Modernize write_inpfile
    Used std::string, gmx::TextOutputFile and gmx::TextWriter to simplify
    and permit testing.
    
    To do so, modified the interface of various helper functionality so
    that it can write a string that the caller can then use with something
    other than a FILE stream if they want to. This would also be more
    convenient for testing them, if we ever do that.
    
    Moved nice_header to stringutil, renamed it niceHeader, and
    permitted its caller to specify the comment character.
    
    Refs #2074
    
    Change-Id: I6ab250c982a8e9e9ee3dacb6575ccc04f17e689c

--- src/gromacs/utility/cuda_version_information.h
+++ src/gromacs/utility/cuda_version_information.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015, by the GROMACS development team, led by
+ * Copyright (c) 2015,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -35,8 +35,8 @@
 #ifndef GMX_GMXLIB_CUDA_TOOLS_CUDA_VERSION_INFORMATION_H
 #define GMX_GMXLIB_CUDA_TOOLS_CUDA_VERSION_INFORMATION_H
 
-#include <cstdio>
+#include <string>
 
-void gmx_print_version_info_cuda_gpu(FILE *fp);
+std::string gmx_print_version_info_cuda_gpu();
 
 #endif
