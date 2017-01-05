31f0c056e10296ccfa3913f1f019f14863e7f830
Aleksei Iupinov <a.yupinov@gmail.com>
2017-01-05 15:32:29 +0100

    Fix compilation with CMAKE_CXX_FLAGS="-Wall -Werror"
    Refs #2073
    
    Change-Id: I9223b13e8abd251d56378052ace4fb4e9169a2aa

--- cmake/gmxTestCXX11.cmake
+++ cmake/gmxTestCXX11.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+# Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -102,6 +102,7 @@ extern template void someFunction<int>();
 int main() {
   // Test nullptr
   double *x = nullptr;
+  (void)x; // Suppressing unused variable warning
   // Test range-based for loops
   int array[5] = { 1, 2, 3, 4, 5 };
   for (int& x : array)
