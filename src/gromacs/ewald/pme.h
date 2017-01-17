de0008dc1f7a3791510d4a3f56d2aac36fe07d6e
Berk Hess <hess@kth.se>
2017-01-17 14:08:09 +0100

    Fix PME order and grid size checks
    The PME grid size restrictions were not checked in grompp.
    Now both grompp and mdrun check the grid size. Both checks are done
    with the same minimum grid size independent of DD and OpenMP settings.
    Renamed calc_grid to calcFftGrid and let it take the minimum grid size
    into account.
    Also corrected an assert that checked for pme_order>=4 iso 3.
    
    Change-Id: I57b300f4a413ea2942fa671be67839be7ae16c39

--- src/gromacs/ewald/pme.h
+++ src/gromacs/ewald/pme.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -65,6 +65,9 @@ struct t_inputrec;
 enum {
     GMX_SUM_GRID_FORWARD, GMX_SUM_GRID_BACKWARD
 };
+
+/*! \brief Return the smallest allowed PME grid size for \p pmeOrder */
+int minimalPmeGridSize(int pmeOrder);
 
 /*! \brief Initialize \p pmedata
  *
