gromacs/gromacs

commit 74474d6febb66c0834f1fd567c191e291950f7ae

Use std::vector in t_state
Changed x, v and cg_p pointers in t_state to PaddedRVecVector.
PaddedRVecVector is currently std::vector<RVec> and is always
sized with one element extra for 4-wide SIMD (gather) loads.
But this will be replaced by a proper class.
t_state is now freed by the default destructor with some issues:
* ekinstate_t is fully converted to std::vector in a child change
* state struct of special algorithms still need conversion

Changes to t_state propagate over large parts of the core code.
This change extracts rvec pointers at intermediate levels to
make this single change manageable.
The state in EM also needed to be updated, which led to a lot
of changes in minimize.cpp, especially to do_lbfgs() which was not
yet converted to use em_state_t.

Change-Id: Ib6ead1f13741ead8dbeeddd118ecdd4bf1cc6584

src/gromacs/mdlib/broadcaststructs.cpp
@@ -56,8 +56,35 @@
 #define   block_bc(cr,   d) gmx_bcast(     sizeof(d),     &(d), (cr))
 #define  nblock_bc(cr, nr, d) { gmx_bcast((nr)*sizeof((d)[0]), (d), (cr)); }
 #define    snew_bc(cr, d, nr) { if (!MASTER(cr)) {snew((d), (nr)); }}
-/* Dirty macro with bAlloc not as an argument */
-#define nblock_abc(cr, nr, d) { if (bAlloc) {snew((d), (nr)); } nblock_bc(cr, (nr), (d)); }
+
+#if !GMX_DOUBLE
+static void nblock_abc(const t_commrec *cr, int numElements, real **v)
+{
+    if (!MASTER(cr))
+    {
+        snew(*v, numElements);
+    }
+    nblock_bc(cr, numElements, *v);
+}
+#endif
+
+static void nblock_abc(const t_commrec *cr, int numElements, double **v)
+{
+    if (!MASTER(cr))
+    {
+        snew(*v, numElements);
+    }
+    nblock_bc(cr, numElements, *v);
+}
+
+static void nblock_abc(const t_commrec *cr, int numElements, std::vector<double> *v)
+{
+    if (!MASTER(cr))
+    {
+        v->resize(numElements);
+    }
+    gmx_bcast(numElements*sizeof(double), v->data(), cr);
+}
 
 static void bc_cstring(const t_commrec *cr, char **s)
 {
@@ -259,10 +286,15 @@ static void bc_groups(const t_commrec *cr, t_symtab *symtab,
     }
 }
 
+static void bcastPaddedRVecVector(const t_commrec *cr, PaddedRVecVector *v, unsigned int n)
+{
+    (*v).resize(n + 1);
+    nblock_bc(cr, n, as_rvec_array(v->data()));
+}
+
 void bcast_state(const t_commrec *cr, t_state *state)
 {
     int      i, nnht, nnhtp;
-    gmx_bool bAlloc;
 
     if (!PAR(cr) || (cr->nnodes - cr->npmenodes <= 1))
     {
@@ -276,10 +308,7 @@ void bcast_state(const t_commrec *cr, t_state *state)
     block_bc(cr, state->nnhpres);
     block_bc(cr, state->nhchainlength);
     block_bc(cr, state->flags);
-    if (state->lambda == NULL)
-    {
-        snew_bc(cr, state->lambda, efptNR)
-    }
+    state->lambda.resize(efptNR);
 
     if (cr->dd)
     {
@@ -292,21 +321,13 @@ void bcast_state(const t_commrec *cr, t_state *state)
     nnht  = (state->ngtc)*(state->nhchainlength);
     nnhtp = (state->nnhpres)*(state->nhchainlength);
 
-    /* We still need to allocate the arrays in state for non-master
-     * ranks, which is done (implicitly via bAlloc) in the dirty,
-     * dirty nblock_abc macro. */
-    bAlloc = !MASTER(cr);
-    if (bAlloc)
-    {
-        state->nalloc = state->natoms;
-    }
     for (i = 0; i < estNR; i++)
     {
         if (state->flags & (1<<i))
         {
             switch (i)
             {
-                case estLAMBDA:  nblock_bc(cr, efptNR, state->lambda); break;
+                case estLAMBDA:  nblock_bc(cr, efptNR, state->lambda.data()); break;
                 case estFEPSTATE: block_bc(cr, state->fep_state); break;
                 case estBOX:     block_bc(cr, state->box); break;
                 case estBOX_REL: block_bc(cr, state->box_rel); break;
@@ -314,25 +335,25 @@ void bcast_state(const t_commrec *cr, t_state *state)
                 case estPRES_PREV: block_bc(cr, state->pres_prev); break;
                 case estSVIR_PREV: block_bc(cr, state->svir_prev); break;
                 case estFVIR_PREV: block_bc(cr, state->fvir_prev); break;
-                case estNH_XI:   nblock_abc(cr, nnht, state->nosehoover_xi); break;
-                case estNH_VXI:  nblock_abc(cr, nnht, state->nosehoover_vxi); break;
-                case estNHPRES_XI:   nblock_abc(cr, nnhtp, state->nhpres_xi); break;
-                case estNHPRES_VXI:  nblock_abc(cr, nnhtp, state->nhpres_vxi); break;
-                case estTC_INT:  nblock_abc(cr, state->ngtc, state->therm_integral); break;
+                case estNH_XI:   nblock_abc(cr, nnht, &state->nosehoover_xi); break;
+                case estNH_VXI:  nblock_abc(cr, nnht, &state->nosehoover_vxi); break;
+                case estNHPRES_XI:   nblock_abc(cr, nnhtp, &state->nhpres_xi); break;
+                case estNHPRES_VXI:  nblock_abc(cr, nnhtp, &state->nhpres_vxi); break;
+                case estTC_INT:  nblock_abc(cr, state->ngtc, &state->therm_integral); break;
                 case estVETA:    block_bc(cr, state->veta); break;
                 case estVOL0:    block_bc(cr, state->vol0); break;
-                case estX:       nblock_abc(cr, state->natoms, state->x); break;
-                case estV:       nblock_abc(cr, state->natoms, state->v); break;
-                case estCGP:     nblock_abc(cr, state->natoms, state->cg_p); break;
+                case estX:       bcastPaddedRVecVector(cr, &state->x, state->natoms);
+                case estV:       bcastPaddedRVecVector(cr, &state->v, state->natoms);
+                case estCGP:     bcastPaddedRVecVector(cr, &state->cg_p, state->natoms);
                 case estDISRE_INITF: block_bc(cr, state->hist.disre_initf); break;
                 case estDISRE_RM3TAV:
                     block_bc(cr, state->hist.ndisrepairs);
-                    nblock_abc(cr, state->hist.ndisrepairs, state->hist.disre_rm3tav);
+                    nblock_abc(cr, state->hist.ndisrepairs, &state->hist.disre_rm3tav);
                     break;
                 case estORIRE_INITF: block_bc(cr, state->hist.orire_initf); break;
                 case estORIRE_DTAV:
                     block_bc(cr, state->hist.norire_Dtav);
-                    nblock_abc(cr, state->hist.norire_Dtav, state->hist.orire_Dtav);
+                    nblock_abc(cr, state->hist.norire_Dtav, &state->hist.orire_Dtav);
                     break;
                 default:
                     gmx_fatal(FARGS,

src/gromacs/domdec/domdec.h
@@ -81,10 +81,6 @@ struct gmx_domdec_zones_t;
 struct t_commrec;
 struct t_inputrec;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /*! \brief Returns the global topology atom number belonging to local atom index i.
  *
  * This function is intended for writing ASCII output
@@ -211,7 +207,11 @@ void dd_setup_dlb_resource_sharing(struct t_commrec           *cr,
 
 /*! \brief Collects local rvec arrays \p lv to \p v on the master rank */
 void dd_collect_vec(struct gmx_domdec_t *dd,
-                    t_state *state_local, rvec *lv, rvec *v);
+                    t_state *state_local, const PaddedRVecVector *lv, rvec *v);
+
+/*! \brief Collects local rvec arrays \p lv to \p v on the master rank */
+void dd_collect_vec(struct gmx_domdec_t *dd,
+                    t_state *state_local, const PaddedRVecVector *lv, PaddedRVecVector *v);
 
 /*! \brief Collects the local state \p state_local to \p state on the master rank */
 void dd_collect_state(struct gmx_domdec_t *dd,
@@ -269,7 +269,7 @@ void dd_partition_system(FILE                *fplog,
                          const gmx_mtop_t    *top_global,
                          const t_inputrec    *ir,
                          t_state             *state_local,
-                         rvec               **f,
+                         PaddedRVecVector    *f,
                          t_mdatoms           *mdatoms,
                          gmx_localtop_t      *top_local,
                          t_forcerec          *fr,
@@ -401,9 +401,5 @@ void set_ddbox_cr(t_commrec *cr, const ivec *dd_nc,
                   const t_inputrec *ir, const matrix box,
                   const t_block *cgs, const rvec *x,
                   gmx_ddbox_t *ddbox);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif

src/gromacs/domdec/domdec_network.cpp
@@ -249,12 +249,13 @@ void dd_bcastc(gmx_domdec_t *dd, int nbytes, void *src, void *dest)
 #endif
 }
 
-void dd_scatter(gmx_domdec_t gmx_unused *dd, int gmx_unused nbytes, void gmx_unused *src, void *dest)
+void dd_scatter(gmx_domdec_t gmx_unused *dd, int gmx_unused nbytes, const void gmx_unused *src, void *dest)
 {
 #if GMX_MPI
     if (dd->nnodes > 1)
     {
-        MPI_Scatter(src, nbytes, MPI_BYTE,
+        /* Some MPI implementions don't specify const */
+        MPI_Scatter(const_cast<void *>(src), nbytes, MPI_BYTE,
                     dest, nbytes, MPI_BYTE,
                     DDMASTERRANK(dd), dd->mpi_comm_all);
     }
@@ -269,17 +270,18 @@ void dd_scatter(gmx_domdec_t gmx_unused *dd, int gmx_unused nbytes, void gmx_unu
     }
 }
 
-void dd_gather(gmx_domdec_t gmx_unused *dd, int gmx_unused nbytes, void gmx_unused *src, void gmx_unused *dest)
+void dd_gather(gmx_domdec_t gmx_unused *dd, int gmx_unused nbytes, const void gmx_unused *src, void gmx_unused *dest)
 {
 #if GMX_MPI
-    MPI_Gather(src, nbytes, MPI_BYTE,
+    /* Some MPI implementions don't specify const */
+    MPI_Gather(const_cast<void *>(src), nbytes, MPI_BYTE,
                dest, nbytes, MPI_BYTE,
                DDMASTERRANK(dd), dd->mpi_comm_all);
 #endif
 }
 
 void dd_scatterv(gmx_domdec_t gmx_unused *dd,
-                 int gmx_unused *scounts, int gmx_unused *disps, void *sbuf,
+                 int gmx_unused *scounts, int gmx_unused *disps, const void *sbuf,
                  int rcount, void *rbuf)
 {
 #if GMX_MPI
@@ -292,7 +294,8 @@ void dd_scatterv(gmx_domdec_t gmx_unused *dd,
             /* MPI does not allow NULL pointers */
             rbuf = &dum;
         }
-        MPI_Scatterv(sbuf, scounts, disps, MPI_BYTE,
+        /* Some MPI implementions don't specify const */
+        MPI_Scatterv(const_cast<void *>(sbuf), scounts, disps, MPI_BYTE,
                      rbuf, rcount, MPI_BYTE,
                      DDMASTERRANK(dd), dd->mpi_comm_all);
     }
@@ -308,7 +311,7 @@ void dd_scatterv(gmx_domdec_t gmx_unused *dd,
 }
 
 void dd_gatherv(gmx_domdec_t gmx_unused *dd,
-                int gmx_unused scount, void gmx_unused *sbuf,
+                int gmx_unused scount, const void gmx_unused *sbuf,
                 int gmx_unused *rcounts, int gmx_unused *disps, void gmx_unused *rbuf)
 {
 #if GMX_MPI
@@ -319,7 +322,8 @@ void dd_gatherv(gmx_domdec_t gmx_unused *dd,
         /* MPI does not allow NULL pointers */
         sbuf = &dum;
     }
-    MPI_Gatherv(sbuf, scount, MPI_BYTE,
+    /* Some MPI implementions don't specify const */
+    MPI_Gatherv(const_cast<void *>(sbuf), scount, MPI_BYTE,
                 rbuf, rcounts, disps, MPI_BYTE,
                 DDMASTERRANK(dd), dd->mpi_comm_all);
 #endif

src/gromacs/domdec/domdec_network.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2008,2009,2010,2012,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2008,2009,2010,2012,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -123,11 +123,11 @@ dd_bcastc(struct gmx_domdec_t *dd, int nbytes, void *src, void *dest);
 
 /*! \brief Scatters \p nbytes from \p src on \p DDMASTERRANK to all PP ranks, received in \p dest */
 void
-dd_scatter(struct gmx_domdec_t *dd, int nbytes, void *src, void *dest);
+dd_scatter(struct gmx_domdec_t *dd, int nbytes, const void *src, void *dest);
 
 /*! \brief Gathers \p nbytes from \p src on all PP ranks, received in \p dest on \p DDMASTERRANK */
 void
-dd_gather(struct gmx_domdec_t *dd, int nbytes, void *src, void *dest);
+dd_gather(struct gmx_domdec_t *dd, int nbytes, const void *src, void *dest);
 
 /*! \brief Scatters \p scounts bytes from \p src on \p DDMASTERRANK to all PP ranks, receiving \p rcount bytes in \p dest.
  *
@@ -135,7 +135,7 @@ dd_gather(struct gmx_domdec_t *dd, int nbytes, void *src, void *dest);
  * If rcount==0, rbuf is allowed to be NULL */
 void
 dd_scatterv(struct gmx_domdec_t *dd,
-            int *scounts, int *disps, void *sbuf,
+            int *scounts, int *disps, const void *sbuf,
             int rcount, void *rbuf);
 
 /*! \brief Gathers \p rcount bytes from \p src on all PP ranks, received in \p scounts bytes in \p dest on \p DDMASTERRANK.
@@ -145,7 +145,7 @@ dd_scatterv(struct gmx_domdec_t *dd,
  * If scount==0, sbuf is allowed to be NULL */
 void
 dd_gatherv(struct gmx_domdec_t *dd,
-           int scount, void *sbuf,
+           int scount, const void *sbuf,
            int *rcounts, int *disps, void *rbuf);
 
 #endif

src/gromacs/domdec/domdec_topology.cpp
@@ -419,7 +419,7 @@ void dd_print_missing_interactions(FILE *fplog, t_commrec *cr,
 
     print_missing_interactions_atoms(fplog, cr, top_global, &top_local->idef);
     write_dd_pdb("dd_dump_err", 0, "dump", top_global, cr,
-                 -1, state_local->x, state_local->box);
+                 -1, as_rvec_array(state_local->x.data()), state_local->box);
 
     std::string errorMessage;
 

src/gromacs/fileio/checkpoint.cpp
@@ -60,6 +60,7 @@
 #include "gromacs/gmxlib/network.h"
 #include "gromacs/math/vec.h"
 #include "gromacs/math/vecdump.h"
+#include "gromacs/math/vectypes.h"
 #include "gromacs/mdtypes/commrec.h"
 #include "gromacs/mdtypes/df_history.h"
 #include "gromacs/mdtypes/energyhistory.h"
@@ -468,6 +469,28 @@ static int do_cpte_reals(XDR *xd, int cptp, int ecpt, int sflags,
     return do_cpte_reals_low(xd, cptp, ecpt, sflags, n, NULL, v, list, ecprREAL);
 }
 
+/* This function stores n along with the reals for reading,
+ * but on reading it assumes that n matches the value in the checkpoint file,
+ * a fatal error is generated when this is not the case.
+ */
+static int do_cpte_reals(XDR *xd, int cptp, int ecpt, int sflags,
+                         int n, std::vector<real> *v, FILE *list)
+{
+    real *v_real;
+    if (list == NULL && (sflags & (1 << ecpt)))
+    {
+        /* Resizes on read, on write the size should already be n */
+        v->resize(n);
+        v_real = v->data();
+    }
+    else
+    {
+        v_real = NULL;
+    }
+
+    return do_cpte_reals_low(xd, cptp, ecpt, sflags, n, NULL, &v_real, list, ecprREAL);
+}
+
 /* This function does the same as do_cpte_reals,
  * except that on reading it ignores the passed value of *n
  * and stored the value read from the checkpoint file in *n.
@@ -612,6 +635,24 @@ static int do_cpte_doubles(XDR *xd, int cptp, int ecpt, int sflags,
     return 0;
 }
 
+static int do_cpte_doubles(XDR *xd, int cptp, int ecpt, int sflags,
+                           int n, std::vector<double> *v, FILE *list)
+{
+    double *v_double;
+    if (list == NULL && (sflags & (1 << ecpt)))
+    {
+        /* Resizes on read, on write the size should already be n */
+        v->resize(n);
+        v_double = v->data();
+    }
+    else
+    {
+        v_double = NULL;
+    }
+
+    return do_cpte_doubles(xd, cptp, ecpt, sflags, n, &v_double, list);
+}
+
 static int do_cpte_double(XDR *xd, int cptp, int ecpt, int sflags,
                           double *r, FILE *list)
 {
@@ -620,10 +661,25 @@ static int do_cpte_double(XDR *xd, int cptp, int ecpt, int sflags,
 
 
 static int do_cpte_rvecs(XDR *xd, int cptp, int ecpt, int sflags,
-                         int n, rvec **v, FILE *list)
+                         int n, std::vector<gmx::RVec> *v, FILE *list)
 {
+    rvec *v_rvec;
+
+    if (list == NULL && (sflags & (1 << ecpt)))
+    {
+        /* We resize the vector here to avoid pointer reallocation in
+         * do_cpte_reals_low. Note the we allocate 1 element extra for SIMD.
+         */
+        v->resize(n + 1);
+        v_rvec = as_rvec_array(v->data());
+    }
+    else
+    {
+        v_rvec = NULL;
+    }
+
     return do_cpte_reals_low(xd, cptp, ecpt, sflags,
-                             n*DIM, NULL, (real **)v, list, ecprRVEC);
+                             n*DIM, NULL, (real **)(&v_rvec), list, ecprRVEC);
 }
 
 static int do_cpte_matrix(XDR *xd, int cptp, int ecpt, int sflags,
@@ -2478,14 +2534,12 @@ void read_checkpoint_trxframe(t_fileio *fp, t_trxframe *fr)
     fr->bX         = (state.flags & (1<<estX));
     if (fr->bX)
     {
-        fr->x     = state.x;
-        state.x   = NULL;
+        fr->x   = getRvecArrayFromPaddedRVecVector(&state.x, state.natoms);
     }
     fr->bV      = (state.flags & (1<<estV));
     if (fr->bV)
     {
-        fr->v     = state.v;
-        state.v   = NULL;
+        fr->v   = getRvecArrayFromPaddedRVecVector(&state.v, state.natoms);
     }
     fr->bF      = FALSE;
     fr->bBox    = (state.flags & (1<<estBOX));
@@ -2493,7 +2547,6 @@ void read_checkpoint_trxframe(t_fileio *fp, t_trxframe *fr)
     {
         copy_mat(state.box, fr->box);
     }
-    done_state(&state);
 }
 
 void list_checkpoint(const char *fn, FILE *out)
@@ -2571,8 +2624,6 @@ void list_checkpoint(const char *fn, FILE *out)
     {
         gmx_file("Cannot read/write checkpoint; corrupt file, or maybe you are out of disk space?");
     }
-
-    done_state(&state);
 }
 
 /* This routine cannot print tons of data, since it is called before the log file is opened. */
@@ -2594,5 +2645,4 @@ read_checkpoint_simulation_part_and_filenames(t_fileio             *fp,
     {
         gmx_file("Cannot read/write checkpoint; corrupt file, or maybe you are out of disk space?");
     }
-    done_state(&state);
 }

src/gromacs/fileio/tpxio.cpp
@@ -3083,28 +3083,33 @@ static void do_tpxheader(t_fileio *fio, gmx_bool bRead, t_tpxheader *tpx,
 }
 
 static int do_tpx(t_fileio *fio, gmx_bool bRead,
-                  t_inputrec *ir, t_state *state, gmx_mtop_t *mtop,
-                  gmx_bool bXVallocated)
+                  t_inputrec *ir, t_state *state, rvec *x, rvec *v,
+                  gmx_mtop_t *mtop)
 {
     t_tpxheader     tpx;
     gmx_mtop_t      dum_top;
     gmx_bool        TopOnlyOK;
-    rvec           *xptr, *vptr;
     int             ePBC;
     gmx_bool        bPeriodicMols;
 
     if (!bRead)
     {
+        GMX_RELEASE_ASSERT(x == NULL && v == NULL, "Passing separate x and v pointers to do_tpx() is not supported when writing");
+
         tpx.natoms    = state->natoms;
         tpx.ngtc      = state->ngtc;
         tpx.fep_state = state->fep_state;
         tpx.lambda    = state->lambda[efptFEP];
         tpx.bIr       = (ir       != NULL);
         tpx.bTop      = (mtop     != NULL);
-        tpx.bX        = (state->x != NULL);
-        tpx.bV        = (state->v != NULL);
+        tpx.bX        = (state->flags & (1 << estX));
+        tpx.bV        = (state->flags & (1 << estV));
         tpx.bF        = FALSE;
         tpx.bBox      = TRUE;
+    }
+    else
+    {
+        GMX_RELEASE_ASSERT(!(x == NULL && v != NULL), "Passing x==NULL and v!=NULL is not supported");
     }
 
     TopOnlyOK = (ir == NULL);
@@ -3116,20 +3121,20 @@ static int do_tpx(t_fileio *fio, gmx_bool bRead,
     if (bRead)
     {
         state->flags  = 0;
-        if (bXVallocated)
+        if (x != NULL)
         {
-            xptr = state->x;
-            vptr = state->v;
             init_state(state, 0, tpx.ngtc, 0, 0, 0);
-            state->natoms = tpx.natoms;
-            state->nalloc = tpx.natoms;
-            state->x      = xptr;
-            state->v      = vptr;
         }
         else
         {
             init_state(state, tpx.natoms, tpx.ngtc, 0, 0, 0);
         }
+    }
+
+    if (x == NULL)
+    {
+        x = as_rvec_array(state->x.data());
+        v = as_rvec_array(state->v.data());
     }
 
 #define do_test(fio, b, p) if (bRead && (p != NULL) && !b) gmx_fatal(FARGS, "No %s in %s",#p, gmx_fio_getname(fio))
@@ -3181,24 +3186,24 @@ static int do_tpx(t_fileio *fio, gmx_bool bRead,
             done_mtop(&dum_top, TRUE);
         }
     }
-    do_test(fio, tpx.bX, state->x);
+    do_test(fio, tpx.bX, x);
     if (tpx.bX)
     {
         if (bRead)
         {
             state->flags |= (1<<estX);
         }
-        gmx_fio_ndo_rvec(fio, state->x, state->natoms);
+        gmx_fio_ndo_rvec(fio, x, tpx.natoms);
     }
 
-    do_test(fio, tpx.bV, state->v);
+    do_test(fio, tpx.bV, v);
     if (tpx.bV)
     {
         if (bRead)
         {
             state->flags |= (1<<estV);
         }
-        gmx_fio_ndo_rvec(fio, state->v, state->natoms);
+        gmx_fio_ndo_rvec(fio, v, tpx.natoms);
     }
 
     // No need to run do_test when the last argument is NULL
@@ -3206,7 +3211,7 @@ static int do_tpx(t_fileio *fio, gmx_bool bRead,
     {
         rvec *dummyForces;
         snew(dummyForces, state->natoms);
-        gmx_fio_ndo_rvec(fio, dummyForces, state->natoms);
+        gmx_fio_ndo_rvec(fio, dummyForces, tpx.natoms);
         sfree(dummyForces);
     }
 
@@ -3323,8 +3328,8 @@ void write_tpx_state(const char *fn,
     fio = open_tpx(fn, "w");
     do_tpx(fio, FALSE,
            const_cast<t_inputrec *>(ir),
-           const_cast<t_state *>(state),
-           const_cast<gmx_mtop_t *>(mtop), FALSE);
+           const_cast<t_state *>(state), NULL, NULL,
+           const_cast<gmx_mtop_t *>(mtop));
     close_tpx(fio);
 }
 
@@ -3334,7 +3339,7 @@ void read_tpx_state(const char *fn,
     t_fileio *fio;
 
     fio = open_tpx(fn, "r");
-    do_tpx(fio, TRUE, ir, state, mtop, FALSE);
+    do_tpx(fio, TRUE, ir, state, NULL, NULL, mtop);
     close_tpx(fio);
 }
 
@@ -3343,22 +3348,17 @@ int read_tpx(const char *fn,
              rvec *x, rvec *v, gmx_mtop_t *mtop)
 {
     t_fileio *fio;
-    t_state   state;
+    t_state   state {};
     int       ePBC;
 
-    state.x = x;
-    state.v = v;
     fio     = open_tpx(fn, "r");
-    ePBC    = do_tpx(fio, TRUE, ir, &state, mtop, TRUE);
+    ePBC    = do_tpx(fio, TRUE, ir, &state, x, v, mtop);
     close_tpx(fio);
-    *natoms = state.natoms;
+    *natoms = mtop->natoms;
     if (box)
     {
         copy_mat(state.box, box);
     }
-    state.x = NULL;
-    state.v = NULL;
-    done_state(&state);
 
     return ePBC;
 }

commit 4fe0885694a5228f2966ce466656c3f8813029cf

Fix missing break in switch
Introduced by 74474d6f

Also make compile with -Wimplicit-fallthrough which is default
with gcc 7.

Change-Id: Ib9f315d2c7d78a3db471350652e29682993398b6

src/external/tinyxml2/tinyxml2.cpp
@@ -30,6 +30,10 @@ distribution.
 #   include <cstddef>
 #endif
 
+#if defined(__GNUC__) && !defined(__INTEL_COMPILER)
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#endif
+
 static const char LINE_FEED				= (char)0x0a;			// all line endings are normalized to LF
 static const char LF = LINE_FEED;
 static const char CARRIAGE_RETURN		= (char)0x0d;			// CR gets filtered out

src/external/tng_io/external/zlib/inflate.c
@@ -91,6 +91,10 @@
 #  endif
 #endif
 
+#if defined(__GNUC__) && !defined(__INTEL_COMPILER)
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#endif
+
 /* function prototypes */
 local void fixedtables OF((struct inflate_state FAR *state));
 local int updatewindow OF((z_streamp strm, const unsigned char FAR *end,

src/gromacs/fileio/tngio.cpp
@@ -966,6 +966,7 @@ void gmx_prepare_tng_writing(const char              *filename,
                         set_writing_interval(*output, interval, 1, fallbackIds[i],
                                              fallbackNames[i], TNG_NON_PARTICLE_BLOCK_DATA,
                                              TNG_GZIP_COMPRESSION);
+                        break;
                     default:
                         continue;
                 }

src/gromacs/fileio/trxio.cpp
@@ -383,7 +383,7 @@ int write_trxframe_indexed(t_trxstatus *status, const t_trxframe *fr, int nind,
                     copy_rvec(fr->f[ind[i]], fout[i]);
                 }
             }
-        /* no break */
+            // fallthrough
         case efXTC:
             if (fr->bX)
             {
@@ -452,7 +452,7 @@ int write_trxframe_indexed(t_trxstatus *status, const t_trxframe *fr, int nind,
             {
                 sfree(fout);
             }
-        /* no break */
+            // fallthrough
         case efXTC:
             sfree(xout);
             break;

src/gromacs/mdlib/broadcaststructs.cpp
@@ -311,9 +311,9 @@ void bcast_state(const t_commrec *cr, t_state *state)
                 case estTC_INT:  nblock_abc(cr, state->ngtc, &state->therm_integral); break;
                 case estVETA:    block_bc(cr, state->veta); break;
                 case estVOL0:    block_bc(cr, state->vol0); break;
-                case estX:       bcastPaddedRVecVector(cr, &state->x, state->natoms);
-                case estV:       bcastPaddedRVecVector(cr, &state->v, state->natoms);
-                case estCGP:     bcastPaddedRVecVector(cr, &state->cg_p, state->natoms);
+                case estX:       bcastPaddedRVecVector(cr, &state->x, state->natoms); break;
+                case estV:       bcastPaddedRVecVector(cr, &state->v, state->natoms); break;
+                case estCGP:     bcastPaddedRVecVector(cr, &state->cg_p, state->natoms); break;
                 case estDISRE_INITF: block_bc(cr, state->hist.disre_initf); break;
                 case estDISRE_RM3TAV:
                     block_bc(cr, state->hist.ndisrepairs);