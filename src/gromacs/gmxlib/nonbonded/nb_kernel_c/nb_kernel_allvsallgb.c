19383d129c55748ee15fc01d08d9f38457d7eeb5
Mark Abraham <mark.j.abraham@gmail.com>
2017-01-26 16:08:46 +0100

    Remove typedef struct in some mdtypes files
    The idiom
    
    typedef struct { /*...*/ } t_forcerec;
    
    is a C anachronism that permits the use of t_forcerec without struct,
    but inhibits the use of
    
    struct forcerec;
    
    forward declarations in all headers that include mdtypes/forcerec.h,
    etc.
    
    Removed this so that in future, e.g. the interface of IForceProvider
    can use t_forcerec in name, rather than resort to hackery on void
    pointers.
    
    Regenerated the group-scheme kernels, which all now need to refer to
    struct t_forcerec because they compile as C.
    
    Change-Id: I5eb07aa018c54cc74df6dfc918b559e84ea996a8

--- src/gromacs/gmxlib/nonbonded/nb_kernel_c/nb_kernel_allvsallgb.c
+++ src/gromacs/gmxlib/nonbonded/nb_kernel_c/nb_kernel_allvsallgb.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2009, The GROMACS Development Team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -241,7 +241,7 @@ void
 nb_kernel_allvsallgb(t_nblist gmx_unused *     nlist,
                      rvec *                    xx,
                      rvec *                    ff,
-                     t_forcerec *              fr,
+                     struct t_forcerec *       fr,
                      t_mdatoms *               mdatoms,
                      nb_kernel_data_t *        kernel_data,
                      t_nrnb *                  nrnb)
