4b1c88dbd377f4039a5ca242092af6cfbf5a1c54
Roland Schulz <roland.schulz@intel.com>
2017-02-08 17:52:08 -0800

    Remove duplicate AVX512 gather definitions
    Better code reuse by having all gather functions call the same
    function. Also more consistent optimization of align
    multiplication.
    
    Change-Id: I446bee828655be9010919456215041a35eb36efb

--- src/gromacs/simd/impl_x86_avx_512/impl_x86_avx_512_simd_float.h
+++ src/gromacs/simd/impl_x86_avx_512/impl_x86_avx_512_simd_float.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -42,6 +42,8 @@
 #include <cstdint>
 
 #include <immintrin.h>
+
+#include "gromacs/utility/real.h"
 
 #include "impl_x86_avx_512_general.h"
 
