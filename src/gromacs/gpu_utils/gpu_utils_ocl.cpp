8ded05469b9837cc3f5345095d3aecd14d7c51e8
Aleksei Iupinov <a.yupinov@gmail.com>
2017-02-17 15:17:18 +0100

    Clearing up GPU task assignment
    Added 2 classes: GpuTaskAssignmentManager (which is used during the setup
    to assign GPUs to ranks and then to tasks within ranks), and GpuTaskManager
    for storing these assignments (which lives inside gpu_opt).
    Accordingly, gmx_gpu_opt_t/gmx_hw_opt_t are not POD anymore.
    Some gpu_utils functions are changed to work with GPU ids instead of
    dev_use lookup indices.
    
    Change-Id: Ib8448746dc336c913c79f3c0c32d7ed188eb3c23

--- src/gromacs/gpu_utils/gpu_utils_ocl.cpp
+++ src/gromacs/gpu_utils/gpu_utils_ocl.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -447,32 +447,18 @@ void get_gpu_device_info_string(char gmx_unused *s, const gmx_gpu_info_t gmx_unu
 }
 
 //! This function is documented in the header file
-gmx_bool init_gpu(const gmx::MDLogger              & /*mdlog*/,
-                  int                              mygpu,
-                  char                            *result_str,
-                  const gmx_gpu_info_t gmx_unused *gpu_info,
-                  const gmx_gpu_opt_t             *gpu_opt
-                  )
+bool init_gpu(const gmx::MDLogger              & /*mdlog*/,
+              int                              gpuId,
+              std::string                      &,
+              const gmx_gpu_info_t gmx_unused *gpu_info
+              )
 {
-    assert(result_str);
-
-    result_str[0] = 0;
-
-    if (mygpu < 0 || mygpu >= gpu_opt->n_dev_use)
-    {
-        char        sbuf[STRLEN];
-        sprintf(sbuf, "Trying to initialize an non-existent GPU: "
-                "there are %d %s-selected GPU(s), but #%d was requested.",
-                gpu_opt->n_dev_use, gpu_opt->bUserSet ? "user" : "auto", mygpu);
-        gmx_incons(sbuf);
-    }
-
     // If the device is NVIDIA, for safety reasons we disable the JIT
     // caching as this is known to be broken at least until driver 364.19;
     // the cache does not always get regenerated when the source code changes,
     // e.g. if the path to the kernel sources remains the same
 
-    if (gpu_info->gpu_dev[mygpu].vendor_e == OCL_VENDOR_NVIDIA)
+    if (gpu_info->gpu_dev[gpuId].vendor_e == OCL_VENDOR_NVIDIA)
     {
         // Ignore return values, failing to set the variable does not mean
         // that something will go wrong later.
@@ -484,7 +470,7 @@ gmx_bool init_gpu(const gmx::MDLogger              & /*mdlog*/,
 #endif
     }
 
-    return TRUE;
+    return true;
 }
 
 //! This function is documented in the header file
--- src/gromacs/mdlib/nbnxn_ocl/nbnxn_ocl_data_mgmt.cpp
+++ src/gromacs/mdlib/nbnxn_ocl/nbnxn_ocl_data_mgmt.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -53,6 +53,7 @@
 #include "gromacs/gpu_utils/oclutils.h"
 #include "gromacs/hardware/detecthardware.h"
 #include "gromacs/hardware/gpu_hw_info.h"
+#include "gromacs/hardware/hardwareassign.h"
 #include "gromacs/math/vectypes.h"
 #include "gromacs/mdlib/force_flags.h"
 #include "gromacs/mdlib/nb_verlet.h"
@@ -692,7 +693,6 @@ void nbnxn_gpu_init(gmx_nbnxn_ocl_t          **p_nb,
                     const gmx_gpu_opt_t       *gpu_opt,
                     const interaction_const_t *ic,
                     nonbonded_verlet_group_t  *nbv_grp,
-                    int                        my_gpu_index,
                     int                        rank,
                     gmx_bool                   bLocalAndNonlocal)
 {
@@ -724,7 +724,7 @@ void nbnxn_gpu_init(gmx_nbnxn_ocl_t          **p_nb,
     snew(nb->timings, 1);
 
     /* set device info, just point it to the right GPU among the detected ones */
-    nb->dev_info = gpu_info->gpu_dev + gpu_opt->dev_use[my_gpu_index];
+    nb->dev_info = gpu_opt->gpuTasks->gpuInfo(GpuTask::NB);
     snew(nb->dev_rundata, 1);
 
     /* init to NULL the debug buffer */
