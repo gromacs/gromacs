3425c1b046da0181bebf051f7e56b97c59afd576
Aleksei Iupinov <a.yupinov@gmail.com>
2017-02-21 16:31:54 +0100

    Remove unused function cu_synchstream_atdat()
    Change-Id: I363010ca73a9d0e3e1ebb13cf20a42fa834fb573

--- src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda_data_mgmt.cu
+++ src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda_data_mgmt.cu
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1026,15 +1026,6 @@ void nbnxn_gpu_free(gmx_nbnxn_cuda_t *nb)
     {
         fprintf(debug, "Cleaned up CUDA data structures.\n");
     }
-}
-
-void cu_synchstream_atdat(gmx_nbnxn_cuda_t *nb, int iloc)
-{
-    cudaError_t  stat;
-    cudaStream_t stream = nb->stream[iloc];
-
-    stat = cudaStreamWaitEvent(stream, nb->timers->stop_atdat, 0);
-    CU_RET_ERR(stat, "cudaStreamWaitEvent failed");
 }
 
 gmx_wallclock_gpu_t * nbnxn_gpu_get_timings(gmx_nbnxn_cuda_t *nb)
