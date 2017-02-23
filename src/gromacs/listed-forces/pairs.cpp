94bbae100f413e00a10ebe7002b91579eff25732
Roland Schulz <roland.schulz@intel.com>
2017-02-22 23:13:24 -0800

    Fix compile of GMX_SIMD=None with ICC
    ICC defines invsqrt in math.h
    
    Change-Id: Ic7107189cb5fb422192f986ebed53f2c2993ff35

--- src/gromacs/listed-forces/pairs.cpp
+++ src/gromacs/listed-forces/pairs.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -607,7 +607,7 @@ do_pairs_simple(int nbonds,
         pbc_dx_aiuc(pbc, xi, xj, dr);
 
         T rsq   = dr[XX]*dr[XX] + dr[YY]*dr[YY] + dr[ZZ]*dr[ZZ];
-        T rinv  = invsqrt(rsq);
+        T rinv  = gmx::invsqrt(rsq);
         T rinv2 = rinv*rinv;
         T rinv6 = rinv2*rinv2*rinv2;
 
