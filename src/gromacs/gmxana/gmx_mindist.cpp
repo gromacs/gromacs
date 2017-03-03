e5242964a563c81e91e04ceea94c141a1772c1d1
David van der Spoel <spoel@xray.bmc.uu.se>
2017-03-03 07:46:05 +0100

    Removes rogue printing to xvg file in gmx mindist
    -xvg none is now adhered to, and printing is preceded by
    a comment.
    
    Fixes #2129
    
    Change-Id: Idf7135c7e74ea17747e0f5cacc9298a619e6c90d

--- src/gromacs/gmxana/gmx_mindist.cpp
+++ src/gromacs/gmxana/gmx_mindist.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -413,15 +413,16 @@ void dist_plot(const char *fn, const char *afile, const char *dfile,
         sprintf(buf, "%simum Distance", bMin ? "Min" : "Max");
         respertime = xvgropen(rfile, buf, output_env_get_time_label(oenv), "Distance (nm)", oenv);
         xvgr_legend(respertime, ng-1, (const char**)leg, oenv);
-        if (bPrintResName)
+        if (bPrintResName && output_env_get_print_xvgr_codes(oenv) )
         {
             fprintf(respertime, "# ");
+
+            for (j = 0; j < nres; j++)
+            {
+                fprintf(respertime, "%s%d ", *(atoms->resinfo[atoms->atom[index[0][residue[j]]].resind].name), atoms->atom[index[0][residue[j]]].resind);
+            }
+            fprintf(respertime, "\n");
         }
-        for (j = 0; j < nres; j++)
-        {
-            fprintf(respertime, "%s%d ", *(atoms->resinfo[atoms->atom[index[0][residue[j]]].resind].name), atoms->atom[index[0][residue[j]]].resind);
-        }
-        fprintf(respertime, "\n");
 
     }
 
