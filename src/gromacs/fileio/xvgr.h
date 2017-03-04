91d6411a62e61e72e58d4fb96551558d7e521d99
David van der Spoel <spoel@xray.bmc.uu.se>
2017-03-04 09:31:46 +0100

    [RFC] Automatic closing of xvgr files.
    This patch defines a class to use instead of raw FILE pointers for
    opening and closing files. It could be an intermediate solution for
    analysis tools and others writing to files, before moving to the new
    AnalysisData framework. It is based on TextOutputFile in order to not
    re-invent the wheel.
    
    Change-Id: I96285b2ca92d3e0441be050b82c7516fb828e619

--- src/gromacs/fileio/xvgr.h
+++ src/gromacs/fileio/xvgr.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2010,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2010,2014,2015,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -40,6 +40,7 @@
 #include <stdio.h>
 
 #include "gromacs/utility/basedefinitions.h"
+#include "gromacs/utility/filestream.h"
 #include "gromacs/utility/real.h"
 
 #ifdef __cplusplus
@@ -158,6 +159,33 @@ void xvgr_box(FILE *out,
               int BoxFill, int BoxColor, int BoxPattern,
               const struct gmx_output_env_t *oenv);
 /* Make a box */
+
+/*! \brief Wrapper around xvgr routines
+ *
+ * This wrapper automates the closing of xvg files in a robust manner
+ * when the variable goes out of scope.
+ *
+ * \todo Extend to a full class by implementing other functions
+ * \todo Implement in analysis tools
+ * \todo Implement error handling in case the file cannot be written
+ */
+class XvgFile : public gmx::TextOutputFile
+{
+    public:
+        /*! \brief Constructor
+         *
+         * \param[in] fn    The outpuf file name
+         * \param[in] title The title of the graph
+         * \param[in] xaxis The X-axis legend
+         * \param[in] yaxis The Y-axis legend
+         * \param[in] oenv  The Gromacs output environment
+         */
+        XvgFile(const char *fn, const char *title, const char *xaxis,
+                const char *yaxis, const struct gmx_output_env_t *oenv) :
+            gmx::TextOutputFile(xvgropen(fn, title, xaxis, yaxis, oenv))
+        {
+        }
+};
 
 int read_xvg_legend(const char *fn, double ***y, int *ny,
                     char **subtitle, char ***legend);
