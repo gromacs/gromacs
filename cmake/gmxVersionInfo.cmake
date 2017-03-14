963da1aba6fb7cca67a0e274474f3483167d08a5
Mark Abraham <mark.j.abraham@gmail.com>
2017-03-14 14:08:55 +0100

    Bumped version to prepare for next patch release
    Change-Id: Ic368ff6186fb35d86fc2e7dfb6c6513eb48235d5

--- cmake/gmxVersionInfo.cmake
+++ cmake/gmxVersionInfo.cmake
@@ -188,7 +188,7 @@
 # The GROMACS convention is that these are the version number of the next
 # release that is going to be made from this branch.
 set(GMX_VERSION_MAJOR 2016)
-set(GMX_VERSION_PATCH 3)
+set(GMX_VERSION_PATCH 4)
 # The suffix, on the other hand, is used mainly for betas and release
 # candidates, where it signifies the most recent such release from
 # this branch; it will be empty before the first such release, as well
