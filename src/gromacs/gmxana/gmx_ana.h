9457333685d589e985420a64cc3725a138a03e8b
David van der Spoel <spoel@xray.bmc.uu.se>
2017-03-15 21:54:41 +0100

    [RFC] Port of gmx clustsize to new framework
    Introduced eftXpm as filename option.
    
    Had to update tests refdata because of formatting changes in test
    output with new setup similar to other trajectoryanalysis tests.
    
    Change-Id: Id7099a5d5a4859cb7bfdf1ca8693ebc6dfa65224

--- src/gromacs/gmxana/gmx_ana.h
+++ src/gromacs/gmxana/gmx_ana.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2008, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -242,9 +242,6 @@ gmx_tune_pme(int argc, char *argv[]);
 
 int
 gmx_velacc(int argc, char *argv[]);
-
-int
-gmx_clustsize(int argc, char *argv[]);
 
 int
 gmx_mdmat(int argc, char *argv[]);
