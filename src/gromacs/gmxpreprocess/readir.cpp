499dbb1a22d7724c52a9a95f5cbb417b95a98efd
Mark Abraham <mark.j.abraham@gmail.com>
2017-03-16 15:29:48 +0100

    Fix string handling for gcc 7
    Pre-release gcc 7 warns about potential buffer overflows, so we may as
    well fix them.
    
    Change-Id: Ia154faf13b84473f073e5d553c7c275c19cbccec

--- src/gromacs/gmxpreprocess/readir.cpp
+++ src/gromacs/gmxpreprocess/readir.cpp
@@ -45,6 +45,7 @@
 #include <cmath>
 
 #include <algorithm>
+#include <string>
 
 #include "gromacs/fileio/readinp.h"
 #include "gromacs/fileio/warninp.h"
@@ -2487,8 +2488,7 @@ void get_ir(const char *mdparin, const char *mdparout,
 
     if (strlen(is->deform) > 0 && ndeform != 6)
     {
-        sprintf(warn_buf, "Cannot parse exactly 6 box deformation velocities from string '%s'", is->deform);
-        warning_error(wi, warn_buf);
+        warning_error(wi, gmx::formatString("Cannot parse exactly 6 box deformation velocities from string '%s'", is->deform).c_str());
     }
     for (i = 0; i < 3; i++)
     {
