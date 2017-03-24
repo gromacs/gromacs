01e022912df2515c1fc3e3cf72c896b2bb79ff06
Berk Hess <hess@kth.se>
2017-03-24 14:39:16 +0100

    Enable dynamic pair list pruning
    This change activates the dynamic pruning scheme and the pruning
    only kernels added in previous commits.
    A heuristic estimate is used to select value for nstlist and
    nstlistPrune that should result in performance that is reasonably
    close to optimal. The nstlist increase code has been moved from
    runner.cpp to nbnxn_tuning.cpp. The KNL check in that code has been
    replaced by a check for Xeon Phi.
    A paragraph has been added to the manual to describe the dynamic
    and rolling list pruning scheme. A reference with all the details
    will be added once the paper has been published.
    
    Change-Id: Ic625858a07083916c8aa3e07f7497488dcfaee9e

--- src/gromacs/mdlib/nbnxn_tuning.h
+++ src/gromacs/mdlib/nbnxn_tuning.h
@@ -63,19 +63,20 @@ struct t_inputrec;
 
 /*! \brief Try to increase nstlist when using the Verlet cut-off scheme
  *
- * \param[in,out] fp       Log file
+ * \param[in,out] fplog    Log file
  * \param[in]     cr       The communication record
  * \param[in]     ir       The input parameter record
- * \param[in]     nstlist_cmdline  The value of nstlist provided on the command line
+ * \param[in]     nstlistOnCmdline  The value of nstlist provided on the command line
  * \param[in]     mtop     The global topology
  * \param[in]     box      The unit cell
- * \param[in]     makeGpuPairList  Tells if we are using a GPU for non-bondeds
+ * \param[in]     useOrEmulateGpuForNonbondeds  Tells if we are using a GPU for non-bondeds
  * \param[in]     cpuinfo  Information about the CPU(s)
  */
-void increase_nstlist(FILE *fp, t_commrec *cr,
-                      t_inputrec *ir, int nstlist_cmdline,
-                      const gmx_mtop_t *mtop, matrix box,
-                      bool makeGpuPairList, const gmx::CpuInfo &cpuinfo);
+void increaseNstlist(FILE *fplog, t_commrec *cr,
+                     t_inputrec *ir, int nstlistOnCmdline,
+                     const gmx_mtop_t *mtop, matrix box,
+                     bool useOrEmulateGpuForNonbondeds,
+                     const gmx::CpuInfo &cpuinfo);
 
 /*! \brief Set up the dynamic pairlist pruning
  *
