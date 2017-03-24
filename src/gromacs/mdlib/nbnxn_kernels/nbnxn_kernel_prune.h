a6e84d00be224aec895a35d7774be503b45bc861
Berk Hess <hess@kth.se>
2017-03-24 14:33:00 +0100

    Add CPU reference and SIMD pruning kernels
    Change-Id: I416946adf67b8261b74093874b348270016585f1

---
+++ src/gromacs/mdlib/nbnxn_kernels/nbnxn_kernel_prune.h
@@ -0,0 +1,50 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016,2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+#include "gromacs/math/vectypes.h"
+#include "gromacs/utility/real.h"
+
+struct nonbonded_verlet_group_t;
+
+/*! \brief Prune all pair-lists in the set with distance \p rlistPrune
+ *
+ * For all pair-lists in the set prunes cluster pairs where all atom pair
+ * distances are beyond \p rlistPrune. The pruning is done from one list
+ * struct to another and then a pointer is changed to point the list pointer
+ * used in the interaction kernels to the pruned list.
+ */
+void nbnxn_kernel_prune(nonbonded_verlet_group_t  *nbvg,
+                        const rvec                *shift_vec,
+                        real                       rlistPrune);
