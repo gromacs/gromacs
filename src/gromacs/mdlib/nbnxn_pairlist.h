6009f2547c664aeeb512dec8239578f0d0a27ea3
Berk Hess <hess@kth.se>
2017-03-24 14:22:21 +0100

    Add dynamic pair-list pruning framework
    The change add the logistic for setting up dynamic pruning of the
    nbnxn pair-lists. Dynamic pruning allows for an increase in nstlist
    and rlist while computing fewer pair interactions in the non-bonded
    kernel. This comes at the cost of running an extra pruning kernel
    every few steps and added communication due to larger buffers.
    
    The kernels, documentation and heuristic for choosing the parameters
    will be added in separate changes.
    
    Change-Id: Id8040b95f812df60f117279267bf551ff4ac8d79

--- src/gromacs/mdlib/nbnxn_pairlist.h
+++ src/gromacs/mdlib/nbnxn_pairlist.h
@@ -159,15 +159,18 @@ typedef struct nbnxn_pairlist_t {
     int                     na_sc;       /* The number of atoms per super cluster    */
     real                    rlist;       /* The radius for constructing the list     */
     int                     nci;         /* The number of i-clusters in the list     */
+    int                     nci0;        /* The number of i-clusters in the unpruned list */
     nbnxn_ci_t             *ci;          /* The i-cluster list, size nci             */
-    int                     ci_nalloc;   /* The allocation size of ci                */
+    nbnxn_ci_t             *ci0;         /* The unpruned i-cluster list, size nci    */
+    int                     ci_nalloc;   /* The allocation size of ci/ci0            */
     int                     nsci;        /* The number of i-super-clusters in the list */
     nbnxn_sci_t            *sci;         /* The i-super-cluster list                 */
     int                     sci_nalloc;  /* The allocation size of sci               */
 
     int                     ncj;         /* The number of j-clusters in the list     */
     nbnxn_cj_t             *cj;          /* The j-cluster list, size ncj             */
-    int                     cj_nalloc;   /* The allocation size of cj                */
+    nbnxn_cj_t             *cj0;         /* The unpruned j-cluster list, size ncj    */
+    int                     cj_nalloc;   /* The allocation size of cj/cj0            */
     int                     ncjInUse;    /* The number of j-clusters that are used by ci entries in this list, will be <= ncj */
 
     int                     ncj4;        /* The total number of 4*j clusters         */
@@ -184,16 +187,18 @@ typedef struct nbnxn_pairlist_t {
 } nbnxn_pairlist_t;
 
 typedef struct {
-    int                nnbl;        /* number of lists */
-    nbnxn_pairlist_t **nbl;         /* lists */
-    nbnxn_pairlist_t **nbl_work;    /* work space for rebalancing lists */
-    gmx_bool           bCombined;   /* TRUE if lists get combined into one (the 1st) */
-    gmx_bool           bSimple;     /* TRUE if the list of of type "simple"
-                                       (na_sc=na_s, no super-clusters used) */
-    int                natpair_ljq; /* Total number of atom pairs for LJ+Q kernel */
-    int                natpair_lj;  /* Total number of atom pairs for LJ kernel   */
-    int                natpair_q;   /* Total number of atom pairs for Q kernel    */
-    t_nblist         **nbl_fep;
+    int                nnbl;             /* number of lists */
+    nbnxn_pairlist_t **nbl;              /* lists */
+    nbnxn_pairlist_t **nbl_work;         /* work space for rebalancing lists */
+    gmx_bool           bCombined;        /* TRUE if lists get combined into one (the 1st) */
+    gmx_bool           bSimple;          /* TRUE if the list of of type "simple"
+                                            (na_sc=na_s, no super-clusters used) */
+    int                natpair_ljq;      /* Total number of atom pairs for LJ+Q kernel */
+    int                natpair_lj;       /* Total number of atom pairs for LJ kernel   */
+    int                natpair_q;        /* Total number of atom pairs for Q kernel    */
+    t_nblist         **nbl_fep;          /* List of free-energy atom pair interactions */
+    bool               prunedSimpleList; /* Has the simple list been pruned and the original list been copied into ci0,nci0? */
+    gmx_int64_t        step;             /* Step at which the list was created */
 } nbnxn_pairlist_set_t;
 
 enum {
