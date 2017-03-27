63444daa0588ad80d50d0d1f03d76f50c6a749d5
Szilárd Páll <pall.szilard@gmail.com>
2017-03-27 19:19:19 +0200

    Allow disabling the explicit use of CUDA textures
    This change implements fallback for the explicit CUDA texture loads
    in the non-bonded kernels. This can be done by defining
    DISABLE_CUDA_TEXTURES. When disabled texture objects/references
    are not initialized either.
    
    Change-Id: Id7cdd6f80da0abe6be5639e80bed6530c3ce25c0

--- src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda.cu
+++ src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda.cu
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -695,18 +695,21 @@ void nbnxn_gpu_wait_for_gpu(gmx_nbnxn_cuda_t *nb,
 /*! Return the reference to the nbfp texture. */
 const struct texture<float, 1, cudaReadModeElementType> &nbnxn_cuda_get_nbfp_texref()
 {
+    assert(!disableCudaTextures);
     return nbfp_texref;
 }
 
 /*! Return the reference to the nbfp_comb texture. */
 const struct texture<float, 1, cudaReadModeElementType> &nbnxn_cuda_get_nbfp_comb_texref()
 {
+    assert(!disableCudaTextures);
     return nbfp_comb_texref;
 }
 
 /*! Return the reference to the coulomb_tab. */
 const struct texture<float, 1, cudaReadModeElementType> &nbnxn_cuda_get_coulomb_tab_texref()
 {
+    assert(!disableCudaTextures);
     return coulomb_tab_texref;
 }
 
