aa2b331fcafbc026178ffcae996cbe216f77cd2b
Szilárd Páll <pall.szilard@gmail.com>
2017-03-29 00:56:32 +0200

    Add CUDA pruning kernels and launch/timing logic
    The CUDA prune kernels have been tuned and tested on CC 2.x-6.x
    architectures. The tunable j4 concurrency parameter was chosen
    identical for both kernel flavors irrespective the input size which
    carries some tradeoffs (documented in the code). With auto-tuning,
    such static choices could be revised.
    
    Scheduling currently launches GPU prune kernels between steps, after the
    force clearing. This has some drawbacks and more sophisticated
    scheduling schemes might be beneficial in the future; these have been
    documented along the declaration of nbnxn_gpu_launch_kernel_pruneonly();
    
    Change-Id: Ia310ecafd7400efb33468d2486782c367a5a026a

--- src/gromacs/gpu_utils/cuda_arch_utils.cuh
+++ src/gromacs/gpu_utils/cuda_arch_utils.cuh
@@ -58,4 +58,27 @@
 static const int warp_size      = 32;
 static const int warp_size_log2 = 5;
 
+/* CUDA architecture technical characteristics. Needs macros because it is used
+ * in the __launch_bounds__ function qualifiers.
+ *
+ * Note that the final #else branch covers al future architectures (current gen
+ * is 6.x as of writing), hence assuming that these *currently defined* upper
+ * limits will not be lowered.
+ */
+#if GMX_PTX_ARCH > 0
+    #if   GMX_PTX_ARCH <= 210  // CC 2.x
+        #define GMX_CUDA_MAX_BLOCKS_PER_MP   8
+        #define GMX_CUDA_MAX_WARPS_PER_MP    48
+        #define GMX_CUDA_MAX_THREADS_PER_MP  1536
+    #elif GMX_PTX_ARCH <= 370  // CC 3.x
+        #define GMX_CUDA_MAX_BLOCKS_PER_MP   16
+        #define GMX_CUDA_MAX_WARPS_PER_MP    64
+        #define GMX_CUDA_MAX_THREADS_PER_MP  2048
+    #else // CC 5.x, 6.x
+        #define GMX_CUDA_MAX_BLOCKS_PER_MP   32
+        #define GMX_CUDA_MAX_WARPS_PER_MP    64
+        #define GMX_CUDA_MAX_THREADS_PER_MP  2048
+    #endif
+#endif
+
 #endif /* CUDA_ARCH_UTILS_CUH_ */
