e735fd062b9908de55616be6433d12e901c79446
Szilárd Páll <pall.szilard@gmail.com>
2017-04-06 01:34:10 +0200

    [RFC] Inform users about the end of run DLB state
    The user will not know whether the run did DLB or not without looking
    through the entire log or standard error. Hence, this change adds a note
    that tells the user about the state of DLB at the end of the simulation.
    
    The note on the fraction of the step used for load balance calculation
    has also been improved.
    
    NOTE: it is not possible to distinguish between the OFF because DLB
    slows down the run and the manually OFF states which, from the point of
    view of reporting is not ideal.
    
    Change-Id: I7a152a26453c00bb0433b738a69fe08ec9caa760

--- src/gromacs/domdec/domdec.cpp
+++ src/gromacs/domdec/domdec.cpp
@@ -104,6 +104,7 @@
 #include "gromacs/utility/qsort_threadsafe.h"
 #include "gromacs/utility/real.h"
 #include "gromacs/utility/smalloc.h"
+#include "gromacs/utility/stringutil.h"
 
 #include "domdec_constraints.h"
 #include "domdec_internal.h"
@@ -5110,16 +5111,39 @@ static void print_dd_load_av(FILE *fplog, gmx_domdec_t *dd)
     {
         float imbalance = comm->load_max*numPpRanks/comm->load_sum - 1;
         lossFraction    = dd_force_imb_perf_loss(dd);
-        fprintf(stderr, "\n");
-        sprintf(buf,
-                " Load balancing based on %d %% of the MD step time\n"
-                " Average load imbalance: %.1f %%\n"
-                " Part of the total run time spent waiting due to load imbalance: %.1f %%\n",
-                static_cast<int>(dd_force_load_fraction(dd)*100 + 0.5),
-                imbalance*100,
-                lossFraction*100);
-        fprintf(fplog, "%s", buf);
-        fprintf(stderr, "%s", buf);
+
+        std::string msg         = "\n";
+        std::string dlbStateStr = "";
+
+        switch (dd->comm->dlbState)
+        {
+            case edlbsOffForever:
+                dlbStateStr = "At the end of the run DLB is disabled by user user-input or due to performance loss observed.";
+                break;
+            case edlbsOffCanTurnOn:
+                dlbStateStr = "During the run DLB was off due to significant imbalance measured.";
+                break;
+            case edlbsOffTemporarilyLocked:
+                dlbStateStr = "At the end of the run DLB is locked (to prevent interference with PME tuning).";
+                break;
+            case edlbsOnCanTurnOff:
+                dlbStateStr = "During the run DLB switched on due to significant imbalance measured.";
+                break;
+            case edlbsOnForever:
+                dlbStateStr = "During the run DLB was permanently on per user-request.";
+                break;
+            default:
+                GMX_ASSERT(false, "Undocumented DLB state");
+        }
+
+        msg += " " + dlbStateStr + "\n";
+        msg += gmx::formatString(" Average load imbalance: %.1f%%.\n", imbalance*100);
+        msg += gmx::formatString(" The balanceable part of the MD step is %d%%, load imbalance is computed from this\n",
+                                 static_cast<int>(dd_force_load_fraction(dd)*100 + 0.5));
+        msg += gmx::formatString(" Part of the total run time spent waiting due to load imbalance: %.1f%%.\n",
+                                 lossFraction*100);
+        fprintf(fplog, "%s", msg.c_str());
+        fprintf(stderr, "%s", msg.c_str());
     }
 
     /* Print during what percentage of steps the  load balancing was limited */
