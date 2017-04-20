f09c09f9fc465b5520591d923d223a4179cd9e65
Berk Hess <hess@kth.se>
2017-04-20 21:04:11 +0200

    Unify nbnxn kernel dispatchers
    Reduced code duplication by merging the three nbnxn kernel dispatch
    functions for C reference, simd_4xn and simd_2xnn into a single
    dispatcher. This also removes the implementation details from
    sim_util.cpp.
    
    More reorganization could be done, but is not included here to
    minimize the size of this change.
    
    The original goal of this change was to conditionally compile all
    files in the simd_4xn and simd_2xnn directory, but currently we
    do not have the conditions available at configure time.
    
    Change-Id: I41f7ae96d267a12cf6024866b59bb0cda7e1cd2f

--- src/gromacs/mdlib/nbnxn_kernels/nbnxn_kernel_common.h
+++ src/gromacs/mdlib/nbnxn_kernels/nbnxn_kernel_common.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -33,43 +33,92 @@
  * the research papers on the package. Check out http://www.gromacs.org.
  */
 
+/*! \internal \file
+ *
+ * \brief
+ * Declares the nbnxn pair interaction kernel function types and kind counts, also declares utility functions used in nbnxn_kernel.cpp.
+ *
+ * \author Berk Hess <hess@kth.se>
+ */
+
 #ifndef _nbnxn_kernel_common_h
 #define _nbnxn_kernel_common_h
 
 #include "gromacs/math/vectypes.h"
+/* nbnxn_atomdata_t and nbnxn_pairlist_t could be forward declared, but that requires modifications in all SIMD kernel files */
+#include "gromacs/mdlib/nbnxn_atomdata.h"
 #include "gromacs/mdlib/nbnxn_pairlist.h"
 #include "gromacs/utility/real.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#if 0
-}
-#endif
+struct interaction_const_t;
 
-/* Clear the force buffer f. Either the whole buffer or only the parts
- * used by the current thread when nbat->bUseBufferFlags is set.
+/*! \brief Pair-interaction kernel type that also calculates energies.
+ */
+typedef void (nbk_func_ener)(const nbnxn_pairlist_t     *nbl,
+                             const nbnxn_atomdata_t     *nbat,
+                             const interaction_const_t  *ic,
+                             rvec                       *shift_vec,
+                             real                       *f,
+                             real                       *fshift,
+                             real                       *Vvdw,
+                             real                       *Vc);
+
+/*! \brief Pointer to \p nbk_func_ener.
+ */
+typedef nbk_func_ener *p_nbk_func_ener;
+
+/*! \brief Pair-interaction kernel type that does not calculates energies.
+ */
+typedef void (nbk_func_noener)(const nbnxn_pairlist_t     *nbl,
+                               const nbnxn_atomdata_t     *nbat,
+                               const interaction_const_t  *ic,
+                               rvec                       *shift_vec,
+                               real                       *f,
+                               real                       *fshift);
+
+/*! \brief Pointer to \p nbk_func_noener.
+ */
+typedef nbk_func_noener *p_nbk_func_noener;
+
+/*! \brief Kinds of electrostatic treatments in SIMD Verlet kernels
+ */
+enum {
+    coulktRF, coulktTAB, coulktTAB_TWIN, coulktEWALD, coulktEWALD_TWIN, coulktNR
+};
+
+/*! \brief Kinds of Van der Waals treatments in SIMD Verlet kernels
+ *
+ * The \p LJCUT_COMB refers to the LJ combination rule for the short range.
+ * The \p EWALDCOMB refers to the combination rule for the grid part.
+ * \p vdwktNR is the number of VdW treatments for the SIMD kernels.
+ * \p vdwktNR_ref is the number of VdW treatments for the C reference kernels.
+ * These two numbers differ, because currently only the reference kernels
+ * support LB combination rules for the LJ-Ewald grid part.
+ */
+enum {
+    vdwktLJCUT_COMBGEOM, vdwktLJCUT_COMBLB, vdwktLJCUT_COMBNONE, vdwktLJFORCESWITCH, vdwktLJPOTSWITCH, vdwktLJEWALDCOMBGEOM, vdwktLJEWALDCOMBLB, vdwktNR = vdwktLJEWALDCOMBLB, vdwktNR_ref
+};
+
+/*! \brief Clears the force buffer.
+ *
+ * Either the whole buffer is cleared or only the parts used
+ * by the current thread when nbat->bUseBufferFlags is set.
  * In the latter case output_index is the task/thread list/buffer index.
  */
 void
 clear_f(const nbnxn_atomdata_t *nbat, int output_index, real *f);
 
-/* Clear the shift forces */
+/*! \brief Clears the shift forces.
+ */
 void
 clear_fshift(real *fshift);
 
-/* Reduce the collected energy terms over the pair-lists/threads */
+/*! \brief Reduces the collected energy terms over the pair-lists/threads.
+ */
 void
 reduce_energies_over_lists(const nbnxn_atomdata_t     *nbat,
                            int                         nlist,
                            real                       *Vvdw,
                            real                       *Vc);
-
-#if 0
-{
-#endif
-#ifdef __cplusplus
-}
-#endif
 
 #endif
