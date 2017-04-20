3d3a794b3e885fea67dac838747d14424ecaf73a
Carsten Kutzner <ckutzne@gwdg.de>
2017-04-20 15:02:58 +0200

    Minor header file cleanup in ED, IMD and CompEL modules
    - removed dependency on wallcycle.h by forward declaring gmx_wallcycle,
      same with gmx_domdec_t
    - merged and sorted "struct xyz" statements in these headers
    - removed an unused gmx_wallcycle declaration in bonded.h
    - additionally fixed 4 doxygen warnings
    
    Change-Id: Ic397cdccb735b409b10802971ab60d8e078cd32e

--- src/gromacs/listed-forces/bonded.h
+++ src/gromacs/listed-forces/bonded.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -63,7 +63,6 @@
 extern "C" {
 #endif
 
-struct gmx_wallcycle;
 struct t_graph;
 struct t_pbc;
 
