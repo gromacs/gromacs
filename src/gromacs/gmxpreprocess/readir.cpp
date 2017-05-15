4bc38d9eaf226dd4d907093b44718bc800f561a8
Berk Hess <hess@kth.se>
2017-05-15 10:49:44 +0200

    Fix LJPME + switch grompp error
    An error call was missing in grompp when LJPME was requested in
    combination with a force or potential switch modifier.
    
    Fixes #2174.
    
    Change-Id: I92bc718869bf98b17efef1bab16c6457e07d0544

--- src/gromacs/gmxpreprocess/readir.cpp
+++ src/gromacs/gmxpreprocess/readir.cpp
@@ -1250,6 +1250,7 @@ void check_ir(const char *mdparin, t_inputrec *ir, t_gromppopts *opts,
                     evdw_names[ir->vdwtype],
                     eintmod_names[eintmodPOTSHIFT],
                     eintmod_names[eintmodNONE]);
+            warning_error(wi, err_buf);
         }
     }
 
