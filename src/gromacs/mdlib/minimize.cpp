gromacs/gromacs

commit 5b88d3c18113ec097bc4f07e0160ce8c2fdec1fd

Remove state duplication in serial runs
Without domain decomposition the local and global state should be
identical. This is easier to ensure with a pointer copy and this also
uses less memory than copying all data. It also gets rid of copy code.

Change-Id: Idad2748a83290b8506e03bb699ebede4d5987dcb

src/gromacs/mdlib/minimize.cpp
@@ -515,17 +515,6 @@ static void swap_em_state(em_state_t *ems1, em_state_t *ems2)
     *ems2 = tmp;
 }
 
-//! Copy coordinate from an EM state to a "normal" state structure
-static void copy_em_coords(em_state_t *ems, t_state *state)
-{
-    int i;
-
-    for (i = 0; (i < state->natoms); i++)
-    {
-        copy_rvec(ems->s.x[i], state->x[i]);
-    }
-}
-
 //! Save the EM trajectory
 static void write_em_traj(FILE *fplog, t_commrec *cr,
                           gmx_mdoutf_t outf,
@@ -535,22 +524,8 @@ static void write_em_traj(FILE *fplog, t_commrec *cr,
                           em_state_t *state,
                           t_state *state_global)
 {
-    int      mdof_flags;
-    gmx_bool bIMDout = FALSE;
+    int mdof_flags = 0;
 
-
-    /* Shall we do IMD output? */
-    if (ir->bIMD)
-    {
-        bIMDout = do_per_step(step, IMD_get_step(ir->imd->setup));
-    }
-
-    if ((bX || bF || bIMDout || confout != NULL) && !DOMAINDECOMP(cr))
-    {
-        copy_em_coords(state, state_global);
-    }
-
-    mdof_flags = 0;
     if (bX)
     {
         mdof_flags |= MDOF_X;

src/gromacs/mdlib/mdoutf.cpp
@@ -267,15 +267,7 @@ void mdoutf_write_to_trajectory_files(FILE *fplog, t_commrec *cr,
                                       t_state *state_local, t_state *state_global,
                                       rvec *f_local)
 {
-    rvec *local_v;
-    rvec *global_v;
     rvec *f_global;
-
-    /* MRS -- defining these variables is to manage the difference
-     * between half step and full step velocities, but there must be a better way . . . */
-
-    local_v  = state_local->v;
-    global_v = state_global->v;
 
     if (DOMAINDECOMP(cr))
     {
@@ -292,8 +284,8 @@ void mdoutf_write_to_trajectory_files(FILE *fplog, t_commrec *cr,
             }
             if (mdof_flags & MDOF_V)
             {
-                dd_collect_vec(cr->dd, state_local, local_v,
-                               global_v);
+                dd_collect_vec(cr->dd, state_local, state_local->v,
+                               state_global->v);
             }
         }
         f_global = of->f_global;
@@ -304,21 +296,10 @@ void mdoutf_write_to_trajectory_files(FILE *fplog, t_commrec *cr,
     }
     else
     {
-        if (mdof_flags & MDOF_CPT)
-        {
-            /* All pointers in state_local are equal to state_global,
-             * but we need to copy the non-pointer entries.
-             */
-            state_global->lambda = state_local->lambda;
-            state_global->veta   = state_local->veta;
-            state_global->vol0   = state_local->vol0;
-            copy_mat(state_local->box, state_global->box);
-            copy_mat(state_local->boxv, state_global->boxv);
-            copy_mat(state_local->svir_prev, state_global->svir_prev);
-            copy_mat(state_local->fvir_prev, state_global->fvir_prev);
-            copy_mat(state_local->pres_prev, state_global->pres_prev);
-        }
-        f_global = f_local;
+        /* We have the whole state locally: copy the local state pointer */
+        state_global = state_local;
+
+        f_global     = f_local;
     }
 
     if (MASTER(cr))
@@ -343,7 +324,7 @@ void mdoutf_write_to_trajectory_files(FILE *fplog, t_commrec *cr,
                 gmx_trr_write_frame(of->fp_trn, step, t, state_local->lambda[efptFEP],
                                     state_local->box, top_global->natoms,
                                     (mdof_flags & MDOF_X) ? state_global->x : NULL,
-                                    (mdof_flags & MDOF_V) ? global_v : NULL,
+                                    (mdof_flags & MDOF_V) ? state_global->v : NULL,
                                     (mdof_flags & MDOF_F) ? f_global : NULL);
                 if (gmx_fio_flush(of->fp_trn) != 0)
                 {
@@ -359,7 +340,7 @@ void mdoutf_write_to_trajectory_files(FILE *fplog, t_commrec *cr,
                                state_local->box,
                                top_global->natoms,
                                (mdof_flags & MDOF_X) ? state_global->x : NULL,
-                               (mdof_flags & MDOF_V) ? global_v : NULL,
+                               (mdof_flags & MDOF_V) ? state_global->v : NULL,
                                (mdof_flags & MDOF_F) ? f_global : NULL);
             }
             /* If only a TNG file is open for compressed coordinate output (no uncompressed
@@ -370,7 +351,7 @@ void mdoutf_write_to_trajectory_files(FILE *fplog, t_commrec *cr,
                                state_local->box,
                                top_global->natoms,
                                (mdof_flags & MDOF_X) ? state_global->x : NULL,
-                               (mdof_flags & MDOF_V) ? global_v : NULL,
+                               (mdof_flags & MDOF_V) ? state_global->v : NULL,
                                (mdof_flags & MDOF_F) ? f_global : NULL);
             }
         }

src/gromacs/mdlib/mdoutf.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -91,7 +91,8 @@ void done_mdoutf(gmx_mdoutf_t of);
  *
  * Writes data to trn, xtc and/or checkpoint. What is written is
  * determined by the mdof_flags defined below. Data is collected to
- * the master node only when necessary.
+ * the master node only when necessary. Without domain decomposition
+ * only data from state_local is used and state_global is ignored.
  */
 void mdoutf_write_to_trajectory_files(FILE *fplog, t_commrec *cr,
                                       gmx_mdoutf_t of,

src/gromacs/mdtypes/state.cpp
@@ -224,25 +224,6 @@ void done_state(t_state *state)
     }
 }
 
-t_state *serial_init_local_state(t_state *state_global)
-{
-    int      i;
-    t_state *state_local;
-
-    snew(state_local, 1);
-
-    /* Copy all the contents */
-    *state_local = *state_global;
-    snew(state_local->lambda, efptNR);
-    /* local storage for lambda */
-    for (i = 0; i < efptNR; i++)
-    {
-        state_local->lambda[i] = state_global->lambda[i];
-    }
-
-    return state_local;
-}
-
 void comp_state(const t_state *st1, const t_state *st2,
                 gmx_bool bRMSD, real ftol, real abstol)
 {

src/gromacs/mdtypes/state.h
@@ -257,8 +257,6 @@ void init_gtc_state(t_state *state, int ngtc, int nnhpres, int nhchainlength);
 
 void init_state(t_state *state, int natoms, int ngtc, int nnhpres, int nhchainlength, int dfhistNumLambda);
 
-t_state *serial_init_local_state(t_state *state_global);
-
 void done_state(t_state *state);
 
 void comp_state(const t_state *st1, const t_state *st2, gmx_bool bRMSD, real ftol, real abstol);

src/programs/mdrun/md.cpp
@@ -422,7 +422,8 @@ double gmx::do_md(FILE *fplog, t_commrec *cr, const gmx::MDLogger &mdlog,
     }
     else
     {
-        state    = serial_init_local_state(state_global);
+        /* Copy the pointer to the global state */
+        state = state_global;
 
         snew(top, 1);
         mdAlgorithmsSetupAtomData(cr, ir, top_global, top, fr,

src/programs/mdrun/repl_ex.cpp
@@ -551,86 +551,16 @@ static void exchange_state(const gmx_multisim_t *ms, int b, t_state *state)
     exchange_rvecs(ms, b, state->v, state->natoms);
 }
 
-static void copy_rvecs(rvec *s, rvec *d, int n)
+static void copy_state_serial(const t_state *src, t_state *dest)
 {
-    int i;
-
-    if (d != NULL)
+    if (dest != src)
     {
-        for (i = 0; i < n; i++)
-        {
-            copy_rvec(s[i], d[i]);
-        }
+        /* Currently the local state is always a pointer to the global
+         * in serial, so we should never end up here.
+         * TODO: Implement a (trivial) t_state copy once converted to C++.
+         */
+        GMX_RELEASE_ASSERT(false, "State copying is currently not implemented in replica exchange");
     }
-}
-
-static void copy_doubles(const double *s, double *d, int n)
-{
-    int i;
-
-    if (d != NULL)
-    {
-        for (i = 0; i < n; i++)
-        {
-            d[i] = s[i];
-        }
-    }
-}
-
-static void copy_reals(const real *s, real *d, int n)
-{
-    int i;
-
-    if (d != NULL)
-    {
-        for (i = 0; i < n; i++)
-        {
-            d[i] = s[i];
-        }
-    }
-}
-
-static void copy_ints(const int *s, int *d, int n)
-{
-    int i;
-
-    if (d != NULL)
-    {
-        for (i = 0; i < n; i++)
-        {
-            d[i] = s[i];
-        }
-    }
-}
-
-#define scopy_rvecs(v, n)   copy_rvecs(state->v, state_local->v, n);
-#define scopy_doubles(v, n) copy_doubles(state->v, state_local->v, n);
-#define scopy_reals(v, n) copy_reals(state->v, state_local->v, n);
-#define scopy_ints(v, n)   copy_ints(state->v, state_local->v, n);
-
-static void copy_state_nonatomdata(t_state *state, t_state *state_local)
-{
-    /* When t_state changes, this code should be updated. */
-    int ngtc, nnhpres;
-    ngtc    = state->ngtc * state->nhchainlength;
-    nnhpres = state->nnhpres* state->nhchainlength;
-    scopy_rvecs(box, DIM);
-    scopy_rvecs(box_rel, DIM);
-    scopy_rvecs(boxv, DIM);
-    state_local->veta = state->veta;
-    state_local->vol0 = state->vol0;
-    scopy_rvecs(svir_prev, DIM);
-    scopy_rvecs(fvir_prev, DIM);
-    scopy_rvecs(pres_prev, DIM);
-    scopy_doubles(nosehoover_xi, ngtc);
-    scopy_doubles(nosehoover_vxi, ngtc);
-    scopy_doubles(nhpres_xi, nnhpres);
-    scopy_doubles(nhpres_vxi, nnhpres);
-    scopy_doubles(therm_integral, state->ngtc);
-    scopy_rvecs(x, state->natoms);
-    scopy_rvecs(v, state->natoms);
-    copy_ints(&(state->fep_state), &(state_local->fep_state), 1);
-    scopy_reals(lambda, efptNR);
 }
 
 static void scale_velocities(t_state *state, real fac)
@@ -1315,7 +1245,7 @@ gmx_bool replica_exchange(FILE *fplog, const t_commrec *cr, struct gmx_repl_ex *
         }
         else
         {
-            copy_state_nonatomdata(state_local, state);
+            copy_state_serial(state_local, state);
         }
 
         if (MASTER(cr))
@@ -1351,7 +1281,7 @@ gmx_bool replica_exchange(FILE *fplog, const t_commrec *cr, struct gmx_repl_ex *
         if (!DOMAINDECOMP(cr))
         {
             /* Copy the global state to the local state data structure */
-            copy_state_nonatomdata(state, state_local);
+            copy_state_serial(state, state_local);
         }
     }
 

commit adabbfdeb274d7b6901ddaccc3d9ec000ef96b58

Fix confout.gro with energy minimization
Commit 5b88d3c18 caused the confout.gro with energy minimization
without domain decomposition to be the smae as the starting structure.

Fixes #2179.

Change-Id: I02389d47573bca0d730608a831ceb393cfec792f

src/gromacs/mdlib/minimize.cpp
@@ -530,16 +530,24 @@ static void write_em_traj(FILE *fplog, t_commrec *cr,
 
     if (confout != nullptr && MASTER(cr))
     {
+        /* With domain decomposition the call above collected the state->s.x
+         * into state_global->x. Without DD we copy the local state pointer.
+         */
+        if (!DOMAINDECOMP(cr))
+        {
+            state_global = &state->s;
+        }
+
         if (ir->ePBC != epbcNONE && !ir->bPeriodicMols && DOMAINDECOMP(cr))
         {
             /* Make molecules whole only for confout writing */
-            do_pbc_mtop(fplog, ir->ePBC, state_global->box, top_global,
+            do_pbc_mtop(fplog, ir->ePBC, state->s.box, top_global,
                         as_rvec_array(state_global->x.data()));
         }
 
         write_sto_conf_mtop(confout,
                             *top_global->name, top_global,
-                            as_rvec_array(state_global->x.data()), nullptr, ir->ePBC, state_global->box);
+                            as_rvec_array(state_global->x.data()), nullptr, ir->ePBC, state->s.box);
     }
 }
 