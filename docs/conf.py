5f646873d518e2d773cd441d50b7e9cbc0a09281
Teemu Murtola <teemu.murtola@gmail.com>
2017-05-23 21:06:45 +0300

    Do not use conf-man.py for non-manpage doc builds
    Avoid importing conf-man.py for Sphinx invocations that do not build man
    pages.  Since it only specifies man page build rules, it is not
    necessary, and some of the targets do not have dependencies that would
    ensure that it is present.
    
    Fixes #2184.
    
    Change-Id: I3fe9cb03667c9a4e1d4bb9e02b65544e8250be31

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2015,2016, by the GROMACS development team, led by
+# Copyright (c) 2015,2016,2017, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -309,7 +309,8 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-execfile('conf-man.py')
+if tags.has('do_man'):
+    execfile('conf-man.py')
 
 # If true, show URL addresses after external links.
 #man_show_urls = False
