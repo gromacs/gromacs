eef028a7fdf531587b363cdeb7e1e77cfaf6c10f
Mark Abraham <mark.j.abraham@gmail.com>
2017-06-01 01:56:51 +0200

    Reform ngpu variable
    This had different interpretations before and after MPI, and you had
    to read the MPI call to work out the meaning.  Made different
    variables that directly reflect the two meanings.
    
    Change-Id: I55b6f0b7d05c3286244231af7a5ef3e031079859

--- src/programs/mdrun/resource-division.h
+++ src/programs/mdrun/resource-division.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -77,6 +77,7 @@ int get_nthreads_mpi(const gmx_hw_info_t *hwinfo,
  */
 void check_resource_division_efficiency(const gmx_hw_info_t *hwinfo,
                                         const gmx_hw_opt_t  *hw_opt,
+                                        int                  numGpusToUseOnThisRank,
                                         gmx_bool             bNtOmpOptionSet,
                                         t_commrec           *cr,
                                         const gmx::MDLogger &mdlog);
