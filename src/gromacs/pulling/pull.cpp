9ca64f1a6520ba092155e30a668a6e682b5e7e2b
Berk Hess <hess@kth.se>
2017-06-08 11:38:17 +0200

    Fix COM pull force with SD
    The reported COM pull force when using the SD integrator was random
    only. Now the pull force is summed over the systematic and random
    SD update components.
    A better solution is to not add the random force at all, but such
    a change should not be done in a release branch.
    
    Fixes #2201.
    
    Change-Id: I10a56b30b952869396d170914bcbc0163299a1c8

--- src/gromacs/pulling/pull.cpp
+++ src/gromacs/pulling/pull.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1325,7 +1325,8 @@ static void do_constraint(struct pull_t *pull, t_pbc *pbc,
             continue;
         }
 
-        pcrd->f_scal = dr_tot[c]/((pull->group[pcrd->params.group[0]].invtm + pull->group[pcrd->params.group[1]].invtm)*dt*dt);
+        /* Accumulate the forces, in case we have multiple constraint steps */
+        pcrd->f_scal += dr_tot[c]/((pull->group[pcrd->params.group[0]].invtm + pull->group[pcrd->params.group[1]].invtm)*dt*dt);
 
         if (vir != NULL && pcrd->params.eGeom != epullgDIRPBC && bMaster)
         {
