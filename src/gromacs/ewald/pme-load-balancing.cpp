9a5b2cb6f831c6165e2a183554dcb6c6d4f7ff08
Berk Hess <hess@kth.se>
2017-06-12 09:10:41 +0200

    [RFC] Fix PME tuning with the group scheme
    Wen PME tuning with the group cut-off scheme, fr->bTwinRange was never
    set. Thus interactions between charge-group pairs at distances between
    rlist and rcoulomb were missing. Since this does not increase the
    non-bonded cost, PME tuning will push up rcoulomb and the grid spacing
    to the maximum possible.
    
    Fixes #2200.
    
    Change-Id: I5c1dae705bf53bd8153e1a2c65b6f5dce08881d7

--- src/gromacs/ewald/pme-load-balancing.cpp
+++ src/gromacs/ewald/pme-load-balancing.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1040,6 +1040,10 @@ void pme_loadbal_do(pme_load_balancing_t *pme_lb,
         fr->rlistlong     = fr->ic->rlistlong;
         fr->rcoulomb      = fr->ic->rcoulomb;
         fr->rvdw          = fr->ic->rvdw;
+        if (pme_lb->cutoff_scheme == ecutsGROUP)
+        {
+            fr->bTwinRange = (fr->rlistlong > fr->rlist);
+        }
 
         if (ir->eDispCorr != edispcNO)
         {
