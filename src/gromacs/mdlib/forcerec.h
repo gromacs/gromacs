d4f0fd5ba8fd8c4918112bbba15013178dac04a9
Teemu Murtola <teemu.murtola@gmail.com>
2017-06-25 17:24:01 +0300

    Generalize IForceProvider
    - Remove knowledge of individual modules from t_forcerec, and hide the
      number of modules, and whether they contribute to a virial or not,
      behind a generic interface.
    - Improve hard-to-understand initialization of separate f_novirsum,
      resolving some TODOs.
    - Add some parameters that probably make sense for other modules beyond
      the electric field one.
    
    This also makes the requirement for all modules to have the same
    parameters for the calculateForces() call explicit.
    
    Change-Id: I4952515c4b707ba458fd267565fd000532ec281e

--- src/gromacs/mdlib/forcerec.h
+++ src/gromacs/mdlib/forcerec.h
@@ -44,7 +44,6 @@
 #include "gromacs/mdtypes/forcerec.h"
 #include "gromacs/timing/wallcycle.h"
 
-struct IForceProvider;
 struct t_commrec;
 struct t_fcdata;
 struct t_filenm;
@@ -100,7 +99,6 @@ void init_interaction_const_tables(FILE                   *fp,
  * \param[in]  mdlog       File for printing
  * \param[out] fr          The forcerec
  * \param[in]  fcd         Force constant data
- * \param[in]  forceProviders  Handle to modules providing forces
  * \param[in]  ir          Inputrec structure
  * \param[in]  mtop        Molecular topology
  * \param[in]  cr          Communication structures
@@ -116,7 +114,6 @@ void init_forcerec(FILE                   *fplog,
                    const gmx::MDLogger    &mdlog,
                    t_forcerec             *fr,
                    t_fcdata               *fcd,
-                   IForceProvider         *forceProviders,
                    const t_inputrec       *ir,
                    const gmx_mtop_t       *mtop,
                    const t_commrec        *cr,
