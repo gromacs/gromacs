48730bdf28d01460e916b306fef0dedeecffe5bc
Mark Abraham <mark.j.abraham@gmail.com>
2017-06-29 11:47:54 +0200

    [WIP] gmx_constexpr is probably redundant
    if so, we can probably sed away gmx_constexpr
    
    Change-Id: Ic78121aaf59277535a086f818f0a2962d79013f6

--- src/gromacs/utility/basedefinitions.h
+++ src/gromacs/utility/basedefinitions.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -235,11 +235,7 @@ typedef uint64_t gmx_uint64_t;
  * random engines), we need to specify it for other compilers.
  */
 #ifndef gmx_constexpr
-#if !defined(_MSC_VER)
 #    define gmx_constexpr constexpr
-#else
-#    define gmx_constexpr
-#endif
 #endif
 
 /*! \def GMX_ALIGNED(type, alignment)
