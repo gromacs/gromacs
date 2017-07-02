935e84030fc70ddb5e01959073a8990c986deba0
Mark Abraham <mark.j.abraham@gmail.com>
2017-07-03 01:22:17 +0200

    Enhance cmake and releng handling of GMX_USE_RDTSCP
    It is possible for a physical or virtual CPU to support avx and not
    rdtscp, or the other way around, so the previous workaround was always
    a bit hacky.
    
    This change refactors the implementation to preserve the existing
    behaviour of both explicitly setting GMX_USE_RDTSCP to be ON or OFF,
    and the default behaviour of relying on the rdtscp detection, unless
    we should make a guess associated with an explicit user choice of SIMD
    level.
    
    It adds GMX_USE_RDTSCP=DETECT, which is now useful in Jenkins, where a
    hypervisor does expose AVX instructions and not RDTSCP, so the above
    fallback is not very effective for (e.g.) matrix configurations where
    we specify the SIMD level. Various jobs now use either OFF or DETECT
    according to the intent of the job. This will be useful in migrating
    verification jobs between hosts without having hidden
    incompatibilities.
    
    Switched usage of HAVE_RDTSCP to be always defined, for better
    checking by the compiler for any errors made.
    
    Improved status messages when the setting changes, and why, if it
    depends on the detection result.
    
    Change-Id: I932c1764d91ec317475ef71400802e7b8d07c887

--- src/gromacs/timing/cyclecounter.h
+++ src/gromacs/timing/cyclecounter.h
@@ -2,7 +2,7 @@
  * This file is part of the GROMACS molecular simulation package.
  *
  * Copyright (c) 1991-2006 David van der Spoel, Erik Lindahl, Berk Hess, University of Groningen.
- * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -46,7 +46,7 @@
 #define GMX_TIMING_CYCLECOUNTER_H
 
 /*
- * define HAVE_RDTSCP to use the serializing rdtscp instruction instead of rdtsc.
+ * Define HAVE_RDTSCP=1 to use the serializing rdtscp instruction instead of rdtsc.
  * This is only supported on newer Intel/AMD hardware, but provides better accuracy.
  */
 #include "config.h"
@@ -221,7 +221,7 @@ static __inline__ gmx_cycles_t gmx_cycles_read(void)
     gmx_cycles_t   cycle;
     unsigned       low, high;
 
-#ifdef HAVE_RDTSCP
+#if HAVE_RDTSCP
     __asm__ __volatile__("rdtscp" : "=a" (low), "=d" (high) :: "ecx" );
 #else
     __asm__ __volatile__("rdtsc" : "=a" (low), "=d" (high));
@@ -255,7 +255,7 @@ static __inline gmx_cycles_t gmx_cycles_read(void)
     return __rdpmccntr64();
 #else
     /* x86 */
-#    ifdef HAVE_RDTSCP
+#    if HAVE_RDTSCP
     unsigned int ui;
     return __rdtscp(&ui);
 #    else
