b86cb175cf2481cf89abbaf1f3f442fefe34c35c
Mark Abraham <mark.j.abraham@gmail.com>
2017-07-03 21:59:49 +0200

    Clean up cmake build-cpu detection
    This resolves some existing TODOs to de-duplicate code,
    while making it more robust to re-execution and proper
    use of the cache.
    
    Change-Id: Ie9c0618778b6da2aace37baa182785604c8f4afe

---
+++ cmake/gmxDetectCpu.cmake
@@ -0,0 +1,122 @@
+#
+# This file is part of the GROMACS molecular simulation package.
+#
+# Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
+# Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+# and including many others, as listed in the AUTHORS file in the
+# top-level source directory and at http://www.gromacs.org.
+#
+# GROMACS is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# GROMACS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with GROMACS; if not, see
+# http://www.gnu.org/licenses, or write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+#
+# If you want to redistribute modifications to GROMACS, please
+# consider that scientific software is very special. Version
+# control is crucial - bugs must be traceable. We will be happy to
+# consider code for inclusion in the official distribution, but
+# derived work must not be called official GROMACS. Details are found
+# in the README & COPYING files - if they are missing, get the
+# official version at http://www.gromacs.org.
+#
+# To help us fund GROMACS development, we humbly ask that you cite
+# the research papers on the package. Check out http://www.gromacs.org.
+
+include(gmxTestInlineASM)
+
+# Ensure things like GMX_TARGET_X86 are available
+include(gmxDetectTargetArchitecture)
+gmx_detect_target_architecture()
+
+# gmx_run_cpu_detection()
+#
+# Try to detect information about the CPU of the build host by
+# building and running the same detection code used by mdrun. This
+# works on all architectures where we are not cross-compiling;
+# depending on the architecture the detection will either use special
+# assembly instructions (like cpuid), preprocessor defines, or probing
+# /proc/cpuinfo on Linux.
+#
+# The TYPE argument is passed as a command-line argument to the
+# detection program, and the terminal output is captured and stored in
+# the cache variable BUILD_CPU_${TYPE} as the result. If the detect
+# program fails to compile, or fails to run, the value of
+# ${DEFAULT_OUTPUT} is stored instead.
+#
+# The function caches information about whether the detection program
+# has already been build or run with this TYPE, so this function
+# should be called freely, even if the call might be repeated within
+# or across invocations of cmake.
+#
+function(gmx_run_cpu_detection TYPE DEFAULT_OUTPUT)
+    string(TOUPPER ${TYPE} UPPERTYPE)
+    string(TOLOWER ${TYPE} LOWERTYPE)
+    if(BUILD_CPU_${UPPERTYPE})
+        return()
+    endif()
+
+    set(OUTPUT_VAR "${DEFAULT_OUTPUT}")
+    # We need to execute the binary, so this only works if not
+    # cross-compiling. However, note that we are NOT limited to x86.
+    if(CMAKE_CROSSCOMPILING)
+        # TODO Need we explain that we're not detecting because we are cross compiling?
+    else()
+        set(BUILD_CPU_BINARY "${PROJECT_BINARY_DIR}/CMakeFiles/GmxDetectCpu${CMAKE_EXECUTABLE_SUFFIX}")
+        if(NOT BUILD_CPU_COMPILED)
+            # Compile the detection program
+            set(GMX_TARGET_X86_VALUE 0)
+            if(GMX_TARGET_X86)
+                set(GMX_TARGET_X86_VALUE 1)
+            endif()
+
+            # for x86 we need inline assembly to use cpuid
+            gmx_test_inline_asm_gcc_x86(GMX_X86_GCC_INLINE_ASM)
+            if(GMX_X86_GCC_INLINE_ASM)
+                set(GCC_INLINE_ASM_DEFINE "-DGMX_X86_GCC_INLINE_ASM=1")
+            else()
+                set(GCC_INLINE_ASM_DEFINE "-DGMX_X86_GCC_INLINE_ASM=0")
+            endif()
+
+            set(_compile_definitions "${GCC_INLINE_ASM_DEFINE} -I${CMAKE_SOURCE_DIR}/src -DGMX_CPUINFO_STANDALONE ${GMX_STDLIB_CXX_FLAGS} -DGMX_TARGET_X86=${GMX_TARGET_X86_VALUE}")
+            set(LINK_LIBRARIES "${GMX_STDLIB_LIBRARIES}")
+            try_compile(BUILD_CPU_COMPILED
+                "${PROJECT_BINARY_DIR}"
+                "${CMAKE_CURRENT_SOURCE_DIR}/src/gromacs/hardware/cpuinfo.cpp"
+                COMPILE_DEFINITIONS "${_compile_definitions}"
+                CMAKE_FLAGS "-DLINK_LIBRARIES=${LINK_LIBRARIES}"
+                OUTPUT_VARIABLE BUILD_CPU_COMPILED_OUTPUT
+                COPY_FILE ${BUILD_CPU_BINARY})
+        endif()
+
+        if(NOT BUILD_CPU_COMPILED)
+            message(STATUS "Did not detect build CPU ${LOWERTYPE} - detection program did not compile")
+        else()
+            # Run the detection program with -type as the argument.
+
+            if(NOT DEFINED BUILD_CPU_RUN_${UPPERTYPE})
+                execute_process(COMMAND ${BUILD_CPU_BINARY} "-${LOWERTYPE}"
+                    RESULT_VARIABLE RESULT_VAR
+                    OUTPUT_VARIABLE OUTPUT_VAR_TEMP
+                    ERROR_QUIET)
+                if (RESULT_VAR EQUAL 0)
+                    string(STRIP "${OUTPUT_VAR_TEMP}" OUTPUT_VAR)
+                    message(STATUS "Detected build CPU ${TYPE} - ${OUTPUT_VAR}")
+                    set(BUILD_CPU_RUN_${UPPERTYPE} TRUE CACHE INTERNAL "Records that cpu detection code with argument -${LOWERTYPE} has run, to prevent re-running")
+                else()
+                    message(STATUS "Did not detect build CPU ${TYPE} - detection program did not run successfully")
+                endif()
+            endif()
+        endif()
+    endif()
+    set(BUILD_CPU_${UPPERTYPE} "${OUTPUT_VAR}" CACHE INTERNAL "Result of running cpu detection code with argument -${LOWERTYPE}")
+endfunction()
--- cmake/gmxSetBuildInformation.cmake
+++ cmake/gmxSetBuildInformation.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+# Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -32,12 +32,12 @@
 # To help us fund GROMACS development, we humbly ask that you cite
 # the research papers on the package. Check out http://www.gromacs.org.
 
-# - Check the username performing the build, as well as date and time
+# Check the username performing the build, as well as date, time, and
+# build CPU features.
 #
-# gmx_set_build_information()
-#
-# The macro variables will be set to the user/host/cpu used for configuration,
-# or anonymous/unknown if it cannot be detected (windows)
+# The following cache variables will be set to the user/host/cpu used
+# for configuration, or anonymous/unknown if it cannot be detected
+# (Windows)
 #
 # BUILD_TIME
 # BUILD_USER
@@ -50,18 +50,11 @@
 # BUILD_CPU_FEATURES
 #
 
-# we rely on inline asm support for GNU!
-include(gmxTestInlineASM)
+include(gmxDetectCpu)
 
-macro(gmx_set_build_information)
-    IF(NOT DEFINED BUILD_USER)
-
-    gmx_test_inline_asm_gcc_x86(GMX_X86_GCC_INLINE_ASM)
-
-    if(GMX_X86_GCC_INLINE_ASM)
-        set(GCC_INLINE_ASM_DEFINE "-DGMX_X86_GCC_INLINE_ASM=1")
-    else()
-        set(GCC_INLINE_ASM_DEFINE "-DGMX_X86_GCC_INLINE_ASM=0")
+function(gmx_set_build_information)
+    if(DEFINED BUILD_USER)
+        return()
     endif()
 
     message(STATUS "Setting build user/date/host/cpu information")
@@ -81,112 +74,15 @@ macro(gmx_set_build_information)
         message(STATUS "Setting build user & time - not on Unix, using anonymous")
     endif()
 
-    # Set up some defaults that will usually be overridden
+    # Set up some defaults that will usually be overwritten
     if(CMAKE_CROSSCOMPILING)
         set(_reason ", cross-compiled")
     endif()
-    set(OUTPUT_CPU_VENDOR   "Unknown${_reason}")
-    set(OUTPUT_CPU_BRAND    "Unknown${_reason}")
-    set(OUTPUT_CPU_FAMILY   "0")
-    set(OUTPUT_CPU_MODEL    "0")
-    set(OUTPUT_CPU_STEPPING "0")
-    set(OUTPUT_CPU_FEATURES "Unknown${_reason}")
-    unset(_reason)
 
-    if(NOT CMAKE_CROSSCOMPILING)
-        # Get CPU information, e.g. for deciding what SIMD support probably exists
-        if(GMX_TARGET_X86)
-            set(GMX_TARGET_X86_VALUE 1)
-        else()
-            set(GMX_TARGET_X86_VALUE 0)
-        endif()
-        set(_compile_definitions "${GCC_INLINE_ASM_DEFINE} -I${CMAKE_SOURCE_DIR}/src -DGMX_CPUINFO_STANDALONE ${GMX_STDLIB_CXX_FLAGS} -DGMX_TARGET_X86=${GMX_TARGET_X86_VALUE}")
-
-        set(GMX_BUILDINFORMATION_BINARY "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/GmxBuildInformation${CMAKE_EXECUTABLE_SUFFIX}")
-        set(LINK_LIBRARIES "${GMX_STDLIB_LIBRARIES}")
-        # TODO Extract this try_compile to a helper function, because
-        # it duplicates code in gmxDetectSimd.cmake
-        try_compile(GMX_BUILDINFORMATION_COMPILED
-            "${CMAKE_CURRENT_BINARY_DIR}"
-            "${CMAKE_CURRENT_SOURCE_DIR}/src/gromacs/hardware/cpuinfo.cpp"
-            COMPILE_DEFINITIONS "${_compile_definitions}"
-            CMAKE_FLAGS "-DLINK_LIBRARIES=${LINK_LIBRARIES}"
-            OUTPUT_VARIABLE GMX_BUILDINFORMATION_COMPILED_OUTPUT
-            COPY_FILE ${GMX_BUILDINFORMATION_BINARY})
-        unset(_compile_definitions)
-
-        if(GMX_BUILDINFORMATION_COMPILED)
-            # TODO Extract this duplication to a helper function (also
-            # from gmxDetectSimd.cmake)
-            if(NOT DEFINED GMX_BUILDINFORMATION_RUN_VENDOR)
-                execute_process(COMMAND ${GMX_BUILDINFORMATION_BINARY} "-vendor"
-                    RESULT_VARIABLE GMX_BUILDINFORMATION_RUN_VENDOR
-                    OUTPUT_VARIABLE OUTPUT_TMP
-                    ERROR_QUIET)
-                set(GMX_BUILDINFORMATION_RUN_VENDOR "${GMX_BUILDINFORMATION_RUN_VENDOR}" CACHE INTERNAL "Result of running cpuinfo code with arg -vendor")
-                if(GMX_BUILDINFORMATION_RUN_VENDOR EQUAL 0)
-                    string(STRIP "${OUTPUT_TMP}" OUTPUT_CPU_VENDOR)
-                endif()
-            endif()
-            if(NOT DEFINED GMX_BUILDINFORMATION_RUN_BRAND)
-                execute_process(COMMAND ${GMX_BUILDINFORMATION_BINARY} "-brand"
-                    RESULT_VARIABLE GMX_BUILDINFORMATION_RUN_BRAND
-                    OUTPUT_VARIABLE OUTPUT_TMP
-                    ERROR_QUIET)
-                set(GMX_BUILDINFORMATION_RUN_BRAND "${GMX_BUILDINFORMATION_RUN_BRAND}" CACHE INTERNAL "Result of running cpuinfo code with arg -brand")
-                if(GMX_BUILDINFORMATION_RUN_BRAND EQUAL 0)
-                    string(STRIP "${OUTPUT_TMP}" OUTPUT_CPU_BRAND)
-                endif()
-            endif()
-            if(NOT DEFINED GMX_BUILDINFORMATION_RUN_FAMILY)
-                execute_process(COMMAND ${GMX_BUILDINFORMATION_BINARY} "-family"
-                    RESULT_VARIABLE GMX_BUILDINFORMATION_RUN_FAMILY
-                    OUTPUT_VARIABLE OUTPUT_TMP
-                    ERROR_QUIET)
-                set(GMX_BUILDINFORMATION_RUN_FAMILY "${GMX_BUILDINFORMATION_RUN_FAMILY}" CACHE INTERNAL "Result of running cpuinfo code with arg -family")
-                if(GMX_BUILDINFORMATION_RUN_FAMILY EQUAL 0)
-                    string(STRIP "${OUTPUT_TMP}" OUTPUT_CPU_FAMILY)
-                endif()
-            endif()
-            if(NOT DEFINED GMX_BUILDINFORMATION_RUN_MODEL)
-                execute_process(COMMAND ${GMX_BUILDINFORMATION_BINARY} "-model"
-                    RESULT_VARIABLE GMX_BUILDINFORMATION_RUN_MODEL
-                    OUTPUT_VARIABLE OUTPUT_TMP
-                    ERROR_QUIET)
-                set(GMX_BUILDINFORMATION_RUN_MODEL "${GMX_BUILDINFORMATION_RUN_MODEL}" CACHE INTERNAL "Result of running cpuinfo code with arg -model")
-                if(GMX_BUILDINFORMATION_RUN_MODEL EQUAL 0)
-                    string(STRIP "${OUTPUT_TMP}" OUTPUT_CPU_MODEL)
-                endif()
-            endif()
-            if(NOT DEFINED GMX_BUILDINFORMATION_RUN_STEPPING)
-                execute_process(COMMAND ${GMX_BUILDINFORMATION_BINARY} "-stepping"
-                    RESULT_VARIABLE GMX_BUILDINFORMATION_RUN_STEPPING
-                    OUTPUT_VARIABLE OUTPUT_TMP
-                    ERROR_QUIET)
-                set(GMX_BUILDINFORMATION_RUN_STEPPING "${GMX_BUILDINFORMATION_RUN_STEPPING}" CACHE INTERNAL "Result of running cpuinfo code with arg -stepping")
-                if(GMX_BUILDINFORMATION_RUN_STEPPING EQUAL 0)
-                    string(STRIP "${OUTPUT_TMP}" OUTPUT_CPU_STEPPING)
-                endif()
-            endif()
-            if(NOT DEFINED GMX_BUILDINFORMATION_RUN_FEATURES)
-                execute_process(COMMAND ${GMX_BUILDINFORMATION_BINARY} "-features"
-                    RESULT_VARIABLE GMX_BUILDINFORMATION_RUN_FEATURES
-                    OUTPUT_VARIABLE OUTPUT_TMP
-                    ERROR_QUIET)
-                set(GMX_BUILDINFORMATION_RUN_FEATURES "${GMX_BUILDINFORMATION_RUN_FEATURES}" CACHE INTERNAL "Result of running cpuinfo code with arg -features")
-                if(GMX_BUILDINFORMATION_RUN_FEATURES EQUAL 0)
-                    string(STRIP "${OUTPUT_TMP}" OUTPUT_CPU_FEATURES)
-                endif()
-            endif()
-        endif()
-    endif()
-
-    set(BUILD_CPU_VENDOR   "${OUTPUT_CPU_VENDOR}"   CACHE INTERNAL "Build CPU vendor")
-    set(BUILD_CPU_BRAND    "${OUTPUT_CPU_BRAND}"    CACHE INTERNAL "Build CPU brand")
-    set(BUILD_CPU_FAMILY   "${OUTPUT_CPU_FAMILY}"   CACHE INTERNAL "Build CPU family")
-    set(BUILD_CPU_MODEL    "${OUTPUT_CPU_MODEL}"    CACHE INTERNAL "Build CPU model")
-    set(BUILD_CPU_STEPPING "${OUTPUT_CPU_STEPPING}" CACHE INTERNAL "Build CPU stepping")
-    set(BUILD_CPU_FEATURES "${OUTPUT_CPU_FEATURES}" CACHE INTERNAL "Build CPU features")
-
-    ENDIF(NOT DEFINED BUILD_USER)
-endmacro(gmx_set_build_information)
+    gmx_run_cpu_detection(vendor   "Unknown${_reason}")
+    gmx_run_cpu_detection(brand    "Unknown${_reason}")
+    gmx_run_cpu_detection(family   "0")
+    gmx_run_cpu_detection(model    "0")
+    gmx_run_cpu_detection(stepping "0")
+    gmx_run_cpu_detection(features "Unknown${_reason}")
+endfunction()
