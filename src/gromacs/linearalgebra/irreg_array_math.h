17640c4c973ef90a2fb0e9903618c6b10701bd3d
RTU <tullman@gwdg.de>
2017-07-24 14:23:08 +0200

    Linear algebra on IrregArray2D and std::vector
    - vector matrix product
    - matrix product
    - LU decomposition with row permutation
    - matrix inversion based on LU decomposition
    - matrix determinant based on LU decomposition
    
    uses the GROMACS SIMD module for SIMD support
    (probably improvable)
    
    Intended to be used for a constraint solver
    in the lambda dynamics module.
    
    May also be useful for other cases like partial
    least squares based functional mode analysis.
    
    Change-Id: Id5b32ac18479b32ddbe6e7d8f16b829c468b66e6
    Depends-On: Ic80a3e9de7c54440a2734a6d798479c452383e5e

---
+++ src/gromacs/linearalgebra/irreg_array_math.h
@@ -0,0 +1,66 @@
+/*
+   This file is part of the GROMACS molecular simulation package.
+
+   Copyright (c) 2017, by the GROMACS development team, led by
+   Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+   and including many others, as listed in the AUTHORS file in the
+   top-level source directory and at http://www.gromacs.org.
+
+   GROMACS is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation; either version 2.1
+   of the License, or (at your option) any later version.
+
+   GROMACS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with GROMACS; if not, see
+   http://www.gnu.org/licenses, or write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+
+   If you want to redistribute modifications to GROMACS, please
+   consider that scientific software is very special. Version
+   control is crucial - bugs must be traceable. We will be happy to
+   consider code for inclusion in the official distribution, but
+   derived work must not be called official GROMACS. Details are found
+   in the README & COPYING files - if they are missing, get the
+   official version at http://www.gromacs.org.
+
+   To help us fund GROMACS development, we humbly ask that you cite
+   the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \defgroup module_linearalgebra Linear Algebra (linearalgebra)
+   \ingroup group_utilitymodules
+   \brief
+   Provides linear algebra functions
+
+   Linear algebra on IrregArray2D and std::vector containers
+
+   - vector matrix product
+   - matrix product
+   - LU decomposition with row permutation
+   - matrix inversion based on LU decomposition
+   - matrix determinant based on LU decomposition
+
+   \author R. Thomas Ullmann <tullman@gwdg.de>
+ */
+/*! \libinternal \file
+   \brief
+    Convenience header for linear algebra operations on IrregArray classes and std::vector
+
+    \date Apr 2016
+
+   \author R. Thomas Ullmann <tullman@gwdg.de>
+   \inlibraryapi
+   \ingroup module_linearalgebra
+ */
+#ifndef GMX_LINEAR_ALGEBRA_IRREG_ARRAY_MATH_H_MATH_H
+#define GMX_LINEAR_ALGEBRA_IRREG_ARRAY_MATH_H_MATH_H
+
+#include "gromacs/linearalgebra/irreg_array_math/irreg_array_2d_lu_decomposition.h"
+#include "gromacs/linearalgebra/irreg_array_math/irreg_array_2d_matrix_products.h"
+
+#endif
