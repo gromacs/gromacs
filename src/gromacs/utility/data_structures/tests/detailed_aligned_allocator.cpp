8286bfb6917b9df78b447f6ba06c9049f38aebdc
RTU <tullman@gwdg.de>
2017-07-24 12:10:42 +0200

    array classes for lambda-dynamics and local alternative topologies
    Function
    ========
    
    The IrregArray classes can have nonuniform lengths of array stripes
    in higher dimensions (see IrregArray2D.h for an illustration).
    They are are allocator-aware for general usability on different devices,
    with different alignment requirements etc.
    They include a variety of convenience constructors and public
    typedefs for easy use in code employing them (see unit test).
    An earlier, simpler version of these arrays has already been in use
    for years in the Monte Carlo simulation software GMCT.
    
    Purpose
    =======
    
    For the lambda-dynamics/FMM method a new topology with local
    alternatives will be needed (e.g., protein sidechain protonation
    states, or at the extreme drug molecules with different substituents
    or entirely different structures). This requirement will force
    us to rewrite at least parts of the code for topology reading
    and generation. This more general topology will be needed for
    purposes other than the lambda-dynamics/FMM too, especially in
    general free energy calculations with N end states.
    
    There are already some Redmine issues on the topic (refs #1653, #1652 and #1332)
    
    Details
    =======
    
    We plan to use the IrregArrayNDs for storing properties of different
    site forms. As an concrete example, they are needed to store
    energy terms for the different states or forms of variables sites,
    because different sites may have very different numbers of forms.
    For example, an ASP sidechain my be modeled with 5 protonation forms,
    while a drug molecule bound nearby may possess dozens of variants
    (and possibly many more) with different substitution patterns,
    different protonation etc. Simply allocating the maximum number
    of array elements for each site will lead to greatly exaggerated
    memory demands, which was the initial motivation for developing
    these array classes. The performance of the array classes in
    reading and writing is very close to that of bare arrays
    (see the performance unit tests), but if required one can also
    work directly with the underlying data array.
    
    The Prefix "Flat" signifies that these variants internally use
    a flat 1D-array, while the classes without this prefix use
    multidimensional data arrays. Surprisingly, in the tests ran so
    far, the Flat variants are slower than the multidimensional ones.
    However, since the relative performance may depend on the
    precise context, it may still be good to have the choice.
    
    See the unit test in
    src/gromacs/utility/data_structures/tests/irreg_array.cpp and
    src/gromacs/utility/data_structures/tests/irreg_array_performance.cpp
    for usage examples and performance tests.
    
    Change-Id: Ic80a3e9de7c54440a2734a6d798479c452383e5e

---
+++ src/gromacs/utility/data_structures/tests/detailed_aligned_allocator.cpp
@@ -0,0 +1,273 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+   \brief
+   Tests for the the irreg_array classes
+
+   For development, the tests can be run with a '-stdout' command-line option
+   to print out the help to stdout instead of using the XML reference
+   framework.
+
+   \author R. Thomas Ullmann <tullman@gwdg.de>
+
+   \ingroup module_utility
+ */
+#include "gmxpre.h"
+
+#include "gromacs/utility/data_structures/detailed_aligned_allocator.h"
+
+#include <vector>
+
+#include "gromacs/utility/data_structures/flat_irreg_array_4d.h"
+#include "gromacs/utility/data_structures/irreg_array_4d.h"
+
+#include "data_struct_test_commons.h"
+
+namespace
+{
+
+using std::endl;
+
+
+class DetailedAlignedAllocatorTest : public gmx::data_struct_test::DataStructTest
+{
+};
+
+/*********************************************************************/
+
+// the following two are just adopted from the AlignedAllocatorTest
+// DefaultAlignedAllocator should do exactly the same as AlignedAllocator
+
+TEST_F(DetailedAlignedAllocatorTest, AllocatorAlign)
+{
+    gmx::DefaultAlignedAllocator<real>   a;
+    real *                               p    = a.allocate(1000);
+
+    // Mask for 128-byte alignment is 128-1 - these bits should be zero in p
+    std::size_t                          mask = static_cast<std::size_t>(128-1);
+
+    EXPECT_EQ(0, reinterpret_cast<std::size_t>(p) & mask);
+    a.deallocate(p, 1000);
+}
+
+
+TEST_F(DetailedAlignedAllocatorTest, Vector)
+{
+    // Mask for 128-byte alignment is 128-1 - these bits should be zero in pointers
+    std::size_t mask = static_cast<std::size_t>(128-1);
+
+    std::vector<double, gmx::DefaultAlignedAllocator<double> > v(10);
+    EXPECT_EQ(0, reinterpret_cast<std::size_t>(v.data()) & mask);
+
+    for (std::size_t i = 10000; i <= 100000; i += 10000)
+    {
+        v.resize(i);
+        EXPECT_EQ(0, reinterpret_cast<std::size_t>(v.data()) & mask);
+    }
+}
+
+TEST_F(DetailedAlignedAllocatorTest, ConstructDestructIrregArrays)
+{
+
+    typedef gmx::DefaultAlignedAllocator<double> allocator_type1;
+    EXPECT_NO_THROW_GMX
+    (
+            {
+                allocator_type1 test_alloc1;
+                double *tmpPtr = test_alloc1.allocate(1);
+                test_alloc1.deallocate(tmpPtr, 1);
+            }
+    )
+    << "Construction destruction of \"DefaultAlignedAllocator\" failed with exception";
+
+    typedef gmx::DetailedAlignedAllocator<double, 64u, 128u> allocator_type2;
+    EXPECT_NO_THROW_GMX
+    (
+            {
+                allocator_type2 test_alloc2;
+                double *tmpPtr = test_alloc2.allocate(1);
+                test_alloc2.deallocate(tmpPtr, 1);
+            }
+    )
+    << "Construction destruction of \"DetailedAlignedAllocator\" failed with exception";
+
+}
+
+TEST_F(DetailedAlignedAllocatorTest, DataStructuresIrregArray1DAlloc)
+{
+
+    typedef gmx::DefaultAlignedAllocator<unsigned int> default_allocator_type;
+
+    gmx::IrregArray1D<unsigned int, default_allocator_type> test_arr1(1, 6, 0u);
+
+    EXPECT_NO_THROW_GMX(
+            {
+                test_arr1[1] = 1;
+                test_arr1[2] = 2;
+                test_arr1[3] = 3;
+                test_arr1[4] = 4;
+                test_arr1[5] = 5;
+                test_arr1[6] = 6;
+            }
+            ) << "Caught exception while assigning via operator[]." << endl;
+
+    EXPECT_TRUE(test_arr1[1] == 1 &&
+                test_arr1[2] == 2 &&
+                test_arr1[3] == 3 &&
+                test_arr1[4] == 4 &&
+                test_arr1[5] == 5 &&
+                test_arr1[6] == 6)
+    << "Test of assignment and retrieval via IrregArray1D::operator[] failed.";
+
+    debug << "test_arr1: " << endl << test_arr1 << endl;
+}
+
+TEST_F(DetailedAlignedAllocatorTest, DataStructuresIrregArray2Alloc)
+{
+    typedef gmx::DefaultAlignedAllocator<unsigned int> default_allocator_type;
+
+    gmx::IrregArray1D<size_t> sizes1((ssize_t)0, (ssize_t)6, (size_t)0);
+    sizes1[0] = 2;
+    sizes1[1] = 1;
+    sizes1[2] = 4;
+    sizes1[3] = 8;
+    sizes1[4] = 5;
+    sizes1[5] = 3;
+    sizes1[6] = 9;
+    gmx::IrregArray2D<unsigned int, default_allocator_type> test_arr1((ssize_t)0, (ssize_t)6, sizes1, 99u);
+
+    EXPECT_NO_THROW_GMX(
+            {
+                unsigned int m = 1;
+                for (ssize_t i = test_arr1.getFirst1(); i <= test_arr1.getLast1(); ++i)
+                {
+                    for (ssize_t j = test_arr1.getFirst2(); j <= test_arr1.getLast2(i); ++j)
+                    {
+                                test_arr1[i][j] = m;
+                                m++;
+                    }
+                }
+            }
+            ) << "Test of assignment to an irregular array via operator[] failed with an exception.";
+
+    debug << "test_arr1: " << endl << test_arr1 << endl;
+}
+
+TEST_F(DetailedAlignedAllocatorTest, DataStructuresIrregArray3DAlloc)
+{
+
+    typedef gmx::DefaultAlignedAllocator<unsigned int> default_allocator_type;
+
+    gmx::IrregArray1D<size_t> sizes1((ssize_t)0, (ssize_t)6, (size_t)0);
+    sizes1[0] = 2;
+    sizes1[1] = 1;
+    sizes1[2] = 4;
+    sizes1[3] = 8;
+    sizes1[4] = 5;
+    sizes1[5] = 3;
+    sizes1[6] = 9;
+
+    // let the array start with index 0, and initialize it with value 99
+    gmx::IrregArray2D<size_t>       sizes2(sizes1.getBegin1(), sizes1.getLast1(), sizes1, 3u);
+
+    gmx::IrregArray3D<unsigned int, default_allocator_type> test_arr1((ssize_t)0, (ssize_t)6, sizes1, sizes2, 99u);
+
+    EXPECT_NO_THROW_GMX(
+            {
+                unsigned int m = 1;
+                for (ssize_t i = test_arr1.getFirst1(); i <= test_arr1.getLast1(); ++i)
+                {
+                    for (ssize_t j = test_arr1.getFirst2(); j <= test_arr1.getLast2(i); ++j)
+                    {
+                        for (ssize_t k = test_arr1.getFirst3(); k <= test_arr1.getLast3(i, j); ++k)
+                        {
+                            test_arr1[i][j][k] = m;
+                            m++;
+                        }
+                    }
+                }
+            }
+            ) << "Test of assignment to an irregular array via operator[] failed with an exception.";
+
+    debug << "test_arr1: " << endl << test_arr1 << endl;
+}
+
+TEST_F(DetailedAlignedAllocatorTest, DataStructuresIrregArray4DAlloc)
+{
+
+//    typedef gmx::DefaultAlignedAllocator<unsigned int> default_allocator_type;
+
+    typedef std::allocator<unsigned int> default_allocator_type;
+
+    gmx::IrregArray1D<size_t> sizes1((ssize_t)0, (ssize_t)6, (size_t)0);
+    sizes1[0] = 2;
+    sizes1[1] = 1;
+    sizes1[2] = 4;
+    sizes1[3] = 8;
+    sizes1[4] = 5;
+    sizes1[5] = 3;
+    sizes1[6] = 9;
+
+    // let the array start with index 0, and initialize it with value 99
+    gmx::IrregArray2D<size_t>       sizes2(sizes1.getBegin1(), sizes1.getLast1(), sizes1, 2u);
+    gmx::IrregArray3D<size_t>       sizes3(sizes1.getBegin1(), sizes1.getLast1(), sizes1, sizes2, 3u);
+
+    gmx::IrregArray4D<unsigned int, default_allocator_type> test_arr1((ssize_t)0, (ssize_t)0, sizes1, sizes2, sizes3, 99u);
+
+    EXPECT_NO_THROW_GMX(
+            {
+                unsigned int m = 1;
+                for (ssize_t i = test_arr1.getFirst1(); i <= test_arr1.getLast1(); ++i)
+                {
+                    for (ssize_t j = test_arr1.getFirst2(); j <= test_arr1.getLast2(i); ++j)
+                    {
+                        for (ssize_t k = test_arr1.getFirst3(); k <= test_arr1.getLast3(i, j); ++k)
+                        {
+                            for (ssize_t l = test_arr1.getFirst4(); l <= test_arr1.getLast4(i, j, k); ++l)
+                            {
+                                test_arr1[i][j][k][l] = m;
+                                m++;
+                            }
+                        }
+                    }
+                }
+            }
+            ) << "Test of assignment to an irregular array via operator[] failed with an exception.";
+
+    debug << "test_arr1: " << endl << test_arr1 << endl;
+}
+
+
+} // namespace
