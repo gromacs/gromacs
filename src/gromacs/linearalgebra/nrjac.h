a6f5d7f38ead25f505f979eedf9078860fe60a67
Berk Hess <hess@kth.se>
2017-07-26 23:09:36 +0200

    Clean up matrix inversion
    This change is mainly a conversion of the orientation restraints
    matrix storage from an array of pointers to a real [5][5] struct.
    This requires a small change to the jacobi code which also affects
    gmx_traj (in a positive way).
    
    Change-Id: I8a960b659e8da847d1e505844535bd6fbc984814

--- src/gromacs/linearalgebra/nrjac.h
+++ src/gromacs/linearalgebra/nrjac.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2010,2014, by the GROMACS development team, led by
+ * Copyright (c) 2010,2014,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -53,8 +53,8 @@ void jacobi(double **a, int n, double d[], double **v, int *nrot);
  * int      *irot = number of jacobi rotations
  */
 
-int m_inv_gen(real **m, int n, real **minv);
-/* Produces minv, a generalized inverse of m.
+int m_inv_gen(real *m, int n, real *minv);
+/* Produces minv, a generalized inverse of m, both stored as linear arrays.
  * Inversion is done via diagonalization,
  * eigenvalues smaller than 1e-6 times the average diagonal element
  * are assumed to be zero.
