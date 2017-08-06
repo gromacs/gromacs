360eb61314abefcf29658864eb99f3b670cadeb6
Mark Abraham <mark.j.abraham@gmail.com>
2017-08-06 13:59:04 +0200

    Replace consistency checking with assertion
    The checks intended for checking that the automatic choice of number
    of thread-MPI ranks have never worked, because we over-write the
    user's choice of hw_opt->nthreads_tmpi. Fixed that by handling those
    sub-cases properly by requiring that the number of GPU IDs supplied
    matches the number of ranks chosen in all the cases, and asserting on
    that.
    
    Even if we would fix the over-writing issue, all of the former
    consistency checks amount to an assertion that the previous code has
    chosen a number of GPU tasks on each node (in gpu_opt.dev_use) that
    matches the number of PP ranks on that node, which can be done in many
    fewer lines of code.
    
    Note that the checking did not have a SIMD-related behaviour, and
    since the way domains communicate is SIMD-agnostic, it's unclear that
    such a check would be useful.
    
    Change-Id: Iea85bb4c297c5ce60c8e25d95a87fe9d9bce6686

--- src/programs/mdrun/runner.cpp
+++ src/programs/mdrun/runner.cpp
@@ -841,7 +841,11 @@ int Mdrunner::mdrunner()
          */
         check_and_update_hw_opt_2(&hw_opt, inputrec->cutoff_scheme);
 
-        // Determine how many thread-MPI ranks to start.
+        /* Determine how many thread-MPI ranks to start.
+         *
+         * TODO Over-writing the user-supplied value here does
+         * prevent any possible subsequent checks from working
+         * correctly. */
         hw_opt.nthreads_tmpi = get_nthreads_mpi(hwinfo,
                                                 &hw_opt,
                                                 userGpuTaskAssignment,
@@ -1125,14 +1129,13 @@ int Mdrunner::mdrunner()
 
         /* NOTE: this print is only for and on one physical node */
         GMX_LOG(mdlog.warning).appendText(gpuUsageReport);
+
+        GMX_RELEASE_ASSERT(cr->nrank_pp_intranode == static_cast<int>(gpuTaskAssignment.size()),
+                           "The number of PP ranks on each node must equal the number of GPU tasks used on each node");
     }
 
-    /* check consistency across ranks of things like SIMD
-     * support and number of GPUs selected */
-    gmx_check_hw_runconf_consistency(mdlog, hwinfo, cr, hw_opt, !userGpuTaskAssignment.empty(), gpuTaskAssignment);
-    /* From now on, the userGpuTaskAssignment should never be used */
-
-    /* Prevent other ranks from continuing after an inconsistency was found.
+    /* Prevent other ranks from continuing after an issue was found
+     * and reported as a fatal error.
      *
      * TODO This function implements a barrier so that MPI runtimes
      * can organize an orderly shutdown if one of the ranks has had to
