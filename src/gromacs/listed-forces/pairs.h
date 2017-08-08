8d6f58ed4c07d03a81f0640685fcecea43f450f4
Berk Hess <hess@kth.se>
2017-08-08 15:19:09 +0200

    Avoid bonded forces loops/timers on inputs with no bondeds
    This excludes near-zero "Bonded F" and "Listed buffer ops." subcounters
    on such inputs.
    Moved the bonded loop and thread parallelization to a separate function.
    Also made the lambda input array const.
    
    Change-Id: I862fc601e8adcf6d0b1eb7bd88390b0ea073e4fb

--- src/gromacs/listed-forces/pairs.h
+++ src/gromacs/listed-forces/pairs.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -63,7 +63,7 @@ void
 do_pairs(int ftype, int nbonds, const t_iatom iatoms[], const t_iparams iparams[],
          const rvec x[], rvec4 f[], rvec fshift[],
          const struct t_pbc *pbc, const struct t_graph *g,
-         real *lambda, real *dvdl, const t_mdatoms *md, const t_forcerec *fr,
+         const real *lambda, real *dvdl, const t_mdatoms *md, const t_forcerec *fr,
          gmx_bool bCalcEnergyAndVirial, gmx_grppairener_t *grppener,
          int *global_atom_index);
 
--- src/gromacs/listed-forces/position-restraints.h
+++ src/gromacs/listed-forces/position-restraints.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -72,7 +72,7 @@ posres_wrapper(t_nrnb             *nrnb,
                const struct t_pbc *pbc,
                const rvec          x[],
                gmx_enerdata_t     *enerd,
-               real               *lambda,
+               const real         *lambda,
                t_forcerec         *fr);
 
 /*! \brief Helper function that wraps calls to posres for free-energy
@@ -84,7 +84,7 @@ posres_wrapper_lambda(struct gmx_wallcycle *wcycle,
                       const struct t_pbc   *pbc,
                       const rvec            x[],
                       gmx_enerdata_t       *enerd,
-                      real                 *lambda,
+                      const real           *lambda,
                       t_forcerec           *fr);
 
 /*! \brief Helper function that wraps calls to fbposres for
