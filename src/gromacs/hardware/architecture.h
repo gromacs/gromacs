118b4c5422297a2e130cb14f117fc7c21b76bfbb
Berk Hess <hess@kth.se>
2017-08-25 22:25:18 +0200

    Consolidate architecture booleans
    The booleans used in the hardware module for identifying architecures
    are now defined in archtecture.h. The duplicate preprocessor code
    in cpuinfo.cpp now also uses these booleans instead.
    Note that hardwareinfo.cpp did not check for _M_AMD64, I don't know
    if this caused issues.
    
    Change-Id: I812482854d346c9290b0a428dada88072dc5a707

---
+++ src/gromacs/hardware/architecture.h
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Declares and defines architecture booleans to minimize preprocessed code
+ *
+ * \ingroup module_hardware
+ */
+#ifndef GMX_ARCHITECTURE_H
+#define GMX_ARCHITECTURE_H
+
+#if defined __i386__ || defined __x86_64__ || defined _M_IX86 || defined _M_X64 || defined _M_AMD64
+//! Constant that tells if the architecture is x86
+static constexpr bool architectureIsX86 = true;
+#else
+//! Constant used tells if the architecture is x86
+static constexpr bool architectureIsX86 = false;
+#endif
+
+#if defined __arm__ || defined __arm || defined _M_ARM || defined __aarch64_
+//! Constant that tells if the architecture is ARM
+static constexpr bool architectureIsArm = true;
+#else
+//! Constant that tells if the architecture is ARM
+static constexpr bool architectureIsArm = false;
+#endif
+
+#if defined __powerpc__ || defined __ppc__ || defined __PPC__
+//! Constant that tells if the architecture is PowerPC
+static constexpr bool architectureIsPowerPC = true;
+#else
+//! Constant that tells if the architecture is PowerPC
+static constexpr bool architectureIsPowerPC = false;
+#endif
+
+#endif // GMX_ARCHITECTURE_H
