650c32b128306eb0ce584dfa60be5be7f274ab8c
Berk Hess <hess@kth.se>
2017-08-25 22:25:18 +0200

    Consolidate architecture booleans
    The booleans used in the hardware module for identifying architecures
    are replaced by a single enum in archtecture.h. The duplicate
    preprocessor code in cpuinfo.cpp now also uses this enum instead.
    Note that hardwareinfo.cpp did not check for __i386, x86, __amd64__
    and _M_AMD64, I don't know if this caused issues.
    
    Also moved gpu_detec_res_str to new file gpu_hw_info.cpp.
    
    Change-Id: I812482854d346c9290b0a428dada88072dc5a707

---
+++ src/gromacs/hardware/architecture.h
@@ -0,0 +1,71 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Declares and defines architecture booleans to minimize preprocessed code
+ *
+ * \ingroup module_hardware
+ */
+#ifndef GMX_ARCHITECTURE_H
+#define GMX_ARCHITECTURE_H
+
+namespace gmx
+{
+
+//! Enum for GROMACS CPU hardware detection support
+enum class Architecture
+{
+    Unknown, //! Not one of the cases below
+    X86,     //! X86
+    Arm,     //! ARM
+    PowerPC  //! IBM PowerPC
+};
+
+//! Constant that tells what the architecture is
+static constexpr Architecture c_architecture =
+#if defined __i386__ || defined __i386 || defined _X86_ || defined _M_IX86 || \
+    defined __x86_64__ || defined __amd64__ || defined _M_X64 || defined _M_AMD64
+    Architecture::X86;
+#elif defined __arm__ || defined __arm || defined _M_ARM || defined __aarch64_
+    Architecture::Arm;
+#elif defined __powerpc__ || defined __ppc__ || defined __PPC__
+    Architecture::PowerPC;
+#else
+    Architecture::Unknown;
+#endif
+
+}      // namespace gmx
+
+#endif // GMX_ARCHITECTURE_H
---
+++ src/gromacs/hardware/gpu_hw_info.cpp
@@ -0,0 +1,51 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#include "gmxpre.h"
+
+#include "gpu_hw_info.h"
+
+#include "config.h"
+
+/* Note that some of the following arrays must match the "GPU support
+ * enumeration" in src/config.h.cmakein, so that GMX_GPU looks up an
+ * array entry. */
+
+// TODO If/when we unify CUDA and OpenCL support code, this should
+// move to a single place in gpu_utils.
+/* Names of the GPU detection/check results (see e_gpu_detect_res_t in hw_info.h). */
+const char * const gpu_detect_res_str[egpuNR] =
+{
+    "compatible", "inexistent", "incompatible", "insane"
+};
