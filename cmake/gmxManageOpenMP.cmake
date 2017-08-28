51fdbd881ff9a23743677a94405a63db513bd105
Berk Hess <hess@kth.se>
2017-08-28 17:28:18 +0200

    Set default GMX_OPENMP_MAX_THREADS to 64
    As there are many new CPU with more than 32 hardware threads and
    GROMACS scales quite well to more than 32 threads,
    GMX_OPENMP_MAX_THREADS is increased from 32 to 64 threads.
    The performance impact of this is that bitmasks are by default
    64-bit instead of 32-but integers, which on 64-bit systems should
    only have a (negligible) effect on cache pressure.
    
    Change-Id: I73d1c79e86f30f7fc69e1f49e1195271435e77b6

--- cmake/gmxManageOpenMP.cmake
+++ cmake/gmxManageOpenMP.cmake
@@ -69,7 +69,7 @@ if(GMX_OPENMP)
 endif()
 gmx_dependent_cache_variable(GMX_OPENMP_MAX_THREADS
     "Maximum number of OpenMP Threads supported. Has to be 32 or a multiple of 64."
-    STRING 32 GMX_OPENMP)
+    STRING 64 GMX_OPENMP)
 mark_as_advanced(GMX_OPENMP_MAX_THREADS)
 math(EXPR MAX_THREAD_MOD "${GMX_OPENMP_MAX_THREADS} % 64")
 if (NOT GMX_OPENMP_MAX_THREADS EQUAL 32 AND NOT ${MAX_THREAD_MOD} EQUAL 0)
