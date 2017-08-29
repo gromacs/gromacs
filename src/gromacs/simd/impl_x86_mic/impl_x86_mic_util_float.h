b7cdd0080f43d3221d2027d0c2598e2c0c2a1735
Roland Schulz <roland.schulz@intel.com>
2017-08-29 14:11:01 -0700

    Fix accuracy for cvtDouble2Float/cprod
    After commit b905792 gave incorrect results with ICC17u4
    
    Change-Id: Ib945e979e9e3144342eed7a5cb4fc56d7f9a4b88

--- src/gromacs/simd/impl_x86_mic/impl_x86_mic_util_float.h
+++ src/gromacs/simd/impl_x86_mic/impl_x86_mic_util_float.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -205,7 +205,7 @@ transposeScatterStoreU(float *              base,
 {
     SimdFInt32 simdoffset;
 
-    assert(sdt::size_t(offset) % 64 == 0);
+    assert(std::size_t(offset) % 64 == 0);
 
     simdoffset = simdLoadFI(offset);
 
