ffc9d0183d9c989aa61fd0570bb37f6239c0f04c
Mark Abraham <mark.j.abraham@gmail.com>
2017-09-03 15:48:10 +0200

    Fix compiler flags for using MKL
    Change-Id: I860e43da8de3b563167ecee1f52e2017a0f27f7f

--- cmake/gmxManageFFTLibraries.cmake
+++ cmake/gmxManageFFTLibraries.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012,2013,2014,2015,2016, by the GROMACS development team, led by
+# Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -120,6 +120,7 @@ elseif(${GMX_FFT_LIBRARY} STREQUAL "MKL")
         # Some versions of icc require this in order that mkl.h can be
         # found at compile time.
         set(EXTRA_C_FLAGS "${EXTRA_C_FLAGS} ${FFT_LINKER_FLAGS}")
+        set(EXTRA_CXX_FLAGS "${EXTRA_CXX_FLAGS} ${FFT_LINKER_FLAGS}")
 
         set(MKL_ERROR_MESSAGE "Make sure you have configured your compiler so that ${FFT_LINKER_FLAGS} will work.")
     else()
