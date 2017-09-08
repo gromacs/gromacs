b6c89ce6aac3244c369384d0491214cfcb209058
Berk Hess <hess@kth.se>
2017-09-08 14:49:40 +0200

    Fix PBC bugs in the swap code
    Fixes #2245
    
    Change-Id: I90e2bed71a2499c63794e420dca383d91e6fc86c

--- src/gromacs/swap/swapcoords.cpp
+++ src/gromacs/swap/swapcoords.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1471,8 +1471,6 @@ void init_swapcoords(
     swapstateIons_t       *gs;
     gmx_bool               bAppend, bStartFromCpt, bRerun;
     gmx_mtop_atomlookup_t  alook = NULL;
-    matrix                 boxCopy;
-
 
     alook = gmx_mtop_atomlookup_init(mtop);
 
@@ -1621,14 +1619,6 @@ void init_swapcoords(
     /* Make a t_pbc struct on all nodes so that the molecules
      * chosen for an exchange can be made whole. */
     snew(s->pbc, 1);
-    /* Every node needs to call set_pbc() and therefore every node needs
-     * to know the box dimensions */
-    copy_mat(box, boxCopy);
-    if (PAR(cr))
-    {
-        gmx_bcast(sizeof(boxCopy), boxCopy, cr);
-    }
-    set_pbc(s->pbc, -1, boxCopy);
 
     if (MASTER(cr))
     {
@@ -2011,6 +2001,7 @@ gmx_bool do_swapcoords(
     sc  = ir->swap;
     s   = sc->si_priv;
 
+    set_pbc(s->pbc, ir->ePBC, box);
 
     /* Assemble the positions of the split groups, i.e. the channels.
      * Here we also pass a shifts array to communicate_group_positions(), so that it can make
