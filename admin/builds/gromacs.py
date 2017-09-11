2496822de6ab58ffbdc696980a49b8c33d0b7471
Mark Abraham <mark.j.abraham@gmail.com>
2017-09-11 18:55:00 +0200

    Express intent of matrix release build types explicitly
    We intend to do primary testing in Jenkins with all assertions
    enabled, so that we maxmize our opportunities for useful feedback. We
    do so in both Debug and Release flavours, so that we have confidence
    that those different builds will remain maximally useful.
    
    Expressing that we are building the Release+assert type explicitly
    will help developers understand when and why they get feedback from
    Jenkins.
    
    This also permits us to build without assertions in a convenient
    way. This is useful for e.g. checking that such builds remain free of
    warnings, which is a useful corner case to have changed, since a
    variable or parameter might only be used in an assertion expression.
    This now happens in one post-submit and all release matrix
    configurations.
    
    Updated the release-matrix contents and documentation to be more
    consistent with that of the other matrices.
    
    Noted TODO for adding icc 17 testing.
    
    Change-Id: I774602e361163337e9602f497e2302148ccbf544

--- admin/builds/gromacs.py
+++ admin/builds/gromacs.py
@@ -41,6 +41,7 @@ extra_options = {
     'static': Option.simple,
     'reference': Option.simple,
     'release': Option.simple,
+    'release-with-assert': Option.simple,
     'release-with-debug-info': Option.simple,
     'asan': Option.simple,
     'mkl': Option.simple,
@@ -66,7 +67,9 @@ def do_build(context):
 
     if context.opts.reference:
         cmake_opts['CMAKE_BUILD_TYPE'] = 'Reference'
-    elif context.opts.release:
+    elif context.opts['release']:
+        cmake_opts['CMAKE_BUILD_TYPE'] = 'Release'
+    elif context.opts['release-with-assert']:
         cmake_opts['CMAKE_BUILD_TYPE'] = 'RelWithAssert'
     elif context.opts['release-with-debug-info']:
         cmake_opts['CMAKE_BUILD_TYPE'] = 'RelWithDebInfo'
