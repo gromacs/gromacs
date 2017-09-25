7bd13d1dc36c7abf989eef6439741506147e50ef
Mark Abraham <mark.j.abraham@gmail.com>
2017-09-25 18:59:19 +0200

    Introduce HostAllocationPolicy
    This permits host-side standard containers and smart pointers to have
    their contents placed in memory suitable for efficient GPU transfer.
    
    The behaviour can be configured at run time during simulation setup,
    so that if we are not running on a GPU, then none of the buffers that
    might be affected actually are. The downside is that all such
    containers now have state.
    
    Change-Id: I9367d0f996de04c21312cef2081cc08148f80561

---
+++ src/gromacs/gpu_utils/tests/gputest.h
@@ -0,0 +1,72 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2017, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Declares test fixture testing GPU utility components.
+ *
+ * \author Mark Abraham <mark.j.abraham@gmail.com>
+ */
+#ifndef GMX_GPU_UTILS_TESTS_GPUTEST_H
+#define GMX_GPU_UTILS_TESTS_GPUTEST_H
+
+#include "gmxpre.h"
+
+#include <gtest/gtest.h>
+
+struct gmx_gpu_info_t;
+
+namespace gmx
+{
+namespace test
+{
+
+class GpuTest : public ::testing::Test
+{
+    public:
+        //! Information about GPUs that are present.
+        gmx_gpu_info_t *gpuInfo_;
+
+        //! Constructor
+        GpuTest();
+        //! Destructor
+        ~GpuTest();
+        //! Getter for convenience in testing
+        bool haveValidGpus() const;
+};
+
+} // namespace
+} // namespace
+
+#endif
