4e8b75b9eb480fcfd24b9698bdb04cca3d5adc8f
Szilárd Páll <pall.szilard@gmail.com>
2017-10-03 01:55:09 +0200

    Refactor scaled box handling for PME/Ewald
    The box scaling code for Ewald and related logic is now encapsulated in
    new box scaler class. This also allows box scaling moving box scaling
    it further down the call chain so that logic is reduced in do_force.
    
    Change-Id: Ic5071b825b9d36daca5f49d6f7c6c50261af1e1b

--- src/gromacs/ewald/ewald-utils.h
+++ src/gromacs/ewald/ewald-utils.h
@@ -34,19 +34,23 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the research papers on the package. Check out http://www.gromacs.org.
  */
-/*! \file
+/*! \libinternal \file
  *
- * \brief Declares functions for computing Ewald splitting coefficients
- *
- * These belong in the maths module because they do simple maths and
- * are used many parts of Gromacs.
+ * \brief Declares utility functions related to Ewald.
  *
  * \author Mark Abraham <mark.j.abraham@gmail.com>
- * \inpublicapi
+ * \author Szilárd Páll <pall.szilard@gmail.com>
+ *
+ * \ingroup module_ewald
  */
 #ifndef GMX_EWALD_UTILS_H
 #define GMX_EWALD_UTILS_H
 
+#include <assert.h>
+
+#include "gromacs/math/vec.h"
+#include "gromacs/mdtypes/inputrec.h"
+#include "gromacs/pbcutil/pbc.h"
 #include "gromacs/utility/real.h"
 
 #ifdef __cplusplus
@@ -86,5 +90,53 @@ calc_ewaldcoeff_lj(real rc, real rtol);
 #ifdef __cplusplus
 }
 #endif
+
+/*! \brief Class to handle box scaling for Ewald and PME.
+ *
+ * At construction contents of inputrec determine whether scaling is necessary
+ * and the scaleBox method returns an box scaled accordingly.
+ */
+class EwaldBoxZScaler
+{
+    bool scaleWithWalls_ = false; /*! True if the simulation uses two walls and the box needs to be scaled in PME */
+    real scalingFactor_  = 1.0;   /*! Box The scaling factor PME uses with walls */
+
+    public:
+        EwaldBoxZScaler() = delete;
+
+        /*! \brief Constructor that takes the input record to initialize Ewald box scaling appropriately. */
+        EwaldBoxZScaler(const t_inputrec *ir)
+        {
+            assert(ir);
+            if (inputrecPbcXY2Walls(ir))
+            {
+                scaleWithWalls_ = true;
+                scalingFactor_  = ir->wall_ewald_zfac;
+            }
+        }
+
+        /*! \brief Copy and scale the box for PME.
+         *
+         * When PME is used with 2D periodicity and two walls, the
+         * copy of the \p box passed is scaled with the Z scaling factor.
+         *
+         * \param[in] box        The current box matrix
+         * \param[out] scaledBox Scaled copy of the box matrix.
+         */
+        void scaleBox(const matrix box,
+                      matrix       scaledBox)
+        {
+            assert(box);
+            assert(*box);
+            assert(scaledBox);
+            assert(*scaledBox);
+
+            copy_mat(box, scaledBox);
+            if (scaleWithWalls_)
+            {
+                svmul(scalingFactor_, scaledBox[ZZ], scaledBox[ZZ]);
+            }
+        }
+};
 
 #endif
