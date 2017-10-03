d327b0b20e7e11606e53e7d35a82da155a9d4c48
Szilárd Páll <pall.szilard@gmail.com>
2017-10-03 16:04:46 +0200

    Move Ewald splitting-related code to ewald-utils
    This code used to be under gromacs/math, but it is an Ewald-only
    functionality, so it belongs to the Ewald module.
    
    Change-Id: Ib2d7def243a88b09f328a8ea1bafe27dd7a783d5

--- src/gromacs/ewald/ewald-utils.cpp
+++ src/gromacs/ewald/ewald-utils.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -36,7 +36,7 @@
  */
 #include "gmxpre.h"
 
-#include "calculate-ewald-splitting-coefficient.h"
+#include "ewald-utils.h"
 
 #include <cmath>
 
