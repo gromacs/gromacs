7fbefb356f72fa7ef3e0942e703ee35cb637cf85
Aleksei Iupinov <a.yupinov@gmail.com>
2017-10-03 16:43:07 +0200

    Bring CUDA texture getters documentation up-to-date
    Change-Id: Id2b588129287a0e2372fd3564f8a24513ae89e0e

--- src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda.h
+++ src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda.h
@@ -43,11 +43,26 @@
 
 //! Set up the cache configuration for the non-bonded kernels.
 void nbnxn_cuda_set_cacheconfig(const gmx_device_info_t *devinfo);
-//! Return the reference to the nbfp texture.
+/*! \brief Return the reference to the nbfp texture.
+ *
+ *  Note: it can return junk when c_disableCudaTextures==false, but we don't
+ *  assert on that condition because the data_mgmt module ends up calling this
+ *  function even if texture references are not used.
+ */
 const struct texture<float, 1, cudaReadModeElementType> &nbnxn_cuda_get_nbfp_texref();
-//! Return the reference to the nbfp_comb texture.
+/*! \brief Return the reference to the nbfp_comb texture.
+ *
+ *  Note: it can return junk when c_disableCudaTextures==false, but we don't
+ *  assert on that condition because the data_mgmt module ends up calling this
+ *  function even if texture references are not used.
+ */
 const struct texture<float, 1, cudaReadModeElementType> &nbnxn_cuda_get_nbfp_comb_texref();
-//! Return the reference to the coulomb_tab.
+/*! \brief Return the reference to the coulomb_tab texture.
+ *
+ *  Note: it can return junk when c_disableCudaTextures==false, but we don't
+ *  assert on that condition because the data_mgmt module ends up calling this
+ *  function even if texture references are not used.
+ */
 const struct texture<float, 1, cudaReadModeElementType> &nbnxn_cuda_get_coulomb_tab_texref();
 
 #endif
