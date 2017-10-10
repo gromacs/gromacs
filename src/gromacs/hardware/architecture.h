bc32432c70878014b0f16a8e4b85bb2eea87c28c
Mark Abraham <mark.j.abraham@gmail.com>
2017-10-10 09:22:02 +0000

    Fix cpuinfo on clang + non-x86
    Compilers that pretend to be GCC often define such symbols, and the
    support for inline assembly does not compile e.g. on ARM. This broke
    CPU detection at cmake time, and subsequent compilation. Probably
    introduced by commit 863768a4dad. The latest ARM compiler is based on
    clang, so we should fix this.
    
    Change-Id: Ia21363b9c0fe112762750d93b9feea267a34319f

--- src/gromacs/hardware/architecture.h
+++ src/gromacs/hardware/architecture.h
@@ -53,10 +53,12 @@ enum class Architecture
     PowerPC  //! IBM PowerPC
 };
 
+#define GMX_IS_X86_32 (defined __i386__ || defined __i386 || defined _X86_ || defined _M_IX86)
+#define GMX_IS_X86_64 (defined __x86_64__ || defined __x86_64 || defined __amd64__ || defined __amd64 || defined _M_X64 || defined _M_AMD64)
+
 //! Constant that tells what the architecture is
 static constexpr Architecture c_architecture =
-#if defined __i386__ || defined __i386 || defined _X86_ || defined _M_IX86 || \
-    defined __x86_64__ || defined __amd64__ || defined _M_X64 || defined _M_AMD64
+#if GMX_IS_X86_32 || GMX_IS_X86_64
     Architecture::X86;
 #elif defined __arm__ || defined __arm || defined _M_ARM || defined __aarch64_
     Architecture::Arm;
