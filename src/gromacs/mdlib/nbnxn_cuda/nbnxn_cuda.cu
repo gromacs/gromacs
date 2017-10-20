86eb171f8bb63940c9d3452f7295ae3acd2d2a27
Szilárd Páll <pall.szilard@gmail.com>
2017-10-20 19:52:13 +0200

    Split off nbnxn GPU timing and staging reduction
    Code reorganization that moves the timing related functions as well as
    energy and shift force reduction into separate functions in both CUDA
    and OpenCL versions of nbnxn_gpu_wait_for_gpu().
    
    Change-Id: Ic5c9694d9de7f80a772e97f5c9e05bab77a3b82a

--- src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda.cu
+++ src/gromacs/mdlib/nbnxn_cuda/nbnxn_cuda.cu
@@ -771,26 +771,107 @@ static void countPruneKernelTime(cu_timers_t               *timers,
     }
 }
 
+/*! \brief Count pruning kernel time if either kernel has been triggered */
+static void nbnxn_gpu_reduce_staged_outputs(nb_staging   nbst,
+                                            int          iLocality,
+                                            bool         reduceEnergies,
+                                            bool         reduceFshift,
+                                            real        *e_lj,
+                                            real        *e_el,
+                                            rvec        *fshift)
+{
+    /* turn energy calculation always on/off (for debugging/testing only) */
+    reduceEnergies = (reduceEnergies || always_ener) && !never_ener;
+
+    /* add up energies and shift forces (only once at local F wait) */
+    if (LOCAL_I(iLocality))
+    {
+        if (reduceEnergies)
+        {
+            *e_lj += *nbst.e_lj;
+            *e_el += *nbst.e_el;
+        }
+
+        if (reduceFshift)
+        {
+            for (int i = 0; i < SHIFTS; i++)
+            {
+                fshift[i][0] += nbst.fshift[i].x;
+                fshift[i][1] += nbst.fshift[i].y;
+                fshift[i][2] += nbst.fshift[i].z;
+            }
+        }
+    }
+}
+
+/*! \brief Do the per-step timing accounting of the nonbonded tasks. */
+static void nbnxn_gpu_accumulate_timings(struct gmx_wallclock_gpu_nbnxn_t *timings,
+                                         cu_timers_t                      *timers,
+                                         const cu_plist_t                 *plist,
+                                         int                               atomLocality,
+                                         bool                              didEnergyKernels,
+                                         bool                              doTiming)
+{
+    /* timing data accumulation */
+    if (!doTiming)
+    {
+        return;
+    }
+
+    /* determine interaction locality from atom locality */
+    int iLocality = gpuAtomToInteractionLocality(atomLocality);
+
+    /* turn energy calculation always on/off (for debugging/testing only) */
+    didEnergyKernels = (didEnergyKernels || always_ener) && !never_ener;
+
+    /* only increase counter once (at local F wait) */
+    if (LOCAL_I(iLocality))
+    {
+        timings->nb_c++;
+        timings->ktime[plist->haveFreshList ? 1 : 0][didEnergyKernels ? 1 : 0].c += 1;
+    }
+
+    /* kernel timings */
+    timings->ktime[plist->haveFreshList ? 1 : 0][didEnergyKernels ? 1 : 0].t +=
+        timers->nb_k[iLocality].getLastRangeTime();
+
+    /* X/q H2D and F D2H timings */
+    timings->nb_h2d_t += timers->nb_h2d[iLocality].getLastRangeTime();
+    timings->nb_d2h_t += timers->nb_d2h[iLocality].getLastRangeTime();
+
+    /* Count the pruning kernel times for both cases:1st pass (at search step)
+       and rolling pruning (if called at the previous step).
+       We do the accounting here as this is the only sync point where we
+       know (without checking or additional sync-ing) that prune tasks in
+       in the current stream have completed (having just blocking-waited
+       for the force D2H). */
+    countPruneKernelTime(timers, timings, iLocality);
+
+    /* only count atdat and pair-list H2D at pair-search step */
+    if (timers->didPairlistH2D[iLocality])
+    {
+        /* atdat transfer timing (add only once, at local F wait) */
+        if (LOCAL_A(atomLocality))
+        {
+            timings->pl_h2d_c++;
+            timings->pl_h2d_t += timers->atdat.getLastRangeTime();
+        }
+
+        timings->pl_h2d_t += timers->pl_h2d[iLocality].getLastRangeTime();
+
+        /* Clear the timing flag for the next step */
+        timers->didPairlistH2D[iLocality] = false;
+    }
+
+
+}
+
 void nbnxn_gpu_wait_for_gpu(gmx_nbnxn_cuda_t *nb,
                             int flags, int aloc,
                             real *e_lj, real *e_el, rvec *fshift)
 {
-    /* NOTE:  only implemented for single-precision at this time */
-    cudaError_t stat;
-
     /* determine interaction locality from atom locality */
-    int iloc = gpuAtomToInteractionLocality(aloc);
-
-    cu_plist_t                       *plist    = nb->plist[iloc];
-    cu_timers_t                      *timers   = nb->timers;
-    struct gmx_wallclock_gpu_nbnxn_t *timings  = nb->timings;
-    nb_staging                        nbst     = nb->nbst;
-
-    bool                              bCalcEner   = flags & GMX_FORCE_ENERGY;
-    bool                              bCalcFshift = flags & GMX_FORCE_VIRIAL;
-
-    /* turn energy calculation always on/off (for debugging/testing only) */
-    bCalcEner = (bCalcEner || always_ener) && !never_ener;
+    int iLocality = gpuAtomToInteractionLocality(aloc);
 
     /* Launch wait/update timers & counters and do reduction into staging buffers
        BUT skip it when during the non-local phase there was actually no work to do.
@@ -799,79 +880,24 @@ void nbnxn_gpu_wait_for_gpu(gmx_nbnxn_cuda_t *nb,
        NOTE: if timing with multiple GPUs (streams) becomes possible, the
        counters could end up being inconsistent due to not being incremented
        on some of the nodes! */
-    if (!canSkipWork(nb, iloc))
+    if (!canSkipWork(nb, iLocality))
     {
-        stat = cudaStreamSynchronize(nb->stream[iloc]);
+        cudaError_t stat = cudaStreamSynchronize(nb->stream[iLocality]);
         CU_RET_ERR(stat, "cudaStreamSynchronize failed in cu_blockwait_nb");
 
-        /* timing data accumulation */
-        if (nb->bDoTime)
-        {
-            /* only increase counter once (at local F wait) */
-            if (LOCAL_I(iloc))
-            {
-                timings->nb_c++;
-                timings->ktime[plist->haveFreshList ? 1 : 0][bCalcEner ? 1 : 0].c += 1;
-            }
+        bool calcEner   = flags & GMX_FORCE_ENERGY;
+        bool calcFshift = flags & GMX_FORCE_VIRIAL;
 
-            /* kernel timings */
-            timings->ktime[plist->haveFreshList ? 1 : 0][bCalcEner ? 1 : 0].t += timers->nb_k[iloc].getLastRangeTime();
+        nbnxn_gpu_accumulate_timings(nb->timings, nb->timers, nb->plist[iLocality], aloc, calcEner, nb->bDoTime);
 
-            /* X/q H2D and F D2H timings */
-            timings->nb_h2d_t += timers->nb_h2d[iloc].getLastRangeTime();
-            timings->nb_d2h_t += timers->nb_d2h[iloc].getLastRangeTime();
-
-            /* Count the pruning kernel times for both cases:1st pass (at search step)
-               and rolling pruning (if called at the previous step).
-               We do the accounting here as this is the only sync point where we
-               know (without checking or additional sync-ing) that prune tasks in
-               in the current stream have completed (having just blocking-waited
-               for the force D2H). */
-            countPruneKernelTime(timers, timings, iloc);
-
-            /* only count atdat and pair-list H2D at pair-search step */
-            if (timers->didPairlistH2D[iloc])
-            {
-                /* atdat transfer timing (add only once, at local F wait) */
-                if (LOCAL_A(aloc))
-                {
-                    timings->pl_h2d_c++;
-                    timings->pl_h2d_t += timers->atdat.getLastRangeTime();
-                }
-
-                timings->pl_h2d_t += timers->pl_h2d[iloc].getLastRangeTime();
-
-                /* Clear the timing flag for the next step */
-                timers->didPairlistH2D[iloc] = false;
-            }
-        }
-
-        /* add up energies and shift forces (only once at local F wait) */
-        if (LOCAL_I(iloc))
-        {
-            if (bCalcEner)
-            {
-                *e_lj += *nbst.e_lj;
-                *e_el += *nbst.e_el;
-            }
-
-            if (bCalcFshift)
-            {
-                for (int i = 0; i < SHIFTS; i++)
-                {
-                    fshift[i][0] += nbst.fshift[i].x;
-                    fshift[i][1] += nbst.fshift[i].y;
-                    fshift[i][2] += nbst.fshift[i].z;
-                }
-            }
-        }
+        nbnxn_gpu_reduce_staged_outputs(nb->nbst, iLocality, calcEner, calcFshift, e_lj, e_el, fshift);
     }
 
     /* Always reset both pruning flags (doesn't hurt doing it even when timing is off). */
-    timers->didPrune[iloc] = timers->didRollingPrune[iloc] = false;
+    nb->timers->didPrune[iLocality] = nb->timers->didRollingPrune[iLocality] = false;
 
     /* Turn off initial list pruning (doesn't hurt if this is not pair-search step). */
-    plist->haveFreshList = false;
+    nb->plist[iLocality]->haveFreshList = false;
 }
 
 const struct texture<float, 1, cudaReadModeElementType> &nbnxn_cuda_get_nbfp_texref()
