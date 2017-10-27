f8c216bead8ff6aa262bd14b21203c37acf8c021
Berk Hess <hess@kth.se>
2017-10-27 08:55:16 +0200

    [WIP] Add awh-init option and test
    Change-Id: I4c041ee3aaea26b349d21e705bb09efa22583ea2

--- src/gromacs/awh/tests/bias.cpp
+++ src/gromacs/awh/tests/bias.cpp
@@ -59,7 +59,9 @@ namespace gmx
 namespace test
 {
 
-//! Struct that gathers all input for setting up and using a Bias
+/*! \internal \brief
+ * Struct that gathers all input for setting up and using a Bias
+ */
 struct AwhTestParameters
 {
     double                 beta;          //!< 1/(kB*T)
@@ -96,10 +98,10 @@ static AwhTestParameters getAwhTestParameters(int eawhgrowth,
     awhBiasParams.targetBetaScaling    = 0;
     awhBiasParams.targetCutoff         = 0;
     awhBiasParams.eGrowth              = eawhgrowth;
-    awhBiasParams.bUserData            = 0;
+    awhBiasParams.bUserData            = FALSE;
     awhBiasParams.errorInitial         = 0.5;
-    awhBiasParams.bShare               = false;
-    awhBiasParams.equilibrateHistogram = false;
+    awhBiasParams.bShare               = FALSE;
+    awhBiasParams.equilibrateHistogram = FALSE;
 
     double                 convFactor  = 1;
     double                 k           = 1000;
@@ -198,7 +200,7 @@ class BiasTest : public ::testing::TestWithParam<BiasTestParameters>
             int    numSamples = coordinates_.size() - 1; // No sample taken at step 0
             GMX_RELEASE_ASSERT(numSamples % params.awhParams.numSamplesUpdateFreeEnergy == 0, "This test is intended to reproduce the situation when the might need to write output during a normal AWH run, therefore the number of samples should be a multiple of the free-energy update interval (but the test should also runs fine without this condition).");
 
-            bias_ = std::unique_ptr<Bias>(new Bias(nullptr, -1, params.awhParams, params.awhBiasParams, params.dimParams, params.beta, mdTimeStep, disableUpdateSkips));
+            bias_ = std::unique_ptr<Bias>(new Bias(nullptr, -1, params.awhParams, params.awhBiasParams, params.dimParams, params.beta, mdTimeStep, "", disableUpdateSkips));
         }
 };
 
@@ -291,7 +293,7 @@ TEST(BiasTest, DetectsCovering)
 
     const double            mdTimeStep   = 0.1;
 
-    Bias                    bias(nullptr, -1, params.awhParams, params.awhBiasParams, params.dimParams, params.beta, mdTimeStep);
+    Bias                    bias(nullptr, -1, params.awhParams, params.awhBiasParams, params.dimParams, params.beta, mdTimeStep, "");
 
     /* We use a trajectory of the sum of two sines to cover the reaction
      * coordinate range in a semi-realistic way. The period is 4*pi=12.57.
