a2fdc89ec63bbcd26f0db083deadbeb09c51a89d
mohammad-ghahremanpour <ghahremanpour.gro@gmail.com>
2017-11-13 16:28:08 +0100

    poldata_tables
    Change-Id: I67705afc12ff2862f58c7caaceff7be671a0c907

--- src/programs/alexandria/poldata_tables.cpp
+++ src/programs/alexandria/poldata_tables.cpp
@@ -64,7 +64,7 @@ static void eemprops_zeta_header(LongTable &lt)
 
     lt.setColumns("ccccc");
 
-    snprintf(longbuf, STRLEN, "The optimized values of the screening factor for the Gaussian and the Slater charges represented by $\\beta$ and $\\xi$, rescpectively.");
+    snprintf(longbuf, STRLEN, "The optimized values of the screening factor for the Gaussian and the Slater charges represented by $\\beta$ and $\\xi$, rescpectively. The atom types are according to the General Amber Force Field (GAFF)~\\cite{Wang2004a}.");
     lt.setCaption(longbuf);
     lt.setLabel("screeningfactor");
     snprintf(longbuf, STRLEN, "Atom type  & \\multicolumn{2}{c}{Non-polarizable} & \\multicolumn{2}{c}{Polarizable}");
