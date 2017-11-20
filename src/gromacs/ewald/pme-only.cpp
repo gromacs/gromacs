b6c5c157c3793f0b3b25dc7b07a0d6231f8be7ab
Aleksei Iupinov <a.yupinov@gmail.com>
2017-11-20 18:14:30 +0100

    Pin coordinates, forces and charges for PME on GPU
    This pins almost all the required buffers for both PME-only and PP+PME
    ranks. The only non-pinned buffer is the FFT grid for not yet enabled
    mixed mode. ChargesA in md_atoms now check for PME actually being
    used on the rank to avoid the unpin assertion.
    
    Change-Id: Ia4c1b7673cc5c32c8d3d3fcf252d9d923b94128d

--- src/gromacs/ewald/pme-only.cpp
+++ src/gromacs/ewald/pme-only.cpp
@@ -79,6 +79,7 @@
 #include "gromacs/fileio/pdbio.h"
 #include "gromacs/gmxlib/network.h"
 #include "gromacs/gmxlib/nrnb.h"
+#include "gromacs/gpu_utils/hostallocator.h"
 #include "gromacs/math/gmxcomplex.h"
 #include "gromacs/math/units.h"
 #include "gromacs/math/vec.h"
@@ -111,15 +112,15 @@ struct gmx_pme_pp {
     int                  peerRankId;     /**< The peer PP rank id                  */
     //@{
     /**< Vectors of A- and B-state parameters used to transfer vectors to PME ranks  */
-    std::vector<real>      chargeA;
-    std::vector<real>      chargeB;
-    std::vector<real>      sqrt_c6A;
-    std::vector<real>      sqrt_c6B;
-    std::vector<real>      sigmaA;
-    std::vector<real>      sigmaB;
+    gmx::HostVector<real>      chargeA;
+    std::vector<real>          chargeB;
+    std::vector<real>          sqrt_c6A;
+    std::vector<real>          sqrt_c6B;
+    std::vector<real>          sigmaA;
+    std::vector<real>          sigmaB;
     //@}
-    std::vector<gmx::RVec> x;    /**< Vector of atom coordinates to transfer to PME ranks */
-    std::vector<gmx::RVec> f;    /**< Vector of atom forces received from PME ranks */
+    gmx::HostVector<gmx::RVec> x; /**< Vector of atom coordinates to transfer to PME ranks */
+    std::vector<gmx::RVec>     f; /**< Vector of atom forces received from PME ranks */
     //@{
     /**< Vectors of MPI objects used in non-blocking communication between multiple PP ranks per PME rank */
     std::vector<MPI_Request> req;
@@ -548,7 +549,14 @@ int gmx_pmeonly(struct gmx_pme_t *pme,
     std::vector<gmx_pme_t *> pmedata;
     pmedata.push_back(pme);
 
-    auto pme_pp = gmx_pme_pp_init(cr);
+    auto       pme_pp       = gmx_pme_pp_init(cr);
+    //TODO the variable below should be queried from the task assignment info
+    const bool useGpuForPme = (runMode == PmeRunMode::GPU) || (runMode == PmeRunMode::Hybrid);
+    if (useGpuForPme)
+    {
+        changePinningPolicy(&pme_pp->chargeA, gmx::PinningPolicy::CanBePinned);
+        changePinningPolicy(&pme_pp->x, gmx::PinningPolicy::CanBePinned);
+    }
 
     init_nrnb(mynrnb);
 
@@ -620,7 +628,7 @@ int gmx_pmeonly(struct gmx_pme_t *pme,
         // fewer lines of code and less parameter passing.
         const int pmeFlags = GMX_PME_DO_ALL_F | (bEnerVir ? GMX_PME_CALC_ENER_VIR : 0);
         gmx::ArrayRef<const gmx::RVec> forces;
-        if (runMode != PmeRunMode::CPU)
+        if (useGpuForPme)
         {
             const bool boxChanged = true;
             //TODO this should be set properly by gmx_pme_recv_coeffs_coords,
