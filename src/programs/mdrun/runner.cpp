b27c46587bf9876a7a386c378925272d0932548e
Aleksei Iupinov <a.yupinov@gmail.com>
2017-11-21 19:08:27 +0100

    Manually free globalState and mdAtoms to avoid unpin assertion
    Change-Id: I512c69d5cf4df8c94238e9bc7cc9aade96142d82

--- src/programs/mdrun/runner.cpp
+++ src/programs/mdrun/runner.cpp
@@ -1216,6 +1216,12 @@ int Mdrunner::mdrunner()
         pmedata = nullptr;
     }
 
+    // FIXME: this is only here to manually unpin mdAtoms->chargeA_ and state->x,
+    // before we destroy the GPU context(s) in free_gpu_resources().
+    // Pinned buffers are associated with contexts in CUDA.
+    mdAtoms.reset(nullptr);
+    globalState.reset(nullptr);
+
     /* Free GPU memory and context */
     free_gpu_resources(fr, cr, shortRangedDeviceInfo);
 
