61165cdbf81a00f2be3f6e0dc2100fc3c96ff861
Berk Hess <hess@kth.se>
2017-11-25 11:22:55 +0100

    [WIP] Fix thread affinity tests
    Change-Id: I2d2454512d9ed2f2bdfd815ce5ae685507cb092d

--- src/gromacs/utility/basenetwork.cpp
+++ src/gromacs/utility/basenetwork.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -200,11 +200,14 @@ int gmx_physicalnode_id_hash(void)
 {
     int hash;
 
-#if GMX_MPI
-    hash = mpi_hostname_hash();
-#else
-    hash = 0;
-#endif
+    if (GMX_MPI)
+    {
+        hash = mpi_hostname_hash();
+    }
+    else
+    {
+        hash = 0;
+    }
 
     if (debug)
     {
