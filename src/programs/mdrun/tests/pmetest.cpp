176e63d42e1444fa3c8354a3bdb1a0a525c0e467
Aleksei Iupinov <a.yupinov@gmail.com>
2017-11-27 10:50:23 +0100

    [WIP] Add tests for PME tuning
    FIXME: the test for single rank mixed mode is expected to fail.
    Either fix the issue or disable the single rank mixed mode.
    
    Change-Id: Id9acd3d9b01b585d772b2009dcea9a9e0b8b0539

--- src/programs/mdrun/tests/pmetest.cpp
+++ src/programs/mdrun/tests/pmetest.cpp
@@ -133,6 +133,10 @@ TEST_F(PmeTest, ReproducesEnergies)
     runModes["PmeOnGpuFftOnCpu"] = {"-pme", "gpu", "-pmefft", "cpu"};
     runModes["PmeOnGpuFftOnGpu"] = {"-pme", "gpu", "-pmefft", "gpu"};
     runModes["PmeOnGpuFftAuto"]  = {"-pme", "gpu", "-pmefft", "auto"};
+    // manual modes but marked for PME tuning
+    runModes["PmeOnCpuTune"]         = {"-pme", "cpu"};
+    runModes["PmeOnGpuFftOnCpuTune"] = {"-pme", "gpu", "-pmefft", "cpu"};
+    runModes["PmeOnGpuFftOnGpuTune"] = {"-pme", "gpu", "-pmefft", "gpu"};
     TestReferenceData    refData;
     TestReferenceChecker rootChecker(refData.rootChecker());
     const bool           thisRankChecks = (gmx_node_rank() == 0);
@@ -154,7 +158,17 @@ TEST_F(PmeTest, ReproducesEnergies)
         runner_.edrFileName_ = fileManager_.getTemporaryFilePath(inputFile + "_" + mode.first + ".edr");
 
         CommandLine commandLine(mode.second);
-        commandLine.append("-notunepme"); // for reciprocal energy reproducibility
+
+        const bool  usePmeTuning = (mode.first.find("Tune") != std::string::npos);
+        if (usePmeTuning)
+        {
+            commandLine.append("-tunepme");
+            commandLine.addOption("-nstlist", nsteps / 10); // to force a few grid switches
+        }
+        else
+        {
+            commandLine.append("-notunepme"); // for reciprocal energy reproducibility
+        }
         if (useSeparatePme)
         {
             commandLine.addOption("-npme", 1);
@@ -182,7 +196,10 @@ TEST_F(PmeTest, ReproducesEnergies)
                     conservedChecker.setDefaultTolerance(conservedTolerance);
                 }
                 conservedChecker.checkReal(conservedEnergy, stepNum.c_str());
-                reciprocalChecker.checkReal(reciprocalEnergy, stepNum.c_str());
+                if (!usePmeTuning)
+                {
+                    reciprocalChecker.checkReal(reciprocalEnergy, stepNum.c_str());
+                }
             }
         }
         // FIXME: without this barrier, one of the mdruns was somehow having a non-PME inputrec (!)
