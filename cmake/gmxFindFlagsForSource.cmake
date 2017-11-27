gromacs/gromacs

commit c4cd99684a103291ba97811b953e82cf0eb38f49

Separate SIMD flag detection from management
This provides separate cmake functions to detect the flags
required to compile each SIMD instruction set. These
functions do not automatically add the SIMD flags to the
default flags, in order to make it possible to detect flags
for many SIMD instruction sets and use different flags for
different files.

Change-Id: I469a0cf0fafe3793d14dbcf0e72fd18f44a3bfe8

cmake/gmxFindFlagsForSource.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2013,2014,2016, by the GROMACS development team, led by
+# Copyright (c) 2013,2014,2016,2017, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -63,15 +63,15 @@ endfunction()
 # SOURCE                    Source code to test
 # LANGUAGE                  Specifies the language as "C" or "CXX"
 # TOOLCHAIN_FLAGS_VARIABLE  Name of a variable that contains any flags already known
-#                           to be needed by the toolchain, to which the first
-#                           working flag will be appended.
-# Args 5 through N          Multiple strings with compiler flags to test
+#                           to be needed by the toolchain (unchanged)
+# NEW_FLAGS_VARIABLE        The first working flag will be appended to this variable.
+# Args 6 through N          Multiple strings with compiler flags to test
 #
 # If gmx_check_compiler_flag() finds a working compiler flag, but the project in
 # gmx_check_source_compiles_with_flags() fails to build source code that needs,
 # the flag, this function sets SUGGEST_BINUTILS_UPDATE in the parent scope to
 # suggest that the calling code tell the user about this issue if needed.
-FUNCTION(GMX_FIND_FLAG_FOR_SOURCE RESULT_VARIABLE SOURCE LANGUAGE TOOLCHAIN_FLAGS_VARIABLE)
+FUNCTION(GMX_FIND_FLAG_FOR_SOURCE RESULT_VARIABLE SOURCE LANGUAGE TOOLCHAIN_FLAGS_VARIABLE NEW_FLAGS_VARIABLE)
     # Insert a blank element last in the list (ie. try without any flags too)
     # This must come last, since some compilers (Intel) might try to emulate
     # emulate AVX instructions with SSE4.1 otherwise.
@@ -100,7 +100,7 @@ FUNCTION(GMX_FIND_FLAG_FOR_SOURCE RESULT_VARIABLE SOURCE LANGUAGE TOOLCHAIN_FLAG
             endif()
             if (${FLAG_WORKS_VARIABLE} AND ${COMPILE_WORKS_VARIABLE})
                 set(${RESULT_VARIABLE} 1 PARENT_SCOPE)
-                set(${TOOLCHAIN_FLAGS_VARIABLE} "${${TOOLCHAIN_FLAGS_VARIABLE}} ${_testflag}" PARENT_SCOPE)
+                set(${NEW_FLAGS_VARIABLE} "${${NEW_FLAGS_VARIABLE}} ${_testflag}" PARENT_SCOPE)
                 break()
             endif()
         endif()
@@ -110,23 +110,23 @@ FUNCTION(GMX_FIND_FLAG_FOR_SOURCE RESULT_VARIABLE SOURCE LANGUAGE TOOLCHAIN_FLAG
 ENDFUNCTION()
 
 # Helper routine to find a flag (from a list) that will compile a specific source (in both C and C++).
+# C_RESULT_VARIABLE             Names a variable that will be set true if a way
+#                               to compile the source as C was found
+# CXX_RESULT_VARIABLE           Names a variable that will be set true if a way
+#                               to compile the source as C++ was found
 # SOURCE                        Source code to test
 # TOOLCHAIN_C_FLAGS_VARIABLE    As input, names a variable that contains flags needed
-#                               by the C toolchain, to which any necessary C compiler
-#                               flag needed to compile the source will be appended.
+#                               by the C toolchain.
 # TOOLCHAIN_CXX_FLAGS_VARIABLE  As input, names a variable that contains flags needed
-#                               by the C++ toolchain, to which any necessary C++ compiler
-#                               flag needed to compile the source will be appended.
-# C_FLAGS_VARIABLE              Names a variable that will be set true if a way
-#                               to compile the source as C was found
-# CXX_FLAGS_VARIABLE            Names a variable that will be set true if a way
-#                               to compile the source as C++ was found
-# Args 6 through N              Multiple strings with compiler flags to test
+#                               by the C++ toolchain.
+# NEW_C_FLAGS_VARIABLE          The first working C flag will be appended to this variable
+# NEW_CXX_FLAGS_VARIABLE        The first working C++ flag will be appended to this variable
+# Args 8 through N              Multiple strings with compiler flags to test
 #
 # If a compile flag is found, but the project in check_c/cxx_source_compiles
 # fails to build, sets SUGGEST_BINUTILS_UPDATE in parent scope to suggest
 # that the calling code tell the user about this issue if needed.
-macro(gmx_find_flags SOURCE TOOLCHAIN_C_FLAGS_VARIABLE TOOLCHAIN_CXX_FLAGS_VARIABLE C_FLAGS_VARIABLE CXX_FLAGS_VARIABLE)
-    gmx_find_flag_for_source(${C_FLAGS_VARIABLE} "${SOURCE}" "C" ${TOOLCHAIN_C_FLAGS_VARIABLE} ${ARGN})
-    gmx_find_flag_for_source(${CXX_FLAGS_VARIABLE} "${SOURCE}" "CXX" ${TOOLCHAIN_CXX_FLAGS_VARIABLE} ${ARGN})
+macro(gmx_find_flags C_RESULT_VARIABLE CXX_RESULT_VARIABLE SOURCE TOOLCHAIN_C_FLAGS_VARIABLE TOOLCHAIN_CXX_FLAGS_VARIABLE C_FLAGS_VARIABLE CXX_FLAGS_VARIABLE)
+    gmx_find_flag_for_source(${C_RESULT_VARIABLE} "${SOURCE}" "C" ${TOOLCHAIN_C_FLAGS_VARIABLE} ${C_FLAGS_VARIABLE} ${ARGN})
+    gmx_find_flag_for_source(${CXX_RESULT_VARIABLE} "${SOURCE}" "CXX" ${TOOLCHAIN_CXX_FLAGS_VARIABLE} ${CXX_FLAGS_VARIABLE} ${ARGN})
 endmacro()

commit 4456f8ac361335a1c00088f28a2435f2dd2d14ae

Fix bugs introduced in c4cd996
- Naming incorrect for gmx_find_simd_avx_512_knl_flags
  Causing KNL function not avaialable and SKL using wrong one
- Flags being appended every time to cache, causing endless loop with
  cmake 3.6.3 and ninja 1.7.2. Appending not necessary. Only case
  which is multi-step (AVX128/FMA) is anyhow appending manual.

Change-Id: I0d9ae1edae937fe871f0085894858746778ffe0f

cmake/gmxFindFlagsForSource.cmake
@@ -64,7 +64,7 @@ endfunction()
 # LANGUAGE                  Specifies the language as "C" or "CXX"
 # TOOLCHAIN_FLAGS_VARIABLE  Name of a variable that contains any flags already known
 #                           to be needed by the toolchain (unchanged)
-# NEW_FLAGS_VARIABLE        The first working flag will be appended to this variable.
+# NEW_FLAGS_VARIABLE        The first working flag will be set to this variable.
 # Args 6 through N          Multiple strings with compiler flags to test
 #
 # If gmx_check_compiler_flag() finds a working compiler flag, but the project in
@@ -100,7 +100,7 @@ FUNCTION(GMX_FIND_FLAG_FOR_SOURCE RESULT_VARIABLE SOURCE LANGUAGE TOOLCHAIN_FLAG
             endif()
             if (${FLAG_WORKS_VARIABLE} AND ${COMPILE_WORKS_VARIABLE})
                 set(${RESULT_VARIABLE} 1 PARENT_SCOPE)
-                set(${NEW_FLAGS_VARIABLE} "${${NEW_FLAGS_VARIABLE}} ${_testflag}" PARENT_SCOPE)
+                set(${NEW_FLAGS_VARIABLE} "${_testflag}" PARENT_SCOPE)
                 break()
             endif()
         endif()
@@ -119,8 +119,8 @@ ENDFUNCTION()
 #                               by the C toolchain.
 # TOOLCHAIN_CXX_FLAGS_VARIABLE  As input, names a variable that contains flags needed
 #                               by the C++ toolchain.
-# NEW_C_FLAGS_VARIABLE          The first working C flag will be appended to this variable
-# NEW_CXX_FLAGS_VARIABLE        The first working C++ flag will be appended to this variable
+# NEW_C_FLAGS_VARIABLE          The first working C flag will be set to this variable
+# NEW_CXX_FLAGS_VARIABLE        The first working C++ flag will be set to this variable
 # Args 8 through N              Multiple strings with compiler flags to test
 #
 # If a compile flag is found, but the project in check_c/cxx_source_compiles

cmake/gmxSimdFlags.cmake
@@ -276,7 +276,7 @@ endfunction()
 
 
 # AVX-512ER (KNL)
-function(gmx_find_simd_avx_512_flags C_FLAGS_RESULT CXX_FLAGS_RESULT C_FLAGS_VARIABLE CXX_FLAGS_VARIABLE)
+function(gmx_find_simd_avx_512_knl_flags C_FLAGS_RESULT CXX_FLAGS_RESULT C_FLAGS_VARIABLE CXX_FLAGS_VARIABLE)
     find_x86_toolchain_flags(TOOLCHAIN_C_FLAGS TOOLCHAIN_CXX_FLAGS)
 
     gmx_find_flags(SIMD_AVX_512_KNL_C_FLAGS_RESULT SIMD_AVX_512_KNL_CXX_FLAGS_RESULT