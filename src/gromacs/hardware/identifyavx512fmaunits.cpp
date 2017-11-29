df7c2315e802f10727e58597b05ed671a19b0a17
Roland Schulz <roland.schulz@intel.com>
2017-11-28 18:04:31 -0800

    Replace intrinsic with inline asm for AVX512 unit test
    Without high optimization, some compilers (icc) produce
    assembly that lead to lots of store-to-load forwarding
    during initialization, which screws up timing results.
    The modified code uses inline asm without loading from
    memory, which is fine since the inline (volatile) asm
    will not be optimized. Tested to work and detect 2 FMA
    units on Core i9-7920X and 1 FMA on Xeon Silver 4116,
    with with gcc-5.4, gcc-7.1, icc 2017 and clang-5 with
    optimization levels from -O0 to -O3. We also avoid
    warning if we override the architecture with the
    AVX-512 flags for the source file containing the asm.
    
    Fixes #2340.
    
    Change-Id: I3aea95b162c55c7773182a69f639dff1a01d0603

--- src/gromacs/hardware/identifyavx512fmaunits.cpp
+++ src/gromacs/hardware/identifyavx512fmaunits.cpp
@@ -46,19 +46,9 @@
 
 #include "identifyavx512fmaunits.h"
 
-#ifdef GMX_IDENTIFY_AVX512_FMA_UNITS_STANDALONE
-#define SIMD_AVX_512_CXX_SUPPORTED 1
-#else
+#ifndef GMX_IDENTIFY_AVX512_FMA_UNITS_STANDALONE
 #include "config.h"
 #endif
-
-#if SIMD_AVX_512_CXX_SUPPORTED
-#include <immintrin.h>
-
-#ifdef _MSC_VER
-#include <intrin.h>
-#endif
-#endif // SIMD_AVX_512_CXX_SUPPORTED
 
 #include <cstdint>
 #include <cstdio>
@@ -76,23 +66,7 @@ namespace gmx
 namespace
 {
 
-#if SIMD_AVX_512_CXX_SUPPORTED
-// Use a local routine to read the timestep counter just on x86 to avoid dependence
-// on the Gromacs cycle counter module.
-uint64_t rdtscp(void)
-{
-#ifdef MSC_VER
-    unsigned int ui;
-    return static_cast<uint64_t>(__rdtscp(&ui));
-#else
-    uint32_t low;
-    uint32_t high;
-
-    __asm__ __volatile__("rdtscp" : "=a" (low), "=d" (high) :: "ecx" );
-    return (static_cast<uint64_t>(high) << 32) | low;
-#endif
-}
-
+#if GMX_X86_GCC_INLINE_ASM && SIMD_AVX_512_CXX_SUPPORTED
 /*\ brief Loop over mixed FMA and shuffle AVX512 instructions
  *
  * This function executes a meaningless loop that includes both
@@ -102,112 +76,87 @@ uint64_t rdtscp(void)
  *
  * \param loopCount  Number of iterations. Each iteration will
  *                   execute 12 FMA and 12 shuffle instructions.
- * \param seed       A double-precision number between 0 and 1.
- *                   To be really certain the loop is not optimized
- *                   away, you should use some timing-related
- *                   function to create this seed at runtime.
- * \return Meaningless floating-point number. Make sure you
- *         add this number to some variable and conditionally
- *         issue a print statement e.g. if it is negative
- *         (which should not happen), again to make sure the loop
- *         cannot be optimized away.
+ * \return Number of cycles used for the loop.
  */
-double
-executeFmaAndShuffleLoop(int     loopCount,
-                         double  seed)
+uint64_t
+timeFmaAndShuffleLoop(uint64_t loopCount)
 {
-    // Make sure all variables are different to avoid gcc optimizing them away
-    __m512d   d0   = _mm512_set1_pd(1.0-0.01*seed);
-    __m512d   d1   = _mm512_set1_pd(1.0-0.02*seed);
-    __m512d   d2   = _mm512_set1_pd(1.0-0.03*seed);
-    __m512d   d3   = _mm512_set1_pd(1.0-0.04*seed);
-    __m512d   d4   = _mm512_set1_pd(1.0-0.05*seed);
-    __m512d   d5   = _mm512_set1_pd(1.0-0.06*seed);
-    __m512d   d6   = _mm512_set1_pd(1.0-0.07*seed);
-    __m512d   d7   = _mm512_set1_pd(1.0-0.08*seed);
-    __m512d   d8   = _mm512_set1_pd(1.0-0.09*seed);
-    __m512d   d9   = _mm512_set1_pd(1.0-0.10*seed);
-    __m512d   d10  = _mm512_set1_pd(1.0-0.11*seed);
-    __m512d   d11  = _mm512_set1_pd(1.0-0.12*seed);
-    __m512d   eps  = _mm512_set1_pd(1e-6);
-    __m512i   i0   = _mm512_set_epi32(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
-    __m512i   i1   = _mm512_set_epi32(0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
-    __m512i   i2   = _mm512_set_epi32(1, 0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2);
-    __m512i   i3   = _mm512_set_epi32(2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3);
-    __m512i   i4   = _mm512_set_epi32(3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4);
-    __m512i   i5   = _mm512_set_epi32(4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5);
-    __m512i   i6   = _mm512_set_epi32(5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6);
-    __m512i   i7   = _mm512_set_epi32(7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8);
-    __m512i   i8   = _mm512_set_epi32(8, 7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9);
-    __m512i   i9   = _mm512_set_epi32(9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10);
-    __m512i   i10  = _mm512_set_epi32(10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12, 11);
-    __m512i   i11  = _mm512_set_epi32(11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13, 12);
-    __m512i   idx  = _mm512_set_epi32(12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 15, 14, 13);
-    __mmask16 mask = static_cast<uint16_t>(0xffff);
+    uint64_t cycles;
+    // Unfortunately we need to resort to inline ASM since we are
+    // making a choice based on timing, and without efficient optimization
+    // (e.g. when doing debugging) the usual intrinsics are often implemented
+    // as independent load/store operations, which completely screws up timing.
+    __asm__ __volatile__("\tvpxord %%zmm0, %%zmm0, %%zmm0\n"
+                         "\tvmovaps %%zmm0, %%zmm1\n"
+                         "\tvmovaps %%zmm0, %%zmm2\n"
+                         "\tvmovaps %%zmm0, %%zmm3\n"
+                         "\tvmovaps %%zmm0, %%zmm4\n"
+                         "\tvmovaps %%zmm0, %%zmm5\n"
+                         "\tvmovaps %%zmm0, %%zmm6\n"
+                         "\tvmovaps %%zmm0, %%zmm7\n"
+                         "\tvmovaps %%zmm0, %%zmm8\n"
+                         "\tvmovaps %%zmm0, %%zmm9\n"
+                         "\tvmovaps %%zmm0, %%zmm10\n"
+                         "\tvmovaps %%zmm0, %%zmm11\n"
+                         "\tvpxord %%zmm12, %%zmm12, %%zmm12\n"
+                         "\tvmovaps %%zmm12, %%zmm13\n"
+                         "\tvmovaps %%zmm12, %%zmm14\n"
+                         "\tvmovaps %%zmm12, %%zmm15\n"
+                         "\tvmovaps %%zmm12, %%zmm16\n"
+                         "\tvmovaps %%zmm12, %%zmm17\n"
+                         "\tvmovaps %%zmm12, %%zmm18\n"
+                         "\tvmovaps %%zmm12, %%zmm19\n"
+                         "\tvmovaps %%zmm12, %%zmm20\n"
+                         "\tvmovaps %%zmm12, %%zmm21\n"
+                         "\tvmovaps %%zmm12, %%zmm22\n"
+                         "\tvmovaps %%zmm12, %%zmm23\n"
+                         "\tvmovaps %%zmm12, %%zmm30\n"
+                         "\trdtscp\n"
+                         "\tsalq $32, %%rdx\n"
+                         "\tmovl %%eax, %%eax\n"
+                         "\tmovq %%rdx, %%rbx\n"
+                         "\torq %%rax, %%rbx\n"
+                         "\tmovq %1, %%rdx\n"
+                         "1:\n"
+                         "\tvfmadd231pd %%zmm0, %%zmm0, %%zmm0\n"
+                         "\tvfmadd231pd %%zmm1, %%zmm1, %%zmm1\n"
+                         "\tvfmadd231pd %%zmm2, %%zmm2, %%zmm2\n"
+                         "\tvfmadd231pd %%zmm3, %%zmm3, %%zmm3\n"
+                         "\tvfmadd231pd %%zmm4, %%zmm4, %%zmm4\n"
+                         "\tvfmadd231pd %%zmm5, %%zmm5, %%zmm5\n"
+                         "\tvfmadd231pd %%zmm6, %%zmm6, %%zmm6\n"
+                         "\tvfmadd231pd %%zmm7, %%zmm7, %%zmm7\n"
+                         "\tvfmadd231pd %%zmm8, %%zmm8, %%zmm8\n"
+                         "\tvfmadd231pd %%zmm9, %%zmm9, %%zmm9\n"
+                         "\tvfmadd231pd %%zmm10, %%zmm10, %%zmm10\n"
+                         "\tvfmadd231pd %%zmm11, %%zmm11, %%zmm11\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm12\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm13\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm14\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm15\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm16\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm17\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm18\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm19\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm20\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm21\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm22\n"
+                         "\tvpermd %%zmm30, %%zmm30, %%zmm23\n"
+                         "\tdec %%rdx\n"
+                         "\tjg 1b\n"
+                         "\trdtscp\n"
+                         "\tsalq $32, %%rdx\n"
+                         "\tmovl %%eax, %%eax\n"
+                         "\torq %%rax, %%rdx\n"
+                         "\tsubq %%rbx, %%rdx\n"
+                         "\tmovq %%rdx, %0\n"
+                         : "=r" (cycles) : "r" (loopCount)
+                         : "rax", "rbx", "rcx", "rdx", "zmm0", "zmm1", "zmm2", "zmm3",
+                         "zmm4", "zmm5", "zmm6", "zmm7", "zmm8", "zmm9", "zmm10",
+                         "zmm11", "zmm12", "zmm13", "zmm14", "zmm15", "zmm16", "zmm17",
+                         "zmm18", "zmm19", "zmm20", "zmm21", "zmm22", "zmm23", "zmm30");
 
-    for (int i = 0; i < loopCount; i++)
-    {
-        d0  = _mm512_fmadd_pd(d0, d0, eps);
-        d1  = _mm512_fmadd_pd(d1, d1, eps);
-        d2  = _mm512_fmadd_pd(d2, d2, eps);
-        d3  = _mm512_fmadd_pd(d3, d3, eps);
-        d4  = _mm512_fmadd_pd(d4, d4, eps);
-        d5  = _mm512_fmadd_pd(d5, d5, eps);
-        d6  = _mm512_fmadd_pd(d6, d6, eps);
-        d7  = _mm512_fmadd_pd(d7, d7, eps);
-        d8  = _mm512_fmadd_pd(d8, d8, eps);
-        d9  = _mm512_fmadd_pd(d9, d9, eps);
-        d10 = _mm512_fmadd_pd(d10, d10, eps);
-        d11 = _mm512_fmadd_pd(d11, d11, eps);
-        // plain permutevar is not yet available in gcc-6.4
-        i0  = _mm512_maskz_permutexvar_epi32(mask, idx, i0);
-        i1  = _mm512_maskz_permutexvar_epi32(mask, idx, i1);
-        i2  = _mm512_maskz_permutexvar_epi32(mask, idx, i2);
-        i3  = _mm512_maskz_permutexvar_epi32(mask, idx, i3);
-        i4  = _mm512_maskz_permutexvar_epi32(mask, idx, i4);
-        i5  = _mm512_maskz_permutexvar_epi32(mask, idx, i5);
-        i6  = _mm512_maskz_permutexvar_epi32(mask, idx, i6);
-        i7  = _mm512_maskz_permutexvar_epi32(mask, idx, i7);
-        i8  = _mm512_maskz_permutexvar_epi32(mask, idx, i8);
-        i9  = _mm512_maskz_permutexvar_epi32(mask, idx, i9);
-        i10 = _mm512_maskz_permutexvar_epi32(mask, idx, i10);
-        i11 = _mm512_maskz_permutexvar_epi32(mask, idx, i11);
-    }
-
-    // Make sure we use all variables in the loop to return a result
-    i0  = _mm512_add_epi32(i0, i1);
-    i2  = _mm512_add_epi32(i2, i3);
-    i4  = _mm512_add_epi32(i4, i5);
-    i6  = _mm512_add_epi32(i6, i7);
-    i8  = _mm512_add_epi32(i8, i9);
-    i10 = _mm512_add_epi32(i10, i11);
-    i0  = _mm512_add_epi32(i0, i2);
-    i4  = _mm512_add_epi32(i4, i6);
-    i8  = _mm512_add_epi32(i8, i10);
-    i0  = _mm512_add_epi32(i0, i4);
-    i0  = _mm512_add_epi32(i0, i8);
-
-    d0 = _mm512_fmadd_pd(d0, d1, d2);
-    d3 = _mm512_fmadd_pd(d3, d4, d5);
-    d6 = _mm512_fmadd_pd(d6, d7, d8);
-    d9 = _mm512_fmadd_pd(d9, d10, d11);
-    d0 = _mm512_add_pd(d0, d3);
-    d6 = _mm512_add_pd(d6, d9);
-    d0 = _mm512_add_pd(d0, d6);
-
-    double data[8];
-    int    idata[16];
-    _mm512_storeu_pd(data, d0);
-    _mm512_storeu_si512(idata, i0);
-
-    double d = 0;
-
-    for (int i = 0; i < 8; i++)
-    {
-        d += data[i] * idata[2*i] * idata[2*i+1];
-    }
-
-    return d;
+    return cycles;
 }
 
 /*\ brief Loop over FMA AVX512 instructions
@@ -219,109 +168,84 @@ executeFmaAndShuffleLoop(int     loopCount,
  *
  * \param loopCount  Number of iterations. Each iteration will
  *                   execute 12 FMA instructions.
- * \param seed       A double-precision number between 0 and 1.
- *                   To be really certain the loop is not optimized
- *                   away, you should use some timing-related
- *                   function to create this seed at runtime.
- * \return Meaningless floating-point number. Make sure you
- *         add this number to some variable and conditionally
- *         issue a print statement e.g. if it is negative
- *         (which should not happen), again to make sure the loop
- *         cannot be optimized away.
+ * \return Number of cycles used for the loop.
  */
-double
-executeFmaOnlyLoop(int    loopCount,
-                   double seed)
+uint64_t
+timeFmaOnlyLoop(uint64_t loopCount)
 {
-    // Make sure all variables are different to avoid gcc optimizing them away
-    __m512d d0  = _mm512_set1_pd(1.0-0.01*seed);
-    __m512d d1  = _mm512_set1_pd(1.0-0.02*seed);
-    __m512d d2  = _mm512_set1_pd(1.0-0.03*seed);
-    __m512d d3  = _mm512_set1_pd(1.0-0.04*seed);
-    __m512d d4  = _mm512_set1_pd(1.0-0.05*seed);
-    __m512d d5  = _mm512_set1_pd(1.0-0.06*seed);
-    __m512d d6  = _mm512_set1_pd(1.0-0.07*seed);
-    __m512d d7  = _mm512_set1_pd(1.0-0.08*seed);
-    __m512d d8  = _mm512_set1_pd(1.0-0.09*seed);
-    __m512d d9  = _mm512_set1_pd(1.0-0.10*seed);
-    __m512d d10 = _mm512_set1_pd(1.0-0.11*seed);
-    __m512d d11 = _mm512_set1_pd(1.0-0.12*seed);
-    __m512d eps = _mm512_set1_pd(1e-6);
+    uint64_t cycles;
+    // Unfortunately we need to resort to inline ASM since we are
+    // making a choice based on timing, and without efficient optimization
+    // (e.g. when doing debugging) the usual intrinsics are often implemented
+    // as independent load/store operations, which completely screws up timing.
+    __asm__ __volatile__("\tvpxord %%zmm0, %%zmm0, %%zmm0\n"
+                         "\tvmovaps %%zmm0, %%zmm1\n"
+                         "\tvmovaps %%zmm0, %%zmm2\n"
+                         "\tvmovaps %%zmm0, %%zmm3\n"
+                         "\tvmovaps %%zmm0, %%zmm4\n"
+                         "\tvmovaps %%zmm0, %%zmm5\n"
+                         "\tvmovaps %%zmm0, %%zmm6\n"
+                         "\tvmovaps %%zmm0, %%zmm7\n"
+                         "\tvmovaps %%zmm0, %%zmm8\n"
+                         "\tvmovaps %%zmm0, %%zmm9\n"
+                         "\tvmovaps %%zmm0, %%zmm10\n"
+                         "\tvmovaps %%zmm0, %%zmm11\n"
+                         "\trdtscp\n"
+                         "\tsalq $32, %%rdx\n"
+                         "\tmovl %%eax, %%eax\n"
+                         "\tmovq %%rdx, %%rbx\n"
+                         "\torq %%rax, %%rbx\n"
+                         "\tmovq %1, %%rdx\n"
+                         "1:\n"
+                         "\tvfmadd231pd %%zmm0, %%zmm0, %%zmm0\n"
+                         "\tvfmadd231pd %%zmm1, %%zmm1, %%zmm1\n"
+                         "\tvfmadd231pd %%zmm2, %%zmm2, %%zmm2\n"
+                         "\tvfmadd231pd %%zmm3, %%zmm3, %%zmm3\n"
+                         "\tvfmadd231pd %%zmm4, %%zmm4, %%zmm4\n"
+                         "\tvfmadd231pd %%zmm5, %%zmm5, %%zmm5\n"
+                         "\tvfmadd231pd %%zmm6, %%zmm6, %%zmm6\n"
+                         "\tvfmadd231pd %%zmm7, %%zmm7, %%zmm7\n"
+                         "\tvfmadd231pd %%zmm8, %%zmm8, %%zmm8\n"
+                         "\tvfmadd231pd %%zmm9, %%zmm9, %%zmm9\n"
+                         "\tvfmadd231pd %%zmm10, %%zmm10, %%zmm10\n"
+                         "\tvfmadd231pd %%zmm11, %%zmm11, %%zmm11\n"
+                         "\tdec %%rdx\n"
+                         "\tjg 1b\n"
+                         "\trdtscp\n"
+                         "\tsalq $32, %%rdx\n"
+                         "\tmovl %%eax, %%eax\n"
+                         "\torq %%rax, %%rdx\n"
+                         "\tsubq %%rbx, %%rdx\n"
+                         "\tmovq %%rdx, %0\n"
+                         : "=r" (cycles) : "r" (loopCount)
+                         : "rax", "rbx", "rcx", "rdx", "zmm0", "zmm1", "zmm2", "zmm3",
+                         "zmm4", "zmm5", "zmm6", "zmm7", "zmm8", "zmm9", "zmm10", "zmm11");
 
-    for (int i = 0; i < loopCount; i++)
-    {
-        d0  = _mm512_fmadd_pd(d0, d0, eps);
-        d1  = _mm512_fmadd_pd(d1, d1, eps);
-        d2  = _mm512_fmadd_pd(d2, d2, eps);
-        d3  = _mm512_fmadd_pd(d3, d3, eps);
-        d4  = _mm512_fmadd_pd(d4, d4, eps);
-        d5  = _mm512_fmadd_pd(d5, d5, eps);
-        d6  = _mm512_fmadd_pd(d6, d6, eps);
-        d7  = _mm512_fmadd_pd(d7, d7, eps);
-        d8  = _mm512_fmadd_pd(d8, d8, eps);
-        d9  = _mm512_fmadd_pd(d9, d9, eps);
-        d10 = _mm512_fmadd_pd(d10, d10, eps);
-        d11 = _mm512_fmadd_pd(d11, d11, eps);
-    }
-
-    // Make sure we use all variables in the loop to return a result
-    d0 = _mm512_fmadd_pd(d0, d1, d2);
-    d3 = _mm512_fmadd_pd(d3, d4, d5);
-    d6 = _mm512_fmadd_pd(d6, d7, d8);
-    d9 = _mm512_fmadd_pd(d9, d10, d11);
-    d0 = _mm512_add_pd(d0, d3);
-    d6 = _mm512_add_pd(d6, d9);
-    d0 = _mm512_add_pd(d0, d6);
-
-    double data[8];
-
-    _mm512_storeu_pd(data, d0);
-
-    double d = 0;
-
-    for (int i = 0; i < 8; i++)
-    {
-        d += data[i];
-    }
-    return d;
+    return cycles;
 }
 
 int
 checkDualAvx512FmaUnits()
 {
     uint64_t timeFmaAndShuf = 1e9;             // Large value
-    uint64_t timeFmaOnly    = 1e9;             // Large value
-    double   dummy;
-    double   seed = (rdtscp() & 0xff) / 256.0; // Create an unpredictable small number between 0 and 1
 
-    // Make sure the CPU is in AVX512 mode by executing a fairly long loop
-    dummy = executeFmaOnlyLoop(100000, seed);
+    // Make sure the CPU is in AVX512 mode by executing a fairly long loop.
+    // Use the return value to make sure it is not optimized away. Later invocations
+    // use fewer iterations, so they should always be faster.
+    uint64_t timeFmaOnly    = timeFmaOnlyLoop(100000);
 
-    // Execute the mixed FMA/shuffle loop three times
+    // Execute the loops three times
     for (int i = 0; i < 3; i++)
     {
-        uint64_t start = rdtscp();
-        dummy += executeFmaAndShuffleLoop(1000, seed);
-        uint64_t res = rdtscp() - start;
-        timeFmaAndShuf = std::min(timeFmaAndShuf, res);
+        timeFmaAndShuf = std::min(timeFmaAndShuf, timeFmaAndShuffleLoop(1000) );
+        timeFmaOnly    = std::min(timeFmaOnly, timeFmaOnlyLoop(1000) );
     }
 
-    // Execute the FMA-only loop three times
-    for (int i = 0; i < 3; i++)
-    {
-        uint64_t start = rdtscp();
-        dummy += executeFmaOnlyLoop(1000, seed);
-        uint64_t res = rdtscp() - start;
-        timeFmaOnly = std::min(timeFmaOnly, res);
-    }
-
-    // Dummy can never be negative, but by using it in the
-    // conditional it cannot be optimized away.
-    return (timeFmaAndShuf > 1.5 * timeFmaOnly || dummy < 0);
+    return (timeFmaAndShuf > 1.5 * timeFmaOnly);
 }
 
+#endif  // GMX_X86_GCC_INLINE_ASM && SIMD_AVX_512_CXX_SUPPORTED
 
-#endif  // SIMD_AVX_512_CXX_SUPPORTED
 
 /*! \brief Mutex to guard the execution of the timing test
  *
@@ -355,7 +279,7 @@ identifyAvx512FmaUnits()
 
             if (haveAvx512Hardware)
             {
-#if SIMD_AVX_512_CXX_SUPPORTED
+#if GMX_X86_GCC_INLINE_ASM && SIMD_AVX_512_CXX_SUPPORTED
                 result = checkDualAvx512FmaUnits() ? 2 : 1;
 #else
                 result = -1; // Cannot run the tests
