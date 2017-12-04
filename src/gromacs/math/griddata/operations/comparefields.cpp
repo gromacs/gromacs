95d23f5ab041b65b4275d3d373404177410af726
Carsten Kutzner <ckutzne@gwdg.de>
2017-12-04 16:57:53 +0100

    [WIP] Density map toolset for GROMACS
    The idea of this toolset is to assemble functionality to deal with
    density maps as e.g. from cryo electron microscopy. It will include i/o
    routines for density maps (ccp4 and xplor format), a module to allow to
    do fitting to cryo-EM densities during the simulation, as well as an
    analysis tool to compute average densities from a trajectory.
    
    Refs #2282 and #2281
    
    The module that does on-the-fly fitting to density maps is at the moment
    implemented using the correlation-based density fitting potential V =
    k*(1 - c.c.)
    as described by Orzechowski and Tama in Biophys. J. 2008 "Flexible
    Fitting
    of High-Resolution X-Ray Structures into Cryoelectron Microscopy Maps
    Using Biased Molecular Dynamics Simulations". Other alternatives
    can/will
    also be added.
    
    Current functionality summary:
    - deal with CCP4 density maps (read/write/embed to .tpr)
    - spread out discrete atomic positions on a density grid using Gaussian
      functions
    - calculate the correlation coefficient (c.c.) between the experimental
      and the simulated density and derive the corresponding atomic forces
    - read in density fitting .mdp parameters (which index group to submit
      to the potential, what k and sigma to use)
    - allow the user to implement time-dependent fitting protocols
      by giving k and sigma as a function of time (similar to the simulated
      annealing protocol)
    - spreading and force calculation are SIMD-accelerated
    
    This patch originates from a) old C code written in the early days of
    4.6,
    so it does not yet conform to today's coding standards in GROMACS.
    It is working as expected, also in parallel, and is hopefully a good
    starting point for a density fitting force provider module.
    b) from the "tide" trajectory analysis module written by Camilo Aponte
    and
    Rodolfo Briones that computes time-averaged electron density map
    from an MD trajectory.
    
    Everything is highly WIP and lots of things need to be done, some of it
    are:
    - get rid of the dependency on the CCP4 library cmaplib. Christian
      already has code to do that
    - restructure the code, move it to more adequate places in the GROMACS
      source tree
    - implement the mdrun part of the fitting as a proper force provider
    module
    - join the g_map and the "tide" tools into a single trajectory analysis
    module
    - implement a smarter way to do the calculation in parallel, currently
      the grid reduction limits the parallel scaling for large grids
    - implement more tests
    - properly document everything
    - incorporate scaling factors for the different atom types
    - ... ? feel free to add what you think is missing and/or useful
    
    Change-Id: I5f18c33e77d68554456c55c6eedfe6a16d7b3ff0

---
+++ src/gromacs/math/griddata/operations/comparefields.cpp
@@ -0,0 +1,190 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+#include "comparefields.h"
+#include "fouriertransform.h"
+#include "gridinterpolator.h"
+#include "gromacs/math/griddata/griddata.h"
+#include "gromacs/mdlib/gmx_omp_nthreads.h"
+#include "gromacs/utility/exceptions.h"
+#include "realfieldmeasure.h"
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <set>
+
+namespace gmx
+{
+
+real correlate(const std::vector<real> &a, const std::vector<real> &b)
+{
+
+    auto aMean = std::accumulate(a.cbegin(), a.cend(), 0.) / a.size();
+    auto aSSE  =
+        std::accumulate(a.cbegin(), a.cend(), 0., [aMean](const real &a, real b) {
+                            return a + gmx::square(b - aMean);
+                        });
+    auto bMean = std::accumulate(b.cbegin(), b.cend(), 0.) / b.size();
+    auto bSSE  =
+        std::accumulate(b.cbegin(), b.cend(), 0., [bMean](const real &a, real b) {
+                            return a + gmx::square(b - bMean);
+                        });
+    std::vector<real> mulArray;
+    std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(mulArray),
+                   [aMean, bMean](real aValue, real bValue) {
+                       return (aValue - aMean) * (bValue - bMean);
+                   });
+    return std::accumulate(mulArray.begin(), mulArray.end(), 0.) /
+           sqrt(aSSE * bSSE);
+}
+
+real correlateWhereReferenceAboveThreshold(const std::vector<real> &reference,
+                                           const std::vector<real> &other,
+                                           real                     threshold)
+{
+    std::vector<real> referenceAboveThreshold;
+    std::vector<real> otherWhereReferenceAboveThreshold;
+
+    auto              otherDatum = other.begin();
+    for (auto &referenceDatum : reference)
+    {
+        if (referenceDatum > threshold)
+        {
+            referenceAboveThreshold.push_back(referenceDatum);
+            otherWhereReferenceAboveThreshold.push_back(*otherDatum);
+        }
+        ++otherDatum;
+    }
+
+    return correlate(referenceAboveThreshold, otherWhereReferenceAboveThreshold);
+};
+
+real crossEntropyWithReference(const std::vector<real> &reference,
+                               const std::vector<real> &other,
+                               const std::vector<real> &other_reference,
+                               real                     unitCellVolume)
+{
+    // for numerical stability use a reference density
+    if (reference.size() != other.size())
+    {
+        GMX_THROW(APIError(
+                          "KL-Divergence calculation requires euqally sized input vectors."));
+    }
+    auto p           = reference.begin();
+    auto q           = other.begin();
+    auto q_reference = other_reference.begin();
+    int  size        = other.size();
+    real sum         = 0;
+#pragma omp parallel for num_threads(std::max( \
+    1, gmx_omp_nthreads_get(emntDefault))) reduction(+ : sum) schedule( \
+    static, size / std::max(1, gmx_omp_nthreads_get(emntDefault)) + 1)
+    for (int i = 0; i < size; ++i)
+    {
+        if ((p[i] > 0) && (q[i] > 0) && (q_reference[i] > 0))
+        {
+            sum += p[i] * log(q[i] / (q_reference[i]));
+        }
+    }
+    return -unitCellVolume * sum;
+}
+
+real crossEntropy(const std::vector<real> &reference,
+                  const std::vector<real> &other, real unitCellVolume)
+{
+    if (reference.size() != other.size())
+    {
+        GMX_THROW(APIError(
+                          "KL-CrossTerm calculation requires euqally sized input vectors."));
+    }
+    auto p    = reference.begin();
+    auto q    = other.begin();
+    int  size = other.size();
+    real sum  = 0;
+#pragma omp parallel for num_threads(std::max( \
+    1, gmx_omp_nthreads_get(emntDefault))) reduction(+ : sum) schedule( \
+    static, size / std::max(1, gmx_omp_nthreads_get(emntDefault)) + 1)
+    for (int i = 0; i < size; ++i)
+    {
+        if ((p[i] > 0) && (q[i] > 0))
+        {
+            sum += p[i] * log(q[i]);
+        }
+    }
+    return -unitCellVolume * sum;
+};
+
+real kullbackLeiblerDivergence(const std::vector<real> &reference,
+                               const std::vector<real> &other,
+                               real                     unitCellVolume)
+{
+    if (reference.size() != other.size())
+    {
+        GMX_THROW(APIError(
+                          "KL-CrossTerm calculation requires euqally sized input vectors."));
+    }
+    auto p    = reference.begin();
+    auto q    = other.begin();
+    int  size = other.size();
+    real sum  = 0;
+#pragma omp parallel for num_threads(std::max( \
+    1, gmx_omp_nthreads_get(emntDefault))) reduction(+ : sum) schedule( \
+    static, size / std::max(1, gmx_omp_nthreads_get(emntDefault)) + 1)
+    for (int i = 0; i < size; ++i)
+    {
+        real pp = p[i];
+        if (pp > 0)
+        {
+            real r = q[i] / pp;
+            if (r > 0)
+            {
+                sum += pp * log(r);
+            }
+        }
+    }
+
+    return -unitCellVolume * sum;
+};
+
+real gridSumAtCoordiantes(const GridDataReal3D    &reference,
+                          const std::vector<RVec> &coordinates)
+{
+    real sum = 0;
+    for (const auto &r : coordinates)
+    {
+        sum += getLinearInterpolationAt(reference, {{r[XX], r[YY], r[ZZ]}});
+    }
+    return sum;
+};
+}
