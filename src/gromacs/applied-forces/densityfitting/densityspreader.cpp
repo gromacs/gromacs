64965cc0b3edba62e39b5713cbdd044514175584
Carsten Kutzner <ckutzne@gwdg.de>
2017-12-04 16:57:53 +0100

    [WIP] Density map toolset for GROMACS
    The idea of this toolset is to assemble functionality to deal with
    density maps as e.g. from cryo electron microscopy. It will include i/o
    routines for density maps (ccp4 and xplor format), a module to allow to
    do fitting to cryo-EM densities during the simulation, as well as an
    analysis tool to compute average densities from a trajectory.
    
    Refs #2282 and #2281
    
    The module that does on-the-fly fitting to density maps is at the moment
    implemented using the correlation-based density fitting potential V =
    k*(1 - c.c.)
    as described by Orzechowski and Tama in Biophys. J. 2008 "Flexible
    Fitting
    of High-Resolution X-Ray Structures into Cryoelectron Microscopy Maps
    Using Biased Molecular Dynamics Simulations". Other alternatives
    can/will
    also be added.
    
    Current functionality summary:
    - deal with CCP4 density maps (read/write/embed to .tpr)
    - spread out discrete atomic positions on a density grid using Gaussian
      functions
    - calculate the correlation coefficient (c.c.) between the experimental
      and the simulated density and derive the corresponding atomic forces
    - read in density fitting .mdp parameters (which index group to submit
      to the potential, what k and sigma to use)
    - allow the user to implement time-dependent fitting protocols
      by giving k and sigma as a function of time (similar to the simulated
      annealing protocol)
    - spreading and force calculation are SIMD-accelerated
    
    This patch originates from a) old C code written in the early days of
    4.6,
    so it does not yet conform to today's coding standards in GROMACS.
    It is working as expected, also in parallel, and is hopefully a good
    starting point for a density fitting force provider module.
    b) from the "tide" trajectory analysis module written by Camilo Aponte
    and
    Rodolfo Briones that computes time-averaged electron density map
    from an MD trajectory.
    
    Everything is highly WIP and lots of things need to be done, some of it
    are:
    - get rid of the dependency on the CCP4 library cmaplib. Christian
      already has code to do that
    - restructure the code, move it to more adequate places in the GROMACS
      source tree
    - implement the mdrun part of the fitting as a proper force provider
    module
    - join the g_map and the "tide" tools into a single trajectory analysis
    module
    - implement a smarter way to do the calculation in parallel, currently
      the grid reduction limits the parallel scaling for large grids
    - implement more tests
    - properly document everything
    - incorporate scaling factors for the different atom types
    - ... ? feel free to add what you think is missing and/or useful
    
    Change-Id: I5f18c33e77d68554456c55c6eedfe6a16d7b3ff0

---
+++ src/gromacs/applied-forces/densityfitting/densityspreader.cpp
@@ -0,0 +1,178 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+#include "gmxpre.h"
+
+#include "densityspreader.h"
+#include "gromacs/math/griddata/griddata.h"
+#include "gromacs/math/container/containeroperation.h"
+#include "gromacs/utility/gmxomp.h"
+#include "gromacs/math/griddata/operations/gausstransform.h"
+#include "gromacs/math/vectypes.h"
+#include <algorithm>
+
+namespace gmx
+{
+
+DensitySpreader::DensitySpreader(const IGrid<DIM> &grid, int numberOfThreads, int n_sigma, real sigma, bool integrate) :
+    simulated_density_(grid.duplicate()), number_of_threads_(numberOfThreads), integrate_(integrate)
+{
+
+#pragma omp parallel for ordered num_threads(number_of_threads_)
+    for (int thread = 0; thread < number_of_threads_; ++thread)
+    {
+    #pragma omp critical
+        {
+            simulated_density_buffer_.emplace_back(GridDataReal3D(grid.duplicate()));
+            gauss_transform_.emplace_back(GaussTransform());
+        }
+    }
+    for (auto &transform : gauss_transform_)
+    {
+        transform.setSigma(sigma);
+        transform.setSpreadWidthInNSigma(n_sigma);
+    }
+}
+
+DensitySpreader::~DensitySpreader()
+{
+};
+
+
+void DensitySpreader::setSigma(real sigma)
+{
+    for (auto &transform : gauss_transform_)
+    {
+        transform.setSigma(sigma);
+    }
+}
+
+
+const GridDataReal3D &
+DensitySpreader::getSpreadGrid() const
+{
+    return simulated_density_;
+}
+
+void DensitySpreader::zero()
+{
+    containeroperation::setZero(&simulated_density_);
+}
+
+
+const GridDataReal3D &
+DensitySpreader::spreadLocalAtoms(PaddedArrayRef<const RVec> x, ArrayRef<const real> weights)
+{
+    std::vector<IVec> minimumUsedGridIndex(number_of_threads_);
+    std::vector<IVec> maximumUsedGridIndex(number_of_threads_);
+
+    const size_t      nAtoms = x.size();
+
+#pragma omp parallel num_threads(number_of_threads_)
+    {
+        const int thread        = gmx_omp_get_thread_num();
+        auto     &densitybuffer = simulated_density_buffer_[thread];
+        std::fill(densitybuffer.begin(), densitybuffer.end(), 0.);
+        gauss_transform_[thread].setGrid(&densitybuffer, integrate_);
+
+        const size_t beginThreadAtoms = thread * (nAtoms / number_of_threads_ );
+        const size_t endThreadAtoms   = (thread == number_of_threads_-1) ?  nAtoms : (thread+1) * ( nAtoms / number_of_threads_);
+
+        for (size_t atomIndex = beginThreadAtoms; atomIndex != endThreadAtoms; ++atomIndex)
+        {
+            gauss_transform_[thread].addTransform(x[atomIndex], weights[atomIndex]);
+        }
+
+        minimumUsedGridIndex[thread] = gauss_transform_[thread].minimumUsedGridIndex();
+        maximumUsedGridIndex[thread] = gauss_transform_[thread].maximumUsedGridIndex();
+    }
+    return sumThreadLocalGrids_(minimumUsedGridIndex, maximumUsedGridIndex);
+}
+
+const GridDataReal3D &
+DensitySpreader::sumThreadLocalGrids_(const std::vector<IVec> &minimumUsedGridIndex, const std::vector<IVec> &maximumUsedGridIndex)
+{
+
+    std::array<int, 3> gridStart;
+    std::array<int, 3> gridEnd;
+    for (int i = XX; i <= ZZ; ++i)
+    {
+        auto ithElementLarger = [i](IVec a, IVec b){
+                return a[i] < b[i];
+            };
+        gridStart[i] = (*std::min_element(std::begin(minimumUsedGridIndex), std::end(minimumUsedGridIndex), ithElementLarger))[i];
+        gridEnd[i]   = (*std::max_element(std::begin(maximumUsedGridIndex), std::end(maximumUsedGridIndex), ithElementLarger))[i];
+    }
+    // add together all thread-local grids, using only density values, where there is
+    // actual spread density
+    int  nGridPointsXX        = gridEnd[XX]-gridStart[XX];
+    for (int gridIndexZZ = gridStart[ZZ]; gridIndexZZ <= gridEnd[ZZ]; ++gridIndexZZ)
+    {
+        for (int gridIndexYY = gridStart[YY]; gridIndexYY <= gridEnd[YY]; ++gridIndexYY)
+        {
+            const std::array<int, DIM>            xRowStartIndex({{gridStart[XX], gridIndexYY, gridIndexZZ}});
+            std::vector<GridDataReal3D::iterator> contributingThreadVoxels;
+            // access rows in thread local grids if they contribute to the density
+            for (int thread = 0; thread < number_of_threads_; ++thread)
+            {
+                if ((minimumUsedGridIndex[thread][ZZ] <= gridIndexZZ) &&
+                    ( gridIndexZZ <= maximumUsedGridIndex[thread][ZZ] ) &&
+                    (minimumUsedGridIndex[thread][YY] <= gridIndexYY) &&
+                    (gridIndexYY <= maximumUsedGridIndex[thread][YY]))
+                {
+                    contributingThreadVoxels.push_back(simulated_density_buffer_[thread].iteratorAtMultiIndex(xRowStartIndex));
+                }
+            }
+
+            auto simulatedDensityVoxelIterator = simulated_density_.iteratorAtMultiIndex(xRowStartIndex);
+            // step though grid row by row
+            for (int gridIndexXX = 0; gridIndexXX <= nGridPointsXX; ++gridIndexXX)
+            {
+                // loop over the local threads, collect voxel contributions and advance all threadlocal iterators to next voxel in row
+                for (auto &threadLocalVoxel : contributingThreadVoxels)
+                {
+                    *simulatedDensityVoxelIterator += *threadLocalVoxel;
+                    ++threadLocalVoxel;
+                }
+                ++simulatedDensityVoxelIterator;
+            }
+        }
+    }
+    return simulated_density_;
+};
+
+
+
+} /* gmx */
---
+++ src/gromacs/math/griddata/operations/convolution.cpp
@@ -0,0 +1,91 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*!  \file
+ * \brief
+ * Defines volume data containers.
+ *
+ * \author Christian Blau <cblau@gwdg.de>
+ * \inpublicapi
+ */
+#include "convolution.h"
+#include "densitypadding.h"
+#include "fouriertransform.h"
+#include "gromacs/math/gmxcomplex.h"
+#include "gromacs/math/utilities.h"
+#include "gromacs/utility/real.h"
+
+namespace gmx
+{
+
+GaussConvolution::GaussConvolution(const GridDataReal3D &input)
+    :  extendBeforePadding_ (input.getGrid().lattice().extend()), input_(input), padded_input_
+          (nullptr)
+{}
+GaussConvolution &GaussConvolution::pad(const CanonicalVectorBasis<DIM>::NdVector &paddingFactor)
+{
+    padded_input_ = padDensity(input_, paddingFactor);
+    return *this;
+}
+
+std::unique_ptr < GridDataReal3D> GaussConvolution::convolute(real sigma)
+{
+    if (padded_input_ != nullptr)
+    {
+        fourierTransform_ =
+            FourierTransformRealToComplex3D().normalize().result(*padded_input_);
+    }
+    else
+    {
+        fourierTransform_ =
+            FourierTransformRealToComplex3D().normalize().result(input_);
+    }
+    auto sigmaSquared          = gmx::square(sigma);
+    auto convoluteWithGaussian = [sigmaSquared](t_complex &value, RVec k) {
+            auto prefactor = 1/(sqrt(2)) * exp(-2.0 * M_PI * M_PI * sigmaSquared * norm2(k));
+            value.re *= prefactor;
+            value.im *= prefactor;
+        };
+
+    ApplyToUnshiftedFourierTransform(*fourierTransform_).apply(convoluteWithGaussian);
+    auto result =
+        FourierTransformComplexToReal3D().normalize().result(*fourierTransform_);
+
+    if (padded_input_ != nullptr)
+    {
+        result = unpadDensity(*result, extendBeforePadding_);
+    }
+    return result;
+};
+}
