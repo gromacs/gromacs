1dad80f35ebe50f48a245733e72b95a182900e5f
Carsten Kutzner <ckutzne@gwdg.de>
2017-12-04 16:57:53 +0100

    [WIP] Density map tools for gromacs.
    Tools to read in density data files, convert structures to densities,
    integrate density information over time.
    
    Change-Id: I3af839d63b3a94e66abdbccc842f4d35d1e153f9

--- src/gromacs/trajectoryanalysis/modules/tide.h
+++ src/gromacs/trajectoryanalysis/modules/tide.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012,2015,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -32,38 +32,35 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the research papers on the package. Check out http://www.gromacs.org.
  */
-/*! \file
+/*! \internal \file
  * \brief
- * Defines an enumeration type for specifying file types for options.
+ * Declares trajectory analysis module for TIme-averaged electron DEnsities
  *
- * \author Teemu Murtola <teemu.murtola@gmail.com>
- * \inpublicapi
- * \ingroup module_options
+ * \author Camilo Aponte <ca.aponte.uniandes.edu.co>
+ * \author Carsten Kutzner <ckutzne@gwdg.de>
+ *
+ * \ingroup module_trajectoryanalysis
  */
-#ifndef GMX_OPTIONS_OPTIONFILETYPE_HPP
-#define GMX_OPTIONS_OPTIONFILETYPE_HPP
+#ifndef GMX_TRAJECTORYANALYSIS_MODULES_TIDE_H
+#define GMX_TRAJECTORYANALYSIS_MODULES_TIDE_H
+
+#include "gromacs/trajectoryanalysis/analysismodule.h"
 
 namespace gmx
 {
 
-/*! \brief
- * Purpose of file(s) provided through an option.
- *
- * \ingroup module_options
- */
-enum OptionFileType {
-    eftUnknown,
-    eftTopology,
-    eftTrajectory,
-    eftEnergy,
-    eftPDB,
-    eftIndex,
-    eftPlot,
-    eftGenericData,
-    eftCCP4,
-    eftXPLOR,
-    eftOptionFileType_NR
+namespace analysismodules
+{
+
+class TimeAveragedDensityInfo
+{
+    public:
+        static const char name[];
+        static const char shortDescription[];
+        static TrajectoryAnalysisModulePointer create();
 };
+
+} // namespace analysismodules
 
 } // namespace gmx
 
