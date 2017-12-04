e8ebdc9fdbfb6a6c8263f1de92179af25696ce47
Carsten Kutzner <ckutzne@gwdg.de>
2017-12-04 16:57:53 +0100

    [WIP] Density map toolset for GROMACS
    The idea of this toolset is to assemble functionality to deal with
    density maps as e.g. from cryo electron microscopy. It will include i/o
    routines for density maps (ccp4 and xplor format), a module to allow to
    do fitting to cryo-EM densities during the simulation, as well as an
    analysis tool to compute average densities from a trajectory.
    
    Refs #2282 and #2281
    
    The module that does on-the-fly fitting to density maps is at the moment
    implemented using the correlation-based density fitting potential V =
    k*(1 - c.c.)
    as described by Orzechowski and Tama in Biophys. J. 2008 "Flexible
    Fitting
    of High-Resolution X-Ray Structures into Cryoelectron Microscopy Maps
    Using Biased Molecular Dynamics Simulations". Other alternatives
    can/will
    also be added.
    
    Current functionality summary:
    - deal with CCP4 density maps (read/write/embed to .tpr)
    - spread out discrete atomic positions on a density grid using Gaussian
      functions
    - calculate the correlation coefficient (c.c.) between the experimental
      and the simulated density and derive the corresponding atomic forces
    - read in density fitting .mdp parameters (which index group to submit
      to the potential, what k and sigma to use)
    - allow the user to implement time-dependent fitting protocols
      by giving k and sigma as a function of time (similar to the simulated
      annealing protocol)
    - spreading and force calculation are SIMD-accelerated
    
    This patch originates from a) old C code written in the early days of
    4.6,
    so it does not yet conform to today's coding standards in GROMACS.
    It is working as expected, also in parallel, and is hopefully a good
    starting point for a density fitting force provider module.
    b) from the "tide" trajectory analysis module written by Camilo Aponte
    and
    Rodolfo Briones that computes time-averaged electron density map
    from an MD trajectory.
    
    Everything is highly WIP and lots of things need to be done, some of it
    are:
    - get rid of the dependency on the CCP4 library cmaplib. Christian
      already has code to do that
    - restructure the code, move it to more adequate places in the GROMACS
      source tree
    - implement the mdrun part of the fitting as a proper force provider
    module
    - join the g_map and the "tide" tools into a single trajectory analysis
    module
    - implement a smarter way to do the calculation in parallel, currently
      the grid reduction limits the parallel scaling for large grids
    - implement more tests
    - properly document everything
    - incorporate scaling factors for the different atom types
    - ... ? feel free to add what you think is missing and/or useful
    
    Change-Id: I5f18c33e77d68554456c55c6eedfe6a16d7b3ff0

---
+++ src/gromacs/applied-forces/densityfitting/localdensfitdata.h
@@ -0,0 +1,116 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+ #ifndef GMX_APPLIEDFORCES_DENSITYFITTING_LOCALDENSFITDATA_H_
+ #define GMX_APPLIEDFORCES_DENSITYFITTING_LOCALDENSFITDATA_H_
+
+ #include "gromacs/domdec/localatomset.h"
+ #include "gromacs/domdec/localatomsetmanager.h"
+
+ #include "densfitdata.h"
+ #include "densityspreader.h"
+ #include "gromacs/utility/alignedallocator.h"
+ #include "gromacs/math/paddedvector.h"
+
+#include <vector>
+namespace gmx
+{
+
+double calc_sum_rhoA_rhoB(const std::vector<float> &vecA,
+                          const std::vector<float> &vecB);
+//! \brief local density fitting data
+class LocalDensfitData
+{
+    public:
+        LocalDensfitData(LocalAtomSet localAtomSet, const DensfitData &parameters, rvec *x_densfit_whole /* Can be NULL! */);
+        ~LocalDensfitData() = default;
+        void updateTimeDependentData(real time, const DensfitData &parameters);
+
+        void do_forces(const GridDataReal3D &referenceMap);
+
+        const GridDataReal3D &simulatedMap() const;
+
+        std::string infoString() const;
+
+        void add_forces(rvec * f) const;
+
+        void triggerShiftUpdate();
+
+        bool afterDomainDecomposition() const;
+
+        void communicate(PaddedArrayRef<RVec> x, const t_commrec * cr, matrix box, int ePBC);
+
+        void spreadAtoms(const t_commrec * cr);
+
+        void sumSimulatedMap(const t_commrec * cr);
+        void calculateCorrelationCoefficient(const GridDataReal3D &referenceMap);
+    private:
+        real k;                          /**< Current value k of strength constant of map
+                                              potential V_fit = k*(1 - cc) with cc being the
+                                              correlation coefficient                        */
+        std::vector<real> weights;       /**< weight of the local atoms for spreadingn and forces */
+        real              sigma;         /**< Current value of Gaussian width used for
+                                              spreading atomic positions on the map          */
+        real              cc;            /**< Correlation coefficient of the two maps        */
+        std::vector<RVec> x_assembled;   /**< Just the positions that are spread             */
+        std::vector<RVec> f_loc;         /**< Forces on the local atoms of the
+                                            density fitting group                          */
+        std::vector<RVec> x_old;         /**< x_assembled from last step                     */
+        std::vector<IVec> x_shifts;      /**< To make the molecule whole                     */
+        std::vector<IVec> extra_shifts;  /**< Shifts added since last NS                     */
+        gmx_bool          bUpdateShifts; /**< Do we have to calculate new shift vectors
+                                              from x_assembled and x_old?                    */
+        int               voxrange;      /**< Max. number of voxels to be computed for a
+                                              single atom in a single dimension x, y, or z   */
+        int               vox_per_atom;  /**< Total number of voxels for one atom (x,y, + z) */
+        double            sum_rho_exp2;  /**< Term needed for the calculation of the
+                                              correlation coefficient and the forces         */
+        LocalAtomSet      localAtoms;
+
+        GridDataReal3D    map_sim_;   /* The map simulated from atomic position data     */
+
+        DensitySpreader   densitySpreader_;
+
+        /* The following two temporary vectors (one for each OpenMP thread) store
+         * erf values around a single atoms, thus we can compute them all in one go,
+         * and with SIMD acceleration */
+        using AlignedRealVector = std::vector < real, gmx::AlignedAllocator < real>>;
+        std::vector<AlignedRealVector> erfVector;  /**< vector of vectors of erf values */
+        std::vector<AlignedRealVector> expVector;  /**< same for exp values             */
+};
+}
+
+
+ #endif /* end of include guard: GMX_APPLIEDFORCES_DENSITYFITTING_LOCALDENSFITDATA_H_ */
---
+++ src/gromacs/fileio/xplor.h
@@ -0,0 +1,89 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Data structure mirroring the content of an xplor-file.
+ *
+ * \author Christian Blau <cblau@gwdg.de>
+ * \inpublicapi
+ * \ingroup module_fileio
+ */
+
+#ifndef GMX_FILEIO_XPLOR_H_
+#define GMX_FILEIO_XPLOR_H_
+#include <array>
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "gromacs/math/vectypes.h"
+#include "gromacs/math/griddata/griddata.h"
+
+
+namespace gmx
+{
+
+struct t_mapdata;
+/*! \brief
+ * A container for the metadata in xplor file formats.
+ *
+ * For a detailed decription see
+ * http://www.scripps.edu/rc/softwaredocs/msi/xplor981/formats.html
+ */
+struct XplorData{
+    XplorData() = default;
+    XplorData(FILE* inputStream);
+
+    void                  maskData(const std::vector<float> &mask, float threshold);
+    void                  write(FILE * outputStream) const;
+
+    RVec                  cell_length;      //!< length of the crystallographic unit cell
+    RVec                  cell_angles;      //!< crystallographic unit cell angles
+
+    std::array<int, DIM>  extend;           //!< the grid extend, check against num_crs
+    std::array<int, DIM>  crs_start;        //!< Start of values in grid, typically 0,0,0
+    std::array<int, DIM>  crs_end;          //!< End of values in grid, typically extend[XX],extend[YY],extend[ZZ]
+
+    float                 mean_value;       //!< mean voxel value   (not always reported,as evident from density)
+    float                 rms_value;        //!< rms of the density (not always reported,as evident from density)
+
+    std::vector<float>    data;             //!< The density data
+
+    const static float    nmToXplorUnits;   //!< Multiply by this factor to obtain nm from xplor units (Aangstrom)
+    const static int      magicCheckNumber; //!< Typically -9999; establishes sucessful file reading
+};
+
+}      // namespace gmx
+#endif /* end of include guard: GMX_FILEIO_XPLOR_H_ */
