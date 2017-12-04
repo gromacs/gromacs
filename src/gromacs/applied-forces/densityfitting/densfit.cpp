38cc3688feee5e837354e5953b58152144b9020d
Carsten Kutzner <ckutzne@gwdg.de>
2017-12-04 16:57:53 +0100

    [WIP] Density map toolset for GROMACS
    The idea of this toolset is to assemble functionality to deal with
    density maps as e.g. from cryo electron microscopy. It will include i/o
    routines for density maps (ccp4 and xplor format), a module to allow to
    do fitting to cryo-EM densities during the simulation, as well as an
    analysis tool to compute average densities from a trajectory.
    
    Refs #2282 and #2281
    
    The module that does on-the-fly fitting to density maps is at the moment
    implemented using the correlation-based density fitting potential V =
    k*(1 - c.c.)
    as described by Orzechowski and Tama in Biophys. J. 2008 "Flexible
    Fitting
    of High-Resolution X-Ray Structures into Cryoelectron Microscopy Maps
    Using Biased Molecular Dynamics Simulations". Other alternatives
    can/will
    also be added.
    
    Current functionality summary:
    - deal with CCP4 density maps (read/write/embed to .tpr)
    - spread out discrete atomic positions on a density grid using Gaussian
      functions
    - calculate the correlation coefficient (c.c.) between the experimental
      and the simulated density and derive the corresponding atomic forces
    - read in density fitting .mdp parameters (which index group to submit
      to the potential, what k and sigma to use)
    - allow the user to implement time-dependent fitting protocols
      by giving k and sigma as a function of time (similar to the simulated
      annealing protocol)
    - spreading and force calculation are SIMD-accelerated
    
    This patch originates from a) old C code written in the early days of
    4.6,
    so it does not yet conform to today's coding standards in GROMACS.
    It is working as expected, also in parallel, and is hopefully a good
    starting point for a density fitting force provider module.
    b) from the "tide" trajectory analysis module written by Camilo Aponte
    and
    Rodolfo Briones that computes time-averaged electron density map
    from an MD trajectory.
    
    Everything is highly WIP and lots of things need to be done, some of it
    are:
    - get rid of the dependency on the CCP4 library cmaplib. Christian
      already has code to do that
    - restructure the code, move it to more adequate places in the GROMACS
      source tree
    - implement the mdrun part of the fitting as a proper force provider
    module
    - join the g_map and the "tide" tools into a single trajectory analysis
    module
    - implement a smarter way to do the calculation in parallel, currently
      the grid reduction limits the parallel scaling for large grids
    - implement more tests
    - properly document everything
    - incorporate scaling factors for the different atom types
    - ... ? feel free to add what you think is missing and/or useful
    
    Change-Id: I5f18c33e77d68554456c55c6eedfe6a16d7b3ff0

---
+++ src/gromacs/applied-forces/densityfitting/densfit.cpp
@@ -0,0 +1,345 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+#include "gmxpre.h"
+#include "config.h"
+
+#include "string.h"
+#include "densfit.h"
+#include "localdensfitdata.h"
+
+#include "gromacs/math/vec.h"
+
+#include "gromacs/fileio/xvgr.h"
+
+#include "gromacs/fileio/gmxfio.h"
+#include "gromacs/mdtypes/inputrec.h"
+#include "gromacs/topology/topology.h"
+#include "gromacs/utility/fatalerror.h"
+#include "gromacs/utility/pleasecite.h"
+#include "gromacs/utility/inmemoryserializer.h"
+#include "gromacs/commandline/filenm.h"
+#include "gromacs/mdlib/sim_util.h"
+
+#include "gromacs/timing/cyclecounter.h"
+
+#include "gromacs/domdec/domdec.h"
+
+#include "gromacs/fileio/griddataio.h"
+
+#include "gromacs/mdlib/broadcaststructs.h"
+
+#define MAXPTR 254
+
+const std::string Densfit::name_ = "Density fitting:";
+
+Densfit::Densfit() {}
+
+Densfit::Densfit(const gmx::DensfitData &parameters)
+{
+    parameters_ = std::unique_ptr<gmx::DensfitData>(new gmx::DensfitData(parameters));
+}
+
+Densfit::~Densfit(){};
+
+const gmx::GridDataReal3D &Densfit::simulatedMap() { return df_->simulatedMap(); }
+
+void Densfit::init(int ePBC, gmx_mtop_t *mtop, rvec *x, matrix box, gmx::LocalAtomSetManager * atomsets, const t_commrec *cr)
+{
+
+    pbc_ = ePBC;
+    if ((PAR(cr)) && !DOMAINDECOMP(cr))
+    {
+        gmx_fatal(FARGS, "%sOnly implemented for domain decomposition.\n", name_.c_str());
+    }
+
+    auto densfitAtoms = atomsets->add(parameters_->fittingGroup().ind_);
+
+    /* To be able to make the density fitting molecule whole: */
+    rvec *x_densfit_pbc = nullptr;
+    snew(x_densfit_pbc, densfitAtoms.numAtomsGlobal()); /* There ... */
+    if (MASTER(cr))
+    {
+        rvec *x_pbc = nullptr;
+        /* Remove pbc and prepare a whole molecule for new_t_gmx_densfit */
+        snew(x_pbc, mtop->natoms); /* There ... */
+        //        m_rveccopy(mtop->natoms,x,x_pbc);
+        copy_rvecn(x, x_pbc, 0, mtop->natoms);
+        do_pbc_first_mtop(nullptr, pbc_, box, mtop, x_pbc);
+        for (size_t i = 0; i < densfitAtoms.numAtomsGlobal(); i++)
+        {
+            copy_rvec(x_pbc[densfitAtoms.globalIndex()[i]], x_densfit_pbc[i]);
+        }
+        sfree(x_pbc); /* ... and back again */
+    }
+    if (PAR(cr))
+    {
+        gmx_bcast(parameters_->fittingGroup().ind_.size() * sizeof(rvec), x_densfit_pbc, cr);
+    }
+
+    df_ = std::unique_ptr<gmx::LocalDensfitData>(new gmx::LocalDensfitData(densfitAtoms, *parameters_, x_densfit_pbc));
+
+    sfree(x_densfit_pbc); /* ... and back again */
+
+}
+
+void Densfit::initOutputFromCommandLineParameters(FILE *fplog, int nfile, const t_filenm *fnm, const gmx_output_env_t *oenv, gmx_bool bAppend, gmx_bool bVerbose)
+{
+    output_ = std::unique_ptr<gmx::DensfitOutput> (new gmx::DensfitOutput(fplog, opt2fn("-d", nfile, fnm), opt2fn("-mo", nfile, fnm), oenv, bAppend));
+    if (bVerbose && output_ && parameters_ != nullptr)
+    {
+        parameters_->printToOut(output_->logFile(), oenv);
+    }
+}
+
+void Densfit::triggerShiftUpdate()
+{
+    df_->triggerShiftUpdate();
+}
+
+void Densfit::add_forces(rvec *f, gmx_int64_t step, real time)
+{
+    if (!parameters_->fitThisStep(step))
+    {
+        return;
+    }
+
+    df_->add_forces(f);
+
+    if (output_ && output_->logFile() && parameters_->outputMapThisStep(step))
+    {
+        fprintf(output_->logFile(), "%12.5e%s\n", time, df_->infoString().c_str());
+    }
+
+}
+
+void Densfit::do_densfit(real t, gmx_int64_t step,
+                         const t_commrec *cr, gmx::PaddedArrayRef<gmx::RVec> x, matrix box,
+                         gmx_wallcycle *wcycle)
+{
+
+    /* If the user requested to have the density fitting forces calculated only
+     * every N steps, we can skip the expensive do_densfit routine. However, we
+     * must always calculate the fitting forces
+     * - at the first step (obviously)
+     * - after domain decompositions since the local atoms have changed
+     */
+    if ((!parameters_->fitThisStep(step) && (!df_->afterDomainDecomposition())))
+    {
+        return;
+    }
+
+    /* Update the time-dependent parameters like k, sigma, etc. */
+    if (parameters_->timeDependent().vary())
+    {
+        df_->updateTimeDependentData(t, *parameters_);
+    }
+
+
+    /* ASSEMBLE THE POSITIONS OF THE ATOMS USED FOR DENSITY FITTING           */
+    /* Broadcast the DF positions such that every node has all of them
+     * Every node contributes its local positions x and stores it in
+     * the collective df->x_assembled array. Do all the communication first!  */
+    wallcycle_start(wcycle, ewcDENSFIT_COMM);
+    df_->communicate(x, cr, box, pbc_);
+    wallcycle_stop(wcycle, ewcDENSFIT_COMM);
+    /* Now all nodes have all of the DF positions in df->x_assembled */
+
+    /* SPREAD THE ATOMS ON THE DENSITY GRID                                   */
+    /* Produces the density map. Each node spreads an equal part of all atoms,
+     * this way we do not need load balancing here                            */
+    wallcycle_start(wcycle, ewcDENSFIT_SPREAD);
+    df_->spreadAtoms(cr, box);
+    wallcycle_stop(wcycle, ewcDENSFIT_SPREAD);
+
+    if (PAR(cr))
+    {
+        wallcycle_start(wcycle, ewcDENSFIT_SUM_GRID);
+        df_->sumSimulatedMap(cr);
+        wallcycle_stop(wcycle, ewcDENSFIT_SUM_GRID);
+    }
+
+
+    if (output_ && parameters_->outputMapThisStep(step))
+    {
+        if (parameters_->keepAndNumberMaps())
+        {
+            gmx::MrcFile().write(output_->outputMapFileNameWithStep(step), df_->simulatedMap());
+        }
+        else
+        {
+            gmx::MrcFile().write(output_->outputMapFileName(), df_->simulatedMap());
+        }
+    }
+
+    if (MASTER(cr))
+    {
+        df_->calculateCorrelationCoefficient(parameters_->referenceMap());
+    }
+    /**************************************************************************/
+    /* CALCULATE THE FORCES FROM THE DENSITY FITTING POTENTIAL                */
+    /* Each rank calculates the forces on its home atoms                      */
+
+    /* Start to count cycles for the dynamic load balancing now */
+    gmx_cycles_t cycles_comp = gmx_cycles_read(); /* Cycles for the density fitting computations
+                                                     only, does not count communication. This
+                                                     counter is used for load-balancing */
+
+    //    long int c1 = gmx_cycles_read();
+    wallcycle_start(wcycle, ewcDENSFIT_FORCES);
+    df_->do_forces(box, parameters_->referenceMap());
+    wallcycle_stop(wcycle, ewcDENSFIT_FORCES);
+    //    fprintf(stderr, "--- Forces took %g M cycles\n",
+    //    0.000001*(gmx_cycles_read()-c1));
+
+    /* Stop the density fitting cycle counter and add the computation-only
+     * cycles to the force cycles for load balancing */
+    cycles_comp = gmx_cycles_read() - cycles_comp;
+
+    if (DOMAINDECOMP(cr) && wcycle)
+    {
+        dd_cycles_add(cr->dd, cycles_comp, ddCyclF);
+    }
+}
+
+void Densfit::spread_atoms(real (*box)[3])
+{
+    df_->spreadAtoms(nullptr, box);
+}
+
+gmx::GridDataReal3D Densfit::referenceMapCopy() const
+{
+    return parameters_->referenceMap();
+};
+
+const gmx::DensfitData &Densfit::parameters() const { return *parameters_; }
+
+namespace gmx
+{
+
+void make_filename(const char *outf_name, int ndigit, int file_nr,
+                   char newname[STRLEN])
+{
+    char  fmt[128];
+    char  nbuf[128];
+
+    /* Strip away the extension */
+    char *outf_base = gmx_strdup(outf_name);
+    char *extpos    = strrchr(outf_base, '.');
+    if (extpos)
+    {
+        *extpos = '\0';
+    }
+
+    int fnr = file_nr;
+    int nd  = 0;
+    do
+    {
+        fnr /= 10;
+        nd++;
+    }
+    while (fnr > 0);
+
+    if (ndigit > 0)
+    {
+        snprintf(fmt, 128, "%%0%dd", ndigit);
+        snprintf(nbuf, 128, fmt, file_nr);
+    }
+    else
+    {
+        snprintf(nbuf, 128, "%d", file_nr);
+    }
+
+    if (extpos)
+    {
+        snprintf(newname, STRLEN, "%s%s.%s", outf_base, nbuf, extpos + 1);
+    }
+    else
+    {
+        snprintf(newname, STRLEN, "%s%s", outf_base, nbuf);
+    }
+
+    free(outf_base);
+}
+
+
+DensfitOutput::DensfitOutput(FILE * fplog, const char *fnLogFile, const char * fnDensity, const gmx_output_env_t *oenv, bool bAppend)
+{
+    if (bAppend)
+    {
+        logFile_ = gmx_fio_fopen(fnLogFile, "a");
+    }
+    else
+    {
+        please_cite(fplog, "Tama2008");
+        logFile_ = xvgropen(fnLogFile, "Density fitting correlation coefficient", "Time (ps)",
+                            "correlation coefficient", oenv);
+        fflush(logFile_);
+    }
+    fnDensity_ = fnDensity;
+}
+
+FILE* DensfitOutput::logFile()
+{
+    return logFile_;
+}
+
+DensfitOutput::~DensfitOutput()
+{
+    if (logFile_) // Close the density fitting output file
+    {
+        if (bAppend_)
+        {
+            gmx_fio_fclose(logFile_);
+        }
+        else
+        {
+            xvgrclose(logFile_);
+        }
+    }
+}
+
+std::string DensfitOutput::outputMapFileName() const
+{
+    return fnDensity_;
+}
+
+std::string DensfitOutput::outputMapFileNameWithStep(gmx_int64_t step) const
+{
+    char         fnWithStep[STRLEN];
+    make_filename(fnDensity_.c_str(), 0, step, fnWithStep);
+    return fnWithStep;
+}
+}
---
+++ src/gromacs/fileio/griddataio.h
@@ -0,0 +1,134 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Reading and writing routines for volume data formats ccp4, mrc and imod.
+ *
+ * \author Christian Blau <cblau@gwdg.de>
+ * \inpublicapi
+ * \ingroup module_fileio
+ */
+
+#ifndef GMX_FILEIO_GRIDDATAIO_H_
+#define GMX_FILEIO_GRIDDATAIO_H_
+#include <array>
+#include <memory>
+#include <string>
+
+#include "gromacs/math/griddata/griddata.h"
+
+namespace gmx
+{
+struct MrcMetaData;
+/*! \brief
+ * Read and write real-space real-valued volume data files
+ * according to the electron microscopy data bank (EMDB) standard.
+ *
+ * The formatting guraranties compliance with 3D EM maps described in
+ * "EMDB Map Distribution Format Description Version 1.01 (c) emdatabank.org 2014"
+ * However, other ccp4, mrc, imod and map formats might be compatible.
+ *
+ * Future implementations for reading crystallographic data or image stacks might
+ * want to split MrcFile into an abstract griddata base class and respective
+ * child implementatons, if demand exists.
+ */
+class MrcFile
+{
+    public:
+        MrcFile();
+        ~MrcFile();
+
+        /*! \brief Write real-spaced, real-valued griddata to file
+         * with default metadata for 3D electron microscopy data.
+         *
+         * \param[in] filename name of the file to write the griddata to, typically *.cpp4, *.mrc or *.map
+         * \param[in] grid_data real-valued, real-space data on a grid
+         */
+        void write(std::string filename, const GridDataReal3D &grid_data);
+
+        /*! \brief Write real-spaced, real-valued griddata to file with user-defined metadata.
+         *
+         * \param[in] filename name of the file to write the griddata to, typically *.cpp4, *.mrc or *.map
+         * \param[in] grid_data real-valued, real-space data on a grid
+         * \param[in] meta struct with own metadata
+         * \param[in] bOwnGridStats calculate min, max, mean and rms self if true, otherwise copy from metadata
+         */
+        void write_with_own_meta(std::string filename, GridDataReal3D &grid_data, const MrcMetaData &meta, bool bOwnGridStats);
+
+        /*! \brief Reads real-spaced, real-valued griddata from file.
+         *
+         * \param[in] filename name of the file from which to read the griddata, typically *.cpp4, *.mrc or *.map
+         * \returns grid_data will be filled with real-valued, real-space data on a grid upon succesful reading
+         */
+        GridDataReal3D read(std::string filename);
+        /*! \brief Reads real-spaced, real-valued voxel data and the map header / metadata.
+         *
+         * \param[in] filename name of the file from which to read the griddata, typically *.cpp4, *.mrc or *.map
+         * \param[in] meta returns the metadata from reading; previous content will be overwritten
+         * \returns grid_data will be filled with real-valued, real-space data on a grid upon succesful reading
+         */
+        GridDataReal3D read_with_meta(std::string filename, MrcMetaData *meta);
+
+        /*! \brief Reads
+         *
+         * \param[in] filename name of the file from which to read the griddata, typically *.cpp4, *.mrc or *.map
+         * \param[in] meta returns the metadata from reading; previous content will be overwritten
+         */
+        void read_meta(std::string filename, MrcMetaData *meta);
+
+
+    private:
+        class Impl;
+        std::unique_ptr<Impl> impl_;
+};
+
+class Df3File
+{
+    private:
+        class SuccessfulDf3Write
+        {
+            public:
+                void writePovray();
+                SuccessfulDf3Write(std::string filename, const GridDataReal3D &grid_data);
+            private:
+                std::string           filename_;
+                const GridDataReal3D &gridData_;
+        };
+    public:
+        SuccessfulDf3Write write(std::string filename, const GridDataReal3D &grid_data);
+};
+
+}      // namespace gmx
+#endif /* end of include guard: GMX_FILEIO_GRIDDATAIO_H_ */
