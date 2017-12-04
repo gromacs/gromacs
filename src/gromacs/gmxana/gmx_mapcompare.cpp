d7d6170a4ad5fa76bb13cef0e5cdb2537d7f32b9
Carsten Kutzner <ckutzne@gwdg.de>
2017-12-04 16:57:53 +0100

    [WIP] Density map toolset for GROMACS
    The idea of this toolset is to assemble functionality to deal with
    density maps as e.g. from cryo electron microscopy. It will include i/o
    routines for density maps (ccp4 and xplor format), a module to allow to
    do fitting to cryo-EM densities during the simulation, as well as an
    analysis tool to compute average densities from a trajectory.
    
    Refs #2282 and #2281
    
    The module that does on-the-fly fitting to density maps is at the moment
    implemented using the correlation-based density fitting potential V =
    k*(1 - c.c.)
    as described by Orzechowski and Tama in Biophys. J. 2008 "Flexible
    Fitting
    of High-Resolution X-Ray Structures into Cryoelectron Microscopy Maps
    Using Biased Molecular Dynamics Simulations". Other alternatives
    can/will
    also be added.
    
    Current functionality summary:
    - deal with CCP4 density maps (read/write/embed to .tpr)
    - spread out discrete atomic positions on a density grid using Gaussian
      functions
    - calculate the correlation coefficient (c.c.) between the experimental
      and the simulated density and derive the corresponding atomic forces
    - read in density fitting .mdp parameters (which index group to submit
      to the potential, what k and sigma to use)
    - allow the user to implement time-dependent fitting protocols
      by giving k and sigma as a function of time (similar to the simulated
      annealing protocol)
    - spreading and force calculation are SIMD-accelerated
    
    This patch originates from a) old C code written in the early days of
    4.6,
    so it does not yet conform to today's coding standards in GROMACS.
    It is working as expected, also in parallel, and is hopefully a good
    starting point for a density fitting force provider module.
    b) from the "tide" trajectory analysis module written by Camilo Aponte
    and
    Rodolfo Briones that computes time-averaged electron density map
    from an MD trajectory.
    
    Everything is highly WIP and lots of things need to be done, some of it
    are:
    - get rid of the dependency on the CCP4 library cmaplib. Christian
      already has code to do that
    - restructure the code, move it to more adequate places in the GROMACS
      source tree
    - implement the mdrun part of the fitting as a proper force provider
    module
    - join the g_map and the "tide" tools into a single trajectory analysis
    module
    - implement a smarter way to do the calculation in parallel, currently
      the grid reduction limits the parallel scaling for large grids
    - implement more tests
    - properly document everything
    - incorporate scaling factors for the different atom types
    - ... ? feel free to add what you think is missing and/or useful
    
    Change-Id: I5f18c33e77d68554456c55c6eedfe6a16d7b3ff0

---
+++ src/gromacs/gmxana/gmx_mapcompare.cpp
@@ -0,0 +1,157 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#include "gmxpre.h"
+
+#include "gromacs/gmxana/gmx_ana.h"
+
+#include "gromacs/commandline/cmdlineparser.h"
+#include "gromacs/commandline/cmdlinehelpcontext.h"
+#include "gromacs/commandline/cmdlinehelpwriter.h"
+#include "gromacs/options/basicoptions.h"
+#include "gromacs/options/filenameoption.h"
+#include "gromacs/options/options.h"
+#include "gromacs/options/optionfiletype.h"
+
+#include "gromacs/math/griddata/griddata.h"
+
+#include "gromacs/fileio/griddataview.h"
+#include "gromacs/fileio/griddataio.h"
+
+#include "gromacs/math/container/containeroperation.h"
+#include "gromacs/math/container/mask.h"
+
+namespace gmx
+{
+
+namespace
+{
+
+//! \brief TODO
+float maskedNormalizedInnerProduct(const GridDataReal3D::container_type &vec1,
+                                   const GridDataReal3D::container_type &vec2,
+                                   GridDataReal3D::value_type            mean1,
+                                   GridDataReal3D::value_type            mean2,
+                                   const Mask                           &mask)
+{
+    double     innerProduct;
+    const auto minMapSize = std::min(vec1.size(), vec2.size());
+#pragma omp parallel shared(innerProduct)
+    {
+
+#pragma omp for reduction(+:innerProduct)
+        for (size_t l = 0; l < minMapSize; l++)
+        {
+            if (mask[l])
+            {
+                innerProduct += (vec1[l]-mean1) * (vec2[l]-mean2);
+            }
+        }
+    }
+
+    return innerProduct;
+}
+
+GridDataReal3D::value_type maskedMean(const GridDataReal3D::container_type &v, const Mask &mask)
+{
+    double sum = 0.;
+#pragma omp parallel shared(sum)
+    {
+#pragma omp for reduction(+:sum)
+        for (size_t l = 0; l < v.size(); ++l)
+        {
+            if (mask[l])
+            {
+                sum += v[l];
+            }
+        }
+    }
+    return sum/v.size();
+}
+
+//! \brief TODO
+float correlationCoefficient(const GridDataReal3D::container_type &map1,
+                             const GridDataReal3D::container_type &map2,
+                             const Mask                           &mask)
+{
+    const auto mean1 = maskedMean(map1, mask);
+    const auto mean2 = maskedMean(map2, mask);
+    return maskedNormalizedInnerProduct(map1, map2, mean1, mean2, mask) / sqrt(maskedNormalizedInnerProduct(map2, map2, mean2, mean2, mask) *
+                                                                               maskedNormalizedInnerProduct(map1, map1, mean1, mean1, mask));
+}
+
+}
+
+int gmx_mapcompare(int argc, char *argv[])
+{
+    std::string desc =
+        "[THISMODULE] calculates the difference between two density maps.";
+
+    std::vector<const char *> bugs =
+    {"This tool is under construction.", ""};
+    Options                   options;
+
+    std::string               fnInput;
+    options.addOption(StringOption("mi").store(&fnInput));
+    std::string               fnCompare;
+    options.addOption(StringOption("compare").store(&fnCompare));
+    std::string               fnMask;
+    options.addOption(StringOption("mask").store(&fnMask).required(false));
+    float                     maskCutOff = 0.01;
+    options.addOption(FloatOption("maskcutoff").store(&maskCutOff).required(false));
+
+    gmx::CommandLineParser(&options).parse(&argc, argv);
+    options.finish();
+
+    const CommandLineHelpContext *context = GlobalCommandLineHelpContext::get();
+    if (context != nullptr)
+    {
+        CommandLineHelpWriter(options).setHelpText(desc)
+            .setKnownIssues(arrayRefFromArray(bugs.data(), bugs.size()))
+            .writeHelp(*context);
+    }
+    Mask mask;
+    if (!fnMask.empty())
+    {
+        mask = Mask(MrcFile().read(fnMask), maskCutOff);
+    }
+    auto map        = MapConverter(fnInput).map();
+    auto compareMap = MapConverter(fnCompare).map();
+
+    fprintf(stdout, "Map correlation coefficient = %g .", correlationCoefficient(map, compareMap, mask));
+
+    return 0;
+}
+
+}
---
+++ src/gromacs/gmxana/gmx_mapnorm.cpp
@@ -0,0 +1,93 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#include "gmxpre.h"
+
+#include "gromacs/gmxana/gmx_ana.h"
+
+#include "gromacs/commandline/cmdlineparser.h"
+#include "gromacs/commandline/cmdlinehelpcontext.h"
+#include "gromacs/commandline/cmdlinehelpwriter.h"
+#include "gromacs/options/basicoptions.h"
+#include "gromacs/options/filenameoption.h"
+#include "gromacs/options/options.h"
+#include "gromacs/options/optionfiletype.h"
+
+#include "gromacs/math/griddata/griddata.h"
+
+#include "gromacs/fileio/griddataview.h"
+#include "gromacs/fileio/griddataio.h"
+
+#include "gromacs/math/container/containeroperation.h"
+#include "gromacs/math/container/mask.h"
+
+
+namespace gmx
+{
+
+int gmx_mapnorm(int argc, char *argv[])
+{
+    std::string desc =
+        "[THISMODULE] normalizes density maps to zero mean and unit standard deviation";
+
+    std::vector<const char *> bugs =
+    {"This tool is under construction.", ""};
+    Options                   options;
+
+    std::string               fnInput;
+    options.addOption(StringOption("mi").store(&fnInput));
+    std::string               fnOutput;
+    options.addOption(StringOption("mo").store(&fnOutput));
+
+    gmx::CommandLineParser(&options).parse(&argc, argv);
+    options.finish();
+
+    const CommandLineHelpContext *context = GlobalCommandLineHelpContext::get();
+    if (context != nullptr)
+    {
+        CommandLineHelpWriter(options).setHelpText(desc)
+            .setKnownIssues(arrayRefFromArray(bugs.data(), bugs.size()))
+            .writeHelp(*context);
+    }
+
+    auto map   = MapConverter(fnInput).map();
+    containeroperation::addScalar(&map, -containermeasure::mean(map));
+    containeroperation::divide(&map, containermeasure::rms(map));
+
+    MapConverter(map).to(fnOutput);
+
+    return 0;
+}
+
+}
