0015fd1ea95b97afe65fda11f4e3d314b16c51e1
Paul Bauer <paul.bauer.q@gmail.com>
2017-12-11 12:53:12 +0100

    Remove PBC before generating TPR with group scheme
    Ensure that all molecules have been made whole before generating the
    run input file when using the group scheme, to avoid error
    messages for large charge groups when molecules are broken
    over PBC boundaries.
    
    Fixes #2339
    
    Change-Id: Iecba013826cbe46e7f70bd674935f9946806ee2e

--- src/gromacs/gmxpreprocess/grompp.cpp
+++ src/gromacs/gmxpreprocess/grompp.cpp
@@ -79,6 +79,7 @@
 #include "gromacs/mdlib/constr.h"
 #include "gromacs/mdlib/genborn.h"
 #include "gromacs/mdlib/perf_est.h"
+#include "gromacs/mdlib/sim_util.h"
 #include "gromacs/mdrunutility/mdmodules.h"
 #include "gromacs/mdtypes/inputrec.h"
 #include "gromacs/mdtypes/md_enums.h"
@@ -645,6 +646,14 @@ new_status(const char *topfile, const char *topppfile, const char *confin,
     set_box_rel(ir, state);
 
     nmismatch = check_atom_names(topfile, confin, sys, &conftop->atoms);
+
+    /* If using the group scheme, make sure charge groups are made whole to avoid errors
+     * in calculating charge group size later on
+     */
+    if (ir->cutoff_scheme == ecutsGROUP && ir->ePBC != epbcNONE)
+    {
+        do_pbc_first_mtop(nullptr, ir->ePBC, state->box, sys, x);
+    }
     done_top(conftop);
     sfree(conftop);
 
