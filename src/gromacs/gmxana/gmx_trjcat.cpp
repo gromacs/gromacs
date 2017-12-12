57b27812a17e5423ecd73187a9a5d7b638209cdc
Erik Lindahl <erik@kth.se>
2017-12-12 01:15:00 +0100

    Disallow ascii formats for gmx trjcat
    Since trjcat (deliberately) does not use any TPR file,
    the tool can't handle trajectory formats such as GRO
    or PDB where atom/residue names are needed.
    
    Fixes #2225.
    
    Change-Id: I55cbfd5f8a3909c1f76e63fa402f0c3243a6f7c7

--- src/gromacs/gmxana/gmx_trjcat.cpp
+++ src/gromacs/gmxana/gmx_trjcat.cpp
@@ -560,11 +560,16 @@ int gmx_trjcat(int argc, char *argv[])
 
     ftpin = fn2ftp(fnms[0]);
 
+    if (ftpin != efTRR && ftpin != efXTC && ftpin != efTNG)
+    {
+        gmx_fatal(FARGS, "gmx trjcat can only handle binary trajectory formats (trr, xtc, tng)");
+    }
+
     for (i = 1; i < nfile_in; i++)
     {
         if (ftpin != fn2ftp(fnms[i]))
         {
-            gmx_fatal(FARGS, "All input files must be of the same format");
+            gmx_fatal(FARGS, "All input files must be of the same (trr, xtc or tng) format");
         }
     }
 
