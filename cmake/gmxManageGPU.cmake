0bbbdcc6f00b25353a5a78ae83bfa19284419d6c
Erik Lindahl <erik@kth.se>
2017-12-15 16:16:02 +0100

    Test that nvcc/host compiler combo works
    Compile a trivial CUDA program during CMake time
    to catch both unsupported nvcc/host compiler
    version combinations and other unknown errors.
    
    Fixes #1616.
    
    Change-Id: I3cc55e4d0db9d6eb01e8a7cd8916cc7a7a1e21fd

--- cmake/gmxManageGPU.cmake
+++ cmake/gmxManageGPU.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
+# Copyright (c) 2012,2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -269,6 +269,33 @@ macro(gmx_gpu_setup)
         if(NOT GMX_OPENMP)
             message(WARNING "To use GPU acceleration efficiently, mdrun requires OpenMP multi-threading. Without OpenMP a single CPU core can be used with a GPU which is not optimal. Note that with MPI multiple processes can be forced to use a single GPU, but this is typically inefficient. You need to set both C and C++ compilers that support OpenMP (CC and CXX environment variables, respectively) when using GPUs.")
         endif()
+
+    # The ability to compile CUDA might be influenced by changes either to the CUDA version,
+    # the CUDA_HOST_COMPILER, the default C/C++ compiler, or compiler flags. To play it safe,
+	# we simply re-run the test that CUDA compiles work, but only echo the result if the
+	# test did not pass before.
+	if(NOT GMX_CUDA_TEST_COMPILER_QUIETLY)
+            message(STATUS "Trying to compile trivial CUDA test program")
+	endif()
+        execute_process(COMMAND ${CUDA_NVCC_EXECUTABLE} -ccbin ${CUDA_HOST_COMPILER} -c ${CUDA_NVCC_FLAGS} ${CMAKE_SOURCE_DIR}/cmake/TestCUDA.cu
+            RESULT_VARIABLE _cuda_test_res
+            OUTPUT_VARIABLE _cuda_test_out
+            ERROR_VARIABLE  _cuda_test_err
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+        if(${_cuda_test_res})
+            message(STATUS "Trying to compile trivial CUDA test program - Failed")
+            if(${_cuda_test_err} MATCHES "nsupported")
+                string(REGEX MATCH "#error[^\n]*" _test_str "${_cuda_test_err}")
+                message(FATAL_ERROR "CUDA/C compiler combination does not seem to be supported. CUDA frequently does not support the latest versions of the host compiler, so you might want to try an earlier C/C++ compiler version and make sure your CUDA compiler and driver are as recent as possible. Error message:\n${_test_str}\n")
+            else()
+                message(FATAL_ERROR "CUDA compiler does not seem to be functional.")
+            endif()
+        elseif(NOT GMX_CUDA_TEST_COMPILER_QUIETLY)
+            message(STATUS "Trying to compile trivial CUDA test program - Success")
+	    set(GMX_CUDA_TEST_COMPILER_QUIETLY TRUE CACHE INTERNAL "Be quiet during next CUDA compiler test")
+        endif()
+
     endif() # GMX_GPU
 
     if (GMX_CLANG_CUDA)
