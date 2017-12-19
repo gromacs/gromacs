f5ad74700092b7813f1d25832a41f1f2ef2790f5
Szilárd Páll <pall.szilard@gmail.com>
2017-12-19 19:20:42 +0100

    Environment variable docs maintenance
    Removed deprecated variables (some ancient, some recent), corrected some
    misspelled ones.
    
    Left a comment with a handy one-liner as docs to discover leftovers.
    
    Refs #2358
    
    Change-Id: Icb80cf661e2a53c5049fe18d687b987fb2c025f1

--- src/gromacs/ewald/pme.cuh
+++ src/gromacs/ewald/pme.cuh
@@ -155,8 +155,8 @@ struct PmeGpuCuda
     /*! \brief A boolean which tells whether the complex and real grids for cuFFT are different or same. Currenty true. */
     bool performOutOfPlaceFFT;
     /*! \brief A boolean which tells if the CUDA timing events are enabled.
-     * True by default, disabled by setting the environment variable GMX_DISABLE_CUDA_TIMING.
-     * FIXME: this should also be disabled if any other GPU task is running concurrently on the same device,
+     *  False by default, can be enabled by setting the environment variable GMX_DISABLE_GPU_TIMING.
+     *  Note: will not be reliable when multiple GPU tasks are running concurrently on the same device context,
      * as CUDA events on multiple streams are untrustworthy.
      */
     bool                                             useTiming;
