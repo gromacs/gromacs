27f0d367bbf2a9f1218354e2e893961486d34b9c
Mark Abraham <mark.j.abraham@gmail.com>
2017-12-24 16:53:27 +1100

    Preliminary cleanup before removing ifdef cplusplus
    Several files used more complex organization, which is best resolved
    manually before we do some scripted refactoring.
    
    Converted two all-vs-all kernels to C++ so that smalloc.h could be
    converted. We'll anyway remove those kernels some time soon.
    
    Change-Id: I49549cff19effa91bf0400ab706d582b13017309

--- src/gromacs/gmxlib/nonbonded/nb_kernel_c/nb_kernel_allvsall.h
+++ src/gromacs/gmxlib/nonbonded/nb_kernel_c/nb_kernel_allvsall.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2014,2015,2017, by the GROMACS development team, led by
+ * Copyright (c) 2012,2014,2015,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -43,10 +43,6 @@
 #include "gromacs/mdtypes/mdatom.h"
 #include "gromacs/mdtypes/nblist.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void
 nb_kernel_allvsall(t_nblist *                nlist,
                    rvec *                    x,
@@ -55,9 +51,5 @@ nb_kernel_allvsall(t_nblist *                nlist,
                    t_mdatoms *               mdatoms,
                    nb_kernel_data_t *        kernel_data,
                    t_nrnb *                  nrnb);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
