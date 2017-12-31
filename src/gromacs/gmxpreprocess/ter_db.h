61e5b7363f232a77ede2e6dbc227a9409110b86a
Erik Lindahl <erik@kth.se>
2017-12-31 15:03:05 +0100

    Don't require matching names between rtp and tdb files
    This was only documented in the source. It's a remnant
    from the days when all force fields were in the same
    directory, and no longer necessary. With this change we
    will properly match all termini to all amino acids.
    
    Fixes #2026, #2027.
    
    Change-Id: Ie7bf8e65892281cc2744146a5525be0c4afdcecf

--- src/gromacs/gmxpreprocess/ter_db.h
+++ src/gromacs/gmxpreprocess/ter_db.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2011,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2011,2014,2015,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -46,10 +46,8 @@ int read_ter_db(const char *ffdir, char ter,
                 t_hackblock **tbptr, gpp_atomtype_t atype);
 /* Read database for N&C terminal hacking */
 
-t_hackblock **filter_ter(int nrtp, t_restp rtp[],
-                         int nb, t_hackblock tb[],
+t_hackblock **filter_ter(int nb, t_hackblock tb[],
                          const char *resname,
-                         const char *rtpname,
                          int *nret);
 /* Return a list of pointers to blocks that match residue name */
 
