2619a2211407fd0b7f8b789d393dbbce995b963f
Erik Lindahl <erik@kth.se>
2017-12-31 14:20:10 +0100

    Allow empty lines in hdb files
    Skip lines that consist only of whitespace. Not a universal
    solution for fixing hdb files, but better than the user
    getting very strange error messages that don't say anything
    about whitespace.
    
    Fixes #2028.
    
    Change-Id: I0962c89d0251f83da7fe0440efd31fb7ca468690

--- src/gromacs/gmxpreprocess/h_db.cpp
+++ src/gromacs/gmxpreprocess/h_db.cpp
@@ -49,6 +49,7 @@
 #include "gromacs/utility/fatalerror.h"
 #include "gromacs/utility/futil.h"
 #include "gromacs/utility/smalloc.h"
+#include "gromacs/utility/stringutil.h"
 
 /* Number of control atoms for each 'add' type.
  *
@@ -150,6 +151,11 @@ static void read_h_db_file(const char *hfn, int *nahptr, t_hackblock **ah)
     aah = *ah;
     while (fgets2(line, STRLEN-1, in))
     {
+        // Skip lines that are only whitespace
+        if (gmx::countWords(line) == 0)
+        {
+            continue;
+        }
         if (sscanf(line, "%s%n", buf, &n) != 1)
         {
             fprintf(stderr, "Error in hdb file: nah = %d\nline = '%s'\n",
