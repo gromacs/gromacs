369ca82760a223fe44e8c6c0a611c80bb05aaf56
Viacheslav Bolnykh <bolnykh@gmail.com>
2018-01-05 18:16:44 +0100

    Implemented QM/MM workflow inside core GROMACS. Fixes Issue #2309
    Implemented MiMiC integrator along with the generation of inter-molecular exclusion lists for QM atoms both in serial and parallel parts of the code
    
    Refs #2309
    
    Change-Id: I35073ad69752e22968c0e2821e2fdf9f0c465b2a

--- src/gromacs/gmxana/gmx_nmr.cpp
+++ src/gromacs/gmxana/gmx_nmr.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -216,7 +216,7 @@ static int get_bounds(const char *topnm,
 
     read_tpx(topnm, ir, box, &natoms, nullptr, nullptr, mtop);
     snew(*ltop, 1);
-    top   = gmx_mtop_generate_local_top(mtop, ir->efep != efepNO);
+    top   = gmx_mtop_generate_local_top(mtop, ir->efep != efepNO, GMX_QMMM_ORIGINAL);
     *ltop = top;
 
     functype = top->idef.functype;
