f9857849fe28f0a2607da46ff72b567ac4fe916c
Erik Lindahl <erik@kth.se>
2018-01-05 11:49:45 +0100

    Add AVX_512 and KNC symbols to FFTW SIMD test
    Otherwise the CMake code might complain loudly about
    FFTW not being accelerated on KNC or KNL hosts.
    
    Change-Id: I888936e9cec18436d775cb3c6eedc21335ab0e95

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
+# Copyright (c) 2012,2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -135,6 +135,8 @@ if (${FFTW}_FOUND)
               ${${FFTW}_FUNCTION_PREFIX}_have_simd_avx2_128
               ${${FFTW}_FUNCTION_PREFIX}_have_simd_avx512
               ${${FFTW}_FUNCTION_PREFIX}_have_simd_avx_128_fma
+              ${${FFTW}_FUNCTION_PREFIX}_have_simd_avx_512
+              ${${FFTW}_FUNCTION_PREFIX}_have_simd_kcvi
               ${${FFTW}_FUNCTION_PREFIX}_have_simd_altivec
               ${${FFTW}_FUNCTION_PREFIX}_have_simd_neon
               ${${FFTW}_FUNCTION_PREFIX}_have_simd_vsx
