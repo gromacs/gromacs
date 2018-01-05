15982106067605c3b4dc4c3aedc1cb2bc3a31712
Viacheslav Bolnykh <bolnykh@gmail.com>
2018-01-05 18:16:44 +0100

    Implemented changes to preprocessor to work with MiMiC QM/MM
    Added new integrator "mimic" and updated usage of QMMM-grps to be used with MiMiC QM/MM. Updated code zeros charges withing QM region and changes bonds to CONNBOND and updates exclusion lists to exclude non-bonded interactions between quantum atoms
    
    Refs #2308
    
    Change-Id: I34e798ae0c54457dad8f9a8f220482468db34708

--- src/gromacs/topology/atoms.h
+++ src/gromacs/topology/atoms.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2012,2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2012,2014,2015,2016,2017, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -152,7 +152,7 @@ void init_t_atoms(t_atoms *atoms, int natoms, gmx_bool bPdbinfo);
 
 void gmx_pdbinfo_init_default(t_pdbinfo *pdbinfo);
 
-t_atoms *copy_t_atoms(const t_atoms *src);
+t_atoms copy_t_atoms(const t_atoms *src);
 /* copy an atoms struct from src to a new one */
 
 void add_t_atoms(t_atoms *atoms, int natom_extra, int nres_extra);
