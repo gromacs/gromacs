b8e60e0ec0d409477b99e86da5f9bf38316bafb2
Aleksei Iupinov <a.yupinov@gmail.com>
2018-01-11 13:04:18 +0100

    Test
    Change-Id: I71a7e50364d52f64ffa99bcd276b4818ba9e61e6

--- src/gromacs/fileio/gmxfio.h
+++ src/gromacs/fileio/gmxfio.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -44,6 +44,10 @@
 #include "gromacs/utility/cstringutil.h"
 #include "gromacs/utility/futil.h"
 #include "gromacs/utility/real.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct t_fileio t_fileio;
 
@@ -176,5 +180,9 @@ int gmx_fio_get_file_md5(t_fileio *fio, gmx_off_t offset,
 
 int xtc_seek_time(t_fileio *fio, real time, int natoms, gmx_bool bSeekForwardOnly);
 
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
