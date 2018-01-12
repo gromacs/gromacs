a4ad9e4eddd48cf2228e257356b96b42ef2bb2e1
Roland Schulz <roland.schulz@intel.com>
2018-01-12 00:46:35 -0800

    Avoid using global variables in programs
    Using global variables defined in libgromacs and
    used from outside causes issues if only lazy
    binding is available.
    
    Change-Id: Id1e960d7c638476d3f7ace339a327040f516f6fc

--- src/gromacs/mdrunutility/repl_ex.h
+++ src/gromacs/mdrunutility/repl_ex.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2011,2012,2013,2014,2015,2017, by the GROMACS development team, led by
+ * Copyright (c) 2011,2012,2013,2014,2015,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -34,7 +34,13 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the research papers on the package. Check out http://www.gromacs.org.
  */
-
+/*! \libinternal \file
+ * \brief
+ * Declares functions for replica exchange.
+ *
+ * \inlibraryapi
+ * \ingroup module_mdrunutility
+ */
 #ifndef _repl_ex_h
 #define _repl_ex_h
 
