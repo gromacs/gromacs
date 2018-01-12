c0bb7db6ce8f1f3a58fb0c1ea3e9aef6e71791a6
Roland Schulz <roland.schulz@intel.com>
2018-01-12 00:46:35 -0800

    Avoid using global variables in programs
    Using global variables defined in libgromacs and
    used from outside causes issues if only lazy
    binding is available.
    
    Move repl_ex to src/gromacs/mdrunutility because
    it uses tmpi and debug.
    
    Change-Id: Id1e960d7c638476d3f7ace339a327040f516f6fc

--- src/programs/mdrun/runner.h
+++ src/programs/mdrun/runner.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015,2017, by the GROMACS development team, led by
+ * Copyright (c) 2015,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -51,10 +51,10 @@
 #include "gromacs/hardware/hw_info.h"
 #include "gromacs/math/vec.h"
 #include "gromacs/mdlib/mdrun.h"
+#include "gromacs/mdrunutility/repl_ex.h"
 #include "gromacs/utility/basedefinitions.h"
 #include "gromacs/utility/real.h"
 
-#include "repl_ex.h"
 
 struct gmx_output_env_t;
 struct ReplicaExchangeParameters;
