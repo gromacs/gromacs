9c9881e29125330439f447bf2e5494460403bee1
Roland Schulz <roland.schulz@intel.com>
2018-01-11 18:00:58 -0800

    Avoid dynamic STL containers in global init
    For global variables (and static class members) using dynamic containers
    causes issues if using LD_PRELOAD to load different libgromacs.
    
    Change-Id: I7bc20b8305fdf5cfedd43d0e1351d9327cdca332

--- src/gromacs/random/tests/tabulatednormaldistribution.cpp
+++ src/gromacs/random/tests/tabulatednormaldistribution.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2015,2016,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -153,7 +153,7 @@ TEST(TabulatedNormalDistributionTest, AltParam)
 
 TEST(TabulatedNormalDistributionTableTest, HasValidProperties)
 {
-    std::vector<real> table = TabulatedNormalDistribution<real>::makeTable();
+    auto table = TabulatedNormalDistribution<real>::makeTable();
 
     EXPECT_EQ(table.size() % 2, 0) << "Table must have even number of entries";
 
