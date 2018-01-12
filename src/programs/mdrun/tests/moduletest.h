0331fcb4f9b938fd859548fd47e3708c44311e93
Aleksei Iupinov <a.yupinov@gmail.com>
2018-01-12 11:55:17 +0100

    Always write the input MDP file for testing just before the grompp call
    This should hopefully help avoid the race issue that was
    briefly observed with the multi-rank PME test
    (the input MDP was occasionally garbled by multiple writes).
    
    Change-Id: Ib624825f79a3dc7b6d3f5e81d8e6b5907f27b35a

--- src/programs/mdrun/tests/moduletest.h
+++ src/programs/mdrun/tests/moduletest.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -133,7 +133,8 @@ class SimulationRunner
         std::string reducedPrecisionTrajectoryFileName_;
         std::string groOutputFileName_;
         std::string ndxFileName_;
-        std::string mdpInputFileName_;
+        std::string mdpInputFileName_;    //!< This file is temporary and will be overwritten with mdpInputContents_ when calling grompp
+        std::string mdpInputContents_;
         std::string mdpOutputFileName_;
         std::string tprFileName_;
         std::string logFileName_;
