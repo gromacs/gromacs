c04a3a9e158d7b41f1c0b61d5314497e03669574
Roland Schulz <roland.schulz@intel.com>
2018-01-12 00:46:35 -0800

    Avoid using global variables in programs
    Using global variables defined in libgromacs and
    used from outside causes issues if only lazy
    binding is available.
    
    Move repl_ex to src/gromacs/mdrunutility because
    it uses tmpi and debug.
    
    Change-Id: Id1e960d7c638476d3f7ace339a327040f516f6fc

--- src/gromacs/gmxpreprocess/insert-molecules.cpp
+++ src/gromacs/gmxpreprocess/insert-molecules.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -606,9 +606,20 @@ int InsertMolecules::run()
 
 }   // namespace
 
-const char InsertMoleculesInfo::name[]             = "insert-molecules";
-const char InsertMoleculesInfo::shortDescription[] =
-    "Insert molecules into existing vacancies";
+
+const char* InsertMoleculesInfo::name()
+{
+    static const char* name = "insert-molecules";
+    return name;
+}
+
+const char* InsertMoleculesInfo::shortDescription()
+{
+    static const char* shortDescription =
+        "Insert molecules into existing vacancies";
+    return shortDescription;
+}
+
 ICommandLineOptionsModulePointer InsertMoleculesInfo::create()
 {
     return ICommandLineOptionsModulePointer(new InsertMolecules());
