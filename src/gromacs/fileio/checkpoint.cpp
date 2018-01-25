e8356b56c2e2a0a6a58a55e1d9869349ebee24c8
Mark Abraham <mark.j.abraham@gmail.com>
2018-01-25 09:34:45 +0100

    Fix bug in checkpoint error checking
    Change-Id: I91a185913356a42949596d8f4ca34ec2394c24a5

--- src/gromacs/fileio/checkpoint.cpp
+++ src/gromacs/fileio/checkpoint.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2008,2009,2010,2011,2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -2879,7 +2879,7 @@ void list_checkpoint(const char *fn, FILE *out)
 
     if (ret == 0)
     {
-        do_cpt_files(gmx_fio_getxdr(fp), TRUE, &outputfiles, &nfiles, out, file_version);
+        ret = do_cpt_files(gmx_fio_getxdr(fp), TRUE, &outputfiles, &nfiles, out, file_version);
     }
 
     if (ret == 0)
