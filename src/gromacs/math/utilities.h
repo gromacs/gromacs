65b4a8531901b630a3a0cfd10f85e80134259100
Aleksei Iupinov <a.yupinov@gmail.com>
2018-02-01 17:16:52 +0100

    Add a basic TPI sanity test
    The floating point exceptions are for now toggled off
    in the test body instead of mdrun itself.
    
    Refs #2391
    
    Change-Id: I41b584f4753b9b0e126256a94c1304f0a870aa73

--- src/gromacs/math/utilities.h
+++ src/gromacs/math/utilities.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013-2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -164,6 +164,9 @@ gmx_greatest_common_divisor(int p, int q);
  * Enables division-by-zero, invalid, and overflow.
  */
 int gmx_feenableexcept();
+
+/*! \brief Disable floating-point exceptions if supported on OS */
+int gmx_fedisableexcept();
 
 /*! \brief Return cut-off to use
  *
