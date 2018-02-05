9a65aa8ec33ba85ef12e0d83a094145aa2f50825
Berk Hess <hess@kth.se>
2018-02-05 10:38:22 +0100

    Fix assertion failure in TPI
    Fixes #2398
    
    Change-Id: I89ddde7bdb6c061411160009df1f87e7c2cef0a1

--- src/gromacs/mdlib/tpi.cpp
+++ src/gromacs/mdlib/tpi.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -291,7 +291,7 @@ double do_tpi(FILE *fplog, t_commrec *cr, const gmx::MDLogger gmx_unused &mdlog,
     /* We need to allocate one element extra, since we might use
      * (unaligned) 4-wide SIMD loads to access rvec entries.
      */
-    f.resize(top_global->natoms + 1);
+    f.resize(gmx::paddedRVecVectorSize(top_global->natoms));
 
     /* Print to log file  */
     walltime_accounting_start(walltime_accounting);
