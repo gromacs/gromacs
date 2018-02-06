df419900bfc9ab55dd5bff2300f97557fefc441d
Aleksei Iupinov <a.yupinov@gmail.com>
2018-02-06 16:29:43 +0100

    Zero unused values to silence the warning
    Change-Id: I8419a823e9a94434c3c534da4bf98ca79ff6ead7

--- src/gromacs/gmxlib/nonbonded/nb_generic.cpp
+++ src/gromacs/gmxlib/nonbonded/nb_generic.cpp
@@ -128,6 +128,11 @@ gmx_nb_generic_kernel(t_nblist *                nlist,
         ewtabscale          = ic->tabq_scale;
         ewtabhalfspace      = 0.5/ewtabscale;
     }
+    else
+    {
+        ewtab          = nullptr;
+        ewtabhalfspace = ewtabscale = 0;
+    }
 
     rcoulomb2           = ic->rcoulomb*ic->rcoulomb;
     rvdw                = ic->rvdw;
