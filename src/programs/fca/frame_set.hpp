64941f84a917c5f862d5728354d606f1396457dc
Berenger Bramas <berenger.bramas@mpcdf.mpg.de>
2018-02-07 14:15:17 +0100

    Added Full Correlation Analysis (FCA)
    FCA of a trajectory follows the spirit of PCA in finding important modes of
    motion through an orthonormal coordinate transformation and subsequent
    sorting of basis vectors.
    It has been developed inside the trajectoryanlysis framework.
    Orignal code from Oliver Lange, then ported to gromacs4.6 by Christian Blau,
    and current patch by Berenger Bramas.
    
    Fixes #2005
    
    Implementation according to:
    
    Lange, Oliver F., and Helmut Grubmüller. "Full correlation analysis of conformational protein dynamics."
    Proteins: Structure, Function, and Bioinformatics 70.4 (2008): 1294-1312.
    
    Change-Id: Id9f8a59a82d9ae2d0ec2a49a19de7363d13c9a44

---
+++ src/programs/fca/frame_set.hpp
@@ -0,0 +1,70 @@
+#ifndef FRAME_SET_HPP
+#define FRAME_SET_HPP
+
+#include <vector>
+#include "utils.hpp"
+#include "gromacs/fileio/trxio.h"
+
+struct gmx_output_env_t;
+
+namespace FCA{
+
+
+class FrameSet {
+    std::vector< std::unique_ptr< rvec[] > > frames;
+    std::vector<real> t_frames;
+    matrix box;
+    int nbAtoms;
+
+public:
+    explicit FrameSet(const char* trajfile,
+                      const gmx_output_env_t* oenv,
+                      const int load_step = 1)
+            : t_frames(0), nbAtoms(0){
+        if(trajfile == nullptr){
+            return;
+        }
+
+        t_trxstatus* status;
+        rvec* x = nullptr;
+        real t_frame;
+        nbAtoms = read_first_x(oenv, &status, trajfile, &t_frame, &x, box);
+
+        int idxFrame = 0;
+        do {
+            if(idxFrame % load_step == 0) {
+                std::unique_ptr< rvec[] > currentFrame(new rvec[nbAtoms]);
+                copy_rvecn(x, currentFrame.get(), 0, nbAtoms);
+                frames.emplace_back(std::move(currentFrame));
+                t_frames.emplace_back(t_frame);
+            }
+            idxFrame += 1;
+        } while(read_next_x(oenv, status, &t_frame, /*nbAtoms,*/ x, box));
+
+        close_trx(status);
+    }
+
+    int getNbAtoms() const{
+        return nbAtoms;
+    }
+
+    const matrix& getBox() const{
+        return box;
+    }
+
+    real getTimeFrame() const{
+        return t_frames.front();
+    }
+
+    const std::vector< std::unique_ptr< rvec[] > >& getFrames() const{
+        return frames;
+    }
+
+    std::vector< std::unique_ptr< rvec[] > > releaseFrames() {
+        return std::move(frames);
+    }
+};
+
+}
+
+#endif
