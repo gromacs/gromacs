fa3593ec6b33ee61eaa51938891dadc6435a0534
Berk Hess <hess@kth.se>
2018-02-07 11:03:46 +0100

    Correct multisim MPI barrier
    An MPI barrier used with multisim was called on incorrect ranks
    and a barrier was missing.
    
    Fixes #2403
    
    Change-Id: I06a66167afc8dca9cdd4ca9b4f9a806984a6ec7a

--- src/programs/mdrun/runner.cpp
+++ src/programs/mdrun/runner.cpp
@@ -1042,7 +1042,14 @@ int Mdrunner::mdrunner()
     }
     if (MULTISIM(cr))
     {
-        MPI_Barrier(cr->ms->mpi_comm_masters);
+        if (MASTER(cr))
+        {
+            MPI_Barrier(cr->ms->mpi_comm_masters);
+        }
+        /* We need another barrier to prevent non-master ranks from contiuing
+         * when an error occured in a different simulation.
+         */
+        MPI_Barrier(cr->mpi_comm_mysim);
     }
 #endif
 
