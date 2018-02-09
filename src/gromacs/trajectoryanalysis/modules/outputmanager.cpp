8ddff022ee403308ca44ff2ba77d679e0b0a8c7b
Paul Bauer <paul.bauer.q@gmail.com>
2018-02-09 16:03:55 +0100

    [RFC] Simple trajectory converter
    Very simple test case for trajectory converter.
    
    Aimed at having a test case that bypasses the analysisdata framework.
    
    Reduces scope further and only opens new file and writes out, nothing
    else.
    
    Refs #2115
    
    Change-Id: Ie91d620c0ea25b7436f5745626b973ec97175d29

---
+++ src/gromacs/trajectoryanalysis/modules/outputmanager.cpp
@@ -0,0 +1,222 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Helper classe for handling output of trajectory data to files.
+ *
+ * \author
+ * \ingroup module_trajectoryanalysis
+ */
+
+
+#include "gmxpre.h"
+
+#include "outputmanager.h"
+
+#include <algorithm>
+
+#include "gromacs/fileio/filetypes.h"
+#include "gromacs/fileio/gmxfio.h"
+#include "gromacs/fileio/trxio.h"
+#include "gromacs/math/vec.h"
+#include "gromacs/options/basicoptions.h"
+#include "gromacs/options/filenameoption.h"
+#include "gromacs/options/ioptionscontainer.h"
+#include "gromacs/pbcutil/pbc.h"
+#include "gromacs/selection/selection.h"
+#include "gromacs/selection/selectionoption.h"
+#include "gromacs/topology/mtop_util.h"
+#include "gromacs/topology/topology.h"
+#include "gromacs/trajectory/trajectoryframe.h"
+#include "gromacs/trajectoryanalysis/analysissettings.h"
+#include "gromacs/utility/arrayref.h"
+#include "gromacs/utility/exceptions.h"
+#include "gromacs/utility/fatalerror.h"
+#include "gromacs/utility/gmxassert.h"
+#include "gromacs/utility/stringutil.h"
+#include "gromacs/utility/unique_cptr.h"
+
+namespace gmx
+{
+
+void
+OutputManager::checkOptions()
+{
+}
+
+void
+OutputManager::setFiletype()
+{
+    if (!name_.empty())
+    {
+        filetype_ = fn2ftp(name_.c_str());
+    }
+    else
+    {
+        GMX_THROW(InvalidInputError("Can not open file with an empty name"));
+    }
+}
+
+void
+OutputManager::closeFile()
+{
+    if (trr_ != nullptr)
+    {
+        close_trx(trr_);
+    }
+    trr_ = nullptr;
+}
+
+t_trxstatus *
+OutputManager::trjOpenTng() const
+{
+    ArrayRef<const int> index     = sel_->atomIndices();
+    int                 natoms    = sel_->atomCount();
+    const char         *indexname = sel_->name();
+    if (!haveMtop() && !haveAtoms())
+    {
+        GMX_THROW(InvalidInputError("Need either atoms or topology information to write TNG file"));
+    }
+    gmx_mtop_t *mtop = nullptr;
+    if (haveMtop())
+    {
+        mtop      = const_cast<gmx_mtop_t*>(mtop_);
+    }
+    else if (haveAtoms())
+    {
+        convertAtomsToMtop(nullptr, nullptr, atoms_, mtop);
+    }
+
+
+    int                 size = index.size();
+    int                *localindex;
+    snew(localindex, size);
+    int                 runner = 0;
+
+    for (const int *ai = index.begin(); ai != index.end(); ++ai)
+    {
+        localindex[runner++] = *ai;
+    }
+
+    t_trxstatus *out =
+        trjtools_gmx_prepare_tng_writing(name_.c_str(),
+                                         filemode_[0],
+                                         nullptr, //infile_, //how to get the input file here?
+                                         nullptr,
+                                         natoms,
+                                         mtop,
+                                         localindex,
+                                         indexname);
+
+    sfree(localindex);
+    return out;
+}
+
+t_trxstatus *
+OutputManager::trjOpenTrr() const
+{
+    return open_trx(name_.c_str(), filemode_.c_str());
+}
+
+t_trxstatus *
+OutputManager::trjOpenPdb() const
+{
+    if (!haveAtoms())
+    {
+        GMX_THROW(InvalidInputError("Need trajectory frame with atom names for pdb file writing"));
+    }
+    return open_trx(name_.c_str(), filemode_.c_str());
+}
+
+t_trxstatus *
+OutputManager::trjOpenGro() const
+{
+    if (!haveAtoms())
+    {
+        GMX_THROW(InvalidInputError("Need trajectory frame with atom names for pdb file writing"));
+    }
+    return open_trx(name_.c_str(), filemode_.c_str());
+}
+
+bool
+OutputManager::haveAtoms() const
+{
+    return atoms_ != nullptr ? true : false;
+}
+
+bool
+OutputManager::haveMtop() const
+{
+    return mtop_ != nullptr ? true : false;
+}
+
+void
+OutputManager::initOutput()
+{
+    setFiletype();
+    switch (filetype_)
+    {
+        case (efTNG):
+            trr_ = trjOpenTng();
+            break;
+        case (efPDB):
+            trr_ = trjOpenPdb();
+            break;
+        case (efGRO):
+            trr_ = trjOpenGro();
+            break;
+        case (efTRR):
+        case (efXTC):
+        case (efG96):
+            trr_ = trjOpenTrr();
+            break;
+        default:
+            GMX_THROW(InvalidInputError("Invalid file type"));
+    }
+}
+
+void
+OutputManager::writeFrame(const t_trxframe write) const
+{
+    t_trxframe        local  = (*const_cast<t_trxframe*>(&write));
+
+    write_trxframe(trr_, &local, nullptr);
+}
+
+
+
+
+
+} // namespace gmx
