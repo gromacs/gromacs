ab4d87ef1b255504d23b619e57d2e98374ba2b3e
Aleksei Iupinov <a.yupinov@gmail.com>
2018-02-09 12:14:19 +0100

    Revert "Enable debug FP exceptions without TPI, not with TPI"
    This reverts commit 9aca3e2865d0cae8283df00f20fbe4b33927d854 until 
    most issues at #2404 are presumed to be fixed.
    
    Change-Id: I5c3b248f2f265794837a6cf980ed4917108a54bb

--- src/programs/mdrun/runner.cpp
+++ src/programs/mdrun/runner.cpp
@@ -947,8 +947,7 @@ int Mdrunner::mdrunner()
                           inputrec->cutoff_scheme == ecutsVERLET);
 
 #ifndef NDEBUG
-    //FIXME - reconcile with gmx_feenableexcept() call from CommandLineModuleManager::run()
-    if (!EI_TPI(inputrec->eI) &&
+    if (EI_TPI(inputrec->eI) &&
         inputrec->cutoff_scheme == ecutsVERLET)
     {
         gmx_feenableexcept();
