f5b5ec3c90b7f039316cdae39ba7a381aa889de9
Berk Hess <hess@kth.se>
2018-02-13 14:30:22 +0100

    Convert gmx_mtop_t to C++
    gmx_mtop_t now uses std vectors for moltype and molblock.
    gmx_mtop_t, gmx_moltype_t and gmx_molblock_t now have constructors
    and destructors.
    Several sub-structs still use C style pointers and will be converted
    later. This change introduces new and delete in some spots to minimize
    the size of this already large change.
    
    Change-Id: I584c5a8fa7b1e2615f66d3350f66887cf1a590c4

--- src/gromacs/gmxana/gmx_energy.cpp
+++ src/gromacs/gmxana/gmx_energy.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -312,7 +312,6 @@ static void get_dhdl_parms(const char *topnm, t_inputrec *ir)
 
     /* all we need is the ir to be able to write the label */
     read_tpx(topnm, ir, box, &natoms, nullptr, nullptr, &mtop);
-    done_mtop(&mtop);
 }
 
 static void einstein_visco(const char *fn, const char *fni, int nsets,
--- src/gromacs/topology/mtop_util.h
+++ src/gromacs/topology/mtop_util.h
@@ -120,7 +120,7 @@ gmx_mtop_atomloop_all_names(gmx_mtop_atomloop_all_t aloop,
  */
 void
 gmx_mtop_atomloop_all_moltype(gmx_mtop_atomloop_all_t aloop,
-                              gmx_moltype_t **moltype, int *at_mol);
+                              const gmx_moltype_t **moltype, int *at_mol);
 
 
 /* Abstract type for atom loop over atoms in all molecule blocks */
@@ -165,7 +165,7 @@ gmx_mtop_ilistloop_init(const gmx_mtop_t *mtop);
  */
 gmx_bool
 gmx_mtop_ilistloop_next(gmx_mtop_ilistloop_t iloop,
-                        t_ilist **ilist_mol, int *nmol);
+                        const t_ilist **ilist_mol, int *nmol);
 
 
 /* Abstract type for ilist loop over all ilists of all molecules */
@@ -187,7 +187,7 @@ gmx_mtop_ilistloop_all_init(const gmx_mtop_t *mtop);
  */
 gmx_bool
 gmx_mtop_ilistloop_all_next(gmx_mtop_ilistloop_all_t iloop,
-                            t_ilist **ilist_mol, int *atnr_offset);
+                            const t_ilist **ilist_mol, int *atnr_offset);
 
 
 /* Returns the total number of interactions in the system of type ftype */
@@ -219,6 +219,8 @@ gmx::BlockRanges gmx_mtop_molecules(const gmx_mtop_t &mtop);
 
 /* Converts a gmx_mtop_t struct to t_topology.
  *
+ * If the lifetime of the returned topology should be longer than that
+ * of mtop, your need to pass freeMtop==true.
  * If freeMTop == true, memory related to mtop will be freed so that done_top()
  * on the result value will free all memory.
  * If freeMTop == false, mtop and the return value will share some of their
