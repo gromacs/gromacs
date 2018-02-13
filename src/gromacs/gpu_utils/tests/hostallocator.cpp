ea874de9d962491254ba9fe7d7731164eb3eadab
Mark Abraham <mark.j.abraham@gmail.com>
2018-02-13 17:11:19 +0100

    Use death test only when supported
    In build farms for bioconda and DebiChem, some environments don't
    support death tests, so hopefully if we use the GoogleTest machinery
    to avoid using them when not supported we can avoid issues in such
    environments.
    
    Refs #2405
    
    Change-Id: I17efe3b620b162d615b91a5f5cd4c8bd458143a5

--- src/gromacs/gpu_utils/tests/hostallocator.cpp
+++ src/gromacs/gpu_utils/tests/hostallocator.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2017, by the GROMACS development team, led by
+ * Copyright (c) 2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -311,8 +311,8 @@ TYPED_TEST(HostAllocatorTest, ManualPinningOperationsWorkWithCuda)
 TYPED_TEST(HostAllocatorTest, ChangingPinningPolicyRequiresCuda)
 {
     typename TestFixture::VectorType input;
-    EXPECT_DEATH(changePinningPolicy(&input, PinningPolicy::CanBePinned),
-                 ".*A suitable build of GROMACS.* is required.*");
+    EXPECT_DEATH_IF_SUPPORTED(changePinningPolicy(&input, PinningPolicy::CanBePinned),
+                              ".*A suitable build of GROMACS.* is required.*");
 }
 
 TYPED_TEST(HostAllocatorTest, ManualPinningOperationsWorkEvenWithoutCuda)
