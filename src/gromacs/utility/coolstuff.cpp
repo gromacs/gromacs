03cb7abaceeb2f5227d996e0846a17ea5ead1a29
Aleksei Iupinov <a.yupinov@gmail.com>
2018-02-15 15:19:33 +0100

    Add the missing comma in the quote
    This was printed as:
    GROMACS reminds you: "It's just B I O L O G Y, can't you see?Joe Jackson" ((null))
    
    Change-Id: I69e86ecda8930fffeeab3a54f2f015339fd9b345

--- src/gromacs/utility/coolstuff.cpp
+++ src/gromacs/utility/coolstuff.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -736,7 +736,7 @@ std::string getCoolQuote()
         { "They don't have half hours in the north", "Carl Caleman" },
         { "Safety lights are for dudes", "Ghostbusters 2016" },
         { "It's 2040 now. Our President is a plant.",  "Ghostbusters 2016" },
-        { "It's just B I O L O G Y, can't you see?" "Joe Jackson" },
+        { "It's just B I O L O G Y, can't you see?", "Joe Jackson" },
         { "Input, output, electricity", "Joni Mitchell" },
         { "Your daddy ain't your daddy but your daddy don't know", "Dalahan" },
         { "Why is the Earth moving 'round the sun? Floating in the vacuum with no purpose, not a one", "Fleet Foxes" },
