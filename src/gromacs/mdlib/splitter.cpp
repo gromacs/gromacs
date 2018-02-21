b71116bbeeae87004eb19345e8fa6b8a2967c374
Mark Abraham <mark.j.abraham@gmail.com>
2018-02-21 16:02:46 +0100

    Introduce constrain_shake
    Also move various functions and fields of general constraint code and
    data to the SHAKE code and data structures. Made some use of inputrec
    const, to suit later changes.
    
    Change-Id: I09b7a719b58f8f8192762431edc139674eaf23ae

--- src/gromacs/mdlib/splitter.cpp
+++ src/gromacs/mdlib/splitter.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -355,7 +355,7 @@ static int merge_sid(int at_start, int at_end, int nsid, t_sid sid[],
 }
 
 void gen_sblocks(FILE *fp, int at_start, int at_end,
-                 t_idef *idef, t_blocka *sblock,
+                 const t_idef *idef, t_blocka *sblock,
                  gmx_bool bSettle)
 {
     t_graph *g;
--- src/gromacs/mdlib/splitter.h
+++ src/gromacs/mdlib/splitter.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2010,2014,2015, by the GROMACS development team, led by
+ * Copyright (c) 2010,2014,2015,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -45,7 +45,7 @@ struct t_blocka;
 struct t_idef;
 
 void gen_sblocks(FILE *fp, int at_start, int at_end,
-                 t_idef *idef, t_blocka *sblock,
+                 const t_idef *idef, t_blocka *sblock,
                  gmx_bool bSettle);
 /* Generate shake blocks from the constraint list. Set bSettle to yes for shake
  * blocks including settles. You normally do not want this.
