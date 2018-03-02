1efdc469e849c19b4c4517a681a53ad44a164977
Szilárd Páll <pall.szilard@gmail.com>
2018-03-02 17:58:41 +0100

    Add Aleksei Iupinov to the contributor section
    Change-Id: I1642f1ca247cf0e11781af4848b74e3d0ebd5082

--- src/gromacs/utility/binaryinformation.cpp
+++ src/gromacs/utility/binaryinformation.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -107,12 +107,14 @@ void printCopyright(gmx::TextWriter *writer)
     static const char * const Contributors[] = {
         "Emile Apol",
         "Rossen Apostolov",
+        "Paul Bauer",
         "Herman J.C. Berendsen",
         "Par Bjelkmar",
         "Aldert van Buuren",
         "Rudi van Drunen",
         "Anton Feenstra",
         "Gerrit Groenhof",
+        "Aleksei Iupinov",
         "Christoph Junghans",
         "Anca Hamuraru",
         "Vincent Hindriksen",
