33093601ff229ed8c8a40e73866a7ff351e8963b
Mark Abraham <mark.j.abraham@gmail.com>
2018-03-02 11:22:42 +0100

    Fixed dvdlambda for SHAKE + FE
    Garbage values have been accumulated to dvdlambda whenever SHAKE was
    used.
    
    Refactoring in 513813b514 removed the lam variable. Previously, it was
    used as a temporary iteration variable over SHAKE blocks, but that
    refactoring to use the main variable changed the logic of the
    following loop.
    
    Fixes #2434
    
    Change-Id: Ia9642f50358ab31ec98f8317f7a1dcda8622a9f1

--- src/gromacs/mdlib/constr.h
+++ src/gromacs/mdlib/constr.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -96,7 +96,7 @@ gmx_bool bshakef(FILE           *log,          /* Log file			*/
                  rvec            x_s[],        /* Coords before update		*/
                  rvec            prime[],      /* Output coords		*/
                  t_nrnb         *nrnb,         /* Performance measure          */
-                 real           *lagr,         /* The Lagrange multipliers     */
+                 real * const    lagr,         /* The Lagrange multipliers     */
                  real            lambda,       /* FEP lambda                   */
                  real           *dvdlambda,    /* FEP force                    */
                  real            invdt,        /* 1/delta_t                    */
