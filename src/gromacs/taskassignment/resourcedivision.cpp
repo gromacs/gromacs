13956ec8fb9f426854958e63bfde96dd4ff2ec99
Berk Hess <hess@kth.se>
2018-03-14 09:59:37 +0100

    Fix multi-sim with one simulation
    The corner case of multi-sim with only a single simulation running
    on a single MPI rank would trigger an MPI error.
    
    Fixes #2448
    
    Change-Id: Ieffa1b3bcc23396751490639643502a7fc84a375

--- src/gromacs/taskassignment/resourcedivision.cpp
+++ src/gromacs/taskassignment/resourcedivision.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -889,7 +889,7 @@ void checkHardwareOversubscription(int                          numThreadsOnThis
     int numRanksOnThisNode   = 1;
     int numThreadsOnThisNode = numThreadsOnThisRank;
 #if GMX_MPI
-    if (PAR(cr) || MULTISIM(cr))
+    if (PAR(cr) || (MULTISIM(cr) && cr->ms->nsim > 1))
     {
         /* Count the threads within this physical node */
         MPI_Comm_size(cr->mpi_comm_physicalnode, &numRanksOnThisNode);
