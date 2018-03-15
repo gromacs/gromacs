gromacs/gromacs

commit e4deba126d8a909a1a68eb4b38d0d722fc34a587

Improve inter-simulation signaling
Use a single boolean for controlling inter-simulation signaling.
This would have prevented issue #2447.
Introduce an inter-simulating signaling frequency. This both
simplifies the code and significantly reduces communiation overhead
for many simulations and/or many ranks per simulation.
Correct the terminimation step print with replica exchange.

Refs #2440 and #2447

Change-Id: I8ed50c5b11564d9a967bcacb15c4fcd3be05caea

src/programs/mdrun/md.cpp
@@ -322,7 +322,7 @@ double gmx::do_md(FILE *fplog, t_commrec *cr, const gmx::MDLogger &mdlog,
     gmx_bool        bGStatEveryStep, bGStat, bCalcVir, bCalcEnerStep, bCalcEner;
     gmx_bool        bNS, bNStList, bSimAnn, bStopCM,
                     bFirstStep, bInitStep, bLastStep = FALSE,
-                    bBornRadii, bUsingEnsembleRestraints;
+                    bBornRadii;
     gmx_bool          bDoDHDL = FALSE, bDoFEP = FALSE, bDoExpanded = FALSE;
     gmx_bool          do_ene, do_log, do_verbose, bRerunWarnNoV = TRUE,
                       bForceUpdate = FALSE, bCPT;
@@ -915,23 +915,34 @@ double gmx::do_md(FILE *fplog, t_commrec *cr, const gmx::MDLogger &mdlog,
     bSumEkinhOld     = FALSE;
     bExchanged       = FALSE;
     bNeedRepartition = FALSE;
-    // TODO This implementation of ensemble orientation restraints is nasty because
-    // a user can't just do multi-sim with single-sim orientation restraints.
-    bUsingEnsembleRestraints = (fcd->disres.nsystems > 1) || (cr->ms && fcd->orires.nr);
-    bool awhUsesMultiSim     = (ir->bDoAwh && ir->awhParams->shareBiasMultisim && cr->ms);
+
+    bool simulationsShareState = false;
+    int  nstSignalComm         = nstglobalcomm;
     {
+        // TODO This implementation of ensemble orientation restraints is nasty because
+        // a user can't just do multi-sim with single-sim orientation restraints.
+        bool usingEnsembleRestraints = (fcd->disres.nsystems > 1) || (cr->ms && fcd->orires.nr);
+        bool awhUsesMultiSim         = (ir->bDoAwh && ir->awhParams->shareBiasMultisim && cr->ms);
+
         // Replica exchange, ensemble restraints and AWH need all
         // simulations to remain synchronized, so they need
         // checkpoints and stop conditions to act on the same step, so
         // the propagation of such signals must take place between
         // simulations, not just within simulations.
         // TODO: Make algorithm initializers set these flags.
-        bool checkpointIsLocal    = !useReplicaExchange && !bUsingEnsembleRestraints && !awhUsesMultiSim;
-        bool stopConditionIsLocal = !useReplicaExchange && !bUsingEnsembleRestraints && !awhUsesMultiSim;
+        simulationsShareState     = useReplicaExchange || usingEnsembleRestraints || awhUsesMultiSim;
         bool resetCountersIsLocal = true;
-        signals[eglsCHKPT]         = SimulationSignal(checkpointIsLocal);
-        signals[eglsSTOPCOND]      = SimulationSignal(stopConditionIsLocal);
+        signals[eglsCHKPT]         = SimulationSignal(simulationsShareState);
+        signals[eglsSTOPCOND]      = SimulationSignal(simulationsShareState);
         signals[eglsRESETCOUNTERS] = SimulationSignal(resetCountersIsLocal);
+
+        if (simulationsShareState)
+        {
+            // Inter-simulation signal communication does not need to happen
+            // often, so we use a minimum of 200 steps to reduce overhead.
+            const int c_minimumInterSimulationSignallingInterval = 200;
+            nstSignalComm = ((c_minimumInterSimulationSignallingInterval + nstglobalcomm - 1)/nstglobalcomm)*nstglobalcomm;
+        }
     }
 
     DdOpenBalanceRegionBeforeForceComputation ddOpenBalanceRegion   = (DOMAINDECOMP(cr) ? DdOpenBalanceRegionBeforeForceComputation::yes : DdOpenBalanceRegionBeforeForceComputation::no);
@@ -1458,7 +1469,7 @@ double gmx::do_md(FILE *fplog, t_commrec *cr, const gmx::MDLogger &mdlog,
                  * to allow for exact continuation, when possible.
                  */
                 signals[eglsSTOPCOND].sig = 1;
-                nsteps_stop               = std::max(ir->nstlist, 2*nstglobalcomm);
+                nsteps_stop               = std::max(ir->nstlist, 2*nstSignalComm);
             }
             else if (gmx_get_stop_condition() == gmx_stop_cond_next)
             {
@@ -1466,7 +1477,7 @@ double gmx::do_md(FILE *fplog, t_commrec *cr, const gmx::MDLogger &mdlog,
                  * This breaks exact continuation.
                  */
                 signals[eglsSTOPCOND].sig = -1;
-                nsteps_stop               = nstglobalcomm + 1;
+                nsteps_stop               = nstSignalComm + 1;
             }
             if (fplog)
             {
@@ -1683,7 +1694,7 @@ double gmx::do_md(FILE *fplog, t_commrec *cr, const gmx::MDLogger &mdlog,
         {
             // Organize to do inter-simulation signalling on steps if
             // and when algorithms require it.
-            bool doInterSimSignal = (!bFirstStep && bDoReplEx) || bUsingEnsembleRestraints || awhUsesMultiSim;
+            bool doInterSimSignal = (simulationsShareState && do_per_step(step, nstSignalComm));
 
             if (bGStat || (!EI_VV(ir->eI) && do_per_step(step+1, nstglobalcomm)) || doInterSimSignal)
             {

commit 90a08fb6d011bcdaa314efcd0fff6688f7d33069

Fix recent mdrun signaling bug
Recent commit e4deba12 accidentally negated the locality condition
for signaling checkpoint and termination in mdrun.

Fixes #2457

Change-Id: I1b7a4c26637766a44cad2581c9c9ec420aa4ab6e

src/programs/mdrun/md.cpp
@@ -932,8 +932,8 @@ double gmx::do_md(FILE *fplog, t_commrec *cr, const gmx::MDLogger &mdlog,
         // TODO: Make algorithm initializers set these flags.
         simulationsShareState     = useReplicaExchange || usingEnsembleRestraints || awhUsesMultiSim;
         bool resetCountersIsLocal = true;
-        signals[eglsCHKPT]         = SimulationSignal(simulationsShareState);
-        signals[eglsSTOPCOND]      = SimulationSignal(simulationsShareState);
+        signals[eglsCHKPT]         = SimulationSignal(!simulationsShareState);
+        signals[eglsSTOPCOND]      = SimulationSignal(!simulationsShareState);
         signals[eglsRESETCOUNTERS] = SimulationSignal(resetCountersIsLocal);
 
         if (simulationsShareState)