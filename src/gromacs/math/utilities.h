e5655ad0a18a8c1724e8ced5eacd5e92347486b6
Aleksei Iupinov <a.yupinov@gmail.com>
2018-03-16 11:18:47 +0100

    Stop gmx_feenableexcept() from returning previous exception settings on Apple
    This makes the function return value unambiguous.
    
    Change-Id: I3e64e769b27fb0fa59b650d18b2fd099a9181025

--- src/gromacs/math/utilities.h
+++ src/gromacs/math/utilities.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -161,7 +161,9 @@ gmx_greatest_common_divisor(int p, int q);
 
 /*! \brief Enable floating-point exceptions if supported on OS
  *
- * Enables division-by-zero, invalid, and overflow.
+ * Enables division-by-zero, invalid value, and overflow.
+ *
+ * \returns 0 if successful in enabling exceptions, anything else in case of failure/unsupported OS.
  */
 int gmx_feenableexcept();
 
