394b8b5445ee815ee0631a917631c5268d0dc0f3
Mark Abraham <mark.j.abraham@gmail.com>
2018-03-16 22:21:48 +0100

    Fix VSX SIMD with gcc 8
    gcc 8 apparently generates different code for the former GROMACS
    code, which seems buggy. See
    https://bugzilla.redhat.com/show_bug.cgi?id=1556989#c3
    and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=84907
    
    Fixes #2421
    
    Change-Id: I31492cd582b785cdfb42e8b999a165a7339ce4be

--- src/gromacs/simd/impl_ibm_vsx/impl_ibm_vsx_simd_float.h
+++ src/gromacs/simd/impl_ibm_vsx/impl_ibm_vsx_simd_float.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -121,14 +121,14 @@ static inline SimdFloat gmx_simdcall
 simdLoadU(const float *m, SimdFloatTag = {})
 {
     return {
-               *reinterpret_cast<const __vector float *>(m)
+               vec_xl(0, m)
     };
 }
 
 static inline void gmx_simdcall
 storeU(float *m, SimdFloat a)
 {
-    *reinterpret_cast<__vector float *>(m) = a.simdInternal_;
+    vec_xst(a.simdInternal_, 0, m);
 }
 
 static inline SimdFloat gmx_simdcall
@@ -157,14 +157,14 @@ static inline SimdFInt32 gmx_simdcall
 simdLoadU(const std::int32_t *m, SimdFInt32Tag)
 {
     return {
-               *reinterpret_cast<const __vector int *>(m)
+               vec_xl(0, m)
     };
 }
 
 static inline void gmx_simdcall
 storeU(std::int32_t * m, SimdFInt32 a)
 {
-    *reinterpret_cast<__vector int *>(m) = a.simdInternal_;
+    vec_xst(a.simdInternal_, 0, m);
 }
 
 static inline SimdFInt32 gmx_simdcall
