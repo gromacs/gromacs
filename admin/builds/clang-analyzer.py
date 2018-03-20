7c96320edc78d5d8aab6f3f6bfe2202b5598dc54
Mark Abraham <mark.j.abraham@gmail.com>
2018-03-20 23:10:32 +0100

    Update clang static analyzer to version 6
    Change-Id: Idb741480544189b68db78be57167e3f4ce577bd7

--- admin/builds/clang-analyzer.py
+++ admin/builds/clang-analyzer.py
@@ -34,7 +34,7 @@
 
 # These options need to match Jenkins labels on the slave where the
 # analysis is intended to run.
-build_options = ['clang-4', 'clang-static-analyzer-4']
+build_options = ['clang-6', 'clang-static-analyzer-6']
 
 def do_build(context):
     cmake_opts = {
