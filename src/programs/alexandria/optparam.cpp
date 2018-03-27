c7e938a7ec10061c92062476fc04e0aba8e0f426
mohammad-ghahremanpour <ghahremanpour.gro@gmail.com>
2018-03-27 10:03:52 +0200

    updates
    Change-Id: I832a6d20b8f48f733f4fa2c9907146ba2ea14f8b

--- src/programs/alexandria/optparam.cpp
+++ src/programs/alexandria/optparam.cpp
@@ -1,31 +1,4 @@
 /*
- * This file is part of the GROMACS molecular simulation package.
- *
- * Copyright (c) 2016,2017, by the GROMACS development team, led by
- * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
- * and including many others, as listed in the AUTHORS file in the
- * top-level source directory and at http://www.gromacs.org.
- *
- * GROMACS is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- *
- * GROMACS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with GROMACS; if not, see
- * http://www.gnu.org/licenses, or write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
- *
- * If you want to redistribute modifications to GROMACS, please
- * consider that scientific software is very special. Version
- * control is crucial - bugs must be traceable. We will be happy to
- * consider code for inclusion in the official distribution, but
-/*
  * This source file is part of the Alexandria program.
  *
  * Copyright (C) 2014-2018 
