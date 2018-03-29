541c16e51c39c140276390054bc01083b98867b7
Mark Abraham <mark.j.abraham@gmail.com>
2018-03-29 08:59:40 +0200

    Re-implemented mdrun -rerun tests
    Used the new infrastructure to let the rerun test check that the
    energies and trajectories (including forces) produced by a rerun
    actually match those produced by a normal mdrun. Such a test running
    in parallel would have prevented #1868, and will help checking that
    any changed or new integrator functionality does the job that it
    should do.
    
    Updated some infrastructure to not care whether it gets passed
    a C or C++ string.
    
    Refs #1868, #2112
    
    Change-Id: Ifcb211aba62ccb79ebaec21a3088c9e60a618c86

--- src/programs/mdrun/tests/moduletest.cpp
+++ src/programs/mdrun/tests/moduletest.cpp
@@ -143,9 +143,15 @@ SimulationRunner::useStringAsNdxFile(const char *ndxString)
 void
 SimulationRunner::useTopGroAndNdxFromDatabase(const char *name)
 {
-    topFileName_ = fileManager_.getInputFilePath((std::string(name) + ".top").c_str());
-    groFileName_ = fileManager_.getInputFilePath((std::string(name) + ".gro").c_str());
-    ndxFileName_ = fileManager_.getInputFilePath((std::string(name) + ".ndx").c_str());
+    useTopGroAndNdxFromDatabase(std::string(name));
+}
+
+void
+SimulationRunner::useTopGroAndNdxFromDatabase(const std::string &name)
+{
+    topFileName_ = fileManager_.getInputFilePath(name + ".top");
+    groFileName_ = fileManager_.getInputFilePath(name + ".gro");
+    ndxFileName_ = fileManager_.getInputFilePath(name + ".ndx");
 }
 
 void
