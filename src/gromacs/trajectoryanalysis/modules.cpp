eaad669b2699b06ca672202879dc8688e71c81b3
Joe Jordan <e.jjordan12@gmail.com>
2018-04-12 16:17:24 +0200

    [WIP] SAXS and SANS forward model
    This is refactors gmx_saxs and gmx_sans to be in the c++
    trajectoryanalysis framework. Since they are both in one
    place now, they both have the option to only compute
    scattering for a subset of atoms, instead of only neutron
    scattering as before.
    
    Change-Id: Ica54d080a19f02269cd5edac7218e7aa23206196

--- src/gromacs/trajectoryanalysis/modules.cpp
+++ src/gromacs/trajectoryanalysis/modules.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2010,2011,2012,2013,2014,2016, by the GROMACS development team, led by
+ * Copyright (c) 2010,2011,2012,2013,2014,2016,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -52,6 +52,7 @@
 #include "modules/pairdist.h"
 #include "modules/rdf.h"
 #include "modules/sasa.h"
+#include "modules/scattering.h"
 #include "modules/select.h"
 #include "modules/trajectory.h"
 
@@ -96,6 +97,7 @@ void registerTrajectoryAnalysisModules(CommandLineModuleManager *manager)
     registerModule<PairDistanceInfo>(manager, group);
     registerModule<RdfInfo>(manager, group);
     registerModule<SasaInfo>(manager, group);
+    registerModule<ScatteringInfo>(manager, group);
     registerModule<SelectInfo>(manager, group);
     registerModule<TrajectoryInfo>(manager, group);
 }
