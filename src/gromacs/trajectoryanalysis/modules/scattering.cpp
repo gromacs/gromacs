ad056c0e1499a7d7837727ef8954def604b8faef
Joe Jordan <e.jjordan12@gmail.com>
2018-04-12 16:17:24 +0200

    [WIP] SAXS and SANS forward model
    This is refactors gmx_saxs and gmx_sans to be in the c++
    trajectoryanalysis framework.
    
    Change-Id: Ica54d080a19f02269cd5edac7218e7aa23206196

---
+++ src/gromacs/trajectoryanalysis/modules/scattering.cpp
@@ -0,0 +1,380 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018 by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Implements gmx::analysismodules::scattering.
+ *
+ * \author Joe Jordan <e.jjordan12@gmail.com>
+ * \ingroup module_trajectoryanalysis
+ */
+#include "gmxpre.h"
+
+#include "scattering.h"
+
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "gromacs/analysisdata/analysisdata.h"
+#include "gromacs/analysisdata/modules/average.h"
+#include "gromacs/analysisdata/modules/histogram.h"
+#include "gromacs/analysisdata/modules/plot.h"
+#include "gromacs/math/vec.h"
+#include "gromacs/options/basicoptions.h"
+#include "gromacs/options/filenameoption.h"
+#include "gromacs/options/ioptionscontainer.h"
+#include "gromacs/pbcutil/pbc.h"
+#include "gromacs/selection/selection.h"
+#include "gromacs/selection/selectionoption.h"
+#include "gromacs/topology/topology.h"
+#include "gromacs/trajectory/trajectoryframe.h"
+#include "gromacs/trajectoryanalysis/analysissettings.h"
+#include "gromacs/utility/arrayref.h"
+#include "gromacs/utility/exceptions.h"
+#include "gromacs/utility/gmxassert.h"
+#include "gromacs/utility/futil.h"
+#include "gromacs/utility/strdb.h"
+#include "gromacs/utility/stringutil.h"
+#include "gromacs/applied-forces/scattering/scattering.h"
+
+namespace gmx
+{
+
+namespace analysismodules
+{
+
+namespace
+{
+
+enum ScatterType {
+    enXray, enNeutron
+};
+static const char *ScatterEnum[] = {
+    "xray", "neutron"
+};
+
+class Scattering : public TrajectoryAnalysisModule
+{
+    public:
+        Scattering();
+
+        virtual void initOptions(IOptionsContainer          *options,
+                                 TrajectoryAnalysisSettings *settings);
+        virtual void initAnalysis(const TrajectoryAnalysisSettings &settings,
+                                  const TopologyInformation        &top);
+        virtual void optionsFinished(TrajectoryAnalysisSettings *settings);
+        virtual void analyzeFrame(int frnr, const t_trxframe &fr, t_pbc *pbc,
+                                  TrajectoryAnalysisModuleData *pdata);
+        virtual void finishAnalysis(int nframes);
+        virtual void writeOutput();
+        void initScattering(const SelectionList       &sel,
+                            const TopologyInformation &top);
+    private:
+        SelectionList                    sel_;
+        std::string                      fnScattering_;
+        std::string                      fnAll_;
+        std::string                      fnReferenceScattering_;
+        std::string                      fnStructureFactor_;
+        double                           startQ_;    
+        double                           endQ_;
+        double                           qSpacing_;
+        int                              numQ_;    
+        bool                             bNormalize_;
+        bool                             bLogarithm_;
+
+        AnalysisData                     distances_;
+        AnalysisDataAverageModulePointer averageModule_;
+        ScatterType                      scatterEnum_;
+        std::vector<double>              qList_;
+        std::vector<double>              refIntensities_;
+        std::vector<double>              refErrors_;
+        std::vector<double>              scatteringDifferences_;
+        std::vector<double>              scatteringAtQ0_;
+                                         
+        friend class                     ComputeScattering;  
+        std::vector<ComputeScattering*>   computeScattering_;
+        void readScattering(std::string  parameterFileName);
+        // Copy and assign disallowed by base.
+};
+
+Scattering::Scattering()
+{
+    averageModule_.reset(new AnalysisDataAverageModule());
+    distances_.addModule(averageModule_);
+
+    distances_.setMultipoint(true);
+    registerAnalysisDataset(&distances_, "dist");
+}
+
+void
+Scattering::initOptions(IOptionsContainer *options, TrajectoryAnalysisSettings *settings)
+{
+    static const char *const desc[] = {
+        "[THISMODULE] calculates neutron scatting curves.[PAR]",
+        "[TT]-o[tt] writes scattering intensity, I(q), as a function of ",
+        "scattering angle, q.",
+    };
+
+    settings->setHelpText(desc);
+    options->addOption(FileNameOption("o").filetype(eftPlot).outputFile()
+                           .store(&fnScattering_).defaultBasename("scattering")
+                           .description("Scattering intensity as a function of q"));
+    options->addOption(SelectionOption("sel").storeVector(&sel_).required().multiValue()
+                       .description("Positions to calculate distances for"));
+    options->addOption(DoubleOption("startq").store(&startQ_)
+                       .defaultValue(0.0)
+                       .description("smallest q value"));
+    options->addOption(DoubleOption("endq").store(&endQ_)
+                       .defaultValue(0.5)
+                       .description("largest q value"));
+    options->addOption(DoubleOption("qspacing").store(&qSpacing_)
+                       .defaultValue(0.0)
+                       .description("spacing of q values"));
+    options->addOption(IntegerOption("numq").store(&numQ_)
+                       .defaultValue(256)
+                       .description("number of q values to evaluate"));
+    options->addOption(BooleanOption("norm").store(&bNormalize_)
+                       .defaultValue(true)
+                       .description("normalize scattering intensities"));
+    options->addOption(BooleanOption("log").store(&bLogarithm_)
+                       .defaultValue(false)
+                       .description("take the log of scattering intensities"));
+    options->addOption(EnumOption<ScatterType>("scatter-type").enumValue(ScatterEnum)
+                       .store(&scatterEnum_)
+                       .description("whether to perform x-ray or neutron scattering"));
+    options->addOption(FileNameOption("ref").filetype(eftGenericData)
+                       .inputFile().store(&fnReferenceScattering_)
+                       .defaultBasename("reference")
+                       .description("reference scattering values"));
+}
+
+void
+Scattering::optionsFinished(TrajectoryAnalysisSettings * /*settings*/)
+{
+    // only use q's from reference file if provided
+    if (!fnReferenceScattering_.empty())
+    {
+        readScattering(fnReferenceScattering_);
+        startQ_ = qList_[0];
+        qSpacing_ = qList_[1] - startQ_;
+        for (size_t qi = 1; qi != qList_.size(); ++qi)
+        {
+            if (qSpacing_ != qList_[qi] - qList_[qi - 1])
+            {
+                GMX_THROW(InconsistentInputError("q spacing in ref must be constant"));
+            }
+        }
+    }
+
+    if (startQ_ < 0.0)
+    {
+        GMX_THROW(InconsistentInputError("-startq cannot be less than 0"));
+    }
+
+    if (endQ_ < startQ_)
+    {
+        GMX_THROW(InconsistentInputError("-endq cannot be less than sQ"));
+    }
+
+    if (numQ_>0 && qSpacing_>0)
+    {
+        GMX_THROW(InconsistentInputError("-numq and qspacing cannot both be set"));
+    }
+
+    //actually set the q values to be used
+    if ((numQ_>0) && ! (qSpacing_>0))
+    {
+        qSpacing_ = (endQ_ - startQ_) / (numQ_ - 1);
+        for (int i = 0; i < numQ_; ++i)
+        {
+            qList_.push_back(startQ_ + qSpacing_ * i);
+        }       
+    }
+    // need:  if (qSpacing_ and not numQ_)
+}
+
+void Scattering::readScattering(std::string parameterFileName)
+{
+    // read gaussian parameters from file if provided, else use defaults
+    FILE *parameterFile;
+    if (!parameterFileName.empty())
+    {
+        parameterFile = fopen(parameterFileName.c_str(), "r");
+    }
+    else
+    {
+        GMX_THROW(gmx::InvalidInputError("FAIL\n"));        
+    }
+    char line[1000];
+    // all non-header lines
+    while (get_a_line(parameterFile, line, 1000))
+    {
+        float scatteringAngle;
+        float intensity;
+        float experimentalError;
+        if (sscanf(line, "%f %f %f", &scatteringAngle, &intensity, &experimentalError) == 3)
+        {
+            qList_.push_back(scatteringAngle);
+            refIntensities_.push_back(intensity);
+            refErrors_.push_back(experimentalError);
+        }
+        else if (sscanf(line, "%f %f", &scatteringAngle, &intensity) == 2)
+        {
+            qList_.push_back(scatteringAngle);
+            refIntensities_.push_back(intensity);
+        }
+    }
+    fclose(parameterFile);
+}
+
+/*! \brief
+ * sets of the list of scattering factors, indexed with selections
+ */
+void Scattering::initScattering(const SelectionList       &sel,
+                                const TopologyInformation &top)
+{
+    for (size_t g = 0; g < sel.size(); ++g)
+    {
+        if (scatterEnum_ == enXray)
+        {
+            ComputeScattering *pCS = new auto(makeXrayInfo(top));
+            computeScattering_.push_back(pCS);
+        }
+        if (scatterEnum_ == enNeutron)
+        {
+            ComputeScattering *pCS = new auto(makeNeutronInfo(top));
+            computeScattering_.push_back(pCS);
+        }
+    }
+}
+
+void
+Scattering::initAnalysis(const TrajectoryAnalysisSettings &settings,
+                         const TopologyInformation        &top)
+{
+    initScattering(sel_, top);
+
+    distances_.setDataSetCount(sel_.size());
+    for (size_t g = 0; g < sel_.size(); ++g)
+    {
+        distances_.setColumnCount(g, qList_.size());
+        scatteringAtQ0_.push_back(computeScattering_[g]->computeS0(sel_[g]));
+    }
+    
+    if (!fnScattering_.empty())
+    {
+        averageModule_->setXAxis(startQ_, qSpacing_);
+        AnalysisDataPlotModulePointer plotm(
+                new AnalysisDataPlotModule(settings.plotSettings()));
+        plotm->setFileName(fnScattering_);
+        plotm->setXFormat(2, 10); //is this precision reasonable?
+        plotm->setYFormat(1, 10); //is this precision reasonable?
+        plotm->setTitle("Scattering intensity: I(q)");
+        plotm->setXLabel("q (1/nm)");
+        plotm->setYLabel("Intensity");
+        averageModule_->addModule(plotm);
+    }
+}
+
+void
+Scattering::analyzeFrame(int frnr, const t_trxframe &fr, t_pbc *pbc,
+                       TrajectoryAnalysisModuleData *pdata)
+{
+    AnalysisDataHandle    scatterHandle   = pdata->dataHandle(distances_);
+    const SelectionList   &sel         = pdata->parallelSelections(sel_);
+    scatterHandle.startFrame(frnr, fr.time);
+    for (size_t g = 0; g < sel.size(); ++g)
+    {
+        scatterHandle.selectDataSet(g);
+        //loop over q's and just store the debeye evaluations in the histogram
+        for (size_t qi = 0; qi != qList_.size(); ++qi)
+        {
+            double Debeye;
+            if (qList_[qi]==0.0)
+            {
+                Debeye = scatteringAtQ0_[g];
+            }
+            else
+            {
+                Debeye = computeScattering_[g]->computeIntensity(pbc, qList_[qi], sel[g]);
+            }
+            if (!fnReferenceScattering_.empty())
+            {
+                Debeye -= refIntensities_[qi];
+            }
+            if (bNormalize_)
+            {
+                Debeye /= scatteringAtQ0_[g];
+            }
+            if (bLogarithm_)
+            {
+                Debeye = log(Debeye);
+            }
+            scatterHandle.setPoint(qi, Debeye, true);
+            scatterHandle.finishPointSet();
+        }
+    }    
+    scatterHandle.finishFrame();
+}
+
+
+void
+Scattering::finishAnalysis(int /*nframes*/)
+{
+    //nothing for now
+}
+
+
+void
+Scattering::writeOutput()
+{
+    //nothing for now
+}
+
+}       // namespace
+
+const char ScatteringInfo::name[]             = "scattering";
+const char ScatteringInfo::shortDescription[] =
+    "Calculate small angle scattering profiles";
+
+TrajectoryAnalysisModulePointer ScatteringInfo::create()
+{
+    return TrajectoryAnalysisModulePointer(new Scattering);
+}
+
+} // namespace analysismodules
+
+} // namespace gmx
