7a0c0b0a357c726b84d94fb9bde3a060fbdc5ddc
Christian Blau <cblau@gwdg.de>
2018-04-13 17:55:50 +0200

    Apply local atom sets in pull code.
    Reduces the dependency of the domain decomposition code on the pull
    code. The remaining dependency is due to dynamic pull groups that at
    the time cannot be handled by the localAtom set manager.
    
    The pull_group_work_t that holds the home atoms during pulling was split
    into a struct that employs the local atom sets and one dynamic group
    class, that cannot yet employ the local atom sets.
    
    Change-Id: Ia022f8cea9970dc973d8869a4ca96644a266003b

--- src/gromacs/pulling/pullutil.cpp
+++ src/gromacs/pulling/pullutil.cpp
@@ -238,7 +238,8 @@ static void make_cyl_refgrps(const t_commrec *cr, struct pull_t *pull, t_mdatoms
 
         if (pcrd->params.eGeom == epullgCYL)
         {
-            pull_group_work_t *pref, *pgrp, *pdyna;
+            pull_group_work_t      *pref, *pgrp;
+            pull_group_dyna_work_t *pdyna;
 
             /* pref will be the same group for all pull coordinates */
             pref  = &pull->group[pcrd->params.group[0]];
@@ -360,12 +361,12 @@ static void make_cyl_refgrps(const t_commrec *cr, struct pull_t *pull, t_mdatoms
 
         if (pcrd->params.eGeom == epullgCYL)
         {
-            pull_group_work_t    *pdyna       = &pull->dyna[c];
-            pull_group_work_t    *pgrp        = &pull->group[pcrd->params.group[1]];
-            PullCoordSpatialData &spatialData = pcrd->spatialData;
+            pull_group_dyna_work_t *pdyna       = &pull->dyna[c];
+            pull_group_work_t      *pgrp        = &pull->group[pcrd->params.group[1]];
+            PullCoordSpatialData   &spatialData = pcrd->spatialData;
 
-            double                wmass       = comm->dbuf_cyl[c*stride+0];
-            double                wwmass      = comm->dbuf_cyl[c*stride+1];
+            double                  wmass       = comm->dbuf_cyl[c*stride+0];
+            double                  wwmass      = comm->dbuf_cyl[c*stride+1];
             pdyna->mwscale                    = 1.0/wmass;
             /* Cylinder pulling can't be used with constraints, but we set
              * wscale and invtm anyhow, in case someone would like to use them.
@@ -435,7 +436,7 @@ static void sum_com_part(const pull_group_work_t *pgrp,
 
     for (int i = ind_start; i < ind_end; i++)
     {
-        int  ii = pgrp->ind_loc[i];
+        int  ii = pgrp->atomSet.localIndex()[i];
         real wm;
         if (pgrp->weight_loc == nullptr)
         {
@@ -517,7 +518,7 @@ static void sum_com_part_cosweight(const pull_group_work_t *pgrp,
 
     for (int i = ind_start; i < ind_end; i++)
     {
-        int  ii  = pgrp->ind_loc[i];
+        int  ii  = pgrp->atomSet.localIndex()[i];
         real m   = mass[ii];
         /* Determine cos and sin sums */
         real cw  = std::cos(x[ii][cosdim]*twopi_box);
@@ -626,23 +627,23 @@ void pull_calc_coms(const t_commrec *cr,
                  * in that case a check group mass != 0 has been done before.
                  */
                 if (pgrp->params.nat == 1 &&
-                    pgrp->nat_loc == 1 &&
-                    md->massT[pgrp->ind_loc[0]] == 0)
+                    pgrp->atomSet.numAtomsLocal() == 1 &&
+                    md->massT[pgrp->atomSet.localIndex()[0]] == 0)
                 {
                     GMX_ASSERT(xp == NULL, "We should not have groups with zero mass with constraints, i.e. xp!=NULL");
 
                     /* Copy the single atom coordinate */
                     for (int d = 0; d < DIM; d++)
                     {
-                        sum_com->sum_wmx[d] = x[pgrp->ind_loc[0]][d];
+                        sum_com->sum_wmx[d] = x[pgrp->atomSet.localIndex()[0]][d];
                     }
                     /* Set all mass factors to 1 to get the correct COM */
                     sum_com->sum_wm  = 1;
                     sum_com->sum_wwm = 1;
                 }
-                else if (pgrp->nat_loc <= c_pullMaxNumLocalAtomsSingleThreaded)
+                else if (pgrp->atomSet.numAtomsLocal() <= c_pullMaxNumLocalAtomsSingleThreaded)
                 {
-                    sum_com_part(pgrp, 0, pgrp->nat_loc,
+                    sum_com_part(pgrp, 0, pgrp->atomSet.numAtomsLocal(),
                                  x, xp, md->massT,
                                  pbc, x_pbc,
                                  sum_com);
@@ -652,8 +653,8 @@ void pull_calc_coms(const t_commrec *cr,
 #pragma omp parallel for num_threads(pull->nthreads) schedule(static)
                     for (int t = 0; t < pull->nthreads; t++)
                     {
-                        int ind_start = (pgrp->nat_loc*(t + 0))/pull->nthreads;
-                        int ind_end   = (pgrp->nat_loc*(t + 1))/pull->nthreads;
+                        int ind_start = (pgrp->atomSet.numAtomsLocal()*(t + 0))/pull->nthreads;
+                        int ind_end   = (pgrp->atomSet.numAtomsLocal()*(t + 1))/pull->nthreads;
                         sum_com_part(pgrp, ind_start, ind_end,
                                      x, xp, md->massT,
                                      pbc, x_pbc,
@@ -691,8 +692,8 @@ void pull_calc_coms(const t_commrec *cr,
 #pragma omp parallel for num_threads(pull->nthreads) schedule(static)
                 for (int t = 0; t < pull->nthreads; t++)
                 {
-                    int ind_start = (pgrp->nat_loc*(t + 0))/pull->nthreads;
-                    int ind_end   = (pgrp->nat_loc*(t + 1))/pull->nthreads;
+                    int ind_start = (pgrp->atomSet.numAtomsLocal()*(t + 0))/pull->nthreads;
+                    int ind_end   = (pgrp->atomSet.numAtomsLocal()*(t + 1))/pull->nthreads;
                     sum_com_part_cosweight(pgrp, ind_start, ind_end,
                                            pull->cosdim, twopi_box,
                                            x, xp, md->massT,
@@ -774,7 +775,6 @@ void pull_calc_coms(const t_commrec *cr,
             {
                 /* Cosine weighting geometry */
                 double csw, snw, wmass, wwmass;
-                int    i, ii;
 
                 /* Determine the optimal location of the cosine weight */
                 csw                   = comm->dbuf[g*3][0];
@@ -792,9 +792,9 @@ void pull_calc_coms(const t_commrec *cr,
                 /* Set the weights for the local atoms */
                 csw *= pgrp->invtm;
                 snw *= pgrp->invtm;
-                for (i = 0; i < pgrp->nat_loc; i++)
+                for (size_t i = 0; i < pgrp->atomSet.numAtomsLocal(); i++)
                 {
-                    ii                  = pgrp->ind_loc[i];
+                    int ii = pgrp->atomSet.localIndex()[i];
                     pgrp->weight_loc[i] = csw*cos(twopi_box*x[ii][pull->cosdim]) +
                         snw*sin(twopi_box*x[ii][pull->cosdim]);
                 }
