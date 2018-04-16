d2b113d6cb260c766a03808e6527f14e8263fb2c
Aleksei Iupinov <a.yupinov@gmail.com>
2018-04-16 17:54:07 +0200

    Add GpuEventSynchronizer class
    This class encapsulates the host synchronisation on a single GPU
    event (currently used for PME mixed mode).
    In the future it can also be extended for inter-queue GPU sync.
    
    Change-Id: I8ea0cb615c041d64ebb0580b5f54bdaa26a6aeea

---
+++ src/gromacs/gpu_utils/gpueventsynchronizer.cuh
@@ -0,0 +1,93 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \libinternal \file
+ *  \brief Implements a GpuEventSynchronizer class for CUDA.
+ *
+ *  \author Aleksei Iupinov <a.yupinov@gmail.com>
+ *  \inlibraryapi
+ */
+#ifndef GMX_GPU_UTILS_GPUEVENTSYNCHRONIZER_CUH
+#define GMX_GPU_UTILS_GPUEVENTSYNCHRONIZER_CUH
+
+#include "gromacs/gpu_utils/gputraits.cuh"
+#include "gromacs/utility/gmxassert.h"
+
+/*! \libinternal \brief
+ * A class which allows for CPU thread to mark and wait for certain GPU stream execution point.
+ * The event can be put into the stream with markEvent() and then later waited on with waitForEvent().
+ * This can be repeated as necessary.
+ * The class generally attempts to tracks the correctness of its state transitions, but
+ * please note that calling waitForEvent() right after the construction might actually succeed.
+ */
+class GpuEventSynchronizer
+{
+    public:
+        GpuEventSynchronizer()
+        {
+            cudaError_t stat = cudaEventCreateWithFlags(&event_, cudaEventDisableTiming);
+            GMX_RELEASE_ASSERT(stat == cudaSuccess, "cudaEventCreate failed");
+        }
+        ~GpuEventSynchronizer()
+        {
+            cudaError_t stat = cudaEventDestroy(event_);
+            GMX_ASSERT(stat == cudaSuccess, "cudaEventDestroy failed");
+        }
+        //! No copying
+        GpuEventSynchronizer(const GpuEventSynchronizer &)       = delete;
+        //! No assignment
+        GpuEventSynchronizer &operator = (GpuEventSynchronizer &&) = delete;
+        //! Moving is disabled but can be considered in the future if needed
+        GpuEventSynchronizer(GpuEventSynchronizer &&)            = delete;
+
+        /*! \brief Marks the synchronization point in the \p stream.
+         * Should be followed by waitForEvent().
+         */
+        inline void markEvent(CommandStream stream)
+        {
+            cudaError_t stat = cudaEventRecord(event_, stream);
+            GMX_ASSERT(stat == cudaSuccess, "cudaEventRecord failed");
+        }
+        /*! \brief Synchronizes the host thread on the marked event. */
+        inline void waitForEvent()
+        {
+            cudaError_t stat = cudaEventSynchronize(event_);
+            GMX_ASSERT(stat == cudaSuccess, "cudaEventSynchronize failed");
+        }
+
+    private:
+        cudaEvent_t event_;
+};
+
+#endif
