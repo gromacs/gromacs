af0de7ff8baec5a1f5294212fd7168808546f917
Szilárd Páll <pall.szilard@gmail.com>
2018-04-17 19:51:59 +0200

    Tweak PME run mode checks
    These should allow compile-time evaluation when mdrun is compiled with
    GMX_GPU=OFF.
    
    As the CPU-only build can now optimize out quite some code, this also
    fixes a gcc 7 warning that an argument of pme_gpu_reduce_outputs()
    may be used uninitialized.
    
    Change-Id: I2397e0dc7e4e2f6587d01d94db27072938e85e7d

--- src/gromacs/mdlib/sim_util.cpp
+++ src/gromacs/mdlib/sim_util.cpp
@@ -1066,7 +1066,7 @@ static void do_force_cutsVERLET(FILE *fplog, const t_commrec *cr,
     const bool useGpuNb       = useGpuNonbonded(fr->nbv);
     const bool useOrEmulGpuNb = useGpuNb || (fr->nbv->emulateGpu == EmulateGpuNonbonded::Yes);
 
-    const auto pmeRunMode = fr->pmedata ? pme_run_mode(fr->pmedata) : PmeRunMode::CPU;
+    const auto pmeRunMode = ((GMX_GPU == GMX_GPU_NONE) || fr->pmedata == nullptr) ? PmeRunMode::CPU : pme_run_mode(fr->pmedata);
     // TODO slim this conditional down - inputrec and duty checks should mean the same in proper code!
     const bool useGpuPme  = EEL_PME(fr->ic->eeltype) && thisRankHasDuty(cr, DUTY_PME) &&
         ((pmeRunMode == PmeRunMode::GPU) || (pmeRunMode == PmeRunMode::Mixed));
