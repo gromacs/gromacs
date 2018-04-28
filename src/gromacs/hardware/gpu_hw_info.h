5dff6908c36184442070cf7d3eba24d03f263721
Roland Schulz <roland.schulz@intel.com>
2018-04-27 23:24:58 -0700

    Recognize Intel GPU as compatible
    Also enable I_PREFETCH by default
    
    Change-Id: I9c3efe70df4d273e463bf49f5ae4f0959a590ebe

--- src/gromacs/hardware/gpu_hw_info.h
+++ src/gromacs/hardware/gpu_hw_info.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2017, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -53,7 +53,7 @@ struct gmx_device_info_t;
  * incompatible driver/runtime. */
 typedef enum
 {
-    egpuCompatible = 0,  egpuNonexistent,  egpuIncompatible, egpuInsane, egpuNR
+    egpuCompatible = 0,  egpuNonexistent,  egpuIncompatible, egpuIncompatibleClusterSize, egpuInsane, egpuNR
 } e_gpu_detect_res_t;
 
 /* Names of the GPU detection/check results */
