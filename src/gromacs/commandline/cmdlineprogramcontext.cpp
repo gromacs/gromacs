ee73a98450fed520eadac197ebff2951b9dd763e
Erik Lindahl <erik@kth.se>
2018-05-11 16:06:59 +0200

    Use default CMake path variables
    Remove our homegrown path variables for binaries,
    libraries, manpages and data in favor of the ones
    documented as part of CMake. The 'gromacs' part of
    the data directory has been moved to a separate
    GMX_INSTALL_DATASUBDIR variable.
    
    Change-Id: I1618428c94ea2cfb90dc1ff5261b26a9307c1a94

--- src/gromacs/commandline/cmdlineprogramcontext.cpp
+++ src/gromacs/commandline/cmdlineprogramcontext.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -186,7 +186,7 @@ bool isAcceptableLibraryPath(const std::string &path)
  */
 bool isAcceptableLibraryPathPrefix(const std::string &path)
 {
-    std::string testPath = Path::join(path, DATA_INSTALL_DIR, "top");
+    std::string testPath = Path::join(path, GMX_INSTALL_GMXDATADIR, "top");
     if (isAcceptableLibraryPath(testPath))
     {
         return true;
