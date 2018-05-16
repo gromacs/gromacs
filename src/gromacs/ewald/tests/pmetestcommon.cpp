67d29b97f367558bb31d6da4373dd9d491875850
Aleksei Iupinov <a.yupinov@gmail.com>
2018-05-16 13:49:14 +0200

    [WIP] Add and enable PME OpenCL
    PME OpenCL kernels (spread, gather, solve) are added.
    PmeGpuContext is taught to compile them. PME with OpenCL
    is enabled everywhere (mdrun, unit tests, integrations tests).
    
    Status: dirty code with loads of fixmes; warp_size still
    (unnecessarily) hardcoded to 32; passing tests on NVidia OpenCL.
    
    Change-Id: Iecf1393ddeb442f9b66ca31100bac9b88f83e52b

--- src/gromacs/ewald/tests/pmetestcommon.cpp
+++ src/gromacs/ewald/tests/pmetestcommon.cpp
@@ -77,7 +77,7 @@ bool pmeSupportsInputForMode(const t_inputrec *inputRec, CodePath mode)
             implemented = true;
             break;
 
-        case CodePath::CUDA:
+        case CodePath::GPU:
             implemented = pme_gpu_supports_input(inputRec, nullptr);
             break;
 
@@ -114,7 +114,7 @@ static PmeSafePointer pmeInitInternal(const t_inputrec         *inputRec,
     {
         init_gpu(dummyLogger, gpuInfo);
     }
-    const auto     runMode       = (mode == CodePath::CPU) ? PmeRunMode::CPU : PmeRunMode::GPU;
+    const auto     runMode       = (mode == CodePath::CPU) ? PmeRunMode::CPU : PmeRunMode::Mixed; //FIXME clFFT plannign failure 4097
     t_commrec      dummyCommrec  = {0};
     gmx_pme_t     *pmeDataRaw    = gmx_pme_init(&dummyCommrec, 1, 1, inputRec, atomCount, false, false, true,
                                                 ewaldCoeff_q, ewaldCoeff_lj, 1, runMode, nullptr, gpuInfo, dummyLogger, persistent);
@@ -138,7 +138,7 @@ static PmeSafePointer pmeInitInternal(const t_inputrec         *inputRec,
             invertBoxMatrix(boxTemp, pme->recipbox);
             break;
 
-        case CodePath::CUDA:
+        case CodePath::GPU:
             pme_gpu_set_testing(pme->gpu, true);
             pme_gpu_update_input_box(pme->gpu, boxTemp);
             break;
@@ -186,7 +186,7 @@ PmeSafePointer pmeInitAtoms(const t_inputrec         *inputRec,
             /* With decomposition there would be more boilerplate atc code here, e.g. do_redist_pos_coeffs */
             break;
 
-        case CodePath::CUDA:
+        case CodePath::GPU:
             gmx_pme_reinit_atoms(pmeSafe.get(), atomCount, charges.data());
             pme_gpu_copy_input_coordinates(pmeSafe->gpu, as_rvec_array(coordinates.data()));
             break;
@@ -219,7 +219,7 @@ static void pmeGetRealGridSizesInternal(const gmx_pme_t      *pme,
             gmx_parallel_3dfft_real_limits(pme->pfft_setup[gridIndex], gridSize, gridOffsetUnused, paddedGridSize);
             break;
 
-        case CodePath::CUDA:
+        case CodePath::GPU:
             pme_gpu_get_real_grid_sizes(pme->gpu, &gridSize, &paddedGridSize);
             break;
 
@@ -289,7 +289,7 @@ void pmePerformSplineAndSpread(gmx_pme_t *pme, CodePath mode, // TODO const qual
             }
             break;
 
-        case CodePath::CUDA:
+        case CodePath::GPU:
             pme_gpu_spread(pme->gpu, gridIndex, fftgrid, computeSplines, spreadCharges);
             break;
 
@@ -353,7 +353,7 @@ void pmePerformSolve(const gmx_pme_t *pme, CodePath mode,
             }
             break;
 
-        case CodePath::CUDA:
+        case CodePath::GPU:
             switch (method)
             {
                 case PmeSolveAlgorithm::Coulomb:
@@ -398,7 +398,7 @@ void pmePerformGather(gmx_pme_t *pme, CodePath mode,
             gather_f_bsplines(pme, pmegrid, !forceReductionWithInput, atc, &atc->spline[threadIndex], scale);
             break;
 
-        case CodePath::CUDA:
+        case CodePath::GPU:
         {
             // Variable initialization needs a non-switch scope
             auto stagingForces = pme_gpu_get_forces(pme->gpu);
@@ -431,7 +431,7 @@ void pmeFinalizeTest(const gmx_pme_t *pme, CodePath mode)
         case CodePath::CPU:
             break;
 
-        case CodePath::CUDA:
+        case CodePath::GPU:
             pme_gpu_synchronize(pme->gpu);
             break;
 
@@ -457,7 +457,7 @@ void pmeSetSplineData(const gmx_pme_t *pme, CodePath mode,
             std::copy(splineValues.begin(), splineValues.end(), splineBuffer);
             break;
 
-        case CodePath::CUDA:
+        case CodePath::GPU:
             std::copy(splineValues.begin(), splineValues.end(), splineBuffer);
             pme_gpu_transform_spline_atom_data(pme->gpu, atc, type, dimIndex, PmeLayoutTransform::HostToGpu);
             break;
@@ -488,7 +488,7 @@ void pmeSetGridLineIndices(const gmx_pme_t *pme, CodePath mode,
 
     switch (mode)
     {
-        case CodePath::CUDA:
+        case CodePath::GPU:
             memcpy(pme->gpu->staging.h_gridlineIndices, gridLineIndices.data(), atomCount * sizeof(gridLineIndices[0]));
             break;
 
@@ -535,7 +535,7 @@ static void pmeSetGridInternal(const gmx_pme_t *pme, CodePath mode,
 
     switch (mode)
     {
-        case CodePath::CUDA: // intentional absence of break, the grid will be copied from the host buffer in testing mode
+        case CodePath::GPU: // intentional absence of break, the grid will be copied from the host buffer in testing mode
         case CodePath::CPU:
             std::memset(grid, 0, paddedGridSize[XX] * paddedGridSize[YY] * paddedGridSize[ZZ] * sizeof(ValueType));
             for (const auto &gridValue : gridValues)
@@ -583,7 +583,7 @@ SplineParamsDimVector pmeGetSplineData(const gmx_pme_t *pme, CodePath mode,
     SplineParamsDimVector    result;
     switch (mode)
     {
-        case CodePath::CUDA:
+        case CodePath::GPU:
             pme_gpu_transform_spline_atom_data(pme->gpu, atc, type, dimIndex, PmeLayoutTransform::GpuToHost);
         // fallthrough
 
@@ -607,7 +607,7 @@ GridLineIndicesVector pmeGetGridlineIndices(const gmx_pme_t *pme, CodePath mode)
     GridLineIndicesVector gridLineIndices;
     switch (mode)
     {
-        case CodePath::CUDA:
+        case CodePath::GPU:
             gridLineIndices = arrayRefFromArray(reinterpret_cast<IVec *>(pme->gpu->staging.h_gridlineIndices), atomCount);
             break;
 
@@ -631,7 +631,7 @@ static SparseGridValuesOutput<ValueType> pmeGetGridInternal(const gmx_pme_t *pme
     SparseGridValuesOutput<ValueType> gridValues;
     switch (mode)
     {
-        case CodePath::CUDA: // intentional absence of break
+        case CodePath::GPU: // intentional absence of break
         case CodePath::CPU:
             gridValues.clear();
             for (int ix = 0; ix < gridSize[XX]; ix++)
@@ -696,7 +696,7 @@ PmeSolveOutput pmeGetReciprocalEnergyAndVirial(const gmx_pme_t *pme, CodePath mo
                     GMX_THROW(InternalError("Test not implemented for this mode"));
             }
             break;
-        case CodePath::CUDA:
+        case CodePath::GPU:
             switch (method)
             {
                 case PmeSolveAlgorithm::Coulomb:
