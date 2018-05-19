519e0f6dd8a7616eb9313f127b294f4c75e8f7c9
Paul Bauer <paul.bauer.q@gmail.com>
2018-05-19 21:29:09 +0200

    [WIP] Introduce interface for coordinate manipulation
    Introduces an interface and some hopefully helpful examples
    for manipulating coordinate frames in the trajectoryanalysis framework.
    
    The coordinate frame manager is supposed to provide the basis
    of all classes that will be used to manipulate coordinate frame data.
    It has to know what the coordinate frame is and provide the
    initialization and data analysis methods.
    
    The analysis class provides a wrapper and way to register other classes
    to be analysed without having to specify and call their analysis methods
    individually (and is shamelessly copied from the analysisdata
    framework).
    
    The actual file writing is done by a separate class where the objects
    are build with the minimal information on what is needed to write a file
    of this kind.
    
    TODO
    Example test cases for the show case methods.
    Add remaining methods to completely modify coordinate frames.
    Improve detection and writeout code to throw on bad user input.
    Handle user supplied connection information for text based output files.
    
    Change-Id: I8863f13eceebb65f3b857f6901026950ad5345a3

---
+++ src/gromacs/coordinatedata/setvelocities.h
@@ -0,0 +1,152 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Set flag for writing velocity information to file or not.
+ *
+ * \author
+ * \inpublicapi
+ * \ingroup module_coordinatedata
+ */
+#ifndef GMX_TRAJECTORYANALYSIS_MODULES_SETVELOCITY_H
+#define GMX_TRAJECTORYANALYSIS_MODULES_SETVELOCITY_H
+
+#include <algorithm>
+
+#include "gromacs/coordinatedata/framemanager.h"
+#include "gromacs/options/ioptionscontainer.h"
+#include "gromacs/trajectory/trajectoryframe.h"
+
+namespace gmx
+{
+
+/*!\brief
+ * SetVelocities class allows changing writing of velocities to file.
+ *
+ * This class allows the user to define if velocities should be written
+ * to the output coordinate file, and checks if they are available from the
+ * currently processed data.
+ *
+ * \inpublicapi
+ * \ingroup module_coordinatedata
+ *
+ */
+class SetVelocities : public IFrameManager
+{
+    public:
+        /*! \brief
+         * Default constructor for SetVelocities should not be used.
+         *
+         * Class should only be initialized with at least the base selection.
+         */
+        SetVelocities() = delete;
+        /*! \brief
+         * Construct SetVelocities object with choice for boolean value.
+         *
+         * Can be used to initialize SetVelocities from outside of trajectoryanalysis
+         * with the user specified option to write coordinate velocities or not.
+         * framework.
+         */
+        explicit SetVelocities(bool velocity) : velocity_(velocity)
+        {
+        }
+        /*! \brief
+         * Copy constructor.
+         */
+        SetVelocities(const SetVelocities &old) = delete;
+        /*! \brief
+         * Assignment operator.
+         */
+        SetVelocities &operator=(const SetVelocities &old) = delete;
+        /*! \brief
+         * Move constructor for SetVelocities.
+         */
+        SetVelocities &operator=(SetVelocities &&old)
+        {
+            velocity_ = std::move(old.velocity_);
+            return *this;
+        }
+        /*! \brief
+         *  Move constructor for SetVelocities.
+         */
+        SetVelocities(SetVelocities &&old) : velocity_(std::move(old.velocity_))
+        {
+        }
+
+        ~SetVelocities() {}
+        /*! \brief
+         * Pass any user input options to the frame manager.
+         *
+         * Currently not used, will be useful to pass user input information to frame manager.
+         */
+        virtual void initFileOptions(IOptionsContainer * /*options*/);
+
+        /*! \brief
+         * Change coordinate frame information for output.
+         *
+         * In this case, the correct flag for writing the velocities is applied
+         * to the output frame, depending on user selection and availability
+         * in the input data.
+         *
+         * \todo should this throw an error if velocity writing is desired
+         * but not possible from input data?
+         *
+         * \param[in] input Coordinate frame to be modified later.
+         */
+        virtual void modifyFrame(const t_trxframe &input);
+        /*! \brief
+         * Sanity check for user input options.
+         *
+         * This function performs the check of the user input for basic sanity issues
+         * and should be called after option processing has been finished.
+         */
+        virtual void checkOptions();
+
+    private:
+        /*! \brief
+         * Flag to specify if velocities should be written.
+         *
+         * Internal storage for the user choice for writing coordinate velocities.
+         */
+        bool                            velocity_;
+};
+
+//! Smart pointer to manage the outputselector object.
+typedef std::unique_ptr<SetVelocities>
+    SetVelocitiesPointer;
+
+} // namespace gmx
+
+#endif
