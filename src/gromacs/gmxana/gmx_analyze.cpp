c770a2b7c8b1d59861fba37063ea73ce3c766070
Roland Schulz <roland.schulz@intel.com>
2018-05-20 00:18:26 -0700

    clang-tidy: Enable bugprone-integer-division
    - Using int{} constructor syntax for intentional
    int division with truncation in fp context.
    - Adds new exactDiv which does integer division
    and asserts that division is without remainder.
    - Converts remaining divsion to fp division.
    
    Also make GMX_ASSERT work in constexpr fn.
    Convert over_alloc_* from macro to function.
    
    Change-Id: Ieaeedd4558865a9dd541d24cf246f5c5733e87a1

--- src/gromacs/gmxana/gmx_analyze.cpp
+++ src/gromacs/gmxana/gmx_analyze.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -336,7 +336,7 @@ static void average(const char *avfile, int avbar_opt,
             fprintf(fp, "@TYPE xydydy\n");
             edge = static_cast<int>(nset*0.05+0.5);
             fprintf(stdout, "Errorbars: discarding %d points on both sides: %d%%"
-                    " interval\n", edge, static_cast<int>(100*(nset-2*edge)/nset+0.5));
+                    " interval\n", edge, static_cast<int>(100.*(nset-2*edge)/nset+0.5));
         }
         else
         {
