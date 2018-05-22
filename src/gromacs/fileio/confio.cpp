8545a83cf583b5bbbd339da1e92834eed66bad82
Paul Bauer <paul.bauer.q@gmail.com>
2018-05-22 17:01:20 +0200

    Fix PQR file output
    PQR files from editconf were always written as fixed format PDB files
    with just the field information added. As pointed out in the linked
    redmine, this can violate the PQR file standard if the field lengths are
    too long, even though the file would still be a valid PDB.
    
    This adds a slightly different form of the writeout that has a flexible,
    PQR conform format.
    
    Fixes #2511
    
    Change-Id: I626380b642a0214970753da289e9c969ce411ea7

--- src/gromacs/fileio/confio.cpp
+++ src/gromacs/fileio/confio.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -101,7 +101,7 @@ void write_sto_conf_indexed(const char *outfile, const char *title,
         case efENT:
         case efPQR:
             out = gmx_fio_fopen(outfile, "w");
-            write_pdbfile_indexed(out, title, atoms, x, ePBC, box, ' ', -1, nindex, index, nullptr, TRUE);
+            write_pdbfile_indexed(out, title, atoms, x, ePBC, box, ' ', -1, nindex, index, nullptr, TRUE, TRUE);
             gmx_fio_fclose(out);
             break;
         case efESP:
