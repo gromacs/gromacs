d63eb4a70fc7c08cb068da3d9bf301b09cfea6eb
Paul Bauer <paul.bauer.q@gmail.com>
2018-05-29 17:04:36 +0200

    Disallow use of empty box information in solvent files
    An empty box information in a solvent file presented to gmx solvate
    could case an infinte loop because the current coordinate would always
    be incremented by zero. Disallowed by introducing new function that
    checks for proper box information.
    
    Fixes #2523
    
    Change-Id: I461509f8aafa2048253faf85ccbe388bbbaf80a7

--- src/gromacs/pbcutil/boxutilities.h
+++ src/gromacs/pbcutil/boxutilities.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -54,6 +54,11 @@ namespace gmx
  * tolerance).
  */
 bool boxesAreEqual(const matrix box1, const matrix box2);
+
+/*! \brief
+ * Returns whether a box is only initialised to zero or not.
+ */
+bool boxIsZero(const matrix box);
 
 } // namespace gmx
 
