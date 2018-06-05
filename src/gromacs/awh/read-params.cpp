cb1ac9a98d49468dc3019882dfc279281ce82ca5
Viveca Lindahl <vivecalindahl@gmail.com>
2018-06-05 22:46:55 +0200

    Make sure that the value of awh-nsamples-update is allowed
    Previously, awh-nsamples-update could be set to zero, leading to
    division by zero. Now it results in a fatal error.
    
    Fixes #2489
    
    Change-Id: I65aae5d5c533b9aab7ea9aae3e317e92a926c18c

--- src/gromacs/awh/read-params.cpp
+++ src/gromacs/awh/read-params.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -538,6 +538,12 @@ AwhParams *readAndCheckAwhParams(int *ninp_p, t_inpfile **inp_p, const t_inputre
     CTYPE("Free energy and bias update interval in number of samples");
     sprintf(opt, "%s-nsamples-update", prefix);
     ITYPE(opt, awhParams->numSamplesUpdateFreeEnergy, 10);
+    if (awhParams->numSamplesUpdateFreeEnergy <= 0)
+    {
+        char buf[STRLEN];
+        sprintf(buf, "%s needs to be an integer > 0", opt);
+        warning_error(wi, buf);
+    }
 
     CTYPE("When true, biases with share-group>0 are shared between multiple simulations");
     sprintf(opt, "%s-share-multisim", prefix);
