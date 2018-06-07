1645a388673d7bb83479196372d3de21cb59816b
Berk Hess <hess@kth.se>
2018-06-07 14:27:44 +0200

    Avoid COM removal moving frozen atoms
    Fixes #2551
    Refs #2542
    
    Change-Id: Ifd7523661248594cce7329dfd86e814b78c9c32c

--- src/gromacs/mdlib/vcm.h
+++ src/gromacs/mdlib/vcm.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -46,6 +46,7 @@
 
 struct gmx_groups_t;
 struct t_inputrec;
+struct t_mdatoms;
 
 typedef struct {
     rvec      p;        /* Linear momentum                     */
@@ -71,6 +72,7 @@ typedef struct {
     tensor       *group_i;     /* Moment of inertia per group         */
     real         *group_mass;  /* Mass per group                      */
     char        **group_name;  /* These two are copies to pointers in */
+    ivec         *nFreeze;     /* Tells whether dimensions are frozen per freeze group */
     t_vcm_thread *thread_vcm;  /* Temporary data per thread and group */
 } t_vcm;
 
@@ -84,8 +86,7 @@ void calc_vcm_grp(int start, int homenr, t_mdatoms *md,
  *
  * With linear modes nullptr can be passed for x.
  */
-void do_stopcm_grp(int homenr,
-                   const unsigned short *group_id,
+void do_stopcm_grp(const t_mdatoms &mdatoms,
                    rvec x[], rvec v[], const t_vcm &vcm);
 
 void check_cm_grp(FILE *fp, t_vcm *vcm, t_inputrec *ir, real Temp_Max);
