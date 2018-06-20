34be89f1264fd0b19c27b72160371d944257a2c7
Mark Abraham <mark.j.abraham@gmail.com>
2018-06-20 09:38:40 +0200

    Split TopologyInformation into its own header
    This will help provide a useful upgrade path for tools to get away
    from t_topology, and eventually into the new framework.
    
    Introduced a builder function to fill TopologyInformation from a tpr
    file, which will be needed shortly for porting old tools.
    
    Refs #1862
    
    Change-Id: If2594cdcd3f9817ff83e7fb73208cdb5c497bb1a

--- src/gromacs/trajectoryanalysis/cmdlinerunner.cpp
+++ src/gromacs/trajectoryanalysis/cmdlinerunner.cpp
@@ -54,6 +54,7 @@
 #include "gromacs/trajectory/trajectoryframe.h"
 #include "gromacs/trajectoryanalysis/analysismodule.h"
 #include "gromacs/trajectoryanalysis/analysissettings.h"
+#include "gromacs/trajectoryanalysis/topologyinformation.h"
 #include "gromacs/utility/exceptions.h"
 #include "gromacs/utility/filestream.h"
 #include "gromacs/utility/gmxassert.h"
