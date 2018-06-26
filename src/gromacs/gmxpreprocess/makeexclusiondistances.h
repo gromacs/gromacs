56a4077b92b316c8900910ceaab9f810eada4dc0
Mark Abraham <mark.j.abraham@gmail.com>
2018-06-26 21:04:03 +0200

    Replace readConformation with TopologyInformation
    Removed that function and renamed its source file to reflect the only
    remaining contents.
    
    Refs #1862
    
    Change-Id: I6616ab8930a90d33ac4a836f97c30f6bea40e29c

--- src/gromacs/gmxpreprocess/makeexclusiondistances.h
+++ src/gromacs/gmxpreprocess/makeexclusiondistances.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2014,2015,2016, by the GROMACS development team, led by
+ * Copyright (c) 2014,2015,2016,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -37,13 +37,10 @@
 
 #include <vector>
 
-#include "gromacs/math/vectypes.h"
 #include "gromacs/utility/real.h"
 
 struct gmx_atomprop;
-struct gmx_mtop_t;
 struct t_atoms;
-struct t_topology;
 
 /*! \brief Allocate and fill an array of inter-atomic half distances
  *
@@ -54,17 +51,5 @@ struct t_topology;
 std::vector<real>
 makeExclusionDistances(const t_atoms *a, gmx_atomprop *aps,
                        real defaultDistance, real scaleFactor);
-
-void readConformation(const char *confin, gmx_mtop_t *top,
-                      std::vector<gmx::RVec> *x, std::vector<gmx::RVec> *v,
-                      int *ePBC, matrix box, const char *statusTitle);
-/*! \brief Read a conformation from a file, allocate and fill data structures.
- *
- * Used by solvate and insert-molecules. The returned pointers *x and
- * *v should be freed by the caller. top should have its destructor
- * called. */
-void readConformation(const char *confin, t_topology *top,
-                      std::vector<gmx::RVec> *x, std::vector<gmx::RVec> *v,
-                      int *ePBC, matrix box, const char *statusTitle);
 
 #endif
