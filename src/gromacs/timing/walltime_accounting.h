9dc76b503017845652310877437d54d04f50c93d
Roland Schulz <roland.schulz@intel.com>
2018-06-30 13:11:16 -0700

    clang-tidy: misc-misplaced-const
    Manual fixes
    
    Change-Id: If4174e56c6974c0188af2f1da496f9aa719b23a3

--- src/gromacs/timing/walltime_accounting.h
+++ src/gromacs/timing/walltime_accounting.h
@@ -108,7 +108,7 @@ walltime_accounting_set_valid_finish(gmx_walltime_accounting_t walltime_accounti
 
 //! Return whether the simulation finished in a way valid for reporting walltime.
 bool
-walltime_accounting_get_valid_finish(const gmx_walltime_accounting_t walltime_accounting);
+walltime_accounting_get_valid_finish(const gmx_walltime_accounting* walltime_accounting);
 
 /*! \brief
  * Calls system timing routines (e.g. clock_gettime) to get the (fractional)
