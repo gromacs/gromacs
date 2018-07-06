a9a3d6c86758c57beb58840f8e1563d1fb2a6e66
Mark Abraham <mark.j.abraham@gmail.com>
2018-07-06 18:41:07 +0200

    [WIP] Replacement for pdb2gmx tests
    This tool now directly asserts upon the .top and .gro files that are
    written out, using (currently onlyt one of) the combinations that are
    tested in regressiontests.
    
    We now omit doing an energy minimization after the string checks,
    which was always a doubtful way to test pdb2gmx.
    
    Added new capability for refdata tests to filter out lines that vary
    at run time by supplying a regex that matches the lines to skip.
    That's not ideal, but useful for now. Better would be to refactor
    tools so that e.g. header output can go to a different stream, but
    first we should have basic tests in place.
    
    Added tests for Regex, to see where the implementation on MSVC works
    differently. Fixed minor bug in c++ stdlib regex implementation of
    Regex.
    
    Minor updates to use compat::make_unique
    
    Refs #1587, #2566
    
    Change-Id: I6a4aeb1a4c460621ca89a0dc6177567fa09d9200

---
+++ src/gromacs/gmxpreprocess/tests/pdb2gmx.cpp
@@ -0,0 +1,109 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Tests for pdb2gmx
+ *
+ * \author Mark Abraham <mark.j.abraham@gmail.com>
+ */
+
+#include "gmxpre.h"
+
+#include "gromacs/gmxpreprocess/pdb2gmx.h"
+
+#include "gromacs/utility/futil.h"
+#include "gromacs/utility/textreader.h"
+
+#include "testutils/cmdlinetest.h"
+#include "testutils/conftest.h"
+#include "testutils/filematchers.h"
+#include "testutils/refdata.h"
+#include "testutils/testfilemanager.h"
+#include "testutils/textblockmatchers.h"
+
+namespace gmx
+{
+namespace test
+{
+namespace
+{
+
+using gmx::test::CommandLine;
+using gmx::test::ConfMatch;
+
+class Pdb2gmxTest : public gmx::test::CommandLineTestBase
+{
+    public:
+        Pdb2gmxTest()
+        {
+            // TODO It would be preferable to just scrub the content
+            // that actually varies, but we don't have enough regular
+            // expression support for that yet.
+            textMatcher_.addLineToSkip("^;[[:blank:]] *File '.*' was generated.*$");
+            textMatcher_.addLineToSkip("^;[[:blank:]]*By user:.*$");
+            textMatcher_.addLineToSkip("^;[[:blank:]]*On host:.*$");
+            textMatcher_.addLineToSkip("^;[[:blank:]]*At date:.*$");
+            textMatcher_.addLineToSkip("^;[[:blank:]]*:-\\).*\\(-:.*$");
+            textMatcher_.addLineToSkip("^;[[:blank:]]*Executable:.*$");
+            textMatcher_.addLineToSkip("^;[[:blank:]]*Data prefix:.*$");
+            textMatcher_.addLineToSkip("^;[[:blank:]]*Working dir:.*$");
+            textMatcher_.addLineToSkip("^;[[:blank:]]*gmxpreprocess-test.*$");
+            setOutputFile("-o", "conf.gro", ConfMatch());
+            setOutputFile("-p", "topol.top", TextFileMatch(textMatcher_));
+        }
+
+        void runTest(const CommandLine &args)
+        {
+            CommandLine &cmdline = commandLine();
+            cmdline.merge(args);
+
+            ASSERT_EQ(0, gmx_pdb2gmx(cmdline.argc(), cmdline.argv()));
+            checkOutputFiles();
+        }
+        FilteringExactTextMatch textMatcher_;
+};
+
+TEST_F(Pdb2gmxTest, BasicWorks)
+{
+    const char *const cmdline[] = {
+        "pdb2gmx", "-ignh", "-ff", "oplsaa", "-water", "tip3p", "-vsite", "none"
+    };
+    setInputFile("-f", "1aml.pdb");
+    runTest(CommandLine(cmdline));
+}
+
+} // namespace
+} // namespace
+} // namespace
