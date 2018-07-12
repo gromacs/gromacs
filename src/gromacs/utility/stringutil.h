b8b453fc62cc989e29efeb803e44c7f5a246a0fb
Roland Schulz <roland.schulz@intel.com>
2018-07-12 15:03:24 -0700

    formatString: warn for type mismatch
    Related #2570
    
    Change-Id: Ie2bc5181a7fee1c0d1a647a12d8c73c4fb3ebe96

--- src/gromacs/utility/stringutil.h
+++ src/gromacs/utility/stringutil.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2011,2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2011,2012,2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -177,7 +177,11 @@ std::string stripString(const std::string &str);
  * instead of requiring a preallocated buffer.  Arbitrary length output is
  * supported.
  */
-std::string formatString(const char *fmt, ...);
+std::string formatString(const char *fmt, ...)
+#ifdef __GNUC__
+__attribute__ ((format (printf, 1, 2)));
+#endif
+;
 /*! \brief
  * Formats a string (vsnprintf() wrapper).
  *
