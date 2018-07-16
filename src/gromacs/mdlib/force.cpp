2a0d0447968f0ae7513c20a3cc37b2748e113299
Prashanth Kanduri <kanduri@cscs.ch>
2018-07-16 13:10:12 +0200

    [WIP] iForceSchedule Abstraction
    Related: #2574
    Future: #2594 #2595
    
    This is the first in a series of patches and commits that aims to
    add the schedule abstraction within GROMACS. This aims to enable
    the development of custom schedules specific to method (FMM/PME/RF/Multigrid),
    hardware architecture (accelerators) or parallelization paradigms (MPI/task-based
    programming).
    
    These implementations of schedules are expected to be easily maintainable
    and modular. Each of the concrete implementations  would optimally
    overlap communication, computation and reduction tasks of the various processes
    involved.
    
    A selectSchedule(...) function would take in the execution context and
    hopefully based on problem definition and hardware, would make an optimal
    selection. This could also be done by running a few 100 iterations in several
    candidate schedules.
    
    Change-Id: Ia1b0f1046edc1579e4851eaddea3621b9008d251

--- src/gromacs/mdlib/force.cpp
+++ src/gromacs/mdlib/force.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -342,8 +342,6 @@ void do_force_lowlevel(t_forcerec           *fr,
                     flags);
 
 
-    *cycles_pme = 0;
-
     /* Do long-range electrostatics and/or LJ-PME, including related short-range
      * corrections.
      */
@@ -465,25 +463,31 @@ void do_force_lowlevel(t_forcerec           *fr,
                         ddCloseBalanceRegionCpu(cr->dd);
                     }
 
-                    wallcycle_start(wcycle, ewcPMEMESH);
-                    status = gmx_pme_do(fr->pmedata,
-                                        0, md->homenr - fr->n_tpi,
-                                        x,
-                                        as_rvec_array(forceWithVirial->force_.data()),
-                                        md->chargeA, md->chargeB,
-                                        md->sqrt_c6A, md->sqrt_c6B,
-                                        md->sigmaA, md->sigmaB,
-                                        box, cr,
-                                        DOMAINDECOMP(cr) ? dd_pme_maxshift_x(cr->dd) : 0,
-                                        DOMAINDECOMP(cr) ? dd_pme_maxshift_y(cr->dd) : 0,
-                                        nrnb, wcycle,
-                                        ewaldOutput.vir_q, ewaldOutput.vir_lj,
-                                        &Vlr_q, &Vlr_lj,
-                                        lambda[efptCOUL], lambda[efptVDW],
-                                        &ewaldOutput.dvdl[efptCOUL],
-                                        &ewaldOutput.dvdl[efptVDW],
-                                        pme_flags);
-                    *cycles_pme = wallcycle_stop(wcycle, ewcPMEMESH);
+                    if (cycles_pme != nullptr)
+                    {
+
+                        *cycles_pme = 0;
+                        wallcycle_start(wcycle, ewcPMEMESH);
+                        status = gmx_pme_do(fr->pmedata,
+                                            0, md->homenr - fr->n_tpi,
+                                            x,
+                                            as_rvec_array(forceWithVirial->force_.data()),
+                                            md->chargeA, md->chargeB,
+                                            md->sqrt_c6A, md->sqrt_c6B,
+                                            md->sigmaA, md->sigmaB,
+                                            box, cr,
+                                            DOMAINDECOMP(cr) ? dd_pme_maxshift_x(cr->dd) : 0,
+                                            DOMAINDECOMP(cr) ? dd_pme_maxshift_y(cr->dd) : 0,
+                                            nrnb, wcycle,
+                                            ewaldOutput.vir_q, ewaldOutput.vir_lj,
+                                            &Vlr_q, &Vlr_lj,
+                                            lambda[efptCOUL], lambda[efptVDW],
+                                            &ewaldOutput.dvdl[efptCOUL],
+                                            &ewaldOutput.dvdl[efptVDW],
+                                            pme_flags);
+                        *cycles_pme = wallcycle_stop(wcycle, ewcPMEMESH);
+
+                    }
                     if (status != 0)
                     {
                         gmx_fatal(FARGS, "Error %d in reciprocal PME routine", status);
