35dce53858d0ea5087d06e18f5b4d8fee95d09c3
Kevin Boyd <kevin.boyd@uconn.edu>
2018-08-01 21:33:19 -0400

    Fix gmx solvate topology updating
    The topology update of gmx solvate was hardcoded to SOL molecules
    (and some of the reported quantities also registered HOH and WAT).
    
    Changed to allow updates based on any solvent molecule information.
    
    Also, now allows for updates with multiple solvent molecule types
    in the input solvent configuration
    
    Added test case for topology update with multiple solvents
    
    fixes #1929
    
    Change-Id: If506e0f509c8a25cb615fb8d3f15b4dfdb239afa

--- src/gromacs/gmxpreprocess/tests/solvate.cpp
+++ src/gromacs/gmxpreprocess/tests/solvate.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2013,2014,2015,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -58,6 +58,7 @@ namespace
 
 using gmx::test::CommandLine;
 using gmx::test::ConfMatch;
+using gmx::test::ExactTextMatch;
 
 class SolvateTest : public gmx::test::CommandLineTestBase
 {
@@ -121,6 +122,26 @@ TEST_F(SolvateTest, shell_Works)
     };
     setInputFile("-cp", "spc-and-methanol.gro");
     commandLine().addOption("-shell", 1.0);
+
+    runTest(CommandLine(cmdline));
+}
+
+TEST_F(SolvateTest, update_Topology_Works)
+{
+    // use solvent box with 2 solvents, check that topology has been updated
+    const char *const cmdline[] = {
+        "solvate"
+    };
+    setInputFile("-cs", "mixed_solvent.gro");
+    setInputFile("-cp", "simple.gro");
+
+    // TODO: Consider adding a convenience method for this.
+    // Copies topology file to where it would be found as an output file, so the copied
+    // .top file is used as both input and output
+    std::string topFileName           = fileManager().getInputFilePath("simple.top");
+    std::string modifiableTopFileName = fileManager().getTemporaryFilePath("simple.top");
+    gmx_file_copy(topFileName.c_str(), modifiableTopFileName.c_str(), true);
+    setOutputFile("-p", "simple.top", ExactTextMatch());
 
     runTest(CommandLine(cmdline));
 }
