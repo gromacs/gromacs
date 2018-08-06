40184092036151e71ad8eda576843ed57efd2a21
Roland Schulz <roland.schulz@intel.com>
2018-08-05 19:17:32 -0700

    Disable FPE for GCC 7.* with optimizations
    Only GCC 7 with RelWithAsserts has issues. And debugging
    has been unsuccessful.
    
    Related #2404
    
    Change-Id: I0ed3161c12cc8c33af3280f30a97a25c5d38cf1b

--- src/gromacs/mdrun/runner.cpp
+++ src/gromacs/mdrun/runner.cpp
@@ -886,14 +886,20 @@ int Mdrunner::mdrunner()
 
     // Enable FP exception but not in Release mode and not for compilers
     // with known buggy FP exception support (clang with any optimization)
-#if !defined NDEBUG && !(defined __clang__ && defined __OPTIMIZE__)
+    // or suspected buggy FP exception support (gcc 7.* with optimization).
+#if !defined NDEBUG && \
+    !((defined __clang__ || (defined(__GNUC__) && !defined(__ICC) && __GNUC__ == 7)) \
+    && defined __OPTIMIZE__)
+    const bool ENABLE_FPE = !EI_TPI(inputrec->eI) &&
+        inputrec->cutoff_scheme == ecutsVERLET;
+#else
+    const bool ENABLE_FPE = false;
+#endif
     //FIXME - reconcile with gmx_feenableexcept() call from CommandLineModuleManager::run()
-    if (!EI_TPI(inputrec->eI) &&
-        inputrec->cutoff_scheme == ecutsVERLET)
+    if (ENABLE_FPE)
     {
         gmx_feenableexcept();
     }
-#endif
 
     // Build a data structure that expresses which kinds of non-bonded
     // task are handled by this rank.
@@ -1380,13 +1386,10 @@ int Mdrunner::mdrunner()
 
     /* Reset FPEs (important for unit tests) by disabling them. Assumes no
      * exceptions were enabled before function was called. */
-#if !defined NDEBUG && !(defined __clang__ && defined __OPTIMIZE__)
-    if (!EI_TPI(inputrec->eI) &&
-        inputrec->cutoff_scheme == ecutsVERLET)
+    if (ENABLE_FPE)
     {
         gmx_fedisableexcept();
     }
-#endif
 
     rc = (int)gmx_get_stop_condition();
 
