308c1cf3fdfe0d882d4413e43b3c0917c2033f37
Paul Bauer <paul.bauer.q@gmail.com>
2018-08-07 15:08:28 +0200

    [WIP] Add C++ coordinate file output manager and modules
    Adds the ICoordinateOutput interface to modulate the writing of
    coordinate files using the outputmanager wrapper.
    The interface is build to support a modular way to perform manipulations
    of coordinate files before writing them to disk, with the outputmanager
    used to register individual modules for manipulations and for wrapping
    the (currently old c) calls to the file writing routines. This is
    intented to be used to be able to abstract away the old file writing
    routines (and as such allowing their replacement in a more easy way) and
    to have a modular interface for the future replacement of analysis tools
    that have a need to write coordinate files.
    
    Change-Id: I73c8e483d595e4393da1536f83e0310b959be50f

---
+++ src/gromacs/fileio/modules/setatoms.h
@@ -0,0 +1,150 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Module to declare if atoms data is available for file writing.
+ *
+ * \author
+ * \inpublicapi
+ * \ingroup fileio
+ */
+#ifndef GMX_FILEIO_SETATOMS_H
+#define GMX_FILEIO_SETATOMS_H
+
+#include <algorithm>
+
+#include "gromacs/fileio/coordinateoutput.h"
+#include "gromacs/trajectory/trajectoryframe.h"
+
+namespace gmx
+{
+
+/*!\brief
+ * SetAtoms class controls availability of atoms data.
+ *
+ * This modules allows the user to specify if a coordiante frame
+ * should contain the t_atoms data structure or not, and sets it in the
+ * new coordinate frame from either the current topology or from
+ * the data in the coordinate frame. The data is later used to identify
+ * if certain output file types are legal or not.
+ *
+ * \inpublicapi
+ * \ingroup fileio
+ *
+ */
+class SetAtoms : public ICoordinateOutput
+{
+    public:
+        /*! \brief
+         * Default constructor for SetAtoms should not be used.
+         *
+         * Class should only be initialized with at least the base selection.
+         */
+        SetAtoms() = delete;
+        /*! \brief
+         * Construct SetAtoms object with choice for boolean value
+         * for availability of the t_atoms struct.
+         *
+         * Can be used to initialize SetAtoms from outside of trajectoryanalysis
+         * framework.
+         */
+        explicit SetAtoms(t_atoms *atoms)
+        {
+            setLocalAtoms_ = setAtoms(atoms);
+        }
+        /*! \brief
+         * Move constructor for SetAtoms.
+         */
+        SetAtoms &operator=(SetAtoms &&old)
+        {
+            setLocalAtoms_ = std::move(old.setLocalAtoms_);
+            return *this;
+        }
+        /*! \brief
+         *  Move constructor for SetAtoms.
+         */
+        SetAtoms(SetAtoms &&)
+        {
+        }
+
+        ~SetAtoms() {}
+
+        /*! \brief
+         * Change coordinate frame information for output.
+         *
+         * Changes the frame t_atoms struct according to user choice and
+         * availability.
+         *
+         * \param[in] input Coordinate frame to be modified later.
+         */
+        virtual void processFrame(const int /*framenumber*/, t_trxframe *input);
+
+    private:
+        //! Local function to check that we have a proper t_atoms struct available.
+        bool haveLocalAtoms()
+        {
+            return haveAtoms();
+        }
+        bool setLocalAtoms()
+        {
+            return haveLocalAtoms() && setLocalAtoms_;
+        }
+        /*! \brief
+         *  Checking if t_trxframe has the atom information saved within.
+         *
+         *  \param[in] input t_trxframe before we start modifying it.
+         */
+        bool haveFrameAtoms(const t_trxframe &input)
+        {
+            return input.bAtoms;
+        }
+        /*! \brief
+         * User variable choice to determine if we want to set new atom information
+         * or not.
+         *
+         * This value is set during construction time and decides if the atom information
+         * in a t_trxframe gets replaced by new, user chosen data, or if the old data (if
+         * available) is going to be used instead.
+         */
+        bool                              setLocalAtoms_;
+};
+
+//! Smart pointer to manage the outputselector object.
+typedef std::unique_ptr<SetAtoms>
+    SetAtomsPointer;
+
+} // namespace gmx
+
+#endif
