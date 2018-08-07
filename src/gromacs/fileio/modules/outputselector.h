d89ccf81a562bd5d47ff32a980fda978e4fe3942
Paul Bauer <paul.bauer.q@gmail.com>
2018-08-07 15:08:28 +0200

    [WIP] Add C++ coordinate file output manager and modules
    Adds the ICoordinateOutput interface to modulate the writing of
    coordinate files using the outputmanager wrapper.
    Also, the interface IModuleCallback is added to allow the modules
    to check their depencies for file output against the parent
    outputmanager so that files will only be created when they action can
    actually succeed.
    The interface is build to support a modular way to perform manipulations
    of coordinate files before writing them to disk, with the outputmanager
    used to register individual modules for manipulations and for wrapping
    the (currently old c) calls to the file writing routines. This is
    intented to be used to be able to abstract away the old file writing
    routines (and as such allowing their replacement in a more easy way) and
    to have a modular interface for the future replacement of analysis tools
    that have a need to write coordinate files.
    
    Change-Id: I73c8e483d595e4393da1536f83e0310b959be50f

---
+++ src/gromacs/fileio/modules/outputselector.h
@@ -0,0 +1,132 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Changes the coordinates in output according to predefined selections.
+ *
+ * \author
+ * \inpublicapi
+ * \ingroup fileio
+ */
+#ifndef GMX_FILEIO_OUTPUTSELECTOR_H
+#define GMX_FILEIO_OUTPUTSELECTOR_H
+
+#include <algorithm>
+
+#include "gromacs/fileio/coordinateoutput.h"
+#include "gromacs/fileio/modulecallback.h"
+#include "gromacs/selection/selectionoption.h"
+#include "gromacs/trajectory/trajectoryframe.h"
+
+namespace gmx
+{
+
+/*!\brief
+ * OutputSelector class controls setting which coordinates are actual written.
+ *
+ * This method controls which coordinates are written from the original file
+ * according to which flags are set in the coordinate frame, and allocates the new
+ * data for coordinates, velocities and forces. The coordinates written are selected
+ * through the select mechanic from user input.
+ *
+ * \inpublicapi
+ * \ingroup fileio
+ *
+ */
+class OutputSelector : public ICoordinateOutput
+{
+    public:
+        /*! \brief
+         * Default constructor for OutputSelector should not be used.
+         *
+         * Class should only be initialized with at least the base selection.
+         */
+        OutputSelector() = delete;
+        /*! \brief
+         * Construct OutputSelector object with initial selection.
+         *
+         * Can be used to initialize OutputSelector from outside of trajectoryanalysis
+         * framework.
+         */
+        explicit OutputSelector(Selection *sel) : ICoordinateOutput(efCoordinateOutput),
+                                                  sel_(sel)
+        {
+        }
+        /*! \brief
+         * Move constructor for OutputSelector.
+         */
+        OutputSelector &operator=(OutputSelector &&old)
+        {
+            sel_         = std::move(old.sel_);
+            return *this;
+        }
+        /*! \brief
+         *  Move constructor for OutputSelector.
+         */
+        OutputSelector(OutputSelector &&old) : ICoordinateOutput(old.moduleFlags_),
+                                               sel_(std::move(old.sel_))
+        {
+        }
+
+        ~OutputSelector() {}
+        /*! \brief
+         * Change coordinate frame information for output.
+         *
+         * Takes the previously internally stored coordinates and saves them again.
+         * Applies correct number of atoms in this case.
+         *
+         * \param[in] input Coordinate frame to be modified later.
+         */
+        virtual void processFrame(const int /*framenumber*/, t_trxframe *input);
+
+    private:
+
+        /*! \brief
+         * Selection of atoms that will be written to disk.
+         *
+         * Internal selection of atoms chosen by the user that will be written
+         * to disk during processing. All actions that the filehandler performs
+         * will only be on those atoms, with the remaining ones being not affected.
+         */
+        Selection                            *sel_;
+};
+
+//! Smart pointer to manage the outputselector object.
+typedef std::unique_ptr<OutputSelector>
+    OutputSelectorPointer;
+
+} // namespace gmx
+
+#endif
---
+++ src/gromacs/fileio/modules/setbox.h
@@ -0,0 +1,143 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Set frame box from user input.
+ *
+ * \author
+ * \inpublicapi
+ * \ingroup fileio
+ */
+#ifndef GMX_FILEIO_SETBOX_H
+#define GMX_FILEIO_SETBOX_H
+
+#include <algorithm>
+
+#include "gromacs/fileio/coordinateoutput.h"
+#include "gromacs/fileio/modulecallback.h"
+#include "gromacs/math/vec.h"
+#include "gromacs/trajectory/trajectoryframe.h"
+
+namespace gmx
+{
+
+/*!\brief
+ * SetBox class allows changing writing of velocities to file.
+ *
+ * This class allows the user to define if velocities should be written
+ * to the output coordinate file, and checks if they are available from the
+ * currently processed data.
+ *
+ * \inpublicapi
+ * \ingroup fileio
+ *
+ */
+class SetBox : public ICoordinateOutput
+{
+    public:
+        /*! \brief
+         * Default constructor for SetBox should not be used.
+         *
+         * Class should only be initialized with at least the base selection.
+         */
+        SetBox() = delete;
+        /*! \brief
+         * Construct SetBox object with choice for boolean value.
+         *
+         * Can be used to initialize SetBox from outside of trajectoryanalysis
+         * with the user specified option to write coordinate velocities or not.
+         * framework.
+         */
+        explicit SetBox(matrix box, bool bHaveNewBox) : ICoordinateOutput(efAnyOutputSupported),
+                                                        bHaveNewBox_(bHaveNewBox)
+        {
+            copy_mat(box, box_);
+        }
+        /*! \brief
+         * Copy constructor.
+         */
+        SetBox(const SetBox &old) = delete;
+        /*! \brief
+         * Assignment operator.
+         */
+        SetBox &operator=(const SetBox &old) = delete;
+        /*! \brief
+         * Move constructor for SetBox.
+         */
+        SetBox &operator=(SetBox &&old)
+        {
+            copy_mat(old.box_, box_);
+            bHaveNewBox_ = std::move(old.bHaveNewBox_);
+            return *this;
+        }
+        /*! \brief
+         *  Move constructor for SetBox.
+         */
+        SetBox(SetBox &&old) : ICoordinateOutput(old.moduleFlags_),
+                               bHaveNewBox_(std::move(old.bHaveNewBox_))
+        {
+            copy_mat(old.box_, box_);
+        }
+
+        ~SetBox() {}
+
+        /*! \brief
+         * Change coordinate frame information for output.
+         *
+         * In this case, the correct flag for writing the velocities is applied
+         * to the output frame, depending on user selection and availability
+         * in the input data.
+         *
+         * \todo should this throw an error if velocity writing is desired
+         * but not possible from input data?
+         *
+         * \param[in] input Coordinate frame to be modified later.
+         */
+        virtual void processFrame(const int /*framenumner*/, t_trxframe *input);
+
+    private:
+        //! New box information from the user.
+        matrix                            box_;
+        //! Decide if new box will be written or not, default false.
+        bool                              bHaveNewBox_;
+};
+
+//! Smart pointer to manage the outputselector object.
+typedef std::unique_ptr<SetBox>
+    SetBoxPointer;
+
+} // namespace gmx
+
+#endif
