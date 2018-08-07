ce86d30a1eccb023d159911808f798700a364149
Paul Bauer <paul.bauer.q@gmail.com>
2018-08-07 15:08:28 +0200

    [WIP] Add C++ coordinate file output manager and modules
    Adds the ICoordinateOutput interface to modulate the writing of
    coordinate files using the outputmanager wrapper.
    Also, the interface IModuleCallback is added to allow the modules
    to check their depencies for file output against the parent
    outputmanager so that files will only be created when they action can
    actually succeed.
    The interface is build to support a modular way to perform manipulations
    of coordinate files before writing them to disk, with the outputmanager
    used to register individual modules for manipulations and for wrapping
    the (currently old c) calls to the file writing routines. This is
    intented to be used to be able to abstract away the old file writing
    routines (and as such allowing their replacement in a more easy way) and
    to have a modular interface for the future replacement of analysis tools
    that have a need to write coordinate files.
    
    Change-Id: I73c8e483d595e4393da1536f83e0310b959be50f

---
+++ src/gromacs/fileio/modules/setvelocities.h
@@ -0,0 +1,141 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Set flag for writing velocity information to file or not.
+ *
+ * \author
+ * \inpublicapi
+ * \ingroup fileio
+ */
+#ifndef GMX_FILEIO_SETVELOCITY_H
+#define GMX_FILEIO_SETVELOCITY_H
+
+#include <algorithm>
+
+#include "gromacs/fileio/coordinateoutput.h"
+#include "gromacs/fileio/modulecallback.h"
+#include "gromacs/trajectory/trajectoryframe.h"
+
+namespace gmx
+{
+
+/*!\brief
+ * SetVelocities class allows changing writing of velocities to file.
+ *
+ * This class allows the user to define if velocities should be written
+ * to the output coordinate file, and checks if they are available from the
+ * currently processed data.
+ *
+ * \inpublicapi
+ * \ingroup fileio
+ *
+ */
+class SetVelocities : public ICoordinateOutput
+{
+    public:
+        /*! \brief
+         * Default constructor for SetVelocities should not be used.
+         *
+         * Class should only be initialized with at least the base selection.
+         */
+        SetVelocities() = delete;
+        /*! \brief
+         * Construct SetVelocities object with choice for boolean value.
+         *
+         * Can be used to initialize SetVelocities from outside of trajectoryanalysis
+         * with the user specified option to write coordinate velocities or not.
+         * framework.
+         */
+        explicit SetVelocities(ChangeSettingType velocity) : ICoordinateOutput(efVelocityOutput),
+                                                             velocity_(velocity)
+        {
+        }
+        /*! \brief
+         * Copy constructor.
+         */
+        SetVelocities(const SetVelocities &old) = delete;
+        /*! \brief
+         * Assignment operator.
+         */
+        SetVelocities &operator=(const SetVelocities &old) = delete;
+        /*! \brief
+         * Move constructor for SetVelocities.
+         */
+        SetVelocities &operator=(SetVelocities &&old)
+        {
+            velocity_ = std::move(old.velocity_);
+            return *this;
+        }
+        /*! \brief
+         *  Move constructor for SetVelocities.
+         */
+        SetVelocities(SetVelocities &&old) : ICoordinateOutput(old.moduleFlags_),
+                                             velocity_(std::move(old.velocity_))
+        {
+        }
+
+        ~SetVelocities() {}
+
+        /*! \brief
+         * Change coordinate frame information for output.
+         *
+         * In this case, the correct flag for writing the velocities is applied
+         * to the output frame, depending on user selection and availability
+         * in the input data.
+         *
+         * \todo should this throw an error if velocity writing is desired
+         * but not possible from input data?
+         *
+         * \param[in] input Coordinate frame to be modified later.
+         */
+        virtual void processFrame(const int /*framenumber*/, t_trxframe *input);
+
+    private:
+        /*! \brief
+         * Flag to specify if velocities should be written.
+         *
+         * Internal storage for the user choice for writing coordinate velocities.
+         */
+        ChangeSettingType                            velocity_;
+};
+
+//! Smart pointer to manage the outputselector object.
+typedef std::unique_ptr<SetVelocities>
+    SetVelocitiesPointer;
+
+} // namespace gmx
+
+#endif
