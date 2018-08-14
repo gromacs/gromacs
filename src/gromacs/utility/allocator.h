126e9375578abc7950c7dd19dac0ff3e140bdc0a
Roland Schulz <roland.schulz@intel.com>
2018-08-14 15:23:06 -0700

    Prevent HostAllocator to be copied/propagated
    A HostVector shouldn't be copied without the calling code
    having a choice on the pinning policy.
    This commit prevents copy construction. Move construction and
    copy and move assignment is still allowed.
    For move construction the allocator is reused. But because
    the memory is reused no new pinning happends.
    For copy/move assignment the pinning policy of the destination
    container is used.
    
    Change-Id: I15034e4db2434ea46eda84e70e3f8e6072660d20

--- src/gromacs/utility/allocator.h
+++ src/gromacs/utility/allocator.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2017, by the GROMACS development team, led by
+ * Copyright (c) 2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -249,6 +249,11 @@ class Allocator : public AllocationPolicy
          */
         bool
         operator!=(const Allocator &rhs) const { return !operator==(rhs); }
+
+        Allocator select_on_container_copy_construction() const
+        {
+            return Allocator(AllocationPolicy::select_on_container_copy_construction());
+        }
 };
 
 }      // namespace gmx
