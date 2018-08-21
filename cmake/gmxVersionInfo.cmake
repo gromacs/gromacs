5d5eb383a4b899b07faf1ab1635803225092ba8a
Paul Bauer <paul.bauer.q@gmail.com>
2018-08-21 11:05:47 +0200

    Version 2018.3
    Added some release notes for issues that were missing them.
    
    Change-Id: I5480e5421387081fb7b6fc26885d983e6b4046b9

--- cmake/gmxVersionInfo.cmake
+++ cmake/gmxVersionInfo.cmake
@@ -203,7 +203,7 @@ set(GMX_VERSION_SUFFIX "")
 # code being able to dynamically link with a version of libgromacs
 # that might not work.
 set(LIBRARY_SOVERSION_MAJOR 3)
-set(LIBRARY_SOVERSION_MINOR 2)
+set(LIBRARY_SOVERSION_MINOR 3)
 set(LIBRARY_VERSION ${LIBRARY_SOVERSION_MAJOR}.${LIBRARY_SOVERSION_MINOR}.0)
 
 #####################################################################
@@ -231,7 +231,7 @@ set(REGRESSIONTEST_BRANCH "refs/heads/release-2018")
 # build the regressiontests tarball with all the right naming. The
 # naming affects the md5sum that has to go here, and if it isn't right
 # release workflow will report a failure.
-set(REGRESSIONTEST_MD5SUM "135149af467a37714a92bc29801cafda" CACHE INTERNAL "MD5 sum of the regressiontests tarball for this GROMACS version")
+set(REGRESSIONTEST_MD5SUM "368aabf0eb4e6fbc9abb7961475f66de" CACHE INTERNAL "MD5 sum of the regressiontests tarball for this GROMACS version")
 
 math(EXPR GMX_VERSION_NUMERIC
      "${GMX_VERSION_MAJOR}*10000 + ${GMX_VERSION_PATCH}")
