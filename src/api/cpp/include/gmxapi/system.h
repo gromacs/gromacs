04d898be12011add0edf94e94df9c21d76dd7387
M. Eric Irrgang <ericirrgang@gmail.com>
2018-08-23 20:40:41 +0300

    add gmxapi::System
    Change-Id: I6a292a3fe91556d06b9c384b1480c891decffd3f

---
+++ src/api/cpp/include/gmxapi/system.h
@@ -0,0 +1,142 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#ifndef GMXAPI_SYSTEM_H
+#define GMXAPI_SYSTEM_H
+/*! \file
+ * \brief Declare container for molecular systems
+ *
+ * \ingroup gmxapi
+ */
+#include <memory>
+
+#include "gmxapi/gmxapi.h"
+
+namespace gmxapi
+{
+
+// Forward declaration for a return type defined elsewhere.
+class Session;
+
+/// Container for molecular model and simulation parameters.
+/*!
+ * \brief Deprecated: A wrapper for gmx::Mdrunner
+ *
+ * It was not intended to end up as such, but gmxapi::System has ended up as basically a
+ * wrapper for the gmx::Mdrunner simulator object. As it is, this class does not fit the
+ * current gmxapi paradigm and will be removed, reworked, or renamed soon.
+ *
+ * # Protocol
+ *
+ * As of gmxapi 0.0.6, a simulation is configured and launched as follows.
+ *
+ * 1. Caller gets a System handle with gmxapi::fromTprFile().
+ * 2. Caller optionally attaches additional MD Modules with getSpec()->addModule(std::shared_ptr<gmxapi::MDModule> module). See gmxapi::MDHolder
+ * 3. Caller gets a runnable object by passing a Context to System::launch()
+ *
+ * During launch() configured gmxapi::MDModules are attached to the simulator, which is
+ * then run by calling run() on the object returned by launch().
+ *
+ * \ingroup gmxapi
+ */
+class System final
+{
+    public:
+        /*! \brief Private implementation class.
+         *
+         * System::Impl does not have a public interface and is only exposed in opaque pointers.
+         */
+        class Impl;
+
+        /// No copy.
+        /*! The semantics of copying a System are ambiguous, so disallow implicit
+         * copy. Some sort of prototype or clone idiom is probably useful, but
+         * needs to explicitly identify any expensive operations.
+         */
+        System(const System &)            = delete;
+        /// No copy.
+        System              &operator=(const System &) = delete;
+
+        /// Allow move.
+        System(System &&) noexcept;
+        /// Allow move.
+        System &operator=(System &&) noexcept;
+
+        /*!
+         * \brief Create by taking ownership of an implementation object.
+         *
+         * \param implementation
+         */
+        explicit System(std::unique_ptr<Impl> &&implementation);
+
+        /// \cond internal
+        /// Destructor defined later to allow unique_ptr members of partially-defined types.
+        ~System();
+        /// \endcond
+
+        /// \}
+
+        /*!
+         * \brief Get the status of the last API call involving this system.
+         *
+         * \return copy of the most recent status.
+         */
+        Status status();
+
+//        /// Get a handle to system atoms.
+//        std::unique_ptr<Atoms> atoms();
+
+    private:
+        /*!
+         * \brief Opaque pointer to implementation.
+         */
+        std::unique_ptr<Impl> impl_;
+};
+
+
+/// Defines an MD workflow from a TPR file.
+/*! The TPR file has sufficient information to fully specify an MD run, though
+ * various parameters are implicit until the work is launched. The TPR filename
+ * provided must refer to identical TPR files at the API client and at the
+ * master rank of the execution host.
+ *
+ * \param filename Filesystem path of TPR file.
+ * \returns gmxapi::System object with the specified workflow.
+ * \ingroup gmxapi
+ */
+std::unique_ptr<gmxapi::System> fromTprFile(std::string filename);
+
+}      // end namespace gmxapi
+
+#endif // include guard
---
+++ src/api/cpp/system.cpp
@@ -0,0 +1,109 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#include <array>
+
+#include "system-impl.h"
+#include "gmxapi/md.h"
+#include "gmxapi/status.h"
+#include "gmxapi/system.h"
+#include "gromacs/utility.h"
+#include "gromacs/compat/make_unique.h"
+#include "gromacs/mdrun/runner.h"
+
+namespace gmxapi
+{
+
+System::Impl::~Impl() = default;
+
+
+Status System::status()
+{
+    assert(impl_ != nullptr);
+    return impl_->status();
+}
+
+System::System(std::unique_ptr<System::Impl> &&implementation) :
+    impl_ {std::forward < std::unique_ptr < System::Impl>>(implementation)}
+{
+    assert(impl_ != nullptr);
+}
+
+System::~System() = default;
+
+System::System(System &&) noexcept = default;
+
+System &System::operator=(System &&) noexcept = default;
+
+std::unique_ptr<gmxapi::System> fromTprFile(std::string filename)
+{
+    // Confirm the file is readable and parseable and note unique identifying information
+    // for when the work spec is used in a different environment.
+
+    // This may produce errors or throw exceptions in the future, but in 0.0.3 only memory allocation
+    // errors are possible, and we do not have a plan for how to recover from them.
+    auto systemImpl = gmx::compat::make_unique<System::Impl>(filename);
+    assert(systemImpl != nullptr);
+    auto system = gmx::compat::make_unique<System>(std::move(systemImpl));
+
+    // The TPR file has enough information for us to
+    //  1. choose an MD engine
+    //  2. Get structure information
+    //  3. Get topology information
+    //  4. Get a lot of simulation and runtime parameters, but not all.
+    // It does not have enough information on its own to determine much about the
+    // necessary computation environment. That comes from environment
+    // introspection and user runtime options.
+
+    return system;
+}
+
+Status System::Impl::status() const
+{
+    return *status_;
+}
+
+System::Impl::Impl(std::string filename) :
+    status_
+{
+    gmx::compat::make_unique<Status>(true)
+},
+filename_ {
+    filename
+}
+{
+    assert(status_ != nullptr);
+}
+
+} // end namespace gmxapi
