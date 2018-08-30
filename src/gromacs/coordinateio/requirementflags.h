8e80094377af7d277e831edb8048d08acaf21a71
Paul Bauer <paul.bauer.q@gmail.com>
2018-08-30 15:08:05 +0200

    Add C++ coordinate file output manager
    Adds the IOutputAdapter interface to organize coordinate output file
    writing, as well as enum flags to communicate requirements between output
    modules and output filetypes, and the outputmanager class to organize
    the writing of new coordinate files.
    
    OutputManager also allows the chaining of modules derived from
    IOutputAdapter together to perform actions that modify the meta
    information contained in a t_trxframe before writing the information to
    disk.
    
    IOutputAdapter can be used to communicate what requirements are specific
    file writing method has on the meta information contained in t_trxframe,
    with modules being able to proclaim those and outputmanager checking
    them against the requested file types before accepting the addition of a
    module to the processing chain.
    
    Change-Id: I1c29701b4ce788918cb6b3a47ca9277898a7bff4

---
+++ src/gromacs/coordinateio/requirementflags.h
@@ -0,0 +1,140 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Enum class defining the different requirements that outputadapter modules
+ * have for the output file type. Depending on matching requirements, they can then be
+ * added to an OutputManager object or not.
+ *
+ * \author Paul Bauer <paul.bauer.q@gmail.com>
+ * \libinternal
+ * \ingroup module_coordinateio
+ */
+#ifndef GMX_COORDINATEIO_REQUIREMENTFLAGS_H
+#define GMX_COORDINATEIO_REQUIREMENTFLAGS_H
+
+namespace gmx
+{
+
+/*!\brief
+ * Enum class ModuleRequirements defines the flags specifying the requirements
+ * of different outputadapter modules.
+ *
+ * When building the object for deciding on the output to a new coordinate file,
+ * the outputmanager needs to be able to validate that the dependencies of attached modules
+ * are fulfilled. Classes and interfaces that include the enum can check their
+ * dependencies against the information encoded in the flags and can then perform
+ * an appropriate reaction if there is a mismatch.
+ *
+ * \todo This should be converted to on enum class, but then it won't be able to
+ * perform the check of the flags as it is done now.
+ *
+ * \libinternal
+ * \ingroup module_coordinateio
+ *
+ */
+enum
+{
+    /*! \brief
+     * Base setting that says that the module has no requirements.
+     *
+     * Sets the flags to default setting to make sure all output methods
+     * are supported.
+     */
+    efBaseOutputManager = 1<<0,
+    /*! \brief
+     * Requires output method to support force output.
+     *
+     * If set, only output method supporting writing of forces to
+     * the coordinate file will work, others will generate an invalid
+     * input error.
+     */
+    efChangeForceModule     = 1<<1,
+    /*! \brief
+     * Requires output method to support velocity output.
+     *
+     * If set, only writing to files that support velocity output will succeed.
+     * Other writing methods will generate an error.
+     *
+     */
+    efChangeVelocityModule        = 1<<2,
+    /*! \brief
+     * Requires output to support changes to selection of coordinates.
+     *
+     * Default for most methods, will need to be able to write coordinates to
+     * output file or generate an error.
+     */
+    efChangeCoordinateSelectionModule      = 1<<3,
+    /*! \brief
+     * Requires support for connection information in output format.
+     *
+     * If set, only file output that supports writing of connection information will succeed.
+     * This means for now that only PDB and TNG files can be written. Other file writing
+     * methods will fail.
+     */
+    efChangeConnectionModule    = 1<<4,
+    /*! \brief
+     * Requires that output format supports the writing of atom information to the file.
+     *
+     * If set, files will only be written if they can output the information from t_atoms
+     * and otherwise create an error while writing.
+     */
+    efChangeAtomInformationModule = 1<<5,
+    /*! \brief
+     * Requires that output format supports custom output precision.
+     *
+     * If set, the coordinate files will only be written if they support the writing
+     * of custom precision of the included data.
+     */
+    efChangeOutputPrecisionModule = 1<<6,
+    /*! \brief
+     * Requires that output format supports changes to time in coordinate frame
+     */
+    efChangeTimeModule = 1<<7,
+    /*! \brief
+     * Requires that output format supports changing box information.
+     */
+    efChangeBoxModule = 1<<8,
+    /*! \brief
+     * Dummy value for testing.
+     *
+     * Flag set to zero to allow adding of dummy module.
+     */
+    efDummyModule = 1<<15,
+};
+
+} // namespace gmx
+
+#endif
