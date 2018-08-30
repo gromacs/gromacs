ab483d2dda63a497d16b25857dc40d03631b39f6
Paul Bauer <paul.bauer.q@gmail.com>
2018-08-30 15:08:05 +0200

    Add C++ coordinate file output manager
    Adds the ICoordinateOutput interface to organize coordinate output file
    writing, IModuleCallback to communicate requirements between output
    modules and output filetypes, and the outputmanager class to organize
    the writing of new coordinate files.
    
    OutputManager also allows the chaining of modules derived from
    ICoordinateOutput together to perform actions that modify the meta
    information contained in a t_trxframe before writing the information to
    disk.
    
    IModuleCallback is used to communicate what requirements are specific
    file writing method has on the meta information contained in t_trxframe,
    with modules being able to proclaim those and outputmanager checking
    them against the requested file types before accepting the addition of a
    module to the processing chain.
    
    TODO Add tests for basic operation of OutputManager.
    
    Change-Id: I1c29701b4ce788918cb6b3a47ca9277898a7bff4

---
+++ src/gromacs/coordinateio/coordinateoutput.h
@@ -0,0 +1,154 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Declares gmx::ICoordinateOutput interface for modifying coordinate
+ * file structures before writing them to disk.
+ *
+ * \author Paul Bauer <paul.bauer.q@gmail.com>
+ * \inlibraryapi
+ * \ingroup module_coordinateio
+ */
+#ifndef GMX_FILEIO_COORDINATEOUTPUT_H
+#define GMX_FILEIO_COORDINATEOUTPUT_H
+
+#include <algorithm>
+#include <memory>
+
+#include "gromacs/coordinateio/modulecallback.h"
+#include "gromacs/trajectory/trajectoryframe.h"
+#include "gromacs/utility/classhelpers.h"
+
+namespace gmx
+{
+
+/*!\brief
+ * CoordinateOutput class for handling trajectory file flag setting and processing.
+ *
+ * This interface provides the base point upon which modules that modify trajectory frame
+ * datastructures should be build. The interface itself does not provide any direct means
+ * to modify the data, but only gives the virtual method to perform work on a
+ * t_trxframe object.
+ *
+ * All modules derived from this interface will be also based on the IModuleCallback
+ * interface used to communicate if a given module is compatible with an output method
+ * implemented elsewhere.
+ *
+ * \inlibraryapi
+ * \ingroup module_coordinateio
+ *
+ */
+class ICoordinateOutput : public IModuleCallback
+{
+    public:
+        /*! \brief
+         * Default constructor for ICoordinateOutput interface.
+         *
+         * When building new modules based on the interface, the module
+         * needs to define its own requirements for file writing using the
+         * \p moduleFlag parameter passed during construction.
+         *
+         * \param[in] moduleFlag Bitmask defining the requirements for
+         *                       data using the given module.
+         */
+        ICoordinateOutput(unsigned long moduleFlag) : IModuleCallback(moduleFlag)
+        {
+        }
+        virtual ~ICoordinateOutput()
+        {
+        }
+        //! Move constructor for old object.
+        explicit ICoordinateOutput(ICoordinateOutput &&old) noexcept : IModuleCallback(old.moduleFlags_)
+        {
+        }
+
+        /*! \brief
+         * Change settings in t_trxframe according to user input.
+         *
+         * \param[in] framenumber Frame number as reported from the
+         *                        trajectoryanalysis framework or set by user.
+         * \param[in] input       Pointer to trajectory analysis frame that will
+         *                        be worked on.
+         */
+        virtual void processFrame(int framenumber, t_trxframe *input) = 0;
+
+        GMX_DISALLOW_COPY_AND_ASSIGN(ICoordinateOutput);
+
+};
+
+//! Smart pointer to manage the output manager object.
+typedef std::shared_ptr<ICoordinateOutput>
+    CoordinateOutputPointer;
+
+//! Enum class for setting basic flags in a t_trxframe
+enum class ChangeSettingType
+{
+    efUnchanged,
+    efUserYes,
+    efUserNo,
+    efUserPossible
+};
+//! Mapping for enums from ChangeSettingType.
+const char *const cChangeSettingTypeEnum[] = {
+    "unchanged", "yes", "no", "possible"
+};
+
+//! Enum class for setting fields new or not.
+enum class ChangeFrameUnchangedYesType
+{
+    efUnchanged,
+    efUserYes
+};
+//! Mapping for enums from ChangeFrameUnchangedYesType.
+const char *const cChangeFrameUnchangedYesTypeEnum[] = {
+    "unchanged", "yes"
+};
+
+//! Enum class for setting frame time from user input.
+enum class ChangeFrameTimeType
+{
+    efUnchanged,
+    efStartTime,
+    efTimeStep
+};
+
+//! Mapping for values from changing frame time.
+const char *const cChangeFrameTimeTypeEnum[] = {
+    "unchanged", "startime", "timestep"
+};
+
+} // namespace gmx
+
+#endif
---
+++ src/gromacs/coordinateio/modulecallback.h
@@ -0,0 +1,146 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Interface for callback from output manager to decide if added modules
+ * are feasible to be added to the output for a given filetype or not.
+ *
+ * \author Paul Bauer <paul.bauer.q@gmail.com>
+ * \inlibraryapi
+ * \ingroup module_coordinateio
+ */
+#ifndef GMX_FILEIO_MODULECALLBACK_H
+#define GMX_FILEIO_MODULECALLBACK_H
+
+#include <algorithm>
+#include <memory>
+
+#include "gromacs/trajectory/trajectoryframe.h"
+#include "gromacs/utility/classhelpers.h"
+
+namespace gmx
+{
+
+/*!\brief
+ * IModuleCallback interface for checking that requested module registration
+ * in output manager is valid.
+ *
+ * When building the object for deciding on the output to a new coordinate file,
+ * the outputmanager needs to be able to validate that the dependencies of attached modules
+ * are fulfilled. Classes and interfaces derived from this interface can check their
+ * dependencies against the information encoded in the flags and can then perform
+ * an appropriate reaction if there is a mismatch.
+ *
+ * \inlibraryapi
+ * \ingroup module_coordinateio
+ *
+ */
+class IModuleCallback
+{
+    public:
+        //! Recognized flags.
+        enum
+        {
+            /*! \brief
+             * Base setting that says that the module has no requirements.
+             *
+             * Sets the flags to default setting to make sure all output methods
+             * are supported.
+             */
+            efAnyOutputSupported = 1<<0,
+            /*! \brief
+             * Requires output method to support force output.
+             *
+             * If set, only output method supporting writing of forces to
+             * the coordinate file will work, others will generate an invalid
+             * input error.
+             */
+            efForceOutput     = 1<<1,
+            /*! \brief
+             * Requires output method to support velocity output.
+             *
+             * If set, only writing to files that support velocity output will succed.
+             * Other writing methods will generate an error.
+             *
+             */
+            efVelocityOutput        = 1<<2,
+            /*! \brief
+             * Requires output to support coordinate output.
+             *
+             * Default for most methods, will need to be able to write coordinates to
+             * output file or generate an error.
+             */
+            efCoordinateOutput      = 1<<3,
+            /*! \brief
+             * Requires support for connection information in output format.
+             *
+             * If set, only file output that supports writing of connection information will succeed.
+             * This means for now that only PDB and TNG files can be written. Other file writing
+             * methods will fail.
+             */
+            efConnectionOutput    = 1<<4,
+            /*! \brief
+             * Requires that output format supports the writing of atom information to the file.
+             *
+             * If set, files will only be written if they can output the information from t_atoms
+             * and otherwise create an error while writing.
+             */
+            efAtomOutput = 1<<5,
+        };
+
+        virtual ~IModuleCallback()
+        {
+        }
+        //! Output format dependent flags to check if we can perform the module addition.
+        unsigned long moduleFlags_ = 0;
+        //! Return the flag status that decides if we can add the module or not.
+        bool checkModuleFlag(unsigned long outputFlags) {return (moduleFlags_ & outputFlags) != 0u; }
+
+    protected:
+        /*! \brief
+         * Default constructor for CoordinateOutput class.
+         */
+        IModuleCallback(unsigned long flag) : moduleFlags_(flag) {}
+
+        GMX_DISALLOW_COPY_AND_ASSIGN(IModuleCallback);
+};
+
+//! Smart pointer to manage the output manager object.
+typedef std::shared_ptr<IModuleCallback>
+    ModuleCallbackPointer;
+
+} // namespace gmx
+
+#endif
