f5746396d9dd4b78c118d9aaa70fc84e12edade3
Paul Bauer <paul.bauer.q@gmail.com>
2018-08-30 15:08:05 +0200

    Add C++ coordinate file output manager
    Adds the ICoordinateOutput interface to organize coordinate output file
    writing, IModuleCallback to communicate requirements between output
    modules and output filetypes, and the outputmanager class to organize
    the writing of new coordinate files.
    
    OutputManager also allows the chaining of modules derived from
    ICoordinateOutput together to perform actions that modify the meta
    information contained in a t_trxframe before writing the information to
    disk.
    
    IModuleCallback is used to communicate what requirements are specific
    file writing method has on the meta information contained in t_trxframe,
    with modules being able to proclaim those and outputmanager checking
    them against the requested file types before accepting the addition of a
    module to the processing chain.
    
    Change-Id: I1c29701b4ce788918cb6b3a47ca9277898a7bff4

---
+++ src/gromacs/coordinateio/outputmanager.h
@@ -0,0 +1,190 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \file
+ * \brief
+ * Output manager takes care of opening files and writing output to them.
+ *
+ * \author
+ * \inpublicapi
+ * \ingroup module_coordinateio
+ */
+#ifndef GMX_COORDINATEIO_OUTPUTMANAGER_H
+#define GMX_COORDINATEIO_OUTPUTMANAGER_H
+
+#include <algorithm>
+#include <utility>
+
+#include "gromacs/coordinateio/coordinateoutput.h"
+#include "gromacs/fileio/filetypes.h"
+#include "gromacs/fileio/trxio.h"
+#include "gromacs/selection/selectionoption.h"
+#include "gromacs/topology/mtop_util.h"
+#include "gromacs/topology/topology.h"
+
+namespace gmx
+{
+
+/*! \libinternal \brief
+ * Storage for output adapters that modify the state of a t_trxframe object.
+ */
+struct OutputAdapterContainer
+{
+    //! Initialize module, same as for frame converters.
+    explicit OutputAdapterContainer(CoordinateOutputPointer module)
+    {
+        module_ = std::move(module);
+    }
+    //! Pointer to the module
+    CoordinateOutputPointer module_;
+};
+//! List of output adapters
+using CoordinateOutputAdapters = std::vector<OutputAdapterContainer>;
+//! Convenience name for using a reference to a list of adapters.
+using CoordinateOutputAdapterRef = gmx::ArrayRef<OutputAdapterContainer>;
+
+/*!\brief
+ * OutputManager class responsible for managing coordinate file output.
+ *
+ * The OutputManager has as its tasks the proper opening and closing of new
+ * teajectory files written from provided trajectory data. It performs the actual
+ * writing to disk and has the ability to recruit modules derived from
+ * ICoordinateOutput to perform changes to the meta information contained
+ * within the t_trxframe datastructures.
+ *
+ * When writing files to disk, the manager decides if a deep copy of the
+ * coordinate file information should be taken or not, depending on if changes
+ * to the stored values are going to be done or not. If the values are changed,
+ * the manager will clean up the information after each frame has been processed.
+ *
+ * \todo Does this make sense, or should the data be stored to avoid numerous
+ * rounds of allocations?
+ *
+ * \inpublicapi
+ * \ingroup module_coordinateio
+ *
+ */
+class OutputManager : public IModuleCallback
+{
+    public:
+        struct OutputManagerBuildHelper;
+
+        ~OutputManager()
+        {
+            closeFile();
+        }
+        /*! \brief
+         * Wrapper around processFrame to allow starting with a const
+         * input of t_trxframe.
+         *
+         * \param[in] framenumber Number of frame being currently processed.
+         * \param[in] input View of the constant t_trxframe object provided by the
+         *                  method that calls the output manager.
+         */
+        void prepareFrame(int framenumber, const t_trxframe &input);
+
+    private:
+        /*! \brief
+         * Minimal used constructor with reference to the correct selection used for file output.
+         */
+        OutputManager(std::string       name,
+                      int               filetype,
+                      unsigned long     flag,
+                      const Selection  &sel,
+                      const gmx_mtop_t *mtop) :
+            IModuleCallback(flag),
+            outputFileName_(std::move(name)),
+            outputFile_(nullptr),
+            filetype_(filetype),
+            sel_(sel),
+            mtop_(mtop)
+        {
+        }
+
+        /*! \brief
+         * Prepare file output.
+         *
+         * Opens the file on disk to allow writing of coordinate data.
+         */
+        void initOutput();
+
+        /*! \brief
+         * Closes new trajectory file after finishing the writing to it.
+         *
+         * Function to properly close opened output files after the file writing is finished.
+         * Has to be called from parent or will be invoked when the filehandler itself is destroyed.
+         */
+        void closeFile();
+
+        //! Name for the new coordinate file.
+        std::string                          outputFileName_;
+
+        //! File pointer to the coordinate file being written.
+        t_trxstatus               *outputFile_;
+
+        //! Storage of file type for determing what kind of file will be written to disk.
+        int          filetype_;
+
+        /*! \brief
+         * Selection of atoms to write out.
+         *
+         * Currently, OutputManager expects that the lifetime of the selection is longer
+         * than that of itself, and that the selection remains unchanged during this lifetime.
+         * A better approach will be to pass a copy of the selection to the it and expect it to
+         * manage the lifetime instead.
+         */
+        const Selection   &sel_;
+
+        /*! \brief  Adds new module to the process chain.
+         *
+         * \param[in] module unique_ptr to the module being added.
+         */
+        void addOutputAdapter(CoordinateOutputPointer module);
+
+        //! Storage for list of output adapters.
+        CoordinateOutputAdapters outputAdapters_;
+
+        //! Pointer to topology information if available.
+        const gmx_mtop_t *mtop_;
+
+        //! Return pointer to topology.
+        const gmx_mtop_t *getMtop() const { return mtop_; }
+};
+
+//! Smart pointer to manage the output manager object.
+using OutputManagerPointer = std::unique_ptr<OutputManager>;
+
+} // namespace gmx
+
+#endif
