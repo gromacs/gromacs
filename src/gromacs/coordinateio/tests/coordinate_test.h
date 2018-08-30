647cc79edbc04c81445e44ed89d5d5c437aedccd
Paul Bauer <paul.bauer.q@gmail.com>
2018-08-30 15:08:05 +0200

    Add C++ coordinate file output manager
    Adds the ICoordinateOutput interface to organize coordinate output file
    writing, IModuleCallback to communicate requirements between output
    modules and output filetypes, and the outputmanager class to organize
    the writing of new coordinate files.
    
    OutputManager also allows the chaining of modules derived from
    ICoordinateOutput together to perform actions that modify the meta
    information contained in a t_trxframe before writing the information to
    disk.
    
    IModuleCallback is used to communicate what requirements are specific
    file writing method has on the meta information contained in t_trxframe,
    with modules being able to proclaim those and outputmanager checking
    them against the requested file types before accepting the addition of a
    module to the processing chain.
    
    Change-Id: I1c29701b4ce788918cb6b3a47ca9277898a7bff4

---
+++ src/gromacs/coordinateio/tests/coordinate_test.h
@@ -0,0 +1,104 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*!\file
+ * \libinternal
+ * \brief
+ * Helper classes for outputmanager and coordinateio tests
+ *
+ * \author Paul Bauer <paul.bauer.q@gmail.com>
+ * \inlibraryapi
+ * \ingroup module_coordinateio
+ */
+
+#ifndef GMX_COORDINATEIO_TESTS_COORDINATEIO_H
+#define GMX_COORDINATEIO_TESTS_COORDINATEIO_H
+
+#include <gtest/gtest.h>
+
+#include "gromacs/compat/make_unique.h"
+#include "gromacs/coordinateio/builder.h"
+#include "gromacs/coordinateio/coordinateoutput.h"
+#include "gromacs/coordinateio/outputmanager.h"
+#include "gromacs/utility/exceptions.h"
+#include "gromacs/utility/smalloc.h"
+
+#include "testutils/cmdlinetest.h"
+#include "testutils/testasserts.h"
+
+namespace gmx
+{
+
+namespace test
+{
+
+class DummyOutputModule : public ICoordinateOutput
+{
+    public:
+        explicit DummyOutputModule(unsigned long flag) :
+            ICoordinateOutput(flag)
+        {
+        }
+        DummyOutputModule(DummyOutputModule &&old) noexcept :
+            ICoordinateOutput(old.moduleFlags_)
+        {}
+
+        ~DummyOutputModule() {}
+
+        virtual void processFrame(int /*framenumber*/, t_trxframe * /*input*/)
+        {}
+};
+
+//! Convenience typedef for dummy module
+typedef std::unique_ptr<DummyOutputModule>
+    DummyOutputModulePointer;
+
+/*!\brief
+ * Create minimal OutputManager using the provided builder.
+ *
+ * \param[in] filename Name of file to create OutputManager for.
+ * \param[in] dummyTopology Pointer to input top or null.
+ * \param[in] adapters Container for the outputadapters.
+ * \throws InconsistentInputError When builder can not create the OutputManager.
+ * \returns unique_ptr to new OutputManager object.
+ */
+OutputManagerPointer createMinimalOutputManager(const std::string       &filename,
+                                                const gmx_mtop_t        *dummyTopology,
+                                                CoordinateOutputAdapters adapters);
+
+} // namespace test
+
+} // namespace gmx
+
+#endif
