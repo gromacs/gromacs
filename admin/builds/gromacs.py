71408def847506ae5a52e3b07df53990b247cb93
Szilárd Páll <pall.szilard@gmail.com>
2018-09-04 23:18:21 +0200

    Update test matrices with new GPU specifiers
    Separates software stack and hardware requirement specification; the
    latter is done using the new gpuhw=vendor syntax.
    
    Note that OpenCL version specified is for now ignored (cmake support
    in follow-up commit).
    
    Refs #2161
    
    Change-Id: Ia1dfb175b2d47579577c5588a71d8b69a1bff07b

--- admin/builds/gromacs.py
+++ admin/builds/gromacs.py
@@ -50,7 +50,7 @@ extra_options = {
     'double': Option.simple,
     'thread-mpi': Option.bool,
     'gpu': Option.bool,
-    'opencl': Option.bool,
+    'nogpu' : Option.bool,
     'clang_cuda': Option.bool,
     'openmp': Option.bool,
     'nranks': Option.string,
@@ -95,7 +95,7 @@ def do_build(context):
         cmake_opts['GMX_SIMD'] = 'None'
     else:
         cmake_opts['GMX_SIMD'] = context.opts.simd
-    if context.opts.gpu or context.opts.opencl:
+    if context.opts.gpu or context.opts.cuda or context.opts.opencl:
         cmake_opts['GMX_GPU'] = 'ON'
         if context.opts.opencl:
             context.env.set_env_var('CUDA_PATH', context.env.cuda_root)
@@ -213,6 +213,9 @@ def do_build(context):
                 # OpenMP should always work when compiled in! Currently not set if
                 # not explicitly set
                 cmd += ' -ntomp 2'
+
+            if context.opts.nogpu:
+                context.env.set_env_var('GMX_DISABLE_GPU_DETECTION', '1')
 
             if context.opts.gpu_id:
                 cmd += ' -gpu_id ' + context.opts.gpu_id
