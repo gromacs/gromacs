fbc73b68be6bb4e11138355ac617ecacf31df4bf
M. Eric Irrgang <ericirrgang@gmail.com>
2018-09-11 13:01:50 +0300

    Move mdrun mainFunction to client code.
    supports gmxapi milestone 4, described at #2605
    
    API clients need to be able to initialize and run MD simulations with
    calls from separate code blocks or even translation units. Also, we
    need clear distinctions between a pre-launch master Mdrunner and the
    non-master Mdrunner threads.
    
    In this change:
    
    * Insert a Builder on which to develop the distinction between user
      interface and implementation code. User interface is handled in
      Director code, while the Builder and code further down the mdrun call
      stack should be user interface agnostic (i.e. not CLI-centric).
    * Introduces a Context object to hold some resources. (see milestone 5
      and issue #2587)
    * Prepare for more stateful Mdrunner objects, documenting resources in
      need of clearer management, describing future use cases and possible
      implementation details.
    * Make Mdrunner non-copyable and not constructable by clients. Worker
      threads can initialize a new Mdrunner from the master instance with
      cloneOnSpawnedThread.
    * Clarify parameter setting for spawned Mdrunner threads.
    * Begin to encapsulate filename options available to client code from
      the actual client code.
    * Remove extraneous variables `nfile` and `fnm`.
    
    Later changes will:
    
    * Separate Mdrunner into Launcher and Worker at mdrunner() call so that
      tMPI runner does not look reentrant.
    * Hide data and clarify ownership / modernize memory management /
      clarify the subjects of the Builder `add` operations.
    * Clarify separation of user interface from API parameters.
    * Flesh out SimulationContext and related classes.
    
    Refs #2605
    
    Change-Id: I1db1d34b07ec0f8ba5f246ab763c74ad9eafe8f3

--- src/programs/mdrun/mdrun.cpp
+++ src/programs/mdrun/mdrun.cpp
@@ -58,8 +58,8 @@
 #include <cstdlib>
 #include <cstring>
 
-#include "gromacs/commandline/filenm.h"
 #include "gromacs/commandline/pargs.h"
+#include "gromacs/compat/make_unique.h"
 #include "gromacs/domdec/domdec.h"
 #include "gromacs/gmxlib/network.h"
 #include "gromacs/mdlib/mdrun.h"
@@ -67,6 +67,7 @@
 #include "gromacs/mdrun/multisim.h"
 #include "gromacs/mdrun/replicaexchange.h"
 #include "gromacs/mdrun/runner.h"
+#include "gromacs/mdrun/simulationcontext.h"
 #include "gromacs/mdrunutility/handlerestart.h"
 #include "gromacs/mdtypes/commrec.h"
 #include "gromacs/utility/arraysize.h"
@@ -92,15 +93,7 @@ static bool is_multisim_option_set(int argc, const char *const argv[])
 //! Implements C-style main function for mdrun
 int gmx_mdrun(int argc, char *argv[])
 {
-    gmx::Mdrunner runner;
-    return runner.mainFunction(argc, argv);
-}
-
-namespace gmx
-{
-
-int Mdrunner::mainFunction(int argc, char *argv[])
-{
+    using namespace gmx;
     const char   *desc[] = {
         "[THISMODULE] is the main computational chemistry engine",
         "within GROMACS. Obviously, it performs Molecular Dynamics simulations,",
@@ -245,6 +238,31 @@ int Mdrunner::mainFunction(int argc, char *argv[])
         "When [TT]mdrun[tt] is started with MPI, it does not run niced by default."
     };
 
+    // Ongoing collection of mdrun options
+    MdrunOptions                     mdrunOptions;
+    // Options for the domain decomposition.
+    DomdecOptions                    domdecOptions;
+    // Parallelism-related user options.
+    gmx_hw_opt_t                     hw_opt;
+    // Command-line override for the duration of a neighbor list with the Verlet scheme.
+    int                              nstlist_cmdline = 0;
+    // Parameters for replica-exchange simulations.
+    ReplicaExchangeParameters        replExParams;
+
+    // Filenames and properties from command-line argument values.
+    auto filenames = gmx::MdFilenames();
+
+    // Print a warning if any force is larger than this (in kJ/mol nm).
+    real                             pforce = -1;
+
+    // Output context for writing text files
+    // \todo Clarify initialization, ownership, and lifetime.
+    gmx_output_env_t                *oenv = nullptr;
+
+    // Handle to file used for logging.
+    // \todo Move to RAII filehandle management; open and close in one place.
+    FILE                            *fplog;
+
     /* Command line options */
     rvec              realddxyz                                               = {0, 0, 0};
     const char       *ddrank_opt_choices[static_cast<int>(DdRankOrder::nr)+1] =
@@ -371,9 +389,13 @@ int Mdrunner::mainFunction(int argc, char *argv[])
     };
     int               rc;
 
-    cr = init_commrec();
+    // pointer-to-t_commrec is the de facto handle type for communications record.
+    // Complete shared / borrowed ownership requires a reference to this stack variable
+    // (or pointer-to-pointer-to-t_commrec) since borrowing code may update the pointer.
+    // \todo Define the ownership and lifetime management semantics for a communication record, handle or value type.
+    t_commrec       * cr = init_commrec();
 
-    unsigned long PCA_Flags = PCA_CAN_SET_DEFFNM;
+    unsigned long     PCA_Flags = PCA_CAN_SET_DEFFNM;
     // With -multidir, the working directory still needs to be
     // changed, so we can't check for the existence of files during
     // parsing.  It isn't useful to do any completion based on file
@@ -383,7 +405,8 @@ int Mdrunner::mainFunction(int argc, char *argv[])
         PCA_Flags |= PCA_DISABLE_INPUT_FILE_CHECKING;
     }
 
-    if (!parse_common_args(&argc, argv, PCA_Flags, nfile, fnm, asize(pa), pa,
+    if (!parse_common_args(&argc, argv, PCA_Flags,
+                           static_cast<int>(filenames().size()), filenames().data(), asize(pa), pa,
                            asize(desc), desc, 0, nullptr, &oenv))
     {
         sfree(cr);
@@ -432,7 +455,9 @@ int Mdrunner::mainFunction(int argc, char *argv[])
     hw_opt.thread_affinity = nenum(thread_aff_opt_choices);
 
     // now check for a multi-simulation
-    gmx::ArrayRef<const std::string> multidir = opt2fnsIfOptionSet("-multidir", nfile, fnm);
+    gmx::ArrayRef<const std::string> multidir = opt2fnsIfOptionSet("-multidir",
+                                                                   static_cast<int>(filenames().size()),
+                                                                   filenames().data());
 
     if (replExParams.exchangeInterval != 0 && multidir.size() < 2)
     {
@@ -444,7 +469,7 @@ int Mdrunner::mainFunction(int argc, char *argv[])
         gmx_fatal(FARGS, "Replica exchange number of exchanges needs to be positive");
     }
 
-    ms = init_multisystem(MPI_COMM_WORLD, multidir);
+    gmx_multisim_t* ms = init_multisystem(MPI_COMM_WORLD, multidir);
 
     /* Prepare the intra-simulation communication */
     // TODO consolidate this with init_commrec, after changing the
@@ -460,7 +485,8 @@ int Mdrunner::mainFunction(int argc, char *argv[])
     }
 #endif
 
-    if (!opt2bSet("-cpi", nfile, fnm))
+    if (!opt2bSet("-cpi",
+                  static_cast<int>(filenames().size()), filenames().data()))
     {
         // If we are not starting from a checkpoint we never allow files to be appended
         // to, since that has caused a ton of strange behaviour and bugs in the past.
@@ -480,9 +506,15 @@ int Mdrunner::mainFunction(int argc, char *argv[])
 
     continuationOptions.appendFilesOptionSet = opt2parg_bSet("-append", asize(pa), pa);
 
-    handleRestart(cr, ms, bTryToAppendFiles, nfile, fnm, &continuationOptions.appendFiles, &continuationOptions.startedFromCheckpoint);
+    handleRestart(cr, ms, bTryToAppendFiles,
+                  static_cast<int>(filenames().size()),
+                  filenames().data(),
+                  &continuationOptions.appendFiles,
+                  &continuationOptions.startedFromCheckpoint);
 
-    mdrunOptions.rerun            = opt2bSet("-rerun", nfile, fnm);
+    mdrunOptions.rerun            = opt2bSet("-rerun",
+                                             static_cast<int>(filenames().size()),
+                                             filenames().data());
     mdrunOptions.ntompOptionIsSet = opt2parg_bSet("-ntomp", asize(pa), pa);
 
     /* We postpone opening the log file if we are appending, so we can
@@ -490,8 +522,12 @@ int Mdrunner::mainFunction(int argc, char *argv[])
        there instead.  */
     if (MASTER(cr) && !continuationOptions.appendFiles)
     {
-        gmx_log_open(ftp2fn(efLOG, nfile, fnm), cr,
-                     continuationOptions.appendFiles, &fplog);
+        gmx_log_open(ftp2fn(efLOG,
+                            static_cast<int>(filenames().size()),
+                            filenames().data()),
+                     cr,
+                     continuationOptions.appendFiles,
+                     &fplog);
     }
     else
     {
@@ -504,12 +540,56 @@ int Mdrunner::mainFunction(int argc, char *argv[])
     domdecOptions.numCells[YY] = roundToInt(realddxyz[YY]);
     domdecOptions.numCells[ZZ] = roundToInt(realddxyz[ZZ]);
 
-    nbpu_opt    = nbpu_opt_choices[0];
-    pme_opt     = pme_opt_choices[0];
-    pme_fft_opt = pme_fft_opt_choices[0];
+    /* The Context is a shared resource owned by the client code.
+     * A more complete design should address handles to resources with appropriate
+     * lifetimes and invariants for the resources allocated to the client,
+     * to the current simulation and to scheduled tasks within the simulation.
+     *
+     * \todo Clarify Context lifetime-management requirements and reconcile with scoped ownership.
+     *
+     * \todo Take ownership of and responsibility for communications record (cr).
+     */
+    auto context = gmx::createSimulationContext(cr);
 
+    /* The named components for the builder exposed here are descriptive of the
+     * state of mdrun at implementation and are not intended to be prescriptive
+     * of future design. (Note the ICommandLineOptions... framework used elsewhere.)
+     * The modules should ultimately take part in composing the Director code
+     * for an extensible Builder.
+     *
+     * In the near term, we assume that resources like domain decomposition and
+     * neighbor lists must be reinitialized between simulation segments.
+     * We would prefer to rebuild resources only as necessary, but we defer such
+     * details to future optimizations.
+     */
+    auto builder = gmx::MdrunnerBuilder(&context);
+    builder.addSimulationMethod(mdrunOptions, pforce);
+    builder.addDomainDecomposition(domdecOptions);
+    // \todo pass by value
+    builder.addNonBonded(nbpu_opt_choices[0]);
+    // \todo pass by value
+    builder.addElectrostatics(pme_opt_choices[0], pme_fft_opt_choices[0]);
+    builder.addNeighborList(nstlist_cmdline);
+    builder.addReplicaExchange(replExParams);
+    // \todo take ownership of multisim resources (ms)
+    builder.addMultiSim(ms);
+    // \todo Provide parallelism resources through SimulationContext.
+    // Need to establish run-time values from various inputs to provide a resource handle to Mdrunner
+    builder.addHardwareOptions(hw_opt);
+    // \todo File names are parameters that should be managed modularly through further factoring.
+    builder.addFilenames(filenames);
+    // Note: The gmx_output_env_t life time is not managed after the call to parse_common_args.
+    // \todo Implement lifetime management for gmx_output_env_t.
+    // \todo Output environment should be configured outside of Mdrunner and provided as a resource.
+    builder.addOutputEnvironment(oenv);
+    // \todo Clean up log file management
+    // Log file opening and closing could be managed entirely within Mdrunner::mdrunner(), or a file-like
+    // object could be owned by the client (such a via SimulationContext) and the handle lent to the
+    // Mdrunner, but currently fplog can be opened or closed in multiple places.
+    builder.addLogFile(&fplog);
+    auto runner = builder.build();
 
-    rc = mdrunner();
+    rc = runner.mdrunner();
 
     /* Log file has to be closed in mdrunner if we are appending to it
        (fplog not set here) */
@@ -525,5 +605,3 @@ int Mdrunner::mainFunction(int argc, char *argv[])
     done_multisim(ms);
     return rc;
 }
-
-}  // namespace gmx
