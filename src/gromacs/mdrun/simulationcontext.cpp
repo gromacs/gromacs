566b3cf34a71d12a280a5c7a544afcf80ae07685
M. Eric Irrgang <ericirrgang@gmail.com>
2018-09-11 13:01:50 +0300

    Move mdrun mainFunction to client code.
    supports gmxapi milestone 4, described at #2605
    
    Insert a Builder on which to develop the distinction between user
    interface and implementation code. User interface is handled in
    Director code, while the Builder and code further down the mdrun call
    stack should be user interface agnostic (i.e. not CLI-centric).
    
    Introduces a Context object to hold some resources. (see milestone 5 and
    issue #2587)
    
    Prepare for more stateful Mdrunner objects.
    
    API clients need to be able to initialize and run MD simulations with
    calls from separate code blocks or even translation units. Also, we
    need clear distinctions between a pre-launch master Mdrunner and the
    non-master Mdrunner threads.
    
    * Make Mdrunner non-copyable and not constructable by clients. Worker
      threads can initialize a new Mdrunner from the master instance with
      cloneOnSpawnedThread)
    * Clarify parameter setting for spawned Mdrunner threads.
    * Begin to encapsulate filename options available to client code from
      the actual client code.
    * Remove extraneous variables `nfile` and `fnm`.
    
    Subsequent changes will:
    
    * separate Mdrunner into Launcher and Worker at mdrunner() call so that
      tMPI runner does not look reentrant.
    * hide data and clarify ownership / modernize memory management /
      clarify the subjects of the Builder `add` operations.
    * clarify separation of user interface from API parameters.
    
    Refs #2605
    
    Change-Id: I1db1d34b07ec0f8ba5f246ab763c74ad9eafe8f3

---
+++ src/gromacs/mdrun/simulationcontext.cpp
@@ -0,0 +1,87 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+
+#include "gmxpre.h"
+
+#include "simulationcontext.h"
+
+#include "gromacs/commandline/filenm.h"
+#include "gromacs/compat/make_unique.h"
+#include "gromacs/domdec/domdec.h"
+#include "gromacs/fileio/oenv.h"
+#include "gromacs/hardware/hw_info.h"
+
+#include "runner.h"
+
+namespace gmx
+{
+//! \cond libapi
+SimulationContext::SimulationContext(t_commrec        ** communicator,
+                                     const gmx_hw_opt_t &hardwareOptions,
+                                     const MdFilenames  &filenames,
+                                     gmx_output_env_t ** outputEnvironment,
+                                     FILE             ** logFile) :
+    communicator_ {communicator},
+hardwareOptions_ {
+    hardwareOptions
+},
+filenames_ {
+    filenames
+},
+outputEnvironment_ {
+    outputEnvironment
+},
+logFile_ {
+    logFile
+}
+{
+}
+
+std::unique_ptr<SimulationContext>
+createSimulationContext(t_commrec        ** simulationCommunicator,
+                        const gmx_hw_opt_t &hardwareOptions,
+                        const MdFilenames  &filenames,
+                        gmx_output_env_t ** outputEnvironment,
+                        FILE             ** logFileHandle)
+{
+    auto context = gmx::compat::make_unique<SimulationContext>(simulationCommunicator,
+                                                               hardwareOptions,
+                                                               filenames,
+                                                               outputEnvironment,
+                                                               logFileHandle);
+    return context;
+}
+//! \endcond
+} // end namespace gmx
