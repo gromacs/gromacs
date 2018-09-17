fb76e5aadb7fef5986e0c05507160353a274b887
Mark Abraham <mark.j.abraham@gmail.com>
2018-09-17 16:17:50 +0200

    Implement PaddedVector
    Implements a vector-like container that maintains padding such that
    SIMD memory operations will always use initialized memory. All use of
    the data takes place via ArrayRef views that can be requested to
    include padding or not, so that this is explicit at the point of
    creation of the view. The PaddedArrayRef view creates some safety
    through strong typing, also.
    
    Refs #2642
    
    Change-Id: Ibcafe161689f4a02f7aa6fd0410edec47300264e

--- src/gromacs/domdec/distribute.cpp
+++ src/gromacs/domdec/distribute.cpp
@@ -201,7 +201,7 @@ static void dd_distribute_dfhist(gmx_domdec_t *dd, df_history_t *dfhist)
 
 static void dd_distribute_state(gmx_domdec_t *dd,
                                 const t_state *state, t_state *state_local,
-                                PaddedRVecVector *f)
+                                PaddedVector<gmx::RVec> *f)
 {
     int nh = state_local->nhchainlength;
 
@@ -540,14 +540,14 @@ static void distributeAtomGroups(const gmx::MDLogger &mdlog,
     }
 }
 
-void distributeState(const gmx::MDLogger &mdlog,
-                     gmx_domdec_t        *dd,
-                     t_state             *state_global,
-                     const gmx_ddbox_t   &ddbox,
-                     t_state             *state_local,
-                     PaddedRVecVector    *f)
+void distributeState(const gmx::MDLogger     &mdlog,
+                     gmx_domdec_t            *dd,
+                     t_state                 *state_global,
+                     const gmx_ddbox_t       &ddbox,
+                     t_state                 *state_local,
+                     PaddedVector<gmx::RVec> *f)
 {
-    rvec *xGlobal = (DDMASTER(dd) ? as_rvec_array(state_global->x.data()) : nullptr);
+    rvec *xGlobal = (DDMASTER(dd) ? state_global->x.rvec_array() : nullptr);
 
     distributeAtomGroups(mdlog, dd,
                          DDMASTER(dd) ? state_global->box : nullptr,
