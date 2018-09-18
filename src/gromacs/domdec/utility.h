415ce59aa76aa549023c68bbb0f7f222dce0de1b
Berk Hess <hess@kth.se>
2018-09-18 14:11:42 +0200

    Update DD cutoffs for update groups
    Due to the distance between atoms to the center of geometry of the
    update group they belong to, there is now a difference between
    atom-atom, domain-domain and atom-domain cutoffs.
    Also adds the cutoff versus box check for update groups.
    
    Change-Id: I474cd1371a1fac22f26b80adcc235bfb10764d34

--- src/gromacs/domdec/utility.h
+++ src/gromacs/domdec/utility.h
@@ -100,4 +100,34 @@ void dd_check_alloc_ncg(t_forcerec       *fr,
                         PaddedRVecVector *f,
                         int               numChargeGroups);
 
+/*! \brief Returns a domain-to-domain cutoff distance given an atom-to-atom cutoff */
+static inline real
+atomToAtomIntoDomainToDomainCutoff(const gmx_domdec_comm_t &comm,
+                                   real                     cutoff)
+{
+    if (comm.useUpdateGroups)
+    {
+        GMX_ASSERT(comm.updateGroupsCog, "updateGroupsCog should be initialized here");
+
+        cutoff += 2*comm.updateGroupsCog->maxUpdateGroupRadius();
+    }
+
+    return cutoff;
+}
+
+/*! \brief Returns an atom-to-domain cutoff distance given a domain-to-domain cutoff */
+static inline real
+domainToDomainIntoAtomToDomainCutoff(const gmx_domdec_comm_t &comm,
+                                     real                     cutoff)
+{
+    if (comm.useUpdateGroups)
+    {
+        GMX_ASSERT(comm.updateGroupsCog, "updateGroupsCog should be initialized here");
+
+        cutoff -= comm.updateGroupsCog->maxUpdateGroupRadius();
+    }
+
+    return cutoff;
+}
+
 #endif
