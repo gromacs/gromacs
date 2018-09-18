f9a52affbf2a36dbe886dc44cb34d7c54d87b5c1
Roland Schulz <roland.schulz@intel.com>
2018-09-17 21:56:35 -0700

    Fix bitmask printing
    Change-Id: I0e9294ad734dfd682cfea3a31011a9b9c7fb9034

--- src/gromacs/listed-forces/manage-threading.cpp
+++ src/gromacs/listed-forces/manage-threading.cpp
@@ -450,14 +450,8 @@ void setup_bonded_threading(bonded_threading_t *bt,
 
             if (gmx_debug_at)
             {
-#if BITMASK_SIZE <= 64 //move into bitmask when it is C++
-                std::string flags = gmx::formatString("%" PRIx64, *mask);
-#else
-                std::string flags = gmx::formatAndJoin(*mask,
-                                                       "", gmx::StringFormatter("%x"));
-#endif
                 fprintf(debug, "block %d flags %s count %d\n",
-                        b, flags.c_str(), c);
+                        b, to_hex_string(*mask).c_str(), c);
             }
         }
     }
