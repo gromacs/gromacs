6f6994aed794ec6bf40528a7c1a4031a2e6255d2
Pascal Merz <pascal.merz@colorado.edu>
2018-09-25 23:11:35 -0600

    [RFC] Use AccumulateGlobals for signal handlers v2
    This explores how the new AccumulateGlobals infrastucture can be used to
    reduce the checkpoint, reset and stop signals passed between ranks and
    simulations.
    
    v2: Reduction implicitly assumed by set and handle point of signal
    
    Signals are in general propagated along with other data, when other data
    is comunicated anyway. They need, however, to know _when_ this happens,
    so that e.g. the signal setting rank (typically master) can execute the
    signalled action at the same time as the other ranks, i.e. after the
    signal got reduced. This is achieved in this change by introducing a
    handleSignal() function for all signal handlers which mirrors the
    setSignal() function. The setSignal() function is called directly before
    global reduction takes place, the handleSignal() directly after. This
    als ensures that the signals get reduced only once (for md-vv and replica
    exchange, multiple calls to compute_globals() are preformed per step).
    Until now, this was achieved by passing in a NullSignaller instead of the
    actual signals in most of these calls, now the setting and handling of
    the calls have been grouped around the "main" compute_globals() call.
    
    Change-Id: If2a35e2c5d9d42d03975cbef1f03b9679899592b

--- src/gromacs/mdlib/checkpointhandler.h
+++ src/gromacs/mdlib/checkpointhandler.h
@@ -55,11 +55,12 @@
 #ifndef GMX_MDLIB_CHECKPOINTHANDLER_H
 #define GMX_MDLIB_CHECKPOINTHANDLER_H
 
+#include <cmath>
 #include <memory>
 
 #include "gromacs/compat/pointers.h"
 #include "gromacs/mdlib/mdrun.h"
-#include "gromacs/mdlib/simulationsignal.h"
+#include "gromacs/mdrunutility/accumulateglobals.h"
 
 struct gmx_walltime_accounting;
 
@@ -82,7 +83,7 @@ enum class CheckpointSignal
  * Master rank sets the checkpointing signal periodically
  * All ranks receive checkpointing signal and set the respective flag
  */
-class CheckpointHandler final
+class CheckpointHandler final : public IAccumulateGlobalsClient
 {
     public:
         /*! \brief CheckpointHandler constructor
@@ -92,12 +93,12 @@ class CheckpointHandler final
          * set or handled.
          */
         CheckpointHandler(
-            compat::not_null<SimulationSignal*> signal,
-            bool                                simulationsShareState,
-            bool                                neverUpdateNeighborList,
-            bool                                isMaster,
-            bool                                writeFinalCheckpoint,
-            real                                checkpointingPeriod);
+            compat::not_null<AccumulateGlobalsBuilder*> accumulateGlobalsBuilder,
+            bool                                        simulationsShareState,
+            bool                                        neverUpdateNeighborList,
+            bool                                        isMaster,
+            bool                                        writeFinalCheckpoint,
+            real                                        checkpointingPeriod);
 
         /*! \brief Decides whether a checkpointing signal needs to be set
          *
@@ -109,6 +110,18 @@ class CheckpointHandler final
             if (rankCanSetSignal_)
             {
                 setSignalImpl(walltime_accounting);
+            }
+        }
+
+        /*! \brief Checks whether a signal has been received
+         *
+         */
+        void handleSignal()
+        {
+            if (static_cast<CheckpointSignal>(lround(signalView_[0])) == CheckpointSignal::doCheckpoint)
+            {
+                signalView_[0]      = static_cast<double>(CheckpointSignal::noSignal);
+                hasUnhandledSignal_ = true;
             }
         }
 
@@ -134,12 +147,23 @@ class CheckpointHandler final
             return checkpointThisStep_;
         }
 
+        /* From IAccumulateGlobalsClient */
+        //! Return the number of values needed to pass signal.
+        int getNumGlobalsRequired() const override;
+        //! Store where to write and read signal
+        void setViewForGlobals(AccumulateGlobals *accumulateGlobals,
+                               ArrayRef<double>   view) override;
+        //! Called (in debug mode) after MPI reduction is complete.
+        void notifyAfterCommunication() override;
+
     private:
         void setSignalImpl(gmx_walltime_accounting *walltime_accounting) const;
 
         void decideIfCheckpointingThisStepImpl(bool bNS, bool bFirstStep, bool bLastStep);
 
-        SimulationSignal &signal_;
+        ArrayRef<double>  signalView_;
+
+        bool              hasUnhandledSignal_;
         bool              checkpointThisStep_;
         int               numberOfNextCheckpoint_;
 
