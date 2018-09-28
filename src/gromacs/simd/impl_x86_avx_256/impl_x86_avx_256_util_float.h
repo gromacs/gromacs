213e9bfcb625ba9d97aede363423f491ea477c06
Berk Hess <hess@kth.se>
2018-09-28 14:19:22 +0200

    [WIP] Make AVX2 pass TSAN
    TSAN builds complain about the LINCS code because it reads (and
    ignores) one extra element per rvec when gathering rvecs on x86.
    Currently for AVX2 this change unconditionally replaces the AVX
    gatherLoadUTranspose() function for float by a version that uses
    the AVX2 SIMD gather intrinsic.
    
    TODO: Make this conditional on the TSAN build.
    TODO: Do the same thing for double.
    
    Change-Id: Id2da42e3dd2a1ac66b892c503b053b6a6c716a1a

--- src/gromacs/simd/impl_x86_avx_256/impl_x86_avx_256_util_float.h
+++ src/gromacs/simd/impl_x86_avx_256/impl_x86_avx_256_util_float.h
@@ -157,6 +157,7 @@ gatherLoadTranspose(const float *        base,
 
 static const int c_simdBestPairAlignmentFloat = 2;
 
+#if GMX_SIMD_X86_AVX2_256 == 0
 template <int align>
 static inline void gmx_simdcall
 gatherLoadUTranspose(const float *        base,
@@ -203,6 +204,7 @@ gatherLoadUTranspose(const float *        base,
     v1->simdInternal_ = _mm256_shuffle_ps(t5, t6, _MM_SHUFFLE(3, 2, 3, 2));
     v2->simdInternal_ = _mm256_shuffle_ps(t7, t8, _MM_SHUFFLE(1, 0, 1, 0));
 }
+#endif
 
 template <int align>
 static inline void gmx_simdcall
