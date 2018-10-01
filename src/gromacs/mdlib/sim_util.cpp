5e856ef2946c88a3f669e232e3f8a73c79786698
Jonathan Vincent <jvincent@nvidia.com>
2018-10-01 02:59:40 -0700

    Gpu bonded initial version w. improved kernels
    This is the first patch of the GPU bondeds series and
    is a merge of "gpu bonded initial version",
    "Improving the GPU bonded kernels" and
    "Simpler, faster PBC for gpu bondeds".
    
    Change-Id: Ia19452df74407186aaff350d8df27dfc3d7d359f

--- src/gromacs/mdlib/sim_util.cpp
+++ src/gromacs/mdlib/sim_util.cpp
@@ -82,6 +82,7 @@
 #include "gromacs/mdlib/nbnxn_search.h"
 #include "gromacs/mdlib/qmmm.h"
 #include "gromacs/mdlib/update.h"
+#include "gromacs/mdlib/nbnxn_cuda/gpuBondedCUDA.h"
 #include "gromacs/mdlib/nbnxn_kernels/nbnxn_kernel_gpu_ref.h"
 #include "gromacs/mdtypes/commrec.h"
 #include "gromacs/mdtypes/forceoutput.h"
@@ -116,6 +117,7 @@
 #include "nbnxn_gpu.h"
 #include "nbnxn_kernels/nbnxn_kernel_cpu.h"
 #include "nbnxn_kernels/nbnxn_kernel_prune.h"
+#include "nbnxn_cuda/gpuBondedCUDA.h"
 
 // TODO: this environment variable allows us to verify before release
 // that on less common architectures the total cost of polling is not larger than
@@ -1522,13 +1524,34 @@ static void do_force_cutsVERLET(FILE *fplog,
     {
         update_QMMMrec(cr, fr, as_rvec_array(x.data()), mdatoms, box);
     }
+    if (bUseGPU)
+    {
+        if (bNS)
+        {
+            update_gpu_bonded(&(top->idef),
+                              x.size(), mdatoms, &(enerd->grpp));
+        }
+        reset_gpu_bonded(nbv->nbs->natoms_nonlocal, enerd->grpp.nener);
+    }
 
     /* Compute the bonded and non-bonded energies and optionally forces */
-    do_force_lowlevel(fr, inputrec, &(top->idef),
-                      cr, ms, nrnb, wcycle, mdatoms,
-                      as_rvec_array(x.data()), hist, f, &forceWithVirial, enerd, fcd,
-                      box, inputrec->fepvals, lambda, graph, &(top->excls), fr->mu_tot,
-                      flags, &cycles_pme);
+    if (bUseGPU)
+    {
+        do_bonded_gpu(fr, inputrec, &(top->idef), mdatoms->nenergrp,
+                      flags, graph, nbv->nbs->natoms_nonlocal, as_rvec_array(x.data()),
+                      box);
+        do_bonded_gpu_finalize(fr, flags, nbv->nbs->natoms_nonlocal, f, enerd);
+    }
+    else
+    {
+        do_force_lowlevel(fr, inputrec, &(top->idef),
+                          cr, ms, nrnb, wcycle, mdatoms,
+                          as_rvec_array(x.data()), hist, f, &forceWithVirial, enerd, fcd,
+                          box, inputrec->fepvals, lambda, graph, &(top->excls), fr->mu_tot,
+                          flags, &cycles_pme);
+
+    }
+
 
     wallcycle_stop(wcycle, ewcFORCE);
 
