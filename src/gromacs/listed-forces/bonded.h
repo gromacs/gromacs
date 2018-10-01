250ab1a64e7b4a49aca11f6b3603436b7ca57846
Jonathan Vincent <jvincent@nvidia.com>
2018-10-01 02:59:40 -0700

    Add CUDA bonded kernels
    TODO describe the functionality
    
    Introducecd GPU-suitable PBC routines
    
    Cleaned up some use of includes in listed-forces module headers, and
    refactored gmx_ffparams_t to be declared in its own header, so that
    the CUDA source does not have to cope with the way it uses a member
    initializer with a user-defined literal suffix.
    
    TODO amend docs/release-notes/performance.rst
    TODO amend docs/user-guide/mdrun-performance.rst
    
    Fixes #2678
    Refs #2675
    
    Change-Id: Ia19452df74407186aaff350d8df27dfc3d7d359f

--- src/gromacs/listed-forces/bonded.h
+++ src/gromacs/listed-forces/bonded.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -47,20 +47,15 @@
 #ifndef GMX_LISTED_FORCES_BONDED_H
 #define GMX_LISTED_FORCES_BONDED_H
 
-#include <cstdio>
-
-#include "gromacs/gmxlib/nrnb.h"
-#include "gromacs/math/vec.h"
-#include "gromacs/mdtypes/fcdata.h"
-#include "gromacs/mdtypes/forcerec.h"
-#include "gromacs/mdtypes/interaction_const.h"
-#include "gromacs/mdtypes/mdatom.h"
-#include "gromacs/topology/idef.h"
+#include "gromacs/math/vectypes.h"
 #include "gromacs/topology/ifunc.h"
 #include "gromacs/utility/basedefinitions.h"
 
-
+struct gmx_cmap_t;
+struct t_fcdata;
 struct t_graph;
+struct t_mdatom;
+struct t_nrnb;
 struct t_pbc;
 
 /*! \brief Calculate bond-angle. No PBC is taken into account (use mol-shift) */
