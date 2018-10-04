5f98013bf20beb26bc42d8e5459d3f8f57cdf693
Mark Abraham <mark.j.abraham@gmail.com>
2018-10-04 20:26:16 +0200

    Update test matrices with new GPU specifiers
    Refs #2161
    
    Change-Id: Ibb865a13e67dc7782b0ac0f03ef9106d0e973238

--- admin/builds/gromacs.py
+++ admin/builds/gromacs.py
@@ -47,8 +47,6 @@ extra_options = {
     'fftpack': Option.simple,
     'double': Option.simple,
     'thread-mpi': Option.bool,
-    'gpu': Option.bool,
-    'opencl': Option.bool,
     'openmp': Option.bool,
     'nranks': Option.string,
     'npme': Option.string,
@@ -87,7 +85,7 @@ def do_build(context):
         cmake_opts['GMX_SIMD'] = 'None'
     else:
         cmake_opts['GMX_SIMD'] = context.opts.simd
-    if context.opts.gpu or context.opts.opencl:
+    if context.opts.cuda or context.opts.opencl:
         cmake_opts['GMX_GPU'] = 'ON'
         if context.opts.opencl:
             context.env.set_env_var('CUDA_PATH', context.env.cuda_root)
@@ -183,6 +181,9 @@ def do_build(context):
                 # OpenMP should always work when compiled in! Currently not set if
                 # not explicitly set
                 cmd += ' -ntomp 2'
+
+            if context.opts.gpuhw == Gpuhw.NONE:
+                context.env.set_env_var('GMX_DISABLE_GPU_DETECTION', '1')
 
             if context.opts.gpu_id:
                 cmd += ' -gpu_id ' + context.opts.gpu_id
