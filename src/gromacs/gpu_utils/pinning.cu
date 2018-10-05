4c93f81eef3e482e64eecef24efe6a2bcf29af3f
Mark Abraham <mark.j.abraham@gmail.com>
2018-10-05 18:09:59 +0200

    More updates to test matrix
    Tested icc 19 and cuda 10 with no GPU
    
    Also fixed recent issue introduced for clang+nvcc
    
    Refs #2629
    
    Change-Id: I194568c7fd32743f64fde9690f64250d715f7ba9

--- src/gromacs/gpu_utils/pinning.cu
+++ src/gromacs/gpu_utils/pinning.cu
@@ -54,12 +54,16 @@
 
 namespace gmx
 {
-#pragma diag_suppress 177 // No unused attribute which works with both MSVC and NVCC
+
+#if !defined(NDEBUG) || defined(DOXYGEN)
+
 //! Is \c ptr aligned on a boundary that is a multiple of \c bytes.
 gmx_unused static inline bool isAligned(const void *ptr, size_t bytes)
 {
     return (reinterpret_cast<intptr_t>(ptr) % bytes) == 0;
 }
+
+#endif
 
 void pinBuffer(void *pointer, std::size_t numBytes) noexcept
 {
