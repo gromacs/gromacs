ba1453be4ea66c8b0949aadf05f35ec8bfc2734f
Magnus Lundborg <lundborg.magnus@gmail.com>
2018-10-08 17:01:23 +0200

    Improving the GPU bonded kernels
    Focusing on cleaning up the kernels and trying to make them easier to
    read.
    
    Change-Id: Ic9ac1ee9acc06b07c4634f1cb2c9bf524d5cce85

--- src/gromacs/mdlib/nbnxn_cuda/gpuBondedCUDA.h
+++ src/gromacs/mdlib/nbnxn_cuda/gpuBondedCUDA.h
@@ -6,12 +6,7 @@
 #include "gromacs/mdtypes/inputrec.h"
 #include "gromacs/mdtypes/enerdata.h"
 
-//#include "gmxpre.h"
 #include "gromacs/mdlib/nbnxn_atomdata.h"
-//#include "gromacs/mdlib/nbnxn_consts.h"
-//#include "gromacs/mdlib/nbnxn_internal.h"
-//#include "gromacs/mdlib/nbnxn_search.h"
-//#include "gromacs/mdlib/nbnxn_util.h"
 
 #include "gromacs/topology/idef.h"
 #include "gromacs/topology/ifunc.h"
@@ -21,13 +16,14 @@
 #define TPB_BONDED 256
 
 //Macro for checking cuda errors following a cuda launch or api call
-#define cudaCheckError() {                                          \
-    cudaError_t e=cudaGetLastError();                                 \
-    if(e!=cudaSuccess) {                                              \
-      printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));           \
-      exit(0); \
-    }                                                                 \
-  }
+#define cudaCheckError() {                                                            \
+    cudaError_t e=cudaGetLastError();                                                 \
+    if(e!=cudaSuccess)                                                                \
+    {                                                                                 \
+        printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e)); \
+        exit(0);                                                                      \
+    }                                                                                 \
+}
   
 #if defined(__CUDACC__)
 #include "nbnxn_cuda.h"
@@ -37,18 +33,13 @@
 #endif
 
 // new stuff for bonded
-void update_gpu_bonded( const t_idef *idef,  const t_forcerec *fr, matrix box,
-                        int size,  const t_mdatoms *md, const real *lambda,
-                        gmx_grppairener_t *grppener );                       
+void update_gpu_bonded(const t_idef *idef,  const t_forcerec *fr, matrix box,
+                        int size,  const t_mdatoms *md, gmx_grppairener_t *grppener); 
 void do_bonded_gpu(t_forcerec *fr, const t_inputrec *ir, const t_idef *idef, 
-                   int flags, const t_graph *graph , int natoms, rvec x[], 
-                   real *lambda, const t_mdatoms *md, 
-                   rvec *input_force, t_lambda *fepvals, gmx_enerdata_t *enerd);
+                   int flags, const t_graph *graph , int natoms, rvec x[]);
 
-void do_bonded_gpu_finalize(t_forcerec *fr, const t_inputrec *ir, const t_idef *idef,
-                   int flags, const t_graph *graph , int natoms, rvec x[],
-                   real *lambda, const t_mdatoms *md,
-                   rvec *input_force, t_lambda *fepvals, gmx_enerdata_t *enerd);
+void do_bonded_gpu_finalize(t_forcerec *fr, int flags, int natoms,
+                            rvec *input_force, gmx_enerdata_t *enerd);
 
 void reset_gpu_bonded(const int size, const int nener);
 
