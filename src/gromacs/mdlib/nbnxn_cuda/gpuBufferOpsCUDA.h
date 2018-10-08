55d870cb1130e12c7f3ddd5ef200e1658a8b47dd
Magnus Lundborg <lundborg.magnus@gmail.com>
2018-10-08 17:01:23 +0200

    Improving the GPU bonded kernels
    Focusing on cleaning up the kernels and trying to make them easier to
    read.
    
    Change-Id: Ic9ac1ee9acc06b07c4634f1cb2c9bf524d5cce85

--- src/gromacs/mdlib/nbnxn_cuda/gpuBufferOpsCUDA.h
+++ src/gromacs/mdlib/nbnxn_cuda/gpuBufferOpsCUDA.h
@@ -9,12 +9,7 @@
 #include "gromacs/ewald/pme.h"
 #include "gromacs/ewald/pme-grid.h"
 
-//#include "gmxpre.h"
 #include "gromacs/mdlib/nbnxn_atomdata.h"
-//#include "gromacs/mdlib/nbnxn_consts.h"
-//#include "gromacs/mdlib/nbnxn_internal.h"
-//#include "gromacs/mdlib/nbnxn_search.h"
-//#include "gromacs/mdlib/nbnxn_util.h"
 
 #define TPB 128 //CUDA threads per block
 
@@ -22,26 +17,32 @@
 #define BO_MAX_RANKS 32
 
 //Macro for checking cuda errors following a cuda launch or api call
-#define cudaCheckError() {                                          \
-    cudaError_t e=cudaGetLastError();                                 \
-    if(e!=cudaSuccess) {                                              \
-      printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));           \
-      exit(0); \
-    }                                                                 \
-  }
+#define cudaCheckError() {                                                            \
+    cudaError_t e=cudaGetLastError();                                                 \
+    if(e!=cudaSuccess)                                                                \
+    {                                                                                 \
+        printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e)); \
+        exit(0);                                                                      \
+    }                                                                                 \
+}
 
 GPU_FUNC_QUALIFIER
-void gpuBufferOpsCopyRvecToNbatReal(int ncxy,int g, int FillLocal,gmx_pme_t* pmedata,nbnxn_atomdata_t    *nbat, gmx_nbnxn_gpu_t *gpu_nbv, const int* a,int a_nalloc,int* na_all,int* cxy_ind,int cell0,int na_sc, int iloc, int stride,  const float x[][3],int xsize,t_commrec *cr) GPU_FUNC_TERM
+void gpuBufferOpsCopyRvecToNbatReal(int ncxy, int g, int FillLocal,
+                                    gmx_pme_t* pmedata, nbnxn_atomdata_t *nbat,
+                                    gmx_nbnxn_gpu_t *gpu_nbv, const int* a,
+                                    int a_nalloc, int* na_all, int* cxy_ind,
+                                    int cell0, int na_sc, int iloc, int stride,
+                                    const float x[][3], int xsize,t_commrec *cr) GPU_FUNC_TERM
 
 GPU_FUNC_QUALIFIER
 void gpuBufferOpsAddNbatFToF(const int* cell,
-				    const nbnxn_atomdata_t *nbat,
-				    gmx_nbnxn_gpu_t *gpu_nbv,
-				    nbnxn_atomdata_output_t *out,
-				    const gmx_pme_t* pmedata,
-				    int nfa,
-				    int a0, int a1,
-				    rvec *f) GPU_FUNC_TERM
+                             const nbnxn_atomdata_t *nbat,
+                             gmx_nbnxn_gpu_t *gpu_nbv,
+                             nbnxn_atomdata_output_t *out,
+                             const gmx_pme_t* pmedata,
+                             int nfa,
+                             int a0, int a1,
+                             rvec *f) GPU_FUNC_TERM
   
 GPU_FUNC_QUALIFIER
 void gpuBufferOpsPMEGatherFromPP(float* dest, int* offset, int* size, int nsender) GPU_FUNC_TERM
@@ -50,27 +51,27 @@ GPU_FUNC_QUALIFIER
 void gpuBufferOpsPMEDistributeToPP(float* src, int* offset, int* copysize, int nsender) GPU_FUNC_TERM
 
 GPU_FUNC_QUALIFIER
-void gpuBufferOpsPPRecvFromPME(float* src, int offset,t_commrec *cr) GPU_FUNC_TERM
+void gpuBufferOpsPPRecvFromPME(float* src, int offset, t_commrec *cr) GPU_FUNC_TERM
 
 
 GPU_FUNC_QUALIFIER
-void gpuBufferOpsInitXSendBufIndexMap(int** mapptr_in, float** shiftptr_in,int size) GPU_FUNC_TERM
+void gpuBufferOpsInitXSendBufIndexMap(int** mapptr_in, float** shiftptr_in, int size) GPU_FUNC_TERM
 
   GPU_FUNC_QUALIFIER
-void gpuBufferOpsInitFRecvBufIndexMap(int** mapptr_in, float** shiftptr_in,int size) GPU_FUNC_TERM
+void gpuBufferOpsInitFRecvBufIndexMap(int** mapptr_in, float** shiftptr_in, int size) GPU_FUNC_TERM
 
 
 GPU_FUNC_QUALIFIER
 gmx_bool gpuBufferOpsTimestepInitFromPP(gmx_bool bNS,
-					gmx_bool bUseGPU,
-					gmx_bool bDutyPPAndPME,
-					t_commrec *cr,
-					int natoms_all) GPU_FUNC_TERM
+                                        gmx_bool bUseGPU,
+                                        gmx_bool bDutyPPAndPME,
+                                        t_commrec *cr,
+                                        int natoms_all) GPU_FUNC_TERM
 
 GPU_FUNC_QUALIFIER
 gmx_bool gpuBufferOpsTimestepInitFromPME(gmx_bool bNS,
-                                             gmx_bool bUseGPU,
-                                             t_commrec *cr) GPU_FUNC_TERM
+                                         gmx_bool bUseGPU,
+                                         t_commrec *cr) GPU_FUNC_TERM
     
 GPU_FUNC_QUALIFIER
 gmx_bool gpuBufferOpsActiveThisTimestep() GPU_FUNC_TERM
@@ -87,7 +88,7 @@ rvec* gpuBufferOpsGetFPtr() GPU_FUNC_TERM
 #include "cuda.h"
 #include "cuda_runtime.h"
 
-void gpuBufferOpsFlagDataReady( cudaStream_t streamIn);
+void gpuBufferOpsFlagDataReady(cudaStream_t streamIn);
 
 
 #endif
