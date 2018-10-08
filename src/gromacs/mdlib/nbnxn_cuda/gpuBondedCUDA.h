076d1f497c3fac9a7d10dccf6bd52623e838e73e
Magnus Lundborg <lundborg.magnus@gmail.com>
2018-10-08 17:01:23 +0200

    Improving the GPU bonded kernels
    Focusing on cleaning up the kernels and trying to make them easier to
    read.
    
    Change-Id: Ic9ac1ee9acc06b07c4634f1cb2c9bf524d5cce85

--- src/gromacs/mdlib/nbnxn_cuda/gpuBondedCUDA.h
+++ src/gromacs/mdlib/nbnxn_cuda/gpuBondedCUDA.h
@@ -1,3 +1,40 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2018, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#ifndef GMX_MDLIB_NBNXN_CUDA_GPUBONDEDCUDA_H
+#define GMX_MDLIB_NBNXN_CUDA_GPUBONDEDCUDA_H
+
 #include "gromacs/mdlib/nbnxn_gpu_types.h"
 #include "gromacs/ewald/pme-internal.h"
 #include "gromacs/ewald/pme.h"
@@ -6,12 +43,7 @@
 #include "gromacs/mdtypes/inputrec.h"
 #include "gromacs/mdtypes/enerdata.h"
 
-//#include "gmxpre.h"
 #include "gromacs/mdlib/nbnxn_atomdata.h"
-//#include "gromacs/mdlib/nbnxn_consts.h"
-//#include "gromacs/mdlib/nbnxn_internal.h"
-//#include "gromacs/mdlib/nbnxn_search.h"
-//#include "gromacs/mdlib/nbnxn_util.h"
 
 #include "gromacs/topology/idef.h"
 #include "gromacs/topology/ifunc.h"
@@ -21,13 +53,14 @@
 #define TPB_BONDED 256
 
 //Macro for checking cuda errors following a cuda launch or api call
-#define cudaCheckError() {                                          \
-    cudaError_t e=cudaGetLastError();                                 \
-    if(e!=cudaSuccess) {                                              \
-      printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));           \
-      exit(0); \
-    }                                                                 \
-  }
+#define cudaCheckError() {                                                            \
+    cudaError_t e=cudaGetLastError();                                                 \
+    if(e!=cudaSuccess)                                                                \
+    {                                                                                 \
+        printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e)); \
+        exit(0);                                                                      \
+    }                                                                                 \
+}
   
 #if defined(__CUDACC__)
 #include "nbnxn_cuda.h"
@@ -37,25 +70,17 @@
 #endif
 
 // new stuff for bonded
-void update_gpu_bonded( const t_idef *idef,  const t_forcerec *fr, matrix box,
-                        int size,  const t_mdatoms *md, const real *lambda,
-                        gmx_grppairener_t *grppener );                       
+void update_gpu_bonded(const t_idef *idef,  const t_forcerec *fr, const matrix box,
+                       const int size,  const t_mdatoms *md, gmx_grppairener_t *grppener); 
 void do_bonded_gpu(t_forcerec *fr, const t_inputrec *ir, const t_idef *idef, 
-                   int flags, const t_graph *graph , int natoms, rvec x[], 
-                   real *lambda, const t_mdatoms *md, 
-                   rvec *input_force, t_lambda *fepvals, gmx_enerdata_t *enerd);
+                   int flags, const t_graph *graph , int natoms, rvec x[]);
 
-void do_bonded_gpu_finalize(t_forcerec *fr, const t_inputrec *ir, const t_idef *idef,
-                   int flags, const t_graph *graph , int natoms, rvec x[],
-                   real *lambda, const t_mdatoms *md,
-                   rvec *input_force, t_lambda *fepvals, gmx_enerdata_t *enerd);
+void do_bonded_gpu_finalize(t_forcerec *fr, int flags, int natoms,
+                            rvec *input_force, gmx_enerdata_t *enerd);
 
 void reset_gpu_bonded(const int size, const int nener);
 
-#define STANDALONE
-
-//#define DEBUG_JV
-
+#endif
 
 
 
