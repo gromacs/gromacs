83b0eaaf5feb17cf08c00c75007e863491359d9d
Mark Abraham <mark.j.abraham@gmail.com>
2018-10-11 21:53:38 +0200

    Task assignment for bonded interactions on CUDA GPUs
    Made a query function to find whether any interactions of supported
    times exist in the global topology, so that we can make efficient
    high-level decisions.
    
    Added free for gpuBondedLists pointer.
    
    Minor cleanup in manage-threading.h
    
    Fixes #2679
    
    Change-Id: I0ebbbd33c2cba5808561111b0ec6160bfd2f840d

--- src/api/cpp/context.cpp
+++ src/api/cpp/context.cpp
@@ -187,6 +187,7 @@ std::shared_ptr<Session> ContextImpl::launch(const Workflow &work)
         builder.addNonBonded(options_.nbpu_opt_choices[0]);
         // \todo pass by value
         builder.addElectrostatics(options_.pme_opt_choices[0], options_.pme_fft_opt_choices[0]);
+        builder.addBondedTaskAssignment(options_.bonded_opt_choices[0]);
         builder.addNeighborList(options_.nstlist_cmdline);
         builder.addReplicaExchange(options_.replExParams);
         // \todo take ownership of multisim resources (ms)
