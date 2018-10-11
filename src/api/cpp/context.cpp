b889ebc478455642c3401610f3884efaff68176c
Mark Abraham <mark.j.abraham@gmail.com>
2018-10-11 21:53:38 +0200

    Task assignment for bonded interactions on CUDA GPUs
    Fixes #2679
    
    Change-Id: I0ebbbd33c2cba5808561111b0ec6160bfd2f840d

--- src/api/cpp/context.cpp
+++ src/api/cpp/context.cpp
@@ -199,6 +199,8 @@ std::shared_ptr<Session> ContextImpl::launch(const Workflow &work)
         { nullptr, "auto", "cpu", "gpu", nullptr };
         const char       *pme_fft_opt_choices[] =
         { nullptr, "auto", "cpu", "gpu", nullptr };
+        const char       *bonded_opt_choices[] =
+        { nullptr, "auto", "cpu", "gpu", nullptr };
         gmx_bool          bTryToAppendFiles     = TRUE;
         const char       *gpuIdsAvailable       = "";
         const char       *userGpuTaskAssignment = "";
@@ -481,6 +483,7 @@ std::shared_ptr<Session> ContextImpl::launch(const Workflow &work)
         builder.addNonBonded(nbpu_opt_choices[0]);
         // \todo pass by value
         builder.addElectrostatics(pme_opt_choices[0], pme_fft_opt_choices[0]);
+        builder.addBondedTaskAssignment(bonded_opt_choices[0]);
         builder.addNeighborList(nstlist_cmdline);
         builder.addReplicaExchange(replExParams);
         // \todo take ownership of multisim resources (ms)
