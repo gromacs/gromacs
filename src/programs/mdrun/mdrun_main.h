52d2644477a9c45a4a4852954840dfb55c13590c
M. Eric Irrgang <ericirrgang@gmail.com>
2018-10-14 15:33:48 +0300

    Preparation for de-duplicating mdrun setup
    Recent work introducing the API extracted MdFilenames and then
    essentially duplicated everything else. This commit expands
    MdFilenames into LegacyOptions, and clarified the plan that all such
    code needs to become either module-based option handling, or aspects
    of SimulationContext suitable for both CLI and API.
    
    This commit introduces the LegacyOptions struct, so that a future
    commit can make it available to the API setup for an mdrun
    session. The layout of the changes is intended to minimize the textual
    diff for the CLI. Code motion for clarity and also use by the API will
    follow.
    
    Removed MdFilenames, made its data a std::vector in LegacyOptions, and
    used it via an ArrayRef. This clarifies that it is read-only data
    supplied by the client.
    
    Change-Id: I3438cfe1ab52eeb2fffbd16f4bd5254b2a74f03f

--- src/programs/mdrun/mdrun_main.h
+++ src/programs/mdrun/mdrun_main.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2013, by the GROMACS development team, led by
+ * Copyright (c) 2013,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -35,6 +35,11 @@
 #ifndef GMX_PROGRAMS_MDRUN_MDRUN_H
 #define GMX_PROGRAMS_MDRUN_MDRUN_H
 
+namespace gmx
+{
+
 int gmx_mdrun(int argc, char *argv[]);
+
+} // namespace gmx
 
 #endif
