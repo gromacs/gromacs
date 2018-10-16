c8e8fb6c72871b72ec97df78cdfc304013ed3c97
Berk Hess <hess@kth.se>
2018-10-16 10:50:20 +0200

    Pin important buffers in CUDA bondeds
    Change-Id: I135be741a72a2ae844f9a26144b76d1d19d03380

--- src/gromacs/listed-forces/bonded.cu
+++ src/gromacs/listed-forces/bonded.cu
@@ -1018,7 +1018,7 @@ update_gpu_bonded(GpuBondedLists *gpuBondedLists)
     // forceatoms
     for (int ftype : ftypesOnGpu)
     {
-        const InteractionList &iList = gpuBondedLists->iLists[ftype];
+        const auto &iList = gpuBondedLists->iLists[ftype];
 
         if (iList.size() > 0)
         {
@@ -1028,7 +1028,7 @@ update_gpu_bonded(GpuBondedLists *gpuBondedLists)
 
             copyToDeviceBuffer(&iListDevice.iatoms, iList.iatoms.data(),
                                0, iList.size(),
-                               *stream, GpuApiCallBehavior::Sync, nullptr);
+                               *stream, GpuApiCallBehavior::Async, nullptr);
         }
     }
 }
@@ -1063,7 +1063,7 @@ launch_bonded_kernels(t_forcerec   *fr,
 
     for (int ftype : ftypesOnGpu)
     {
-        const InteractionList &iList = gpuBondedLists->iLists[ftype];
+        const auto &iList = gpuBondedLists->iLists[ftype];
 
         if (iList.size() > 0)
         {
@@ -1097,7 +1097,7 @@ launch_bonded_kernels(t_forcerec   *fr,
 
     for (int ftype : ftypesOnGpu)
     {
-        const InteractionList &iList = fr->gpuBondedLists->iLists[ftype];
+        const auto &iList = fr->gpuBondedLists->iLists[ftype];
 
         if (iList.size() > 0)
         {
@@ -1223,7 +1223,7 @@ bonded_gpu_get_energies(t_forcerec *fr,  gmx_enerdata_t *enerd)
     float        *vtot   = gpuBondedLists->vtot.data();
     copyFromDeviceBuffer(vtot, &gpuBondedLists->vtotDevice,
                          0, F_NRE,
-                         *stream, GpuApiCallBehavior::Sync, nullptr);
+                         *stream, GpuApiCallBehavior::Async, nullptr);
     cudaError_t stat = cudaStreamSynchronize(*stream);
     CU_RET_ERR(stat, "D2H transfer failed");
 
