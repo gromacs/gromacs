80e695a165d01d21537328034f3e4b40af6487fa
Berk Hess <hess@kth.se>
2018-10-16 10:50:20 +0200

    Pin important buffers in CUDA bondeds
    This allows for asynchronous transfers.
    
    Change-Id: I0f251c17bdd44856bf773641633e8fa78e6be894

--- src/gromacs/listed-forces/bonded.cu
+++ src/gromacs/listed-forces/bonded.cu
@@ -1037,7 +1037,7 @@ update_gpu_bonded(GpuBondedLists *gpuBondedLists)
     // forceatoms
     for (int ftype : ftypesOnGpu)
     {
-        const InteractionList &iList = gpuBondedLists->iLists[ftype];
+        const auto &iList = gpuBondedLists->iLists[ftype];
 
         if (iList.size() > 0)
         {
@@ -1047,7 +1047,7 @@ update_gpu_bonded(GpuBondedLists *gpuBondedLists)
 
             copyToDeviceBuffer(&iListDevice.iatoms, iList.iatoms.data(),
                                0, iList.size(),
-                               *stream, GpuApiCallBehavior::Sync, nullptr);
+                               *stream, GpuApiCallBehavior::Async, nullptr);
         }
     }
 }
@@ -1082,7 +1082,7 @@ launch_bonded_kernels(t_forcerec   *fr,
 
     for (int ftype : ftypesOnGpu)
     {
-        const InteractionList &iList = gpuBondedLists->iLists[ftype];
+        const auto &iList = gpuBondedLists->iLists[ftype];
 
         if (iList.size() > 0)
         {
@@ -1116,7 +1116,7 @@ launch_bonded_kernels(t_forcerec   *fr,
 
     for (int ftype : ftypesOnGpu)
     {
-        const InteractionList &iList = fr->gpuBondedLists->iLists[ftype];
+        const auto &iList = fr->gpuBondedLists->iLists[ftype];
 
         if (iList.size() > 0)
         {
@@ -1250,7 +1250,7 @@ bonded_gpu_get_energies(t_forcerec *fr,  gmx_enerdata_t *enerd)
     float        *vtot   = gpuBondedLists->vtot.data();
     copyFromDeviceBuffer(vtot, &gpuBondedLists->vtotDevice,
                          0, F_NRE,
-                         *stream, GpuApiCallBehavior::Sync, nullptr);
+                         *stream, GpuApiCallBehavior::Async, nullptr);
     cudaError_t stat = cudaStreamSynchronize(*stream);
     CU_RET_ERR(stat, "D2H transfer failed");
 
