64d2d0afff9ef164e5badda08437c72c6a8ea9e9
Paul Bauer <paul.bauer.q@gmail.com>
2018-10-17 14:47:33 +0200

    Fix segmentation fault in gmx helix
    Changed the logic in tpxio to only try to access the mtop
    datastructure if it is actually present.
    
    Fixes #2701
    
    Change-Id: I268af10dc125a57758f5f3e79e4b14a25050b3b3

--- src/gromacs/fileio/tpxio.cpp
+++ src/gromacs/fileio/tpxio.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -3494,7 +3494,10 @@ int read_tpx(const char *fn,
     fio     = open_tpx(fn, "r");
     ePBC    = do_tpx(fio, TRUE, ir, &state, x, v, mtop);
     close_tpx(fio);
-    *natoms = mtop->natoms;
+    if (mtop != nullptr && natoms != nullptr)
+    {
+        *natoms = mtop->natoms;
+    }
     if (box)
     {
         copy_mat(state.box, box);
