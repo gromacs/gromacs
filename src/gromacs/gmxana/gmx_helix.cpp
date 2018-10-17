436b14b40571a39bdbf738381af0af620fb018c7
Paul Bauer <paul.bauer.q@gmail.com>
2018-10-17 14:47:33 +0200

    Fix segmentation fault in gmx helix
    The code needed to use a mtop datastructure but never declared it. Fixed
    now.
    
    Fixes #2701
    
    Change-Id: I268af10dc125a57758f5f3e79e4b14a25050b3b3

--- src/gromacs/gmxana/gmx_helix.cpp
+++ src/gromacs/gmxana/gmx_helix.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -206,8 +206,13 @@ int gmx_helix(int argc, char *argv[])
 
     /* Read reference frame from tpx file to compute helix length */
     snew(xref, top->atoms.nr);
-    read_tpx(ftp2fn(efTPR, NFILE, fnm),
-             nullptr, nullptr, &natoms, xref, nullptr, nullptr);
+    {
+        gmx_mtop_t mtop;
+        init_mtop(&mtop);
+        read_tpx(ftp2fn(efTPR, NFILE, fnm),
+                 nullptr, nullptr, &natoms, xref, nullptr, &mtop);
+        done_mtop(&mtop);
+    }
     calc_hxprops(nres, bb, xref);
     do_start_end(nres, bb, &nbb, bbindex, &nca, caindex, bRange, rStart, rEnd);
     sfree(xref);
