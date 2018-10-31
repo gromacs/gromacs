28bee9e71061815cac58642770c653911b30ab7b
David van der Spoel <spoel@xray.bmc.uu.se>
2018-10-31 13:06:06 +0100

    Change get_atom_index to allow multiple molecules.
    Make gromacs take into account atoms in all molecules.
    This allows Hessian matrix creation to work over all
    molecules in a simulation. An update to the regression
    tests has been submitted as well.
    
    Fixes #2720
    
    Change-Id: I9fc2dd05cb1c67d46b6d4c3d9139e6b3bed72f17

--- src/gromacs/topology/mtop_util.cpp
+++ src/gromacs/topology/mtop_util.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2008,2009,2010,2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2008,2009,2010,2012,2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1011,17 +1011,16 @@ t_topology gmx_mtop_t_to_t_topology(gmx_mtop_t *mtop, bool freeMTop)
 std::vector<size_t> get_atom_index(const gmx_mtop_t *mtop)
 {
 
-    std::vector<size_t>       atom_index;
-    gmx_mtop_atomloop_block_t aloopb = gmx_mtop_atomloop_block_init(mtop);
-    const t_atom             *atom;
-    int                       nmol, j = 0;
-    while (gmx_mtop_atomloop_block_next(aloopb, &atom, &nmol))
+    std::vector<size_t>      atom_index;
+    gmx_mtop_atomloop_all_t  aloop = gmx_mtop_atomloop_all_init(mtop);
+    const t_atom            *atom;
+    int                      at_global;
+    while (gmx_mtop_atomloop_all_next(aloop, &at_global, &atom))
     {
         if (atom->ptype == eptAtom)
         {
-            atom_index.push_back(j);
+            atom_index.push_back(at_global);
         }
-        j++;
     }
     return atom_index;
 }
