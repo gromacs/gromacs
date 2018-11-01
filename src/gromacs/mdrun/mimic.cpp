fc3875dec1fb9e7a8b9f03de6253ae147cf26d88
Mark Abraham <mark.j.abraham@gmail.com>
2018-11-01 12:49:03 +0100

    Introduce PpForceWorkload
    Assigns responsibility for knowing what work is required for the force
    calculation of an MD step to a single object. Moved actual control of
    executing any necessary CUDA bonded work to the new schedule
    object. Changed low-level routines to assert when invalid calls are
    made, because only one place should control whether work is done.
    
    This prepares for making GpuBondedLists an opaque type, when
    bonded_gpu_have_interactions will not be able to be an inline
    function.
    
    Refs #2724, #2574
    
    Change-Id: Ie59b790c54170692b0221f5eb3812643ba6f61d6

--- src/gromacs/mdrun/mimic.cpp
+++ src/gromacs/mdrun/mimic.cpp
@@ -453,7 +453,7 @@ void gmx::Integrator::do_mimic()
                                 constr, enerd, fcd,
                                 state, f.arrayRefWithPadding(), force_vir, mdatoms,
                                 nrnb, wcycle, graph, groups,
-                                shellfc, fr, t, mu_tot,
+                                shellfc, fr, ppForceWorkload, t, mu_tot,
                                 vsite,
                                 ddOpenBalanceRegion, ddCloseBalanceRegion);
         }
@@ -471,7 +471,7 @@ void gmx::Integrator::do_mimic()
                      state->box, state->x.arrayRefWithPadding(), &state->hist,
                      f.arrayRefWithPadding(), force_vir, mdatoms, enerd, fcd,
                      state->lambda, graph,
-                     fr, vsite, mu_tot, t, ed,
+                     fr, ppForceWorkload, vsite, mu_tot, t, ed,
                      GMX_FORCE_NS | force_flags,
                      ddOpenBalanceRegion, ddCloseBalanceRegion);
         }
