4d8ce640dd1bc1e40cd12ca77eb185b02207687f
Paul Bauer <paul.bauer.q@gmail.com>
2018-11-07 14:49:26 +0100

    [RFC] Add test script for gmxapi
    Adds test script for gmxapi as well as entries in the pre-submit matrix
    and some code to gromacs.py to run the test script.
    
    TODO discuss which agents are running the api tests.
    
    Refs #2756
    
    Change-Id: I9ad2995263c7798c2c4569377be701d4a8ec7291

---
+++ admin/builds/gmxapitest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Do nothing for now
+# TODO activate this script
+
+# source $HOME/py2env/bin/activate
+# export PYTHON=`which python`
+# pip uninstall -y gmx
+# (cd gmxapi && ./ci_scripts/pygmx.sh) > pygmx.out; echo $?
+# (cd sample_restraint && ./ci_scripts/sample_restraint.sh) > sample_restraint.out 2>&1; echo $?
+# deactivate
+
+echo "Currently not running the gmxapi tests"
