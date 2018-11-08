03737b0cafac0fbe7445e291c418756ec5f7365b
Mark Abraham <mark.j.abraham@gmail.com>
2018-11-08 17:13:25 +0100

    Disabled FPE checking for non-release builds
    This means that Jenkins will no longer give any reports, false or
    true, from floating-point exceptions. Test binaries can still have FPE
    checking enabled manually, however.
    
    The content of this change should not be merged into the release-2019
    branch.
    
    Fixes #2750
    
    Change-Id: I12c5e1a08e43e48cb7012edb6d5e8aea08baddc8

--- src/testutils/testinit.cpp
+++ src/testutils/testinit.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -161,7 +161,9 @@ void initTestUtils(const char *dataPath, const char *tempPath, bool usesMpi,
                    bool usesHardwareDetection, int *argc, char ***argv)
 {
 #ifndef NDEBUG
-    gmx_feenableexcept();
+    // Disabled for the rest of the lifetime of release-2018 branch
+    // to prevent false positives.
+    // gmx_feenableexcept();
 #endif
     const CommandLineProgramContext &context = initForCommandLine(argc, argv);
     try
