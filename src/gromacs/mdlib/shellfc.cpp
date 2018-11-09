184f4e08d3c2a1c205aba6503283f74e0beda304
Mark Abraham <mark.j.abraham@gmail.com>
2018-11-09 08:12:26 +0100

    Fix inefficient shell minimization
    In 5268b73d6cd, a shallow copy was used instead of the former deep
    copy.
    
    Fixes #2705
    
    Change-Id: Iad8a0b0793304e2c10d9abf3a180cfe9bdc85c4c

--- src/gromacs/mdlib/shellfc.cpp
+++ src/gromacs/mdlib/shellfc.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2008, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1160,8 +1160,12 @@ void relax_shell_flexcon(FILE *fplog, t_commrec *cr, gmx_bool bVerbose,
          * shell positions are updated, therefore the other particles must
          * be set here.
          */
-        pos[Min] = state->x;
-        pos[Try] = state->x;
+        std::copy(state->x.begin(),
+                  state->x.end(),
+                  pos[Min].begin());
+        std::copy(state->x.begin(),
+                  state->x.end(),
+                  pos[Try].begin());
     }
 
     if (bVerbose && MASTER(cr))
