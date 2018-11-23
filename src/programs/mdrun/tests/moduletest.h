9bb04f3dc03eb1856a1f28d1b2fac1a1e8fd85fd
David van der Spoel <spoel@xray.bmc.uu.se>
2018-11-23 22:05:22 +0100

    Added new testing code for normal mode analysis.
    Added new default mdp fileds in tests for rvdw
    and rcoulomb.
    
    Part of #2771
    
    Change-Id: Ifb44841b36be5076fe8d77d5982876f881fdc69b

--- src/programs/mdrun/tests/moduletest.h
+++ src/programs/mdrun/tests/moduletest.h
@@ -100,6 +100,8 @@ class SimulationRunner
         void useStringAsNdxFile(const char *ndxString);
         //! Use a standard .top and .gro file as input to grompp
         void useTopGroAndNdxFromDatabase(const std::string &name);
+        //! Use a standard .top and .g96 file as input to grompp
+        void useTopG96AndNdxFromDatabase(const std::string &name);
         //! Use a standard .gro file as input to grompp
         void useGroFromDatabase(const char *name);
         //! Calls grompp (on rank 0, with a customized command line) to prepare for the mdrun test
@@ -110,6 +112,8 @@ class SimulationRunner
         int callGromppOnThisRank(const CommandLine &callerRef);
         //! Convenience wrapper for a default call to \c callGromppOnThisRank
         int callGromppOnThisRank();
+        //! Calls nmeig for testing
+        int callNmeig();
         //! Calls mdrun for testing with a customized command line
         int callMdrun(const CommandLine &callerRef);
         /*! \brief Convenience wrapper for calling mdrun for testing
@@ -137,6 +141,7 @@ class SimulationRunner
         std::string tprFileName_;
         std::string logFileName_;
         std::string edrFileName_;
+        std::string mtxFileName_;
         std::string cptFileName_;
         std::string swapFileName_;
         int         nsteps_;
