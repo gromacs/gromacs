230258223124fdb3d903016be5c3b1400b5574dc
Berk Hess <hess@kth.se>
2018-12-13 12:06:54 +0100

    Fix LINCS triangle constraining with OpenMP
    LINCS would only apply double the iterations to constraints
    in triangles when the last OpenMP task contained at least one such
    triangle.
    
    Fixes #2808
    
    Change-Id: Ia9a402f3ada87dbf73f74eda943b3fa4bfb3559b

--- src/gromacs/mdlib/clincs.cpp
+++ src/gromacs/mdlib/clincs.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1278,7 +1278,7 @@ static void set_lincs_matrix(struct gmx_lincsdata *li, real *invmass, real lambd
         {
             set_lincs_matrix_task(li, &li->task[th], invmass,
                                   &ncc_triangle, &nCrossTaskTriangles);
-            ntriangle = li->task[th].ntriangle;
+            ntriangle += li->task[th].ntriangle;
         }
         GMX_CATCH_ALL_AND_EXIT_WITH_FATAL_ERROR;
     }
