9c600ce1605eac1105180a0071289f00e10673ad
Kevin Boyd <kevin.boyd@uconn.edu>
2018-12-24 15:53:17 -0500

    Add grompp check for usage of "define" field in mdp
    Users can misspell the strings specified with -D in the topology. This  now
    check that every -D field has a corresponding #ifdef and/or #ifndef and/or
    C macro-style text replacement in the topology.
    
    Fixed one unit test with unnecessary define field
    
    refs #1975
    
    Change-Id: Ie6329b234a60dde8efc34fb788e6296f241651ed

--- src/gromacs/gmxpreprocess/gmxcpp.h
+++ src/gromacs/gmxpreprocess/gmxcpp.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2008, The GROMACS development team.
- * Copyright (c) 2012,2014,2015,2018, by the GROMACS development team, led by
+ * Copyright (c) 2012,2014,2015,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -85,5 +85,9 @@ void cpp_done(gmx_cpp_t handle);
    variable.
  */
 char *cpp_error(gmx_cpp_t *handlep, int status);
+
+/* Returns warning message if strings defined in mdp define section (e.g. -DFLEXIBLE)
+ * were not not found when processing the topology */
+std::string checkAndWarnForUnusedDefines(const gmx_cpp &handle);
 
 #endif
