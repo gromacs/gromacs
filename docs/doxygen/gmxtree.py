cb92bbb9aba43b7c1458f3ee9f3db17ff0e191a0
Berk Hess <hess@kth.se>
2019-01-04 11:54:27 +0100

    Move nbnxn files to nbnxn directory
    This change moves all files related to the nbnxn code to an new
    nbnxn subdirectory. This change is only code motion and file renaming.
    All nbnxn setup code in forcerec.cpp is moved to setup.cpp.
    Removed nbnxn_ and _simd... pre- and postfixes from most files.
    
    Change-Id: I39c17769c566f78b84211510c5ac17793fd131fa

--- docs/doxygen/gmxtree.py
+++ docs/doxygen/gmxtree.py
@@ -2,7 +2,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2014,2015,2016,2017,2018, by the GROMACS development team, led by
+# Copyright (c) 2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -1080,4 +1080,4 @@ class GromacsTree(object):
         return (self._files['src/config.h'],
                 self._files['src/gromacs/simd/simd.h'],
                 self._files['src/gromacs/ewald/pme-simd.h'],
-                self._files['src/gromacs/mdlib/nbnxn_simd.h'])
+                self._files['src/gromacs/nbnxn/simd.h'])
