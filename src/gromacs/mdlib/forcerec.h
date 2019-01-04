a723255e83f4544433de992f276a3e037f60fef9
Berk Hess <hess@kth.se>
2019-01-04 11:54:27 +0100

    [WIP] Move nbnxn files to nbnxn directory
    Change-Id: I39c17769c566f78b84211510c5ac17793fd131fa

--- src/gromacs/mdlib/forcerec.h
+++ src/gromacs/mdlib/forcerec.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -165,13 +165,6 @@ gmx_bool can_use_allvsall(const t_inputrec *ir,
 /* Returns if we can use all-vs-all loops.
  * If bPrintNote==TRUE, prints a note, if necessary, to stderr
  * and fp (if !=NULL) on the master node.
- */
-
-gmx_bool nbnxn_simd_supported(const gmx::MDLogger &mdlog,
-                              const t_inputrec    *ir);
-/* Return if CPU SIMD support exists for the given inputrec
- * If the return value is FALSE and fplog/cr != NULL, prints a fallback
- * message to fplog/stderr.
  */
 
 /* Compute the average C6 and C12 params for LJ corrections */
