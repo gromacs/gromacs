247d8fc572d7fb4c6cccf2ecd96413321a78c9b7
Kevin Boyd <kevin.boyd@uconn.edu>
2019-01-13 11:04:55 -0500

    Fix tiny leak in pleasecite.cpp
    Change-Id: Iee2846d42a8b843c2e70de2b7f4e33e889fad90a

--- src/gromacs/utility/pleasecite.cpp
+++ src/gromacs/utility/pleasecite.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -434,7 +434,7 @@ writeSourceDoi(FILE *fp)
         return;
     }
 
-    const char *doiString = wrap_lines(gmxDOI(), LINE_WIDTH, 0, FALSE);
+    char *doiString = wrap_lines(gmxDOI(), LINE_WIDTH, 0, FALSE);
 
     if (fp == nullptr)
     {
@@ -444,4 +444,6 @@ writeSourceDoi(FILE *fp)
     fprintf(fp, "%s%s\n", "https://doi.org/", doiString);
     fprintf(fp, "-------- -------- --- Thank You --- -------- --------\n\n");
     fflush(fp);
+
+    sfree(doiString);
 }
