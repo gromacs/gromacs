2e84a90309df5c9a5701b5a4789d1b20bf1469a8
Kevin Boyd <kevin.boyd@uconn.edu>
2019-01-13 11:01:38 -0500

    Fix small leak in constraints
    Change-Id: I68370a3cec7d39846c26c46f293fd8855540252d

--- src/gromacs/mdlib/constr.cpp
+++ src/gromacs/mdlib/constr.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1122,6 +1122,10 @@ Constraints::Impl::Impl(const gmx_mtop_t     &mtop_p,
 
 Constraints::Impl::~Impl()
 {
+    for (auto blocka : at2con_mt)
+    {
+        done_blocka(&blocka);
+    }
     if (bSettleErrorHasOccurred != nullptr)
     {
         sfree(bSettleErrorHasOccurred);
