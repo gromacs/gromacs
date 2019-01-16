aa0b3a0b0e21b5685b551b5470db467a6f8e6bca
David van der Spoel <spoel@xray.bmc.uu.se>
2019-01-16 10:01:34 +0100

    Tests for alternative bond potentials.
    Make-over of previous bondedtests and update
    of code to test everything.
    
    TODO:
    - Make filenames in tests/refdata more meaningful.
    - Case dependent precision.
    
    Part of #2795
    
    Change-Id: I15e4e5053fb0d1dd5968614bd06f899f0d28dc3a

--- src/gromacs/listed-forces/tests/bonded.cpp
+++ src/gromacs/listed-forces/tests/bonded.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2016,2017,2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -46,6 +46,7 @@
 #include <cmath>
 
 #include <memory>
+#include <unordered_map>
 
 #include <gtest/gtest.h>
 
@@ -95,35 +96,121 @@ void checkOutput(test::TestReferenceChecker *checker,
     checker->checkReal(output.energy, "Epot ");
     // Should still be zero if not doing FEP, so may as well test it.
     checker->checkReal(output.dvdlambda, "dVdlambda ");
-    // TODO This is pretty inefficient, perhaps we should just check
-    // whether the central box fields have their values.
-    checker->checkSequence(std::begin(output.fshift), std::end(output.fshift), "ShiftForces");
+    checker->checkVector(output.fshift[CENTRAL], "Central shift forces");
     checker->checkSequence(std::begin(output.f), std::end(output.f), "Forces");
 }
 
-class BondedTest : public ::testing::TestWithParam<std::tuple<std::vector<gmx::RVec>, int> >
+/*! \brief Utility to to fill iparams struct
+ *
+ * \param[in]  ftype Function type
+ * \param[in]  fep   Whether or not to use free energy
+ * \param[out] ip    Pointer to iparams struct
+ */
+void fillIparams(int ftype, bool fep, t_iparams *ip)
+{
+    switch (ftype)
+    {
+        case F_BONDS:
+        case F_G96BONDS:
+            if (fep)
+            {
+                ip->harmonic = { 0.8, 50.0, 0.6, 40.0 };
+            }
+            else
+            {
+                ip->harmonic = { 0.8, 50.0, 0.8, 50.0 };
+            }
+            break;
+        case F_MORSE:
+            ip->morse = { 0.8, 50.0, 2.0, 0.8, 50.0, 2.0 };
+            break;
+        case F_CUBICBONDS:
+            ip->cubic = { 0.8, 50.0, 2.0 };
+            break;
+        case F_FENEBONDS:
+            ip->fene = { 1.01, 5.0 };
+            break;
+        case F_ANGLES:
+            if (fep)
+            {
+                ip->harmonic = { 100, 50.0, 95.0, 30.0 };
+            }
+            else
+            {
+                ip->harmonic = { 100, 50.0, 100.0, 50.0 };
+            }
+            break;
+        case F_PDIHS:
+            if (fep)
+            {
+                ip->pdihs = { -100.0, 10, 1, -80, 20 };
+            }
+            else
+            {
+                ip->pdihs = { -100.0, 10, 1, -100, 10 };
+            }
+            break;
+        case  F_IDIHS:
+            if (fep)
+            {
+                ip->harmonic = { 0.0, 5.0, 0.0, 5.0 };
+            }
+            else
+            {
+                ip->harmonic = { 0.0, 5.0, 35.5, 10.0 };
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+/*! \brief Utility to to fill iatoms struct
+ *
+ * \param[in]  ftype  Function type
+ * \param[out] iatoms Pointer to iatoms struct
+ */
+void fillIatoms(int ftype, std::vector<t_iatom> *iatoms)
+{
+    std::unordered_map<int, std::vector<int> > ia =
+    { { 2, { 0, 0, 1, 0, 1, 2, 0, 2, 3 } },
+      { 3, { 0, 0, 1, 2, 0, 1, 2, 3 } },
+      { 4, { 0, 0, 1, 2, 3 } }};
+    int nral = interaction_function[ftype].nratoms;
+    for (auto &i : ia[nral])
+    {
+        iatoms->push_back(i);
+    }
+}
+
+class ListedForcesTest : public ::testing::TestWithParam<std::tuple<std::vector<gmx::RVec>, int, int, bool> >
 {
     protected:
         matrix                     box_;
         t_pbc                      pbc_;
         std::vector<gmx::RVec>     x_;
         int                        epbc_;
+        int                        ftype_;
+        bool                       fep_;
         test::TestReferenceData    refData_;
         test::TestReferenceChecker checker_;
-        BondedTest( ) :
+        ListedForcesTest( ) :
             checker_(refData_.rootChecker())
         {
             // We need quite specific tolerances here since angle functions
             // etc. are not very precise and reproducible.
-            test::FloatingPointTolerance tolerance(test::FloatingPointTolerance(1.0e-4, 2.0e-7,
+            test::FloatingPointTolerance tolerance(test::FloatingPointTolerance(1.0e-4, 1.0e-6,
                                                                                 1.0e-6, 1.0e-12,
                                                                                 1000000000, 10000, false));
             checker_.setDefaultTolerance(tolerance);
-            x_ = std::get<0>(GetParam());
+            x_     = std::get<0>(GetParam());
+            epbc_  = std::get<1>(GetParam());
+            ftype_ = std::get<2>(GetParam());
+            fep_   = std::get<3>(GetParam());
             clear_mat(box_);
             box_[0][0] = box_[1][1] = box_[2][2] = 1.5;
-            epbc_      = std::get<1>(GetParam());
             set_pbc(&pbc_, epbc_, box_);
+
         }
 
         void testBondAngle()
@@ -156,100 +243,65 @@ class BondedTest : public ::testing::TestWithParam<std::tuple<std::vector<gmx::R
             checker_.checkInteger(t2, "t2");
             checker_.checkInteger(t3, "t3");
         }
-        void testIfunc(test::TestReferenceChecker *checker,
-                       const int                   ftype,
-                       const std::vector<t_iatom> &iatoms,
-                       const t_iparams            &iparams,
-                       const real                  lambda)
+        void testOneIfunc(test::TestReferenceChecker *checker,
+                          const std::vector<t_iatom> &iatoms,
+                          const t_iparams            &iparams,
+                          const real                  lambda)
         {
             SCOPED_TRACE(std::string("Testing PBC ") + epbc_names[epbc_]);
-
             int                           ddgatindex = 0;
             OutputQuantities              output;
-            output.energy = bondedFunction(ftype)(iatoms.size(),
-                                                  iatoms.data(),
-                                                  &iparams,
-                                                  as_rvec_array(x_.data()),
-                                                  output.f, output.fshift,
-                                                  &pbc_,
-                                                  /* const struct t_graph *g */ nullptr,
-                                                  lambda, &output.dvdlambda,
-                                                  /* const struct t_mdatoms *md */ nullptr,
-                                                  /* struct t_fcdata *fcd */ nullptr,
-                                                  &ddgatindex);
+            output.energy = bondedFunction(ftype_)(iatoms.size(),
+                                                   iatoms.data(),
+                                                   &iparams,
+                                                   as_rvec_array(x_.data()),
+                                                   output.f, output.fshift,
+                                                   &pbc_,
+                                                   /* const struct t_graph *g */ nullptr,
+                                                   lambda, &output.dvdlambda,
+                                                   /* const struct t_mdatoms *md */ nullptr,
+                                                   /* struct t_fcdata *fcd */ nullptr,
+                                                   &ddgatindex);
             checkOutput(checker, output);
+        }
+        void testIfunc()
+        {
+            test::TestReferenceChecker thisChecker =
+                checker_.checkCompound("FunctionType",
+                                       interaction_function[ftype_].name).checkCompound("FEP", (fep_ ? "Yes" : "No"));
+            t_iparams            iparams = {{0}};
+            fillIparams(ftype_, fep_, &iparams);
+            std::vector<t_iatom> iatoms;
+            fillIatoms(ftype_, &iatoms);
+            if (fep_)
+            {
+                const int numLambdas = 3;
+                for (int i = 0; i < numLambdas; ++i)
+                {
+                    const real lambda       = i / (numLambdas - 1.0);
+                    auto       valueChecker = thisChecker.checkCompound("Lambda", toString(lambda));
+                    testOneIfunc(&valueChecker, iatoms, iparams, lambda);
+                }
+            }
+            else
+            {
+                testOneIfunc(&thisChecker, iatoms, iparams, 0.0);
+            }
         }
 };
 
-TEST_P (BondedTest, BondAngle)
+TEST_P (ListedForcesTest, Ifunc)
 {
-    testBondAngle();
+    testIfunc();
 }
 
-TEST_P (BondedTest, DihedralAngle)
+//! Function types for testing
+std::vector<int> c_fTypes =
 {
-    testDihedralAngle();
-}
-
-TEST_P (BondedTest, IfuncBonds)
-{
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 0, 1, 2, 0, 2, 3 };
-    t_iparams            iparams;
-    iparams.harmonic.rA  = iparams.harmonic.rB  = 0.8;
-    iparams.harmonic.krA = iparams.harmonic.krB = 50.0;
-    const real lambda = 0.0;
-    testIfunc(&checker_, F_BONDS, iatoms, iparams, lambda);
-}
-
-TEST_P (BondedTest, IfuncAngles)
-{
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 2, 0, 1, 2, 3 };
-    t_iparams            iparams;
-    real                 k = 50.0;
-    iparams.harmonic.rA  = iparams.harmonic.rB  = 100.0;
-    iparams.harmonic.krA = iparams.harmonic.krB = k;
-    const real lambda = 0.0;
-    testIfunc(&checker_, F_ANGLES, iatoms, iparams, lambda);
-}
-
-TEST_P (BondedTest, IfuncProperDihedrals)
-{
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 2, 3 };
-    t_iparams            iparams;
-    iparams.pdihs.phiA = iparams.pdihs.phiB = -100.0;
-    iparams.pdihs.cpA  = iparams.pdihs.cpB  = 10.0;
-    iparams.pdihs.mult = 1;
-    const real lambda = 0.0;
-    testIfunc(&checker_, F_PDIHS, iatoms, iparams, lambda);
-}
-
-TEST_P (BondedTest, IfuncImproperDihedrals)
-{
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 2, 3 };
-    t_iparams            iparams;
-    iparams.harmonic.rA  = iparams.harmonic.rB  = 0.0;
-    iparams.harmonic.krA = iparams.harmonic.krB = 5.0;
-    const real lambda = 0.0;
-    testIfunc(&checker_, F_IDIHS, iatoms, iparams, lambda);
-}
-
-TEST_P (BondedTest, IfuncImproperDihedralsFEP)
-{
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 2, 3 };
-    t_iparams            iparams;
-    iparams.harmonic.rA  = iparams.harmonic.rB  = 0.0;
-    iparams.harmonic.krA = iparams.harmonic.krB = 5.0;
-    iparams.harmonic.rB  = 35.5;
-    iparams.harmonic.krB = 10.0;
-
-    const int numLambdas = 3;
-    for (int i = 0; i < numLambdas; ++i)
-    {
-        const real lambda       = i / (numLambdas - 1.0);
-        auto       valueChecker = checker_.checkCompound("Lambda", toString(lambda));
-        testIfunc(&valueChecker, F_IDIHS, iatoms, iparams, lambda);
-    }
-}
+    F_BONDS, F_G96BONDS, F_CUBICBONDS, F_MORSE, F_FENEBONDS,
+    F_ANGLES,
+    F_PDIHS, F_IDIHS
+};
 
 //! Coordinates for testing
 std::vector<std::vector<gmx::RVec> > c_coordinatesForTests =
@@ -262,7 +314,10 @@ std::vector<std::vector<gmx::RVec> > c_coordinatesForTests =
 //! PBC values for testing
 std::vector<int> c_pbcForTests = { epbcNONE, epbcXY, epbcXYZ };
 
-INSTANTIATE_TEST_CASE_P(ForPbcValues, BondedTest, ::testing::Combine(::testing::ValuesIn(c_coordinatesForTests), ::testing::ValuesIn(c_pbcForTests)));
+//! FEP on or off
+std::vector<bool> c_Fep = { false, true };
+
+INSTANTIATE_TEST_CASE_P(Ifunc, ListedForcesTest, ::testing::Combine(::testing::ValuesIn(c_coordinatesForTests), ::testing::ValuesIn(c_pbcForTests), ::testing::ValuesIn(c_fTypes), ::testing::ValuesIn(c_Fep)));
 }  // namespace
 
 }  // namespace gmx
