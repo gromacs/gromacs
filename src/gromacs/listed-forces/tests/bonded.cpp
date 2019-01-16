52e509c39c2acc6c781f3b335df5e8b53cca4a1c
David van der Spoel <spoel@xray.bmc.uu.se>
2019-01-16 10:01:34 +0100

    Tests for listed force potentials.
    Make-over of previous bondedtests and update
    of code to test everything.
    
    TODO:
    - Refinement of case dependent precision.
    - Tests for restraints and polarization.
    
    Part of #2795
    
    Change-Id: I15e4e5053fb0d1dd5968614bd06f899f0d28dc3a

--- src/gromacs/listed-forces/tests/bonded.cpp
+++ src/gromacs/listed-forces/tests/bonded.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2016,2017,2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -46,6 +46,7 @@
 #include <cmath>
 
 #include <memory>
+#include <unordered_map>
 
 #include <gtest/gtest.h>
 
@@ -95,166 +96,211 @@ void checkOutput(test::TestReferenceChecker *checker,
     checker->checkReal(output.energy, "Epot ");
     // Should still be zero if not doing FEP, so may as well test it.
     checker->checkReal(output.dvdlambda, "dVdlambda ");
-    // TODO This is pretty inefficient, perhaps we should just check
-    // whether the central box fields have their values.
-    checker->checkSequence(std::begin(output.fshift), std::end(output.fshift), "ShiftForces");
+    checker->checkVector(output.fshift[CENTRAL], "Central shift forces");
     checker->checkSequence(std::begin(output.f), std::end(output.f), "Forces");
 }
 
-class BondedTest : public ::testing::TestWithParam<std::tuple<std::vector<gmx::RVec>, int> >
+/*! \brief Input structure for listed forces tests
+ */
+struct iListInput
+{
+    //! Function type
+    int       ftype;
+    //! Tolerance for float evaluation
+    float     ftoler;
+    //! Tolerance for double evaluation
+    double    dtoler;
+    //! Do free energy perturbation?
+    bool      fep;
+    //! Interaction parameters
+    t_iparams iparams;
+};
+
+/*! \brief Utility to fill iatoms struct
+ *
+ * \param[in]  ftype  Function type
+ * \param[out] iatoms Pointer to iatoms struct
+ */
+void fillIatoms(int ftype, std::vector<t_iatom> *iatoms)
+{
+    std::unordered_map<int, std::vector<int> > ia =
+    { { 2, { 0, 0, 1, 0, 1, 2, 0, 2, 3 } },
+      { 3, { 0, 0, 1, 2, 0, 1, 2, 3 } },
+      { 4, { 0, 0, 1, 2, 3 } }};
+    EXPECT_TRUE(ftype >= 0 && ftype < F_NRE);
+    int nral = interaction_function[ftype].nratoms;
+    for (auto &i : ia[nral])
+    {
+        iatoms->push_back(i);
+    }
+}
+
+class ListedForcesTest : public ::testing::TestWithParam<std::tuple<iListInput, std::vector<gmx::RVec>, int> >
 {
     protected:
         matrix                     box_;
         t_pbc                      pbc_;
         std::vector<gmx::RVec>     x_;
         int                        epbc_;
+        iListInput                 input_;
         test::TestReferenceData    refData_;
         test::TestReferenceChecker checker_;
-        BondedTest( ) :
+        ListedForcesTest( ) :
             checker_(refData_.rootChecker())
         {
-            // We need quite specific tolerances here since angle functions
-            // etc. are not very precise and reproducible.
-            test::FloatingPointTolerance tolerance(test::FloatingPointTolerance(1.0e-4, 2.0e-7,
-                                                                                1.0e-6, 1.0e-12,
-                                                                                1000000000, 10000, false));
-            checker_.setDefaultTolerance(tolerance);
-            x_ = std::get<0>(GetParam());
+            input_ = std::get<0>(GetParam());
+            x_     = std::get<1>(GetParam());
+            epbc_  = std::get<2>(GetParam());
             clear_mat(box_);
             box_[0][0] = box_[1][1] = box_[2][2] = 1.5;
-            epbc_      = std::get<1>(GetParam());
             set_pbc(&pbc_, epbc_, box_);
+
+            // We need quite specific tolerances here since angle functions
+            // etc. are not very precise and reproducible.
+            test::FloatingPointTolerance tolerance(test::FloatingPointTolerance(input_.ftoler, 1.0e-6,
+                                                                                input_.dtoler, 1.0e-12,
+                                                                                10000, 100, false));
+            checker_.setDefaultTolerance(tolerance);
         }
-
-        void testBondAngle()
-        {
-            rvec  r_ij, r_kj;
-            real  cosine_angle, angle;
-            int   t1, t2;
-
-            angle = bond_angle(x_[0], x_[1], x_[2], &pbc_,
-                               r_ij, r_kj, &cosine_angle,
-                               &t1, &t2);
-            checker_.checkReal(angle, "angle");
-            checker_.checkReal(cosine_angle, "cosine_angle");
-            checker_.checkInteger(t1, "t1");
-            checker_.checkInteger(t2, "t2");
-        }
-
-        void testDihedralAngle()
-        {
-            rvec  r_ij, r_kj, r_kl, m, n;
-            real  angle;
-            int   t1, t2, t3;
-
-            angle = dih_angle(x_[0], x_[1], x_[2], x_[3], &pbc_,
-                              r_ij, r_kj, r_kl, m, n,
-                              &t1, &t2, &t3);
-
-            checker_.checkReal(angle, "angle");
-            checker_.checkInteger(t1, "t1");
-            checker_.checkInteger(t2, "t2");
-            checker_.checkInteger(t3, "t3");
-        }
-        void testIfunc(test::TestReferenceChecker *checker,
-                       const int                   ftype,
-                       const std::vector<t_iatom> &iatoms,
-                       const t_iparams            &iparams,
-                       const real                  lambda)
+        void testOneIfunc(test::TestReferenceChecker *checker,
+                          const std::vector<t_iatom> &iatoms,
+                          const real                  lambda)
         {
             SCOPED_TRACE(std::string("Testing PBC ") + epbc_names[epbc_]);
-
-            int                           ddgatindex = 0;
-            OutputQuantities              output;
-            output.energy = bondedFunction(ftype)(iatoms.size(),
-                                                  iatoms.data(),
-                                                  &iparams,
-                                                  as_rvec_array(x_.data()),
-                                                  output.f, output.fshift,
-                                                  &pbc_,
-                                                  /* const struct t_graph *g */ nullptr,
-                                                  lambda, &output.dvdlambda,
-                                                  /* const struct t_mdatoms *md */ nullptr,
-                                                  /* struct t_fcdata *fcd */ nullptr,
-                                                  &ddgatindex);
+            std::vector<int> ddgatindex = { 0, 1, 2, 3 };
+            OutputQuantities output;
+            output.energy = bondedFunction(input_.ftype)(iatoms.size(),
+                                                         iatoms.data(),
+                                                         &input_.iparams,
+                                                         as_rvec_array(x_.data()),
+                                                         output.f, output.fshift,
+                                                         &pbc_,
+                                                         /* const struct t_graph *g */ nullptr,
+                                                         lambda, &output.dvdlambda,
+                                                         /* const struct t_mdatoms *md */ nullptr,
+                                                         /* struct t_fcdata *fcd */ nullptr,
+                                                         ddgatindex.data());
+            // Internal consistency test of both test input
+            // and bonded functions.
+            EXPECT_TRUE((input_.fep || (output.dvdlambda == 0.0)));
             checkOutput(checker, output);
+        }
+        void testIfunc()
+        {
+            test::TestReferenceChecker thisChecker =
+                checker_.checkCompound("FunctionType",
+                                       interaction_function[input_.ftype].name).checkCompound("FEP", (input_.fep ? "Yes" : "No"));
+            std::vector<t_iatom> iatoms;
+            fillIatoms(input_.ftype, &iatoms);
+            if (input_.fep)
+            {
+                const int numLambdas = 3;
+                for (int i = 0; i < numLambdas; ++i)
+                {
+                    const real lambda       = i / (numLambdas - 1.0);
+                    auto       valueChecker = thisChecker.checkCompound("Lambda", toString(lambda));
+                    testOneIfunc(&valueChecker, iatoms, lambda);
+                }
+            }
+            else
+            {
+                testOneIfunc(&thisChecker, iatoms, 0.0);
+            }
         }
 };
 
-TEST_P (BondedTest, BondAngle)
+TEST_P (ListedForcesTest, Ifunc)
 {
-    testBondAngle();
+    testIfunc();
 }
 
-TEST_P (BondedTest, DihedralAngle)
+//! Default absolute floating point tolerance
+const float  ftol = 1e-6;
+//! Default absolute floating point tolerance
+const double dtol = 1e-8;
+//! Function types for testing bonds. Add new terms at the end.
+std::vector<iListInput> c_InputBonds =
 {
-    testDihedralAngle();
-}
+    { F_BONDS, ftol, dtol, false,
+      { .harmonic.rA = 0.15, .harmonic.krA = 500.0, .harmonic.rB = 0.15, .harmonic.krB = 500.0 } },
+    { F_BONDS, ftol, dtol, true,
+      { .harmonic.rA = 0.15, .harmonic.krA = 500.0, .harmonic.rB = 0.17, .harmonic.krB = 400.0 } },
+    { F_G96BONDS, ftol, dtol, false,
+      { .harmonic.rA = 0.15, .harmonic.krA = 50.0, .harmonic.rB = 0.15, .harmonic.krB = 50.0 } },
+    { F_G96BONDS, ftol, dtol, true,
+      { .harmonic.rA = 0.15, .harmonic.krA = 50.0, .harmonic.rB = 0.17, .harmonic.krB = 40.0 } }, 
+    { F_CUBICBONDS, ftol, dtol, false,
+      { .cubic.b0 = 0.16, .cubic.kb = 50.0, .cubic.kcub = 2.0 } },
+    { F_MORSE, ftol, dtol, true,
+      { .morse.b0A = 0.15, .morse.cbA = 50.0, .morse.betaA = 2.0,
+        .morse.b0B = 0.17, .morse.cbB = 40.0, .morse.betaB = 1.6 } },
+    { F_MORSE, ftol, dtol, false,
+      { .morse.b0A = 0.15, .morse.cbA = 50.0, .morse.betaA = 2.0,
+        .morse.b0B = 0.15, .morse.cbB = 50.0, .morse.betaB = 2.0 } },
+    { F_FENEBONDS, ftol, dtol, false, 
+      { .fene.bm = 0.4, .fene.kb = 5.0 } }
+};
 
-TEST_P (BondedTest, IfuncBonds)
+//! Function types for testing angles. Add new terms at the end.
+std::vector<iListInput> c_InputAngles =
 {
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 0, 1, 2, 0, 2, 3 };
-    t_iparams            iparams;
-    iparams.harmonic.rA  = iparams.harmonic.rB  = 0.8;
-    iparams.harmonic.krA = iparams.harmonic.krB = 50.0;
-    const real lambda = 0.0;
-    testIfunc(&checker_, F_BONDS, iatoms, iparams, lambda);
-}
-
-TEST_P (BondedTest, IfuncAngles)
+    { F_ANGLES, ftol, dtol, false,
+      { .harmonic.rA = 100.0, .harmonic.krA = 50.0, .harmonic.rB = 100.0, .harmonic.krB = 50.0 } },
+    { F_ANGLES, ftol, dtol, true,
+      { .harmonic.rA = 100.0, .harmonic.krA = 50.0, .harmonic.rB = 95.0, .harmonic.krB = 30.0 } },
+    { F_G96ANGLES, ftol, dtol, false,
+      { .harmonic.rA = 100.0, .harmonic.krA = 50.0, .harmonic.rB = 100.0, .harmonic.krB = 50.0 } },
+    { F_G96ANGLES, ftol, dtol, true,
+      { .harmonic.rA = 100.0, .harmonic.krA = 50.0, .harmonic.rB = 95.0, .harmonic.krB = 30.0 } },
+    { F_LINEAR_ANGLES, ftol, dtol, true,
+      { .linangle.klinA = 50.0, .linangle.aA = 0.4, .linangle.klinB = 40.0, .linangle.aB = 0.6 } },
+    { F_LINEAR_ANGLES, ftol, dtol, false,
+      { .linangle.klinA = 50.0, .linangle.aA = 0.5, .linangle.klinB = 50.0, .linangle.aB = 0.5 } },
+    { F_CROSS_BOND_BONDS, ftol, dtol, false,
+      { .cross_bb.r1e = 0.8, .cross_bb.r2e = 0.7, .cross_bb.krr = 45.0 } },
+    { F_CROSS_BOND_ANGLES, ftol, dtol, false,
+      { .cross_bb.r1e = 0.8, .cross_bb.r2e = 0.7, .cross_bb.krr = 45.0 } },
+    { F_UREY_BRADLEY, ftol, dtol, true,
+      { .u_b.thetaA = 100.0, .u_b.kthetaA = 45.0, .u_b.r13A = 0.3,  .u_b.kUBA = 5.0,
+        .u_b.thetaB =  90.0, .u_b.kthetaB = 47.0, .u_b.r13B = 0.32, .u_b.kUBB = 7.0 } },
+    { F_UREY_BRADLEY, ftol, dtol, false,
+      { .u_b.thetaA =  90.0, .u_b.kthetaA = 47.0, .u_b.r13A = 0.31, .u_b.kUBA = 6.0,
+        .u_b.thetaB =  90.0, .u_b.kthetaB = 47.0, .u_b.r13B = 0.31, .u_b.kUBB = 6.0 } },
+    { F_QUARTIC_ANGLES, ftol, dtol, false,
+      { .qangle.theta = 87.0, .qangle.c = { 1.1, 2.2, 3.3, 4.5, 6.7 } } }
+};
+  
+//! Function types for testing dihedrals. Add new terms at the end.
+std::vector<iListInput> c_InputDihs =
 {
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 2, 0, 1, 2, 3 };
-    t_iparams            iparams;
-    real                 k = 50.0;
-    iparams.harmonic.rA  = iparams.harmonic.rB  = 100.0;
-    iparams.harmonic.krA = iparams.harmonic.krB = k;
-    const real lambda = 0.0;
-    testIfunc(&checker_, F_ANGLES, iatoms, iparams, lambda);
-}
-
-TEST_P (BondedTest, IfuncProperDihedrals)
-{
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 2, 3 };
-    t_iparams            iparams;
-    iparams.pdihs.phiA = iparams.pdihs.phiB = -100.0;
-    iparams.pdihs.cpA  = iparams.pdihs.cpB  = 10.0;
-    iparams.pdihs.mult = 1;
-    const real lambda = 0.0;
-    testIfunc(&checker_, F_PDIHS, iatoms, iparams, lambda);
-}
-
-TEST_P (BondedTest, IfuncImproperDihedrals)
-{
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 2, 3 };
-    t_iparams            iparams;
-    iparams.harmonic.rA  = iparams.harmonic.rB  = 0.0;
-    iparams.harmonic.krA = iparams.harmonic.krB = 5.0;
-    const real lambda = 0.0;
-    testIfunc(&checker_, F_IDIHS, iatoms, iparams, lambda);
-}
-
-TEST_P (BondedTest, IfuncImproperDihedralsFEP)
-{
-    std::vector<t_iatom> iatoms = { 0, 0, 1, 2, 3 };
-    t_iparams            iparams;
-    iparams.harmonic.rA  = iparams.harmonic.rB  = 0.0;
-    iparams.harmonic.krA = iparams.harmonic.krB = 5.0;
-    iparams.harmonic.rB  = 35.5;
-    iparams.harmonic.krB = 10.0;
-
-    const int numLambdas = 3;
-    for (int i = 0; i < numLambdas; ++i)
-    {
-        const real lambda       = i / (numLambdas - 1.0);
-        auto       valueChecker = checker_.checkCompound("Lambda", toString(lambda));
-        testIfunc(&valueChecker, F_IDIHS, iatoms, iparams, lambda);
-    }
-}
-
+    { F_PDIHS, ftol, dtol, true,
+      { .pdihs.phiA = -100.0, .pdihs.cpA = 10, .pdihs.mult = 2,
+        .pdihs.phiB =  -80.0, .pdihs.cpB = 20 } },
+    { F_PDIHS, ftol, dtol, false,
+      { .pdihs.phiA = -90.0, .pdihs.cpA = 30, .pdihs.mult = 2,
+        .pdihs.phiB = -90.0, .pdihs.cpB = 30 } },
+    { F_IDIHS, ftol, dtol, true,
+      { .harmonic.rA =  0.0, .harmonic.krA = 5.0, .harmonic.rB = 35.5, .harmonic.krB = 10.0 } },
+    { F_IDIHS, ftol, dtol, false,
+      { .harmonic.rA = 10.0, .harmonic.krA = 7.0, .harmonic.rB = 10.0, .harmonic.krB =  7.0 } },
+    { F_RBDIHS, ftol, dtol, true,
+      { .rbdihs.rbcA = { -5.35, 13.6, 8.4, -16.7, 0.3, 12.4 },
+        .rbdihs.rbcB = { -6.35, 12.6, 8.1, -10.7, 0.9, 15.4 } } },
+    { F_RBDIHS, ftol, dtol, false,
+      { .rbdihs.rbcA = { -7.35, 13.6, 8.4, -16.7, 1.3, 12.4 },
+        .rbdihs.rbcB = { -7.35, 13.6, 8.4, -16.7, 1.3, 12.4 } } },
+    { F_CBTDIHS, ftol, dtol, true,
+      { .cbtdihs.cbtcA = { -5.35, 13.6, 8.4, -16.7, 0.3, 12.4 },
+        .cbtdihs.cbtcB = { -6.35, 12.6, 8.1, -10.7, 0.9, 15.4 } } },
+    { F_CBTDIHS, ftol, dtol, false,
+      { .cbtdihs.cbtcA = { -7.35, 13.6, 8.4, -16.7, 1.3, 12.4 },
+        .cbtdihs.cbtcB = { -7.35, 13.6, 8.4, -16.7, 1.3, 12.4 } } },
+};
+ 
 //! Coordinates for testing
 std::vector<std::vector<gmx::RVec> > c_coordinatesForTests =
 {
-    {{  0.0, 0.0, 0.0 }, { 0.0, 0.0, 1.0 }, { 0.0, 1.0, 1.0 }, { 1.0, 1.0, 1.0 }},
+    {{  0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.2 }, { 0.005, 0.0, 0.1 }, { -0.001, 0.1, 0.0 }},
     {{  0.5, 0.0, 0.0 }, { 0.5, 0.0, 0.15 }, { 0.5, 0.07, 0.22 }, { 0.5, 0.18, 0.22 }},
     {{ -0.1143, -0.0282, 0.0 }, { 0.0, 0.0434, 0.0 }, { 0.1185, -0.0138, 0.0 }, { -0.0195, 0.1498, 0.0 }}
 };
@@ -262,7 +308,12 @@ std::vector<std::vector<gmx::RVec> > c_coordinatesForTests =
 //! PBC values for testing
 std::vector<int> c_pbcForTests = { epbcNONE, epbcXY, epbcXYZ };
 
-INSTANTIATE_TEST_CASE_P(ForPbcValues, BondedTest, ::testing::Combine(::testing::ValuesIn(c_coordinatesForTests), ::testing::ValuesIn(c_pbcForTests)));
+INSTANTIATE_TEST_CASE_P(Bond, ListedForcesTest, ::testing::Combine(::testing::ValuesIn(c_InputBonds), ::testing::ValuesIn(c_coordinatesForTests), ::testing::ValuesIn(c_pbcForTests)));
+
+INSTANTIATE_TEST_CASE_P(Angle, ListedForcesTest, ::testing::Combine(::testing::ValuesIn(c_InputAngles), ::testing::ValuesIn(c_coordinatesForTests), ::testing::ValuesIn(c_pbcForTests)));
+
+INSTANTIATE_TEST_CASE_P(Dihedral, ListedForcesTest, ::testing::Combine(::testing::ValuesIn(c_InputDihs), ::testing::ValuesIn(c_coordinatesForTests), ::testing::ValuesIn(c_pbcForTests)));
+
 }  // namespace
 
 }  // namespace gmx
