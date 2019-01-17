982e6fae0e11fc83d7a455118d4d3e04e65ee761
Kevin Boyd <kevin.boyd@uconn.edu>
2019-01-16 19:30:15 -0500

    Move non-analysis tools from gmxana to gmxpreprocess
    Renamed genpr to genrestr to align with actual gmx <tool> call
    
    refs #2727
    
    Change-Id: I83a1dc6b39915372c4178768354d334c7e911ae2

--- src/gromacs/gmxpreprocess/genion.cpp
+++ src/gromacs/gmxpreprocess/genion.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -36,6 +36,8 @@
  */
 #include "gmxpre.h"
 
+#include "genion.h"
+
 #include <cctype>
 #include <cmath>
 #include <cstdlib>
@@ -43,7 +45,6 @@
 
 #include "gromacs/commandline/pargs.h"
 #include "gromacs/fileio/confio.h"
-#include "gromacs/gmxana/gmx_ana.h"
 #include "gromacs/math/units.h"
 #include "gromacs/math/utilities.h"
 #include "gromacs/math/vec.h"
--- src/gromacs/gmxpreprocess/genrestr.cpp
+++ src/gromacs/gmxpreprocess/genrestr.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -36,6 +36,8 @@
  */
 #include "gmxpre.h"
 
+#include "genrestr.h"
+
 #include <cmath>
 #include <cstring>
 
@@ -43,7 +45,6 @@
 
 #include "gromacs/commandline/pargs.h"
 #include "gromacs/fileio/confio.h"
-#include "gromacs/gmxana/gmx_ana.h"
 #include "gromacs/math/vec.h"
 #include "gromacs/topology/index.h"
 #include "gromacs/topology/topology.h"
@@ -53,7 +54,7 @@
 #include "gromacs/utility/futil.h"
 #include "gromacs/utility/smalloc.h"
 
-int gmx_genpr(int argc, char *argv[])
+int gmx_genrestr(int argc, char *argv[])
 {
     const char        *desc[] = {
         "[THISMODULE] produces an #include file for a topology containing",
