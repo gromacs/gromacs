2bb15b5bb00b8781becfe69b8892747b9ebf1e4e
Christian Blau <cblau@gwdg.de>
2019-02-04 14:26:39 +0100

    Modernise rmsd and rho evaluation routine.
    Removes conditionals from rmsd / rho-factor calculation routines.
    Template for calculation method.
    
    Index as default nullptr parameter removes need for _ind functions.
    
    Should speed up rmsd and rho calculations.
    
    Change-Id: I7ff82415ea243301c1cc43832cb6989647040a8c

--- src/gromacs/math/tests/dofit.cpp
+++ src/gromacs/math/tests/dofit.cpp
@@ -55,6 +55,9 @@ namespace
 
 using gmx::RVec;
 using gmx::test::defaultRealTolerance;
+using gmx::findStructureSimilarity;
+using gmx::RMSD;
+using gmx::RhoMeasure;
 class StructureSimilarityTest : public ::testing::Test
 {
     protected:
@@ -70,32 +73,32 @@ class StructureSimilarityTest : public ::testing::Test
 
 TEST_F(StructureSimilarityTest, StructureComparedToSelfHasZeroRMSD)
 {
-    EXPECT_REAL_EQ_TOL(0., rmsdev(c_nAtoms, m_, x1_, x1_), defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(0., findStructureSimilarity<RMSD>(c_nAtoms, m_, x1_, x1_), defaultRealTolerance());
 }
 
 TEST_F(StructureSimilarityTest, StructureComparedToSelfHasZeroRho)
 {
-    EXPECT_REAL_EQ_TOL(0., rhodev(c_nAtoms, m_, x1_, x1_), defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(0., findStructureSimilarity<RhoMeasure>(c_nAtoms, m_, x1_, x1_), defaultRealTolerance());
 }
 
 TEST_F(StructureSimilarityTest, YieldsCorrectRMSD)
 {
-    EXPECT_REAL_EQ_TOL(sqrt(2.0), rmsdev(c_nAtoms, m_, x1_, x2_), defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(sqrt(2.0), findStructureSimilarity<RMSD>(c_nAtoms, m_, x1_, x2_), defaultRealTolerance());
 }
 
 TEST_F(StructureSimilarityTest, YieldsCorrectRho)
 {
-    EXPECT_REAL_EQ_TOL(2., rhodev(c_nAtoms, m_, x1_, x2_), defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(2., findStructureSimilarity<RhoMeasure>(c_nAtoms, m_, x1_, x2_), defaultRealTolerance());
 }
 
 TEST_F(StructureSimilarityTest, YieldsCorrectRMSDWithIndex)
 {
-    EXPECT_REAL_EQ_TOL(sqrt(2.0), rmsdev_ind(index_.size(), index_.data(), m_, x1_, x2_), defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(sqrt(2.0), findStructureSimilarity<RMSD>(index_.size(), m_, x1_, x2_, index_.data()), defaultRealTolerance());
 }
 
 TEST_F(StructureSimilarityTest, YieldsCorrectRhoWidthIndex)
 {
-    EXPECT_REAL_EQ_TOL(2., rhodev_ind(index_.size(), index_.data(), m_, x1_, x2_), defaultRealTolerance());
+    EXPECT_REAL_EQ_TOL(2., findStructureSimilarity<RhoMeasure>(index_.size(), m_, x1_, x2_, index_.data()), defaultRealTolerance());
 }
 
 } // namespace
