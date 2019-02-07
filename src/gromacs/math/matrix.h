41c8b5ced2ed4c66d4709b1b091cc7eedfc7809f
Christian Blau <cblau@gwdg.de>
2019-02-07 14:19:53 +0100

    [RFC] upper triangle constraint 3x3 matrices view
    Implement constraint view on matrices where the upper triangle is
    enforced to be zero. Accessing matrix elements in the upper triangle
    will return a reference to a zero'd variable.
    
    Instead of a full constraint matrix class similar to the Matrix3x3, I
    have the impression that it is usually the view on the Matrix that we
    would like to employ. For that the constraint matrix is much easier to
    implement, as done in this patch.
    
    Refs #2835
    
    Change-Id: Ia8369d2d2a61fd2a79017d16759e0bd1499c2fa1

--- src/gromacs/math/matrix.h
+++ src/gromacs/math/matrix.h
@@ -45,6 +45,7 @@
 
 #include <array>
 
+#include "gromacs/math/vectypes.h"
 #include "gromacs/utility/real.h"
 
 #include "multidimarray.h"
@@ -62,6 +63,66 @@ using BasicMatrix3x3 = MultiDimArray<std::array<ElementType, 3*3>, extents <3, 3
  * \note will replace the C-style real[3][3] "matrix"
  */
 using Matrix3x3 = BasicMatrix3x3<real>;
+
+/*! \libinternal 
+ * \brief Constraint view on matrices where the upper triangle is enforced zero.
+ * \tparam ElementType type of element to be viewed, may be const.
+ */
+template <class ElementType>
+class Constraint3x3View : public BasicMatrix3x3<ElementType>::view_type
+{
+    public:
+        //! Type of base class
+        using base_type = typename BasicMatrix3x3<ElementType>::view_type;
+        //! Reference type
+        using reference = typename BasicMatrix3x3<ElementType>::reference;
+        //! Const reference type
+        using const_reference = typename BasicMatrix3x3<ElementType>::const_reference;
+        //! Constructing the constraint view by setting the base class view
+        Constraint3x3View(const base_type &view) : base_type {view}
+        { }
+        /*! \brief Element access operator.
+         * Operator is modified so that accessing an element in the upper triangle
+         * yields access to a reference containing zero.
+         *
+         * \tparam IndexType type of access index, templating allows access with
+         *                   size_t and int
+         * \returns Reference to matrix element or zero element.
+         */
+        template<class IndexType>
+        constexpr reference operator()(IndexType i, IndexType j)
+        {
+            zeroElement = 0;
+            return i > j ? zeroElement : base_type::operator()(i, j);
+        }
+        /*! \brief Const element access operator.
+         * Operator is modified so that accessing an element in the upper triangle
+         * yields access to a reference containing zero.
+         *
+         * \tparam IndexType type of access index, templating allows access with
+         *                   size_t and int
+         * \param[in] i first index to be accessed
+         * \param[in] j second index to be accessed
+         * \returns Const reference to matrix element or zero element.
+         */
+        template<class IndexType>
+        constexpr const_reference operator()(IndexType i, IndexType j) const
+        {
+            zeroElement = 0;
+            return i > j ? zeroElement : base_type::operator()(i, j);
+        }
+        /*! \brief Bracket operator for compability reasons returns an RVec.
+         * \param[in] i column to be indexed
+         * \returns vector with column elements
+         */
+        constexpr BasicVector<ElementType> operator[](int i) const
+        {
+            return {(*this)(i, 0), (*this)(i, 1), (*this)(i, 2)};
+        }
+
+    private:
+        ElementType zeroElement = 0;
+};
 
 } // namespace gmx
 
