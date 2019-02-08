4c5636dd28c28d2a8a092305cf183f136d2f1d72
Berk Hess <hess@kth.se>
2019-02-08 10:57:48 +0100

    Disallow rerun with perturbed masses or constraints
    Since rerun no longer integrates, free-energy contributions due to
    changing masses and constraints are no longer computed. Now rerun
    exits with a fatal error when such a system is provided.
    
    Fixes #2849
    
    Change-Id: Ic1550fce22f68c573b52d6895e1e61398b671e51

--- src/gromacs/mdlib/constr.h
+++ src/gromacs/mdlib/constr.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -119,6 +119,9 @@ class Constraints
 
         /*! \brief Returns the total number of flexible constraints in the system. */
         int numFlexibleConstraints() const;
+
+        /*! \brief Returns whether the system contains perturbed constraints */
+        bool havePerturbedConstraints() const;
 
         /*! \brief Set up all the local constraints for the domain.
          *
