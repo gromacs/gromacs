3d903435101a1207e8f17759e692f2819acfe841
Szilárd Páll <pall.szilard@gmail.com>
2019-02-12 18:38:43 +0100

    Clarify force buffer setup code in do_force
    Refactored code and made conditionals non-nested to improve
    the ease of understanding when is a common or separate buffer used for
    the forces when direct virial contribution is computed.
    
    Also add subcounter for force buffer clearing which also helps annotate
    code that should be conditional on whether any of these buffers are used
    to accumulate or only to copy into (e.g. with everything offloaded to a
    GPU).
    
    Refs #2802
    
    Change-Id: I3fa5a3e4e4adf5cfe0eb417f0c1c3d0ed4a96769

--- src/gromacs/timing/wallcycle.h
+++ src/gromacs/timing/wallcycle.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2008, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -79,6 +79,8 @@ enum {
     ewcsEWALD_CORRECTION,
     ewcsNB_X_BUF_OPS,
     ewcsNB_F_BUF_OPS,
+    ewcsCLEAR_FORCE_BUFFER,
+    ewcsTEST,
     ewcsNR
 };
 
