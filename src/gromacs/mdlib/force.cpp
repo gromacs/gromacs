23a3144bdd2a3844110a345dff9dd967a03642a2
Berk Hess <hess@kth.se>
2019-02-13 09:55:35 +0100

    Fix mass Delta H double counting
    With separate mass lambda parameters, the perturbed mass contribution
    to Delta H (not dH/dlambda) was double counted. This was due to an
    incorrect fix for issue #2703 for missing Delta H contribution.
    
    Refs #2703
    Fixes #2849
    
    Change-Id: I1349260e1e90a17a6b7dbe0d239d9474dcfa928c

--- src/gromacs/mdlib/force.cpp
+++ src/gromacs/mdlib/force.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -784,7 +784,7 @@ void sum_dhdl(gmx_enerdata_t *enerd, gmx::ArrayRef<const real> lambda, t_lambda 
                 enerpart_lambda += dlam*enerd->term[F_DVDL_CONSTR];
             }
 
-            if (j == efptMASS)
+            if (j == efptMASS && !fepvals->separate_dvdl[j])
             {
                 enerpart_lambda += dlam*enerd->term[F_DKDL];
             }
