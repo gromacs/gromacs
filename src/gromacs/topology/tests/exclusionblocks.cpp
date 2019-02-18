6120eaf037b0f773b53b1ed28ba959d1b5b3f1a4
Paul Bauer <paul.bauer.q@gmail.com>
2019-02-18 15:30:16 +0100

    Add tests for ExclusionBlocks
    Change-Id: I35d2e38e1ae29576f559bf2a2307f3ffb2d28cbe

---
+++ src/gromacs/topology/tests/exclusionblocks.cpp
@@ -0,0 +1,153 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2019, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Implements test of exclusionblock routines
+ *
+ * \author Paul Bauer <paul.bauer.q@gmail.com>
+ * \ingroup module_topology
+ */
+#include "gmxpre.h"
+
+#include "gromacs/topology/exclusionblocks.h"
+
+#include <gtest/gtest.h>
+
+#include "gromacs/topology/block.h"
+#include "gromacs/utility/arrayref.h"
+#include "gromacs/utility/smalloc.h"
+
+namespace gmx
+{
+namespace
+{
+
+static void hackBlocka(t_blocka *b, gmx::ArrayRef<const int> index)
+{
+    srenew(b->index, b->nr+2);
+    srenew(b->a, b->nra+index.size());
+    for (int i = 0; (i < index.ssize()); i++)
+    {
+        b->a[b->nra++] = index[i];
+    }
+    b->nr++;
+    b->index[b->nr] = b->nra;
+}
+
+static void fillBlocka(t_blocka *b)
+{
+    init_blocka(b);
+    // Prepare a dummy group
+    std::vector<int> index = {0, 4, 7};
+    // Add the group to the index
+    hackBlocka(b, index);
+    index = {1, 5, 8, 10};
+    hackBlocka(b, index);
+    index = {2, 6, 9, 11, 12};
+    hackBlocka(b, index);
+}
+
+static void initialize(ExclusionBlocks *b, int natom)
+{
+    initExclusionBlocks(b, natom);
+    EXPECT_EQ(b->nr, natom);
+    ASSERT_NE(b->nra, nullptr);
+    ASSERT_NE(b->a, nullptr);
+    for (int i = 0; i < natom; i++)
+    {
+        EXPECT_EQ(b->a[i], nullptr);
+    }
+}
+
+TEST(ExclusionBlockTest, EmptyOnInit)
+{
+    ExclusionBlocks b;
+    int             natom  = 3;
+    initialize(&b, natom);
+}
+
+TEST(ExclusionBlockTest, ConvertBlockAToExclusionBlocks)
+{
+    t_blocka ba;
+    fillBlocka(&ba);
+
+    ExclusionBlocks b;
+    int             natom = 3;
+    initialize(&b, natom);
+
+    ASSERT_EQ(b.nr, ba.nr);
+
+    blockaToExclusionBlocks(&ba, &b);
+
+    for (int i = 0; i < natom; i++)
+    {
+        int index  = ba.index[i];
+        for (int j = 0; j < b.nra[i]; j++)
+        {
+            int pos = index + j;
+            EXPECT_EQ(b.a[i][j], ba.a[pos]);
+        }
+    }
+}
+
+TEST(ExclusionBlocks, MergeExclusions)
+{
+    t_blocka ba;
+    fillBlocka(&ba);
+
+    ExclusionBlocks b;
+    int             natom = 3;
+    initialize(&b, natom);
+
+    ASSERT_EQ(b.nr, ba.nr);
+
+    blockaToExclusionBlocks(&ba, &b);
+
+    mergeExclusions(&ba, &b);
+
+    for (int i = 0; i < natom; i++)
+    {
+        int index  = ba.index[i];
+        for (int j = 0; j < b.nra[i]; j++)
+        {
+            int pos = index + j;
+            EXPECT_EQ(b.a[i][j], ba.a[pos]);
+        }
+    }
+}
+
+}  // namespace
+
+}  // namespace gmx
