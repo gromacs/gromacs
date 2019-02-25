98f5086bb55216824c63513f17d9dc42f10747ec
Szilárd Páll <pall.szilard@gmail.com>
2019-02-25 16:29:57 +0100

    Better document GMX_OPENMP_MAX_THREADS
    Change-Id: I2c44f8b2331d69fde5fa242b5e53132813a2cb31

--- src/gromacs/listed-forces/manage-threading.cpp
+++ src/gromacs/listed-forces/manage-threading.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -346,7 +346,7 @@ calc_bonded_reduction_mask(int                       natoms,
     if (bondedThreading.nthreads > BITMASK_SIZE)
     {
 #pragma omp master
-        gmx_fatal(FARGS, "You are using %d OpenMP threads, which is larger than GMX_OPENMP_MAX_THREADS (%d). Decrease the number of OpenMP threads or rebuild GROMACS with a larger value for GMX_OPENMP_MAX_THREADS.",
+        gmx_fatal(FARGS, "You are using %d OpenMP threads, which is larger than GMX_OPENMP_MAX_THREADS (%d). Decrease the number of OpenMP threads or rebuild GROMACS with a larger value for GMX_OPENMP_MAX_THREADS passed to CMake.",
                   bondedThreading.nthreads, GMX_OPENMP_MAX_THREADS);
 #pragma omp barrier
     }
