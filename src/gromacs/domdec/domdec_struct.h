21c0653b0beae5d1e77348c95d84f0d588ec8e9c
Alan Gray <alang@nvidia.com>
2019-02-26 04:43:22 -0800

    GPU halo exchange for position buffer
    This patch has two functions: an initialization fn to create a
    structure containing information required to perform the exchange
    (including a map of indices involved in the exchange), and
    the exchange itself. Init fn is activated with SCHEDULEPUREMULTIGPU
    environment variable, pending implementation of proper scheduling
    mechanism. Exchange fn is not yet called, awaiting availability of
    GPU position buffer ops.
    
    TODO: Currently requires CUDA-aware MPI. Future work to implement
    fall-back when using threadMPI or non CUDA-aware MPI.
    
    Implements part of #2890
    
    Change-Id: I8e6473481ad4d943df78d7019681bfa821bd5798

--- src/gromacs/domdec/domdec_struct.h
+++ src/gromacs/domdec/domdec_struct.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -123,6 +123,22 @@ struct gmx_ddbox_t {
     rvec normal[DIM];
 };
 
+/* domdec_gpu host-side structure */
+typedef struct
+{
+    int   *h_indexMap     = nullptr; //map of indices to be sent from this rank
+    int   *d_indexMap     = nullptr; //device copy of above
+    int    indexMapSize   = 0;       //size of index map
+    float *d_sendBuf      = nullptr; //device buffer for sending packed data
+    int    sendRank       = 0;       //rank to send data to
+    int    recvRank       = 0;       //rank to recv data from
+    int    copySizeSend   = 0;       //send copy size from this rank
+    int    copySizeRecv   = 0;       //recv copy size to this rank
+    int    recvOffset     = 0;       //offset for data recieved by this rank
+    bool   usePBC         = false;   //Periodic Boundary Conditions for this rank
+    float  h_xshift[3];              //X shift values
+    float* d_xshift;                 //device copy of above
+} ddGpu_t;
 
 struct gmx_domdec_t { //NOLINT(clang-analyzer-optin.performance.Padding)
     /* The DD particle-particle nodes only */
@@ -222,6 +238,9 @@ struct gmx_domdec_t { //NOLINT(clang-analyzer-optin.performance.Padding)
     /* gmx_pme_recv_f buffer */
     int   pme_recv_f_alloc = 0;
     rvec *pme_recv_f_buf   = nullptr;
+
+    /* info required for GPU halo exchange comms */
+    ddGpu_t ddGpu;
 };
 
 //! Are we the master node for domain decomposition
