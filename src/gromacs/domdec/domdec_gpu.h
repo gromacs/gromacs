6f04b749c830b98993e226204acb110211f723bc
Alan Gray <alang@nvidia.com>
2019-02-26 04:43:22 -0800

    GPU halo exchange for position buffer
    This patch has two functions: an initialization fn to create a
    structure containing information required to perform the exchange
    (including a map of indices involved in the exchange), and
    the exchange itself. Init fn is activated with SCHEDULEPUREMULTIGPU
    environment valiable, pending implementation of proper scheduling
    mechanism. Exchange fn is not yet called, awaiting availability of
    GPU position buffer ops.
    
    Implements part of #2816
    
    Change-Id: I8e6473481ad4d943df78d7019681bfa821bd5798

---
+++ src/gromacs/domdec/domdec_gpu.h
@@ -0,0 +1,67 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2017,2018,2019, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \libinternal \file
+ * \brief Declares functions for GPU halo exchange.
+ *
+ * \author Alan Gray <alang@nvidia.com>
+ * \inlibraryapi
+ * \ingroup module_domdec
+ */
+
+#ifndef GMX_DDGPU_H
+#define GMX_DDGPU_H
+
+#include "gromacs/domdec/domdec_internal.h"
+#include "gromacs/gpu_utils/gpu_macros.h"
+
+/*! \brief
+ * Initialization for GPU halo exchange of position buffer
+ * \param [inout] dd      domdec structure, to be populated with index and param info for exchange
+ * \param [in] box        box matrix required for shift information
+ */
+CUDA_FUNC_QUALIFIER
+void dd_init_move_x_gpu(gmx_domdec_t gmx_unused *dd, matrix gmx_unused box) CUDA_FUNC_TERM
+
+
+/*! \brief
+ * GPU halo exchange of position buffer
+ * \param [in] dd           domdec structure, ocntaining index and param info for exchange
+ * \param [inout] x_d_ptr   pointer to position buffer in GPU memory
+ * \param [in] stream       CUDA stream to be used for buffer packing operation
+ */
+CUDA_FUNC_QUALIFIER
+void dd_move_x_gpu(gmx_domdec_t gmx_unused *dd, rvec gmx_unused *x_d_ptr, void gmx_unused *stream) CUDA_FUNC_TERM
+
+#endif
