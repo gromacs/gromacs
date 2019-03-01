898a5a1de6e086c5752732a78ce22e0c20d7140c
Thomas Ullmann <thomas.ullmann@mpibpc.mpg.de>
2019-03-01 03:06:21 +0100

    array classes for lambda-dynamics
    Function
    ========
    
    The IrregArray classes can have nonuniform lengths of array stripes
    in higher dimensions (see IrregArray2D.h for an illustration). They
    are allocator-aware for general usability on different devices,
    with different alignment requirements etc. They include a variety of
    convenience constructors and public typedefs for easy use in code
    employing them (see unit test). An earlier, simpler version of these
    arrays has already been in use for years in the Monte Carlo simulation
    software GMCT.
    
    Purpose
    =======
    
    For the lambda-dynamics/FMM method a new topology with local alternatives
    will be needed (e.g., protein sidechain protonation states, or at the
    extreme drug molecules with different substituents or entirely different
    structures). This requirement will force us to rewrite at least parts
    of the code for topology reading and generation. This more general
    topology will be needed for purposes other than the lambda-dynamics/FMM
    too, especially in general free energy calculations with N end states.
    There are already some Redmine issues on the topic (refs #1653, #1652 and #1332)
    
    Details
    =======
    
    We plan to use the IrregArrayNDs for storing properties of different
    site forms. As an concrete example, they are needed to store energy terms
    for the different states or forms of variables sites, because different
    sites may have very different numbers of forms. For example, an ASP sidechain
    my be modeled with 5 protonation forms, while a drug molecule bound nearby
    may possess dozens of variants (and possibly many more) with different
    substitution patterns, different protonation etc. Simply allocating the
    maximum number of array elements for each site will lead to greatly exaggerated
    memory demands, which was the initial motivation for developing these array
    classes. The performance of the array classes in reading and writing is very
    close to that of bare arrays (see the performance unit tests), but if required
    one can also work directly with the underlying data array. The Prefix "Flat"
    signifies that these variants internally use a flat 1D-array, while the classes
    without this prefix use multidimensional data arrays. Surprisingly, in the
    tests ran so far, the Flat variants are slower than the multidimensional ones.
    However, since the relative performance may depend on the precise context, it
    may still be good to have the choice. See the unit test in
    src/gromacs/utility/data_structures/tests/irreg_array.cpp and
    src/gromacs/utility/data_structures/tests/irreg_array_performance.cpp
    for usage examples and performance tests.
    
    Related to #2791.
    
    Change-Id: Ic80a3e9de7c54440a2734a6d798479c452383e5e

---
+++ src/gromacs/utility/data_structures/tests/data_struct_test_commons.cpp
@@ -0,0 +1,93 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2019, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+   \brief
+   implementation file for data_struct_test_commons.h
+
+   \author R. Thomas Ullmann <thomas.ullmann@mpibpc.mpg.de>
+
+   \ingroup module_utility
+ */
+#include "gmxpre.h"
+
+#include "data_struct_test_commons.h"
+
+#include "gromacs/options/basicoptions.h"
+#include "gromacs/options/options.h"
+
+#include "testutils/testoptions.h"
+
+namespace gmx
+{
+
+namespace data_struct_test
+{
+
+
+// can be set to via commandline option flag -[no]debug
+//! \cond
+// flag that enables (true) or disables (false) debug output
+bool        g_debugBool = false;
+// flag that enables (true) or disables (false) error output
+bool        g_errorBool = true;
+// verbosity level
+int         g_blabLevel = 0;
+
+// file for redirecting error output
+std::string g_errFileName;
+// file for redirecting other output
+std::string g_outFileName;
+
+GMX_TEST_OPTIONS(MyTestOptions, options)
+{
+    options->addOption(gmx::BooleanOption("debug").store(&g_debugBool)
+                           .description("Print additional debug output if available."));
+    options->addOption(gmx::BooleanOption("error").store(&g_errorBool)
+                           .description("Print error output."));
+#ifdef GMX_LAMBDA_SITE_IS_DEVEL
+    options->addOption(gmx::IntegerOption("blab").store(&g_blabLevel)
+                           .description("Print additional debug output if available."));
+    options->addOption(gmx::StringOption("errfile").store(&g_errFileName)
+                           .description("Redirect error output to a file."));
+    options->addOption(gmx::StringOption("outfile").store(&g_outFileName)
+                           .description("Redirect regular output to a file."));
+#endif
+}
+//! \endcond
+
+
+} // namespace data_struct_test
+
+} // namespace gmx
