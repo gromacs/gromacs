929343d11b53d1351c7090170d6cdda931eddce8
M. Eric Irrgang <ericirrgang@gmail.com>
2019-04-02 18:57:39 +0300

    More Python testing infrastructure for gmxapi.
    * Add and improve test fixtures.
    * Generate MD input files for tests.
    
    Refs: #2756
    Refs: #2896
    
    Change-Id: I15ac8e44844cbf699cfe362e2fa443d07a355b3d

--- python_packaging/src/test/conftest.py
+++ python_packaging/src/test/conftest.py
@@ -34,19 +34,36 @@
 
 """Configuration and fixtures for pytest."""
 
-import pytest
-import tempfile
+import json
+import logging
 import os
+import shutil
+import tempfile
+
+import pytest
+
 
 
 @pytest.fixture()
 def cleandir():
-    """Provide a clean working directory for tests.
+    """Provide a clean temporary working directory for a test.
 
     Example usage:
 
         import os
         import pytest
+
+        @pytest.mark.usefixtures("cleandir")
+        def test_cwd_starts_empty():
+            assert os.listdir(os.getcwd()) == []
+            with open("myfile", "w") as f:
+                f.write("hello")
+
+        def test_cwd_also_starts_empty(cleandir):
+            assert os.listdir(os.getcwd()) == []
+            assert os.path.abspath(os.getcwd()) == os.path.abspath(cleandir)
+            with open("myfile", "w") as f:
+                f.write("hello")
 
         @pytest.mark.usefixtures("cleandir")
         class TestDirectoryInit(object):
@@ -55,10 +72,142 @@ def cleandir():
                 with open("myfile", "w") as f:
                     f.write("hello")
 
-            def test_cwd_again_starts_empty(self):
+            def test_cwd_also_starts_empty(self):
                 assert os.listdir(os.getcwd()) == []
+                with open("myfile", "w") as f:
+                    f.write("hello")
 
     Ref: https://docs.pytest.org/en/latest/fixture.html#using-fixtures-from-classes-modules-or-projects
     """
+    oldpath = os.getcwd()
     newpath = tempfile.mkdtemp()
     os.chdir(newpath)
+    yield newpath
+    os.chdir(oldpath)
+    # TODO: Allow override to prevent removal of the temporary directory
+    shutil.rmtree(newpath)
+
+
+@pytest.fixture(scope='session')
+def gmxcli():
+    # TODO: (#2896) Find a more canonical way to identify the GROMACS commandline wrapper binary.
+    #  We should be able to get the GMXRC contents and related hints from a gmxapi
+    #  package resource or from module attributes of a ``gromacs`` stub package.
+    command = shutil.which('gmx')
+    if command is None:
+        gmxbindir = os.getenv('GMXBIN')
+        if gmxbindir is None:
+            gromacsdir = os.getenv('GROMACS_DIR')
+            if gromacsdir is not None and gromacsdir != '':
+                gmxbindir = os.path.join(gromacsdir, 'bin')
+        if gmxbindir is None:
+            gmxapidir = os.getenv('gmxapi_DIR')
+            if gmxapidir is not None and gmxapidir != '':
+                gmxbindir = os.path.join(gmxapidir, 'bin')
+        if gmxbindir is not None:
+            gmxbindir = os.path.abspath(gmxbindir)
+            command = shutil.which('gmx', path=gmxbindir)
+        else:
+            message = "Tests need 'gmx' command line tool, but could not find it on the path."
+            raise RuntimeError(message)
+    if command is None:
+        message = "Tests need 'gmx' command line tool, but could not find it on the path."
+        raise RuntimeError(message)
+    try:
+        assert os.access(command, os.X_OK)
+    except Exception as E:
+        raise RuntimeError('"{}" is not an executable gmx wrapper program') from E
+    yield command
+
+
+@pytest.fixture(scope='class')
+def spc216(gmxcli):
+    """Provide a TPR input file for a simple simulation.
+
+    Prepare the MD input in a freshly created working directory.
+    """
+    import gmxapi as gmx
+
+    # TODO: (#2896) Fetch MD input from package / library data.
+    # Example:
+    #     import pkg_resources
+    #     # Note: importing pkg_resources means setuptools is required for running this test.
+    #     # Get or build TPR file from data bundled via setup(package_data=...)
+    #     # Ref https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
+    #     from gmx.data import tprfilename
+
+    tempdir = tempfile.mkdtemp()
+
+    testdir = os.path.dirname(__file__)
+    with open(os.path.join(testdir, 'testdata.json'), 'r') as fh:
+        testdata = json.load(fh)
+
+    # TODO: (#2756) Don't rely on so many automagical behaviors (as described in comments below)
+
+    structurefile = os.path.join(tempdir, 'structure.gro')
+    # We let `gmx solvate` use the default solvent. Otherwise, we would do
+    #     gro_input = testdata['solvent_structure']
+    #     with open(structurefile, 'w') as fh:
+    #         fh.write('\n'.join(gro_input))
+    #         fh.write('\n')
+
+    topfile = os.path.join(tempdir, 'topology.top')
+    top_input = testdata['solvent_topology']
+    # `gmx solvate` will append a line to the provided file with the molecule count,
+    # so we strip the last line from the input topology.
+    with open(topfile, 'w') as fh:
+        fh.write('\n'.join(top_input[:-1]))
+        fh.write('\n')
+
+    solvate = gmx.commandline_operation(gmxcli,
+                                        arguments=['solvate', '-box', '5', '5', '5'],
+                                        # We use the default solvent instead of specifying one.
+                                        # input_files={'-cs': structurefile},
+                                        output_files={'-p': topfile,
+                                                      '-o': structurefile,
+                                                      }
+                                        )
+    assert solvate.output.returncode.result() == 0
+
+    mdp_input = [('integrator', 'md'),
+                 ('cutoff-scheme', 'Verlet'),
+                 ('nsteps', 1000),
+                 ('nstxout', 100),
+                 ('nstvout', 100),
+                 ('nstfout', 100),
+                 ('tcoupl', 'v-rescale'),
+                 ('tc-grps', 'System'),
+                 ('tau-t', 1),
+                 ('ref-t', 298)]
+    mdp_input = '\n'.join([' = '.join([str(item) for item in kvpair]) for kvpair in mdp_input])
+    mdpfile = os.path.join(tempdir, 'md.mdp')
+    with open(mdpfile, 'w') as fh:
+        fh.write('\n'.join(mdp_input))
+        fh.write('\n')
+    tprfile = os.path.join(tempdir, 'topol.tpr')
+    # We don't use mdout_mdp, but if we don't specify it to grompp,
+    # it will be created in the current working directory.
+    mdout_mdp = os.path.join(tempdir, 'mdout.mdp')
+
+    grompp = gmx.commandline_operation(gmxcli, 'grompp',
+                                       input_files={
+                                           '-f': mdpfile,
+                                           '-p': solvate.output.file['-p'],
+                                           '-c': solvate.output.file['-o'],
+                                           '-po': mdout_mdp,
+                                       },
+                                       output_files={'-o': tprfile})
+    tprfilename = grompp.output.file['-o'].result()
+    if grompp.output.returncode.result() != 0:
+        logging.debug(grompp.output.erroroutput.result())
+        raise RuntimeError('grompp failed in spc216 testing fixture.')
+
+    # TODO: more inspection of grompp errors...
+    assert os.path.exists(tprfilename)
+    yield tprfilename
+
+    # Clean up.
+    # Note: these lines are not executed (and tempdir is not removed) if there
+    # are exceptions before `yield`
+    # TODO: Allow override to prevent removal of the temporary directory
+    shutil.rmtree(tempdir)
