f9300d3bdd3cc4bce1da90072ddceaa9bbddf4bf
Paul Bauer <paul.bauer.q@gmail.com>
2019-05-21 10:14:36 +0200

    Fix PQR formatting
    Was still broken after the last fix a few months ago.
    
    Fixes #2955
    
    Change-Id: I55b1296918c723654db505d1797114e1358e5aeb

--- src/gromacs/fileio/pdbio.cpp
+++ src/gromacs/fileio/pdbio.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -288,7 +288,7 @@ gmx_fprintf_pqr_atomline(FILE *            fp,
     res_seq_number  = res_seq_number % 10000;
 
     int n = fprintf(fp,
-                    "%s %d %s %s %c %d %8.3f %8.3f %8.3f %6.2f %6.2f\n",
+                    "%-6s%5d %-4.4s%4.4s%c%4d %8.3f %8.3f %8.3f %6.2f %6.2f\n",
                     pdbtp[record],
                     atom_seq_number,
                     atom_name,
