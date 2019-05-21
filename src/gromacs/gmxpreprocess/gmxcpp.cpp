2dc359736e0edf3ed85abb790dee669edeabef90
Paul Bauer <paul.bauer.q@gmail.com>
2019-05-21 10:31:42 +0200

    Add more info for wrong line endings
    Adds  some more info for users that encounter preprocessing errors due
    to wring line endings.
    
    Change-Id: Id76404befdf598438f1b1fa5da98812cc31e3c5f

--- src/gromacs/gmxpreprocess/gmxcpp.cpp
+++ src/gromacs/gmxpreprocess/gmxcpp.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -564,7 +564,7 @@ int cpp_read_line(gmx_cpp_t *handlep, int n, char buf[])
             if (!bEOF)
             {
                 /* Something strange happened, fgets returned NULL,
-                 * but we are not at EOF.
+                 * but we are not at EOF. Maybe wrong line endings?
                  */
                 return eCPP_UNKNOWN;
             }
@@ -716,7 +716,7 @@ char *cpp_error(gmx_cpp_t *handlep, int status)
     const char *ecpp[] = {
         "OK", "File not found", "End of file", "Syntax error", "Interrupted",
         "Invalid file handle",
-        "File not open", "Unknown error", "Error status out of range"
+        "File not open", "Unknown error, perhaps your text file uses wrong line endings?", "Error status out of range"
     };
     gmx_cpp_t   handle = *handlep;
 
