gromacs/gromacs

commit 96d28d6b043c74901adbb4634b2eab6134f01bae

Reorder code before thread-MPI spawns threads
gmx_check_thread_affinity_set was called before thread-MPI threads
were spawned, because originally that was necessary. But the intended
functionality is to provide feedback to users about whether thread
affinity was set by the OpenMP library or something else. Nothing else
uses the thread_affinity value in the meantime. So, it only needs to
be called just before OpenMP is initialized. The reordering makes
clear that some reduction code within this function only needs to run
with real MPI.

check_and_update_hw_opt_1 does modify hw_opt, but none of the relevant
variables were accessed in the preceding code, so the reordering is
safe. It now takes a boolean rather than a handle to t_commrec, which
decreases the code coupling and will shortly be useful. A check for
mdrun -ntmpi -npme has been moved into this routine.

check_and_update_hw_opt_2 only related to the group scheme and can
be removed.

The fatal error for group-scheme .tpr files can safely be moved
later, alongside other code specific to the Verlet scheme.

Reruns can safely clear the velocity vector later.

The log file makes a request to cite GROMACS papers, which is now
implemented in a simple function elsewhere.

Change-Id: I1e5990656c391da81aec215f86b94efc752002d4

src/gromacs/mdrun/runner.cpp
@@ -248,7 +248,7 @@ t_commrec *Mdrunner::spawnThreads(int numThreadsToLaunch) const
 
 #if GMX_THREAD_MPI
     /* now spawn new threads that start mdrunner_start_fn(), while
-       the main thread returns, we set thread affinity later */
+       the main thread returns. Thread affinity is handled later. */
     if (tMPI_Init_fn(TRUE, numThreadsToLaunch, TMPI_AFFINITY_NONE,
                      mdrunner_start_fn, static_cast<const void*>(this)) != TMPI_SUCCESS)
     {
@@ -663,18 +663,8 @@ int Mdrunner::mdrunner()
         }
     }
 
-    if (fplog != nullptr)
-    {
-        /* Print references after all software/hardware printing */
-        please_cite(fplog, "Abraham2015");
-        please_cite(fplog, "Pall2015");
-        please_cite(fplog, "Pronk2013");
-        please_cite(fplog, "Hess2008b");
-        please_cite(fplog, "Spoel2005a");
-        please_cite(fplog, "Lindahl2001a");
-        please_cite(fplog, "Berendsen95a");
-        writeSourceDoi(fplog);
-    }
+    // Print citation requests after all software/hardware printing
+    pleaseCiteGromacs(fplog);
 
     std::unique_ptr<t_state> globalState;
 
@@ -685,47 +675,13 @@ int Mdrunner::mdrunner()
 
         /* Read (nearly) all data required for the simulation */
         read_tpx_state(ftp2fn(efTPR, filenames.size(), filenames.data()), inputrec, globalState.get(), &mtop);
-
-        /* In rerun, set velocities to zero if present */
-        if (doRerun && ((globalState->flags & (1 << estV)) != 0))
-        {
-            // rerun does not use velocities
-            GMX_LOG(mdlog.info).asParagraph().appendText(
-                    "Rerun trajectory contains velocities. Rerun does only evaluate "
-                    "potential energy and forces. The velocities will be ignored.");
-            for (int i = 0; i < globalState->natoms; i++)
-            {
-                clear_rvec(globalState->v[i]);
-            }
-            globalState->flags &= ~(1 << estV);
-        }
-
-        if (inputrec->cutoff_scheme != ecutsVERLET)
-        {
-            gmx_fatal(FARGS, "This group-scheme .tpr file can no longer be run by mdrun. Please update to the Verlet scheme, or use an earlier version of GROMACS if necessary.");
-        }
     }
 
-    /* Check and update the hardware options for internal consistency */
-    check_and_update_hw_opt_1(mdlog, &hw_opt, cr, domdecOptions.numPmeRanks);
-
-    /* Early check for externally set process affinity. */
-    gmx_check_thread_affinity_set(mdlog, cr,
-                                  &hw_opt, hwinfo->nthreads_hw_avail, FALSE);
+    // Check and update the hardware options for internal consistency
+    check_and_update_hw_opt_1(mdlog, &hw_opt, SIMMASTER(cr), domdecOptions.numPmeRanks);
 
     if (GMX_THREAD_MPI && SIMMASTER(cr))
     {
-        if (domdecOptions.numPmeRanks > 0 && hw_opt.nthreads_tmpi <= 0)
-        {
-            gmx_fatal(FARGS, "You need to explicitly specify the number of MPI threads (-ntmpi) when using separate PME ranks");
-        }
-
-        /* Since the master knows the cut-off scheme, update hw_opt for this.
-         * This is done later for normal MPI and also once more with tMPI
-         * for all tMPI ranks.
-         */
-        check_and_update_hw_opt_2(&hw_opt, inputrec->cutoff_scheme);
-
         bool useGpuForNonbonded = false;
         bool useGpuForPme       = false;
         try
@@ -861,6 +817,20 @@ int Mdrunner::mdrunner()
 
     if (SIMMASTER(cr))
     {
+        /* In rerun, set velocities to zero if present */
+        if (doRerun && ((globalState->flags & (1 << estV)) != 0))
+        {
+            // rerun does not use velocities
+            GMX_LOG(mdlog.info).asParagraph().appendText(
+                    "Rerun trajectory contains velocities. Rerun does only evaluate "
+                    "potential energy and forces. The velocities will be ignored.");
+            for (int i = 0; i < globalState->natoms; i++)
+            {
+                clear_rvec(globalState->v[i]);
+            }
+            globalState->flags &= ~(1 << estV);
+        }
+
         /* now make sure the state is initialized and propagated */
         set_state_entries(globalState.get(), inputrec);
     }
@@ -998,6 +968,10 @@ int Mdrunner::mdrunner()
         gmx_bcast(sizeof(box), box, cr);
     }
 
+    if (inputrec->cutoff_scheme != ecutsVERLET)
+    {
+        gmx_fatal(FARGS, "This group-scheme .tpr file can no longer be run by mdrun. Please update to the Verlet scheme, or use an earlier version of GROMACS if necessary.");
+    }
     /* Update rlist and nstlist. */
     if (inputrec->cutoff_scheme == ecutsVERLET)
     {
@@ -1057,9 +1031,13 @@ int Mdrunner::mdrunner()
     fflush(stderr);
 #endif
 
-    /* Check and update hw_opt for the cut-off scheme */
-    check_and_update_hw_opt_2(&hw_opt, inputrec->cutoff_scheme);
-
+    // If mdrun -pin auto honors any affinity setting that already
+    // exists. If so, it is nice to provide feedback about whether
+    // that existing affinity setting was from OpenMP or something
+    // else, so we run this code both before and after we initialize
+    // the OpenMP support.
+    gmx_check_thread_affinity_set(mdlog, cr,
+                                  &hw_opt, hwinfo->nthreads_hw_avail, FALSE);
     /* Check and update the number of OpenMP threads requested */
     checkAndUpdateRequestedNumOpenmpThreads(&hw_opt, *hwinfo, cr, ms, physicalNodeComm.size_,
                                             pmeRunMode, mtop);

src/gromacs/mdrunutility/threadaffinity.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -550,17 +550,10 @@ gmx_check_thread_affinity_set(const gmx::MDLogger &mdlog,
         bAllSet = bAllSet && (CPU_ISSET(i, &mask_current) != 0);
     }
 
-#if GMX_MPI
-    int isInitialized;
-    MPI_Initialized(&isInitialized);
-    /* Before OpenMP initialization, thread-MPI is not yet initialized.
-     * With thread-MPI bAllSet will be the same on all MPI-ranks, but the
-     * MPI_Allreduce then functions as a barrier before setting affinities.
-     */
-    if (isInitialized)
+    // With thread-MPI, bAllSet must already be the same on all ranks.
+#if GMX_LIB_MPI
     {
-        gmx_bool  bAllSet_All;
-
+        bool bAllSet_All;
         MPI_Allreduce(&bAllSet, &bAllSet_All, 1, MPI_C_BOOL, MPI_LAND, MPI_COMM_WORLD);
         bAllSet = bAllSet_All;
     }

src/gromacs/taskassignment/resourcedivision.cpp
@@ -660,7 +660,7 @@ static void print_hw_opt(FILE *fp, const gmx_hw_opt_t *hw_opt)
 
 void check_and_update_hw_opt_1(const gmx::MDLogger &mdlog,
                                gmx_hw_opt_t        *hw_opt,
-                               const t_commrec     *cr,
+                               const bool           isSimulationMasterRank,
                                int                  nPmeRanks)
 {
     /* Currently hw_opt only contains default settings or settings supplied
@@ -698,7 +698,7 @@ void check_and_update_hw_opt_1(const gmx::MDLogger &mdlog,
      * The other threads receive a partially processed hw_opt from the master
      * thread and should not set hw_opt->totNumThreadsIsAuto again.
      */
-    if (!GMX_THREAD_MPI || SIMMASTER(cr))
+    if (!GMX_THREAD_MPI || isSimulationMasterRank)
     {
         /* Check if mdrun is free to choose the total number of threads */
         hw_opt->totNumThreadsIsAuto = (hw_opt->nthreads_omp == 0 && hw_opt->nthreads_omp_pme == 0 && hw_opt->nthreads_tot == 0);
@@ -785,6 +785,11 @@ void check_and_update_hw_opt_1(const gmx::MDLogger &mdlog,
         }
     }
 
+    if (GMX_THREAD_MPI && nPmeRanks > 0 && hw_opt->nthreads_tmpi <= 0)
+    {
+        gmx_fatal(FARGS, "You need to explicitly specify the number of MPI threads (-ntmpi) when using separate PME ranks");
+    }
+
     if (debug)
     {
         print_hw_opt(debug, hw_opt);
@@ -794,23 +799,6 @@ void check_and_update_hw_opt_1(const gmx::MDLogger &mdlog,
      * on. */
     GMX_RELEASE_ASSERT(!(hw_opt->nthreads_omp_pme >= 1 && hw_opt->nthreads_omp <= 0),
                        "PME thread count should only be set when the normal thread count is also set");
-}
-
-void check_and_update_hw_opt_2(gmx_hw_opt_t *hw_opt,
-                               int           cutoff_scheme)
-{
-    if (cutoff_scheme == ecutsGROUP)
-    {
-        /* We only have OpenMP support for PME only nodes */
-        if (hw_opt->nthreads_omp > 1)
-        {
-            gmx_fatal(FARGS, "OpenMP threads have been requested with cut-off scheme %s, but these are only supported "
-                      "with cut-off scheme %s",
-                      ecutscheme_names[cutoff_scheme],
-                      ecutscheme_names[ecutsVERLET]);
-        }
-        hw_opt->nthreads_omp = 1;
-    }
 }
 
 void checkAndUpdateRequestedNumOpenmpThreads(gmx_hw_opt_t         *hw_opt,

src/gromacs/taskassignment/resourcedivision.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -100,12 +100,8 @@ void check_resource_division_efficiency(const gmx_hw_info_t *hwinfo,
 /*! \brief Checks we can do when we don't (yet) know the cut-off scheme */
 void check_and_update_hw_opt_1(const gmx::MDLogger &mdlog,
                                gmx_hw_opt_t        *hw_opt,
-                               const t_commrec     *cr,
+                               bool                 isSimulationMasterRank,
                                int                  nPmeRanks);
-
-/*! \brief Checks we can do when we know the cut-off scheme */
-void check_and_update_hw_opt_2(gmx_hw_opt_t *hw_opt,
-                               int           cutoff_scheme);
 
 /*! \brief Check, and if necessary update, the number of OpenMP threads requested
  *

src/gromacs/utility/pleasecite.cpp
@@ -422,6 +422,10 @@ void please_cite(FILE *fp, const char *key)
     fflush(fp);
 }
 
+namespace
+{
+
+//! Write a message to \c fp to request citation also of the source-code DOI.
 void
 writeSourceDoi(FILE *fp)
 {
@@ -447,4 +451,23 @@ writeSourceDoi(FILE *fp)
     fprintf(fp, "%s%s\n", "https://doi.org/", doiString.c_str());
     fprintf(fp, "-------- -------- --- Thank You --- -------- --------\n\n");
     fflush(fp);
+}
+
+} // namespace
+
+void pleaseCiteGromacs(FILE *fplog)
+{
+    if (fplog == nullptr)
+    {
+        return;
+    }
+
+    please_cite(fplog, "Abraham2015");
+    please_cite(fplog, "Pall2015");
+    please_cite(fplog, "Pronk2013");
+    please_cite(fplog, "Hess2008b");
+    please_cite(fplog, "Spoel2005a");
+    please_cite(fplog, "Lindahl2001a");
+    please_cite(fplog, "Berendsen95a");
+    writeSourceDoi(fplog);
 }

src/gromacs/utility/pleasecite.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -51,8 +51,8 @@
 void
 please_cite(FILE *fp, const char *key);
 
-//! Write a message to \c fp to request citation also of the source-code DOI.
+//! Write to \c fplog to request citation of GROMACS papers and source DOI.
 void
-writeSourceDoi(FILE *fp);
+pleaseCiteGromacs(FILE *fplog);
 
 #endif

commit 5c15e1c2eacc840b977ff3fd683edab453f0e5ae

Fix MPI deadlock in affinity setting
Commit 96d28d6b introduced the possiblity for deadlocks when
the affinity masks changed over time.

Fixes #2989

Change-Id: I454b38deb9ff11c90cdf4a19aaa80e79e8898df5

src/gromacs/mdrun/runner.cpp
@@ -994,7 +994,7 @@ int Mdrunner::mdrunner()
     // that existing affinity setting was from OpenMP or something
     // else, so we run this code both before and after we initialize
     // the OpenMP support.
-    gmx_check_thread_affinity_set(mdlog, cr,
+    gmx_check_thread_affinity_set(mdlog,
                                   &hw_opt, hwinfo->nthreads_hw_avail, FALSE);
     /* Check and update the number of OpenMP threads requested */
     checkAndUpdateRequestedNumOpenmpThreads(&hw_opt, *hwinfo, cr, ms, physicalNodeComm.size_,
@@ -1190,7 +1190,7 @@ int Mdrunner::mdrunner()
          * - which indicates that probably the OpenMP library has changed it
          * since we first checked).
          */
-        gmx_check_thread_affinity_set(mdlog, cr,
+        gmx_check_thread_affinity_set(mdlog,
                                       &hw_opt, hwinfo->nthreads_hw_avail, TRUE);
 
         int numThreadsOnThisNode, intraNodeThreadOffset;

src/gromacs/mdrunutility/threadaffinity.cpp
@@ -464,7 +464,6 @@ gmx_set_thread_affinity(const gmx::MDLogger         &mdlog,
  */
 void
 gmx_check_thread_affinity_set(const gmx::MDLogger &mdlog,
-                              const t_commrec     *cr,
                               gmx_hw_opt_t        *hw_opt,
                               int  gmx_unused      nthreads_hw_avail,
                               gmx_bool             bAfterOpenmpInit)
@@ -494,16 +493,8 @@ gmx_check_thread_affinity_set(const gmx::MDLogger &mdlog,
             }
         }
 
-        /* With thread-MPI this is needed as pinning might get turned off,
-         * which needs to be known before starting thread-MPI.
-         * With thread-MPI hw_opt is processed here on the master rank
-         * and passed to the other ranks later, so we only do this on master.
-         */
-        if (!SIMMASTER(cr))
-        {
-            return;
-        }
 #if !GMX_THREAD_MPI
+        // TODO: Remove this return so we get early reporting without thread-MPI as well
         return;
 #endif
     }
@@ -526,6 +517,8 @@ gmx_check_thread_affinity_set(const gmx::MDLogger &mdlog,
         {
             fprintf(debug, "Failed to query affinity mask (error %d)", ret);
         }
+        // TODO: This might cause divergence between ranks on the same node.
+        //       Replace this return by a boolean and combine with bAllSet.
         return;
     }
 
@@ -540,18 +533,29 @@ gmx_check_thread_affinity_set(const gmx::MDLogger &mdlog,
             fprintf(debug, "%d hardware threads detected, but %d was returned by CPU_COUNT",
                     nthreads_hw_avail, CPU_COUNT(&mask_current));
         }
+        // TODO: This might cause divergence between ranks on the same node.
+        //       Replace this return by a boolean and combine with bAllSet.
         return;
     }
 #endif /* CPU_COUNT */
 
+    /* Here we check whether all bits of the affinity mask are set.
+     * Note that this mask can change while the program is executing,
+     * e.g. by the system dynamically enabling or disabling cores or
+     * by some scheduler changing the affinities. Thus we can not assume
+     * that the result is the same on all ranks within a node
+     * when running this code at different times.
+     */
     gmx_bool bAllSet = TRUE;
     for (int i = 0; (i < nthreads_hw_avail && i < CPU_SETSIZE); i++)
     {
         bAllSet = bAllSet && (CPU_ISSET(i, &mask_current) != 0);
     }
 
-    // With thread-MPI, bAllSet must already be the same on all ranks.
-#if GMX_LIB_MPI
+#if GMX_MPI
+    int mpiIsInitialized;
+    MPI_Initialized(&mpiIsInitialized);
+    if (mpiIsInitialized)
     {
         bool bAllSet_All;
         MPI_Allreduce(&bAllSet, &bAllSet_All, 1, MPI_C_BOOL, MPI_LAND, MPI_COMM_WORLD);

src/gromacs/mdrunutility/threadaffinity.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -116,7 +116,7 @@ gmx_set_thread_affinity(const gmx::MDLogger         &mdlog,
  * variables for setting the affinity are set.
  */
 void
-gmx_check_thread_affinity_set(const gmx::MDLogger &mdlog, const t_commrec *cr,
+gmx_check_thread_affinity_set(const gmx::MDLogger &mdlog,
                               gmx_hw_opt_t *hw_opt, int ncpus,
                               gmx_bool bAfterOpenmpInit);
 