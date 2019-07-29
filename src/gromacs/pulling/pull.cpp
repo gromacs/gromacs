457d046bd141a7258cfda4614a23709d911adaab
Berk Hess <hess@kth.se>
2019-07-29 13:45:34 +0200

    Fix segmentation fault with cosine pulling
    Both grompp and mdrun would fail with a segmentation fault when
    using cosine-weighted COM pulling.
    
    Fixes #3023
    
    Change-Id: I6342a579251d21ba07a42d23ce578ae1e20c3286

--- src/gromacs/pulling/pull.cpp
+++ src/gromacs/pulling/pull.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1598,7 +1598,7 @@ void dd_make_local_pull_groups(const t_commrec *cr, struct pull_t *pull)
 
     for (pull_group_work_t &group : pull->group)
     {
-        if (group.epgrppbc == epgrppbcCOS || !group.globalWeights.empty())
+        if (!group.globalWeights.empty())
         {
             group.localWeights.resize(group.atomSet.numAtomsLocal());
             for (size_t i = 0; i < group.atomSet.numAtomsLocal(); ++i)
--- src/gromacs/pulling/pullutil.cpp
+++ src/gromacs/pulling/pullutil.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -578,6 +578,15 @@ void pull_calc_coms(const t_commrec *cr,
     for (size_t g = 0; g < pull->group.size(); g++)
     {
         pull_group_work_t *pgrp      = &pull->group[g];
+
+        /* Cosine-weighted COMs behave different from all other weighted COMs
+         * in the sense that the weights depend on instantaneous coordinates,
+         * not on pre-set weights. Thus we resize the local weight buffer here.
+         */
+        if (pgrp->epgrppbc == epgrppbcCOS)
+        {
+            pgrp->localWeights.resize(pgrp->atomSet.localIndex().size());
+        }
 
         auto               comBuffer =
             gmx::arrayRefFromArray(comm->comBuffer.data() + g*c_comBufferStride, c_comBufferStride);
