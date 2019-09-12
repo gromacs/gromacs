522ef8ca2b733d608c8060e460aa28985cdbf093
M. Eric Irrgang <ericirrgang@gmail.com>
2019-09-12 12:56:35 +0300

    Do not use gromacs-toolchain in darwin gmxapi builds.
    Add workaround for toolchain issue on OS X.
    
    Refs #3085
    
    Change-Id: I96a8242b14c86abd4926b6abf5a401c37953b9bc

--- python_packaging/src/setup.py
+++ python_packaging/src/setup.py
@@ -148,14 +148,16 @@ if gmxapi_DIR != os.path.commonpath([gmxapi_DIR, gmx_toolchain]):
         gmxapi_DIR
     ))
 
-cmake_platform_hints = ['-DCMAKE_TOOLCHAIN_FILE={}'.format(gmx_toolchain)]
 if sys.platform == 'darwin':
     # TODO: Reconcile with cross-compilation CMake toolchain.
     # In some cases, compatibility settings are more relevant to libpython, and
     # in others libgmxapi. It is not completely clear where they can or should
     # be determined and set.
-    cmake_platform_hints.extend(['-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9',
-                                 '-DCMAKE_OSX_ARCHITECTURES:STRING=x86_64'])
+    cmake_platform_hints = ['-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9',
+                            '-DCMAKE_OSX_ARCHITECTURES:STRING=x86_64']
+else:
+    # TODO: (Issue 3085) Can toolchain be used on OS X builds?
+    cmake_platform_hints = ['-DCMAKE_TOOLCHAIN_FILE={}'.format(gmx_toolchain)]
 
 # TODO: Use package-specific hinting variable.
 # We want to be sure that we find a <package>-config.cmake associated with the
