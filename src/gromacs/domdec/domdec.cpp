gromacs/gromacs

commit ed91e159f0ef4cc3e5a398eed314eb7119de3ec5

Update testing matrices and fix warnings
Updated to test gcc 9, clang 8, and cmake 3.15.1.

Also fixes for compilation with gcc 9 and clang 9 (still in release
candidate, not yet released)

Added a gmx_source_file_warning_suppression() CMake helper function
(like the recently introduced gmx_target_warning_suppression()) to
manage per-source-file suppressions. This is used to suppress a
warning about the abuse of t_inputrec that is too difficult to fix
without replacing all of the handling of initialization and broadcast
of t_inputrec and subfields.

Refs #2899, #3006
Fixes #3007

Change-Id: I3a31a40c472d420adbb38d11ed5121702d7b3f7d

src/gromacs/domdec/domdec.cpp
@@ -1308,6 +1308,7 @@ static void setup_neighbor_relations(gmx_domdec_t *dd)
     }
 
     int nzone  = (1 << dd->ndim);
+    GMX_ASSERT(dd->ndim < DIM, "Invalid number of dimensions");
     int nizone = (1 << std::max(dd->ndim - 1, 0));
     assert(nizone >= 1 && nizone <= DD_MAXIZONE);
 

cmake/gmxCFlags.cmake
@@ -110,11 +110,27 @@ endfunction()
 # targets may use the same suppression.
 #
 # This is generally used to suppress warnings judged not worth fixing
-# in code external to, or generated by, GROMACS.
+# in code external to, or generated by, GROMACS, or code that is
+# more efficient to work around and later replace, rather than fix.
 function(gmx_target_warning_suppression TARGET WARNING_FLAG VARNAME)
     check_cxx_compiler_flag(${WARNING_FLAG} ${VARNAME})
     if(${VARNAME})
         target_compile_options(${TARGET} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${WARNING_FLAG}>)
+    endif()
+endfunction()
+
+# Add the WARNING_FLAG to the compile flags for SOURCE_FILE if the
+# compiler supports that flag. VARNAME is used to cache the result of
+# the check whether the compiler supports that flag, as multiple
+# targets may use the same suppression.
+#
+# This is generally used to suppress warnings judged not worth fixing
+# in code external to, or generated by, GROMACS, or code that is
+# more efficient to work around and later replace, rather than fix.
+function(gmx_source_file_warning_suppression SOURCE_FILE WARNING_FLAG VARNAME)
+    check_cxx_compiler_flag(${WARNING_FLAG} ${VARNAME})
+    if(${VARNAME})
+        set_source_files_properties(${SOURCE_FILE} PROPERTIES COMPILE_FLAGS ${WARNING_FLAG})
     endif()
 endfunction()
 

src/external/CMakeLists.txt
@@ -59,6 +59,7 @@ if(BUILD_TESTING AND GMX_BUILD_UNITTESTS)
         target_compile_definitions(gtest PRIVATE _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
         target_compile_definitions(gtest_main PRIVATE _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING)
     endif()
+    gmx_target_warning_suppression(gmock -Wno-deprecated-copy HAVE_NO_DEPRECATED_COPY)
 
     set(GTEST_IS_THREADSAFE "${GTEST_IS_THREADSAFE}" PARENT_SCOPE)
 endif()

src/gromacs/CMakeLists.txt
@@ -210,6 +210,9 @@ if (NOT GMX_BUILD_MDRUN_ONLY)
     add_subdirectory(selection)
 endif()
 
+# Suppress a warning about our abuse of t_inputrec
+gmx_source_file_warning_suppression(mdtypes/inputrec.cpp -Wno-class-memaccess HAS_NO_CLASS_MEMACCESS)
+
 # Handle the object libraries that contain the source file
 # dependencies that need special handling because they are generated
 # or external code.

src/gromacs/applied_forces/densityfittingamplitudelookup.cpp
@@ -57,6 +57,8 @@ namespace gmx
 class DensityFittingAmplitudeLookupImpl
 {
     public:
+        DensityFittingAmplitudeLookupImpl() = default;
+        DensityFittingAmplitudeLookupImpl(const DensityFittingAmplitudeLookupImpl &) = default;
         virtual ~DensityFittingAmplitudeLookupImpl() = default;
 
         virtual const std::vector<real> &operator()(const t_mdatoms    &atoms,
@@ -69,7 +71,9 @@ namespace
 class UnitAmplitudes final : public DensityFittingAmplitudeLookupImpl
 {
     public:
-        ~UnitAmplitudes() override = default;
+        UnitAmplitudes() = default;
+        UnitAmplitudes(const UnitAmplitudes &) = default;
+        ~UnitAmplitudes() override             = default;
         std::unique_ptr<DensityFittingAmplitudeLookupImpl> clone() override;
         const std::vector<real> &operator()(const t_mdatoms    &atoms,
                                             ArrayRef<const int> localIndex) override;
@@ -80,9 +84,7 @@ class UnitAmplitudes final : public DensityFittingAmplitudeLookupImpl
 
 std::unique_ptr<DensityFittingAmplitudeLookupImpl> UnitAmplitudes::clone()
 {
-    auto clone = std::make_unique<UnitAmplitudes>();
-    clone->amplitude_ = amplitude_;
-    return std::move(clone);
+    return std::make_unique<UnitAmplitudes>(*this);
 };
 
 const std::vector<real> &UnitAmplitudes::operator()(const t_mdatoms     & /*atoms*/,
@@ -99,7 +101,9 @@ const std::vector<real> &UnitAmplitudes::operator()(const t_mdatoms     & /*atom
 class ChargesAsAmplitudes final : public DensityFittingAmplitudeLookupImpl
 {
     public:
-        ~ChargesAsAmplitudes() override = default;
+        ChargesAsAmplitudes() = default;
+        ChargesAsAmplitudes(const ChargesAsAmplitudes &) = default;
+        ~ChargesAsAmplitudes() override                  = default;
         std::unique_ptr<DensityFittingAmplitudeLookupImpl> clone() override;
         const std::vector<real> &operator()(const t_mdatoms    &atoms,
                                             ArrayRef<const int> localIndex) override;
@@ -110,9 +114,7 @@ class ChargesAsAmplitudes final : public DensityFittingAmplitudeLookupImpl
 
 std::unique_ptr<DensityFittingAmplitudeLookupImpl> ChargesAsAmplitudes::clone()
 {
-    auto clone = std::make_unique<ChargesAsAmplitudes>();
-    clone->amplitude_ = amplitude_;
-    return std::move(clone);
+    return std::make_unique<ChargesAsAmplitudes>(*this);
 };
 
 const std::vector<real> &ChargesAsAmplitudes::operator()(const t_mdatoms    &atoms,
@@ -132,7 +134,9 @@ const std::vector<real> &ChargesAsAmplitudes::operator()(const t_mdatoms    &ato
 class MassesAsAmplitudes final : public DensityFittingAmplitudeLookupImpl
 {
     public:
-        ~MassesAsAmplitudes() override = default;
+        MassesAsAmplitudes() = default;
+        MassesAsAmplitudes(const MassesAsAmplitudes &) = default;
+        ~MassesAsAmplitudes() override                 = default;
         std::unique_ptr<DensityFittingAmplitudeLookupImpl> clone() override;
         const std::vector<real> &operator()(const t_mdatoms    &atoms,
                                             ArrayRef<const int> localIndex) override;
@@ -143,9 +147,7 @@ class MassesAsAmplitudes final : public DensityFittingAmplitudeLookupImpl
 
 std::unique_ptr<DensityFittingAmplitudeLookupImpl> MassesAsAmplitudes::clone()
 {
-    auto clone = std::make_unique<MassesAsAmplitudes>();
-    clone->amplitude_ = amplitude_;
-    return std::move(clone);
+    return std::make_unique<MassesAsAmplitudes>(*this);
 };
 
 const std::vector<real> &MassesAsAmplitudes::operator()(const t_mdatoms    &atoms,

src/gromacs/domdec/domdec_constraints.cpp
@@ -470,7 +470,6 @@ int dd_make_local_constraints(gmx_domdec_t *dd, int at_start,
     else
     {
         int t0_set;
-        int thread;
 
         /* Do the constraints, if present, on the first thread.
          * Do the settles on all other threads.
@@ -478,7 +477,7 @@ int dd_make_local_constraints(gmx_domdec_t *dd, int at_start,
         t0_set = ((!at2con_mt.empty() && dc->nthread > 1) ? 1 : 0);
 
 #pragma omp parallel for num_threads(dc->nthread) schedule(static)
-        for (thread = 0; thread < dc->nthread; thread++)
+        for (int thread = 0; thread < dc->nthread; thread++)
         {
             try
             {
@@ -524,7 +523,7 @@ int dd_make_local_constraints(gmx_domdec_t *dd, int at_start,
         }
 
         /* Combine the generate settles and requested indices */
-        for (thread = 1; thread < dc->nthread; thread++)
+        for (int thread = 1; thread < dc->nthread; thread++)
         {
             t_ilist   *ilst;
             int        ia;

src/gromacs/domdec/domdec_topology.cpp
@@ -1734,7 +1734,6 @@ static int make_local_bondeds_excls(gmx_domdec_t *dd,
     int                izone, cg0, cg1;
     real               rc2;
     int                nbonded_local;
-    int                thread;
     gmx_reverse_top_t *rt;
 
     if (dd->reverse_top->bInterCGInteractions)
@@ -1781,7 +1780,7 @@ static int make_local_bondeds_excls(gmx_domdec_t *dd,
 
         const int numThreads = rt->th_work.size();
 #pragma omp parallel for num_threads(numThreads) schedule(static)
-        for (thread = 0; thread < numThreads; thread++)
+        for (int thread = 0; thread < numThreads; thread++)
         {
             try
             {

src/gromacs/ewald/pme_redistribute.cpp
@@ -125,12 +125,10 @@ static void pme_calc_pidx_wrapper(gmx::ArrayRef<const gmx::RVec>  x,
                                   const matrix                    recipbox,
                                   PmeAtomComm                    *atc)
 {
-    int nthread, thread, slab;
-
-    nthread = atc->nthread;
+    int nthread = atc->nthread;
 
 #pragma omp parallel for num_threads(nthread) schedule(static)
-    for (thread = 0; thread < nthread; thread++)
+    for (int thread = 0; thread < nthread; thread++)
     {
         try
         {
@@ -144,9 +142,9 @@ static void pme_calc_pidx_wrapper(gmx::ArrayRef<const gmx::RVec>  x,
     }
     /* Non-parallel reduction, since nslab is small */
 
-    for (thread = 1; thread < nthread; thread++)
+    for (int thread = 1; thread < nthread; thread++)
     {
-        for (slab = 0; slab < atc->nslab; slab++)
+        for (int slab = 0; slab < atc->nslab; slab++)
         {
             atc->count_thread[0][slab] += atc->count_thread[thread][slab];
         }

commit 5c97b47eb5f39d47ca7a5f69fde38d75fb37958b

Fix incorrect DD ndim assertion
An incorrect assertion was introduced in recent commit ed91e159.

Merged that assertion with a similar one nearby

Change-Id: Idff2a0d8a1985a19019233d29c4804294824297c

src/gromacs/domdec/domdec.cpp
@@ -1280,7 +1280,7 @@ static void setup_neighbor_relations(gmx_domdec_t *dd)
     ivec                    tmp, s;
     gmx_domdec_zones_t     *zones;
     gmx_domdec_ns_ranges_t *izone;
-    GMX_ASSERT(dd->ndim >= 0, "Must have non-negative number of dimensions for DD");
+    GMX_ASSERT((dd->ndim >= 0) && (dd->ndim <= DIM), "Must have valid number of dimensions for DD");
 
     for (d = 0; d < dd->ndim; d++)
     {
@@ -1301,7 +1301,6 @@ static void setup_neighbor_relations(gmx_domdec_t *dd)
     }
 
     int nzone  = (1 << dd->ndim);
-    GMX_ASSERT(dd->ndim < DIM, "Invalid number of dimensions");
     int nizone = (1 << std::max(dd->ndim - 1, 0));
     assert(nizone >= 1 && nizone <= DD_MAXIZONE);
 