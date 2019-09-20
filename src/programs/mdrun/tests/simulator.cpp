a8758ff07cfd75447a611bca14b1e409e00c3e62
Pascal Merz <pascal.merz@me.com>
2019-09-19 22:59:59 -0600

    Run simulator equivalence tests in double precision only
    The simulator equivalence tests are a great help in the effort of
    rewriting the simulator framework, but might be too sensitive for
    general use. They have been shown to be extremely sensitive to
    order of operations which inevitably changes in day-by-day
    development. Most recent test failures could be traced back to
    the absence of a parenthesis in otherwise identical code (for
    md-vv), and to the differences between the SIMD and non-SIMD
    code paths (for md), respectively. Double precision builds seem to
    be less affected by such re-ordering, so this commit changes the
    simulator equivalence tests to be only enabled for double precision
    builds. As the simulator rewriting progresses, and the new code paths
    get covered by the existing tests, we might want to move away from
    these type of equivalence tests altogether.
    
    Change-Id: Ia4ad269959efd207242795ecedb88eaf7a870589

--- src/programs/mdrun/tests/simulator.cpp
+++ src/programs/mdrun/tests/simulator.cpp
@@ -160,10 +160,12 @@ TEST_P(SimulatorComparisonTest, WithinTolerances)
             trajectoryComparison);
 }
 
-// TODO The time for OpenCL kernel compilation means these tests time
-// out. Once that compilation is cached for the whole process, these
-// tests can run in such configurations.
-#if GMX_GPU != GMX_GPU_OPENCL
+// TODO: The time for OpenCL kernel compilation means these tests time
+//       out. Once that compilation is cached for the whole process, these
+//       tests can run in such configurations.
+// These tests are very sensitive, so we only run them in double precision.
+// As we change call ordering, they might actually become too strict to be useful.
+#if GMX_GPU != GMX_GPU_OPENCL && GMX_DOUBLE
 INSTANTIATE_TEST_CASE_P(SimulatorsAreEquivalentDefaultModular, SimulatorComparisonTest,
                             ::testing::Combine(
                                     ::testing::Combine(::testing::Values("argon12", "tip3p5"),
