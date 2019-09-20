85ef063c53f77a3423b2943d16d1d2f3759aa1bf
Paul Bauer <paul.bauer.q@gmail.com>
2019-09-20 14:03:08 +0200

    Remove update flag again
    Disables assignment of update task to different hardware and removes
    user option.
    
    Change-Id: I7780ac54f65809cbd91386a0d6a92b7bee35ab26

--- src/gromacs/mdrun/legacymdrunoptions.h
+++ src/gromacs/mdrun/legacymdrunoptions.h
@@ -164,7 +164,7 @@ class LegacyMdrunOptions
 
         ImdOptions       &imdOptions = mdrunOptions.imdOptions;
 
-        t_pargs           pa[48] = {
+        t_pargs           pa[47] = {
 
             { "-dd",      FALSE, etRVEC, {&realddxyz},
               "Domain decomposition grid, 0 is optimize" },
@@ -227,8 +227,6 @@ class LegacyMdrunOptions
               "Perform PME FFT calculations on" },
             { "-bonded",     FALSE, etENUM, {bonded_opt_choices},
               "Perform bonded calculations on" },
-            { "-update", FALSE, etENUM, {update_opt_choices},
-              "Perform update and constraints on"},
             { "-v",       FALSE, etBOOL, {&mdrunOptions.verbose},
               "Be loud and noisy" },
             { "-pforce",  FALSE, etREAL, {&pforce},
