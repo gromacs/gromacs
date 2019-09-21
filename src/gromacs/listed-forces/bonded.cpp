8fc3cc55a0a0721fc9c336f142a33025d18b4d5f
Berk Hess <hess@kth.se>
2019-09-21 11:36:32 +0200

    Fix incorrect shift forces for CMAP
    The shift force indices were inverted for the second and third
    atom in the CMAP term, leading to incorrect virial and pressure
    contributions when these atoms resided in different periodic images.
    
    Fixes #2845 and #2867
    
    Change-Id: I1946a1d375d6c62e4e6d23ee25b92b42a3d4a6f7

--- src/gromacs/listed-forces/bonded.cpp
+++ src/gromacs/listed-forces/bonded.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -3267,7 +3267,7 @@ cmap_dihs(int nbonds,
         rvec_inc(fshift[t21], f1_k);
         rvec_inc(fshift[t31], f1_l);
 
-        rvec_inc(fshift[t21], f2_i);
+        rvec_inc(fshift[t12], f2_i);
         rvec_inc(fshift[CENTRAL], f2_j);
         rvec_inc(fshift[t22], f2_k);
         rvec_inc(fshift[t32], f2_l);
