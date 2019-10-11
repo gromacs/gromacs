gromacs/gromacs

commit 9409fa5fb6ecb8202b0384b6f174937b2cd490fa

Remove incorrect GPU task dependency
The depndency between nonlocal reduction and PME forces is not necessary
and therefore it is removed.

Change-Id: Ic7eba307b519f0fb8bde24a2cf676a22d64065ba

src/gromacs/mdlib/sim_util.cpp
@@ -1494,12 +1494,7 @@ void do_force(FILE                                     *fplog,
             if (useGpuFBufOps == BufferOpsUseGpu::True)
             {
                 std::vector<GpuEventSynchronizer*> dependencyList;
-                dependencyList.reserve(2);
-
-                if (useGpuPmeFReduction)
-                {
-                    dependencyList.push_back(pme_gpu_get_f_ready_synchronizer(fr->pmedata));
-                }
+                dependencyList.reserve(1);
 
                 // TODO: move this into DomainLifetimeWorkload, including the second part of the condition
                 // The bonded and free energy CPU tasks can have non-local force contributions
@@ -1512,9 +1507,6 @@ void do_force(FILE                                     *fplog,
                     dependencyList.push_back(stateGpu->getForcesReadyOnDeviceEvent(gmx::StatePropagatorDataGpu::AtomLocality::NonLocal));
                 }
 
-                //
-                // FIXME: are we adding a PME->nonlocal dep here?
-                //
                 nbv->atomdata_add_nbat_f_to_f_gpu(Nbnxm::AtomLocality::NonLocal,
                                                   stateGpu->getForces(),
                                                   pme_gpu_get_device_f(fr->pmedata),
@@ -1684,7 +1676,7 @@ void do_force(FILE                                     *fplog,
                                               stateGpu->getForces(),
                                               pme_gpu_get_device_f(fr->pmedata),
                                               dependencyList,
-                                              useGpuPmeFReduction, haveLocalForceContribInCpuBuffer);
+                                              false, haveLocalForceContribInCpuBuffer);
             // This function call synchronizes the local stream
             nbv->wait_for_gpu_force_reduction(Nbnxm::AtomLocality::Local);
             stateGpu->copyForcesFromGpu(forceWithShift, gmx::StatePropagatorDataGpu::AtomLocality::Local);

commit 4761b51f17aad9291afc9c22eba70fba212a6fcc

Wrong locality was used when ignoring the PME force contribution
The PME forces have no no-local contribution, hence they should be
ignored when non-local forces are reduced. This fixes the bug with
local PME forces ignored instead of non-local. The bug was
introduced in 9409fa5f.

Change-Id: Ie27c71d1f51ca4022f196a9fa114af42685fcbcf

src/gromacs/mdlib/sim_util.cpp
@@ -1513,7 +1513,7 @@ void do_force(FILE                                     *fplog,
                                                   stateGpu->getForces(),
                                                   pme_gpu_get_device_f(fr->pmedata),
                                                   dependencyList,
-                                                  useGpuPmeFReduction, haveNonLocalForceContribInCpuBuffer);
+                                                  false, haveNonLocalForceContribInCpuBuffer);
                 stateGpu->copyForcesFromGpu(forceOut.forceWithShiftForces().force(), gmx::StatePropagatorDataGpu::AtomLocality::NonLocal);
             }
             else
@@ -1678,7 +1678,7 @@ void do_force(FILE                                     *fplog,
                                               stateGpu->getForces(),
                                               pme_gpu_get_device_f(fr->pmedata),
                                               dependencyList,
-                                              false, haveLocalForceContribInCpuBuffer);
+                                              useGpuPmeFReduction, haveLocalForceContribInCpuBuffer);
             // This function call synchronizes the local stream
             nbv->wait_for_gpu_force_reduction(Nbnxm::AtomLocality::Local);
             stateGpu->copyForcesFromGpu(forceWithShift, gmx::StatePropagatorDataGpu::AtomLocality::Local);