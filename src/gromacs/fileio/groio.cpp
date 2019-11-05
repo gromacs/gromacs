0a698f1520d4fc93974bf9336a4cd6cf870c68c8
Berk Hess <hess@kth.se>
2019-11-05 09:41:02 +0100

    Fix .gro large box writing
    Contrary to the format specification in the manual, the box components
    in the .gro file were not separated by whitespace when components
    (except for the first one) were >= 1000 or <= -100. Now a space is
    always printed. This might not match the original GROMOS format,
    but most codes reading .gro file assume space separation.
    Note formatting of boxes that did have spacing is not changed.
    
    Fixes #3176
    
    Change-Id: I8cfcde98b46550b16b9f6bc299dc0b67454b2437

--- src/gromacs/fileio/groio.cpp
+++ src/gromacs/fileio/groio.cpp
@@ -439,14 +439,14 @@ static void write_hconf_box(FILE *out, const matrix box)
     if ((box[XX][YY] != 0.0f) || (box[XX][ZZ] != 0.0f) || (box[YY][XX] != 0.0f) || (box[YY][ZZ] != 0.0f) ||
         (box[ZZ][XX] != 0.0f) || (box[ZZ][YY] != 0.0f))
     {
-        fprintf(out, "%10.5f%10.5f%10.5f%10.5f%10.5f%10.5f%10.5f%10.5f%10.5f\n",
+        fprintf(out, "%10.5f %9.5f %9.5f %9.5f %9.5f %9.5f %9.5f %9.5f %9.5f\n",
                 box[XX][XX], box[YY][YY], box[ZZ][ZZ],
                 box[XX][YY], box[XX][ZZ], box[YY][XX],
                 box[YY][ZZ], box[ZZ][XX], box[ZZ][YY]);
     }
     else
     {
-        fprintf(out, "%10.5f%10.5f%10.5f\n",
+        fprintf(out, "%10.5f %9.5f %9.5f\n",
                 box[XX][XX], box[YY][YY], box[ZZ][ZZ]);
     }
 }
