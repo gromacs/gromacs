2538721686acc5c0e85413f11c7cb872dfeda6a9
Sebastian Keller <keller@cscs.ch>
2019-11-20 09:22:35 +0100

    fixed array init
    Change-Id: I67e73ed05bd5b35f18427758de6f38f0105eff7f

--- src/gromacs/nblib/box.cpp
+++ src/gromacs/nblib/box.cpp
@@ -8,24 +8,35 @@
 
 namespace nblib {
 
-Box::Box(real l) {
+Box::Box(real l)
+{
     if (std::isnan(l) or std::isinf(l))
     {
         GMX_THROW(gmx::InvalidInputError("Cannot have NaN or Inf box length."));
     }
             
-    box_[XX] = l;
-    box_[YY*DIM + ZZ] = l;
-    box_[ZZ*DIM + ZZ] = l;
+    box_[XX][XX] = l;
+    box_[YY][YY] = l;
+    box_[ZZ][ZZ] = l;
 }
 
-    Box::Box(real x, real y, real z)
+Box::Box(real x, real y, real z)
 {
-    box_[XX] = x;
-    box_[YY*DIM + ZZ] = y;
-    box_[ZZ*DIM + ZZ] = z;
+    if (std::isnan(x) or std::isinf(x) or
+        std::isnan(y) or std::isinf(y) or
+        std::isnan(z) or std::isinf(z))
+    {
+        GMX_THROW(gmx::InvalidInputError("Cannot have NaN or Inf box length."));
+    }
+
+    box_[XX][XX] = x;
+    box_[YY][YY] = y;
+    box_[ZZ][ZZ] = z;
 }
 
-std::array<real, DIM*DIM> Box::getMatrix() { return box_; }
+Box::data_type Box::getMatrix()
+{
+    return box_;
+}
 
 }
