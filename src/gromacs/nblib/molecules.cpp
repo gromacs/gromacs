430ceade15d7549741c25195d3767bd499e3c3af
ejjordan <ejjordan@kth.se>
2019-11-21 12:11:42 +0100

    fix improper function definition in MoleculeType

--- src/gromacs/nblib/molecules.cpp
+++ src/gromacs/nblib/molecules.cpp
@@ -55,7 +55,7 @@ void MoleculeType::addHarmonicBond(HarmonicType harmonicBond)
     harmonicInteractions_.push_back(harmonicBond);
 }
 
-void addExclusion(int atomWithExclusion, int atomToExclude)
+void MoleculeType::addExclusion(int atomWithExclusion, int atomToExclude)
 {
     exclusions_.emplace_back(std::make_tuple(atomWithExclusion, atomToExclude));
 }
