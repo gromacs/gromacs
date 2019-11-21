bf4547cf33cd2cd6bcd62a00cd0e7c0b71a59e33
Sebastian Keller <keller@cscs.ch>
2019-11-21 15:58:25 +0100

    range based fillExclusions
    Change-Id: I8cde7a6e06c509813d26b1063b6bd4e23f03233e

--- src/gromacs/nblib/topology.cpp
+++ src/gromacs/nblib/topology.cpp
@@ -18,56 +18,53 @@ namespace nblib {
 t_blocka TopologyBuilder::fillExclusionsList(std::vector<std::tuple<MoleculeType, int>> moleculesList)
 {
     //std::array<gmx::ExclusionBlock, numAtomsTotal> exclusionBlockGlobal;
-    std::vector<gmx::ExclusionBlock> exclusionBlockGlobal(numAtoms_);
+    std::vector<gmx::ExclusionBlock> exclusionBlockGlobal;
+    exclusionBlockGlobal.reserve(numAtoms_);
 
     t_blocka tBlockGlobal;
-    size_t globalIndex = 0;
 
-    for (size_t iMolTypes = 0; iMolTypes < moleculesList.size(); iMolTypes++)
+    //! compare tuples by comparing the first element
+    auto firstLowerThan = [](auto tup1, auto tup2) { return std::get<0>(tup1) < std::get<0>(tup2); };
+
+    for (auto &molNumberTuple : moleculesList)
     {
-        MoleculeType& molecule = std::get<0>(moleculesList[iMolTypes]);
-        size_t numMols = std::get<1>(moleculesList[iMolTypes]);
-
-        int numAtomsInMolecule = molecule.numAtomsInMolecule();
+        MoleculeType& molecule = std::get<0>(molNumberTuple);
+        size_t numMols = std::get<1>(molNumberTuple);
 
         std::vector<std::tuple<int, int>> exclusionList = molecule.exclusions_;
-        std::vector<gmx::ExclusionBlock> exclusionsWithinMolecule(numAtomsInMolecule);
+        //! sorted (by first tuple element as key) list of exclusion pairs
+        std::sort(std::begin(exclusionList), std::end(exclusionList), firstLowerThan);
+        std::vector<gmx::ExclusionBlock> exclusionBlockPerMolecule;
 
-        for (int iAtoms = 0; iAtoms < numAtomsInMolecule; iAtoms++)
+        //! it1 will point to start of range
+        auto it1 = std::lower_bound(std::begin(exclusionList), std::end(exclusionList), exclusionList[0], firstLowerThan);
+        auto it2 = std::upper_bound(std::begin(exclusionList), std::end(exclusionList), exclusionList[0], firstLowerThan);
+
+        //! loop over all exlusions in molecule
+        while (it1 != std::end(exclusionList))
         {
-            /*!
-             * 1. From the exclusion list, extract all exclusions for the atom marked
-             *    by iAtom
-             * 2. Create an ExclusionBlock object and put into array of exclusion blocks (repeat this
-             *    process numAtomsInMolecule)
-             * 3. Duplicate this numMol times
-             * 4. Concatonate this array with the global array
-             * 5. Convert the array of exclusion blocks to a global t_blocka
-             */
-
-            for (size_t iPair = 0; iPair < exclusionList.size(); iPair++)
+            gmx::ExclusionBlock localBlock;
+            //! loop over all exclusions for current atom (=std::get<0>(*it1))
+            for ( ; it1 != it2; ++it1)
             {
-                std::vector<int> atomsToExclude;
-                if (std::get<0>(exclusionList[iPair]) == iAtoms)
-                {
-                    atomsToExclude.emplace_back(std::get<1>(exclusionList[iPair]));
-                }
-                gmx::ExclusionBlock exclusionBlock;
-                exclusionBlock.atomNumber = atomsToExclude;
+                localBlock.atomNumber.push_back(std::get<1>(*it1));
+            }
 
-                exclusionsWithinMolecule[iPair] = exclusionBlock;
-            }
-            for (size_t iMols = 0; iMols < numMols; iMols++ )
-            {
-                exclusionBlockGlobal[globalIndex] = exclusionsWithinMolecule[iAtoms];
-                globalIndex++;
-            }
+            exclusionBlockPerMolecule.push_back(localBlock);
+
+            if (it1 != end(exclusionList))
+                it2 = std::upper_bound(it1, std::end(exclusionList), *it1, firstLowerThan);
+        }
+
+        for (int i = 0; i < numMols; ++i)
+        {
+            std::copy(std::begin(exclusionBlockPerMolecule), std::end(exclusionBlockPerMolecule),
+                      std::back_inserter(exclusionBlockGlobal));
         }
     }
 
-    //! At the very end, outside of these 4 for loops, convert the exclusionBlockGlobal into
+    //! At the very end, convert the exclusionBlockGlobal into
     //! a massive t_blocka and return
-
     gmx::exclusionBlocksToBlocka(exclusionBlockGlobal, &tBlockGlobal);
 
     return tBlockGlobal;
@@ -79,15 +76,15 @@ Topology TopologyBuilder::buildTopology()
     return topology_;
 }
 
-TopologyBuilder& TopologyBuilder::add(const MoleculeType moleculeType, const int nMolecules)
+TopologyBuilder& TopologyBuilder::addMolecule(const MoleculeType molecule, const int nMolecules)
 {
     /*!
      * 1. Push-back a tuple of molecule type and nMolecules
      * 2. Append exclusion list into the data structure
      */
 
-    molecules_.emplace_back(std::make_tuple(moleculeType, nMolecules));
-    numAtoms_ += nMolecules*moleculeType.numAtomsInMolecule();
+    molecules_.emplace_back(std::make_tuple(molecule, nMolecules));
+    numAtoms_ += nMolecules*molecule.numAtomsInMolecule();
 }
 
 const std::vector<real>& Topology::getMasses() const
