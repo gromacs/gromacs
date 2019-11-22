61b41d1aec720983f7499f4777930cedf2278de9
Victor Holanda <victorusu@gmail.com>
2019-11-22 10:36:37 +0100

    Split setup to nbkerneloption, update simulationState

--- src/gromacs/nblib/tests/simstate.cpp
+++ src/gromacs/nblib/tests/simstate.cpp
@@ -44,7 +44,7 @@
 #include "gmxpre.h"
 
 #include "gromacs/nblib/box.h"
-#include "gromacs/nblib/setup.h"
+#include "gromacs/nblib/nbkerneloptions.h"
 #include "gromacs/nblib/topology.h"
 #include "gromacs/nblib/molecules.h"
 #include "gromacs/nblib/simulationstate.h"
