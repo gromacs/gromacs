6dd2746df3fdaa99e73ecc0785f576bbd23c30fe
Berk Hess <hess@kth.se>
2019-11-28 11:58:48 +0100

    Remove assertion failure in AWH without initial stage
    When not using the initial stage in AWH, we should not check
    for coverage and histogram anomalies at step 0.
    
    Fixes #3217
    
    Change-Id: Iee19c4740c02ce56c0b43ad89efb6e37f4ad1a0d

--- src/gromacs/awh/biasparams.h
+++ src/gromacs/awh/biasparams.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2015,2016,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2015,2016,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -156,7 +156,7 @@ class BiasParams
          */
         bool isCheckCoveringStep(int64_t step) const
         {
-            return step % numStepsCheckCovering_ == 0;
+            return step > 0 && (step % numStepsCheckCovering_ == 0);
         }
 
         /*! \brief
