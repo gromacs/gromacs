5b8ee0f297f6e21a11a12d85859fe4a9d782544d
Erik Lindahl <erik@kth.se>
2019-12-31 12:00:43 +0100

    Add link to paper explaining GROMOS twin-range issues.
    Change-Id: I81e89817ce9441bb3d25bc43b00b4cc995590c3a

--- src/gromacs/gmxpreprocess/topio.cpp
+++ src/gromacs/gmxpreprocess/topio.cpp
@@ -886,8 +886,14 @@ static char** read_topol(const char*                           infile,
                 "multiple-time-stepping scheme for a twin-range cut-off. When used with "
                 "a single-range cut-off (or a correct Trotter multiple-time-stepping scheme), "
                 "physical properties, such as the density, might differ from the intended values. "
-                "Check if molecules in your system are affected by such issues before proceeding. "
-                "Further information may be available at https://redmine.gromacs.org/issues/2884.");
+                "Since there are researchers actively working on validating GROMOS with modern "
+                "integrators we have not yet removed the GROMOS force fields, but you should be "
+                "aware of these issues and check if molecules in your system are affected before "
+                "proceeding. "
+                "Further information is available at https://redmine.gromacs.org/issues/2884 , "
+                "and a longer explanation of our decision to remove physically incorrect "
+                "algorithms "
+                "can be found at https://doi.org/10.26434/chemrxiv.11474583.v1 .");
     }
 
     cpp_done(handle);
