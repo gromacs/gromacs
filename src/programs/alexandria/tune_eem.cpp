f8f410fc9d9fdc886e106ffbfb93c47c2d639276
mohammad-ghahremanpour <ghahremanpour.gro@gmail.com>
2020-01-26 08:30:12 -0500

    use getBestParam() istead of getParam() in tune_eem.cpp line 665
    Change-Id: Ib7be9b14652d7916f25586bd821d87e4912778db

--- src/programs/alexandria/tune_eem.cpp
+++ src/programs/alexandria/tune_eem.cpp
@@ -662,7 +662,7 @@ void OptACM::toPolData(const std::vector<bool> gmx_unused &changed)
     auto     pd          = poldata();
     bool     distributed = getEemtypeDistributed(pd->getChargeModel());
     auto    *ic          = indexCount();
-    auto     param       = Bayes::getParam();
+    auto     param       = Bayes::getBestParam();
     auto     psigma      = Bayes::getPsigma();
     if (psigma.empty())
     {
