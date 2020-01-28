6ff0d332d98d795adaa9c998e8f4cfdbfbdd610e
Szilárd Páll <pall.szilard@gmail.com>
2020-01-28 19:24:13 +0100

    Avoid (re)initializing GPU update without DD
    Unless repartitioning happens the GPU update-constraints module's
    expensive initialization only neeeds to be called once.
    
    Fixes #3358
    
    Change-Id: Ic2a147985da5907961a3e3277110bbae52e6cadc

--- src/gromacs/mdrun/md.cpp
+++ src/gromacs/mdrun/md.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2011-2019, by the GROMACS development team, led by
+ * Copyright (c) 2011-2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1254,7 +1254,7 @@ void gmx::LegacySimulator::do_md()
 
         if (useGpuForUpdate)
         {
-            if (bNS)
+            if (bNS && (bFirstStep || DOMAINDECOMP(cr)))
             {
                 integrator->set(stateGpu->getCoordinates(), stateGpu->getVelocities(),
                                 stateGpu->getForces(), top.idef, *mdatoms, ekind->ngtc);
