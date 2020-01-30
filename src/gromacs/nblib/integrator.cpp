e4c6008c1f57e8e5ba545c908a2ff48240e2d524
Sebastian Keller <keller@cscs.ch>
2020-01-30 11:45:40 +0100

    applied clang-format
    Change-Id: I09d143eeec548f88551e3dcc8ad9bc85b8e9c164

--- src/gromacs/nblib/integrator.cpp
+++ src/gromacs/nblib/integrator.cpp
@@ -48,7 +48,8 @@
 #include "gromacs/nbnxm/atomdata.h"
 #include "gromacs/pbcutil/pbc.h"
 
-namespace nblib {
+namespace nblib
+{
 
 void integrateCoordinates(const std::vector<nbnxn_atomdata_output_t>& nbvAtomsOut,
                           const NBKernelOptions&                      options,
@@ -62,8 +63,8 @@ void integrateCoordinates(const std::vector<nbnxn_atomdata_output_t>& nbvAtomsOu
         auto force = nbvAtomsOut[0].f;
         for (int dim = 0; dim < DIM; dim++)
         {
-            real vel      = force[dim]*options.timestep;
-            real newCoord = currentCoords[atomI][dim] + vel*options.timestep;
+            real vel               = force[dim] * options.timestep;
+            real newCoord          = currentCoords[atomI][dim] + vel * options.timestep;
             nextCoords[atomI][dim] = newCoord;
         }
     }
