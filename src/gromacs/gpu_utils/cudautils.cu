e0b71725aa093e1984747ebaa6781d429db53669
Alan Gray <alangraygerrit@gmail.com>
2020-02-05 09:18:34 -0800

    Bug fix to avoid passing null object into cudaDestroyTextureObject
    In the initial dummy context creation sanity-check part of the
    simulation, a null object is passed into this function. This can cause
    runtime failures with certain versions of CUDA. This change only calls
    the function when the object is non-null.
    
    Change-Id: I8118f6086fa2227c8dc697e4634878a12fe3676f

--- src/gromacs/gpu_utils/cudautils.cu
+++ src/gromacs/gpu_utils/cudautils.cu
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2012,2014,2015,2016,2017,2018,2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -178,7 +178,7 @@ void initParamLookupTable(T*& d_ptr, cudaTextureObject_t& texObj, const T* h_ptr
 template<typename T>
 void destroyParamLookupTable(T* d_ptr, cudaTextureObject_t texObj)
 {
-    if (!c_disableCudaTextures)
+    if (!c_disableCudaTextures && texObj)
     {
         CU_RET_ERR(cudaDestroyTextureObject(texObj), "cudaDestroyTextureObject on texObj failed");
     }
