dad5f85782dff0bda225cbcf8a5e9266efd33d8e
Pascal Merz <pascal.merz@me.com>
2020-02-20 18:11:13 -0700

    Use legacy code path for t_graph (modular simulator)
    This change redirects simulations which will require a t_graph object
    to the legacy code path. It also adds an assert to catch cases in which
    the redirection would fail in a more graceful way.
    
    Fixes #3389
    
    Change-Id: I17a70e504c206b2798cd2439c0c0ff5d5cd112e3

--- src/gromacs/modularsimulator/modularsimulator.cpp
+++ src/gromacs/modularsimulator/modularsimulator.cpp
@@ -53,6 +53,7 @@
 #include "gromacs/mdlib/checkpointhandler.h"
 #include "gromacs/mdlib/constr.h"
 #include "gromacs/mdlib/energyoutput.h"
+#include "gromacs/mdlib/forcerec.h"
 #include "gromacs/mdlib/mdatoms.h"
 #include "gromacs/mdlib/resethandler.h"
 #include "gromacs/mdlib/stat.h"
@@ -944,6 +945,10 @@ bool ModularSimulator::isInputCompatible(bool                             exitOn
             isInputCompatible
             && conditionalAssert(!doMembed,
                                  "Membrane embedding is not supported by the modular simulator.");
+    const bool useGraph = !areMoleculesDistributedOverPbc(*inputrec, globalTopology, MDLogger());
+    isInputCompatible =
+            isInputCompatible
+            && conditionalAssert(!useGraph, "Graph is not supported by the modular simulator.");
     // TODO: Change this to the boolean passed when we merge the user interface change for the GPU update.
     isInputCompatible =
             isInputCompatible
