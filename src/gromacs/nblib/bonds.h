6e22e49305b82fd54b816c93a8d9e65374709248
Victor Holanda <victorusu@gmail.com>
2020-02-25 14:35:52 +0100

    Introduce Bond classes
    This introduces the Harmonic, G96 and Half-attractive quartic bond
    types.

---
+++ src/gromacs/nblib/bonds.h
@@ -0,0 +1,108 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2020, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \inpublicapi \file
+ * \brief
+ * Implements nblib simulation box
+ *
+ * \author Victor Holanda <victor.holanda@cscs.ch>
+ * \author Joe Jordan <ejjordan@kth.se>
+ * \author Prashanth Kanduri <kanduri@cscs.ch>
+ * \author Sebastian Keller <keller@cscs.ch>
+ * \author Artem Zhmurov <zhmurov@gmail.com>
+ */
+#ifndef GROMACS_BONDS_H
+#define GROMACS_BONDS_H
+
+#include <vector>
+
+#include "atomtype.h"
+
+namespace nblib
+{
+using ParticleName  = std::string;
+using ForceConstant = real;
+using EquilDistance = real;
+
+//! Harmonic bond type
+//
+// It represents the interaction of the form
+// V(r; forceConstant, equilDistance) = 0.5 * forceConstant * (r - equilDistance)^2
+class HarmonicBond
+{
+public:
+    HarmonicBond(ParticleName atomName, ForceConstant forceConstant, EquilDistance equilDistance);
+
+private:
+    ParticleName _atomName;
+    ForceConstant _forceConstant;
+    EquilDistance _equilDistance;
+
+};
+
+
+//! GROMOS bond type
+//
+// It represents the interaction of the form
+// V(r; forceConstant, equilDistance) = 0.25 * forceConstant * (r^2 - equilDistance^2)^2
+class G96Bond
+{
+public:
+    G96Bond(ParticleName atomName, ForceConstant forceConstant, EquilDistance equilDistance);
+
+private:
+    ParticleName _atomName;
+    ForceConstant _forceConstant;
+    EquilDistance _equilDistance;
+
+};
+
+//! Half-attractive quartic bond type
+//
+// It represents the interaction of the form
+// V(r; forceConstant, equilDistance) = 0.5 * forceConstant * (r - equilDistance)^4
+class HalfAttractiveQuartic
+{
+public:
+    HalfAttractiveQuartic(ParticleName atomName, ForceConstant forceConstant, EquilDistance equilDistance);
+
+private:
+    ParticleName _atomName;
+    ForceConstant _forceConstant;
+    EquilDistance _equilDistance;
+
+};
+
+} // namespace nblib
+#endif // GROMACS_BONDS_H
