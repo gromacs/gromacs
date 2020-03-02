7ea1936afeacc064ce3f9d3e31ccfd28435ce81f
Berk Hess <hess@kth.se>
2020-03-02 16:20:26 +0100

    Fix too small pairlist buffer on Intel GPUs
    The pairlist buffer generated for Intel GPUs was slightly too small,
    because it assumed a 4x4 atom-cluster pair kernel instead of 4x2.
    This also fixes incorrect kernel setup messages in the log file.
    
    Fixes #3407
    
    Change-Id: I744f98a934b5d9f60e0708cb064571aa6e389c44

--- src/gromacs/nbnxm/nbnxm_geometry.h
+++ src/gromacs/nbnxm/nbnxm_geometry.h
@@ -1,7 +1,8 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2016 by the GROMACS development team.
+ * Copyright (c) 2017,2018,2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -81,7 +82,7 @@ static constexpr gmx::EnumerationArray<KernelType, int> JClusterSizePerKernelTyp
 #else
       0, 0,
 #endif
-      c_nbnxnGpuClusterSize, c_nbnxnGpuClusterSize }
+      c_nbnxnGpuClusterSize, c_nbnxnGpuClusterSize / 2 }
 };
 
 /* Returns whether the pair-list corresponding to nb_kernel_type is simple */
