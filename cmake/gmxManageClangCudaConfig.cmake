gromacs/gromacs

commit 8ff984a32037947c7224cb46e72627d32ba5d81e

Fix log file flag output
Fixes #3099

Change-Id: I5049e8807b87c83959a0a2a5b3439b904d5edc8b

cmake/gmxManageGPU.cmake
@@ -148,11 +148,12 @@ endif()
 # Note that semicolon is used as separator for nvcc.
 #
 # Parameters:
-#   COMPILER_INFO   - [output variable] string with compiler path, ID and
-#                     some compiler-provided information
-#   COMPILER_FLAGS  - [output variable] flags for the compiler
+#   COMPILER_INFO         - [output variable] string with compiler path, ID and
+#                           some compiler-provided information
+#   DEVICE_COMPILER_FLAGS - [output variable] device flags for the compiler
+#   HOST_COMPILER_FLAGS   - [output variable] host flags for the compiler, if propagated
 #
-macro(get_cuda_compiler_info COMPILER_INFO COMPILER_FLAGS)
+macro(get_cuda_compiler_info COMPILER_INFO DEVICE_COMPILER_FLAGS HOST_COMPILER_FLAGS)
     if(NOT GMX_CLANG_CUDA)
         if(CUDA_NVCC_EXECUTABLE)
 
@@ -171,9 +172,11 @@ macro(get_cuda_compiler_info COMPILER_INFO COMPILER_FLAGS)
                 string(TOUPPER ${CMAKE_BUILD_TYPE} _build_type)
                 SET(_compiler_flags "${CUDA_NVCC_FLAGS_${_build_type}}")
                 if(CUDA_PROPAGATE_HOST_FLAGS)
-                    string(REGEX REPLACE "[ ]+" ";" _cxx_flags_nospace "${BUILD_CXXFLAGS}")
+                    set(${HOST_COMPILER_FLAGS} BUILD_CXXFLAGS)
+                else()
+                    set(${HOST_COMPILER_FLAGS} "")
                 endif()
-                SET(${COMPILER_FLAGS} "${CUDA_NVCC_FLAGS}${CUDA_NVCC_FLAGS_${_build_type}}; ${_cxx_flags_nospace}")
+                SET(${DEVICE_COMPILER_FLAGS} "${CUDA_NVCC_FLAGS}${CUDA_NVCC_FLAGS_${_build_type}}")
             else()
                 SET(${COMPILER_INFO} "N/A")
                 SET(${COMPILER_FLAGS} "N/A")

docs/doxygen/check-source.py
@@ -136,6 +136,19 @@ def check_include(fileobj, includedfile, reporter):
             return
         reporter.code_issue(includedfile,
                 "includes local file as {0}".format(includedfile))
+    # To report compiler flags in the log file, include files must be
+    # generated by CMake, with a name that is only known at generation
+    # time (not even configuration time). This looks like the
+    # inclusion of a non-local file to the source checker. We usually
+    # suppress issues with generated files using file-name matching,
+    # but this is not suitable in this case because it is hard to map
+    # the repository file named src/compilerflaginfo.h.cmakein to two
+    # generated header files
+    # $builddir/src/compilerflaginfo-$buildtype-CXX.h and
+    # $builddir/src/compilerflaginfo-$buildtype-C.h. So we use a hack
+    # to just avoid the warning for this special source file.
+    if "compilerflaginfo" in str(includedfile):
+        return
     if not otherfile:
         reporter.code_issue(includedfile,
                 "includes non-local file as {0}".format(includedfile))

src/CMakeLists.txt
@@ -36,20 +36,29 @@
 # Output compiler and CFLAGS used
 ######################################
 include(GetCompilerInfo.cmake)
-get_compiler_info(C BUILD_C_COMPILER BUILD_CFLAGS)
-get_compiler_info(CXX BUILD_CXX_COMPILER BUILD_CXXFLAGS)
+get_compiler_info(C BUILD_C_COMPILER)
+get_compiler_info(CXX BUILD_CXX_COMPILER)
 if(GMX_USE_CUDA)
     if(NOT GMX_CLANG_CUDA)
         GMX_SET_CUDA_NVCC_FLAGS()
     endif()
 
-    get_cuda_compiler_info(CUDA_COMPILER_INFO CUDA_COMPILER_FLAGS)
+    get_cuda_compiler_info(CUDA_COMPILER_INFO CUDA_DEVICE_COMPILER_FLAGS CUDA_HOST_COMPILER_FLAGS)
 endif()
 
 string(TOUPPER "${CMAKE_BUILD_TYPE}" CMAKE_BUILD_TYPE_UPPER)
 configure_file(config.h.cmakein config.h)
 configure_file(gmxpre-config.h.cmakein gmxpre-config.h)
 configure_file(buildinfo.h.cmakein buildinfo.h ESCAPE_QUOTES)
+# Make a file with compiler flags used for libgromacs for each
+# langauge and build configuration.  The one that corresponds to
+# CMAKE_BUILD_TYPE is #included into buildinfo.h and populates the
+# fields e.g. printed to the log file.
+file(GENERATE
+    OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/compilerflaginfo-$<CONFIG>-$<COMPILE_LANGUAGE>.h
+    INPUT ${CMAKE_CURRENT_SOURCE_DIR}/compilerflaginfo.h.cmakein
+    CONDITION $<CONFIG:${CMAKE_BUILD_TYPE}>
+    )
 
 ####
 set(IGNORED_CLANG_ALL_WARNINGS

src/GetCompilerInfo.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012,2013,2014, by the GROMACS development team, led by
+# Copyright (c) 2012,2013,2014,2019, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -39,10 +39,7 @@
 #   LANGUAGE       - C or CXX, the compiler to check for
 #   BUILD_COMPILER - [output variable] string with compiler path, ID and
 #                    some compiler-provided information
-#   BUILD_FLAGS    - [output variable] flags for the compiler
 #
-macro(get_compiler_info LANGUAGE BUILD_COMPILER BUILD_FLAGS)
+macro(get_compiler_info LANGUAGE BUILD_COMPILER)
     set(${BUILD_COMPILER} "${CMAKE_${LANGUAGE}_COMPILER} ${CMAKE_${LANGUAGE}_COMPILER_ID} ${CMAKE_${LANGUAGE}_COMPILER_VERSION}")
-    string(TOUPPER "${CMAKE_BUILD_TYPE}" _build_type)
-    set(${BUILD_FLAGS} "${CMAKE_${LANGUAGE}_FLAGS} ${CMAKE_${LANGUAGE}_FLAGS_${_build_type}}")
 endmacro()

src/buildinfo.h.cmakein
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2012,2013,2014,2015,2017,2018, by the GROMACS development team, led by
+ * Copyright (c) 2012,2013,2014,2015,2017,2018,2019, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -42,14 +42,12 @@
 /** C compiler used to build */
 #define BUILD_C_COMPILER        "@BUILD_C_COMPILER@"
 
-/** C compiler flags used to build */
-#define BUILD_CFLAGS            "@BUILD_CFLAGS@"
+#include "compilerflaginfo-@CMAKE_BUILD_TYPE@-C.h"
 
 /** C++ compiler flags used to build, or empty string if no C++ */
 #define BUILD_CXX_COMPILER      "@BUILD_CXX_COMPILER@"
 
-/** C++ compiler flags used to build */
-#define BUILD_CXXFLAGS          "@BUILD_CXXFLAGS@"
+#include "compilerflaginfo-@CMAKE_BUILD_TYPE@-CXX.h"
 
 /** Installation prefix (default location of data files) */
 #define CMAKE_INSTALL_PREFIX    "@CMAKE_INSTALL_PREFIX@"
@@ -72,8 +70,10 @@
 /** CUDA compiler version information */
 #define CUDA_COMPILER_INFO "@CUDA_COMPILER_INFO@"
 
-/** CUDA compiler flags */
-#define CUDA_COMPILER_FLAGS "@CUDA_COMPILER_FLAGS@"
+/** CUDA compiler flags (device flags, plus host flags if propagated)*/
+#define CUDA_DEVICE_COMPILER_FLAGS "@CUDA_DEVICE_COMPILER_FLAGS@"
+#define CUDA_HOST_COMPILER_FLAGS @CUDA_HOST_COMPILER_FLAGS@
+#define CUDA_COMPILER_FLAGS CUDA_DEVICE_COMPILER_FLAGS CUDA_HOST_COMPILER_FLAGS
 
 /** OpenCL include dir */
 #define OPENCL_INCLUDE_DIR "@OpenCL_INCLUDE_DIR@"

src/compilerflaginfo.h.cmakein
@@ -1,0 +1,43 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2019, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+/*! \internal \file
+ * \brief
+ * Compiler flag information from the build system.
+ *
+ * Used for log output.
+ */
+
+/** $<COMPILE_LANGUAGE> compiler flags used to build */
+#define BUILD_$<COMPILE_LANGUAGE>FLAGS "$<JOIN:$<TARGET_PROPERTY:libgromacs,COMPILE_OPTIONS>, >"

commit 3c325b04a6ba328429cd4cd35f6864816d424017

Fix Clang Cuda flags
- Add -fcuda-flush-denormals-to-zero to match nvcc flags
- Fix flag reporting. Was broken by 8ff984a32037947
  which didn't make the change consistent for clang cuda.

cmake/gmxManageClangCudaConfig.cmake
@@ -80,7 +80,7 @@ if (GMX_CUDA_TARGET_SM)
 endif()
 
 # default flags
-list(APPEND _CUDA_CLANG_FLAGS "-x cuda" "-ffast-math")
+list(APPEND _CUDA_CLANG_FLAGS "-x cuda" "-ffast-math" "-fcuda-flush-denormals-to-zero")
 # Workaround for clang>=9 (Bug 45533). No CUDA file uses OpenMP.
 list(APPEND _CUDA_CLANG_FLAGS "-fno-openmp")
 # CUDA toolkit

cmake/gmxManageGPU.cmake
@@ -1,7 +1,7 @@
 #
 # This file is part of the GROMACS molecular simulation package.
 #
-# Copyright (c) 2012,2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
+# Copyright (c) 2012,2013,2014,2015,2016,2017,2018,2019,2020, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -186,7 +186,8 @@ macro(get_cuda_compiler_info COMPILER_INFO DEVICE_COMPILER_FLAGS HOST_COMPILER_F
         # CXX compiler is the CUDA compiler
         set(${COMPILER_INFO} "${CMAKE_CXX_COMPILER}  ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}")
         # there are some extra flags
-        set(${COMPILER_FLAGS} "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${_build_type}} ${GMX_CUDA_CLANG_FLAGS}")
+        set(${HOST_COMPILER_FLAGS} BUILD_CXXFLAGS)
+        set(${DEVICE_COMPILER_FLAGS} "${GMX_CUDA_CLANG_FLAGS}")
     endif()
 endmacro ()
 