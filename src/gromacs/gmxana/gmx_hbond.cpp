657bfbda22f12a048996fd6eb722b0c4fb12a558
Paul Bauer <paul.bauer.q@gmail.com>
2020-06-11 12:31:09 +0000

    Fix false initialization values in gmx hbond
    Previous refactoring left some variables in an uninitialized state or
    with values from previous code paths.
    
    Fixes #3550
    
    Change-Id: I43257a0aa6292aa462de41f0e6d60b50d6ea3cf4

--- src/gromacs/gmxana/gmx_hbond.cpp
+++ src/gromacs/gmxana/gmx_hbond.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2008, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017,2018,2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -3411,6 +3411,7 @@ int gmx_hbond(int argc, char* argv[])
                 mat.ny = hb->nrhb;
 
                 mat.matrix.resize(mat.nx, mat.ny);
+                mat.axis_x.resize(mat.nx);
                 for (auto& value : mat.matrix.toArrayRef())
                 {
                     value = 0;
@@ -3448,11 +3449,15 @@ int gmx_hbond(int argc, char* argv[])
                 mat.label_y = bContact ? "Contact Index" : "Hydrogen Bond Index";
                 mat.bDiscrete = true;
                 mat.map.resize(2);
-                for (auto& m : mat.map)
                 {
-                    m.code.c1 = hbmap[i];
-                    m.desc    = hbdesc[i];
-                    m.rgb     = hbrgb[i];
+                    int i = 0;
+                    for (auto& m : mat.map)
+                    {
+                        m.code.c1 = hbmap[i];
+                        m.desc    = hbdesc[i];
+                        m.rgb     = hbrgb[i];
+                        i++;
+                    }
                 }
                 fp = opt2FILE("-hbm", NFILE, fnm, "w");
                 write_xpm_m(fp, mat);
