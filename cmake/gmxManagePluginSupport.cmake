649af69b44d50355387c9ac1e6e68ad243ca2e88
Erik Lindahl <erik.lindahl@gmail.com>
2020-06-15 09:21:48 +0000

    Replace trivalue options with plain booleans
    Our CMake auto-detection has been a long discussion since
    it both complicates the CMake code and leads to complex
    interactions when multiple auto-settings interact with
    each other, and when/how things can change.
    
    This is a first step towards removing such auto settings,
    by altering "nice to have, but not required" to "off" by
    default (GMX_BUILD_HELP, GMX_HWLOC).
    
    The option GMX_LOAD_PLUGINS was on by default, and
    the build generates errors if it's turned off, so for now
    I set it to "on", and will adress the compile error when
    it's disabled in a separate change.
    
    With these changes, the trivalue options are no more.

--- cmake/gmxManagePluginSupport.cmake
+++ cmake/gmxManagePluginSupport.cmake
@@ -32,67 +32,41 @@
 # To help us fund GROMACS development, we humbly ask that you cite
 # the research papers on the package. Check out http://www.gromacs.org.
 
-include(gmxOptionUtilities)
+# Executing this macro will add the option GMX_USE_PLUGINS, which
+# makes it possible to dynamically load modules at runtime.
+# This is a very neat feature, and should virtually always work
+# on Linux, but for now it will not work without shared libraries.
+# For this reason we disable it by default, to avoid triggering
+# errors here when dynamic libraries are disabled elsewhere.
+macro(gmx_manage_plugin_support)
+    option(GMX_USE_PLUGINS "Enable support for dynamic plugins (e.g. VMD-supported file formats)" OFF)
+    mark_as_advanced(GMX_USE_PLUGINS)
 
-# Sets GMX_USE_PLUGINS=ON in the parent scope if the toolchain and
-# user selections permit the build to support plugin loading.
-function(gmx_manage_plugin_support)
-    option(GMX_LOAD_PLUGINS "Compile with plugin support, needed to read VMD supported file formats" ON)
-    mark_as_advanced(GMX_LOAD_PLUGINS)
+    if(GMX_USE_PLUGINS)
 
-    # Find out if non-Windows builds can support plugins. Native Windows
-    # neither needs nor has library support.
-    if (NOT WIN32)
-        # TODO Make a proper find_package for dlopen to find
-        # dlfcn.h. The CMake variable CMAKE_DL_LIBS works magically
-        # for the library, however.
-        include(gmxTestdlopen)
-        gmx_test_dlopen(HAVE_DLOPEN)
-    endif()
+        message(STATUS "Checking build environment for dynamic plugins")
 
-    # Keep the status line quiet unless something relevant has
-    # changed.
-    gmx_check_if_changed(EMIT_STATUS_MESSAGES GMX_LOAD_PLUGINS BUILD_SHARED_LIBS HAVE_DLOPEN)
-
-    # Whether GROMACS will really try to compile support for VMD
-    # plugins.
-    set(GMX_USE_PLUGINS OFF)
-
-    # Plugins are supported natively on Windows
-    if (WIN32 OR (BUILD_SHARED_LIBS AND HAVE_DLOPEN))
-        set(GMX_USE_PLUGINS ${GMX_LOAD_PLUGINS})
-    elseif(GMX_LOAD_PLUGINS)
-        # Can't support plugins for some reason. If the user required
-        # plugins, emit fatal errors. Otherwise, emit status messages
-        # for AUTO and be silent for OFF.
-        set(message "")
-        if (NOT HAVE_DLOPEN)
-            set(message "${message}dlopen() support for using dynamic plugins for VMD-supported file formats is missing. ")
-        endif()
         if(NOT BUILD_SHARED_LIBS)
-            set(message "${message}GROMACS only supports plugins in a build that uses shared libraries, which can be disabled for various reasons. BUILD_SHARED_LIBS=on and a toolchain that supports dynamic linking is required. (Hint: GMX_PREFER_STATIC_LIBS and GMX_BUILD_MDRUN_ONLY can influence the default BUILD_SHARED_LIBS, so if you need plugins, reconsider those choices.) ")
+            message(FATAL_ERROR "Shared libraries not built - required for dynamic plugins")
         endif()
-        if (GMX_LOAD_PLUGINS_FORCE)
-            message(FATAL_ERROR "${message}Cannot build with GMX_LOAD_PLUGINS=${GMX_LOAD_PLUGINS}.")
+
+        # Plugins are supported natively on Windows, so nothing to check if WIN32 is set
+
+        if (NOT WIN32)
+            # TODO Make a proper find_package for dlopen to find
+            # dlfcn.h. The CMake variable CMAKE_DL_LIBS works magically
+            # for the library, however.
+            include(gmxTestdlopen)
+            gmx_test_dlopen(HAVE_DLOPEN)
+	    if(NOT HAVE_DLOPEN)
+	        message(FATAL_ERROR "dlopen() support missing - required for dynamic plugins")
+            endif()
         endif()
-        if (GMX_LOAD_PLUGINS_AUTO AND EMIT_STATUS_MESSAGES)
-            message(STATUS "${message}")
-        endif()
+
+	message(STATUS "Checking build environment for dynamic plugins - supported")
+
+	list(APPEND GMX_EXTRA_LIBRARIES ${CMAKE_DL_LIBS}) # magic cross-platform pre-set variable for dlopen library
+	set(PKG_DL_LIBS "-l${CMAKE_DL_LIBS}")
+
     endif()
-
-    if(EMIT_STATUS_MESSAGES)
-        if(GMX_USE_PLUGINS)
-            MESSAGE(STATUS "Using dynamic plugins (e.g VMD-supported file formats)")
-        else()
-            MESSAGE(STATUS "Not using dynamic plugins (e.g VMD-supported file formats)")
-        endif()
-    endif()
-    set(GMX_USE_PLUGINS ${GMX_USE_PLUGINS} PARENT_SCOPE)
-endfunction()
-
-gmx_manage_plugin_support()
-
-if(GMX_USE_PLUGINS)
-    list(APPEND GMX_EXTRA_LIBRARIES ${CMAKE_DL_LIBS}) # magic cross-platform pre-set variable for dlopen library
-    set(PKG_DL_LIBS "-l${CMAKE_DL_LIBS}")
-endif()
+endmacro()
