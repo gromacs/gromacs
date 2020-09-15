gromacs/gromacs

commit e0f481ae943b4fac5ddd3c6ed7f1af981f1d7de6

Divide default communicator from DD communicators
The communicators mpi_comm_mysim and mpi_comm_mygroup inside
t_commrec got initialized in init_commrec (to MPI_COMM_WORLD
if no multisim, to a subset otherwise). These communicators
were then used in subsequent setup work, before they got
reassigned during the construction of the DDBuilder object
and the construction of the actual domain decomposition object.
Effectively, this means that the same communicators (and, hence,
identical function calls) do very different things depending on
whether they get used before or after the setup of domain
decomposition. It also means that before DD set up, mpi_comm_mysim
and mpi_comm_mygroup are *identical*.

This change introduces an additional communicator within
t_commrec, mpiDefaulCommunicator, which helps to make these
implicit assumptions explicit. Consequently, this also redefines
PAR(cr), MASTER(cr), and SIMMASTER(cr).

This change will allow to move the sim and group communicators,
which are now only created at DD time, into the DD object,
logically separating the DD object from t_commrec.

Refs #2395

src/gromacs/mdrun/runner.cpp
@@ -832,8 +832,9 @@ int Mdrunner::mdrunner()
         physicalNodeComm = PhysicalNodeCommunicator(communicator, gmx_physicalnode_id_hash());
     }
 
-    GMX_RELEASE_ASSERT(communicator == MPI_COMM_WORLD, "Must have valid world communicator");
-    CommrecHandle crHandle = init_commrec(communicator, ms);
+    GMX_RELEASE_ASSERT(ms || communicator == MPI_COMM_WORLD,
+                       "Must have valid world communicator unless running a multi-simulation");
+    CommrecHandle crHandle = init_commrec(communicator);
     t_commrec*    cr       = crHandle.get();
     GMX_RELEASE_ASSERT(cr != nullptr, "Must have valid commrec");
 
@@ -1248,7 +1249,7 @@ int Mdrunner::mdrunner()
                 .appendTextFormatted(
                         "This is simulation %d out of %d running as a composite GROMACS\n"
                         "multi-simulation job. Setup for this simulation:\n",
-                        ms->sim, ms->nsim);
+                        ms->simulationIndex_, ms->numSimulations_);
     }
     GMX_LOG(mdlog.warning)
             .appendTextFormatted("Using %d MPI %s\n", cr->nnodes,

src/gromacs/mdrun/simulationcontext.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2018,2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -60,7 +60,9 @@ SimulationContext::SimulationContext(MPI_Comm                          communica
                        "Without it, the communicator must be null.");
     if (!multiSimDirectoryNames.empty())
     {
-        multiSimulation_ = std::make_unique<gmx_multisim_t>(communicator, multiSimDirectoryNames);
+        multiSimulation_ = buildMultiSimulation(communicator, multiSimDirectoryNames);
+        // Use the communicator resulting from the split for the multi-simulation.
+        communicator_ = multiSimulation_->simulationComm_;
     }
 }
 

src/gromacs/applied_forces/tests/electricfield.cpp
@@ -109,7 +109,7 @@ public:
         std::vector<real> chargeA{ 1 };
         md.homenr                   = ssize(chargeA);
         md.chargeA                  = chargeA.data();
-        CommrecHandle      cr       = init_commrec(MPI_COMM_WORLD, nullptr);
+        CommrecHandle      cr       = init_commrec(MPI_COMM_WORLD);
         matrix             boxDummy = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
         ForceProviderInput forceProviderInput({}, md, 0.0, boxDummy, *cr);
 

src/gromacs/awh/awh.cpp
@@ -149,7 +149,7 @@ Awh::Awh(FILE*                 fplog,
     int numSharingSimulations = 1;
     if (awhParams.shareBiasMultisim && isMultiSim(multiSimRecord_))
     {
-        numSharingSimulations = multiSimRecord_->nsim;
+        numSharingSimulations = multiSimRecord_->numSimulations_;
     }
 
     /* Initialize all the biases */

src/gromacs/awh/biassharing.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2017,2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2017,2018,2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -83,7 +83,7 @@ void biasesAreCompatibleForSharingBetweenSimulations(const AwhParams&           
                                                      const std::vector<size_t>& pointSize,
                                                      const gmx_multisim_t*      multiSimComm)
 {
-    const int numSim = multiSimComm->nsim;
+    const int numSim = multiSimComm->numSimulations_;
 
     /* We currently enforce subsequent shared biases to have consecutive
      * share-group values starting at 1. This means we can reduce shared
@@ -105,7 +105,7 @@ void biasesAreCompatibleForSharingBetweenSimulations(const AwhParams&           
         }
     }
     std::vector<int> numShareAll(numSim);
-    numShareAll[multiSimComm->sim] = numShare;
+    numShareAll[multiSimComm->simulationIndex_] = numShare;
     gmx_sumi_sim(numShareAll.size(), numShareAll.data(), multiSimComm);
     for (int sim = 1; sim < numSim; sim++)
     {
@@ -117,8 +117,8 @@ void biasesAreCompatibleForSharingBetweenSimulations(const AwhParams&           
     }
 
     std::vector<int> intervals(numSim * 2);
-    intervals[numSim * 0 + multiSimComm->sim] = awhParams.nstSampleCoord;
-    intervals[numSim * 1 + multiSimComm->sim] = awhParams.numSamplesUpdateFreeEnergy;
+    intervals[numSim * 0 + multiSimComm->simulationIndex_] = awhParams.nstSampleCoord;
+    intervals[numSim * 1 + multiSimComm->simulationIndex_] = awhParams.numSamplesUpdateFreeEnergy;
     gmx_sumi_sim(intervals.size(), intervals.data(), multiSimComm);
     for (int sim = 1; sim < numSim; sim++)
     {
@@ -142,7 +142,7 @@ void biasesAreCompatibleForSharingBetweenSimulations(const AwhParams&           
         if (awhParams.awhBiasParams[b].shareGroup > 0)
         {
             std::vector<int64_t> pointSizes(numSim);
-            pointSizes[multiSimComm->sim] = pointSize[b];
+            pointSizes[multiSimComm->simulationIndex_] = pointSize[b];
             gmx_sumli_sim(pointSizes.size(), pointSizes.data(), multiSimComm);
             for (int sim = 1; sim < numSim; sim++)
             {

src/gromacs/awh/biasstate.cpp
@@ -153,9 +153,9 @@ void sumPmf(gmx::ArrayRef<PointState> pointState,
     {
         return;
     }
-    GMX_ASSERT(multiSimComm != nullptr && numSharedUpdate % multiSimComm->nsim == 0,
-               "numSharedUpdate should be a multiple of multiSimComm->nsim");
-    GMX_ASSERT(numSharedUpdate == multiSimComm->nsim,
+    GMX_ASSERT(multiSimComm != nullptr && numSharedUpdate % multiSimComm->numSimulations_ == 0,
+               "numSharedUpdate should be a multiple of multiSimComm->numSimulations_");
+    GMX_ASSERT(numSharedUpdate == multiSimComm->numSimulations_,
                "Sharing within a simulation is not implemented (yet)");
 
     std::vector<double> buffer(pointState.size());
@@ -734,7 +734,7 @@ void sumHistograms(gmx::ArrayRef<PointState> pointState,
     /* Sum histograms over multiple simulations if needed. */
     if (numSharedUpdate > 1)
     {
-        GMX_ASSERT(numSharedUpdate == multiSimComm->nsim,
+        GMX_ASSERT(numSharedUpdate == multiSimComm->numSimulations_,
                    "Sharing within a simulation is not implemented (yet)");
 
         /* Collect the weights and counts in linear arrays to be able to use gmx_sumd_sim. */

src/gromacs/gmxlib/network.cpp
@@ -47,7 +47,6 @@
 #include <cstring>
 
 #include "gromacs/commandline/filenm.h"
-#include "gromacs/mdrunutility/multisim.h"
 #include "gromacs/mdtypes/commrec.h"
 #include "gromacs/utility/basenetwork.h"
 #include "gromacs/utility/cstringutil.h"
@@ -55,14 +54,13 @@
 #include "gromacs/utility/futil.h"
 #include "gromacs/utility/gmxassert.h"
 #include "gromacs/utility/gmxmpi.h"
-#include "gromacs/utility/mpiinplacebuffers.h"
 #include "gromacs/utility/real.h"
 #include "gromacs/utility/smalloc.h"
 
 /* The source code in this file should be thread-safe.
       Please keep it that way. */
 
-CommrecHandle init_commrec(MPI_Comm communicator, const gmx_multisim_t* ms)
+CommrecHandle init_commrec(MPI_Comm communicator)
 {
     CommrecHandle handle;
     t_commrec*    cr;
@@ -83,22 +81,10 @@ CommrecHandle init_commrec(MPI_Comm communicator, const gmx_multisim_t* ms)
     sizeOfCommunicator = 1;
 #endif
 
-    if (ms != nullptr)
-    {
-#if GMX_MPI
-        MPI_Comm_split(communicator, ms->sim, rankInCommunicator, &cr->mpiDefaultCommunicator);
-        cr->sizeOfDefaultCommunicator = sizeOfCommunicator / ms->nsim;
-        MPI_Comm_rank(cr->mpiDefaultCommunicator, &cr->rankInDefaultCommunicator);
-#else
-        gmx_fatal(FARGS, "Multisim can only run with MPI.");
-#endif
-    }
-    else
-    {
-        cr->mpiDefaultCommunicator    = communicator;
-        cr->sizeOfDefaultCommunicator = sizeOfCommunicator;
-        cr->rankInDefaultCommunicator = rankInCommunicator;
-    }
+    cr->mpiDefaultCommunicator    = communicator;
+    cr->sizeOfDefaultCommunicator = sizeOfCommunicator;
+    cr->rankInDefaultCommunicator = rankInCommunicator;
+
     // For now, we want things to go horribly wrong if this is used too early...
     // TODO: Remove when communicators are removed from commrec (#2395)
     cr->nnodes           = -1;
@@ -109,19 +95,6 @@ CommrecHandle init_commrec(MPI_Comm communicator, const gmx_multisim_t* ms)
 
     // TODO cr->duty should not be initialized here
     cr->duty = (DUTY_PP | DUTY_PME);
-
-#if GMX_MPI && !MPI_IN_PLACE_EXISTS
-    /* initialize the MPI_IN_PLACE replacement buffers */
-    snew(cr->mpb, 1);
-    cr->mpb->ibuf        = nullptr;
-    cr->mpb->libuf       = nullptr;
-    cr->mpb->fbuf        = nullptr;
-    cr->mpb->dbuf        = nullptr;
-    cr->mpb->ibuf_alloc  = 0;
-    cr->mpb->libuf_alloc = 0;
-    cr->mpb->fbuf_alloc  = 0;
-    cr->mpb->dbuf_alloc  = 0;
-#endif
 
     return handle;
 }
@@ -135,7 +108,6 @@ void done_commrec(t_commrec* cr)
             // TODO: implement
             // done_domdec(cr->dd);
         }
-        done_mpi_in_place_buf(cr->mpb);
     }
 #if GMX_MPI
     // TODO We need to be able to free communicators, but the

src/gromacs/gmxlib/network.h
@@ -49,7 +49,6 @@
 #include "gromacs/utility/stringutil.h"
 #include "gromacs/utility/unique_cptr.h"
 
-struct gmx_multisim_t;
 struct t_commrec;
 struct t_filenm;
 
@@ -60,7 +59,7 @@ void done_commrec(t_commrec* cr);
 using CommrecHandle = gmx::unique_cptr<t_commrec, done_commrec>;
 
 //! Allocate, initialize and return the commrec.
-CommrecHandle init_commrec(MPI_Comm communicator, const gmx_multisim_t* ms);
+CommrecHandle init_commrec(MPI_Comm communicator);
 
 struct t_commrec* reinitialize_commrec_for_this_thread(const t_commrec* cro);
 

commit afa36e60916b72cb3c3bc8d1006e76e44d5c7162

Fix task assignment with multiruns
Change e0f481ae broke the physical node hardware detection and task
assignment by passing the simulation instead of the world communicator
to the creation of the physical node communicator. Now the world
communicator is stored and passed.

Fixes #3666

src/gromacs/mdrun/runner.cpp
@@ -352,6 +352,7 @@ Mdrunner Mdrunner::cloneOnSpawnedThread() const
     newRunner.pforce          = pforce;
     // Give the spawned thread the newly created valid communicator
     // for the simulation.
+    newRunner.worldCommunicator   = MPI_COMM_WORLD;
     newRunner.communicator        = MPI_COMM_WORLD;
     newRunner.ms                  = ms;
     newRunner.startingBehavior    = startingBehavior;
@@ -398,7 +399,8 @@ void Mdrunner::spawnThreads(int numThreadsToLaunch)
 
     // Give the master thread the newly created valid communicator for
     // the simulation.
-    communicator = MPI_COMM_WORLD;
+    worldCommunicator = MPI_COMM_WORLD;
+    communicator      = MPI_COMM_WORLD;
     threadMpiMdrunnerAccessBarrier();
 #else
     GMX_UNUSED_VALUE(numThreadsToLaunch);
@@ -773,7 +775,7 @@ int Mdrunner::mdrunner()
     // this is expressed, e.g. by expressly running detection only the
     // master rank for thread-MPI, rather than relying on the mutex
     // and reference count.
-    PhysicalNodeCommunicator physicalNodeComm(communicator, gmx_physicalnode_id_hash());
+    PhysicalNodeCommunicator physicalNodeComm(worldCommunicator, gmx_physicalnode_id_hash());
     hwinfo = gmx_detect_hardware(mdlog, physicalNodeComm);
 
     gmx_print_detected_hardware(fplog, isSimulationMasterRank && isMasterSim(ms), mdlog, hwinfo);
@@ -844,7 +846,7 @@ int Mdrunner::mdrunner()
         spawnThreads(hw_opt.nthreads_tmpi);
         // The spawned threads enter mdrunner() and execution of
         // master and spawned threads joins at the end of this block.
-        physicalNodeComm = PhysicalNodeCommunicator(communicator, gmx_physicalnode_id_hash());
+        physicalNodeComm = PhysicalNodeCommunicator(worldCommunicator, gmx_physicalnode_id_hash());
     }
 
     GMX_RELEASE_ASSERT(ms || communicator == MPI_COMM_WORLD,
@@ -1908,6 +1910,9 @@ private:
     //! Command-line override for the duration of a neighbor list with the Verlet scheme.
     int nstlist_ = 0;
 
+    //! World communicator, used for hardware detection and task assignment
+    MPI_Comm worldCommunicator_ = MPI_COMM_NULL;
+
     //! Multisim communicator handle.
     gmx_multisim_t* multiSimulation_;
 
@@ -1957,8 +1962,9 @@ Mdrunner::BuilderImplementation::BuilderImplementation(std::unique_ptr<MDModules
                                                        compat::not_null<SimulationContext*> context) :
     mdModules_(std::move(mdModules))
 {
-    communicator_    = context->communicator_;
-    multiSimulation_ = context->multiSimulation_.get();
+    worldCommunicator_ = context->worldCommunicator_;
+    communicator_      = context->simulationCommunicator_;
+    multiSimulation_   = context->multiSimulation_.get();
 }
 
 Mdrunner::BuilderImplementation::~BuilderImplementation() = default;
@@ -2007,6 +2013,8 @@ Mdrunner Mdrunner::BuilderImplementation::build()
     newRunner.replExParams = replicaExchangeParameters_;
 
     newRunner.filenames = filenames_;
+
+    newRunner.worldCommunicator = worldCommunicator_;
 
     newRunner.communicator = communicator_;
 

src/gromacs/mdrun/runner.h
@@ -261,6 +261,13 @@ private:
     //! \brief Non-owning handle to file used for logging.
     t_fileio* logFileHandle = nullptr;
 
+    /*! \brief Non-owning handle to world communication data structure for task assigment.
+     *
+     * With real MPI, gets a value from the SimulationContext
+     * supplied to the MdrunnerBuilder. With thread-MPI gets a
+     * value after threads have been spawned. */
+    MPI_Comm worldCommunicator = MPI_COMM_NULL;
+
     /*! \brief Non-owning handle to communication data structure.
      *
      * With real MPI, gets a value from the SimulationContext

src/gromacs/mdrun/simulationcontext.cpp
@@ -50,19 +50,23 @@
 namespace gmx
 {
 //! \cond libapi
-SimulationContext::SimulationContext(MPI_Comm                          communicator,
+SimulationContext::SimulationContext(MPI_Comm                          worldCommunicator,
                                      const ArrayRef<const std::string> multiSimDirectoryNames) :
-    communicator_(communicator)
+    worldCommunicator_(worldCommunicator)
 {
-    GMX_RELEASE_ASSERT((GMX_LIB_MPI && (communicator != MPI_COMM_NULL))
-                               || (!GMX_LIB_MPI && (communicator == MPI_COMM_NULL)),
+    GMX_RELEASE_ASSERT((GMX_LIB_MPI && (worldCommunicator != MPI_COMM_NULL))
+                               || (!GMX_LIB_MPI && (worldCommunicator == MPI_COMM_NULL)),
                        "With real MPI, a non-null communicator is required. "
                        "Without it, the communicator must be null.");
-    if (!multiSimDirectoryNames.empty())
+    if (multiSimDirectoryNames.empty())
     {
-        multiSimulation_ = buildMultiSimulation(communicator, multiSimDirectoryNames);
+        simulationCommunicator_ = worldCommunicator;
+    }
+    else
+    {
+        multiSimulation_ = buildMultiSimulation(worldCommunicator, multiSimDirectoryNames);
         // Use the communicator resulting from the split for the multi-simulation.
-        communicator_ = multiSimulation_->simulationComm_;
+        simulationCommunicator_ = multiSimulation_->simulationComm_;
     }
 }
 

src/gromacs/mdrun/simulationcontext.h
@@ -108,10 +108,26 @@ public:
     /*!
      * \brief Construct
      *
-     * \param communicator            MPI communicator for this (set of) simulations
+     * \param worldCommunicator       MPI communicator for this simulation context
      * \param multiSimDirectoryNames  Names of any directories used with -multidir
      */
-    explicit SimulationContext(MPI_Comm communicator, ArrayRef<const std::string> multiSimDirectoryNames);
+    explicit SimulationContext(MPI_Comm                    worldCommunicator,
+                               ArrayRef<const std::string> multiSimDirectoryNames);
+
+    /*!
+     * \brief MPI communicator object for this GROMACS instance.
+     *
+     * With real MPI,
+     *   the gmx wrapper binary has called MPI_Init, thus
+     *     MPI_COMM_WORLD is now valid to use, and
+     *   (in future) the gmxapi runner will handle such details
+     *     (e.g. via mpi4py) before creating its SimulationContext.
+     *
+     * With thread-MPI in both cases, the communicator is set up later
+     * during the process of spawning the threads that will be the MPI
+     * ranks. (Multi-simulation is not supported with thread-MPI.)
+     */
+    MPI_Comm worldCommunicator_ = MPI_COMM_NULL;
 
     /*!
      * \brief MPI communicator object for this simulation object.
@@ -121,18 +137,12 @@ public:
      *     MPI_COMM_WORLD is now valid to use, and
      *   (in future) the gmxapi runner will handle such details
      *     (e.g. via mpi4py) before creating its SimulationContext.
-     * In both cases, if a multi-simulation is in use, then its
-     * communicator(s) are found in multiSimulation_. This
-     * communicator is that of all ranks from all simulations, and
-     * will later be split into one for each simulation.
-     * TODO Perhaps (for simplicity) that communicator splitting
-     * task can be undertaken during multi-sim setup.
      *
      * With thread-MPI in both cases, the communicator is set up later
      * during the process of spawning the threads that will be the MPI
      * ranks. (Multi-simulation is not supported with thread-MPI.)
      */
-    MPI_Comm communicator_ = MPI_COMM_NULL;
+    MPI_Comm simulationCommunicator_ = MPI_COMM_NULL;
 
     /*!
      * \brief Multi-sim handler (if required by e.g. gmx mdrun