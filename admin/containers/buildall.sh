gromacs/gromacs

commit 48f9056c196fda338b83e74212d4f9bc5ab3474e

Allow simpler use of scripted container builds
There's no need to have a list of supported versions of things in the
script that supports building CI images. We should have a list, e.g.
in the install guide, but not here.

One of the primary use cases for the script is building new
images. Those are most often needed for new versions of things, and
expanding the supported versions list is needless work.

Removed use of const and relaxed types for versions that e.g. need
not be integers.

Made suggestions in the doc strings of valid choices.

Note, had to force-push this branch to get out of non-linear merge
history hell.

Fixes #3622

admin/containers/utility.py
@@ -58,21 +58,16 @@ parsers for tools.
     Instead, inherit from it with the *parents* argument to :py:class:`argparse.ArgumentParser`
 """
 
-# TODO: Try using distutils.version.StrictVersion.
 parser.add_argument('--cmake', type=str, default='3.13.0',
-                    choices=['3.9.6', '3.11.4', '3.13.0', '3.14.7', '3.15.7', '3.16.6', '3.17.2'],
                     help='Selection of CMake version to provide to base image')
 compiler_group = parser.add_mutually_exclusive_group()
 compiler_group.add_argument('--gcc', type=int, nargs='?', const=7, default=7,
-                            choices=[5, 6, 7, 8, 9],
                             help='Select GNU compiler tool chain. (Default) '
                                  'Some checking is implemented to avoid incompatible combinations')
 compiler_group.add_argument('--llvm', type=str, nargs='?', const='7', default=None,
-                            choices=['3.6', '6', '7', '8', '9'],
                             help='Select LLVM compiler tool chain. '
                                  'Some checking is implemented to avoid incompatible combinations')
 compiler_group.add_argument('--icc', type=int, nargs='?', const=19, default=None,
-                            choices=[19, 20],
                             help='Select Intel compiler tool chain. '
                                  'Some checking is implemented to avoid incompatible combinations')
 # TODO currently the installation merely gets the latest beta version of oneAPI,
@@ -82,31 +77,24 @@ compiler_group.add_argument('--oneapi', type=str, nargs='?', const="", default="
                             help='Select Intel oneAPI package version.')
 
 linux_group = parser.add_mutually_exclusive_group()
-linux_group.add_argument('--ubuntu', type=str, nargs='?', const='18.04', default='20.04',
-                         choices=['16.04', '18.04', '20.04'],
+linux_group.add_argument('--ubuntu', type=str, nargs='?', const='20.04', default='20.04',
                          help='Select Ubuntu Linux base image. (default: ubuntu 20.04)')
 linux_group.add_argument('--centos', type=str, nargs='?', const='7', default=None,
-                         choices=['6', '7'],
                          help='Select Centos Linux base image.')
 
 parser.add_argument('--cuda', type=str, nargs='?', const='10.2', default=None,
-                    choices=['9.0', '10.0', '10.1', '10.2', '11.0'],
                     help='Select a CUDA version for a base Linux image from NVIDIA.')
 
 parser.add_argument('--mpi', type=str, nargs='?', const='openmpi', default=None,
-                    choices=['openmpi', 'impi'],
                     help='Enable MPI (default disabled) and optionally select distribution (default: openmpi)')
 
 parser.add_argument('--tsan', type=str, nargs='?', const='llvm', default=None,
-                    choices=['llvm'],
                     help='Build special compiler versions with TSAN OpenMP support')
 
 parser.add_argument('--opencl', type=str, nargs='?', const='nvidia', default=None,
-                    choices=['nvidia', 'intel', 'amd'],
                     help='Provide environment for OpenCL builds')
 
 parser.add_argument('--clfft', type=str, nargs='?', const='master', default=None,
-                    choices=['master', 'develop'],
                     help='Add external clFFT libraries to the build image')
 
 parser.add_argument('--doxygen', type=str, nargs='?', const='1.8.5', default=None,

commit 5b707a66ea5e6f6ba8c2018cb032645c9c308aa7

Fix script building all CI containers and add gcovr
Additions in 48f9056c196fda338b83e74212d4f9bc5ab3474e broke the script
by changing the default values, while
e9cb08d17b06bc5649d316ee06159b633db9b3d8 inadvertently made all images
use the oneapi images as base.

Also add gcovr tool to documentation images, preparing for coverage
builds.

Change-Id: I0eb9a3bf1e8b45dd4b4a3a41fd214678c4c1c4c2

admin/containers/buildall.sh
@@ -51,7 +51,7 @@ tags[${#tags[@]}]=$tag
 python3 $SCRIPT --cmake 3.17.2 --llvm --doxygen | docker build -t $tag -
 
 echo "Run the following to upload the updated images."
-echo docker login
+echo "docker login"
 for tag in "${tags[@]}"; do
-  echo docker push $tag
+  echo "docker push $tag"
 done

admin/containers/scripted_gmx_docker_builds.py
@@ -174,6 +174,7 @@ def get_llvm_packages(args) -> typing.Iterable[str]:
     # If we use the package version of LLVM, we need to install extra packages for it.
     if (args.llvm is not None) and (args.tsan is None):
         return ['libomp-dev',
+                'libomp5',
                 'clang-format-' + str(args.llvm),
                 'clang-tidy-' + str(args.llvm)]
     else:
@@ -475,7 +476,7 @@ def add_documentation_dependencies(input_args,
         return
     output_stages['main'] += hpccm.primitives.shell(
         commands=['sed -i \'/\"XPS\"/d;/\"PDF\"/d;/\"PS\"/d;/\"EPS\"/d;/disable ghostscript format types/d\' /etc/ImageMagick-6/policy.xml'])
-    output_stages['main'] += hpccm.building_blocks.pip(pip='pip3', packages=['sphinx==1.6.1'])
+    output_stages['main'] += hpccm.building_blocks.pip(pip='pip3', packages=['sphinx==1.6.1', 'gcovr'])
     if input_args.doxygen == '1.8.5':
         doxygen_commit = 'ed4ed873ab0e7f15116e2052119a6729d4589f7a'
         output_stages['main'] += hpccm.building_blocks.generic_autotools(

admin/containers/utility.py
@@ -73,12 +73,13 @@ compiler_group.add_argument('--icc', type=int, nargs='?', const=19, default=None
 # TODO currently the installation merely gets the latest beta version of oneAPI,
 # not a specific version. GROMACS probably doesn't need to address that until
 # oneAPI makes an official release.
-compiler_group.add_argument('--oneapi', type=str, nargs='?', const="", default="2021.1-beta08",
+compiler_group.add_argument('--oneapi', type=str, nargs='?', const="2021.1-beta08", default=None,
                             help='Select Intel oneAPI package version.')
 
 linux_group = parser.add_mutually_exclusive_group()
-linux_group.add_argument('--ubuntu', type=str, nargs='?', const='20.04', default='20.04',
-                         help='Select Ubuntu Linux base image. (default: ubuntu 20.04)')
+# Ubuntu 20+ is not yet tested. See issue #3680
+linux_group.add_argument('--ubuntu', type=str, nargs='?', const='18.04', default='18.04',
+                         help='Select Ubuntu Linux base image. (default: ubuntu 18.04)')
 linux_group.add_argument('--centos', type=str, nargs='?', const='7', default=None,
                          help='Select Centos Linux base image.')
 