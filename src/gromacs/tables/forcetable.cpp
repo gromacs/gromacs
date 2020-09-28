a41b39b5c1eefda7814ccc2019b5446aa5f7710c
Berk Hess <hess@kth.se>
2020-09-28 14:24:26 +0000

    Fix dispersion correction table generation
    Dispersion correction table generation was incorrectly skipped
    during PME tuning, which lead to an assertion failure with LJ-PME.
    
    Fixes #3677

--- src/gromacs/tables/forcetable.cpp
+++ src/gromacs/tables/forcetable.cpp
@@ -3,7 +3,8 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017 by the GROMACS development team.
+ * Copyright (c) 2018,2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1465,11 +1466,6 @@ makeDispersionCorrectionTable(FILE* fp, const interaction_const_t* ic, real rtab
 {
     GMX_RELEASE_ASSERT(ic->vdwtype != evdwUSER || tabfn,
                        "With VdW user tables we need a table file name");
-
-    if (tabfn == nullptr)
-    {
-        return std::unique_ptr<t_forcetable>(nullptr);
-    }
 
     t_forcetable* fullTable = make_tables(fp, ic, tabfn, rtab, 0);
     /* Copy the contents of the table to one that has just dispersion
