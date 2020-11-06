8be699d265de8b0aed52ee2467425b77f3da9fc6
Andrey Alekseenko <al42and@gmail.com>
2020-11-06 13:19:56 +0100

    Suggest environment variable to limit visible SYCL devices
    As with OpenCL, there is no portable way to do it, so we suggest the
    currently working Intel option.

--- src/gromacs/taskassignment/decidegpuusage.cpp
+++ src/gromacs/taskassignment/decidegpuusage.cpp
@@ -97,8 +97,10 @@ const char* g_specifyEverythingFormatString =
         // is AMD OpenCL, which offers this variable.
         "GPU_DEVICE_ORDINAL"
 #    elif GMX_GPU_SYCL
-        // SYCL-TODO:
-        "How to restrict visible devices in SYCL?"
+        // As with OpenCL, there are no portable way to do it.
+        // Intel reference: https://github.com/intel/llvm/blob/sycl/sycl/doc/EnvironmentVariables.md
+        // While SYCL_DEVICE_FILTER is a better option, as of 2021.1-beta10 it is not yet supported.
+        "SYCL_DEVICE_ALLOWLIST"
 #    else
 #        error "Unreachable branch"
 #    endif
