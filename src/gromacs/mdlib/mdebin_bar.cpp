dd801b38ce79bcd58a70474a9d9bc97ba9c65d16
Berk Hess <hess@kth.se>
2020-11-09 21:37:31 +0100

    Fix dHdl and foreign energy clearing at checkpointing
    The code copying dH/dl and foreign lambda energies to the checkpoint
    data structures copied the wrong way around leading to zero values
    being written to energy file for steps in between the last energy
    frame and the checkpoint.
    
    Fixes #3763

--- src/gromacs/mdlib/mdebin_bar.cpp
+++ src/gromacs/mdlib/mdebin_bar.cpp
@@ -3,7 +3,8 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013,2014,2015,2016,2017,2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2013,2014,2015,2016,2017 by the GROMACS development team.
+ * Copyright (c) 2018,2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -729,7 +730,7 @@ void mde_delta_h_coll_update_energyhistory(const t_mde_delta_h_coll* dhc, energy
     {
         std::vector<real>& dh = deltaH->dh[i];
         dh.resize(dhc->dh[i].ndh);
-        std::copy(dh.begin(), dh.end(), dhc->dh[i].dh);
+        std::copy(dhc->dh[i].dh, dhc->dh[i].dh + dhc->dh[i].ndh, dh.begin());
     }
     deltaH->start_time   = dhc->start_time;
     deltaH->start_lambda = dhc->start_lambda;
