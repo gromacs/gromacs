gromacs/gromacs

commit 01b2f20bd58829f2e51d8622faa7bcf0a203b196

Fused GPU bonded kernels
To reduce the GPU kernel launch times the
GPU bonded kernels have been fused to a single kernel.

Refs #2818

Change-Id: I2299e1857df5db469739e3aefbc0f771968a6bd5

src/gromacs/listed_forces/gpubonded_impl.cu
@@ -39,6 +39,7 @@
  * \author Berk Hess <hess@kth.se>
  * \author Szilárd Páll <pall.szilard@gmail.com>
  * \author Mark Abraham <mark.j.abraham@gmail.com>
+ * \author Magnus Lundborg <lundborg.magnus@gmail.com>
  *
  * \ingroup module_listed_forces
  */
@@ -47,6 +48,7 @@
 
 #include "gpubonded_impl.h"
 
+#include "gromacs/gpu_utils/cuda_arch_utils.cuh"
 #include "gromacs/gpu_utils/cudautils.cuh"
 #include "gromacs/gpu_utils/devicebuffer.h"
 #include "gromacs/gpu_utils/gpu_vec.cuh"
@@ -85,6 +87,18 @@ GpuBonded::Impl::Impl(const gmx_ffparams_t &ffparams,
         iListsDevice[ftype].nr     = 0;
         iListsDevice[ftype].iatoms = nullptr;
         iListsDevice[ftype].nalloc = 0;
+    }
+
+    kernelParams_.forceparamsDevice      = forceparamsDevice;
+    kernelParams_.xqDevice               = xqDevice;
+    kernelParams_.forceDevice            = forceDevice;
+    kernelParams_.fshiftDevice           = fshiftDevice;
+    kernelParams_.vtotDevice             = vtotDevice;
+    for (int i = 0; i < nFtypesOnGpu; i++)
+    {
+        kernelParams_.iatoms[i]          = nullptr;
+        kernelParams_.ftypeRangeStart[i] = 0;
+        kernelParams_.ftypeRangeEnd[i]   = -1;
     }
 }
 
@@ -136,6 +150,20 @@ static void convertIlistToNbnxnOrder(const t_ilist       &src,
     }
 }
 
+//! Returns \p input rounded up to the closest multiple of \p factor.
+static inline int roundUpToFactor(const int input, const int factor)
+{
+    GMX_ASSERT(factor > 0, "The factor to round up to must be > 0.");
+
+    int remainder = input % factor;
+
+    if (remainder == 0)
+    {
+        return (input);
+    }
+    return (input + (factor - remainder));
+}
+
 // TODO Consider whether this function should be a factory method that
 // makes an object that is the only one capable of the device
 // operations needed for the lifetime of an interaction list. This
@@ -143,7 +171,12 @@ static void convertIlistToNbnxnOrder(const t_ilist       &src,
 // of naming this method for the problem of what to name the
 // BondedDeviceInteractionListHandler type.
 
-//! Divides bonded interactions over threads and GPU
+/*! Divides bonded interactions over threads and GPU.
+ *  The bonded interactions are assigned by interaction type to GPU threads. The intereaction
+ *  types are assigned in blocks sized as <warp_size>. The beginning and end (thread index) of each
+ *  interaction type are stored in kernelParams_. Pointers to the relevant data structures on the
+ *  GPU are also stored in kernelParams_.
+ */
 void
 GpuBonded::Impl::updateInteractionListsAndDeviceBuffers(ArrayRef<const int>  nbnxnAtomOrder,
                                                         const t_idef        &idef,
@@ -153,6 +186,7 @@ GpuBonded::Impl::updateInteractionListsAndDeviceBuffers(ArrayRef<const int>  nbn
 {
     // TODO wallcycle sub start
     haveInteractions_ = false;
+    int ftypesCounter = 0;
 
     for (int ftype : ftypesOnGpu)
     {
@@ -189,11 +223,39 @@ GpuBonded::Impl::updateInteractionListsAndDeviceBuffers(ArrayRef<const int>  nbn
                                0, iList.size(),
                                stream, GpuApiCallBehavior::Async, nullptr);
         }
+        kernelParams_.ftypesOnGpu[ftypesCounter]   = ftype;
+        kernelParams_.nrFTypeIAtoms[ftypesCounter] = iList.size();
+        int nBonds = iList.size() / (interaction_function[ftype].nratoms + 1);
+        kernelParams_.nrFTypeBonds[ftypesCounter]  = nBonds;
+        kernelParams_.iatoms[ftypesCounter]        = iListsDevice[ftype].iatoms;
+        if (ftypesCounter == 0)
+        {
+            kernelParams_.ftypeRangeStart[ftypesCounter] = 0;
+        }
+        else
+        {
+            kernelParams_.ftypeRangeStart[ftypesCounter] = kernelParams_.ftypeRangeEnd[ftypesCounter - 1] + 1;
+        }
+        kernelParams_.ftypeRangeEnd[ftypesCounter] = kernelParams_.ftypeRangeStart[ftypesCounter] + roundUpToFactor(nBonds, warp_size) - 1;
+
+        GMX_ASSERT(nBonds > 0 || kernelParams_.ftypeRangeEnd[ftypesCounter] <= kernelParams_.ftypeRangeStart[ftypesCounter],
+                   "Invalid GPU listed forces setup. nBonds must be > 0 if there are threads allocated to do work on that interaction function type.");
+        GMX_ASSERT(kernelParams_.ftypeRangeStart[ftypesCounter] % warp_size == 0 && (kernelParams_.ftypeRangeEnd[ftypesCounter] + 1) % warp_size == 0,
+                   "The bonded interactions must be assigned to the GPU in blocks of warp size.");
+
+        ftypesCounter++;
     }
 
     xqDevice     = static_cast<float4 *>(xqDevicePtr);
     forceDevice  = static_cast<fvec *>(forceDevicePtr);
     fshiftDevice = static_cast<fvec *>(fshiftDevicePtr);
+
+    kernelParams_.xqDevice          = xqDevice;
+    kernelParams_.forceDevice       = forceDevice;
+    kernelParams_.fshiftDevice      = fshiftDevice;
+    kernelParams_.forceparamsDevice = forceparamsDevice;
+    kernelParams_.vtotDevice        = vtotDevice;
+
     // TODO wallcycle sub stop
 }
 

src/gromacs/listed_forces/gpubonded.h
@@ -60,11 +60,11 @@ struct t_forcerec;
 struct t_idef;
 struct t_inputrec;
 
+/*! \brief The number on bonded function types supported on GPUs */
+static constexpr int nFtypesOnGpu = 8;
+
 namespace gmx
 {
-
-/*! \brief The number on bonded function types supported on GPUs */
-constexpr int c_numFtypesOnGpu = 8;
 
 /*! \brief List of all bonded function types supported on GPUs
  *
@@ -73,7 +73,7 @@ constexpr int c_numFtypesOnGpu = 8;
  * \note The function types in the list are ordered on increasing value.
  * \note Currently bonded are only supported with CUDA, not with OpenCL.
  */
-constexpr std::array<int, c_numFtypesOnGpu> ftypesOnGpu =
+constexpr std::array<int, nFtypesOnGpu> ftypesOnGpu =
 {
     F_BONDS,
     F_ANGLES,
@@ -131,10 +131,10 @@ class GpuBonded
         /*! \brief Returns whether there are bonded interactions
          * assigned to the GPU */
         bool haveInteractions() const;
-        /*! \brief Launches bonded kernels on a GPU */
-        void launchKernels(const t_forcerec *fr,
-                           int               forceFlags,
-                           const matrix      box);
+        /*! \brief Launches bonded kernel on a GPU */
+        void launchKernel(const t_forcerec *fr,
+                          int               forceFlags,
+                          const matrix      box);
         /*! \brief Launches the transfer of computed bonded energies. */
         void launchEnergyTransfer();
         /*! \brief Waits on the energy transfer, and accumulates bonded energies to \c enerd. */

src/gromacs/listed_forces/gpubonded_impl.cpp
@@ -192,9 +192,9 @@ GpuBonded::haveInteractions() const
 }
 
 void
-GpuBonded::launchKernels(const t_forcerec * /* fr */,
-                         int            /* forceFlags */,
-                         const matrix   /* box */)
+GpuBonded::launchKernel(const t_forcerec * /* fr */,
+                        int            /* forceFlags */,
+                        const matrix   /* box */)
 {
 }
 

src/gromacs/listed_forces/gpubonded_impl.h
@@ -52,6 +52,7 @@
 #include "gromacs/gpu_utils/gputraits.cuh"
 #include "gromacs/gpu_utils/hostallocator.h"
 #include "gromacs/listed_forces/gpubonded.h"
+#include "gromacs/pbcutil/pbc_aiuc.h"
 #include "gromacs/topology/idef.h"
 
 struct gmx_ffparams_t;
@@ -71,6 +72,57 @@ struct HostInteractionList
 
     //! List of interactions, see \c HostInteractionLists
     HostVector<int> iatoms = {{}, gmx::HostAllocationPolicy(gmx::PinningPolicy::PinnedIfSupported)};
+};
+
+/* \brief Bonded parameters and GPU pointers
+ *
+ * This is used to accumulate all the parameters and pointers so they can be passed
+ * to the GPU as a single structure.
+ *
+ */
+struct BondedCudaKernelParameters
+{
+    //! Periodic boundary data
+    PbcAiuc             pbcAiuc;
+    //! Scale factor
+    float               scaleFactor;
+    //! The bonded types on GPU
+    int                 ftypesOnGpu[nFtypesOnGpu];
+    //! The number of interaction atom (iatom) elements for every function type
+    int                 nrFTypeIAtoms[nFtypesOnGpu];
+    //! The number of bonds for every function type
+    int                 nrFTypeBonds[nFtypesOnGpu];
+    //! The start index in the range of each interaction type
+    int                 ftypeRangeStart[nFtypesOnGpu];
+    //! The end index in the range of each interaction type
+    int                 ftypeRangeEnd[nFtypesOnGpu];
+
+    //! Force parameters (on GPU)
+    t_iparams          *forceparamsDevice;
+    //! Coordinates before the timestep (on GPU)
+    const float4       *xqDevice;
+    //! Forces on atoms (on GPU)
+    fvec               *forceDevice;
+    //! Force shifts on atoms (on GPU)
+    fvec               *fshiftDevice;
+    //! Total Energy (on GPU)
+    float              *vtotDevice;
+    //! Interaction list atoms (on GPU)
+    t_iatom            *iatoms[nFtypesOnGpu];
+
+    BondedCudaKernelParameters()
+    {
+        matrix boxDummy = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
+
+        setPbcAiuc(0, boxDummy, &pbcAiuc);
+
+        scaleFactor       = 1.0;
+        forceparamsDevice = nullptr;
+        xqDevice          = nullptr;
+        forceDevice       = nullptr;
+        fshiftDevice      = nullptr;
+        vtotDevice        = nullptr;
+    }
 };
 
 /*! \internal \brief Implements GPU bondeds */
@@ -96,11 +148,11 @@ class GpuBonded::Impl
                                                     void                *forceDevice,
                                                     void                *fshiftDevice);
 
-        /*! \brief Launches bonded kernels on a GPU */
+        /*! \brief Launches bonded kernel on a GPU */
         template <bool calcVir, bool calcEner>
         void
-        launchKernels(const t_forcerec *fr,
-                      const matrix      box);
+        launchKernel(const t_forcerec *fr,
+                     const matrix      box);
         /*! \brief Returns whether there are bonded interactions
          * assigned to the GPU */
         bool haveInteractions() const;
@@ -136,6 +188,9 @@ class GpuBonded::Impl
 
         //! \brief Bonded GPU stream, not owned by this module
         CommandStream         stream;
+
+        //! Parameters and pointers, passed to the CUDA kernel
+        BondedCudaKernelParameters kernelParams_;
 };
 
 }   // namespace gmx

src/gromacs/mdlib/sim_util.cpp
@@ -1079,7 +1079,7 @@ void do_force(FILE                                     *fplog,
         if (ppForceWorkload->haveGpuBondedWork && !havePPDomainDecomposition(cr))
         {
             wallcycle_sub_start(wcycle, ewcsLAUNCH_GPU_BONDED);
-            fr->gpuBonded->launchKernels(fr, flags, box);
+            fr->gpuBonded->launchKernel(fr, flags, box);
             wallcycle_sub_stop(wcycle, ewcsLAUNCH_GPU_BONDED);
         }
 
@@ -1146,7 +1146,7 @@ void do_force(FILE                                     *fplog,
             if (ppForceWorkload->haveGpuBondedWork)
             {
                 wallcycle_sub_start(wcycle, ewcsLAUNCH_GPU_BONDED);
-                fr->gpuBonded->launchKernels(fr, flags, box);
+                fr->gpuBonded->launchKernel(fr, flags, box);
                 wallcycle_sub_stop(wcycle, ewcsLAUNCH_GPU_BONDED);
             }
 

commit 8aa14d11ff775055794360a655fe800deea298a8

Stage bonded kernel atomics through shared memory
Fixes performance bug introduced in 01b2f20bd5 by staging energy step
atomics through shared memory rather than have all threads write
atomically directly to global memory.

Fixes #3443

src/gromacs/listed_forces/gpubonded_impl.cu
@@ -115,6 +115,8 @@ GpuBonded::Impl::Impl(const gmx_ffparams_t& ffparams,
     kernelLaunchConfig_.gridSize[0]  = (fTypeRangeEnd + c_threadsPerBlock) / c_threadsPerBlock;
     kernelLaunchConfig_.gridSize[1]  = 1;
     kernelLaunchConfig_.gridSize[2]  = 1;
+    kernelLaunchConfig_.sharedMemorySize =
+            SHIFTS * sizeof(float3) + (c_threadsPerBlock / warp_size) * 3 * sizeof(float);
 }
 
 GpuBonded::Impl::~Impl()

src/gromacs/listed_forces/gpubondedkernels.cu
@@ -723,11 +723,15 @@ template<bool calcVir, bool calcEner>
 __global__ void exec_kernel_gpu(BondedCudaKernelParameters kernelParams)
 {
     assert(blockDim.y == 1 && blockDim.z == 1);
-    const int  tid          = blockIdx.x * blockDim.x + threadIdx.x;
-    float      vtot_loc     = 0;
-    float      vtotVdw_loc  = 0;
-    float      vtotElec_loc = 0;
-    __shared__ float3 sm_fShiftLoc[SHIFTS];
+    const int tid          = blockIdx.x * blockDim.x + threadIdx.x;
+    float     vtot_loc     = 0;
+    float     vtotVdw_loc  = 0;
+    float     vtotElec_loc = 0;
+
+    extern __shared__ char sm_dynamicShmem[];
+    char*                  sm_nextSlotPtr = sm_dynamicShmem;
+    float3*                sm_fShiftLoc   = (float3*)sm_nextSlotPtr;
+    sm_nextSlotPtr += SHIFTS * sizeof(float3);
 
     if (calcVir)
     {
@@ -802,9 +806,42 @@ __global__ void exec_kernel_gpu(BondedCudaKernelParameters kernelParams)
     {
         float* vtotVdw  = kernelParams.d_vTot + F_LJ14;
         float* vtotElec = kernelParams.d_vTot + F_COUL14;
-        atomicAdd(kernelParams.d_vTot + fType, vtot_loc);
-        atomicAdd(vtotVdw, vtotVdw_loc);
-        atomicAdd(vtotElec, vtotElec_loc);
+
+        // Stage atomic accumulation through shared memory:
+        // each warp will accumulate its own partial sum
+        // and then a single thread per warp will accumulate this to the global sum
+
+        int numWarps = blockDim.x / warpSize;
+        int warpId   = threadIdx.x / warpSize;
+
+        // Shared memory variables to hold block-local partial sum
+        float* sm_vTot = (float*)sm_nextSlotPtr;
+        sm_nextSlotPtr += numWarps * sizeof(float);
+        float* sm_vTotVdw = (float*)sm_nextSlotPtr;
+        sm_nextSlotPtr += numWarps * sizeof(float);
+        float* sm_vTotElec = (float*)sm_nextSlotPtr;
+
+        if (threadIdx.x % warpSize == 0)
+        {
+            // One thread per warp initializes to zero
+            sm_vTot[warpId]     = 0.;
+            sm_vTotVdw[warpId]  = 0.;
+            sm_vTotElec[warpId] = 0.;
+        }
+        __syncwarp(); // All threads in warp must wait for initialization
+
+        // Perform warp-local accumulation in shared memory
+        atomicAdd(sm_vTot + warpId, vtot_loc);
+        atomicAdd(sm_vTotVdw + warpId, vtotVdw_loc);
+        atomicAdd(sm_vTotElec + warpId, vtotElec_loc);
+
+        __syncwarp(); // Ensure all threads in warp have completed
+        if (threadIdx.x % warpSize == 0)
+        { // One thread per warp accumulates partial sum into global sum
+            atomicAdd(kernelParams.d_vTot + fType, sm_vTot[warpId]);
+            atomicAdd(vtotVdw, sm_vTotVdw[warpId]);
+            atomicAdd(vtotElec, sm_vTotElec[warpId]);
+        }
     }
     /* Accumulate shift vectors from shared memory to global memory on the first SHIFTS threads of the block. */
     if (calcVir)