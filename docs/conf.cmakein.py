f96a0d2a3f11b6736a6319e17d4834e41a8f03d8
Paul Bauer <paul.bauer.q@gmail.com>
2021-01-06 08:55:14 +0100

    Add new GROMACS logo
    Replaces previous logo with new vector graphics one.
    Logo (C) by Cathrine Bergh.

--- docs/conf.cmakein.py
+++ docs/conf.cmakein.py
@@ -361,7 +361,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = 'reference-manual/plots/peregrine.png'
+latex_logo = 'reference-manual/plots/GMX_logos/gmx_falcon_blue.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
