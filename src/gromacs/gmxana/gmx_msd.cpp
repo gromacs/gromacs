27afb4718b734153b88cc1d64f10573ddb7e872a
Mark Abraham <mark.j.abraham@gmail.com>
2021-01-25 11:39:56 +0100

    Log progress from gmx msd better
    Refs #3284

--- src/gromacs/gmxana/gmx_msd.cpp
+++ src/gromacs/gmxana/gmx_msd.cpp
@@ -4,7 +4,7 @@
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
  * Copyright (c) 2013,2014,2015,2016,2017 by the GROMACS development team.
- * Copyright (c) 2018,2019,2020, by the GROMACS development team, led by
+ * Copyright (c) 2018,2019,2020,2021, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -622,6 +622,7 @@ static void printmol(t_corr*                 curr,
         }
     }
     xvgrclose(out);
+    fprintf(stdout, "Wrote per-molecule output to %s\n", fn);
     do_view(oenv, fn, "-graphtype bar");
 
     /* Compute variance, stddev and error */
@@ -648,6 +649,7 @@ static void printmol(t_corr*                 curr,
             pdbinfo[i].bfac *= scale;
         }
         write_sto_conf(fn_pdb, "molecular MSD", &top->atoms, x, nullptr, pbcType, box);
+        fprintf(stdout, "Wrote frame for -tpdb to %s\n", fn_pdb);
     }
 }
 
