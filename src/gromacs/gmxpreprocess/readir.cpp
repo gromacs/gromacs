6c10c4466121307e5842bda207034bb7a441e384
Pascal Merz <pascal.merz@me.com>
2021-02-10 23:23:24 -0700

    Disable cos acceleration for non leap-frog code paths
    The cos acceleration is only applied in leap-frog. grompp and mdrun
    would, however, happily accept non-zero cos-accel input, and report
    viscosity-related quantities without ever applying the acceleration
    for all other integrators.
    
    This change adds a check at grompp time (to avoid new tprs with
    non-implemented parameter combinations being created), and at
    runner time (to catch older tprs). Should probably be backported
    to the supported releases.
    
    Refs #3903

--- src/gromacs/gmxpreprocess/readir.cpp
+++ src/gromacs/gmxpreprocess/readir.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
- * Copyright (c) 2013-2020, by the GROMACS development team, led by
+ * Copyright (c) 2013-2020,2021, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -1402,6 +1402,12 @@ void check_ir(const char*                   mdparin,
     if (ir->bAdress)
     {
         gmx_fatal(FARGS, "AdResS simulations are no longer supported");
+    }
+
+    // cosine acceleration is only supported in leap-frog
+    if (ir->cos_accel != 0.0 && ir->eI != eiMD)
+    {
+        warning_error(wi, "cos-acceleration is only supported by integrator = md");
     }
 }
 
