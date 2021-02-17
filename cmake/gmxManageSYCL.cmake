534a3910fef75d70490e5806e23d7c8e556e4db6
Andrey Alekseenko <al42and@gmail.com>
2021-02-17 10:44:43 +0000

    Add -DGMX_SYCL_HIPSYCL cmake option
    When supplied, use hipSYCL's CMake module to set up compile options.
    Otherwise, use our own code to configure flags for Intel's LLVM-based
    SYCL compiler.

--- cmake/gmxManageSYCL.cmake
+++ cmake/gmxManageSYCL.cmake
@@ -39,82 +39,95 @@ set(REQUIRED_SYCL_MIN_VERSION ${REQUIRED_SYCL_MIN_VERSION_MAJOR}.${REQUIRED_SYCL
 
 set(GMX_GPU_SYCL ON)
 
+# CMake issue tracking the efforts to make a universal upstream module:
+# https://gitlab.kitware.com/cmake/cmake/-/issues/21711
+
+option(GMX_SYCL_HIPSYCL "Use hipSYCL instead of Intel/Clang for SYCL compilation" OFF)
+
 if(GMX_DOUBLE)
     message(FATAL_ERROR "SYCL acceleration is not available in double precision")
 endif()
 
 include(gmxFindFlagsForSource)
 
-if(CMAKE_CXX_COMPILER MATCHES "dpcpp")
-    # At least Intel dpcpp defaults to having SYCL enabled for all code. This leads to two problems:
-    #
-    # 1. Compiles take ~3x longer, since every file has to be compiled for multiple targets.
-    # 2. We get a ton of warnings for the device-specific pass when the compiler sees our SIMD code.
-    #
-    # To avoid this, we attempt to find a flag to disable SYCL for non-SYCL files. Unfortunately,
-    # when using gmx_find_flag_for_source() that includes calling check_cxx_compiler_flag(),
-    # this in turn exposes a bug in dpcpp, where an object file compiles with -fno-sycl leads to
-    # a failed link stage (when the same flag is not used). Since none of this is critical, we handle
-    # it by merely checking if it works to compile a source fils with this flag, and choking if SYCL
-    # is still enabled.
-
-    if(NOT CHECK_DISABLE_SYCL_CXX_FLAGS_QUIETLY)
-        message(STATUS "Checking for flags to disable SYCL")
+if(GMX_SYCL_HIPSYCL)
+    if(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        message(FATAL_ERROR "hipSYCL can only be built with Clang++ compiler")
     endif()
-
-    gmx_check_source_compiles_with_flags(
-        "int main() { return 0; }"
-        "-fno-sycl"
-        "CXX"
-        DISABLE_SYCL_CXX_FLAGS_RESULT)
-
-    if(DISABLE_SYCL_CXX_FLAGS_RESULT)
-        set(DISABLE_SYCL_CXX_FLAGS "-fno-sycl")
-    endif()
-    if(NOT CHECK_DISABLE_SYCL_CXX_FLAGS_QUIETLY)
-        if(DISABLE_SYCL_CXX_FLAGS_RESULT)
-            message(STATUS "Checking for flags to disable SYCL - -fno-sycl")
-        else()
-            message(WARNING "Cannot find flags to disable SYCL for non-SYCL hardware-specific C++ code. Expect many warnings, but they are likely benign.")
+    set(HIPSYCL_CLANG "${CMAKE_CXX_COMPILER}")
+    find_package(hipsycl REQUIRED)
+else()
+    if(CMAKE_CXX_COMPILER MATCHES "dpcpp")
+        # At least Intel dpcpp defaults to having SYCL enabled for all code. This leads to two problems:
+        #
+        # 1. Compiles take ~3x longer, since every file has to be compiled for multiple targets.
+        # 2. We get a ton of warnings for the device-specific pass when the compiler sees our SIMD code.
+        #
+        # To avoid this, we attempt to find a flag to disable SYCL for non-SYCL files. Unfortunately,
+        # when using gmx_find_flag_for_source() that includes calling check_cxx_compiler_flag(),
+        # this in turn exposes a bug in dpcpp, where an object file compiles with -fno-sycl leads to
+        # a failed link stage (when the same flag is not used). Since none of this is critical, we handle
+        # it by merely checking if it works to compile a source fils with this flag, and choking if SYCL
+        # is still enabled.
+    
+        if(NOT CHECK_DISABLE_SYCL_CXX_FLAGS_QUIETLY)
+            message(STATUS "Checking for flags to disable SYCL")
         endif()
-        set(CHECK_DISABLE_SYCL_CXX_FLAGS_QUIETLY 1 CACHE INTERNAL "Keep quiet on future calls to detect no-SYCL flags" FORCE)
+    
+        gmx_check_source_compiles_with_flags(
+            "int main() { return 0; }"
+            "-fno-sycl"
+            "CXX"
+            DISABLE_SYCL_CXX_FLAGS_RESULT)
+    
+        if(DISABLE_SYCL_CXX_FLAGS_RESULT)
+            set(DISABLE_SYCL_CXX_FLAGS "-fno-sycl")
+        endif()
+        if(NOT CHECK_DISABLE_SYCL_CXX_FLAGS_QUIETLY)
+            if(DISABLE_SYCL_CXX_FLAGS_RESULT)
+                message(STATUS "Checking for flags to disable SYCL - -fno-sycl")
+            else()
+                message(WARNING "Cannot find flags to disable SYCL for non-SYCL hardware-specific C++ code. Expect many warnings, but they are likely benign.")
+            endif()
+            set(CHECK_DISABLE_SYCL_CXX_FLAGS_QUIETLY 1 CACHE INTERNAL "Keep quiet on future calls to detect no-SYCL flags" FORCE)
+        endif()
     endif()
-endif()
-
-# Find the flags to enable (or re-enable) SYCL with Intel extensions. In case we turned it off above,
-# it's important that we check the combination of both flags, to make sure the second one re-enables SYCL.
-if(NOT CHECK_SYCL_CXX_FLAGS_QUIETLY)
-    message(STATUS "Checking for flags to enable SYCL")
-endif()
-gmx_find_flag_for_source(SYCL_CXX_FLAGS_RESULT
-    "#include <CL/sycl.hpp>
-     namespace sycl = cl::sycl;
-     int main(){
-         sycl::queue q(sycl::default_selector{});
-         return 0;
-     }
-     " "CXX" DISABLE_SYCL_CXX_FLAGS SYCL_CXX_FLAGS "-fsycl -fsycl-device-code-split=per_kernel")
-
-if(NOT CHECK_SYCL_CXX_FLAGS_QUIETLY)
-    if(SYCL_CXX_FLAGS_RESULT)
-        message(STATUS "Checking for flags to enable SYCL - ${SYCL_CXX_FLAGS}")
+    
+    # Find the flags to enable (or re-enable) SYCL with Intel extensions. In case we turned it off above,
+    # it's important that we check the combination of both flags, to make sure the second one re-enables SYCL.
+    if(NOT CHECK_SYCL_CXX_FLAGS_QUIETLY)
+        message(STATUS "Checking for flags to enable SYCL")
     endif()
-    set(CHECK_SYCL_CXX_FLAGS_QUIETLY 1 CACHE INTERNAL "Keep quiet on future calls to detect SYCL flags" FORCE)
-endif()
+    gmx_find_flag_for_source(SYCL_CXX_FLAGS_RESULT
+        "#include <CL/sycl.hpp>
+         namespace sycl = cl::sycl;
+         int main(){
+             sycl::queue q(sycl::default_selector{});
+             return 0;
+         }
+         " "CXX" DISABLE_SYCL_CXX_FLAGS SYCL_CXX_FLAGS "-fsycl -fsycl-device-code-split=per_kernel")
+    
+    if(NOT CHECK_SYCL_CXX_FLAGS_QUIETLY)
+        if(SYCL_CXX_FLAGS_RESULT)
+            message(STATUS "Checking for flags to enable SYCL - ${SYCL_CXX_FLAGS}")
+        endif()
+        set(CHECK_SYCL_CXX_FLAGS_QUIETLY 1 CACHE INTERNAL "Keep quiet on future calls to detect SYCL flags" FORCE)
+    endif()
+    
+    if(NOT SYCL_CXX_FLAGS_RESULT)
+        message(FATAL_ERROR "Cannot compile with SYCL Intel compiler. Try a different compiler or disable SYCL.")
+    endif()
 
-if(NOT SYCL_CXX_FLAGS_RESULT)
-    message(FATAL_ERROR "Cannot compile with SYCL Intel compiler. Try a different compiler or disable SYCL.")
+    # Add function wrapper similar to the one used by ComputeCPP and hipSYCL
+    function(add_sycl_to_target)
+        cmake_parse_arguments(
+            PARSE_ARGV 0 # No positional arguments
+            ARGS # Prefix for the resulting variables
+            "" # No options
+            "TARGET" # One-value keyword
+            "SOURCES" # Multi-value keyword
+        )
+        set_source_files_properties(${ARGS_SOURCES} PROPERTIES COMPILE_FLAGS "${SYCL_CXX_FLAGS}")
+        target_link_libraries(${ARGS_TARGET} PRIVATE ${SYCL_CXX_FLAGS})
+    endfunction(add_sycl_to_target)
 endif()
-
-# Add function wrapper similar to the one used by ComputeCPP and hipSYCL
-function(add_sycl_to_target)
-    cmake_parse_arguments(
-        PARSE_ARGV 0 # No positional arguments
-        ARGS # Prefix for the resulting variables
-        "" # No options
-        "TARGET" # One-value keyword
-        "SOURCES" # Multi-value keyword
-    )
-    set_source_files_properties(${ARGS_SOURCES} PROPERTIES COMPILE_FLAGS "${SYCL_CXX_FLAGS}")
-    target_link_libraries(${ARGS_TARGET} PRIVATE ${SYCL_CXX_FLAGS})
-endfunction(add_sycl_to_target)
