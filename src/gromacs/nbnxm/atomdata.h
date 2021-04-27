753631beedb67d3e3404073f42691eb9dc99feae
ejjordan <ejjordan@kth.se>
2021-04-27 16:45:56 +0200

    Remove leftover function declaration from nbnxm atomdata
    MR !1483 neglected to remove the declaration of function
    nbnxn_atomdata_init. This MR rectifies that. Also removed
    and unneeded include.

--- src/gromacs/nbnxm/atomdata.h
+++ src/gromacs/nbnxm/atomdata.h
@@ -52,7 +52,6 @@
 #include "gromacs/gpu_utils/hostallocator.h"
 #include "gromacs/math/vectypes.h"
 #include "gromacs/mdtypes/locality.h"
-#include "gromacs/utility/basedefinitions.h"
 #include "gromacs/utility/bitmask.h"
 #include "gromacs/utility/real.h"
 
@@ -286,7 +285,7 @@ public:
     //! The format of f, enum
     int FFormat;
     //! Do we need to update shift_vec every step?
-    gmx_bool bDynamicBox;
+    bool bDynamicBox;
     //! Shift vectors, copied from t_forcerec
     gmx::HostVector<gmx::RVec> shift_vec;
     //! stride for a coordinate in x (usually 3 or 4)
@@ -308,7 +307,7 @@ public:
     //! Reduction related data
     //! \{
     //! Use the flags or operate on all atoms
-    gmx_bool bUseBufferFlags;
+    bool bUseBufferFlags;
     //! Flags for buffer zeroing+reduc.
     std::vector<gmx_bitmask_t> buffer_flags;
     //! \}
@@ -328,22 +327,6 @@ enum
     enbnxninitcombruleNONE
 };
 
-/*! \brief Initialize the non-bonded atom data structure.
- *
- * The enum for nbatXFormat is in the file defining nbnxn_atomdata_t.
- * Copy the ntypes*ntypes*2 sized nbfp non-bonded parameter list
- * to the atom data structure.
- * enbnxninitcombrule sets what combination rule data gets stored in nbat.
- */
-void nbnxn_atomdata_init(const gmx::MDLogger&      mdlog,
-                         nbnxn_atomdata_t*         nbat,
-                         Nbnxm::KernelType         kernelType,
-                         int                       enbnxninitcombrule,
-                         int                       ntype,
-                         gmx::ArrayRef<const real> nbfp,
-                         int                       n_energygroups,
-                         int                       nout);
-
 //! Sets the atomdata after pair search
 void nbnxn_atomdata_set(nbnxn_atomdata_t*         nbat,
                         const Nbnxm::GridSet&     gridSet,
@@ -352,9 +335,7 @@ void nbnxn_atomdata_set(nbnxn_atomdata_t*         nbat,
                         gmx::ArrayRef<const int>  atomInfo);
 
 //! Copy the shift vectors to nbat
-void nbnxn_atomdata_copy_shiftvec(gmx_bool                 dynamic_box,
-                                  gmx::ArrayRef<gmx::RVec> shift_vec,
-                                  nbnxn_atomdata_t*        nbat);
+void nbnxn_atomdata_copy_shiftvec(bool dynamic_box, gmx::ArrayRef<gmx::RVec> shift_vec, nbnxn_atomdata_t* nbat);
 
 /*! \brief Transform coordinates to xbat layout
  *
