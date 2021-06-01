f293eec9426b5ae0308414fcdf471e75e3c13908
Paul Bauer <paul.bauer.q@gmail.com>
2021-06-01 14:27:23 +0000

    Fix gmx nmr -viol option

--- src/gromacs/gmxana/gmx_nmr.cpp
+++ src/gromacs/gmxana/gmx_nmr.cpp
@@ -4,7 +4,7 @@
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
  * Copyright (c) 2013,2014,2015,2016,2017 by the GROMACS development team.
- * Copyright (c) 2018,2019,2020, by the GROMACS development team, led by
+ * Copyright (c) 2018,2019,2020,2021, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -468,6 +468,11 @@ int gmx_nmr(int argc, char* argv[])
     }
     nset = 0;
 
+    if (bDisRe && (bORIRE || bOTEN))
+    {
+        gmx_fatal(FARGS, "Cannot do sum of violation (-viol) and other analysis in a single call.");
+    }
+
     fp = open_enx(ftp2fn(efEDR, NFILE, fnm), "r");
     do_enxnms(fp, &nre, &enm);
     free_enxnms(nre, enm);
@@ -619,7 +624,7 @@ int gmx_nmr(int argc, char* argv[])
         }
         nbounds = get_bounds(&bounds, &index, &pair, &npairs, top->idef);
         snew(violaver, npairs);
-        out_disre = xvgropen(opt2fn("-o", NFILE, fnm), "Sum of Violations", "Time (ps)", "nm", oenv);
+        out_disre = xvgropen(opt2fn("-viol", NFILE, fnm), "Sum of Violations", "Time (ps)", "nm", oenv);
         xvgr_legend(out_disre, 2, drleg, oenv);
         if (bDRAll)
         {
