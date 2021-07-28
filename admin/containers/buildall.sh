9e11ac5fe8de1ab0a13e6caa06b2a9d3e61edd80
Mark Abraham <mark.j.abraham@gmail.com>
2021-07-28 08:33:06 +0200

    Update to support CUDA 11.4
    Update to test the newest supported CUDA now that it can support GROMACS
    
    Refs #3912

--- admin/containers/buildall.sh
+++ admin/containers/buildall.sh
@@ -10,7 +10,7 @@ SCRIPT=$PWD/scripted_gmx_docker_builds.py
 # thread and no MPI configurations.
 
 args[${#args[@]}]="--gcc 11 --clfft --mpi openmpi --rocm"
-args[${#args[@]}]="--gcc 10 --cuda 11.2.2 --clfft --mpi openmpi"
+args[${#args[@]}]="--gcc 10 --cuda 11.4.0 --clfft --mpi openmpi"
 args[${#args[@]}]="--gcc 7 --cuda 11.0 --clfft --mpi openmpi"
 args[${#args[@]}]="--llvm 11 --tsan"
 args[${#args[@]}]="--llvm 8 --cuda 11.0 --clfft --mpi openmpi"
@@ -18,7 +18,7 @@ args[${#args[@]}]="--llvm 12 --clfft --mpi openmpi --rocm"
 args[${#args[@]}]="--oneapi 2021.1.1"
 args[${#args[@]}]="--oneapi 2021.2.0 --intel-compute-runtime 21.21.19914"
 args[${#args[@]}]="--llvm --doxygen --mpi openmpi --venvs 3.7.7"
-args[${#args[@]}]="--llvm 11 --cuda 11.2.2 --hipsycl 0bf6420aab18 --rocm 4.2"
+args[${#args[@]}]="--llvm 11 --cuda 11.4.0 --hipsycl 0bf6420aab18 --rocm 4.2"
 
 echo "Building the following images."
 for arg_string in "${args[@]}"; do
