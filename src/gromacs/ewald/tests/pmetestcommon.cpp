0e7d0872748fc3f9fb541fb0c8151e2de12bafec
Andrey Alekseenko <al42and@gmail.com>
2021-09-09 13:06:39 +0000

    Enable GPU PME unit tests

--- src/gromacs/ewald/tests/pmetestcommon.cpp
+++ src/gromacs/ewald/tests/pmetestcommon.cpp
@@ -1,7 +1,8 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2016,2017,2018,2019,2020, by the GROMACS development team, led by
+ * Copyright (c) 2016,2017,2018,2019,2020 by the GROMACS development team.
+ * Copyright (c) 2021, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -888,7 +889,7 @@ const char* codePathToString(CodePath codePath)
 PmeTestHardwareContext::PmeTestHardwareContext() : codePath_(CodePath::CPU) {}
 
 PmeTestHardwareContext::PmeTestHardwareContext(TestDevice* testDevice) :
-    codePath_(CodePath::CPU),
+    codePath_(CodePath::GPU),
     testDevice_(testDevice)
 {
     setActiveDevice(testDevice_->deviceInfo());
