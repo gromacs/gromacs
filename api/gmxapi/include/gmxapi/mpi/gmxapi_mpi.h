2d2f1768b49b903fe057881fef100109f602257e
M. Eric Irrgang <ericirrgang@gmail.com>
2021-10-08 13:05:22 +0300

    Add missing `#include`.

--- api/gmxapi/include/gmxapi/mpi/gmxapi_mpi.h
+++ api/gmxapi/include/gmxapi/mpi/gmxapi_mpi.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2020, by the GROMACS development team, led by
+ * Copyright (c) 2020,2021, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -35,6 +35,8 @@
 
 #ifndef GMXAPI_MPI_H
 #define GMXAPI_MPI_H
+
+#include <cassert>
 
 #include <memory>
 
