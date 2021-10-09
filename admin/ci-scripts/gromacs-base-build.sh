d3dc59f256b08e9e9bb7c3ea15d6cafe2cf745f7
Andrey Alekseenko <al42and@gmail.com>
2021-10-09 21:07:24 +0200

    Reduce noise in CI logs from the "for" loop
    Bash "-x" option reports every iteration of the loop. While we can unset
    it temporarily, we can get rid of the loop altogether and user find's
    built-in capabilities. It will also be more robust w.r.t. "bad"
    filenames.
    
    Follow-up of MR !2008.

--- admin/ci-scripts/gromacs-base-build.sh
+++ admin/ci-scripts/gromacs-base-build.sh
@@ -9,5 +9,5 @@ awk '/warning/,/warning.*generated|^$/' buildLogFile.log testBuildLogFile.log \
 grep "cannot be built" buildLogFile.log testBuildLogFile.log | tee -a buildErrors.log || true
 $CMAKE --build . --target install 2>&1 | tee installBuildLogFile.log
 if [ -s buildErrors.log ] ; then echo "Found compiler warning during build"; cat buildErrors.log; exit 1; fi
-for file in `find . -mindepth 1 -name "*.o" ! -type l` ; do echo $file ; rm $file ; done 2>&1 > remove-build-objects.log
+find . -mindepth 1 -name '*.o' ! -type l -printf '%p\n' -delete 2>&1 > remove-build-objects.log
 cd ..
