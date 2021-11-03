0e5d689bffc449177e948e2c3751ee6c36df71a8
M. Eric Irrgang <ericirrgang@gmail.com>
2021-11-03 15:04:56 +0300

    Let buildall.sh merely advise.
    Actually building all images could take a very long time.
    This change let's buildall.sh generate the various commands that might
    be needed in a copy-paste friendly manner.
    
    Fixes #4266

--- admin/containers/buildall.sh
+++ admin/containers/buildall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ev
+set -e
 
 SCRIPT=$PWD/scripted_gmx_docker_builds.py
 
@@ -21,23 +21,48 @@ args[${#args[@]}]="--oneapi 2021.4.0 --intel-compute-runtime"
 args[${#args[@]}]="--llvm --doxygen --mpi openmpi --venvs 3.7.7"
 args[${#args[@]}]="--llvm 12 --cuda 11.4.1 --hipsycl 4481c03 --rocm 4.3"
 
-echo "Building the following images."
+echo
+echo "Consider pulling the following images for build layer cache."
+echo
 for arg_string in "${args[@]}"; do
   # shellcheck disable=SC2086
-  python3 -m utility $arg_string
+  echo "docker pull $(python3 -m utility $arg_string)"
 done
 echo
+echo
 
+echo "To build with cache hints:"
+echo
 for arg_string in "${args[@]}"; do
   # shellcheck disable=SC2086
   tag=$(python3 -m utility $arg_string)
   tags[${#tags[@]}]=$tag
   # shellcheck disable=SC2086
-  python3 $SCRIPT $arg_string | docker build -t $tag -
+  echo "$(which python3) $SCRIPT $arg_string | docker build -t $tag --cache-from $tag -"
 done
+unset tags
+echo
+echo
+
+echo "To build without extra cache hints:"
+echo
+for arg_string in "${args[@]}"; do
+  # shellcheck disable=SC2086
+  tag=$(python3 -m utility $arg_string)
+  tags[${#tags[@]}]=$tag
+  # shellcheck disable=SC2086
+  echo "$(which python3) $SCRIPT $arg_string | docker build -t $tag -"
+done
+unset tags
+echo
+echo
 
 echo "Run the following to upload the updated images."
 echo "docker login registry.gitlab.com -u <token name> -p <hash>"
-for tag in "${tags[@]}"; do
+echo
+for arg_string in "${args[@]}"; do
+  # shellcheck disable=SC2086
+  tag=$(python3 -m utility $arg_string)
+  tags[${#tags[@]}]=$tag
   echo "docker push $tag"
 done
