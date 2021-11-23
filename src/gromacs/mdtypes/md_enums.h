gromacs/gromacs

commit 3ba95aac31a47eaf90ad7c5394b4cbe3d9a88a1a

Refactor md_enums
The old enum handling caused clang-tidy to complain about the globally
defined symbols for the enum strings, and also violated type safety by
using the enums both as enumerations and integers.

Changed all exposed enum strings and their corresponding char arrays
to classes and enumeration arrays.

Used enumeration array in a few places where it made sense to me.

Was not able to get rid of some instances where the enum to integer
conversion was abused.

Moved enum reading template to header to have it available for reading
all enums.

src/gromacs/modularsimulator/forceelement.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2019,2020, by the GROMACS development team, led by
+ * Copyright (c) 2019,2020,2021, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -51,6 +51,7 @@
 
 #include "gromacs/domdec/dlbtiming.h"
 #include "gromacs/mdtypes/md_enums.h"
+#include "gromacs/utility/enumerationhelpers.h"
 #include "gromacs/utility/real.h"
 
 #include "modularsimulatorinterfaces.h"
@@ -193,7 +194,7 @@ private:
      * Used if FEP is off, since do_force
      * requires lambda to be allocated anyway
      */
-    std::array<real, efptNR> lambda_;
+    gmx::EnumerationArray<FreeEnergyPerturbationType, real> lambda_;
 
     // Access to ISimulator data
     //! Handles logging.

commit 786988563f968d0a0adea3e18e44bb5353499a9c

Fix undersized ForceElement::lambda_ array
Lambdas are indexed with FreeEnergyPerturbationCouplingType, not
FreeEnergyPerturbationType. Since we pass around this array in a
type-unsafe manner, as a pointer, the error was not detected.

The bug was introduced back in 3ba95aac (MR !1149).

Fixes #4212, #4234.

src/gromacs/mdtypes/md_enums.h
@@ -457,7 +457,7 @@ enum class FreeEnergyPerturbationType : int
     Static,
     //! then the states change monotonically throughout the simulation
     SlowGrowth,
-    //! then expanded ensemble simulations are occuring
+    //! then expanded ensemble simulations are occurring
     Expanded,
     Count,
     Default = No
@@ -465,7 +465,7 @@ enum class FreeEnergyPerturbationType : int
 //! String corresponding to FEP type.
 const char* enumValueToString(FreeEnergyPerturbationType enumValue);
 
-//! Free energy pertubation coupling types.
+//! Free energy perturbation coupling types.
 enum class FreeEnergyPerturbationCouplingType : int
 {
     Fep,

src/gromacs/modularsimulator/forceelement.h
@@ -204,7 +204,7 @@ private:
      * Used if FEP is off, since do_force
      * requires lambda to be allocated anyway
      */
-    gmx::EnumerationArray<FreeEnergyPerturbationType, real> lambda_;
+    gmx::EnumerationArray<FreeEnergyPerturbationCouplingType, real> lambda_;
 
     // Access to ISimulator data
     //! Handles logging.