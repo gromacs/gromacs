880faad74b3c03e2780f0e91553ae9cf53311c69
Mark Abraham <mark.j.abraham@gmail.com>
2021-11-26 14:52:19 +0000

    Catch compilation errors during build rather than test phase

--- admin/ci-scripts/gromacs-base-build.sh
+++ admin/ci-scripts/gromacs-base-build.sh
@@ -10,6 +10,7 @@ awk '/warning/,/warning.*generated|^$/' buildLogFile.log testBuildLogFile.log \
       | grep -v "CMake" | tee buildErrors.log || true
 grep "cannot be built" buildLogFile.log testBuildLogFile.log | tee -a buildErrors.log || true
 grep "fatal error" buildLogFile.log testBuildLogFile.log | tee -a buildErrors.log || true
+grep "error generated when compiling" buildLogFile.log testBuildLogFile.log | tee -a buildErrors.log || true
 
 # Find linking errors:
 grep "^/usr/bin/ld:" buildLogFile.log testBuildLogFile.log | tee -a buildErrors.log || true
