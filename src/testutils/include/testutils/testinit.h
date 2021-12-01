gromacs/gromacs

commit aa3e6f1755f4688ef3a015e8ddc2fc1207418a75

Use dynamic registration for PME gather unit tests
Now these tests register themselves dynamically for all
devices detected, and if the test cannot run it provides
the standard GoogleTest skip message. Particular test cases
can now be described by name, including which device they
run on.

Refs #4255

src/testutils/include/testutils/testinit.h
@@ -43,6 +43,10 @@
 #ifndef GMX_TESTUTILS_TESTINIT_H
 #define GMX_TESTUTILS_TESTINIT_H
 
+#include <string>
+
+#include <gtest/gtest.h>
+
 namespace gmx
 {
 
@@ -63,6 +67,8 @@ namespace test
  * \param[in] tempPath Filepath to temporary files.
  * \param[in] usesMpi  If the test is run with MPI or not.
  * \param[in] usesHardwareDetection If hardwaredetection is enabled.
+ * \param[in] registersDynamically  Whether dynamical GoogleTest registration
+ *                                  is used by this test binary
  * \param[in] argc Number of cmdline options
  * \param[in] argv Cmdline options.
  *
@@ -72,6 +78,7 @@ void initTestUtils(const char* dataPath,
                    const char* tempPath,
                    bool        usesMpi,
                    bool        usesHardwareDetection,
+                   bool        registersDynamically,
                    int*        argc,
                    char***     argv);
 
@@ -80,6 +87,8 @@ void initTestUtils(const char* dataPath,
  * Finalizes the test utilities library.
  *
  * \param[in] usesHardwareDetection If hardwaredetection is enabled.
+ * \param[in] registersDynamically  Whether dynamical GoogleTest registration
+ *                                  is used by this test binary
  *
  * Does not throw.  Terminates the program with a non-zero error code if an
  * error occurs.
@@ -88,8 +97,91 @@ void initTestUtils(const char* dataPath,
  *
  * \ingroup module_testutils
  */
-void finalizeTestUtils(bool usesHardwareDetection);
+void finalizeTestUtils(bool usesHardwareDetection, bool registersDynamically);
 //! \endcond
+
+/*! \brief Declaration of function used to dynamically register
+ * GoogleTest tests.
+ *
+ * When a test binary uses gmx_add_gtest_executable(exename
+ * DYNAMIC_REGISTRATION ...) it must provide an implementation of this
+ * method. The method is called before RUN_ALL_TESTS() and is expected
+ * to call ::testing::RegisterTest to register tests dynamically. This
+ * approach might be necessary to run the tests in a stable way over
+ * whichever hardware is detected at run time.
+ *
+ * Normal test binaries do not need to implement this function.
+ *
+ * ::gmx::test::TestHardwareEnvironment::gmxSetUp() should be called before
+ * this method, in case the test hardware environment is needed to help
+ * decide which tests to register. */
+void registerTestsDynamically();
+
+/*! \brief Register tests dynamically based on the execution context
+ *
+ * This template reduces code duplication across the dynamically
+ * registered tests, letting them register their tests more tersely.
+ *
+ * \tparam TestFixture  The type of the test fixture
+ * \tparam TestCase     The type of the test case (derived from \c TestFixture)
+ * \tparam Combinations The interal GoogleTest type describing the
+ *                        return from \c ::testing::Combine() intended
+ *                        to generate test parameters of type \c
+ *                        TestFixture::ParamType (which is typically a
+ *                        tuple).
+ *
+ * \param[in] testSuiteName           The name of the test suite that shares the \c TestFixture
+ * \param[in] makeBriefNameOfTestCase Function that will make the brief name of the test case,
+ *                                      used for naming the refdata file
+ * \param[in] makeFullNameOfTestCase  Function that will make the full name of the test case
+ * \param[in] combinations            A generator of test values produced with
+ *                                      \c ::testing::Combine()
+ *
+ * Note that \c Combinations is actually well defined relative to \c
+ * TestFixture::ParamType, but its concrete type is an internal
+ * GoogleTest type, so we do not want to express it in code. In C++20,
+ * it would be better to declare the function parameter like `const
+ * auto& combinations` to achieve the same effect of hiding the
+ * concrete type. */
+template<typename TestFixture, typename TestCase, typename Combinations>
+void registerTests(const std::string& testSuiteName,
+                   std::string (*makeBriefNameOfTestCase)(
+                           const typename ::testing::TestParamInfo<typename TestFixture::ParamType>&),
+                   std::string (*makeFullNameOfTestCase)(
+                           const typename ::testing::TestParamInfo<typename TestFixture::ParamType>&,
+                           const std::string&),
+                   const Combinations& combinations)
+{
+    // It is not good practice to use GoogleTest's internal type here,
+    // but it's a practical alternative that lets us use GoogleTest's Combine
+    // in the code that declares the values passed to registered tests.
+    //
+    // Normally the use of this type is hidden behind a call to a
+    // INSTANTIATE_TEST_SUITE_P macro. If GoogleTest do change things
+    // such that this breaks, it may be simple to fix it. Or if not,
+    // we can always manually build or enumerate the Cartesian product
+    // of values that it generates and use that in the loop below.
+    const auto testParamGenerator =
+            ::testing::internal::ParamGenerator<typename TestFixture::ParamType>(combinations);
+    for (const auto& parameters : testParamGenerator)
+    {
+        ::testing::TestParamInfo<typename TestFixture::ParamType> testParamInfo(parameters, 0);
+        std::string testName = makeBriefNameOfTestCase(testParamInfo);
+        // This returns a testing::TestInfo object that leaks. That's currently not
+        // a problem for a short-lived test binary. But if it did become one, then we
+        // should fill and return a std::vector<std::unique_ptr<TestInfo>> whose lifetime
+        // is managed in testinit.cpp.
+        ::testing::RegisterTest(testSuiteName.c_str(),
+                                makeFullNameOfTestCase(testParamInfo, testName).c_str(),
+                                nullptr,
+                                testName.c_str(),
+                                __FILE__,
+                                __LINE__,
+                                // Important to use the fixture type as the return type here, even
+                                // though we construct an object of the derived type.
+                                [=]() -> TestFixture* { return new TestCase(parameters); });
+    }
+}
 
 } // namespace test
 } // namespace gmx

src/external/googletest/README.Gromacs
@@ -13,7 +13,8 @@ This README.Gromacs file is new, of course.
 
 Conditional compilation based on __clang_analyzer__ is used to work
 around reports from the clang static analyzer in
-googletest/include/gtest/gtest-matchers.h and
+googletest/include/gtest/gtest.h,
+googletest/include/gtest/gtest-matchers.h, and
 googlemock/include/gmock/gmock-matchers.h.
 
 Some warning-generation flags are removed from

src/external/googletest/googletest/include/gtest/gtest.h
@@ -2468,12 +2468,16 @@ TestInfo* RegisterTest(const char* test_suite_name, const char* test_name,
     Factory factory_;
   };
 
+#ifndef __clang_analyzer__
   return internal::MakeAndRegisterTestInfo(
       test_suite_name, test_name, type_param, value_param,
       internal::CodeLocation(file, line), internal::GetTypeId<TestT>(),
       internal::SuiteApiResolver<TestT>::GetSetUpCaseOrSuite(file, line),
       internal::SuiteApiResolver<TestT>::GetTearDownCaseOrSuite(file, line),
       new FactoryImpl{std::move(factory)});
+#else
+  return nullptr;
+#endif
 }
 
 }  // namespace testing

src/gromacs/ewald/pme_gpu_internal.h
@@ -97,7 +97,8 @@ enum class PmeSplineDataType
 enum class GridOrdering
 {
     YZX,
-    XYZ
+    XYZ,
+    Count
 };
 
 /*! \libinternal \brief

src/gromacs/ewald/tests/CMakeLists.txt
@@ -32,7 +32,9 @@
 # To help us fund GROMACS development, we humbly ask that you cite
 # the research papers on the package. Check out http://www.gromacs.org.
 
-gmx_add_unit_test(EwaldUnitTests ewald-test HARDWARE_DETECTION
+gmx_add_unit_test(EwaldUnitTests ewald-test
+    HARDWARE_DETECTION
+    DYNAMIC_REGISTRATION
     CPP_SOURCE_FILES
         pmebsplinetest.cpp
         pmegathertest.cpp

src/gromacs/ewald/tests/pmegathertest.cpp
@@ -52,6 +52,7 @@
 
 #include "testutils/refdata.h"
 #include "testutils/test_hardware_environment.h"
+#include "testutils/testinit.h"
 #include "testutils/testasserts.h"
 
 #include "pmetestcommon.h"
@@ -231,11 +232,18 @@ std::map<std::string, TestSystem> c_testSystems = {
 };
 
 /*! \brief Convenience typedef of the test input parameters - unit cell box, PME interpolation
- * order, grid dimensions, grid values, overwriting/reducing the input forces, atom count.
+ * order, grid dimensions, grid values, test system, PME hardware context index.
  */
-typedef std::tuple<std::string, int, IVec, std::string, std::string> GatherInputParameters;
+typedef std::tuple<std::string, int, IVec, std::string, std::string, int> GatherInputParameters;
 
-//! Help GoogleTest name our test cases
+/*! \brief Help GoogleTest name our test cases
+ *
+ * This is intended to work like a custom test-naming function that
+ * would be passed as the fourth argument to INSTANTIATE_TEST_SUITE_P,
+ * except that we are not using that macro for these tests. Only the
+ * components of GatherInputParameters that affect the reference data
+ * values affect this name. Hardware context does not affect this
+ * name. */
 std::string nameOfTest(const testing::TestParamInfo<GatherInputParameters>& info)
 {
     std::string testName = formatString(
@@ -264,20 +272,61 @@ std::string nameOfTest(const testing::TestParamInfo<GatherInputParameters>& info
     return testName;
 }
 
+/*! \brief Help GoogleTest name our test cases
+ *
+ * This is intended to work like a custom test-naming function that
+ * would be passed as the fourth argument to INSTANTIATE_TEST_SUITE_P,
+ * except that we are not using that macro for these tests. All
+ * components of GatherInputParameters affect this name. */
+std::string fullNameOfTest(const testing::TestParamInfo<GatherInputParameters>& info,
+                           const std::string&                                   testName)
+{
+    // Note that makeRefDataFileName() relies on finding "WorksOn" in
+    // the name of the test case, so it can remove the information
+    // about the hardware context and all following text from the name
+    // of the file used for refdata.
+    const int hardwareContextIndex = std::get<5>(info.param);
+    return formatString(
+            "WorksOn_%s_%s", makeHardwareContextName(hardwareContextIndex).c_str(), testName.c_str());
+}
+
 //! Test fixture
 class GatherTest : public ::testing::TestWithParam<GatherInputParameters>
 {
 public:
     GatherTest() = default;
+    //! Sets the input atom data references and programs once
+};
+
+/*! \brief Test case whose body checks that gather works
+ *
+ * Normally the declaration of this class would be produced by a call
+ * to a macro like TEST_P(GatherTest, WorksWith). That macro places
+ * the body of the test case in the TestBody() method, which here is
+ * done explicitly.
+ *
+ * Note that it is important to use parameters_ to access the values
+ * that describe the particular test case, rather than the usual
+ * GoogleTest function GetParam(), because the latter no longer
+ * works. */
+class GatherTestBody : public GatherTest
+{
+public:
+    //! Constructor
+    explicit GatherTestBody(const GatherInputParameters& parameters) : parameters_(parameters) {}
+
+    //! The test parameters with which the test case was instantiated
+    GatherInputParameters parameters_;
 
     //! The test
-    static void runTest()
+    void TestBody() override
     {
         /* Getting the input */
         int         pmeOrder;
         IVec        gridSize;
         std::string boxName, gridValuesName, testSystemName;
-        std::tie(boxName, pmeOrder, gridSize, gridValuesName, testSystemName) = GetParam();
+        int         contextIndex;
+        std::tie(boxName, pmeOrder, gridSize, gridValuesName, testSystemName, contextIndex) = parameters_;
         Matrix3x3                        box               = c_inputBoxes.at(boxName);
         const SparseRealGridValuesInput& nonZeroGridValues = c_inputGrids.at(gridValuesName);
         TestSystem                       testSystem        = c_testSystems.at(testSystemName);
@@ -293,22 +342,21 @@ public:
         inputRec.coulombtype = CoulombInteractionType::Pme;
         inputRec.epsilon_r   = 1.0;
 
-        TestReferenceData refData;
-        for (const auto& pmeTestHardwareContext : getPmeTestHardwareContexts())
+        // Extra indentation retained only to help code review, can be removed later
         {
-            pmeTestHardwareContext.activate();
-            CodePath   codePath       = pmeTestHardwareContext.codePath();
-            const bool supportedInput = pmeSupportsInputForMode(
-                    *getTestHardwareEnvironment()->hwinfo(), &inputRec, codePath);
-            if (!supportedInput)
+            const PmeTestHardwareContext& pmeTestHardwareContext =
+                    getPmeTestHardwareContexts()[contextIndex];
+            CodePath               codePath = pmeTestHardwareContext.codePath();
+            MessageStringCollector messages = getSkipMessagesIfNecessary(
+                    *getTestHardwareEnvironment()->hwinfo(), inputRec, codePath);
+            if (!messages.isEmpty())
             {
-                /* Testing the failure for the unsupported input */
-                EXPECT_THROW_GMX(pmeInitWrapper(&inputRec, codePath, nullptr, nullptr, nullptr, box),
-                                 NotImplementedError);
-                continue;
+                GTEST_SKIP() << messages.toString();
             }
+            pmeTestHardwareContext.activate();
+            SCOPED_TRACE("Testing on " + pmeTestHardwareContext.description());
 
-            /* Describing the test uniquely */
+            // Describe the test uniquely in case it fails
             SCOPED_TRACE(
                     formatString("Testing force gathering on %s for PME grid size %d %d %d"
                                  ", order %d, %d atoms",
@@ -361,6 +409,7 @@ public:
             pmeFinalizeTest(pmeSafe.get(), codePath);
 
             /* Check the output forces correctness */
+            TestReferenceData    refData(makeRefDataFileName());
             TestReferenceChecker forceChecker(refData.rootChecker());
             const auto           ulpTolerance = 3 * pmeOrder;
             forceChecker.setDefaultTolerance(relativeToleranceAsUlp(1.0, ulpTolerance));
@@ -369,13 +418,6 @@ public:
     }
 };
 
-//! Test for PME force gathering
-TEST_P(GatherTest, WorksWith)
-{
-    checkTestNameLength();
-    EXPECT_NO_THROW_GMX(runTest());
-}
-
 //! Moved out from instantiations for readability
 const auto c_inputBoxNames = ::testing::Values("rect", "tric");
 //! Moved out from instantiations for readability
@@ -383,16 +425,21 @@ const auto c_inputGridNames = ::testing::Values("first", "second");
 //! Moved out from instantiations for readability
 const auto c_inputTestSystemNames = ::testing::Values("1 atom", "2 atoms", "13 atoms");
 
-//! Instantiation of the PME gathering test
-INSTANTIATE_TEST_SUITE_P(Pme,
-                         GatherTest,
-                         ::testing::Combine(c_inputBoxNames,
-                                            ::testing::ValuesIn(c_inputPmeOrders),
-                                            ::testing::ValuesIn(c_inputGridSizes),
-                                            c_inputGridNames,
-                                            c_inputTestSystemNames),
-                         nameOfTest);
-
 } // namespace
+
+void registerDynamicalPmeGatherTests(const Range<int> hardwareContextIndexRange)
+{
+    // Form the Cartesian product of all test values we might check
+    const auto testCombinations = ::testing::Combine(
+            c_inputBoxNames,
+            ::testing::ValuesIn(c_inputPmeOrders),
+            ::testing::ValuesIn(c_inputGridSizes),
+            c_inputGridNames,
+            c_inputTestSystemNames,
+            ::testing::Range(*hardwareContextIndexRange.begin(), *hardwareContextIndexRange.end()));
+    gmx::test::registerTests<GatherTest, GatherTestBody, decltype(testCombinations)>(
+            "Pme_GatherTest", nameOfTest, fullNameOfTest, testCombinations);
+}
+
 } // namespace test
 } // namespace gmx

src/gromacs/ewald/tests/pmetestcommon.cpp
@@ -38,6 +38,7 @@
  * Implements common routines for PME tests.
  *
  * \author Aleksei Iupinov <a.yupinov@gmail.com>
+ * \author Mark Abraham <mark.j.abraham@gmail.com>
  * \ingroup module_ewald
  */
 #include "gmxpre.h"
@@ -74,6 +75,7 @@
 
 #include "testutils/test_hardware_environment.h"
 #include "testutils/testasserts.h"
+#include "testutils/testinit.h"
 
 class DeviceContext;
 
@@ -106,6 +108,31 @@ bool pmeSupportsInputForMode(const gmx_hw_info_t& hwinfo, const t_inputrec* inpu
         default: GMX_THROW(InternalError("Test not implemented for this mode"));
     }
     return implemented;
+}
+
+MessageStringCollector getSkipMessagesIfNecessary(const gmx_hw_info_t& hwinfo,
+                                                  const t_inputrec&    inputRec,
+                                                  const CodePath       codePath)
+{
+    // Note that we can't call GTEST_SKIP() from within this method,
+    // because it only returns from the current function. So we
+    // collect all the reasons why the test cannot run, return them
+    // and skip in a higher stack frame.
+
+    MessageStringCollector messages;
+    messages.startContext("Test is being skipped because:");
+
+    if (codePath == CodePath::CPU)
+    {
+        // Everything is implemented, no reason to skip
+        return messages;
+    }
+
+    std::string errorMessage;
+    messages.appendIf(!pme_gpu_supports_build(&errorMessage), errorMessage);
+    messages.appendIf(!pme_gpu_supports_hardware(hwinfo, &errorMessage), errorMessage);
+    messages.appendIf(!pme_gpu_supports_input(inputRec, &errorMessage), errorMessage);
+    return messages;
 }
 
 uint64_t getSplineModuliDoublePrecisionUlps(int splineOrder)
@@ -929,6 +956,54 @@ PmeOutput pmeGetReciprocalEnergyAndVirial(const gmx_pme_t* pme, CodePath mode, P
     return output;
 }
 
+std::string makeRefDataFileName()
+{
+    // By default, the reference data filename is set via a call to
+    // gmx::TestFileManager::getTestSpecificFileName() that queries
+    // GoogleTest and gets a string that includes the return value for
+    // nameOfTest(). The logic here must match that of the call to
+    // ::testing::RegisterTest, so that it works as intended. In
+    // particular, the name must include a "WorksOn" substring that
+    // precedes the name of the hardware context, so that this can be
+    // removed.
+    //
+    // Get the info about the test
+    const ::testing::TestInfo* testInfo = ::testing::UnitTest::GetInstance()->current_test_info();
+
+    // Get the test name and prepare to remove the part describing the
+    // hardware context.
+    std::string testName(testInfo->name());
+    auto        worksOnPos = testName.find("WorksOn");
+    GMX_RELEASE_ASSERT(worksOnPos != testName.size(),
+                       "Test name must include the 'WorksOn' fragment");
+
+    // Build the complete refdata filename like
+    // getTestSpecificFilename() would do it for a non-dynamical
+    // parameterized test.
+    std::string refDataFileName = formatString("%s_%sWorksWith_%s.xml",
+                                               testInfo->test_suite_name(),
+                                               testName.substr(0, worksOnPos).c_str(),
+                                               testInfo->value_param());
+    // Use the check that the name isn't too long
+    checkTestNameLength(refDataFileName);
+    return refDataFileName;
+}
+
+std::string makeHardwareContextName(const int hardwareContextIndex)
+{
+    std::optional<int> gpuId = getPmeTestHardwareContexts()[hardwareContextIndex].gpuId();
+    std::string        description;
+    if (gpuId.has_value())
+    {
+        description = "GPU" + std::to_string(gpuId.value());
+    }
+    else
+    {
+        description = "CPU";
+    }
+    return description;
+}
+
 PmeTestHardwareContext::PmeTestHardwareContext() : codePath_(CodePath::CPU) {}
 
 PmeTestHardwareContext::PmeTestHardwareContext(TestDevice* testDevice) :
@@ -946,6 +1021,16 @@ std::string PmeTestHardwareContext::description() const
         case CodePath::CPU: return "CPU";
         case CodePath::GPU: return "GPU (" + testDevice_->description() + ")";
         default: return "Unknown code path.";
+    }
+}
+
+std::optional<int> PmeTestHardwareContext::gpuId() const
+{
+    switch (codePath_)
+    {
+        case CodePath::CPU: return std::nullopt;
+        case CodePath::GPU: return testDevice_->id();
+        default: return std::nullopt;
     }
 }
 
@@ -976,6 +1061,13 @@ ArrayRef<const PmeTestHardwareContext> getPmeTestHardwareContexts()
         }
     }
     return s_pmeTestHardwareContexts;
+}
+
+void registerTestsDynamically()
+{
+    auto       contexts = getPmeTestHardwareContexts();
+    Range<int> contextIndexRange(0, contexts.size());
+    registerDynamicalPmeGatherTests(contextIndexRange);
 }
 
 } // namespace test

src/gromacs/ewald/tests/pmetestcommon.h
@@ -37,6 +37,7 @@
  * Describes common routines and types for PME tests.
  *
  * \author Aleksei Iupinov <a.yupinov@gmail.com>
+ * \author Mark Abraham <mark.j.abraham@gmail.com>
  * \ingroup module_ewald
  */
 #ifndef GMX_EWALD_PME_TEST_COMMON_H
@@ -44,12 +45,15 @@
 
 #include <array>
 #include <map>
+#include <optional>
 #include <vector>
 
 #include "gromacs/ewald/pme.h"
 #include "gromacs/ewald/pme_gpu_internal.h"
 #include "gromacs/math/gmxcomplex.h"
 #include "gromacs/mdtypes/state_propagator_data_gpu.h"
+#include "gromacs/utility/message_string_collector.h"
+#include "gromacs/utility/range.h"
 #include "gromacs/utility/unique_cptr.h"
 
 #include "testutils/test_device.h"
@@ -124,6 +128,13 @@ enum class PmeSolveAlgorithm : int
 
 //! Tells if this generally valid PME input is supported for this mode
 bool pmeSupportsInputForMode(const gmx_hw_info_t& hwinfo, const t_inputrec* inputRec, CodePath mode);
+
+/*! \brief Returns message describing why PME in this \c mode is not
+ * supported for this \c inputRec on this \c hwinfo, or empty of
+ * messages when PME is supported. */
+MessageStringCollector getSkipMessagesIfNecessary(const gmx_hw_info_t& hwinfo,
+                                                  const t_inputrec&    inputRec,
+                                                  CodePath             mode);
 
 //! Spline moduli are computed in double precision, so they're very good in single precision
 constexpr int64_t c_splineModuliSinglePrecisionUlps = 1;
@@ -210,13 +221,15 @@ struct PmeTestHardwareContext
     CodePath codePath_;
     //! Returns a human-readable context description line
     std::string description() const;
+    //! Returns an optional GPU ID, with a valid value when the context is for a GPU
+    std::optional<int> gpuId() const;
     //! Pointer to the global test hardware device (if on GPU)
     TestDevice* testDevice_ = nullptr;
     //! PME GPU program if needed
     PmeGpuProgramStorage pmeGpuProgram_ = nullptr;
-    // Constructor for CPU context
+    //! Constructor for CPU context
     PmeTestHardwareContext();
-    // Constructor for GPU context
+    //! Constructor for GPU context
     explicit PmeTestHardwareContext(TestDevice* testDevice);
 
     //! Get the code path
@@ -243,6 +256,30 @@ struct PmeTestHardwareContext
 
 //! Return a view of the current PME test hardware contexts
 ArrayRef<const PmeTestHardwareContext> getPmeTestHardwareContexts();
+
+/*! \brief Construct a refdata filename for a test
+ *
+ * We want the same reference data to apply to every hardware context
+ * for which we test PME. That means we need to store it in a file
+ * whose name relates to the name of the test, but excluding the part
+ * related to the context. */
+std::string makeRefDataFileName();
+
+/*! \brief Make a terse description of the hardware context suitable
+ * for use in naming the test case.
+ *
+ * The full hardware device description is used in a SCOPED_TRACE
+ * message, but that is too long for the test name and has too many
+ * possible characters that might break GoogleTest. */
+std::string makeHardwareContextName(int hardwareContextIndex);
+
+/*! \brief Functions that dynamically register test cases
+ *
+ * This are called by registerTestsDynamically and customize the
+ * range of test cases to suit the available hardware. */
+//!\{
+void registerDynamicalPmeGatherTests(Range<int> contextIndexRange);
+//!\}
 
 //! A couple of valid inputs for boxes.
 extern const std::map<std::string, Matrix3x3> c_inputBoxes;

commit 36612d8a1a1cc13ad6550a43a184313a64fcdc61

Allow check-source to pass
Commit aa3e6f1755f4688ef3a015e8ddc2fc1207418a75 introduced
some failures to check-source. This fixes those.

Note that there are many check-source failures that are not
caught by the regexes we have that are still broken.

src/testutils/include/testutils/testinit.h
@@ -106,7 +106,7 @@ void finalizeTestUtils(bool usesHardwareDetection, bool registersDynamically);
  * When a test binary uses gmx_add_gtest_executable(exename
  * DYNAMIC_REGISTRATION ...) it must provide an implementation of this
  * method. The method is called before RUN_ALL_TESTS() and is expected
- * to call ::testing::RegisterTest to register tests dynamically. This
+ * to call testing::RegisterTest to register tests dynamically. This
  * approach might be necessary to run the tests in a stable way over
  * whichever hardware is detected at run time.
  *
@@ -125,7 +125,7 @@ void registerTestsDynamically();
  * \tparam TestFixture  The type of the test fixture
  * \tparam TestCase     The type of the test case (derived from \c TestFixture)
  * \tparam Combinations The interal GoogleTest type describing the
- *                        return from \c ::testing::Combine() intended
+ *                        return from \c testing::Combine() intended
  *                        to generate test parameters of type \c
  *                        TestFixture::ParamType (which is typically a
  *                        tuple).
@@ -135,7 +135,7 @@ void registerTestsDynamically();
  *                                      used for naming the refdata file
  * \param[in] makeFullNameOfTestCase  Function that will make the full name of the test case
  * \param[in] combinations            A generator of test values produced with
- *                                      \c ::testing::Combine()
+ *                                      \c testing::Combine()
  *
  * Note that \c Combinations is actually well defined relative to \c
  * TestFixture::ParamType, but its concrete type is an internal