6f394cdc988e4b89c148c242fa6095eb4b69de8c
Szilárd Páll <pall.szilard@gmail.com>
2022-01-05 18:43:48 +0100

    Bump minimum NVIDIA hardware support
    The required CUDA toolkit (11.0) only supports compute capability >=3.5.
    
    Refs #4211 #3708

--- cmake/gmxManageCuda.cmake
+++ cmake/gmxManageCuda.cmake
@@ -2,7 +2,7 @@
 # This file is part of the GROMACS molecular simulation package.
 #
 # Copyright (c) 2012,2013,2014,2015,2016 by the GROMACS development team.
-# Copyright (c) 2017,2018,2019,2020,2021, by the GROMACS development team, led by
+# Copyright (c) 2017,2018,2019,2020,2021,2022, by the GROMACS development team, led by
 # Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
 # and including many others, as listed in the AUTHORS file in the
 # top-level source directory and at http://www.gromacs.org.
@@ -35,7 +35,7 @@
 
 # The earliest version of the CUDA toolkit that supports c++17 is 11.0
 set(REQUIRED_CUDA_VERSION 11.0)
-set(REQUIRED_CUDA_COMPUTE_CAPABILITY 3.0)
+set(REQUIRED_CUDA_COMPUTE_CAPABILITY 3.5)
 
 set(GMX_GPU_CUDA ON)
 
