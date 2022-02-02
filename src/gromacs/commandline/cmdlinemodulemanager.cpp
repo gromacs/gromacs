c3dae4d2f679253a90b21e2675870d2546ea69c6
Erik Lindahl <erik.lindahl@gmail.com>
2022-02-02 09:02:20 +0000

    Do not print full copyright info to stdout
    This is part of our modifications to list both current and
    previous authors, but only in the log file.
    
    Refs #4322, #4323

--- src/gromacs/commandline/cmdlinemodulemanager.cpp
+++ src/gromacs/commandline/cmdlinemodulemanager.cpp
@@ -2,7 +2,8 @@
  * This file is part of the GROMACS molecular simulation package.
  *
  * Copyright (c) 2012,2013,2014,2015,2016 by the GROMACS development team.
- * Copyright (c) 2017,2018,2019,2020,2021, by the GROMACS development team, led by
+ * Copyright (c) 2017,2018,2019,2020,2021 by the GROMACS development team.
+ * Copyright (c) 2022, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -152,14 +153,14 @@ CommandLineCommonOptionsHolder::CommandLineCommonOptionsHolder() :
     bHidden_(false),
     bQuiet_(false),
     bVersion_(false),
-    bCopyright_(true),
+    bCopyright_(false),
     niceLevel_(19),
     bNiceSet_(false),
     bBackup_(true),
     bFpexcept_(false),
     debugLevel_(0)
 {
-    binaryInfoSettings_.copyright(true);
+    binaryInfoSettings_.copyright(false);
 }
 
 CommandLineCommonOptionsHolder::~CommandLineCommonOptionsHolder() {}
