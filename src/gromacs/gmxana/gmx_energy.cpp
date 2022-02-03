7676c5c816f903d185238d9a9da9af758ae21256
Berk Hess <hess@kth.se>
2022-02-03 10:05:10 +0100

    Avoid NaN RMSD for gmx energy output
    Fixes #4374

--- src/gromacs/gmxana/gmx_energy.cpp
+++ src/gromacs/gmxana/gmx_energy.cpp
@@ -4,7 +4,7 @@
  * Copyright (c) 1991-2000, University of Groningen, The Netherlands.
  * Copyright (c) 2001-2004, The GROMACS development team.
  * Copyright (c) 2013,2014,2015,2016,2017 by the GROMACS development team.
- * Copyright (c) 2018,2019,2020,2021, by the GROMACS development team, led by
+ * Copyright (c) 2018,2019,2020,2021,2022, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -575,7 +575,7 @@ static void calc_averages(int nset, enerdata_t* edat, int nbmin, int nbmax)
         }
         else
         {
-            edat->s[i].rmsd = std::sqrt(sum2 / np - gmx::square(edat->s[i].av));
+            edat->s[i].rmsd = std::sqrt(std::max(sum2 / np - gmx::square(edat->s[i].av), 0.0));
         }
 
         if (edat->nframes > 1)
