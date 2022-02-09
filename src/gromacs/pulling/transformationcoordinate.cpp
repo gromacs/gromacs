e9c61feca52f8699fb86709aa7ce829c3cdfbab9
Berk Hess <hess@kth.se>
2022-02-09 10:34:08 +0100

    Remove unnecessary pull transformation optimization
    It should happen very rarely that a force on a transformation
    pull coordinate is zero and therefore it is not worth optimizing
    for this case. In particular, we should not use a fixed absolute
    tolerance.

--- src/gromacs/pulling/transformationcoordinate.cpp
+++ src/gromacs/pulling/transformationcoordinate.cpp
@@ -1,10 +1,9 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2021, by the GROMACS development team, led by
- * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
- * and including many others, as listed in the AUTHORS file in the
- * top-level source directory and at http://www.gromacs.org.
+ * Copyright 2021- The GROMACS Authors
+ * and the project initiators Erik Lindahl, Berk Hess and David van der Spoel.
+ * Consult the AUTHORS/COPYING files and https://www.gromacs.org for details.
  *
  * GROMACS is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -18,7 +17,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with GROMACS; if not, see
- * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * https://www.gnu.org/licenses, or write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
  *
  * If you want to redistribute modifications to GROMACS, please
@@ -27,10 +26,10 @@
  * consider code for inclusion in the official distribution, but
  * derived work must not be called official GROMACS. Details are found
  * in the README & COPYING files - if they are missing, get the
- * official version at http://www.gromacs.org.
+ * official version at https://www.gromacs.org.
  *
  * To help us fund GROMACS development, we humbly ask that you cite
- * the research papers on the package. Check out http://www.gromacs.org.
+ * the research papers on the package. Check out https://www.gromacs.org.
  */
 #include "gmxpre.h"
 
@@ -141,11 +140,6 @@ static void distributeTransformationPullCoordForce(pull_coord_work_t*           
                                                    gmx::ArrayRef<pull_coord_work_t> variableCoords,
                                                    const double transformationCoordForce)
 {
-    if (std::abs(transformationCoordForce) < 1e-9)
-    {
-        // the force is effectively 0. Don't proceed and distribute it recursively
-        return;
-    }
     GMX_ASSERT(pcrd->params.eGeom == PullGroupGeometry::Transformation,
                "We shouldn't end up here when not using a transformation pull coordinate.");
     GMX_ASSERT(ssize(variableCoords) == pcrd->params.coordIndex,
