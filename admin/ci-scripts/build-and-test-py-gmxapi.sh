a6a4f780a6c11e006c37d370aea0c86aaf3e368e
M. Eric Irrgang <ericirrgang@gmail.com>
2022-04-13 14:29:13 +0300

    Job script tidying.
    * Remove no-longer-used GitLab-CI environment variables.
    * Fix an output file (wrong variable name).
    * Remove unused job artifacts.
    * Document script inputs (environment variables).
    * Allow for alternative gromacs suffixes.
    * Shell script linting.
    
    Preliminary clean-up for #3563.

--- admin/ci-scripts/build-and-test-py-gmxapi.sh
+++ admin/ci-scripts/build-and-test-py-gmxapi.sh
@@ -2,6 +2,13 @@
 #
 # Build, install, and test the gmxapi Python package.
 #
+# Inputs (environment variables):
+#
+#     GROMACS_ROOT: absolute path to GROMACS installation prefix.
+#     GMX_SUFFIX: (optional) suffix as used by template/CMakeLists.txt (E.g. "_mpi")
+#     PY_UNIT_TEST_XML: XML output file path for basic pytest suite
+#     PY_MPI_UNIT_TEST_XML: XML output file path for MPI pytest suite
+#
 # This script assumes an activated Python venv with the
 # gmxapi dependencies already installed, with `python` resolvable by the shell
 # to the appropriate Python interpreter.
@@ -22,7 +29,7 @@ pushd python_packaging/src
   # Build and install the gmxapi Python package.
   # Use the documented mechanism for getting GROMACS installation and build system hints.
   # See docs/gmxapi/userguide/install.rst and docs/release-notes/2022/major/portability.rst
-  CMAKE_ARGS="-Dgmxapi_ROOT=$GROMACS_ROOT -C $GROMACS_ROOT/share/cmake/gromacs/gromacs-hints.cmake" \
+  CMAKE_ARGS="-Dgmxapi_ROOT=$GROMACS_ROOT -C $GROMACS_ROOT/share/cmake/gromacs${GMX_SUFFIX}/gromacs-hints${GMX_SUFFIX}.cmake" \
       python -m pip install \
           --no-build-isolation \
           --no-cache-dir \
@@ -32,23 +39,22 @@ pushd python_packaging/src
 popd
 
 # Run Python unit tests.
-python -m pytest python_packaging/src/test --junitxml=$PY_UNIT_TEST_XML --threads=2
+python -m pytest python_packaging/src/test --junitxml="$PY_UNIT_TEST_XML" --threads=2
 
 # Note: Multiple pytest processes getting --junitxml output file argument
 # may cause problems, so we set the option on only one of the launched processes.
 # See also Multiple Instruction Multiple Data Model for OpenMPI mpirun:
 # https://www.open-mpi.org/doc/v3.0/man1/mpiexec.1.php
-PROGRAM=(`which python` -m mpi4py -m pytest \
+PROGRAM=("$(which python)" -m mpi4py -m pytest \
         -p no:cacheprovider \
-        $PWD/python_packaging/src/test \
+        "$PWD"/python_packaging/src/test \
         --threads=1)
-# shellcheck disable=SC2068
-if [ -x `which mpiexec` ]; then
+if [ -x "$(which mpiexec)" ]; then
     PYTHONDONTWRITEBYTECODE=1 \
     mpiexec --allow-run-as-root \
       -x OMP_NUM_THREADS=1 \
       --mca opal_warn_on_missing_libcuda 0 \
       --mca orte_base_help_aggregate 0 \
-      -n 1 ${PROGRAM[@]} --junitxml=$PLUGIN_MPI_TEST_XML : \
-      -n 1 ${PROGRAM[@]}
+      -n 1 "${PROGRAM[@]}" --junitxml="$PY_MPI_UNIT_TEST_XML" : \
+      -n 1 "${PROGRAM[@]}"
 fi
