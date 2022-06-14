a7a9620047d407b50d7c968c15abcdfba72de116
Gaurav Garg <gaugarg@nvidia.com>
2022-06-14 12:05:25 +0000

    Fix clang-tidy error
    Introduced in !2629 (e93d12c9)
    
    Failed pipeline: https://gitlab.com/gromacs/gromacs/-/jobs/2585245637
    
    Refs: #3884

--- src/gromacs/ewald/pme_gpu_internal.cpp
+++ src/gromacs/ewald/pme_gpu_internal.cpp
@@ -849,11 +849,11 @@ static gmx::FftBackend getFftBackend(const PmeGpu* pmeGpu)
     }
     else if (GMX_GPU_SYCL)
     {
-        if ((GMX_SYCL_DPCPP != 0) && (GMX_FFT_MKL != 0))
+        if ((GMX_SYCL_DPCPP != 0) && (GMX_FFT_MKL != 0)) // NOLINT(misc-redundant-expression)
         {
             return gmx::FftBackend::SyclMkl;
         }
-        else if ((GMX_SYCL_HIPSYCL != 0) && (GMX_HIPSYCL_HAVE_HIP_TARGET != 0))
+        else if ((GMX_SYCL_HIPSYCL != 0) && (GMX_HIPSYCL_HAVE_HIP_TARGET != 0)) // NOLINT(misc-redundant-expression)
         {
             return gmx::FftBackend::SyclRocfft;
         }
