772d22cc9c6d4a9717d3684e225343e45ccc98dd
Mark Abraham <mark.j.abraham@gmail.com>
2022-06-28 10:08:27 +0200

    Update CUDA CI container to gcc 9
    This prepares to update the required GCC version to 9
    
    Updated the recipe for the container and used it in CI matrix.
    
    Nvidia have changed their GPG signing strategy. Old containers can't
    use apt-update without getting a GPG key from Nvidia. hpccm doesn't
    support getting that key with the things that are already found in the
    container.  So instead we update the minimum CUDA version tested to
    11.0.3 because that container already has the right key in it.
    
    Refs #4093
    Refs #3708

--- admin/containers/buildall.sh
+++ admin/containers/buildall.sh
@@ -11,7 +11,7 @@ SCRIPT=$PWD/scripted_gmx_docker_builds.py
 
 args[${#args[@]}]="--gcc 11 --clfft --mpi openmpi --rocm"
 args[${#args[@]}]="--gcc 11 --cuda 11.4.1 --clfft --mpi openmpi --nvhpcsdk 22.5"
-args[${#args[@]}]="--gcc 7 --cuda 11.0 --clfft --mpi openmpi --heffte v2.2.0"
+args[${#args[@]}]="--gcc 9 --cuda 11.0.3 --clfft --mpi openmpi --heffte v2.2.0"
 args[${#args[@]}]="--gcc 9 --mpi openmpi --cp2k 8.2"
 args[${#args[@]}]="--gcc 9 --mpi openmpi --cp2k 9.1"
 args[${#args[@]}]="--llvm 11 --cuda 11.4.1"
