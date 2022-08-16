a1ce7fc674ae0fb7ffdb9534d6e930ece8da6935
M. Eric Irrgang <ericirrgang@gmail.com>
2022-08-16 14:48:22 +0300

    Improve web page load time.
    Use an SVG instead of a 1.4MB PNG for the logo.

--- docs/conf.cmakein.py
+++ docs/conf.cmakein.py
@@ -282,7 +282,7 @@ html_short_title = u'GROMACS ' + version
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'reference-manual/plots/GMX_logos/gmx_logo_blue.png'
+html_logo = 'reference-manual/plots/GMX_logos/gmx_logo_blue.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
