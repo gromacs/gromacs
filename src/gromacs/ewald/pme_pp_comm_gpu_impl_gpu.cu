gromacs/gromacs

commit 049d9013f6ca6bb5425534e4fb020d49fec7b662

Fix event usage in PP-PME direct comm
When we send coordinates from the CPU, we don't need to wait for them
to be ready on the GPU.

Refs #3988

src/gromacs/ewald/pme_pp_comm_gpu_impl_gpu.cu
@@ -63,7 +63,10 @@ void PmePpCommGpu::Impl::sendCoordinatesToPmePeerToPeer(Float3*               se
                                                         GpuEventSynchronizer* coordinatesReadyOnDeviceEvent)
 {
     // ensure stream waits until coordinate data is available on device
-    coordinatesReadyOnDeviceEvent->enqueueWaitEvent(pmePpCommStream_);
+    if (coordinatesReadyOnDeviceEvent)
+    {
+        coordinatesReadyOnDeviceEvent->waitForEvent();
+    }
 
     cudaError_t stat = cudaMemcpyAsync(remotePmeXBuffer_,
                                        sendPtr,

src/gromacs/ewald/pme_pp.cpp
@@ -267,13 +267,15 @@ static void gmx_pme_send_coeffs_coords(t_forcerec*                    fr,
             {
                 if (sendCoordinatesFromGpu)
                 {
+                    GMX_ASSERT(coordinatesReadyOnDeviceEvent != nullptr,
+                               "When sending coordinates from GPU, a synchronization event should "
+                               "be provided");
                     fr->pmePpCommGpu->sendCoordinatesToPmeFromGpu(
                             fr->stateGpu->getCoordinates(), n, coordinatesReadyOnDeviceEvent);
                 }
                 else
                 {
-                    fr->pmePpCommGpu->sendCoordinatesToPmeFromCpu(
-                            const_cast<gmx::RVec*>(x.data()), n, coordinatesReadyOnDeviceEvent);
+                    fr->pmePpCommGpu->sendCoordinatesToPmeFromCpu(const_cast<gmx::RVec*>(x.data()), n);
                 }
             }
             else

src/gromacs/ewald/pme_pp_comm_gpu.h
@@ -105,11 +105,8 @@ public:
     /*! \brief Push coordinates buffer from host memory directly to GPU memory on PME task
      * \param[in] sendPtr Buffer with coordinate data
      * \param[in] sendSize Number of elements to send
-     * \param[in] coordinatesReadyOnDeviceEvent Event recorded when coordinates are available on device
      */
-    void sendCoordinatesToPmeFromCpu(RVec*                 sendPtr,
-                                     int                   sendSize,
-                                     GpuEventSynchronizer* coordinatesReadyOnDeviceEvent);
+    void sendCoordinatesToPmeFromCpu(RVec* sendPtr, int sendSize);
 
     /*! \brief
      * Return pointer to buffer used for staging PME force on GPU

src/gromacs/ewald/pme_pp_comm_gpu_impl.cpp
@@ -99,9 +99,7 @@ void PmePpCommGpu::sendCoordinatesToPmeFromGpu(DeviceBuffer<RVec> /* sendPtr */,
                "implementation.");
 }
 
-void PmePpCommGpu::sendCoordinatesToPmeFromCpu(RVec* /* sendPtr */,
-                                               int /* sendSize */,
-                                               GpuEventSynchronizer* /* coordinatesOnDeviceEvent */)
+void PmePpCommGpu::sendCoordinatesToPmeFromCpu(RVec* /* sendPtr */, int /* sendSize */)
 {
     GMX_ASSERT(!impl_,
                "A CPU stub for PME-PP GPU communication was called instead of the correct "

src/gromacs/ewald/pme_pp_comm_gpu_impl_gpu.cpp
@@ -194,7 +194,10 @@ void PmePpCommGpu::Impl::sendCoordinatesToPmeGpuAwareMpi(Float3*               s
                                                          GpuEventSynchronizer* coordinatesReadyOnDeviceEvent)
 {
     // ensure coordinate data is available on device before we start transfer
-    coordinatesReadyOnDeviceEvent->waitForEvent();
+    if (coordinatesReadyOnDeviceEvent)
+    {
+        coordinatesReadyOnDeviceEvent->waitForEvent();
+    }
 
 #if GMX_MPI
     Float3* sendptr_x = sendPtr;
@@ -264,11 +267,9 @@ void PmePpCommGpu::sendCoordinatesToPmeFromGpu(DeviceBuffer<RVec>    sendPtr,
     impl_->sendCoordinatesToPme(sendPtr, sendSize, coordinatesReadyOnDeviceEvent);
 }
 
-void PmePpCommGpu::sendCoordinatesToPmeFromCpu(RVec*                 sendPtr,
-                                               int                   sendSize,
-                                               GpuEventSynchronizer* coordinatesReadyOnDeviceEvent)
+void PmePpCommGpu::sendCoordinatesToPmeFromCpu(RVec* sendPtr, int sendSize)
 {
-    impl_->sendCoordinatesToPme(sendPtr, sendSize, coordinatesReadyOnDeviceEvent);
+    impl_->sendCoordinatesToPme(sendPtr, sendSize, nullptr);
 }
 
 DeviceBuffer<Float3> PmePpCommGpu::getGpuForceStagingPtr()

src/gromacs/mdlib/sim_util.cpp
@@ -1525,7 +1525,7 @@ void do_force(FILE*                               fplog,
                                  reinitGpuPmePpComms,
                                  pmeSendCoordinatesFromGpu,
                                  stepWork.useGpuPmeFReduction,
-                                 localXReadyOnDevice,
+                                 pmeSendCoordinatesFromGpu ? localXReadyOnDevice : nullptr,
                                  wcycle);
     }
 

commit de33948aef0ffacc22c8dc13467402c885a35208

Fix P2P PP-PME communication event sync broken in 049d901

src/gromacs/ewald/pme_pp_comm_gpu_impl_gpu.cu
@@ -65,7 +65,7 @@ void PmePpCommGpu::Impl::sendCoordinatesToPmePeerToPeer(Float3*               se
     // ensure stream waits until coordinate data is available on device
     if (coordinatesReadyOnDeviceEvent)
     {
-        coordinatesReadyOnDeviceEvent->waitForEvent();
+        coordinatesReadyOnDeviceEvent->enqueueWaitEvent(pmePpCommStream_);
     }
 
     cudaError_t stat = cudaMemcpyAsync(remotePmeXBuffer_,