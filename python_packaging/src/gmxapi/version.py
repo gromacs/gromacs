f6c8f13a76fe5c997b135a382b9a64842a94e1f5
M. Eric Irrgang <ericirrgang@gmail.com>
2022-08-20 10:59:03 +0000

    Allow gmxapi client packages to find GROMACS libraries with CMake 3.24.0.
    Client package workaround for unpatched GROMACS releases:
    Do `find_package(GROMACS)` before `find_package(gmxapi)` in client
    software. Works around a problematic cmake exports file when
    GROMACS was configured and installed with CMake 3.24.
    
    Ref #4563

--- python_packaging/src/gmxapi/version.py
+++ python_packaging/src/gmxapi/version.py
@@ -71,7 +71,7 @@ from .exceptions import FeatureNotAvailableError
 _major = 0
 _minor = 3
 _micro = 2
-_suffix = ''
+_suffix = '.post1'
 
 # Reference https://www.python.org/dev/peps/pep-0440/
 # and https://packaging.pypa.io/en/latest/version/
