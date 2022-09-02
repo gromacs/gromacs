504fcfba281d8cc175511729a9ff9bca59337926
Paul Bauer <paul.bauer.q@gmail.com>
2022-09-02 13:33:04 +0200

    Add song lyrics cool quotes

--- src/gromacs/utility/coolstuff.cpp
+++ src/gromacs/utility/coolstuff.cpp
@@ -1703,6 +1703,11 @@ std::string getCoolQuote()
         { "It is not critical to add the next quote to a patch release", "Paul Bauer" },
         { "It is a cute toxin.", "Rebecca Howard" },
         { "Everything is failing", "Paul Bauer" },
+        { "Requiem, bring the dissident from slumber", "Bad Religion" },
+        { "I can't relate to you", "Bad Religion" },
+        { "You are wrong!", "NOFX" },
+        { "The final page is written in the books of history", "Bad Religion" },
+        { "Would you give it all up to live again?", "Bad Religion" },
 
     };
 
