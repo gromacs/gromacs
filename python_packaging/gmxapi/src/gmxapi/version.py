bf55a5447ff2530fc8fd2769b7ce8a4576836e7d
M. Eric Irrgang <ericirrgang@gmail.com>
2022-09-07 07:53:02 +0000

    Update minimum required GROMACS version for gmxapi Python package
    Closes #4576
    
    Closes #4576

--- python_packaging/gmxapi/src/gmxapi/version.py
+++ python_packaging/gmxapi/src/gmxapi/version.py
@@ -71,7 +71,7 @@ from .exceptions import FeatureNotAvailableError
 _major = 0
 _minor = 4
 _micro = 0
-_suffix = 'a2'
+_suffix = 'a3'
 
 # Reference https://www.python.org/dev/peps/pep-0440/
 # and https://packaging.pypa.io/en/latest/version/
