gromacs/gromacs

commit 0ad4a2ede3ff6edd0f27baf73418fed5c2af6a33

Template and unify nbnxm SIMD kernels
The nbnxm SIMD non-bonded kernel outer and inner loop bodys have
been rewritten to remove all code duplication. This is done by
using std::arrays instead of multiple 'i'-registers with
suffixes. Additionally the code is made much readable and flexible
by putting all Coulomb and LJ interaction functions in inline
functions which are selected by template specialization.
All kernel flavor switches are now controlled internally by
constexpr variables. The kernel generation has not yet been changed,
this is left to a next step.

Note that kernel_outer.h and kernel_inner.h are now identical for
4xM and 2xMM. These should be de-duplicated, but are left separate
for easier review.

Refs #4559

src/gromacs/nbnxm/simd_load_store_functions.h
@@ -1,0 +1,272 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright 2022- The GROMACS Authors
+ * and the project initiators Erik Lindahl, Berk Hess and David van der Spoel.
+ * Consult the AUTHORS/COPYING files and https://www.gromacs.org for details.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * https://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at https://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out https://www.gromacs.org.
+ */
+/*! \internal \file
+ *
+ * \brief
+ * Defines functions to load data to and store data from SIMD registers
+ * for the SIMD 4xM and 2xMM kernels.
+ *
+ * \author Berk Hess <hess@kth.se>
+ * \ingroup module_nbnxm
+ */
+#ifndef GMX_NBNXM_SIMD_LOAD_STORE_FUNCTIONS_H
+#define GMX_NBNXM_SIMD_LOAD_STORE_FUNCTIONS_H
+
+#include "config.h"
+
+#include "gromacs/math/vectypes.h"
+#include "gromacs/simd/simd.h"
+#include "gromacs/utility/real.h"
+
+namespace gmx
+{
+
+//! Load a single real for an i-atom into \p iRegister
+template<KernelLayout kernelLayout>
+inline std::enable_if_t<kernelLayout == KernelLayout::r4xM, SimdReal> loadIAtomData(const real* ptr,
+                                                                                    const int offset,
+                                                                                    const int iRegister)
+{
+    return SimdReal(ptr[offset + iRegister]);
+}
+
+//! Load a pair of consecutive reals for two i-atom into the respective halves of \p iRegister
+template<KernelLayout kernelLayout>
+inline std::enable_if_t<kernelLayout == KernelLayout::r2xMM, SimdReal>
+loadIAtomData(const real* ptr, const int offset, const int iRegister)
+{
+    return loadU1DualHsimd(ptr + offset + iRegister * 2);
+}
+
+//! Returns a SIMD register containing GMX_SIMD_REAL_WIDTH reals loaded from ptr + offset
+template<KernelLayout kernelLayout>
+inline std::enable_if_t<kernelLayout == KernelLayout::r4xM, SimdReal> loadJAtomData(const real* ptr,
+                                                                                    const int offset)
+{
+    return load<SimdReal>(ptr + offset);
+}
+
+//! Returns a SIMD register containing a duplicate sequence of GMX_SIMD_REAL_WIDTH/2 reals loaded from ptr + offset
+template<KernelLayout kernelLayout>
+inline std::enable_if_t<kernelLayout == KernelLayout::r2xMM, SimdReal> loadJAtomData(const real* ptr,
+                                                                                     const int offset)
+{
+    return loadDuplicateHsimd(ptr + offset);
+}
+
+#if GMX_SIMD_HAVE_INT32_LOGICAL
+//! Define SimdBitMask as an integer SIMD register
+typedef SimdInt32 SimdBitMask;
+#else
+//! Define SimdBitMask as a real SIMD register
+typedef SimdReal SimdBitMask;
+#endif
+
+//! Loads no interaction masks, returns an empty array
+template<bool loadMasks, KernelLayout kernelLayout>
+inline std::enable_if_t<!loadMasks, std::array<SimdBool, 0>>
+loadSimdPairInteractionMasks(const int excl, SimdBitMask* filterBitMasksV, const real* simdInteractionArray)
+{
+    return std::array<SimdBool, 0>{};
+
+    GMX_UNUSED_VALUE(excl);
+    GMX_UNUSED_VALUE(filterBitMasksV);
+    GMX_UNUSED_VALUE(simdInteractionArray);
+}
+
+//! Loads interaction masks for a cluster pair for 4xM kernel layout
+template<bool loadMasks, KernelLayout kernelLayout>
+inline std::enable_if_t<loadMasks && kernelLayout == KernelLayout::r4xM, std::array<SimdBool, c_nbnxnCpuIClusterSize>>
+loadSimdPairInteractionMasks(const int excl, SimdBitMask* filterBitMasksV, const real* simdInteractionArray)
+{
+    using namespace gmx;
+
+    std::array<SimdBool, c_nbnxnCpuIClusterSize> interactionMasksV;
+
+#if GMX_SIMD_HAVE_INT32_LOGICAL
+    /* Load integer interaction mask */
+    SimdInt32 mask_pr_S(excl);
+    for (int i = 0; i < c_nbnxnCpuIClusterSize; i++)
+    {
+        interactionMasksV[i] = cvtIB2B(testBits(mask_pr_S & filterBitMasksV[i]));
+    }
+
+    GMX_UNUSED_VALUE(simdInteractionArray);
+#elif GMX_SIMD_HAVE_LOGICAL
+    union
+    {
+#    if GMX_DOUBLE
+        std::int64_t i;
+#    else
+        std::int32_t i;
+#    endif
+        real         r;
+    } conv;
+
+    conv.i = excl;
+    SimdReal mask_pr_S(conv.r);
+
+    for (int i = 0; i < c_nbnxnCpuIClusterSize; i++)
+    {
+        interactionMasksV[i] = testBits(mask_pr_S & filterBitMasksV[i]);
+    }
+
+    GMX_UNUSED_VALUE(simdInteractionArray);
+#else
+    // Neither real or integer bitwise logical operations supported.
+    // Load masks from memory instead.
+    SimdReal zero = setZero();
+    for (int i = 0; i < c_nbnxnCpuIClusterSize; i++)
+    {
+        interactionMasksV[i] =
+                (zero < load<SimdReal>(simdInteractionArray
+                                       + GMX_SIMD_REAL_WIDTH * ((excl >> (i * UNROLLJ)) & 0xF)));
+    }
+
+    GMX_UNUSED_VALUE(filterBitMasksV);
+#endif
+
+    return interactionMasksV;
+}
+
+//! Loads interaction masks for a cluster pair for 2xMM kernel layout
+template<bool loadMasks, KernelLayout kernelLayout>
+inline std::enable_if_t<loadMasks && kernelLayout == KernelLayout::r2xMM, std::array<SimdBool, c_nbnxnCpuIClusterSize / 2>>
+loadSimdPairInteractionMasks(const int excl, SimdBitMask* filterBitMasksV, const real* simdInteractionArray)
+{
+    using namespace gmx;
+
+    std::array<SimdBool, c_nbnxnCpuIClusterSize / 2> interactionMasksV;
+
+#if GMX_SIMD_HAVE_INT32_LOGICAL
+    SimdInt32 mask_pr_S(excl);
+    for (int i = 0; i < c_nbnxnCpuIClusterSize / 2; i++)
+    {
+        interactionMasksV[i] = cvtIB2B(testBits(mask_pr_S & filterBitMasksV[i]));
+    }
+
+    GMX_UNUSED_VALUE(simdInteractionArray);
+#elif GMX_SIMD_HAVE_LOGICAL
+    union
+    {
+#    if GMX_DOUBLE
+        std::int64_t i;
+#    else
+        std::int32_t i;
+#    endif
+        real         r;
+    } conv;
+
+    conv.i = excl;
+    SimdReal mask_pr_S(conv.r);
+
+    for (int i = 0; i < c_nbnxnCpuIClusterSize / 2; i++)
+    {
+        interactionMasksV[i] = testBits(mask_pr_S & filterBitMasksV[i]);
+    }
+
+    GMX_UNUSED_VALUE(simdInteractionArray);
+#else
+#    error "the SIMD architecture does not support 2xMM interaction mask loading"
+#endif
+
+    return interactionMasksV;
+}
+
+//! Adds energies to temporary energy group pair buffers for the 4xM kernel layout
+template<KernelLayout kernelLayout, std::size_t offsetJJSize>
+inline void accumulateGroupPairEnergies4xM(SimdReal energies,
+                                           real*    groupPairEnergyBuffersPtr,
+                                           const std::array<int, offsetJJSize>& offsetJJ)
+{
+    using namespace gmx;
+
+    static_assert(offsetJJSize == GMX_SIMD_REAL_WIDTH / 2);
+
+    /* We need to balance the number of store operations with
+     * the rapidly increasing number of combinations of energy groups.
+     * We add to a temporary buffer for 1 i-group vs 2 j-groups.
+     */
+    for (int jj = 0; jj < GMX_SIMD_REAL_WIDTH / 2; jj++)
+    {
+        SimdReal groupPairEnergyBuffers =
+                load<SimdReal>(groupPairEnergyBuffersPtr + offsetJJ[jj] + jj * GMX_SIMD_REAL_WIDTH);
+
+        store(groupPairEnergyBuffersPtr + offsetJJ[jj] + jj * GMX_SIMD_REAL_WIDTH,
+              groupPairEnergyBuffers + energies);
+    }
+}
+
+//! Adds energies to temporary energy group pair buffers for the 2xMM kernel layout
+template<KernelLayout kernelLayout, std::size_t offsetJJSize>
+inline void accumulateGroupPairEnergies2xMM(SimdReal energies,
+                                            real*    groupPairEnergyBuffersPtr0,
+                                            real*    groupPairEnergyBuffersPtr1,
+                                            const std::array<int, offsetJJSize>& offsetJJ)
+{
+    static_assert(offsetJJSize == GMX_SIMD_REAL_WIDTH / 2);
+
+    for (int jj = 0; jj < GMX_SIMD_REAL_WIDTH / 2; jj++)
+    {
+        incrDualHsimd(groupPairEnergyBuffersPtr0 + offsetJJ[jj] + jj * GMX_SIMD_REAL_WIDTH / 2,
+                      groupPairEnergyBuffersPtr1 + offsetJJ[jj] + jj * GMX_SIMD_REAL_WIDTH / 2,
+                      energies);
+    }
+}
+
+//! Return the number of atoms pairs that are within the cut-off distance
+template<int nR>
+inline int pairCountWithinCutoff(SimdReal rSquaredV[nR], SimdReal cutoffSquared)
+{
+    alignas(GMX_SIMD_ALIGNMENT) real tmp[GMX_SIMD_REAL_WIDTH];
+
+    int npair = 0;
+    for (int i = 0; i < nR; i++)
+    {
+        store(tmp, cutoffSquared - rSquaredV[i]);
+        for (int j = 0; j < GMX_SIMD_REAL_WIDTH; j++)
+        {
+            if (tmp[j] >= 0)
+            {
+                npair++;
+            }
+        }
+    }
+
+    return npair;
+}
+
+} // namespace gmx
+
+#endif // GMX_NBNXM_SIMD_LOAD_STORE_FUNCTIONS_H

src/gromacs/nbnxm/kernels_simd_2xmm/kernel_common.h
@@ -31,6 +31,9 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the research papers on the package. Check out https://www.gromacs.org.
  */
+#include "gromacs/nbnxm/simd_coulomb_functions.h"
+#include "gromacs/nbnxm/simd_lennardjones_functions.h"
+#include "gromacs/nbnxm/simd_load_store_functions.h"
 #include "gromacs/pbcutil/ishift.h"
 #include "gromacs/simd/simd.h"
 #include "gromacs/simd/simd_math.h"
@@ -62,63 +65,6 @@ static_assert(UNROLLI == c_nbnxnCpuIClusterSize, "UNROLLI should match the i-clu
 #if !defined GMX_NBNXN_SIMD_2XNN && !defined GMX_NBNXN_SIMD_4XN
 #    error "Must define an NBNxN kernel flavour before including NBNxN kernel utility functions"
 #endif
-
-// We use the FDV0 tables for width==4 (when we can load it in one go), or if we don't have any unaligned loads
-#if GMX_SIMD_REAL_WIDTH == 4 || !GMX_SIMD_HAVE_GATHER_LOADU_BYSIMDINT_TRANSPOSE_REAL
-#    define TAB_FDV0
-#endif
-
-#if defined UNROLLJ
-/* As add_ener_grp, but for two groups of UNROLLJ/2 stored in
- * a single SIMD register.
- */
-static inline void add_ener_grp_halves(gmx::SimdReal e_S, real* v0, real* v1, const int* offset_jj)
-{
-    for (int jj = 0; jj < (UNROLLJ / 2); jj++)
-    {
-        incrDualHsimd(v0 + offset_jj[jj] + jj * GMX_SIMD_REAL_WIDTH / 2,
-                      v1 + offset_jj[jj] + jj * GMX_SIMD_REAL_WIDTH / 2,
-                      e_S);
-    }
-}
-#endif
-
-#if GMX_SIMD_HAVE_INT32_LOGICAL
-typedef gmx::SimdInt32 SimdBitMask;
-#else
-typedef gmx::SimdReal SimdBitMask;
-#endif
-
-
-static inline void gmx_simdcall gmx_load_simd_2xnn_interactions(int            excl,
-                                                                SimdBitMask    filter_S0,
-                                                                SimdBitMask    filter_S2,
-                                                                gmx::SimdBool* interact_S0,
-                                                                gmx::SimdBool* interact_S2)
-{
-    using namespace gmx;
-#if GMX_SIMD_HAVE_INT32_LOGICAL
-    SimdInt32 mask_pr_S(excl);
-    *interact_S0 = cvtIB2B(testBits(mask_pr_S & filter_S0));
-    *interact_S2 = cvtIB2B(testBits(mask_pr_S & filter_S2));
-#elif GMX_SIMD_HAVE_LOGICAL
-    union
-    {
-#    if GMX_DOUBLE
-        std::int64_t i;
-#    else
-        std::int32_t i;
-#    endif
-        real         r;
-    } conv;
-
-    conv.i = excl;
-    SimdReal mask_pr_S(conv.r);
-
-    *interact_S0 = testBits(mask_pr_S & filter_S0);
-    *interact_S2 = testBits(mask_pr_S & filter_S2);
-#endif
-}
 
 /* All functionality defines are set here, except for:
  * CALC_ENERGIES, ENERGY_GROUPS which are defined before.

src/gromacs/nbnxm/kernels_simd_4xm/kernel_common.h
@@ -31,11 +31,13 @@
  * To help us fund GROMACS development, we humbly ask that you cite
  * the research papers on the package. Check out https://www.gromacs.org.
  */
+#include "gromacs/nbnxm/simd_coulomb_functions.h"
+#include "gromacs/nbnxm/simd_lennardjones_functions.h"
+#include "gromacs/nbnxm/simd_load_store_functions.h"
 #include "gromacs/pbcutil/ishift.h"
 #include "gromacs/simd/simd.h"
 #include "gromacs/simd/simd_math.h"
 #include "gromacs/simd/vector_operations.h"
-#include "gromacs/utility/basedefinitions.h"
 #ifdef CALC_COUL_EWALD
 #    include "gromacs/math/utilities.h"
 #endif
@@ -62,90 +64,6 @@ static_assert(UNROLLI == c_nbnxnCpuIClusterSize, "UNROLLI should match the i-clu
 #if !defined GMX_NBNXN_SIMD_2XNN && !defined GMX_NBNXN_SIMD_4XN
 #    error "Must define an NBNxN kernel flavour before including NBNxN kernel utility functions"
 #endif
-
-// We use the FDV0 tables for width==4 (when we can load it in one go), or if we don't have any unaligned loads
-#if GMX_SIMD_REAL_WIDTH == 4 || !GMX_SIMD_HAVE_GATHER_LOADU_BYSIMDINT_TRANSPOSE_REAL
-#    define TAB_FDV0
-#endif
-
-
-#ifdef UNROLLJ
-/* Add energy register to possibly multiple terms in the energy array */
-static inline void add_ener_grp(gmx::SimdReal e_S, real* v, const int* offset_jj)
-{
-    using namespace gmx;
-
-    /* We need to balance the number of store operations with
-     * the rapidly increases number of combinations of energy groups.
-     * We add to a temporary buffer for 1 i-group vs 2 j-groups.
-     */
-    for (int jj = 0; jj < (UNROLLJ / 2); jj++)
-    {
-        SimdReal v_S;
-
-        v_S = load<SimdReal>(v + offset_jj[jj] + jj * GMX_SIMD_REAL_WIDTH);
-        store(v + offset_jj[jj] + jj * GMX_SIMD_REAL_WIDTH, v_S + e_S);
-    }
-}
-#endif
-
-#if GMX_SIMD_HAVE_INT32_LOGICAL
-typedef gmx::SimdInt32 SimdBitMask;
-#else
-typedef gmx::SimdReal SimdBitMask;
-#endif
-
-static inline void gmx_simdcall gmx_load_simd_4xn_interactions(int                    excl,
-                                                               SimdBitMask gmx_unused filter_S0,
-                                                               SimdBitMask gmx_unused filter_S1,
-                                                               SimdBitMask gmx_unused filter_S2,
-                                                               SimdBitMask gmx_unused filter_S3,
-                                                               const real gmx_unused* simd_interaction_array,
-                                                               gmx::SimdBool* interact_S0,
-                                                               gmx::SimdBool* interact_S1,
-                                                               gmx::SimdBool* interact_S2,
-                                                               gmx::SimdBool* interact_S3)
-{
-    using namespace gmx;
-#if GMX_SIMD_HAVE_INT32_LOGICAL
-    /* Load integer interaction mask */
-    SimdInt32 mask_pr_S(excl);
-    *interact_S0 = cvtIB2B(testBits(mask_pr_S & filter_S0));
-    *interact_S1 = cvtIB2B(testBits(mask_pr_S & filter_S1));
-    *interact_S2 = cvtIB2B(testBits(mask_pr_S & filter_S2));
-    *interact_S3 = cvtIB2B(testBits(mask_pr_S & filter_S3));
-#elif GMX_SIMD_HAVE_LOGICAL
-    union
-    {
-#    if GMX_DOUBLE
-        std::int64_t i;
-#    else
-        std::int32_t i;
-#    endif
-        real         r;
-    } conv;
-
-    conv.i = excl;
-    SimdReal mask_pr_S(conv.r);
-
-    *interact_S0 = testBits(mask_pr_S & filter_S0);
-    *interact_S1 = testBits(mask_pr_S & filter_S1);
-    *interact_S2 = testBits(mask_pr_S & filter_S2);
-    *interact_S3 = testBits(mask_pr_S & filter_S3);
-#else
-    // Neither real or integer bitwise logical operations supported.
-    // Load masks from memory instead.
-    SimdReal zero = setZero();
-    *interact_S0  = (zero < load<SimdReal>(simd_interaction_array
-                                          + GMX_SIMD_REAL_WIDTH * ((excl >> (0 * UNROLLJ)) & 0xF)));
-    *interact_S1  = (zero < load<SimdReal>(simd_interaction_array
-                                          + GMX_SIMD_REAL_WIDTH * ((excl >> (1 * UNROLLJ)) & 0xF)));
-    *interact_S2  = (zero < load<SimdReal>(simd_interaction_array
-                                          + GMX_SIMD_REAL_WIDTH * ((excl >> (2 * UNROLLJ)) & 0xF)));
-    *interact_S3  = (zero < load<SimdReal>(simd_interaction_array
-                                          + GMX_SIMD_REAL_WIDTH * ((excl >> (3 * UNROLLJ)) & 0xF)));
-#endif
-}
 
 /* All functionality defines are set here, except for:
  * CALC_ENERGIES, ENERGY_GROUPS which are defined before.

src/gromacs/nbnxm/nbnxm_simd.h
@@ -34,7 +34,7 @@
 /*! \internal \file
  *
  * \brief
- * Defines constants used to know which Nbnxm kernel flavours (4xn or 2xnn)
+ * Defines constants used to know which nbNxM kernel flavours (4xM or 2xMM)
  * can be supported by the SIMD layer in use.
  *
  * \author Berk Hess <hess@kth.se>
@@ -49,11 +49,20 @@
 #include "gromacs/simd/simd.h"
 #include "gromacs/utility/real.h"
 
+#include "pairlistparams.h"
+
+//! The types of nbNxM SIMD kernel layout
+enum class KernelLayout
+{
+    r4xM, //!< 4 'i'-registers each containing data for interaction with M j-atoms
+    r2xMM //!< 2 'i'-registers each containing duplicated data, { M, M }, for interaction with M j-atoms
+};
+
 #if GMX_SIMD && GMX_USE_SIMD_KERNELS
 /*! \brief The nbnxn SIMD 4xN and 2x(N+N) kernels can be added independently.
  * Currently the 2xNN SIMD kernels only make sense with:
- *  8-way SIMD: 4x4 setup, works with AVX-256 in single precision
- * 16-way SIMD: 4x8 setup, not currently in use, but worked with Intel MIC
+ *  8-way SIMD: 4x4 setup, performance wise only useful on CPUs without FMA or on AMD Zen1
+ * 16-way SIMD: 4x8 setup, used in single precision with 512 bit wide SIMD
  */
 #    if GMX_SIMD_REAL_WIDTH == 2 || GMX_SIMD_REAL_WIDTH == 4 || GMX_SIMD_REAL_WIDTH == 8
 #        define GMX_NBNXN_SIMD_4XN
@@ -66,6 +75,77 @@
 #        error "No SIMD kernel type defined"
 #    endif
 
+// We use the FDV0 tables for width==4 (when we can load it in one go), or if we don't have any unaligned loads
+#    if GMX_SIMD_REAL_WIDTH == 4 || !GMX_SIMD_HAVE_GATHER_LOADU_BYSIMDINT_TRANSPOSE_REAL
+static constexpr bool c_useTableFormatFDV0 = true;
+#    else
+static constexpr bool c_useTableFormatFDV0 = false;
+#    endif
+
+/*! \brief Helper function to generate an std::array of SimdReal using a lambda
+ *
+ * An example of intended usage is:
+ * auto rV = genArr<4>([&](int i) { return rSquaredV[i] * rInvV[i]; });
+ *
+ * \tparam N  The size of the array
+ * \tparam F  The function type, deduced
+ * \param[in] f  The function
+ */
+template<int N, class F>
+std::array<gmx::SimdReal, N> genArr(F f)
+{
+    static_assert(N <= 2 || N == 4);
+
+    if constexpr (N == 0)
+    {
+        return std::array<gmx::SimdReal, 0>{};
+    }
+    else if constexpr (N == 1)
+    {
+        return std::array<gmx::SimdReal, 1>{ f(0) };
+    }
+    else if constexpr (N == 2)
+    {
+        return std::array<gmx::SimdReal, 2>{ f(0), f(1) };
+    }
+    else
+    {
+        return std::array<gmx::SimdReal, 4>{ f(0), f(1), f(2), f(3) };
+    }
+}
+
+/*! \brief Helper function to generate an std::array of SimdBool using a lambda
+ *
+ * An example of intended usage is:
+ * auto withinCutoffV = genBoolArr<nR>([&](int i) { return rSquaredV[i] < rc2_S; });
+ *
+ * \tparam N  The size of the array
+ * \tparam F  The function type, deduced
+ * \param[in] f  The function
+ */
+template<int N, class F>
+std::array<gmx::SimdBool, N> genBoolArr(F f)
+{
+    static_assert(N <= 2 || N == 4);
+
+    if constexpr (N == 0)
+    {
+        return std::array<gmx::SimdBool, 0>{};
+    }
+    else if constexpr (N == 1)
+    {
+        return std::array<gmx::SimdBool, 1>{ f(0) };
+    }
+    else if constexpr (N == 2)
+    {
+        return std::array<gmx::SimdBool, 2>{ f(0), f(1) };
+    }
+    else
+    {
+        return std::array<gmx::SimdBool, 4>{ f(0), f(1), f(2), f(3) };
+    }
+}
+
 #endif // GMX_SIMD && GMX_USE_SIMD_KERNELS
 
-#endif
+#endif // GMX_NBNXM_NBNXM_SIMD

commit b9f277e0f7b60b2c11bc47e6b4108fde9ce9b13b

Fix nbnxm 2xMM energy group accumulation
Broken by recent commit 0ad4a2ede3f.

src/gromacs/nbnxm/simd_load_store_functions.h
@@ -224,7 +224,7 @@ inline void accumulateGroupPairEnergies2xMM(SimdReal energies,
 {
     static_assert(offsetJJSize == GMX_SIMD_REAL_WIDTH / 4);
 
-    for (int jj = 0; jj < GMX_SIMD_REAL_WIDTH / 2; jj++)
+    for (int jj = 0; jj < GMX_SIMD_REAL_WIDTH / 4; jj++)
     {
         incrDualHsimd(groupPairEnergyBuffersPtr0 + offsetJJ[jj] + jj * GMX_SIMD_REAL_WIDTH / 2,
                       groupPairEnergyBuffersPtr1 + offsetJJ[jj] + jj * GMX_SIMD_REAL_WIDTH / 2,