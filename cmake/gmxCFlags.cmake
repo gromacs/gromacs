0e1f1e250ebcf3583f3865b62a23d9f63e3364ca
Andrey Alekseenko <al42and@gmail.com>
2022-09-29 10:53:59 +0000

    Silence ICPX warning in debug mode about lack of optimizations
    Starting from Intel oneAPI DPC++/C++ Compiler 2022.2.0 (part of oneAPI 2022.3.0),
    when compiling in Debug mode, a warning was printed for every file:
    
        icpx: warning: Note that use of '-g' without any optimization-level option will turn off most compiler optimizations similar to use of '-O0' [-Wdebug-disables-optimization]

--- cmake/gmxCFlags.cmake
+++ cmake/gmxCFlags.cmake
@@ -352,6 +352,9 @@ macro (gmx_c_flags)
             GMX_TEST_CFLAG(CFLAGS_WARN "-Wall;-Wno-unused;-Wunused-value;-Wunused-parameter" GMXC_CFLAGS)
             GMX_TEST_CFLAG(CFLAGS_WARN_EXTRA "-Wpointer-arith" GMXC_CFLAGS_EXTRA)
         endif()
+        if (CMAKE_BUILD_TYPE MATCHES "Debug")
+            GMX_TEST_CFLAG(CFLAGS_NO_DEBUG_DISABLES_OPTIMIZATION "-Wno-debug-disables-optimization" GMXC_CFLAGS)
+        endif()
         GMX_TEST_CFLAG(CFLAGS_WARN_NO_MISSING_FIELD_INITIALIZERS "-Wno-missing-field-initializers" GMXC_CFLAGS)
     endif()
 
@@ -375,6 +378,9 @@ macro (gmx_c_flags)
         endif()
         GMX_TEST_CXXFLAG(CXXFLAGS_WARN_NO_RESERVED_IDENTIFIER "-Wno-reserved-identifier" GMXC_CXXFLAGS) # LLVM BUG #50644
         GMX_TEST_CXXFLAG(CXXFLAGS_WARN_NO_MISSING_FIELD_INITIALIZERS "-Wno-missing-field-initializers" GMXC_CXXFLAGS)
+        if (CMAKE_BUILD_TYPE MATCHES "Debug")
+            GMX_TEST_CXXFLAG(CXXFLAGS_NO_DEBUG_DISABLES_OPTIMIZATION "-Wno-debug-disables-optimization" GMXC_CXXFLAGS)
+        endif()
         # Some versions of Intel ICPX compiler (at least 2021.1.1 to 2021.3.0) fail to unroll a loop
         # in sycl::accessor::__init, and emit -Wpass-failed=transform-warning. This is a useful
         # warning, but mostly noise right now. Probably related to using shared memory accessors.
