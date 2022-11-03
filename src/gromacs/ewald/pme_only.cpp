gromacs/gromacs

commit bd0dfc998a87baab2eba8c046a1b7978a10e0c33

Properly release gmx_pme_t datastructures
We had been leaking things like FFT plans.

`gmx_pme_reinit` overwrites its destination argument, which can be
the same as its source, so we have to dance around it.

Also free the PME load balancer buffer at the end.

Refs #4522, #4387

src/gromacs/ewald/pme_only.cpp
@@ -200,8 +200,11 @@ static gmx_pme_t* gmx_pmeonly_switch(std::vector<gmx_pme_t*>* pmedata,
              * So, just some grid size updates in the GPU kernel parameters.
              * TODO: this should be something like gmx_pme_update_split_params()
              */
-            gmx_pme_reinit(&pme, cr, pme, ir, grid_size, ewaldcoeff_q, ewaldcoeff_lj);
-            return pme;
+            gmx_pme_t* pmeNew;
+            gmx_pme_reinit(&pmeNew, cr, pme, ir, grid_size, ewaldcoeff_q, ewaldcoeff_lj);
+            gmx_pme_destroy(pme, false);
+            pme = pmeNew;
+            return pmeNew;
         }
     }
 

src/gromacs/ewald/pme_load_balancing.cpp
@@ -867,11 +867,16 @@ static void pme_load_balance(pme_load_balancing_t*          pme_lb,
         if ((pme_lb->setup[pme_lb->cur].pmedata == nullptr)
             || pme_gpu_task_enabled(pme_lb->setup[pme_lb->cur].pmedata))
         {
-            /* Generate a new PME data structure,
-             * copying part of the old pointers.
-             */
+            gmx_pme_t* newPmeData;
+            // Generate a new PME data structure, copying part of the old pointers.
             gmx_pme_reinit(
-                    &set->pmedata, cr, pme_lb->setup[0].pmedata, &ir, set->grid, set->ewaldcoeff_q, set->ewaldcoeff_lj);
+                    &newPmeData, cr, pme_lb->setup[0].pmedata, &ir, set->grid, set->ewaldcoeff_q, set->ewaldcoeff_lj);
+            // Destroy the old structure. Must be done after gmx_pme_reinit in case pme_lb->cur is 0.
+            if (set->pmedata != nullptr)
+            {
+                gmx_pme_destroy(set->pmedata, false);
+            }
+            set->pmedata = newPmeData;
         }
         *pmedata = set->pmedata;
     }
@@ -1179,6 +1184,14 @@ void pme_loadbal_done(pme_load_balancing_t* pme_lb, FILE* fplog, const gmx::MDLo
     if (fplog != nullptr && (pme_lb->cur > 0 || pme_lb->elimited != PmeLoadBalancingLimit::No))
     {
         print_pme_loadbal_settings(pme_lb, fplog, mdlog, bNonBondedOnGPU);
+    }
+    for (int i = 0; i < gmx::ssize(pme_lb->setup); i++)
+    {
+        // current element is stored in forcerec and free'd in Mdrunner::mdruner, together with shared data
+        if (i != pme_lb->cur)
+        {
+            gmx_pme_destroy(pme_lb->setup[i].pmedata, false);
+        }
     }
 
     delete pme_lb;

commit 38ef92b0a12dec4eef341521788c2445a217b86b

Fix double-free with separate GPU PME rank
Fixes bug introduced in bd0dfc9 which caused a double free and segfault
at the end of the run when using PME tuning and separate GPU PME ranks.

Fixes #4649

src/gromacs/ewald/pme_only.cpp
@@ -625,7 +625,7 @@ static void gmx_pme_send_force_vir_ener(const gmx_pme_t& pme, gmx_pme_pp* pme_pp
 #endif
 }
 
-int gmx_pmeonly(struct gmx_pme_t*               pme,
+int gmx_pmeonly(struct gmx_pme_t**              pmeFromRunnerPtr,
                 const t_commrec*                cr,
                 t_nrnb*                         mynrnb,
                 gmx_wallcycle*                  wcycle,
@@ -642,13 +642,16 @@ int gmx_pmeonly(struct gmx_pme_t*               pme,
     real    lambda_lj  = 0;
     int     maxshift_x = 0, maxshift_y = 0;
     float   cycles;
-    int     count;
     bool    computeEnergyAndVirial = false;
     int64_t step;
 
-    /* This data will only use with PME tuning, i.e. switching PME grids */
+    gmx_pme_t* pmeFromRunner = *pmeFromRunnerPtr;
+
+    // This data will only use with PME tuning, i.e. switching PME grids
+    // The first element comes from outside, the rest is generated here,
+    // but all elements are freed at the end of this function or when switching configs.
     std::vector<gmx_pme_t*> pmedata;
-    pmedata.push_back(pme);
+    pmedata.push_back(pmeFromRunner);
 
     auto pme_pp = gmx_pme_pp_init(cr);
 
@@ -669,11 +672,11 @@ int gmx_pmeonly(struct gmx_pme_t*               pme,
         {
             pme_pp->pmeCoordinateReceiverGpu = std::make_unique<gmx::PmeCoordinateReceiverGpu>(
                     pme_pp->mpi_comm_mysim, deviceStreamManager->context(), pme_pp->ppRanks);
-            pme_pp->pmeForceSenderGpu =
-                    std::make_unique<gmx::PmeForceSenderGpu>(pme_gpu_get_f_ready_synchronizer(pme),
-                                                             pme_pp->mpi_comm_mysim,
-                                                             deviceStreamManager->context(),
-                                                             pme_pp->ppRanks);
+            pme_pp->pmeForceSenderGpu = std::make_unique<gmx::PmeForceSenderGpu>(
+                    pme_gpu_get_f_ready_synchronizer(pmeFromRunner),
+                    pme_pp->mpi_comm_mysim,
+                    deviceStreamManager->context(),
+                    pme_pp->ppRanks);
         }
         // TODO: Special PME-only constructor is used here. There is no mechanism to prevent from using the other constructor here.
         //       This should be made safer.
@@ -681,13 +684,15 @@ int gmx_pmeonly(struct gmx_pme_t*               pme,
                 &deviceStreamManager->stream(gmx::DeviceStreamType::Pme),
                 deviceStreamManager->context(),
                 GpuApiCallBehavior::Async,
-                pme_gpu_get_block_size(pme),
+                pme_gpu_get_block_size(pmeFromRunner),
                 wcycle);
     }
 
     clear_nrnb(mynrnb);
 
-    count = 0;
+    // the current PME data structure, may change due to PME tuning
+    gmx_pme_t* pme               = pmeFromRunner;
+    bool       haveStartedTiming = false;
     do /****** this is a quasi-loop over time steps! */
     {
         /* The reason for having a loop here is PME grid tuning/switching */
@@ -732,8 +737,11 @@ int gmx_pmeonly(struct gmx_pme_t*               pme,
             break;
         }
 
-        if (count == 0)
+        // Starting wall-time accounting is delayed to avoid the PME rank
+        // start its timer while the PP is still doing initialization.
+        if (!haveStartedTiming)
         {
+            haveStartedTiming = true;
             wallcycle_start(wcycle, WallCycleCounter::Run);
             walltime_accounting_start_time(walltime_accounting);
         }
@@ -815,15 +823,16 @@ int gmx_pmeonly(struct gmx_pme_t*               pme,
         cycles = wallcycle_stop(wcycle, WallCycleCounter::PmeMesh);
         gmx_pme_send_force_vir_ener(*pme, pme_pp.get(), output, cycles);
 
-        count++;
     } /***** end of quasi-loop, we stop with the break above */
     while (TRUE);
 
-    // The first element, `pme`, will be freed outside this function
-    for (size_t i = 1; i < pmedata.size(); i++)
+    for (size_t i = 0; i < pmedata.size(); i++)
     {
-        gmx_pme_destroy(pmedata[i], false);
+        bool destroySharedData = (i == pmedata.size() - 1);
+        gmx_pme_destroy(pmedata[i], destroySharedData);
     }
+    // reset the PME pointer passed from runner
+    *pmeFromRunnerPtr = nullptr;
     walltime_accounting_end_time(walltime_accounting);
 
     return 0;

src/gromacs/ewald/pme_only.h
@@ -61,7 +61,7 @@ class DeviceStreamManager;
 }
 
 /*! \brief Called on the nodes that do PME exclusively */
-int gmx_pmeonly(gmx_pme_t*                      pme,
+int gmx_pmeonly(gmx_pme_t**                     pme,
                 const t_commrec*                cr,
                 t_nrnb*                         mynrnb,
                 gmx_wallcycle*                  wcycle,

src/gromacs/mdrun/runner.cpp
@@ -2138,7 +2138,7 @@ int Mdrunner::mdrunner()
         GMX_RELEASE_ASSERT(pmedata, "pmedata was NULL while cr->duty was not DUTY_PP");
         /* do PME only */
         walltime_accounting = walltime_accounting_init(gmx_omp_nthreads_get(ModuleMultiThread::Pme));
-        gmx_pmeonly(pmedata,
+        gmx_pmeonly(&pmedata,
                     cr,
                     &nrnb,
                     wcycle.get(),