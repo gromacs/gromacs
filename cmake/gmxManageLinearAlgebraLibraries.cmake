gromacs/gromacs

commit baf1f8f481f6aa585922ba4c2d3efd1b108f1fa4

Describe origin of linear algebra routines more clearly
This is useful logging information in its own right.

Formerly, when MKL is linked and used for either CPU or FFT, its use
and version are reported there. Then its use was reported again
without context. Now there is context and the report doesn't look like
a pointless duplicate.

cmake/gmxManageLinearAlgebraLibraries.cmake
@@ -81,6 +81,7 @@ macro(manage_linear_algebra_library name function_in_library)
                     message(STATUS "MKL and GMX_${name}_USER were both specified. Using the latter for ${name}.")
                 endif()
             endif()
+            set(GMX_DESCRIBE_${name} "External - user-supplied")
         endif()
 
         if(NOT _library_was_found AND HAVE_LIBMKL)
@@ -93,6 +94,7 @@ macro(manage_linear_algebra_library name function_in_library)
                 # (and probably tweak other things).
 #                list(APPEND LINEAR_ALGEBRA_LIBRARIES ${FFT_LINKER_FLAGS} ${FFT_LIBRARIES})
                 set(_library_was_found 1)
+                set(GMX_DESCRIBE_${name} "External - MKL")
             else()
                 set(_message_text "Intel's MKL was specified, and it should provide ${name}, but it does not. ")
             endif()
@@ -109,11 +111,15 @@ macro(manage_linear_algebra_library name function_in_library)
             if (${name}_FOUND)
                 set(_libraries_to_link ${${name}_LIBRARIES})
                 set(_library_was_found 1)
+                set(GMX_DESCRIBE_${name} "External - detected on the system")
             endif()
         endif()
 
-        if (NOT _library_was_found AND NOT _find_quietly)
-            message(STATUS "${_message_text}Using GROMACS built-in ${name}.")
+        if (NOT _library_was_found)
+            if (NOT _find_quietly)
+                message(STATUS "${_message_text}Using GROMACS built-in ${name}.")
+            endif()
+            set(GMX_DESCRIBE_${name} "Internal" CACHE STRING)
         endif()
     endif()
 
@@ -178,6 +184,8 @@ endmacro()
 #
 # This function sets the following variables in its parent scope:
 #     LINEAR_ALGEBRA_LIBRARIES  will be set as required to add libraries required for linear algebra
+#     GMX_DESCRIBE_BLAS         describes the origin of BLAS routines used in GROMACS
+#     GMX_DESCRIBE_LAPACK       describes the origin of LAPACK routines used in GROMACS
 #
 function(gmxManageLinearAlgebraLibraries)
     include(CheckFunctionExists)
@@ -190,6 +198,8 @@ function(gmxManageLinearAlgebraLibraries)
 
     # Propagate the new local value to the parent scope
     set(LINEAR_ALGEBRA_LIBRARIES "${LINEAR_ALGEBRA_LIBRARIES}" PARENT_SCOPE)
+    set(GMX_DESCRIBE_BLAS ${GMX_DESCRIBE_BLAS} PARENT_SCOPE)
+    set(GMX_DESCRIBE_LAPACK ${GMX_DESCRIBE_LAPACK} PARENT_SCOPE)
 endfunction()
 
 gmxManageLinearAlgebraLibraries()

src/config.h.cmakein
@@ -437,6 +437,12 @@
 /* Define if we have MKL available */
 #cmakedefine01 HAVE_LIBMKL
 
+// Describes the origin of BLAS routines
+#define GMX_DESCRIBE_BLAS "@GMX_DESCRIBE_BLAS@"
+
+// Describes the origin of LAPACK routines
+#define GMX_DESCRIBE_LAPACK "@GMX_DESCRIBE_LAPACK@"
+
 /* Define if we have lmfit support */
 #cmakedefine01 HAVE_LMFIT
 

src/gromacs/utility/binaryinformation.cpp
@@ -393,15 +393,27 @@ void gmx_print_version_info(gmx::TextWriter* writer)
     writer->writeLine(formatString("C++ compiler:       %s", BUILD_CXX_COMPILER));
     writer->writeLine(formatString(
             "C++ compiler flags: %s %s", BUILD_CXXFLAGS, CMAKE_BUILD_CONFIGURATION_CXX_FLAGS));
-#if HAVE_LIBMKL
-    /* MKL might be used for LAPACK/BLAS even if FFTs use FFTW, so keep it separate */
-    MKLVersion mklVersion;
-    mkl_get_version(&mklVersion);
-    writer->writeLine(formatString("Intel MKL version:  %d.%d.%d",
-                                   mklVersion.MajorVersion,
-                                   mklVersion.MinorVersion,
-                                   mklVersion.UpdateVersion));
-#endif
+
+    // Describe the BLAS and LAPACK libraries. We generally don't know
+    // much about what external library was detected, but we do in the
+    // case of MKL so then it is reported.
+    if (HAVE_LIBMKL && std::strstr(GMX_DESCRIBE_BLAS, "MKL") != nullptr)
+    {
+        writer->writeLine(formatString("BLAS library:       %s", describeMkl().c_str()));
+    }
+    else
+    {
+        writer->writeLine(formatString("BLAS library:       %s", GMX_DESCRIBE_BLAS));
+    }
+    if (HAVE_LIBMKL && std::strstr(GMX_DESCRIBE_LAPACK, "MKL") != nullptr)
+    {
+        writer->writeLine(formatString("LAPACK library:     %s", describeMkl().c_str()));
+    }
+    else
+    {
+        writer->writeLine(formatString("LAPACK library:     %s", GMX_DESCRIBE_LAPACK));
+    }
+
 #if GMX_GPU_OPENCL
     writer->writeLine(formatString("OpenCL include dir: %s", OPENCL_INCLUDE_DIR));
     writer->writeLine(formatString("OpenCL library:     %s", OPENCL_LIBRARY));

commit fecc87e1ae6f46a6fb40ad70c2228073bbfe6ec9

Fix CMake "set" error
Introduced in baf1f8f4 (MR !3361).

```
CMake Error at cmake/gmxManageLinearAlgebraLibraries.cmake:122 (set):
  set given invalid arguments for CACHE mode.
```

cmake/gmxManageLinearAlgebraLibraries.cmake
@@ -119,7 +119,7 @@ macro(manage_linear_algebra_library name function_in_library)
             if (NOT _find_quietly)
                 message(STATUS "${_message_text}Using GROMACS built-in ${name}.")
             endif()
-            set(GMX_DESCRIBE_${name} "Internal" CACHE STRING)
+            set(GMX_DESCRIBE_${name} "Internal")
         endif()
     endif()
 