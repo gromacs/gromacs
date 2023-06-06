gromacs/gromacs

commit 164e43b8dfb9620d211d0f9940767951ae8c7640

GPU PME Pipelining: wait for grid zeroing
A synchronization was missing between the grid clearing at the end
of one step and spread kernels on the next step.

Fixes #4734

src/gromacs/ewald/pme_gpu_internal.cpp
@@ -1529,7 +1529,11 @@ void pme_gpu_spread(const PmeGpu*                  pmeGpu,
                                             && !writeGlobalOrSaveSplines);
         if (kernelParamsPtr->usePipeline != 0)
         {
-            int numStagesInPipeline = pmeCoordinateReceiverGpu->ppCommNumSenderRanks();
+            const int numStagesInPipeline = pmeCoordinateReceiverGpu->ppCommNumSenderRanks();
+
+            GpuEventSynchronizer* gridsReadyForSpread = &pmeGpu->archSpecific->pmeGridsReadyForSpread;
+            gridsReadyForSpread->markEvent(pmeGpu->archSpecific->pmeStream_);
+            gridsReadyForSpread->setConsumptionLimits(numStagesInPipeline, numStagesInPipeline);
 
             for (int i = 0; i < numStagesInPipeline; i++)
             {
@@ -1544,16 +1548,18 @@ void pme_gpu_spread(const PmeGpu*                  pmeGpu,
                     senderRank = i;
                 }
 
+                DeviceStream* launchStream = pmeCoordinateReceiverGpu->ppCommStream(senderRank);
+                gridsReadyForSpread->enqueueWaitEvent(*launchStream);
+
                 // set kernel configuration options specific to this stage of the pipeline
                 std::tie(kernelParamsPtr->pipelineAtomStart, kernelParamsPtr->pipelineAtomEnd) =
                         pmeCoordinateReceiverGpu->ppCommAtomRange(senderRank);
-                const int blockCount       = static_cast<int>(std::ceil(
+                const int blockCount = static_cast<int>(std::ceil(
                         static_cast<float>(kernelParamsPtr->pipelineAtomEnd - kernelParamsPtr->pipelineAtomStart)
                         / atomsPerBlock));
-                auto      dimGrid          = pmeGpuCreateGrid(pmeGpu, blockCount);
-                config.gridSize[0]         = dimGrid.first;
-                config.gridSize[1]         = dimGrid.second;
-                DeviceStream* launchStream = pmeCoordinateReceiverGpu->ppCommStream(senderRank);
+                auto      dimGrid    = pmeGpuCreateGrid(pmeGpu, blockCount);
+                config.gridSize[0]   = dimGrid.first;
+                config.gridSize[1]   = dimGrid.second;
 
 
 #if c_canEmbedBuffers

src/gromacs/ewald/pme_gpu_types_host_impl.h
@@ -108,6 +108,12 @@ struct PmeGpuSpecific
     GpuEventSynchronizer syncFftToPmeGrid;
     /*! \brief Triggered after spline/spread computations have been completed. */
     GpuEventSynchronizer spreadCompleted;
+    /*! \brief Triggered after the end-of-step tasks in the PME stream are complete.
+     *
+     * Required only in case of GPU PME pipelining, when we launch Spread kernels in
+     * separate streams.
+     * */
+    GpuEventSynchronizer pmeGridsReadyForSpread;
 
     /* Settings which are set at the start of the run */
     /*! \brief A boolean which tells whether the complex and real grids for cu/clFFT are different or same. Currently true. */

commit 202567babba3a112184c80b944ea9af96ef5f40b

Fix for crash when CUDA Graphs are enabled on multi-GPU
Introduced in 164e43b8

Fixes #4786

src/gromacs/ewald/pme.h
@@ -406,14 +406,15 @@ GPU_FUNC_QUALIFIER void pme_gpu_prepare_computation(gmx_pme_t*     GPU_FUNC_ARGU
  * \param[in] useGpuDirectComm               Whether direct GPU PME-PP communication is active
  * \param[in]  pmeCoordinateReceiverGpu      Coordinate receiver object, which must be valid when
  *                                           direct GPU PME-PP communication is active
+ * \param[in] useMdGpuGraph                  Whether MD GPU Graph is in use.
  */
-GPU_FUNC_QUALIFIER void pme_gpu_launch_spread(
-        gmx_pme_t*                     GPU_FUNC_ARGUMENT(pme),
-        GpuEventSynchronizer*          GPU_FUNC_ARGUMENT(xReadyOnDevice),
-        gmx_wallcycle*                 GPU_FUNC_ARGUMENT(wcycle),
-        real                           GPU_FUNC_ARGUMENT(lambdaQ),
-        bool                           GPU_FUNC_ARGUMENT(useGpuDirectComm),
-        gmx::PmeCoordinateReceiverGpu* GPU_FUNC_ARGUMENT(pmeCoordinateReceiverGpu)) GPU_FUNC_TERM;
+GPU_FUNC_QUALIFIER void pme_gpu_launch_spread(gmx_pme_t*            GPU_FUNC_ARGUMENT(pme),
+                                              GpuEventSynchronizer* GPU_FUNC_ARGUMENT(xReadyOnDevice),
+                                              gmx_wallcycle*        GPU_FUNC_ARGUMENT(wcycle),
+                                              real                  GPU_FUNC_ARGUMENT(lambdaQ),
+                                              bool GPU_FUNC_ARGUMENT(useGpuDirectComm),
+                                              gmx::PmeCoordinateReceiverGpu* GPU_FUNC_ARGUMENT(pmeCoordinateReceiverGpu),
+                                              bool GPU_FUNC_ARGUMENT(useMdGpuGraph)) GPU_FUNC_TERM;
 
 /*! \brief
  * Launches middle stages of PME (FFT R2C, solving, FFT C2R) either on GPU or on CPU, depending on the run mode.

src/gromacs/ewald/pme_gpu.cpp
@@ -195,7 +195,8 @@ void pme_gpu_launch_spread(gmx_pme_t*                     pme,
                            gmx_wallcycle*                 wcycle,
                            const real                     lambdaQ,
                            const bool                     useGpuDirectComm,
-                           gmx::PmeCoordinateReceiverGpu* pmeCoordinateReceiverGpu)
+                           gmx::PmeCoordinateReceiverGpu* pmeCoordinateReceiverGpu,
+                           const bool                     useMdGpuGraph)
 {
     GMX_ASSERT(pme_gpu_active(pme), "This should be a GPU run of PME but it is not enabled.");
     GMX_ASSERT(xReadyOnDevice || !pme->bPPnode, "Need a valid xReadyOnDevice on PP+PME ranks.");
@@ -224,6 +225,7 @@ void pme_gpu_launch_spread(gmx_pme_t*                     pme,
                    lambdaQ,
                    useGpuDirectComm,
                    pmeCoordinateReceiverGpu,
+                   useMdGpuGraph,
                    wcycle);
 }
 

src/gromacs/ewald/pme_gpu_internal.cpp
@@ -1741,6 +1741,7 @@ void pme_gpu_spread(const PmeGpu*                  pmeGpu,
                     const real                     lambda,
                     const bool                     useGpuDirectComm,
                     gmx::PmeCoordinateReceiverGpu* pmeCoordinateReceiverGpu,
+                    const bool                     useMdGpuGraph,
                     gmx_wallcycle*                 wcycle)
 {
     GMX_ASSERT(
@@ -1853,8 +1854,14 @@ void pme_gpu_spread(const PmeGpu*                  pmeGpu,
             const int numStagesInPipeline = pmeCoordinateReceiverGpu->ppCommNumSenderRanks();
 
             GpuEventSynchronizer* gridsReadyForSpread = &pmeGpu->archSpecific->pmeGridsReadyForSpread;
-            gridsReadyForSpread->markEvent(pmeGpu->archSpecific->pmeStream_);
-            gridsReadyForSpread->setConsumptionLimits(numStagesInPipeline, numStagesInPipeline);
+            // Sync on grid zeroing is required except when GPU graphs are in use,
+            // In which case the sync is already present through the zeroing being
+            // explicitly included in the graph
+            if (!useMdGpuGraph)
+            {
+                gridsReadyForSpread->markEvent(pmeGpu->archSpecific->pmeStream_);
+                gridsReadyForSpread->setConsumptionLimits(numStagesInPipeline, numStagesInPipeline);
+            }
 
             for (int i = 0; i < numStagesInPipeline; i++)
             {
@@ -1866,8 +1873,10 @@ void pme_gpu_spread(const PmeGpu*                  pmeGpu,
                 wallcycle_start(wcycle, WallCycleCounter::LaunchGpuPme);
 
                 DeviceStream* launchStream = pmeCoordinateReceiverGpu->ppCommStream(senderRank);
-                gridsReadyForSpread->enqueueWaitEvent(*launchStream);
-
+                if (!useMdGpuGraph)
+                {
+                    gridsReadyForSpread->enqueueWaitEvent(*launchStream);
+                }
                 // set kernel configuration options specific to this stage of the pipeline
                 std::tie(kernelParamsPtr->pipelineAtomStart, kernelParamsPtr->pipelineAtomEnd) =
                         pmeCoordinateReceiverGpu->ppCommAtomRange(senderRank);

src/gromacs/ewald/pme_gpu_internal.h
@@ -374,6 +374,7 @@ void pme_gpu_destroy_3dfft(const PmeGpu* pmeGpu);
  * \param[in]  useGpuDirectComm          Whether direct GPU PME-PP communication is active
  * \param[in]  pmeCoordinateReceiverGpu  Coordinate receiver object, which must be valid when
  *                                       direct GPU PME-PP communication is active
+ * \param[in]  useMdGpuGraph             Whether MD GPU Graph is in use.
  * \param[in]  wcycle                    The wallclock counter.
  */
 GPU_FUNC_QUALIFIER void pme_gpu_spread(const PmeGpu*         GPU_FUNC_ARGUMENT(pmeGpu),
@@ -385,6 +386,7 @@ GPU_FUNC_QUALIFIER void pme_gpu_spread(const PmeGpu*         GPU_FUNC_ARGUMENT(p
                                        real                  GPU_FUNC_ARGUMENT(lambda),
                                        bool                  GPU_FUNC_ARGUMENT(useGpuDirectComm),
                                        gmx::PmeCoordinateReceiverGpu* GPU_FUNC_ARGUMENT(pmeCoordinateReceiverGpu),
+                                       bool           GPU_FUNC_ARGUMENT(useMdGpuGraph),
                                        gmx_wallcycle* GPU_FUNC_ARGUMENT(wcycle)) GPU_FUNC_TERM;
 
 /*! \libinternal \brief

src/gromacs/ewald/pme_only.cpp
@@ -786,7 +786,8 @@ int gmx_pmeonly(struct gmx_pme_t**              pmeFromRunnerPtr,
                                   wcycle,
                                   lambda_q,
                                   pme_pp->useGpuDirectComm,
-                                  pme_pp->pmeCoordinateReceiverGpu.get());
+                                  pme_pp->pmeCoordinateReceiverGpu.get(),
+                                  pme_pp->useMdGpuGraph);
             pme_gpu_launch_complex_transforms(pme, wcycle, stepWork);
             pme_gpu_launch_gather(pme, wcycle, lambda_q);
             output = pme_gpu_wait_finish_task(pme, computeEnergyAndVirial, lambda_q, wcycle);

src/gromacs/mdlib/sim_util.cpp
@@ -728,6 +728,7 @@ static void computeSpecialForces(FILE*                          fplog,
  * \param[in]  stepWork             Step schedule flags
  * \param[in]  xReadyOnDevice       Event synchronizer indicating that the coordinates are ready in the device memory.
  * \param[in]  lambdaQ              The Coulomb lambda of the current state.
+ * \param[in]  useMdGpuGraph        Whether MD GPU Graph is in use.
  * \param[in]  wcycle               The wallcycle structure
  */
 static inline void launchPmeGpuSpread(gmx_pme_t*            pmedata,
@@ -735,6 +736,7 @@ static inline void launchPmeGpuSpread(gmx_pme_t*            pmedata,
                                       const StepWorkload&   stepWork,
                                       GpuEventSynchronizer* xReadyOnDevice,
                                       const real            lambdaQ,
+                                      bool                  useMdGpuGraph,
                                       gmx_wallcycle*        wcycle)
 {
     wallcycle_start(wcycle, WallCycleCounter::PmeGpuMesh);
@@ -742,7 +744,7 @@ static inline void launchPmeGpuSpread(gmx_pme_t*            pmedata,
     bool                           useGpuDirectComm         = false;
     gmx::PmeCoordinateReceiverGpu* pmeCoordinateReceiverGpu = nullptr;
     pme_gpu_launch_spread(
-            pmedata, xReadyOnDevice, wcycle, lambdaQ, useGpuDirectComm, pmeCoordinateReceiverGpu);
+            pmedata, xReadyOnDevice, wcycle, lambdaQ, useGpuDirectComm, pmeCoordinateReceiverGpu, useMdGpuGraph);
     wallcycle_stop(wcycle, WallCycleCounter::PmeGpuMesh);
 }
 
@@ -1568,6 +1570,7 @@ void do_force(FILE*                               fplog,
                            stepWork,
                            localXReadyOnDevice,
                            lambda[static_cast<int>(FreeEnergyPerturbationCouplingType::Coul)],
+                           simulationWork.useMdGpuGraph,
                            wcycle);
     }
 

src/gromacs/ewald/tests/pmetestcommon.cpp
@@ -370,6 +370,7 @@ void pmePerformSplineAndSpread(gmx_pme_t* pme,
                            lambdaQ,
                            useGpuDirectComm,
                            pmeCoordinateReceiverGpu,
+                           false,
                            nullptr);
         }
         break;