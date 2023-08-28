gromacs/gromacs

commit 0376266a9446abcde7bf80d3b70f227abb8a0fbb

Increase and check for limits of mdrun output files
The checkpoint and trr file store the number of elements in vectors
as an int. This put the limit on #atoms at max_int/3. Now we cast
from unsigned int to int, which doubles these limits.
There is now a check in mdrun for these limits so the user gets
a clear error message when limits are exceeded.

Removed unused nptr argument from doVectorLow() in checkpoint.cpp.

Refs #4627

src/gromacs/fileio/checkpoint.cpp
@@ -713,15 +713,13 @@ static void convertArrayRealPrecision(const char gmx_unused* c, int gmx_unused* 
  *
  * When not listing, we use either v or vector, depending on which is !=NULL.
  * If nval >= 0, nval is used; on read this should match the passed value.
- * If nval n<0, *nptr (with v) or vector->size() is used. On read using v,
- * the value is stored in nptr
+ * If nval n<0, vector->size() is used.
  */
 template<typename T, typename AllocatorType, typename Enum>
 static int doVectorLow(XDR*                           xd,
                        Enum                           ecpt,
                        int                            sflags,
-                       int                            nval,
-                       int*                           nptr,
+                       const int64_t                  nval,
                        T**                            v,
                        std::vector<T, AllocatorType>* vector,
                        FILE*                          list,
@@ -733,29 +731,32 @@ static int doVectorLow(XDR*                           xd,
 
     bool_t res = 0;
 
-    int numElemInTheFile;
+    unsigned int numElemInTheFile;
     if (list == nullptr)
     {
         if (nval >= 0)
         {
-            GMX_RELEASE_ASSERT(nptr == nullptr, "With nval>=0 we should have nptr==NULL");
+            // Remove this check when we store int64_t in the file
+            GMX_RELEASE_ASSERT(nval <= gmx::sc_checkpointMaxAtomCount,
+                               "Vector size in checkpoint beyond max uint");
+
             numElemInTheFile = nval;
         }
         else
         {
-            if (v != nullptr)
-            {
-                GMX_RELEASE_ASSERT(nptr != nullptr, "With nval<0 we should have nptr!=NULL");
-                numElemInTheFile = *nptr;
-            }
-            else
-            {
-                numElemInTheFile = vector->size();
-            }
+            GMX_RELEASE_ASSERT(v == nullptr, "With nval<0 we should have v=nullptr");
+            // Remove this check when we store int64_t in the file
+            GMX_RELEASE_ASSERT(
+                    vector->size() <= static_cast<std::size_t>(std::numeric_limits<unsigned int>::max()),
+                    "Vector size in checkpoint beyond max uint");
+
+            numElemInTheFile = vector->size();
         }
     }
     /* Read/write the vector element count */
-    res = xdr_int(xd, &numElemInTheFile);
+    /* We store an unsigned int as a signed int to avoid changing the file format */
+    int* numElemInTheFileIntPtr = reinterpret_cast<int*>(&numElemInTheFile);
+    res                         = xdr_int(xd, numElemInTheFileIntPtr);
     if (res == 0)
     {
         return -1;
@@ -781,15 +782,12 @@ static int doVectorLow(XDR*                           xd,
             if (numElemInTheFile != nval)
             {
                 gmx_fatal(FARGS,
-                          "Count mismatch for state entry %s, code count is %d, file count is %d\n",
+                          "Count mismatch for state entry %s, code count is %" PRId64
+                          ", file count is %d\n",
                           enumValueToString(ecpt),
                           nval,
                           numElemInTheFile);
             }
-        }
-        else if (nptr != nullptr)
-        {
-            *nptr = numElemInTheFile;
         }
 
         bool typesMatch = (xdrTypeInTheFile == xdrTypeInTheCode);
@@ -874,8 +872,7 @@ static int doVectorLow(XDR*                           xd,
 template<typename T, typename Enum>
 static int doVector(XDR* xd, Enum ecpt, int sflags, std::vector<T>* vector, FILE* list, int numElements = -1)
 {
-    return doVectorLow<T>(
-            xd, ecpt, sflags, numElements, nullptr, nullptr, vector, list, CptElementType::real);
+    return doVectorLow<T>(xd, ecpt, sflags, numElements, nullptr, vector, list, CptElementType::real);
 }
 
 //! \brief Read/Write an ArrayRef<real>.
@@ -884,7 +881,7 @@ static int doRealArrayRef(XDR* xd, Enum ecpt, int sflags, gmx::ArrayRef<real> ve
 {
     real* v_real = vector.data();
     return doVectorLow<real, std::allocator<real>>(
-            xd, ecpt, sflags, vector.size(), nullptr, &v_real, nullptr, list, CptElementType::real);
+            xd, ecpt, sflags, vector.size(), &v_real, nullptr, list, CptElementType::real);
 }
 
 //! Convert from view of RVec to view of real.
@@ -915,7 +912,7 @@ static int doRvecVector(XDR* xd, Enum ecpt, int sflags, PaddedVectorOfRVecType* 
         using realAllocator =
                 typename std::allocator_traits<typename PaddedVectorOfRVecType::allocator_type>::template rebind_alloc<real>;
         return doVectorLow<real, realAllocator>(
-                xd, ecpt, sflags, numReals, nullptr, nullptr, nullptr, list, CptElementType::real);
+                xd, ecpt, sflags, numReals, nullptr, nullptr, list, CptElementType::real);
     }
 }
 
@@ -927,21 +924,21 @@ template<typename Enum>
 static int do_cpte_reals(XDR* xd, Enum ecpt, int sflags, int n, real** v, FILE* list)
 {
     return doVectorLow<real, std::allocator<real>>(
-            xd, ecpt, sflags, n, nullptr, v, nullptr, list, CptElementType::real);
+            xd, ecpt, sflags, n, v, nullptr, list, CptElementType::real);
 }
 
 template<typename Enum>
 static int do_cpte_real(XDR* xd, Enum ecpt, int sflags, real* r, FILE* list)
 {
     return doVectorLow<real, std::allocator<real>>(
-            xd, ecpt, sflags, 1, nullptr, &r, nullptr, list, CptElementType::real);
+            xd, ecpt, sflags, 1, &r, nullptr, list, CptElementType::real);
 }
 
 template<typename Enum>
 static int do_cpte_ints(XDR* xd, Enum ecpt, int sflags, int n, int** v, FILE* list)
 {
     return doVectorLow<int, std::allocator<int>>(
-            xd, ecpt, sflags, n, nullptr, v, nullptr, list, CptElementType::integer);
+            xd, ecpt, sflags, n, v, nullptr, list, CptElementType::integer);
 }
 
 template<typename Enum>
@@ -963,7 +960,7 @@ template<typename Enum>
 static int do_cpte_doubles(XDR* xd, Enum ecpt, int sflags, int n, double** v, FILE* list)
 {
     return doVectorLow<double, std::allocator<double>>(
-            xd, ecpt, sflags, n, nullptr, v, nullptr, list, CptElementType::real);
+            xd, ecpt, sflags, n, v, nullptr, list, CptElementType::real);
 }
 
 template<typename Enum>
@@ -980,7 +977,7 @@ static int do_cpte_matrix(XDR* xd, Enum ecpt, int sflags, matrix v, FILE* list)
 
     vr  = &(v[0][0]);
     ret = doVectorLow<real, std::allocator<real>>(
-            xd, ecpt, sflags, DIM * DIM, nullptr, &vr, nullptr, nullptr, CptElementType::matrix3x3);
+            xd, ecpt, sflags, DIM * DIM, &vr, nullptr, nullptr, CptElementType::matrix3x3);
 
     if (list && ret == 0)
     {
@@ -1005,7 +1002,7 @@ static int do_cpte_nmatrix(XDR* xd, Enum ecpt, int sflags, int n, real** v, FILE
     for (i = 0; i < n; i++)
     {
         reti = doVectorLow<real, std::allocator<real>>(
-                xd, ecpt, sflags, n, nullptr, &(v[i]), nullptr, nullptr, CptElementType::matrix3x3);
+                xd, ecpt, sflags, n, &(v[i]), nullptr, nullptr, CptElementType::matrix3x3);
         if (list && reti == 0)
         {
             sprintf(name, "%s[%d]", enumValueToString(ecpt), i);
@@ -1067,7 +1064,7 @@ static int do_cpte_matrices(XDR* xd, Enum ecpt, int sflags, int n, matrix** v, F
         }
     }
     ret = doVectorLow<real, std::allocator<real>>(
-            xd, ecpt, sflags, nf * DIM * DIM, nullptr, &vr, nullptr, nullptr, CptElementType::matrix3x3);
+            xd, ecpt, sflags, nf * DIM * DIM, &vr, nullptr, nullptr, CptElementType::matrix3x3);
     for (i = 0; i < nf; i++)
     {
         for (j = 0; j < DIM; j++)
@@ -1318,6 +1315,10 @@ static int do_cpt_footer(XDR* xd, CheckPointVersion file_version)
 
 static int do_cpt_state(XDR* xd, int fflags, t_state* state, FILE* list)
 {
+    GMX_RELEASE_ASSERT(
+            static_cast<unsigned int>(state->natoms) <= std::numeric_limits<unsigned int>::max() / 3,
+            "Can not write more than max_int/3 atoms to checkpoint");
+
     int       ret    = 0;
     const int sflags = state->flags;
     // If reading, state->natoms was probably just read, so

src/gromacs/fileio/checkpoint.h
@@ -37,6 +37,7 @@
 
 #include <cstdio>
 
+#include <limits>
 #include <vector>
 
 #include "gromacs/compat/pointers.h"
@@ -65,6 +66,9 @@ struct MDModulesNotifiers;
 class KeyValueTreeObject;
 class ReadCheckpointDataHolder;
 class WriteCheckpointDataHolder;
+
+//! The maximum number of atoms that can be stored in a checkpoint file
+static constexpr int64_t sc_checkpointMaxAtomCount = std::numeric_limits<unsigned int>::max() / 3;
 
 /*! \brief Read to a key-value-tree value used for checkpointing.
  *

src/gromacs/fileio/trrio.cpp
@@ -42,6 +42,7 @@
 #include "gromacs/mdtypes/md_enums.h"
 #include "gromacs/utility/fatalerror.h"
 #include "gromacs/utility/futil.h"
+#include "gromacs/utility/gmxassert.h"
 #include "gromacs/utility/smalloc.h"
 
 static int nFloatSize(gmx_trr_header_t* sh)
@@ -54,15 +55,15 @@ static int nFloatSize(gmx_trr_header_t* sh)
     }
     else if (sh->x_size)
     {
-        nflsize = sh->x_size / (sh->natoms * DIM);
+        nflsize = sh->x_size / (static_cast<unsigned int>(sh->natoms) * DIM);
     }
     else if (sh->v_size)
     {
-        nflsize = sh->v_size / (sh->natoms * DIM);
+        nflsize = sh->v_size / (static_cast<unsigned int>(sh->natoms) * DIM);
     }
     else if (sh->f_size)
     {
-        nflsize = sh->f_size / (sh->natoms * DIM);
+        nflsize = sh->f_size / (static_cast<unsigned int>(sh->natoms) * DIM);
     }
     else
     {
@@ -123,17 +124,20 @@ static gmx_bool do_trr_frame_header(t_fileio* fio, bool bRead, gmx_trr_header_t*
         sprintf(buf, "GMX_trn_file");
         *bOK = *bOK && gmx_fio_do_string(fio, buf);
     }
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->ir_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->e_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->box_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->vir_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->pres_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->top_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->sym_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->x_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->v_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->f_size);
-    *bOK = *bOK && gmx_fio_do_int(fio, sh->natoms);
+    *bOK             = *bOK && gmx_fio_do_int(fio, sh->ir_size);
+    *bOK             = *bOK && gmx_fio_do_int(fio, sh->e_size);
+    *bOK             = *bOK && gmx_fio_do_int(fio, sh->box_size);
+    *bOK             = *bOK && gmx_fio_do_int(fio, sh->vir_size);
+    *bOK             = *bOK && gmx_fio_do_int(fio, sh->pres_size);
+    *bOK             = *bOK && gmx_fio_do_int(fio, sh->top_size);
+    *bOK             = *bOK && gmx_fio_do_int(fio, sh->sym_size);
+    int* xSizeIntPtr = reinterpret_cast<int*>(&sh->x_size);
+    *bOK             = *bOK && gmx_fio_do_int(fio, *xSizeIntPtr);
+    int* vSizeIntPtr = reinterpret_cast<int*>(&sh->v_size);
+    *bOK             = *bOK && gmx_fio_do_int(fio, *vSizeIntPtr);
+    int* fSizeIntPtr = reinterpret_cast<int*>(&sh->f_size);
+    *bOK             = *bOK && gmx_fio_do_int(fio, *fSizeIntPtr);
+    *bOK             = *bOK && gmx_fio_do_int(fio, sh->natoms);
 
     if (!*bOK)
     {
@@ -212,6 +216,9 @@ static gmx_bool do_trr_frame(t_fileio* fio,
     snew(sh, 1);
     if (!bRead)
     {
+        GMX_RELEASE_ASSERT(*natoms < sc_trrMaxAtomCount,
+                           "Can not write more than max_int/3 atoms to trr");
+
         sh->box_size = (box) ? sizeof(matrix) : 0;
         sh->x_size   = ((x) ? (*natoms * sizeof(x[0])) : 0);
         sh->v_size   = ((v) ? (*natoms * sizeof(v[0])) : 0);

src/gromacs/fileio/trrio.h
@@ -35,6 +35,7 @@
 #define GMX_FILEIO_TRRIO_H
 
 #include <filesystem>
+#include <limits>
 
 #include "gromacs/math/vectypes.h"
 #include "gromacs/utility/basedefinitions.h"
@@ -68,21 +69,23 @@
 
 struct t_fileio;
 
+static constexpr int64_t sc_trrMaxAtomCount = std::numeric_limits<unsigned int>::max() / 3;
+
 /* This struct describes the order and the  */
 /* sizes of the structs in a trr file, sizes are given in bytes. */
 typedef struct gmx_trr_header_t
 {
-    gmx_bool bDouble;   /* Double precision?                   */
-    int      ir_size;   /* Backward compatibility              */
-    int      e_size;    /* Backward compatibility              */
-    int      box_size;  /* Non zero if a box is present        */
-    int      vir_size;  /* Backward compatibility              */
-    int      pres_size; /* Backward compatibility              */
-    int      top_size;  /* Backward compatibility              */
-    int      sym_size;  /* Backward compatibility              */
-    int      x_size;    /* Non zero if coordinates are present */
-    int      v_size;    /* Non zero if velocities are present  */
-    int      f_size;    /* Non zero if forces are present      */
+    gmx_bool     bDouble;   /* Double precision?                   */
+    int          ir_size;   /* Backward compatibility              */
+    int          e_size;    /* Backward compatibility              */
+    int          box_size;  /* Non zero if a box is present        */
+    int          vir_size;  /* Backward compatibility              */
+    int          pres_size; /* Backward compatibility              */
+    int          top_size;  /* Backward compatibility              */
+    int          sym_size;  /* Backward compatibility              */
+    unsigned int x_size;    /* Non zero if coordinates are present */
+    unsigned int v_size;    /* Non zero if velocities are present  */
+    unsigned int f_size;    /* Non zero if forces are present      */
 
     int     natoms;    /* The total number of atoms           */
     int64_t step;      /* Current step number                 */

src/gromacs/mdrun/runner.cpp
@@ -72,6 +72,7 @@
 #include "gromacs/fileio/gmxfio.h"
 #include "gromacs/fileio/oenv.h"
 #include "gromacs/fileio/tpxio.h"
+#include "gromacs/fileio/trrio.h"
 #include "gromacs/gmxlib/network.h"
 #include "gromacs/gmxlib/nrnb.h"
 #include "gromacs/gpu_utils/device_stream_manager.h"
@@ -927,6 +928,19 @@ int Mdrunner::mdrunner()
          */
         applyGlobalSimulationState(
                 *inputHolder_.get(), partialDeserializedTpr.get(), globalState.get(), inputrec.get(), &mtop);
+
+        static_assert(sc_trrMaxAtomCount == sc_checkpointMaxAtomCount);
+        if (mtop.natoms > sc_checkpointMaxAtomCount)
+        {
+            gmx_fatal(FARGS,
+                      "System has %d atoms, which is more than can be stored in checkpoint and trr "
+                      "files (max %" PRId64 ")",
+                      mtop.natoms,
+                      sc_checkpointMaxAtomCount);
+        }
+
+        // The XTC format has been updated to support up to 2^31-1 atoms, which is anyway the
+        // largest supported by GROMACS, so no need for any particular check here.
     }
 
     /* Check and update the hardware options for internal consistency */

commit 3c5a9ab0aeaad952ca20a59d92e66ea02c6edb09

Fix signedness in format strings
Minor issues introduced in 0376266a944 and 644f53588d3.

src/gromacs/fileio/checkpoint.cpp
@@ -783,7 +783,7 @@ static int doVectorLow(XDR*                           xd,
             {
                 gmx_fatal(FARGS,
                           "Count mismatch for state entry %s, code count is %" PRId64
-                          ", file count is %d\n",
+                          ", file count is %u\n",
                           enumValueToString(ecpt),
                           nval,
                           numElemInTheFile);

src/gromacs/gmxpreprocess/gen_maxwell_velocities.cpp
@@ -107,7 +107,7 @@ void maxwell_speed(real tempi, int seed, gmx_mtop_t* mtop, rvec v[], const gmx::
         seed = static_cast<int>(gmx::makeRandomSeed());
         GMX_LOG(logger.info)
                 .asParagraph()
-                .appendTextFormatted("Using random seed %u for generating velocities", seed);
+                .appendTextFormatted("Using random seed %d for generating velocities", seed);
     }
     gmx::ThreeFry2x64<> rng(seed, gmx::RandomDomain::MaxwellVelocities);
 