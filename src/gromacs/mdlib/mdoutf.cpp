e480a335885d8ddb1f3cc79a6f27948b8e0e43ac
Eric Hartmann <eric.hartmann@h-its.org>
2023-09-18 09:29:57 +0000

    throw error on failure to copy state.cpt to state_prev.cpt
    Closes #4859
    
    Closes #4859

--- src/gromacs/mdlib/mdoutf.cpp
+++ src/gromacs/mdlib/mdoutf.cpp
@@ -450,10 +450,14 @@ static void write_checkpoint(const char*                     fn,
                  * If renames are atomic (such as in POSIX systems),
                  * this copying should be unneccesary.
                  */
-                gmx_file_copy(fn, buf, FALSE);
-                /* We don't really care if this fails:
-                 * there's already a new checkpoint.
-                 */
+                if (gmx_file_copy(fn, buf, FALSE) != 0)
+                {
+                    GMX_THROW(gmx::FileIOError(
+                            gmx::formatString("Cannot rename checkpoint file from %s to %s; maybe "
+                                              "you are out of disk space?",
+                                              fn,
+                                              buf)));
+                }
             }
             else
             {
