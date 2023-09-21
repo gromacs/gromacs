gromacs/gromacs

commit 28697ebf6b5e32755220d0f55d68cd503042b3a1

Improve the deform option
The deform option would scale the coordinates and did not correct
the velocity of atoms when they were shifted by a periodic vector.
These issues have been fixed now, which makes the deform option
suitable for shear flows.
To initialize the flow the mdp option deform-init-flow has been added.

Fixes #4607

Closes #4607

src/gromacs/mdlib/md_support.cpp
@@ -47,6 +47,7 @@
 #include "gromacs/math/units.h"
 #include "gromacs/math/utilities.h"
 #include "gromacs/math/vec.h"
+#include "gromacs/mdlib/boxdeformation.h"
 #include "gromacs/mdlib/coupling.h"
 #include "gromacs/mdlib/gmx_omp_nthreads.h"
 #include "gromacs/mdlib/simulationsignal.h"
@@ -78,13 +79,38 @@
 #include "gromacs/utility/smalloc.h"
 #include "gromacs/utility/snprintf.h"
 
-static void calc_ke_part_normal(gmx::ArrayRef<const gmx::RVec> v,
+template<bool haveBoxDeformation>
+static void calc_ke_part_normal(const matrix                   deform,
+                                gmx::ArrayRef<const gmx::RVec> x,
+                                gmx::ArrayRef<const gmx::RVec> v,
+                                const matrix                   box,
                                 const t_grpopts*               opts,
                                 const t_mdatoms*               md,
                                 gmx_ekindata_t*                ekind,
                                 t_nrnb*                        nrnb,
                                 gmx_bool                       bEkinAveVel)
 {
+    matrix gmx_unused deformFlowMatrix;
+    if constexpr (haveBoxDeformation)
+    {
+        GMX_ASSERT(ekind->systemMomenta, "Need system momenta with box deformation");
+
+        if (opts->ngtc != 1)
+        {
+            // With box deformation we can only correct Ekin of the whole system for COM motion.
+            // grompp now does this check, but we could have read an old tpr file.
+            gmx_fatal(FARGS,
+                      "With box deformation a single temperature coupling group is required.");
+        }
+
+        gmx::setBoxDeformationFlowMatrix(deform, box, deformFlowMatrix);
+    }
+    else
+    {
+        GMX_UNUSED_VALUE(deform);
+        GMX_UNUSED_VALUE(box);
+    }
+
     int                         g;
     gmx::ArrayRef<t_grp_tcstat> tcstat = ekind->tcstat;
 
@@ -108,7 +134,14 @@ static void calc_ke_part_normal(gmx::ArrayRef<const gmx::RVec> v,
         }
     }
     ekind->dekindl_old = ekind->dekindl;
-    int nthread        = gmx_omp_nthreads_get(ModuleMultiThread::Update);
+    if constexpr (haveBoxDeformation)
+    {
+        ekind->systemMomenta->momentumOldHalfStep = ekind->systemMomenta->momentumHalfStep;
+        ekind->systemMomenta->momentumFullStep.clear();
+        ekind->systemMomenta->momentumHalfStep.clear();
+    }
+
+    const int nthread = gmx_omp_nthreads_get(ModuleMultiThread::Update);
 
 #pragma omp parallel for num_threads(nthread) schedule(static)
     for (int thread = 0; thread < nthread; thread++)
@@ -135,6 +168,13 @@ static void calc_ke_part_normal(gmx::ArrayRef<const gmx::RVec> v,
         }
         *dekindl_sum = 0.0;
 
+        SystemMomentum* systemMomentumWork;
+        if constexpr (haveBoxDeformation)
+        {
+            systemMomentumWork = ekind->systemMomentumWork[thread].get();
+            systemMomentumWork->clear();
+        }
+
         gt = 0;
         for (n = start_t; n < end_t; n++)
         {
@@ -144,17 +184,41 @@ static void calc_ke_part_normal(gmx::ArrayRef<const gmx::RVec> v,
             }
             hm = 0.5 * md->massT[n];
 
+            gmx::RVec vn = v[n];
+            if constexpr (haveBoxDeformation)
+            {
+                // Subtract the deformation flow profile.
+                // Note that this profile does not have a universal zero point. This means
+                // that the zero point chosen here affects the kinetic energy. We correct
+                // for this later by subtracting the velocity of the whole system which
+                // we compute below as well.
+                for (d = 0; (d < DIM); d++)
+                {
+                    vn[d] -= iprod(x[n], deformFlowMatrix[d]);
+                }
+            }
+
             for (d = 0; (d < DIM); d++)
             {
                 for (m = 0; (m < DIM); m++)
                 {
                     /* if we're computing a full step velocity, v[d] has v(t).  Otherwise, v(t+dt/2) */
-                    ekin_sum[gt][m][d] += hm * v[n][m] * v[n][d];
+                    ekin_sum[gt][m][d] += hm * vn[m] * vn[d];
+                }
+
+                if constexpr (haveBoxDeformation)
+                {
+                    systemMomentumWork->momentum[d] += md->massT[n] * vn[d];
                 }
             }
             if (md->nMassPerturbed && md->bPerturbed[n])
             {
-                *dekindl_sum += 0.5 * (md->massB[n] - md->massA[n]) * iprod(v[n], v[n]);
+                *dekindl_sum += 0.5 * (md->massB[n] - md->massA[n]) * iprod(vn, vn);
+            }
+
+            if constexpr (haveBoxDeformation)
+            {
+                systemMomentumWork->mass += md->massT[n];
             }
         }
     }
@@ -167,10 +231,24 @@ static void calc_ke_part_normal(gmx::ArrayRef<const gmx::RVec> v,
             if (bEkinAveVel)
             {
                 m_add(tcstat[g].ekinf, ekind->ekin_work[thread][g], tcstat[g].ekinf);
+
+                if constexpr (haveBoxDeformation)
+                {
+                    ekind->systemMomenta->momentumFullStep.momentum +=
+                            ekind->systemMomentumWork[thread]->momentum;
+                    ekind->systemMomenta->momentumFullStep.mass += ekind->systemMomentumWork[thread]->mass;
+                }
             }
             else
             {
                 m_add(tcstat[g].ekinh, ekind->ekin_work[thread][g], tcstat[g].ekinh);
+
+                if constexpr (haveBoxDeformation)
+                {
+                    ekind->systemMomenta->momentumHalfStep.momentum +=
+                            ekind->systemMomentumWork[thread]->momentum;
+                    ekind->systemMomenta->momentumHalfStep.mass += ekind->systemMomentumWork[thread]->mass;
+                }
             }
         }
 
@@ -178,6 +256,11 @@ static void calc_ke_part_normal(gmx::ArrayRef<const gmx::RVec> v,
     }
 
     inc_nrnb(nrnb, eNR_EKIN, md->homenr);
+
+    if constexpr (!haveBoxDeformation)
+    {
+        GMX_UNUSED_VALUE(x);
+    }
 }
 
 static void calc_ke_part_visc(const matrix                   box,
@@ -257,7 +340,9 @@ static void calc_ke_part_visc(const matrix                   box,
     inc_nrnb(nrnb, eNR_EKIN, homenr);
 }
 
-static void calc_ke_part(gmx::ArrayRef<const gmx::RVec> x,
+static void calc_ke_part(const bool                     haveBoxDeformation,
+                         const matrix                   deform,
+                         gmx::ArrayRef<const gmx::RVec> x,
                          gmx::ArrayRef<const gmx::RVec> v,
                          const matrix                   box,
                          const t_grpopts*               opts,
@@ -268,11 +353,59 @@ static void calc_ke_part(gmx::ArrayRef<const gmx::RVec> x,
 {
     if (ekind->cosacc.cos_accel == 0)
     {
-        calc_ke_part_normal(v, opts, md, ekind, nrnb, bEkinAveVel);
+        if (haveBoxDeformation)
+        {
+            calc_ke_part_normal<true>(deform, x, v, box, opts, md, ekind, nrnb, bEkinAveVel);
+        }
+        else
+        {
+            calc_ke_part_normal<false>(deform, x, v, box, opts, md, ekind, nrnb, bEkinAveVel);
+        }
     }
     else
     {
         calc_ke_part_visc(box, x, v, opts, md, ekind, nrnb, bEkinAveVel);
+    }
+}
+
+static void correctEkin(matrix ekin, const SystemMomentum& systemMomentum)
+{
+    GMX_ASSERT(systemMomentum.mass > 0, "Expect a postive system mass");
+    const double halfInvMass = 0.5 / systemMomentum.mass;
+
+    for (int d1 = 0; d1 < DIM; d1++)
+    {
+        for (int d2 = 0; d2 < DIM; d2++)
+        {
+            ekin[d1][d2] -= systemMomentum.momentum[d1] * systemMomentum.momentum[d2] * halfInvMass;
+        }
+    }
+}
+
+static void correctEkinForBoxDeformation(gmx_ekindata_t* ekind,
+                                         const bool      haveEkinFromAverageVelocities,
+                                         const bool      correctEkinHOld)
+{
+    if (haveEkinFromAverageVelocities)
+    {
+        for (auto& tcstat : ekind->tcstat)
+        {
+            correctEkin(tcstat.ekinf, ekind->systemMomenta->momentumFullStep);
+        }
+    }
+    else
+    {
+        if (correctEkinHOld)
+        {
+            for (auto& tcstat : ekind->tcstat)
+            {
+                correctEkin(tcstat.ekinh_old, ekind->systemMomenta->momentumOldHalfStep);
+            }
+        }
+        for (auto& tcstat : ekind->tcstat)
+        {
+            correctEkin(tcstat.ekinh, ekind->systemMomenta->momentumHalfStep);
+        }
     }
 }
 
@@ -331,7 +464,7 @@ void compute_globals(gmx_global_stat*               gstat,
     {
         if (!bReadEkin)
         {
-            calc_ke_part(x, v, box, &(ir->opts), mdatoms, ekind, nrnb, bEkinAveVel);
+            calc_ke_part(fr->haveBoxDeformation, ir->deform, x, v, box, &(ir->opts), mdatoms, ekind, nrnb, bEkinAveVel);
         }
     }
 
@@ -372,6 +505,11 @@ void compute_globals(gmx_global_stat*               gstat,
                 wallcycle_stop(wcycle, WallCycleCounter::MoveE);
             }
             signalCoordinator->finalizeSignals();
+
+            if (fr->haveBoxDeformation && bTemp && !bReadEkin)
+            {
+                correctEkinForBoxDeformation(ekind, bEkinAveVel, *bSumEkinhOld);
+            }
             *bSumEkinhOld = FALSE;
         }
     }

api/legacy/include/gromacs/mdtypes/inputrec.h
@@ -738,4 +738,7 @@ bool haveFreeEnergyType(const t_inputrec& ir, int fepType);
 bool fepLambdasChangeAtSameRate(
         const gmx::EnumerationArray<FreeEnergyPerturbationCouplingType, std::vector<double>>& allLambdas);
 
+//! Return whether the box is continuously deformed
+bool ir_haveBoxDeformation(const t_inputrec& ir);
+
 #endif /* GMX_MDTYPES_INPUTREC_H */

api/legacy/include/gromacs/pbcutil/pbc.h
@@ -325,10 +325,19 @@ void put_atoms_in_box(PbcType pbcType, const matrix box, gmx::ArrayRef<gmx::RVec
  *
  * \param[in]     pbcType    The pbc type
  * \param[in]     box        The simulation box
+ * \param[in]     haveBoxDeformation  Whether the box is being continously deformed
+ * \param[in]     boxDeformation      The deformation speed of the box components in units of nm/ps
  * \param[in,out] x          The coordinates of the atoms
+ * \param[in,out] v          The velocities of the atoms
  * \param[in]     nth        number of threads to be used in the given module
  */
-void put_atoms_in_box_omp(PbcType pbcType, const matrix box, gmx::ArrayRef<gmx::RVec> x, gmx_unused int nth);
+void put_atoms_in_box_omp(PbcType                  pbcType,
+                          const matrix             box,
+                          bool                     haveBoxDeformation,
+                          const matrix             boxDeformation,
+                          gmx::ArrayRef<gmx::RVec> x,
+                          gmx::ArrayRef<gmx::RVec> v,
+                          gmx_unused int           nth);
 
 /*! \brief Put atoms inside triclinic box
  *
@@ -357,11 +366,21 @@ void put_atoms_in_compact_unitcell(PbcType pbcType, int ecenter, const matrix bo
  *
  * \param[in]     fplog     Log file
  * \param[in]     pbcType   The PBC type
+ * \param[in]     correctVelocitiesForBoxDeformation  Whether to correct the velocities for continous box deformation
+ * \param[in]     boxDeformation  The box deformation velocity
  * \param[in]     box       The simulation box
  * \param[in]     mtop      System topology definition
  * \param[in,out] x         The coordinates of the atoms
+ * \param[in,out] v         The velocities of the atoms, needed with correctVelocitiesForBoxDeformation
  */
-void do_pbc_first_mtop(FILE* fplog, PbcType pbcType, const matrix box, const gmx_mtop_t* mtop, rvec x[]);
+void do_pbc_first_mtop(FILE*                    fplog,
+                       PbcType                  pbcType,
+                       bool                     correctVelocitiesForBoxDeformation,
+                       const matrix             boxDeformation,
+                       const matrix             box,
+                       const gmx_mtop_t*        mtop,
+                       gmx::ArrayRef<gmx::RVec> x,
+                       gmx::ArrayRef<gmx::RVec> v);
 
 /*! \brief Make molecules consisting of multiple charge groups whole by shifting positions
  *
@@ -371,5 +390,40 @@ void do_pbc_first_mtop(FILE* fplog, PbcType pbcType, const matrix box, const gmx
  * \param[in,out] x         The coordinates of the atoms
  */
 void do_pbc_mtop(PbcType pbcType, const matrix box, const gmx_mtop_t* mtop, rvec x[]);
+
+
+/*! \brief Sets the box deformation rate
+ *
+ * \param[in]      boxDeformation      The deformation speed of the box components in units of nm/ps
+ * \param[in]      box                 The box
+ * \param[out]     boxDeformationRate  The deformation rate of the box in units of 1/ps
+ */
+void setBoxDeformationRate(const matrix boxDeformation, const matrix box, matrix boxDeformationRate);
+
+/*! \brief Correct the velocity of a particle for displacement along a flow field
+ *
+ * \tparam         invertDisplacement  When true, correct for -displacement
+ * \param[in]      boxDeformationRate  The deformation rate of the box in units of 1/ps
+ * \param[in,out]  v                   The velocity field to be corrected
+ * \param[in]      displacement        The coordinate displacement to correct for
+ */
+template<bool invertDisplacement>
+static inline void correctVelocityForDisplacement(const matrix boxDeformationRate, rvec v, const rvec displacement)
+{
+    for (int d1 = 0; d1 < DIM; d1++)
+    {
+        for (int d2 = 0; d2 <= d1; d2++)
+        {
+            if constexpr (invertDisplacement)
+            {
+                v[d2] -= boxDeformationRate[d1][d2] * displacement[d1];
+            }
+            else
+            {
+                v[d2] += boxDeformationRate[d1][d2] * displacement[d1];
+            }
+        }
+    }
+}
 
 #endif

api/nblib/gmxcalculatorcpu.cpp
@@ -145,7 +145,8 @@ void GmxNBForceCalculatorCpu::CpuImpl::updatePairlist(gmx::ArrayRef<gmx::RVec> c
     const real particleDensity = static_cast<real>(coordinates.size()) / det(legacyBox);
 
     // If particles are too far outside the box, the grid setup can fail
-    put_atoms_in_box_omp(PbcType::Xyz, box.legacyMatrix(), coordinates, backend_.numThreads_);
+    put_atoms_in_box_omp(
+            PbcType::Xyz, box.legacyMatrix(), false, nullptr, coordinates, {}, backend_.numThreads_);
 
     // Put particles on a grid based on bounds specified by the box
     nbnxn_put_on_grid(backend_.nbv_.get(),

src/gromacs/domdec/distribute.cpp
@@ -52,6 +52,7 @@
 #include "gromacs/mdtypes/commrec.h"
 #include "gromacs/mdtypes/df_history.h"
 #include "gromacs/mdtypes/state.h"
+#include "gromacs/pbcutil/pbc.h"
 #include "gromacs/topology/topology.h"
 #include "gromacs/utility/enumerationhelpers.h"
 #include "gromacs/utility/fatalerror.h"
@@ -286,8 +287,11 @@ static inline int computeAtomGroupDomainIndex(const gmx_domdec_t& dd,
                                               int                                    atomBegin,
                                               int                                    atomEnd,
                                               const matrix                           box,
-                                              rvec*                                  pos)
+                                              rvec*                                  pos,
+                                              rvec*                                  velocities)
 {
+    const bool haveBoxDeformation = dd.comm->systemInfo.haveBoxDeformation;
+
     /* Set the reference location for assigning the group */
     rvec cog;
     int  numAtoms = atomEnd - atomBegin;
@@ -337,6 +341,11 @@ static inline int computeAtomGroupDomainIndex(const gmx_domdec_t& dd,
                 for (int a = atomBegin; a < atomEnd; a++)
                 {
                     rvec_dec(pos[a], box[d]);
+                    if (haveBoxDeformation)
+                    {
+                        correctVelocityForDisplacement<true>(
+                                dd.comm->systemInfo.boxDeformationRate, velocities[a], box[d]);
+                    }
                     if (bScrew)
                     {
                         pos[a][YY] = box[YY][YY] - pos[a][YY];
@@ -356,6 +365,11 @@ static inline int computeAtomGroupDomainIndex(const gmx_domdec_t& dd,
                 for (int a = atomBegin; a < atomEnd; a++)
                 {
                     rvec_inc(pos[a], box[d]);
+                    if (haveBoxDeformation)
+                    {
+                        correctVelocityForDisplacement<false>(
+                                dd.comm->systemInfo.boxDeformationRate, velocities[a], box[d]);
+                    }
                     if (bScrew)
                     {
                         pos[a][YY] = box[YY][YY] - pos[a][YY];
@@ -381,6 +395,7 @@ static std::vector<std::vector<int>> getAtomGroupDistribution(const gmx::MDLogge
                                                               const matrix         box,
                                                               const gmx_ddbox_t&   ddbox,
                                                               rvec                 pos[],
+                                                              rvec                 velocities[],
                                                               gmx_domdec_t*        dd)
 {
     AtomDistribution& ma = *dd->ma;
@@ -415,7 +430,7 @@ static std::vector<std::vector<int>> getAtomGroupDistribution(const gmx::MDLogge
                     const int   atomBegin   = atomOffset + block.begin();
                     const int   atomEnd     = atomOffset + block.end();
                     const int   domainIndex = computeAtomGroupDomainIndex(
-                            *dd, ddbox, triclinicCorrectionMatrix, cellBoundaries, atomBegin, atomEnd, box, pos);
+                            *dd, ddbox, triclinicCorrectionMatrix, cellBoundaries, atomBegin, atomEnd, box, pos, velocities);
 
                     for (int atomIndex : block)
                     {
@@ -436,7 +451,7 @@ static std::vector<std::vector<int>> getAtomGroupDistribution(const gmx::MDLogge
         for (int atom = 0; atom < mtop.natoms; atom++)
         {
             int domainIndex = computeAtomGroupDomainIndex(
-                    *dd, ddbox, triclinicCorrectionMatrix, cellBoundaries, atom, atom + 1, box, pos);
+                    *dd, ddbox, triclinicCorrectionMatrix, cellBoundaries, atom, atom + 1, box, pos, velocities);
 
             indices[domainIndex].push_back(atom);
             ma.domainGroups[domainIndex].numAtoms += 1;
@@ -480,7 +495,8 @@ static void distributeAtomGroups(const gmx::MDLogger& mdlog,
                                  const gmx_mtop_t&    mtop,
                                  const matrix         box,
                                  const gmx_ddbox_t*   ddbox,
-                                 rvec                 pos[])
+                                 rvec                 pos[],
+                                 rvec                 velocities[])
 {
     AtomDistribution* ma   = dd->ma.get();
     int *             ibuf = nullptr, buf2[2] = { 0, 0 };
@@ -497,7 +513,7 @@ static void distributeAtomGroups(const gmx::MDLogger& mdlog,
             check_screw_box(box);
         }
 
-        groupIndices = getAtomGroupDistribution(mdlog, mtop, box, *ddbox, pos, dd);
+        groupIndices = getAtomGroupDistribution(mdlog, mtop, box, *ddbox, pos, velocities, dd);
 
         for (int rank = 0; rank < dd->nnodes; rank++)
         {
@@ -568,8 +584,9 @@ void distributeState(const gmx::MDLogger& mdlog,
                      t_state*             state_local)
 {
     rvec* xGlobal = (DDMAIN(dd) ? state_global->x.rvec_array() : nullptr);
+    rvec* vGlobal = (DDMAIN(dd) ? state_global->v.rvec_array() : nullptr);
 
-    distributeAtomGroups(mdlog, dd, mtop, DDMAIN(dd) ? state_global->box : nullptr, &ddbox, xGlobal);
+    distributeAtomGroups(mdlog, dd, mtop, DDMAIN(dd) ? state_global->box : nullptr, &ddbox, xGlobal, vGlobal);
 
     dd_distribute_state(dd, state_global, state_local);
 }

src/gromacs/domdec/domdec.cpp
@@ -2075,6 +2075,9 @@ static DDSystemInfo getSystemInfo(const gmx::MDLogger&              mdlog,
     }
     systemInfo.cellsizeLimit = std::max(systemInfo.cellsizeLimit, systemInfo.constraintCommunicationRange);
 
+    systemInfo.haveBoxDeformation = ir_haveBoxDeformation(ir);
+    setBoxDeformationRate(ir.deform, box, systemInfo.boxDeformationRate);
+
     return systemInfo;
 }
 

src/gromacs/domdec/domdec_internal.h
@@ -455,6 +455,11 @@ struct DDSystemInfo
     bool filterBondedCommunication = false;
     //! Whether to increase the multi-body cut-off beyond the minimum required
     bool increaseMultiBodyCutoff = false;
+
+    //! Whether we have continous box deformation
+    bool haveBoxDeformation;
+    //! The box deformation rate in units of 1/ps
+    matrix boxDeformationRate;
 };
 
 /*! \brief Settings that affect the behavior of the domain decomposition

src/gromacs/domdec/redistribute.cpp
@@ -535,17 +535,35 @@ static void calcGroupMove(FILE*                     fplog,
 
 static void applyPbcAndSetMoveFlags(const gmx::UpdateGroupsCog&     updateGroupsCog,
                                     gmx::ArrayRef<const PbcAndFlag> pbcAndFlags,
+                                    const bool                      haveBoxDeformation,
+                                    const matrix                    boxDeformationRate,
                                     int                             atomBegin,
                                     int                             atomEnd,
                                     gmx::ArrayRef<gmx::RVec>        atomCoords,
+                                    gmx::ArrayRef<gmx::RVec>        atomVelocities,
                                     gmx::ArrayRef<int>              move)
 {
-    for (int a = atomBegin; a < atomEnd; a++)
+    if (!haveBoxDeformation)
     {
-        const PbcAndFlag& pbcAndFlag = pbcAndFlags[updateGroupsCog.cogIndex(a)];
-        rvec_inc(atomCoords[a], pbcAndFlag.pbcShift);
-        /* Temporarily store the flag in move */
-        move[a] = pbcAndFlag.moveFlag;
+        for (int a = atomBegin; a < atomEnd; a++)
+        {
+            const PbcAndFlag& pbcAndFlag = pbcAndFlags[updateGroupsCog.cogIndex(a)];
+            rvec_inc(atomCoords[a], pbcAndFlag.pbcShift);
+            /* Temporarily store the flag in move */
+            move[a] = pbcAndFlag.moveFlag;
+        }
+    }
+    else
+    {
+        for (int a = atomBegin; a < atomEnd; a++)
+        {
+            const PbcAndFlag& pbcAndFlag = pbcAndFlags[updateGroupsCog.cogIndex(a)];
+            rvec_inc(atomCoords[a], pbcAndFlag.pbcShift);
+            // Correct the velocity for the position change along the flow profile
+            correctVelocityForDisplacement<false>(boxDeformationRate, atomVelocities[a], pbcAndFlag.pbcShift);
+            /* Temporarily store the flag in move */
+            move[a] = pbcAndFlag.moveFlag;
+        }
     }
 }
 
@@ -648,9 +666,12 @@ void dd_redistribute_cg(FILE*         fplog,
                 const int numHomeAtoms = comm->atomRanges.numHomeAtoms();
                 applyPbcAndSetMoveFlags(updateGroupsCog,
                                         pbcAndFlags,
+                                        comm->systemInfo.haveBoxDeformation,
+                                        comm->systemInfo.boxDeformationRate,
                                         (thread * numHomeAtoms) / nthread,
                                         ((thread + 1) * numHomeAtoms) / nthread,
                                         state->x,
+                                        state->v,
                                         move);
             }
             else

commit c4a0bf0eb00e4490cfb9bc88fba7f9e205541cc0

Suppress clang-tidy warnings
These were caused by commit 28697ebf6b

src/gromacs/mdlib/md_support.cpp
@@ -141,6 +141,7 @@ static void calc_ke_part_normal(const matrix                   deform,
         ekind->systemMomenta->momentumHalfStep.clear();
     }
 
+    // NOLINTNEXTLINE(readability-misleading-indentation)
     const int nthread = gmx_omp_nthreads_get(ModuleMultiThread::Update);
 
 #pragma omp parallel for num_threads(nthread) schedule(static)
@@ -175,6 +176,7 @@ static void calc_ke_part_normal(const matrix                   deform,
             systemMomentumWork->clear();
         }
 
+        // NOLINTNEXTLINE(readability-misleading-indentation)
         gt = 0;
         for (n = start_t; n < end_t; n++)
         {
@@ -198,6 +200,7 @@ static void calc_ke_part_normal(const matrix                   deform,
                 }
             }
 
+            // NOLINTNEXTLINE(readability-misleading-indentation)
             for (d = 0; (d < DIM); d++)
             {
                 for (m = 0; (m < DIM); m++)