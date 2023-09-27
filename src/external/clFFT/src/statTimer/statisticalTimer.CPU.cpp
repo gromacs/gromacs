aad538fb0f0c435f64ce6362b071ec6cb41a23e6
Vedran Miletić <vedran@miletic.net>
2023-09-27 09:10:21 +0000

    Replaced legacy std::{unary,binary}_function in clFFT code
    These symbols were deprecated in C++11 and removed in C++17. The
    replacement is provided by std::function, which is used here.
    
    Standard-compliant C++17 compiler is not required to provide these
    symbols and in Clang 16 with libc++ they are explicitely unavailable.

--- src/external/clFFT/src/statTimer/statisticalTimer.CPU.cpp
+++ src/external/clFFT/src/statTimer/statisticalTimer.CPU.cpp
@@ -62,7 +62,7 @@ std::basic_string<TCHAR> commatize (T number)
 
 //	Functor object to help with accumulating values in vectors
 template< typename T >
-struct Accumulator: public std::unary_function< T, void >
+struct Accumulator: public std::function< void(T) >
 {
 	T acc;
 
