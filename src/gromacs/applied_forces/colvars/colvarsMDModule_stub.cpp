c6b2c30bcb1b774676f1c11e8f170afd0342aa9c
Hubert Santuz <hubert.santuz@gmail.com>
2024-02-12 09:26:58 +0000

    Fix grompp/mdrun crash when GMX_USE_COLVARS=None
    Colvars mdp fields handler was not properly constructed when Colvars library was not compiled. Now, mdp output will have the same colvars fields values as when Colvars is compiled but not activated.
    
    Now a CI configuration tests this case.
    
    Fixes #4980

--- src/gromacs/applied_forces/colvars/colvarsMDModule_stub.cpp
+++ src/gromacs/applied_forces/colvars/colvarsMDModule_stub.cpp
@@ -48,6 +48,7 @@
 #include "gromacs/fileio/checkpoint.h"
 #include "gromacs/mdtypes/commrec.h"
 #include "gromacs/mdtypes/imdmodule.h"
+#include "gromacs/mdtypes/imdpoptionprovider.h"
 #include "gromacs/utility/keyvaluetreebuilder.h"
 
 #include "colvarsMDModule.h"
@@ -60,9 +61,41 @@ namespace
 {
 
 /*! \internal
+ * \brief Colvars Options
+ *
+ * Stub Implementation in case Colvars library is not compiled
+ */
+class ColvarsOptions final : public IMdpOptionProvider
+{
+
+private:
+    const std::string c_colvarsModuleName = "colvars";
+    const std::string c_activeTag_        = "active";
+
+public:
+    void initMdpTransform(IKeyValueTreeTransformRules* /*rules*/) override {}
+
+    /*! \brief Create Colvars mdp output even if Colvars is not compiled.
+     * This is to be consistent with the case where Colvars is compiled but not activated
+     */
+    void buildMdpOutput(KeyValueTreeObjectBuilder* builder) const override
+    {
+        // new empty line before writing colvars mdp values
+        builder->addValue<std::string>("comment-" + c_colvarsModuleName + "empty-line", "");
+
+        builder->addValue<std::string>("comment-" + c_colvarsModuleName + "-module",
+                                       "; Colvars bias");
+        builder->addValue<bool>(c_colvarsModuleName + "-" + c_activeTag_, false);
+    }
+
+    void initMdpOptions(IOptionsContainerWithSections* /*options*/) override {}
+};
+
+
+/*! \internal
  * \brief Colvars module
  *
- * Stub Implementation
+ * Stub Implementation in case Colvars library is not compiled
  */
 class ColvarsMDModule final : public IMDModule
 {
@@ -75,11 +108,14 @@ public:
 
     void subscribeToSimulationSetupNotifications(MDModulesNotifiers* /*notifier*/) override {}
 
-    IMdpOptionProvider* mdpOptionProvider() override { return nullptr; }
+    IMdpOptionProvider* mdpOptionProvider() override { return &colvarsOptionsStub_; }
 
     IMDOutputProvider* outputProvider() override { return nullptr; }
 
     void initForceProviders(ForceProviders* /*forceProviders*/) override {}
+
+private:
+    ColvarsOptions colvarsOptionsStub_;
 };
 
 
