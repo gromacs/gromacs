583f2dc80ad9b07bb9c161ef45771dca1916089a
Berk Hess <hess@kth.se>
2024-02-29 08:47:59 +0000

    Replace ListOfLists assert by enable_if
    The removes the restriction to default constructible types.

--- api/legacy/include/gromacs/utility/listoflists.h
+++ api/legacy/include/gromacs/utility/listoflists.h
@@ -123,12 +123,10 @@ public:
 
     /*! \brief Appends a new list with \p numElements elements
      *
-     * \note Type T should be default constructible.
+     * \note Type T has to be default constructible.
      */
-    void pushBackListOfSize(int numElements)
+    std::enable_if_t<std::is_default_constructible_v<T>, void> pushBackListOfSize(int numElements)
     {
-        static_assert(std::is_default_constructible_v<T>);
-
         elements_.resize(elements_.size() + numElements);
         listRanges_.push_back(int(elements_.size()));
     }
