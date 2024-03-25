gromacs/gromacs

commit d837bf554cb4a695ea18bfb76d7e7600d6cfd5d8

Fix disabling of CUDA Graphs with CPU forces
There is an MPI reduction to determine if any rank has CPU forces, and
if so then disable graphs across all ranks. Previously, this was using
a combination of MPI_SUM and MPI_BYTE which can cause undefined
behaviour. Now fixed to use MPI_INTEGER (using temporary variables).

src/gromacs/mdlib/mdgraph_gpu_impl.cu
@@ -171,12 +171,18 @@ void MdGpuGraph::Impl::disableForDomainIfAnyPpRankHasCpuForces(bool disableGraph
     if (havePPDomainDecomposition_)
     {
         // If disabled on any domain, disable on all domains
-        MPI_Allreduce(&disableGraphAcrossAllPpRanks,
-                      &disableGraphAcrossAllPpRanks_,
-                      sizeof(bool),
-                      MPI_BYTE,
+        int disableGraphAcrossAllPpRanksReductionInput  = disableGraphAcrossAllPpRanks;
+        int disableGraphAcrossAllPpRanksReductionOutput = 0;
+        MPI_Allreduce(&disableGraphAcrossAllPpRanksReductionInput,
+                      &disableGraphAcrossAllPpRanksReductionOutput,
+                      1,
+                      MPI_INT,
                       MPI_SUM,
                       mpiComm_);
+        if (disableGraphAcrossAllPpRanksReductionOutput > 0)
+        {
+            disableGraphAcrossAllPpRanks_ = true;
+        }
     }
 }
 

commit 7d6b3b2cc1bc2d5093214433f47d8c09bf759dfa

Static cast on bool to int conversion
Fixes clang-tidy build failure introduced by d837bf554c

src/gromacs/mdlib/mdgraph_gpu_impl.cu
@@ -171,7 +171,7 @@ void MdGpuGraph::Impl::disableForDomainIfAnyPpRankHasCpuForces(bool disableGraph
     if (havePPDomainDecomposition_)
     {
         // If disabled on any domain, disable on all domains
-        int disableGraphAcrossAllPpRanksReductionInput  = disableGraphAcrossAllPpRanks;
+        int disableGraphAcrossAllPpRanksReductionInput = static_cast<int>(disableGraphAcrossAllPpRanks);
         int disableGraphAcrossAllPpRanksReductionOutput = 0;
         MPI_Allreduce(&disableGraphAcrossAllPpRanksReductionInput,
                       &disableGraphAcrossAllPpRanksReductionOutput,