d8b13251015122bd4c2d44d0c9bba4dc04206f8b
Andrey Alekseenko <al42and@gmail.com>
2024-04-02 17:22:36 +0200

    Revert "Second attempt at fixing NVCC++ crash"
    This reverts commit d60039d1439ee99662492f85fd1e8889d9514c14.
    
    Turns out, the NVC++ crashes in CI were caused by compiler cache, so
    this workaround is not needed.

--- src/gromacs/mdlib/update.cpp
+++ src/gromacs/mdlib/update.cpp
@@ -1752,11 +1752,7 @@ void Update::Impl::update_coords(const t_inputrec&                 inputRecord,
     /* ############# START The update of velocities and positions ######### */
     int nth = gmx_omp_nthreads_get(ModuleMultiThread::Update);
 
-    // nvc++ 24.1+ version has bug due to which it generates incorrect OMP code for this region
-    // (more specifically do_update_sd()) so disable this until nvc++ gets fixed.
-#if !defined(__NVCOMPILER)
-#    pragma omp parallel for num_threads(nth) schedule(static)
-#endif
+#pragma omp parallel for num_threads(nth) schedule(static)
     for (int th = 0; th < nth; th++)
     {
         try
