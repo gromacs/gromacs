gromacs/gromacs

commit 219026b9b3455f3f2f4dee27e0b4cd58f788c5f4

SYCL: Pass rolling prune part via global memory
Same change was done in CUDA for GPU Graphs in 1fdbafbcc0.

Refs #4906

src/gromacs/nbnxm/sycl/nbnxm_sycl_kernel_pruneonly.cpp
@@ -64,15 +64,16 @@ namespace Nbnxm
  */
 template<bool haveFreshList>
 auto nbnxmKernelPruneOnly(sycl::handler& cgh,
+                          const int      numSci,
+                          const int      numParts,
                           const Float4* __restrict__ gm_xq,
                           const Float3* __restrict__ gm_shiftVec,
                           nbnxn_cj_packed_t* __restrict__ gm_plistCJPacked,
                           const nbnxn_sci_t* __restrict__ gm_plistSci,
                           unsigned int* __restrict__ gm_plistIMask,
+                          int* __restrict__ gm_rollingPruningPart,
                           const float rlistOuterSq,
-                          const float rlistInnerSq,
-                          const int   numParts,
-                          const int   part)
+                          const float rlistInnerSq)
 {
     /* shmem buffer for i x+q pre-loading */
     sycl::local_accessor<Float4, 1> sm_xq(
@@ -100,6 +101,21 @@ auto nbnxmKernelPruneOnly(sycl::handler& cgh,
         const int      tidx  = tidxj * c_clSize + tidxi;
         const unsigned tidxz = itemIdx.get_local_id(0);
         const unsigned bidx  = itemIdx.get_group(0);
+
+        int part = gm_rollingPruningPart[bidx];
+        itemIdx.barrier(fence_space::global_and_local);
+        if (tidxi == 0 && tidxj == 0 && tidxz == 0)
+        {
+            gm_rollingPruningPart[bidx] = (part + 1) % numParts;
+        }
+
+        // Kernel has been launched with max number of blocks across all passes (plist.nsci/numParts),
+        // but the last pass will require 1 less block, so extra block should return early.
+        const unsigned int numSciInPart = (numSci - part) / numParts;
+        if (bidx >= numSciInPart)
+        {
+            return; // Since the whole block is exiting, it is fine w.r.t. group_barrier
+        }
 
         const sycl::sub_group sg   = itemIdx.get_sub_group();
         const unsigned        widx = tidx / warpSize;
@@ -218,7 +234,7 @@ auto nbnxmKernelPruneOnly(sycl::handler& cgh,
 
 //! \brief Leap Frog SYCL prune-only kernel launch code.
 template<bool haveFreshList, class... Args>
-void launchNbnxmKernelPruneOnly(const DeviceStream& deviceStream, const int numSciInPart, Args&&... args)
+void launchNbnxmKernelPruneOnly(const DeviceStream& deviceStream, const int numSciInPartMax, Args&&... args)
 {
     using kernelNameType = NbnxmKernelPruneOnly<haveFreshList>;
 
@@ -227,7 +243,7 @@ void launchNbnxmKernelPruneOnly(const DeviceStream& deviceStream, const int numS
      *   and j-cluster concurrency, in x, y, and z, respectively.
      * - The 1D block-grid contains as many blocks as super-clusters.
      */
-    const unsigned long     numBlocks = numSciInPart;
+    const unsigned long     numBlocks = numSciInPartMax;
     const sycl::range<3>    blockSize{ c_syclPruneKernelJPackedConcurrency, c_clSize, c_clSize };
     const sycl::range<3>    globalSize{ numBlocks * blockSize[0], blockSize[1], blockSize[2] };
     const sycl::nd_range<3> range{ globalSize, blockSize };
@@ -251,11 +267,7 @@ void chooseAndLaunchNbnxmKernelPruneOnly(bool haveFreshList, Args&&... args)
             haveFreshList);
 }
 
-void launchNbnxmKernelPruneOnly(NbnxmGpu*                 nb,
-                                const InteractionLocality iloc,
-                                const int                 numParts,
-                                const int                 part,
-                                const int                 numSciInPart)
+void launchNbnxmKernelPruneOnly(NbnxmGpu* nb, const InteractionLocality iloc, const int numParts, const int numSciInPartMax)
 {
     NBAtomDataGpu*      adat          = nb->atdat;
     NBParamGpu*         nbp           = nb->nbparam;
@@ -265,16 +277,17 @@ void launchNbnxmKernelPruneOnly(NbnxmGpu*                 nb,
 
     chooseAndLaunchNbnxmKernelPruneOnly(haveFreshList,
                                         deviceStream,
-                                        numSciInPart,
+                                        numSciInPartMax,
+                                        plist->nsci,
+                                        numParts,
                                         adat->xq.get_pointer(),
                                         adat->shiftVec.get_pointer(),
                                         plist->cjPacked.get_pointer(),
                                         plist->sci.get_pointer(),
                                         plist->imask.get_pointer(),
+                                        plist->d_rollingPruningPart.get_pointer(),
                                         nbp->rlistOuter_sq,
-                                        nbp->rlistInner_sq,
-                                        numParts,
-                                        part);
+                                        nbp->rlistInner_sq);
 }
 
 } // namespace Nbnxm

src/gromacs/nbnxm/sycl/nbnxm_sycl.cpp
@@ -60,7 +60,6 @@ void gpu_launch_kernel_pruneonly(NbnxmGpu* nb, const InteractionLocality iloc, c
 
         /* Set rollingPruningNumParts to signal that it is not set */
         plist->rollingPruningNumParts = 0;
-        plist->rollingPruningPart     = 0;
     }
     else
     {
@@ -75,28 +74,23 @@ void gpu_launch_kernel_pruneonly(NbnxmGpu* nb, const InteractionLocality iloc, c
         }
     }
 
-    /* Use a local variable for part and update in plist, so we can return here
-     * without duplicating the part increment code.
+    /* Compute the max number of list entries to prune across all passes
+     * Note that the actual number for a specific pass will be computed inside the kernel.
+     * Also note that this implementation (parts tracking on device) differs from some
+     * other backends (parts tracking on host, passed as kernel argument).
      */
-    const int part = plist->rollingPruningPart;
 
-    plist->rollingPruningPart++;
-    if (plist->rollingPruningPart >= plist->rollingPruningNumParts)
-    {
-        plist->rollingPruningPart = 0;
-    }
-
-    /* Compute the number of list entries to prune in this pass */
-    const int numSciInPart = (plist->nsci - part) / numParts;
+    /* Compute the max number of list entries to prune in this pass */
+    const int numSciInPartMax = (plist->nsci) / numParts;
 
     /* Don't launch the kernel if there is no work to do */
-    if (numSciInPart <= 0)
+    if (numSciInPartMax <= 0)
     {
         plist->haveFreshList = false;
         return;
     }
 
-    launchNbnxmKernelPruneOnly(nb, iloc, numParts, part, numSciInPart);
+    launchNbnxmKernelPruneOnly(nb, iloc, numParts, numSciInPartMax);
 
     if (plist->haveFreshList)
     {

src/gromacs/nbnxm/sycl/nbnxm_sycl_kernel_pruneonly.h
@@ -56,14 +56,12 @@ using gmx::InteractionLocality;
  * \param nb Non-bonded parameters.
  * \param iloc Interaction locality.
  * \param numParts Total number of rolling-prune parts.
- * \param part Number of the part to prune.
- * \param numSciInPart Number of superclusters in \p part.
+ * \param numSciInPartMax Maximum number of superclusters in a part.
  */
 void launchNbnxmKernelPruneOnly(NbnxmGpu*                 nb,
                                 const InteractionLocality iloc,
                                 const int                 numParts,
-                                const int                 part,
-                                const int                 numSciInPart);
+                                const int                 numSciInPartMax);
 
 } // namespace Nbnxm
 

commit 2489a5903b0d28e99f8d0b5303bfd21ac3d5608c

Fix minor performance regression in SYCL Pruning kernel
Introduced in 219026b9b345 (!3991).

About ~6% improvement to rolling prune kernel on MI250X;
no significant changes when pruning the fresh list.

No significant changes on PVC1100 (but that's without kernel serialization).

src/gromacs/nbnxm/sycl/nbnxm_sycl_kernel_pruneonly.cpp
@@ -100,7 +100,7 @@ auto nbnxmKernelPruneOnly(sycl::handler& cgh,
         const unsigned tidxj = itemIdx.get_local_id(1);
         const int      tidx  = tidxj * c_clSize + tidxi;
         const unsigned tidxz = itemIdx.get_local_id(0);
-        const unsigned bidx  = itemIdx.get_group(0);
+        const int      bidx  = itemIdx.get_group(0);
 
         int part = gm_rollingPruningPart[bidx];
         itemIdx.barrier(fence_space::global_and_local);
@@ -111,14 +111,14 @@ auto nbnxmKernelPruneOnly(sycl::handler& cgh,
 
         // Kernel has been launched with max number of blocks across all passes (plist.nsci/numParts),
         // but the last pass will require 1 less block, so extra block should return early.
-        const unsigned int numSciInPart = (numSci - part) / numParts;
+        const int numSciInPart = (numSci - part) / numParts;
         if (bidx >= numSciInPart)
         {
             return; // Since the whole block is exiting, it is fine w.r.t. group_barrier
         }
 
         const sycl::sub_group sg   = itemIdx.get_sub_group();
-        const unsigned        widx = tidx / warpSize;
+        const int             widx = tidx / warpSize;
 
         // my i super-cluster's index = sciOffset + current bidx * numParts + part
         const nbnxn_sci_t nbSci          = gm_plistSci[bidx * numParts + part];