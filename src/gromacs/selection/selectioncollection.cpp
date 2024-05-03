8c425dc7416e72eec411baa14174501adf37fe00
Mark Abraham <mark.j.abraham@gmail.com>
2024-05-03 12:26:14 +0200

    Fix some MSVC warnings

--- src/gromacs/selection/selectioncollection.cpp
+++ src/gromacs/selection/selectioncollection.cpp
@@ -780,7 +780,8 @@ SelectionList SelectionCollection::parseFromFile(const std::filesystem::path& fi
     }
     catch (GromacsException& ex)
     {
-        ex.prependContext(formatString("Error in parsing selections from file '%s'", filename.c_str()));
+        ex.prependContext(formatString("Error in parsing selections from file '%s'",
+                                       filename.string().c_str()));
         throw;
     }
 }
