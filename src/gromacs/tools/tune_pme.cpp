1820ba9aa94418675746b5438a62b3c12db40456
Gilles Gouaillardet <gilles@rist.or.jp>
2024-05-22 11:35:49 +0000

    tune_pme: fix MPI/GPU detection
    add missing whitespace in order to match the formatting changes
    previously done in gmx_print_version_info() with
    commit gromacs/gromacs@ffe565fe7a8aef750edf86f08d7f4fe9857e90a1

--- src/gromacs/tools/tune_pme.cpp
+++ src/gromacs/tools/tune_pme.cpp
@@ -688,11 +688,12 @@ static void check_mdrun_works(gmx_bool    bThreads,
     FILE*      fp;
     const char filename[] = "benchtest.log";
 
-    /* This string should always be identical to the one in copyrite.c,
-     * gmx_print_version_info() in the GMX_MPI section */
-    const char match_mpi[]     = "MPI library:        MPI";
+    /* This string should always be identical to the one in
+     * utility/binaryinformation.cpp gmx_print_version_info()
+     * in the GMX_MPI section */
+    const char match_mpi[]     = "MPI library:         MPI";
     const char match_mdrun[]   = "Executable: ";
-    const char match_nogpu[]   = "GPU support:        disabled";
+    const char match_nogpu[]   = "GPU support:         disabled";
     gmx_bool   bMdrun          = FALSE;
     gmx_bool   bMPI            = FALSE;
     gmx_bool   bHaveGpuSupport = TRUE;
