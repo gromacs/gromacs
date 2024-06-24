e78bcc0ddeb0db15de13ef54efaa3a8a109b9fbd
Dmitry Morozov <aracsmd@gmail.com>
2024-06-24 20:06:32 +0000

    Fix for stripExtension() usage in QMMM module
    Small fix. Previously `stripExtension()` returned `std::string` as the result, now it returns `std::filesystem::path` fixed that to correct usage in QMMM MdModule.

--- src/gromacs/applied_forces/qmmm/qmmmoptions.cpp
+++ src/gromacs/applied_forces/qmmm/qmmmoptions.cpp
@@ -308,7 +308,7 @@ void QMMMOptions::processTprFilename(const MdRunInputFilename& tprFilename)
 
     parameters_.qmFileNameBase_ =
             stripExtension(std::filesystem::path(tprFilename.mdRunFilename_).filename())
-                    .append("_cp2k")
+                    .concat("_cp2k")
                     .u8string();
 }
 
