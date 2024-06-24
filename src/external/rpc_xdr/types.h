gromacs/gromacs

commit 1f4a152a8f5c650999b9e6da14d91cff4f3d89ea

Use RPC XDR as external library, based on a recent official version.
Modifications in the GROMACS distribution are clearly explained.

Fixes: #1241, #5048.

Closes #1241 and #5048

src/external/rpc_xdr/types.h
@@ -1,0 +1,106 @@
+/*
+ * Copyright (c) 2010, Oracle America, Inc.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name of the "Oracle America, Inc." nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *   COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *   INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *   GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* fixincludes should not add extern "C" to this file */
+/*
+ * Rpc additions to <sys/types.h>
+ */
+#ifndef _RPC_TYPES_H
+#define _RPC_TYPES_H 1
+
+typedef int bool_t;
+typedef int enum_t;
+/* This needs to be changed to uint32_t in the future */
+typedef unsigned long rpcprog_t;
+typedef unsigned long rpcvers_t;
+typedef unsigned long rpcproc_t;
+typedef unsigned long rpcprot_t;
+typedef unsigned long rpcport_t;
+
+#define        __dontcare__    -1
+
+#ifndef FALSE
+#      define  FALSE   (0)
+#endif
+
+#ifndef TRUE
+#      define  TRUE    (1)
+#endif
+
+#ifndef NULL
+#      define  NULL 0
+#endif
+
+#include <stdlib.h>		/* For malloc decl.  */
+#define mem_alloc(bsize)	malloc(bsize)
+/*
+ * XXX: This must not use the second argument, or code in xdr_array.c needs
+ * to be modified.
+ */
+#define mem_free(ptr, bsize)	free(ptr)
+
+#ifndef makedev /* ie, we haven't already included it */
+#include <sys/types.h>
+#endif
+
+#if defined __APPLE_CC__ || defined __FreeBSD__
+# define __u_char_defined
+# define __daddr_t_defined
+#endif
+
+#ifndef __u_char_defined
+typedef __u_char u_char;
+typedef __u_short u_short;
+typedef __u_int u_int;
+typedef __u_long u_long;
+typedef __quad_t quad_t;
+typedef __u_quad_t u_quad_t;
+typedef __fsid_t fsid_t;
+# define __u_char_defined
+#endif
+#ifndef __daddr_t_defined
+typedef __daddr_t daddr_t;
+typedef __caddr_t caddr_t;
+# define __daddr_t_defined
+#endif
+
+#include <sys/time.h>
+#include <sys/param.h>
+
+#include <netinet/in.h>
+
+#ifndef INADDR_LOOPBACK
+#define       INADDR_LOOPBACK         (u_long)0x7F000001
+#endif
+#ifndef MAXHOSTNAMELEN
+#define        MAXHOSTNAMELEN  64
+#endif
+
+#endif /* rpc/types.h */

commit 8a3cc40eb7a27b924d567b503fe46d40e173268d

Fixed RPC XDR compilation errors on Mac and Windows
The compilation errors were introduced in commit 1f4a152a.

src/external/rpc_xdr/types.h
@@ -28,6 +28,14 @@
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/* This file has been modified in the GROMACS distribution by, e.g.:
+ * - removing macros for defining types such as:
+ * "-typedef __u_char u_char;"
+ * - removing network related definitions.
+ * - adding stdint.h header.
+ * - removing headers that are no longer necessary.
+ */
+
 /* fixincludes should not add extern "C" to this file */
 /*
  * Rpc additions to <sys/types.h>
@@ -66,41 +74,6 @@ typedef unsigned long rpcport_t;
  */
 #define mem_free(ptr, bsize)	free(ptr)
 
-#ifndef makedev /* ie, we haven't already included it */
-#include <sys/types.h>
-#endif
-
-#if defined __APPLE_CC__ || defined __FreeBSD__
-# define __u_char_defined
-# define __daddr_t_defined
-#endif
-
-#ifndef __u_char_defined
-typedef __u_char u_char;
-typedef __u_short u_short;
-typedef __u_int u_int;
-typedef __u_long u_long;
-typedef __quad_t quad_t;
-typedef __u_quad_t u_quad_t;
-typedef __fsid_t fsid_t;
-# define __u_char_defined
-#endif
-#ifndef __daddr_t_defined
-typedef __daddr_t daddr_t;
-typedef __caddr_t caddr_t;
-# define __daddr_t_defined
-#endif
-
-#include <sys/time.h>
-#include <sys/param.h>
-
-#include <netinet/in.h>
-
-#ifndef INADDR_LOOPBACK
-#define       INADDR_LOOPBACK         (u_long)0x7F000001
-#endif
-#ifndef MAXHOSTNAMELEN
-#define        MAXHOSTNAMELEN  64
-#endif
+#include <stdint.h>
 
 #endif /* rpc/types.h */

src/external/rpc_xdr/xdr.cpp
@@ -43,19 +43,21 @@
  *  libc_hidden_nolink_sunrpc (xdr_free, GLIBC_2_0)
  *  #endif"
  * - using fputs() instead of __fxprintf()
+ * - changing u_int to unsigned int, u_long to unsigned long,
+ *   u_short to unsigned short and u_char to unsigned char.
+ * - changing caddr_t to char*.
+ * - removing functions xdr_hyper(), xdr_uhyper(),
+ *   xdr_longlong_t() and xdr_u_longlong_t().
  * - removing headers that are no longer necessary.
  */
 
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
-#include <libintl.h>
-#include <wchar.h>
 #include <stdint.h>
 
 #include "types.h"
 #include "xdr.h"
-// #include <shlib-compat.h>
 
 
 /*
@@ -63,7 +65,7 @@
  */
 #define XDR_FALSE	((long) 0)
 #define XDR_TRUE	((long) 1)
-#define LASTUNSIGNED	((u_int) 0-1)
+#define LASTUNSIGNED	((unsigned int) 0-1)
 
 /*
  * for unit alignment
@@ -132,7 +134,7 @@ xdr_int (XDR *xdrs, int *ip)
  * XDR unsigned integers
  */
 bool_t
-xdr_u_int (XDR *xdrs, u_int *up)
+xdr_u_int (XDR *xdrs, unsigned int *up)
 {
 #if UINT_MAX < ULONG_MAX
   long l;
@@ -140,7 +142,7 @@ xdr_u_int (XDR *xdrs, u_int *up)
   switch (xdrs->x_op)
     {
     case XDR_ENCODE:
-      l = (u_long) * up;
+      l = (unsigned long) * up;
       return XDR_PUTLONG (xdrs, &l);
 
     case XDR_DECODE:
@@ -148,14 +150,14 @@ xdr_u_int (XDR *xdrs, u_int *up)
 	{
 	  return FALSE;
 	}
-      *up = (u_int) (u_long) l;
+      *up = (unsigned int) (unsigned long) l;
       /* Fall through.  */
     case XDR_FREE:
       return TRUE;
     }
   return FALSE;
 #elif UINT_MAX == ULONG_MAX
-  return xdr_u_long (xdrs, (u_long *) up);
+  return xdr_u_long (xdrs, (unsigned long *) up);
 #elif UINT_MAX == USHRT_MAX
   return xdr_short (xdrs, (short *) up);
 #else
@@ -192,7 +194,7 @@ xdr_long (XDR *xdrs, long *lp)
  * compatibility. Instead xdr_u_int() should be used.
  */
 bool_t
-xdr_u_long (XDR *xdrs, u_long *ulp)
+xdr_u_long (XDR *xdrs, unsigned long *ulp)
 {
   switch (xdrs->x_op)
     {
@@ -208,7 +210,7 @@ xdr_u_long (XDR *xdrs, u_long *ulp)
       }
 
     case XDR_ENCODE:
-      if (sizeof (uint32_t) != sizeof (u_long)
+      if (sizeof (uint32_t) != sizeof (unsigned long)
 	  && (uint32_t) *ulp != *ulp)
 	return FALSE;
 
@@ -218,80 +220,6 @@ xdr_u_long (XDR *xdrs, u_long *ulp)
       return TRUE;
     }
   return FALSE;
-}
-
-/*
- * XDR hyper integers
- * same as xdr_u_hyper - open coded to save a proc call!
- */
-bool_t
-xdr_hyper (XDR *xdrs, quad_t *llp)
-{
-  long int t1, t2;
-
-  if (xdrs->x_op == XDR_ENCODE)
-    {
-      t1 = (long) ((*llp) >> 32);
-      t2 = (long) (*llp);
-      return (XDR_PUTLONG(xdrs, &t1) && XDR_PUTLONG(xdrs, &t2));
-    }
-
-  if (xdrs->x_op == XDR_DECODE)
-    {
-      if (!XDR_GETLONG(xdrs, &t1) || !XDR_GETLONG(xdrs, &t2))
-	return FALSE;
-      *llp = ((quad_t) t1) << 32;
-      *llp |= (uint32_t) t2;
-      return TRUE;
-    }
-
-  if (xdrs->x_op == XDR_FREE)
-    return TRUE;
-
-  return FALSE;
-}
-
-/*
- * XDR hyper integers
- * same as xdr_hyper - open coded to save a proc call!
- */
-bool_t
-xdr_u_hyper (XDR *xdrs, u_quad_t *ullp)
-{
-  long int t1, t2;
-
-  if (xdrs->x_op == XDR_ENCODE)
-    {
-      t1 = (unsigned long) ((*ullp) >> 32);
-      t2 = (unsigned long) (*ullp);
-      return (XDR_PUTLONG(xdrs, &t1) && XDR_PUTLONG(xdrs, &t2));
-    }
-
-  if (xdrs->x_op == XDR_DECODE)
-    {
-      if (!XDR_GETLONG(xdrs, &t1) || !XDR_GETLONG(xdrs, &t2))
-	return FALSE;
-      *ullp = ((u_quad_t) t1) << 32;
-      *ullp |= (uint32_t) t2;
-      return TRUE;
-    }
-
-  if (xdrs->x_op == XDR_FREE)
-    return TRUE;
-
-  return FALSE;
-}
-
-bool_t
-xdr_longlong_t (XDR *xdrs, quad_t *llp)
-{
-  return xdr_hyper (xdrs, llp);
-}
-
-bool_t
-xdr_u_longlong_t (XDR *xdrs, u_quad_t *ullp)
-{
-  return xdr_u_hyper (xdrs, ullp);
 }
 
 /*
@@ -326,14 +254,14 @@ xdr_short (XDR *xdrs, short *sp)
  * XDR unsigned short integers
  */
 bool_t
-xdr_u_short (XDR *xdrs, u_short *usp)
+xdr_u_short (XDR *xdrs, unsigned short *usp)
 {
   long l;
 
   switch (xdrs->x_op)
     {
     case XDR_ENCODE:
-      l = (u_long) * usp;
+      l = (unsigned long) * usp;
       return XDR_PUTLONG (xdrs, &l);
 
     case XDR_DECODE:
@@ -341,7 +269,7 @@ xdr_u_short (XDR *xdrs, u_short *usp)
 	{
 	  return FALSE;
 	}
-      *usp = (u_short) (u_long) l;
+      *usp = (unsigned short) (unsigned long) l;
       return TRUE;
 
     case XDR_FREE:
@@ -372,9 +300,9 @@ xdr_char (XDR *xdrs, char *cp)
  * XDR an unsigned char
  */
 bool_t
-xdr_u_char (XDR *xdrs, u_char *cp)
+xdr_u_char (XDR *xdrs, unsigned char *cp)
 {
-  u_int u;
+  unsigned int u;
 
   u = (*cp);
   if (!xdr_u_int (xdrs, &u))
@@ -470,9 +398,9 @@ xdr_enum (XDR *xdrs, enum_t *ep)
  * cp points to the opaque object and cnt gives the byte length.
  */
 bool_t
-xdr_opaque (XDR *xdrs, caddr_t cp, u_int cnt)
+xdr_opaque (XDR *xdrs, char* cp, unsigned int cnt)
 {
-  u_int rndup;
+  unsigned int rndup;
   static char crud[BYTES_PER_XDR_UNIT];
 
   /*
@@ -497,7 +425,7 @@ xdr_opaque (XDR *xdrs, caddr_t cp, u_int cnt)
 	}
       if (rndup == 0)
 	return TRUE;
-      return XDR_GETBYTES (xdrs, (caddr_t)crud, rndup);
+      return XDR_GETBYTES (xdrs, (char *)crud, rndup);
 
     case XDR_ENCODE:
       if (!XDR_PUTBYTES (xdrs, cp, cnt))
@@ -520,10 +448,10 @@ xdr_opaque (XDR *xdrs, caddr_t cp, u_int cnt)
  * If *cpp is NULL maxsize bytes are allocated
  */
 bool_t
-xdr_bytes (XDR *xdrs, char **cpp, u_int *sizep, u_int maxsize)
+xdr_bytes (XDR *xdrs, char **cpp, unsigned int *sizep, unsigned int maxsize)
 {
   char *sp = *cpp;	/* sp is the actual string pointer */
-  u_int nodesize;
+  unsigned int nodesize;
 
   /*
    * first deal with the length since xdr bytes are counted
@@ -649,13 +577,13 @@ xdr_union (XDR *xdrs,
  * of the string as specified by a protocol.
  */
 bool_t
-xdr_string (XDR *xdrs, char **cpp, u_int maxsize)
+xdr_string (XDR *xdrs, char **cpp, unsigned int maxsize)
 {
   char *sp = *cpp;	/* sp is the actual string pointer */
   /* Initialize to silence the compiler.  It is not really needed because SIZE
      never actually gets used without being initialized.  */
-  u_int size = 0;
-  u_int nodesize;
+  unsigned int size = 0;
+  unsigned int nodesize;
 
   /*
    * first deal with the length since xdr strings are counted-strings

src/external/rpc_xdr/xdr.h
@@ -32,20 +32,24 @@
  */
 
 /* This file has been modified in the GROMACS distribution by, e.g.:
- * - Removing the __THROW macro attribute from declarations.
+ * - removing the __THROW macro attribute from declarations.
+ * - removing the __BEGIN_DECLS and __END_DECLS macros.
+ * - changing "#define _RPC_XDR_H 1" to "#define _RPC_XDR_H"
+ * - changing u_int to unsigned int, u_long to unsigned long,
+ *   u_short to unsigned short and u_char to unsigned char.
+ * - changing caddr_t to char*.
+ * - removing functions xdr_hyper(), xdr_uhyper(),
+ *   xdr_longlong_t() and xdr_u_longlong_t().
+ * - removing headers that are no longer necessary.
  */
 
 #ifndef _RPC_XDR_H
-#define _RPC_XDR_H 1
+#define _RPC_XDR_H
 
-#include <features.h>
-#include <sys/types.h>
 #include "types.h"
 
 /* We need FILE.  */
 #include <stdio.h>
-
-__BEGIN_DECLS
 
 /*
  * XDR provides a conventional way for converting between C data
@@ -120,15 +124,15 @@ struct XDR
 	/* get a long from underlying stream */
 	bool_t (*x_putlong) (XDR *__xdrs, const long *__lp);
 	/* put a long to " */
-	bool_t (*x_getbytes) (XDR *__xdrs, caddr_t __addr, u_int __len);
+	bool_t (*x_getbytes) (XDR *__xdrs, char* __addr, unsigned int __len);
 	/* get some bytes from " */
-	bool_t (*x_putbytes) (XDR *__xdrs, const char *__addr, u_int __len);
+	bool_t (*x_putbytes) (XDR *__xdrs, const char *__addr, unsigned int __len);
 	/* put some bytes to " */
-	u_int (*x_getpostn) (const XDR *__xdrs);
+	unsigned int (*x_getpostn) (const XDR *__xdrs);
 	/* returns bytes off from beginning */
-	bool_t (*x_setpostn) (XDR *__xdrs, u_int __pos);
+	bool_t (*x_setpostn) (XDR *__xdrs, unsigned int __pos);
 	/* lets you reposition the stream */
-	int32_t *(*x_inline) (XDR *__xdrs, u_int __len);
+	int32_t *(*x_inline) (XDR *__xdrs, unsigned int __len);
 	/* buf quick ptr to buffered data */
 	void (*x_destroy) (XDR *__xdrs);
 	/* free privates of this xdr_stream */
@@ -138,10 +142,10 @@ struct XDR
 	/* put a int to " */
       }
      *x_ops;
-    caddr_t x_public;		/* users' data */
-    caddr_t x_private;		/* pointer to private data */
-    caddr_t x_base;		/* private used for position info */
-    u_int x_handy;		/* extra private word */
+    char* x_public;		/* users' data */
+    char* x_private;		/* pointer to private data */
+    char* x_base;		/* private used for position info */
+    unsigned int x_handy;		/* extra private word */
   };
 
 /*
@@ -162,9 +166,9 @@ typedef bool_t (*xdrproc_t) (XDR *, void *,...);
  * XDR          *xdrs;
  * int32_t      *int32p;
  * long         *longp;
- * caddr_t       addr;
- * u_int         len;
- * u_int         pos;
+ * char*         addr;
+ * unsigned int  len;
+ * unsigned int  pos;
  */
 #define XDR_GETINT32(xdrs, int32p)                      \
 	(*(xdrs)->x_ops->x_getint32)(xdrs, int32p)
@@ -266,14 +270,14 @@ struct xdr_discrim
  */
 #define IXDR_GET_LONG(buf) ((long)IXDR_GET_U_INT32(buf))
 #define IXDR_PUT_LONG(buf, v) ((long)IXDR_PUT_INT32(buf, (long)(v)))
-#define IXDR_GET_U_LONG(buf)	      ((u_long)IXDR_GET_LONG(buf))
+#define IXDR_GET_U_LONG(buf)	      ((unsigned long)IXDR_GET_LONG(buf))
 #define IXDR_PUT_U_LONG(buf, v)	      IXDR_PUT_LONG(buf, (long)(v))
 
 
 #define IXDR_GET_BOOL(buf)            ((bool_t)IXDR_GET_LONG(buf))
 #define IXDR_GET_ENUM(buf, t)         ((t)IXDR_GET_LONG(buf))
 #define IXDR_GET_SHORT(buf)           ((short)IXDR_GET_LONG(buf))
-#define IXDR_GET_U_SHORT(buf)         ((u_short)IXDR_GET_LONG(buf))
+#define IXDR_GET_U_SHORT(buf)         ((unsigned short)IXDR_GET_LONG(buf))
 
 #define IXDR_PUT_BOOL(buf, v)         IXDR_PUT_LONG(buf, (long)(v))
 #define IXDR_PUT_ENUM(buf, v)         IXDR_PUT_LONG(buf, (long)(v))
@@ -288,15 +292,11 @@ struct xdr_discrim
  */
 extern bool_t xdr_void (void);
 extern bool_t xdr_short (XDR *__xdrs, short *__sp);
-extern bool_t xdr_u_short (XDR *__xdrs, u_short *__usp);
+extern bool_t xdr_u_short (XDR *__xdrs, unsigned short *__usp);
 extern bool_t xdr_int (XDR *__xdrs, int *__ip);
-extern bool_t xdr_u_int (XDR *__xdrs, u_int *__up);
+extern bool_t xdr_u_int (XDR *__xdrs, unsigned int *__up);
 extern bool_t xdr_long (XDR *__xdrs, long *__lp);
-extern bool_t xdr_u_long (XDR *__xdrs, u_long *__ulp);
-extern bool_t xdr_hyper (XDR *__xdrs, quad_t *__llp);
-extern bool_t xdr_u_hyper (XDR *__xdrs, u_quad_t *__ullp);
-extern bool_t xdr_longlong_t (XDR *__xdrs, quad_t *__llp);
-extern bool_t xdr_u_longlong_t (XDR *__xdrs, u_quad_t *__ullp);
+extern bool_t xdr_u_long (XDR *__xdrs, unsigned long *__ulp);
 extern bool_t xdr_int8_t (XDR *__xdrs, int8_t *__ip);
 extern bool_t xdr_uint8_t (XDR *__xdrs, uint8_t *__up);
 extern bool_t xdr_int16_t (XDR *__xdrs, int16_t *__ip);
@@ -305,32 +305,30 @@ extern bool_t xdr_int32_t (XDR *__xdrs, int32_t *__ip);
 extern bool_t xdr_uint32_t (XDR *__xdrs, uint32_t *__up);
 extern bool_t xdr_int64_t (XDR *__xdrs, int64_t *__ip);
 extern bool_t xdr_uint64_t (XDR *__xdrs, uint64_t *__up);
-extern bool_t xdr_quad_t (XDR *__xdrs, quad_t *__ip);
-extern bool_t xdr_u_quad_t (XDR *__xdrs, u_quad_t *__up);
 extern bool_t xdr_bool (XDR *__xdrs, bool_t *__bp);
 extern bool_t xdr_enum (XDR *__xdrs, enum_t *__ep);
-extern bool_t xdr_array (XDR * _xdrs, caddr_t *__addrp, u_int *__sizep,
-			 u_int __maxsize, u_int __elsize, xdrproc_t __elproc)
+extern bool_t xdr_array (XDR * _xdrs, char** __addrp, unsigned int *__sizep,
+			 unsigned int __maxsize, unsigned int __elsize, xdrproc_t __elproc)
     ;
-extern bool_t xdr_bytes (XDR *__xdrs, char **__cpp, u_int *__sizep,
-			 u_int __maxsize);
-extern bool_t xdr_opaque (XDR *__xdrs, caddr_t __cp, u_int __cnt);
-extern bool_t xdr_string (XDR *__xdrs, char **__cpp, u_int __maxsize);
+extern bool_t xdr_bytes (XDR *__xdrs, char **__cpp, unsigned int *__sizep,
+			 unsigned int __maxsize);
+extern bool_t xdr_opaque (XDR *__xdrs, char* __cp, unsigned int __cnt);
+extern bool_t xdr_string (XDR *__xdrs, char **__cpp, unsigned int __maxsize);
 extern bool_t xdr_union (XDR *__xdrs, enum_t *__dscmp, char *__unp,
 			 const struct xdr_discrim *__choices,
 			 xdrproc_t __dfault);
 extern bool_t xdr_char (XDR *__xdrs, char *__cp);
-extern bool_t xdr_u_char (XDR *__xdrs, u_char *__cp);
-extern bool_t xdr_vector (XDR *__xdrs, char *__basep, u_int __nelem,
-			  u_int __elemsize, xdrproc_t __xdr_elem);
+extern bool_t xdr_u_char (XDR *__xdrs, unsigned char *__cp);
+extern bool_t xdr_vector (XDR *__xdrs, char *__basep, unsigned int __nelem,
+			  unsigned int __elemsize, xdrproc_t __xdr_elem);
 extern bool_t xdr_float (XDR *__xdrs, float *__fp);
 extern bool_t xdr_double (XDR *__xdrs, double *__dp);
-extern bool_t xdr_reference (XDR *__xdrs, caddr_t *__xpp, u_int __size,
+extern bool_t xdr_reference (XDR *__xdrs, char** __xpp, unsigned int __size,
 			     xdrproc_t __proc);
 extern bool_t xdr_pointer (XDR *__xdrs, char **__objpp,
-			   u_int __obj_size, xdrproc_t __xdr_obj);
+			   unsigned int __obj_size, xdrproc_t __xdr_obj);
 extern bool_t xdr_wrapstring (XDR *__xdrs, char **__cpp);
-extern u_long xdr_sizeof (xdrproc_t, void *);
+extern unsigned long xdr_sizeof (xdrproc_t, void *);
 
 /*
  * Common opaque bytes objects used by many rpc protocols;
@@ -339,7 +337,7 @@ extern u_long xdr_sizeof (xdrproc_t, void *);
 #define MAX_NETOBJ_SZ 1024
 struct netobj
 {
-  u_int n_len;
+  unsigned int n_len;
   char *n_bytes;
 };
 typedef struct netobj netobj;
@@ -351,16 +349,16 @@ extern bool_t xdr_netobj (XDR *__xdrs, struct netobj *__np);
  */
 
 /* XDR using memory buffers */
-extern void xdrmem_create (XDR *__xdrs, const caddr_t __addr,
-			   u_int __size, enum xdr_op __xop);
+extern void xdrmem_create (XDR *__xdrs, const char* __addr,
+			   unsigned int __size, enum xdr_op __xop);
 
 /* XDR using stdio library */
 extern void xdrstdio_create (XDR *__xdrs, FILE *__file, enum xdr_op __xop)
     ;
 
 /* XDR pseudo records for tcp */
-extern void xdrrec_create (XDR *__xdrs, u_int __sendsize,
-			   u_int __recvsize, caddr_t __tcp_handle,
+extern void xdrrec_create (XDR *__xdrs, unsigned int __sendsize,
+			   unsigned int __recvsize, char* __tcp_handle,
 			   int (*__readit) (char *, char *, int),
 			   int (*__writeit) (char *, char *, int));
 
@@ -375,7 +373,5 @@ extern bool_t xdrrec_eof (XDR *__xdrs);
 
 /* free memory buffers for xdr */
 extern void xdr_free (xdrproc_t __proc, char *__objp);
-
-__END_DECLS
 
 #endif /* rpc/xdr.h */

src/external/rpc_xdr/xdr_array.cpp
@@ -43,6 +43,9 @@
  *  #endif"
  * - using fputs() instead of __fxprintf()
  * - using explicit cast to (char *) with calloc.
+ * - changing u_int to unsigned int, u_long to unsigned long,
+ *   u_short to unsigned short and u_char to unsigned char.
+ * - changing caddr_t to char*.
  * - removing headers that are no longer necessary.
  */
 
@@ -50,11 +53,9 @@
 #include <string.h>
 #include "types.h"
 #include "xdr.h"
-#include <libintl.h>
 #include <limits.h>
-#include <wchar.h>
 
-#define LASTUNSIGNED	((u_int)0-1)
+#define LASTUNSIGNED	((unsigned int)0-1)
 
 
 /*
@@ -67,19 +68,19 @@
 bool_t
 xdr_array (XDR *xdrs,
 	   /* array pointer */
-	   caddr_t *addrp,
+	   char** addrp,
 	   /* number of elements */
-	   u_int *sizep,
+	   unsigned int *sizep,
 	   /* max numberof elements */
-	   u_int maxsize,
+	   unsigned int maxsize,
 	   /* size in bytes of each element */
-	   u_int elsize,
+	   unsigned int elsize,
 	   /* xdr routine to handle each element */
 	   xdrproc_t elproc)
 {
-  u_int i;
-  caddr_t target = *addrp;
-  u_int c;		/* the actual element count */
+  unsigned int i;
+  char* target = *addrp;
+  unsigned int c;		/* the actual element count */
   bool_t stat = TRUE;
 
   /* like strings, arrays are really counted arrays */
@@ -152,10 +153,10 @@ xdr_array (XDR *xdrs,
  * > xdr_elem: routine to XDR each element
  */
 bool_t
-xdr_vector (XDR *xdrs, char *basep, u_int nelem, u_int elemsize,
+xdr_vector (XDR *xdrs, char *basep, unsigned int nelem, unsigned int elemsize,
 	    xdrproc_t xdr_elem)
 {
-  u_int i;
+  unsigned int i;
   char *elptr;
 
   elptr = basep;

src/external/rpc_xdr/xdr_float.cpp
@@ -38,21 +38,16 @@
 /* This file has been modified in the GROMACS distribution by, e.g.:
  * - removing macros such as:
  *  libc_hidden_nolink_sunrpc (xdr_float, GLIBC_2_0)
+ * - instead of using non-portable header for endianness
+ *   detect the endianness by calculating the double precision
+ *   word order, using an algorithm from GROMACS.
  * - removing headers that are no longer necessary.
  */
 
 #include <stdio.h>
-#include <endian.h>
 
 #include "types.h"
 #include "xdr.h"
-
-/*
- * NB: Not portable.
- * This routine works on Suns (Sky / 68000's) and Vaxen.
- */
-
-#define LSW	(__FLOAT_WORD_ORDER == __BIG_ENDIAN)
 
 #ifdef vax
 
@@ -212,6 +207,56 @@ xdr_double (XDR *xdrs, double *dp)
 	register struct dbl_limits *lim;
 	int i;
 #endif
+
+    /* Windows and some other systems don't define double-precision
+     * word order in the header files, so unfortunately we have
+     * to calculate it!
+     *
+     * For thread safety, we calculate it every time: locking this would
+     * be more expensive.
+     *
+     * Copyright The GROMACS Authors
+     */
+    int LSW = -1;          /* Least significant fp word */
+
+
+    int*        ip;
+    int32_t     tmp[2];
+
+    if (LSW < 0)
+    {
+        double x = 0.987654321; /* Just a number */
+
+        /* Possible representations in IEEE double precision:
+         * (S=small endian, B=big endian)
+         *
+         * Byte order, Word order, Hex
+         *     S           S       b8 56 0e 3c dd 9a ef 3f
+         *     B           S       3c 0e 56 b8 3f ef 9a dd
+         *     S           B       dd 9a ef 3f b8 56 0e 3c
+         *     B           B       3f ef 9a dd 3c 0e 56 b8
+         */
+
+        unsigned char ix = *(reinterpret_cast<char*>(&x));
+
+        if (ix == 0xdd || ix == 0x3f)
+        {
+            LSW = 1; /* Big endian word order */
+        }
+        else if (ix == 0xb8 || ix == 0x3c)
+        {
+            LSW = 0; /* Small endian word order */
+        }
+        else /* Catch strange errors */
+        {
+            printf("Error when detecting floating-point word order.\n"
+                   "Do you have a non-IEEE system?\n"
+                   "If possible, use the XDR libraries provided with your system,\n"
+                   "instead of the GROMACS fallback XDR source.\n");
+            exit(0);
+        }
+    }
+
 
 	switch (xdrs->x_op) {
 

src/external/rpc_xdr/xdr_stdio.cpp
@@ -44,10 +44,15 @@
  *  #endif"
  * - removing macros for fflush, fread, ftell and fwrite.
  * - removing an explicit cast to (struct xdr_ops *).
+ * - changing u_int to unsigned int, u_long to unsigned long,
+ *   u_short to unsigned short and u_char to unsigned char.
+ * - changing caddr_t to char*.
  * - Changing
  * "static const struct xdr_ops xdrstdio_ops = ..."
  * to
  * "static struct XDR::xdr_ops xdrstdio_ops = ..."
+ * - Adding xdr_swapbytes, xdr_htonl and xdr_ntohl functions
+ *   from GROMACS.
  * - removing headers that are no longer necessary.
  */
 
@@ -57,11 +62,11 @@
 
 static bool_t xdrstdio_getlong (XDR *, long *);
 static bool_t xdrstdio_putlong (XDR *, const long *);
-static bool_t xdrstdio_getbytes (XDR *, caddr_t, u_int);
-static bool_t xdrstdio_putbytes (XDR *, const char *, u_int);
-static u_int xdrstdio_getpos (const XDR *);
-static bool_t xdrstdio_setpos (XDR *, u_int);
-static int32_t *xdrstdio_inline (XDR *, u_int);
+static bool_t xdrstdio_getbytes (XDR *, char *, unsigned int);
+static bool_t xdrstdio_putbytes (XDR *, const char *, unsigned int);
+static unsigned int xdrstdio_getpos (const XDR *);
+static bool_t xdrstdio_setpos (XDR *, unsigned int);
+static int32_t *xdrstdio_inline (XDR *, unsigned int);
 static void xdrstdio_destroy (XDR *);
 static bool_t xdrstdio_getint32 (XDR *, int32_t *);
 static bool_t xdrstdio_putint32 (XDR *, const int32_t *);
@@ -83,6 +88,54 @@ static struct XDR::xdr_ops xdrstdio_ops =
   xdrstdio_putint32		/* serialize a int */
 };
 
+/* Copyright The GROMACS Authors */
+static uint32_t xdr_swapbytes(uint32_t x)
+{
+    uint32_t y;
+    int          i;
+    char*        px = reinterpret_cast<char*>(&x);
+    char*        py = reinterpret_cast<char*>(&y);
+
+    for (i = 0; i < 4; i++)
+    {
+        py[i] = px[3 - i];
+    }
+
+    return y;
+}
+
+/* Copyright The GROMACS Authors */
+static uint32_t xdr_htonl(uint32_t x)
+{
+    short s = 0x0F00;
+    if (*(reinterpret_cast<char*>(&s)) == static_cast<char>(0x0F))
+    {
+        /* bigendian, do nothing */
+        return x;
+    }
+    else
+    {
+        /* smallendian,swap bytes */
+        return xdr_swapbytes(x);
+    }
+}
+
+/* Copyright The GROMACS Authors */
+static uint32_t xdr_ntohl(uint32_t x)
+{
+    short s = 0x0F00;
+    if (*(reinterpret_cast<char*>(&s)) == static_cast<char>(0x0F))
+    {
+        /* bigendian, do nothing */
+        return x;
+    }
+    else
+    {
+        /* smallendian, swap bytes */
+        return xdr_swapbytes(x);
+    }
+}
+
 /*
  * Initialize a stdio xdr stream.
  * Sets the xdr stream handle xdrs for use on the stream file.
@@ -95,7 +148,7 @@ xdrstdio_create (XDR *xdrs, FILE *file, enum xdr_op op)
   /* We have to add the const since the `struct xdr_ops' in `struct XDR'
      is not `const'.  */
   xdrs->x_ops = &xdrstdio_ops;
-  xdrs->x_private = (caddr_t) file;
+  xdrs->x_private = (char *) file;
   xdrs->x_handy = 0;
   xdrs->x_base = 0;
 }
@@ -116,24 +169,24 @@ xdrstdio_getlong (XDR *xdrs, long *lp)
 {
   uint32_t mycopy;
 
-  if (fread ((caddr_t) &mycopy, 4, 1, (FILE *) xdrs->x_private) != 1)
+  if (fread ((char *) &mycopy, 4, 1, (FILE *) xdrs->x_private) != 1)
     return FALSE;
-  *lp = (long) ntohl (mycopy);
+  *lp = (long) xdr_ntohl (mycopy);
   return TRUE;
 }
 
 static bool_t
 xdrstdio_putlong (XDR *xdrs, const long *lp)
 {
-  int32_t mycopy = htonl ((uint32_t) *lp);
+  int32_t mycopy = xdr_htonl ((uint32_t) *lp);
 
-  if (fwrite ((caddr_t) &mycopy, 4, 1, (FILE *) xdrs->x_private) != 1)
+  if (fwrite ((char *) &mycopy, 4, 1, (FILE *) xdrs->x_private) != 1)
     return FALSE;
   return TRUE;
 }
 
 static bool_t
-xdrstdio_getbytes (XDR *xdrs, const caddr_t addr, u_int len)
+xdrstdio_getbytes (XDR *xdrs, char * addr, unsigned int len)
 {
   if ((len != 0) && (fread (addr, (int) len, 1,
 			    (FILE *) xdrs->x_private) != 1))
@@ -142,7 +195,7 @@ xdrstdio_getbytes (XDR *xdrs, const caddr_t addr, u_int len)
 }
 
 static bool_t
-xdrstdio_putbytes (XDR *xdrs, const char *addr, u_int len)
+xdrstdio_putbytes (XDR *xdrs, const char *addr, unsigned int len)
 {
   if ((len != 0) && (fwrite (addr, (int) len, 1,
 			     (FILE *) xdrs->x_private) != 1))
@@ -150,20 +203,20 @@ xdrstdio_putbytes (XDR *xdrs, const char *addr, u_int len)
   return TRUE;
 }
 
-static u_int
+static unsigned int
 xdrstdio_getpos (const XDR *xdrs)
 {
-  return (u_int) ftell ((FILE *) xdrs->x_private);
+  return (unsigned int) ftell ((FILE *) xdrs->x_private);
 }
 
 static bool_t
-xdrstdio_setpos (XDR *xdrs, u_int pos)
+xdrstdio_setpos (XDR *xdrs, unsigned int pos)
 {
   return fseek ((FILE *) xdrs->x_private, (long) pos, 0) < 0 ? FALSE : TRUE;
 }
 
 static int32_t *
-xdrstdio_inline (XDR *xdrs, u_int len)
+xdrstdio_inline (XDR *xdrs, unsigned int len)
 {
   /*
    * Must do some work to implement this: must insure
@@ -182,19 +235,19 @@ xdrstdio_getint32 (XDR *xdrs, int32_t *ip)
 {
   int32_t mycopy;
 
-  if (fread ((caddr_t) &mycopy, 4, 1, (FILE *) xdrs->x_private) != 1)
+  if (fread ((char *) &mycopy, 4, 1, (FILE *) xdrs->x_private) != 1)
     return FALSE;
-  *ip = ntohl (mycopy);
+  *ip = xdr_ntohl (mycopy);
   return TRUE;
 }
 
 static bool_t
 xdrstdio_putint32 (XDR *xdrs, const int32_t *ip)
 {
-  int32_t mycopy = htonl (*ip);
+  int32_t mycopy = xdr_htonl (*ip);
 
   ip = &mycopy;
-  if (fwrite ((caddr_t) ip, 4, 1, (FILE *) xdrs->x_private) != 1)
+  if (fwrite ((char *) ip, 4, 1, (FILE *) xdrs->x_private) != 1)
     return FALSE;
   return TRUE;
 }