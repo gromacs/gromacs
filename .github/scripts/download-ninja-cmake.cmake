2447fd95b93d00fd8472a52a6fada09eece2a42c
Andrey Alekseenko <al42and@gmail.com>
2024-08-01 12:01:11 +0000

    Update GitHub CI configs
    Bootstrapping:
    - Bump CMake version to 3.28.0 from 3.18.4
    - Bump CCache and Ninja versions to latest
    - Update CCache download logic to use the official releases
    - Move CCache download logic together with CMake and Ninja
    - Reduce verbosity of file download and extraction
    
    macOS:
    - Bump macOS 11 GCC to use macOS-latest (14 on ARM at the moment) with GCC 14
    
    Linux ARM:
    - Drop Ubuntu AArch64 job relying on QEMU, as `macos-latest` are using ARM (M1)
    
    Refs #5014
    Closes #4646
    
    Closes #4646

--- .github/scripts/download-ninja-cmake.cmake
+++ .github/scripts/download-ninja-cmake.cmake
@@ -33,6 +33,7 @@
 
 set(cmake_version $ENV{CMAKE_VERSION})
 set(ninja_version $ENV{NINJA_VERSION})
+set(ccache_version $ENV{CCACHE_VERSION})
 
 message(STATUS "Using host CMake version: ${CMAKE_VERSION}")
 message(STATUS "Using RUNNER_OS: $ENV{RUNNER_OS}")
@@ -40,25 +41,29 @@ message(STATUS "Using RUNNER_ARCH: $ENV{RUNNER_ARCH}")
 
 if ("$ENV{RUNNER_OS}" STREQUAL "Windows")
     set(ninja_suffix "win.zip")
-    set(cmake_suffix "win64-x64.zip")
-    set(cmake_dir "cmake-${cmake_version}-win64-x64/bin")
+    set(cmake_suffix "windows-x86_64.zip")
+    set(cmake_dir "cmake-${cmake_version}-windows-x86_64/bin")
+    set(ccache_suffix "windows-x86_64.zip")
+    set(ccache_dir "ccache-${ccache_version}-windows-x86_64")
 elseif ("$ENV{RUNNER_OS}" STREQUAL "macOS")
     set(ninja_suffix "mac.zip")
-    set(cmake_suffix "Darwin-x86_64.tar.gz")
-    set(cmake_dir "cmake-${cmake_version}-Darwin-x86_64/CMake.app/Contents/bin")
-elseif ("$ENV{RUNNER_OS}" STREQUAL "Linux" AND "$ENV{RUNNER_ARCH}" STREQUAL "ARM64")
-    set(ninja_suffix "linux.zip")
-    set(cmake_suffix "linux-aarch64.tar.gz")
-    set(cmake_dir "cmake-${cmake_version}-linux-aarch64/bin")
+    set(cmake_suffix "macos-universal.tar.gz")
+    set(cmake_dir "cmake-${cmake_version}-macos-universal/CMake.app/Contents/bin")
+    set(ccache_suffix "darwin.tar.gz")
+    set(ccache_dir "ccache-${ccache_version}-darwin")
 endif()
 
 set(ninja_url "https://github.com/ninja-build/ninja/releases/download/v${ninja_version}/ninja-${ninja_suffix}")
-file(DOWNLOAD "${ninja_url}" ./ninja.zip SHOW_PROGRESS)
+file(DOWNLOAD "${ninja_url}" ./ninja.zip)
 execute_process(COMMAND ${CMAKE_COMMAND} -E tar xvf ./ninja.zip)
 
 set(cmake_url "https://github.com/Kitware/CMake/releases/download/v${cmake_version}/cmake-${cmake_version}-${cmake_suffix}")
-file(DOWNLOAD "${cmake_url}" ./cmake.zip SHOW_PROGRESS)
-execute_process(COMMAND ${CMAKE_COMMAND} -E tar xvf ./cmake.zip)
+file(DOWNLOAD "${cmake_url}" ./cmake.zip)
+execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf ./cmake.zip)
+
+set(ccache_url "https://github.com/ccache/ccache/releases/download/v${ccache_version}/ccache-${ccache_version}-${ccache_suffix}")
+file(DOWNLOAD "${ccache_url}" ./ccache.zip)
+execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf ./ccache.zip)
 
 # Add to PATH environment variable
 file(TO_CMAKE_PATH "$ENV{GITHUB_WORKSPACE}/${cmake_dir}" cmake_dir)
@@ -69,8 +74,12 @@ endif()
 file(APPEND "$ENV{GITHUB_PATH}" "$ENV{GITHUB_WORKSPACE}${path_separator}${cmake_dir}")
 
 if (NOT "$ENV{RUNNER_OS}" STREQUAL "Windows")
+    file(RENAME "${ccache_dir}/ccache" "./ccache")
     execute_process(
     COMMAND chmod +x ninja
     COMMAND chmod +x ${cmake_dir}/cmake
+    COMMAND chmod +x ccache
     )
+else()
+    file(RENAME "${ccache_dir}/ccache.exe" "./ccache.exe")
 endif()
