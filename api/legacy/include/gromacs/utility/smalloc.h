4170b99a6a728f6e0abda72be37d5be2044db7d3
Vedran Miletić <vedran.miletic@mpcdf.mpg.de>
2024-08-14 22:14:17 +0200

    Replaced deprecated std::is_pod with std::is_standard_layout
    Fixes #5097

--- api/legacy/include/gromacs/utility/smalloc.h
+++ api/legacy/include/gromacs/utility/smalloc.h
@@ -197,8 +197,8 @@ void save_free_aligned(const char* name, const char* file, int line, void* ptr);
 template<typename T>
 static inline void gmx_snew_impl(const char* name, const char* file, int line, T*& ptr, size_t nelem)
 {
-    // TODO: Use std::is_pod_v when CUDA 11 is a requirement.
-    static_assert(std::is_pod<T>::value, "snew() called on C++ type");
+    // TODO: Use std::is_standard_layout_v when CUDA 11 is a requirement.
+    static_assert(std::is_standard_layout<T>::value, "snew() called on C++ type");
     // NOLINTNEXTLINE bugprone-sizeof-expression
     ptr = static_cast<T*>(save_calloc(name, file, line, nelem, sizeof(T)));
 }
@@ -206,8 +206,8 @@ static inline void gmx_snew_impl(const char* name, const char* file, int line, T
 template<typename T>
 static inline void gmx_srenew_impl(const char* name, const char* file, int line, T*& ptr, size_t nelem)
 {
-    // TODO: Use std::is_pod_v when CUDA 11 is a requirement.
-    static_assert(std::is_pod<T>::value, "srenew() called on C++ type");
+    // TODO: Use std::is_standard_layout_v when CUDA 11 is a requirement.
+    static_assert(std::is_standard_layout<T>::value, "srenew() called on C++ type");
     // NOLINTNEXTLINE bugprone-sizeof-expression
     ptr = static_cast<T*>(save_realloc(name, file, line, ptr, nelem, sizeof(T)));
 }
@@ -215,8 +215,8 @@ static inline void gmx_srenew_impl(const char* name, const char* file, int line,
 template<typename T>
 static inline void gmx_smalloc_impl(const char* name, const char* file, int line, T*& ptr, size_t size)
 {
-    // TODO: Use std::is_pod_v when CUDA 11 is a requirement.
-    static_assert(std::is_pod<T>::value, "smalloc() called on C++ type");
+    // TODO: Use std::is_standard_layout_v when CUDA 11 is a requirement.
+    static_assert(std::is_standard_layout<T>::value, "smalloc() called on C++ type");
     ptr = static_cast<T*>(save_malloc(name, file, line, size));
 }
 /** C++ helper for snew_aligned(). */
@@ -224,8 +224,8 @@ template<typename T>
 static inline void
 gmx_snew_aligned_impl(const char* name, const char* file, int line, T*& ptr, size_t nelem, size_t alignment)
 {
-    // TODO: Use std::is_pod_v when CUDA 11 is a requirement.
-    static_assert(std::is_pod<T>::value, "snew_aligned() called on C++ type");
+    // TODO: Use std::is_standard_layout_v when CUDA 11 is a requirement.
+    static_assert(std::is_standard_layout<T>::value, "snew_aligned() called on C++ type");
     ptr = static_cast<T*>(save_calloc_aligned(name, file, line, nelem, sizeof(T), alignment));
 }
 /** C++ helper for sfree(). */
@@ -233,7 +233,8 @@ template<typename T>
 static inline void gmx_sfree_impl(const char* name, const char* file, int line, T* ptr)
 {
     // TODO: Use std::is_pod_v and std::is_void_v when CUDA 11 is a requirement.
-    static_assert(std::is_pod<T>::value || std::is_void<T>::value, "sfree() called on C++ type");
+    static_assert(std::is_standard_layout<T>::value || std::is_void<T>::value,
+                  "sfree() called on C++ type");
     save_free(name, file, line, ptr);
 }
 /** C++ helper for sfree_aligned(). */
@@ -241,7 +242,7 @@ template<typename T>
 static inline void gmx_sfree_aligned_impl(const char* name, const char* file, int line, T* ptr)
 {
     // TODO: Use std::is_pod_v and std::is_void_v when CUDA 11 is a requirement.
-    static_assert(std::is_pod<T>::value || std::is_void<T>::value,
+    static_assert(std::is_standard_layout<T>::value || std::is_void<T>::value,
                   "sfree_aligned() called on C++ type");
     save_free_aligned(name, file, line, ptr);
 }
