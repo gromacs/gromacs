gromacs/gromacs

commit 1503b6e819fe6523fdd87a667a4292dc2c32400d

Refactor DD zones code into DomdecZones class
Changes the gmx_domdec_zones_t struct into a proper class and moves
its declaration and implementation into separate files.
The zone_ncg1 from gmx_domdec_comm_t is also moved into this class.

This change is only refactoring.

src/gromacs/domdec/domdec_zones.h
@@ -1,0 +1,193 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright 2024- The GROMACS Authors
+ * and the project initiators Erik Lindahl, Berk Hess and David van der Spoel.
+ * Consult the AUTHORS/COPYING files and https://www.gromacs.org for details.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * https://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at https://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out https://www.gromacs.org.
+ */
+/*! \libinternal \file
+ *
+ * \brief Declares the DomdecZones class and helper structs
+ *
+ * A DomdecZones object is (or should be) the only data structure that
+ * is shared from the domain decomposition module to the Nbnxm module.
+ *
+ * \inlibraryapi
+ * \ingroup module_domdec
+ *
+ * \author Berk Hess <hess@kth.se>
+ */
+
+#ifndef GMX_DOMDEC_ZONES_H
+#define GMX_DOMDEC_ZONES_H
+
+#include <array>
+
+#include "gromacs/math/vectypes.h"
+#include "gromacs/utility/gmxassert.h"
+#include "gromacs/utility/range.h"
+
+struct gmx_ddbox_t;
+struct gmx_domdec_t;
+
+namespace gmx
+{
+
+template<typename>
+class ArrayRef;
+
+//! The maximum possible number of zones, 2 along each dimension in the eighth shell method
+static constexpr int sc_maxNumZones = 8;
+//! The maximum possible number of i-zones, half of the zones are needed to cover all pairs
+static constexpr int sc_maxNumIZones = sc_maxNumZones / 2;
+
+/*! \internal
+ * \brief Triclinic corners and the Cartesian bounding box
+ *
+ * Note that at decomposition steps atoms can be, slightly, ouf of these bounds
+ * when update groups are used. At other steps somes atoms can (and usually will)
+ * move/diffuse outside of these bounds.
+ */
+struct gmx_domdec_zone_size_t
+{
+    /* Zone lower corner in triclinic coordinates         */
+    gmx::RVec x0 = { 0, 0, 0 };
+    /* Zone upper corner in triclinic coordinates         */
+    gmx::RVec x1 = { 0, 0, 0 };
+    /* Zone bounding box lower corner in Cartesian coords */
+    gmx::RVec bb_x0 = { 0, 0, 0 };
+    /* Zone bounding box upper corner in Cartesian coords */
+    gmx::RVec bb_x1 = { 0, 0, 0 };
+};
+
+/*! \internal
+ * \brief Class for handling atom ranges and dimensions of domain decomposition zones
+ *
+ * In the eighth-shell domain decomposition there is a home zone and up to seven
+ * zones in the halo. Along each dimension which is decomposed, there are two rows of zones,
+ * resulting in a maximum of 2^3=8 zones.
+ * For computing pair interactions, each pair only once, we take atom pairs between pairs
+ * of zones where the two zones are labelled "i" and "j", like atom pairs in the pair list.
+ * This class provides a list of zone pairs (i,j) from which to collect atom pairs for computing
+ * all pair interactions within a given cut-off distance. These pairs are provided as
+ * range of consecutive j-zones for each i-zone.
+ */
+class DomdecZones
+{
+public:
+    DomdecZones(ArrayRef<const int> ddDims);
+
+    //! Returns the number of zones
+    int numZones() const { return num_; }
+
+    //! Returns the number of "i-zones" for non-bonded pair interactions
+    int numIZones() const { return numIZones_; }
+
+    //! Returns the j-zone range for i-zone \p iZone
+    const Range<int>& jZoneRange(int iZone) const { return jZoneRanges_[iZone]; }
+
+    //! Returns the shift of zone \p zone with respect to the home zone
+    const IVec& shift(int zone) const { return shifts_[zone]; }
+
+    //! Returns the atom index range for zone \p zone
+    const Range<int> atomRange(int zone) const
+    {
+        return { atomRanges_[zone], atomRanges_[zone + 1] };
+    }
+
+    //! Returns the atom index range end for atoms from directly neighboring domains for zone \p zone
+    int directNeighborAtomRangeEnd(int zone) const { return directNeighborAtomRangeEnd_[zone]; }
+
+    //! Returns the j-atom index range for i-zone \p iZone
+    Range<int> jAtomRange(const int iZone) const
+    {
+        GMX_ASSERT(iZone < numIZones_, "iZone should be in range");
+
+        const auto& jZoneRange = jZoneRanges_[iZone];
+
+        return { atomRanges_[*jZoneRange.begin()], atomRanges_[*jZoneRange.end()] };
+    }
+
+    //! Returns the sizes for zone \p zone
+    const gmx_domdec_zone_size_t& sizes(int zone) const { return sizes_[zone]; }
+
+    /*! \brief Sets the end of the atom range for zone \p zone
+     *
+     * \param[in] zone                       The zone to set the range for
+     * \param[in] atomRangeEnd               The end of the atom range
+     * \param[in] rangeIsFromDirectNeighbor  Whether this range comes from directly neighboring domains only
+     */
+    void setAtomRangeEnd(const int zone, const int atomRangeEnd, const bool rangeIsFromDirectNeighbor)
+    {
+        GMX_ASSERT(atomRangeEnd >= atomRanges_[zone],
+                   "atomRangeEnd should be >= than the previous end");
+
+        for (int z = zone + 1; z < num_ + 1; z++)
+        {
+            atomRanges_[z] = atomRangeEnd;
+        }
+
+        if (zone == 0 || rangeIsFromDirectNeighbor)
+        {
+            directNeighborAtomRangeEnd_[zone] = atomRangeEnd;
+        }
+    }
+
+    /*! \brief Set zone dimensions for a range of zones
+     *
+     * \param[in,out] dd          The domain decomposition struct
+     * \param[in]     box         The box
+     * \param[in]     ddbox       The domain decomposition box struct
+     * \param[in]     zoneRange   The range of zones to set sizes for
+     */
+    void setSizes(const gmx_domdec_t&   dd,
+                  const matrix          box,
+                  const gmx_ddbox_t*    ddbox,
+                  const gmx::Range<int> zoneRange);
+
+private:
+    //! The number of zones including the home zone
+    int num_ = 0;
+    //! The number of i-zones for computing pair interactions
+    int numIZones_ = 0;
+    //! The j-zones ranges for each i-zone;
+    std::array<Range<int>, sc_maxNumIZones> jZoneRanges_;
+    //! The shift of the zones with respect to the home zone
+    std::array<IVec, sc_maxNumZones> shifts_;
+    //! The atom range boundaries for the zones
+    std::array<int, sc_maxNumZones + 1> atomRanges_ = { 0 };
+    //! The atom range end for atoms from directly neighboring domains only
+    std::array<int, sc_maxNumZones> directNeighborAtomRangeEnd_ = { 0 };
+    //! Boundaries of the zones
+    std::array<gmx_domdec_zone_size_t, sc_maxNumZones> sizes_;
+};
+
+} // namespace gmx
+
+#endif

src/gromacs/domdec/domdec.cpp
@@ -155,22 +155,6 @@ static const char* enumValueToString(DlbState enumValue)
     return dlbStateNames[enumValue];
 }
 
-/* The DD zone order */
-static const ivec dd_zo[DD_MAXZONE] = { { 0, 0, 0 }, { 1, 0, 0 }, { 1, 1, 0 }, { 0, 1, 0 },
-                                        { 0, 1, 1 }, { 0, 0, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
-
-/* The non-bonded zone-pair setup for domain decomposition
- * The first number is the i-zone, the second number the first j-zone seen by
- * this i-zone, the third number the last+1 j-zone seen by this i-zone.
- * As is, this is for 3D decomposition, where there are 4 i-zones.
- * With 2D decomposition use only the first 2 i-zones and a last+1 j-zone of 4.
- * With 1D decomposition use only the first i-zone and a last+1 j-zone of 2.
- */
-static const int ddNonbondedZonePairRanges[DD_MAXIZONE][3] = { { 0, 0, 8 },
-                                                               { 1, 3, 6 },
-                                                               { 2, 5, 6 },
-                                                               { 3, 5, 7 } };
-
 //! The minimum step interval for DD algorithms requiring global communication
 static constexpr int sc_minimumGCStepInterval = 100;
 
@@ -246,9 +230,9 @@ void dd_store_state(const gmx_domdec_t& dd, t_state* state)
     state->ddp_count_cg_gl = dd.ddp_count;
 }
 
-gmx_domdec_zones_t* domdec_zones(gmx_domdec_t* dd)
+const gmx::DomdecZones& getDomdecZones(const gmx_domdec_t& dd)
 {
-    return &dd->comm->zones;
+    return dd.zones;
 }
 
 int dd_numAtomsZones(const gmx_domdec_t& dd)
@@ -383,7 +367,7 @@ void dd_move_f(gmx_domdec_t* dd, gmx::ForceWithShiftForces* forceWithShiftForces
     gmx::ArrayRef<gmx::RVec> fshift = forceWithShiftForces->shiftForces();
 
     gmx_domdec_comm_t& comm    = *dd->comm;
-    int                nzone   = comm.zones.n / 2;
+    int                nzone   = dd->zones.numZones() / 2;
     int                nat_tot = comm.atomRanges.end(DDAtomRanges::Type::Zones);
     for (int d = dd->ndim - 1; d >= 0; d--)
     {
@@ -1095,69 +1079,6 @@ static void setup_neighbor_relations(gmx_domdec_t* dd)
                     dd->neighbor[d][0],
                     dd->neighbor[d][1]);
         }
-    }
-
-    const int nzone  = (1 << dd->ndim);
-    const int nizone = (1 << std::max(dd->ndim - 1, 0));
-    GMX_RELEASE_ASSERT(nizone >= 1 && nizone <= DD_MAXIZONE,
-                       "We should have at least one i-zone and not more than the number of zones");
-
-    gmx_domdec_zones_t& zones = dd->comm->zones;
-
-    zones.n = nzone;
-
-    for (int i = 0; i < nzone; i++)
-    {
-        int m = 0;
-        clear_ivec(zones.shift[i]);
-        for (int d = 0; d < dd->ndim; d++)
-        {
-            zones.shift[i][dd->dim[d]] = dd_zo[i][m++];
-        }
-    }
-
-    for (int iZoneIndex = 0; iZoneIndex < nizone; iZoneIndex++)
-    {
-        GMX_RELEASE_ASSERT(
-                ddNonbondedZonePairRanges[iZoneIndex][0] == iZoneIndex,
-                "The first element for each ddNonbondedZonePairRanges should match its index");
-
-        DDPairInteractionRanges iZone;
-        iZone.iZoneIndex = iZoneIndex;
-        /* dd_zp3 is for 3D decomposition, for fewer dimensions use only
-         * j-zones up to nzone.
-         */
-        iZone.jZoneRange = gmx::Range<int>(std::min(ddNonbondedZonePairRanges[iZoneIndex][1], nzone),
-                                           std::min(ddNonbondedZonePairRanges[iZoneIndex][2], nzone));
-        for (int dim = 0; dim < DIM; dim++)
-        {
-            if (dd->numCells[dim] == 1)
-            {
-                /* All shifts should be allowed */
-                iZone.shift0[dim] = -1;
-                iZone.shift1[dim] = 1;
-            }
-            else
-            {
-                /* Determine the min/max j-zone shift wrt the i-zone */
-                iZone.shift0[dim] = 1;
-                iZone.shift1[dim] = -1;
-                for (int jZone : iZone.jZoneRange)
-                {
-                    int shift_diff = zones.shift[jZone][dim] - zones.shift[iZoneIndex][dim];
-                    if (shift_diff < iZone.shift0[dim])
-                    {
-                        iZone.shift0[dim] = shift_diff;
-                    }
-                    if (shift_diff > iZone.shift1[dim])
-                    {
-                        iZone.shift1[dim] = shift_diff;
-                    }
-                }
-            }
-        }
-
-        zones.iZones.push_back(iZone);
     }
 
     if (!isDlbDisabled(dd->comm->dlbState))
@@ -2740,7 +2661,10 @@ static DDSettings getDDSettings(const gmx::MDLogger&     mdlog,
     return ddSettings;
 }
 
-gmx_domdec_t::gmx_domdec_t(const t_inputrec& ir) : unitCellInfo(ir) {}
+gmx_domdec_t::gmx_domdec_t(const t_inputrec& ir, gmx::ArrayRef<const int> ddDims) :
+    unitCellInfo(ir), zones(ddDims)
+{
+}
 
 gmx_domdec_t::~gmx_domdec_t() = default;
 
@@ -2930,7 +2854,8 @@ std::unique_ptr<gmx_domdec_t> DomainDecompositionBuilder::Impl::build(LocalAtomS
                                                                       const t_state& localState,
                                                                       ObservablesReducerBuilder* observablesReducerBuilder)
 {
-    auto dd = std::make_unique<gmx_domdec_t>(ir_);
+    auto dd = std::make_unique<gmx_domdec_t>(
+            ir_, arrayRefFromArray(ddGridSetup_.ddDimensions, ddGridSetup_.numDDDimensions));
 
     copy_ivec(ddCellIndex_, dd->ci);
 

src/gromacs/domdec/domdec.h
@@ -85,6 +85,7 @@ namespace gmx
 {
 struct AtomInfoWithinMoleculeBlock;
 class DeviceStreamManager;
+class DomdecZones;
 class ForceWithShiftForces;
 class MDLogger;
 class RangePartitioning;
@@ -109,8 +110,8 @@ int ddglatnr(const gmx_domdec_t* dd, int i);
  */
 void dd_store_state(const gmx_domdec_t& dd, t_state* state);
 
-/*! \brief Returns a pointer to the gmx_domdec_zones_t struct */
-struct gmx_domdec_zones_t* domdec_zones(struct gmx_domdec_t* dd);
+/*! \brief Returns a const reference to the gmx::DomdecZones object */
+const gmx::DomdecZones& getDomdecZones(const gmx_domdec_t& dd);
 
 /*! \brief Returns the range for atoms in zones*/
 int dd_numAtomsZones(const gmx_domdec_t& dd);

src/gromacs/domdec/domdec_internal.h
@@ -64,7 +64,6 @@ enum class DdRankOrder : int;
 }
 // namespace
 
-
 //! Indices to communicate in a dimension
 struct gmx_domdec_ind_t
 {
@@ -73,15 +72,15 @@ struct gmx_domdec_ind_t
      * cell that requires communication, the last entry contains the total
      * number of atoms that needs to be communicated.
      */
-    int nsend[DD_MAXIZONE + 2] = {};
-    int nrecv[DD_MAXIZONE + 2] = {};
+    int nsend[gmx::sc_maxNumIZones + 2] = {};
+    int nrecv[gmx::sc_maxNumIZones + 2] = {};
     //! @}
     //! The charge groups to send
     std::vector<int> index;
     //! @{
     /* The atom range for non-in-place communication */
-    int cell2at0[DD_MAXIZONE] = {};
-    int cell2at1[DD_MAXIZONE] = {};
+    int cell2at0[gmx::sc_maxNumIZones] = {};
+    int cell2at1[gmx::sc_maxNumIZones] = {};
     //! @}
 };
 
@@ -625,9 +624,6 @@ struct gmx_domdec_comm_t // NOLINT (clang-analyzer-optin.performance.Padding)
     /**< The old \p cell_x1, to check cg displacements */
     gmx::RVec old_cell_x1 = { 0, 0, 0 };
 
-    /** The communication setup and charge group boundaries for the zones */
-    gmx_domdec_zones_t zones;
-
     /* The zone limits for DD dimensions 1 and 2 (not 0), determined from
      * cell boundaries of neighboring cells for staggered grids when using
      * dynamic load balancing.
@@ -655,9 +651,6 @@ struct gmx_domdec_comm_t // NOLINT (clang-analyzer-optin.performance.Padding)
      *  stored as DD partitioning call count.
      */
     int64_t main_cg_ddp_count = 0;
-
-    /** The number of cg's received from the direct neighbors */
-    std::array<int, DD_MAXZONE> zone_ncg1 = { 0 };
 
     /** The atom ranges in the local state */
     DDAtomRanges atomRanges;

src/gromacs/domdec/domdec_struct.h
@@ -48,6 +48,7 @@
 #include <memory>
 #include <vector>
 
+#include "gromacs/domdec/domdec_zones.h"
 #include "gromacs/gpu_utils/hostallocator.h"
 #include "gromacs/math/vectypes.h"
 #include "gromacs/topology/block.h"
@@ -55,11 +56,6 @@
 #include "gromacs/utility/gmxmpi.h"
 #include "gromacs/utility/range.h"
 #include "gromacs/utility/real.h"
-
-//! Max number of zones in domain decomposition
-#define DD_MAXZONE 8
-//! Max number of izones in domain decomposition
-#define DD_MAXIZONE 4
 
 struct AtomDistribution;
 struct gmx_domdec_comm_t;
@@ -80,53 +76,6 @@ class LocalAtomSetManager;
 class LocalTopologyChecker;
 class GpuHaloExchange;
 } // namespace gmx
-
-/*! \internal
- * \brief Pair interaction zone and atom range for an i-zone
- */
-struct DDPairInteractionRanges
-{
-    //! The index of this i-zone in the i-zone list
-    int iZoneIndex = -1;
-    //! The range of j-zones
-    gmx::Range<int> jZoneRange;
-    //! The i-atom range
-    gmx::Range<int> iAtomRange;
-    //! The j-atom range
-    gmx::Range<int> jAtomRange;
-    //! Minimum shifts to consider
-    gmx::IVec shift0 = { 0, 0, 0 };
-    //! Maximum shifts to consider
-    gmx::IVec shift1 = { 0, 0, 0 };
-};
-
-typedef struct gmx_domdec_zone_size
-{
-    /* Zone lower corner in triclinic coordinates         */
-    gmx::RVec x0 = { 0, 0, 0 };
-    /* Zone upper corner in triclinic coordinates         */
-    gmx::RVec x1 = { 0, 0, 0 };
-    /* Zone bounding box lower corner in Cartesian coords */
-    gmx::RVec bb_x0 = { 0, 0, 0 };
-    /* Zone bounding box upper corner in Cartesian coords */
-    gmx::RVec bb_x1 = { 0, 0, 0 };
-} gmx_domdec_zone_size_t;
-
-struct gmx_domdec_zones_t
-{
-    /* The number of zones including the home zone */
-    int n = 0;
-    /* The shift of the zones with respect to the home zone */
-    std::array<gmx::IVec, DD_MAXZONE> shift;
-    /* The charge group boundaries for the zones */
-    std::array<int, DD_MAXZONE + 1> cg_range;
-    /* The pair interaction zone and atom ranges per each i-zone */
-    std::vector<DDPairInteractionRanges> iZones;
-    /* Boundaries of the zones */
-    std::array<gmx_domdec_zone_size_t, DD_MAXZONE> size;
-    /* The cg density of the home zone */
-    real dens_zone0 = 0;
-};
 
 struct gmx_ddbox_t
 {
@@ -162,7 +111,7 @@ struct UnitCellInfo
 struct gmx_domdec_t
 { //NOLINT(clang-analyzer-optin.performance.Padding)
     //! Constructor, only partial for now
-    gmx_domdec_t(const t_inputrec& ir);
+    gmx_domdec_t(const t_inputrec& ir, gmx::ArrayRef<const int> ddDims);
     ~gmx_domdec_t();
 
     /* The DD particle-particle nodes only */
@@ -194,6 +143,9 @@ struct gmx_domdec_t
 
     /* Forward and backward neighboring cells, indexed by 0 to ndim */
     int neighbor[DIM][2] = { { 0, 0 }, { 0, 0 }, { 0, 0 } };
+
+    /* The shift, atom ranges and dimensions of the DD zones */
+    gmx::DomdecZones zones;
 
     /* Only available on the main node */
     std::unique_ptr<AtomDistribution> ma;

commit dd770a51d7964784859020b6cee0459e06c9530a

Fix DD bugs with multiple communication pulses
Recent commit 1503b6e8 broke DD communication with multiple pulses
along a dimension. Now the logic is reverted back to that from
before the aforementioned commit.

src/gromacs/domdec/domdec_zones.h
@@ -122,7 +122,10 @@ public:
     }
 
     //! Returns the atom index range end for atoms from directly neighboring domains for zone \p zone
-    int directNeighborAtomRangeEnd(int zone) const { return directNeighborAtomRangeEnd_[zone]; }
+    int directNeighborAtomRangeEnd(int zone) const
+    {
+        return atomRanges_[zone] + numAtomFromDirectNeighbors_[zone];
+    }
 
     //! Returns the j-atom index range for i-zone \p iZone
     Range<int> jAtomRange(const int iZone) const
@@ -148,14 +151,11 @@ public:
         GMX_ASSERT(atomRangeEnd >= atomRanges_[zone],
                    "atomRangeEnd should be >= than the previous end");
 
-        for (int z = zone + 1; z < num_ + 1; z++)
-        {
-            atomRanges_[z] = atomRangeEnd;
-        }
+        atomRanges_[zone + 1] = atomRangeEnd;
 
         if (zone == 0 || rangeIsFromDirectNeighbor)
         {
-            directNeighborAtomRangeEnd_[zone] = atomRangeEnd;
+            numAtomFromDirectNeighbors_[zone] = atomRangeEnd - atomRanges_[zone];
         }
     }
 
@@ -182,8 +182,8 @@ private:
     std::array<IVec, sc_maxNumZones> shifts_;
     //! The atom range boundaries for the zones
     std::array<int, sc_maxNumZones + 1> atomRanges_ = { 0 };
-    //! The atom range end for atoms from directly neighboring domains only
-    std::array<int, sc_maxNumZones> directNeighborAtomRangeEnd_ = { 0 };
+    //! The number of atoms in the zone that are communicated from direct neighbors (these come first)
+    std::array<int, sc_maxNumZones> numAtomFromDirectNeighbors_ = { 0 };
     //! Boundaries of the zones
     std::array<gmx_domdec_zone_size_t, sc_maxNumZones> sizes_;
 };

src/gromacs/domdec/partition.cpp
@@ -455,10 +455,13 @@ static void dd_move_cellx(gmx_domdec_t* dd, const gmx_ddbox_t* ddbox, rvec cell_
     }
 }
 
-//! Sets the charge-group zones to be equal to the home zone.
+//! Sets the atom-range for the home zone to \p dd->numHomeAtoms and all other zones empty
 static void set_zones_numHomeAtoms(gmx_domdec_t* dd)
 {
-    dd->zones.setAtomRangeEnd(0, dd->numHomeAtoms, true);
+    for (int zone = 0; zone < dd->zones.numZones(); zone++)
+    {
+        dd->zones.setAtomRangeEnd(zone, dd->numHomeAtoms, true);
+    }
 }
 
 //! Restore atom groups for the charge groups.
@@ -2194,6 +2197,9 @@ static void setup_dd_communication(gmx_domdec_t* dd, matrix box, gmx_ddbox_t* dd
         }
         nzone += nzone;
     }
+
+    GMX_ASSERT(*zones.atomRange(zones.numZones() - 1).end() == nat_tot,
+               "The zone atom counts should cover the whole atom range");
 
     comm->atomRanges.setEnd(DDAtomRanges::Type::Zones, nat_tot);
 