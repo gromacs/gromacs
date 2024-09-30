gromacs/gromacs

commit 5be7b7b6bc54d4d592b2f467a7ed6bdb9510231e

Changed moved atom count in Nbnxm putAtomsOnGrid()
Instead of taking the number of atoms moved to other DD domains,
the Nbnxm putAtomsOnGrid() and putOnGrid() methods now take
the number of actual atoms that will be put on the grid.

src/gromacs/mdrun/tpi.cpp
@@ -826,7 +826,7 @@ double TestParticleInsertion::insertIntoFrame(const double           t,
 
             /* Put the inserted molecule on it's own search grid */
             fr_.nbv->putAtomsOnGrid(
-                    box, 1, xInit, xInit, nullptr, testAtomsRange_, -1, fr_.atomInfo, x, 0, nullptr);
+                    box, 1, xInit, xInit, nullptr, testAtomsRange_, *testAtomsRange_.end(), -1, fr_.atomInfo, x, nullptr);
 
             /* TODO: Avoid updating all atoms at every bNS step */
             fr_.nbv->setAtomProperties(mdatoms_.typeA, mdatoms_.chargeA, fr_.atomInfo);
@@ -1187,8 +1187,17 @@ void LegacySimulator::do_tpi()
         /* Put all atoms except for the inserted ones on the grid */
         rvec vzero       = { 0, 0, 0 };
         rvec boxDiagonal = { box[XX][XX], box[YY][YY], box[ZZ][ZZ] };
-        fr_->nbv->putAtomsOnGrid(
-                box, 0, vzero, boxDiagonal, nullptr, { 0, *testAtomsRange.begin() }, -1, fr_->atomInfo, x, 0, nullptr);
+        fr_->nbv->putAtomsOnGrid(box,
+                                 0,
+                                 vzero,
+                                 boxDiagonal,
+                                 nullptr,
+                                 { 0, *testAtomsRange.begin() },
+                                 *testAtomsRange.begin(),
+                                 -1,
+                                 fr_->atomInfo,
+                                 x,
+                                 nullptr);
 
         gmx_edsam* const ed = nullptr;
 

src/gromacs/nbnxm/grid.cpp
@@ -1378,9 +1378,9 @@ void Grid::setCellIndices(int                  ddZone,
                           GridSetData*         gridSetData,
                           ArrayRef<GridWork>   gridWork,
                           const Range<int>     atomRange,
+                          const int            numGridAtomsWithoutFillers,
                           ArrayRef<const int>  atomInfo,
                           ArrayRef<const RVec> x,
-                          const int            numAtomsMoved,
                           nbnxn_atomdata_t*    nbat)
 {
     cellOffset_ = cellOffset;
@@ -1425,6 +1425,7 @@ void Grid::setCellIndices(int                  ddZone,
     numCellsColumnMax_ = ncz_max;
 
     /* Resize grid and atom data which depend on the number of cells */
+    const int numAtomsMoved = atomRange.size() - numGridAtomsWithoutFillers;
     resizeForNumberOfCells(atomIndexEnd(), numAtomsMoved, ddZone, gridSetData, nbat);
 
     if (debug)
@@ -1548,17 +1549,16 @@ real generateAndFill2DGrid(Grid*                  grid,
                            const rvec             upperCorner,
                            const UpdateGroupsCog* updateGroupsCog,
                            const Range<int>       atomRange,
+                           const int              numGridAtomsWithoutFillers,
                            real*                  atomDensity,
                            const real             maxAtomGroupRadius,
                            ArrayRef<const RVec>   x,
                            const int              ddZone,
                            const int*             move,
-                           const int              numAtomsMoved,
                            const bool             computeGridDensityRatio)
 {
-    const int n = atomRange.size();
-
-    grid->setDimensions(ddZone, n - numAtomsMoved, lowerCorner, upperCorner, atomDensity, maxAtomGroupRadius);
+    grid->setDimensions(
+            ddZone, numGridAtomsWithoutFillers, lowerCorner, upperCorner, atomDensity, maxAtomGroupRadius);
 
     for (GridWork& work : gridWork)
     {

api/nblib/gmxcalculatorcpu.cpp
@@ -169,10 +169,10 @@ void GmxNBForceCalculatorCpu::CpuImpl::updatePairlist(gmx::ArrayRef<gmx::RVec> c
                                   upperCorner,
                                   nullptr,
                                   { 0, int(coordinates.size()) },
+                                  coordinates.size(),
                                   particleDensity,
                                   system_.particleInfo_,
                                   coordinates,
-                                  0,
                                   nullptr);
 
     backend_.nbv_->constructPairlist(

src/gromacs/domdec/partition.cpp
@@ -2839,7 +2839,9 @@ void dd_partition_system(FILE*                     fplog,
         {
             homeZoneVolume *= dd->zones.sizes(0).x1[dim] - dd->zones.sizes(0).x0[dim];
         }
-        const real atomDensity = (dd->numHomeAtoms - ncg_moved) / homeZoneVolume;
+        // The home atom list still contains moved atoms, compute the new atom count
+        const int  newNumHomeAtoms = dd->numHomeAtoms - ncg_moved;
+        const real atomDensity     = newNumHomeAtoms / homeZoneVolume;
 
         fr->nbv->putAtomsOnGrid(state_local->box,
                                 0,
@@ -2847,10 +2849,10 @@ void dd_partition_system(FILE*                     fplog,
                                 dd->zones.sizes(0).bb_x1,
                                 comm->updateGroupsCog.get(),
                                 { 0, dd->numHomeAtoms },
+                                newNumHomeAtoms,
                                 atomDensity,
                                 fr->atomInfo,
                                 state_local->x,
-                                ncg_moved,
                                 bRedist ? comm->movedBuffer.data() : nullptr);
 
         if (debug)

src/gromacs/mdlib/sim_util.cpp
@@ -1352,8 +1352,17 @@ static void doPairSearch(const t_commrec*             cr,
         const rvec vzero       = { 0.0_real, 0.0_real, 0.0_real };
         const rvec boxDiagonal = { box[XX][XX], box[YY][YY], box[ZZ][ZZ] };
         wallcycle_sub_start(wcycle, WallCycleSubCounter::NBSGridLocal);
-        nbv->putAtomsOnGrid(
-                box, 0, vzero, boxDiagonal, nullptr, { 0, mdatoms.homenr }, -1, fr->atomInfo, x.unpaddedArrayRef(), 0, nullptr);
+        nbv->putAtomsOnGrid(box,
+                            0,
+                            vzero,
+                            boxDiagonal,
+                            nullptr,
+                            { 0, mdatoms.homenr },
+                            mdatoms.homenr,
+                            -1,
+                            fr->atomInfo,
+                            x.unpaddedArrayRef(),
+                            nullptr);
         wallcycle_sub_stop(wcycle, WallCycleSubCounter::NBSGridLocal);
     }
     else

src/gromacs/nbnxm/benchmark/bench_setup.cpp
@@ -268,10 +268,10 @@ static std::unique_ptr<nonbonded_verlet_t> setupNbnxmForBenchInstance(const Nbnx
                         upperCorner,
                         nullptr,
                         { 0, int(system.coordinates.size()) },
+                        system.coordinates.size(),
                         atomDensity,
                         atomInfo,
                         system.coordinates,
-                        0,
                         nullptr);
 
     nbv->constructPairlist(InteractionLocality::Local, system.excls, 0, &nrnb);

src/gromacs/nbnxm/grid.h
@@ -287,9 +287,9 @@ public:
                         GridSetData*            gridSetData,
                         ArrayRef<GridWork>      gridWork,
                         Range<int>              atomRange,
+                        int                     numGridAtomsWithoutFillers,
                         ArrayRef<const int32_t> atomInfo,
                         ArrayRef<const RVec>    x,
-                        int                     numAtomsMoved,
                         nbnxn_atomdata_t*       nbat);
 
     //! Determine in which grid columns atoms should go, store cells and atom counts in \p cell and \p cxy_na
@@ -399,13 +399,13 @@ private:
  * \param[in] lowerCorner   The minimum Cartesian coordinates of the grid
  * \param[in] upperCorner   The maximum Cartesian coordinates of the grid
  * \param[in] updateGroupsCog  The center of geometry of update groups, can be nullptr
- * \param[in] atomRange     The range of atoms to put on this grid
+ * \param[in] atomRange     The range of atoms to put on this grid, may include moved atoms
+ * \param[in] numGridAtomsWithoutFillers  The number of non-filler atoms to put on this grid
  * \param[in,out] atomDensity  The atom density, will be computed when <= 0
  * \param[in] maxAtomGroupRadius  The maximum radius of atom groups
  * \param[in] x             The coordinates of the atoms
  * \param[in] ddZone        The domain decomposition zone
  * \param[in] move          Tells whether atoms have moved to another DD domain
- * \param[in] numAtomsMoved  The number of atoms that moved to another DD domain
  * \param[in] computeGridDensityRatio  When true, return the grid density ratio
  *
  * \returns When \p computeGridDensityRatio==true, the ratio of the effective 2D grid density and the uniform grid density
@@ -417,12 +417,12 @@ real generateAndFill2DGrid(Grid*                  grid,
                            const rvec             upperCorner,
                            const UpdateGroupsCog* updateGroupsCog,
                            Range<int>             atomRange,
+                           int                    numGridAtomsWithoutFillers,
                            real*                  atomDensity,
                            real                   maxAtomGroupRadius,
                            ArrayRef<const RVec>   x,
                            int                    ddZone,
                            const int*             move,
-                           int                    numAtomsMoved,
                            bool                   computeGridDensityRatio);
 
 } // namespace gmx

src/gromacs/nbnxm/gridset.cpp
@@ -161,10 +161,10 @@ void GridSet::putOnGrid(const matrix            box,
                         const rvec              upperCorner,
                         const UpdateGroupsCog*  updateGroupsCog,
                         const Range<int>        atomRange,
+                        const int               numGridAtoms,
                         real                    atomDensity,
                         ArrayRef<const int32_t> atomInfo,
                         ArrayRef<const RVec>    x,
-                        const int               numAtomsMoved,
                         const int*              move,
                         nbnxn_atomdata_t*       nbat)
 {
@@ -176,11 +176,11 @@ void GridSet::putOnGrid(const matrix            box,
     {
         copy_mat(box, box_);
 
-        numRealAtomsLocal_ = *atomRange.end() - numAtomsMoved;
+        numRealAtomsLocal_ = numGridAtoms;
         /* We assume that nbnxn_put_on_grid is called first
          * for the local atoms (gridIndex=0).
          */
-        numRealAtomsTotal_ = *atomRange.end() - numAtomsMoved;
+        numRealAtomsTotal_ = numGridAtoms;
 
         maxAtomGroupRadius = (updateGroupsCog ? updateGroupsCog->maxUpdateGroupRadius() : 0);
 
@@ -241,12 +241,12 @@ void GridSet::putOnGrid(const matrix            box,
                                                  upperCorner,
                                                  updateGroupsCog,
                                                  atomRange,
+                                                 numGridAtoms,
                                                  &atomDensity,
                                                  maxAtomGroupRadius,
                                                  x,
                                                  ddZone,
                                                  move,
-                                                 numAtomsMoved,
                                                  computeGridDensityRatio);
 
         iteration++;
@@ -254,7 +254,7 @@ void GridSet::putOnGrid(const matrix            box,
 
     /* Copy the already computed cell indices to the grid and sort, when needed */
     grid.setCellIndices(
-            ddZone, cellOffset, &gridSetData_, gridWork_, atomRange, atomInfo, x, numAtomsMoved, nbat);
+            ddZone, cellOffset, &gridSetData_, gridWork_, atomRange, numGridAtoms, atomInfo, x, nbat);
 
     if (gridIndex == gmx::ssize(grids_) - 1)
     {

commit 4ce8d2db1306e232924d6959dd179b1f106e94f0

Fix Nbnxm grid atom count with TPI
This bug was introduced in recent commit 5be7b7b6.

Also added an assertion that catches such cases.

Fixes #5157

Closes #5157

src/gromacs/mdrun/tpi.cpp
@@ -826,7 +826,7 @@ double TestParticleInsertion::insertIntoFrame(const double           t,
 
             /* Put the inserted molecule on it's own search grid */
             fr_.nbv->putAtomsOnGrid(
-                    box, 1, xInit, xInit, nullptr, testAtomsRange_, *testAtomsRange_.end(), -1, fr_.atomInfo, x, nullptr);
+                    box, 1, xInit, xInit, nullptr, testAtomsRange_, testAtomsRange_.size(), -1, fr_.atomInfo, x, nullptr);
 
             /* TODO: Avoid updating all atoms at every bNS step */
             fr_.nbv->setAtomProperties(mdatoms_.typeA, mdatoms_.chargeA, fr_.atomInfo);

src/gromacs/nbnxm/grid.cpp
@@ -1563,6 +1563,9 @@ real generateAndFill2DGrid(Grid*                  grid,
                            const int*             move,
                            const bool             computeGridDensityRatio)
 {
+    GMX_ASSERT(numGridAtomsWithoutFillers <= atomRange.size(),
+               "The real atoms are a subset of atomRange");
+
     grid->setDimensions(
             ddZone, numGridAtomsWithoutFillers, lowerCorner, upperCorner, atomDensity, maxAtomGroupRadius);
 