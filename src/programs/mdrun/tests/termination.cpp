5baf097c119e734ddd20f93a476693d9a41308ae
Mark Abraham <mark.j.abraham@gmail.com>
2024-10-01 17:04:22 +0200

    Temporarily disable test case
    This test is unstable in a CUDA-graphs build, so we are disabling it
    while we understand and fix the underlying issue.
    
    Refs #5162
    Refs #5138

--- src/programs/mdrun/tests/termination.cpp
+++ src/programs/mdrun/tests/termination.cpp
@@ -48,6 +48,7 @@
 #include "gmxpre.h"
 
 #include <cstdio>
+#include <cstdlib>
 
 #include <filesystem>
 #include <string>
@@ -120,6 +121,12 @@ TEST_F(MdrunTerminationTest, CheckpointRestartAppendsByDefault)
 
 TEST_F(MdrunTerminationTest, WritesCheckpointAfterMaxhTerminationAndThenRestarts)
 {
+    if (std::getenv("GMX_CUDA_GRAPH") != nullptr)
+    {
+        // See https://gitlab.com/gromacs/gromacs/-/issues/5162
+        GTEST_SKIP() << "Test is unstable with CUDA graphs";
+    }
+
     runner_.useTopGroAndNdxFromDatabase("spc2");
     organizeMdpFile(&runner_, 100);
     EXPECT_EQ(0, runner_.callGrompp());
