gromacs/gromacs

commit b3fb7f7aee03802cf939766fd3c6da7625753893

SYCL GPU Pairlist sorting
Improve NBNXM kernel performance by making sure the work is more
evenly distributed.

The approach closely follows CUDA / HIP approach.

`sycl::popcount` implementation added for AdaptiveCpp 23.10 and earlier.

Prefix sum inspired by oneDPL. They use `sycl::joint_exclusive_scan`
over a single work-group for small inputs, and our current histogram
size if small enough, so we just do the same instead of pulling in
the whole library.

Fixes #4979

Closes #4979

src/gromacs/nbnxm/sycl/nbnxm_sycl.cpp
@@ -50,6 +50,8 @@
 namespace Nbnxm
 {
 
+static void launchSciSortOnGpu(gpu_plist* plist, const DeviceStream& deviceStream);
+
 void gpu_launch_kernel_pruneonly(NbnxmGpu* nb, const InteractionLocality iloc, const int numParts)
 {
     gpu_plist* plist = nb->plist[iloc];
@@ -91,6 +93,10 @@ void gpu_launch_kernel_pruneonly(NbnxmGpu* nb, const InteractionLocality iloc, c
     }
 
     launchNbnxmKernelPruneOnly(nb, iloc, numParts, numSciInPartMax);
+    if (plist->haveFreshList && nbnxmSortListsOnGpu())
+    {
+        launchSciSortOnGpu(plist, *nb->deviceStreams[iloc]);
+    }
 
     if (plist->haveFreshList)
     {
@@ -102,6 +108,7 @@ void gpu_launch_kernel_pruneonly(NbnxmGpu* nb, const InteractionLocality iloc, c
         nb->didRollingPrune[iloc] = true; // Mark that rolling pruning has been done
     }
 }
+
 
 void gpu_launch_kernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, const Nbnxm::InteractionLocality iloc)
 {
@@ -126,7 +133,126 @@ void gpu_launch_kernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, const Nb
         return;
     }
 
-    launchNbnxmKernel(nb, stepWork, iloc);
+    /* Whether we need to call a combined prune and interaction kernel or just an interaction
+     * kernel. doPrune being true implies we are not using dynamic pruning and are in the first
+     * call to the interaction kernel after a neighbour list step */
+    bool doPrune = (plist->haveFreshList && !nb->timers->interaction[iloc].didPrune);
+
+    launchNbnxmKernel(nb, stepWork, iloc, doPrune);
+    if (doPrune && nbnxmSortListsOnGpu())
+    {
+        launchSciSortOnGpu(plist, *nb->deviceStreams[iloc]);
+    }
+}
+
+/*! \brief SYCL exclusive prefix sum kernel for list sorting.
+ *
+ * As of oneAPI 2024.1, \c oneapi::dpl::experimental::exclusive_scan_async for inputs <= 16384
+ * elements simply launches a single work-group and uses sycl::joint_exclusive_scan. We have,
+ * somewhat arbitrary, input size of 8192, so we're fine replicating the same approach.
+ *
+ * NVIDIA's CUB uses fancier approach ("Single-pass Parallel Prefix Scan with Decoupled Look-back"),
+ * but we are unlikely to need it here since this kernel is very small anyway.
+ *
+ * \tparam workGroupSize Size of the (only) work-group.
+ * \tparam nElements Input array size; should be a multiple of workGroupSize.
+ * \param gm_input Input data buffer, should contain nElements elements of type \c int.
+ * \param gm_output Output data buffer, should have enough space for nElements elements of type \c int.
+ *
+ * \warning This kernel should be launched with only a single work-group of size workGroupSize.
+ * \warning This kernel is inefficient for large inputs (oneDPL uses it with <= 16384 elements).
+ */
+template<int workGroupSize, int nElements>
+static auto nbnxnKernelExclusivePrefixSum(const int* __restrict__ gm_input, int* __restrict__ gm_output)
+{
+    static_assert(nElements % workGroupSize == 0, "This simple scan kernel does not handle padding");
+    return [=](sycl::nd_item<1> itemIdx) {
+        const sycl::group<1> workGroup = itemIdx.get_group();
+        sycl::joint_exclusive_scan(
+                workGroup, gm_input, gm_input + nElements, gm_output, 0, sycl::plus<int>{});
+    };
+}
+//! SYCL kernel name
+class ExclusivePrefixSum;
+
+/*! \brief SYCL bucket sci sort kernel.
+ *
+ *  Sorts sci in order from most to least neighbours, using the count sort algorithm
+ *
+ *  Unlike the cpu version of sci sort, this kernel uses counts which only contain pairs which have
+ *  not been masked out, giving an ordering which more accurately represents the work which will be
+ *  done in the non bonded force kernel. The counts themselves are generated in the prune kernel.
+ *
+ * \param gm_sci Unsorted pair list.
+ * \param gm_sciCount Total number of sci with exactly \c i neighbours
+ * \param gm_sciOffset Exclusive prefix sum of gm_sciCount. \c gm_sciOffset[i] is the offset that
+ *                     the first sci with \c i neighbours will have in the sorted sci
+ *                     list. All other sci with i neighbours will be placed randomly in
+ *                     positions \c gm_sciOffset[i] to \c gm_sciOffset[i+1] exclusive.
+ * \param gm_sciSorted Sorted pair list.
+ */
+static auto nbnxnKernelBucketSciSort(const nbnxn_sci_t* __restrict__ gm_sci,
+                                     const int* __restrict__ gm_sciCount,
+                                     int* __restrict__ gm_sciOffset,
+                                     nbnxn_sci_t* __restrict__ gm_sciSorted)
+{
+    return [=](sycl::id<1> itemIdx) {
+        using sycl::memory_order, sycl::memory_scope, sycl::access::address_space;
+
+        const int         idx      = itemIdx[0];
+        const nbnxn_sci_t sci      = gm_sci[idx];
+        const int         sciCount = gm_sciCount[idx];
+        // Choose an order in the sorted list for sci with the same number of neighbours as each other.
+        // We want each sci with the same count to go somewhere in the list between sciOffset[count]
+        // to sciOffset[count+1] exclusive.
+        // As the amount of work for each of these in the non bonded force kernel will
+        // be equivalent, the order within these bounds does not matter.
+        sycl::atomic_ref<int, memory_order::relaxed, memory_scope::device, address_space::global_space> offsetRef(
+                gm_sciOffset[sciCount]);
+
+        const int sciOffset = offsetRef.fetch_add(1);
+        // Insert the sci into the sorted list at the chosen index
+        gm_sciSorted[sciOffset] = sci;
+    };
+}
+//! SYCL kernel name
+class BucketSciSort;
+
+template<int workGroupSize>
+static void launchPrefixSumKernel(sycl::queue& q, gpuPlistSorting* sorting)
+{
+    q.submit(GMX_SYCL_DISCARD_EVENT[&](sycl::handler & cgh) {
+        cgh.parallel_for<ExclusivePrefixSum>(
+                sycl::nd_range<1>{ workGroupSize, workGroupSize },
+                nbnxnKernelExclusivePrefixSum<workGroupSize, c_sciHistogramSize>(
+                        sorting->sciHistogram.get_pointer(), sorting->sciOffset.get_pointer()));
+    });
+}
+
+static void launchBucketSortKernel(sycl::queue& q, gpu_plist* plist)
+{
+    const size_t size = plist->nsci;
+    q.submit(GMX_SYCL_DISCARD_EVENT[&](sycl::handler & cgh) {
+        cgh.parallel_for<BucketSciSort>(
+                sycl::range<1>{ size },
+                nbnxnKernelBucketSciSort(plist->sci.get_pointer(),
+                                         plist->sorting.sciCount.get_pointer(),
+                                         plist->sorting.sciOffset.get_pointer(),
+                                         plist->sorting.sciSorted.get_pointer()));
+    });
+}
+static void launchSciSortOnGpu(gpu_plist* plist, const DeviceStream& deviceStream)
+{
+    sycl::queue q = deviceStream.stream();
+
+    /* We are launching a single work-group, and it should be, in principle, as large as possible.
+     * E.g., on PVC1100, wgSizeScan=1024 is ~1.2 times faster than wgSizeScan=512, and on
+     * MI250X ~1.7 times faster. But Intel iGPUs only handle 512.
+     * It should be autodetected, but for now we just use the universally supported value.
+     * The kernel takes 10-100µs and is run rarely, so it is of minor concern. */
+    constexpr int wgSizeScan = 512;
+    launchPrefixSumKernel<wgSizeScan>(q, &plist->sorting);
+    launchBucketSortKernel(q, plist);
 }
 
 } // namespace Nbnxm

src/gromacs/gpu_utils/sycl_kernel_utils.h
@@ -244,4 +244,28 @@ __device__ __host__
 }
 #endif
 
+#if GMX_SYCL_HIPSYCL && !(defined(ACPP_VERSION_MAJOR) && ACPP_VERSION_MAJOR >= 24)
+namespace sycl
+{
+/*! \brief Popcount instruction for SYCL
+ *
+ * sycl::popcount is missing in AdaptiveCpp prior to 24.02.0.
+ * This is very basic version using Clang built-in.
+ */
+template<typename T>
+T popcount(T x)
+{
+    if constexpr (sizeof(T) == 4)
+    {
+        return __builtin_popcount(x);
+    }
+    else
+    {
+        static_assert(sizeof(T) == 8);
+        return __builtin_popcountll(x);
+    }
+}
+} // namespace sycl
+#endif
+
 #endif /* GMX_GPU_UTILS_SYCL_KERNEL_UTILS_H */

src/gromacs/nbnxm/nbnxm_gpu_data_mgmt.h
@@ -67,7 +67,7 @@ void gpu_free_platform_specific(NbnxmGpu* nb);
 void getExclusiveScanWorkingArraySize(size_t& scan_size, gpu_plist* d_plist, const DeviceStream& deviceStream);
 #if !GMX_GPU_CUDA
 // CUDA's CUB requires temporary data storage, and is defined there.
-// SYCL's oneDPL does not need one, so we can return 0 there too.
+// Current SYCL implementation does not need one, so we can return 0 there too.
 inline void getExclusiveScanWorkingArraySize(size_t& scan_size, gpu_plist*, const DeviceStream&)
 {
     scan_size = 0;

src/gromacs/nbnxm/pairlist.h
@@ -211,7 +211,7 @@ static constexpr int c_nbnxnGpuExclSize =
 //! Whether we want to use GPU for neighbour list sorting
 constexpr bool nbnxmSortListsOnGpu()
 {
-    return (GMX_GPU_CUDA != 0);
+    return (GMX_GPU_CUDA || GMX_GPU_SYCL);
 }
 
 /*! \internal

src/gromacs/nbnxm/sycl/nbnxm_sycl_kernel.cpp
@@ -102,33 +102,32 @@ extern template void launchNbnxmKernelHelper<64, true, true>(NbnxmGpu* nb, const
 // clang-format on
 
 template<int subGroupSize>
-void launchNbnxmKernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, const InteractionLocality iloc)
+void launchNbnxmKernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, const InteractionLocality iloc, bool doPrune)
 {
-    const bool doPruneNBL     = (nb->plist[iloc]->haveFreshList && !nb->didPrune[iloc]);
     const bool doCalcEnergies = stepWork.computeEnergy;
 
     gmx::dispatchTemplatedFunction(
             [&](auto doPruneNBL_, auto doCalcEnergies_) {
                 launchNbnxmKernelHelper<subGroupSize, doPruneNBL_, doCalcEnergies_>(nb, stepWork, iloc);
             },
-            doPruneNBL,
+            doPrune,
             doCalcEnergies);
 }
 
-void launchNbnxmKernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, const InteractionLocality iloc)
+void launchNbnxmKernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, const InteractionLocality iloc, bool doPrune)
 {
     const int subGroupSize = getNbnxmSubGroupSize(nb->deviceContext_->deviceInfo());
     switch (subGroupSize)
     {
         // Ensure any changes are in sync with device_management_sycl.cpp, nbnxm_sycl_kernel_body.h, and the #if above
 #if SYCL_NBNXM_SUPPORTS_SUBGROUP_SIZE_8
-        case 8: launchNbnxmKernel<8>(nb, stepWork, iloc); break;
+        case 8: launchNbnxmKernel<8>(nb, stepWork, iloc, doPrune); break;
 #endif
 #if SYCL_NBNXM_SUPPORTS_SUBGROUP_SIZE_32
-        case 32: launchNbnxmKernel<32>(nb, stepWork, iloc); break;
+        case 32: launchNbnxmKernel<32>(nb, stepWork, iloc, doPrune); break;
 #endif
 #if SYCL_NBNXM_SUPPORTS_SUBGROUP_SIZE_64
-        case 64: launchNbnxmKernel<64>(nb, stepWork, iloc); break;
+        case 64: launchNbnxmKernel<64>(nb, stepWork, iloc, doPrune); break;
 #endif
         default: GMX_RELEASE_ASSERT(false, "Unsupported sub-group size");
     }

src/gromacs/nbnxm/sycl/nbnxm_sycl_kernel.h
@@ -63,8 +63,9 @@ using gmx::InteractionLocality;
  * \param nb Non-bonded parameters.
  * \param stepWork Workload flags for the current step.
  * \param iloc Interaction locality.
+ * \param doPrune Whether to do neighborlist pruning.
  */
-void launchNbnxmKernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, const InteractionLocality iloc);
+void launchNbnxmKernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, InteractionLocality iloc, bool doPrune);
 
 } // namespace Nbnxm
 

src/gromacs/nbnxm/sycl/nbnxm_sycl_kernel_body.h
@@ -375,7 +375,7 @@ static inline void reduceForceJAmdDpp(Float3 f, const int tidxi, const int aidx,
  * reduced in log2(cl_Size) steps using shift and atomically accumulate them into \p a_f.
  */
 static inline void reduceForceJShuffle(Float3                   f,
-                                       const sycl::nd_item<3>   itemIdx,
+                                       const sycl::nd_item<3>&  itemIdx,
                                        const int                tidxi,
                                        const int                aidx,
                                        sycl::global_ptr<Float3> a_f)
@@ -460,7 +460,7 @@ static inline float groupReduce(const sycl::nd_item<3> itemIdx,
  */
 static inline void reduceForceJGeneric(sycl::local_ptr<float>   sm_buf,
                                        Float3                   f,
-                                       const sycl::nd_item<3>   itemIdx,
+                                       const sycl::nd_item<3>&  itemIdx,
                                        const int                tidxi,
                                        const int                tidxj,
                                        const int                aidx,
@@ -895,6 +895,8 @@ static auto nbnxmKernel(sycl::handler& cgh,
                         const Float2* __restrict__ gm_nbfp /* used iff !ljComb<vdwType> */,
                         const Float2* __restrict__ gm_nbfpComb /* used iff ljEwald<vdwType> */,
                         const float* __restrict__ gm_coulombTab /* used iff elecEwaldTab<elecType> */,
+                        int* __restrict__ gm_sciHistogram,      /* used iff doPruneNBL */
+                        int* __restrict__ gm_sciCount,          /* used iff doPruneNBL */
                         const int             numTypes,
                         const float           rCoulombSq,
                         const float           rVdwSq,
@@ -978,6 +980,17 @@ static auto nbnxmKernel(sycl::handler& cgh,
                                                           : (needExtraElementForReduction ? 1 : 0);
     sycl::local_accessor<float, 1> sm_reductionBuffer(sycl::range<1>(sm_reductionBufferSize), cgh);
 
+    auto sm_prunedPairCount = [&]() {
+        if constexpr (doPruneNBL && nbnxmSortListsOnGpu())
+        {
+            return sycl::local_accessor<int, 1>(sycl::range<1>(1), cgh);
+        }
+        else
+        {
+            return nullptr;
+        }
+    }();
+
     /* Flag to control the calculation of exclusion forces in the kernel
      * We do that with Ewald (elec/vdw) and RF. Cut-off only has exclusion for energy terms. */
     constexpr bool doExclusionForces =
@@ -1059,6 +1072,17 @@ static auto nbnxmKernel(sycl::handler& cgh,
                 }
             }
         }
+
+        if constexpr (doPruneNBL && nbnxmSortListsOnGpu())
+        {
+            /* Initialise one int for reducing prunedPairCount over warps */
+            if (tidx == 0)
+            {
+                sm_prunedPairCount[0] = 0;
+            }
+        }
+        int prunedPairCount = 0;
+
         itemIdx.barrier(fence_space::local_space);
 
         float ewaldCoeffLJ_6_6; // Only needed if (props.vdwEwald)
@@ -1389,6 +1413,10 @@ static auto nbnxmKernel(sycl::handler& cgh,
                 /* Update the imask with the new one which does not contain the
                  * out of range clusters anymore. */
                 gm_plistCJPacked[jPacked].imei[imeiIdx].imask = imask;
+                if constexpr (nbnxmSortListsOnGpu())
+                {
+                    prunedPairCount += sycl::popcount(imask);
+                }
             }
         } // for (int jPacked = cijPackedBegin; jPacked < cijPackedEnd; jPacked += 1)
 
@@ -1410,6 +1438,26 @@ static auto nbnxmKernel(sycl::handler& cgh,
             {
                 atomicFetchAdd(gm_energyVdw[0], energyVdwGroup);
                 atomicFetchAdd(gm_energyElec[0], energyElecGroup);
+            }
+        }
+        if constexpr (doPruneNBL && nbnxmSortListsOnGpu())
+        {
+            /* aggregate neighbour counts, to be used in bucket sci sort */
+            /* One thread in each warp contributes the count for that warp as soon as it reaches
+             * here. Masks are calculated per warp in a warp synchronising operation, so no
+             * syncthreads required here. */
+            if (sg.leader())
+            {
+                atomicFetchAddLocal(sm_prunedPairCount[0], prunedPairCount);
+            }
+            itemIdx.barrier(fence_space::local_space);
+            prunedPairCount = sm_prunedPairCount[0];
+            if (tidxi == 0 && tidxj == 0)
+            {
+                /* one thread in the block writes the final count for this sci */
+                int index = sycl::max(c_sciHistogramSize - prunedPairCount - 1, 0);
+                atomicFetchAdd(gm_sciHistogram[index], 1);
+                gm_sciCount[bidx] = index;
             }
         }
     };
@@ -1476,13 +1524,16 @@ void launchNbnxmKernelHelper(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, co
             adat->eElec.get_pointer(),
             adat->eLJ.get_pointer(),
             plist->cjPacked.get_pointer(),
-            plist->sci.get_pointer(),
+            (doPruneNBL || !nbnxmSortListsOnGpu()) ? plist->sci.get_pointer()
+                                                   : plist->sorting.sciSorted.get_pointer(),
             plist->excl.get_pointer(),
             adat->ljComb.get_pointer(),
             adat->atomTypes.get_pointer(),
             nbp->nbfp.get_pointer(),
             nbp->nbfp_comb.get_pointer(),
             nbp->coulomb_tab.get_pointer(),
+            plist->sorting.sciHistogram.get_pointer(),
+            plist->sorting.sciCount.get_pointer(),
             adat->numTypes,
             nbp->rcoulomb_sq,
             nbp->rvdw_sq,

src/gromacs/nbnxm/sycl/nbnxm_sycl_kernel_pruneonly.cpp
@@ -72,12 +72,24 @@ auto nbnxmKernelPruneOnly(sycl::handler& cgh,
                           const nbnxn_sci_t* __restrict__ gm_plistSci,
                           unsigned int* __restrict__ gm_plistIMask,
                           int* __restrict__ gm_rollingPruningPart,
+                          int* __restrict__ gm_sciHistogram,
+                          int* __restrict__ gm_sciCount,
                           const float rlistOuterSq,
                           const float rlistInnerSq)
 {
     /* shmem buffer for i x+q pre-loading */
     sycl::local_accessor<Float4, 1> sm_xq(
             sycl::range<1>(c_nbnxnGpuNumClusterPerSupercluster * c_clSize), cgh);
+    auto sm_prunedPairCount = [&]() {
+        if constexpr (haveFreshList && nbnxmSortListsOnGpu())
+        {
+            return sycl::local_accessor<int, 1>(sycl::range<1>(1), cgh);
+        }
+        else
+        {
+            return nullptr;
+        }
+    }();
 
     constexpr int warpSize = c_clSize * c_splitClSize;
 
@@ -145,8 +157,18 @@ auto nbnxmKernelPruneOnly(sycl::handler& cgh,
                 sm_xq[(tidxj + i) * c_clSize + tidxi] = xi;
             }
         }
+        /* Initialise one int for reducing prunedPairCount over warps */
+        if constexpr (haveFreshList && nbnxmSortListsOnGpu())
+        {
+            if (tidx == 0 && tidxz == 0)
+            {
+                sm_prunedPairCount[0] = 0;
+            }
+        }
+
         itemIdx.barrier(fence_space::local_space);
 
+        int prunedPairCount = 0;
         /* loop over the j clusters = seen by any of the atoms in the current super-cluster.
          * The loop stride c_syclPruneKernelJPackedConcurrency ensures that consecutive warps-pairs
          * are assigned consecutive jPacked's entries. */
@@ -224,11 +246,38 @@ auto nbnxmKernelPruneOnly(sycl::handler& cgh,
                 {
                     /* copy the list pruned to rlistOuter to a separate buffer */
                     gm_plistIMask[jPacked * c_nbnxnGpuClusterpairSplit + widx] = imaskFull;
+                    if constexpr (nbnxmSortListsOnGpu())
+                    {
+                        /* add to neighbour count, to be used in bucket sci sort */
+                        prunedPairCount += sycl::popcount(imaskNew);
+                    }
                 }
                 /* update the imask with only the pairs up to rlistInner */
                 gm_plistCJPacked[jPacked].imei[widx].imask = imaskNew;
             } // (imaskCheck)
         } // for (int jPacked = cijPackedBegin + tidxz; jPacked < cijPackedEnd; jPacked += c_syclPruneKernelJPackedConcurrency)
+
+
+        /* aggregate neighbour counts, to be used in bucket sci sort */
+        if constexpr (haveFreshList && nbnxmSortListsOnGpu())
+        {
+            /* One thread in each warp contributes the count for that warp as soon as it reaches
+             * here. Masks are calculated per warp in a warp synchronising operation, so no
+             * syncthreads required here. */
+            if (sg.leader())
+            {
+                atomicFetchAddLocal(sm_prunedPairCount[0], prunedPairCount);
+            }
+            itemIdx.barrier(fence_space::local_space);
+            prunedPairCount = sm_prunedPairCount[0];
+            if (tidxi == 0 && tidxj == 0 && tidxz == 0)
+            {
+                /* one thread in the block writes the final count for this sci */
+                int index = sycl::max(c_sciHistogramSize - prunedPairCount - 1, 0);
+                atomicFetchAdd(gm_sciHistogram[index], 1);
+                gm_sciCount[bidx * numParts + part] = index;
+            }
+        }
     };
 }
 
@@ -283,9 +332,13 @@ void launchNbnxmKernelPruneOnly(NbnxmGpu* nb, const InteractionLocality iloc, co
                                         adat->xq.get_pointer(),
                                         adat->shiftVec.get_pointer(),
                                         plist->cjPacked.get_pointer(),
-                                        plist->sci.get_pointer(),
+                                        (haveFreshList || !nbnxmSortListsOnGpu())
+                                                ? plist->sci.get_pointer()
+                                                : plist->sorting.sciSorted.get_pointer(),
                                         plist->imask.get_pointer(),
                                         plist->d_rollingPruningPart.get_pointer(),
+                                        plist->sorting.sciHistogram.get_pointer(),
+                                        plist->sorting.sciCount.get_pointer(),
                                         nbp->rlistOuter_sq,
                                         nbp->rlistInner_sq);
 }

commit ae1823e6f092a11c63ab700da584cd89aa375268

SYCL pairlist sorting: runtime sorting size selection
On Intel Gen9, this fixes the runtime error and allows GROMACS to run.
The problem was introduced in b3fb7f7a (!4150).

On PVC1100 an MI250X, this makes the kernel ~15% and ~55% faster,
respectively, with little impact on the overall performance.

Fixes #5139

Closes #5139

src/gromacs/hardware/device_information.h
@@ -208,6 +208,8 @@ struct DeviceInformation
     std::optional<int> hardwareVersionPatch;
     //! Does the device support SYCL Graph
     bool supportsSyclGraph;
+    //! Max. work-group shape supported by the device
+    int maxWorkGroupSize;
 #endif
     /*! \brief UUID of the device, when available
      *

src/gromacs/hardware/device_management_sycl.cpp
@@ -680,6 +680,9 @@ std::vector<std::unique_ptr<DeviceInformation>> findDevices()
             }
         }
 
+        deviceInfos[i]->maxWorkGroupSize =
+                syclDevice.get_info<sycl::info::device::max_work_group_size>();
+
 #if GMX_HAVE_GPU_GRAPH_SUPPORT && defined(SYCL_EXT_ONEAPI_GRAPH) && SYCL_EXT_ONEAPI_GRAPH
         deviceInfos[i]->supportsSyclGraph = syclDevice.has(sycl::aspect::ext_oneapi_graph);
 #    if HAVE_SYCL_ASPECT_EXT_ONEAPI_LIMITED_GRAPH

src/gromacs/nbnxm/sycl/nbnxm_sycl.cpp
@@ -50,7 +50,7 @@
 namespace gmx
 {
 
-static void launchSciSortOnGpu(GpuPairlist* plist, const DeviceStream& deviceStream);
+static void launchSciSortOnGpu(GpuPairlist* plist, const int maxWorkGroupSize, const DeviceStream& deviceStream);
 
 void gpu_launch_kernel_pruneonly(NbnxmGpu* nb, const InteractionLocality iloc, const int numParts)
 {
@@ -95,7 +95,8 @@ void gpu_launch_kernel_pruneonly(NbnxmGpu* nb, const InteractionLocality iloc, c
     launchNbnxmKernelPruneOnly(nb, iloc, numParts, numSciInPartMax);
     if (plist->haveFreshList && nbnxmSortListsOnGpu())
     {
-        launchSciSortOnGpu(plist, *nb->deviceStreams[iloc]);
+        const auto& deviceInfo = nb->deviceContext_->deviceInfo();
+        launchSciSortOnGpu(plist, deviceInfo.maxWorkGroupSize, *nb->deviceStreams[iloc]);
     }
 
     if (plist->haveFreshList)
@@ -141,7 +142,8 @@ void gpu_launch_kernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, const In
     launchNbnxmKernel(nb, stepWork, iloc, doPrune);
     if (doPrune && nbnxmSortListsOnGpu())
     {
-        launchSciSortOnGpu(plist, *nb->deviceStreams[iloc]);
+        const auto& deviceInfo = nb->deviceContext_->deviceInfo();
+        launchSciSortOnGpu(plist, deviceInfo.maxWorkGroupSize, *nb->deviceStreams[iloc]);
     }
 }
 
@@ -173,6 +175,7 @@ static auto nbnxnKernelExclusivePrefixSum(const int* __restrict__ gm_input, int*
     };
 }
 //! SYCL kernel name
+template<int>
 class ExclusivePrefixSum;
 
 /*! \brief SYCL bucket sci sort kernel.
@@ -222,7 +225,7 @@ template<int workGroupSize>
 static void launchPrefixSumKernel(sycl::queue& q, GpuPairlistSorting* sorting)
 {
     gmx::syclSubmitWithoutEvent(q, [&](sycl::handler& cgh) {
-        cgh.parallel_for<ExclusivePrefixSum>(
+        cgh.parallel_for<ExclusivePrefixSum<workGroupSize>>(
                 sycl::nd_range<1>{ workGroupSize, workGroupSize },
                 nbnxnKernelExclusivePrefixSum<workGroupSize, c_sciHistogramSize>(
                         sorting->sciHistogram.get_pointer(), sorting->sciOffset.get_pointer()));
@@ -241,17 +244,29 @@ static void launchBucketSortKernel(sycl::queue& q, GpuPairlist* plist)
                                          plist->sorting.sciSorted.get_pointer()));
     });
 }
-static void launchSciSortOnGpu(GpuPairlist* plist, const DeviceStream& deviceStream)
+static void launchSciSortOnGpu(GpuPairlist* plist, const int maxWorkGroupSize, const DeviceStream& deviceStream)
 {
     sycl::queue q = deviceStream.stream();
 
     /* We are launching a single work-group, and it should be, in principle, as large as possible.
      * E.g., on PVC1100, wgSizeScan=1024 is ~1.2 times faster than wgSizeScan=512, and on
-     * MI250X ~1.7 times faster. But Intel iGPUs only handle 512.
-     * It should be autodetected, but for now we just use the universally supported value.
-     * The kernel takes 10-100µs and is run rarely, so it is of minor concern. */
-    constexpr int wgSizeScan = 512;
-    launchPrefixSumKernel<wgSizeScan>(q, &plist->sorting);
+     * MI250X ~1.7 times faster. But some Intel iGPUs only handle 256. */
+    if (maxWorkGroupSize >= 1024)
+    {
+        launchPrefixSumKernel<1024>(q, &plist->sorting);
+    }
+    else if (maxWorkGroupSize >= 512)
+    {
+        launchPrefixSumKernel<512>(q, &plist->sorting);
+    }
+    else if (maxWorkGroupSize >= 256)
+    {
+        launchPrefixSumKernel<256>(q, &plist->sorting);
+    }
+    else
+    {
+        launchPrefixSumKernel<128>(q, &plist->sorting);
+    }
     launchBucketSortKernel(q, plist);
 }
 