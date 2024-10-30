9b7f7ae660ba8ab56bea2a2c4eec31e41c3bf5fc
Berk Hess <hess@kth.se>
2024-10-30 19:04:39 +0000

    Add effect of perturbed masses to foreign Hamiltonian differences
    The effect of perturbed masses was missing from the foreign Hamiltonian
    differences. This meant that effects of perturbed masses were ignored
    by ``gmx bar``. Note that perturbed masses did contribute to dH/dlambda.
    
    Also the masses contributed to the kinetic energy component, whereas they
    should contribute to the mass component.
    
    Fixes #5195

--- src/gromacs/mdlib/enerdata_utils.cpp
+++ src/gromacs/mdlib/enerdata_utils.cpp
@@ -313,7 +313,7 @@ void accumulatePotentialEnergies(gmx_enerdata_t* enerd, gmx::ArrayRef<const real
 void ForeignLambdaTerms::accumulateKinetic(int listIndex, double energy, double dhdl)
 {
     energies_[listIndex] += energy;
-    dhdl_[listIndex][FreeEnergyPerturbationCouplingType::Temperature] += dhdl;
+    dhdl_[listIndex][FreeEnergyPerturbationCouplingType::Mass] += dhdl;
 }
 
 void ForeignLambdaTerms::finalizeKineticContributions(gmx::ArrayRef<const real> energyTerms,
@@ -326,10 +326,7 @@ void ForeignLambdaTerms::finalizeKineticContributions(gmx::ArrayRef<const real> 
 
     // Treat current lambda, the deltaH contribution is 0 as delta-lambda=0 for the current lambda
     accumulateKinetic(0, 0.0, energyTerms[F_DVDL_CONSTR]);
-    if (!fepvals.separate_dvdl[FreeEnergyPerturbationCouplingType::Mass])
-    {
-        accumulateKinetic(0, 0.0, energyTerms[F_DKDL]);
-    }
+    accumulateKinetic(0, 0.0, dhdlMass);
 
     for (int i = 0; i < fepvals.n_lambda; i++)
     {
@@ -346,11 +343,10 @@ void ForeignLambdaTerms::finalizeKineticContributions(gmx::ArrayRef<const real> 
         const double dlam = fepvals.all_lambda[lambdaIndex][i] - lambda[static_cast<int>(lambdaIndex)];
         accumulateKinetic(1 + i, dlam * energyTerms[F_DVDL_CONSTR], energyTerms[F_DVDL_CONSTR]);
 
-        if (!fepvals.separate_dvdl[FreeEnergyPerturbationCouplingType::Mass])
         {
             const double dlam = fepvals.all_lambda[FreeEnergyPerturbationCouplingType::Mass][i]
                                 - lambda[static_cast<int>(FreeEnergyPerturbationCouplingType::Mass)];
-            accumulateKinetic(1 + i, dlam * energyTerms[F_DKDL], energyTerms[F_DKDL]);
+            accumulateKinetic(1 + i, dlam * dhdlMass, dhdlMass);
         }
     }
 }
