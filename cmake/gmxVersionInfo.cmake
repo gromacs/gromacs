41ce8a9f6f7f67a47fee31e120e04f5ac507d49e
Lukas Müllender <lukas.muellender@gmail.com>
2024-11-18 16:04:30 +0000

    2025 beta release
    Updated MD5sum.

--- cmake/gmxVersionInfo.cmake
+++ cmake/gmxVersionInfo.cmake
@@ -245,7 +245,7 @@ set(REGRESSIONTEST_BRANCH "release-2025")
 # directory name within the regressiontests tarball, which affects the
 # md5sum of the tarball. The matching md5sum has to go here, and if it
 # isn't right the real release workflow will report a failure.
-set(REGRESSIONTEST_MD5SUM "d205b88092da42b04e112b636a7a7a77" CACHE INTERNAL "MD5 sum of the regressiontests tarball for this GROMACS version")
+set(REGRESSIONTEST_MD5SUM "0daa957bdbcd605de10e9aa357aa62f5" CACHE INTERNAL "MD5 sum of the regressiontests tarball for this GROMACS version")
 
 # If you are making a custom fork of GROMACS, please describe your
 # fork, perhaps with its version number, in the value of
