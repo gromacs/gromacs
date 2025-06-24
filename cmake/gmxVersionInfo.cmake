2287ecb08bad2f92d68463eee441077f47ce043b
Paul Bauer <paul.bauer.q@gmail.com>
2025-06-24 15:01:28 +0200

    GROMACS 2025 HIP fork

--- cmake/gmxVersionInfo.cmake
+++ cmake/gmxVersionInfo.cmake
@@ -264,7 +264,7 @@ set(REGRESSIONTEST_MD5SUM "280e3aaf5c7e65a200035ad7e45bd6e3" CACHE INTERNAL "MD5
 # If you are distributing a patch to GROMACS, then this change would
 # be great as part of your patch. Otherwise for personal use, you can
 # also just set a CMake cache variable.
-set(GMX_VERSION_STRING_OF_FORK "" CACHE INTERNAL
+set(GMX_VERSION_STRING_OF_FORK "2025-HIP-ENABLEMENT" CACHE INTERNAL
     "Version string for forks of GROMACS to set to describe themselves")
 mark_as_advanced(GMX_VERSION_STRING_OF_FORK)
 if (GMX_VERSION_STRING_OF_FORK)
