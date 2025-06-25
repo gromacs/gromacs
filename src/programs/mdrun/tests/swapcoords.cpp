3f9d1a894182a0affc47010cde23f54d9eefb170
Mark Abraham <mark.j.abraham@gmail.com>
2025-06-25 12:04:08 +0000

    Improve swap-coordinates testing
    Switch the test input to the regressiontests/swap_z input.
    
    Check also the swap.xvg output files so that we know that correct swaps happen.
    
    This prepares for modernization of swap-code memory handling and modularization
    of checkpoint serialization.

--- src/programs/mdrun/tests/swapcoords.cpp
+++ src/programs/mdrun/tests/swapcoords.cpp
@@ -46,8 +46,14 @@
 
 #include <gtest/gtest.h>
 
+#include "gromacs/utility/filestream.h"
+#include "gromacs/utility/path.h"
+
 #include "testutils/cmdlinetest.h"
+#include "testutils/refdata.h"
+#include "testutils/testasserts.h"
 #include "testutils/testfilemanager.h"
+#include "testutils/xvgtest.h"
 
 #include "moduletest.h"
 
@@ -61,13 +67,26 @@ class CompelTest : public MdrunTestFixture
 };
 
 /* This test ensures that the compel protocol can be run, that all of
- * the swapcoords parameters from the .mdp file are understood, and that
- * the swap state variables can be written to and read from checkpoint. */
+ * the swapcoords parameters from the .mdp file are understood, that
+ * the output .xvg file is correct, and that the swap state variables
+ * can be written to and read from checkpoint.
+ *
+ * The setup checks whether the swap-coordinates protocol performs
+ * ion/water exchanges in the z direction.  The system is a typical
+ * computational electrophysiology setup with two octanol membranes in
+ * the x-y plane with water and ions inbetween. At the start, there
+ * are 9 anions and 9 cations in one compartment (A) and 10 anions and
+ * 10 cations in the other (B). The swap parameters are set such that
+ * all ions should end up in compartment A in the swap attempted
+ * during the first step, and remain there.
+ *
+ * The former swap_z regressiontest did the first 2 steps of this test,
+ * ie. without the checkpoint restart. */
 TEST_F(CompelTest, SwapCanRun)
 {
-    runner_.useTopGroAndNdxFromDatabase("OctaneSandwich");
+    runner_.useTopGroAndNdxFromDatabase("OctaneSandwich-z");
     const std::string mdpContents = R"(
-        dt                       = 0.005
+        dt                       = 0.004
         nsteps                   = 2
         tcoupl                   = V-rescale
         tc-grps                  = System
@@ -82,20 +101,20 @@ TEST_F(CompelTest, SwapCanRun)
         massw_split0             = yes
         massw_split1             = no
         solvent_group            = SOL
-        cyl0_r                   = 1
-        cyl0_up                  = 0.5
-        cyl0_down                = 0.5
-        cyl1_r                   = 1
-        cyl1_up                  = 0.5
-        cyl1_down                = 0.5
-        coupl_steps              = 5
+        cyl0_r                   = 0.9
+        cyl0_up                  = 0.75
+        cyl0_down                = 0.75
+        cyl1_r                   = 0.9
+        cyl1_up                  = 0.75
+        cyl1_down                = 0.75
+        coupl_steps              = 1
         iontypes                 = 2
         iontype0-name            = NA+
-        iontype0-in-A            = 8
-        iontype0-in-B            = 11
+        iontype0-in-A            = 19
+        iontype0-in-B            = 0
         iontype1-name            = CL-
-        iontype1-in-A            = -1
-        iontype1-in-B            = -1
+        iontype1-in-A            = 19
+        iontype1-in-B            = 0
         threshold                = 1
      )";
 
@@ -111,11 +130,35 @@ TEST_F(CompelTest, SwapCanRun)
     // Do an initial mdrun that writes a checkpoint file
     ::gmx::test::CommandLine firstCaller(swapCaller);
     ASSERT_EQ(0, runner_.callMdrun(firstCaller));
+
+    // Test the swap output xvg file
+    const auto           xvgTolerance = relativeToleranceAsFloatingPoint(5.0, 1e-4);
+    TestReferenceData    data;
+    TestReferenceChecker checker = data.rootChecker();
+    if (File::exists(runner_.swapFileName_, File::returnFalseOnError))
+    {
+        TestReferenceChecker fileChecker(checker.checkCompound("File", "swap output after 2 steps"));
+        TextInputFile    swapXvgFileStream(runner_.swapFileName_);
+        XvgMatchSettings matchSettings;
+        matchSettings.tolerance = xvgTolerance;
+        checkXvgFile(&swapXvgFileStream, &fileChecker, matchSettings);
+    }
+
     // Continue mdrun from that checkpoint file
     ::gmx::test::CommandLine secondCaller(swapCaller);
     secondCaller.addOption("-cpi", runner_.cptOutputFileName_);
     runner_.nsteps_ = 2;
     ASSERT_EQ(0, runner_.callMdrun(secondCaller));
+
+    // Test the updated swap output file
+    if (File::exists(runner_.swapFileName_, File::returnFalseOnError))
+    {
+        TestReferenceChecker fileChecker(checker.checkCompound("File", "swap output after 4 steps"));
+        TextInputFile    swapXvgFileStream(runner_.swapFileName_);
+        XvgMatchSettings matchSettings;
+        matchSettings.tolerance = xvgTolerance;
+        checkXvgFile(&swapXvgFileStream, &fileChecker, matchSettings);
+    }
 }
 
 
