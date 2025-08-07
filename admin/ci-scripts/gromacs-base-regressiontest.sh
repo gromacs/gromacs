180693ff5ecedddc24e2007761670ead525dc491
Andrey Alekseenko <al42and@gmail.com>
2025-08-08 00:43:04 +0200

    CI: Rearrage scripts
    - Organize regstest job the same way unit tests are done, with a helper script.
    - Gather all the workarounds in one place.
    - Remove outdated handling for <sm_70 NVIDIA devices.

--- admin/ci-scripts/gromacs-base-regressiontest.sh
+++ admin/ci-scripts/gromacs-base-regressiontest.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/env bash
 set -e
-echo "Running on host:" $KUBERNETES_HOSTNAME
-cd $BUILD_DIR
+cd regressiontests
 # Needed to run MPI enabled code in the docker images, until we set up different users
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 export ASAN_OPTIONS="check_initialization_order=1:detect_invalid_pointer_pairs=1:strict_init_order=true:strict_string_checks=true:detect_stack_use_after_return=true"
-export UBSAN_OPTIONS=halt_on_error=1:print_stacktrace=1:suppressions=$CI_PROJECT_DIR/admin/ubsan-suppressions.txt
+export LSAN_OPTIONS="suppressions=$CI_PROJECT_DIR/admin/lsan-suppressions.txt:print_suppressions=0"
 
-# If $GMX_TEST_REQUIRED_NUMBER_OF_DEVICES is not set and we have GPUs, set it
-if [[ -z "$GMX_TEST_REQUIRED_NUMBER_OF_DEVICES" ]] && [[ -n "$GPU_VENDOR" ]] ; then
-    echo "export GMX_TEST_REQUIRED_NUMBER_OF_DEVICES=\"$GPU_COUNT\"";
-    export GMX_TEST_REQUIRED_NUMBER_OF_DEVICES="$GPU_COUNT";
-fi
 if grep -qF 'NVIDIA' <<< "$GPU_VENDOR"; then
     nvidia-smi -L && nvidia-smi || true;
     if [[ "$GMX_ENABLE_NVSHMEM" != "" ]] && [[ "$GPU_COUNT" -eq "2" ]]; then
-        # In CI with dual GPUs NVSHMEM cannot support more than 2 MPI processes/GPU
-        # which happens in multi sim tests so we disable them
-        echo "Disabling MdrunMultiSim tests as with GMX_ENABLE_NVSHMEM it does not work on dual GPU setup without MPS"
-        EXTRA_FLAGS="--exclude-regex MdrunMultiSim "
         # if NVSHMEM uses NCCL it requires more than 4 GB of GPU RAM as min memory,
         # in CI we've GPUs like T400 which have 4 GB RAM so we disable NVSHMEM's
         # NCCL usage, as NCCL isn't required in our NVSHMEM usage.
@@ -39,8 +29,6 @@ if grep -qF 'NVIDIA' <<< "$GPU_VENDOR"; then
         # set nvshmem cumem allocation granularity to 2MB, default is 512MB.
         export NVSHMEM_CUMEM_GRANULARITY=2097152
     fi
-    # Speed up device re-initialization, especially when running multiple tests in parallel
-    export CUDA_DEVICE_MAX_CONNECTIONS=2  # default is 8
 fi
 if grep -qF 'AMD' <<< "$GPU_VENDOR"; then
     clinfo -l || true;
@@ -50,38 +38,10 @@ if grep -qF 'AMD' <<< "$GPU_VENDOR"; then
         export UCX_TLS="sm,self,rocm_copy"
     fi
     export HSA_ENABLE_SDMA=0  # Work around CI issues, Issue #5341
-    export GPU_MAX_HW_QUEUES=2  # Prevent "amdgpu: Runlist is getting oversubscribed", Issue #5341
 fi
 if grep -qF 'INTEL' <<< "$GPU_VENDOR"; then
     sycl-ls || true;
     export SYCL_CACHE_PERSISTENT=1  # Issue #4218
 fi
-LABEL_REGEX=
-if [[ -n "$GMX_TEST_LABELS" ]] ; then
-    LABEL_REGEX="--label-regex $GMX_TEST_LABELS"
-fi
 
-TESTS_REGEX=
-# Check GMX_TESTS_TO_RUN_REGEX to see whether the caller wants to run
-# all tests, or just a subset.
-if [[ -n "$GMX_TESTS_TO_RUN_REGEX" ]] ; then
-    TESTS_REGEX="--tests-regex $GMX_TESTS_TO_RUN_REGEX"
-fi
-
-ctest -D $CTEST_RUN_MODE $LABEL_REGEX $TESTS_REGEX $EXTRA_FLAGS --parallel $KUBERNETES_CPU_LIMIT --output-on-failure | tee ctestLog.log || true
-
-EXITCODE=$?
-
-if [[ "$CTEST_RUN_MODE" == "ExperimentalMemCheck" ]] ; then
-    TEST_XML_OUTPUT="DynamicAnalysis.xml"
-else
-    TEST_XML_OUTPUT="Test.xml"
-fi
-
-awk '/The following tests FAILED/,/^Errors while running CTest|^$/' ctestLog.log | tee ctestErrors.log
-xsltproc $CI_PROJECT_DIR/scripts/CTest2JUnit.xsl Testing/`head -n 1 < Testing/TAG`/$TEST_XML_OUTPUT > JUnitTestResults.xml
-if [ -s ctestErrors.log ] || [ $EXITCODE != 0 ] ; then
-    echo "Error during running ctest";
-    exit 1;
-fi
-cd .
+perl gmxtest.pl $REGRESSIONTEST_PARALLEL $REGRESSIONTEST_TOTAL_RANK_NUMBER -ntomp $REGRESSIONTEST_OMP_RANK_NUMBER -npme $REGRESSIONTEST_PME_RANK_NUMBER $REGRESSIONTEST_DOUBLE $REGRESSIONTEST_MPI_RUN_COMMAND -xml all
