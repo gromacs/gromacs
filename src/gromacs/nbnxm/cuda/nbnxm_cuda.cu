fef83d793c867a42fb9908909c7a321fba6e1c59
Andrey Alekseenko <al42and@gmail.com>
2025-08-12 21:26:55 +0200

    Fix shmem size calculation for CUDA Prune Only kernel
    - On devices with CC < 7.0, increase by 4 bytes (573fe18d)
    - On devices with CC >= 7.0, decrease by (128 - 4) bytes
    
    Fixes #5385

--- src/gromacs/nbnxm/cuda/nbnxm_cuda.cu
+++ src/gromacs/nbnxm/cuda/nbnxm_cuda.cu
@@ -591,14 +591,21 @@ void gpu_launch_kernel(NbnxmGpu* nb, const gmx::StepWorkload& stepWork, const In
 }
 
 /*! Calculates the amount of shared memory required by the CUDA kernel in use. */
-static inline int calc_shmem_required_prune(const int num_threads_z)
+static inline int calc_shmem_required_prune(const int num_threads_z, const DeviceInformation* deviceInfo)
 {
-    int shmem;
+    const int  archMajor = deviceInfo->prop.major;
+    const bool preloadCj = archMajor < 7;
+    int        shmem;
 
     /* i-atom x in shared memory */
     shmem = c_nbnxnGpuNumClusterPerSupercluster * c_clSize * sizeof(float4);
-    /* cj in shared memory, for each warp separately */
-    shmem += num_threads_z * c_nbnxnGpuClusterpairSplit * c_nbnxnGpuJgroupSize * sizeof(int);
+    if (preloadCj)
+    {
+        /* cj in shared memory, for each warp separately */
+        shmem += num_threads_z * c_nbnxnGpuClusterpairSplit * c_nbnxnGpuJgroupSize * sizeof(int);
+    }
+    /* add 1 int for pruned pair count */
+    shmem += sizeof(int);
 
     return shmem;
 }
@@ -670,11 +677,12 @@ void gpu_launch_kernel_pruneonly(NbnxmGpu* nb, const InteractionLocality iloc, c
     int nblock        = calc_nb_kernel_nblock(numSciInPartMax, &nb->deviceContext_->deviceInfo());
 
     KernelLaunchConfig config;
-    config.blockSize[0]     = c_clSize;
-    config.blockSize[1]     = c_clSize;
-    config.blockSize[2]     = num_threads_z;
-    config.gridSize[0]      = nblock;
-    config.sharedMemorySize = calc_shmem_required_prune(num_threads_z);
+    config.blockSize[0] = c_clSize;
+    config.blockSize[1] = c_clSize;
+    config.blockSize[2] = num_threads_z;
+    config.gridSize[0]  = nblock;
+    config.sharedMemorySize =
+            calc_shmem_required_prune(num_threads_z, &nb->deviceContext_->deviceInfo());
 
     if (debug)
     {
