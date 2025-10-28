01c2678ea9535feef5bc64e1222f6f4b1573124f
Andrey Alekseenko <al42and@gmail.com>
2025-10-28 16:19:23 +0100

    Fix one leftover Wshadow warning

--- src/gromacs/gpu_utils/hip_sycl_kernel_utils.h
+++ src/gromacs/gpu_utils/hip_sycl_kernel_utils.h
@@ -287,14 +287,14 @@ template<typename ValueType>
 class AmdFastBuffer
 {
 private:
-    const ValueType* buffer;
+    const ValueType* buffer_;
 
 public:
-    GMX_DEVICE_ATTRIBUTE AmdFastBuffer(const ValueType* buffer) : buffer(buffer) {}
+    GMX_DEVICE_ATTRIBUTE AmdFastBuffer(const ValueType* buffer) : buffer_(buffer) {}
     template<typename IndexType, std::enable_if_t<std::is_integral<IndexType>::value, bool> = true>
     inline GMX_DEVICE_ATTRIBUTE GMX_ALWAYS_INLINE_ATTRIBUTE const ValueType& operator[](IndexType idx) const
     {
-        return *indexedAddress(buffer, idx);
+        return *indexedAddress(buffer_, idx);
     }
 };
 
