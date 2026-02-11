0ccea7f57d60e9e15336f2967336422b4adefa82
Petter Johansson <pettjoha@kth.se>
2026-02-11 11:36:35 +0100

    Fix memory leaks in H5MD tests
    Leaks reported by ASAN.

--- src/gromacs/fileio/h5md/tests/h5md.cpp
+++ src/gromacs/fileio/h5md/tests/h5md.cpp
@@ -1031,6 +1031,7 @@ TEST_F(H5mdReadNextFrame, Works)
     EXPECT_FALSE(file().readNextFrame(frame))
             << "Must return false when no more frames exist to read";
     done_frame(frame);
+    sfree(frame);
 }
 
 TEST_F(H5mdReadNextFrame, ReturnsFalseBeforeDataIsWritten)
@@ -1051,6 +1052,7 @@ TEST_F(H5mdReadNextFrame, ReturnsFalseBeforeDataIsWritten)
     file().writeNextFrame(valuesToWrite, valuesToWrite, valuesToWrite, c_unusedBox, 0, 0.0);
     EXPECT_TRUE(file().readNextFrame(frame));
     done_frame(frame);
+    sfree(frame);
 }
 
 TEST_F(H5mdReadNextFrame, WorksIfNoDataSetsExists)
@@ -1074,6 +1076,7 @@ TEST_F(H5mdReadNextFrame, WorksIfNoDataSetsExists)
     EXPECT_FALSE(frame->bStep);
     EXPECT_FALSE(frame->bTime);
     done_frame(frame);
+    sfree(frame);
 }
 
 TEST_F(H5mdReadNextFrame, WorksIfOnlyPositionDataExists)
@@ -1105,6 +1108,7 @@ TEST_F(H5mdReadNextFrame, WorksIfOnlyPositionDataExists)
                         ::testing::FloatEq(),
                         constArrayRefFromArray(reinterpret_cast<real*>(boxToWrite), DIM * DIM)));
     done_frame(frame);
+    sfree(frame);
 }
 
 TEST_F(H5mdReadNextFrame, WorksIfOnlyVelocityDataExists)
@@ -1131,6 +1135,7 @@ TEST_F(H5mdReadNextFrame, WorksIfOnlyVelocityDataExists)
     EXPECT_FALSE(frame->bF);
     EXPECT_FALSE(frame->bBox);
     done_frame(frame);
+    sfree(frame);
 }
 
 TEST_F(H5mdReadNextFrame, WorksIfOnlyForceDataExists)
@@ -1158,6 +1163,7 @@ TEST_F(H5mdReadNextFrame, WorksIfOnlyForceDataExists)
     EXPECT_THAT(asRVecArray(frame->f, frame->natoms), ::testing::Pointwise(::testing::Eq(), valuesToWrite));
     EXPECT_FALSE(frame->bBox);
     done_frame(frame);
+    sfree(frame);
 }
 
 TEST_F(H5mdReadNextFrame, DataSetsWithDifferentStepFrequenciesAreReadInOrder)
@@ -1273,6 +1279,7 @@ TEST_F(H5mdReadNextFrame, DataSetsWithDifferentStepFrequenciesAreReadInOrder)
         EXPECT_FLOAT_EQ(frame->time, static_cast<double>(step5));
     }
     done_frame(frame);
+    sfree(frame);
 }
 
 TEST_F(H5mdReadNextFrame, MissingTimeDataSetsAreHandled)
@@ -1368,6 +1375,7 @@ TEST_F(H5mdReadNextFrame, MissingTimeDataSetsAreHandled)
         EXPECT_FLOAT_EQ(frame->time, time6);
     }
     done_frame(frame);
+    sfree(frame);
 }
 
 TEST_F(H5mdReadNextFrame, NonTrajectoryFrameBoolsInTrxFrameAreFalse)
@@ -1402,6 +1410,7 @@ TEST_F(H5mdReadNextFrame, NonTrajectoryFrameBoolsInTrxFrameAreFalse)
 #endif
 
     done_frame(frame);
+    sfree(frame);
 }
 
 } // namespace
