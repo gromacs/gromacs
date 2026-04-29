gromacs/gromacs

commit cfb34361738c3b5a963d8697d335381560af04b7

Move npmenodes from t_commrec to gmx_domdec_t

src/gromacs/timing/wallcycle.cpp
@@ -47,6 +47,7 @@
 #include <string>
 #include <vector>
 
+#include "gromacs/domdec/domdec_struct.h"
 #include "gromacs/math/functions.h"
 #include "gromacs/mdtypes/commrec.h"
 #include "gromacs/timing/cyclecounter.h"
@@ -397,7 +398,7 @@ WallcycleCounts wallcycle_sum(const t_commrec* cr, gmx_wallcycle* wc)
 
     subtract_cycles(wcc, WallCycleCounter::PmeFft, WallCycleCounter::PmeFftComm);
 
-    if (cr->npmenodes == 0)
+    if (cr->dd && cr->dd->numPmeOnlyRanks == 0)
     {
         /* All nodes do PME (or no PME at all) */
         subtract_cycles(wcc, WallCycleCounter::Force, WallCycleCounter::PmeMesh);

src/gromacs/domdec/domdec.cpp
@@ -2862,8 +2862,6 @@ DomainDecompositionBuilder::Impl::Impl(const MDLogger&           mdlog,
                      gridSetupCellsizeLimit,
                      ddbox_);
 
-    cr_->npmenodes = ddGridSetup_.numPmeOnlyRanks;
-
     ddRankSetup_ = getDDRankSetup(
             mdlog_, cr_->mpiDefaultCommunicator.size(), options_.rankOrder, ddGridSetup_, ir_);
 
@@ -2888,6 +2886,8 @@ DomainDecompositionBuilder::Impl::build(LocalAtomSetManager*       atomSets,
 
     dd->comm->ddRankSetup        = ddRankSetup_;
     dd->comm->cartesianRankSetup = cartSetup_;
+
+    dd->numPmeOnlyRanks = ddGridSetup_.numPmeOnlyRanks;
 
     dd->hasPPDuty  = hasPPDuty_;
     dd->hasPmeDuty = hasPmeDuty_;

src/gromacs/domdec/domdec_struct.h
@@ -143,6 +143,7 @@ struct gmx_domdec_t
     gmx::IVec main_ci  = { 0, 0, 0 };
     int       mainrank = 0;
     /* Communication with the PME only nodes */
+    int                   numPmeOnlyRanks      = 0;
     int                   pme_nodeid           = 0;
     gmx_bool              pme_receive_vir_ener = false;
     gmx_pme_comm_n_box_t* cnb                  = nullptr;

src/gromacs/fileio/checkpoint.cpp
@@ -49,6 +49,7 @@
 #include <memory>
 #include <type_traits>
 
+#include "gromacs/domdec/domdec_struct.h"
 #include "gromacs/fileio/filetypes.h"
 #include "gromacs/fileio/gmxfio.h"
 #include "gromacs/fileio/gmxfio_xdr.h"
@@ -2592,12 +2593,13 @@ static void check_match(FILE*                           fplog,
     if (cr->mpiDefaultCommunicator.size() > 1 && reproducibilityRequested)
     {
         // TODO: These checks are incorrect (see redmine #3309)
-        check_int(fplog, "#PME-ranks", cr->npmenodes, headerContents.npme, &mm);
+        const int numPmeOnlyRanks = (cr->dd ? cr->dd->numPmeOnlyRanks : 0);
+        check_int(fplog, "#PME-ranks", numPmeOnlyRanks, headerContents.npme, &mm);
 
         int npp = cr->mpiDefaultCommunicator.size();
-        if (cr->npmenodes >= 0)
+        if (numPmeOnlyRanks >= 0)
         {
-            npp -= cr->npmenodes;
+            npp -= numPmeOnlyRanks;
         }
         int npp_f = headerContents.nnodes;
         if (headerContents.npme >= 0)

src/gromacs/mdlib/mdoutf.cpp
@@ -319,9 +319,9 @@ static void write_checkpoint(const char*                     fn,
     char      buf[1024], suffix[5 + STEPSTRSIZE], sbuf[STEPSTRSIZE];
     t_fileio* ret;
 
-    if (haveDDAtomOrdering(*cr))
+    if (cr->dd)
     {
-        npmenodes = cr->npmenodes;
+        npmenodes = cr->dd->numPmeOnlyRanks;
     }
     else
     {

src/gromacs/mdrun/minimize.cpp
@@ -3361,8 +3361,7 @@ void LegacySimulator::do_nm()
 
     /* Make evaluate_energy do a single node force calculation */
     t_commrec crSingleRank(gmx::MpiComm(gmx::MpiComm::SingleRank{}));
-    crSingleRank.npmenodes = cr_->npmenodes;
-    crSingleRank.dd        = cr_->dd;
+    crSingleRank.dd = cr_->dd;
     EnergyEvaluator energyEvaluator{ fpLog_,
                                      mdLog_,
                                      &crSingleRank,

src/gromacs/mdrun/runner.cpp
@@ -741,7 +741,7 @@ static void finish_run(FILE*                     fplog,
         wallcycle_print(fplog,
                         mdlog,
                         cr->commMySim.size(),
-                        cr->npmenodes,
+                        cr->dd ? cr->dd->numPmeOnlyRanks : 0,
                         nthreads_pp,
                         nthreads_pme,
                         elapsed_time_over_all_ranks,
@@ -1481,11 +1481,9 @@ int Mdrunner::mdrunner()
     }
     else
     {
-        /* PME, if used, is done on all nodes with 1D decomposition */
+        // DD is inactive, so all ranks have all PP and PME duties
         cr->commMyGroup = MpiComm(cr->mpiDefaultCommunicator);
         cr->commMySim   = MpiComm(cr->mpiDefaultCommunicator);
-
-        cr->npmenodes = 0;
 
         if (inputrec->pbcType == PbcType::Screw)
         {
@@ -2001,7 +1999,7 @@ int Mdrunner::mdrunner()
                 nTypePerturbed = mdAtoms->mdatoms()->nTypePerturbed;
             }
         }
-        if (cr->npmenodes > 0)
+        if (cr->dd && cr->dd->numPmeOnlyRanks > 0)
         {
             /* The PME only nodes need to know nChargePerturbed(FEP on Q) and nTypePerturbed(FEP on LJ)*/
             gmx_bcast(sizeof(nChargePerturbed), &nChargePerturbed, cr->commMySim.comm());

src/gromacs/mdtypes/commrec.h
@@ -73,8 +73,6 @@ struct t_commrec
      * All communication within some simulation should happen
      * in mpi_comm_mysim, or its subset mpi_comm_mygroup.
      */
-    //! The number of separate PME ranks, 0 when no separate PME ranks are used
-    int npmenodes = 0;
 
     /* MPI communicators within a single simulation
      * Note: other parts of the code may further subset these communicators.

commit 010dfd1c9fba6d2b8e2daa906d2c3a8a62078f57

Fix wallcycle table in single-rank runs
Fixes conditional bug added in cfb3436173 which broke the wallcycle
accounting without domain decomposition.

Fixes #5614

src/gromacs/timing/wallcycle.cpp
@@ -398,14 +398,14 @@ WallcycleCounts wallcycle_sum(const t_commrec* cr, gmx_wallcycle* wc)
 
     subtract_cycles(wcc, WallCycleCounter::PmeFft, WallCycleCounter::PmeFftComm);
 
-    if (cr->dd && cr->dd->numPmeOnlyRanks == 0)
+    if (cr->dd == nullptr || cr->dd->numPmeOnlyRanks == 0)
     {
         /* All nodes do PME (or no PME at all) */
         subtract_cycles(wcc, WallCycleCounter::Force, WallCycleCounter::PmeMesh);
     }
     else
     {
-        /* The are PME-only nodes */
+        /* There are PME-only nodes */
         if (wcc[WallCycleCounter::PmeMesh].n > 0)
         {
             GMX_ASSERT(wcc[WallCycleCounter::PmeGpuMesh].c == 0,